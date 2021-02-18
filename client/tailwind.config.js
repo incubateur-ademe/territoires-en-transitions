0234ee45b3b639cb13831c1ee63865d4de37b407
Fanny Cheung <f.cheung.hk@gmail.com>
2021-02-18 10:33:36 +0000

    Add header navigation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
       './dist/**/*.html',
     ],
   },
+  variants: {
+    extend: {
+      boxShadow: ['active'],
+      backgroundColor: ['active'],
+      textColor: ['active'],
+    }
+  },
   darkMode: false, // or 'media' or 'class'
 }
