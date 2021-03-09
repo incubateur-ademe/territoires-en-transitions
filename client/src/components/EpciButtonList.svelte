adc39e6f8ed1abcfe1dee08b91f51b15d6d5b1b9
Florian dErfurth <florian@derfurth.com>
2021-03-09 10:49:48 +0000

    better index page

--- client/src/components/EpciButtonList.svelte
+++ client/src/components/EpciButtonList.svelte
@@ -2,13 +2,13 @@
     import {epcis_default} from "../../vendors/epcis_default";
     import Button from "./shared/Button.svelte";
 </script>
-<section class="p-10">
-    <ul class="pb-10 flex justify-evenly max-w-2xl m-auto">
+<section class="">
+    <ul class="flex flex-wrap gap-4 ">
         {#each Object.entries(epcis_default) as [id, name]}
             <Button asLink
                     href="mesures.html?epci_id={id}"
                     label="{name}"
-                    classNames="px-6 py-3 border-green-700 border-2 hover:bg-green-100 active:bg-green-600"
+                    classNames="text-center border-green-700 border-2 hover:bg-green-100 active:bg-green-600"
             />
         {/each}
     </ul>
