39920e18d38f98155b78e44cba867c6dbef387ec
derfurth <florian@derfurth.com>
2021-03-26 09:53:47 +0100

    - fix cors ?

--- api/app.py
+++ api/app.py
@@ -16,20 +16,18 @@ app.include_router(action_status.router)
 app.include_router(mesure_custom.router)
 app.include_router(indicateur_value.router)
 
-# FIXME not safe
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 register_tortoise(
     app,
     db_url=tortoise_config.db_url,
     generate_schemas=tortoise_config.generate_schemas,
     modules=tortoise_config.modules,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    # FIXME not safe
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
