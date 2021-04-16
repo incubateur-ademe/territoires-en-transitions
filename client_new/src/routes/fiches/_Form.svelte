d0d440e95f633f91063c4b5cf0bff0d1cd91bc89
Florian d'Erfurth <florian@derfurth.com>
2021-04-16 17:13:28 +0200

    Shows a rather mysterious error message.

--- client_new/src/routes/fiches/_Form.svelte
+++ client_new/src/routes/fiches/_Form.svelte
@@ -22,8 +22,10 @@
         if (!data.custom_id) return;
         data.budget = Number.parseFloat(budget.toString()) || 0
         const fiche = new FicheActionStorable(data)
-        await ficheActionStore.store(fiche)
-        window.location.href = `/fiches/?epci_id=${data.epci_id}`
+        const saved = await ficheActionStore.store(fiche)
+
+        if (saved.uid) window.location.href = `/fiches/?epci_id=${data.epci_id}`
+        else window.alert('Une erreur est survenue lors de la sauvegarde de la fiche action.')
     }
 
     let flatActions: ActionReferentiel[]
@@ -51,12 +53,12 @@
 
     <div class="flex flex-col w-full md:w-3/4 pb-10">
 
-        <label for="fiche_create_custom_id" class="text-xl">Identifiant</label>
-        <input id="fiche_create_custom_id"
-               bind:value={data.custom_id}
-               use:validate={data.custom_id}
+        <label class="text-xl" for="fiche_create_custom_id">Identifiant</label>
+        <input bind:value={data.custom_id}
+               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+               id="fiche_create_custom_id"
                maxlength="36"
-               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+               use:validate={data.custom_id}>
         {#if $validity.dirty && !$validity.valid}
             <span class="validation-hint">
                {$validity.message}
@@ -65,19 +67,19 @@
         <div class="p-5"></div>
 
 
-        <label for="fiche_create_titre" class="text-xl">Titre</label>
-        <input id="fiche_create_titre"
-               bind:value={data.titre}
-               maxlength="100"
-               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+        <label class="text-xl" for="fiche_create_titre">Titre</label>
+        <input bind:value={data.titre}
+               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+               id="fiche_create_titre"
+               maxlength="100">
         <div class="p-5"></div>
 
-        <CategoriePicker ficheActionUid={data.uid} />
+        <CategoriePicker ficheActionUid={data.uid}/>
 
-        <label for="fiche_create_description" class="text-xl">Description</label>
-        <textarea id="fiche_create_description"
-                  bind:value={data.description}
-                  class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"></textarea>
+        <label class="text-xl" for="fiche_create_description">Description</label>
+        <textarea bind:value={data.description}
+                  class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+                  id="fiche_create_description"></textarea>
         <div class="p-5"></div>
 
 
@@ -88,48 +90,48 @@
         <div class="p-5"></div>
 
 
-        <label for="fiche_create_porteur" class="text-xl">Porteur</label>
-        <input id="fiche_create_porteur"
-               bind:value={data.porteur}
-               maxlength="100"
-               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+        <label class="text-xl" for="fiche_create_porteur">Porteur</label>
+        <input bind:value={data.porteur}
+               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+               id="fiche_create_porteur"
+               maxlength="100">
         <div class="p-5"></div>
 
-        <label for="fiche_create_budget" class="text-xl">Budget global</label>
-        <input id="fiche_create_budget"
-               type="number"
-               bind:value={budget}
-               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+        <label class="text-xl" for="fiche_create_budget">Budget global</label>
+        <input bind:value={budget}
+               class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+               id="fiche_create_budget"
+               type="number">
         <div class="p-5"></div>
 
-        <label for="fiche_create_commentaire" class="text-xl">Commentaire</label>
-        <textarea id="fiche_create_commentaire"
-                  bind:value={data.commentaire}
-                  class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"></textarea>
+        <label class="text-xl" for="fiche_create_commentaire">Commentaire</label>
+        <textarea bind:value={data.commentaire}
+                  class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+                  id="fiche_create_commentaire"></textarea>
         <div class="p-5"></div>
 
         <span class="text-xl">Calendrier</span>
         <div class="flex">
             <div class="flex-1 flex flex-col pr-1">
                 <label for="fiche_create_debut">date de début</label>
-                <input id="fiche_create_debut"
-                       type="date"
-                       bind:value={data.date_debut}
-                       class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+                <input bind:value={data.date_debut}
+                       class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+                       id="fiche_create_debut"
+                       type="date">
                 <div class="p-5"></div>
             </div>
 
             <div class="flex-1 flex flex-col pl-1">
                 <label for="fiche_create_fin">date de fin</label>
-                <input id="fiche_create_fin"
-                       type="date"
-                       bind:value={data.date_fin}
-                       class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100">
+                <input bind:value={data.date_fin}
+                       class="border border-gray-300 p-2 my-2 focus:outline-none focus:ring-2 ring-green-100"
+                       id="fiche_create_fin"
+                       type="date">
                 <div class="p-5"></div>
             </div>
         </div>
 
-        <label for="fiche_create_commentaire" class="text-xl">Actions du référentiel</label>
+        <label class="text-xl" for="fiche_create_commentaire">Actions du référentiel</label>
         {#if flatActions}
             <MultiSelect id='lang' bind:value={data.referentiel_action_ids}>
                 {#each flatActions as action}
@@ -139,9 +141,9 @@
         {/if}
 
         <div class="p-10"></div>
-        <Button full
+        <Button classNames="md:w-1/3 self-end"
+                full
                 label="Valider"
-                on:click={handleSave}
-                classNames="md:w-1/3 self-end"/>
+                on:click={handleSave}/>
     </div>
 </section>
