c4dd5e0aecaa47992d923de823bed296cc711e84
Florian d'Erfurth <florian@derfurth.com>
2021-04-19 17:29:38 +0200

    - force sapper to generate export html files

--- client_new/src/routes/index.svelte
+++ client_new/src/routes/index.svelte
@@ -1,20 +1,26 @@
+<script>
+    import NavDev from "../components/shared/NavDev.svelte";
+</script>
 <style>
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    h1 {
+        font-size: 2.8em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
 </style>
 
 <svelte:head>
-	<title>page temporaire</title>
+    <title>page temporaire</title>
 </svelte:head>
 
 <h1>Déploiement en cours</h1>
+<div class="hidden">
+    <NavDev/>
+</div>
