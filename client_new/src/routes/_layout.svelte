c14005ec540d0b5f724232ca3d1ada0881a31e6e
Fanny Cheung <dev@ynote.hk>
2021-04-19 19:19:22 +0200

    Use Svelte preprocessor to process TailwindCSS

--- client_new/src/routes/_layout.svelte
+++ client_new/src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Nav from '../components/shared/Nav.svelte';
+	import Tailwind from '../components/Tailwind.svelte'
+	import Nav from '../components/shared/Nav.svelte'
 
 	export let segment: string;
 </script>
@@ -15,6 +16,7 @@
 	}
 </style>
 
+<Tailwind />
 <Nav {segment}/>
 
 <main>
