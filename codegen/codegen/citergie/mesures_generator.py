ff583da585485da5cb3c91e8141d79cf3860fdfd
Florian d'Erfurth <florian@derfurth.com>
2021-04-23 18:33:04 +0200

    propagate action thematiques for an easier search

--- codegen/codegen/citergie/mesures_generator.py
+++ codegen/codegen/citergie/mesures_generator.py
@@ -105,11 +105,27 @@ def relativize_ids(actions: List[dict], referentiel_slug: str) -> None:
         if 'actions' in action.keys():
             relativize_ids(action['actions'], referentiel_slug)
 
+
 def clean_thematiques(actions: List[dict]) -> List:
     cleaned_actions = actions.copy()
     for action in cleaned_actions:
         theme = action['climat_pratic_id'].strip() if 'climat_pratic_id' in action.keys() else ''
         action['thematique_id'] = theme if theme else 'pas_de_theme'
+    return cleaned_actions
+
+
+def propagate_thematiques(actions: List[dict], thematique_id: str = '') -> List:
+    """Propagatee thematiques ids to children"""
+    cleaned_actions = actions.copy()
+
+    for action in cleaned_actions:
+        if thematique_id:
+            if 'thematique_id' in action.keys() and action['thematique_id']:
+                continue
+            action['thematique_id'] = thematique_id
+        if action['actions']:
+            action['actions'] = propagate_thematiques(action['actions'], action['thematique_id'])
+
     return cleaned_actions
 
 
--- codegen/codegen/cli_generate.py
+++ codegen/codegen/cli_generate.py
@@ -7,7 +7,7 @@ import codegen.paths as paths
 from codegen.citergie.indicators_generator import build_indicators, render_indicators_as_html
 from codegen.citergie.mesures_generator import render_mesure_as_json, render_mesure_as_html, build_mesure, \
     render_mesures_summary_as_html, filter_indicateurs_by_mesure_id, build_action, render_actions_as_typescript, \
-    relativize_ids, clean_thematiques
+    relativize_ids, clean_thematiques, propagate_thematiques
 from codegen.climat_pratic.thematiques_generator import build_thematiques, render_thematiques_as_typescript
 from codegen.codegen.python import render_markdown_as_python
 from codegen.codegen.typescript import render_markdown_as_typescript
@@ -102,11 +102,12 @@ def actions(
 
     relativize_ids(actions_economie_circulaire, 'economie_circulaire')
 
-    actions_citergie = clean_thematiques(actions_citergie)
-    actions_economie_circulaire = clean_thematiques(actions_economie_circulaire)
+    all_actions = actions_citergie + actions_economie_circulaire
+    all_actions = clean_thematiques(all_actions)
+    all_actions = propagate_thematiques(all_actions)
 
     if output_typescript:
-        typescript = render_actions_as_typescript(actions_citergie + actions_economie_circulaire)
+        typescript = render_actions_as_typescript(all_actions)
         filename = os.path.join(client_output_dir, 'actions_referentiels.ts')
         write(filename, typescript)
 
