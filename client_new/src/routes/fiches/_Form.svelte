e26131cd8083333a9addf117cac9aa6331b67b29
Florian d'Erfurth <florian@derfurth.com>
2021-05-11 10:12:49 +0200

    fix title size

--- client_new/src/routes/fiches/_Form.svelte
+++ client_new/src/routes/fiches/_Form.svelte
@@ -223,7 +223,7 @@
         <div class="p-5"></div>
         <div class="my-2 p-2">
             <div class="flex flex-row w-full items-center">
-                <h3 class="text-2xl">Indicateurs personalisés</h3>
+                <h3 class="text-xl">Indicateurs personalisés</h3>
                 <div class="flex flex-grow"></div>
                 <Button colorVariant={showIndicateurCreation ? 'ash' : 'nettle'}
                         on:click={() => showIndicateurCreation = true }
