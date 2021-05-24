c4c07a8ff89adb830d11f57928d4420b1862376b
Florian d'Erfurth <florian@derfurth.com>
2021-05-24 13:23:41 +0200

    points from reférentiel v3

--- codegen/codegen/economie_circulaire/referentiel_extractor.py
+++ codegen/codegen/economie_circulaire/referentiel_extractor.py
@@ -9,6 +9,42 @@ def stripped(value) -> str:
     return value.strip() if isinstance(value, str) else ''
 
 
+def points_from_ponderation(ponderation) -> int:
+    return int(float(ponderation) * 100)
+
+
+def parse_referentiel_points(referentiel: str) -> dict:
+    header = [
+        'axe',
+        'orientation',
+        'intitulé',
+        'niveau_1',
+        'niveau_2',
+        'niveau_3',
+        'niveau_4',
+        'niveau_5',
+        'total',
+        'pondération_orientation',
+    ]
+
+    niveaux = list(range(1, 6))
+
+    calculs = pd.read_excel(referentiel, dtype=str, sheet_name='Calculs', header=1)
+    calculs = calculs.iloc[1:22, 1: len(header) + 1]  # crop the table
+    calculs.columns = header
+    points = {}
+
+    for index, row in calculs.iterrows():
+        points[row['orientation']] = points_from_ponderation(row['pondération_orientation'])
+
+        for niveau in niveaux:
+            if not stripped(row[f'niveau_{niveau}']):
+                continue
+            points[f"{row['orientation']}.{niveau}"] = points_from_ponderation(row[f'niveau_{niveau}'])
+
+    return points
+
+
 def parse_referentiel_eci_xlsx(referentiel: str) -> List[dict]:
     """
     Read the référentiel excel file
@@ -19,6 +55,7 @@ def parse_referentiel_eci_xlsx(referentiel: str) -> List[dict]:
               'typologie', 'description', "exemples", 'ponderation', 'critere', 'unite', 'principe', 'preuve', 'poids']
     sheets = ['Axe 1', 'Axe 2', 'Axe 3', 'Axe 4', 'Axe 5']
     orientations = []
+    points = parse_referentiel_points(referentiel)
 
     for sheet in sheets:
         axe = pd.read_excel(referentiel, dtype=str, sheet_name=sheet, header=1)
@@ -28,10 +65,12 @@ def parse_referentiel_eci_xlsx(referentiel: str) -> List[dict]:
         # the actual parsing loop
         for index, row in axe.iterrows():
             # print(f'parsing {sheet} row {index}')
-            if stripped(row['orientation_n']):
+            orientation_n = stripped(row['orientation_n'])
+            if orientation_n:
                 orientation = {
-                    'id': row['orientation_n'].strip(),
+                    'id': orientation_n,
                     'nom': row['orientation_titre'].strip(),
+                    'points': points[orientation_n] if orientation_n in points.keys() else '',
                     'description': stripped(row['orientation_description']),
                     'actions': [],
                 }
@@ -44,19 +83,15 @@ def parse_referentiel_eci_xlsx(referentiel: str) -> List[dict]:
                 nom, description = row['description'].split('\n', 1)
                 numero, nom = nom.split(':', 1)
                 numero = re.search(r'\d+', numero).group(0)
-                ponderation = re.search(r"[-+]?\d*\.\d+|\d+", row['ponderation']).group(0)
-                points = int(float(ponderation) * 100)
+                niveau_n = f'{orientation["id"]}.{numero}'
                 niveau = {
-                    'id': f'{orientation["id"]}.{numero}',
+                    'id': niveau_n,
                     'nom': nom.strip(),
                     'description': description.strip(),
-                    # 'typologie': stripped(row['typologie']),
                     'exemples': stripped(row['exemples']),
-                    'points': points,
-                    # 'ponderation': row['ponderation'],
+                    'points': points[niveau_n] if niveau_n in points.keys() else '',
                     'critère': stripped(row['critere']),
                     'preuve': stripped(row['preuve']),
-                    'poids': stripped(row['poids']),
                     'actions': [],
                 }
                 orientation['actions'].append(niveau)
@@ -77,7 +112,6 @@ def parse_referentiel_eci_xlsx(referentiel: str) -> List[dict]:
                                 tache = {
                                     'id': f'{niveau["id"]}.{tache_index}',
                                     'nom': stripped(nom).capitalize(),
-                                    'points': int(points * pourcentage / 100),
                                     'poids': stripped(row['poids']),
                                     'actions': [],
                                 }
