8f1df23359ca068f15448ac9f874ca9ece7eaf5c
Florian d'Erfurth <florian@derfurth.com>
2021-05-27 17:47:06 +0200

    revert environment

--- client_new/src/api/currentEnvironment.ts
+++ client_new/src/api/currentEnvironment.ts
@@ -1,26 +1,14 @@
 type ProductionEnvironment = 'local' | 'sandbox' | 'staging' | 'release'
 
-import {stores} from '@sapper/app';
-
-const {page} = stores();
-
-
 export const getCurrentEnvironment = (): ProductionEnvironment => {
-    const hostname = page.host;
-    const environment = environmentFromHostname(hostname)
-    if (environment) return environment
+    const hostname = window.location.hostname;
+    if (hostname.substring(0, 10) === 'localhost') return 'local';
+    if (hostname === 'sandbox.territoiresentransitions.fr') return 'sandbox'
+    if (hostname === 'staging.territoiresentransitions.fr') return 'staging'
     throw `no environment for ${hostname}`
 }
 
 export const testUIVisibility = (): boolean => {
     const environment = getCurrentEnvironment()
-    return environment === 'local' || environment === 'sandbox'
+    return environment === 'local' || environment === 'sandbox';
 }
-
-const environmentFromHostname = (hostname: string) => {
-    if (hostname.substring(0, 10) === 'localhost') return 'local'
-    if (hostname === 'sandbox.territoiresentransitions.fr') return 'sandbox'
-    if (hostname === 'staging.territoiresentransitions.fr') return 'staging'
-}
-
-export let currentEnvironment: string; // = getCurrentEnvironment()
