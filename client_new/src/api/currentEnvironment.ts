ac6b918fa28894dbe3e5e6a45c26031529bf3f2a
Florian d'Erfurth <florian@derfurth.com>
2021-05-28 08:45:23 -0700

    update api and environment (#127)
    Remplace staging par app au niveau du client

--- client_new/src/api/currentEnvironment.ts
+++ client_new/src/api/currentEnvironment.ts
@@ -1,10 +1,10 @@
-type ProductionEnvironment = 'local' | 'sandbox' | 'staging' | 'release'
+type ProductionEnvironment = 'local' | 'sandbox' | 'app' | 'release'
 
 export const getCurrentEnvironment = (): ProductionEnvironment => {
     const hostname = window.location.hostname;
     if (hostname.substring(0, 10) === 'localhost') return 'local';
     if (hostname === 'sandbox.territoiresentransitions.fr') return 'sandbox'
-    if (hostname === 'staging.territoiresentransitions.fr') return 'staging'
+    if (hostname === 'app.territoiresentransitions.fr') return 'app'
     throw `no environment for ${hostname}`
 }
 
