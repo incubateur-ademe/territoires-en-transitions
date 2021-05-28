7a9bf2697be18ea45ba00b14d11b3c3bc02e5edb
Fanny Cheung <dev@ynote.hk>
2021-05-28 12:24:11 +0200

    Correction des imports JavaScript erronés (#116)

--- client_new/src/api/localStore.ts
+++ client_new/src/api/localStore.ts
@@ -14,6 +14,7 @@ import type {IndicateurPersonnaliseInterface} from "../../generated/models/indic
 import {IndicateurPersonnaliseValueStorable} from "../storables/IndicateurPersonnaliseValueStorable";
 import type {IndicateurPersonnaliseValueInterface} from "../../generated/models/indicateur_personnalise_value";
 import {IndicateurReferentielCommentaireStorable} from "../storables/IndicateurReferentielCommentaireStorable";
+import type {IndicateurReferentielCommentaireInterface} from "../../generated/models/indicateur_referentiel_commentaire";
 
 export const storeKey = 'territoiresentransitions'
 
@@ -198,4 +199,10 @@ export const indicateurPersonnaliseValueStore = new LocalStore<IndicateurPersonn
     pathname: IndicateurPersonnaliseValueStorable.pathname,
     serializer: (storable) => storable,
     deserializer: (serialized) => new IndicateurPersonnaliseValueStorable(serialized as IndicateurPersonnaliseValueInterface),
+});
+
+export const indicateurCommentaireStore = new LocalStore<IndicateurCommentaireStorable>({
+    pathname: IndicateurCommentaireStorable.pathname,
+    serializer: (storable) => storable,
+    deserializer: (serialized) => new IndicateurCommentaireStorable(serialized as IndicateurReferentielCommentaireInterface),
 });
