fc816063602b4f838867396173c672855bd7812d
Florian d'Erfurth <florian@derfurth.com>
2021-06-04 17:44:22 +0200

    update index to generate auth pages

--- app.territoiresentransitions.fr/src/routes/index.svelte
+++ app.territoiresentransitions.fr/src/routes/index.svelte
@@ -92,4 +92,8 @@
     <a href="fiches/edition/">Fiche action</a>
     <a href="actions_referentiels/">Référentiels</a>
     <a href="indicateurs/">Indicateurs</a>
+    <a href="auth/signin/">Se connecter</a>
+    <a href="auth/signout/">Se déconnecter</a>
+    <a href="auth/redirect/">Connexion</a>
+    <a href="auth/register/">Créer un compte</a>
 </div>
