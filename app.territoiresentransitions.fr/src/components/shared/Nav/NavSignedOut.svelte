2a97ea42fd375bca4fa32c211d39e5df9833d390
Fanny Cheung <dev@ynote.hk>
2021-06-08 17:04:32 +0200

    Réparation de la navigation avec Sapper (#163)

--- app.territoiresentransitions.fr/src/components/shared/Nav/NavSignedOut.svelte
+++ app.territoiresentransitions.fr/src/components/shared/Nav/NavSignedOut.svelte
@@ -13,25 +13,12 @@
         }
     });
 </script>
-<nav class="bg-seagreen-400">
-    <ul class="container mx-auto lg:px-20 px-4 p-4 flex text-xl">
-        <li class="mr-4">
-            <a class="p-1 rounded hover:bg-gray-100 hover:text-gray-700 focus:bg-gray-100 focus:text-gray-700 active:text-gray-900 active:shadow-inner active:bg-white"
-               href="/?epci_id={epciId}">
-                Territoires en Transitions
-            </a>
-        </li>
 
-        <li class="mr-4 flex-grow">
-        </li>
-
-        {#if !segment}
-            <li class="mr-4">
-                <a class="p-1 rounded hover:bg-gray-100 hover:text-gray-700 focus:bg-gray-100 focus:text-gray-700 active:text-gray-900 active:shadow-inner active:bg-white"
-                   href="connexion/?epci_id={epciId}">
-                    Se connecter
-                </a>
-            </li>
-        {/if}
-    </ul>
-</nav>
+{#if !segment}
+    <li class="mr-4">
+        <a class="p-1 rounded hover:bg-gray-100 hover:text-gray-700 focus:bg-gray-100 focus:text-gray-700 active:text-gray-900 active:shadow-inner active:bg-white"
+           href="connexion/?epci_id={epciId}">
+            Se connecter
+        </a>
+    </li>
+{/if}
