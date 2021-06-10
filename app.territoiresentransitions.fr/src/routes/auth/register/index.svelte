c6fdb214d6346fb7fc2c3d219ac23505d6c0cced
Florian d'Erfurth <florian@derfurth.com>
2021-06-10 15:08:41 +0200

    registration page

--- app.territoiresentransitions.fr/src/routes/auth/register/index.svelte
+++ app.territoiresentransitions.fr/src/routes/auth/register/index.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import {UtilisateurInscriptionInterface} from "../../../../../generated/models/utilisateur_inscription";
-    import {maximumLengthValidatorBuilder, numbersOnlyValidator, requiredValidator} from "../../../api/validators";
-    import {alwaysValid, joinValidators, validate} from "../../../api/validator";
+    import {maximumLengthValidatorBuilder, requiredValidator} from "../../../api/validators";
+    import {joinValidators, validate} from "../../../api/validator";
     import LabeledTextInput from "../../../components/shared/Forms/LabeledTextInput.svelte";
     import Button from "../../../components/shared/Button/Button.svelte";
-    import PickButton from "../../../components/shared/Button/PickButton.svelte";
     import {getCurrentAPI} from "../../../api/currentAPI";
 
     const politique_vie_privee = "https://www.ademe.fr/lademe/infos-pratiques/politique-protection-donnees-a-caractere-personnel"
@@ -23,6 +22,8 @@
         vie_privee: requiredValidator,
     }
 
+    let registrationResponse: Response;
+
     const toggle_politique = () => {
         inscription.vie_privee = inscription.vie_privee ? '' : politique_vie_privee
     }
@@ -36,52 +37,82 @@
         const api = getCurrentAPI()
         const endpoint = `${api}/v2/auth/register`
 
-        const response = await fetch(endpoint, {
+        registrationResponse = await fetch(endpoint, {
             method: 'POST',
-            mode: 'no-cors', // fixme: not normal
+            mode: 'cors',
             body: JSON.stringify(inscription)
         });
-
-        console.log(response)
-        console.log(await response.json())
     }
 </script>
 
-<h1>Registration form</h1>
 <section class="flex flex-col">
-    <form class="flex flex-col w-full md:w-3/4 pb-10">
-        <LabeledTextInput bind:value={inscription.email}
-                          maxlength="300"
-                          validator={validators.email}>
-            <div class="text-xl">Adresse mail</div>
-        </LabeledTextInput>
-        <div class="p-5"></div>
+    <div class="pb-10"></div>
 
-        <LabeledTextInput bind:value={inscription.nom}
-                          maxlength="300"
-                          validator={validators.nom}>
-            <div class="text-xl">Nom</div>
-        </LabeledTextInput>
-        <div class="p-5"></div>
+    {#if registrationResponse}
+        {#if registrationResponse.ok}
+            <h1 class="text-2xl">Votre compte</h1>
+            <div class="pb-10"></div>
+            <p>Votre compte "{inscription.email}" a bien été créé.</p>
+            <div class="p-5"></div>
+            <p>Lors de votre première connexion cliquez sur "Mot de passe oublié ?" afin de créer votre mot de
+                passe.</p>
+            <div class="p-5"></div>
+            <div>
+                <Button asLink href="/auth/signin/">
+                    Me connecter
+                </Button>
+            </div>
+        {:else }
+            <h1 class="text-2xl">Erreur</h1>
+            <div class="pb-10"></div>
+            <p>Votre compte "{inscription.email}" n'a pas pû être créé.</p>
+        {/if}
 
-        <LabeledTextInput bind:value={inscription.prenom}
-                          maxlength="300"
-                          validator={validators.prenom}>
-            <div class="text-xl">Prénom</div>
-        </LabeledTextInput>
-        <div class="p-5"></div>
+    {:else }
 
-        <PickButton picked={inscription.vie_privee}
-                    handlePick={toggle_politique}
-                    handleUnpick={toggle_politique}
-                    pickLabel="Je n'accepte pas la politique de protection des données à caractère personnel"
-                    unpickLabel="✓ J'accepte la politique de protection des données à caractère personnel"
-        />
-        <a href={politique_vie_privee}>Politique de protection des données à caractère personnel de l'ADEME</a>
-        <div class="p-5"></div>
+        <h1 class="text-2xl">Créer un compte</h1>
+        <div class="pb-10"></div>
+        <form class="flex flex-col w-full md:w-3/4 pb-10">
+            <LabeledTextInput bind:value={inscription.email}
+                              maxlength="300"
+                              validator={validators.email}>
+                <div class="text-xl">Adresse mail</div>
+            </LabeledTextInput>
+            <div class="p-5"></div>
 
-        <Button on:click={register}>
-            Créer mon compte
-        </Button>
-    </form>
+            <LabeledTextInput bind:value={inscription.nom}
+                              maxlength="300"
+                              validator={validators.nom}>
+                <div class="text-xl">Nom</div>
+            </LabeledTextInput>
+            <div class="p-5"></div>
+
+            <LabeledTextInput bind:value={inscription.prenom}
+                              maxlength="300"
+                              validator={validators.prenom}>
+                <div class="text-xl">Prénom</div>
+            </LabeledTextInput>
+            <div class="p-5"></div>
+
+            <a class="flex w-full underline text-blue-600" href={politique_vie_privee}>Politique de protection des
+                données à
+                caractère personnel de l'ADEME</a>
+            <div class="pb-3"></div>
+
+            {#if inscription.vie_privee}
+                <Button on:click={toggle_politique} classNames="w-full">
+                    ✓ J'accepte la politique de protection des données à caractère personnel
+                </Button>
+            {:else }
+                <Button on:click={toggle_politique} classNames="w-full">
+                    Je n'accepte pas la politique de protection des données à caractère personnel
+                </Button>
+            {/if}
+            <div class="p-5"></div>
+
+            <Button on:click={register}>
+                Créer mon compte
+            </Button>
+        </form>
+    {/if}
 </section>
