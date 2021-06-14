24631988ab437813d5e405a749ee29f0b833b1b8
newick <erick@entre-quote.com>
2021-06-14 16:51:33 +0200

    commence la mise en place du design system

---
+++ app.territoiresentransitions.fr/src/routes/_head.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Variables from "../../../components/DesignSystem/Variables.svelte";
+    import DesignSystem from "../../../components/DesignSystem/DesignSystem.svelte";
+</script>
+
+<Variables/>
+<DesignSystem/>
