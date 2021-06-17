c435f3f2c80ad84c808a58c306f787534f6034f2
Florian d'Erfurth <florian@derfurth.com>
2021-06-17 17:29:10 +0200

    epci list view

---
+++ app.territoiresentransitions.fr/src/routes/epcis/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    /**
+     * Shows all Epcis.
+     */
+    import {EpciStorable} from "../../storables/EpciStorable";
+    import {onMount} from "svelte";
+    import {currentUtilisateurDroits} from "../../api/authentication";
+    import {UtilisateurDroits} from "../../../../generated/models/utilisateur_droits";
+
+    let allEpcis: EpciStorable[] = []
+    let userEpcis: EpciStorable[] = []
+
+    onMount(async () => {
+        const stores = await import('../../api/hybridStores')
+        const utilisateurDroits: UtilisateurDroits[] = await currentUtilisateurDroits()
+
+        allEpcis = await stores.epciStore.retrieveAll()
+        userEpcis = allEpcis.filter((epci) => utilisateurDroits.filter((droits) => droits.ecriture && droits.epci_id === epci.id))
+    })
+</script>
+
+<div>
+    <h2>Bienvenue !</h2>
+
+    {#each userEpcis as epci}
+        <p>{epci.nom}</p>
+    {/each}
+</div>
+
+<div>
+    <h2>Toutes les collectivités</h2>
+    {#each allEpcis as epci}
+        <p>{epci.nom}</p>
+    {/each}
+</div>
