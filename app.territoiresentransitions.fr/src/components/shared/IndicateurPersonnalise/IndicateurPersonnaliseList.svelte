53ff4df213431e929bd225dc93fc1c16792a9ded
newick <erick@entre-quote.com>
2021-06-21 13:48:41 +0200

    commence à habiller les indicateurs

--- app.territoiresentransitions.fr/src/components/shared/IndicateurPersonnalise/IndicateurPersonnaliseList.svelte
+++ app.territoiresentransitions.fr/src/components/shared/IndicateurPersonnalise/IndicateurPersonnaliseList.svelte
@@ -24,11 +24,17 @@
 
 </script>
 
-<div class="flex flex-row w-full items-center">
+<style>
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
 
-    <h3 class="text-2xl">Indicateurs personnalisés</h3>
-    <div class="flex flex-grow"></div>
-    <Button colorVariant={showCreation ? 'ash' : 'nettle'}
+<div>
+    <h2>Mes indicateurs</h2>
+    <Button classNames="fr-btn" ton colorVariant={showCreation ? 'ash' : 'nettle'}
             on:click={handleNewIndicateur}>
         Nouvel indicateur
     </Button>
