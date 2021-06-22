36c16b4372e7e9230a10fb6ab0a9a785ceeea182
Florian d'Erfurth <florian@derfurth.com>
2021-06-22 04:38:19 -0700

    Flow d'authentification avec l'UI pour tester (#182)

---
+++ app.territoiresentransitions.fr/src/api/authentication.ts
@@ -0,0 +1,50 @@
+import jwt_decode from "jwt-decode";
+import {UtilisateurConnecteStorable} from "../storables/UtilisateurConnecteStorable";
+import {utilisateurConnecteStore} from "./localStore";
+
+
+/**
+ * Save the access token, the user is considered connected as long as the token is valid.
+ */
+export const saveTokens = (accessToken: string, refreshToken: string) => {
+    const decoded = jwt_decode(accessToken)
+    const storable = new UtilisateurConnecteStorable({
+        ademe_user_id: decoded['sub'] || '',
+        access_token: accessToken,
+        refresh_token: refreshToken,
+        email: decoded['email'] || '',
+        nom: decoded['family_name'] || '',
+        prenom: decoded['given_name'] || '',
+    })
+    utilisateurConnecteStore.store(storable)
+}
+
+/**
+ * Returns true if the user have a valid token.
+ */
+export const isConnected = (): boolean => {
+    const utilisateur = utilisateurConnecteStore.retrieveById(UtilisateurConnecteStorable.id)
+    if (utilisateur == null) return false
+
+    const decoded = jwt_decode(utilisateur.access_token)
+    const secondsLeft = decoded['exp'] - (Date.now() / 1000)
+
+    return secondsLeft > 0
+}
+
+/**
+ * Retrieve current user, returns an UtilisateurConnecte if the user is connected otherwise returns null.
+ */
+export const currentUser = (): UtilisateurConnecteStorable | null => {
+    if (isConnected())
+        return utilisateurConnecteStore.retrieveById(UtilisateurConnecteStorable.id)
+    return null
+}
+
+
+/**
+ * Sign the current user out. Return true on success, false otherwise.
+ */
+export const signOut = (): boolean => {
+    return utilisateurConnecteStore.deleteById(UtilisateurConnecteStorable.id)
+}
--- app.territoiresentransitions.fr/src/routes/index.svelte
+++ app.territoiresentransitions.fr/src/routes/index.svelte
@@ -92,4 +92,9 @@
     <a href="fiches/edition/">Fiche action</a>
     <a href="actions_referentiels/">Référentiels</a>
     <a href="indicateurs/">Indicateurs</a>
+    <a href="auth/signin/">Se connecter</a>
+    <a href="auth/signout/">Se déconnecter</a>
+    <a href="auth/redirect/">Connexion</a>
+    <a href="auth/token_signin/">Connexion</a>
+    <a href="auth/register/">Créer un compte</a>
 </div>
