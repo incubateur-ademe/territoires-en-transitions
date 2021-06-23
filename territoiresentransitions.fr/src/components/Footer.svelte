6f9b65d8a2d976277fdac95f191b915e996e76e8
Nicolas Dievart <nicolas.dievart@gmail.com>
2021-06-23 16:23:06 +0200

    Premiere version de la page d'accueil de territoiresentransitions.fr (#177)

---
+++ territoiresentransitions.fr/src/components/Footer.svelte
@@ -0,0 +1,36 @@
+<script>
+  import links from './../content/footer/links.md';
+  import SvelteMarkdown from 'svelte-markdown';
+  import FooterLink from './Footer/FooterLink.svelte';
+
+  const renderers = {
+    link: FooterLink
+  }
+</script>
+
+<footer class="fr-footer fr-mt-10w" role="contentinfo" id="footer">
+  <div class="fr-container">
+    <div class="fr-footer__body">
+      <div class="fr-footer__brand fr-enlarge-link">
+        <p class="fr-logo">
+          république
+          <br />française
+        </p>
+        <div class="fr-header__ademe">
+          <picture>
+            <source srcset="./img/ademe.jpg 118w" sizes="118px" />
+            <img src="./img/ademe.jpg" alt="ADEME" loading="lazy" />
+          </picture>
+        </div>
+      </div>
+    </div>
+    <div class="fr-footer__bottom">
+      <ul class="fr-footer__bottom-list">
+        <SvelteMarkdown source={links.content} {renderers} />
+      </ul>
+      <div class="fr-footer__bottom-copy">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ territoiresentransitions.fr/src/components/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+  import links from './../content/header/links.md';
+  import SvelteMarkdown from 'svelte-markdown';
+  import HeaderLink from './Header/HeaderLink.svelte';
+  import EmptyParagraph from './Markdown/EmptyParagraph.svelte';
+
+  const renderers = {
+    link: HeaderLink,
+    paragraph: EmptyParagraph
+  }
+</script>
+
+<header role="banner" class="fr-header">
+  <div class="fr-container">
+    <div class="fr-header__body-row">
+      <div class="fr-header__brand fr-enlarge-link">
+        <div class="fr-header__brand-top">
+          <div class="fr-header__logo">
+            <p class="fr-logo">
+              République<br /> française
+            </p>
+          </div>
+        </div>
+        <div class="fr-header__ademe">
+          <picture>
+            <source srcset="./img/ademe.jpg 118w" sizes="118px" />
+            <img src="./img/ademe.jpg" alt="ADEME" loading="lazy" />
+          </picture>
+        </div>
+        <div class="fr-header__service">
+          <a href="/" title="Accueil">
+            <p class="fr-header__service-title">Territoires en Transitions</p>
+          </a>
+        </div>
+      </div>
+      <div class="fr-header__tools">
+        <div class="fr-header__tools-links">
+          <ul class="fr-links-group">
+            <SvelteMarkdown source={links.content} {renderers} />
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="fr-header__menu fr-modal" id="header-navigation" aria-labelledby="button-1">
+      <div class="fr-container">
+        <button class="fr-link--close fr-link" aria-controls="header-navigation">Fermer</button>
+        <div class="fr-header__menu-links" />
+      </div>
+    </div>
+  </div>
+</header>
+
+<style>
+  .fr-header__brand-top {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    width: auto;
+  }
+</style>
