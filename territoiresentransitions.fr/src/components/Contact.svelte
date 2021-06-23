15bb52126cd89ca68b47184bca08a02cb5fcacda
Fanny Cheung <dev@ynote.hk>
2021-06-23 12:32:06 +0200

    Update style

--- territoiresentransitions.fr/src/components/Contact.svelte
+++ territoiresentransitions.fr/src/components/Contact.svelte
@@ -5,10 +5,15 @@
   class="fr-col-sm-12 fr-col-md-6 fr-col-lg-6"
 >
   <label class="fr-label" for="email">Votre email :</label>
-  <input class="fr-input" type="text" id="email" name="email" />
+  <input class="fr-input h-mb" type="text" id="email" name="email" />
 
   <label class="fr-label" for="body">Votre message :</label>
-  <textarea class="fr-input" type="text" id="body" name="body"></textarea>
+  <textarea class="fr-input" rows="7" type="text" id="body" name="body"></textarea>
 
-  <input class="fr-input fr-mt-3w" type="submit" name="submit" value="Envoyer" />
+  <input class="fr-btn fr-mt-3w" type="submit" name="submit" value="Envoyer" />
 </form>
+<style>
+  .h-mb {
+    margin-bottom: 2.5rem;
+  }
+</style>
