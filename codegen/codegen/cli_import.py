601df0ebf990af992cb7d4dc4dbadcfcf345a362
Florian d'Erfurth <florian@derfurth.com>
2021-06-28 19:04:19 +0200

    wip dteci cli import

--- codegen/codegen/cli_import.py
+++ codegen/codegen/cli_import.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import List
+from typing import List, Dict
 
 import pandas as pd
 import typer
@@ -22,10 +22,36 @@ def dteci(
     """
     Load dteci data (one shot)
     """
-    table_correspondance = load_json(os.path.join(data_dir, 'table_correspondance.json'))
+    table_correspondance: list = load_json(os.path.join(data_dir, 'table_correspondance.json'))
     server_niveaux = load_json(os.path.join(data_dir, 'server_niveaux.json'))
-    server_territoires = load_json(os.path.join(data_dir, 'server_territoires.json'))
+    server_territoires: Dict[dict] = load_json(os.path.join(data_dir, 'server_territoires.json'))
     collectivites = pd.read_excel(os.path.join(data_dir, 'collectivités.xlsx'), dtype=str, sheet_name=0, header=0)
+
+    nom_repris: List[str] = [row['nom'] for index, row in collectivites.iterrows()
+                             if str(row['Repris']).strip().startswith('o')]
+
+    territoires_repris: List[dict] = [territoire for territoire in server_territoires.values()
+                                      if territoire['nom'] in nom_repris]
+
+    id_repris: List[str] = [t['id'] for t in territoires_repris]
+    niveaux_repris: List[dict] = [n for n in server_niveaux.values() if n['territoireId'] in id_repris]
+
+    # iterate on the correspondance table
+    for axe in table_correspondance:
+        for orientation in axe['orientations']:
+            for niveau in orientation['niveaux']:
+                # process corresponding niveau data.
+                indicateur: dict = niveau.get('indicateur', {})
+                choix: list = indicateur.get('choix', [])
+                niveau_data = [n for n in niveaux_repris if n['niveauId'] == niveau['id']]
+
+                for c in choix:
+                    chosen = [n for n in niveau_data if c['id']
+                              in [v['id'] for v in n.get('valeur', {}).get('ids', [])]]
+                    pass
+
+                pass
+
     pass
 
 
