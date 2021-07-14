146d16b36b1aab3a56a54ae8e70a3cbf2450c525
Fanny Cheung <dev@ynote.hk>
2021-07-14 17:38:04 +0200

    Ajout du composant Button (#392)

--- app.territoiresentransitions.fr/src/routes/auth/token_signin/index.svelte
+++ app.territoiresentransitions.fr/src/routes/auth/token_signin/index.svelte
@@ -2,7 +2,7 @@
     /**
      * Allow to use tokens as a way to login for development purposes.
      */
-    import Button from "../../../components/shared/Button/Button.svelte";
+    import Button from '../../../components/shared/ButtonV2/Button.svelte'
 
     let accessToken = ''
     let refreshToken = ''
@@ -24,39 +24,69 @@
     }
 </style>
 
-<div class="flex flex-col">
-    <h1 class="text-2xl">Connexion par token</h1>
-    <div>
-        <a class="text-blue-600"
-           href="https://sandbox.territoiresentransitions.fr/auth/signin"
-           target="_blank">Se connecter</a>
-        puis récupérer les tokens dans les dev tools sur la réponse de l'endpoint
-        <pre>v2/auth/token</pre> ou depuis localstorage.
+<div class="fr-container">
+    <div class="fr-grid-row fr-mb-2w">
+        <h1>Connexion par token</h1>
+        <div>
+            Cette action permet d'enregistrer un <pre>AccessToken</pre> collecté via sandbox ou la production dans
+            <pre>LocalStorage</pre>. Pour cela :
+            <ul>
+                <li class="fr-pl-2w">
+                    - <a
+                       href="https://sandbox.territoiresentransitions.fr/auth/signin"
+                       target="_blank">Se connecter</a>
+                </li>
+                <li class="fr-pl-2w">
+                    - Puis, récupérer les tokens dans les dev tools sur la réponse de l'endpoint
+                    <pre>v2/auth/token</pre> ou depuis <pre>LocalStorage</pre> et les coller dans leschamps suivants.
+                </li>
+            </ul>
+
+        </div>
     </div>
-    <div class="pb-5"></div>
 
-    <h2 class="text-xl">Access Token</h2>
-    <textarea bind:value={accessToken} class="w-full" rows="10"></textarea>
-    <div class="pb-5"></div>
+    <div class="fr-grid-row">
+        <h2>Access Token</h2>
+    </div>
 
-    <h2 class="text-xl">Refresh Token</h2>
-    <textarea bind:value={refreshToken} class="w-full" rows="10"></textarea>
-    <div class="pb-5"></div>
+    <div class="fr-grid-row fr-mb-2w">
+        <textarea class="fr-col-12" bind:value={accessToken} rows="10"></textarea>
+    </div>
 
-    <Button on:click={save}>Se connecter</Button>
-    <div class="pb-5"></div>
+    <div class="fr-grid-row">
+        <h2>Refresh Token</h2>
+    </div>
+
+    <div class="fr-grid-row fr-mb-2w">
+        <textarea class="fr-col-12" bind:value={refreshToken} rows="10"></textarea>
+    </div>
+
+    <div class="fr-grid-row fr-mb-2w">
+        <Button on:click={save}>Se connecter</Button>
+    </div>
 
     <hr>
-    <h1 class="text-2xl">Connexion avec un faux token</h1>
-    <div>
-        Fonctionne avec l'API en mode <pre>AUTH_DISABLED_DUMMY_USER</pre> enregistre un faux token dans localstorage.
+
+    <div class="fr-grid-row">
+        <h1>Connexion avec un faux token</h1>
+        <div class="fr-mb-2w">
+            Cette méthode fonctionne avec l'API en mode <pre>AUTH_DISABLED_DUMMY_USER</pre>. Cette action enregistre un
+            faux token dans <pre>LocalStorage</pre>.
+        </div>
     </div>
-    <Button on:click={saveDummy}>Utiliser un faux token</Button>
+
+    <div class="fr-grid-row fr-mb-2w">
+        <Button on:click={saveDummy}>Utiliser un faux token</Button>
+    </div>
 
     <hr>
-    <h1 class="text-2xl">Vérifier l'identité</h1>
-    <a href="/auth/identity">Identity</a>
+
+    <div class="fr-grid-row">
+        <h1>Vérifier l'identité</h1>
+    </div>
+
+    <p class="fr-mb-2w">
+        <Button classNames="fr-mr-1w" href="/auth/identity" colorVariant="whitelily">Aller à la page identity</Button>
+        <Button href="/auth/identity" colorVariant="whitelily">Aller sur les EPCIs</Button>
+    </p>
 </div>
-
-
-
