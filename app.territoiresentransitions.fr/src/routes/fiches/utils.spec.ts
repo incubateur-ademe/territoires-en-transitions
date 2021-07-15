41b94dc8104dad6f78e1bbb0c1e47a9eae6bbfb2
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-07-15 12:16:38 +0200

    Better sort function to handle 1.1.10, 1.1.9, and others

--- app.territoiresentransitions.fr/src/routes/fiches/utils.spec.ts
+++ app.territoiresentransitions.fr/src/routes/fiches/utils.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { FicheAction } from "../../../../generated/models/fiche_action";
 import { FicheActionCategorie } from "../../../../generated/models/fiche_action_categorie";
-import { categorizeAndSortFiches } from "./utils";
+import { categorizeAndSortFiches, sortFiches } from "./utils";
 
 const makeCategorie = (uid: string, fiche_actions_uids: string[] = []) =>
   new FicheActionCategorie({
@@ -69,5 +69,23 @@ describe("categorize fiches", () => {
         ]);
       });
     });
+  });
+});
+
+describe("sort fiches", () => {
+  it("sorts correctly when numbers are 1.9 and 1.10", () => {
+    const fiche_9 = makeFicheAction("1.9", "1.9");
+    const fiche_10 = makeFicheAction("1.10", "1.10");
+    expect(sortFiches([fiche_10, fiche_9])).eql([fiche_9, fiche_10]);
+  });
+  it("sorts correctly when numbers are 1.1,  1.1.A and 1.1.B", () => {
+    const fiche_11 = makeFicheAction("1.1", "1.1");
+    const fiche_11A = makeFicheAction("1.1.A", "1.1.A");
+    const fiche_11B = makeFicheAction("1.1.B", "1.1.B");
+    expect(sortFiches([fiche_11, fiche_11B, fiche_11A])).eql([
+      fiche_11,
+      fiche_11A,
+      fiche_11B,
+    ]);
   });
 });
