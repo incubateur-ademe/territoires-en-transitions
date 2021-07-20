ff28947559bd120f5f259b2fe77665a260749a68
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-07-20 11:56:29 +0200

    Remove old app folder

--- app.territoiresentransitions.fr/cypress/support/index.js
+++
@@ -1,5 +0,0 @@
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Import Cypress plugin snapshots to enable visual testing
-import 'cypress-plugin-snapshots/commands'
--- app.territoiresentransitions.fr/src/routes/actions_referentiels/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { ActionReferentiel } from "../../../../generated/models/action_referentiel";
-
-export const searchById = (actions: ActionReferentiel[], id: string): ActionReferentiel | void => {
-    for (let action of actions) {
-        if (action.id === id) return action;
-        const found = searchById(action.actions, id);
-        if (found) return found;
-    }
-}
