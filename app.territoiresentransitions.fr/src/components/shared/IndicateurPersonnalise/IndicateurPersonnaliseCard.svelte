07f5cdd55086c520cb6d99cff066dd1f2d7e2bef
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-08-02 13:15:38 +0200

    Retrieve description when it has been edited in IndicateurPerso

--- app.territoiresentransitions.fr/src/components/shared/IndicateurPersonnalise/IndicateurPersonnaliseCard.svelte
+++ app.territoiresentransitions.fr/src/components/shared/IndicateurPersonnalise/IndicateurPersonnaliseCard.svelte
@@ -11,11 +11,19 @@
 	let editing = false
 	const epciId = getCurrentEpciId()
 
-	const retrieveCommentaire = async () => {
+	const retrieveIndicateur = async (): Promise<IndicateurPersonnaliseStorable | undefined > => {
 		const indicateurValues = await indicateurPersonnaliseStore.retrieveAtPath(
 			`${epciId}/${indicateur.uid}` // TODO : This should probably be `id`, like for IndicateurReferentiel
 		)
-		return indicateurValues.length ? indicateurValues[0].meta.commentaire : ""
+		return indicateurValues.length ? indicateurValues[0]: undefined
+	}
+	const retrieveDescription = async () => {
+		const indicateur = await retrieveIndicateur()
+		return indicateur ? indicateur.description : ""
+	}
+	const retrieveCommentaire = async () => {
+		const indicateur = await retrieveIndicateur()
+		return indicateur ? indicateur.meta.commentaire : ""
 	}
 	const saveCommentaire = async (value: string) => {
 		// Note : here, we're a bit limited because meta is typed as `any` instead of {commentaire: string}
@@ -33,8 +41,10 @@
 		<h5 class="text-lg">Modifier l'indicateur</h5>
 		<IndicateurForm
 			bind:indicateurUid={indicateur.uid}
-			on:save={() => {
+			on:save={async () => {
 				editing = false
+				indicateur.description = await retrieveDescription() // TODO : this would not be necessary if we had this state in our store.
+
 			}}
 		/>
 	</RowCard>
@@ -44,7 +54,7 @@
 			class="fr-btn fr-btn--secondary fr-btn--sm fr-fi-edit-fill"
             style="float: right;"
 			title="Modifier l'indicateur"
-			on:click|preventDefault={() => {
+			on:click|preventDefault={ () => {
 				editing = true
 			}}
 		/>
@@ -53,7 +63,7 @@
 			indicateurDescription={indicateur.description}
 			indicateurId={indicateur.uid}
 			{saveCommentaire}
-			{retrieveCommentaire}
+			retrieveCommentaire={retrieveCommentaire}
 		/>
 	</div>
 {/if}
