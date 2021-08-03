3da73483493f68e68c0984285b08ecaec0c4d51a
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-08-03 14:37:01 +0200

    Test de l'API dans la CI [bis] (#475)
    * Fix paths in href.
    
    * configure jest in sveltekit
    
    * Add unit test client in CI
    
    * Change  name in package.json
    
    * Attempt to run codegen test only when md changed
    
    * Add space at the end of a md, to test CI
    
    * Add job to check changed files
    
    * Modify md file, to test check-file job
    
    * No md file have been modified, so codegen-test-and-generate should not run
    
    * Condition only on step run poetry tests
    
    * Black format of API
    
    * Black format of Codegen
    
    * Notation should give percentage 100 when action is done but gives no points
    
    * Minor front changes
    
    * Replace NC and 0% by non concernee
    
    * Move .github/workflows from api to root
    
    * Rename test yml
    
    * Rename pipelines and set working directory in pytest
    
    * Apply black and add black checking in CI
    
    * Check black format after installing dependencies
    
    * Use black from github action
    
    * apply black on tools
    
    * Rempve  test from src
    
    * Black formatting

--- tools/tools/cli_deploy.py
+++ tools/tools/cli_deploy.py
@@ -10,12 +10,14 @@ app = typer.Typer()
 
 @app.command()
 def upload_client(
-        subdomain: str = typer.Option('sandbox', "--subdomain", "-s"),
-        app_dir: str = typer.Option('../app.territoiresentransitions.fr/build', "--client-new", "-cn"),
+    subdomain: str = typer.Option("sandbox", "--subdomain", "-s"),
+    app_dir: str = typer.Option(
+        "../app.territoiresentransitions.fr/build", "--client-new", "-cn"
+    ),
 ) -> None:
     """Upload files into a sub domain bucket"""
     s3 = make_s3_client()
-    bucket = f'{subdomain}.territoiresentransitions.fr'
+    bucket = f"{subdomain}.territoiresentransitions.fr"
     count = upload_folder(bucket, app_dir, s3)
     typer.echo(f"Uploaded {count} files to '{bucket}'.")
 
@@ -31,9 +33,11 @@ def upload_folder(bucket: str, path: str, s3: BaseClient) -> int:
     with typer.progressbar(filenames) as progress:
         for filename in progress:
             name = filename.lstrip(path)
-            if os.name == 'nt':
-                name = name.replace("\\", "/")[1:]  # replace slashes and remove the preceding slash
-            
-            s3.upload_file(filename, bucket, name, ExtraArgs={'ACL': 'public-read'})
+            if os.name == "nt":
+                name = name.replace("\\", "/")[
+                    1:
+                ]  # replace slashes and remove the preceding slash
+
+            s3.upload_file(filename, bucket, name, ExtraArgs={"ACL": "public-read"})
             count += 1
     return count
