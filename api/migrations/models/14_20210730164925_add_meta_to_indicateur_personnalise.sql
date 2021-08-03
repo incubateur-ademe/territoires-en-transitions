fe2300d502342d7320284aae18a1deb52b99d06c
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-08-03 11:27:58 +0200

    Change for simple quote

--- api/migrations/models/14_20210730164925_add_meta_to_indicateur_personnalise.sql
+++ api/migrations/models/14_20210730164925_add_meta_to_indicateur_personnalise.sql
@@ -1,4 +1,4 @@
 -- upgrade --
-ALTER TABLE "indicateurpersonnalise" ADD "meta" JSONB NOT NULL DEFAULT "{}";
+ALTER TABLE "indicateurpersonnalise" ADD "meta" JSONB NOT NULL DEFAULT '{}';
 -- downgrade --
 ALTER TABLE "indicateurpersonnalise" DROP COLUMN "meta";
