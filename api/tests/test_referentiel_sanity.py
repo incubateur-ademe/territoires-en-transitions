b21259eb263aa34fcbfecb659dcd3f20e9713c9e
Florian d'Erfurth <florian@derfurth.com>
2021-08-09 14:47:54 +0200

    black format

--- api/tests/test_referentiel_sanity.py
+++ api/tests/test_referentiel_sanity.py
@@ -11,9 +11,11 @@ def _points_error_message(referentiel: Referentiel, index: tuple) -> str:
     total_points = sum([referentiel_eci.points[child] for child in children])
     total_percentage = sum([referentiel_eci.percentages[child] for child in children])
 
-    return f"Le total en pourcentage des {len(children)} enfants de {referentiel.actions[index].id} " \
-           f"n'est pas égal à 100% mais est égal à {total_percentage * 100}%. " \
-           f"Le total des points n'est pas de {referentiel.points[index]} mais est égal à {total_points}."
+    return (
+        f"Le total en pourcentage des {len(children)} enfants de {referentiel.actions[index].id} "
+        f"n'est pas égal à 100% mais est égal à {total_percentage * 100}%. "
+        f"Le total des points  n'est pas de {referentiel.points[index]} mais est égal à {total_points}."
+    )
 
 
 @pytest.mark.parametrize("index", referentiel_eci.backward)
@@ -22,4 +24,6 @@ def test_eci_action_points(index):
     children = referentiel_eci.children(index)
     if children:
         total_points = sum([referentiel_eci.points[child] for child in children])
-        assert math.isclose(total_points, referentiel_eci.points[index]), _points_error_message(referentiel_eci, index)
+        assert math.isclose(
+            total_points, referentiel_eci.points[index]
+        ), _points_error_message(referentiel_eci, index)
