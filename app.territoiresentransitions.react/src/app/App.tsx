687d9a2114ebdf785f80b3e36a5e2d5da13b5bb3
Florian d'Erfurth <florian@derfurth.com>
2021-08-11 15:19:44 +0200

    indicateur

--- app.territoiresentransitions.react/src/app/App.tsx
+++ app.territoiresentransitions.react/src/app/App.tsx
@@ -1,103 +1,96 @@
-import { Navigation } from "app/Navigation/Navigation";
-import { ActionsReferentiels } from "app/pages/ActionsReferentiels/ActionsReferentiels";
-import { Epcis } from "app/pages/Epcis/Epcis";
-import { overmind } from "core-logic/overmind";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useParams,
-  useRouteMatch,
-  Redirect,
-} from "react-router-dom";
+import {Navigation} from "app/Navigation/Navigation";
+import {ActionsReferentiels} from "app/pages/ActionsReferentiels/ActionsReferentiels";
+import {Epcis} from "app/pages/Epcis/Epcis";
+import {BrowserRouter as Router, Redirect, Route, Switch, useParams, useRouteMatch,} from "react-router-dom";
 
 import {Footer, Header} from "ui";
 import {Indicateurs} from "./pages/Indicateurs/Indicateurs";
 
 const Connected = () => {
-  let { path } = useRouteMatch();
-  const { epciId } = useParams<{ epciId: string }>();
-  overmind.actions.setCurrentEpci(epciId);
 
-  if (false) {
-    // todo redirect when user is not authenticated: https://reactrouter.com/web/example/auth-workflow
-    return <Redirect to="" />;
-  }
-  return (
-      <>
-        <Route path={`${path}/actions_referentiels`}>
-          <ActionsReferentiels/>
-        </Route>
-        <Route path={`${path}/indicateurs`}>
-          <Indicateurs/>
-        </Route>
-      </>
-  );
+    const {path, url} = useRouteMatch();
+    const {epciId} = useParams<{ epciId: string }>();
+    console.log("path in connected is ", path, "url is", url, " and EPCI ID is", epciId)
+
+    if (false) {
+        // todo redirect when user is not authenticated: https://reactrouter.com/web/example/auth-workflow
+        return <Redirect to=""/>;
+    }
+    return (
+        <>
+            <Route path={`${path}/actions_referentiels/`}>
+                <ActionsReferentiels/>
+            </Route>
+            <Route path={`${path}/indicateurs/`}>
+                <Indicateurs/>
+            </Route>
+        </>
+    );
 };
 
 export const App = () => {
-  return (
-    <Router>
-      <Header nav={<Navigation />} />
+    return (
+        <Router>
+            <Header nav={<Navigation/>}/>
 
-      <Switch>
-        <Route exact path="/">
-          <Epcis />
-        </Route>
-        <Route path={`/epcis/:epciId`}>
-          <Epcis />
-        </Route>
+            <Switch>
+                <Route exact path="/">
+                    <Epcis/>
+                </Route>
+                <Route path={`/epcis/:epciId`}>
+                    <Epcis/>
+                </Route>
 
-        <Route path={`/:epciId`}>
-          <Connected></Connected>
-        </Route>
-      </Switch>
+                <Route path={`/:epciId`}>
+                    <Connected/>
+                </Route>
+            </Switch>
 
-      <Footer
-        description={
-          <div>
-            <p className="fr-footer__content-desc">
-              Territoires en transitions accompagne les collectivités afin de
-              les aider à piloter plus facilement leur transition écologique.
-            </p>
-            <p className="fr-footer__content-desc">
-              Vous rencontrez une difficulté ? Une suggestion pour nous aider à
-              améliorer l'outil ? Écrivez-nous à :
-              <a href="mailto:aide@territoiresentransitions.fr?subject=Aide sur app.territoiresentransitions.fr">
-                aide@territoiresentransitions.fr
-              </a>
-            </p>
-          </div>
-        }
-        navigation={
-          <div>
-            {" "}
-            <div className="fr-footer__bottom">
-              <ul className="fr-footer__bottom-list">
-                <li className="fr-footer__bottom-item">
-                  <a
-                    className="fr-footer__bottom-link"
-                    href="https://territoiresentransitions.fr/mentions-legales/"
-                    target="_self"
-                  >
-                    Mentions légales
-                  </a>
-                </li>
-                <li className="fr-footer__bottom-item">
-                  <a
-                    className="fr-footer__bottom-link"
-                    href="https://www.ademe.fr/lademe/infos-pratiques/politique-protection-donnees-a-caractere-personnel"
-                    rel="external noreferrer"
-                    target="_blank"
-                  >
-                    Protection des données
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        }
-      />
-    </Router>
-  );
+            <Footer
+                description={
+                    <div>
+                        <p className="fr-footer__content-desc">
+                            Territoires en transitions accompagne les collectivités afin de
+                            les aider à piloter plus facilement leur transition écologique.
+                        </p>
+                        <p className="fr-footer__content-desc">
+                            Vous rencontrez une difficulté ? Une suggestion pour nous aider à
+                            améliorer l'outil ? Écrivez-nous à :
+                            <a href="mailto:aide@territoiresentransitions.fr?subject=Aide sur app.territoiresentransitions.fr">
+                                aide@territoiresentransitions.fr
+                            </a>
+                        </p>
+                    </div>
+                }
+                navigation={
+                    <div>
+                        {" "}
+                        <div className="fr-footer__bottom">
+                            <ul className="fr-footer__bottom-list">
+                                <li className="fr-footer__bottom-item">
+                                    <a
+                                        className="fr-footer__bottom-link"
+                                        href="https://territoiresentransitions.fr/mentions-legales/"
+                                        target="_self"
+                                    >
+                                        Mentions légales
+                                    </a>
+                                </li>
+                                <li className="fr-footer__bottom-item">
+                                    <a
+                                        className="fr-footer__bottom-link"
+                                        href="https://www.ademe.fr/lademe/infos-pratiques/politique-protection-donnees-a-caractere-personnel"
+                                        rel="external noreferrer"
+                                        target="_blank"
+                                    >
+                                        Protection des données
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                }
+            />
+        </Router>
+    );
 };
--- app.territoiresentransitions.react/src/app/pages/Indicateurs/IndicateurReferentielCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/Indicateurs/IndicateurReferentielCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import {IndicateurValueStorable} from "storables/IndicateurValueStorable";
 import {indicateurReferentielCommentaireStore, indicateurValueStore} from "../../../core-logic/api/hybridStores";
 import {IndicateurReferentielCommentaireStorable} from "../../../storables/IndicateurReferentielCommentaireStorable";
-import {overmind} from "../../../core-logic/overmind";
+import {overmind, useAppState} from "../../../core-logic/overmind";
 
 
 const ExpandPanel = (props: { content: string, title: string }) => (
@@ -24,7 +24,7 @@ const DescriptionPanel = (props: { description: string }) => (
 const years: number[] = Array.from({length: (2022 - 2010)}, (v, k) => k + 2010);
 
 
-class IndicateurReferentielValueInput extends React.Component<{ year: number, indicateur: IndicateurReferentiel }, { value: IndicateurValueStorable | null }> {
+class _IndicateurReferentielValueInput extends React.Component<{ year: number, indicateur: IndicateurReferentiel }, { value: IndicateurValueStorable | null }> {
     constructor(props: Readonly<{ year: number; indicateur: IndicateurReferentiel }> | { year: number; indicateur: IndicateurReferentiel }) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
@@ -67,6 +67,39 @@ class IndicateurReferentielValueInput extends React.Component<{ year: number, in
 }
 
 
+function IndicateurReferentielValueInput(props: { year: number, indicateur: IndicateurReferentiel }) {
+    const [value, setValue] = React.useState('');
+    const epci_id = useAppState().epciId;
+    console.log("IndicateurReferentielValueInput epciId is ", epci_id)
+    if (!epci_id) {
+        return (<div>EPCI ? </div>)
+    }
+
+    const id = IndicateurValueStorable.buildId(epci_id, props.indicateur.id, props.year)
+    indicateurValueStore
+        .retrieveById(id)
+        .then((storable) => setValue(storable?.value ?? ''))
+
+    const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const inputValue = event.currentTarget.value;
+        indicateurValueStore.store(
+            new IndicateurValueStorable(
+                {
+                    epci_id: epci_id,
+                    indicateur_id: props.indicateur.id,
+                    year: props.year,
+                    value: inputValue
+                }
+            )).then((storable) => setValue(storable.value));
+    };
+    return (
+        <label>
+            {props.year}
+            <input className="fr-input" defaultValue={value} onChange={handleChange}/>
+        </label>
+    );
+}
+
 const IndicateurReferentielValues = (props: { indicateur: IndicateurReferentiel }) => (
     <ul className="bg-grey">
         {years.map((year) =>
@@ -87,7 +120,8 @@ class IndicateurReferentielCommentaire extends React.Component<{ indicateur: Ind
     // todo on update.
 
     componentDidMount() {
-        const id = IndicateurReferentielCommentaireStorable.buildId('test', this.props.indicateur.id);
+        const epci_id = overmind.state.epciId!;
+        const id = IndicateurReferentielCommentaireStorable.buildId(epci_id, this.props.indicateur.id);
         indicateurReferentielCommentaireStore
             .retrieveById(id)
             .then((storable) => this.setState({commentaire: storable}));
