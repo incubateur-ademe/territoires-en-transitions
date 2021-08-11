b4fe02b53ac3f1a1efa10e646e00faa4dea02fee
Florian d'Erfurth <florian@derfurth.com>
2021-08-11 18:04:55 +0200

    better server

--- app.territoiresentransitions.react/server.js
+++ app.territoiresentransitions.react/server.js
@@ -1,8 +1,12 @@
+var path = require('path');
 var express = require('express');
-
 var app = express();
 var directory = '/' + (process.env.STATIC_DIR || 'build')
 app.use(express.static(__dirname + directory));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + directory + '/index.html'));
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
