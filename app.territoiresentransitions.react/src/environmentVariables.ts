f9efcad98187d4ef2a6cfdcaa5d5fe07a479926c
Florian d'Erfurth <florian@derfurth.com>
2021-08-11 16:26:50 +0200

    Changes by raph

--- app.territoiresentransitions.react/src/environmentVariables.ts
+++ app.territoiresentransitions.react/src/environmentVariables.ts
@@ -1,14 +1,16 @@
 const processEnv = process.env;
 
-const backendHost =
-  processEnv.NODE_ENV === "production" || processEnv.NODE_ENV === "test"
-    ? window.location.host
-    : "http://localhost:8000";
+
+const getBackendHost = () => {
+  if (processEnv.FLAVOR === "sandbox") return "https://sandboxterritoires.osc-fr1.scalingo.io"
+  else if (processEnv.FLAVOR === "prod") return "https://territoiresentransitions.osc-fr1.scalingo.io"
+  else return "http://localhost:8000"
+}
 
 export const ENV = {
   node_env: process.env.NODE_ENV,
   logActionsDuration: process.env.REACT_APP_LOG_ACTION_DURATION === "TRUE",
-  backendHost,
+  backendHost: getBackendHost(),
 };
 
 console.log("ENV :", JSON.stringify(ENV, null, 2));
