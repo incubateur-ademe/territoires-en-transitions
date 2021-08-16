599bd0532c4da050fcce4ecc4ff43ec14112e190
Florian d'Erfurth <florian@derfurth.com>
2021-08-16 18:30:21 +0200

    wip auth

---
+++ app.territoiresentransitions.react/src/app/pages/Auth/IdentityPage.tsx
@@ -0,0 +1,19 @@
+import {connected, currentUser} from "core-logic/api/authentication";
+
+export const IdentityPage = () => {
+    const isConnected = connected();
+    const user = currentUser();
+
+    if (isConnected) {
+        return (
+            <div>
+                <span>Vous êtes connecté </span>
+                {user !== null && <span>en tant que {user.prenom} {user.nom} ({user.email})</span>}
+                <span>.</span>
+            </div>
+        );
+    } else {
+        return (<div><p>Vous n'êtes pas connecté</p></div>);
+    }
+
+};
--- app.territoiresentransitions.react/src/ui/forms/LabeledTextInput.tsx
+++ app.territoiresentransitions.react/src/ui/forms/LabeledTextInput.tsx
@@ -1,25 +1,67 @@
+import {alwaysValid, Validator} from "core-logic/api/validator";
+
+import {v4 as uuid} from 'uuid'
+import {useState} from "react";
+
 type LabeledTextInputProps = {
-  label: string;
-  maxLength?: number;
-  onChange: (value: string) => void;
+    label: string;
+    maxLength?: number;
+    onChange: (value: string) => void;
+    validator: Validator;
+    maxlength: number | undefined;
+    id: string | undefined;
+    validateOnMount: boolean;
+    hint: string;
+    initialValue: String,
 };
 
-export const LabeledTextInput = ({
-  label,
-  maxLength,
-  onChange,
-}: LabeledTextInputProps) => (
-  <fieldset>
-    <label className="fr-label">
-      {label}
-      <slot />
-    </label>
+const labeledTextInputDefaultProps = {
+    validator: alwaysValid,
+    validateOnMount: true,
+    initialValue: '',
+    hint: '',
+};
 
-    <input
-      className="fr-input"
-      maxLength={maxLength}
-      onChange={(e) => onChange(e.target.value)}
-      // on:keyup={() => (errorMessage = validator(value))}
-    />
-  </fieldset>
-);
+/**
+ * A text input with a label on top
+ *
+ * One can use the label prop to display an _unstyled_ text on top of the textarea.
+ * In order to style the label text, a child element should be passed instead.
+ */
+const LabeledTextInput = (props: LabeledTextInputProps) => {
+
+    const htmlId = props.id ?? uuid();
+
+    const [errorMessage, setErrorMessage] = useState<String | null>(
+        props.validateOnMount ?
+            props.validator(props.initialValue) :
+            null
+    );
+
+
+    return (
+        <fieldset>
+            <label className="fr-label" htmlFor={htmlId}>
+                {props.label}
+                <slot/>
+            </label>
+
+            {(!errorMessage && props.hint) && <div className="hint">{props.hint}</div>}
+            {errorMessage !== null && <div className="hint">{errorMessage}</div>}
+
+            <input
+                id={htmlId}
+                className="fr-input"
+                maxLength={props.maxLength}
+                onChange={(e) => {
+                    setErrorMessage(props.validator(e.target.value))
+                    props.onChange(e.target.value);
+                }}
+            />
+        </fieldset>
+    );
+};
+
+LabeledTextInput.defaultProps = labeledTextInputDefaultProps;
+
+export default LabeledTextInput;
