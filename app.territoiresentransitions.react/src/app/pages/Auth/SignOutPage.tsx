db8e77cbf90aaebfa4b03e4e329cd5f1151f5e73
Florian d'Erfurth <florian@derfurth.com>
2021-08-18 10:14:30 +0200

    auth done

--- app.territoiresentransitions.react/src/app/pages/Auth/SignOutPage.tsx
+++ app.territoiresentransitions.react/src/app/pages/Auth/SignOutPage.tsx
@@ -1,5 +1,9 @@
 import {signOutRedirect} from 'core-logic/api/authRedirect';
 
+/**
+ * Erase local tokens then redirects to the ADEME keycloak sign out form,
+ * one could use the signOutRedirect() directly.
+ */
 export const SignOutPage = () => {
   signOutRedirect();
   return <h1 className="fr-h1">Redirection vers moncompte.ademe.fr</h1>;
