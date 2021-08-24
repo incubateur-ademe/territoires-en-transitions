3c015cdf3add374cbaad46967f602eedc2f89904
Florian d'Erfurth <florian@derfurth.com>
2021-08-24 17:55:26 +0200

    epci cards

--- app.territoiresentransitions.react/src/app/pages/Epcis/Epcis.tsx
+++ app.territoiresentransitions.react/src/app/pages/Epcis/Epcis.tsx
@@ -3,11 +3,8 @@ import {AddDialog} from 'app/pages/Epcis/_AddDialog';
 import 'app/DesignSystem/buttons.css';
 import 'app/DesignSystem/core.css';
 import 'app/DesignSystem/variables.css';
-
-import {makeStyles} from '@material-ui/core';
-import React, {useEffect} from 'react';
-import {useActions, useAppState} from 'core-logic/overmind';
-import {EpciCard} from 'app/pages/Epcis/_EpciCard';
+import React from 'react';
+import {EpciCard, OwnedEpciCard} from 'app/pages/Epcis/_EpciCard';
 import {allSortedEpcis, currentUserEpcis} from 'core-logic/hooks';
 
 const Epcis = () => {
@@ -27,15 +24,15 @@ const Epcis = () => {
         <h2 className="fr-h2 mb-20 text-center">Vos collectivités</h2>
 
         <div className="grid grid-cols-4 gap-3">
-          <div className="flex flex-col items-center pt-4 pr-6 pb-10 bg-beige">
+          {usersEpcis.map(epci => (
+            <OwnedEpciCard epci={epci} key={epci.id} />
+          ))}
+          <div className="flex flex-col items-center pt-4 pr-6 pb-10 bg-beige border-bf500 border-b-4">
             <h3 className="fr-h3">…</h3>
             <button className="fr-btn fr-btn--sm" onClick={handleClickOpen}>
               Ajouter ma collectivité
             </button>
           </div>
-          {usersEpcis.map(epci => (
-            <EpciCard epci={epci} key={epci.id} />
-          ))}
         </div>
       </section>
 
--- app.territoiresentransitions.react/src/app/pages/Epcis/_EpciCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/Epcis/_EpciCard.tsx
@@ -12,8 +12,9 @@ const EpciCardLink = (props: {label: string; linkTo: string}) => (
 
 type EpciCardProps = {epci: EpciStorable};
 export const EpciCard = ({epci}: EpciCardProps) => (
-  <div className="flex flex-col items-center pt-4 pr-6 pb-10 bg-beige">
+  <div className="flex flex-col items-center pt-4 pr-6 pb-10 bg-beige border-bf500 border-t-4">
     <h3 className="fr-h3 pb-6 text-center ">{epci.nom}</h3>
+
     <EpciCardLink
       label="Plan d'actions"
       linkTo={`/collectivite/${epci.id}/plan_actions`}
@@ -30,3 +31,12 @@ export const EpciCard = ({epci}: EpciCardProps) => (
     />
   </div>
 );
+
+export const OwnedEpciCard = ({epci}: EpciCardProps) => {
+  return (
+    <div className="relative">
+      <span className="bg-yellow-400 absolute top-0 right-0 p-3"> ❌</span>
+      <EpciCard epci={epci} />
+    </div>
+  );
+};
