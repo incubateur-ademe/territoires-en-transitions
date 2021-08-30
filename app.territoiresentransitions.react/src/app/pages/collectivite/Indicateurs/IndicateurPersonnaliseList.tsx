6a92050388700581a314852afeb1a53c49d79126
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-08-30 20:18:11 +0200

    Indicateur creation and edition are now modal

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPersonnaliseList.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPersonnaliseList.tsx
@@ -1,6 +1,6 @@
 import {IndicateurPersonnaliseStorable} from 'storables/IndicateurPersonnaliseStorable';
 import {IndicateurPersonnaliseCard} from 'app/pages/collectivite/Indicateurs/IndicateurPersonnaliseCard';
-import {IndicateurPersonnaliseCreatorExpandable} from './IndicateurPersonnaliseCreatorExpandable';
+import {IndicateurPersonnaliseCreationDialog} from './IndicateurPersonnaliseCreationDialog';
 import {useAllStorables} from 'core-logic/hooks';
 import {indicateurPersonnaliseStore} from 'core-logic/api/hybridStores';
 
@@ -12,9 +12,11 @@ export const IndicateurPersonnaliseList = () => {
 
   return (
     <div className="app mx-5 mt-5">
-      <IndicateurPersonnaliseCreatorExpandable buttonClasses="float-right" />
-      <div className="flex flex-col justify-between">
-        <h2 className="fr-h2">Mes indicateurs</h2>
+      <div className="flex flex-col justify-between ">
+        <div className="flex justify-between">
+          <h2 className="fr-h2">Mes indicateurs</h2>
+          <IndicateurPersonnaliseCreationDialog />
+        </div>
         <section className="flex flex-col">
           {indicateurs.map(indicateur => (
             <IndicateurPersonnaliseCard
