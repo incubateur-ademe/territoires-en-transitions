d20447126c21e84761b1763b3115a5f1ce45e8af
Florian d'Erfurth <florian@derfurth.com>
2021-08-30 18:03:30 +0200

    better indicateur layout

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurDescriptionPanel.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurDescriptionPanel.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const IndicateurDescriptionPanel = (props: {description: string}) => {
   return (
-    <div className={'border-t border-b border-gray-300'}>
+    <div className="border-t border-gray-300">
       <CrossExpandPanelWithHtmlContent
         title="Description"
         content={props.description}
