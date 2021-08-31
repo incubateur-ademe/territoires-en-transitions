b910598c8eb806dbb236916f45c7356a709dedb2
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-08-31 12:10:44 +0200

    Retour button in fiche action creator

---
+++ app.territoiresentransitions.react/src/ui/shared/RetourButton.tsx
@@ -0,0 +1,13 @@
+import {useHistory} from 'react-router-dom';
+
+export const RetourButton = () => {
+  const history = useHistory();
+  return (
+    <button
+      className="fr-link fr-fi-arrow-left-line fr-link--icon-left"
+      onClick={history.goBack}
+    >
+      Retour
+    </button>
+  );
+};
