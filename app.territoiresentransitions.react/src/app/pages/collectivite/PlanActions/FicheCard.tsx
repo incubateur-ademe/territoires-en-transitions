0c096d5f7aa642ba4e2a6b10b0844efdbf8869d5
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-09-02 16:03:56 +0200

    Format title if fiche action without numero

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/FicheCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/FicheCard.tsx
@@ -21,13 +21,13 @@ export const FicheCard = (props: FicheCardProps) => {
   const epciId = useEpciId();
   const fiche = props.fiche;
   const avancement = fiche.avancement as Avancement;
-
+  const formatedTitle = fiche.custom_id
+    ? `${fiche.custom_id} - ${fiche.titre}`
+    : fiche.titre;
   return (
     <article className="bg-beige mb-5 px-4 py-2 flex flex-row items-center justify-between">
       <Link to={`/collectivite/${epciId}/fiche/${fiche.uid}`}>
-        <h3 className="fr-h3">
-          {fiche.custom_id} - {fiche.titre}
-        </h3>
+        <h3 className="fr-h3">{formatedTitle}</h3>
       </Link>
       <div className="flex flex-row whitespace-nowrap items-center">
         <AvancementTag avancement={avancement} />
