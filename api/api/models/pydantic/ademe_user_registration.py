e9cdca6e2367b236965675a1971b529d29298e54
Florian d'Erfurth <florian@derfurth.com>
2021-09-06 11:56:41 +0200

    restore models

---
+++ api/api/models/pydantic/ademe_user_registration.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class AdemeUserRegistration(BaseModel):
+    """Data sent to the ADEME users API auth/register endpoint to create a new user"""
+
+    email: str
+    firstname: str
+    lastname: str
---
+++ api/api/models/tortoise/indicateur_personnalise.py
@@ -0,0 +1,28 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+
+class IndicateurPersonnalise(models.Model):
+    id = fields.IntField(pk=True)
+    epci_id = fields.CharField(max_length=36)
+    uid = fields.CharField(max_length=36)
+    custom_id = fields.CharField(max_length=36)
+    nom = fields.CharField(max_length=300)
+    description = fields.TextField()
+    unite = fields.CharField(max_length=36)
+    meta = fields.JSONField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    latest = fields.BooleanField()
+    deleted = fields.BooleanField()
+
+
+IndicateurPersonnalise_Pydantic = pydantic_model_creator(
+    IndicateurPersonnalise, name="IndicateurPersonnalise"
+)
+IndicateurPersonnaliseIn_Pydantic = pydantic_model_creator(
+    IndicateurPersonnalise,
+    name="IndicateurPersonnaliseIn",
+    exclude_readonly=True,
+    exclude=("latest", "deleted"),
+)
