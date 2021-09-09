3545abf048565913fb55c9ca13b204a2b7aa10a6
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-09-09 22:10:39 +0200

    Add notation endpoint for cae

--- api/api/routers/v2/notation.py
+++ api/api/routers/v2/notation.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter
 from api.models.generated.action_referentiel_score import ActionReferentielScore
 from api.models.tortoise.action_status import ActionStatus, ActionStatus_Pydantic
 from api.notation.notation import Notation, Status, UnknownActionIndex
-from api.notation.referentiels import referentiel_eci  # referentiel_cae
+from api.notation.referentiels import referentiel_eci, referentiel_cae
 
 router = APIRouter(prefix="/v2/notation")
 
@@ -33,23 +33,23 @@ async def get_eci_scores(epci_id: str):
     return notation.compute_and_get_scores()
 
 
-# @router.get("/cae/{epci_id}/all", response_model=List[ActionReferentielScore])
-# async def get_cae_scores(epci_id: str) -> List[ActionReferentielScore]:
-#     query = ActionStatus.filter(epci_id=epci_id, latest=True)
-#     statuses: List[ActionStatus] = await ActionStatus_Pydantic.from_queryset(query)
-#     notation = Notation(referentiel_cae)
+@router.get("/cae/{epci_id}/all", response_model=List[ActionReferentielScore])
+async def get_cae_scores(epci_id: str) -> List[ActionReferentielScore]:
+    query = ActionStatus.filter(epci_id=epci_id, latest=True)
+    statuses: List[ActionStatus] = await ActionStatus_Pydantic.from_queryset(query)
+    notation = Notation(referentiel_cae)
 
-#     for status in statuses:
-#         if get_referentiel_from_status(status) == "cae":
-#             index = tuple(status.action_id.split("__")[-1].split("."))
+    for status in statuses:
+        if get_referentiel_from_status(status) == "cae":
+            index = tuple(status.action_id.split("__")[-1].split("."))
 
-#             # convert the avancement set by the user to a statut for the notation engine
-#             notation_statut = Status.from_action_status_avancement(status.avancement)
+            # convert the avancement set by the user to a statut for the notation engine
+            notation_statut = Status.from_action_status_avancement(status.avancement)
 
-#             # set the status in the epci notation so the scores can be computed.
-#             try:
-#                 notation.set_status(index, notation_statut)
-#             except UnknownActionIndex:
-#                 print(f"Warning - UnknownActionIndex {index}")
+            # set the status in the epci notation so the scores can be computed.
+            try:
+                notation.set_status(index, notation_statut)
+            except UnknownActionIndex:
+                print(f"Warning - UnknownActionIndex {index}")
 
-#     return notation.compute_and_get_scores()
+    return notation.compute_and_get_scores()
--- api/tests/api/conftest.py
+++ api/tests/api/conftest.py
@@ -1,21 +1,21 @@
-# from typing import Generator
+from typing import Generator
 
-# import pytest
-# from fastapi.testclient import TestClient
-# from tortoise.contrib.test import finalizer, initializer
+import pytest
+from fastapi.testclient import TestClient
+from tortoise.contrib.test import finalizer, initializer
 
-# from api.app import app
-# from api.config.database import DB_MODELS
-
-
-# @pytest.fixture(scope="module", autouse=True)
-# def client() -> Generator:
-#     initializer(DB_MODELS)
-#     with TestClient(app) as c:
-#         yield c
-#     finalizer()
+from api.app import app
+from api.config.database import DB_MODELS
 
 
-# @pytest.fixture(scope="module")
-# def event_loop(client: TestClient) -> Generator:
-#     yield client.task.get_loop()
+@pytest.fixture(scope="module", autouse=True)
+def client() -> Generator:
+    initializer(DB_MODELS)
+    with TestClient(app) as c:
+        yield c
+    finalizer()
+
+
+@pytest.fixture(scope="module")
+def event_loop(client: TestClient) -> Generator:
+    yield client.task.get_loop()
