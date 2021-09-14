5762f871dedd9cd332a32f0d1b46a2023b30a282
Florian d'Erfurth <florian@derfurth.com>
2021-09-14 17:16:03 +0200

    wip referentiel

---
+++ app.territoiresentransitions.react/src/ui/shared/LazyDetails.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+interface LazyDetailsProps {
+  summary: React.ReactNode;
+  children: React.ReactNode;
+  startOpen: boolean;
+  onChange: (opened: boolean) => void;
+}
+
+/**
+ * A details like component that attaches children on open.
+ */
+export function LazyDetails(props: LazyDetailsProps) {
+  const [open, setOpen] = useState(props.startOpen);
+  return (
+    <section className="flex flex-col">
+      <header
+        className="w-full cursor-pointer"
+        onClick={e => {
+          e.preventDefault();
+
+          props.onChange(!open);
+          setOpen(!open);
+        }}
+      >
+        {props.summary}
+      </header>
+      {open && props.children}
+    </section>
+  );
+}
+
+LazyDetails.defaultProps = {
+  startOpen: false,
+  onChange: () => undefined,
+};
---
+++ app.territoiresentransitions.react/src/ui/shared/actions/ExpandableAction.tsx
@@ -0,0 +1,56 @@
+import {ActionReferentiel} from 'generated/models/action_referentiel';
+import React, {useState} from 'react';
+import {LazyDetails} from 'ui/shared/LazyDetails';
+import {
+  ActionReferentielTitle,
+  ActionReferentielTitleCard,
+  ProgressStat,
+} from 'ui/referentiels';
+import {Chevron} from 'ui/shared/Chevron';
+
+/**
+ * An expandable action used for "axes" and "orientations, shows Scores.
+ *
+ * Note: could be made recursive to simplify display on "axes" pages.
+ */
+export const ExpandableAction = ({action}: {action: ActionReferentiel}) => {
+  const [opened, setOpened] = useState(false);
+  return (
+    <div className="mt-2">
+      <LazyDetails
+        summary={
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex items-center mr-6">
+                <ActionReferentielTitle
+                  action={action}
+                  className="fr-h6 mb-0"
+                />
+              </div>
+              <Chevron direction={opened ? 'down' : 'left'} />
+            </div>
+            <div className="mb-2">
+              <ProgressStat
+                className="w-100"
+                action={action}
+                position="right"
+                showPoints={false}
+              />
+            </div>
+          </div>
+        }
+        onChange={setOpened}
+      >
+        <div className=" ml-8 mb-6">
+          {action.actions.map(action => (
+            <ActionReferentielTitleCard
+              key={action.id}
+              action={action}
+              referentiel="cae"
+            />
+          ))}
+        </div>
+      </LazyDetails>
+    </div>
+  );
+};
