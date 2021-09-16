a517a0501ac21aa61314bc993e9ce6b227cc4933
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-09-16 09:27:28 +0200

    Fix router tests following Char => Float in field value type

--- api/api/models/tortoise/indicateur_value.py
+++ api/api/models/tortoise/indicateur_value.py
@@ -6,7 +6,7 @@ class IndicateurValue(models.Model):
     id = fields.IntField(pk=True)
     epci_id = fields.CharField(max_length=36)
     indicateur_id = fields.CharField(max_length=36)
-    value = fields.FloatField(max_length=36)
+    value = fields.FloatField()
     year = fields.IntField(max_length=4)
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
