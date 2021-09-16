29f6f3298417b0642b5fd84d97ff477d918e3287
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-09-16 12:09:05 +0200

    Pairing with Maitane : Adding points to Jauge

--- app.territoiresentransitions.react/src/utils/progressStat.ts
+++ app.territoiresentransitions.react/src/utils/progressStat.ts
@@ -21,6 +21,19 @@ export const inferStateFromScore = (
   }
 };
 
+const approxEqual = (a: number, b: number) => Math.abs(a - b) < 0.0001;
+
+export const toFixed = (n: number) => {
+  if (approxEqual(Math.round(n), n)) return n.toFixed(0);
+  else if (approxEqual(Math.round(n * 10), n * 10)) return n.toFixed(1);
+  else return n.toFixed(2);
+};
+
 export const percentageTextFromScore = (
   score: ActionReferentielScoreStorable | null
-) => (score ? `${(score.percentage * 100).toFixed(1)}% ` : '0.0% ');
+) => (score ? `${toFixed(score.percentage * 100)}% ` : '0% ');
+
+export const pointsTextFromScore = (
+  score: ActionReferentielScoreStorable | null
+) =>
+  score ? `(${toFixed(score.points)}/${toFixed(score.potentiel)})` : '(../..)';
