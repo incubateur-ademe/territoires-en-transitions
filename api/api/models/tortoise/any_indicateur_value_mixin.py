9ce0f2fa36bbabc682fc5d9c7c2c82443d24b3c4
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-09-20 16:33:35 +0200

    Value not nullable

--- api/api/models/tortoise/any_indicateur_value_mixin.py
+++ api/api/models/tortoise/any_indicateur_value_mixin.py
@@ -5,7 +5,7 @@ class IndicateurValueMixin:
     id = fields.IntField(pk=True)
     epci_id = fields.CharField(max_length=36)
     indicateur_id = fields.CharField(max_length=36)
-    value = fields.FloatField()
+    value = fields.FloatField(null=True)
     year = fields.IntField(max_length=4)
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
