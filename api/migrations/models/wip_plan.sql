a5cebff82460b5fbd06b2c0e76d3531ca7b36eb7
Florian d'Erfurth <florian@derfurth.com>
2021-09-22 16:34:35 +0200

    wip sql migration

--- api/migrations/models/wip_plan.sql
+++ api/migrations/models/wip_plan.sql
@@ -1,3 +1,23 @@
+-- install uuid extension.
+create extension "uuid-ossp";
+
+-- first pass create default categories
+insert into ficheactioncategorie (epci_id, uid, parent_uid, nom, fiche_actions_uids)
+select epci_id, uuid_generate_v4(), '', 'Fiches actions non classées', json_agg(uid)
+from (
+         select epci_id, uid
+         from ficheaction
+         where latest
+           and not deleted
+
+           and uid not in
+               (select jsonb_array_elements_text(fiche_actions_uids)
+                from ficheactioncategorie
+               )
+     ) orphans
+group by epci_id;
+
+-- second pass make plans
 insert into planaction (epci_id, uid, nom, categories, fiches_by_category, latest, deleted)
 
 select epci_id,
@@ -17,14 +37,17 @@ from (
                     ) fiches_by_category
 
          from (
-                  select distinct on (uid) epci_id,
-                                           uid,
-                                           nom,
-                                           fiche_actions_uids,
-                                           jsonb_array_elements_text(fiche_actions_uids) fiche_uids
+                  select epci_id,
+                         uid,
+                         nom,
+                         fiche_uids
                   from ficheactioncategorie
+                           cross join lateral (
+                      select array_agg(d.elem::text) as fiche_uids
+                      from jsonb_array_elements_text(fiche_actions_uids) as d(elem)
+                      ) expanded
                   where latest
                     and not deleted
-              ) as epci_cats
+              ) epci_cats
          group by epci_id
-     ) as results;
+     ) results;
