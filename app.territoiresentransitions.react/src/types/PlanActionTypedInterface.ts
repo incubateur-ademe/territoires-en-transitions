e1cd0dfd160344ab1c8bd919c0253f135b6d8918
Florian d'Erfurth <florian@derfurth.com>
2021-09-23 11:33:53 +0200

    optional parent

--- app.territoiresentransitions.react/src/types/PlanActionTypedInterface.ts
+++ app.territoiresentransitions.react/src/types/PlanActionTypedInterface.ts
@@ -3,7 +3,7 @@ import {PlanAction} from 'generated/models/plan_action';
 export interface Categorie {
   nom: string;
   uid: string;
-  parent: string;
+  parent?: string;
 }
 
 export interface PlanActionStructure {
