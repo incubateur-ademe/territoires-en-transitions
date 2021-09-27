9442fc7eb2ede1ee6e36602c72b8b9537c0be9cd
Florian d'Erfurth <florian@derfurth.com>
2021-09-27 11:50:51 +0200

    rm deprecated

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/deprecatedDefaultCategorie.ts
+++
@@ -1,9 +0,0 @@
-import {FicheActionCategorieStorable} from 'storables/FicheActionCategorieStorable';
-
-export const deprecatedDefaultCategorie = new FicheActionCategorieStorable({
-  uid: 'default',
-  epci_id: '',
-  nom: 'Sans catégorie',
-  parent_uid: '',
-  fiche_actions_uids: [],
-});
