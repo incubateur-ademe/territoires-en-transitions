eba0caf40afdf96c42af50de559f64ce34d5facb
Florian d'Erfurth <florian@derfurth.com>
2021-10-05 16:35:19 +0200

    sql

--- api/sql_reports/user_count.sql
+++ api/sql_reports/user_count.sql
@@ -1,7 +1,6 @@
 with daily as (
     select created_at::date as day, count(created_at) as count
     from utilisateurdroits
-    where created_at > '2021-06-1' and created_at < '2021-12-1'
     group by day
 )
 select day,
