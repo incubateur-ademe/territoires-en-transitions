3077715f86b8f25f74d959188fb5a6dd56cc8862
Florian d'Erfurth <florian@derfurth.com>
2021-10-06 10:23:53 +0200

    plans on avancement

--- app.territoiresentransitions.react/src/app/pages/Epcis/_DetailedPlanActions.tsx
+++ app.territoiresentransitions.react/src/app/pages/Epcis/_DetailedPlanActions.tsx
@@ -2,8 +2,17 @@ import {Spacer} from 'ui/shared';
 import {ficheActionAvancementColors} from 'app/theme';
 import {FicheActionAvancement} from 'types';
 import {DetailedEpciCardPropsLink} from 'app/pages/Epcis/_DetailedEpciCardPropsLink';
-import {useEpciPlanActionAvancementSummmary} from 'core-logic/hooks';
+import {
+  useAllStorables,
+  useEpciPlanActionAvancementSummmary,
+} from 'core-logic/hooks';
 import * as R from 'ramda';
+import React, {useState} from 'react';
+import {UiDialogButton} from 'ui';
+import {PlanCreationForm} from 'app/pages/collectivite/PlanActions/Forms/PlanCreationForm';
+import {PlanActionStorable} from 'storables';
+import {getPlanActionStoreForEpci} from 'core-logic/api/hybridStores';
+import {PlanActionTyped} from 'types/PlanActionTypedInterface';
 
 const addSAtTheEndOfWordIfCountGreaterThan1 = (props: {
   count: number;
@@ -14,34 +23,56 @@ const addSAtTheEndOfWordIfCountGreaterThan1 = (props: {
 const hiddenIfNullable = (variable: any) => `${variable ? '' : 'hidden'}`;
 
 export const DetailedPlanActions = ({epciId}: {epciId: string}) => {
+  const [creating, setCreating] = useState<boolean>(false);
+  const store = getPlanActionStoreForEpci(epciId);
+  const plans = useAllStorables<PlanActionStorable>(store);
+  plans.sort((a, b) => a.nom.localeCompare(b.nom));
+
   return (
     <div>
       {' '}
       <div className="flex justify-between items-center">
         <div className="text-lg font-bold">Plan d'actions</div>
-        {/* <button className="fr-btn fr-btn--secondary fr-btn--sm">
-          + Nouveau{' '}
-        </button> */}
+        <UiDialogButton
+          title="Nouveau"
+          opened={creating}
+          setOpened={setCreating}
+          useFrBtn={false}
+          buttonClasses="whitespace-nowrap pt-2"
+        >
+          <Spacer />
+          <PlanCreationForm onSave={() => setCreating(false)} />
+        </UiDialogButton>
       </div>
-      <Spacer size={1} />
-      <div className="flex justify-between items-center">
-        <div className="text-sm font-bold">Mon plan d'actions</div>
-        <DetailedEpciCardPropsLink
-          label="Voir"
-          linkTo={`/collectivite/${epciId}/plan_actions`} // TODO link to specific plan d'action
-        />
-      </div>
-      <Spacer size={2} />
-      <div className="flex flex-col items-center">
-        <FicheActionAvancementCountBarAndLegend epciId={epciId} />
-      </div>
+      {plans.map(plan => (
+        <div key={plan.id} className="flex flex-col">
+          <Spacer size={1} />
+          <div className="flex justify-between items-center">
+            <div className="text-sm font-bold">{plan.nom}</div>
+            <DetailedEpciCardPropsLink
+              label="Voir"
+              linkTo={`/collectivite/${epciId}/plan_action/${plan.uid}`}
+            />
+          </div>
+          <Spacer size={2} />
+          <div className="flex flex-col items-center">
+            <FicheActionAvancementCountBarAndLegend
+              plan={plan as PlanActionTyped}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-const FicheActionAvancementCountBarAndLegend = ({epciId}: {epciId: string}) => {
+const FicheActionAvancementCountBarAndLegend = ({
+  plan,
+}: {
+  plan: PlanActionTyped;
+}) => {
   const epciPlanActionAvancementSummmary =
-    useEpciPlanActionAvancementSummmary(epciId);
+    useEpciPlanActionAvancementSummmary(plan);
 
   if (
     !epciPlanActionAvancementSummmary.avancementsCount ||
@@ -91,14 +122,14 @@ const FicheActionAvancementCountBarAndLegend = ({epciId}: {epciId: string}) => {
               width: `${faitePercentage}%`,
               backgroundColor: ficheActionAvancementColors['faite'],
             }}
-          ></div>
+          />
           <div
             className=" h-5"
             style={{
               width: `${enCoursPercentage}%`,
               backgroundColor: ficheActionAvancementColors['en_cours'],
             }}
-          ></div>
+          />
         </div>
       </div>
 
