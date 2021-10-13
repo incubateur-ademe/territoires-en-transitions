b061fb8b8f462114a34a874e89a33f1a53b34f32
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-10-13 16:42:47 +0200

    Remove exper in index

--- app.territoiresentransitions.react/src/generated/models/index.ts
+++ app.territoiresentransitions.react/src/generated/models/index.ts
@@ -9,6 +9,5 @@ export * from './indicateur_referentiel';
 export * from './indicateur_referentiel_commentaire';
 export * from './fiche_action';
 export * from './fiche_action_categorie';
-export * from './utilisateur_connecte';
 export * from './action_custom';
 export * from './plan_action';
