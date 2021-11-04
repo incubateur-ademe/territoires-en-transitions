69c493f8b26bbe3da7fe7d2fb1410fa7f56c2bc6
Florian d'Erfurth <florian@derfurth.com>
2021-11-04 01:05:52 -0700

    wip stuff (#846)

--- realtime/setup.py
+++ realtime/setup.py
@@ -4,7 +4,6 @@ from setuptools import find_packages, setup
 
 packages = find_packages(exclude=[])
 
-
 HERE = pathlib.Path(__file__).parent
 
 setup(name="realtime", version="0.1", packages=packages, requires=[])
