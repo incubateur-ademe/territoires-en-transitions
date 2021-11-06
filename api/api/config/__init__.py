8775b57550e0b93e5784fd3e07089d76b07a441e
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-06 11:24:18 +0100

    Remove API folder

--- api/api/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Application config"""
-from api.config.database import TortoiseSettings
-from api.config.openapi import OpenAPISettings
-
-tortoise_config = TortoiseSettings.generate()
-openapi_config = OpenAPISettings.generate()
--- api/api/models/pydantic/ademe_user_registration.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class AdemeUserRegistration(BaseModel):
-    """Data sent to the ADEME users API auth/register endpoint to create a new user"""
-
-    email: str
-    firstname: str
-    lastname: str
