8b430c0553141b47fbed6363281e8e5b0300c2c1
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-08 21:54:11 +0100

    Toggle filter action with statuses non renseignees

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionReferentielAvancement.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionReferentielAvancement.tsx
@@ -13,6 +13,11 @@ import {DescriptionContextAndRessourcesDialogButton} from './_DescriptionContext
 import {OrientationQuickNav} from 'app/pages/collectivite/Referentiels/QuickNav';
 import {searchActionById} from 'utils/actions';
 import {IndicateurReferentielCard} from 'app/pages/collectivite/Indicateurs/IndicateurReferentielCard';
+import {useState} from 'react';
+import {Switch} from '@material-ui/core';
+import {ActionStatusStorable} from 'storables';
+import {useAllStorables} from 'core-logic/hooks';
+import {actionStatusStore} from 'core-logic/api/hybridStores';
 
 const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
   const action = searchActionById(actionId, referentielActions);
@@ -22,6 +27,17 @@ const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
   const relatedIndicateurs = indicateurs.filter(indicateur =>
     isIndicateurRelatedToAction(indicateur, action)
   );
+
+  const [hideActionWithStatusRenseignee, setHideActionWithStatusRenseignee] =
+    useState(false);
+
+  const actionStatuses =
+    useAllStorables<ActionStatusStorable>(actionStatusStore);
+  const idsOfActionWithStatuses = actionStatuses
+    .filter(actionStatus => actionStatus.avancement)
+    .map(actionStatus => actionStatus.action_id);
+
+  console.log(idsOfActionWithStatuses);
   return (
     <div className="fr-container">
       <div className="mt-8 mb-16">
@@ -39,21 +55,52 @@ const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
           className="w-full mb-10"
           showPoints={true}
         />
-        <div className="w-2/3">
-          <DescriptionContextAndRessourcesDialogButton action={action} />
+        <div className="flex justify-between">
+          <div className="w-1/3">
+            <DescriptionContextAndRessourcesDialogButton action={action} />
+          </div>
+          <div>
+            Afficher uniquement les actions non-renseignées
+            <Switch
+              color="primary"
+              value={hideActionWithStatusRenseignee}
+              inputProps={{'aria-label': 'Switch A'}}
+              onClick={event =>
+                setHideActionWithStatusRenseignee(
+                  !hideActionWithStatusRenseignee
+                )
+              }
+            />
+          </div>
         </div>
       </div>
 
       <section>
         <h2 className="fr-h2"> Les actions</h2>
-        {action.actions.map(action => (
-          <ActionReferentielAvancementRecursiveCard
-            action={action}
-            key={action.id}
-            displayAddFicheActionButton={true}
-            displayProgressStat={true}
-          />
-        ))}
+        {action.id}{' '}
+        {action.actions.every(action =>
+          idsOfActionWithStatuses.includes(action.id)
+        )}
+        {action.actions.map(action => {
+          if (
+            hideActionWithStatusRenseignee &&
+            action.actions.every(action =>
+              idsOfActionWithStatuses.includes(action.id)
+            )
+          )
+            return null;
+          else {
+            return (
+              <ActionReferentielAvancementRecursiveCard
+                action={action}
+                key={action.id}
+                displayAddFicheActionButton={true}
+                displayProgressStat={true}
+                hideIfStatusRenseigne={hideActionWithStatusRenseignee}
+              />
+            );
+          }
+        })}
       </section>
 
       <Spacer />
