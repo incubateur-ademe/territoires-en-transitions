2a439ee21bbc3b64308546c224d2a60087b8d905
Florian d'Erfurth <florian@derfurth.com>
2021-11-09 15:45:49 +0100

    real epcis only

--- api/sql_reports/daily_created_indicateur_referentiel_resultat_count.sql
+++ api/sql_reports/daily_created_indicateur_referentiel_resultat_count.sql
@@ -1,8 +1,13 @@
-with daily as (
+with real_epci as (
+    select uid
+    from epci
+    where siren != ''
+),  daily as (
   select created_at::date as day,
     count(created_at) as count
   from indicateurresultat
-  where epci_id != 'test'
+    join real_epci on real_epci.uid = indicateurresultat.epci_id
+  where real_epci.uid is not null
     and indicateur_id like '%indicateur_id_regex'
   group by day
 )
