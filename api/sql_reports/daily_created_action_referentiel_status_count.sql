883ef485ae346cc838eb6cae8f17806d8bf310cd
Florian d'Erfurth <florian@derfurth.com>
2021-11-09 15:29:09 +0100

    wip stats fix

--- api/sql_reports/daily_created_action_referentiel_status_count.sql
+++ api/sql_reports/daily_created_action_referentiel_status_count.sql
@@ -1,14 +1,34 @@
-with daily as (
-  select created_at::date as day,
-    count(created_at) as count
-  from actionstatus
-  where epci_id != 'test'
-    and action_id like '%action_id_regex'
-  group by day
-)
-select day as date,
-  count,
-  sum(count) over (
-    order by day rows between unbounded preceding and current row
-  )::integer as cumulated_count
+with real_epci as (
+    select uid
+    from epci
+    where siren != ''
+),
+     statut as (
+         select action_id like 'citergie%' as cae,
+                action_id like 'economie%' as eci,
+                epci_id,
+                created_at
+         from actionstatus
+                  join real_epci on real_epci.uid = actionstatus.epci_id
+         where real_epci.uid is not null
+     ),
+
+     daily as (
+         select created_at::date  as day,
+                count(case when cae then 1 end) as cae_count,
+                count(case when eci then 1 end) as eci_count
+         from statut
+         group by day
+     )
+select day            as date,
+       cae_count,
+       sum(cae_count) over (
+           order by day rows between unbounded preceding and current row
+           )::integer as cumulated_count,
+
+       eci_count,
+       sum(eci_count) over (
+           order by day rows between unbounded preceding and current row
+           )::integer as cumulated_count
+
 from daily;
