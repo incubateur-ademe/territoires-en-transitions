9b03a11003982ad2e3911a292fc2cff8d28312b7
Florian d'Erfurth <florian@derfurth.com>
2021-11-10 17:56:06 +0100

    typedef cli

--- data_layer/typedef_cli.py
+++ data_layer/typedef_cli.py
@@ -1,5 +1,7 @@
 import json
 import os
+import subprocess
+from pathlib import Path
 from urllib.parse import urlparse
 
 import psycopg
@@ -32,10 +34,20 @@ def initialized_cursor(connection: Connection) -> Cursor:
 
 
 def main(
-    output: str = typer.Argument("generated"),
-    business: bool = typer.Option(False, help="generate business typedefs"),
-    client: bool = typer.Option(False, help="generate client typedefs"),
-    postgres_url: str = typer.Argument(default_postgres_url, envvar="POSTGRES_URL"),
+    output_dir: str = typer.Argument("generated", help="The output folder."),
+    postgres_url: str = typer.Argument(
+        default_postgres_url,
+        envvar="POSTGRES_URL",
+        help="A postgres url with a 'table_as_json_typedef' table",
+    ),
+    jtd_codegen: str = typer.Argument(
+        "jtd-codegen", envvar="JTD_CODEGEN", help="The jtd-codegen executable."
+    ),
+    business: bool = typer.Option(False, help="Generate business typedefs."),
+    client: bool = typer.Option(False, help="Generate client typedefs."),
+    remove_jtd_files: bool = typer.Option(
+        True, help="Remove intermediary json type def files from the output folder."
+    ),
 ):
     if (business and client) or (not business and not client):
         typer.echo("use either --business OR --client", err=True)
@@ -43,26 +55,58 @@ def main(
 
     if business:
         tables = business_tables
-        typer.echo("generating business typedefs...")
+        language = "python"
+        typer.echo("Generating business typedefs...")
     elif client:
         tables = client_tables
-        typer.echo("generating business typedefs...")
+        language = "typescript"
+        typer.echo("Generating client typedefs...")
 
     cursor = initialized_cursor(postgres_connection(postgres_url))
     cursor.execute("select * from table_as_json_typedef")
     schemas = cursor.fetchall()
-    for schema_row in schemas:
-        title = schema_row[0]
-        read = schema_row[1]
-        write = schema_row[2]
+    saved = []
 
-        if title in tables["read"]:
-            with open(os.path.join(output, f"{title}-read.json"), "w") as file:
-                json.dump(read, file, indent="  ")
+    with typer.progressbar(schemas) as progress:
+        for schema_row in progress:
+            title = schema_row[0]
+            read = schema_row[1]
+            write = schema_row[2]
 
-        if title in tables["write"]:
-            with open(os.path.join(output, f"{title}-write.json"), "w") as file:
-                json.dump(write, file, indent="  ")
+            if title in tables["read"]:
+                filename = f"{title}_read.jtd.json"
+                with open(os.path.join(output_dir, filename), "w") as file:
+                    json.dump(read, file, indent="  ")
+                    saved.append((f"{title}_read", filename))
+
+            if title in tables["write"]:
+                filename = f"{title}_write.jtd.json"
+                with open(os.path.join(output_dir, filename), "w") as file:
+                    json.dump(write, file, indent="  ")
+                    saved.append((f"{title}_write", filename))
+
+    typer.echo("Executing jtd-codegen")
+    with typer.progressbar(saved) as progress:
+        for title, filename in progress:
+            input = os.path.join(output_dir, filename)
+            output = os.path.join(output_dir, title)
+            Path(output).mkdir(exist_ok=True)
+            subprocess.run(
+                [
+                    jtd_codegen,
+                    input,
+                    f"--{language}-out",
+                    output,
+                ],
+                shell=True,
+            )
+            os.path.join(output_dir, filename)
+
+    if remove_jtd_files:
+        typer.echo("Removing intermediary json type def files.")
+        with typer.progressbar(saved) as progress:
+            for title, filename in progress:
+                os.remove(os.path.join(output_dir, filename))
 
 
 if __name__ == "__main__":
