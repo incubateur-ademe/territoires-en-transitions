9ccf1810cbc08911457932079286634f45ebfa6e
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-15 08:38:32 +0100

    Fix sorting order identifiant (10 after 2)

--- business/business/domain/use_cases/extract_referentiel_actions_to_csv.py
+++ business/business/domain/use_cases/extract_referentiel_actions_to_csv.py
@@ -1,6 +1,8 @@
 from dataclasses import asdict
 from functools import cmp_to_key
 
+import pandas as pd
+
 from business.domain.models import commands
 from business.domain.models.action_definition import ActionDefinition
 from business.domain.ports.referentiel_repo import AbstractReferentielRepository
@@ -16,14 +18,7 @@ class ExtractReferentielActionsToCsv(UseCase):
         self.referentiel_repo = referentiel_repo
 
     def execute(self, command: commands.ExtractReferentielActionsToCsv):
-        # definitions = self.referentiel_repo.get_all_definitions_from_referentiel(
-        #     command.referentiel
-        # )
 
-        # sorted_definitions = sorted(
-        #     definitions, key=cmp_to_key(self.compare_definition_identifiants)
-        # )
-        # sorted_action_ids = [definition.action_id for definition in sorted_definitions]
         points_by_id = {
             str(points.action_id): asdict(points)
             for points in self.referentiel_repo.get_all_points_from_referentiel(
@@ -37,7 +32,6 @@ class ExtractReferentielActionsToCsv(UseCase):
                 command.referentiel
             )
         }
-        import pandas as pd
 
         columns = ["identifiant", "nom", "value"]
         df = pd.concat([pd.DataFrame(points_by_id), pd.DataFrame(definitions_by_id)]).T[
@@ -45,31 +39,26 @@ class ExtractReferentielActionsToCsv(UseCase):
         ]
         df_sorted = df.sort_values(
             by="identifiant",
-            # key=cmp_to_key(self.compare_definition_identifiants),
+            key=lambda s: s.map(cmp_to_key(self.compare_definition_identifiants)),  # type: ignore
         )
         df_sorted.to_csv(command.csv_path)
 
-    # @staticmethod
-    # def compare_definition_identifiants(
-    #     definition_a: ActionDefinition, definition_b: ActionDefinition
-    # ) -> int:
-    #     breakpoint()
-    #     identifiant_a = definition_a.identifiant
-    #     identifiant_b = definition_b.identifiant
+    @staticmethod
+    def compare_definition_identifiants(identifiant_a: str, identifiant_b: str) -> int:
 
-    #     if identifiant_a == identifiant_b:
-    #         return 0
+        if identifiant_a == identifiant_b:
+            return 0
 
-    #     identifiant_a_array = identifiant_a.split(".")
-    #     identifiant_b_array = identifiant_b.split(".")
+        identifiant_a_array = identifiant_a.split(".")
+        identifiant_b_array = identifiant_b.split(".")
 
-    #     for k in range(min(len(identifiant_a_array), len(identifiant_b_array))):
-    #         index_a = identifiant_a_array[k]
-    #         index_b = identifiant_b_array[k]
+        for k in range(min(len(identifiant_a_array), len(identifiant_b_array))):
+            index_a = int(identifiant_a_array[k] or 0)
+            index_b = int(identifiant_b_array[k] or 0)
 
-    #         if index_a < index_b:
-    #             return -1
-    #         elif index_a > index_b:
-    #             return 1
-    #     # this means that the two identifiant have same root. Then parent is the one with shorter length.
-    #     return -1 if len(identifiant_a_array) < len(identifiant_b_array) else 1
+            if index_a < index_b:
+                return -1
+            elif index_a > index_b:
+                return 1
+        # this means that the two identifiant have same root. Then parent is the one with shorter length.
+        return -1 if len(identifiant_a_array) < len(identifiant_b_array) else 1
--- business/tests/unit/test_extract_referentiel_actions_to_csv.py
+++ business/tests/unit/test_extract_referentiel_actions_to_csv.py
@@ -20,7 +20,8 @@ def test_can_extract_referentiel_actions_to_csv():
     mkdir(csv_path.parent)
     remove_file(csv_path)
     children, definitions, points = make_dummy_referentiel(
-        action_ids=["eci", "eci_1", "eci_2", "eci_1.1", "eci_2.0"], referentiel="eci"
+        action_ids=["eci", "eci_1", "eci_2", "eci_1.1", "eci_2.0", "eci_10"],
+        referentiel="eci",
     )
     referentiel_repo = InMemoryReferentielRepository(children, definitions, points)
     use_case = ExtractReferentielActionsToCsv(referentiel_repo)
@@ -35,7 +36,9 @@ def test_can_extract_referentiel_actions_to_csv():
 
     df = pd.read_csv(csv_path, index_col=0)
     assert all(df.columns.values == ["identifiant", "nom", "value"])
-    assert all(df.index.values == ["eci", "eci_1", "eci_1.1", "eci_2", "eci_2.0"])
+    assert all(
+        df.index.values == ["eci", "eci_1", "eci_1.1", "eci_2", "eci_2.0", "eci_10"]
+    )
 
 
 def test_convert_eci_referentiel_actions_to_csv():
