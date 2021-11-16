0153079731d007a301bd2748b1132e31d826504e
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-16 20:13:17 +0100

    Change text in Audit Dialog

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/_AuditDialogEconomieCirculaire.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/_AuditDialogEconomieCirculaire.tsx
@@ -2,11 +2,9 @@ import {Checkbox} from '@material-ui/core';
 import * as R from 'ramda';
 
 const auditCheckListsEci = {
-  'Faire acte de candidature': [
-    "Le courrier d'acte de candidature (motivation et palier visé, précision des compétences, engagement à améliorer de façon continue sa politique ECi, identité et coordonnées du référent technique)",
-    'Le règlement du label signé',
-    'Les derniers statuts approuvés par l’autorité préfectorale ou arrêté préfectoral portant création de l’EPCI',
-    "Éventuellement la délibération d'engagement dans le processus de labellisation",
+  '': [
+    "Renseigner 100% du référentiel d'évaluation",
+    'Fournir les preuves attendues pour justifier les scores : Se référer à la rubrique "preuve" des différentes tâches',
   ],
   'Avoir un score minimal sur certaines sous-actions': [
     '100% à la sous-action 1.1.1',
@@ -14,8 +12,11 @@ const auditCheckListsEci = {
     "100% à la sous-action 3.2.0 (si le montant annuel d'achats de la collectivité est supérieur à 100 millions d'€)",
     "100% aux sous-actions 2.1.0, 2.2.0, 2.3.0 et 4.1.0 (c'est-à-dire respecter la règlementation si l'EPCI est concernée dans le cadre des compétences exercées)",
   ],
-  'Fournir les preuves attendues pour justifier les scores': [
-    'Se référer à la rubrique "preuve" des différentes tâches',
+  'Faire acte de candidature en fournissant :': [
+    "Le courrier d'acte de candidature (motivation et palier visé, précision des compétences, engagement à améliorer de façon continue sa politique ECi, identité et coordonnées du référent technique)",
+    'Le règlement du label signé',
+    'Les derniers statuts approuvés par l’autorité préfectorale ou arrêté préfectoral portant création de l’EPCI',
+    "Éventuellement la délibération d'engagement dans le processus de labellisation",
   ],
 };
 
@@ -31,11 +32,11 @@ export const AuditDialogEconomieCirculaire = () => (
     <div className="text-center fr-h3 -mt-4">ÉCONOMIE CIRCULAIRE</div>
     <div className="mt-6 flex  flex-col gap-4">
       <div className="text-xl">
-        Pour pouvoir faire une demande d'audit, il faut :
+        Pour pouvoir faire une demande d'audit, vous devez :
       </div>
       {R.keys(auditCheckListsEci).map(checkListTitle => (
         <div key={checkListTitle}>
-          <div className="font-semibold text-xl mb-4">{checkListTitle}</div>
+          <div className="font-semibold text-lg mb-4">{checkListTitle}</div>
           <div className="flex flex-col gap-4">
             {auditCheckListsEci[checkListTitle].map(checkListItem => (
               <div key={checkListItem}>
@@ -46,6 +47,11 @@ export const AuditDialogEconomieCirculaire = () => (
           </div>
         </div>
       ))}
+      <div>
+        Une fois les conditions remplies, merci de signaler votre intention de
+        demande d'audit au Bureau d'Appui par mail à dteci@ademe.fr qui vous
+        informera de la procédure à suivre.
+      </div>
     </div>
   </div>
 );
