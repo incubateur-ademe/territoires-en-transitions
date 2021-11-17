9b96eabd98290007e514d73d8f40ca765dc149ef
Florian d'Erfurth <florian@derfurth.com>
2021-11-17 16:54:36 +0100

    fix repo tests

--- app.territoiresentransitions.react/src/core-logic/api/repositories/ActionCommentaireRepository.spec.ts
+++ app.territoiresentransitions.react/src/core-logic/api/repositories/ActionCommentaireRepository.spec.ts
@@ -5,7 +5,7 @@ describe('Action-Commentaire repo should retrieve data-layer default commentaire
   it('Retrieves the default commentaire of action cae_1.2.3', async () => {
     const result = await actionCommentaireRepository.fetch({
       epciId: 1,
-      actionId: 'cae_1.2.3',
+      actionId: 'cae_1.2.3.1',
     });
     expect(result).not.toBeNull();
     expect(result!.epci_id).toEqual(1);
