ff04a2b665ec07f2e4dfcff2acb4ec3d202db33c
Florian d'Erfurth <florian@derfurth.com>
2021-11-18 17:18:54 +0100

    wip sql score handling

--- data_layer/postgres/development.sql
+++ data_layer/postgres/development.sql
@@ -100,29 +100,120 @@ from action_statut
 --------------------------------
 ----------- SCORE --------------
 --------------------------------
+
 create table score
 (
     id                     serial primary key,
-    epci_id                integer references epci                            not null,
-    action_id              action_id references action_relation               not null,
-    points                 real                                               not null,
-    potentiel              real                                               not null,
-    referentiel_points     real                                               not null,
-    concernee              bool                                               not null,
-    previsionnel           real                                               not null,
-    total_taches_count     int                                                not null,
-    completed_taches_count int                                                not null,
-    created_at             timestamp with time zone default CURRENT_TIMESTAMP not null
+    epci_id                integer references epci                not null,
+    action_id              action_id references action_relation   not null,
+    points                 real                                   not null,
+    potentiel              real                                   not null,
+    referentiel_points     real                                   not null,
+    concernee              bool                                   not null,
+    previsionnel           real                                   not null,
+    total_taches_count     int                                    not null,
+    completed_taches_count int                                    not null,
+    created_at             timestamp with time zone default Now() not null
 );
 
+comment on table score is 'Score data is created by the business';
+comment on column score.created_at is
+    'Used to group scores in batches because rows created during a transaction have the same values';
 
-create view client_score
+create table client_scores
+(
+    id               serial primary key,
+    epci_id          integer references epci  not null,
+    referentiel      referentiel              not null,
+    scores           jsonb                    not null,
+    score_created_at timestamp with time zone not null
+);
+comment on table client_scores is 'Client score data is generated from score on trigger';
+comment on column client_scores.score_created_at is 'Equal score.created_at.';
+
+
+create or replace function
+    get_score_batches_for_epci(
+    epci_id integer
+)
+    returns table
+            (
+                epci_id     int,
+                referentiel referentiel,
+                scores      jsonb,
+                created_at  timestamptz
+            )
 as
-select action_id, referentiel, points, potentiel
+$$
+select score.epci_id,
+       action_relation.referentiel,
+       jsonb_agg(
+               jsonb_build_object(
+                       'action_id', action_id,
+                       'point', points,
+                       'potentiel', potentiel,
+                       'referentiel', action_relation.referentiel,
+                       'referentiel_points', referentiel_points,
+                       'concernee', concernee,
+                       'previsionnel', previsionnel,
+                       'total_taches_count', total_taches_count,
+                       'completed_taches_count', completed_taches_count
+                   )
+           ) as scores,
+       created_at
 from score
-         join action_relation on action_id = action_relation.id;
+         join action_relation on
+    action_id = action_relation.id
+where score.epci_id = $1
+group by score.epci_id, action_relation.referentiel, created_at;
+$$ language sql;
 
 
+create or replace function
+    should_create_client_scores_for_epci(
+    epci_id integer,
+    created timestamp with time zone
+) returns bool
+as
+$$
+select count(*) > 0
+from score
+where score.epci_id = $1
+  and score.created_at = $2;
+$$ language sql;
+
+
+
+create or replace function after_score_update_insert_client_scores() returns trigger as
+$$
+declare
+    existingClientScoreCount bool;
+begin
+    -- find existing client scores
+    select should_create_client_scores_for_epci(NEW.epci_id, NEW.created_at)
+    into existingClientScoreCount;
+
+    if existingClientScoreCount
+    then
+        -- insert client score
+        insert into client_scores (epci_id, referentiel, scores, score_created_at)
+        select batches.epci_id,
+               batches.referentiel,
+               batches.scores,
+               batches.created_at
+        from get_score_batches_for_epci(NEW.epci_id) as batches;
+    end if;
+    return null;
+end;
+$$ language 'plpgsql';
+
+create constraint trigger after_score_write
+    after update or insert
+    on score
+    deferrable
+        initially deferred
+    for each row
+execute procedure after_score_update_insert_client_scores();
 
 --------------------------------
 --------ACTION COMMENTAIRE------
@@ -268,6 +359,3 @@ select title,
 from json_type_def;
 comment on view table_as_json_typedef is
     'Json type definition for all public tables (including views). Only non nullable/non default fields are listed';
-
-select *
-from table_as_json_typedef;
---
+++ data_layer/postgres/tests/14-insert_fake_scores.sql
@@ -0,0 +1,12 @@
+insert into score
+values (default,
+        1,
+        'cae_1.2.3',
+        0,
+        10,
+        10,
+        true,
+        10,
+        10,
+        7,
+        default);
