fb78b48edd2447158b3cec35bccc1543dac29760
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-18 18:57:25 +0100

    Broken test : integration/test_triggers :p

--- data_layer/tests/utils/sql_factories.py
+++ data_layer/tests/utils/sql_factories.py
@@ -26,3 +26,29 @@ def make_sql_insert_user(user_uid="17440546-f389-4d4f-bfdb-b0c94a1bd0f9"):
     return f"""
     insert into auth.users(id) values ('{user_uid}');
     """
+
+
+def make_sql_insert_score(
+    epci_id=1,
+    action_id="cae_1.2.3",
+    points=100,
+    potentiel=100,
+    referentiel_points=100,
+    concernee=True,
+    previsionnel=100,
+    total_taches_count=9,
+    completed_taches_count=6,
+    created_at="2021-01-01",
+):
+    sql = f"""insert into score (epci_id, action_id, points, potentiel, referentiel_points, concernee, previsionnel, total_taches_count, completed_taches_count, created_at)
+            values ({epci_id},
+                    '{action_id}',
+                    {points},
+                    {potentiel},
+                    {referentiel_points},
+                    {concernee},
+                    {previsionnel},
+                    {total_taches_count},
+                    {completed_taches_count},
+                    '{created_at}'::timestamptz);"""
+    return sql
