14ac3c13b907bbdeca8402706b1f75da86405a70
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-11-26 18:12:25 +0100

    WIP start new screen to add EPCI

--- app.territoiresentransitions.react/src/app/pages/Epcis/_AddDialog.tsx
+++ app.territoiresentransitions.react/src/app/pages/Epcis/_AddDialog.tsx
@@ -1,5 +1,6 @@
 import Dialog from '@material-ui/core/Dialog';
 import {commands} from 'core-logic/commands';
+import {EpciRead} from 'generated/dataLayer/epci_read';
 import React from 'react';
 import {EpciStorable} from 'storables';
 import {LabeledTextInput, SelectInput} from 'ui';
@@ -7,63 +8,40 @@ import {LabeledTextInput, SelectInput} from 'ui';
 export interface AddDialogProps {
   open: boolean;
   close: () => void;
-  epcis: EpciStorable[];
+  epcis: EpciRead[];
 }
 
 export const AddDialog = (props: AddDialogProps) => {
   const {open, close, epcis} = props;
   const selectInputValues = epcis.map(epciStorable => ({
-    value: epciStorable.id,
+    value: epciStorable.siren,
     label: epciStorable.nom,
   }));
 
-  const [selectedEpciId, setSelectedEpciId] = React.useState('');
-  const [inputEpciNom, setInputEpciNom] = React.useState('');
+  const [selectedEpciSiren, setSelectedEpciSiren] = React.useState<string>();
+  // const [inputEpciNom, setInputEpciNom] = React.useState('');
 
   // const overmindActions = useActions();
 
   const submit = async () => {
-    if (selectedEpciId) {
-      // Add rights.
-      // const auth = await import("../../api/authentication");
-      // await auth.addDroits(selectedEpciId, true);
-      await commands.epcisCommands.addEpciToMyList(selectedEpciId);
-    } else if (inputEpciNom.trim()) {
-      // Create Epci
-      await commands.epcisCommands.createNewEpci(inputEpciNom);
-    }
-
+    console.log('Should add EPCI ', selectedEpciSiren, ' to user rights.... ');
+    // await commands.epcisCommands.addEpciToMyList(selectedEpciId);
     close();
   };
 
   return (
     <Dialog open={open} onClose={close} maxWidth="md" fullWidth={true}>
       <div className="py-7">
-        <h3 className="text-center pb-4"> Ajouter ma collectivité </h3>
-        <div className="flex flex-row w-full">
-          <div className="flex flex-col w-1/2 p-5">
-            <h1 className="text-xl">Ma collectivité a déjà un compte</h1>
-            <SelectInput
-              label="Sélectionner une collectivité"
-              options={selectInputValues}
-              defaultValue=""
-              onChange={epciId => {
-                setSelectedEpciId(epciId);
-              }}
-            />
-          </div>
-          <div className="flex flex-col w-1/2 p-5">
-            <h1 className="text-xl">
-              Ma collectivité n'a pas encore de compte
-            </h1>
-            <LabeledTextInput
-              label="Créer une collectivité"
-              maxLength={100}
-              onChange={event => {
-                setInputEpciNom(event.target.value);
-              }}
-            />
-          </div>
+        <h3 className="text-center pb-4"> Rejoindre ma collectivité </h3>
+        <div className="flex flex-row justify-center">
+          <SelectInput
+            label="Sélectionner une collectivité"
+            options={selectInputValues}
+            defaultValue=""
+            onChange={epciSiren => {
+              setSelectedEpciSiren(epciSiren);
+            }}
+          />
         </div>
 
         <div className="flex justify-center mt-8">
@@ -71,7 +49,7 @@ export const AddDialog = (props: AddDialogProps) => {
             className="fr-btn fr-btn--secondary fr-btn--sm"
             onClick={submit}
           >
-            Ajouter
+            Rejoindre
           </button>
         </div>
       </div>
--- app.territoiresentransitions.react/src/core-logic/api/observables/ScoreObservable.ts
+++ app.territoiresentransitions.react/src/core-logic/api/observables/ScoreObservable.ts
@@ -1,7 +1,7 @@
 import {Observable} from 'rxjs';
 
 class ScoreObservable {
-  private epciId: number;
-  private referentielId: string;
-  private socket: ScoreSocket;
+  // private epciId: number;
+  // private referentielId: string;
+  // private socket: ScoreSocket;
 }
