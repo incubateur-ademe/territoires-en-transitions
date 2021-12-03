94d06002cb21d3c300ff76ce8be9aa3c109899f2
Florian d'Erfurth <florian@derfurth.com>
2021-12-03 11:45:20 +0100

    typing works with supabase docker

--- data_layer/postgres/fakes/11-insert_fake_user.sql
+++
@@ -1,8 +0,0 @@
-set search_path to public;
-
--- password yolododo
-INSERT INTO auth.users (id, email, encrypted_password) VALUES ('17440546-f389-4d4f-bfdb-b0c94a1bd0f9', 'yolo@dodo.com', '$2a$10$n8hCY1kKn3BIX56bB3LlQuudbH0m5C7Oqr2dJ8LsIMvlr5JWNGyC.');
-
--- password yulududu
-INSERT INTO auth.users (id, email, encrypted_password) VALUES ('c98235a0-60e7-4ceb-9172-0a991cce0386', 'yulu@dudu.com', '$2a$10$opicVZ0IKapg1o76c6hMPOYsNwx8mKsCBwLSbZlnwvKvgk6n0bMMq');
-
