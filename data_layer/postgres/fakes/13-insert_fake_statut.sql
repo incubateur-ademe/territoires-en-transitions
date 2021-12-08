4bc20947d4c6c8916a1d0e6c6374f1b3f40281cb
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-12-08 15:39:09 +0100

    View elses_collectivite

--- data_layer/postgres/fakes/13-insert_fake_statut.sql
+++ data_layer/postgres/fakes/13-insert_fake_statut.sql
@@ -3,7 +3,7 @@
 -- - 11-insert_fake_epcis.sql
 -- - 10-insert_fake_user.sql
 insert into
-    action_statut(epci_id, action_id, avancement, concerne, modified_by)
+    action_statut(collectivite_id, action_id, avancement, concerne, modified_by)
     values (1, 'cae_1.1.1.1.1', 'fait', true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
 
 
