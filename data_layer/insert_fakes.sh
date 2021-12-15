efe7ff655ffd96f01f67e67ec265a871e21868f2
Florian d'Erfurth <florian@derfurth.com>
2021-12-15 21:38:10 +0100

    fake plan

--- data_layer/insert_fakes.sh
+++ data_layer/insert_fakes.sh
@@ -3,3 +3,4 @@ psql --host localhost --port 50001 --dbname postgres --user postgres --file "./p
 # psql --host localhost --port 50001 --dbname postgres --user postgres --file "./postgres/fakes/12-insert_fake_referentiel.sql"
 psql --host localhost --port 50001 --dbname postgres --user postgres --file "./postgres/fakes/13-insert_fake_statut.sql"
 psql --host localhost --port 50001 --dbname postgres --user postgres --file "./postgres/fakes/14-insert_fake_action_commentaire.sql"
+psql --host localhost --port 50001 --dbname postgres --user postgres --file "./postgres/fakes/15-insert_fake_plan_action.sql"
