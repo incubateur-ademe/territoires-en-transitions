17ef626c72cd46a04ed363a32ac4a453942c2e4d
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-12-27 17:55:58 +0100

    Add indicateur lie in ActionReferentielAvancement component

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionReferentielAvancement.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionReferentielAvancement.tsx
@@ -6,24 +6,50 @@ import {
   ProgressStatStatic,
 } from 'ui/referentiels';
 import 'app/DesignSystem/buttons.css';
-import {Spacer} from 'ui/shared';
+import {AddFicheActionButton, Spacer} from 'ui/shared';
 import {DescriptionContextAndRessourcesDialogButton} from './_DescriptionContextAndRessourcesDialogButton';
 import {OrientationQuickNav} from 'app/pages/collectivite/Referentiels/QuickNav';
 import {searchActionById} from 'utils/actions';
 import {IndicateurReferentielCard} from 'app/pages/collectivite/Indicateurs/IndicateurReferentielCard';
 import {IndicateurDefinitionRead} from 'generated/dataLayer/indicateur_definition_read';
 import {scoreBloc} from 'core-logic/observables/scoreBloc';
+import {indicateurActionReadEndpoint} from 'core-logic/api/endpoints/IndicateurActionReadEndpoint';
+import {useEffect, useState} from 'react';
+import {useAllIndicateurDefinitions} from 'core-logic/hooks/indicateur_definition';
 
+const useActionLinkedIndicateurDefinitions = (actionId: string) => {
+  const [linkedIndicateurDefinitions, setLinkedIndicateurDefinitions] =
+    useState<IndicateurDefinitionRead[]>([]);
+
+  const allIndicateurDefinitions = useAllIndicateurDefinitions();
+  useEffect(() => {
+    indicateurActionReadEndpoint.getBy({}).then(allIndicateurActions => {
+      const linkedIndicateurDefinitions = allIndicateurActions
+        .filter(indicateurAction => indicateurAction.action_id === actionId)
+        .map(linkedIndicateurAction =>
+          allIndicateurDefinitions.find(
+            indicateurDefinition =>
+              indicateurDefinition.id === linkedIndicateurAction.indicateur_id
+          )
+        );
+
+      setLinkedIndicateurDefinitions(
+        linkedIndicateurDefinitions.filter(
+          definition => !!definition
+        ) as IndicateurDefinitionRead[]
+      );
+    });
+  }, [allIndicateurDefinitions]);
+  return linkedIndicateurDefinitions;
+};
 const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
   const action = searchActionById(actionId, referentielActions);
   if (!action) {
     return <Link to="./referentiels" />;
   }
-  // TODO !
-  const relatedIndicateurDefinitions: IndicateurDefinitionRead[] = [];
-  // const relatedIndicateurs = indicateurs.filter(indicateur =>
-  //   isIndicateurRelatedToAction(indicateur, action)
-  // );
+  const actionLinkedIndicateurDefinitions =
+    useActionLinkedIndicateurDefinitions(actionId);
+
   return (
     <div className="fr-container">
       <div className="mt-8 mb-16">
@@ -33,7 +59,7 @@ const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
             className="fr-h1 mb-3 w-9/12 text-gray-900"
             action={action}
           />
-          {/* <AddFicheActionButton actionId={action.id} /> */}
+          <AddFicheActionButton actionId={action.id} />
         </div>
         <ProgressStatStatic
           action={action}
@@ -62,11 +88,11 @@ const ActionReferentielAvancement = ({actionId}: {actionId: string}) => {
       <Spacer />
       <section>
         <h2 className="fr-h2">Les indicateurs</h2>
-        {relatedIndicateurDefinitions.length === 0 && (
+        {actionLinkedIndicateurDefinitions.length === 0 && (
           <p>Cette action ne comporte pas d'indicateur</p>
         )}
 
-        {relatedIndicateurDefinitions.map(definition => (
+        {actionLinkedIndicateurDefinitions.map(definition => (
           <IndicateurReferentielCard
             key={definition.id}
             definition={definition}
