a6d0156ecfcaaf318fb3658aeaa97d50eb48df6f
derfurth <florian@derfurth.com>
2022-01-06 19:24:44 +0100

    round domaines

--- app.territoiresentransitions.react/src/utils/toFixed.ts
+++ app.territoiresentransitions.react/src/utils/toFixed.ts
@@ -1,7 +1,8 @@
 const approxEqual = (a: number, b: number) => Math.abs(a - b) < 0.0001;
 
-export const toFixed = (n: number) => {
-  if (approxEqual(Math.round(n), n)) return n.toFixed(0);
-  else if (approxEqual(Math.round(n * 10), n * 10)) return n.toFixed(1);
-  else return n.toFixed(1);
+export const toFixed = (n: number, d?: number) => {
+  d = d === undefined ? 1 : d;
+  if (approxEqual(Math.round(n), n)) return n.toFixed(d);
+  else if (approxEqual(Math.round(n * 10), n * 10)) return n.toFixed();
+  else return n.toFixed(d);
 };
