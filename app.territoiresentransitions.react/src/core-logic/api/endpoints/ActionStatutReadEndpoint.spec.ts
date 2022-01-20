0c77d3b373246ab39705868c2135221da79be425
derfurth <florian@derfurth.com>
2022-01-20 15:26:58 +0100

    Add authentication to collectivité data tests

--- app.territoiresentransitions.react/src/core-logic/api/endpoints/ActionStatutReadEndpoint.spec.ts
+++ app.territoiresentransitions.react/src/core-logic/api/endpoints/ActionStatutReadEndpoint.spec.ts
@@ -1,7 +1,13 @@
 import '@testing-library/jest-dom/extend-expect';
 import {actionStatutReadEndpoint} from 'core-logic/api/endpoints/ActionStatutReadEndpoint';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {yiliCredentials} from 'test_utils/collectivites';
 
 describe('Action-statut reading endpoint should retrieve data-layer default statuses', () => {
+  beforeEach(async () => {
+    await supabaseClient.auth.signIn(yiliCredentials);
+  });
+
   it('Retrieves at least one status when collectivite_id is given', async () => {
     const results = await actionStatutReadEndpoint.getBy({collectivite_id: 1});
     expect(actionStatutReadEndpoint.lastResponse?.status).toBe(200);
