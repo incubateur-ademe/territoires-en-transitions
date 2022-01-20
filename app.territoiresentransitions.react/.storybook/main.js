2be839ec4c6c312fd171c78db74d32bac009767f
Marc Rutkowski <marc@attractive-media.fr>
2022-01-20 18:18:39 +0100

    ajoute la configuration et les deps nécessaires au storybook

---
+++ app.territoiresentransitions.react/.storybook/main.js
@@ -0,0 +1,25 @@
+/**
+ * Configuration générale du storybook
+ */
+
+module.exports = {
+  // pattern de recherche des stories
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  // extensions chargées
+  addons: [
+    // pour faire des liens entre les stories
+    '@storybook/addon-links',
+    // les extensions les plus utilisées (Actions, Controls, Docs...)
+    '@storybook/addon-essentials',
+    // pour charger la surcharge de la config create-react-app fournie par craco
+    'storybook-preset-craco',
+    // et la faire fonctionner avec le addon Docs
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+  framework: '@storybook/react',
+};
