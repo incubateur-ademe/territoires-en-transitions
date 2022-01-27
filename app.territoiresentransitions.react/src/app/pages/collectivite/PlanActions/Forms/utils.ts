377574bcc47bf01a49a5f92ea2c54a5089f5a568
Florian d'Erfurth <florian@derfurth.com>
2022-01-27 15:43:51 +0100

    fix emoji action title

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/Forms/utils.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/Forms/utils.ts
@@ -1,11 +1,11 @@
 import {refToEmoji} from 'utils/refToEmoji';
-import {ActionReferentiel} from 'types/action_referentiel';
 import {IndicateurReferentiel} from 'types/indicateur_referentiel';
 import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
+import {ActionTitleRead} from 'core-logic/api/endpoints/ActionTitleReadEndpoint';
 
 export const actionToEmoji = (
-  action: ActionReferentiel | ActionDefinitionSummary
-) => (action.id.startsWith('eco') ? refToEmoji['eci'] : refToEmoji['cae']);
+  action: ActionDefinitionSummary | ActionTitleRead
+) => (action.referentiel === 'eci' ? refToEmoji['eci'] : refToEmoji['cae']);
 
 export const indicateurToEmoji = (indicateur: IndicateurReferentiel) =>
   indicateur.id.startsWith('eco') ? refToEmoji['eci'] : refToEmoji['cae'];
