89a9b9fe22761991ef524a785788fec041870239
Florian d'Erfurth <florian@derfurth.com>
2022-01-27 15:43:29 +0100

    update fiche action picker to use titles endpoint

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/Forms/ActionsField.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlanActions/Forms/ActionsField.tsx
@@ -1,21 +1,10 @@
 import {FC} from 'react';
 import {FieldProps} from 'formik';
 import {v4 as uuid} from 'uuid';
-import {actionsById} from 'utils/actions';
-import {actions} from 'generated/data/referentiels';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import {actionToEmoji, shortenLabel} from './utils';
-
-const allActions = actionsById(actions);
-const allActionIds = [...allActions.keys()]
-  .sort((a, b) => a.localeCompare(b))
-  .filter(id => !['eci', 'cae'].includes(id)); // filter root actions
-
-const renderActionOption = (id: string) => {
-  const action = allActions.get(id)!;
-  return `${actionToEmoji(action)} ${action.id_nomenclature} - ${action.nom}`;
-};
+import {useActionTitleList} from 'core-logic/hooks/referentiel';
 
 type ActionsFieldProps = {
   label: string;
@@ -25,8 +14,6 @@ type ActionsFieldProps = {
 
 /**
  * A material UI tags field (multi picker) for action ids.
- *
- * Could use generics.
  */
 export const ActionsField: FC<ActionsFieldProps & FieldProps> = ({
   field, // { name, value, onChange, onBlur }
@@ -36,6 +23,15 @@ export const ActionsField: FC<ActionsFieldProps & FieldProps> = ({
   const htmlId = props.id ?? uuid();
   const errorMessage = errors[field.name];
   const isTouched = touched[field.name];
+  const titles = useActionTitleList('all');
+
+  const allActionIds = titles
+    .filter(title => title.type !== 'referentiel')
+    .map(title => title.id);
+  const renderActionOption = (actionId: string) => {
+    const title = titles.find(title => title.id === actionId)!;
+    return `${actionToEmoji(title)} ${title.identifiant} - ${title.nom}`;
+  };
 
   return (
     <fieldset>
