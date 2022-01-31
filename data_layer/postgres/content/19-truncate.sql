6ff4e48724999a77427a55682d1f7726e8c8fc57
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-01-31 15:37:43 +0100

    Regenerate the sql content files from latest markdonws

--- data_layer/postgres/content/19-truncate.sql
+++ data_layer/postgres/content/19-truncate.sql
@@ -1,1 +1,1 @@
-TRUNCATE collectivite, epci, action_definition, action_relation, action_computed_points, indicateur_definition RESTART IDENTITY CASCADE; 
+TRUNCATE action_definition, action_computed_points, indicateur_action, indicateur_commentaire, indicateur_resultat, indicateur_objectif, fiche_action_indicateur, indicateur_definition RESTART IDENTITY CASCADE; 
