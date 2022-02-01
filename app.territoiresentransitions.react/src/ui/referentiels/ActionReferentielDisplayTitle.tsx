7a3f5c76867a3b18416b6b042c62a169717d94a3
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-02-01 08:26:35 +0100

    ActionRefTitle centering and margin (scroll fix)

--- app.territoiresentransitions.react/src/ui/referentiels/ActionReferentielDisplayTitle.tsx
+++ app.territoiresentransitions.react/src/ui/referentiels/ActionReferentielDisplayTitle.tsx
@@ -78,12 +78,14 @@ export const ActionReferentielDisplayTitle = ({
   action: ActionReferentiel;
 }) => {
   return (
-    <div className="flex flex-row align-middle items-center font-bold gap-2">
-      <ActionReferentielTitlePill action={action} />
-      <div className="py-2">
-        <span className="fr-text--lg">{action.nom} </span>
-        <ActionPotentiel action={action} scoreBloc={scoreBloc} />
+    <div className="flex justify-between my-6 items-center">
+      <div className="flex flex-row align-middle items-center font-bold gap-2">
+        <ActionReferentielTitlePill action={action} />
+        <div>
+          <span className="fr-text--lg">{action.nom} </span>
+        </div>
       </div>
+      <ActionPotentiel action={action} scoreBloc={scoreBloc} />
     </div>
   );
 };
