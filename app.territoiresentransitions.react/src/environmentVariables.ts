839c76264bf5f9ea78ff5f42c49e3a1b64f2291f
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-02-05 09:55:16 +0100

    Fix CI env variables

--- app.territoiresentransitions.react/src/environmentVariables.ts
+++ app.territoiresentransitions.react/src/environmentVariables.ts
@@ -1,17 +1,13 @@
-const getBackendHost = () => {
-  if (process.env.REACT_APP_FLAVOR === 'sandbox')
-    return 'https://sandboxterritoires.osc-fr1.scalingo.io';
-  else if (process.env.REACT_APP_FLAVOR === 'prod')
-    return 'https://territoiresentransitions.osc-fr1.scalingo.io';
-  else return 'http://localhost:8000';
-};
-
 export const ENV = {
   node_env: process.env.NODE_ENV,
   logActionsDuration: process.env.REACT_APP_LOG_ACTION_DURATION === 'TRUE',
-  backendHost: getBackendHost(),
-  supabase_anon_key: process.env.REACT_APP_SUPABASE_KEY,
+  supabase_anon_key:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIwODU0MCwiZXhwIjoxOTc0MzYzNzQwfQ.zcaQfHd3VA7XgJmdGfmV86OLVJT9s2MTmSy-e69BpUY', //process.env.REACT_APP_SUPABASE_KEY,
   supabase_url: process.env.REACT_APP_SUPABASE_URL,
 };
 
+console.log(
+  'process.env.REACT_APP_SUPABASE_URL : ',
+  process.env.REACT_APP_SUPABASE_URL
+);
 console.log('ENV :', JSON.stringify(ENV, null, 2));
