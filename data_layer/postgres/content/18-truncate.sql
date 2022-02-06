b8cd6df25f86b814fad5fe09e9c179cd1b739e5d
Florian d'Erfurth <florian@derfurth.com>
2022-02-06 14:30:27 +0100

    add tables to truncate

--- data_layer/postgres/content/18-truncate.sql
+++ data_layer/postgres/content/18-truncate.sql
@@ -1,1 +1,3 @@
-TRUNCATE action_definition, action_computed_points, indicateur_action, indicateur_commentaire, indicateur_resultat, indicateur_objectif, fiche_action_indicateur, indicateur_definition RESTART IDENTITY CASCADE; 
+TRUNCATE epci, commune, collectivite, action_definition, action_computed_points, indicateur_action,
+    indicateur_commentaire,
+    indicateur_resultat, indicateur_objectif, fiche_action_indicateur, indicateur_definition RESTART IDENTITY CASCADE;
