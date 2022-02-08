6647d1ea627ae0463435549b09e1971e8143a862
Marc Rutkowski <marc@attractive-media.fr>
2022-02-08 15:40:52 +0100

    désactive le log ENV en mode test et utilise la var. d'env. pour la clé supabase

--- app.territoiresentransitions.react/src/environmentVariables.ts
+++ app.territoiresentransitions.react/src/environmentVariables.ts
@@ -1,13 +1,10 @@
 export const ENV = {
   node_env: process.env.NODE_ENV,
   logActionsDuration: process.env.REACT_APP_LOG_ACTION_DURATION === 'TRUE',
-  supabase_anon_key:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIwODU0MCwiZXhwIjoxOTc0MzYzNzQwfQ.zcaQfHd3VA7XgJmdGfmV86OLVJT9s2MTmSy-e69BpUY', //process.env.REACT_APP_SUPABASE_KEY,
+  supabase_anon_key: process.env.REACT_APP_SUPABASE_KEY,
   supabase_url: process.env.REACT_APP_SUPABASE_URL,
 };
 
-console.log(
-  'process.env.REACT_APP_SUPABASE_URL : ',
-  process.env.REACT_APP_SUPABASE_URL
-);
-console.log('ENV :', JSON.stringify(ENV, null, 2));
+if (ENV.node_env !== 'test') {
+  console.log('ENV :', JSON.stringify(ENV, null, 2));
+}
