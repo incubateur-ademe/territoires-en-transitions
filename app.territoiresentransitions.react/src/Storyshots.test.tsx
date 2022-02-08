73358eb7073548a3ac7d4d4f62a79de15aa5d920
Marc Rutkowski <marc@attractive-media.fr>
2022-02-08 14:42:19 +0100

    active le snapshot testing pour les stories (storyshots)

---
+++ app.territoiresentransitions.react/src/Storyshots.test.tsx
@@ -0,0 +1,35 @@
+/*
+  Réalise un "snapshot testing" de toutes les stories du storybook
+  Ref: https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-core
+*/
+
+import initStoryshots, {
+  Stories2SnapsConverter,
+} from '@storybook/addon-storyshots';
+import {act, create} from 'react-test-renderer';
+
+const converter = new Stories2SnapsConverter();
+
+initStoryshots({
+  asyncJest: true,
+  // fonction de test spécifique pour éviter certains avertissements
+  // Ref: https://github.com/storybookjs/storybook/issues/7745
+  test: async ({story, context, done}) => {
+    const filename = converter.getSnapshotFileName(context);
+    if (!filename) {
+      return;
+    }
+
+    // rendu de la story
+    const renderer = create(story.render());
+
+    // attends le changement d'état
+    await act(() => new Promise(resolve => setTimeout(resolve)));
+
+    // vérifie le storyshot
+    expect(renderer.toJSON()).toMatchSpecificSnapshot(filename);
+
+    renderer.unmount();
+    done();
+  },
+});
