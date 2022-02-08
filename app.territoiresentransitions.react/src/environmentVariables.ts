26e4855447b82fb0f0fba9f8a7b735b1f3e6e890
Florian d'Erfurth <florian@derfurth.com>
2022-02-08 15:39:32 +0100

    fix hardcoded env var

--- app.territoiresentransitions.react/src/environmentVariables.ts
+++ app.territoiresentransitions.react/src/environmentVariables.ts
@@ -1,8 +1,7 @@
 export const ENV = {
   node_env: process.env.NODE_ENV,
   logActionsDuration: process.env.REACT_APP_LOG_ACTION_DURATION === 'TRUE',
-  supabase_anon_key:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIwODU0MCwiZXhwIjoxOTc0MzYzNzQwfQ.zcaQfHd3VA7XgJmdGfmV86OLVJT9s2MTmSy-e69BpUY', //process.env.REACT_APP_SUPABASE_KEY,
+  supabase_anon_key: process.env.REACT_APP_SUPABASE_KEY,
   supabase_url: process.env.REACT_APP_SUPABASE_URL,
 };
 
