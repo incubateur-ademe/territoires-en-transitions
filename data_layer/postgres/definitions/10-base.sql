92a8d9e5c90102213af0367a3ca77882209f2467
Florian d'Erfurth <florian@derfurth.com>
2022-02-10 16:18:13 +0100

    add is service_role RPC

--- data_layer/postgres/definitions/10-base.sql
+++ data_layer/postgres/definitions/10-base.sql
@@ -11,3 +11,29 @@ begin
                              'hint', 'Tip it over and pour it out.');
 end;
 $$ language plpgsql;
+
+
+create or replace function
+    is_authenticated()
+    returns boolean
+as
+$$
+begin
+    return auth.role() = 'authenticated';
+end;
+$$ language plpgsql;
+comment on function is_authenticated is
+    'Returns true if current user is authenticated.';
+
+
+create or replace function
+    is_service_role()
+    returns text
+as
+$$
+begin
+    return auth.role() = 'service_role';
+end;
+$$ language plpgsql;
+comment on function is_service_role is
+    'Returns true if current user is the service role, that is using the secret key.';
