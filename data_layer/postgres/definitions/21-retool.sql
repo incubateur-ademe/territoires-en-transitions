882796108bc3167e2300d857919e95dbf218c0c1
Florian d'Erfurth <florian@derfurth.com>
2022-02-14 10:24:50 +0100

    retool user droits shape

--- data_layer/postgres/definitions/21-retool.sql
+++ data_layer/postgres/definitions/21-retool.sql
@@ -1,14 +1,18 @@
 create or replace function
     retool_user_list()
     returns table(
-        droits private_utilisateur_droit,
-        personne dcp
+        droit_id integer,
+        nom text,
+        prenom text,
+        email text,
+        collectivite text
     )
 as
 $$
-select d as droits, p as utlisateur
+select d.id, p.nom, p.prenom, p.email, nc.nom
 from private_utilisateur_droit d
     join dcp p on p.user_id = d.user_id
+    join named_collectivite nc on d.collectivite_id = nc.collectivite_id
 where is_service_role()
 $$ language sql;
 comment on function retool_user_list is
