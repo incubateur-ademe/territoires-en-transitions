9bfdc1e94e759c96f529e5d5c4808aee18bd3ce2
Florian d'Erfurth <florian@derfurth.com>
2022-02-15 17:23:56 +0100

    Update existing action definitions rpc; rename business_upsert_indicateurs

--- data_layer/postgres/definitions/22-business_rpc.sql
+++ data_layer/postgres/definitions/22-business_rpc.sql
@@ -1,4 +1,4 @@
-create or replace function upsert_indicateurs(
+create or replace function business_upsert_indicateurs(
     indicateur_definitions indicateur_definition[],
     indicateur_actions indicateur_action[]
 ) returns void as
@@ -10,7 +10,7 @@ begin
     if is_service_role()
     then
         -- upsert definition
-        foreach def in array indicateur_definitions
+        foreach def in array business_upsert_indicateurs.indicateur_definitions
             loop
                 insert into indicateur_definition
                 values (default,
@@ -25,13 +25,13 @@ begin
                         def.parent)
                 on conflict (id)
                     do update set indicateur_group  = def.indicateur_group,
-                                          identifiant       = def.identifiant,
-                                          valeur_indicateur = def.valeur_indicateur,
-                                          nom               = def.nom,
-                                          description       = def.description,
-                                          unite             = def.unite,
-                                          obligation_eci    = def.obligation_eci,
-                                          parent            = def.parent;
+                                  identifiant       = def.identifiant,
+                                  valeur_indicateur = def.valeur_indicateur,
+                                  nom               = def.nom,
+                                  description       = def.description,
+                                  unite             = def.unite,
+                                  obligation_eci    = def.obligation_eci,
+                                  parent            = def.parent;
                 -- delete indicateur actions
                 delete
                 from indicateur_action
@@ -49,3 +49,39 @@ begin
     end if;
 end
 $$ language plpgsql;
+comment on function business_upsert_indicateurs is
+    'Upsert indicateurs and update indicateur action relationships.';
+
+
+create or replace function business_update_action_definitions(
+    definitions action_definition[]
+) returns void as
+$$
+declare
+    def action_definition;
+begin
+    if is_service_role()
+    then
+        foreach def in array business_update_action_definitions.definitions
+            loop
+                update action_definition
+                set referentiel = def.referentiel,
+                    identifiant = def.identifiant,
+                    nom         = def.nom,
+                    description = def.description,
+                    contexte    = def.contexte,
+                    exemples    = def.exemples,
+                    ressources  = def.ressources,
+                    preuve      = def.preuve,
+                    points      = def.points,
+                    pourcentage = def.pourcentage
+                where action_id = def.action_id;
+            end loop;
+    else
+        perform set_config('response.status', '401', true);
+    end if;
+
+end;
+$$ language plpgsql;
+comment on function business_update_action_definitions is
+    'Update existing action definitions.';
