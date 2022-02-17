4c46376dd946129d23e5268fddd7ae1e0b6589e3
Florian d'Erfurth <florian@derfurth.com>
2022-02-17 10:35:29 +0100

    refacto droit tests so it works with pgtap

--- data_layer/postgres/tests/utilisateur_droits.sql
+++ data_layer/postgres/tests/utilisateur_droits.sql
@@ -1,23 +1,23 @@
--- original function
-create or replace function auth.uid() returns uuid as $$
-select nullif(current_setting('request.jwt.claim.sub', true), '')::uuid;
-$$ language sql stable;
+create extension if not exists pgtap with schema extensions;
+
+begin;
+select plan(2);
 
 -- make uid work as if yolododo user is connected
-create or replace function auth.uid() returns uuid as $$
+create or replace function auth.uid() returns uuid as
+$$
 select '17440546-f389-4d4f-bfdb-b0c94a1bd0f9'::uuid;
 $$ language sql stable;
 
-select * from owned_collectivite;
+select ialike(
+    (claim_collectivite(10) -> 'message')::text,
+    '%Vous êtes référent%',
+    'should return the success message first time'
+);
 
-select * from active_collectivite;
-
-select * from elses_collectivite;
-
-
--- fixme string comparison broken 😭
--- should return the success message first time
-select (claim_collectivite(1) -> 'message')::text = 'Vous avez rejoint la collectivité.';
-
--- should return the failure message the second time as the epci have been claimed
-select (claim_collectivite(1) -> 'message')::text like 'La collectivité dispose déjà d''un référent.';
+select ialike(
+    (claim_collectivite(10) -> 'message')::text,
+    '%La collectivité dispose déjà d''un référent%',
+    'should return the failure message the second time'
+);
+rollback;
