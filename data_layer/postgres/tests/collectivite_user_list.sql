96d1a8118184f42a9b156ca06cce7030a3f6946a
Florian d'Erfurth <florian@derfurth.com>
2022-02-28 10:51:12 +0100

    test sql

---
+++ data_layer/postgres/tests/collectivite_user_list.sql
@@ -0,0 +1,17 @@
+create extension if not exists pgtap with schema extensions;
+
+begin;
+select plan(1);
+
+-- make uid work as if yolododo user is connected
+create or replace function auth.uid() returns uuid as
+$$
+select '17440546-f389-4d4f-bfdb-b0c94a1bd0f9'::uuid;
+$$ language sql stable;
+
+SELECT results_eq(
+    'select to_json(personnes[1]) ->> ''email'' as email from collectivite_user_list(1) where role_name = ''referent'';',
+    'select to_json(p) ->> ''email'' as email from dcp p where user_id = ''17440546-f389-4d4f-bfdb-b0c94a1bd0f9'';',
+    'email of referent 1 should be the same as yolododo'
+    );
+rollback;
