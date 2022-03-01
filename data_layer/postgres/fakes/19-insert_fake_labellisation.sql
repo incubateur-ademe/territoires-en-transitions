8a94460d1dd937218a2c19332a2dcba95e09cd52
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-03-01 22:23:44 +0100

    Create table labellisation in DL and implement LabellisationReadEndpoint in Front
    (cherry picked from commit 7c2407c331ca984da6ef367f5dce4b6cdac0496a)

---
+++ data_layer/postgres/fakes/19-insert_fake_labellisation.sql
@@ -0,0 +1,5 @@
+insert into labellisation(collectivite_id, obtenue_le, etoiles, score_realise, score_programme)
+values (1, '2018-01-01' , 1, .31, .42);
+
+insert into labellisation(collectivite_id, obtenue_le, etoiles, score_realise, score_programme)
+values (1, '2020-01-01' , 2, .56, .62);
