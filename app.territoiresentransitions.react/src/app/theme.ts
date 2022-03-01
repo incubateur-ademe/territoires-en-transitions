83cc99511c16704e7cc969f2bab677059b316651
Marc Rutkowski <marc@attractive-media.fr>
2022-03-01 18:10:44 +0100

    ajoute les composants pour définir l'état d'avancement détaillé

--- app.territoiresentransitions.react/src/app/theme.ts
+++ app.territoiresentransitions.react/src/app/theme.ts
@@ -1,13 +1,13 @@
 import {ActionAvancement} from 'generated/dataLayer/action_statut_read';
 
 export const actionAvancementColors: Record<
-  ActionAvancement | 'non_concerne',
+  ActionAvancement | 'non_concerne' | 'detaille',
   string
 > = {
   fait: '#0063CB',
   programme: '#00A95F',
-  pas_fait: '#FF6384',
+  pas_fait: '#CE0500',
   non_renseigne: '#565656',
   non_concerne: '#D3D3D3',
-  detaille: '#ff7f00',
+  detaille: '#E7A969',
 };
