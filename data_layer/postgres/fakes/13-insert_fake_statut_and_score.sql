8d595e0e369090ee174633c7877b30c166fe0c14
Florian d'Erfurth <florian@derfurth.com>
2022-03-03 14:34:22 +0100

    fix numeric precision

--- data_layer/postgres/fakes/13-insert_fake_statut_and_score.sql
+++ data_layer/postgres/fakes/13-insert_fake_statut_and_score.sql
@@ -8,4 +8,4 @@ insert into
 
 insert into
     action_statut(collectivite_id, action_id, avancement, avancement_detaille, concerne, modified_by)
-    values (1, 'cae_1.1.1.1.2', 'detaille', ARRAY[.2, .7, .1], true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
+    values (1, 'cae_1.1.1.1.2', 'detaille', '{ 0.2, 0.7, 0.1}' , true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
