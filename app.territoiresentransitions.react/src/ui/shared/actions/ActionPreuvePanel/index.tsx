11ee340096504d8b230c9c373d2b1411fdd6f589
Marc Rutkowski <marc@attractive-media.fr>
2022-03-07 17:25:25 +0100

    charge le panneau "preuves" en mode lazy

--- app.territoiresentransitions.react/src/ui/shared/actions/ActionPreuvePanel/index.tsx
+++ app.territoiresentransitions.react/src/ui/shared/actions/ActionPreuvePanel/index.tsx
@@ -1,1 +1,11 @@
-export {ActionPreuvePanel} from './ActionPreuvePanel';
+import {lazy, Suspense} from 'react';
+import {renderLoader} from 'utils/renderLoader';
+import {TActionPreuvePanelProps} from './ActionPreuvePanel';
+
+const Panel = lazy(() => import('./ActionPreuvePanel'));
+
+export const ActionPreuvePanel = (props: TActionPreuvePanelProps) => (
+  <Suspense fallback={renderLoader()}>
+    <Panel {...props} />
+  </Suspense>
+);
