996665dc78021bf631669911decc0417008b3577
Marc Rutkowski <marc@attractive-media.fr>
2022-03-07 15:46:55 +0100

    expose le client supabase aux tests E2E

--- app.territoiresentransitions.react/src/app/E2E.tsx
+++ app.territoiresentransitions.react/src/app/E2E.tsx
@@ -1,5 +1,6 @@
 import {useHistory} from 'react-router-dom';
 import {authBloc} from 'core-logic/observables/authBloc';
+import {supabaseClient} from 'core-logic/api/supabase';
 
 /**
  * Expose un objet window.e2e lorsque l'appli fonctionne dans Cypress
@@ -8,7 +9,7 @@ export const E2E = () => {
   if ('Cypress' in window) {
     // expose l'objet history ainsi que le client supabase à l'env. E2E
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).e2e = {history: useHistory(), authBloc};
+    (window as any).e2e = {history: useHistory(), authBloc, supabaseClient};
   }
   return null;
 };
