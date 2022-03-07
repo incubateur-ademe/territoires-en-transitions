6183e627f318f7fefa36d75ce53b6e2ddcd2c731
Marc Rutkowski <marc@attractive-media.fr>
2022-03-07 10:47:57 +0100

    appelle remove_from_collectivite au lieu de remove_user_from_collectivite pour retirer un utilisateur de la collectivité

--- app.territoiresentransitions.react/src/core-logic/api/procedures/collectiviteProcedures.ts
+++ app.territoiresentransitions.react/src/core-logic/api/procedures/collectiviteProcedures.ts
@@ -59,13 +59,10 @@ export const removeUser = async (
   collectiviteId: number,
   userId: string
 ): Promise<RemoveUserResponse | null> => {
-  const {data, error} = await supabaseClient.rpc(
-    'remove_user_from_collectivite',
-    {
-      id: collectiviteId,
-      user_id: userId,
-    }
-  );
+  const {data, error} = await supabaseClient.rpc('remove_from_collectivite', {
+    collectivite_id: collectiviteId,
+    user_id: userId,
+  });
 
   if (error || !data) {
     return null;
