6cf8ea75834d9202dc0f1d288c764f1cc3cbe6e1
Florian d'Erfurth <florian@derfurth.com>
2022-03-09 14:16:17 +0100

    add collectivité test and update triggers

---
+++ data_layer/postgres/migrations/04-collectivite_test.sql
@@ -0,0 +1,7 @@
+drop trigger before_commune_write on commune;
+drop trigger before_epci_write on epci;
+drop function before_epci_write_create_collectivite;
+
+-- then execute 12-collectivite.sql
+--- l 47:61
+--- l 82:134
