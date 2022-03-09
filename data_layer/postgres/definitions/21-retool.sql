372563b5af666bcfdb760e506334d16a44681b01
Florian d'Erfurth <florian@derfurth.com>
2022-03-09 14:42:06 +0100

    exclude collectivités from stats

--- data_layer/postgres/definitions/21-retool.sql
+++ data_layer/postgres/definitions/21-retool.sql
@@ -70,6 +70,7 @@ select c.collectivite_id,
        nom
 from named_collectivite c
 where collectivite_id in (select collectivite_id from private_utilisateur_droit where active)
+  and collectivite_id not in (select collectivite_id from collectivite_test)
 order by nom;
 comment on view retool_active_collectivite is
     'Active collectivités as defined by métier.';
