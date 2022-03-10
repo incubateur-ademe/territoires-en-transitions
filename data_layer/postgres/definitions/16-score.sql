4a43e0bdf49f0c3b29dd64cf4ef435587dee1437
Florian d'Erfurth <florian@derfurth.com>
2022-03-10 19:23:48 +0100

    create 'virtual' events for scores to be computed for newly activated collectivités

--- data_layer/postgres/definitions/16-score.sql
+++ data_layer/postgres/definitions/16-score.sql
@@ -6,7 +6,7 @@ create table client_scores
     -- id               serial primary key,
     collectivite_id  integer references collectivite not null,
     referentiel      referentiel                     not null,
-    scores           jsonb                            not null,
+    scores           jsonb                           not null,
     score_created_at timestamp with time zone        not null,
     primary key (collectivite_id, referentiel)
 );
@@ -28,22 +28,48 @@ comment on column client_scores.score_created_at is 'Equal score.created_at.';
 --------------------------------
 create or replace view unprocessed_action_statut_update_event
 as
-with latest_score as (
-    select collectivite_id, referentiel, max(score_created_at) as max_date
-    from client_scores
-    group by collectivite_id, referentiel
-),
-     latest_event as (
-         select collectivite_id, referentiel, max(created_at) as max_date
-         from action_statut_update_event
-         group by collectivite_id, referentiel
-     )
-select latest_event.collectivite_id, latest_event.referentiel, latest_event.max_date as created_at
-from latest_event
- full join latest_score on latest_event.referentiel = latest_score.referentiel
- and latest_event.collectivite_id = latest_score.collectivite_id
-  where latest_event.max_date > latest_score.max_date or  latest_score.max_date is null;
-
+with
+    -- equivalent to active collectivite
+    unique_collectivite_droit as (
+        select named_collectivite.collectivite_id, min(created_at) as max_date
+        from named_collectivite
+                 join private_utilisateur_droit
+                      on named_collectivite.collectivite_id = private_utilisateur_droit.collectivite_id
+        where private_utilisateur_droit.active
+        group by named_collectivite.collectivite_id
+    ),
+    -- virtual events, so we consider someone joining a collectivite as a statuts update
+    virtual_inital_event as (
+        select collectivite_id,
+               unnest('{eci, cae}'::referentiel[]) as referentiel,
+               max_date
+        from unique_collectivite_droit
+    ),
+    -- the latest from virtual and action statut update event
+    latest_event as (
+        select v.collectivite_id,
+               v.referentiel,
+               max(coalesce(v.max_date, r.created_at)) as max_date
+        from virtual_inital_event v
+                 full join action_statut_update_event r on r.collectivite_id = v.collectivite_id
+        group by v.collectivite_id, v.referentiel
+    ),
+    -- events that are not processed
+    unprocessed as (
+        select *
+        from latest_event e
+        where collectivite_id not in (
+            -- processed means that the score is more recent than the event
+            select collectivite_id
+            from client_scores s
+            where s.score_created_at > e.max_date
+        )
+    )
+select unprocessed.collectivite_id,
+       unprocessed.referentiel,
+       unprocessed.max_date as created_at
+from unprocessed
+;
 comment on view unprocessed_action_statut_update_event is
     'To be used by business to compute only what is necessary.';
 
