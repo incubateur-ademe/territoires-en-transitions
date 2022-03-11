96981f076123ee52c33284efa9e505e81f9409d4
Florian d'Erfurth <florian@derfurth.com>
2022-03-11 11:48:33 +0100

    migration file

---
+++ data_layer/postgres/migrations/05-fixes_and_improvements.sql
@@ -0,0 +1,13 @@
+-- from 16-score.sql
+--- l 29:72 create or replace view unprocessed_action_statut_update_event
+
+
+-- from 14a-referentiel_views.sql
+--- l 1:12 naturalsort
+--- l 89:113 create or replace view action_definition_summary
+
+drop view retool_user_list;
+-- from 21-retool.sql
+--- l 22:37 create or replace view retool_user_list
+
+
