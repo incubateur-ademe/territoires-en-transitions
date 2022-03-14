02b2682ce28c63b45bf134580cd6192642c8410f
Marc Rutkowski <marc@attractive-media.fr>
2022-03-14 11:09:44 +0100

    complète l'affichage du niveau de labellisation avec les infobulles voulues

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/Star.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/Star.tsx
@@ -1,4 +1,4 @@
-export const Star = ({fill}: {fill: string}) => (
+export const Star = ({fill, title}: {fill: string; title?: string}) => (
   <svg
     width="39"
     height="38"
@@ -6,6 +6,7 @@ export const Star = ({fill}: {fill: string}) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
+    {title ? <title>{title}</title> : null}
     <path
       d="M19.5002 30.434L7.74518 37.014L10.3702 23.8006L0.478516 14.654L13.8569 13.0673L19.5002 0.833984L25.1435 13.0673L38.5219 14.654L28.6302 23.8006L31.2552 37.014L19.5002 30.434Z"
       fill={fill}
@@ -13,4 +14,9 @@ export const Star = ({fill}: {fill: string}) => (
   </svg>
 );
 
-export const GreyStar = () => <Star fill="#e5e5e5" />;
+export const GreyStar = (props: {title?: string}) => (
+  <Star {...props} fill="#e5e5e5" />
+);
+export const BlueStar = (props: {title?: string}) => (
+  <Star {...props} fill="#0063CB" />
+);
