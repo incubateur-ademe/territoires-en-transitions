6863a82e34b648bda1eb985f800f0767defd180b
Marc Rutkowski <marc@attractive-media.fr>
2022-03-14 13:57:00 +0100

    fixe l'insertion des données de tests de la labellisation

--- data_layer/postgres/fakes/19-insert_fake_labellisation.sql
+++ data_layer/postgres/fakes/19-insert_fake_labellisation.sql
@@ -1,5 +1,5 @@
-insert into labellisation(collectivite_id, obtenue_le, etoiles, score_realise, score_programme)
-values (1, '2018-01-01' , 1, .31, .42);
+insert into labellisation(collectivite_id, referentiel, obtenue_le, etoiles, score_realise, score_programme)
+values (1, 'eci', '2018-01-01' , 1, .31, .42);
 
-insert into labellisation(collectivite_id, obtenue_le, etoiles, score_realise, score_programme)
-values (1, '2020-01-01' , 2, .56, .62);
+insert into labellisation(collectivite_id, referentiel, obtenue_le, etoiles, score_realise, score_programme)
+values (1, 'cae', '2020-01-01' , 2, .56, .62);
