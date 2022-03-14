9da2b40a4c61e567500d4e591dc70e4f16bdf28e
Marc Rutkowski <marc@attractive-media.fr>
2022-03-14 11:09:44 +0100

    complète l'affichage du niveau de labellisation avec les infobulles voulues

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/NiveauLabellisation.stories.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/NiveauLabellisation.stories.tsx
@@ -13,8 +13,8 @@ const Template: Story<TNiveauLabellisationProps> = args => (
   <NiveauLabellisation {...args} />
 );
 
-export const NonLabellise0 = Template.bind({});
-NonLabellise0.args = {
+export const NonLabellise = Template.bind({});
+NonLabellise.args = {
   labellisation: {
     collectivite_id: 1,
     etoiles: 0,
@@ -23,12 +23,68 @@ NonLabellise0.args = {
   },
 };
 
-export const NonLabellise1 = Template.bind({});
-NonLabellise1.args = {
+export const UneEtoile = Template.bind({});
+UneEtoile.args = {
   labellisation: {
     collectivite_id: 1,
-    etoiles: 0,
-    score_realise: 0.56,
+    etoiles: 1,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.2,
     score_programme: 0.62,
+  },
+};
+
+export const DeuxEtoilesPossible = Template.bind({});
+DeuxEtoilesPossible.args = {
+  labellisation: {
+    collectivite_id: 1,
+    etoiles: 1,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.36,
+    score_programme: 0.62,
+  },
+};
+
+export const DeuxEtoiles = Template.bind({});
+DeuxEtoiles.args = {
+  labellisation: {
+    collectivite_id: 1,
+    etoiles: 2,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.37,
+    score_programme: 0.62,
+  },
+};
+
+export const TroisEtoiles = Template.bind({});
+TroisEtoiles.args = {
+  labellisation: {
+    collectivite_id: 1,
+    etoiles: 3,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.51,
+    score_programme: 0.62,
+  },
+};
+
+export const QuatreEtoiles = Template.bind({});
+QuatreEtoiles.args = {
+  labellisation: {
+    collectivite_id: 1,
+    etoiles: 4,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.65,
+    score_programme: 0.8,
+  },
+};
+
+export const CinqEtoiles = Template.bind({});
+CinqEtoiles.args = {
+  labellisation: {
+    collectivite_id: 1,
+    etoiles: 5,
+    obtenue_le: new Date('2022-03-14T09:43:00.251Z'),
+    score_realise: 0.75,
+    score_programme: 0.8,
   },
 };
--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/NiveauLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/NiveauLabellisation.tsx
@@ -1,11 +1,13 @@
 /**
  * Affiche le niveau de labellisation d'une collectivité
  */
+
 import {LabellisationRead} from 'generated/dataLayer/labellisation_read';
-import {GreyStar} from './Star';
+import {toPercentString} from 'utils/score';
+import {BlueStar, GreyStar} from './Star';
 
 const MAX_STARS = 5;
-const PALIER1 = 0.35;
+const STEPS = [0.35, 0.5, 0.65, 0.75];
 
 export type TNiveauLabellisationProps = {
   labellisation: LabellisationRead;
@@ -13,18 +15,18 @@ export type TNiveauLabellisationProps = {
 
 export const NiveauLabellisation = (props: TNiveauLabellisationProps) => {
   const {labellisation} = props;
-  const {obtenue_le} = labellisation;
+  const {etoiles} = labellisation;
 
-  if (!obtenue_le) {
+  if (!etoiles) {
     return <NonLabellise {...props} />;
   }
 
-  return <p>render something here...</p>;
+  return <NiveauActuel {...props} />;
 };
 
 const NonLabellise = (props: TNiveauLabellisationProps) => {
-  const {labellisation} = props;
-  const {score_realise} = labellisation;
+  //const {labellisation} = props;
+  // const {score_realise} = labellisation;
 
   return (
     <div className="flex flex-col items-center">
@@ -32,10 +34,11 @@ const NonLabellise = (props: TNiveauLabellisationProps) => {
       <div className="flex space-x-4 pb-12">
         {Array(MAX_STARS)
           .fill(0)
-          .map(() => (
-            <GreyStar />
+          .map((v, i) => (
+            <GreyStar key={`s${i}`} />
           ))}
       </div>
+      {/*
       <h2>Objectif : première étoile</h2>
       <div className="max-w-sm">
         <p className="font-bold mb-3">Pré-requis pour la demande d’audit</p>
@@ -45,7 +48,7 @@ const NonLabellise = (props: TNiveauLabellisationProps) => {
           <li>Fournir les documents officiels pour votre candidature</li>
         </ul>
         {score_realise > PALIER1 ? (
-          <div className="fr-alert fr-alert--info fr-alert--sm">
+            <div className="fr-alert fr-alert--info fr-alert--sm">
             <p>
               Bravo, vous avez plus de 35% d’actions réalisées ! Pour candidater
               directement à la deuxième étoile, ajoutez les preuves pour ces
@@ -53,6 +56,64 @@ const NonLabellise = (props: TNiveauLabellisationProps) => {
             </p>
           </div>
         ) : null}
+      </div>
+        */}
+    </div>
+  );
+};
+
+const NiveauActuel = (props: TNiveauLabellisationProps) => {
+  const {labellisation} = props;
+  const {etoiles, obtenue_le, score_realise, score_programme} = labellisation;
+
+  return (
+    <div className="flex flex-col items-center">
+      <h2>Niveau de labellisation actuel</h2>
+      <div className="flex space-x-4 pb-12">
+        {Array(MAX_STARS)
+          .fill(0)
+          .map((v, i) => {
+            // étoile déjà obtenue
+            if (i < etoiles) {
+              return (
+                <BlueStar
+                  key={`s${i}`}
+                  title={`Score labellisé en ${obtenue_le?.getFullYear()} : ${toPercentString(
+                    score_realise
+                  )}\nAmbition à quatre ans : ${toPercentString(
+                    score_programme
+                  )}`}
+                />
+              );
+            }
+            // étoile suivante
+            if (i === etoiles) {
+              // Cas particulier deuxième étoile si le score courant est supérieur à 35%
+              if (i === 1 && score_realise > STEPS[0]) {
+                return (
+                  <GreyStar
+                    key={`s${i}`}
+                    title="Vous atteignez le score requis, félicitations ! Candidatez dès à présent ou continuez à progresser jusqu’à la fin de votre cycle de labellisation."
+                  />
+                );
+              }
+              return (
+                <GreyStar
+                  key={`s${i}`}
+                  title={`Plus que ${toPercentString(
+                    STEPS[i - 1] - score_realise
+                  )} à réaliser pour candidater, d'après votre score actuel !`}
+                />
+              );
+            }
+            // prochaine étoile
+            return (
+              <GreyStar
+                key={`s${i}`}
+                title={`Score requis : ${toPercentString(STEPS[i - 1])}`}
+              />
+            );
+          })}
       </div>
     </div>
   );
