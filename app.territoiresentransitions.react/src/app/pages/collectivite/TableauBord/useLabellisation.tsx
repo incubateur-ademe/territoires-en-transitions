e97316068ea39ddba98c36a7cdab6085ea96b324
Marc Rutkowski <marc@attractive-media.fr>
2022-03-15 17:17:01 +0100

    affiche des infobulles distinctes pour chaque étoile reflétant le niveau de labellisation
    + ajoute les stories nécessaires pour couvrir plus de cas

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/useLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauBord/useLabellisation.tsx
@@ -4,18 +4,30 @@ import {LabellisationRead} from 'generated/dataLayer/labellisation_read';
 import {useEffect, useState} from 'react';
 
 export const useLabellisation = (referentiel: string) => {
-  const [labellisation, setLabellisation] = useState<LabellisationRead | null>(
-    null
-  );
+  const [labellisation, setLabellisation] =
+    useState<LabellisationReadByEtoiles | null>(null);
 
   const collectivite_id = useCollectiviteId()!;
   useEffect(() => {
     if (!labellisation) {
       labellisationReadEndpoint
         .getBy({collectivite_id, referentiel})
-        .then(res => setLabellisation(res[0]));
+        .then(res => setLabellisation(res.reduce(byEtoiles, {})));
     }
   }, [labellisation]);
 
   return labellisation;
+};
+
+export type LabellisationReadByEtoiles = {
+  [key: number]: LabellisationRead;
+};
+
+const byEtoiles = (
+  dict: LabellisationReadByEtoiles,
+  item: LabellisationRead
+) => {
+  const {etoiles} = item;
+  const index = etoiles || 0; // pour gérer la valeur null
+  return dict[index] ? dict : {...dict, [index]: item};
 };
