6b929e0e3071fe083a8758db7f659ac6c538893b
Marc Rutkowski <marc@attractive-media.fr>
2022-03-17 18:27:04 +0100

    ajoute des commentaires

--- app.territoiresentransitions.react/src/types/ClientScore.ts
+++ app.territoiresentransitions.react/src/types/ClientScore.ts
@@ -7,7 +7,14 @@ export interface ActionScore {
   point_programme: number;
   point_pas_fait: number;
   point_non_renseigne: number;
+  /** Potentiel éventuellement réduit ou augmenté par le statut non concerné ou
+   * la personnalisation. Valeur identique à `point_referentiel` si pas de
+   * modification. */
   point_potentiel: number;
+  /** Potentiel éventuellement réduit ou augmenté par la personnalisation.
+   * Valeur identique à `point_referentiel` si pas de modification. */
+  point_potentiel_perso: number;
+  /** Potentiel initial (défini par le référentiel) */
   point_referentiel: number;
   concerne: boolean;
   total_taches_count: number;
