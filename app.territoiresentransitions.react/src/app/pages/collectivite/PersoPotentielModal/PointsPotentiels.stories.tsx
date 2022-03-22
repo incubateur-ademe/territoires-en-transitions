248ba829694203710431f1c8b729bf21c723b5fe
Marc Rutkowski <marc@attractive-media.fr>
2022-03-22 17:51:00 +0100

    ajoute des stories pour l'affichage du potentiel de points d'une action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PersoPotentielModal/PointsPotentiels.stories.tsx
@@ -0,0 +1,33 @@
+import {Story, Meta} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {PointsPotentiels, TPointsPotentielsProps} from './PointsPotentiels';
+import {ACTION_DEF, SCORE, SCORE_MODIFIE} from './fixture.json';
+
+export default {
+  component: PointsPotentiels,
+} as Meta;
+
+const Template: Story<TPointsPotentielsProps> = args => (
+  <PointsPotentiels {...args} />
+);
+
+export const NonPersonnalise = Template.bind({});
+NonPersonnalise.args = {
+  actionDef: ACTION_DEF,
+  actionScore: SCORE,
+  onEdit: null,
+};
+
+export const Personnalise = Template.bind({});
+Personnalise.args = {
+  actionDef: ACTION_DEF,
+  actionScore: SCORE_MODIFIE,
+  onEdit: null,
+};
+
+export const PersonnaliseEtEditable = Template.bind({});
+PersonnaliseEtEditable.args = {
+  actionDef: ACTION_DEF,
+  actionScore: SCORE_MODIFIE,
+  onEdit: action('onEdit'),
+};
