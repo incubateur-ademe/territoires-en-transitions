767dfd6830029a76c9722f57650516811b49dd02
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-03-22 18:45:41 +0100

    Add models IndentiteOptions and IdentiteCollectivite

--- business/business/personnalisation/engine/models.py
+++ business/business/personnalisation/engine/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional, Union
+from typing import List, Literal, Optional, Set, Union
 
 from business.referentiel.domain.models.question import QuestionType
 
@@ -15,3 +15,22 @@ class Question:
     id: str
     type: QuestionType
     choix_ids: Optional[List[str]] = None
+
+
+IdentiteTypeOption = Literal["syndicat", "commune"]
+IdentitePopulationOption = Literal["moins_de_100000"]
+IdentiteLocalisationOption = Literal["DOM"]
+
+
+@dataclass
+class IdentiteOptions:
+    type: List[IdentiteTypeOption] = ["syndicat", "commune"]
+    population: List[IdentitePopulationOption] = ["moins_de_100000"]
+    localisation: List[IdentiteLocalisationOption] = ["DOM"]
+
+
+@dataclass
+class IdentiteCollectivite:
+    type: Set[IdentiteTypeOption]
+    population: Set[IdentitePopulationOption]
+    localisation: Set[IdentiteLocalisationOption]
