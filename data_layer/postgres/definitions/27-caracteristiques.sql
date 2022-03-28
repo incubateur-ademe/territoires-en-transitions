740fdb50287ba6658f8b1b24f97cf0a69afefdf9
Florian d'Erfurth <florian@derfurth.com>
2022-03-28 15:32:45 +0200

    Add collectivite_id question_display.

--- data_layer/postgres/definitions/27-caracteristiques.sql
+++ data_layer/postgres/definitions/27-caracteristiques.sql
@@ -19,11 +19,60 @@ with population_commune as (
          from collectivite c
      )
 select c.id,
-       coalesce(pc.population, '{}') as population,
-       coalesce(tc.type, '{}')       as type,        -- todo incomplete
-       '{}'                          as localisation -- todo incomplete
+       coalesce(pc.population, '{}')                as population,
+       coalesce(tc.type, '{}')::type_collectivite[] as type,        -- todo incomplete
+       '{}'                                         as localisation -- todo incomplete
 from collectivite c
          left join population_commune pc on pc.collectivite_id = c.id
          left join type_collectivite tc on tc.collectivite_id = c.id
 ;
+
+create view question_display as
+with actions as (
+    select question_id, array_agg(action_id) action_ids
+    from question_action
+    group by question_id
+), q as (
+    select q.id    as id,
+           a.action_ids,
+           thematique_id,
+           type,
+           t.nom   as thematique_nom,
+           description,
+           types_collectivites_concernees,
+           formulation,
+           ordonnancement,
+           cx.json as choix
+    from question q
+             join question_thematique t on t.id = q.thematique_id
+             join actions a on q.id = a.question_id
+             left join lateral (select array_agg(
+                                               json_build_object(
+                                                       'id', c.id,
+                                                       'label', c.formulation,
+                                                       'ordonnancement', c.ordonnancement
+                                                   )) as json
+                                from question_choix c
+                                where c.question_id = q.id) cx on true
+)
+select q.id,
+       action_ids,
+       i.id as collectivite_id,
+       thematique_id,
+       q.type,
+       thematique_nom,
+       description,
+       types_collectivites_concernees,
+       formulation,
+       ordonnancement,
+       choix,
+       population,
+       localisation
+from q
+    join collectivite_identite i on q.types_collectivites_concernees && i.type
+;
+comment on view question_display is
+    'Questions avec leurs choix pour l''affichage dans le client';
+
+
 
