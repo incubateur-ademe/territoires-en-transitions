6c07ede315d6d9f372d5eb77c47bab69fbcc9f9e
Marc Rutkowski <marc@attractive-media.fr>
2022-03-31 17:16:17 +0200

    centralise la fonction de formatage des décimales
    + ajoute un identifiant pour les tests e2e

--- app.territoiresentransitions.react/src/utils/toFixed.ts
+++ app.territoiresentransitions.react/src/utils/toFixed.ts
@@ -1,3 +1,6 @@
 export const toFixed = (n: number, d = 1) => {
   return Number(Number(`${n}e${d}`).toFixed() + `e-${d}`);
 };
+
+export const toLocaleFixed = (n: number, maximumFractionDigits = 1): string =>
+  n.toLocaleString(undefined, {maximumFractionDigits});
