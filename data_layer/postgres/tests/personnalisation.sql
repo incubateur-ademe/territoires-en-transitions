2c95c57937ab4ad01107fc7be754737d75b5b698
Florian d'Erfurth <florian@derfurth.com>
2022-04-01 19:26:00 +0200

    fix personnalisation tests

--- data_layer/postgres/tests/personnalisation.sql
+++ data_layer/postgres/tests/personnalisation.sql
@@ -8,14 +8,22 @@ select has_table('personnalisation');
 select has_table('personnalisation_regle');
 select has_function('business_upsert_personnalisations');
 
+-- override is_service_role to run business_upsert_personnalisations as service.
+create or replace function is_service_role() returns bool as
+$$
+select True;
+$$ language sql stable;
+
+-- insert a personnalisation
 do
 $$
     declare
         personnalisation json;
     begin
-        personnalisation = '{
-          "action_id": "eci_1.1.1",
-          "titre": "Yoloo",
+        personnalisation = '
+        {
+          "action_id": "eci_1",
+          "titre": "Yolo ?",
           "description": "....",
           "regles": [
             {
@@ -29,19 +37,19 @@ $$
               "description": "yono"
             }
           ]
-        }'::json;
+        }
+        '::json;
         perform business_upsert_personnalisations(array [personnalisation]);
     end
-
 $$ language plpgsql;
 
 
-select results_eq('select count(*)::int as count from personnalisation;',
-                  'select 1::int as count;',
-                  'One personnalisation should  be inserted');
+select results_eq('select titre from personnalisation where action_id = ''eci_1'';',
+                  'select ''Yolo ?''',
+                  'Personnalisation Yolo should be present');
 
-select results_eq('select count(*)::int as count from personnalisation_regle;',
+select results_eq('select count(*)::int as count from personnalisation_regle where action_id = ''eci_1'';',
                   'select 2::int as count;',
-                  'Two règles should be inserted');
+                  'Two règles should be present');
 rollback;
 
