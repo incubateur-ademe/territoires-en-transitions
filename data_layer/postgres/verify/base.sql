86dcea6211df93468f5cef00ff20c207f33d91fa
Florian d'Erfurth <florian@derfurth.com>
2022-04-03 12:11:20 +0200

    Datalayer can be started with models and data using docker-compose.

---
+++ data_layer/postgres/verify/base.sql
@@ -0,0 +1,4 @@
+select modified_at from abstract_modified_at where false;
+select has_function_privilege('teapot()', 'execute');
+select has_function_privilege('is_authenticated()', 'execute');
+select has_function_privilege('is_service_role()', 'execute');
