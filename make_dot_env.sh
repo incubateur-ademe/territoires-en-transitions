e9e67a456c494fc2d99aba8d6900cc677da9b272
Florian d'Erfurth <florian@derfurth.com>
2022-04-04 11:25:27 +0200

    Removes all test environment keys. Update docs.

---
+++ make_dot_env.sh
@@ -0,0 +1,5 @@
+envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./data_layer/supabase/kong.sample.yml > ./data_layer/supabase/kong.yml
+envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./data_layer/.env.sample > ./data_layer/.env
+envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./.env.sample > ./.env
+envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
+envsubst '${SUPABASE_ANON_KEY}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
