6d5f5ba8376e3e9702c3dc15ff7ef6b9d37df1c2
Marc Rutkowski <marc@attractive-media.fr>
2022-04-07 15:18:18 +0200

    rend ré-utilisable l'appel différé à un callback lors de la saisie dans un champ

--- app.territoiresentransitions.react/src/ui/UiSearchBar.tsx
+++ app.territoiresentransitions.react/src/ui/UiSearchBar.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useDebouncedInput} from './shared/useDebouncedInput';
 
 export const UiSearchBar = ({
   search,
@@ -7,30 +7,17 @@ export const UiSearchBar = ({
   search: (value: string) => void;
   debouncePeriod?: number;
 }) => {
-  const [query, setQuery] = useState('');
-  const [timeoutId, setTimeoutId] = useState<number | undefined>();
+  const [query, onChange] = useDebouncedInput('', search, debouncePeriod);
 
-  const onChange = (event: any) => {
-    setQuery(event.target.value);
-  };
-
-  const onKeyUp = () => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-      setTimeoutId(undefined);
-    }
-    setTimeoutId(setTimeout(search, debouncePeriod, query));
-  };
   return (
     <div className="flex">
       <div className=" border-b-2 border-bf500 flex items-center">
         <input
           className="bg-beige w-80 p-2"
           key="random1"
-          value={query}
+          value={query || ''}
           placeholder={'Rechercher'}
           onChange={onChange}
-          onKeyUp={onKeyUp}
         />
         <div className="bg-bf500 h-full p-2">
           <div className="fr-fi-search-line text-white" />
