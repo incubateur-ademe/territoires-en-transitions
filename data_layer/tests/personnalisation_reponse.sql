eae79dc3fee9652068a0f034dccbf48d6b36aabc
Florian d'Erfurth <florian@derfurth.com>
2022-04-11 20:19:24 +0200

    fix unprocessed_reponse_update_event test by dropping timestamp trigger fn.

--- data_layer/tests/personnalisation_reponse.sql
+++ data_layer/tests/personnalisation_reponse.sql
@@ -13,6 +13,11 @@ select has_table('reponse_update_event');
 select has_function('after_reponse_insert_write_event');
 select has_view('unprocessed_reponse_update_event');
 
+truncate reponse_binaire;
+truncate reponse_choix;
+truncate reponse_proportion;
+truncate reponse_update_event;
+truncate personnalisation_consequence;
 
 -- make uid work as if yolododo user was connected.
 create or replace function auth.uid() returns uuid as
@@ -52,22 +57,15 @@ select results_eq('select created_at as time from unprocessed_reponse_update_eve
                       'from reponse_proportion where collectivite_id = 1 and question_id = ''energie_1'';',
                   'An unprocessed event should be present with the same time as the reponse.');
 
--- make update_modified_at write the actual time that changes inside the transaction.
-create or replace function update_modified_at() returns trigger
-as
-$$
-begin
-    new.modified_at = clock_timestamp();
-    return new;
-end;
-$$ language plpgsql;
+-- drop timestamp trigger
+drop trigger set_modified_at_before_personnalisation_consequence_update on personnalisation_consequence;
 
--- insert a consequence like the business would do
-insert into personnalisation_consequence (collectivite_id, consequences)
-values (1, '{}'::jsonb)
-on conflict (collectivite_id) do update set consequences = '{}'::jsonb;
+-- insert a consequence like the business would do but set it in the future
+insert into personnalisation_consequence (collectivite_id, consequences, modified_at)
+values (1, '{}'::jsonb, now() + interval '1 second');
 
 select is_empty('select created_at as time from unprocessed_reponse_update_event where collectivite_id = 1',
                 'No event should be present since the consequence have been set after reponse.');
+
 rollback;
 
