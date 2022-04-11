624f6b1f0623370a20605020e1568f9248beb9f7
Marc Rutkowski <marc@attractive-media.fr>
2022-04-11 17:37:14 +0200

    fixe les variables d'env. pour faire passer les tests E2E en CI

--- make_dot_env.sh
+++ make_dot_env.sh
@@ -2,3 +2,4 @@ envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./data_layer/sup
 envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./.env.sample > ./.env
 envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
 envsubst '${SUPABASE_ANON_KEY}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
+cp ./e2e/.env.sample ./e2e/.env
