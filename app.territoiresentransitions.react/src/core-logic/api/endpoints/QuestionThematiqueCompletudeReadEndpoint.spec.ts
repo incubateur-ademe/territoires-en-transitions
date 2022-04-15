5dc5a7cd378ce49e4cfa064e6c257d2772d8f296
Marc Rutkowski <marc@attractive-media.fr>
2022-04-15 17:05:07 +0200

    gère le chargement des questions rattachées à une thématique

---
+++ app.territoiresentransitions.react/src/core-logic/api/endpoints/QuestionThematiqueCompletudeReadEndpoint.spec.ts
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom/extend-expect';
+import {QuestionThematiqueCompletudeReadEndpoint} from 'core-logic/api/endpoints/QuestionThematiqueCompletudeReadEndpoint';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {yuluCredentials} from 'test_utils/collectivites';
+
+describe('Question completude by thematique reading endpoint ', () => {
+  it('should retrieve items by collectivite_id', async () => {
+    const questionThematiqueReadEndpoint =
+      new QuestionThematiqueCompletudeReadEndpoint([]);
+    await supabaseClient.auth.signIn(yuluCredentials); // Yulu has no rights on collectivite #1
+
+    const results = await questionThematiqueReadEndpoint.getBy({
+      collectivite_id: 1,
+    });
+
+    expect(results.length).toEqual(13);
+
+    expect(results).toEqual(
+      expect.arrayContaining([
+        {
+          collectivite_id: 1,
+          id: 'dechets',
+          nom: 'Déchets',
+          referentiels: ['eci', 'cae'],
+          completude: 'a_completer',
+        },
+        {
+          collectivite_id: 1,
+          id: 'centre_polarite',
+          nom: 'Centre polarité',
+          referentiels: ['cae'],
+          completude: 'complete',
+        },
+      ])
+    );
+  });
+});
