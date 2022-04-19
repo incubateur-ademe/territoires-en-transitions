2e3093df36e6b115a0ea23cca11fcc0f34813dfb
Marc Rutkowski <marc@attractive-media.fr>
2022-04-19 16:00:57 +0200

    ajoute le bouton "Afficher la catégorie suivante" dans la page "Personnalisation d'une thématique"
    (cherry picked from commit 49eb4411fb78b79b7f0a874693e4ece07a6a2162)

--- app.territoiresentransitions.react/src/app/pages/collectivite/PersoReferentielThematique/ThematiqueQR.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PersoReferentielThematique/ThematiqueQR.tsx
@@ -5,12 +5,17 @@
 import {useState} from 'react';
 import {QuestionReponseList} from '../PersoPotentielModal/PersoPotentielQR';
 import {SwitchLabelLeft} from 'ui/shared/SwitchLabelLeft';
+import {CarteIdentiteRead} from 'generated/dataLayer/carte_identite_read';
 import {TQuestionThematiqueRead} from 'generated/dataLayer/question_thematique_read';
 import {
   TChangeReponse,
   TQuestionReponse,
 } from 'generated/dataLayer/reponse_write';
-import {makeCollectivitePersoRefUrl} from 'app/paths';
+import {
+  makeCollectivitePersoRefUrl,
+  makeCollectivitePersoRefThematiqueUrl,
+} from 'app/paths';
+import {CarteIdentite} from './CarteIdentite';
 
 export type TThematiqueQRProps = {
   thematique: TQuestionThematiqueRead;
@@ -18,6 +23,10 @@ export type TThematiqueQRProps = {
     id: number;
     nom: string;
   };
+  /** Carte d'identité de la collectivité */
+  identite?: CarteIdentiteRead;
+  /** Identifiant de la thématique suivante */
+  nextThematiqueId?: string | null;
   /** Liste des questions/réponses associées à la thématique */
   questionReponses: TQuestionReponse[];
   /** Fonction appelée quand une réponse est modifiée */
@@ -25,7 +34,8 @@ export type TThematiqueQRProps = {
 };
 
 export const ThematiqueQR = (props: TThematiqueQRProps) => {
-  const {collectivite, questionReponses, onChange} = props;
+  const {collectivite, identite, nextThematiqueId, questionReponses, onChange} =
+    props;
   const [onlyNoResponse, setOnlyNoResponse] = useState(false);
 
   const qrList = onlyNoResponse
@@ -47,13 +57,32 @@ export const ThematiqueQR = (props: TThematiqueQRProps) => {
         </SwitchLabelLeft>
       </div>
       <div className="max-w-3xl self-center mt-10">
+        {identite ? (
+          <>
+            <CarteIdentite identite={identite} />
+            <h3 className="fr-mb-0">
+              Questions pour la personnalisation des référentiels
+            </h3>
+          </>
+        ) : null}
         <QuestionReponseList questionReponses={qrList} onChange={onChange} />
         <a
-          className="fr-btn fr-btn--icon-left fr-fi-arrow-left-line self-start mt-10"
+          className="fr-btn fr-btn--secondary fr-btn--icon-left fr-fi-arrow-left-line self-start fr-mt-5w"
           href={makeCollectivitePersoRefUrl({collectiviteId: collectivite.id})}
         >
           Revenir au sommaire
         </a>
+        {nextThematiqueId ? (
+          <a
+            className="fr-btn fr-btn--icon-right fr-fi-arrow-right-line self-start fr-mt-5w fr-ml-3w"
+            href={makeCollectivitePersoRefThematiqueUrl({
+              collectiviteId: collectivite.id,
+              thematiqueId: nextThematiqueId,
+            })}
+          >
+            Afficher la catégorie suivante
+          </a>
+        ) : null}
       </div>
     </div>
   );
