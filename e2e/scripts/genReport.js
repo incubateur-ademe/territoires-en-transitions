6b94a2c90ef26456fd1aa032a42eaa6dc85e60f5
Marc Rutkowski <marc@attractive-media.fr>
2022-04-19 10:15:41 +0200

    ajoute la génération d'un rapport de tests E2E

---
+++ e2e/scripts/genReport.js
@@ -0,0 +1,36 @@
+/**
+ * Génère un rapport HTML à partir des fichiers JSON générés lors de l'exécution des tests
+ */
+const os = require('os');
+const path = require('path');
+const report = require('multiple-cucumber-html-reporter');
+
+const reportName = 'Territoires en Transitions';
+const reportPath = path.join(__dirname, '..', './report');
+
+report.generate({
+  jsonDir: path.join(__dirname, '..', './cypress/cucumber-json/'),
+  reportPath,
+  reportName,
+  pageTitle: reportName,
+  displayDuration: true,
+  displayReportTime: true,
+  pageFooter: '',
+  customData: {
+    title: 'Run info',
+    data: [
+      { label: 'Version', value: process.env.VERSION },
+      { label: 'Rapport généré le', value: new Date().toLocaleString('fr-FR') },
+    ],
+  },
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: '??',
+    },
+    platform: {
+      name: os.platform(),
+      version: os.release(),
+    },
+  },
+});
