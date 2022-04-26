2d76432cb95676ddb54075702af34bffecd2d1d0
Florian d'Erfurth <florian@derfurth.com>
2022-04-26 12:03:59 +0200

    fix private schema usage. add migration.

--- data_layer/postgres/definitions/27-caracteristiques.sql
+++ data_layer/postgres/definitions/27-caracteristiques.sql
@@ -1,4 +1,7 @@
 create schema if not exists private;
+grant usage on schema private to postgres, anon, authenticated, service_role;
+comment on schema private is
+    'Regroupe des fonctions privées afin que le schema public ne comprenne que les éléments pour l''API.';
 
 create or replace function private.population_buckets(population integer)
     returns text[]
