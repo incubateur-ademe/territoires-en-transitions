1733d2603c018a1da16c6e5d6164b70d0e1a8420
Florian d'Erfurth <florian@derfurth.com>
2022-04-28 14:07:30 +0200

    Enlève les fichiers de migration.

--- data_layer/migrations/09-fix_permissions.sql
+++
@@ -1,4 +0,0 @@
--- Allow to execute private functions from public view
-grant usage on schema private to postgres, anon, authenticated, service_role;
-comment on schema private is
-    'Regroupe des fonctions privées afin que le schema public ne comprenne que les éléments pour l''API.'
