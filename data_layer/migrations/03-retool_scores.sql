a0caaabe05eee8974bdbebde03b91b95f0e77e41
Florian d'Erfurth <florian@derfurth.com>
2022-04-28 14:07:30 +0200

    Enlève les fichiers de migration.

--- data_layer/migrations/03-retool_scores.sql
+++
@@ -1,9 +0,0 @@
-drop view retool_score;
-drop view retool_completude;
-
-alter table client_scores
-    alter column scores
-    set data type jsonb
-    using scores::jsonb;
-
--- then execute 21 retool
