e16faae0a0b7b4d99fa1f1ddafd5ca983517c448
Marc Rutkowski <marc@attractive-media.fr>
2022-05-02 17:38:32 +0200

    isole le code du gestionnaire de ressources

--- app.territoiresentransitions.react/src/ui/shared/ResourceManager/ResourceManager.stories.tsx
+++ app.territoiresentransitions.react/src/ui/shared/ResourceManager/ResourceManager.stories.tsx
@@ -1,15 +1,22 @@
 import {Story, Meta} from '@storybook/react';
-import {AddPreuve, TAddPreuveProps} from './AddPreuve';
+import {action} from '@storybook/addon-actions';
+import {ResourceManager, TResourceManagerProps} from './index';
 
 const DEFAULT_ARGS = {
-  action: {id: '"eci_1.1.2'},
+  onClose: action('onClose'),
+  handlers: {
+    addFileFromLib: action('addFileFromLib'),
+    addLink: action('addLink'),
+  },
 };
 export default {
-  component: AddPreuve,
+  component: ResourceManager,
   args: DEFAULT_ARGS,
 } as Meta;
 
-const Template: Story<TAddPreuveProps> = args => <AddPreuve {...args} />;
+const Template: Story<TResourceManagerProps> = args => (
+  <ResourceManager {...args} />
+);
 
 export const Lien = Template.bind({});
 Lien.parameters = {storyshots: false}; // désactive le storyshot à cause d'un pb dans react-dsfr <File> (?)
---
+++ app.territoiresentransitions.react/src/ui/shared/ResourceManager/index.tsx
@@ -0,0 +1,41 @@
+/**
+ * Affiche le sélecteur de ressources (fichiers ou liens)
+ */
+import {Tabs, Tab} from '@dataesr/react-dsfr';
+import {AddLink, TAddLink} from './AddLink';
+import {AddFile, TAddFileFromLib} from './AddFile';
+import {AddFromLib} from './AddFromLib';
+
+export type TResourceManagerHandlers = {
+  /** ajoute un fichier sélectionné depuis la bibliothèque */
+  addFileFromLib: TAddFileFromLib;
+  /** ajoute un lien */
+  addLink: TAddLink;
+};
+
+export type TResourceManagerProps = {
+  /** Index de l'onglet actif */
+  defaultActiveTab?: number;
+  /** Gestionnaires d'événements */
+  handlers: TResourceManagerHandlers;
+  onClose: () => void;
+};
+
+export const ResourceManager = (props: TResourceManagerProps) => {
+  const {defaultActiveTab, handlers} = props;
+  const {addFileFromLib, addLink} = handlers;
+
+  return (
+    <Tabs defaultActiveTab={defaultActiveTab}>
+      <Tab label="&nbsp;Lien" icon="fr-fi-links-fill">
+        <AddLink {...props} onAddLink={addLink} />
+      </Tab>
+      <Tab label="&nbsp;Fichier" icon="fr-fi-upload-2-fill">
+        <AddFile {...props} onAddFileFromLib={addFileFromLib} />
+      </Tab>
+      <Tab label="&nbsp;Bibliothèque" icon="fr-fi-archive-line">
+        <AddFromLib {...props} onAddFileFromLib={addFileFromLib} />
+      </Tab>
+    </Tabs>
+  );
+};
