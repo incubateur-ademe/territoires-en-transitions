a3d808a0671fbd0ddd81b853a9642e3a45ca6626
Marc Rutkowski <marc@attractive-media.fr>
2022-05-02 16:05:13 +0200

    améliore le partage du dialogue d'ajout de ressources (fichier ou lien)

---
+++ app.territoiresentransitions.react/src/ui/shared/actions/AddPreuve/useAddPreuveToAction.tsx
@@ -0,0 +1,48 @@
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {preuveFichierWriteEndpoint} from 'core-logic/api/endpoints/PreuveFichierWriteEndpoint';
+import {preuveLienWriteEndpoint} from 'core-logic/api/endpoints/PreuveLienWriteEndpoint';
+import {TAddFileFromLib} from './AddPreuveFichier';
+import {TAddLink} from './AddPreuveLien';
+
+type TResourceDialog = (action_id: string) => {
+  /** ajoute un fichier sélectionné depuis la bibliothèque */
+  addFileFromLib: TAddFileFromLib;
+  /** ajoute un lien */
+  addLink: TAddLink;
+};
+
+/** Renvoie les gestionnaires d'événement du dialogue d'ajout de liens et
+ * fichiers à une action */
+export const useAddPreuveToAction: TResourceDialog = (action_id: string) => {
+  const collectivite_id = useCollectiviteId();
+
+  // associe un fichier sélectionné depuis la bibliothèque à une action
+  const addFileFromLib: TAddFileFromLib = filename =>
+    collectivite_id
+      ? preuveFichierWriteEndpoint.save({
+          action_id,
+          collectivite_id,
+          commentaire: '',
+          filename,
+        })
+      : Promise.resolve(false);
+
+  // associe un lien+titre à une action
+  const addLink: TAddLink = (titre, url) =>
+    collectivite_id
+      ? preuveLienWriteEndpoint
+          .save({
+            action_id,
+            collectivite_id,
+            titre,
+            url,
+            commentaire: '',
+          })
+          .then(preuveLien => Boolean(preuveLien))
+      : Promise.resolve(false);
+
+  return {
+    addFileFromLib,
+    addLink,
+  };
+};
