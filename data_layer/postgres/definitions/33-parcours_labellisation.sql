d266a6368a1587a08b0a1ccfef9eb5de150ea59f
Florian d'Erfurth <florian@derfurth.com>
2022-05-05 14:27:19 +0200

    improve parcours, more tests

--- data_layer/postgres/definitions/33-parcours_labellisation.sql
+++ data_layer/postgres/definitions/33-parcours_labellisation.sql
@@ -14,7 +14,14 @@ create or replace function
             )
 as
 $$
-with criteres as (select *
+with etoiles as (select *
+                 from labellisation.etoiles(labellisation_parcours.collectivite_id)),
+     all_critere as (select *
+                     from labellisation.critere_action(labellisation_parcours.collectivite_id)),
+     current_critere as (select c.*
+                         from all_critere c
+                                  join etoiles e on e.referentiel = c.referentiel and e.etoile_objectif = c.etoiles),
+     criteres as (select *
                   from (select c.referentiel,
                                bool_and(c.atteint) as atteints,
                                jsonb_agg(
@@ -22,6 +29,7 @@ with criteres as (select *
                                                'formulation', formulation,
                                                'action_id', c.action_id,
                                                'rempli', c.atteint,
+                                               'etoile', c.etoiles,
                                                'action_identifiant', ad.identifiant,
                                                'statut_ou_score',
                                                case
@@ -36,40 +44,40 @@ with criteres as (select *
                                                    end
                                            )
                                    )               as liste
-                        from labellisation.criteres(labellisation_parcours.collectivite_id) c
+                        from all_critere c
                                  join action_definition ad on c.action_id = ad.action_id
                         group by c.referentiel) ral)
 select e.referentiel,
        e.etoile_objectif,
-       rs.complet                             as completude_ok,
+       rs.complet                                      as completude_ok,
 
        jsonb_build_object(
                'score_a_realiser', cs.score_a_realiser,
                'score_fait', cs.score_fait,
                'atteint', cs.atteint,
-               'etoiles', cs.etoile_objectif) as critere_score,
+               'etoiles', cs.etoile_objectif)          as critere_score,
 
-       criteres.liste                         as criteres_action,
-       criteres.atteints and cs.atteint and cf.atteint       as rempli,
+       criteres.liste                                  as criteres_action,
+       criteres.atteints and cs.atteint and cf.atteint as rempli,
        calendrier.information,
 
        case
            when demande.etoiles is null
                then null
            else jsonb_build_object('demandee_le', demande.date, 'etoiles', demande.etoiles)
-           end                                as derniere_demande,
+           end                                         as derniere_demande,
 
        case
            when obtention.etoiles is null
                then null
            else jsonb_build_object('obtenue_le', obtention.date, 'etoiles', obtention.etoiles)
-           end                                as derniere_labellisation
+           end                                         as derniere_labellisation
 
-from labellisation.etoiles(labellisation_parcours.collectivite_id) as e
+from etoiles as e
          join criteres on criteres.referentiel = e.referentiel
          left join labellisation.referentiel_score(labellisation_parcours.collectivite_id) rs
                    on rs.referentiel = e.referentiel
-         left join labellisation.critere_score(labellisation_parcours.collectivite_id) cs
+         left join labellisation.critere_score_global(labellisation_parcours.collectivite_id) cs
                    on cs.referentiel = e.referentiel
          left join labellisation.critere_fichier(labellisation_parcours.collectivite_id) cf
                    on cf.referentiel = e.referentiel
@@ -83,7 +91,6 @@ from labellisation.etoiles(labellisation_parcours.collectivite_id) as e
                             from labellisation l
                             where l.collectivite_id = labellisation_parcours.collectivite_id
                               and l.referentiel = e.referentiel) obtention on true
-
 $$
     language sql security definer;
 comment on function labellisation_parcours is
