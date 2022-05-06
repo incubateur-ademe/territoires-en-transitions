0cfbf6b97ee68c8d2653ded391d69b8fd3fa847f
Marc Rutkowski <marc@attractive-media.fr>
2022-05-06 21:27:16 +0200

    ajoute les hooks pour les preuves, la demande et le parcours de labellisation

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useAddPreuves.tsx
@@ -0,0 +1,29 @@
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {labellisationFichierWriteEndpoint} from 'core-logic/api/endpoints/LabellisationFichierWriteEndpoint';
+import {TAddFileFromLib} from 'ui/shared/ResourceManager/AddFile';
+
+type TAddDocs = (demande_id: number) => {
+  /** ajoute un fichier sélectionné depuis la bibliothèque */
+  addFileFromLib: TAddFileFromLib;
+};
+
+/** Renvoie les gestionnaires d'événements du dialogue d'ajout de
+ * fichiers au parcours de labellisation en cours */
+export const useAddPreuves: TAddDocs = (demande_id: number) => {
+  const collectivite_id = useCollectiviteId();
+
+  // associe un fichier sélectionné depuis la bibliothèque
+  const addFileFromLib: TAddFileFromLib = filename =>
+    collectivite_id
+      ? labellisationFichierWriteEndpoint.save({
+          demande_id,
+          collectivite_id,
+          commentaire: '',
+          filename,
+        })
+      : Promise.resolve(false);
+
+  return {
+    addFileFromLib,
+  };
+};
