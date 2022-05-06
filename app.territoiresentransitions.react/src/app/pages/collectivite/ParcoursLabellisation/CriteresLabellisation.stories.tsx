aae31857400f2c87a424eefe595467316d5cfdb1
Marc Rutkowski <marc@attractive-media.fr>
2022-05-06 21:38:14 +0200

    assemble les différents critères

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/CriteresLabellisation.stories.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/CriteresLabellisation.stories.tsx
@@ -1,5 +1,4 @@
 import {Story, Meta} from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 import {
   CriteresLabellisation,
   TCriteresLabellisationProps,
@@ -11,10 +10,31 @@ export default {
 } as Meta;
 
 const Template: Story<TCriteresLabellisationProps> = args => (
-  <CriteresLabellisation {...args} />
+  <CriteresLabellisation collectiviteId={1} {...args} />
 );
 
-export const PremiereEtoile = Template.bind({});
-PremiereEtoile.args = {
+export const PremiereEtoileECI = Template.bind({});
+PremiereEtoileECI.args = {
   parcours: fixture.parcours1,
+  demande: {
+    id: 1,
+    en_cours: true,
+    collectivite_id: 1,
+    referentiel: 'eci',
+    etoiles: '1',
+  },
+  preuves: [],
+};
+
+export const PremiereEtoileCAE = Template.bind({});
+PremiereEtoileCAE.args = {
+  parcours: {...fixture.parcours1, referentiel: 'cae'},
+  demande: {
+    id: 1,
+    en_cours: true,
+    collectivite_id: 1,
+    referentiel: 'cae',
+    etoiles: '1',
+  },
+  preuves: [],
 };
