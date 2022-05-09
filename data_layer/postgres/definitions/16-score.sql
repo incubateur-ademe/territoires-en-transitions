0bd6b845fcdd32eebb75718f3af61c1a81e65388
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-05-09 17:39:03 +0200

    Fix view unprocessed_action_statut_update_event to re-compute all scores when referetiel has been updaetd

--- data_layer/postgres/definitions/16-score.sql
+++ data_layer/postgres/definitions/16-score.sql
@@ -51,23 +51,37 @@ with
                v.referentiel,
                max(coalesce(v.max_date, r.created_at)) as max_date
         from virtual_inital_event v
-                 full join action_statut_update_event r on r.collectivite_id = v.collectivite_id
+             full join action_statut_update_event r on r.collectivite_id = v.collectivite_id
         group by v.collectivite_id, v.referentiel
+    ),
+    -- last time points where updated for a referentiel 
+    latest_referentiel_modification as (
+      select referentiel, max(modified_at) as referentiel_last_modified_at 
+      from action_computed_points acp
+      left join action_relation ar on ar.id = acp.action_id
+      group by (referentiel)
+    ),
+    -- score require to be processed either if a statut is updated or if computed_points changed 
+    latest_score_update_required as (
+      select collectivite_id, r.referentiel, GREATEST(e.max_date::timestamp, 
+      r.referentiel_last_modified_at::timestamp) as required_at  
+      from latest_event e
+      left join latest_referentiel_modification r on r.referentiel = e.referentiel
     ),
     -- events that are not processed
     unprocessed as (
         select *
-        from latest_event e
+        from latest_score_update_required lsur
         where collectivite_id not in (
             -- processed means that the score is more recent than the event
             select collectivite_id
             from client_scores s
-            where s.score_created_at > e.max_date
+            where s.score_created_at > lsur.required_at
         )
     )
 select unprocessed.collectivite_id,
        unprocessed.referentiel,
-       unprocessed.max_date as created_at
+       unprocessed.required_at as created_at
 from unprocessed
 ;
 comment on view unprocessed_action_statut_update_event is
