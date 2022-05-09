51259cac0b5bdf8ec57caefc1f89e95bd87ac80a
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-05-09 15:16:03 +0200

    Fix migration file 10-add-cae_5.1.2.1.1

--- data_layer/migrations/10-add-cae_5.1.2.1.1.sql
+++ data_layer/migrations/10-add-cae_5.1.2.1.1.sql
@@ -8,7 +8,7 @@ update action_statut set action_id = 'cae_5.1.2.1.5' where action_id = 'cae_5.1.
 
 -- Replace 5.1.2.1.3 into 5.1.2.1.4
 update action_commentaire set action_id = 'cae_5.1.2.1.4' where action_id = 'cae_5.1.2.1.3'; 
-update action_statut set action_id = 'cae_5.1.2.1.4.' where action_id = 'cae_5.1.2.1.3'; 
+update action_statut set action_id = 'cae_5.1.2.1.4' where action_id = 'cae_5.1.2.1.3'; 
 
 -- Replace 5.1.2.1.2 into 5.1.2.1.3
 update action_commentaire set action_id = 'cae_5.1.2.1.3' where action_id = 'cae_5.1.2.1.2'; 
