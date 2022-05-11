ee717690a8e4b24320d3a1bcb7d322dbd6e4b76f
Marc Rutkowski <marc@attractive-media.fr>
2022-05-11 14:17:39 +0200

    affiche un message plutôt qu'une page vide lorsque le parcours n'est pas disponible (référentiel non renseigné)

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/ParcoursLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/ParcoursLabellisation.tsx
@@ -1,10 +1,16 @@
-import {useCollectiviteId, useReferentielId} from 'core-logic/hooks/params';
+import {Link} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
+import {useCollectiviteId, useReferentielId} from 'core-logic/hooks/params';
 import {referentielToName} from 'app/labels';
 import {CriteresLabellisation} from './CriteresLabellisation';
 import {usePreuves} from './usePreuves';
 import {useParcoursLabellisation} from './useParcoursLabellisation';
 import {Header} from './Header';
+import {ReferentielOfIndicateur} from 'types/litterals';
+import {
+  makeCollectiviteReferentielUrl,
+  ReferentielParamOption,
+} from 'app/paths';
 
 export default observer(() => {
   const collectiviteId = useCollectiviteId();
@@ -12,7 +18,45 @@ export default observer(() => {
   const {parcours, demande} = useParcoursLabellisation(referentiel);
   const preuves = usePreuves(demande?.id);
 
-  return collectiviteId && demande && parcours ? (
+  // cas particulier : le référentiel n'est pas du tout renseigné
+  if (!parcours) {
+    return (
+      <>
+        <h1 className="text-center fr-mt-4w fr-mb-1w">
+          Parcours de labellisation
+        </h1>
+        {referentiel ? (
+          <p className="text-center text-[22px]">
+            Référentiel{' '}
+            {referentielToName[referentiel as ReferentielOfIndicateur]}
+          </p>
+        ) : null}
+        <main className="fr-container mt-9 mb-16">
+          <p>
+            Ce référentiel n’est pas encore renseigné pour votre collectivité.
+            Pour commencer à visualiser votre progression, mettez à jour les
+            statuts des actions.
+          </p>
+
+          {collectiviteId && referentiel ? (
+            <div className="flex justify-center">
+              <Link
+                className="fr-btn fr-btn--secondary "
+                to={makeCollectiviteReferentielUrl({
+                  collectiviteId,
+                  referentielId: referentiel as ReferentielParamOption,
+                })}
+              >
+                Mettre à jour le référentiel
+              </Link>
+            </div>
+          ) : null}
+        </main>
+      </>
+    );
+  }
+
+  return collectiviteId && parcours ? (
     <>
       <h1 className="text-center fr-mt-4w fr-mb-1w">
         Parcours de labellisation
