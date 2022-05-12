cbf45e6df18022b40f1ee093356329adcc72d94a
Marc Rutkowski <marc@attractive-media.fr>
2022-05-12 13:05:54 +0200

    fixe le rechargement du parcours lors du changement d'un référentiel à un autre

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useParcoursLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useParcoursLabellisation.tsx
@@ -10,6 +10,11 @@ import {LabellisationDemandeRead} from 'generated/dataLayer/labellisation_demand
 import {labellisationDemandeReadEndpoint} from 'core-logic/api/endpoints/LabellisationDemandeReadEndpoint';
 import {authBloc, currentCollectiviteBloc} from 'core-logic/observables';
 
+type SubToRef = {
+  ref: string;
+  sub: RealtimeSubscription;
+};
+
 /** Renvoie les données de labellisation de la collectivité courante */
 export const useParcoursLabellisation = (
   referentiel: string | null
@@ -22,9 +27,7 @@ export const useParcoursLabellisation = (
     null
   );
   const [demande, setDemande] = useState<LabellisationDemandeRead | null>(null);
-  const [subscription, setSubscription] = useState<RealtimeSubscription | null>(
-    null
-  );
+  const [subscription, setSubscription] = useState<SubToRef | null>(null);
 
   // crée la demande
   const createDemande = async (
@@ -69,7 +72,11 @@ export const useParcoursLabellisation = (
   };
 
   // charge les données du parcours
-  const fetch = async () => {
+  const fetch = async (ref: string | null) => {
+    if (!ref) {
+      return;
+    }
+
     if (collectivite_id) {
       const parcoursList = await labellisationParcoursReadEndpoint.getBy({
         collectivite_id,
@@ -77,8 +84,7 @@ export const useParcoursLabellisation = (
 
       // trouve le parcours pour le référentiel
       const p =
-        parcoursList?.find(parcours => parcours.referentiel === referentiel) ||
-        null;
+        parcoursList?.find(parcours => parcours.referentiel === ref) || null;
 
       if (p) {
         const {criteres_action} = p;
@@ -93,46 +99,62 @@ export const useParcoursLabellisation = (
       }
     }
   };
-  useEffect(() => {
-    fetch();
-  }, [collectivite_id, referentiel]);
 
   // recharge les données après un changement de statut d'une action
-  const refetch = (payload: {new: {collectivite_id: number}}) => {
+  const refetch = (ref: string, payload: {new: {collectivite_id: number}}) => {
     if (collectivite_id && payload?.new?.collectivite_id === collectivite_id) {
       // si on recharge trop vite lors du changement de statut, il semblerait que la
       // RPC appelée par labellisationParcoursReadEndpoint ne soit pas à jour (?)
       // alors on ajoute un delai avec de recharger
-      setTimeout(() => fetch(), 300);
+      setTimeout(() => fetch(ref), 500);
     }
   };
 
+  // souscrit aux changements de statuts dans un référentiel
+  const subscribe = (ref: string) => {
+    const subscribeTo = `action_statut:collectivite_id=eq.${collectivite_id}`;
+    const sub = supabaseClient
+      .from(subscribeTo)
+      .on('INSERT', payload => refetch(ref, payload))
+      .on('UPDATE', payload => refetch(ref, payload))
+      .subscribe();
+    setSubscription({sub, ref});
+  };
+
+  // chargement initial des données
+  useEffect(() => {
+    if (referentiel) {
+      fetch(referentiel);
+    }
+  }, [collectivite_id, referentiel, subscription]);
+
   // souscrit aux changements de statuts si ce n'est pas déjà fait
   useEffect(() => {
-    if (!subscription && collectivite_id && referentiel) {
-      const subscribeTo = `action_statut:collectivite_id=eq.${collectivite_id}`;
-      const sub = supabaseClient
-        .from(subscribeTo)
-        .on('INSERT', refetch)
-        .on('UPDATE', refetch)
-        .subscribe();
-      setSubscription(sub);
+    if (collectivite_id && referentiel) {
+      if (subscription && subscription.ref !== referentiel) {
+        subscription.sub.unsubscribe();
+      }
+      subscribe(referentiel);
     }
 
     // supprime la souscription quand le composant est démonté
     return () => {
-      subscription?.unsubscribe();
-      setSubscription(null);
+      if (subscription) {
+        subscription.sub.unsubscribe();
+        setSubscription(null);
+      }
     };
   }, [collectivite_id, referentiel]);
 
   // recharge les données quand la demande est mise à jour lors de l'envoi
   useEffect(() => {
-    labellisationDemandeWriteEndpoint.addListener(fetch);
+    labellisationDemandeWriteEndpoint.addListener(() => fetch(referentiel));
     return () => {
-      labellisationDemandeWriteEndpoint.removeListener(fetch);
+      labellisationDemandeWriteEndpoint.removeListener(() =>
+        fetch(referentiel)
+      );
     };
-  }, []);
+  }, [collectivite_id, referentiel]);
 
   // renvoie le parcours correspondant au référentiel courant
   return {parcours, demande};
