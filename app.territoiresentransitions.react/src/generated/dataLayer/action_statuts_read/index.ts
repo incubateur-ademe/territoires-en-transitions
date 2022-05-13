3fe1afc173d8ec087126532de9d2d70169644bd1
Marc Rutkowski <marc@attractive-media.fr>
2022-05-13 16:04:17 +0200

    ajoute le tableau pour la vue Détail des tâches (WIP)
    (cherry picked from commit 14faceeaa3c8f9a9852f57a652d293d8d625d3b3)

---
+++ app.territoiresentransitions.react/src/generated/dataLayer/action_statuts_read/index.ts
@@ -0,0 +1,27 @@
+export interface IActionStatutsRead {
+  collectivite_id: number;
+  action_id: string;
+  referentiel: 'eci' | 'cae';
+  type: 'referentiel' | 'axe' | 'sous-axe' | 'action' | 'sous-action' | 'tache';
+  children: string[];
+  parents: string[];
+  depth: number;
+  have_children: boolean;
+  identifiant: string;
+  nom: string;
+  description: string;
+  have_exemples: boolean;
+  have_preuve: boolean;
+  have_ressources: boolean;
+  have_reduction_potentiel: boolean;
+  have_perimetre_evaluation: boolean;
+  have_contexte: boolean;
+  avancement:
+    | null
+    | 'fait'
+    | 'pas_fait'
+    | 'programme'
+    | 'non_renseigne'
+    | 'detaille';
+  avancement_detaille: null | [number, number, number];
+}
