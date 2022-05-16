42544e55fb93ebc01860645c3223da25045e3281
Florian d'Erfurth <florian@derfurth.com>
2022-05-16 16:59:03 +0200

    Aggregate action statuts for descendants, so we can filter parents by descendant statuts.

--- data_layer/postgres/definitions/34-filterable_referentiel_view.sql
+++ data_layer/postgres/definitions/34-filterable_referentiel_view.sql
@@ -21,46 +21,37 @@ with recursive
                      join action_relation c
                           on c.parent = p.id
             where not c.id = any (parents)),
-    parent_child as
-        -- +-----+-------+
-        -- |p    |c      |
-        -- +-----+-------+
-        -- |cae  |cae_2  |
-        -- |cae  |cae_6  |
-        -- |cae_2|cae_2.1|
-        (select parent.id as id, child.id as c
-         from action_relation child
-                  left join action_relation parent on parent.id = child.parent),
-    parent_children as
-        -- +-------+-------------------------------------+
-        -- |p      |children                             |
-        -- +-------+-------------------------------------+
-        -- |cae    |{cae_6,cae_1,cae_2,cae_4,cae_3,cae_5}|
-        -- |cae_1  |{cae_1.1,cae_1.2,cae_1.3}            |
-        -- |cae_1.1|{cae_1.1.2,cae_1.1.3,cae_1.1.1}      |
-        (select id, array_agg(c) as children
-         from parent_child
-         group by id)
+    parent_descendants as
+        -- +-------+---------------------------------------------+
+        -- |p      |descendants                                  |
+        -- +-------+---------------------------------------------+
+        -- |cae    |{cae_2,cae_6,cae_3,cae_1,... cae_2.1.2.6,...}|
+        -- |cae_1  |{cae_1.1,cae_1.2,cae_1.3,...,cae_1.2.4,...}  |
+        -- |cae_1.1|{cae_1.1.2,cae_1.1.3,...,cae_1.1.3.3...}     |
+        (select ar.id, array_agg(afp.id) as descendants
+         from action_relation ar
+                  join actions_from_parents afp on ar.id = any (afp.parents)
+         group by ar.id)
 -- +---+-------------------------------------+-----+
 -- |id |children                             |depth|
 -- +---+-------------------------------------+-----+
 -- |cae|{cae_4,cae_5,cae_2,cae_6,cae_1,cae_3}|0    |
 -- |eci|{eci_2,eci_3,eci_4,eci_1,eci_5}      |0    |
-select actions_from_parents.id                               as action_id,
-       actions_from_parents.referentiel                      as referentiel,
-       coalesce(parent_children.children, '{}'::action_id[]) as children,
-       parent_children.children is not null                  as have_children,
-       actions_from_parents.parents                          as parents,
-       actions_from_parents.depth                            as depth,
+select actions_from_parents.id                                     as action_id,
+       actions_from_parents.referentiel                            as referentiel,
+       coalesce(parent_descendants.descendants, '{}'::action_id[]) as descendants,
+       parent_descendants.descendants is not null                  as have_children,
+       actions_from_parents.parents                                as ascendants,
+       actions_from_parents.depth                                  as depth,
        coalesce(
                case
                    when actions_from_parents.referentiel = 'cae'
                        then ('{"axe", "sous-axe", "action", "sous-action", "tache"}'::action_type[])[actions_from_parents.depth]
                    else ('{"axe", "action", "sous-action", "tache"}'::action_type[])[actions_from_parents.depth]
                    end
-           , 'referentiel')                                  as type
+           , 'referentiel')                                        as type
 from actions_from_parents
-         left join parent_children on actions_from_parents.id = parent_children.id
+         left join parent_descendants on actions_from_parents.id = parent_descendants.id
 order by naturalsort(actions_from_parents.id);
 
 
@@ -72,8 +63,8 @@ select
     d.action_id,
     d.referentiel,
     h.type,
-    h.children,
-    h.parents,
+    h.descendants,
+    h.ascendants,
     h.depth,
     h.have_children,
 
@@ -90,7 +81,10 @@ select
 
     -- action statuts:
     s.avancement,
-    s.avancement_detaille
+    s.avancement_detaille,
+
+    -- children status: the set of statuts of all children
+    cs.avancements               as avancement_descendants
 
 from collectivite c
          -- definitions
@@ -98,6 +92,29 @@ from collectivite c
          join action_hierarchy h on d.action_id = h.action_id
     -- collectivité data
          left join action_statut s on c.id = s.collectivite_id and s.action_id = d.action_id
+         left join lateral (
+    -- loop on every row to aggregate descendants statuts
+    select case
+               -- aucun descendant
+               when h.descendants = '{}' then
+                   '{}'::avancement[]
+               -- aucun statut pour les enfants
+               when count(*) = 0 then
+                   '{non_renseigne}'::avancement[]
+               -- des statuts mais pas pour chaque enfant
+               when array_length(h.descendants, 1) != count(*) then
+                   '{non_renseigne}'::avancement[] || array_agg(s.avancement)
+               -- des statuts pour chaque enfant
+               else
+                   array_agg(s.avancement)
+               end
+               as avancements
+    from action_statut s
+    where c.id = s.collectivite_id
+      and s.action_id = any (h.descendants)
+    ) cs on true
 order by c.id;
 
-
+select action_id, avancement, avancement_descendants, ascendants, descendants
+from action_statuts
+where collectivite_id = 1
