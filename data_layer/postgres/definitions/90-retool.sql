0fad7cb19e83cdd2fb3c57d4fbd9b346772e6b5e
Florian d'Erfurth <florian@derfurth.com>
2022-05-20 15:39:29 +0200

    add missing view

--- data_layer/postgres/definitions/90-retool.sql
+++ data_layer/postgres/definitions/90-retool.sql
@@ -226,3 +226,16 @@ as
 select l.*, nom as collectivite_nom
 from labellisation l
          join named_collectivite nc on l.collectivite_id = nc.collectivite_id;
+
+create or replace view retool_labellisation_demande
+as
+select ld.id,
+       ld.en_cours,
+       ld.collectivite_id,
+       ld.referentiel,
+       ld.etoiles,
+       ld.date,
+       nc.nom
+from labellisation_demande ld
+         left join named_collectivite nc on ld.collectivite_id = nc.collectivite_id
+;
