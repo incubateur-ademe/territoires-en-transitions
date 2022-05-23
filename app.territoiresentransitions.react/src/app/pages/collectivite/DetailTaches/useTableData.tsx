aa7ebf23772e160d0d9507469955b81a233e07e3
Marc Rutkowski <marc@attractive-media.fr>
2022-05-23 18:30:47 +0200

    gère la mise à jour des statuts et affiche le nombre de tâches, le total et le bouton de remise à zéro des filtres

--- app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/useTableData.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/useTableData.tsx
@@ -1,8 +1,13 @@
 import {useCallback, useMemo, useState} from 'react';
-import {useQuery} from 'react-query';
+import {useMutation, useQuery, useQueryClient} from 'react-query';
 import {TableOptions} from 'react-table';
 import {useCollectiviteId, useReferentielId} from 'core-logic/hooks/params';
-import {fetchActionStatutsList, TacheDetail} from './queries';
+import {
+  fetchActionStatutsList,
+  getTotalTachesCount,
+  TacheDetail,
+  updateTacheStatut,
+} from './queries';
 
 export type UseTableData = () => TableData;
 
@@ -14,10 +19,18 @@ export type TableData = {
   >;
   /** Indique que le chargement des données est en cours */
   isLoading: boolean;
+  /** Indique que le changement de statut est en cours */
+  isSaving: boolean;
   /** filtres actifs */
   filters: string[];
+  /** Nombre de lignes après filtrage */
+  count: number;
+  /** Nombre total de lignes */
+  total: number;
   /** pour remettre à jour les filtres */
   setFilters: (filters: string[]) => void;
+  /** pour changer le statut d'une tâche */
+  updateStatut: (action_id: string, value: string) => void;
 };
 
 /**
@@ -26,15 +39,37 @@ export type TableData = {
 export const useTableData: UseTableData = () => {
   const collectivite_id = useCollectiviteId();
   const referentiel = useReferentielId();
+  const queryClient = useQueryClient();
 
   // filtre initial
   const [filters, setFilters] = useState(['non_renseigne']);
 
   // chargement des données
-  const {data: rows, isLoading} = useQuery(
+  const {data, isLoading} = useQuery(
     ['action_statuts', collectivite_id, referentiel, ...filters],
     () => fetchActionStatutsList(collectivite_id, referentiel, filters)
   );
+  const {rows, count} = data || {};
+
+  // charge le nombre total de tâches du référentiel
+  const {data: total} = useQuery(['action_statuts', referentiel], () =>
+    getTotalTachesCount(referentiel)
+  );
+
+  // met à jour un statut
+  const {mutate, isLoading: isSaving} = useMutation(updateTacheStatut);
+  const updateStatut = (action_id: string, avancement: string) => {
+    if (collectivite_id && !isSaving) {
+      mutate(
+        {collectivite_id, action_id, avancement},
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries('action_statuts');
+          },
+        }
+      );
+    }
+  };
 
   // extrait les lignes de 1er niveau
   const firstLevel = useMemo(
@@ -67,5 +102,9 @@ export const useTableData: UseTableData = () => {
     filters,
     setFilters,
     isLoading,
+    isSaving,
+    count: count || 0,
+    total: total || 0,
+    updateStatut,
   };
 };
