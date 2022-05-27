be30623bc243e094836d787315e2dbb6e3a052d9
Florian d'Erfurth <florian@derfurth.com>
2022-05-27 17:12:27 +0200

    fix permissions

--- data_layer/postgres/definitions/33a-demande_labellisation.sql
+++ data_layer/postgres/definitions/33a-demande_labellisation.sql
@@ -3,16 +3,23 @@ create or replace function
     returns labellisation.demande
 as
 $$
-insert into labellisation.demande (collectivite_id, referentiel, etoiles)
-values (labellisation_demande.collectivite_id, labellisation_demande.referentiel, labellisation_demande.etoiles)
-on conflict do nothing ;
+with data as (select labellisation_demande.collectivite_id,
+                     labellisation_demande.referentiel,
+                     labellisation_demande.etoiles
+              where is_any_role_on(labellisation_demande.collectivite_id))
+
+insert
+into labellisation.demande (collectivite_id, referentiel, etoiles)
+select *
+from data
+on conflict do nothing;
 
 select *
 from labellisation.demande ld
 where ld.collectivite_id = labellisation_demande.collectivite_id
   and ld.referentiel = labellisation_demande.referentiel
   and ld.etoiles = labellisation_demande.etoiles
-$$ language sql;
+$$ language sql security definer;
 comment on function labellisation_demande is
     'Renvoie la demande de labellisation pour une collectivité, un référentiel et un nombre d''étoiles donnés.'
         'Crée une demande en cours si aucune demande correspondante n''existe.';
@@ -23,9 +30,16 @@ create or replace function
     returns labellisation.demande
 as
 $$
-insert into labellisation.demande (collectivite_id, referentiel, etoiles, en_cours)
-values (labellisation_submit_demande.collectivite_id, labellisation_submit_demande.referentiel,
-        labellisation_submit_demande.etoiles, false)
+
+with data as (select labellisation_submit_demande.collectivite_id,
+                     labellisation_submit_demande.referentiel,
+                     labellisation_submit_demande.etoiles
+              where is_any_role_on(labellisation_submit_demande.collectivite_id))
+
+insert
+into labellisation.demande (collectivite_id, referentiel, etoiles, en_cours)
+select *, false
+from data
 on conflict (collectivite_id, referentiel, etoiles) do update set en_cours = false;
 
 select *
@@ -33,7 +47,7 @@ from labellisation.demande ld
 where ld.collectivite_id = labellisation_submit_demande.collectivite_id
   and ld.referentiel = labellisation_submit_demande.referentiel
   and ld.etoiles = labellisation_submit_demande.etoiles
-$$ language sql;
+$$ language sql security definer ;
 comment on function labellisation_submit_demande is
     'Soumet une demande de labellisation pour une collectivité, un référentiel et un nombre d''étoiles donnés.'
         'Met à jour ou créé une demande qui n''est pas en cours.';
