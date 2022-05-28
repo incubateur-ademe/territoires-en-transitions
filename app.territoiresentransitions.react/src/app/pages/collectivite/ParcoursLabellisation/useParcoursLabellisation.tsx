2a4f870ccd78d4f8c50f3aad1f6aa70ae0bbd03d
Marc Rutkowski <marc@attractive-media.fr>
2022-05-28 14:12:30 +0200

    utilise les nouveaux endpoints pour la demande de labellisation (et simplifie le code avec react-query)

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useParcoursLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useParcoursLabellisation.tsx
@@ -1,14 +1,13 @@
 import {useEffect, useState} from 'react';
+import {useQuery, useQueryClient} from 'react-query';
 import {RealtimeSubscription} from '@supabase/realtime-js';
 import {LabellisationParcoursRead} from 'generated/dataLayer/labellisation_parcours_read';
-import {labellisationParcoursReadEndpoint} from 'core-logic/api/endpoints/LabellisationParcoursReadEndpoint';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {supabaseClient} from 'core-logic/api/supabase';
-import {labellisationDemandeWriteEndpoint} from 'core-logic/api/endpoints/LabellisationDemandeWriteEndpoint';
-import {LabellisationDemandeWrite} from 'generated/dataLayer/labellisation_demande_write';
 import {LabellisationDemandeRead} from 'generated/dataLayer/labellisation_demande_read';
-import {labellisationDemandeReadEndpoint} from 'core-logic/api/endpoints/LabellisationDemandeReadEndpoint';
-import {authBloc, currentCollectiviteBloc} from 'core-logic/observables';
+import {fetchParcours, getReferentielParcours} from './queries';
+import {useDemandeLabellisation} from './useDemandeLabellisation';
+import {ReferentielParamOption} from 'app/paths';
 
 type SubToRef = {
   ref: string;
@@ -23,91 +22,26 @@ export const useParcoursLabellisation = (
   demande: LabellisationDemandeRead | null;
 } => {
   const collectivite_id = useCollectiviteId();
-  const [parcours, setParcours] = useState<LabellisationParcoursRead | null>(
-    null
-  );
-  const [demande, setDemande] = useState<LabellisationDemandeRead | null>(null);
   const [subscription, setSubscription] = useState<SubToRef | null>(null);
-
-  // crée la demande
-  const createDemande = async (
-    p: LabellisationParcoursRead
-  ): Promise<LabellisationDemandeWrite | null> => {
-    if (
-      collectivite_id &&
-      !currentCollectiviteBloc.readonly &&
-      authBloc.connected
-    ) {
-      const {referentiel, etoiles} = p;
-      return labellisationDemandeWriteEndpoint.save({
-        collectivite_id,
-        etoiles,
-        referentiel,
-      });
-    }
-    return Promise.resolve(null);
-  };
-
-  // charge les données de la demande
-  const fetchDemande = async (p: LabellisationParcoursRead) => {
-    if (collectivite_id && p) {
-      const {referentiel, etoiles} = p;
-      // charge les demandes
-      const demandes = await labellisationDemandeReadEndpoint.getBy({
-        collectivite_id,
-        etoiles,
-        referentiel,
-      });
-
-      if (!demandes?.length) {
-        // crée la demande si elle n'existe pas
-        const demande = await createDemande(p);
-        if (demande) {
-          setDemande(demande as LabellisationDemandeRead);
-        }
-      } else {
-        setDemande(demandes[0]);
-      }
-    }
-  };
+  const queryClient = useQueryClient();
 
   // charge les données du parcours
-  const fetch = async (ref: string | null) => {
-    if (!ref) {
-      return;
-    }
+  const {data: parcoursList} = useQuery(
+    ['labellisation_parcours', collectivite_id],
+    () => fetchParcours(collectivite_id)
+  );
+  const parcours = getReferentielParcours(parcoursList, referentiel);
+  const {etoiles} = parcours || {};
 
-    if (collectivite_id) {
-      const parcoursList = await labellisationParcoursReadEndpoint.getBy({
-        collectivite_id,
-      });
-
-      // trouve le parcours pour le référentiel
-      const p =
-        parcoursList?.find(parcours => parcours.referentiel === ref) || null;
-
-      if (p) {
-        const {criteres_action} = p;
-        setParcours({
-          ...p,
-          // trie les critères action
-          criteres_action: criteres_action.sort((a, b) => a.prio - b.prio),
-        });
-        fetchDemande(p);
-      } else {
-        setParcours(null);
-      }
-    }
-  };
+  // charge les données de la demande
+  const demande = useDemandeLabellisation(
+    referentiel as ReferentielParamOption,
+    etoiles
+  );
 
   // recharge les données après un changement de statut d'une action
-  const refetch = (ref: string, payload: {new: {collectivite_id: number}}) => {
-    if (collectivite_id && payload?.new?.collectivite_id === collectivite_id) {
-      // si on recharge trop vite lors du changement de statut, il semblerait que la
-      // RPC appelée par labellisationParcoursReadEndpoint ne soit pas à jour (?)
-      // alors on ajoute un delai avec de recharger
-      setTimeout(() => fetch(ref), 500);
-    }
+  const refetch = () => {
+    queryClient.invalidateQueries(['labellisation_parcours', collectivite_id]);
   };
 
   // souscrit aux changements de statuts dans un référentiel
@@ -115,18 +49,11 @@ export const useParcoursLabellisation = (
     const subscribeTo = `action_statut:collectivite_id=eq.${collectivite_id}`;
     const sub = supabaseClient
       .from(subscribeTo)
-      .on('INSERT', payload => refetch(ref, payload))
-      .on('UPDATE', payload => refetch(ref, payload))
+      .on('INSERT', refetch)
+      .on('UPDATE', refetch)
       .subscribe();
     setSubscription({sub, ref});
   };
-
-  // chargement initial des données
-  useEffect(() => {
-    if (referentiel) {
-      fetch(referentiel);
-    }
-  }, [collectivite_id, referentiel, subscription]);
 
   // souscrit aux changements de statuts si ce n'est pas déjà fait
   useEffect(() => {
@@ -146,16 +73,6 @@ export const useParcoursLabellisation = (
     };
   }, [collectivite_id, referentiel]);
 
-  // recharge les données quand la demande est mise à jour lors de l'envoi
-  useEffect(() => {
-    labellisationDemandeWriteEndpoint.addListener(() => fetch(referentiel));
-    return () => {
-      labellisationDemandeWriteEndpoint.removeListener(() =>
-        fetch(referentiel)
-      );
-    };
-  }, [collectivite_id, referentiel]);
-
   // renvoie le parcours correspondant au référentiel courant
-  return {parcours, demande};
+  return {parcours: parcours || null, demande: demande || null};
 };
