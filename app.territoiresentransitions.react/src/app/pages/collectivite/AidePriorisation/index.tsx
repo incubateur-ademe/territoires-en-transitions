92675534d7980477800a37040a439d94684b56d0
Marc Rutkowski <marc@attractive-media.fr>
2022-06-02 15:57:07 +0200

    supprime temporairement l'affichage des filtres dans la vue Priorisation

--- app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/index.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/index.tsx
@@ -3,25 +3,9 @@ import {Table} from './Table';
 
 export default () => {
   const tableData = useTableData();
-  const {count, total, filters, setFilters} = tableData;
-  const countFilters =
-    filters?.length && !filters.includes('tous') ? filters.length : 0;
-  const labelFilters = countFilters > 1 ? 'filtres actifs' : 'filtre actif';
-  const labelTaches = 'tâche' + (count > 1 ? 's' : '');
 
   return (
     <>
-      <p>
-        {countFilters} {labelFilters} ; {count} {labelTaches} sur {total}
-        {countFilters > 0 ? (
-          <button
-            className="fr-link fr-link--icon-left fr-fi-close-circle-fill fr-ml-2w"
-            onClick={() => setFilters(null)}
-          >
-            Désactiver tous les filtres
-          </button>
-        ) : null}
-      </p>
       <Table tableData={tableData} />
     </>
   );
