0c4909dfe22acb10f27552cde5f0e34e31d3a8b6
Florian d'Erfurth <florian@derfurth.com>
2022-06-08 15:42:14 +0200

    rm typing, fix label bug.

--- app.territoiresentransitions.react/src/app/pages/ToutesLesCollectivites/Filtres.tsx
+++ app.territoiresentransitions.react/src/app/pages/ToutesLesCollectivites/Filtres.tsx
@@ -1,97 +1,85 @@
 import {
-  niveauLabellisationCollectiviteFilterLibelleRecord,
-  populationCollectiviteFilterLibelleRecord,
-  referentielCollectiviteFilterLibelleRecord,
-  tauxRemplissageCollectiviteFilterLibelleRecord as tauxRemplissageCollectiviteFilterLibelleRecord,
-  libelleRecordToOptions,
-  typeCollectiviteFilterLibelleRecord,
-  trierParFilterLibelleRecord,
+  niveauLabellisationCollectiviteOptions,
+  populationCollectiviteOptions,
+  referentielCollectiviteOptions,
+  tauxRemplissageCollectiviteOptions,
+  typeCollectiviteOptions,
+  trierParOptions,
 } from 'app/pages/ToutesLesCollectivites/filtreLibelles';
 import {MultiSelectCheckboxes} from 'app/pages/ToutesLesCollectivites/MultiSelectCheckboxes';
 import {MultiSelectDropdown} from 'app/pages/ToutesLesCollectivites/MultiSelectDropdown';
 import {SelectDropdown} from 'app/pages/ToutesLesCollectivites/SelectDropdown';
-import {
-  TNiveauLabellisationFiltreOption,
-  TPopulationFiltreOption,
-  TReferentielFiltreOption,
-  TTauxRemplissageFiltreOption,
-  TTrierParFiltreOption,
-  TTypeFiltreOption,
-} from 'app/pages/ToutesLesCollectivites/types';
+
 import {RegionRead} from 'generated/dataLayer/region_read';
 import {DepartementRead} from 'generated/dataLayer/departement_read';
 
 export const TypeCollectiviteFiltre = (props: {
-  selected: TTypeFiltreOption[];
-  onChange: (selected: TTypeFiltreOption[]) => void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }) => (
   <MultiSelectCheckboxes
     title="Type de collectivité"
-    options={libelleRecordToOptions(typeCollectiviteFilterLibelleRecord)}
+    options={typeCollectiviteOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
 );
 
 export const PopulationCollectiviteFiltre = (props: {
-  selected: TPopulationFiltreOption[];
-  onChange: (selected: TPopulationFiltreOption[]) => void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }) => (
   <MultiSelectCheckboxes
     title="Population"
-    options={libelleRecordToOptions(populationCollectiviteFilterLibelleRecord)}
+    options={populationCollectiviteOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
 );
 
 export const ReferentielCollectiviteFiltre = (props: {
-  selected: TReferentielFiltreOption[];
-  onChange: (selected: TReferentielFiltreOption[]) => void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }) => (
   <MultiSelectCheckboxes
     title="Référentiel"
-    options={libelleRecordToOptions(referentielCollectiviteFilterLibelleRecord)}
+    options={referentielCollectiviteOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
 );
 
 export const NiveauDeLabellisationCollectiviteFiltre = (props: {
-  selected: TNiveauLabellisationFiltreOption[];
-  onChange: (selected: TNiveauLabellisationFiltreOption[]) => void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }) => (
   <MultiSelectCheckboxes
     title="Niveau de labellisation"
-    options={libelleRecordToOptions(
-      niveauLabellisationCollectiviteFilterLibelleRecord
-    )}
+    options={niveauLabellisationCollectiviteOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
 );
 
 export const TauxRemplissageCollectiviteFiltre = (props: {
-  selected: TTauxRemplissageFiltreOption[];
-  onChange: (selected: TTauxRemplissageFiltreOption[]) => void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }) => (
   <MultiSelectCheckboxes
     title="Taux de remplissage"
-    options={libelleRecordToOptions(
-      tauxRemplissageCollectiviteFilterLibelleRecord
-    )}
+    options={tauxRemplissageCollectiviteOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
 );
 
 export const TrierParFiltre = (props: {
-  selected?: TTrierParFiltreOption;
-  onChange: (selected?: TTrierParFiltreOption) => void;
+  selected?: string;
+  onChange: (selected?: string) => void;
 }) => (
   <SelectDropdown
     title="Trier par"
-    options={libelleRecordToOptions(trierParFilterLibelleRecord)}
+    options={trierParOptions}
     onChange={props.onChange}
     selected={props.selected}
   />
