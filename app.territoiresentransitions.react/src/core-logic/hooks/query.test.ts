7d04837d2dbdb91941414ccfadb26cffb4036b56
Marc Rutkowski <marc@attractive-media.fr>
2022-06-09 15:45:51 +0200

    supprime les paramètres inconnus de l'URL

--- app.territoiresentransitions.react/src/core-logic/hooks/query.test.ts
+++ app.territoiresentransitions.react/src/core-logic/hooks/query.test.ts
@@ -7,13 +7,27 @@ describe('searchParamsToObject', () => {
       {prop3: ['v4']},
       {p1: 'prop1', p2: 'prop2', p3: 'prop3'}
     );
-    expect(result).toMatchObject({
+    expect(result).toEqual({
       prop1: ['v1'],
       prop2: ['v2', 'v3'],
       prop3: ['v4'],
     });
   });
 
+  it('doit filtrer les paramètres non définis dans la table de correspondances', () => {
+    const result = searchParamsToObject(
+      new URLSearchParams('?p1=v1&p2=v2,v3'),
+      {prop3: ['v4']},
+      {p1: 'prop1', p3: 'prop3'}
+    );
+    expect(result).toEqual({
+      prop1: ['v1'],
+      prop3: ['v4'],
+    });
+  });
+});
+
+describe('objectToSearchParams', () => {
   it("doit renvoyer une chaîne de paramètres d'URL, à partir d'un objet et d'une table de correspondance entre noms des propriétés et noms raccourcis pour l'URL", () => {
     const result = objectToSearchParams(
       {
@@ -24,5 +38,17 @@ describe('searchParamsToObject', () => {
       {prop1: 'p1', prop2: 'p2', prop3: 'p3'}
     );
     expect(result).toEqual('p1=v1&p2=v2,v3&p3=v4');
+  });
+
+  it('doit filtrer les paramètres non définis dans la table de correspondances', () => {
+    const result = objectToSearchParams(
+      {
+        prop1: ['v1'],
+        prop2: ['v2', 'v3'],
+        prop3: ['v4'],
+      },
+      {prop1: 'p1', prop2: 'p2'}
+    );
+    expect(result).toEqual('p1=v1&p2=v2,v3');
   });
 });
