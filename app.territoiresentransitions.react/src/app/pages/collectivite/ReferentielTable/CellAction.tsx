97acbe9aeaa73da43afe25dadd5d0ba08ebe7337
Marc Rutkowski <marc@attractive-media.fr>
2022-06-13 12:17:43 +0200

    change la manière de calculer le décalage à gauche des lignes en fonction du niveau

--- app.territoiresentransitions.react/src/app/pages/collectivite/ReferentielTable/CellAction.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ReferentielTable/CellAction.tsx
@@ -9,23 +9,41 @@ export type TCellProps = CellProps<ActionReferentiel> & {
   maxDepth?: number | null;
 };
 
+// décalage à gauche des lignes en fonction du niveau
+const paddingByLevel: Record<number, number> = {
+  1: 0,
+  2: 16,
+  // au dessus de 2 un décalage supplémentaire est appliqué par l'affichage de l'identifiant, il n'est donc pâs reporté ici
+  3: 16,
+  4: 26,
+  5: 36,
+};
+
+// décalage supplémentaire appliqué quand on affiche le bouton Expand
+const EXPAND_OFFSET = 36;
+
 /**
  * Affiche le nom d'une action, éventuellement précédé de l'identifiant et d'un
  * picto reflétant l'état plié/déplié lorsqu'il y a des descandants
  */
 export const CellAction = (props: TCellProps) => {
   const {row, value, collectiviteId, referentielId, maxDepth} = props;
-  const {depth, have_children, identifiant} = row.original;
-  const style = {
-    paddingLeft: (depth - 1) * (depth > 2 && have_children ? 8 : 24),
-  };
+  const {depth, identifiant} = row.original;
+  const haveSubrows = row.subRows.length > 0;
   const isNotMaxDepth = !maxDepth || depth < maxDepth;
+  const showExpand = haveSubrows && isNotMaxDepth;
+
+  // applique un décalage en fonction du niveau + un décalage optionnel pour
+  // compenser l'absence du bouton Expand lorsque c'est nécessaire
+  const style = {
+    paddingLeft: paddingByLevel[depth] + (showExpand ? 0 : EXPAND_OFFSET),
+  };
 
   return collectiviteId && referentielId ? (
     <>
       {depth > 2 ? <span className="identifiant">{identifiant}</span> : null}
       <span style={style}>
-        {have_children && isNotMaxDepth ? <Expand {...props} /> : null}
+        {showExpand ? <Expand {...props} /> : null}
         <span className={depth === 3 && isNotMaxDepth ? 'pill' : undefined}>
           <Link
             className="hover:underline"
@@ -52,5 +70,5 @@ const Expand = ({row}: TCellProps) => {
     isExpanded ? 'arrow-down' : 'arrow-right',
     depth < 3 ? 'before:bg-white' : 'before:bg-black',
   ].join(' ');
-  return <span className={className} {...row.getToggleRowExpandedProps()} />;
+  return <button className={className} {...row.getToggleRowExpandedProps()} />;
 };
