62ba3eceac99f27f6f09abf054477f1b8750cea7
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-06-13 16:05:13 +0200

    Migration file to remove cae_6.5.1.1.1

---
+++ data_layer/migrations/16-remove-cae_6.5.1.1.1.sql
@@ -0,0 +1,10 @@
+-- execute 38-migration_contenu.sql
+--select private.move_action_data('cae_6.5.1.1.2', 'cae_6.5.1.1.1'); 
+select private.move_action_data('cae_6.5.1.1.3', 'cae_6.5.1.1.2'); 
+select private.move_action_data('cae_6.5.1.1.4', 'cae_6.5.1.1.3'); 
+select private.move_action_data('cae_6.5.1.1.5', 'cae_6.5.1.1.4'); 
+
+-- Remove action cae_6.5.1.1.5
+delete from action_definition where action_id = 'cae_6.5.1.1.5'; 
+delete from action_computed_points where action_id = 'cae_6.5.1.1.5'; 
+delete from action_relation where id = 'cae_6.5.1.1.5'; 
