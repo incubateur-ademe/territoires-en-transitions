152252102a7e6933625726c7acce7d68ea4c94bf
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-06-13 23:04:00 +0200

    Fix migration file to remove action cae_6.5.1.1.1

--- data_layer/migrations/16-remove-cae_6.5.1.1.1.sql
+++ data_layer/migrations/16-remove-cae_6.5.1.1.1.sql
@@ -1,5 +1,6 @@
 -- execute 38-migration_contenu.sql
---select private.move_action_data('cae_6.5.1.1.2', 'cae_6.5.1.1.1'); 
+select private.remove_action_data('cae_6.5.1.1.1');
+select private.move_action_data('cae_6.5.1.1.2', 'cae_6.5.1.1.1'); 
 select private.move_action_data('cae_6.5.1.1.3', 'cae_6.5.1.1.2'); 
 select private.move_action_data('cae_6.5.1.1.4', 'cae_6.5.1.1.3'); 
 select private.move_action_data('cae_6.5.1.1.5', 'cae_6.5.1.1.4'); 
