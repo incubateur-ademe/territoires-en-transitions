57b39bd172407aef6da6b2530a32bc1fde08417a
Marc Rutkowski <marc@attractive-media.fr>
2022-06-27 11:30:46 +0200

    ajoute les hooks pour le header et commence à les raccorder (WIP) [skip ci]

--- app.territoiresentransitions.react/src/app/Layout/Header/Header.tsx
+++ app.territoiresentransitions.react/src/app/Layout/Header/Header.tsx
@@ -1,259 +1,61 @@
+import {CollectiviteNavigation, Navigation} from 'app/Navigation';
 import {
   authBloc,
   AuthBloc,
   currentCollectiviteBloc,
   CurrentCollectiviteBloc,
-  CurrentCollectiviteObserved,
 } from 'core-logic/observables';
 import {observer} from 'mobx-react-lite';
 import {CollectiviteRedirector} from 'app/Redirector';
 import {RejoindreCetteCollectiviteDialog} from 'app/pages/MesCollectivites/RejoindreCetteCollectiviteDialog';
 import {getReferentContacts} from 'core-logic/api/procedures/collectiviteProcedures';
 import LogoRepubliqueFrancaise from 'ui/logo/LogoRepubliqueFrancaise';
-import {
-  allCollectivitesPath,
-  makeCollectiviteIndicateursUrl,
-  makeCollectiviteLabellisationUrl,
-  makeCollectivitePersoRefUrl,
-  makeCollectivitePlanActionUrl,
-  makeCollectiviteReferentielUrl,
-  makeCollectiviteTableauBordUrl,
-  makeCollectiviteUsersUrl,
-  planActionDefaultId,
-} from 'app/paths';
-import HeaderNavigation from './HeaderNavigation';
-import CollectiviteNavigation from './CollectiviteNavigation';
-import MobileNavigation from './MobileNavigation';
 
-/** FAKE DATA -> TODO: Replace with hook */
-const fakeCollectivite: CurrentCollectiviteObserved | null = {
-  nom: 'Collectivité 1',
-  collectivite_id: 1,
-  role_name: null,
-};
-
-// const fakeCollectivite = null;
-
-const fakeOwnedCollectivites: CurrentCollectiviteObserved[] = [
-  {
-    nom: 'Collectivité 1',
-    collectivite_id: 1,
-    role_name: null,
-  },
-  {
-    nom: 'Collectivité 2',
-    collectivite_id: 2,
-    role_name: null,
-  },
-  {
-    nom: 'Collectivité 3',
-    collectivite_id: 3,
-    role_name: null,
-  },
-];
-
-const isConnected = false;
-
-const fakeUser = {
-  name: 'Émeline',
-};
-
-export interface CollectiviteNavSingle {
-  displayOnlyToMember?: boolean;
-  label: string;
-  path: string;
-}
-
-export interface CollectiviteNavDropdown {
-  isSelectCollectivite?: boolean;
-  displayOnlyToMember?: boolean;
-  menuLabel: string;
-  listPathsAndLabels: CollectiviteNavSingle[];
-}
-
-export const isSingleNavItemDropdown = (
-  item: CollectiviteNavSingle | CollectiviteNavDropdown
-): item is CollectiviteNavDropdown =>
-  (item as CollectiviteNavDropdown).listPathsAndLabels !== undefined;
-
-const collectiviteNav: (CollectiviteNavSingle | CollectiviteNavDropdown)[] = [
-  {
-    label: 'Tableau de bord',
-    path: makeCollectiviteTableauBordUrl({collectiviteId: 1}),
-  },
-  {
-    menuLabel: 'Climat Air Énergie',
-    listPathsAndLabels: [
-      {
-        label: 'Référentiel',
-        path: makeCollectiviteReferentielUrl({
-          collectiviteId: 1,
-          referentielId: 'cae',
-        }),
-      },
-      {
-        label: 'Indicateurs',
-        path: makeCollectiviteIndicateursUrl({
-          collectiviteId: 1,
-          indicateurView: 'cae',
-        }),
-      },
-      {
-        label: 'Labellisation',
-        path: makeCollectiviteLabellisationUrl({
-          collectiviteId: 1,
-          referentielId: 'cae',
-        }),
-      },
-    ],
-  },
-  {
-    menuLabel: 'Économie circulaire',
-    listPathsAndLabels: [
-      {
-        label: 'Référentiel',
-        path: makeCollectiviteReferentielUrl({
-          collectiviteId: 1,
-          referentielId: 'eci',
-        }),
-      },
-      {
-        label: 'Indicateurs',
-        path: makeCollectiviteIndicateursUrl({
-          collectiviteId: 1,
-          indicateurView: 'eci',
-        }),
-      },
-      {
-        label: 'Labellisation',
-        path: makeCollectiviteLabellisationUrl({
-          collectiviteId: 1,
-          referentielId: 'eci',
-        }),
-      },
-    ],
-  },
-  {
-    menuLabel: 'Pilotage',
-    listPathsAndLabels: [
-      {
-        label: "Plans d'action",
-        path: makeCollectivitePlanActionUrl({
-          collectiviteId: 1,
-          planActionUid: planActionDefaultId,
-        }),
-      },
-      {
-        label:
-          'Indicateurs contrat de relance et de transition écologique (CRTE)',
-        path: makeCollectiviteIndicateursUrl({
-          collectiviteId: 1,
-          indicateurView: 'crte',
-        }),
-      },
-      {
-        label: 'Indicateurs personnalisés',
-        path: makeCollectiviteIndicateursUrl({
-          collectiviteId: 1,
-          indicateurView: 'perso',
-        }),
-      },
-    ],
-  },
-  {
-    displayOnlyToMember: true,
-    menuLabel: 'Paramètres',
-    listPathsAndLabels: [
-      {
-        label: 'Personnalisation des référentiels',
-        path: makeCollectivitePersoRefUrl({
-          collectiviteId: 1,
-        }),
-      },
-      {
-        label: 'Gestion des membres',
-        path: makeCollectiviteUsersUrl({
-          collectiviteId: 1,
-        }),
-      },
-    ],
-  },
-  {
-    label: 'Toutes les collectivités',
-    path: allCollectivitesPath,
-  },
-];
-/** END FAKE DATA */
-
-export const HeaderObserver = observer(
+const HeaderObserver = observer(
   ({
     authBloc,
     currentCollectiviteBloc,
-    isConnected,
-    collectivite,
-    ownedCollectivite,
   }: {
     authBloc: AuthBloc;
     currentCollectiviteBloc: CurrentCollectiviteBloc;
-    isConnected: boolean;
-    collectivite: CurrentCollectiviteObserved | null;
-    ownedCollectivite: CurrentCollectiviteObserved[] | null;
-  }) => {
-    return (
-      <>
-        <CollectiviteRedirector />
-        <header role="banner" className="header fr-header ">
-          <div className="fr-header__body">
-            <div className="fr-container">
-              <div className="fr-header__body-row header__row">
-                <div className="fr-header__brand fr-enlarge-link pointer-events-none lg:pointer-events-auto">
-                  <div className="fr-header__brand-top !w-auto">
-                    <div className="fr-header__logo">
-                      <LogoRepubliqueFrancaise />
-                    </div>
-                  </div>
-                  <div className="fr-header__ademe flex-shrink-0">
-                    <img
-                      src="https://territoiresentransitions.fr/img/ademe.jpg"
-                      alt="logo ADEME"
-                      loading="lazy"
-                      className="h-20"
-                    />
-                  </div>
-                  <div className="fr-header__service">
-                    <a href="/" title="Accueil">
-                      <p className="fr-header__service-title pointer-events-auto">
-                        Territoires en Transitions
-                      </p>
-                    </a>
-                    <p className="text-sm">
-                      Accompagner la transition écologique des collectivités
-                    </p>
+  }) => (
+    <>
+      <CollectiviteRedirector />
+      <header role="banner" className="header fr-header ">
+        <div className="fr-header__body">
+          <div className="fr-container">
+            <div className="fr-header__body-row header__row">
+              <div className="fr-header__brand fr-enlarge-link hidden md:block">
+                <div className="fr-header__brand-top">
+                  <div className="fr-header__logo">
+                    <LogoRepubliqueFrancaise />
                   </div>
                 </div>
-                <HeaderNavigation isConnected={isConnected} user={fakeUser} />
-                <MobileNavigation
-                  isConnected={isConnected}
-                  user={fakeUser}
-                  collectiviteNav={collectiviteNav}
-                  currentCollectivite={collectivite}
-                  ownedCollectivites={ownedCollectivite}
-                />
+                <div className="fr-header__ademe">
+                  <img
+                    src="https://territoiresentransitions.fr/img/ademe.jpg"
+                    alt="logo ADEME"
+                    loading="lazy"
+                    className="h-20"
+                  />
+                </div>
+                <div className="fr-header__service">
+                  <a href="/" title="Accueil">
+                    <p className="fr-header__service-title">
+                      Territoires en Transitions
+                    </p>
+                  </a>
+                </div>
               </div>
+              <Navigation />
             </div>
           </div>
-          {collectivite !== null && ownedCollectivite !== null && (
-            <CollectiviteNavigation
-              collectiviteNav={collectiviteNav}
-              currentCollectivite={collectivite}
-              ownedCollectivites={ownedCollectivite}
-            />
-          )}
-        </header>
-        <CollectiviteReadOnlyBanner bloc={currentCollectiviteBloc} />
-      </>
-    );
-  }
+        </div>
+        <CollectiviteHeader bloc={currentCollectiviteBloc} />
+      </header>
+      <CollectiviteReadOnlyBanner bloc={currentCollectiviteBloc} />
+    </>
+  )
 );
 
 const CollectiviteReadOnlyBanner = observer(
@@ -272,16 +74,32 @@ const CollectiviteReadOnlyBanner = observer(
   }
 );
 
-const Header = () => {
-  return (
-    <HeaderObserver
-      authBloc={authBloc}
-      currentCollectiviteBloc={currentCollectiviteBloc}
-      isConnected={isConnected}
-      collectivite={fakeCollectivite}
-      ownedCollectivite={fakeOwnedCollectivites}
-    />
-  );
-};
+const CollectiviteHeader = observer(
+  ({bloc}: {bloc: CurrentCollectiviteBloc}) => {
+    return (
+      <>
+        <div className="fr-container">
+          {bloc.currentCollectivite !== null && (
+            <div>
+              <div className="flex flex-row justify-between">
+                <CollectiviteNavigation />
+                <div className="flex items-center font-bold">
+                  {bloc.currentCollectivite.nom}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
+);
+
+const Header = () => (
+  <HeaderObserver
+    currentCollectiviteBloc={currentCollectiviteBloc}
+    authBloc={authBloc}
+  />
+);
 
 export default Header;
