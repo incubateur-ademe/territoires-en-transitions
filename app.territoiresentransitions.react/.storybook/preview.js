9dda2f6987c1a59207b5c0d8ec957374168a1ee3
Marc Rutkowski <marc@attractive-media.fr>
2022-06-30 15:02:41 +0200

    ajoute un mock du AuthContext dans le storybook
    (nécessaire pour pouvoir monter dans le storybook des composants utilisants useAuth)

--- app.territoiresentransitions.react/.storybook/preview.js
+++ app.territoiresentransitions.react/.storybook/preview.js
@@ -4,6 +4,7 @@
 import {addDecorator} from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {AuthContext} from '../src/core-logic/api/auth/AuthProvider';
 
 // charge les styles de l'appli
 import '../src/css/tailwind.css';
@@ -12,12 +13,21 @@ import '../src/css/app.css';
 // pour faire fonctionner storybook avec react-router
 addDecorator(StoryRouter());
 
-// pour faire fonctionner storybook avec react-query
+// pour faire fonctionner storybook avec react-query et notre contexte d'auth.
 const queryClient = new QueryClient();
+const mockAuthState = {
+  connect: () => Promise.resolve(true),
+  disconnect: () => Promise.resolve(true),
+  isConnected: false,
+  user: null,
+  authError: null,
+};
 export const decorators = [
   Story => (
     <QueryClientProvider client={queryClient}>
-      <Story />
+      <AuthContext.Provider value={mockAuthState}>
+        <Story />
+      </AuthContext.Provider>
     </QueryClientProvider>
   ),
 ];
