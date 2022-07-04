62259e878ec1abc82830735433b21fbbdb1efd85
Florian d'Erfurth <florian@derfurth.com>
2022-07-04 17:03:23 +0200

    Migre les vues materialisées (legacy).

---
+++ data_layer/migrations/17-materialized_stats.sql
@@ -0,0 +1,7 @@
+drop view stats_rattachements;
+drop view stats_unique_active_collectivite;
+drop view stats_unique_active_users;
+
+-- execute collectivite.sql l:13
+-- execute collectivite.sql l:26
+-- execute utilisateur.sql l:6
