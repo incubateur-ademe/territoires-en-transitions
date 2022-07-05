19be3dc819c3950b02d3249b2caf8a912f237bb6
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-05 14:58:52 +0200

    Migration to add maintenance using sqlitch

--- data_layer/sqitch/deploy/utils/maintenance.sql
+++ data_layer/sqitch/deploy/utils/maintenance.sql
@@ -1,16 +1,24 @@
+-- Deploy tet:utils/maintenance to pg
+
+BEGIN;
+
 create table maintenance
 (
     id              serial primary key,
-    begins_at timestamp with time zone        not null,
+    begins_at timestamp with time zone      not null,
     ends_at timestamp with time zone        not null
 );
 comment on table maintenance is
     'Utilisé par le client pour afficher un bandeau de maintenance lorsque la base de données est en maintenance';
 
+alter table maintenance
+    enable row level security;
 
-create or replace view ongoing_maintenance as (
+create view ongoing_maintenance as 
     select now(), begins_at, ends_at from maintenance 
-     WHERE now() < ends_at and  now()::date = begins_at::date
-     order by ends_at desc limit 1
-); 
+    where now() < ends_at and  now()::date = begins_at::date
+    order by ends_at desc limit 1; 
 
+
+
+COMMIT;
