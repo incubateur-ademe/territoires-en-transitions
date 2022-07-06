a36ee36ce01baa04d367fa50c62320f42a2b1a74
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-06 17:22:13 +0200

    Add button to download Tabeau Bord charts as PNG and fix tooltip on polar area

---
+++ app.territoiresentransitions.react/src/ui/charts/CanvasDownloadButton.tsx
@@ -0,0 +1,29 @@
+export type TCanvasDownloadButtonProps = {
+  fileName: string;
+  canvasId: string;
+  buttonText: string;
+};
+
+export const CanvasDownloadButton = (props: TCanvasDownloadButtonProps) => {
+  const linkId = `download_${props.canvasId}`;
+  return (
+    <a
+      className="fr-btn fr-btn--secondary"
+      id={linkId}
+      download={`${props.fileName}`}
+      href=""
+      onClick={() => {
+        const canvas = document.getElementById(
+          props.canvasId
+        ) as HTMLCanvasElement;
+        if (canvas) {
+          (document.getElementById(linkId) as HTMLLinkElement).href =
+            canvas.toDataURL();
+        }
+      }}
+    >
+      <div className="fr-fi-download-line mr-2 text-xs"></div>
+      {props.buttonText}
+    </a>
+  );
+};
