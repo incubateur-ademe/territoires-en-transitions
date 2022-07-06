6e6a1fe778c288c15b7e0e5a7933cc3c8c69f522
Florian d'Erfurth <florian@derfurth.com>
2022-07-06 18:13:52 +0200

    Droits v2, niveaux d'accès

---
+++ data_layer/sqitch/verify/utilisateur/droits_v2.sql
@@ -0,0 +1,24 @@
+-- Verify tet:utilisateur/droits_v2 on pg
+
+BEGIN;
+
+comment on type niveau_acces is '';
+
+select id,
+       user_id,
+       collectivite_id,
+       role_name,
+       active,
+       created_at,
+       modified_at,
+       niveau_acces
+from postgres.public.private_utilisateur_droit
+where false;
+
+select has_function_privilege('have_one_of_niveaux_acces(niveau_acces[], integer)', 'execute');
+select has_function_privilege('have_admin_acces(integer)', 'execute');
+select has_function_privilege('have_edition_acces(integer)', 'execute');
+select has_function_privilege('have_lecture_acces(integer)', 'execute');
+select has_function_privilege('is_bucket_writer(text)', 'execute');
+
+ROLLBACK;
