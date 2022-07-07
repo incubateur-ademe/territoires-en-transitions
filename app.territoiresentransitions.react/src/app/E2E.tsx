b6a9097bae2abcecb3648fe6e705aed746395be8
Marc Rutkowski <marc@attractive-media.fr>
2022-07-07 10:49:21 +0200

    fixe l'utilisation de l'auth. dans les tests E2E

--- app.territoiresentransitions.react/src/app/E2E.tsx
+++ app.territoiresentransitions.react/src/app/E2E.tsx
@@ -1,15 +1,17 @@
 import {useHistory} from 'react-router-dom';
-import {authBloc} from 'core-logic/observables/authBloc';
 import {supabaseClient} from 'core-logic/api/supabase';
+import {useAuth} from 'core-logic/api/auth/AuthProvider';
 
 /**
  * Expose un objet window.e2e lorsque l'appli fonctionne dans Cypress
  */
 export const E2E = () => {
+  const auth = useAuth();
+
   if ('Cypress' in window) {
     // expose l'objet history ainsi que le client supabase à l'env. E2E
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).e2e = {history: useHistory(), authBloc, supabaseClient};
+    (window as any).e2e = {history: useHistory(), auth, supabaseClient};
   }
   return null;
 };
