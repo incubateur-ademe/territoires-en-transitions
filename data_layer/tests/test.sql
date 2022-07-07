3605cc1e8cad3868c1cce16002a58b132c63e2c7
Florian d'Erfurth <florian@derfurth.com>
2022-07-07 18:56:00 +0200

    Fonctions pour tester les droits.

---
+++ data_layer/tests/test.sql
@@ -0,0 +1,39 @@
+begin;
+select plan(7);
+
+select *
+into random_admin
+from test_add_random_user(10, 'admin');
+
+select set_has(
+               'select id, email from auth.users;',
+               'select user_id as id, email from random_admin;',
+               'The random user should be in the auth.users table.'
+           );
+select set_has(
+               'select user_id, nom, prenom, email from dcp;',
+               'select user_id, nom, prenom, email from random_admin;',
+               'The random user should be in the dcp table.'
+           );
+
+select set_has(
+               'select user_id from private_utilisateur_droit where collectivite_id = 10 and niveau_acces = ''admin'';',
+               'select user_id from random_admin;',
+               'The random user should be in the droits table.'
+           );
+
+select test.identify_as('yolo@dodo.com');
+select ok(
+               (select auth.uid() = '17440546-f389-4d4f-bfdb-b0c94a1bd0f9'),
+               'When identifying as yolo, auth.uid() should return his id.'
+           );
+select ok((is_authenticated()), 'When identifying as yolo is_authenticated should be true');
+
+select test.identify_as('66666666-6666-6666-6666-666666666666'::uuid);
+select ok(
+               (select auth.uid() = '66666666-6666-6666-6666-666666666666'),
+               'When identifying with an uuid, the auth.uid should return it'
+           );
+select ok((is_authenticated()), 'When identifying with an uuid is_authenticated should be true');
+
+rollback;
