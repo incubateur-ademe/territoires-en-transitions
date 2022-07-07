ac94420010c5c73097d06858a423a9ede73c3af9
Marc Rutkowski <marc@attractive-media.fr>
2022-07-07 10:36:07 +0200

    supprime une page inutilisée

--- app.territoiresentransitions.react/src/app/pages/Auth/AuthRoutes.tsx
+++ app.territoiresentransitions.react/src/app/pages/Auth/AuthRoutes.tsx
@@ -1,16 +1,12 @@
 import {Route} from 'react-router-dom';
-import {IdentityPage} from 'app/pages/Auth/IdentityPage';
 import {RegisterPage} from 'app/pages/Auth/RegisterPage';
 import {SignInPage} from 'app/pages/Auth/SignInPage';
-import {identityPath, signUpPath, signInPath, resetPwdPath} from 'app/paths';
+import {signUpPath, signInPath, resetPwdPath} from 'app/paths';
 import {ResetPasswordPage} from './ResetPasswordPage';
 
 export const AuthRoutes = () => {
   return (
     <>
-      <Route path={identityPath}>
-        <IdentityPage />
-      </Route>
       <Route path={signUpPath}>
         <RegisterPage />
       </Route>
