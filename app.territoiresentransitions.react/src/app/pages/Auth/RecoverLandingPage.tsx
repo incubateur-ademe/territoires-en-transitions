c2a5f29d25ef40d315f4f518f51559c8c12fb601
Marc Rutkowski <marc@attractive-media.fr>
2022-07-12 15:08:54 +0200

    extrait la vérification du jeton de récupération de mdp dans un hook et centralise la redirection dans Redirector

--- app.territoiresentransitions.react/src/app/pages/Auth/RecoverLandingPage.tsx
+++ app.territoiresentransitions.react/src/app/pages/Auth/RecoverLandingPage.tsx
@@ -1,7 +1,6 @@
 import {lazy, Suspense} from 'react';
 import {renderLoader} from 'utils/renderLoader';
 import {Spacer} from 'ui/shared/Spacer';
-import {useParams} from 'react-router-dom';
 
 const RecoverLanding = lazy(() => import('./RecoverLanding'));
 
@@ -11,10 +10,9 @@ const RecoverLanding = lazy(() => import('./RecoverLanding'));
  * On arrive ici depuis un lien.
  */
 export const RecoverLandingPage = () => {
-  const {token} = useParams<{token: string}>();
   return (
     <Suspense fallback={renderLoader()}>
-      <RecoverLanding token={token} />
+      <RecoverLanding />
       <Spacer />
     </Suspense>
   );
