218fa873e67e952d3cb726b8adefc9f26d5dc364
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-12 11:00:51 +0200

    Add migrations file for PR #2248

---
+++ data_layer/migrations/18-remove-cae_taches_without_siblings.sql
@@ -0,0 +1,12 @@
+select private.move_action_data('cae_2.1.3.2.1', 'cae_2.1.3.2'); 
+select private.remove_action_data('cae_2.1.3.2.1');
+
+select private.move_action_data('cae_4.1.1.5.1', 'cae_4.1.1.5'); 
+select private.remove_action_data('cae_4.1.1.5.1');
+
+select private.move_action_data('cae_4.3.3.1.1', 'cae_4.3.3.1'); 
+select private.remove_action_data('cae_4.3.3.1.1');
+
+
+select private.move_action_data('cae_4.3.3.5.1', 'cae_4.3.3.5'); 
+select private.remove_action_data('cae_4.3.3.5.1');
