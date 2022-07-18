362824b151854bc38a1ce48ee3fec29fc1a41f66
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-18 15:32:25 +0200

    [DL] Create function to update acces and update function to remove user

--- data_layer/sqitch/deploy/utilisateur/membre.sql
+++ data_layer/sqitch/deploy/utilisateur/membre.sql
@@ -124,6 +124,63 @@ comment on function update_collectivite_membre_champ_intervention is
     'Met à jour le champs champ_intervention d''un membre s''il est autorisé à le faire';
 
 
+create function update_collectivite_membre_niveau_acces(collectivite_id integer, membre_id uuid, niveau_acces referentiel[])
+    returns json
+as
+$$
+declare
+    invitation_id uuid;
+begin
+    if have_admin_acces(update_collectivite_membre_niveau_acces.collectivite_id) 
+    then
+       if membre_id in (select user_id from private_collectivite_membre pcm where pcm.collectivite_id = update_collectivite_membre_niveau_acces.collectivite_id)
+          then 
+            update  private_utilisateur_droit
+            set 
+            niveau_acces =  update_collectivite_membre_niveau_acces.niveau_acces,
+            modified_at = now()
+            where user_id = membre_id and private_utilisateur_droit.collectivite_id = update_collectivite_membre_niveau_acces.collectivite_id; 
+          else 
+            return json_build_object('error', 'Cet utilisateur n''est pas membre de la collectivité.');
+          end if;
+        return json_build_object('message', 'La niveau_acces du membre a été mise à jour.');
+    else
+        perform set_config('response.status', '401', true);
+        return json_build_object('error', 'Vous n''avez pas les droits admin, vous ne pouvez pas éditer le niveau d''acces de ce membre.');
+    end if;
+end
+$$ language plpgsql security definer;
+comment on function update_collectivite_membre_niveau_acces is
+    'Met à jour le niveau d''acces d''un membre si l''utilisateur connecté est admin';
+
+drop function remove_user_from_collectivite; 
+create function remove_membre_from_collectivite(collectivite_id integer, membre_id uuid)
+    returns json
+as
+$$
+begin
+    if have_admin_acces(remove_membre_from_collectivite.collectivite_id) 
+       or auth.uid() = membre_id
+    then
+       if membre_id in (select user_id from private_collectivite_membre pcm where pcm.collectivite_id = remove_membre_from_collectivite.collectivite_id)
+          then 
+            update  private_utilisateur_droit
+            set active = false,
+            modified_at = now()
+            where user_id = membre_id and private_utilisateur_droit.collectivite_id = remove_membre_from_collectivite.collectivite_id; 
+          else 
+            return json_build_object('error', 'Cet utilisateur n''est pas membre de la collectivité.');
+          end if;
+        return json_build_object('message', 'Les accès de l''utilisateur ont été supprimés.');
+    else
+        perform set_config('response.status', '401', true);
+        return json_build_object('error', 'Vous n''avez pas les droits admin, vous ne pouvez pas retirer les droits d''accès d''uu utilisateur');
+    end if;
+end
+$$ language plpgsql security definer;
+comment on function remove_membre_from_collectivite is
+    'Supprime les accès d''un membre si l''utilisateur connecté est admin';
+
 
 create function collectivite_membres(id integer)
     returns table
@@ -141,18 +198,20 @@ create function collectivite_membres(id integer)
 as
 $$
 with droits_dcp as 
-(select p.user_id, p.prenom, p.nom, p.email, p.telephone, d.niveau_acces
+(select d.collectivite_id, p.user_id, p.prenom, p.nom, p.email, p.telephone, d.niveau_acces
 from private_utilisateur_droit d
     left join dcp p on p.user_id = d.user_id
     
 where d.collectivite_id = collectivite_membres.id
   and d.active)
 select m.user_id, prenom, nom, email, telephone, niveau_acces , fonction, details_fonction, champ_intervention
-from droits_dcp 
+from droits_dcp d
 left join private_collectivite_membre m
-on m.user_id = droits_dcp.user_id and  m.collectivite_id = collectivite_membres.id
+on m.user_id = d.user_id and m.collectivite_id = d.collectivite_id
+where m.user_id is not null
 $$ language sql security definer;
 comment on function collectivite_membres is
     'Retourne les informations sur tous les membres d''une collectivité étant donné l''id.''';
+
 
 COMMIT;
