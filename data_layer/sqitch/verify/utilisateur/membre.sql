4253e39b66e47e19012c34c5ba6802c4d36af093
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-19 10:06:29 +0200

    [DL] Add tests on membres and verify

--- data_layer/sqitch/verify/utilisateur/membre.sql
+++ data_layer/sqitch/verify/utilisateur/membre.sql
@@ -2,6 +2,17 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+
+select user_id, collectivite_id, fonction, details_fonction, champ_intervention, created_at, modified_at
+from private_collectivite_membre
+where false;
+
+
+select has_function_privilege('update_collectivite_membre_details_fonction(integer, uuid, text)', 'execute');
+select has_function_privilege('update_collectivite_membre_fonction(integer, uuid, membre_fonction)', 'execute');
+select has_function_privilege('update_collectivite_membre_champ_intervention(integer, uuid, referentiel[])', 'execute');
+select has_function_privilege('update_collectivite_membre_niveau_acces(integer, uuid, niveau_acces)', 'execute');
+select has_function_privilege('remove_membre_from_collectivite(integer, uuid)', 'execute');
+select has_function_privilege('collectivite_membres(integer)', 'execute');
 
 ROLLBACK;
