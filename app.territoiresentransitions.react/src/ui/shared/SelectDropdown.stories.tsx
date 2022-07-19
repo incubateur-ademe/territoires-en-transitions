5fb1ec8aaac1fe5b2e5a25ec7dfb4662fcf806ca
Arthur Molinos <arthur.molinos@gmail.com>
2022-07-20 01:55:13 +0200

    Ajout de stories et storyshots pour SelectDropdown

---
+++ app.territoiresentransitions.react/src/ui/shared/SelectDropdown.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useState} from 'react';
+import {SelectDropdown} from './SelectDropdown';
+
+export default {
+  component: SelectDropdown,
+};
+
+const fakeLabels: Record<any, string> = {
+  option1: 'Option 1',
+  option2: 'Option 2',
+  option3: 'Option 3',
+};
+
+export const AucuneOptionSelectionee = () => {
+  const [value, setValue] = useState(undefined);
+  return (
+    <SelectDropdown
+      labels={fakeLabels}
+      value={value}
+      onSelect={v => setValue(v)}
+    />
+  );
+};
+
+export const OptionSelectionee = () => {
+  const [value, setValue] = useState('option2');
+  return (
+    <SelectDropdown
+      labels={fakeLabels}
+      value={value}
+      onSelect={v => setValue(v)}
+    />
+  );
+};
