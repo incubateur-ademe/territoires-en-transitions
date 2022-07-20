b255ff049f35771d36fde41b75ddaa4bfb03ad99
Marc Rutkowski <marc@attractive-media.fr>
2022-07-20 12:09:01 +0200

    sépare le rendu du champ de recherche du mécanisme de debounce
    (pour permettre la réutilisation)

--- app.territoiresentransitions.react/src/ui/UiSearchBar.tsx
+++ app.territoiresentransitions.react/src/ui/UiSearchBar.tsx
@@ -1,3 +1,4 @@
+import {ChangeEventHandler} from 'react';
 import {useDebouncedInput} from './shared/useDebouncedInput';
 
 export const UiSearchBar = ({
@@ -12,12 +13,25 @@ export const UiSearchBar = ({
   const [query, onChange] = useDebouncedInput('', search, debouncePeriod);
 
   return (
+    <InputSearch value={query} placeholder={placeholder} onChange={onChange} />
+  );
+};
+
+export const InputSearch = ({
+  value,
+  placeholder,
+  onChange,
+}: {
+  value?: string;
+  placeholder: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}) => {
+  return (
     <div className="border border-bf500 rounded-tr rounded-tl">
       <div className="flex items-center w-full border-b-2 border-bf500">
         <input
           className="bg-beige p-2 w-full placeholder-gray-500"
-          key="random1"
-          value={query || ''}
+          value={value || ''}
           placeholder={placeholder}
           onChange={onChange}
         />
