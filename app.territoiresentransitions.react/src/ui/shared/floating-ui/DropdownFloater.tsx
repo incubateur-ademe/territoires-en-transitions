df1b1b80a3d4e9a20f07114c45006945d9944c2a
Florian d'Erfurth <florian@derfurth.com>
2022-07-20 15:56:53 +0200

    Composant générique Dropdown Floater

---
+++ app.territoiresentransitions.react/src/ui/shared/floating-ui/DropdownFloater.tsx
@@ -0,0 +1,74 @@
+import {cloneElement, useState} from 'react';
+import {
+  useFloating,
+  offset,
+  useInteractions,
+  useDismiss,
+  shift,
+  useClick,
+  FloatingPortal,
+  useFocus,
+  FloatingFocusManager,
+  Placement,
+} from '@floating-ui/react-dom-interactions';
+
+type DropdownFloaterProps = {
+  children: JSX.Element;
+  render: (data: {close: () => void}) => React.ReactNode;
+  placement?: Placement;
+};
+
+const DropdownFloater = ({
+  render,
+  children,
+  placement,
+}: DropdownFloaterProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const {x, y, strategy, reference, floating, context} = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: placement ?? 'bottom',
+    middleware: [offset(4), shift()],
+  });
+
+  const {getReferenceProps, getFloatingProps} = useInteractions([
+    useClick(context),
+    useDismiss(context),
+    useFocus(context),
+  ]);
+
+  return (
+    <>
+      {cloneElement(
+        children,
+        getReferenceProps({ref: reference, isOpen, ...children.props})
+      )}
+      {isOpen && (
+        <FloatingPortal>
+          <FloatingFocusManager context={context}>
+            <div
+              className="w-max bg-white shadow-md z-50"
+              {...getFloatingProps({
+                ref: floating,
+                style: {
+                  position: strategy,
+                  top: y ?? '',
+                  left: x ?? '',
+                },
+              })}
+            >
+              {render({
+                close: () => {
+                  setIsOpen(false);
+                },
+              })}
+            </div>
+          </FloatingFocusManager>
+        </FloatingPortal>
+      )}
+    </>
+  );
+};
+
+export default DropdownFloater;
