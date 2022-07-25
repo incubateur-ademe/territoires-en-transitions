125db04e7ecd7d5444b1248c337467b3b6cde03b
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-07-25 15:59:25 +0200

    Review fixes with Florian

--- data_layer/tests/referent_contacts.sql
+++ data_layer/tests/referent_contacts.sql
@@ -9,14 +9,14 @@ $$ language sql stable;
 
 -- check that yolododo is included whithin the referents
 select results_eq(
-               'select email from admin_contacts(1);',
+               'select email from referent_contacts(1);',
                'select email from dcp where user_id = ''17440546-f389-4d4f-bfdb-b0c94a1bd0f9'';',
                'email of referent 1 should be the same as yolododo'
            );
 
 -- when the collectivite has no referent yet 
 select is_empty(
-               'select * from admin_contacts(10)',
-               'no admin contacts'
+               'select * from referent_contacts(10)',
+               'no referent contacts'
            );
 rollback;
