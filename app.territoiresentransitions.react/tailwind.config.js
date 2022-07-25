387b1ebc591faf40108768fcb1b0271fa80eb97e
Arthur Molinos <arthur.molinos@gmail.com>
2022-07-25 16:53:23 +0200

    Ajout d'une couleur dans la config tailwind

--- app.territoiresentransitions.react/tailwind.config.js
+++ app.territoiresentransitions.react/tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         // Design system
         bf500: '#000091',
         bf525: '#6A6AF4',
+        // En rapport avec la dernière version Figma du DSFR
+        // nomenclature Figma: light/text/default-info
+        // nomenclature tailwind proposée: t = text -> tDefaultInfo
+        tDefaultInfo: '#0063CB',
         bf925: '#E3E3FD',
         bf975: '#f5f5fe',
         grey425: '#666',
