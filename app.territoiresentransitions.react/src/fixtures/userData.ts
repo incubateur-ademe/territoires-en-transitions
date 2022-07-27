19437ca2c62711aae3dd353d28abf69ec6066cb2
Arthur Molinos <arthur.molinos@gmail.com>
2022-07-27 16:39:11 +0200

    Création du formulaire et du message d'invitation

---
+++ app.territoiresentransitions.react/src/fixtures/userData.ts
@@ -0,0 +1,19 @@
+import {UserData} from 'core-logic/api/auth/AuthProvider';
+
+export const fakeUserData: UserData = {
+  app_metadata: {},
+  aud: '',
+  confirmed_at: '2022-02-10T14:28:45.304559Z',
+  created_at: '2022-02-10T14:28:45.262861Z',
+  email: 'yolo@dodo.com',
+  email_confirmed_at: '2022-02-10T14:28:45.304559Z',
+  id: '1',
+  identities: [],
+  last_sign_in_at: '2022-07-26T12:38:32.0722822Z',
+  prenom: 'Yolo',
+  nom: 'Dodo',
+  phone: '',
+  role: 'authenticated',
+  updated_at: '2022-07-26T12:38:33.382147Z',
+  user_metadata: {},
+};
