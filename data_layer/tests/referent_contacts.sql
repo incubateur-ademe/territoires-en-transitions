e192bdf73bb0922f2d01bce31c92606cc217f8a1
Florian d'Erfurth <florian@derfurth.com>
2022-08-03 13:26:38 +0200

    Répare le test `referent_contacts`.

--- data_layer/tests/referent_contacts.sql
+++ data_layer/tests/referent_contacts.sql
@@ -2,21 +2,18 @@ begin;
 select plan(2);
 
 -- make uid work as if yolododo user is connected
-create or replace function auth.uid() returns uuid as
-$$
-select '17440546-f389-4d4f-bfdb-b0c94a1bd0f9'::uuid;
-$$ language sql stable;
+select test.identify_as('yolo@dodo.com');
 
 -- check that yolododo is included whithin the referents
-select results_eq(
+select set_has(
                'select email from referent_contacts(1);',
-               'select email from dcp where user_id = ''17440546-f389-4d4f-bfdb-b0c94a1bd0f9'';',
-               'email of referent 1 should be the same as yolododo'
+               'select email from dcp where email = ''yolo@dodo.com'';',
+               'Referents of collectivité #1 should contains yolododo.'
            );
 
 -- when the collectivite has no referent yet
 select is_empty(
                'select * from referent_contacts(10)',
-               'no referent contacts'
+               'Collectivité #10 referent contacts should be empty'
            );
 rollback;
