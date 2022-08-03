c7be718d1f48aa6f9d92ff32f4c0f7d351f9edd2
Florian d'Erfurth <florian@derfurth.com>
2022-08-03 13:26:20 +0200

    Met à jour le test `unprocessed_action_statut_update_event` pour utiliser les niveaux accès.

--- data_layer/tests/unprocessed_action_statut_update_event.sql
+++ data_layer/tests/unprocessed_action_statut_update_event.sql
@@ -1,7 +1,7 @@
 begin;
 select plan(8);
 
--- prepare 
+-- prepare
 truncate client_scores;
 truncate action_statut;
 truncate action_statut_update_event;
@@ -14,7 +14,7 @@ insert into action_computed_points(action_id, value, modified_at) values ('cae',
 
 
 -- collectivite #1 activated at 06:03 a.m.
-insert into private_utilisateur_droit (user_id, collectivite_id, role_name, active, created_at) values ('17440546-f389-4d4f-bfdb-b0c94a1bd0f9', 1, 'referent', true, '2022-01-01 06:03:00.000000 +00:00');
+insert into private_utilisateur_droit (user_id, collectivite_id, niveau_acces, active, created_at) values ('17440546-f389-4d4f-bfdb-b0c94a1bd0f9', 1, 'admin', true, '2022-01-01 06:03:00.000000 +00:00');
 
 
 -- [WHEN NO SCORES YET] Should have one row per referentiel and per collectivite
@@ -49,13 +49,13 @@ select ok((select count(*) = 1
            );
 
 -- Now, if scores are calculated at 08:05 a.m. for CAE, view should become empty
-update client_scores set score_created_at='2022-01-01 08:05:00.000000 +00:00' where collectivite_id = 1 and referentiel='cae'; 
+update client_scores set score_created_at='2022-01-01 08:05:00.000000 +00:00' where collectivite_id = 1 and referentiel='cae';
 select is_empty('select * from unprocessed_action_statut_update_event',
                 'If last scores were calculated later then last statut update, view is empty');
 
 -- [UPDATE REFERENTIEL SHOULD TRIGGER EVALUATION ]
 -- Now, an update occured at 09:00 a.m. in the referentiel computed points of ECI
-truncate action_computed_points; 
+truncate action_computed_points;
 insert into action_computed_points(action_id, value, modified_at) values ('eci', 500.0, '2022-01-01 09:00:00.000000 +00:00');
 
 
@@ -72,7 +72,7 @@ select ok((select count(*) = 1
 
 -- [ACTIVATING A COLLECTIVITE SHOULD TRIGGER EVALUATION ]
 -- Collectivite #2 activated at 10:00 a.m.
-insert into private_utilisateur_droit (user_id, collectivite_id, role_name, active, created_at) values ('17440546-f389-4d4f-bfdb-b0c94a1bd0f9', 2, 'referent', true,'2022-01-01 10:00:00.000000 +00:00');
+insert into private_utilisateur_droit (user_id, collectivite_id, niveau_acces, active, created_at) values ('17440546-f389-4d4f-bfdb-b0c94a1bd0f9', 2, 'admin', true,'2022-01-01 10:00:00.000000 +00:00');
 select ok((select count(*) = 2
            from unprocessed_action_statut_update_event
            where collectivite_id='2' and created_at='2022-01-01 10:00:00.000000 +00:00'),
