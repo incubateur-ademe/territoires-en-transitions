9e6300003c81bcb2fb4ff866ed096526d3de0b97
Florian d'Erfurth <florian@derfurth.com>
2022-08-09 17:26:40 +0200

    Met à jour le verify de la dernière feature.

--- data_layer/postgres/verify/sqitch.sql
+++ data_layer/postgres/verify/sqitch.sql
@@ -4,4 +4,4 @@ select has_function_privilege('teapot()', 'execute');
 select 1 / count(*)
 from sqitch.changes
 -- should be the latest change.
-where change = 'utilisateur/invitation_v2';
+where change = 'utilisateur/membre';
