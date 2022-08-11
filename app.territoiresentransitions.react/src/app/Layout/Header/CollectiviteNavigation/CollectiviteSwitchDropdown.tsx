ca9c14addb3f2312dcff55b6bec7576ea2465005
Arthur Molinos <arthur.molinos@gmail.com>
2022-08-11 11:29:51 +0200

    Laisser encadré quand une seule collectivité dans la navigation

--- app.territoiresentransitions.react/src/app/Layout/Header/CollectiviteNavigation/CollectiviteSwitchDropdown.tsx
+++ app.territoiresentransitions.react/src/app/Layout/Header/CollectiviteNavigation/CollectiviteSwitchDropdown.tsx
@@ -38,12 +38,16 @@ const CollectiviteSwitchDropdown = ({
   return (
     <>
       {collectiviteList.length === 0 ? (
-        <div className="flex items-center max-w-sm ml-auto p-4 text-sm font-bold">
-          <span className="line-clamp-1">{currentCollectivite.nom}</span>
-          <CollectiviteAccesChip
-            acces={currentCollectivite.niveau_acces}
-            className="ml-4"
-          />
+        <div className="flex ml-auto pl-2 w-full max-w-sm">
+          <div className="flex items-center w-full mt-auto mb-2 py-2 px-4 font-bold border border-gray-300">
+            <span className="mr-auto line-clamp-1 border border-transparent">
+              {currentCollectivite.nom}
+            </span>
+            <CollectiviteAccesChip
+              acces={currentCollectivite.niveau_acces}
+              className="ml-4"
+            />
+          </div>
         </div>
       ) : (
         <DropdownFloater
