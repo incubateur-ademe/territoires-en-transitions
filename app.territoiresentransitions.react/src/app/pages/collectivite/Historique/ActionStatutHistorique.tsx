c7dfb20ec9dd2115b3293c0b467511f96007e30e
Florian d'Erfurth <florian@derfurth.com>
2022-08-16 11:10:40 +0200

    Intégration de l'historique des statuts dans la vue référentiel.

--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/ActionStatutHistorique.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/ActionStatutHistorique.tsx
@@ -1,23 +1,6 @@
-import {ActionAvancement} from 'generated/dataLayer/action_statut_read';
+import {IHistoricalActionStatutRead} from 'generated/dataLayer/historical_action_statut_read';
 
-export type TActionStatutHistoriqueProps = {
-  tache_id: string; // cae_1.2.3.1
-  action_id: string; // cae_1.2.3
-  tache_identifiant: string; // 1.2.3.1
-  tache_nom: string;
-  action_identifiant: string; // 1.2.3
-  action_nom: string;
-  collectivite_id: number;
-  avancement: ActionAvancement;
-  previous_avancement: ActionAvancement | null;
-  avancement_detaille: null | number[];
-  previous_avancement_detaille: null | number[];
-  concerne: boolean;
-  previous_concerne: null | boolean;
-  modified_by_id: string; // 123e4567-e89b-12d3-a456-426614174000
-  modified_at: string; // '2022-08-08T15:12:22.940172+00:00'
-  modified_by_nom: string; // Richard Evans
-};
+export type TActionStatutHistoriqueProps = IHistoricalActionStatutRead;
 
 export const ActionStatutHistorique = (props: TActionStatutHistoriqueProps) => (
   <div>
