accdcc13aa154c385df54c754281b7ead5d3e8ef
Florian d'Erfurth <florian@derfurth.com>
2022-08-18 19:19:20 +0200

    Debounce l'historisation des réponses. Réécriture en commande dynamique.

--- data_layer/sqitch/deploy/evaluation/reponse_historique.sql
+++ data_layer/sqitch/deploy/evaluation/reponse_historique.sql
@@ -4,169 +4,118 @@
 
 BEGIN;
 
---- Tous les éléments sont les mêmes pour les 3 types de réponses
---- on aurait pu faire moins de copier/coller grace à un EXECUTE
---- au détriment de la lisibilité.
-
--- Réponse choix
-create table historique.reponse_choix
+-- A temporary type to declare the composite array containing names and types
+create type historique.question_reponse as
 (
-    collectivite_id  integer,
-    question_id      question_id,
-    reponse          choix_id,
-    previous_reponse choix_id,
-    modified_by      uuid,
-    modified_at      timestamp with time zone
+    type question_type,
+    stored text
 );
-alter table historique.reponse_choix
-    enable row level security;
-create function historique.save_reponse_choix() returns trigger
-as
+
+-- Instead of copying code, we execute a series of dynamic commands per reponse type.
+-- https://www.postgresql.org/docs/14/plpgsql-statements.html#PLPGSQL-STATEMENTS-EXECUTING-DYN
+do
 $$
-begin
-    insert into historique.reponse_choix
-    values (new.collectivite_id,
-            new.question_id,
-            new.reponse,
-            old.reponse,
-            auth.uid(),
-            now());
-    return new;
-end;
-$$ language plpgsql security definer;
+    declare
+        a historique.question_reponse[];
+        qr historique.question_reponse;
+    begin
+        a = array [('binaire', 'bool'), ('choix', 'choix_id'), ('proportion', 'float')];
+        foreach qr in array a
+            loop
 
-create trigger save_history
-    after insert or update
-    on reponse_choix
-    for each row
-execute procedure historique.save_reponse_choix();
+-- create a table of named `reponse_type` that store a reponse of type `stored`
+execute
+'create table historique.reponse_' || qr.type
+||' ('
+||'     id               serial primary key,'
+||'     collectivite_id  integer,'
+||'     question_id      question_id,'
+||'     reponse          ' || qr.stored || ','
+||'     previous_reponse ' || qr.stored || ','
+||'     modified_by      uuid,'
+||'     modified_at      timestamp with time zone'
+||' );';
 
-create view historique.reponse_choix_display
-as
-select collectivite_id,
-       question_id,
-       reponse,
-       lag(reponse) over w as  previous_reponse,
-       modified_at,
-       lag(modified_at) over w previous_modified_at,
-       modified_by,
-       lag(modified_by) over w previous_modified_by
-from historique.reponse_choix
-    window w as (
-        partition by collectivite_id, question_id
-        order by modified_at
-        rows between 1 preceding and current row )
-order by collectivite_id,
-         question_id,
-         modified_at desc;
+-- enable rls for that table
+execute
+'alter table historique.reponse_' || qr.type || ' enable row level security;';
 
+-- create a function named `save_reponse_type`.
+execute
+'create function historique.save_reponse_' || qr.type || '()'
+||' returns trigger'
+||' as'
+||' $string$'
+||' declare'
+||'     updated integer;'
+||' begin'
+--- try to update before inserting to debounce.
+||' update historique.reponse_' || qr.type
+||' set reponse = new.reponse,'
+||'     previous_reponse = old.reponse,'
+||'     modified_at = now()'
+||' where id in (select id'
+||'              from historique.reponse_' || qr.type
+||'              where collectivite_id = new.collectivite_id'
+||'                and question_id = question_id'
+||'                and modified_by = auth.uid()'
+||'                and modified_at > new.modified_at - interval ''1 hour'''
+||'              order by modified_by desc'
+||'              limit 1)'
+||' returning id into updated;'
+--- if nothing was updated, insert.
+||' if updated is null'
+||' then'
+||' insert into historique.reponse_' || qr.type
+||'    (collectivite_id, question_id, reponse, previous_reponse, modified_by, modified_at)'
+||' values (new.collectivite_id,'
+||'         new.question_id,'
+||'         new.reponse,'
+||'         old.reponse,'
+||'         auth.uid(),'
+||'         now());'
+||' end if;'
+||' return new;'
+||' end;'
+||' $string$ language plpgsql security definer;';
 
--- Réponse proportion
-create table historique.reponse_proportion
-(
-    collectivite_id  integer,
-    question_id      question_id,
-    reponse          float,
-    previous_reponse float,
-    modified_by      uuid,
-    modified_at      timestamp with time zone
-);
-alter table historique.reponse_proportion
-    enable row level security;
-create function historique.save_reponse_proportion() returns trigger
-as
-$$
-begin
-    insert into historique.reponse_proportion
-    values (new.collectivite_id,
-            new.question_id,
-            new.reponse,
-            old.reponse,
-            auth.uid(),
-            now());
-    return new;
-end;
-$$ language plpgsql security definer;
+-- create a trigger on the existing public table `reponse_type`
+execute
+'create trigger save_history'
+||'     after insert or update'
+||'     on reponse_' || qr.type
+||'     for each row'
+||' execute procedure historique.save_reponse_' || qr.type || '();';
 
-create trigger save_history
-    after insert or update
-    on reponse_proportion
-    for each row
-execute procedure historique.save_reponse_proportion();
+-- create an intermediary display view that compute previous values.
+execute
+'create view historique.reponse_' || qr.type || '_display'
+||' as'
+||' select collectivite_id,'
+||'        question_id,'
+||'        reponse,'
+||'        lag(reponse) over w as  previous_reponse,'
+||'        modified_at,'
+||'        lag(modified_at) over w previous_modified_at,'
+||'        modified_by,'
+||'        lag(modified_by) over w previous_modified_by'
+||' from historique.reponse_' || qr.type
+||'     window w as ('
+||'         partition by collectivite_id, question_id'
+||'         order by modified_at'
+||'         rows between 1 preceding and current row )'
+||' order by collectivite_id,'
+||'          question_id,'
+||'          modified_at desc;';
 
-create view historique.reponse_proportion_display
-as
-select collectivite_id,
-       question_id,
-       reponse,
-       lag(reponse) over w as  previous_reponse,
-       modified_at,
-       lag(modified_at) over w previous_modified_at,
-       modified_by,
-       lag(modified_by) over w previous_modified_by
-from historique.reponse_proportion
-    window w as (
-        partition by collectivite_id, question_id
-        order by modified_at
-        rows between 1 preceding and current row )
-order by collectivite_id,
-         question_id,
-         modified_at desc;
+            end loop;
+    end ;
+$$;
 
+-- We don't need the type anymore.
+drop type historique.question_reponse;
 
--- Réponse binaire
-create table historique.reponse_binaire
-(
-    collectivite_id  integer,
-    question_id      question_id,
-    reponse          bool,
-    previous_reponse bool,
-    modified_by      uuid,
-    modified_at      timestamp with time zone
-);
-alter table historique.reponse_binaire
-    enable row level security;
-create function historique.save_reponse_binaire() returns trigger
-as
-$$
-begin
-    insert into historique.reponse_binaire
-    values (new.collectivite_id,
-            new.question_id,
-            new.reponse,
-            old.reponse,
-            auth.uid(),
-            now());
-    return new;
-end;
-$$ language plpgsql security definer;
-
-create trigger save_history
-    after insert or update
-    on reponse_binaire
-    for each row
-execute procedure historique.save_reponse_binaire();
-
-create view historique.reponse_binaire_display
-as
-select collectivite_id,
-       question_id,
-       reponse,
-       lag(reponse) over w as  previous_reponse,
-       modified_at,
-       lag(modified_at) over w previous_modified_at,
-       modified_by,
-       lag(modified_by) over w previous_modified_by
-from historique.reponse_binaire
-    window w as (
-        partition by collectivite_id, question_id
-        order by modified_at
-        rows between 1 preceding and current row )
-order by collectivite_id,
-         question_id,
-         modified_at desc;
-
-
+-- Fuse all response types using json values.
 create view historique.reponse_display
 as
 select 'binaire' :: question_type as question_type,
@@ -209,5 +158,6 @@ from historique.reponse_proportion_display
 order by collectivite_id,
          question_id,
          modified_at desc;
+
 
 COMMIT;
