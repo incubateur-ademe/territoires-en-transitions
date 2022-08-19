f672bf7ef01a567e7839ebf864e7aa98b753da6a
Florian d'Erfurth <florian@derfurth.com>
2022-08-19 16:22:08 +0200

    retire la vue `action_statut_historique` remplacée par `historique`.

--- data_layer/sqitch/deploy/referentiel/action_statut_historique.sql
+++ data_layer/sqitch/deploy/referentiel/action_statut_historique.sql
@@ -52,36 +52,4 @@ create trigger save_history
     for each row
 execute procedure historique.save_action_statut();
 
-
-create view action_statut_historique
-as
-with action_history as (select *
-                        from historique.action_statut
-                        order by modified_at desc),
-     actions as (select * from action_hierarchy ah where ah.type = 'action')
-select h.action_id                                                               as tache_id,
-       ah.action_id                                                              as action_id,
-       td.identifiant                                                            as tache_identifiant,
-       td.nom                                                                    as tache_nom,
-       ad.identifiant                                                            as action_identifiant,
-       ad.nom                                                                    as action_nom,
-       collectivite_id,
-       avancement,
-       previous_avancement,
-       avancement_detaille,
-       previous_avancement_detaille,
-       concerne,
-       previous_concerne,
-       modified_by                                                               as modified_by_id,
-       h.modified_at,
-       coalesce(ud.prenom || ' ' || ud.nom, 'Équipe territoires en transitions') as modified_by_nom
-from action_history h
-         join actions ah on h.action_id = any (ah.descendants)
-         join action_definition ad on ah.action_id = ad.action_id -- definition de l'action
-         join action_definition td on h.action_id = td.action_id -- definition de la tache
-         left join utilisateur.dcp_display ud on h.modified_by = ud.user_id
-order by modified_at desc;
-comment on view action_statut_historique is
-    'Historique des modification des statuts.';
-
 COMMIT;
