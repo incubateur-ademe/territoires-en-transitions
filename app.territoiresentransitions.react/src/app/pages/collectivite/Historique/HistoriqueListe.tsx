3998de6e241937f237edb3a68d3c3f36ab1675b9
Marc Rutkowski <marc@attractive-media.fr>
2022-08-23 09:49:21 +0200

    ajoute la pagination dans le composant d'affichage de l'historique

--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueListe.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueListe.tsx
@@ -1,41 +1,52 @@
 import {FC} from 'react';
 import {useCollectiviteId} from 'core-logic/hooks/params';
+import {Pagination} from 'app/pages/ToutesLesCollectivites/components/Pagination';
 import {useHistoriqueItemListe} from 'app/pages/collectivite/Historique/useHistoriqueItemListe';
 import HistoriqueItemActionStatut from 'app/pages/collectivite/Historique/actionStatut/HistoriqueItemActionStatut';
 import HistoriqueItemActionPrecision from 'app/pages/collectivite/Historique/actionPrecision/HistoriqueItemActionPrecision';
-import {THistoriqueItem, THistoriqueItemProps} from './types';
+import {THistoriqueItemProps, THistoriqueProps} from './types';
 import HistoriqueItemReponse from './reponse/HistoriqueItemReponse';
+import {NB_ITEMS_PER_PAGE} from './filters';
 
 export const HistoriqueListe = ({
-  historiqueItemListe,
-}: {
-  historiqueItemListe: THistoriqueItem[];
-}) => {
+  items,
+  total,
+  filters,
+  setFilters,
+}: THistoriqueProps) => {
   return (
-    <div className="flex flex-col gap-5">
-      {historiqueItemListe.map(item => {
-        const {type, action_id, modified_at} = item;
-        const Item = historiqueParType[type];
-        return (
-          <div
-            data-test={`action-statut-historique-${action_id}`}
-            key={`${action_id}-${modified_at}`}
-          >
-            <Item item={item} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="flex flex-col gap-5">
+        {items.map(item => {
+          const {type, action_id, modified_at} = item;
+          const Item = historiqueParType[type];
+          return (
+            <div
+              data-test={`action-statut-historique-${action_id}`}
+              key={`${action_id}-${modified_at}`}
+            >
+              <Item item={item} />
+            </div>
+          );
+        })}
+      </div>
+      {total !== 0 && (
+        <div className="flex justify-center mt-6 md:mt-12">
+          <Pagination
+            nbOfPages={Math.ceil(total / NB_ITEMS_PER_PAGE)}
+            selectedPage={filters.page ?? 1}
+            onChange={selected => setFilters({...filters, page: selected})}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
 export default ({actionId}: {actionId?: string}) => {
   const collectivite_id = useCollectiviteId()!;
-  const historiqueItemListe = useHistoriqueItemListe({
-    collectivite_id,
-    action_id: actionId,
-  });
-  return <HistoriqueListe historiqueItemListe={historiqueItemListe} />;
+  const historique = useHistoriqueItemListe(collectivite_id, actionId);
+  return <HistoriqueListe {...historique} />;
 };
 
 const historiqueParType: {[k: string]: FC<THistoriqueItemProps>} = {
--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/JournalActivite.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/JournalActivite.tsx
@@ -1,30 +1,23 @@
 import {useCollectiviteId} from 'core-logic/hooks/params';
-import {THistoriqueItem} from './types';
+import {THistoriqueProps} from './types';
 import {HistoriqueListe} from './HistoriqueListe';
 import {useHistoriqueItemListe} from './useHistoriqueItemListe';
-
-export type TJournalActiviteProps = {
-  historiqueItemListe: THistoriqueItem[];
-};
 
 /**
  * Affiche le journal d'activité d'une collectivité
  */
-export const JournalActivite = (props: TJournalActiviteProps) => {
-  const {historiqueItemListe} = props;
+export const JournalActivite = (props: THistoriqueProps) => {
   return (
     <main data-test="JournalActivite" className="fr-container mt-9 mb-16">
       <h1 className="text-center fr-mt-4w fr-mb-1w">Journal d'activité</h1>
       <hr />
-      <HistoriqueListe historiqueItemListe={historiqueItemListe} />
+      <HistoriqueListe {...props} />
     </main>
   );
 };
 
 export default () => {
   const collectivite_id = useCollectiviteId()!;
-  const historiqueItemListe = useHistoriqueItemListe({
-    collectivite_id,
-  });
-  return <JournalActivite historiqueItemListe={historiqueItemListe} />;
+  const historique = useHistoriqueItemListe(collectivite_id);
+  return <JournalActivite {...historique} />;
 };
