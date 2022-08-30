04adf8e1da34c004c10a18fc94c7de48b5b6f423
Florian d'Erfurth <florian@derfurth.com>
2022-08-30 11:22:06 +0200

    Supprime les fichiers SQL d'avant sqitch.

--- data_layer/postgres/definitions/37-filterable_collectivites_cron.sql
+++
@@ -1,5 +0,0 @@
-create extension if not exists pg_cron;
-
-select cron.schedule('refresh_collectivite_card',
-                     '0 0 * * *', -- every day
-                     $$refresh materialized view collectivite_card$$);
