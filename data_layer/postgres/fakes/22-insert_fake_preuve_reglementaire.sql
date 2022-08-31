2b2437009e8794e1a04caf411caf3a7ac2b055f9
Florian d'Erfurth <florian@derfurth.com>
2022-08-31 17:12:34 +0200

    Copie preuve réglementaire de la branche `metier_preuves_v2`

---
+++ data_layer/postgres/fakes/22-insert_fake_preuve_reglementaire.sql
@@ -0,0 +1,8 @@
+insert into preuve_reglementaire_definition (id, action_id, nom, description)
+values ('pcaet_rapport_diag', 'cae_1.1.1.3', 'Rapports diagnostic, stratégie et plan d''actions du PCAET',
+        'PCAET avec EES et résumé non technique ; éventuel bilan intermédiaire Preuve identique à la 1.1.2.1'),
+       ('pcaet_deliberation', 'cae_1.1.1.3', 'Délibération d''engagement dans la Convention des Maires', ''),
+       ('pcaet_rapport_dev_durable', 'cae_1.1.1.3', 'Rapport Développement Durable', ''),
+       ('agenda', 'cae_1.1.1.3', 'Agenda 21 / Agenda 2030', ''),
+       ('pcaet_ees', 'cae_1.1.2.1', 'Rapports diagnostic, stratégie et plan d''actions du PCAET', 'PCAET avec EES et résumé non technique ; éventuel bilan intermédiaire Preuve identique à la 1.1.1.3'),
+       ('etude_vulnerabilite', 'cae_1.1.3.2', 'Etude de vulnérabilité au changement climatique', '');
---
+++ data_layer/sqitch/verify/referentiel/preuve_reglementaire.sql
@@ -0,0 +1,7 @@
+-- Verify tet:referentiel/preuve_reglementaire on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
