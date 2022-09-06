40e76f2667dc2227120e3bae7f28a849e76a874e
Arthur Molinos <arthur.molinos@gmail.com>
2022-09-06 18:03:11 +0200

    Transforme le dropdown profil dans le header pour utiliser DropdownFloater

--- app.territoiresentransitions.react/src/app/Layout/Header/HeaderNavigation/HeaderNavigation.tsx
+++ app.territoiresentransitions.react/src/app/Layout/Header/HeaderNavigation/HeaderNavigation.tsx
@@ -6,8 +6,7 @@ import {
   signUpPath,
 } from 'app/paths';
 import {TAuthContext} from 'core-logic/api/auth/AuthProvider';
-import {LogoutBtn} from 'app/Layout/Header/LogoutBtn';
-import {activeTabClassName} from 'app/Layout/Header/CollectiviteNavigation/CollectiviteNavigation';
+import HeaderNavigationProfileDropdown from './HeaderNavigationProfileDropdown';
 
 type Props = {
   auth: TAuthContext;
@@ -33,30 +32,8 @@ const HeaderNavigation = ({auth}: Props) => {
             <div className="fr-fi-question-line mr-2" />
             Aide
           </a>
-          {isConnected ? (
-            <div data-test="connectedMenu" className="group relative">
-              <button className="fr-link" style={{maxWidth: '15rem'}}>
-                <div className="fr-fi-account-line mr-2" />
-                <span className="line-clamp-1">{user?.prenom}</span>
-                <div className="fr-fi-arrow-down-s-line ml-2 scale-90 group-focus-within:rotate-180" />
-              </button>
-              <nav className="bg-white invisible absolute inset-x-0 top-full transition-all opacity-0 drop-shadow-md group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1 z-50">
-                <ul>
-                  <li className="fr-nav__item border-b border-gray-200">
-                    <NavLink
-                      to={monComptePath}
-                      className="fr-nav__link before:!hidden"
-                      activeClassName={activeTabClassName}
-                    >
-                      <span className="px-3">Profil</span>
-                    </NavLink>
-                  </li>
-                  <li className="fr-nav__item">
-                    <LogoutBtn auth={auth} />
-                  </li>
-                </ul>
-              </nav>
-            </div>
+          {isConnected && user ? (
+            <HeaderNavigationProfileDropdown user={user} auth={auth} />
           ) : (
             <>
               <Link data-test="signup" className="fr-link" to={signUpPath}>
---
+++ app.territoiresentransitions.react/src/app/Layout/Header/HeaderNavigation/HeaderNavigationProfileDropdown.tsx
@@ -0,0 +1,74 @@
+import {forwardRef, Ref} from 'react';
+import {NavLink} from 'react-router-dom';
+import classNames from 'classnames';
+
+import DropdownFloater from 'ui/shared/floating-ui/DropdownFloater';
+import {activeTabClassName} from 'app/Layout/Header/CollectiviteNavigation/CollectiviteNavigation';
+import {monComptePath} from 'app/paths';
+import {TAuthContext, UserData} from 'core-logic/api/auth/AuthProvider';
+import {LogoutBtn} from '../LogoutBtn';
+
+const HeaderNavigationProfileDropdown = ({
+  user,
+  auth,
+}: {
+  user: UserData;
+  auth: TAuthContext;
+}) => {
+  return (
+    <>
+      <DropdownFloater
+        placement="bottom-start"
+        render={({close}) => (
+          <nav data-test="connectedMenu">
+            <ul className="m-0 p-0">
+              <li className="fr-nav__item pb-0 border-b border-gray-200">
+                <NavLink
+                  to={monComptePath}
+                  className="fr-nav__link before:!hidden !shadow-none"
+                  activeClassName={activeTabClassName}
+                  onClick={close}
+                >
+                  <span className="px-3">Profil</span>
+                </NavLink>
+              </li>
+              <li className="fr-nav__item pb-0">
+                <LogoutBtn auth={auth} />
+              </li>
+            </ul>
+          </nav>
+        )}
+      >
+        <HeaderNavigationProfileDropdownButtonDisplayed user={user} />
+      </DropdownFloater>
+    </>
+  );
+};
+
+export default HeaderNavigationProfileDropdown;
+
+const HeaderNavigationProfileDropdownButtonDisplayed = forwardRef(
+  (
+    {
+      user,
+      isOpen,
+      ...props
+    }: {
+      isOpen?: boolean;
+      user: UserData;
+    },
+    ref?: Ref<HTMLDivElement>
+  ) => (
+    <div ref={ref} {...props}>
+      <button className="fr-link" style={{maxWidth: '15rem'}}>
+        <div className="fr-fi-account-line mr-2" />
+        <span className="line-clamp-1">{user.prenom}</span>
+        <div
+          className={classNames('fr-fi-arrow-down-s-line ml-2 scale-90', {
+            ['rotate-180']: isOpen,
+          })}
+        />
+      </button>
+    </div>
+  )
+);
