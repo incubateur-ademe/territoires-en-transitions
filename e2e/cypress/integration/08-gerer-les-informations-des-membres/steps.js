90c51cf60a2b0d8f80ce184e6e8374edb3c601fa
Arthur Molinos <arthur.molinos@gmail.com>
2022-09-08 11:15:19 +0200

    test e2e - modifier mes informations personnelles

--- e2e/cypress/integration/08-gerer-les-informations-des-membres/steps.js
+++ e2e/cypress/integration/08-gerer-les-informations-des-membres/steps.js
@@ -1,106 +1,62 @@
 /// <reference types="Cypress" />
 
-import {LocalSelectors} from './selectors';
+import { Selectors } from "../common/selectors";
+import { LocalSelectors } from "./selectors";
+
+const tableauMembresSelector = Selectors["tableau des membres"];
 
 beforeEach(() => {
   // enregistre les définitions locales
-  cy.wrap(LocalSelectors).as('LocalSelectors');
+  cy.wrap(LocalSelectors).as("LocalSelectors");
 });
 
-Given('le tableau charge les informations', () => {
-  cy.get(LocalSelectors['tableau des membres'].selector).within(() => {
-    cy.get('[data-test=Loading]').should('be.visible');
-    cy.get('[data-test=Loading]').should('not.exist');
+Given("le tableau charge les informations", () => {
+  cy.get(tableauMembresSelector.selector).within(() => {
+    cy.get("[data-test=Loading]").should("be.visible");
+    cy.get("[data-test=Loading]").should("not.exist");
   });
 });
 
 Given(
-  'le tableau des membres doit contenir les informations suivantes',
-  (dataTable) => {
-    cy.get(LocalSelectors['tableau des membres'].selector).within(() => {
-      // Attend la disparition du chargement.
-      cy.get('[data-test=Loading]').should('not.exist');
-      cy.wrap(dataTable.rows()).each(
-        (
-          [
-            nom,
-            mail,
-            // telephone,
-            fonction,
-            champ_intervention,
-            details_fonction,
-            acces,
-          ],
-          index,
-        ) => {
-          cy.get(`tbody tr:nth(${index})`).within(() => {
-            cy.get('td:first').should('contain.text', nom);
-            cy.get('td:first').should('contain.text', mail);
-            // cy.get('td:nth(1)').should('contain.text', telephone);
-            cy.get('td:nth(1)').should('contain.text', fonction);
-            cy.get('td:nth(2)').should('contain.text', champ_intervention);
-            cy.get('td:nth(3)').should('contain.text', details_fonction);
-            cy.get('td:nth(4)').should('contain.text', acces);
-          });
-        },
-      );
-    });
-  },
-);
-
-Given(
   /le tableau des membres ne doit pas contenir l'utilisateur "([^"]+)"/,
   (mail) => {
-    cy.get(LocalSelectors['tableau des membres'].selector).should(
-      'not.contain',
-      mail,
-    );
-  },
+    cy.get(tableauMembresSelector.selector).should("not.contain", mail);
+  }
 );
 
-const clickOnDropdownValue =   (champ, email, value) => {
-    if (champ === 'details_fonction') {
-      getUtilisateurRow(email).within(() => {
-        cy.root().
-          find('[data-test="details_fonction-textarea"]').
-          clear().
-          type(value + '{enter}');
-      });
-    } else {
-      getUtilisateurRow(email).within(() => {
-        cy.root().
-          find(`[data-test="${champ}-dropdown"] [aria-label="ouvrir le menu"]`).
-          click();
-      });
-      cy.root().get(`#floating-ui-root [aria-label="${value}"]`).click();
-    }
-  };
+const clickOnDropdownValue = (champ, email, value) => {
+  if (champ === "details_fonction") {
+    getUtilisateurRow(email).within(() => {
+      cy.root()
+        .find('[data-test="details_fonction-textarea"]')
+        .clear()
+        .type(value + "{enter}");
+    });
+  } else {
+    getUtilisateurRow(email).within(() => {
+      cy.root()
+        .find(`[data-test="${champ}-dropdown"] [aria-label="ouvrir le menu"]`)
+        .click();
+    });
+    cy.root().get(`#floating-ui-root [aria-label="${value}"]`).click();
+  }
+};
 
 When(
   /je modifie le champ "([^"]+)" de "([^"]+)" en "([^"]+)"/,
-  clickOnDropdownValue,
+  clickOnDropdownValue
 );
 When(
   /je clique sur la valeur "([^"]+)" du champ "([^"]+)" de "([^"]+)"/,
-  (value, champ, email) => clickOnDropdownValue(champ, email, value),
+  (value, champ, email) => clickOnDropdownValue(champ, email, value)
 );
 
 const getUtilisateurRow = (email) => cy.get(`[data-test="MembreRow-${email}"]`);
 
+Given(/je vois une modale intitulée "([^"]+)"/, (titre) => {
+  cy.get(LocalSelectors["modale"].selector).should("contain", titre);
+});
 
-Given(
-  /je vois une modale intitulée "([^"]+)"/,
-  (titre) => {
-    cy.get(LocalSelectors['modale'].selector).should(
-      'contain',
-      titre,
-    );
-  },
-);
-
-Given(
-  /je clique sur le bouton "([^"]+)" de la modale/,
-  (ariaLabel) => {
-    cy.get(`[aria-label="${ariaLabel}"]`).click();
-  },
-);
+Given(/je clique sur le bouton "([^"]+)" de la modale/, (ariaLabel) => {
+  cy.get(`[aria-label="${ariaLabel}"]`).click();
+});
---
+++ e2e/cypress/integration/11-modifier-mes-informations-personnelles.feature
@@ -0,0 +1,24 @@
+# language: fr
+
+Fonctionnalité: Modifier mes informations personnelles
+
+  Scénario: Modifier mon prénom et mon nom
+    Etant donné que les informations des membres sont réinitialisées
+    Et que je suis connecté en tant que "yolo"
+
+    Quand je visite la vue "Mon compte"
+    Alors un formulaire de modification de compte est affiché
+    # et le formulaire verifie les donnees suivantes
+
+    Quand je modifie le champ "prenom" en "Yolosan"
+    Et que je recharge la page
+    Alors le champ "prenom" doit contenir "Yolosan"
+
+    Quand je modifie le champ "nom" en "Dodoran"
+    Et que je recharge la page
+    Alors le champ "nom" doit contenir "Dodoran"
+
+    Quand je suis sur la page "Gestion des membres" de la collectivité "1"
+    Alors le tableau des membres doit contenir les informations suivantes
+      | nom             | mail          | fonction         | champ_intervention                    | details_fonction                     | acces   |
+      | Yolosan Dodoran | yolo@dodo.com | Référent·e       | Climat Air ÉnergieÉconomie Circulaire | Référent YOLO de cette collectivité  | Admin   |
