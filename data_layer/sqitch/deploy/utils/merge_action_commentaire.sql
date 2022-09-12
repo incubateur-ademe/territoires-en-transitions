334ca87c44a2a09488f5874ceed11b814fb4c468
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-09-12 11:42:12 +0200

    Ajout d'un utilitaire pour merger les commentaires de deux actions

---
+++ data_layer/sqitch/deploy/utils/merge_action_commentaire.sql
@@ -0,0 +1,27 @@
+-- Deploy tet:utils/merge_action_commentaire to pg
+
+BEGIN;
+
+create function
+    private.merge_action_commentaire(a action_id, b action_id)
+    returns void
+as
+$$
+with 
+commentaire_1 as (select * from action_commentaire where action_id = a ),
+commentaire_2 as (select * from action_commentaire where action_id = b ),
+merged as
+(
+select commentaire_1.collectivite_id, 
+case when commentaire_1.commentaire != commentaire_2.commentaire then concat(commentaire_1.commentaire, ' ', commentaire_2.commentaire) else commentaire_1.commentaire end as new_commentaire_1
+from commentaire_1 left join commentaire_2 on commentaire_1.collectivite_id = commentaire_2.collectivite_id)
+update action_commentaire 
+set commentaire = merged.new_commentaire_1
+from merged 
+where action_commentaire.collectivite_id = merged.collectivite_id
+
+$$ language sql;
+comment on function private.merge_action_commentaire is
+    'Merge les commentaires de deux actions en les concaténant.';
+
+COMMIT;
