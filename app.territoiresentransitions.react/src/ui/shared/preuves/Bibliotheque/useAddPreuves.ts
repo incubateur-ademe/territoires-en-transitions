2ee9d8ab73a2c7efbea0e69df7eb97446d51bc7f
Marc Rutkowski <marc@attractive-media.fr>
2022-09-12 14:54:16 +0200

    ajoute les fonctions d'ajout des preuves

---
+++ app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/useAddPreuves.ts
@@ -0,0 +1,96 @@
+import {useMutation, useQueryClient} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+
+// on peut ajouter une preuve sous forme de...
+type TFileOrLink =
+  // ...référence à un fichier de la bibliothèque
+  | {
+      file_id: string;
+      commentaire: string;
+    }
+  // ..ou de lien
+  | {
+      url: string;
+      titre: string;
+      commentaire: string;
+    };
+
+/** Ajoute une preuve réglementaire à une action */
+type TAddPreuveReglementaireArgs = {
+  collectivite_id: number;
+  preuve_id: string;
+} & TFileOrLink;
+export const useAddPreuveReglementaire = () => {
+  const onSuccess = useRefetchOnSuccess();
+
+  return useMutation(
+    async (preuve: TAddPreuveReglementaireArgs) =>
+      supabaseClient.from('preuve_reglementaire').insert(preuve),
+    {
+      mutationKey: 'add_preuve_reglementaire',
+      onSuccess,
+    }
+  );
+};
+
+/** Ajoute une preuve complémentaire à une action */
+type TAddPreuveComplementaireArgs = {
+  collectivite_id: number;
+  action_id: string;
+} & TFileOrLink;
+export const useAddPreuveComplementaire = () => {
+  const onSuccess = useRefetchOnSuccess();
+
+  return useMutation(
+    async (preuve: TAddPreuveComplementaireArgs) =>
+      supabaseClient.from('preuve_complementaire').insert(preuve),
+    {
+      mutationKey: 'add_preuve_complementaire',
+      onSuccess,
+    }
+  );
+};
+
+/** Ajoute une preuve à une demande de labellisation */
+type TAddPreuveLabellisationArgs = {
+  collectivite_id: number;
+  demande_id: string;
+} & TFileOrLink;
+export const useAddPreuveLabellisation = () => {
+  const onSuccess = useRefetchOnSuccess();
+
+  return useMutation(
+    async (preuve: TAddPreuveLabellisationArgs) =>
+      supabaseClient.from('preuve_labellisation').insert(preuve),
+    {
+      mutationKey: 'add_preuve_labellisation',
+      onSuccess,
+    }
+  );
+};
+
+/** Ajoute un rapport de visite annuelle */
+type TAddPreuveRapportArgs = {
+  collectivite_id: number;
+  date: string;
+} & TFileOrLink;
+export const useAddPreuveRapport = () => {
+  const onSuccess = useRefetchOnSuccess();
+
+  return useMutation(
+    async (preuve: TAddPreuveRapportArgs) =>
+      supabaseClient.from('preuve_rapport').insert(preuve),
+    {
+      mutationKey: 'add_preuve_rapport',
+      onSuccess,
+    }
+  );
+};
+
+// recharge la liste des preuves après un insert réussi
+const useRefetchOnSuccess = () => {
+  const queryClient = useQueryClient();
+  return (data: unknown, variables: {collectivite_id: number}) => {
+    queryClient.invalidateQueries(['preuve', variables.collectivite_id]);
+  };
+};
