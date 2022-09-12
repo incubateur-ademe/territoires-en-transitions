ff3331e3bd708575f8504f3938503897cee8d11b
Arthur Molinos <arthur.molinos@gmail.com>
2022-09-12 15:31:10 +0200

    update test e2e avec l'ajout de l'ouverture de la modal de modification d'email

--- e2e/cypress/integration/11-modifier-mes-informations-personnelles/selectors.js
+++ e2e/cypress/integration/11-modifier-mes-informations-personnelles/selectors.js
@@ -2,4 +2,7 @@ export const LocalSelectors = {
   "formulaire de modification de compte": {
     selector: "[data-test=modification-compte-formulaire]",
   },
+  "modale de modification d'email": {
+    selector: "[data-test=modification-email-modal]",
+  },
 };
