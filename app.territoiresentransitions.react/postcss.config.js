0a28a5ccce2ac79de818dae9472912141cea91ac
Arthur Molinos <arthur.molinos@gmail.com>
2022-09-14 15:28:35 +0200

    update Tailwind pour utiliser la v3

--- app.territoiresentransitions.react/postcss.config.js
+++ app.territoiresentransitions.react/postcss.config.js
@@ -1,25 +1,6 @@
-// const postcssNested = require('postcss-nested')
-// const tailwindcss = require('tailwindcss')
-// const autoprefixer = require('autoprefixer')
-// const cssnano = require('cssnano')
-
-// const mode = process.env.FLAVOUR
-// const dev = mode === 'development'
-
-// module.exports = {
-//   plugins: [
-//     postcssNested,
-//     tailwindcss: { config: './tailwindcss-config.js' },
-//     autoprefixer,
-//     !dev &&
-//       cssnano({
-//         preset: 'default'
-//       })
-//   ]
-// }
-
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {config: './tailwind.config.js'},
-//   },
-// };
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
