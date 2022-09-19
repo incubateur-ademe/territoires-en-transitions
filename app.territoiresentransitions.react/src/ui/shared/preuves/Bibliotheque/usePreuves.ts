a25b3e4852df5fe07b752d4ba39a7cb6a23596ca
Marc Rutkowski <marc@attractive-media.fr>
2022-09-19 10:59:35 +0200

    complète les clés de tri de la liste des preuves

--- app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/usePreuves.ts
+++ app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/usePreuves.ts
@@ -3,7 +3,11 @@ import {useQuery} from 'react-query';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {TPreuve, TPreuveType} from './types';
 
-type TFilters = {action_id?: string; demande_id?: number};
+type TFilters = {
+  action_id?: string;
+  demande_id?: number;
+  preuve_types?: TPreuveType[];
+};
 
 // charge les données
 const fetch = async (collectivite_id: number, filters?: TFilters) => {
@@ -15,20 +19,30 @@ const fetch = async (collectivite_id: number, filters?: TFilters) => {
     .order('preuve_reglementaire->>nom' as 'preuve_reglementaire', {
       ascending: true,
     })
+    .order('rapport->>date' as 'rapport', {ascending: false})
+    .order('demande->>referentiel' as 'demande', {ascending: true})
+    .order('demande->>etoiles' as 'demande', {ascending: false})
     .order('lien->>titre' as 'lien', {ascending: true})
     .order('fichier->>filename' as 'fichier', {ascending: true})
     .eq('collectivite_id', collectivite_id);
 
-  // éventuellement filtrées par action (et ses sous-actions)
+  // éventuellement filtrées par action (et ses sous-actions si l'id se termine
+  // par "%")
   const action_id = filters?.action_id;
   if (action_id) {
-    query.ilike('action->>action_id' as 'action', `${action_id}%`);
+    query.ilike('action->>action_id' as 'action', action_id);
   }
 
   // ou par demande de labellisation
   const demande_id = filters?.demande_id;
   if (demande_id) {
     query.eq('demande->>id' as 'demande', demande_id);
+  }
+
+  // ou par type(s)
+  const preuve_types = filters?.preuve_types;
+  if (preuve_types) {
+    query.in('preuve_type', preuve_types);
   }
 
   const {data, error} = await query;
@@ -62,13 +76,21 @@ export const usePreuvesParType = (filters?: TFilters) => {
 };
 
 // indexe une liste de preuves par type
-type TPreuvesParType = Record<TPreuveType, TPreuve[]>;
+export type TPreuvesParType = Record<TPreuveType, TPreuve[]>;
 const groupByType = (preuves: TPreuve[]) => {
-  return preuves.reduce<TPreuvesParType>(
+  const preuvesParType = preuves.reduce<TPreuvesParType>(
     (dict, preuve) => ({
       ...dict,
       [preuve.preuve_type]: [...(dict[preuve.preuve_type] || []), preuve],
     }),
     {} as TPreuvesParType
   );
+
+  // filtre supplémentaire pour éviter les preuves labellisation vides (?)
+  return {
+    ...preuvesParType,
+    labellisation: preuvesParType.labellisation?.filter(
+      ({fichier, lien}) => fichier || lien
+    ),
+  };
 };
