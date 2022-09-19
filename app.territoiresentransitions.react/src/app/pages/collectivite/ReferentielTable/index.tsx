29d40bc2f5d8d19f2500209c950847cf368ac19a
Marc Rutkowski <marc@attractive-media.fr>
2022-09-19 18:33:50 +0200

    Revert "rend optionnel l'en-tête des vues tabulaires"
    This reverts commit e5eba50483aabcafb8c44efb5fd97d1665aa29a9.

--- app.territoiresentransitions.react/src/app/pages/collectivite/ReferentielTable/index.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ReferentielTable/index.tsx
@@ -9,31 +9,18 @@ type Table = <T extends Record<string, unknown>>(props: {
   className?: string;
   isLoading: boolean;
   table: TableInstance<T>;
-  referentiel?: ReferentielParamOption;
-  noHeader?: boolean;
-  customHeaderProps?: Record<string, unknown>;
+  customHeaderProps: Record<string, unknown>;
   customCellProps?: Record<string, unknown>;
 }) => JSX.Element;
 
 const ReferentielTable: Table = props => {
-  const {
-    className,
-    isLoading,
-    table,
-    referentiel,
-    noHeader,
-    customHeaderProps,
-    customCellProps,
-  } = props;
+  const {className, isLoading, table, customHeaderProps, customCellProps} =
+    props;
   const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} =
     table;
 
   const collectiviteId = useCollectiviteId();
-
-  // si le nom du référentiel n'est pas passé en prop on l'obtient de l'url
-  const referentielIdFromUrl = useReferentielId() as ReferentielParamOption;
-  const referentielId = referentiel || referentielIdFromUrl;
-
+  const referentielId = useReferentielId() as ReferentielParamOption;
   const cellProps = useMemo(
     () => ({collectiviteId, referentielId, ...(customCellProps || {})}),
     [collectiviteId, referentielId, customCellProps]
@@ -50,19 +37,17 @@ const ReferentielTable: Table = props => {
       {...getTableProps()}
       className={`referentiel-table ${referentielId} ${className || ''}`}
     >
-      {noHeader ? null : (
-        <div className="header">
-          {headerGroups.map(headerGroup => (
-            <div className="row" {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <div className="cell" {...column.getHeaderProps()}>
-                  {column.render('Header', customHeaderProps)}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="header">
+        {headerGroups.map(headerGroup => (
+          <div className="row" {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <div className="cell" {...column.getHeaderProps()}>
+                {column.render('Header', customHeaderProps)}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
       <div className="body" {...getTableBodyProps()}>
         {isLoading ? (
           <div className="message">Chargement en cours...</div>
