8f9954324e90c3a88471417cb628856e0cc33eae
Florian d'Erfurth <florian@derfurth.com>
2022-09-26 11:55:55 +0200

    Utilise `active_collectivite` et renomme `stats_real_collectivites` en `stats_active_real_collectivites`.

--- data_layer/sqitch/revert/stats/collectivite.sql
+++ data_layer/sqitch/revert/stats/collectivite.sql
@@ -1,9 +1,15 @@
--- Revert tet:stats/collectivite from pg
+-- Deploy tet:stats/collectivite to pg
+-- requires: collectivite/collectivite
+-- requires: utilisateur/droits
 
 BEGIN;
 
-drop view stats_unique_active_collectivite;
-drop view stats_rattachements;
-drop view stats_real_collectivites;
+alter view stats_active_real_collectivites rename to stats_real_collectivites;
+
+create or replace view stats_real_collectivites
+as
+select *
+from named_collectivite
+where collectivite_id not in (select collectivite_id from collectivite_test);
 
 COMMIT;
