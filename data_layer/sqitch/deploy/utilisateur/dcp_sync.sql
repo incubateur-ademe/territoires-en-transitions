32209f30c9f5a063cd82311262f5063b15ebf7c9
Florian d'Erfurth <florian@derfurth.com>
2022-09-26 14:52:53 +0200

    Mets à jour l'email des DCPs après le changement de d'email de compte.

---
+++ data_layer/sqitch/deploy/utilisateur/dcp_sync.sql
@@ -0,0 +1,27 @@
+-- Deploy tet:utilisateur/dcp_sync to pg
+
+BEGIN;
+
+create function
+    utilisateur.sync_dcp()
+    returns trigger
+as
+$$
+begin
+    update public.dcp
+    set email = new.email
+    where user_id = new.id;
+    return new;
+end;
+$$ language plpgsql;
+comment on function utilisateur.sync_dcp is
+    'Mets à jour les DCP lorsque l''email change.';
+
+
+create trigger before_user_update
+    after update
+    on auth.users
+    for each row
+execute procedure utilisateur.sync_dcp();
+
+COMMIT;
