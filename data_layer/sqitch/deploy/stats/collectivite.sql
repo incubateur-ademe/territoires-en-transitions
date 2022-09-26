43733c29bffad3af9f1545e9287bd8342eec582a
Florian d'Erfurth <florian@derfurth.com>
2022-09-26 11:55:55 +0200

    Utilise `active_collectivite` et renomme `stats_real_collectivites` en `stats_active_real_collectivites`.

--- data_layer/sqitch/deploy/stats/collectivite.sql
+++ data_layer/sqitch/deploy/stats/collectivite.sql
@@ -4,42 +4,12 @@
 
 BEGIN;
 
-create view stats_real_collectivites
+create or replace view stats_real_collectivites
 as
 select *
-from named_collectivite
+from active_collectivite ac
 where collectivite_id not in (select collectivite_id from collectivite_test);
 
-create materialized view stats_rattachements
-as
-with daily as (
-    select created_at::date as day, count(created_at) as count
-    from private_utilisateur_droit d
-             join stats_real_collectivites c on d.collectivite_id = c.collectivite_id
-    group by day
-)
-select day                                                                                      as date,
-       count,
-       sum(count) over (order by day rows between unbounded preceding and current row)::integer as cumulated_count
-from daily;
-
-create materialized view stats_unique_active_collectivite
-as
-with unique_collectivite_droit as (
-    select d.collectivite_id, min(created_at) as created_at
-    from private_utilisateur_droit d
-             join stats_real_collectivites c on d.collectivite_id = c.collectivite_id
-    group by d.collectivite_id
-),
-     daily as (
-         select created_at::date as day, count(created_at) as count
-         from unique_collectivite_droit d
-                  join stats_real_collectivites c on d.collectivite_id = c.collectivite_id
-         group by day
-     )
-select day                                                                                      as date,
-       count,
-       sum(count) over (order by day rows between unbounded preceding and current row)::integer as cumulated_count
-from daily;
+alter view stats_real_collectivites rename to stats_active_real_collectivites;
 
 COMMIT;
