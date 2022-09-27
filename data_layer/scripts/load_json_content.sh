34339b5f5121c5195b1619753b1ecd823189f420
Florian d'Erfurth <florian@derfurth.com>
2022-09-27 18:13:46 +0200

    Nouveau loader.

--- data_layer/scripts/load_json_content.sh
+++ data_layer/scripts/load_json_content.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "Uploading json to $SUPABASE_URL/rest/v1/..."
 curl -X POST \
      -H "apikey: $SERVICE_ROLE_KEY" \
      -H "Authorization: Bearer $SERVICE_ROLE_KEY" \
      -H "Content-Type: application/json" \
      -d @../content/preuves.json \
-"$SUPABASE_URL/rest/v1/preuve_reglementaire_json" \
+"$SUPABASE_URL/rest/v1/preuve_reglementaire_json"
--- data_layer/scripts/loaded_content.sh
+++ data_layer/scripts/loaded_content.sh
@@ -7,4 +7,5 @@ psql -c "select 1" || exit 1
 psql -v ON_ERROR_STOP=1 --file "$DATALAYER_DIR"/verify/sqitch.sql || exit 1
 psql -v ON_ERROR_STOP=1 --file "$DATALAYER_DIR"/verify/content.sql || exit 1
 psql -v ON_ERROR_STOP=1 --file "$DATALAYER_DIR"/verify/fakes.sql || exit 1
+psql -v ON_ERROR_STOP=1 --file "$DATALAYER_DIR"/verify/preuves.sql || exit 1
 exit 0
