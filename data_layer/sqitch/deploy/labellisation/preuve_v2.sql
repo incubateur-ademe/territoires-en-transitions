42dab4007273871bd1986a1ae20d62bcb4c66281
Florian d'Erfurth <florian@derfurth.com>
2022-09-27 17:18:01 +0200

    Chargement des preuves réglementaires en json.

--- data_layer/sqitch/deploy/labellisation/preuve_v2.sql
+++ data_layer/sqitch/deploy/labellisation/preuve_v2.sql
@@ -176,8 +176,9 @@ select 'reglementaire',
 from collectivite c -- toutes les collectivités ...
          left join preuve_reglementaire_definition prd on true -- ... x toutes les preuves réglementaires
          left join preuve_reglementaire pr on prd.id = pr.preuve_id
+         left join preuve_action pa on prd.id = pa.preuve_id
          left join labellisation.bibliotheque_fichier_snippet fs on fs.id = pr.fichier_id
-         left join labellisation.action_snippet snippet on snippet.action_id = prd.action_id and snippet.collectivite_id = c.id
+         left join labellisation.action_snippet snippet on snippet.action_id = pa.action_id and snippet.collectivite_id = c.id
 union all
 
 select 'labellisation',
--- data_layer/sqitch/revert/referentiel/preuve_reglementaire.sql
+++ data_layer/sqitch/revert/referentiel/preuve_reglementaire.sql
@@ -2,8 +2,12 @@
 
 BEGIN;
 
-drop function business_upsert_preuves(preuve_reglementaire_definition[]);
+drop trigger after_preuve_json on preuve_reglementaire_json;
+drop function labellisation.upsert_preuves_reglementaire(jsonb);
+drop function labellisation.upsert_preuves_reglementaire_after_json_insert();
+drop table preuve_action;
 drop table preuve_reglementaire_definition;
+drop table preuve_reglementaire_json;
 drop domain preuve_id;
 
 COMMIT;
