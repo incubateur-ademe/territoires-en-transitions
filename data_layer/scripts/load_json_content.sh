94aef5846b979be163ce15e95d0cca188bac8658
Florian d'Erfurth <florian@derfurth.com>
2022-09-27 17:18:01 +0200

    Chargement des preuves réglementaires en json.

---
+++ data_layer/scripts/load_json_content.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -X POST \
+     -H "apikey: $SERVICE_ROLE_KEY" \
+     -H "Authorization: Bearer $SERVICE_ROLE_KEY" \
+     -H "Content-Type: application/json" \
+     -d @../content/preuves.json \
+"$SUPABASE_URL/rest/v1/preuve_reglementaire_json" \
