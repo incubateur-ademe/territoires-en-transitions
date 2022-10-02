9a8f29ac31a8a013154b0ba69da8fb49e0840ea0
Florian d'Erfurth <florian@derfurth.com>
2022-10-02 16:00:42 +0200

    Améliore la fonction de merge des commentaires.

--- data_layer/sqitch/deploy/utils/merge_action_commentaire.sql
+++ data_layer/sqitch/deploy/utils/merge_action_commentaire.sql
@@ -7,22 +7,24 @@ create function
     returns void
 as
 $$
-with 
-commentaire_1 as (select * from action_commentaire where action_id = a ),
-commentaire_2 as (select * from action_commentaire where action_id = b ),
-merged as
-(
-select commentaire_1.collectivite_id, 
-case when commentaire_1.commentaire != commentaire_2.commentaire then concat(commentaire_1.commentaire, ' ', commentaire_2.commentaire) else commentaire_1.commentaire end as new_commentaire_1
-from commentaire_1 left join commentaire_2 on commentaire_1.collectivite_id = commentaire_2.collectivite_id)
-update action_commentaire 
-set commentaire = merged.new_commentaire_1
-from merged 
-where action_commentaire.collectivite_id = merged.collectivite_id
-and action_commentaire.action_id in (a, b);
+with commentaire_a as (select * from action_commentaire where action_id = a),
+     commentaire_b as (select * from action_commentaire where action_id = b),
+     merged as (select commentaire_a.collectivite_id,
+                       case -- si les commentaires sont identiques, alors le résultat du merge est null.
+                           when commentaire_a.commentaire != commentaire_b.commentaire then
+                               concat(commentaire_a.commentaire, ' ', commentaire_b.commentaire) end as commentaire
+                from commentaire_a
+                         left join commentaire_b on commentaire_a.collectivite_id = commentaire_b.collectivite_id)
+update action_commentaire
+set commentaire = merged.commentaire
+from merged
+where merged.commentaire is not null -- on update seulement si les commentaires ont étés fusionnés
+  and action_commentaire.collectivite_id = merged.collectivite_id
+  and action_commentaire.action_id in (a, b);
 
 $$ language sql;
 comment on function private.merge_action_commentaire is
-    'Merge les commentaires de deux actions en les concaténant.';
+    'Fusionne les commentaires de deux actions en les concaténant. '
+        'Si les commentaires a et b sont différents, alors leurs valeurs deviendront "a b".';
 
 COMMIT;
