71fc2b49235f75e213e19cc15d26fef108fa6a73
Arthur Molinos <arthur.molinos@gmail.com>
2022-10-05 12:03:59 +0200

    Création de la route et la navigation RejoindreUneCollectivite dans le profil

--- app.territoiresentransitions.react/src/app/pages/Profil/ProfileRoutes.tsx
+++ app.territoiresentransitions.react/src/app/pages/Profil/ProfileRoutes.tsx
@@ -1,6 +1,11 @@
 import {Route} from 'react-router-dom';
 import MonCompte from 'app/pages/Profil/MonCompte/MonCompte';
-import {monComptePath, profilPath} from 'app/paths';
+import RejoindreUneCollectivite from 'app/pages/Profil/RejoindreUneCollectivite/RejoindreUneCollectivite';
+import {
+  monComptePath,
+  profilPath,
+  rejoindreUneCollectivitePath,
+} from 'app/paths';
 
 export const ProfilRoutes = () => {
   return (
@@ -8,9 +13,9 @@ export const ProfilRoutes = () => {
       <Route exact path={[profilPath, monComptePath]}>
         <MonCompte />
       </Route>
-      {/* <Route path={mesCollectivitesPath}>
-        <div>Hello</div>
-      </Route> */}
+      <Route path={rejoindreUneCollectivitePath}>
+        <RejoindreUneCollectivite />
+      </Route>
     </>
   );
 };
