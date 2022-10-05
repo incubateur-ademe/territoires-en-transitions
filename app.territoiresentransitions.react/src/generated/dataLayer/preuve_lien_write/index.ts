d258e044e8df4e49f40115b9066d584cfba659f3
Marc Rutkowski <marc@attractive-media.fr>
2022-10-05 10:01:14 +0200

    suppression d'anciens fichiers

--- app.territoiresentransitions.react/src/generated/dataLayer/preuve_lien_write/index.ts
+++
@@ -1,8 +0,0 @@
-export interface PreuveLienWrite {
-  id?: number;
-  collectivite_id: number;
-  action_id: string;
-  url: string;
-  titre: string;
-  commentaire: string;
-}
--- app.territoiresentransitions.react/src/generated/dataLayer/preuve_read/index.ts
+++
@@ -1,9 +0,0 @@
-import {PreuveFichierRead} from '../preuve_fichier_read';
-import {PreuveLienRead} from '../preuve_lien_read';
-
-export interface PreuveParams {
-  collectivite_id: number;
-  action_id: string;
-}
-
-export type PreuveRead = PreuveLienRead | PreuveFichierRead;
