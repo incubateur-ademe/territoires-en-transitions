a2b4a716343785a2901d46b727eae823dcc0c353
Marc Rutkowski <marc@attractive-media.fr>
2022-10-10 09:01:30 +0200

    ajoute le composant permettant de télécharger tous les documents associés à une action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/DownloadDocs.tsx
@@ -0,0 +1,120 @@
+import {useQueries, useQuery, useQueryClient} from 'react-query';
+import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {usePreuves} from 'ui/shared/preuves/Bibliotheque/usePreuves';
+import {TFichier} from 'ui/shared/preuves/Bibliotheque/types';
+import {saveBlob} from 'ui/shared/preuves/Bibliotheque/saveBlob';
+
+export type TDownloadDocsProps = {
+  action: ActionDefinitionSummary;
+};
+
+const URL = `/zip`;
+
+// durée de validité des liens générées (en secondes)
+const LINKS_EXPIRES_IN_SEC = 360;
+
+const BTN = 'fr-btn fr-btn--secondary fr-btn--icon-left fr-btn--sm';
+
+/**
+ * Affiche le bouton de téléchargement des documents preuves
+ */
+export const DownloadDocs = (props: TDownloadDocsProps) => {
+  const {action} = props;
+  const {refetch, isFetching} = useDownloadDocs(action);
+  const queryClient = useQueryClient();
+
+  return isFetching ? (
+    <p>
+      Téléchargement en cours...
+      <button
+        className={`${BTN} fr-ml-4w fr-fi-close-line`}
+        onClick={() => queryClient.cancelQueries('zip-action')}
+      >
+        Annuler
+      </button>
+    </p>
+  ) : (
+    <button
+      className={`${BTN} fr-fi-download-line`}
+      disabled={isFetching}
+      onClick={() => refetch()}
+    >
+      Télécharger toutes les preuves
+    </button>
+  );
+};
+
+/**
+ * Renvoie une instance de `useQuery` permettant de déclencher (en appelant la
+ * fonction `refetch`) la génération et le téléchargement d'un zip et contenant
+ * tous les fichiers associés à une sous-action.
+ */
+const useDownloadDocs = (action: ActionDefinitionSummary) => {
+  const {referentiel, identifiant} = action;
+  const collectivite = useCurrentCollectivite();
+  const {nom} = collectivite || {};
+
+  // récupère la liste des fichiers à télécharger
+  const preuves = usePreuves({action, withSubActions: true});
+
+  // indexe les fichiers par leur clé (pour avoir l'unicité) et retransforme en
+  // tableau les valeurs restantes
+  const fichiers = Object.values(
+    preuves.reduce((filenameByHash, {fichier}) => {
+      return fichier
+        ? {...filenameByHash, [fichier.hash]: fichier}
+        : filenameByHash;
+    }, {} as Record<string, TFichier>)
+  );
+
+  // génère les URLs de téléchargement
+  const signedUrls = useSignedUrls(fichiers);
+  // et le nom du fichier cible
+  const filename = `${referentiel}_${identifiant}_${nom}.zip`;
+
+  // appelle le endpoint de génération du zip
+  return useQuery(
+    'zip-action',
+    async ({signal}) => {
+      if (collectivite && signedUrls) {
+        const response = await fetch(URL, {
+          signal,
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({signedUrls}),
+        });
+        // sauvegarde si le téléchargement a réussi
+        if (response.status === 200) {
+          const blob = await response.blob();
+          await saveBlob(blob, filename);
+        }
+      }
+    },
+    {enabled: false}
+  );
+};
+
+// crée une url signée temporaire pour chaque fichier
+// on n'utilise pas `createSignedUrls` (au pluriel) car cela semble ne pas
+// fonctionner correctement en environnement local (mais fonctionne sur sandbox)
+const useSignedUrls = (fichiers: TFichier[]) => {
+  return useQueries(
+    fichiers.map(({bucket_id, hash}) => {
+      return {
+        queryKey: ['signed-urls', bucket_id, hash],
+        queryFn: () =>
+          supabaseClient.storage
+            .from(bucket_id)
+            .createSignedUrl(hash, LINKS_EXPIRES_IN_SEC),
+        staleTime: LINKS_EXPIRES_IN_SEC * 1000,
+      };
+    })
+  )
+    .map((reply, index) => {
+      const {data} = reply;
+      return {filename: fichiers[index].filename, url: data?.signedURL || null};
+    })
+    .filter(fichier => Boolean(fichier.url));
+};
--- app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/openPreuve.tsx
+++ app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/openPreuve.tsx
@@ -1,5 +1,6 @@
 import {supabaseClient} from 'core-logic/api/supabase';
 import {TPreuve} from './types';
+import {saveBlob} from './saveBlob';
 
 /**
  * Ouvre un document :
@@ -18,28 +19,12 @@ export const openPreuve = async (preuve: TPreuve) => {
 
 const downloadPreuve = async (preuve: TPreuve) => {
   const {filename, hash, bucket_id} = preuve.fichier!;
-  // télécharge le fichier car l'ouverture directe de l'URL ne fonctionne
-  // pas, les headers d'authenfication étant absents
+
+  // télécharge le fichier
   const {data} = await supabaseClient.storage.from(bucket_id).download(hash);
 
-  // si le téléchargement a réussi
   if (data) {
-    // crée un blob
-    const blobURL = URL.createObjectURL(data);
-
-    // crée un lien invisible
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-
-    // affecte au lien le blob et le nom du fichier téléchargé
-    a.href = blobURL;
-    a.download = filename;
-
-    // déclenche le téléchargement puis supprime le lien
-    a.click();
-    setTimeout(() => {
-      window.URL.revokeObjectURL(blobURL);
-      document.body.removeChild(a);
-    }, 0);
+    // et le sauvegarde si le téléchargement a réussi
+    saveBlob(data, filename);
   }
 };
