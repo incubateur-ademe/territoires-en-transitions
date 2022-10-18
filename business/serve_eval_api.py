7fb1ac49ee16ca41b7a6cd14d47143e240a529df
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-10-18 16:41:28 +0200

    Fix import logging

--- business/serve_eval_api.py
+++ business/serve_eval_api.py
@@ -1,13 +1,13 @@
 from os import environ
 import uvicorn
-from logging import logger
 
 
 PORT = int(environ.get("PORT", "8888"))
 
+
 def serve():
     """Run function for Scalingo, see `Procfile`"""
-    logger.info(f"Running uvicorn on {PORT}")
+    print(f"Running uvicorn on {PORT}")
     uvicorn.run("api.app:app", host="0.0.0.0", port=PORT, workers=1)
 
 
