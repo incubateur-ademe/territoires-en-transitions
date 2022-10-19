0c69d848d7d446ab31f390efc1562bf0f0999d90
Florian d'Erfurth <florian@derfurth.com>
2022-10-19 10:14:48 +0200

    Supprime des tests client.

--- app.territoiresentransitions.react/src/core-logic/api/endpoints/ActionStatutReadEndpoint.spec.ts
+++ app.territoiresentransitions.react/src/core-logic/api/endpoints/ActionStatutReadEndpoint.spec.ts
@@ -23,38 +23,4 @@ describe('Action-statut reading endpoint should retrieve data-layer default stat
     expect(actionStatutReadEndpoint.lastResponse?.status).toBe(200);
     expect(results.length).toBeGreaterThanOrEqual(1);
   });
-
-  it('Retrieves status with basic avancement fait for collectivite 1 at cae_1.1.1.1.1 (for any connected user)', async () => {
-    const actionStatutReadEndpoint = new ActionStatutReadEndpoint([]);
-
-    await supabaseClient.auth.signIn(yuluCredentials); // Yulu is connected but has no rights on collectivite #1
-
-    const results = await actionStatutReadEndpoint.getBy({
-      collectivite_id: 1,
-      action_id: 'cae_1.1.1.1.1',
-    });
-    expect(results.length).toBeGreaterThanOrEqual(1);
-    expect(results[0]).toMatchObject({
-      action_id: 'cae_1.1.1.1.1',
-      collectivite_id: 1,
-      avancement: 'fait',
-    });
-  });
-  it('Retrieves status with detailed avancement (.2, .7, .1) for collectivite 1 at cae_1.1.1.1.2 (for any connected user)', async () => {
-    const actionStatutReadEndpoint = new ActionStatutReadEndpoint([]);
-
-    await supabaseClient.auth.signIn(yuluCredentials); // Yulu is connected but has no rights on collectivite #1
-
-    const results = await actionStatutReadEndpoint.getBy({
-      collectivite_id: 1,
-      action_id: 'cae_1.1.1.1.2',
-    });
-    expect(results.length).toBeGreaterThanOrEqual(1);
-    expect(results[0]).toMatchObject({
-      action_id: 'cae_1.1.1.1.2',
-      collectivite_id: 1,
-      avancement: 'detaille',
-      avancement_detaille: [0.2, 0.7, 0.1],
-    });
-  });
 });
