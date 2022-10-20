64b2244e2068e917b3c8a29fc7e57d220e4f8be2
Florian d'Erfurth <florian@derfurth.com>
2022-10-20 15:01:03 +0200

    Rework de la feature fichier preuves pour fonctionner avec les données de prod

--- data_layer/sqitch/deploy/labellisation/fichier_preuve.sql
+++ data_layer/sqitch/deploy/labellisation/fichier_preuve.sql
@@ -4,31 +4,18 @@
 
 BEGIN;
 
--- on enlève `referentiel/preuve` qui est emmêlé avec la présente `labellisation/fichier_preuve`
-drop view if exists retool_score; -- depends on preuve.
+-- drop les dépendances.
+drop function add_bibliotheque_fichier;
 drop view preuve;
-drop function upsert_preuve_fichier;
-drop function delete_preuve_fichier;
+drop view labellisation.bibliotheque_fichier_snippet;
+drop view bibliotheque_fichier;
 
--- partially remove previous version
-drop function upsert_labellisation_preuve_fichier;
-drop function delete_labellisation_preuve_fichier;
-drop view action_preuve_fichier;
-drop trigger remove_preuve_fichier_before_file_delete on storage.objects;
-drop view action_labellisation_preuve_fichier;
-drop trigger remove_labellisation_preuve_fichier_before_file_delete on storage.objects;
-drop function remove_labellisation_preuve_fichier;
+-- modifie la table
+alter table labellisation.bibliotheque_fichier
+-- modifie la longueur max pour que le nom de fichier le plus long en prod puisse être utilisé comme hash.
+    alter column hash type varchar(160);
 
-
-create table labellisation.bibliotheque_fichier
-(
-    id              serial primary key,
-    collectivite_id integer references collectivite,
-    hash            varchar(64), -- sha 256 calculé par le client et utilisé comme filename dans storage
-    filename        text,        -- le nom d'origine du fichier chez l'utilisateur
-    unique (collectivite_id, hash)
-);
-
+-- restaure les dépendances.
 create view bibliotheque_fichier
 as
 select bf.id,
@@ -42,6 +29,17 @@ from labellisation.bibliotheque_fichier bf
          join collectivite_bucket cb on cb.collectivite_id = bf.collectivite_id
          join storage.objects o on o.name = bf.hash and o.bucket_id = cb.bucket_id;
 
+
+-- Vue partielle `bibliotheque_fichier` en json.
+create view labellisation.bibliotheque_fichier_snippet as
+select id,
+       jsonb_build_object(
+               'filename', filename,
+               'hash', hash,
+               'bucket_id', bucket_id,
+               'filesize', filesize) as snippet
+from public.bibliotheque_fichier;
+comment on view bibliotheque_fichier is 'Vue partielle de la table `public.bibliotheque_fichier` en json.';
 
 create function add_bibliotheque_fichier(
     collectivite_id integer,
@@ -92,5 +90,100 @@ end;
 $$ language plpgsql security definer;
 comment on function add_bibliotheque_fichier is
     'Ajoute un fichier présent dan le bucket de la collectivité à l''adresse `bucket/hash`, dans la bibliothèque de fichiers.';
+
+
+-- La vue utilisée par le client qui regroupe tout les types de preuves.
+create or replace view preuve
+as
+-- Les preuves complémentaires.
+select -- champs communs
+       'complementaire':: preuve_type              as preuve_type,
+       pc.id,
+       pc.collectivite_id,
+       fs.snippet                                  as fichier,
+       pc.lien,
+       pc.commentaire,
+       pc.modified_at                              as created_at,
+       pc.modified_by                              as created_by,
+       utilisateur.modified_by_nom(pc.modified_by) as created_by_nom,
+       snippet.snippet                             as action,
+       null:: jsonb                                as preuve_reglementaire,
+       null:: jsonb                                as demande,
+       null:: jsonb                                as rapport
+from preuve_complementaire pc
+         left join labellisation.bibliotheque_fichier_snippet fs
+                   on fs.id = pc.fichier_id
+         left join labellisation.action_snippet snippet
+                   on snippet.action_id = pc.action_id
+                       and snippet.collectivite_id = pc.collectivite_id
+union all
+
+-- Toutes les preuves réglementaires par collectivité, avec ou sans fichier.
+select 'reglementaire',
+       pr.id,
+       c.id,
+       fs.snippet,
+       pr.lien,
+       pr.commentaire,
+       pr.modified_at,
+       pr.modified_by,
+       utilisateur.modified_by_nom(pr.modified_by),
+       snippet.snippet,
+       (select jsonb_build_object(
+                       'id', prd.id,
+                       'nom', prd.nom,
+                       'description', prd.description)),
+       null,
+       null
+from collectivite c -- toutes les collectivités ...
+         left join preuve_reglementaire_definition prd on true -- ... x toutes les preuves réglementaires
+         left join preuve_reglementaire pr on prd.id = pr.preuve_id and c.id = pr.collectivite_id
+         left join preuve_action pa on prd.id = pa.preuve_id
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = pr.fichier_id
+         left join labellisation.action_snippet snippet
+                   on snippet.action_id = pa.action_id and snippet.collectivite_id = c.id
+union all
+
+select 'labellisation',
+       p.id,
+       d.collectivite_id,
+       fs.snippet,
+       p.lien,
+       p.commentaire,
+       p.modified_at,
+       p.modified_by,
+       utilisateur.modified_by_nom(p.modified_by),
+       null,
+       null,
+       (select jsonb_build_object(
+                       'en_cours', d.en_cours,
+                       'referentiel', d.referentiel,
+                       'etoiles', d.etoiles,
+                       'date', d.date,
+                       'id', d.id
+                   )),
+       null
+from labellisation.demande d
+         join preuve_labellisation p on p.demande_id = d.id
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
+
+union all
+select 'rapport',
+       p.id,
+       p.collectivite_id,
+       fs.snippet,
+       p.lien,
+       p.commentaire,
+       p.modified_at,
+       p.modified_by,
+       utilisateur.modified_by_nom(p.modified_by),
+       null,
+       null,
+       null,
+       jsonb_build_object('date', p.date)
+from preuve_rapport p
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
+;
+comment on view preuve is 'Regroupe tout les types de preuves.';
 
 COMMIT;
--- data_layer/sqitch/revert/labellisation/fichier_preuve.sql
+++ data_layer/sqitch/revert/labellisation/fichier_preuve.sql
@@ -4,338 +4,185 @@
 
 BEGIN;
 
--- drop current version
+-- drop les dépendances.
 drop function add_bibliotheque_fichier;
+drop view preuve;
+drop view labellisation.bibliotheque_fichier_snippet;
 drop view bibliotheque_fichier;
-drop table labellisation.bibliotheque_fichier;
 
--- restore `labellisation/fichier_preuve`
--- Create file action relation table.
--- create table preuve_fichier
--- (
---     collectivite_id integer references collectivite      not null,
---     action_id       action_id references action_relation not null,
---     file_id         uuid references storage.objects      not null,
---     commentaire     text                                 not null default '',
---     primary key (collectivite_id, action_id, file_id)
--- );
--- comment on table preuve_fichier is
---     'A preuve fichier links a file from storage to an action for a collectivité.';
--- alter table preuve_fichier
---     enable row level security;
---
--- create policy allow_read
---     on preuve_fichier for select
---     using (is_authenticated());
---
--- create policy allow_insert
---     on preuve_fichier for insert
---     with check (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = preuve_fichier.file_id)));
---
--- create policy allow_update
---     on preuve_fichier for update
---     using (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = preuve_fichier.file_id)));
---
--- create policy allow_delete
---     on preuve_fichier for delete
---     using (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = preuve_fichier.file_id)));
+-- modifie la table
+alter table labellisation.bibliotheque_fichier
+    alter column hash type varchar(64);
 
-
--- Convenience upsert functions so the client does not have to know about object ids
-create or replace function upsert_preuve_fichier(
-    collectivite_id integer,
-    action_id action_id,
-    filename text,
-    commentaire text
-)
-    returns void
+-- restaure les dépendances.
+create view bibliotheque_fichier
 as
-$$
-with file as (select obj.id
-              from storage.objects obj
-                       join collectivite_bucket cb on obj.bucket_id = cb.bucket_id
-              where cb.collectivite_id = upsert_preuve_fichier.collectivite_id
-                and obj.name = filename)
-insert
-into preuve_fichier(collectivite_id, action_id, file_id, commentaire)
-select collectivite_id, action_id, file.id, upsert_preuve_fichier.commentaire
-from file
-on conflict (collectivite_id, action_id, file_id) do update set commentaire = upsert_preuve_fichier.commentaire;
-$$ language sql;
-comment on function upsert_preuve_fichier is 'Upsert a preuve';
+select bf.id,
+       bf.collectivite_id,
+       bf.hash,
+       bf.filename,
+       o.bucket_id                      as bucket_id,
+       o.id                             as file_id,
+       (o.metadata ->> 'size')::integer as filesize
+from labellisation.bibliotheque_fichier bf
+         join collectivite_bucket cb on cb.collectivite_id = bf.collectivite_id
+         join storage.objects o on o.name = bf.hash and o.bucket_id = cb.bucket_id;
 
-create or replace function delete_preuve_fichier(
+
+-- Vue partielle `bibliotheque_fichier` en json.
+create view labellisation.bibliotheque_fichier_snippet as
+select id,
+       jsonb_build_object(
+               'filename', filename,
+               'hash', hash,
+               'bucket_id', bucket_id,
+               'filesize', filesize) as snippet
+from public.bibliotheque_fichier;
+comment on view bibliotheque_fichier is 'Vue partielle de la table `public.bibliotheque_fichier` en json.';
+
+create function add_bibliotheque_fichier(
     collectivite_id integer,
-    action_id action_id,
+    hash varchar(64),
     filename text
 )
-    returns void
+    returns bibliotheque_fichier
 as
 $$
-with file as (select obj.id
-              from storage.objects obj
-                       join collectivite_bucket cb on obj.bucket_id = cb.bucket_id
-              where cb.collectivite_id = delete_preuve_fichier.collectivite_id
-                and obj.name = filename)
-delete
-from preuve_fichier
-where preuve_fichier.collectivite_id = delete_preuve_fichier.collectivite_id
-  and preuve_fichier.action_id = delete_preuve_fichier.action_id
-  and preuve_fichier.file_id in (select id from file);
-$$ language sql;
-comment on function delete_preuve_fichier is 'Delete a preuve';
-
-
--- Client view
-create view action_preuve_fichier
-as
-select c.id                            as collectivite_id,
-       cb.bucket_id                    as bucket_id,
-       p.action_id                     as action_id,
-       obj.name                        as filename,
-       cb.bucket_id || '/' || obj.name as path,
-       coalesce(p.commentaire, '')     as commentaire
-from collectivite c
-         join collectivite_bucket cb on c.id = cb.collectivite_id
-         join lateral (select id, name, path_tokens
-                       from storage.objects o
-                       where o.bucket_id = cb.bucket_id) as obj on true
-         left join preuve_fichier p on p.file_id = obj.id;
-
-
--- Handle file deletion.
-create or replace function remove_preuve_fichier()
-    returns trigger
-as
-$$
+declare
+    object_id    uuid;
+    inserted     integer;
+    return_value bibliotheque_fichier;
 begin
-    delete
-    from preuve_fichier
-    where file_id = old.id;
-    return old;
-end
+    if have_edition_acces(add_bibliotheque_fichier.collectivite_id)
+    then
+        select o.id
+        into object_id
+        from storage.objects o
+                 join collectivite_bucket cb on o.bucket_id = cb.bucket_id
+        where cb.collectivite_id = add_bibliotheque_fichier.collectivite_id
+          and o.name = add_bibliotheque_fichier.hash;
+
+        if object_id is null
+        then
+            perform set_config('response.status', '404', true);
+            return null;
+        end if;
+
+        insert into labellisation.bibliotheque_fichier(collectivite_id, hash, filename)
+        values (add_bibliotheque_fichier.collectivite_id,
+                add_bibliotheque_fichier.hash,
+                add_bibliotheque_fichier.filename)
+        returning id into inserted;
+
+        select *
+        from bibliotheque_fichier bf
+        where bf.id = inserted
+        into return_value;
+
+        perform set_config('response.status', '201', true);
+        return return_value;
+    else
+        perform set_config('response.status', '403', true);
+        return null;
+    end if;
+end;
 $$ language plpgsql security definer;
+comment on function add_bibliotheque_fichier is
+    'Ajoute un fichier présent dan le bucket de la collectivité à l''adresse `bucket/hash`, dans la bibliothèque de fichiers.';
 
-create trigger remove_preuve_fichier_before_file_delete
-    before delete
-    on storage.objects
-    for each row
-execute procedure remove_preuve_fichier();
 
--- create table preuve_lien
--- (
---     id              serial primary key,
---     collectivite_id integer references collectivite,
---     action_id       action_id references action_relation,
---     url             text            not null,
---     titre           text            not null,
---     commentaire     text default '' not null
--- );
--- comment on table preuve_lien is 'A preuve lien links an url to an action of a collectivité.';
---
--- create policy allow_read
---     on preuve_lien for select
---     using (is_authenticated());
---
--- create policy allow_insert
---     on preuve_lien for insert
---     with check (is_any_role_on(collectivite_id));
---
--- create policy allow_update
---     on preuve_lien for update
---     using (is_any_role_on(collectivite_id));
---
--- create policy allow_delete
---     on preuve_lien for delete
---     using (is_any_role_on(collectivite_id));
---
-
+-- La vue utilisée par le client qui regroupe tout les types de preuves.
 create or replace view preuve
 as
-select 'fichier' as type,
-       null      as id,
-       f.action_id,
-       f.collectivite_id,
-       f.commentaire,
-       f.filename,
-       f.bucket_id,
-       f.path,
-       null      as url,
-       null      as titre
-from action_preuve_fichier f
-union
-select 'lien' as type,
-       l.id,
-       l.action_id,
-       l.collectivite_id,
-       l.commentaire,
-       null   as filename,
-       null   as bucket_id,
-       null   as path,
-       l.url,
-       l.titre
-from preuve_lien l;
-comment on view preuve is
-    'List both preuve fichier and preuve lien';
+-- Les preuves complémentaires.
+select -- champs communs
+       'complementaire':: preuve_type              as preuve_type,
+       pc.id,
+       pc.collectivite_id,
+       fs.snippet                                  as fichier,
+       pc.lien,
+       pc.commentaire,
+       pc.modified_at                              as created_at,
+       pc.modified_by                              as created_by,
+       utilisateur.modified_by_nom(pc.modified_by) as created_by_nom,
+       snippet.snippet                             as action,
+       null:: jsonb                                as preuve_reglementaire,
+       null:: jsonb                                as demande,
+       null:: jsonb                                as rapport
+from preuve_complementaire pc
+         left join labellisation.bibliotheque_fichier_snippet fs
+                   on fs.id = pc.fichier_id
+         left join labellisation.action_snippet snippet
+                   on snippet.action_id = pc.action_id
+                       and snippet.collectivite_id = pc.collectivite_id
+union all
 
+-- Toutes les preuves réglementaires par collectivité, avec ou sans fichier.
+select 'reglementaire',
+       pr.id,
+       c.id,
+       fs.snippet,
+       pr.lien,
+       pr.commentaire,
+       pr.modified_at,
+       pr.modified_by,
+       utilisateur.modified_by_nom(pr.modified_by),
+       snippet.snippet,
+       (select jsonb_build_object(
+                       'id', prd.id,
+                       'nom', prd.nom,
+                       'description', prd.description)),
+       null,
+       null
+from collectivite c -- toutes les collectivités ...
+         left join preuve_reglementaire_definition prd on true -- ... x toutes les preuves réglementaires
+         left join preuve_reglementaire pr on prd.id = pr.preuve_id and c.id = pr.collectivite_id
+         left join preuve_action pa on prd.id = pa.preuve_id
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = pr.fichier_id
+         left join labellisation.action_snippet snippet
+                   on snippet.action_id = pa.action_id and snippet.collectivite_id = c.id
+union all
 
--- restore la version précédente
--- create table labellisation_preuve_fichier
--- (
---     collectivite_id integer references collectivite          not null,
---     demande_id      integer references labellisation.demande not null,
---     file_id         uuid references storage.objects          not null,
---     commentaire     text                                     not null default '',
---     primary key (collectivite_id, demande_id, file_id)
--- );
--- comment on table labellisation_preuve_fichier is
---     'Lie un fichier à une demande de labellisation d''une collectivité. '
---         'On peut lier plusieurs fichiers à une demande.';
---
--- alter table labellisation_preuve_fichier
---     enable row level security;
---
--- create policy allow_read
---     on labellisation_preuve_fichier for select
---     using (is_authenticated());
---
--- create policy allow_insert
---     on labellisation_preuve_fichier for insert
---     with check (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = labellisation_preuve_fichier.file_id)));
---
--- create policy allow_update
---     on labellisation_preuve_fichier for update
---     using (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = labellisation_preuve_fichier.file_id)));
---
--- create policy allow_delete
---     on labellisation_preuve_fichier for delete
---     using (is_bucket_writer(
---         (select o.bucket_id
---          from storage.objects o
---          where o.id = labellisation_preuve_fichier.file_id)));
+select 'labellisation',
+       p.id,
+       d.collectivite_id,
+       fs.snippet,
+       p.lien,
+       p.commentaire,
+       p.modified_at,
+       p.modified_by,
+       utilisateur.modified_by_nom(p.modified_by),
+       null,
+       null,
+       (select jsonb_build_object(
+                       'en_cours', d.en_cours,
+                       'referentiel', d.referentiel,
+                       'etoiles', d.etoiles,
+                       'date', d.date,
+                       'id', d.id
+                   )),
+       null
+from labellisation.demande d
+         join preuve_labellisation p on p.demande_id = d.id
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
 
-create function upsert_labellisation_preuve_fichier(
-    collectivite_id integer,
-    demande_id integer,
-    filename text,
-    commentaire text
-)
-    returns void
-as
-$$
-with file as (select obj.id
-              from storage.objects obj
-                       join collectivite_bucket cb on obj.bucket_id = cb.bucket_id
-              where cb.collectivite_id = upsert_labellisation_preuve_fichier.collectivite_id
-                and obj.name = filename)
-insert
-into labellisation_preuve_fichier(collectivite_id, demande_id, file_id, commentaire)
-select collectivite_id, demande_id, file.id, upsert_labellisation_preuve_fichier.commentaire
-from file
-on conflict (collectivite_id, demande_id, file_id) do update set commentaire = upsert_labellisation_preuve_fichier.commentaire;
-$$ language sql;
-comment on function upsert_labellisation_preuve_fichier is
-    'Upsert une preuve de demande de labellisation.';
-
-create function delete_labellisation_preuve_fichier(
-    collectivite_id integer,
-    demande_id integer,
-    filename text
-)
-    returns void
-as
-$$
-with file as (select obj.id
-              from storage.objects obj
-                       join collectivite_bucket cb on obj.bucket_id = cb.bucket_id
-              where cb.collectivite_id = delete_labellisation_preuve_fichier.collectivite_id
-                and obj.name = filename)
-delete
-from labellisation_preuve_fichier
-where labellisation_preuve_fichier.collectivite_id = delete_labellisation_preuve_fichier.collectivite_id
-  and labellisation_preuve_fichier.demande_id = delete_labellisation_preuve_fichier.demande_id
-  and labellisation_preuve_fichier.file_id in (select id from file);
-$$ language sql;
-comment on function delete_labellisation_preuve_fichier is
-    'Supprime une preuve de demande de labellisation.';
-
-
-create view action_labellisation_preuve_fichier
-as
-select c.id                            as collectivite_id,
-       cb.bucket_id                    as bucket_id,
-       p.demande_id                    as demande_id,
-       obj.name                        as filename,
-       cb.bucket_id || '/' || obj.name as path,
-       coalesce(p.commentaire, '')     as commentaire
-from collectivite c
-         join collectivite_bucket cb on c.id = cb.collectivite_id
-         join lateral (select id, name, path_tokens
-                       from storage.objects o
-                       where o.bucket_id = cb.bucket_id) as obj on true
-         left join labellisation_preuve_fichier p on p.file_id = obj.id;
-comment on view action_labellisation_preuve_fichier is
-    'Fichier preuve par demande de labellisation par collectivité.';
-
-
--- Handle file deletion.
-create function remove_labellisation_preuve_fichier()
-    returns trigger
-as
-$$
-begin
-    delete
-    from labellisation_preuve_fichier
-    where file_id = old.id;
-    return old;
-end
-$$ language plpgsql security definer;
-
-create trigger remove_labellisation_preuve_fichier_before_file_delete
-    before delete
-    on storage.objects
-    for each row
-execute procedure remove_labellisation_preuve_fichier();
-
-
-create or replace function
-    labellisation.critere_fichier(collectivite_id integer)
-    returns table
-            (
-                referentiel   referentiel,
-                preuve_nombre integer,
-                atteint       bool
-            )
-as
-$$
-with ref as (select unnest(enum_range(null::referentiel)) as referentiel)
-select r.referentiel,
-       count(lpf.file_id),
-       count(lpf.file_id) > 0
-from ref r
-         left join lateral (select *
-                            from labellisation.demande ld
-                            where ld.referentiel = r.referentiel
-                              and ld.collectivite_id = critere_fichier.collectivite_id) ld on true
-         left join labellisation_preuve_fichier lpf on ld.id = lpf.demande_id
-group by r.referentiel;
-$$ language sql;
-comment on function labellisation.critere_fichier is
-    'Renvoie le critère fichier preuve pour une collectivité donnée.';
+union all
+select 'rapport',
+       p.id,
+       p.collectivite_id,
+       fs.snippet,
+       p.lien,
+       p.commentaire,
+       p.modified_at,
+       p.modified_by,
+       utilisateur.modified_by_nom(p.modified_by),
+       null,
+       null,
+       null,
+       jsonb_build_object('date', p.date)
+from preuve_rapport p
+         left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
+;
+comment on view preuve is 'Regroupe tout les types de preuves.';
 
 COMMIT;
