945d205c38fb163baedfe13796d2388adc3480f5
Florian d'Erfurth <florian@derfurth.com>
2022-10-24 16:52:39 +0200

    breaking: Permet d'insérer des states directement dans la vue

--- data_layer/postgres/fakes/22-audit_state.sql
+++ data_layer/postgres/fakes/22-audit_state.sql
@@ -1,7 +1,7 @@
 insert into audit(id, collectivite_id, referentiel, auditeur)
 values (1, 1, 'eci', '5f407fc6-3634-45ff-a988-301e9088096a');
 
-insert into action_audit_state(audit_id, action_id, collectivite_id, ordre_du_jour, avis, statut, modified_by)
+insert into labellisation.action_audit_state(audit_id, action_id, collectivite_id, ordre_du_jour, avis, statut, modified_by)
 values
 (1, 'eci_2.2', 1, true, 'avis test 2.2', 'en_cours', '5f407fc6-3634-45ff-a988-301e9088096a'),
 (1, 'eci_2.3', 1, false, 'avis test 2.3', 'audite', '5f407fc6-3634-45ff-a988-301e9088096a'),
