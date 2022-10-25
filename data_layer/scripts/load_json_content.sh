5c4530eea56619e7592bc667cbaeadb2ce7b5a9c
Florian d'Erfurth <florian@derfurth.com>
2022-10-25 17:31:36 +0200

    Permet de mettre à jour les contenus du référentiel à partir d'un fichier json.

--- data_layer/scripts/load_json_content.sh
+++ data_layer/scripts/load_json_content.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-echo "Uploading json to $SUPABASE_URL/rest/v1/..."
+echo "Uploading json to endpoints at $SUPABASE_URL/rest/v1/..."
+
+echo "eci.json into referentiel_json"
+curl -X POST \
+     -H "apikey: $SERVICE_ROLE_KEY" \
+     -H "Authorization: Bearer $SERVICE_ROLE_KEY" \
+     -H "Content-Type: application/json" \
+     -d @../content/eci.json \
+"$SUPABASE_URL/rest/v1/referentiel_json"
+
+echo "cae.json into referentiel_json"
+curl -X POST \
+     -H "apikey: $SERVICE_ROLE_KEY" \
+     -H "Authorization: Bearer $SERVICE_ROLE_KEY" \
+     -H "Content-Type: application/json" \
+     -d @../content/cae.json \
+"$SUPABASE_URL/rest/v1/referentiel_json"
+
+
+echo "preuves.json into preuve_reglementaire_json"
 curl -X POST \
      -H "apikey: $SERVICE_ROLE_KEY" \
      -H "Authorization: Bearer $SERVICE_ROLE_KEY" \
