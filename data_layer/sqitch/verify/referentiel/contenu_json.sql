3a2dedf45daa23ccdcc471de2028dad722c6b666
Florian d'Erfurth <florian@derfurth.com>
2022-10-25 17:31:36 +0200

    Permet de mettre à jour les contenus du référentiel à partir d'un fichier json.

---
+++ data_layer/sqitch/verify/referentiel/contenu_json.sql
@@ -0,0 +1,12 @@
+-- Verify tet:referentiel/action_definition on pg
+
+BEGIN;
+
+select definitions, children, created_at
+from referentiel_json
+where false;
+
+select has_function_privilege('upsert_actions(jsonb, jsonb)', 'execute');
+select has_function_privilege('private.upsert_referentiel_after_json_insert()', 'execute');
+
+ROLLBACK;
