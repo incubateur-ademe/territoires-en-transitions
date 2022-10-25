db247ef57d745b5b58a57bfe18961623f97ad7b2
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-10-25 15:33:40 +0200

    Ajout d'un trigger à la suppression d'un commentaire. Correction de la projection created_by_nom.

--- data_layer/sqitch/revert/referentiel/action_discussion.sql
+++ data_layer/sqitch/revert/referentiel/action_discussion.sql
@@ -3,6 +3,7 @@
 BEGIN;
 
 drop view action_discussion_feed;
+drop function supprimer_discussion();
 drop table action_discussion_commentaire;
 drop table action_discussion;
 drop type action_discussion_statut;
