bf05f0bab08cc2080f4ed1bb2e86f0ad682ac401
Marc Rutkowski <marc@attractive-media.fr>
2022-10-26 18:18:54 +0200

    ajoute le typage et les hooks pour lire le statut d'audit du référentiel et MàJ les statuts d'audit des actions

--- app.territoiresentransitions.react/src/app/pages/collectivite/Audit/useActionAuditStatut.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/useActionAuditStatut.ts
@@ -3,55 +3,51 @@ import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId, useReferentielId} from 'core-logic/hooks/params';
 import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
 import {TActionAuditStatut} from './types';
-import {Referentiel} from 'types/litterals';
 
 export type TActionDef = Pick<
   ActionDefinitionSummary,
   'id' | 'identifiant' | 'referentiel'
 >;
 
-type TFilters = {
-  action?: TActionDef;
-};
-
 // charge les données
-export const fetch = async (
-  collectivite_id: number,
-  referentiel: Referentiel,
-  filters?: TFilters
-) => {
-  // lit la liste des statuts d'audit des actions
+export const fetch = async (collectivite_id: number, action: TActionDef) => {
+  // lit le statut d'audit d'une action
   const query = supabaseClient
-    .from<TActionAuditStatut>('action_audit_state_list')
+    .from<TActionAuditStatut>('action_audit_state')
     .select()
-    .match({collectivite_id, referentiel});
-
-  const action = filters?.action;
-  if (action) {
-    query.eq('action_id', action.id);
-  }
+    .eq('collectivite_id', collectivite_id)
+    .eq('action_id', action.id);
 
   const {data, error} = await query;
 
-  if (error || !data) {
-    return [];
+  if (error || !data?.length) {
+    return null;
   }
 
-  return data;
+  return data[0] as TActionAuditStatut;
 };
 
 /**
  * Statut d'audit d'une action du référentiel et de la collectivité courante.
+ * Renvoi un objet par défaut si le statut n'est pas trouvé.
  */
 export const useActionAuditStatut = (action: TActionDef) => {
   const collectivite_id = useCollectiviteId();
   const referentiel = useReferentielId();
-  const {data} = useQuery(
-    ['action_audit_state_list', collectivite_id, referentiel, action],
+  const defaultStatut = {
+    collectivite_id,
+    referentiel,
+    action_id: action.id,
+    ordre_du_jour: false,
+    avis: '',
+    statut: 'non_audite',
+  } as TActionAuditStatut;
+
+  return useQuery(
+    ['action_audit_state', collectivite_id, referentiel, action.id],
     () =>
       collectivite_id && referentiel
-        ? fetch(collectivite_id, referentiel as Referentiel, {action})
-        : []
+        ? fetch(collectivite_id, action).then(data => data || defaultStatut)
+        : defaultStatut
   );
-  return data?.[0] || null;
 };
