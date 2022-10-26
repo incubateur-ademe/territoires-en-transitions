4fd6e3e79bd81a4a5b96602988620312e45907dd
Marc Rutkowski <marc@attractive-media.fr>
2022-10-26 18:18:54 +0200

    ajoute le typage et les hooks pour lire le statut d'audit du référentiel et MàJ les statuts d'audit des actions

--- app.territoiresentransitions.react/src/app/pages/collectivite/Audit/types.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/types.ts
@@ -2,6 +2,17 @@ import {Referentiel} from 'types/litterals';
 
 export type TAuditStatut = 'non_audite' | 'en_cours' | 'audite';
 
+// statut de l'audit en cours
+export type TAudit = {
+  id: number;
+  collectivite_id: number;
+  referentiel: Referentiel;
+  demande_id: number | null;
+  auditeur: string;
+  date_debut: string;
+  date_fin: string | null;
+};
+
 // statut d'audit d'une action
 export type TActionAuditStatut = {
   collectivite_id: number;
@@ -10,6 +21,6 @@ export type TActionAuditStatut = {
   action_id: string;
   state_id: number;
   statut: TAuditStatut;
-  avis: string | null;
+  avis: string;
   ordre_du_jour: boolean;
 };
---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/useUpdateActionAuditStatut.ts
@@ -0,0 +1,29 @@
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useMutation, useQueryClient} from 'react-query';
+import {TActionAuditStatut} from './types';
+
+// renvoie une fonction de modification du statut d'audit d'une action
+export const useUpdateActionAuditStatut = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async (actionAuditStatut: TActionAuditStatut) => {
+      const {collectivite_id, action_id, ordre_du_jour, avis, statut} =
+        actionAuditStatut;
+      return supabaseClient
+        .from<TActionAuditStatut>('action_audit_state')
+        .insert({collectivite_id, action_id, ordre_du_jour, avis, statut});
+    },
+    {
+      mutationKey: 'update_action_audit_state',
+      onSuccess: (data: unknown, variables: TActionAuditStatut) => {
+        const {collectivite_id, referentiel} = variables;
+        queryClient.invalidateQueries([
+          'action_audit_state',
+          collectivite_id,
+          referentiel,
+        ]);
+      },
+    }
+  );
+};
