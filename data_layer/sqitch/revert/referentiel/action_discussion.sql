3e75a5ba6961f0cfb80d0f4ce7ea88ff7db7a2c1
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-10-26 09:56:49 +0200

    Correction de l'ordre de le revert

--- data_layer/sqitch/revert/referentiel/action_discussion.sql
+++ data_layer/sqitch/revert/referentiel/action_discussion.sql
@@ -3,8 +3,8 @@
 BEGIN;
 
 drop view action_discussion_feed;
-drop function supprimer_discussion();
 drop table action_discussion_commentaire;
+drop function supprimer_discussion();
 drop table action_discussion;
 drop type action_discussion_statut;
 
