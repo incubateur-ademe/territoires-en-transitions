8273ac48a49ba12e2aa96555811de653c464658e
Florian d'Erfurth <florian@derfurth.com>
2022-10-27 19:11:36 +0200

    fix: revert

--- data_layer/sqitch/revert/referentiel/action_discussion.sql
+++ data_layer/sqitch/revert/referentiel/action_discussion.sql
@@ -2,10 +2,15 @@
 
 BEGIN;
 
+drop table if exists test.action_discussion_commentaire cascade;
+drop table if exists test.action_discussion cascade;
+
 drop view action_discussion_feed;
 drop table action_discussion_commentaire;
 drop function supprimer_discussion();
 drop table action_discussion;
+drop function have_discussion_lecture_acces(integer);
+drop function have_discussion_edition_acces(integer);
 drop type action_discussion_statut;
 
 COMMIT;
