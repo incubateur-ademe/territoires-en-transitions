9ea273f8468c9ccf6d462c64540f861b19164e2f
Arthur Molinos <arthur.molinos@gmail.com>
2022-10-27 13:52:31 +0200

    Creation du composant NouvelleDiscussion avec champ de saisie

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionDiscussions/ActionDiscussionNouvelleDiscussion.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import ActionDiscussionTextarea from './ActionDiscussionTextarea';
+import {useAddDiscussionToAction} from './data/useAddDiscussionToAction';
+
+type Props = {
+  actionId: string;
+};
+
+/** Input permettant de créer une nouvelle discussion dans une action */
+const ActionDiscussionNouvelleDiscussion = ({actionId}: Props) => {
+  const [commentaire, setCommentaire] = useState<string>('');
+
+  const {mutate: handleCreateDiscussion} = useAddDiscussionToAction(actionId);
+
+  return (
+    <div
+      data-test="ActionDiscussionsNouvelleDiscussion"
+      className="py-3 px-4 bg-gray-100"
+    >
+      <ActionDiscussionTextarea
+        commentaire={commentaire}
+        onChange={setCommentaire}
+        placeholder="Écrire un nouveau commentaire..."
+      />
+
+      {commentaire.trim().length > 0 && (
+        <div className="mt-4 ml-2">
+          <button
+            className="fr-btn"
+            onClick={() => {
+              setCommentaire('');
+              handleCreateDiscussion(commentaire);
+            }}
+          >
+            Publier
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ActionDiscussionNouvelleDiscussion;
