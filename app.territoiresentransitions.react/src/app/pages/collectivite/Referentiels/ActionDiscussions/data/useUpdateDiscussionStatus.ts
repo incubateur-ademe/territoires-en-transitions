9e848a089702e964ce0240fd16bb67b48b06e2b8
Arthur Molinos <arthur.molinos@gmail.com>
2022-10-27 13:40:40 +0200

    Création des hooks et queries

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionDiscussions/data/useUpdateDiscussionStatus.ts
@@ -0,0 +1,37 @@
+import {useMutation, useQueryClient} from 'react-query';
+
+import {supabaseClient} from 'core-logic/api/supabase';
+import {TActionDiscussion, TActionDiscussionStatut} from './types';
+
+/**
+ * Update le status d'une discussions
+ */
+export const useUpdateDiscussionStatus = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(update, {
+    mutationKey: 'update_discussion_status',
+    onSuccess: () => {
+      queryClient.invalidateQueries(['action_discussion_feed']);
+    },
+  });
+};
+
+type UpdateArgs = {
+  discussion_id: number;
+  status: TActionDiscussionStatut;
+};
+
+/**
+ * update query
+ */
+const update = async (args: UpdateArgs) => {
+  const {error} = await supabaseClient
+    .from<TActionDiscussion>('action_discussion')
+    .update({status: args.status})
+    .eq('id', args.discussion_id);
+
+  if (error) throw error?.message;
+
+  return true;
+};
