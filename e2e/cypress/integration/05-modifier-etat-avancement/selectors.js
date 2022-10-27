63138be976003ca984b4bc2a0c1aab6219cc2ee2
Marc Rutkowski <marc@attractive-media.fr>
2022-10-27 18:37:22 +0200

    ajoute des scénarios e2e pour tester l'audit d'une action

---
+++ e2e/cypress/integration/05-modifier-etat-avancement/selectors.js
@@ -0,0 +1,14 @@
+export const LocalSelectors = {
+  'état audit action': {
+    selector: '[data-test=action-audit-statut]',
+  },
+  'état audit action lecture seule': {
+    selector: '[data-test=action-audit-statut-ro]',
+  },
+  'avis audit': {
+    selector: '[data-test=avis] textarea',
+  },
+  "ajouter à l'ordre du jour": {
+    selector: '#ordre_du_jour',
+  },
+};
