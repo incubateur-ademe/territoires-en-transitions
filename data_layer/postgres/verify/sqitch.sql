a37d2c51d770695c74c0b2b44fdb47d0edff906a
Florian d'Erfurth <florian@derfurth.com>
2022-10-28 13:40:06 +0200

    Met à jour la dernière feature.

--- data_layer/postgres/verify/sqitch.sql
+++ data_layer/postgres/verify/sqitch.sql
@@ -4,4 +4,4 @@ select has_function_privilege('teapot()', 'execute');
 select 1 / count(*)
 from sqitch.changes
 -- should be the latest change.
-where change = 'referentiel/action_discussion';
+where change = 'evaluation/score_service';
