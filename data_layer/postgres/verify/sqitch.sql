39b31b04e7c16c2a8e1cd92268e905570185e307
Florian d'Erfurth <florian@derfurth.com>
2022-11-04 11:40:45 +0100

    Met à jour le revert et le verify.

--- data_layer/postgres/verify/sqitch.sql
+++ data_layer/postgres/verify/sqitch.sql
@@ -4,4 +4,4 @@ select has_function_privilege('teapot()', 'execute');
 select 1 / count(*)
 from sqitch.changes
 -- should be the latest change.
-where change = 'referentiel/preuve_reglementaire';
+where change = 'referentiel/action_discussion';
