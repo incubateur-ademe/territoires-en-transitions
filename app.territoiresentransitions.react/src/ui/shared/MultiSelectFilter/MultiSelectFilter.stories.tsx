9e2c1a7d36764b01d51c48137134083038bc0597
Marc Rutkowski <marc@attractive-media.fr>
2022-11-07 10:03:36 +0100

    utilise le MultiSelectDropdown plutôt que material-ui dans les filtres/colonne des vues tabulaires

--- app.territoiresentransitions.react/src/ui/shared/MultiSelectFilter/MultiSelectFilter.stories.tsx
+++ app.territoiresentransitions.react/src/ui/shared/MultiSelectFilter/MultiSelectFilter.stories.tsx
@@ -1,6 +1,5 @@
 import {Story, Meta} from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-import {MultiSelectFilter, TMultiSelectFilterProps} from './index';
+import {MultiSelectFilter, TMultiSelectFilterProps, ITEM_ALL} from './index';
 
 export default {
   component: MultiSelectFilter,
@@ -10,14 +9,23 @@ const Template: Story<TMultiSelectFilterProps> = args => (
   <MultiSelectFilter {...args} />
 );
 
-export const Exemple1 = Template.bind({});
-Exemple1.args = {
+const items = [
+  {value: ITEM_ALL, label: 'Toutes'},
+  {value: 'opt1', label: 'Option 1'},
+  {value: 'opt2', label: 'Option 2'},
+  {value: 'opt3', label: 'Option 3'},
+];
+
+export const Toutes = Template.bind({});
+Toutes.args = {
+  label: 'Libellé',
+  values: [ITEM_ALL],
+  items,
+};
+
+export const Selection = Template.bind({});
+Selection.args = {
   label: 'Libellé',
   values: ['opt1', 'opt3'],
-  items: [
-    {value: 'opt1', label: 'Option 1'},
-    {value: 'opt2', label: 'Option 2'},
-    {value: 'opt3', label: 'Option 3'},
-  ],
+  items,
 };
-Exemple1.parameters = {storyshots: false};
