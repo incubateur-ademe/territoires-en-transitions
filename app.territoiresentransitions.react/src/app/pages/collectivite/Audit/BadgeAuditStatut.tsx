7f730694a6cc433be188b998198ca7f1d05be510
Marc Rutkowski <marc@attractive-media.fr>
2022-11-07 10:06:00 +0100

    extrait le composant badge représentant un statut d'audit d'une action
    (pour pouvoir l'utiliser aussi dans le filtre de la vue tabulaire)

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/BadgeAuditStatut.tsx
@@ -0,0 +1,23 @@
+import {Badge, TBadgeProps} from 'ui/shared/Badge';
+import {TAuditStatut} from './types';
+
+export const statutToOption: Record<
+  TAuditStatut,
+  {label: string; badge: TBadgeProps['status']}
+> = {
+  non_audite: {label: 'Non audité', badge: 'warning'},
+  en_cours: {label: 'Audit en cours', badge: 'info'},
+  audite: {label: 'Audité', badge: 'success'},
+};
+
+/**
+ * Affiche un badge représentant un statut d'audit
+ */
+export const BadgeAuditStatut = ({statut}: {statut: TAuditStatut}) => {
+  const {label, badge} = statutToOption[statut];
+  return (
+    <Badge status={badge} className="fr-badge--no-icon">
+      {label}
+    </Badge>
+  );
+};
