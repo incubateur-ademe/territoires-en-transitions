9bc2a15308a46501f6810a1216445c3b6a903e0e
Florian d'Erfurth <florian@derfurth.com>
2022-11-10 15:05:29 +0100

    Teste la personnalisation.

--- api_tests/tests/evaluation/action_statut.test.ts
+++ api_tests/tests/evaluation/action_statut.test.ts
@@ -9,6 +9,8 @@ import { signIn, signOut } from "../../lib/auth.ts";
 import { ActionScore, ClientScores } from "../../lib/types/clientScores.ts";
 import { ActionStatut } from "../../lib/types/actionStatut.ts";
 import { Avancement } from "../../lib/types/avancement.ts";
+import { testReset } from "../../lib/rpcs/testReset.ts";
+import { saveReponse } from "../../lib/rpcs/saveReponse.ts";
 
 function scoreById(clientScores: ClientScores, actionId: string): ActionScore {
   return clientScores.scores.filter((s: ActionScore) =>
@@ -16,7 +18,8 @@ function scoreById(clientScores: ClientScores, actionId: string): ActionScore {
   )[0]!;
 }
 
-Deno.test("Calcul des scores après insertion des statuts", async () => {
+Deno.test("Calcul des scores après la modification de statuts", async () => {
+  await testReset();
   await signIn("yolododo");
 
   const statut = {
@@ -67,6 +70,53 @@ Deno.test("Calcul des scores après insertion des statuts", async () => {
   assertEquals(actionScores2.point_programme, 0);
   assertNotEquals(actionScores2.point_fait, 0);
   assertEquals(actionScores2.point_fait, actionScores2.point_potentiel);
+
+  // on se déconnecte pour libérer les ressources
+  await signOut();
+});
+
+Deno.test("Calcul de 'eci_2.4.2' après la réponse à la question 'dechets_1'", async (t) => {
+  await testReset();
+  await signIn("yolododo");
+
+  const reponse = {
+    collectivite_id: 2,
+    question_id: "dechets_1",
+    reponse: false,
+  };
+  await saveReponse(reponse);
+
+  // on attend le calcul des scores
+  await delay(200);
+  let clientScores = await supabase.from<ClientScores>("client_scores")
+    .select()
+    .eq("collectivite_id", 2)
+    .eq("referentiel", "eci");
+
+  const eci242_1 = scoreById(clientScores.data![0], "eci_2.4.2");
+  assertEquals(
+    eci242_1.desactive,
+    true,
+    "Sans la compétence 'dechet_1' l'action devrait être désactivée",
+  );
+
+  // on change notre réponse
+  reponse.reponse = true;
+  await saveReponse(reponse);
+
+  // on attend de nouveau le calcul des scores
+  await delay(200);
+  clientScores = await supabase.from<ClientScores>("client_scores")
+    .select()
+    .eq("collectivite_id", 2)
+    .eq("referentiel", "eci");
+
+  const eci242_2 = scoreById(clientScores.data![0], "eci_2.4.2");
+  assertEquals(
+    eci242_2.desactive,
+    false,
+    "Avec la compétence 'dechet_1' l'action ne devrait pas être désactivée",
+  );
 
   // on se déconnecte pour libérer les ressources
   await signOut();
