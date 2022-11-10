9ee6af24020724cf0e9dcf9b34f142bf158e20cf
Florian d'Erfurth <florian@derfurth.com>
2022-11-10 15:05:29 +0100

    Teste la personnalisation.

---
+++ api_tests/lib/rpcs/saveReponse.ts
@@ -0,0 +1,12 @@
+import { supabase } from "../supabase.ts";
+import { SaveReponseParams } from "../types/saveReponseParams.ts";
+
+export async function saveReponse(params: SaveReponseParams): Promise<void> {
+  const { status } = await supabase.rpc(
+    "save_reponse",
+    params,
+  );
+  if (status !== 200) {
+    throw `La RPC 'save_reponse' devrait renvoyer un code 200. (${status} != 200)`;
+  }
+}
---
+++ api_tests/lib/types/saveReponseParams.ts
@@ -0,0 +1,5 @@
+export type SaveReponseParams = {
+  collectivite_id: number;
+  question_id: string;
+  reponse: string | boolean | number;
+};
