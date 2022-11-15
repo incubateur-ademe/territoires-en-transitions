4e19a2802fcf51ef7d743ae1cb1a6bd96d1f0830
Florian d'Erfurth <florian@derfurth.com>
2022-11-15 11:34:10 +0100

    Répare la labellisation en mettant à jour la fonction `convert_client_scores` pour tenir compte de l'absence du champ `referentiel` dans les nouveaux JSON.

--- data_layer/sqitch/verify/evaluation/score_summary@v1.10.0.sql
+++ data_layer/sqitch/verify/evaluation/score_summary@v1.10.0.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
+select *
+from private.action_score
+where false;
+
 select has_function_privilege('private.convert_client_scores(jsonb)', 'execute');
+select has_function_privilege('private.score_summary_of(private.action_score)', 'execute');
 
 ROLLBACK;
