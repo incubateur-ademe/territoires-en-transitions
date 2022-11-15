d060e66b5f5b93cb0851f7bd356fc7499f91bdfc
Florian d'Erfurth <florian@derfurth.com>
2022-11-15 11:34:10 +0100

    Répare la labellisation en mettant à jour la fonction `convert_client_scores` pour tenir compte de l'absence du champ `referentiel` dans les nouveaux JSON.

--- data_layer/sqitch/revert/evaluation/score_summary.sql
+++ data_layer/sqitch/revert/evaluation/score_summary.sql
@@ -1,9 +1,33 @@
--- Revert tet:evaluation/score_summary from pg
+-- Deploy tet:evaluation/score_summary to pg
+-- requires: evaluation/client_scores
 
 BEGIN;
 
-drop function private.score_summary_of(score private.action_score);
-drop function private.convert_client_scores(scores jsonb);
-drop table private.action_score;
+create or replace function
+    private.convert_client_scores(scores jsonb)
+    returns setof private.action_score
+as
+$$
+select (score ->> 'referentiel')::referentiel,
+       (score ->> 'action_id')::action_id,
+       (score ->> 'concerne')::boolean,
+       (score ->> 'desactive')::boolean,
+       (score ->> 'point_fait')::float,
+       (score ->> 'point_pas_fait')::float,
+       (score ->> 'point_potentiel')::float,
+       (score ->> 'point_programme')::float,
+       (score ->> 'point_referentiel')::float,
+       (score ->> 'total_taches_count')::integer,
+       (score ->> 'point_non_renseigne')::float,
+       (score ->> 'point_potentiel_perso')::float,
+       (score ->> 'completed_taches_count')::integer,
+       (score ->> 'fait_taches_avancement')::float,
+       (score ->> 'pas_fait_taches_avancement')::float,
+       (score ->> 'programme_taches_avancement')::float,
+       (score ->> 'pas_concerne_taches_avancement')::float
+from jsonb_array_elements(scores) as score
+$$ language sql;
+comment on function private.convert_client_scores is
+    'Convert json data from business to typed scores.';
 
 COMMIT;
