60e13a710289ced374e675ed4b96adaa9255da93
Arthur Molinos <arthur.molinos@gmail.com>
2022-11-15 11:50:21 +0100

    Remplace l'ancien MultiSelectFilter avec le nouveau

--- app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/FiltrePhase.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/FiltrePhase.tsx
@@ -1,4 +1,6 @@
-import {ITEM_ALL, MultiSelectFilter} from 'ui/shared/MultiSelectFilter';
+import classNames from 'classnames';
+import {ITEM_ALL} from 'ui/shared/select/commons';
+import {MultiSelectFilter} from 'ui/shared/select/MultiSelectFilter';
 import {TFiltreProps} from './filters';
 
 export const PHASE = 'phase';
@@ -14,15 +16,24 @@ export const phaseItems = [
  * Affiche le filtre par phase
  */
 export const FiltrePhase = (props: TFiltreProps) => {
-  const {className, filters, setFilters} = props;
+  const {filters, setFilters} = props;
 
   return (
     <MultiSelectFilter
-      className={`filtre-phase ${className || ''}`}
-      label="Phase"
       values={filters[PHASE]}
-      items={phaseItems}
-      onChange={newValues => setFilters({...filters, [PHASE]: newValues})}
+      options={phaseItems}
+      onSelect={newValues => setFilters({...filters, [PHASE]: newValues})}
+      renderSelection={values => (
+        <span
+          className={classNames(
+            'fr-fi--sm w-full text-center text-bf500 font-bold',
+            {'fr-fi-filter-fill': values.includes(ITEM_ALL)},
+            {'fr-fi-filter-line': !values.includes(ITEM_ALL)}
+          )}
+        >
+          &nbsp;Phase
+        </span>
+      )}
     />
   );
 };
--- app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/FiltreStatut.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/FiltreStatut.tsx
@@ -1,4 +1,6 @@
-import {MultiSelectFilter, ITEM_ALL} from 'ui/shared/MultiSelectFilter';
+import classNames from 'classnames';
+import {ITEM_ALL} from 'ui/shared/select/commons';
+import {MultiSelectFilter} from 'ui/shared/select/MultiSelectFilter';
 import {TFiltreProps} from './filters';
 import {ITEMS} from './SelectStatut';
 import './statuts.css';
@@ -10,15 +12,24 @@ const FILTER_NAME = 'statut';
  * Affiche le filtre par statuts
  */
 export const FiltreStatut = (props: TFiltreProps) => {
-  const {className, filters, setFilters} = props;
+  const {filters, setFilters} = props;
 
   return (
     <MultiSelectFilter
-      className={`filtre-statut ${className || ''}`}
-      label="Statut"
       values={filters[FILTER_NAME]}
-      items={items}
-      onChange={values => setFilters({...filters, [FILTER_NAME]: values})}
+      options={items}
+      onSelect={values => setFilters({...filters, [FILTER_NAME]: values})}
+      renderSelection={values => (
+        <span
+          className={classNames(
+            'fr-fi--sm w-full text-center text-bf500 font-bold',
+            {'fr-fi-filter-fill': values.includes(ITEM_ALL)},
+            {'fr-fi-filter-line': !values.includes(ITEM_ALL)}
+          )}
+        >
+          &nbsp;Statut
+        </span>
+      )}
     />
   );
 };
