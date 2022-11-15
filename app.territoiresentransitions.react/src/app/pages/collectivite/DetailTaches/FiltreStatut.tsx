ae040e2c0c1a0787b25aa59ae20853362c694407
Arthur Molinos <arthur.molinos@gmail.com>
2022-11-15 15:10:55 +0100

    Remplace ancien SelectDropdown par le nouveau dans des composants

--- app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/FiltreStatut.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/DetailTaches/FiltreStatut.tsx
@@ -1,9 +1,12 @@
 import classNames from 'classnames';
-import {ITEM_ALL} from 'ui/shared/select/commons';
+
 import {MultiSelectFilter} from 'ui/shared/select/MultiSelectFilter';
+import ActionStatutBadge from 'ui/shared/actions/ActionStatutBadge';
+
+import {ITEM_ALL} from 'ui/shared/select/commons';
 import {TFiltreProps} from './filters';
 import {ITEMS} from './SelectStatut';
-import './statuts.css';
+import {ActionAvancement} from 'generated/dataLayer/action_statut_read';
 
 const items = [{value: ITEM_ALL, label: 'Tous les statuts'}, ...ITEMS];
 const FILTER_NAME = 'statut';
@@ -30,6 +33,13 @@ export const FiltreStatut = (props: TFiltreProps) => {
           &nbsp;Statut
         </span>
       )}
+      renderOption={option =>
+        option === ITEM_ALL ? (
+          <span className="leading-6">Tous les statuts</span>
+        ) : (
+          <ActionStatutBadge statut={option as ActionAvancement} small />
+        )
+      }
     />
   );
 };
