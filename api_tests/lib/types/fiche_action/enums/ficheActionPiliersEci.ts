f7c023485e1e71dd1b4890ce5f9877cca27f158f
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-11-16 16:26:47 +0100

    Ajout nouvelles tables fiche_action et plan_action

---
+++ api_tests/lib/types/fiche_action/enums/ficheActionPiliersEci.ts
@@ -0,0 +1,8 @@
+export type FicheActionPiliersEci =
+    "Approvisionnement durable"|
+    "Écoconception"|
+    "Écologie industrielle (et territoriale)"|
+    "Économie de la fonctionnalité"|
+    "Consommation responsable"|
+    "Allongement de la durée d’usage"|
+    "Recyclage";
