f6b9bdbd4a02f714292648279293dac58b9dd56c
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-11-25 15:03:20 +0100

    Modification fonction est_auditeur

--- data_layer/sqitch/deploy/labellisation/audit.sql
+++ data_layer/sqitch/deploy/labellisation/audit.sql
@@ -109,4 +109,44 @@ insert into audit_auditeur (select id as audit_id, auditeur
                             from migration.audit
                             where auditeur is not null) ;
 
+
+create or replace function est_auditeur(col integer) returns boolean as
+$$
+with
+    ref as (select unnest(enum_range(null::referentiel)) as referentiel),
+    audit_en_cours as (
+        select aa.auditeur
+        from ref
+                 left join labellisation.current_audit(est_auditeur.col,ref.referentiel) a on true
+                 left join audit_auditeur aa on a.id = aa.audit_id
+    )
+select coalesce(bool_or(auth.uid()=audit_en_cours.auditeur), false) from audit_en_cours;
+$$ language sql;
+comment on function est_auditeur is
+    'Dit si l''utilisateur courant est auditeur sur une collectivité
+     Paramètres :
+        col             collectivite.id';
+
+create or replace view collectivite_niveau_acces
+as
+with current_droits as (select *
+                        from private_utilisateur_droit
+                        where user_id = auth.uid()
+                          and active)
+select named_collectivite.collectivite_id,
+       named_collectivite.nom,
+       niveau_acces,
+       est_auditeur(named_collectivite.collectivite_id) as est_auditeur
+from named_collectivite
+         left join current_droits on named_collectivite.collectivite_id = current_droits.collectivite_id
+order by unaccent(nom);
+
+
+create or replace view mes_collectivites
+as
+select *
+from collectivite_niveau_acces
+where niveau_acces is not null
+order by unaccent(nom);
+
 COMMIT;
--- data_layer/sqitch/verify/labellisation/audit.sql
+++ data_layer/sqitch/verify/labellisation/audit.sql
@@ -14,4 +14,15 @@ from labellisation.action_audit_state
 where false;
 
 select has_function_privilege('labellisation.current_audit(integer, referentiel)', 'execute');
+
+select collectivite_id, nom, niveau_acces, est_auditeur
+from collectivite_niveau_acces
+where false;
+
+select collectivite_id, nom, niveau_acces, est_auditeur
+from mes_collectivites
+where false;
+
+select has_function_privilege('est_auditeur(integer)', 'execute');
+
 ROLLBACK;
