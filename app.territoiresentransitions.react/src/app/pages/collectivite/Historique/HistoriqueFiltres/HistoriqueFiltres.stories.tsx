1fd701e034e36e5ea45d63a9fd9bbf13f0ae6611
Arthur Molinos <arthur.molinos@gmail.com>
2022-11-26 15:29:11 +0100

    Mise à jour de la story HistoriqueFiltres

--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueFiltres/HistoriqueFiltres.stories.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueFiltres/HistoriqueFiltres.stories.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import {Story, Meta} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 
-import HistoriqueFiltres, {
-  HistoriqueFiltresProps,
-  TSetFilters,
-} from './HistoriqueFiltres';
+import HistoriqueFiltres, {HistoriqueFiltresProps} from './HistoriqueFiltres';
+import {TSetFilters} from '../filters';
 
 export default {
   component: HistoriqueFiltres,
@@ -20,5 +18,26 @@ const handlers = {
 };
 
 export const Defaut = Template.bind({});
-const filtresArgs = {...handlers};
+const filtresArgs = {
+  itemsNumber: 0,
+  initialFilters: {collectivite_id: 1},
+  filters: {
+    collectivite_id: 1,
+  },
+  ...handlers,
+};
 Defaut.args = filtresArgs;
+
+export const AvecFiltres = Template.bind({});
+const avecFiltresArgs = {
+  itemsNumber: 4,
+  initialFilters: {collectivite_id: 1},
+  filters: {
+    collectivite_id: 1,
+    types: ['action_statut'],
+    startDate: '2022-11-03',
+    endDate: '2022-11-24',
+  },
+  ...handlers,
+};
+AvecFiltres.args = avecFiltresArgs;
