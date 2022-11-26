8f48499b3ab6c2ed73422e161a9ca6b69f343e32
Arthur Molinos <arthur.molinos@gmail.com>
2022-11-26 15:06:09 +0100

    Mise à jour de la query/hook pour accueillir les filtres

--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/filters.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/filters.tsx
@@ -1,24 +1,50 @@
+import {ITEM_ALL} from 'ui/shared/select/commons';
+import {HistoriqueType} from './types';
+
 export const NB_ITEMS_PER_PAGE = 10;
+
+export type TFilterType = HistoriqueType | 'tous';
+
+export const filtresTypeOptions: {value: TFilterType; label: string}[] = [
+  {value: ITEM_ALL, label: 'Tous'},
+  {value: 'action_statut', label: 'Action : statut'},
+  {value: 'action_precision', label: 'Action : texte'},
+  // {value: 'reponse', label: 'Caractéristique de la collectivité'},
+  // {value: 'preuve', label: 'Documents et preuves'},
+  // {value: 'membre', label: 'Membre'},
+  // {value: 'indicateur', label: 'Indicateur'},
+  // {value: 'plan_action_arborescence', label: "Plan d'action: arborescence"},
+  // {value: 'plan_action_fiche', label: "Plan d'action: arborescence"},
+];
 
 export type TFilters = {
   /** filtre par collectivité */
-  collectivite_id: number;
+  collectivite_id?: number;
   /** par action */
   action_id?: string;
   /** par membres de la collectivité */
-  members?: [string];
+  modified_by?: string[];
+  /** Par type d'historique */
+  types?: TFilterType[];
   /** par plage de dates */
-  startDate?: Date;
-  endDate?: Date;
+  startDate?: string;
+  endDate?: string;
   /** index de la page voulue */
   page?: number;
 };
 
-export type TSetFilters = (newFilter: TFilters | null) => void;
+export type TSetFilters = (newFilter: TFilters) => void;
+
+export type TInitialFilters = {collectivite_id: number; action_id?: string};
+
+export type TFiltreProps = {
+  filters: TFilters;
+  setFilters: TSetFilters;
+};
 
 export const nameToShortNames = {
-  members: 'm',
-  type: 't',
+  modified_by: 'm',
+  types: 't',
   startDate: 's',
   endDate: 'e',
   page: 'p',
