012f4b36f05e8f08fc08319436f82001a3658367
Florian d'Erfurth <florian@derfurth.com>
2022-11-28 14:57:11 +0100

    Rework les stats des collectivités.

--- data_layer/sqitch/deploy/stats/collectivite.sql
+++ data_layer/sqitch/deploy/stats/collectivite.sql
@@ -4,12 +4,61 @@
 
 BEGIN;
 
-create or replace view stats_real_collectivites
-as
-select *
-from active_collectivite ac
-where collectivite_id not in (select collectivite_id from collectivite_test);
+-- on `drop` avec `if exist` car la prod n'est pas synchro avec sandbox,
+-- ces fonctionnalités pré-datant l'utilisation de Sqitch.
+drop materialized view if exists stats_unique_active_collectivite;
+drop view if exists stats_unique_active_collectivite;
+drop materialized view if exists stats_real_collectivites;
+drop view if exists stats_real_collectivites;
+drop materialized view if exists stats_rattachements;
+drop view if exists stats_rattachements;
 
-alter view stats_real_collectivites rename to stats_active_real_collectivites;
+create or replace view stats.collectivite_active
+as
+select collectivite_id
+from collectivite
+         join private_utilisateur_droit
+              on collectivite.id = private_utilisateur_droit.collectivite_id
+where private_utilisateur_droit.active
+  and collectivite_id not in (select collectivite_id from collectivite_test)
+group by collectivite_id;
+comment on view stats.collectivite_active
+    is 'Les collectivités actives, soit les collectivités avec au moins un utilisateur rattaché.';
+
+create materialized view stats_unique_active_collectivite
+as
+with unique_collectivite_droit as (select d.collectivite_id,
+                                          min(created_at) as created_at
+                                   from private_utilisateur_droit d
+                                            join stats.collectivite_active c on d.collectivite_id = c.collectivite_id
+                                   group by d.collectivite_id),
+     daily as (select created_at::date  as day,
+                      count(created_at) as count
+               from unique_collectivite_droit d
+               group by day)
+select day                                                                                      as date,
+       count,
+       sum(count) over (order by day rows between unbounded preceding and current row)::integer as cumulated_count
+from daily;
+comment on materialized view stats_unique_active_collectivite
+    is 'L''évolution des activations des collectivités, soit l''évolution des rattachements initiaux.';
+
+create materialized view stats_rattachements
+as
+with daily as (select created_at::date  as day,
+                      count(created_at) as count
+               from private_utilisateur_droit d
+                        join stats.collectivite_active c on d.collectivite_id = c.collectivite_id
+               where active
+               group by day)
+select day                                                                                      as date,
+       count,
+       sum(count) over (order by day rows between unbounded preceding and current row)::integer as cumulated_count
+from daily;
+comment on materialized view stats_rattachements
+    is 'L''évolution des rattachements des utilisateurs aux collectivités, soit l''évolution de la création des droits.';
+
+refresh materialized view stats_unique_active_collectivite;
+refresh materialized view stats_rattachements;
 
 COMMIT;
--- data_layer/sqitch/verify/stats/collectivite.sql
+++ data_layer/sqitch/verify/stats/collectivite.sql
@@ -2,8 +2,16 @@
 
 BEGIN;
 
-select collectivite_id, nom
-from stats_active_real_collectivites
+select collectivite_id
+from stats.collectivite_active
+where false;
+
+select date, count, cumulated_count
+from stats_unique_active_collectivite
+where false;
+
+select date, count, cumulated_count
+from stats_rattachements
 where false;
 
 ROLLBACK;
