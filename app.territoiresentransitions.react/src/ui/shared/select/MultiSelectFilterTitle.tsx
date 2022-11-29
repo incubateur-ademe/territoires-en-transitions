bc8f7e007354bccd0f59851d1cbb2b3d5ebe4b59
Marc Rutkowski <marc@attractive-media.fr>
2022-11-29 16:04:35 +0100

    rétabli le bon picto dans les titres des multi-select, suivant que l'option "tous" est sélectionnée ou non

--- app.territoiresentransitions.react/src/ui/shared/select/MultiSelectFilterTitle.tsx
+++ app.territoiresentransitions.react/src/ui/shared/select/MultiSelectFilterTitle.tsx
@@ -15,8 +15,8 @@ export const MultiSelectFilterTitle = (props: TMultiSelectFilterTitleProps) => {
     <span
       className={classNames(
         'fr-fi--sm w-full text-center text-bf500 font-bold',
-        {'fr-fi-filter-fill': values.includes(ITEM_ALL)},
-        {'fr-fi-filter-line': !values.includes(ITEM_ALL)}
+        {'fr-fi-filter-fill': !values.includes(ITEM_ALL)},
+        {'fr-fi-filter-line': values.includes(ITEM_ALL)}
       )}
     >
       &nbsp;{label}
