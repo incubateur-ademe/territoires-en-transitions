b87b32417a95c941e843f0790db9337ec9910e35
Florian d'Erfurth <florian@derfurth.com>
2022-12-01 14:30:47 +0100

    Triggers scores pre-audit.

--- data_layer/sqitch/revert/labellisation/comparaison_audit.sql
+++ data_layer/sqitch/revert/labellisation/comparaison_audit.sql
@@ -2,6 +2,13 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+drop trigger after_write_update_audit_scores on audit;
+drop trigger after_write_update_audit_scores on personnalisation_consequence;
+drop function labellisation.update_audit_score_on_personnalisation;
+drop function labellisation.update_audit_scores;
+drop function labellisation.evaluate_audit_statuts;
+drop function labellisation.audit_evaluation_payload;
+drop function labellisation.pre_audit_service_statuts;
+drop table pre_audit_scores;
 
 COMMIT;
