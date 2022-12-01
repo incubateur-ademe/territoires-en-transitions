10354775cc38d93bf8644d1b036f1ccf35d408ea
Florian d'Erfurth <florian@derfurth.com>
2022-12-01 09:44:23 +0100

    Permet de reconstituer les statuts d'une collectivité pour un moment donné.

--- data_layer/sqitch/deploy/referentiel/action_statut_historique.sql
+++ data_layer/sqitch/deploy/referentiel/action_statut_historique.sql
@@ -3,72 +3,41 @@
 
 BEGIN;
 
-create table historique.action_statut
-(
-    id                           serial primary key,
-    collectivite_id              integer,
-    action_id                    action_id,
-    avancement                   avancement,
-    previous_avancement          avancement,
-    avancement_detaille          numeric[],
-    previous_avancement_detaille numeric[],
-    concerne                     boolean,
-    previous_concerne            boolean,
-    modified_by                  uuid,
-    previous_modified_by         uuid,
-    modified_at                  timestamp with time zone,
-    previous_modified_at         timestamp with time zone
-);
-
-alter table historique.action_statut
-    enable row level security;
-
-
-create function historique.save_action_statut() returns trigger
-as
-$$
-declare
-    updated integer;
+create function historique.action_statuts_at(
+    collectivite_id int,
+    referentiel referentiel,
+    "time" timestamp with time zone
+)
+    returns setof historique.action_statut
 begin
-    update historique.action_statut
-    set avancement                   = new.avancement,
-        avancement_detaille          = new.avancement_detaille,
-        modified_at                  = new.modified_at
-    where id in (select id
-                 from historique.action_statut
-                 where collectivite_id = new.collectivite_id
-                   and action_id = new.action_id
-                   and modified_by = new.modified_by
-                   and modified_at > new.modified_at - interval '1 hour'
-                 order by modified_by desc
-                 limit 1)
-    returning id into updated;
-
-    if updated is null
-    then
-        insert into historique.action_statut
-        values (default,
-                new.collectivite_id,
-                new.action_id,
-                new.avancement,
-                old.avancement,
-                new.avancement_detaille,
-                old.avancement_detaille,
-                new.concerne,
-                old.concerne,
-                auth.uid(),
-                old.modified_by,
-                new.modified_at,
-                old.modified_at);
-    end if;
-    return new;
-end ;
-$$ language plpgsql security definer;
-
-create trigger save_history
-    after insert or update
-    on action_statut
-    for each row
-execute procedure historique.save_action_statut();
+    atomic
+    with latest as (select max(modified_at) as max, action_id
+                    from historique.action_statut s
+                             join action_relation ar on s.action_id = ar.id
+                    where s.collectivite_id = action_statuts_at.collectivite_id
+                      and ar.referentiel = action_statuts_at.referentiel
+                      and s.modified_at <= action_statuts_at.time
+                    group by action_id)
+    select id,
+           s.collectivite_id,
+           s.action_id,
+           avancement,
+           previous_avancement,
+           avancement_detaille,
+           previous_avancement_detaille,
+           concerne,
+           previous_concerne,
+           modified_by,
+           previous_modified_by,
+           modified_at,
+           previous_modified_at
+    from historique.action_statut s
+             join latest l
+                  on s.action_id = l.action_id
+                      and s.modified_at = l.max
+    where s.collectivite_id = action_statuts_at.collectivite_id;
+end;
+comment on function historique.action_statuts_at
+    is 'Les statuts d''une collectivité à un moment donné.';
 
 COMMIT;
--- data_layer/sqitch/verify/referentiel/action_statut_historique.sql
+++ data_layer/sqitch/verify/referentiel/action_statut_historique.sql
@@ -2,24 +2,6 @@
 
 BEGIN;
 
-select collectivite_id,
-       action_id,
-       avancement,
-       previous_avancement,
-       avancement_detaille,
-       previous_avancement_detaille,
-       concerne,
-       previous_concerne,
-       modified_by,
-       previous_modified_by,
-       modified_at,
-       previous_modified_at
-from historique.action_statut
-where false;
-
-comment on trigger save_history on action_statut is null;
-
-select has_function_privilege('historique.save_action_statut()', 'execute');
-
+select has_function_privilege('historique.action_statut_at(int, timestamp with time zone)', 'execute');
 
 ROLLBACK;
