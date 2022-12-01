ac7e340a7c20031e8067365acaa0f24e0819fa71
Arthur Molinos <arthur.molinos@gmail.com>
2022-12-01 11:15:34 +0100

    N'affiche plus les membres non activés dans le filtre "Membre" de l'historique

--- app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueFiltres/FiltreMembre.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Historique/HistoriqueFiltres/FiltreMembre.tsx
@@ -8,13 +8,21 @@ import {TFiltreProps} from '../filters';
 const FiltreMembre = ({filters, setFilters}: TFiltreProps) => {
   const {membres} = useCollectiviteMembres();
 
-  const collectiviteMemberList = [
-    {value: ITEM_ALL, label: 'Tous'},
-    ...membres.map(m => ({
-      value: m.user_id!,
-      label: `${m.prenom} ${m.nom}`,
-    })),
-  ];
+  // Tableau d'options pour le multi-select
+  const memberList: {value: string; label: string}[] = [];
+  // Ajoute l'option "Tous" s'il y a plus d'une option
+  if (membres.length > 1) {
+    memberList.push({value: ITEM_ALL, label: 'Tous'});
+  }
+  // Transformation des données membres en options pour le multi-select
+  membres
+    .filter(m => m.user_id)
+    .map(m =>
+      memberList.push({
+        value: m.user_id!,
+        label: `${m.prenom} ${m.nom}`,
+      })
+    );
 
   return (
     <HistoriqueFiltreField title="Membre de la collectivité">
@@ -25,7 +33,7 @@ const FiltreMembre = ({filters, setFilters}: TFiltreProps) => {
             ? undefined
             : filters.modified_by
         }
-        options={collectiviteMemberList}
+        options={memberList}
         onSelect={newValues =>
           setFilters({
             ...filters,
