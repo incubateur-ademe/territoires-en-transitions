8a0a523c8454fff4285f4f1a46aa241fd6132e32
Florian d'Erfurth <florian@derfurth.com>
2022-12-01 20:16:40 +0100

    Refacto de la vue tabulaire.

--- data_layer/sqitch/deploy/referentiel/vue_tabulaire@v1.15.0.sql
+++ data_layer/sqitch/deploy/referentiel/vue_tabulaire@v1.15.0.sql
@@ -5,10 +5,87 @@
 
 BEGIN;
 
-alter view private.action_hierarchy set schema public;
-comment on view public.action_hierarchy is 'Vue dynamique qui calcule la hiérarchie des actions.';
+create view action_hierarchy
+as
+with recursive
+    actions_from_parents as
+        -- +---------+-------------------+-----+
+        -- |id       |parents            |depth|
+        -- +---------+-------------------+-----+
+        -- |eci_2.2.0|{eci,eci_2,eci_2.2}|3    |
+        -- |eci_2.2.1|{eci,eci_2,eci_2.2}|3    |
+        (
+            -- Actions with no parent, our starting point
+            select id, referentiel, '{}'::action_id[] as parents, 0 as depth
+            from action_relation
+            where action_relation.parent is null
 
-create or replace view action_statuts
+            union all
+
+            -- Recursively find sub-actions and append them to the result-set
+            select c.id, c.referentiel, parents || c.parent, depth + 1
+            from actions_from_parents p
+                     join action_relation c
+                          on c.parent = p.id
+            where not c.id = any (parents)),
+    parent_descendants as
+        -- +-------+---------------------------------------------+
+        -- |p      |descendants                                  |
+        -- +-------+---------------------------------------------+
+        -- |cae    |{cae_2,cae_6,cae_3,cae_1,... cae_2.1.2.6,...}|
+        -- |cae_1  |{cae_1.1,cae_1.2,cae_1.3,...,cae_1.2.4,...}  |
+        -- |cae_1.1|{cae_1.1.2,cae_1.1.3,...,cae_1.1.3.3...}     |
+        (select ar.id, array_agg(afp.id) as descendants
+         from action_relation ar
+                  join actions_from_parents afp on ar.id = any (afp.parents)
+         group by ar.id),
+    parent_leaves as
+        -- +-------+---------------------------------------------+
+        -- |p      |descendants                                  |
+        -- +-------+---------------------------------------------+
+        -- |cae    |{cae_2,cae_6,cae_3,cae_1,... cae_2.1.2.6,...}|
+        -- |cae_1  |{cae_1.1,cae_1.2,cae_1.3,...,cae_1.2.4,...}  |
+        -- |cae_1.1|{cae_1.1.2,cae_1.1.3,...,cae_1.1.3.3...}     |
+        (select ar.id,
+                array_agg(afp.id) filter ( where d is null ) as leaves
+         from action_relation ar
+                  join actions_from_parents afp on ar.id = any (afp.parents)
+                  left join parent_descendants d on d.id = afp.id
+         group by ar.id)
+-- +---+-------------------------------------+-----+
+-- |id |children                             |depth|
+-- +---+-------------------------------------+-----+
+-- |cae|{cae_4,cae_5,cae_2,cae_6,cae_1,cae_3}|0    |
+-- |eci|{eci_2,eci_3,eci_4,eci_1,eci_5}      |0    |
+select actions_from_parents.id                                     as action_id,
+       actions_from_parents.referentiel                            as referentiel,
+       coalesce(parent_descendants.descendants, '{}'::action_id[]) as descendants,
+       coalesce(parent_leaves.leaves, '{}'::action_id[])           as leaves,
+       parent_descendants.descendants is not null                  as have_children,
+       actions_from_parents.parents                                as ascendants,
+       actions_from_parents.depth                                  as depth,
+       coalesce(
+               case
+                   when actions_from_parents.referentiel = 'cae'
+                       then ('{"axe", "sous-axe", "action", "sous-action", "tache"}'::action_type[])[actions_from_parents.depth]
+                   else ('{"axe", "action", "sous-action", "tache"}'::action_type[])[actions_from_parents.depth]
+                   end
+           , 'referentiel')                                        as type
+from actions_from_parents
+         left join parent_descendants on actions_from_parents.id = parent_descendants.id
+         left join parent_leaves on parent_leaves.id = parent_descendants.id
+order by naturalsort(actions_from_parents.id);
+
+
+create view private.action_scores
+as
+select cs.collectivite_id,
+       unpacked.*
+from client_scores cs
+         join lateral (select * from private.convert_client_scores(cs.scores)) unpacked on true;
+
+
+create view action_statuts
 as
 select
     -- client will filter on:
@@ -68,7 +145,7 @@ select
 from collectivite c
          -- definitions
          left join action_definition d on true
-         join public.action_hierarchy h on d.action_id = h.action_id
+         join action_hierarchy h on d.action_id = h.action_id
     -- collectivité data
          left join action_statut s on c.id = s.collectivite_id and s.action_id = d.action_id
          left join private.action_scores sc on c.id = sc.collectivite_id and sc.action_id = d.action_id
@@ -101,9 +178,5 @@ where sc is null
    or (sc.concerne and not sc.desactive)
 order by c.id,
          naturalsort(d.identifiant);
-
-drop materialized view action_referentiel;
-drop materialized view private.action_node;
-drop function private.to_tabular_score;
 
 COMMIT;
