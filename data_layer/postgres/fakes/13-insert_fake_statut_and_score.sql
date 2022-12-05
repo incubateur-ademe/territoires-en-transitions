65954656be1ef269a879b3dca78295656b37c566
Florian d'Erfurth <florian@derfurth.com>
2022-12-05 15:56:52 +0100

    Fonctions de comparaison

--- data_layer/postgres/fakes/13-insert_fake_statut_and_score.sql
+++ data_layer/postgres/fakes/13-insert_fake_statut_and_score.sql
@@ -3,9 +3,9 @@
 -- - 11-insert_fake_epcis.sql
 -- - 10-insert_fake_user.sql
 insert into
-    action_statut(collectivite_id, action_id, avancement, concerne, modified_by)
-    values (1, 'cae_1.1.1.1.1', 'fait', true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
+    action_statut(collectivite_id, action_id, avancement, concerne, modified_by, modified_at)
+    values (1, 'cae_1.1.1.1.1', 'fait', true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9', now() - interval '1 month');
 
 insert into
-    action_statut(collectivite_id, action_id, avancement, avancement_detaille, concerne, modified_by)
-    values (1, 'cae_1.1.1.1.2', 'detaille', '{ 0.2, 0.7, 0.1}' , true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
+    action_statut(collectivite_id, action_id, avancement, avancement_detaille, concerne, modified_by, modified_at)
+    values (1, 'cae_1.1.1.1.2', 'detaille', '{ 0.2, 0.7, 0.1}' , true, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9',now() - interval '1 month');
