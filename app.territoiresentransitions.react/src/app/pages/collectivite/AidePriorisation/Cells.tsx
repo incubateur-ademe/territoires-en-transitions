dc8f1b7ea30a4ed5612e3d5789a2992508635f9c
Marc Rutkowski <marc@attractive-media.fr>
2022-12-07 14:21:15 +0100

    ajoute un état optionnnel "modified" aux cellules de pourcentage et points

--- app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/Cells.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/AidePriorisation/Cells.tsx
@@ -1,35 +1,57 @@
 import {CellProps} from 'react-table';
 import {getMaxDepth, PriorisationRow} from './queries';
 import {toLocaleFixed} from 'utils/toFixed';
+import classNames from 'classnames';
 
-type TCellProps = CellProps<PriorisationRow> & {referentiel: string | null};
+type TCellProps = CellProps<PriorisationRow> & {
+  referentiel: string | null;
+  modified?: boolean;
+};
+type TCellValueProps = Pick<TCellProps, 'value' | 'modified'>;
 
 const isNullable = (value: number | undefined | null): boolean =>
   !value || isNaN(value);
 
 /**
+ * Affiche une cellule contenant une valeur
+ */
+const CellValue = (props: Omit<TCellValueProps, 'value'> & {children: any}) => {
+  const {children, modified} = props;
+
+  return (
+    <span
+      className={classNames('cell-value inline-block w-full text-right', {
+        modified,
+      })}
+    >
+      {children}
+    </span>
+  );
+};
+
+/**
  * Affiche une cellule contenant un pourcentage
  */
-export const CellPercent = (props: TCellProps) => {
+export const CellPercent = (props: TCellValueProps) => {
   const {value} = props;
 
   return (
-    <span className="inline-block w-full text-right">
+    <CellValue {...props}>
       {isNullable(value) ? 0 : toLocaleFixed(value * 100)} %
-    </span>
+    </CellValue>
   );
 };
 
 /**
  * Affiche une cellule contenant un nombre de points
  */
-export const CellPoints = (props: TCellProps) => {
+export const CellPoints = (props: TCellValueProps) => {
   const {value} = props;
 
   return (
-    <span className="inline-block w-full text-right">
+    <CellValue {...props}>
       {isNullable(value) ? 0 : toLocaleFixed(value)}
-    </span>
+    </CellValue>
   );
 };
 
