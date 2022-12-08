beb7c84e20777277f156b1b0756b4b6562f63a10
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-12-08 10:16:07 +0100

    Ajout des indicateurs dans la fiche action

--- api_tests/lib/types/fiche_action/enums/ficheActionPiliersEci.ts
+++
@@ -1,8 +0,0 @@
-export type FicheActionPiliersEci =
-    "Approvisionnement durable"|
-    "Écoconception"|
-    "Écologie industrielle (et territoriale)"|
-    "Économie de la fonctionnalité"|
-    "Consommation responsable"|
-    "Allongement de la durée d’usage"|
-    "Recyclage";
