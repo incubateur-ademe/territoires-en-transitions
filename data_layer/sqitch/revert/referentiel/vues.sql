c0f86a1b96acba0ee39e44796353d790ce67d4be
Florian d'Erfurth <florian@derfurth.com>
2022-12-08 11:02:51 +0100

    Répare le revert.

--- data_layer/sqitch/revert/referentiel/vues.sql
+++ data_layer/sqitch/revert/referentiel/vues.sql
@@ -4,7 +4,12 @@
 
 BEGIN;
 
-create or replace view action_definition_summary
+drop function action_down_to_tache;
+drop function referentiel_down_to_action;
+drop view action_title;
+drop view action_definition_summary;
+
+create view action_definition_summary
 as
 select id,
        action_definition.referentiel,
@@ -32,5 +37,66 @@ from action_definition
 order by naturalsort(action_id);
 comment on view action_definition_summary is
     'The minimum information to display an action';
+
+create function action_down_to_tache(
+    referentiel referentiel,
+    identifiant text
+)
+    returns setof action_definition_summary as
+$$
+declare
+    referentiel_action_depth integer;
+begin
+    if action_down_to_tache.referentiel = 'cae'
+    then
+        select 3 into referentiel_action_depth;
+    else
+        select 2 into referentiel_action_depth;
+    end if;
+    return query
+        select *
+        from action_definition_summary
+        where action_definition_summary.referentiel = action_down_to_tache.referentiel
+          and action_definition_summary.identifiant like action_down_to_tache.identifiant || '%'
+          and action_definition_summary.depth >= referentiel_action_depth - 1;
+end
+$$ language plpgsql;
+comment on function action_down_to_tache is 'Returns referentiel action summary down to the tache level';
+
+
+create function referentiel_down_to_action(
+    referentiel referentiel
+)
+    returns setof action_definition_summary as
+$$
+declare
+    referentiel_action_depth integer;
+begin
+    if referentiel_down_to_action.referentiel = 'cae'
+    then
+        select 3 into referentiel_action_depth;
+    else
+        select 2 into referentiel_action_depth;
+    end if;
+    return query
+        select *
+        from action_definition_summary
+        where action_definition_summary.referentiel = referentiel_down_to_action.referentiel
+          and action_definition_summary.depth <= referentiel_action_depth;
+end;
+$$ language plpgsql;
+comment on function referentiel_down_to_action is 'Returns referentiel action summary down to the action level';
+
+create view action_title
+as
+select id,
+       referentiel,
+       children,
+       type,
+       identifiant,
+       nom
+from action_definition_summary;
+comment on view action_title is
+    'Titles only';
 
 COMMIT;
