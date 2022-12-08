8e7c65e0d4cb9bd005c3f9feafb0b699a499309f
Marc Rutkowski <marc@attractive-media.fr>
2022-12-08 10:13:20 +0100

    ajoute l'export des données de comparaison avant/après audit

--- app.territoiresentransitions.react/src/app/pages/collectivite/AuditComparaison/index.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/AuditComparaison/index.tsx
@@ -1,10 +1,34 @@
 import {useTableData} from './useTableData';
 import {AuditComparaisonTable} from './AuditComparaisonTable';
+import {useReferentielId} from 'core-logic/hooks/params';
+import {useDownloadComparaisonScoreAuditAsCSV} from './useDownloadComparaisonScoreAuditAsCSV';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
 
 const AuditComparaison = () => {
   const tableData = useTableData();
 
-  return <AuditComparaisonTable tableData={tableData} />;
+  const referentiel = useReferentielId();
+  const collectivite = useCurrentCollectivite();
+  const {download, disabled} = useDownloadComparaisonScoreAuditAsCSV(
+    collectivite,
+    referentiel
+  );
+
+  return (
+    <>
+      <AuditComparaisonTable tableData={tableData} />
+      <button
+        data-test="export-audit-comp"
+        className="fr-btn fr-btn--icon-left fr-fi-download-line"
+        disabled={disabled}
+        onClick={() => {
+          download();
+        }}
+      >
+        Exporter
+      </button>
+    </>
+  );
 };
 
 export default AuditComparaison;
