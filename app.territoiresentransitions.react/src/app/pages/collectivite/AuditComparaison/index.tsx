bbe5bf1e3f4657e0ed05123dd191a725e48cb825
Marc Rutkowski <marc@attractive-media.fr>
2022-12-12 18:57:10 +0100

    ajoute l'export des scores pendant l'audit

--- app.territoiresentransitions.react/src/app/pages/collectivite/AuditComparaison/index.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/AuditComparaison/index.tsx
@@ -1,17 +1,17 @@
 import {useTableData} from './useTableData';
 import {AuditComparaisonTable} from './AuditComparaisonTable';
 import {useReferentielId} from 'core-logic/hooks/params';
-import {useDownloadComparaisonScoreAuditAsCSV} from './useDownloadComparaisonScoreAuditAsCSV';
 import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {useExportAuditScores} from './export/useExportAuditScore';
 
 const AuditComparaison = () => {
   const tableData = useTableData();
 
   const referentiel = useReferentielId();
   const collectivite = useCurrentCollectivite();
-  const {download, disabled} = useDownloadComparaisonScoreAuditAsCSV(
-    collectivite,
-    referentiel
+  const {exportAuditScores, isLoading} = useExportAuditScores(
+    referentiel,
+    collectivite
   );
 
   return (
@@ -20,9 +20,9 @@ const AuditComparaison = () => {
       <button
         data-test="export-audit-comp"
         className="fr-btn fr-btn--icon-left fr-fi-download-line"
-        disabled={disabled}
+        disabled={isLoading}
         onClick={() => {
-          download();
+          exportAuditScores();
         }}
       >
         Exporter
