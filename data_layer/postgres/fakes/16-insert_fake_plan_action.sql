dcb3c631fccffd52cd6152be218f5c1387cd0f83
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-12-12 17:30:24 +0100

    Complétion des tests ts

--- data_layer/postgres/fakes/16-insert_fake_plan_action.sql
+++ data_layer/postgres/fakes/16-insert_fake_plan_action.sql
@@ -20,10 +20,9 @@ values (default,
         'commentaire',
         '17440546-f389-4d4f-bfdb-b0c94a1bd0f9');
 
-insert into fiche_action (id, titre, description, thematiques, piliers_eci, collectivite_id)
+insert into fiche_action (titre, description, thematiques, piliers_eci, collectivite_id)
 values
     (
-        1,
         'fiche 1',
         'description',
         array[
@@ -35,24 +34,24 @@ values
             ],
         1
     ),
-    (2,'fiche 2','description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],1),
-    (3,'fiche 3','description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],2)
+    ('fiche 2','description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],1),
+    ('fiche 3','description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],2)
 ;
-insert into partenaires_tags (id, nom, collectivite_id)
-values (1, 'ptag1', 1), (2, 'ptag2', 1), (3, 'ptag3', 2);
+insert into partenaires_tags (nom, collectivite_id)
+values ('ptag1', 1), ('ptag2', 1), ('ptag3', 2);
 
-insert into structures_tags (id, nom, collectivite_id)
-values (1, 'stag1', 1), (2, 'stag2', 2), (3, 'stag3', 2);
+insert into structures_tags (nom, collectivite_id)
+values ('stag1', 1), ('stag2', 2), ('stag3', 2);
 
-insert into users_tags(id, nom, collectivite_id)
-values (1, 'user1', 1), (2, 'user2', 1), (3, 'user3', 2);
+insert into users_tags(nom, collectivite_id)
+values ('user1', 1), ('user2', 1), ('user3', 2);
 
-insert into plan_action
-values (1,'Plan 1',1,null),
-       (2, 'Plan 1.1', 1, 1),
-       (3, 'Plan 1.2', 1, 1),
-       (4, 'Plan 1.1.1', 1, 2),
-       (5,'Plan 2',2,null);
+insert into plan_action (nom, collectivite_id, parent)
+values ('Plan 1',1,null), -- id 1
+       ('Plan 1.1', 1, 1), -- id 2
+       ('Plan 1.2', 1, 1), -- id 3
+       ('Plan 1.1.1', 1, 2), -- id 4
+       ('Plan 2',2,null); -- id 5
 
 select upsert_fiche_action_liens(
                1,
