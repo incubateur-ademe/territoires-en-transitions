d39ab3ef075be3bbb253309154e1cc7c6178b4aa
Amandine Jacquelin <conta@jacquelinamandine.fr>
2022-12-19 17:05:34 +0100

    Modification des fonctions d'ajout et suppression des objets liés aux fiches actions

--- data_layer/tests/plan_action/plan_action.sql
+++ data_layer/tests/plan_action/plan_action.sql
@@ -1,22 +1,22 @@
 begin;
-select plan(14);
+select plan(16);
 
-truncate fiche_action_annexes;
-truncate annexes cascade;
-truncate fiche_action_indicateur_personnalise;
+truncate fiche_action_annexe;
+truncate annexe cascade;
 truncate fiche_action_indicateur;
 truncate fiche_action_action;
-truncate fiche_action_referents;
-truncate fiche_action_pilotes;
-truncate users_tags cascade;
-truncate fiche_action_structures_tags;
-truncate structures_tags cascade;
-truncate fiche_action_partenaires_tags;
-truncate partenaires_tags cascade;
-truncate fiche_action_plan_action;
-truncate plan_action cascade;
+truncate fiche_action_referent;
+truncate fiche_action_pilote;
+truncate personne_tag cascade;
+truncate fiche_action_structure_tag;
+truncate structure_tag cascade;
+truncate fiche_action_partenaire_tag;
+truncate partenaire_tag cascade;
+truncate fiche_action_axe;
+truncate axe cascade;
 truncate fiche_action cascade;
 
+-- Test fiche_action
 insert into fiche_action (id, titre, description, thematiques, piliers_eci, collectivite_id)
 values
     (
@@ -35,84 +35,80 @@ values
     (2,'fiche 2','test description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],1),
     (3,'fiche 3','test description',array[]::fiche_action_thematiques[],array[]::fiche_action_piliers_eci[],2)
 ;
-
 select ok((select count(*)=3 from fiche_action), 'Il devrait y avoir trois fiches action');
 
-insert into partenaires_tags (id, nom, collectivite_id)
-values (1, 'ptag1', 1), (2, 'ptag2', 1), (3, 'ptag3', 2);
-
-insert into structures_tags (id, nom, collectivite_id)
-values (1, 'stag1', 1), (2, 'stag2', 2), (3, 'stag3', 2);
-
-insert into users_tags(id, nom, collectivite_id)
-values (1, 'user1', 1), (2, 'user2', 1), (3, 'user3', 2);
-
-insert into plan_action
+-- Test axe
+insert into axe
 values (1,'Test 1',1,null),
        (2, 'Test 1.1', 1, 1),
        (3, 'Test 1.2', 1, 1),
        (4, 'Test 1.1.1', 1, 2),
        (5,'Test 2',2,null);
 
-select ok((select count(*)=5 from plan_action), 'Il devrait y avoir cinq plans action');
+select ok((select count(*)=5 from axe), 'Il devrait y avoir cinq axes');
+select ajouter_fiche_action_dans_un_axe(1, 4);
+select ajouter_fiche_action_dans_un_axe(2, 4);
+select ajouter_fiche_action_dans_un_axe(3, 3);
+select ajouter_fiche_action_dans_un_axe(1, 5);
+select ajouter_fiche_action_dans_un_axe(2, 5);
+select enlever_fiche_action_d_un_axe(2, 5);
+select ok ((select count(*)=4 from fiche_action_axe),
+           'Il devrait y avoir 4 entrées dans fiche_action_axe');
 
--- TODO annexes
+-- Test partenaire
+select ajouter_partenaire(1, (select pt.*::partenaire_tag from (select null as id, 'part1' as nom, 1 as collectivite_id) pt limit 1));
+select ajouter_partenaire(1, (select pt.*::partenaire_tag from (select null as id, 'part2' as nom, 1 as collectivite_id) pt limit 1));
+select ajouter_partenaire(2, (select pt.*::partenaire_tag from (select null as id, 'part3' as nom, 2 as collectivite_id) pt limit 1));
+select enlever_partenaire(3, (select ajouter_partenaire(3, (select pt.*::partenaire_tag from (select null as id, 'part4' as nom, 2 as collectivite_id) pt limit 1))));
+select ok ((select count(*)=4 from partenaire_tag),
+           'Il devrait y avoir 3 entrées dans partenaire_tag');
+select ok ((select count(*)=3 from fiche_action_partenaire_tag),
+    'Il devrait y avoir 3 entrées dans fiche_action_partenaire_tag');
 
-select upsert_fiche_action_liens(
-    1,
-    array [1, 2],
-    array [1],
-    array[1],
-    array['298235a0-60e7-4ceb-9172-0a991cce0386'::uuid],
-    array[2],
-    array[]::uuid[],
-    array[]::integer[],
-    array [3, 5],
-    array[]::action_id[],
-    array[]::indicateur_id[],
-    array[]::integer[]
-);
-select upsert_fiche_action_liens(
-    2,
-    array [1],
-    array [1],
-    array[2],
-    array[]::uuid[],
-    array[1],
-    array[]::uuid[],
-    array[]::integer[],
-    array [4],
-    array[]::action_id[],
-    array[]::indicateur_id[],
-    array[]::integer[]
-);
-select upsert_fiche_action_plan_action(3, array [4]);
+-- Test structure
+select ajouter_structure(1, (select st.*::structure_tag from (select null as id, 'stru1' as nom, 1 as collectivite_id) st limit 1));
+select ajouter_structure(1, (select st.*::structure_tag from (select null as id, 'stru2' as nom, 1 as collectivite_id) st limit 1));
+select ajouter_structure(2, (select st.*::structure_tag from (select null as id, 'stru3' as nom, 2 as collectivite_id) st limit 1));
+select enlever_structure(3, (select ajouter_structure(3, (select st.*::structure_tag from (select null as id, 'stru4' as nom, 2 as collectivite_id) st limit 1))));
+select ok ((select count(*)=4 from structure_tag),
+           'Il devrait y avoir 4 entrées dans structure_tag');
+select ok ((select count(*)=3 from fiche_action_structure_tag),
+           'Il devrait y avoir 3 entrées dans fiche_action_structure_tag');
 
-select ok ((select count(*)=3 from fiche_action_partenaires_tags),
-    'Il devrait y avoir 3 entrées dans fiche_action_partenaires_tags');
-select ok ((select count(*)=2 from fiche_action_structures_tags),
-           'Il devrait y avoir 2 entrées dans fiche_action_structures_tags');
-select ok ((select count(*)=3 from fiche_action_pilotes),
-           'Il devrait y avoir 3 entrées dans fiche_action_pilotes');
-select ok ((select count(*)=2 from fiche_action_referents),
-           'Il devrait y avoir 2 entrées dans fiche_action_referents');
-select ok ((select count(*)=0 from fiche_action_annexes),
-           'Il devrait y avoir 0 entrées dans fiche_action_annexes');
-select ok ((select count(*)=4 from fiche_action_plan_action),
-           'Il devrait y avoir 4 entrées dans fiche_action_plan_action');
-select ok ((select count(*)=0 from fiche_action_action),
-           'Il devrait y avoir 0 entrées dans fiche_action_action');
-select ok ((select count(*)=0 from fiche_action_indicateur),
-           'Il devrait y avoir 0 entrées dans fiche_action_indicateur');
-select ok ((select count(*)=0 from fiche_action_indicateur_personnalise),
-           'Il devrait y avoir 0 entrées dans fiche_action_action_personalise');
+-- Test personne
+select ajouter_pilote(1, (select pe.*::personne from (select 'pe1' as nom, 1 as collectivite_id, null as personne_tag_id, null as utilisateur_uuid) pe limit 1));
+select ajouter_pilote(1, (select pe.*::personne from (select null as nom, 1 as collectivite_id, null as personne_tag_id, '17440546-f389-4d4f-bfdb-b0c94a1bd0f9' as utilisateur_uuid) pe limit 1));
+select enlever_pilote(3, (select ajouter_pilote(3, (select pe.*::personne from (select 'pe2' as nom, 1 as collectivite_id, null as personne_tag_id, null as utilisateur_uuid) pe limit 1))));
+select ok ((select count(*)=2 from personne_tag),
+           'Il devrait y avoir 2 entrées dans personne_tag');
+select ok ((select count(*)=2 from fiche_action_pilote),
+           'Il devrait y avoir 2 entrées dans fiche_action_pilote');
+select enlever_referent(3, (select ajouter_referent(3, (select pe.*::personne from (select 'pe3' as nom, 1 as collectivite_id, null as personne_tag_id, null as utilisateur_uuid) pe limit 1))));
+select ajouter_referent(1, (select pe.*::personne from (select pt.nom, pt.collectivite_id, pt.id as personne_tag_id, null as utilisateur_uuid from personne_tag pt where nom = 'pe2') pe limit 1));
+select ok ((select count(*)=3 from personne_tag),
+           'Il devrait y avoir 3 entrées dans personne_tag');
+select ok ((select count(*)=1 from fiche_action_referent),
+           'Il devrait y avoir 1 entrées dans fiche_action_referent');
 
-select upsert_fiche_action_partenaires(1, array [2]);
-select ok ((select count(*)=2 from fiche_action_partenaires_tags),
-           'Il devrait y avoir 2 entrées dans fiche_action_partenaires_tags');
+-- Test annexe TODO
+select ok ((select count(*)=0 from fiche_action_annexe),
+           'Il devrait y avoir 0 entrées dans fiche_action_annexe');
+-- Test action
+select ajouter_action(1, 'eci_2.1');
+select ajouter_action(3, 'eci_2.2');
+select enlever_action(3, 'eci_2.2');
+select ok ((select count(*)=1 from fiche_action_action),
+           'Il devrait y avoir 1 entrées dans fiche_action_action');
+-- Test indicateur
+select ajouter_indicateur(1, (select pe.*::indicateur_global from (select 'eci_5' as indicateur_id, null as indicateur_personnalise_id, null as nom, null as description, null as unite) pe limit 1));
+select ajouter_indicateur(2, (select pe.*::indicateur_global from (select null as indicateur_id, 1 as indicateur_personnalise_id, null as nom, null as description, null as unite) pe limit 1));
+select ajouter_indicateur(3, (select pe.*::indicateur_global from (select 'eci_5' as indicateur_id, null as indicateur_personnalise_id, null as nom, null as description, null as unite) pe limit 1));
+select enlever_indicateur(3, (select pe.*::indicateur_global from (select 'eci_5' as indicateur_id, null as indicateur_personnalise_id, null as nom, null as description, null as unite) pe limit 1));
+select ok ((select count(*)=2 from fiche_action_indicateur),
+           'Il devrait y avoir 2 entrées dans fiche_action_indicateur');
 
 select ok ((select count(*)=3 from fiches_action),
             'Il devrait y avoir 3 entrées dans la vue');
 
-select isnt_empty('select recursive_plan_action(1)', 'La fonction devrait retourner un jsonb');
+select isnt_empty('select plan_action(1)', 'La fonction devrait retourner un jsonb');
 rollback;
