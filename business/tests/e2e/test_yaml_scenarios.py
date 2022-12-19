e3d94ea185aa6e2f393dd45eafdcbf18b0182ba2
Florian d'Erfurth <florian@derfurth.com>
2022-12-19 14:48:56 +0100

    Déplace les tests yaml et crée des sous tests pour une meilleure lisibilité.

--- business/tests/e2e/test_yaml_scenarios.py
+++ business/tests/e2e/test_yaml_scenarios.py
@@ -6,86 +6,91 @@ from business.utils.models.action_statut import (
     DetailedAvancement,
 )
 
+yaml_tests_dir = "../markdown/tests"
+yaml_tests_glob = "**/*.test.yml"
+paths = [(str(path).split('/')[-1], path) for path in list(Path(yaml_tests_dir).glob(yaml_tests_glob))]
 
-def test_compare_inputs_to_results(test_post_personnalize, test_post_evaluate):
-    pathlist = Path("./tests/yaml").glob('**/*.test.yml')
 
-    # Pour chaque fichier yaml
-    for path in pathlist:
-        path_in_str = str(path)
+def test_yaml_files_should_be_present():
+    assert len(paths) > 0, f'Des tests yaml devraient être présents dans {yaml_tests_dir}'
+
+
+@pytest.mark.parametrize("name, path", paths)
+def test_scores_should_match_yaml_expectations(test_post_personnalize, test_post_evaluate, name, path):
+    path_in_str = str(path)
+    print('')
+    print('_' * 80)
+    print(path_in_str)
+
+    # On ouvre le fichier et on charge les données
+    with open(path) as f:
+        data = yaml.safe_load(f)
+        print(data['Test'])
         print('')
-        print('_' * 80)
-        print(path_in_str)
 
-        # On ouvre le fichier et on charge les données
-        with open(path) as f:
-            data = yaml.safe_load(f)
-            print(data['Test'])
-            print('')
+        # Convertit les réponses
+        reponses = []
+        if "Réponses" in data:
+            for question_id in data["Réponses"].keys():
+                reponse = data["Réponses"][question_id]
+                if reponse is False:
+                    reponse = 'NON'
+                elif reponse is True:
+                    reponse = 'OUI'
+                reponses.append(Reponse(question_id, reponse))
 
-            # Convertit les réponses
-            reponses = []
-            if "Réponses" in data:
-                for question_id in data["Réponses"].keys():
-                    reponse = data["Réponses"][question_id]
-                    if reponse is False:
-                        reponse = 'NON'
-                    elif reponse is True:
-                        reponse = 'OUI'
-                    reponses.append(Reponse(question_id, reponse))
+        # Lit les caractéristiques de la collectivité
+        identite = None
+        if "Collectivité" in data:
+            collectivite = data['Collectivité']
+            identite = IdentiteCollectivite(
+                type=collectivite.get('type') or [],
+                population=collectivite.get('population') or [],
+                localisation=collectivite.get('localisation') or [],
+            )
 
-            # Lit les caratéristiques de la collectivité
-            identite = None
-            if "Collectivité" in data:
-                collectivite = data['Collectivité']
-                identite = IdentiteCollectivite(
-                    type=collectivite.get('type') or [],
-                    population=collectivite.get('population') or [],
-                    localisation=collectivite.get('localisation') or [],
-                )
+        # On calcule la personnalisation à partir des réponses et de l'identité
+        consequences = test_post_personnalize(reponses, identite)
 
-            # On calcule la personnalisation à partir des réponses et de l'identité
-            consequences = test_post_personnalize(reponses, identite)
+        # Lit les statuts
+        statuts = []
+        if "Statuts" in data:
+            for action_id in data['Statuts'].keys():
+                statut = data['Statuts'][action_id]
+                avancement = None
+                if statut == 'fait':
+                    avancement = DetailedAvancement(fait=1.0, programme=0.0, pas_fait=0.0)
+                elif statut == 'programme':
+                    avancement = DetailedAvancement(fait=0.0, programme=1.0, pas_fait=0.0)
+                elif statut == 'pas_fait':
+                    avancement = DetailedAvancement(fait=0.0, programme=0.0, pas_fait=1.0)
+                statuts.append(ActionStatut(
+                    action_id=action_id,
+                    detailed_avancement=avancement,
+                    concerne=statut != 'non_concerne'
+                ))
 
-            # Lit les statuts
-            statuts = []
-            if "Statuts" in data:
-                for action_id in data['Statuts'].keys():
-                    statut = data['Statuts'][action_id]
-                    avancement = None
-                    if statut == 'fait':
-                        avancement = DetailedAvancement(fait=1.0, programme=0.0, pas_fait=0.0)
-                    elif statut == 'programme':
-                        avancement = DetailedAvancement(fait=0.0, programme=1.0, pas_fait=0.0)
-                    elif statut == 'pas_fait':
-                        avancement = DetailedAvancement(fait=0.0, programme=0.0, pas_fait=1.0)
-                    statuts.append(ActionStatut(
-                        action_id=action_id,
-                        detailed_avancement=avancement,
-                        concerne=statut != 'non_concerne'
-                    ))
+        # Pour chaque référentiel on calcule les scores et on les ajoute à [scores].
+        scores = dict()
+        for ref in ['cae', 'eci']:
+            ref_consequences = {  # on ne garde que les consequence du référentiel
+                action_id: consequence
+                for action_id, consequence in consequences.items()
+                if action_id.startswith(ref)
+            }
+            ref_statuts = [statut for statut in statuts if statut.action_id.startswith(ref)]
+            # Simule un appel à l'API
+            ref_scores = test_post_evaluate(ref, ref_statuts, ref_consequences)
+            scores = scores | ref_scores
 
-            # Pour chaque référentiel on calcul les scores et on les ajoute à [scores].
-            scores = dict()
-            for ref in ['cae', 'eci']:
-                ref_consequences = {  # on ne garde que les consequence du référentiel
-                    action_id: consequence
-                    for action_id, consequence in consequences.items()
-                    if action_id.startswith(ref)
-                }
-                ref_statuts = [statut for statut in statuts if statut.action_id.startswith(ref)]
-                # Simule un appel à l'API
-                ref_scores = test_post_evaluate(ref, ref_statuts, ref_consequences)
-                scores = scores | ref_scores
-
-            # Pour chaque score du yaml
-            for action_id in data['Scores'].keys():
-                have = vars(scores[action_id])
-                want = data['Scores'][action_id]
-                test = want.pop('test', '')
-                print(f'{action_id}: {test}')
-                print(f'- have {have}')
-                print(f'- want {want}')
-                # On compare chaque valeur avec l'objet obtenu par l'API
-                for k in [k for k in want.keys() if k != 'test']:
-                    assert have[k] == want[k], f'Erreur sur "{k}" de "{action_id}" pour: {test}'
+        # Pour chaque score du yaml
+        for action_id in data['Scores'].keys():
+            have = vars(scores[action_id])
+            want = data['Scores'][action_id]
+            test = want.pop('test', '')
+            print(f'{action_id}: {test}')
+            print(f'- have {have}')
+            print(f'- want {want}')
+            # On compare chaque valeur avec l'objet obtenu par l'API
+            for k in [k for k in want.keys() if k != 'test']:
+                assert have[k] == want[k], f'Erreur sur "{k}" de "{action_id}" pour: {test}'
