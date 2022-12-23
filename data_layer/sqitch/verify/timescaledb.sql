f0154d71673288ddcbf35c404e232ebb08f82227
Florian d'Erfurth <florian@derfurth.com>
2022-12-23 10:39:52 +0100

    Timescale DB revert et verify.

--- data_layer/sqitch/verify/timescaledb.sql
+++ data_layer/sqitch/verify/timescaledb.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+select has_function_privilege('create_hypertable(regclass, name, name, integer, name, name, anyelement, boolean, boolean, regproc, boolean, text, regproc, regproc, integer, name[])', 'execute');
 
 ROLLBACK;
