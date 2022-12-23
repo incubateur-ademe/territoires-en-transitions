5c6e25bf92b8272b9564fed517f4d6fa5c4e2052
Florian d'Erfurth <florian@derfurth.com>
2022-12-23 16:03:07 +0100

    Améliore le tracker.

--- app.territoiresentransitions.react/src/core-logic/hooks/useTracker.ts
+++ app.territoiresentransitions.react/src/core-logic/hooks/useTracker.ts
@@ -4,6 +4,7 @@ import {useAuth} from 'core-logic/api/auth/AuthProvider';
 import {supabaseClient} from 'core-logic/api/supabase';
 import {useLocalisation} from 'core-logic/hooks/useLocalisation';
 import {ENV} from 'environmentVariables';
+import {useMemo, useRef, useState} from 'react';
 
 /**
  * Représente l'utilisation d'une fonctionnalité
@@ -31,21 +32,45 @@ const track = async (usage: Usage): Promise<boolean> => {
 export const useTracker = (): (usage: Usage) => Promise<boolean> => {
   const collectivite_id = useCollectiviteId();
   const localisation = useLocalisation();
+  const ref = useRef();
   const {user} = useAuth();
 
-  const tracker = (usage: Usage) => {
-    if (collectivite_id)
-      usage['collectivite_id'] = collectivite_id;
-    if (user)
-      usage['user_id'] = user.id;
-    usage.page = localisation.page;
+  const factory = () => {
+    // Garde la dernière valeur d'usage pour éviter d'envoyer
+    // un même usage plusieurs fois de suite.
+    let tracked: Usage | null;
 
-    if (ENV.node_env === 'development') {
-      console.info('\x1B[0;96mtrack usage\x1B[m', 'usage', usage);
-    }
+    const tracker = async (usage: Usage): Promise<boolean> => {
+      // on ajoute les valeurs courantes à l'usage
+      if (collectivite_id)
+        usage['collectivite_id'] = collectivite_id;
+      if (user)
+        usage['user_id'] = user.id;
+      usage.page = localisation.page;
 
-    return track(usage);
+      // on évite d'envoyer un même usage plusieurs fois de suite
+      if (isEqual(tracked, usage)) return false;
+      tracked = usage;
+      if (ENV.node_env === 'development') {
+        console.info('\x1B[0;96mtrack usage\x1B[m', 'usage', usage);
+      }
+
+      return track(usage);
+    };
+
+    return tracker;
   };
 
-  return tracker;
+  // On renvoie un tracker par ref.
+  return useMemo(factory, [ref.current]);
+};
+
+const isEqual = (a: Usage | null, b: Usage | null) => {
+  if (!a && !b) return true;
+  if (!a || !b) return false;
+  return a.page === b.page
+    && a.action === b.action
+    && a.fonction === b.fonction
+    && a.collectivite_id === b.collectivite_id
+    && a.user_id === b.user_id;
 };
