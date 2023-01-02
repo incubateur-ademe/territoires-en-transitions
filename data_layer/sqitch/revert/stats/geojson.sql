8e8697aa407515f1b1df221666049c4cf102e6af
Florian d'Erfurth <florian@derfurth.com>
2023-01-02 16:27:18 +0100

    Ajoute le revert et le verify.

--- data_layer/sqitch/revert/stats/geojson.sql
+++ data_layer/sqitch/revert/stats/geojson.sql
@@ -2,10 +2,10 @@
 
 BEGIN;
 
+drop view stats.collectivite_geojson;
 drop table stats.epci_geojson;
 drop table stats.commune_geojson;
 drop table stats.departement_geojson;
 drop table stats.region_geojson;
-drop view stats.collectivite_geojson;
 
 COMMIT;
--- data_layer/sqitch/revert/stats/vues_BI.sql
+++ data_layer/sqitch/revert/stats/vues_BI.sql
@@ -2,60 +2,6 @@
 
 BEGIN;
 
-create or replace view stats.monthly_bucket
-as
-with serie as (select generate_series(0, (extract(month from age(now(), date '2021-01-01')) +
-                                          extract(year from age(now(), date '2021-01-01')) * 12)) as m),
-     month as
-         (select date '2021-01-01' + interval '1 month' * serie.m as start
-          from serie)
-select month.start::date                                  as first_day,
-       (month.start + (interval '1 month - 1 day'))::date as last_day
-from month;
-
-create materialized view stats.connection
-as
-with utilisateur as (select distinct pud.user_id
-                     from private_utilisateur_droit pud
-                              join stats.collectivite c on pud.collectivite_id = c.collectivite_id
-                     where pud.collectivite_id not in (select collectivite_id from collectivite_test)
-                       and pud.active),
-     event as (select payload ->> 'actor_id' as user_id,
-                      created_at::date
-               from utilisateur u
-                        join auth.audit_log_entries
-                             on (payload ->> 'actor_id')::uuid = u.user_id
-               where payload ->> 'action' = 'login'
-                  or payload ->> 'action' = 'token_refreshed'
-               order by created_at desc)
-select md5(user_id) as utilisateur,
-       created_at
-from event
-group by created_at, user_id
-order by created_at desc;
-comment on materialized view stats.connection
-    is 'Les connection utilisateurs reconstituée à partir des événements '
-        '`login` et `token_refreshed` avec maximum d''un événement par utilisateur par jour.';
-
-create materialized view stats.evolution_connection
-as
-select m.first_day                        as mois,
-       (select count(*)
-        from stats.connection c
-        where c.created_at >= m.first_day
-          and c.created_at <= m.last_day) as connections,
-       (select count(distinct c.utilisateur)
-        from stats.connection c
-        where c.created_at >= m.first_day
-          and c.created_at <= m.last_day) as utilisateurs_uniques,
-       (select count(*)
-        from stats.connection c
-        where c.created_at <= m.last_day) as total_connections,
-       (select count(distinct c.utilisateur)
-        from stats.connection c
-        where c.created_at <= m.last_day) as total_utilisateurs_uniques
-from stats.monthly_bucket m;
-
 create or replace function
     stats.refresh_views()
     returns void
@@ -77,5 +23,7 @@ begin
 end ;
 $$ language plpgsql security definer;
 
+drop view public.stats_carte_collectivite_active;
+drop materialized view stats.carte_collectivite_active;
 
 COMMIT;
