1ce8a04de49b6a5c941c2dd68c49c0cb35d616c3
Marc Rutkowski <marc@attractive-media.fr>
2023-01-02 17:03:23 +0100

    ajoute le squelette du nouveau site statique [skip ci]

---
+++ site/app/layout.tsx
@@ -0,0 +1,23 @@
+import { DsfrHead } from '@codegouvfr/react-dsfr/next-appdir/DsfrHead';
+import { DsfrProvider } from '@codegouvfr/react-dsfr/next-appdir/DsfrProvider';
+import { getColorSchemeHtmlAttributes } from '@codegouvfr/react-dsfr/next-appdir/getColorSchemeHtmlAttributes';
+import StartDsfr from './StartDsfr';
+import { defaultColorScheme } from './defaultColorScheme';
+import AppHeader from './AppHeader';
+
+export default function RootLayout({ children }: { children: JSX.Element }) {
+  return (
+    <html {...getColorSchemeHtmlAttributes({ defaultColorScheme })}>
+      <head>
+        <StartDsfr />
+        <DsfrHead defaultColorScheme={defaultColorScheme} />
+      </head>
+      <body>
+        <DsfrProvider defaultColorScheme={defaultColorScheme}>
+          <AppHeader />
+          {children}
+        </DsfrProvider>
+      </body>
+    </html>
+  );
+}
