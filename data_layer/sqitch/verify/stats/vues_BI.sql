8172850db13b5a6d35324ea9b6f0b4c34296e1b9
Florian d'Erfurth <florian@derfurth.com>
2023-01-02 16:27:18 +0100

    Ajoute le revert et le verify.

--- data_layer/sqitch/verify/stats/vues_BI.sql
+++ data_layer/sqitch/verify/stats/vues_BI.sql
@@ -2,12 +2,30 @@
 
 BEGIN;
 
-select utilisateur, created_at
-from stats.connection
-where false;
+select collectivite_id,
+       nom,
+       type_collectivite,
+       nature_collectivite,
+       code_siren_insee,
+       region_name,
+       region_code,
+       departement_name,
+       departement_code,
+       population_totale,
+       geojson
+from public.stats_carte_collectivite_active where false;
 
-select mois, connections, utilisateurs_uniques, total_connections, total_utilisateurs_uniques
-from stats.evolution_connection
-where false;
+select collectivite_id,
+       nom,
+       type_collectivite,
+       nature_collectivite,
+       code_siren_insee,
+       region_name,
+       region_code,
+       departement_name,
+       departement_code,
+       population_totale,
+       geojson
+from stats.carte_collectivite_active where false;
 
 ROLLBACK;
