b742310901e9aeb08bd6e5c5aad54b549718d983
Marc Rutkowski <marc@attractive-media.fr>
2023-01-05 18:51:07 +0100

    améliore la gestion du cas "référentiel non rempli" dans la page "Audit et labellisation"

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useIsUnchangedReferentiel.ts
@@ -0,0 +1,23 @@
+import {useQuery} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+
+/** Détermine si un référentiel n'a pas encore été édité */
+export const useIsUnchangedReferentiel = (
+  collectivite_id: number | null,
+  referentiel: string | null
+) => {
+  const {data} = useQuery(
+    ['unchanged_referentiel', collectivite_id, referentiel],
+    async () => {
+      if (!collectivite_id || !referentiel) {
+        return 0;
+      }
+      const {count} = await supabaseClient
+        .from('action_statuts')
+        .select('*', {head: true, count: 'exact'})
+        .match({collectivite_id, referentiel});
+      return count || 0;
+    }
+  );
+  return !data;
+};
