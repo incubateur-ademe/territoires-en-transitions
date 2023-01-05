bee75500600911a3401df16c037184f1f607928c
Marc Rutkowski <marc@attractive-media.fr>
2023-01-05 18:45:32 +0100

    ajoute le rapport d'audit dans la modale de validation et fixe la fermeture du dialogue

--- app.territoiresentransitions.react/src/app/pages/collectivite/Audit/ValiderAudit.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/ValiderAudit.tsx
@@ -1,9 +1,13 @@
+import {Database} from 'types/database.types';
 import Modal, {RenderProps} from 'ui/shared/floating-ui/Modal';
+import PreuveDoc from 'ui/shared/preuves/Bibliotheque/PreuveDoc';
 import {AddRapportButton} from './AddRapportButton';
-import {TAudit} from './types';
+import {useRapportAudit} from './useAudit';
 
-export type TValiderAuditProps = RenderProps & {
-  audit: TAudit;
+type TAuditRow = Database['public']['Tables']['audit']['Row'];
+export type TValiderAuditProps = {
+  audit: TAuditRow;
+  onValidate: (audit?: TAuditRow) => void;
 };
 
 /**
@@ -27,17 +31,13 @@ const auditLabellisation =
 /**
  * Affiche la modale de validation d'un audit
  */
-export const ValiderAuditModal = (props: TValiderAuditProps) => {
-  const {audit, close} = props;
+export const ValiderAuditModal = (props: RenderProps & TValiderAuditProps) => {
+  const {audit, close, onValidate} = props;
   const {id: audit_id, demande_id} = audit;
 
-  // validation
-  const handleValidate = () => {
-    close();
-  };
-
-  // on peut valider si le rapport a été attaché à l'audit
-  const canValidate = false;
+  // on peut valider seulement si le rapport a été attaché à l'audit
+  const rapport = useRapportAudit(audit_id);
+  const canValidate = Boolean(rapport);
 
   return (
     <div>
@@ -48,13 +48,17 @@ export const ValiderAuditModal = (props: TValiderAuditProps) => {
         de la communauté).
       </p>
       <AddRapportButton audit_id={audit_id} />
+      {rapport ? <PreuveDoc preuve={rapport} readonly /> : null}
       <p className="fr-mt-4w">
         {demande_id ? auditLabellisation : auditSansLabellisation}
       </p>
       <div className="flex">
         <button
           className="fr-btn fr-btn--sm fr-mr-2w"
-          onClick={() => handleValidate()}
+          onClick={() => {
+            onValidate(audit);
+            close();
+          }}
           disabled={!canValidate}
         >
           Valider l'audit
