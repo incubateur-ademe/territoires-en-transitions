1c925640efada9e54ba869c2e8fc50318c0277ba
Florian d'Erfurth <florian@derfurth.com>
2023-01-08 19:17:41 +0100

    Rework des tranches de complétude.

--- data_layer/sqitch/deploy/stats/completude.sql
+++ data_layer/sqitch/deploy/stats/completude.sql
@@ -3,33 +3,23 @@
 
 BEGIN;
 
+drop materialized view stats_tranche_completude;
 create materialized view stats_tranche_completude
 as
-with completude as (
-    select coalesce(completude_eci, .0) as completude_eci,
-           coalesce(completude_cae, .0) as completude_cae
-    from retool_completude
-),
-     range as (
-         select unnest('{0,       0.00001, 50, 80,  100}'::float[])         as start,
-                unnest('{0.00001, 50,      80, 100, 100.1}'::float[]) as "end"
-     ),
-     eci as (
-         select r.start, count(eci.*) as count
-         from range r
-                  left join completude eci on eci.completude_eci >= start and eci.completude_eci < "end"
-         group by r.start
-     ),
-     cae as (
-         select r.start, count(cae.*) as count
-         from range r
-                  left join completude cae on cae.completude_cae >= start and cae.completude_cae < "end"
-         group by r.start
-     )
-select range.start::int || ':' || range."end"::int as bucket, eci.count as eci, cae.count as cae
-from range
-         left join eci on eci.start = range.start
-         left join cae on cae.start = range.start
-order by range.start;
+with bounds as (select unnest('{0, 20, 50, 80,  100}'::numeric[]) as bound),
+     ranges as (select numrange(lower.bound, upper.bound) as range,
+                       lower.bound                        as lower_bound,
+                       upper.bound                        as upper_bound
+                from bounds lower
+                         left join lateral (select * from bounds b where b.bound > lower.bound limit 1) upper on true),
+     completude as (select coalesce(completude_eci, .0) as completude_eci,
+                           coalesce(completude_cae, .0) as completude_cae
+                    from stats.pourcentage_completude)
+select lower_bound, upper_bound, eci, cae
+from ranges r
+         left join lateral (select count(*) filter ( where r.range @> c.completude_eci::numeric ) as eci,
+                                   count(*) filter ( where r.range @> c.completude_cae::numeric ) as cae
+                            from completude c) in_range on true
+;
 
 COMMIT;
