78362562b0b8e14fbf20a87af8af7e74dc332439
Marc Rutkowski <marc@attractive-media.fr>
2023-01-09 10:32:00 +0100

    affiche tous les documents téléversés depuis la modale de validation de l'audit et autorise l'édition/suppression

--- app.territoiresentransitions.react/src/app/pages/collectivite/Audit/ValiderAudit.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/ValiderAudit.tsx
@@ -2,7 +2,7 @@ import {Database} from 'types/database.types';
 import Modal, {RenderProps} from 'ui/shared/floating-ui/Modal';
 import PreuveDoc from 'ui/shared/preuves/Bibliotheque/PreuveDoc';
 import {AddRapportButton} from './AddRapportButton';
-import {useRapportAudit} from './useAudit';
+import {useRapportsAudit} from './useAudit';
 
 type TAuditRow = Database['public']['Tables']['audit']['Row'];
 export type TValiderAuditProps = {
@@ -35,9 +35,9 @@ export const ValiderAuditModal = (props: RenderProps & TValiderAuditProps) => {
   const {audit, close, onValidate} = props;
   const {id: audit_id, demande_id} = audit;
 
-  // on peut valider seulement si le rapport a été attaché à l'audit
-  const rapport = useRapportAudit(audit_id);
-  const canValidate = Boolean(rapport);
+  // on peut valider seulement si au moins un rapport a été attaché à l'audit
+  const rapports = useRapportsAudit(audit_id);
+  const canValidate = Boolean(rapports?.length);
 
   return (
     <div>
@@ -48,7 +48,9 @@ export const ValiderAuditModal = (props: RenderProps & TValiderAuditProps) => {
         de la communauté).
       </p>
       <AddRapportButton audit_id={audit_id} />
-      {rapport ? <PreuveDoc preuve={rapport} readonly /> : null}
+      {rapports?.map(rapport => (
+        <PreuveDoc key={rapport.id} preuve={rapport} />
+      ))}
       <p className="fr-mt-4w">
         {demande_id ? auditLabellisation : auditSansLabellisation}
       </p>
