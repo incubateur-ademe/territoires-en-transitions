3074951f163ec02dd9754458efd5cedaec7d0583
Florian d'Erfurth <florian@derfurth.com>
2023-01-11 12:29:42 +0100

    En tête responsive.

--- site/app/AppHeader.tsx
+++ site/app/AppHeader.tsx
@@ -2,82 +2,114 @@ import Header from '@codegouvfr/react-dsfr/Header';
 import Image from 'next/image';
 
 function Brand() {
-  return <div className="fr-header__brand fr-enlarge-link">
-    <div className="fr-header__brand-top">
-      <div className=""><a href="/" title="Territoires en Transitions">
-        <p className="fr-logo">République<br />Française</p></a>
-      </div>
-      <div className="fr-header__ademe">
+  return <div className="fr-header__brand-top">
+    <div className="fr-header__logo">
+      <p className="fr-logo">
+        République
+        <br />
+        Française
+      </p>
+    </div>
+    <div className="fr-header__operator">
+      <div className="fr-grid-row" style={{minWidth: 160 + 'px'}}>
         <Image src="/ademe.jpg" alt="ADEME" width="70" height="80" />
-        <Image src="/territoire-engage.jpg" alt="ADEME" width="80" height="80" />
+        <Image src="/territoire-engage.jpg" alt="ADEME" width="80"
+               height="80" />
       </div>
-      <div className="fr-header__service"><a href="/" title="Accueil"><p
-        className="fr-header__service-title pointer-events-auto">Territoires
-        en Transitions</p></a><p className="text-sm">Accompagner la
-        transition écologique des collectivités</p></div>
+    </div>
+    <div className="fr-header__navbar">
+      <button data-fr-opened="false"
+              aria-controls="modal-header__menu" aria-haspopup="menu"
+              id="button-861" title="Menu"
+              className="fr-btn--menu fr-btn">
+        Menu
+      </button>
     </div>
   </div>;
 }
 
-function Tools() {
-  return <div className="fr-header__tools">
-    <div className="fr-header__tools-links">
-      <ul className="fr-btns-group">
-        <li>
-          <a href="https://app.territoiresentransitions.fr/auth/signup"
-             className="fr-btn fr-icon-add-circle-line">Créer
-            un compte</a>
-        </li>
-        <li>
-          <a href="https://app.territoiresentransitions.fr/auth/signin"
-             className="fr-btn fr-icon-account-line">Se connecter</a>
-        </li>
-      </ul>
+function Links() {
+  return <ul className="fr-btns-group">
+    <li>
+      <a href="https://app.territoiresentransitions.fr/auth/signup"
+         className="fr-btn fr-icon-add-circle-line">
+        Créer un compte
+      </a>
+    </li>
+    <li>
+      <a href="https://app.territoiresentransitions.fr/auth/signin"
+         className="fr-btn fr-icon-account-line">
+        Se connecter
+      </a>
+    </li>
+  </ul>;
+}
+
+function Items() {
+  return <ul className="fr-nav__list">
+    <li className="fr-nav__item">
+      <a href="/stats"
+         target="_self"
+         aria-controls="modal-header__menu"
+         className="fr-nav__link">
+        Statistiques publiques
+      </a>
+    </li>
+  </ul>;
+}
+
+function Body() {
+  return <div className="fr-header__body">
+    <div className="fr-container">
+      <div className="fr-header__body-row">
+        <div className="fr-header__brand fr-enlarge-link">
+          <Brand />
+          <div className="fr-header__service">
+            <a href="/" aria-current="page"
+               title="Accueil - Système de Design de l'État"
+               className="router-link-exact-active router-link-active">
+              <p className="fr-header__service-title">
+                Territoires en Transitions
+                <br />
+                <span style={{fontSize: 'small', fontWeight: 'normal'}}>
+                Accompagner la transition écologique des collectivités
+              </span>
+              </p>
+            </a>
+          </div>
+        </div>
+        <div className="fr-header__tools">
+          <div className="fr-header__tools-links">
+            <Links />
+          </div>
+        </div>
+      </div>
     </div>
   </div>;
 }
 
 function Menu() {
-  return <div className="fr-header__menu fr-modal"
-              id="modal-:S2:"
-              data-fr-js-modal="true"
-              data-fr-js-header-modal="true">
+  return <div id="modal-header__menu" aria-labelledby="button-861"
+              className="fr-header__menu fr-modal">
     <div className="fr-container">
-      <button className="fr-btn--close fr-btn"
-              aria-controls="modal-:S2:"
-              title="Fermer"
-              data-fr-js-modal-button="true">
+      <button aria-controls="modal-header__menu"
+              className="fr-btn fr-btn--close">
         Fermer
       </button>
-
       <div className="fr-header__menu-links">
-        <ul className="fr-btns-group"></ul>
+        <Links />
       </div>
-
-      <nav className="fr-nav" role="navigation" aria-label="Menu principal"
-           data-fr-js-navigation="true">
-        <ul className="fr-nav__list">
-          <li className="fr-nav__item" data-fr-js-navigation-item="true"><a
-            href="/stats" target="_self" className="fr-nav__link">Statistiques
-            publiques</a>
-          </li>
-        </ul>
+      <nav id="navigation-866" role="navigation" aria-label="Menu principal"
+           className="fr-nav">
+        <Items />
       </nav>
-
     </div>
   </div>;
 }
 
 const AppHeader = () => (
-  <header role="banner" className="fr-header">
-    <div className="fr-header__body">
-      <div className="fr-container">
-        <div className="fr-header__body-row">
-          <Brand />
-          <Tools />
-        </div>
-      </div>
-    </div>
+  <header role="banner" id="header-navigation" className="fr-header">
+    <Body />
     <Menu />
   </header>
 );
