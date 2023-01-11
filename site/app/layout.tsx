11d763357e1fed88cf27b65c01d551d3e8280e86
Marc Rutkowski <marc@attractive-media.fr>
2023-01-11 17:36:29 +0100

    fixe la favicon

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -12,6 +12,25 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
       <head>
         <StartDsfr />
         <DsfrHead defaultColorScheme={defaultColorScheme} />
+        <title>Territoires en Transitions</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="description"
+          content="Territoires en transitions accompagne les collectivités afin de les aider à piloter plus facilement leur transition écologique."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </head>
       <body>
         <DsfrProvider defaultColorScheme={defaultColorScheme}>
