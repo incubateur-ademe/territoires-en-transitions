0923b9715cece978cf99df22fa07b94488626d4c
Marc Rutkowski <marc@attractive-media.fr>
2023-01-11 16:09:21 +0100

    supprime une trace

--- site/app/stats/CollectiviteActivesEtTotalParType.tsx
+++ site/app/stats/CollectiviteActivesEtTotalParType.tsx
@@ -3,7 +3,7 @@
 import useSWR from 'swr';
 import { ResponsiveWaffle } from '@nivo/waffle';
 import { supabase } from '../initSupabase';
-import { bottomLegend, getLegendData, theme } from './shared';
+import { bottomLegend, theme } from './shared';
 
 function useCollectiviteActivesEtTotalParType() {
   return useSWR('stats_collectivite_actives_et_total_par_type', async () => {
@@ -43,17 +43,14 @@ export default function CollectiviteActivesEtTotalParType() {
     return null;
   }
 
-  const { categories } = data;
-  const legendData = getLegendData(categories);
-  console.log(legendData);
-
+  const { categories, total } = data;
   return (
     <div style={{ height: 400 }}>
       <ResponsiveWaffle
         colors={['#21AB8E', '#FF732C']}
         theme={theme}
-        data={data.categories}
-        total={data.total || 0}
+        data={categories}
+        total={total || 0}
         rows={10}
         columns={10}
         margin={{ top: 0, right: 30, bottom: 10, left: 30 }}
