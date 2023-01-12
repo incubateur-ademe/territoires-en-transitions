a5fb44d0eadd3c6a210ea54c0d27c866c1c10846
Florian d'Erfurth <florian@derfurth.com>
2023-01-12 16:33:47 +0100

    test a minima de stats.refresh_views()

--- data_layer/tests/stats/vues_BI.sql
+++ data_layer/tests/stats/vues_BI.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan(5);
+select plan(6);
 
 select isnt_empty('select * from imports.departement');
 select isnt_empty('select * from imports.region');
@@ -18,5 +18,10 @@ select is_empty(
                     left join stats.iso_3166 i on i.nom = r.libelle
                where i is null;',
                'On devrait pouvoir rapprocher toutes les régions des codes ISO.');
+
+select function_returns('stats', 'refresh_views', 'void', 'La fonction stats.refresh devrait renvoyer void');
+
+-- appel la fonction pour vérifier qu'elle ne crashe pas
+select stats.refresh_views();
 
 rollback;
