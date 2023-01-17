f91838090dceb0f7de54abfb23d2544d07ba8c23
Florian d'Erfurth <florian@derfurth.com>
2023-01-17 17:32:35 +0100

    Script de conversion des plans d'action.

---
+++ data_layer/plan_action_import/xlsx_to_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+if __name__ == '__main__':
+    pathlist = Path("./").glob('**/*.xlsx')
+
+    # Pour chaque fichier xlsx
+    for path in pathlist:
+        path_in_str = str(path)
+        print(f'reading {path_in_str}...')
+
+        # On lit la premiere feuille avec les commentaires
+        sheet = pd.read_excel(path, 2, header=1)
+        sheet.columns = ["axe", "sous_axe", "sous_sous_axe", "num_action", "titre", "description", "objectifs", "resultats_attendus",
+         "cibles", "structure_pilote", "moyens", "partenaires", "personne_referente", "elu_referent", "financements",
+         "budget", "statut", "priorite", "date_debut", "date_fin", "amelioration_continue", "calendrier", "notes"]
+        sheet = sheet[sheet['titre'].notna()]
+        save_path = Path("./").joinpath(path.parts[-1].replace('.xlsx', '.csv'))
+        sheet.to_csv(save_path, index=False, header=True, encoding='UTF-8')
