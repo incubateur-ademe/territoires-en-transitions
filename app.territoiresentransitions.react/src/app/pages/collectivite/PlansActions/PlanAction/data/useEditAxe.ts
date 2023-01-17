31c104ef1a115ccac26f7117371967c5dc45fa89
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-17 12:26:54 +0100

    Crée les hook d'ajout et d'edition d'axe

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/useEditAxe.ts
@@ -0,0 +1,32 @@
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useMutation, useQueryClient} from 'react-query';
+
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {TPlanActionAxeUpdate} from './types/alias';
+
+const updateAxe = async (axe: TPlanActionAxeUpdate) => {
+  let query = supabaseClient.from('axe').update(axe).eq('id', axe.id);
+
+  const {error, data} = await query;
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
+
+/**
+ * Édite un axe dans un plan d'action
+ */
+export const useEditAxe = (planId: number) => {
+  const queryClient = useQueryClient();
+  const collectivite_id = useCollectiviteId();
+
+  return useMutation(updateAxe, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['plans_actions', collectivite_id]);
+      queryClient.invalidateQueries(['plan_action', planId]);
+    },
+  });
+};
