3d9a2b7e10557c60d5002a5c7f7fdca90fc7e16c
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-17 12:28:58 +0100

    Crée un composant réutilisable pour ajouter un titre ou créer une fiche dans un axe

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/AxeActions.tsx
@@ -0,0 +1,29 @@
+import {useCreateFicheAction} from '../FicheAction/data/useUpsertFicheAction';
+import {useAddAxe} from './data/useUpsertAxe';
+
+type Props = {
+  axeId: number;
+  planActionId: number;
+};
+
+export const AxeActions = ({axeId, planActionId}: Props) => {
+  const {mutate: addAxe} = useAddAxe(axeId, planActionId);
+  const {mutate: createFiche} = useCreateFicheAction({axeId, planActionId});
+
+  return (
+    <div className="flex items-center gap-6">
+      <button
+        className="fr-btn fr-btn--secondary fr-btn--sm"
+        onClick={() => addAxe()}
+      >
+        Ajouter un nouveau titre
+      </button>
+      <button
+        className="fr-btn fr-btn--secondary fr-btn--sm"
+        onClick={() => createFiche()}
+      >
+        Créer une fiche action
+      </button>
+    </div>
+  );
+};
