41c63082f4541b6898dad370bd27f3d79a7868f9
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-17 12:29:51 +0100

    refacto PlanAction et PlanActionAxe pour pouvoir créer et éditer les axes d'un plan

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanAction.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanAction.tsx
@@ -1,10 +1,17 @@
-import {useCollectiviteId} from 'core-logic/hooks/params';
+import {useRef, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import {TPlanAction} from './data/types/PlanAction';
-import {usePlanAction} from './data/usePlanAction';
-import PlanActionSousAxe from './PlanActionSousAxe';
+
 import PlanActionHeader from './PlanActionHeader';
 import PlanActionAxe from './PlanActionAxe';
+import PictoLeaf from 'ui/pictogrammes/PictoLeaf';
+import {AxeActions} from './AxeActions';
+import FicheActionCard from '../FicheAction/FicheActionCard';
+
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {makeCollectivitePlanActionFicheUrl} from 'app/paths';
+import {usePlanAction} from './data/usePlanAction';
+import {useEditAxe} from './data/useEditAxe';
+import {TPlanAction} from './data/types/PlanAction';
 
 type PlanActionProps = {
   plan: TPlanAction;
@@ -13,8 +20,21 @@ type PlanActionProps = {
 export const PlanAction = ({plan}: PlanActionProps) => {
   const collectivite_id = useCollectiviteId();
 
+  const {mutate: updatePlan} = useEditAxe(plan.id);
+
+  const [titre, setTitre] = useState(plan.nom);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEditButtonClick = () => {
+    if (inputRef && inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   const displaySousAxe = (axe: TPlanAction) => (
-    <PlanActionSousAxe
+    <PlanActionAxe
+      key={axe.id}
       plan_id={plan.id}
       axe={axe}
       displayAxe={displaySousAxe}
@@ -24,20 +44,70 @@ export const PlanAction = ({plan}: PlanActionProps) => {
   return (
     <div className="w-full">
       <div className="bg-indigo-400">
-        <h4 className="max-w-4xl mx-auto m-0 py-8 px-10 text-white">
-          {plan.nom ?? 'Sans titre'}
+        <h4 className="max-w-4xl flex items-center mx-auto m-0 py-8 px-10 text-white">
+          <input
+            ref={inputRef}
+            type="text"
+            className="w-full placeholder:text-white focus:placeholder:text-gray-200"
+            value={titre}
+            placeholder={'Sans titre'}
+            onChange={evt => setTitre(evt.target.value)}
+            onBlur={() =>
+              titre &&
+              titre.length > 0 &&
+              titre !== plan.nom &&
+              updatePlan({id: plan.id, nom: titre})
+            }
+          />
+          <button
+            className="fr-fi-edit-line w-10 h-10"
+            onClick={handleEditButtonClick}
+          />
         </h4>
       </div>
       <div className="max-w-4xl mx-auto px-10">
         <PlanActionHeader plan={plan} collectivite_id={collectivite_id!} />
-        {plan.enfants.map(enfant => (
-          <PlanActionAxe
-            key={enfant.id}
-            plan_id={plan.id}
-            axe={enfant}
-            displayAxe={displaySousAxe}
-          />
-        ))}
+        {plan.enfants || plan.fiches ? (
+          <>
+            <div className="mb-4">
+              <AxeActions planActionId={plan.id} axeId={plan.id} />
+              {plan.fiches && (
+                <div className="grid grid-cols-2 gap-4 mt-6">
+                  {plan.fiches.map(fiche => (
+                    <FicheActionCard
+                      key={fiche.id}
+                      ficheAction={fiche}
+                      link={makeCollectivitePlanActionFicheUrl({
+                        collectiviteId: fiche.collectivite_id!,
+                        planActionUid: plan.id.toString(),
+                        ficheUid: fiche.id!.toString(),
+                      })}
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+            {plan.enfants &&
+              plan.enfants.map(enfant => (
+                <PlanActionAxe
+                  key={enfant.id}
+                  plan_id={plan.id}
+                  axe={enfant}
+                  displayAxe={displaySousAxe}
+                />
+              ))}
+          </>
+        ) : (
+          <div>
+            <div className="flex flex-col items-center mt-8">
+              <PictoLeaf className="w-24 fill-gray-400" />
+              <div className="my-6 text-gray-500">
+                Aucune arborescence pour l'instant
+              </div>
+              <AxeActions planActionId={plan.id} axeId={plan.id} />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionHeader.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionHeader.tsx
@@ -1,6 +1,3 @@
-import {makeCollectivitePlanActionUrl} from 'app/paths';
-import {Link} from 'react-router-dom';
-
 import {TPlanAction} from './data/types/PlanAction';
 
 type TPlanActionHeader = {
@@ -12,17 +9,6 @@ const PlanActionHeader = ({collectivite_id, plan}: TPlanActionHeader) => {
   return (
     <div className="">
       <div className="py-6 flex justify-between">
-        <Link
-          className="p-1 underline text-gray-500 !shadow-none hover:text-gray-600"
-          to={() =>
-            makeCollectivitePlanActionUrl({
-              collectiviteId: collectivite_id,
-              planActionUid: plan.id.toString(),
-            })
-          }
-        >
-          {plan.nom}
-        </Link>
         {/** Actions */}
         {/* <div className="flex items-center gap-4">
           <div className="border border-gray-300">
