4be40bb720cc972dcb6dc39c564dd1d278868076
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-17 12:29:51 +0100

    refacto PlanAction et PlanActionAxe pour pouvoir créer et éditer les axes d'un plan

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionAxe.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionAxe.tsx
@@ -1,9 +1,13 @@
-import {makeCollectivitePlanActionFicheUrl} from 'app/paths';
+import {useRef, useState} from 'react';
 import classNames from 'classnames';
-import {useState} from 'react';
-import {TFicheAction} from '../FicheAction/data/types/alias';
+
 import FicheActionCard from '../FicheAction/FicheActionCard';
+import {AxeActions} from './AxeActions';
+
+import {makeCollectivitePlanActionFicheUrl} from 'app/paths';
+import {TFicheAction} from '../FicheAction/data/types/alias';
 import {TPlanAction} from './data/types/PlanAction';
+import {useEditAxe} from './data/useEditAxe';
 
 type Props = {
   plan_id: number;
@@ -12,25 +16,74 @@ type Props = {
 };
 
 const PlanActionAxe = ({plan_id, axe, displayAxe}: Props) => {
+  const {mutate: updatePlan} = useEditAxe(plan_id);
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const [isEditable, setIsEditable] = useState(false);
+  const [nom, setNom] = useState(axe.nom);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEditButtonClick = () => {
+    setIsEditable(true);
+    setTimeout(() => {
+      if (inputRef && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 100);
+  };
 
   return (
     <div>
-      <button
-        className="flex items-center py-4 w-full"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div
-          className={classNames('fr-fi-arrow-right-s-line scale-90 mt-1 mr-4', {
-            'rotate-90': isOpen,
-          })}
+      <div className="group relative flex items-center">
+        <button
+          className="flex items-center py-3 pr-4 w-full"
+          onClick={() => !isEditable && setIsOpen(!isOpen)}
+        >
+          <div
+            className={classNames(
+              'fr-fi-arrow-right-s-line scale-90 mt-1 mr-3',
+              {
+                'rotate-90': isOpen,
+              }
+            )}
+          />
+          <input
+            ref={inputRef}
+            type="text"
+            className={classNames(
+              'w-full mb-0 text-left disabled:cursor-pointer disabled:text-gray-900',
+              {
+                'font-bold': isOpen && !isEditable,
+                'placeholder:text-gray-900': !isEditable,
+              }
+            )}
+            value={nom}
+            placeholder={'Sans titre'}
+            disabled={!isEditable}
+            onChange={e => {
+              setNom(e.target.value);
+            }}
+            onBlur={e => {
+              nom &&
+                nom.length > 0 &&
+                nom !== axe.nom &&
+                updatePlan({id: axe.id, nom: nom ?? null});
+              setIsEditable(false);
+            }}
+          />
+        </button>
+        <button
+          className="fr-fi-edit-line hidden group-hover:block p-2 text-gray-500 scale-90"
+          onClick={handleEditButtonClick}
         />
-        <h6 className="mb-0 text-left">{axe.nom}</h6>
-      </button>
+      </div>
       {isOpen && (
-        <div className="ml-12">
+        <div className="flex flex-col gap-4 mt-3 ml-12">
+          <AxeActions planActionId={plan_id} axeId={axe.id} />
           {axe.fiches && (
-            <div className="flex flex-col gap-4 my-6">
+            <div className="grid grid-cols-2 gap-4">
               {axe.fiches.map((fiche: TFicheAction) => (
                 <FicheActionCard
                   key={fiche.id}
@@ -44,8 +97,10 @@ const PlanActionAxe = ({plan_id, axe, displayAxe}: Props) => {
               ))}
             </div>
           )}
-          {axe.enfants &&
-            axe.enfants.map((axe: TPlanAction) => displayAxe(axe))}
+          <div>
+            {axe.enfants &&
+              axe.enfants.map((axe: TPlanAction) => displayAxe(axe))}
+          </div>
         </div>
       )}
     </div>
