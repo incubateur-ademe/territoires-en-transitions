6990c801f54ca5db3886031aae4198a296f2c2fe
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-17 12:26:54 +0100

    Crée les hook d'ajout et d'edition d'axe

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useUpsertFicheAction.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useUpsertFicheAction.ts
@@ -3,7 +3,10 @@ import {supabaseClient} from 'core-logic/api/supabase';
 import {useMutation, useQueryClient} from 'react-query';
 
 import {useCollectiviteId} from 'core-logic/hooks/params';
-import {makeCollectiviteFicheNonClasseeUrl} from 'app/paths';
+import {
+  makeCollectiviteFicheNonClasseeUrl,
+  makeCollectivitePlanActionFicheUrl,
+} from 'app/paths';
 import {FicheActionVueRow} from './types/ficheActionVue';
 
 /** Upsert une fiche action pour une collectivité */
@@ -24,24 +27,51 @@ const upsertFicheAction = async (fiche: FicheActionVueRow) => {
 
 /**
  * Crée une nouvelle fiche action pour une collectivité
+ * Si l'id d'un axe est donnée en argument, la fiche est ajoutée à cet axe
  */
-export const useCreateFicheAction = () => {
+type Args = {
+  axeId?: number;
+  planActionId?: number;
+};
+
+export const useCreateFicheAction = (args?: Args) => {
   const queryClient = useQueryClient();
   const collectivite_id = useCollectiviteId();
   const history = useHistory();
 
   return useMutation(
-    () => upsertFicheAction({collectivite_id: collectivite_id!} as never),
+    () =>
+      upsertFicheAction({
+        collectivite_id: collectivite_id!,
+        axes:
+          args && args.axeId
+            ? [{id: args?.axeId, collectivite_id: collectivite_id!}]
+            : null,
+      } as never),
     {
       meta: {disableToast: true},
       onSuccess: data => {
-        queryClient.invalidateQueries(['fiches_non_classees', collectivite_id]);
-        history.push(
-          makeCollectiviteFicheNonClasseeUrl({
-            collectiviteId: collectivite_id!,
-            ficheUid: data[0].id!.toString(),
-          })
-        );
+        if (args && args?.axeId) {
+          queryClient.invalidateQueries(['plan_action', args.planActionId]);
+          history.push(
+            makeCollectivitePlanActionFicheUrl({
+              collectiviteId: collectivite_id!,
+              ficheUid: data[0].id!.toString(),
+              planActionUid: args.planActionId!.toString(),
+            })
+          );
+        } else {
+          queryClient.invalidateQueries([
+            'fiches_non_classees',
+            collectivite_id,
+          ]);
+          history.push(
+            makeCollectiviteFicheNonClasseeUrl({
+              collectiviteId: collectivite_id!,
+              ficheUid: data[0].id!.toString(),
+            })
+          );
+        }
       },
     }
   );
---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/useUpsertAxe.ts
@@ -0,0 +1,69 @@
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useMutation, useQueryClient} from 'react-query';
+
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {TPlanActionAxeRow, TPlanActionAxeUpdate} from './types/alias';
+import {useHistory} from 'react-router-dom';
+import {makeCollectivitePlanActionUrl} from 'app/paths';
+
+/**
+ * Upsert un axe pour une collectivité.
+ * S'il n'a pas de parent, alors cela est considéré comme un nouveau plan
+ */
+export const upsertAxe = async (axe: TPlanActionAxeRow) => {
+  let query = supabaseClient.from('axe').upsert(axe).select();
+
+  const {error, data} = await query;
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
+
+/**
+ * Crée un nouveau plan d'action pour une collectivité
+ */
+export const useCreatePlanAction = () => {
+  const queryClient = useQueryClient();
+  const collectivite_id = useCollectiviteId();
+  const history = useHistory();
+
+  return useMutation(
+    // Considéré comme un plan d'action car on ne donne pas de parent
+    () => upsertAxe({collectivite_id: collectivite_id!} as never),
+    {
+      meta: {disableToast: true},
+      onSuccess: data => {
+        queryClient.invalidateQueries(['plans_actions', collectivite_id]);
+        history.push(
+          makeCollectivitePlanActionUrl({
+            collectiviteId: collectivite_id!,
+            planActionUid: data[0].id!.toString(),
+          })
+        );
+      },
+    }
+  );
+};
+
+/**
+ * Ajoute un sous-axe à un axe
+ */
+export const useAddAxe = (parentId: number, planActionId: number) => {
+  const queryClient = useQueryClient();
+  const collectivite_id = useCollectiviteId();
+
+  return useMutation(
+    // Considéré comme un axe car on fourni un parent
+    () =>
+      upsertAxe({collectivite_id: collectivite_id!, parent: parentId} as never),
+    {
+      meta: {disableToast: true},
+      onSuccess: data => {
+        queryClient.invalidateQueries(['plan_action', planActionId]);
+      },
+    }
+  );
+};
