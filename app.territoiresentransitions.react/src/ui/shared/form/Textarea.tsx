b66df13dfa9badb4fa0b365158e4386ed5b18a62
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-18 09:58:09 +0100

    Retravaille Textarea et useAutoSizeTextarea pour pouvoir recevoir une ref custom

--- app.territoiresentransitions.react/src/ui/shared/form/Textarea.tsx
+++ app.territoiresentransitions.react/src/ui/shared/form/Textarea.tsx
@@ -1,41 +1,56 @@
 import classNames from 'classnames';
-import React, {TextareaHTMLAttributes} from 'react';
+import React, {
+  ForwardedRef,
+  forwardRef,
+  RefObject,
+  TextareaHTMLAttributes,
+} from 'react';
 import {useAutoSizeTextarea} from 'ui/shared/form/useAutoSizeTextarea';
 
 export type TTextarea<T> = {
+  // ref?: RefObject<HTMLTextAreaElement>;
   onInputChange?: (value: string) => void;
   /** Minimum height pour le textarea. Valeur défaut "2.25rem" */
   minHeight?: string;
 } & TextareaHTMLAttributes<T>;
 
 /** Composant générique Textarea avec auto-resize */
-const Textarea = <T extends HTMLTextAreaElement>({
-  value,
-  onInputChange,
-  placeholder,
-  className,
-  minHeight = '2.25rem',
-  ...props
-}: TTextarea<T>) => {
-  // Appelée au changement de valeur du textarea
-  const textareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    onInputChange && onInputChange(event.target.value);
-  };
+const Textarea = forwardRef(
+  <T extends HTMLTextAreaElement>(
+    {
+      value,
+      onInputChange,
+      placeholder,
+      className,
+      minHeight = '2.25rem',
+      ...props
+    }: TTextarea<T>,
+    ref: ForwardedRef<HTMLTextAreaElement> | null
+  ) => {
+    // Appelée au changement de valeur du textarea
+    const textareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      onInputChange && onInputChange(event.target.value);
+    };
 
-  const textareaRef = useAutoSizeTextarea(value?.toString(), minHeight);
+    const textareaRef = useAutoSizeTextarea(
+      value?.toString(),
+      minHeight,
+      ref as RefObject<HTMLTextAreaElement>
+    );
 
-  return (
-    <textarea
-      ref={textareaRef}
-      className={classNames(
-        `w-full py-2 px-3 text-sm cursor-text rounded-lg outline outline-offset-0 !outline-1 outline-gray-300 focus:outline-blue-500 ${className}`
-      )}
-      onChange={textareaChange}
-      value={value}
-      placeholder={placeholder}
-      {...props}
-    />
-  );
-};
+    return (
+      <textarea
+        ref={textareaRef}
+        className={classNames(
+          `w-full py-2 px-3 text-sm cursor-text rounded-lg outline outline-offset-0 !outline-1 outline-gray-300 focus:outline-blue-500 ${className}`
+        )}
+        onChange={textareaChange}
+        value={value}
+        placeholder={placeholder}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Textarea;
--- app.territoiresentransitions.react/src/ui/shared/form/useAutoSizeTextarea.ts
+++ app.territoiresentransitions.react/src/ui/shared/form/useAutoSizeTextarea.ts
@@ -1,22 +1,28 @@
-import {useEffect, useRef} from 'react';
+import {RefObject, useLayoutEffect, useRef} from 'react';
 
 /**
  * Rend un champ de saisi qui se redimensionne en fonction de son contenu.
  * copié/adapté depuis: https://www.kindacode.com/article/react-typescript-create-an-autosize-textarea-from-scratch/
  * (il ne parait pas nécessaire d'ajouter une dépendance supplémentaire pour cette fonctionnalité)
  */
-export const useAutoSizeTextarea = (value?: string, minHeight?: string) => {
+export const useAutoSizeTextarea = (
+  value?: string,
+  minHeight?: string,
+  ref?: RefObject<HTMLTextAreaElement> | null
+) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
+  const usedRef = ref ? ref : textareaRef;
+
   // Permet de set la taille du textarea au changement de valeur
-  useEffect(() => {
-    if (textareaRef && textareaRef.current) {
-      textareaRef.current.style.height = '0px';
-      textareaRef.current.style.minHeight = minHeight ?? '2.5rem';
-      const scrollHeight = textareaRef.current.scrollHeight;
-      textareaRef.current.style.height = scrollHeight + 'px';
+  useLayoutEffect(() => {
+    if (usedRef && usedRef.current) {
+      usedRef.current.style.height = '0px';
+      usedRef.current.style.minHeight = minHeight ?? '2.5rem';
+      const scrollHeight = usedRef.current.scrollHeight;
+      usedRef.current.style.height = scrollHeight + 'px';
     }
   }, [value]);
 
-  return textareaRef;
+  return usedRef;
 };
