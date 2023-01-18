2fab4368ecfd229abdea078bd93f1e22a9f5d2ed
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-18 17:46:03 +0100

    Désactive le champs date de fin et le clean si amelioration continue est cochée

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/FicheActionFormDateInput.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/FicheActionFormDateInput.tsx
@@ -1,4 +1,4 @@
-import {InputHTMLAttributes, useState} from 'react';
+import {InputHTMLAttributes, useEffect, useState} from 'react';
 import {format} from 'date-fns';
 
 type Props<T> = {
@@ -7,20 +7,29 @@ type Props<T> = {
 
 const FicheActionFormDateInput = <T extends HTMLInputElement>({
   initialValue,
+  disabled,
   onBlur,
+  ...props
 }: Props<T>) => {
-  const [value, setValue] = useState(
-    initialValue ? format(new Date(initialValue), 'yyyy-MM-dd') : undefined
-  );
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    setValue(initialValue ? format(new Date(initialValue), 'yyyy-MM-dd') : '');
+  }, [initialValue]);
+
   return (
     <div className="fr-input-wrap fr-fi-calendar-line">
       <input
         type="date"
         className="fr-input w-full p-2"
         pattern="\d{4}-\d{2}-\d{2}"
-        value={value}
-        onChange={e => setValue(e.target.value)}
+        value={value ?? ''}
+        onChange={e => {
+          setValue(e.target.value);
+        }}
         onBlur={onBlur}
+        disabled={disabled}
+        {...props}
       />
     </div>
   );
