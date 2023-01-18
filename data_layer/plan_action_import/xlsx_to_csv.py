3b791948643c2704bc97e64cdf3778aaf73a0841
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-01-18 15:22:29 +0100

    Ajout endpoint pour importer plan action format csv

--- data_layer/plan_action_import/xlsx_to_csv.py
+++ data_layer/plan_action_import/xlsx_to_csv.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
         print(f'reading {path_in_str}...')
 
         # On lit la premiere feuille avec les commentaires
-        sheet = pd.read_excel(path, 2, header=1)
+        sheet = pd.read_excel(path, 0, header=1, dtype="object")
         sheet.columns = ["axe", "sous_axe", "sous_sous_axe", "num_action", "titre", "description", "objectifs", "resultats_attendus",
          "cibles", "structure_pilote", "moyens", "partenaires", "personne_referente", "elu_referent", "financements",
-         "budget", "statut", "priorite", "date_debut", "date_fin", "amelioration_continue", "calendrier", "notes"]
+         "budget", "statut", "priorite", "date_debut", "date_fin", "amelioration_continue", "calendrier", "notes", "collectivite_id" ,"plan_nom"]
         sheet = sheet[sheet['titre'].notna()]
         save_path = Path("./").joinpath(path.parts[-1].replace('.xlsx', '.csv'))
         sheet.to_csv(save_path, index=False, header=True, encoding='UTF-8')
