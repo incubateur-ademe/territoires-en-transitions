1e35bbb289be35c74e64e7503ba6a7c66213aaf8
Arthur Molinos <arthur.molinos@gmail.com>
2023-01-19 11:34:34 +0100

    Redirige vers le plan d'action quand on supprime un fiche dans un plan d'action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useDeleteFicheAction.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useDeleteFicheAction.ts
@@ -2,8 +2,11 @@ import {supabaseClient} from 'core-logic/api/supabase';
 import {useMutation, useQueryClient} from 'react-query';
 
 import {useCollectiviteId} from 'core-logic/hooks/params';
-import {useHistory} from 'react-router-dom';
-import {makeCollectivitePlansActionsBaseUrl} from 'app/paths';
+import {useHistory, useParams} from 'react-router-dom';
+import {
+  makeCollectivitePlanActionUrl,
+  makeCollectivitePlansActionsBaseUrl,
+} from 'app/paths';
 
 /** Supprime une fiche action d'une collectivité */
 const deleteFicheAction = async (ficheId: number) => {
@@ -25,14 +28,27 @@ export const useDeleteFicheAction = () => {
   const queryClient = useQueryClient();
   const collectivite_id = useCollectiviteId();
   const history = useHistory();
+  const {planUid} = useParams<{planUid: string}>();
 
   return useMutation(deleteFicheAction, {
     meta: {disableToast: true},
     onSuccess: () => {
       queryClient.invalidateQueries(['fiches_non_classees', collectivite_id]);
-      history.push(
-        makeCollectivitePlansActionsBaseUrl({collectiviteId: collectivite_id!})
-      );
+      queryClient.invalidateQueries(['plan_action', planUid]);
+      if (planUid) {
+        history.push(
+          makeCollectivitePlanActionUrl({
+            collectiviteId: collectivite_id!,
+            planActionUid: planUid,
+          })
+        );
+      } else {
+        history.push(
+          makeCollectivitePlansActionsBaseUrl({
+            collectiviteId: collectivite_id!,
+          })
+        );
+      }
     },
   });
 };
