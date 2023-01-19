673551df201618c74ff374f33cef5c315024dd6c
Florian d'Erfurth <florian@derfurth.com>
2023-01-19 11:24:37 +0100

    Ajoute l'audit au parcours

--- data_layer/sqitch/verify/labellisation/demande.sql
+++ data_layer/sqitch/verify/labellisation/demande.sql
@@ -3,6 +3,8 @@
 BEGIN;
 
 select has_function_privilege('labellisation_demande(integer, referentiel, labellisation.etoile)', 'execute');
-select has_function_privilege('labellisation_submit_demande(integer, referentiel, labellisation.etoile)', 'execute');
+select has_function_privilege('labellisation_submit_demande(integer, referentiel, labellisation.etoile, labellisation.sujet_demande)', 'execute');
+select has_function_privilege('labellisation.validation_demande()', 'execute');
+select has_function_privilege('labellisation_parcours(integer)', 'execute');
 
 ROLLBACK;
