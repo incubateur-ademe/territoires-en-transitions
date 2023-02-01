5bfe87d0845bfe08b90d36266a1501c86ad31371
Marc Rutkowski <marc@attractive-media.fr>
2023-02-01 11:12:01 +0100

    met à jour l'en-tête de labellisation pour gérer la demande d'audit

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/HeaderLabellisation.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/HeaderLabellisation.tsx
@@ -5,104 +5,76 @@
 import {ReactNode, useState} from 'react';
 import {useReferentielId} from 'core-logic/hooks/params';
 import {PageHeaderLeft} from 'ui/PageHeader';
-import {useHasActiveCOT, useIsAuditeur} from '../Audit/useAudit';
 import {ValiderAudit} from '../Audit/ValiderAudit';
 import {DemandeLabellisationModal} from './DemandeLabellisationModal';
 import {numLabels} from './numLabels';
-import {useParcoursLabellisation} from './useParcoursLabellisation';
-import {useValidateAudit} from './useValidateAudit';
-
-type TParcoursLabellisation = ReturnType<typeof useParcoursLabellisation>;
+import {
+  useCycleLabellisation,
+  TCycleLabellisation,
+} from './useCycleLabellisation';
+import {useStartAudit, TStartAudit} from './useStartAudit';
+import {useValidateAudit, TValidateAudit} from './useValidateAudit';
+import {TEtoiles} from './types';
 
 export type THeaderLabellisationProps = {
-  parcours: TParcoursLabellisation['parcours'];
-  demande: TParcoursLabellisation['demande'];
-  audit: TParcoursLabellisation['audit'];
-  isAuditeur: boolean;
-  onStartAudit: () => void;
-  onValidateAudit: ReturnType<typeof useValidateAudit>['mutate'];
+  parcoursLabellisation: TCycleLabellisation;
+  onStartAudit: TStartAudit;
+  onValidateAudit: TValidateAudit;
 };
 
 export const HeaderLabellisation = (props: THeaderLabellisationProps) => {
   const [opened, setOpened] = useState(false);
-  const {parcours, demande, audit, isAuditeur, onStartAudit, onValidateAudit} =
-    props;
+  const {parcoursLabellisation, onStartAudit, onValidateAudit} = props;
+  const {parcours, status, isAuditeur, isCOT, labellisable} =
+    parcoursLabellisation;
 
   if (!parcours) {
     return null;
   }
 
-  const {
-    etoiles,
-    completude_ok,
-    rempli,
-    derniere_demande,
-    derniere_labellisation,
-  } = parcours;
-
-  const demande_envoyee = demande && !demande.en_cours && derniere_demande;
-  const audit_en_cours = audit && !audit.valide;
-  const audit_valide = audit && audit.valide;
-
-  // on peut soumettre la demande de labellisation si...
-  const canSubmit =
-    // la demande est toujours en cours
-    !demande_envoyee &&
-    // et le référentiel est rempli
-    completude_ok &&
-    // et tous les critères sont atteints
-    rempli;
+  const headerMessageContent = getHeaderMessageContent(parcoursLabellisation);
+  const {collectivite_id, referentiel, etoiles, audit} = parcours;
+  const canSubmitDemande = labellisable || isCOT;
 
   return (
     <PageHeaderLeft>
-      {derniere_labellisation ? (
-        <p className="m-0">
-          <span className="capitalize">
-            {numLabels[derniere_labellisation.etoiles]}
-          </span>{' '}
-          étoile depuis le{' '}
-          {new Date(derniere_labellisation.obtenue_le).toLocaleDateString(
-            'fr-FR',
-            {dateStyle: 'long'}
-          )}
-        </p>
-      ) : null}
+      <DerniereLabellisation parcoursLabellisation={parcoursLabellisation} />
       <h2 className="fr-mb-2w">Objectif : {numLabels[etoiles]} étoile</h2>
-      {!demande_envoyee && !audit_en_cours && !isAuditeur ? (
+      {status === 'non_demandee' && !isAuditeur ? (
         <button
           className="fr-btn self-start"
-          disabled={!canSubmit}
+          disabled={!canSubmitDemande}
           onClick={() => setOpened(true)}
         >
-          {etoiles === '1'
+          {etoiles === '1' && !isCOT
             ? 'Demander la première étoile'
             : 'Demander un audit'}
         </button>
       ) : null}
-      {demande_envoyee && !audit_en_cours && isAuditeur ? (
-        <button className="fr-btn self-start" onClick={onStartAudit}>
+      {status === 'demande_envoyee' && isAuditeur ? (
+        <button
+          className="fr-btn self-start"
+          onClick={() =>
+            audit &&
+            referentiel &&
+            onStartAudit({collectivite_id, referentiel, audit_id: audit.id})
+          }
+        >
           Commencer l'audit
         </button>
       ) : null}
-      {demande_envoyee && !audit ? (
-        <HeaderMessage>Demande envoyée</HeaderMessage>
-      ) : null}
-      {audit_valide ? (
-        <HeaderMessage>Labellisation en cours</HeaderMessage>
-      ) : null}
-      {audit_en_cours && !isAuditeur ? (
-        <HeaderMessage>Audit en cours</HeaderMessage>
-      ) : null}
-      {audit_en_cours && isAuditeur ? (
+      {headerMessageContent && (
+        <HeaderMessage>{headerMessageContent}</HeaderMessage>
+      )}
+      {status === 'audit_en_cours' && isAuditeur ? (
         <ValiderAudit
-          audit={audit}
-          onValidate={audit => audit && onValidateAudit(audit)}
+          audit={audit!}
+          onValidate={audit => onValidateAudit(audit!)}
         />
       ) : null}
-      {demande && !demande_envoyee ? (
+      {status === 'non_demandee' || status === 'demande_envoyee' ? (
         <DemandeLabellisationModal
-          demande={demande}
-          parcours={parcours}
+          parcoursLabellisation={parcoursLabellisation}
           opened={opened}
           setOpened={setOpened}
         />
@@ -111,30 +83,71 @@ export const HeaderLabellisation = (props: THeaderLabellisationProps) => {
   );
 };
 
+// renvoi le message d'entête correspondant au positionnement de la collectivité
+// dans le parcours de labellisation
+const getHeaderMessageContent = (
+  parcoursLabellisation: TCycleLabellisation
+) => {
+  const {status, isAuditeur} = parcoursLabellisation;
+
+  if (status === 'demande_envoyee') {
+    return 'Demande envoyée';
+  }
+
+  if (status === 'audit_en_cours' && !isAuditeur) {
+    return 'Audit en cours';
+  }
+
+  if (status === 'audit_valide') {
+    return 'Labellisation en cours';
+  }
+
+  // pas de message dans les autres cas
+  return null;
+};
+
 const HeaderMessage = ({children}: {children: ReactNode}) => (
   <p className="m-0 fr-text-mention--grey" data-test="HeaderMessage">
     {children}
   </p>
 );
 
-const HeaderLabellisationConnected = () => {
-  const hasActiveCOT = useHasActiveCOT();
+/** Message  "<n-ième> étoile depuis le <date obtention>" */
+const DerniereLabellisation = ({
+  parcoursLabellisation,
+}: {
+  parcoursLabellisation: TCycleLabellisation;
+}) => {
+  const {parcours} = parcoursLabellisation;
+  const {labellisation} = parcours || {};
+  const {etoiles, obtenue_le} = labellisation || {};
 
+  if (!etoiles) {
+    return null;
+  }
+
+  return (
+    <p className="m-0">
+      <span className="capitalize">
+        {numLabels[etoiles as unknown as TEtoiles]}
+      </span>{' '}
+      étoile depuis le{' '}
+      {new Date(obtenue_le!).toLocaleDateString('fr-FR', {
+        dateStyle: 'long',
+      })}
+    </p>
+  );
+};
+
+const HeaderLabellisationConnected = () => {
   const referentiel = useReferentielId();
-  const parcoursLabellisation = useParcoursLabellisation(referentiel);
-  const isAuditeur = useIsAuditeur();
-  const {parcours, demande, audit} = parcoursLabellisation;
-  const onStartAudit = () => {
-    console.log('TODO');
-  };
+  const parcoursLabellisation = useCycleLabellisation(referentiel);
+  const {mutate: onStartAudit} = useStartAudit();
   const {mutate: onValidateAudit} = useValidateAudit();
 
-  return parcours || hasActiveCOT ? (
+  return parcoursLabellisation?.parcours ? (
     <HeaderLabellisation
-      parcours={parcours}
-      demande={demande}
-      audit={audit}
-      isAuditeur={isAuditeur}
+      parcoursLabellisation={parcoursLabellisation}
       onStartAudit={onStartAudit}
       onValidateAudit={onValidateAudit}
     />
