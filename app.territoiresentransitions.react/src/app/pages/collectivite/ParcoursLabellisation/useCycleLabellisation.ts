09847f1781d8d83f4a5779b02f7f5c7dd49dd6c4
Marc Rutkowski <marc@attractive-media.fr>
2023-02-01 10:34:01 +0100

    met à jour le chargement des données du cycle courant de labellisation/audit d'une collectivité et détermine un état consolidé

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useCycleLabellisation.ts
@@ -0,0 +1,125 @@
+import {useQuery} from 'react-query';
+import {TLabellisationParcours} from 'app/pages/collectivite/ParcoursLabellisation/types';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {useIsAuditeur} from '../Audit/useAudit';
+import {useCarteIdentite} from '../PersoReferentielThematique/useCarteIdentite';
+
+// données du cycle de labellisation/audit actuel d'une collectivité
+export type TCycleLabellisation = {
+  parcours: TLabellisationParcours | null;
+  status: TCycleLabellisationStatus;
+  isAuditeur: boolean;
+  isCOT: boolean;
+  labellisable: boolean;
+};
+
+// état consolidé du cycle de labellisation/audit
+type TCycleLabellisationStatus =
+  | 'non_demandee'
+  | 'demande_envoyee'
+  | 'audit_en_cours'
+  | 'audit_valide';
+
+/** Renvoie les données de labellisation/audit de la collectivité courante */
+export const useCycleLabellisation = (
+  referentiel: string | null
+): TCycleLabellisation => {
+  const collectivite_id = useCollectiviteId();
+  const isAuditeur = useIsAuditeur();
+  const identite = useCarteIdentite(collectivite_id);
+
+  // charge les données du parcours
+  const {data: parcoursList} = useQuery(
+    ['labellisation_parcours', collectivite_id],
+    () => fetchParcours(collectivite_id)
+  );
+
+  // extrait le parcours correspondant au référentiel courant
+  const parcours = getReferentielParcours(parcoursList, referentiel);
+  const {completude_ok, rempli} = parcours || {};
+
+  // états dérivés
+  const status = getParcoursStatus(parcours);
+  const isCOT = Boolean(identite?.is_cot);
+  // on peut soumettre la demande de labellisation si...
+  const labellisable = Boolean(
+    // pas d'audit ou de labellisation demandée
+    status === 'non_demandee' &&
+      // et le référentiel est rempli
+      completude_ok &&
+      // et tous les critères sont atteints
+      rempli
+  );
+
+  return {
+    parcours,
+    status,
+    isAuditeur,
+    isCOT,
+    labellisable,
+  };
+};
+
+// détermine l'état consolidé du cycle
+const getParcoursStatus = (
+  parcours: TLabellisationParcours | null
+): TCycleLabellisationStatus => {
+  if (!parcours) {
+    return 'non_demandee';
+  }
+  const {demande, audit} = parcours;
+  if (audit?.valide) {
+    return 'audit_valide';
+  }
+  if (audit?.date_debut) {
+    return 'audit_en_cours';
+  }
+  if (demande?.date) {
+    return 'demande_envoyee';
+  }
+  return 'non_demandee';
+};
+
+// charge les parcours (eci/cae) de labellisation d'une collectivité donnée
+const fetchParcours = async (
+  collectivite_id: number | null
+): Promise<TLabellisationParcours[] | null> => {
+  if (!collectivite_id) {
+    return null;
+  }
+
+  const {data, error} = await supabaseClient
+    .rpc('labellisation_parcours', {
+      collectivite_id,
+    })
+    .select();
+
+  if (error || !data) {
+    return null;
+  }
+  return data.map(d => ({
+    ...d,
+    collectivite_id, // on ajoute l'id qui n'est pas redonné par la vue
+  })) as TLabellisationParcours[];
+};
+
+export const getReferentielParcours = (
+  parcoursList: TLabellisationParcours[] | null | undefined,
+  referentiel: string | null
+) => {
+  const parcours: TLabellisationParcours | undefined = parcoursList?.find(
+    p => p.referentiel === referentiel
+  );
+
+  if (!parcours) {
+    return null;
+  }
+
+  const {criteres_action} = parcours;
+  return {
+    ...parcours,
+    // trie les critères action
+    criteres_action: criteres_action.sort((a, b) => a.prio - b.prio),
+  };
+};
