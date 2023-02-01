13339a8645c78f440633338ccfe0c1ce6c94e155
Marc Rutkowski <marc@attractive-media.fr>
2023-02-01 11:15:07 +0100

    ajoute le dialogue de choix du sujet de la demande d'audit pour les collectivités avec un COT actif

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/DemandeLabellisationModal.stories.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/DemandeLabellisationModal.stories.tsx
@@ -7,67 +7,107 @@ import {
 
 export default {
   component: DemandeLabellisationModal,
+  // on va désactiver les storyshots à cause du composant Dialog de mui
+  parameters: {storyshots: false},
 } as Meta;
 
 const Template: Story<TDemandeLabellisationModalProps> = args => (
   <DemandeLabellisationModal opened setOpened={action('setOpened')} {...args} />
 );
 
-// on va désactiver les storyshots à cause du composant Dialog de mui
-const parameters = {storyshots: false};
-
 export const Etoile1 = Template.bind({});
 Etoile1.args = {
-  demande: {en_cours: true},
-  parcours: {
-    etoiles: '1',
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    parcours: {
+      collectivite_id: 1,
+      referentiel: 'eci',
+      etoiles: '1',
+    },
   },
 };
-Etoile1.parameters = parameters;
 
 export const Etoile2_3_4_ECI = Template.bind({});
 Etoile2_3_4_ECI.args = {
-  demande: {en_cours: true},
-  parcours: {
-    etoiles: '2',
-    referentiel: 'eci',
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    parcours: {
+      collectivite_id: 1,
+      etoiles: '2',
+      referentiel: 'eci',
+    },
   },
 };
-Etoile2_3_4_ECI.parameters = parameters;
 
 export const Etoile2_3_4_CAE = Template.bind({});
 Etoile2_3_4_CAE.args = {
-  demande: {en_cours: true},
-  parcours: {
-    etoiles: '2',
-    referentiel: 'cae',
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    parcours: {
+      collectivite_id: 1,
+      etoiles: '2',
+      referentiel: 'cae',
+    },
   },
 };
-Etoile2_3_4_CAE.parameters = parameters;
 
 export const Etoile5 = Template.bind({});
 Etoile5.args = {
-  demande: {en_cours: true},
-  parcours: {
-    etoiles: '5',
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    parcours: {
+      collectivite_id: 1,
+      etoiles: '5',
+      referentiel: 'cae',
+    },
   },
 };
-Etoile5.parameters = parameters;
 
 export const Etoile1Envoyee = Template.bind({});
 Etoile1Envoyee.args = {
-  demande: {en_cours: false},
-  parcours: {
-    etoiles: '1',
+  parcoursLabellisation: {
+    status: 'demande_envoyee',
+    parcours: {
+      collectivite_id: 1,
+      etoiles: '1',
+    },
   },
 };
-Etoile1Envoyee.parameters = parameters;
 
 export const Etoile_2_3_4_5_Envoyee = Template.bind({});
 Etoile_2_3_4_5_Envoyee.args = {
-  demande: {en_cours: false},
-  parcours: {
-    etoiles: '2',
+  parcoursLabellisation: {
+    status: 'demande_envoyee',
+    parcours: {
+      collectivite_id: 1,
+      etoiles: '2',
+    },
   },
 };
-Etoile_2_3_4_5_Envoyee.parameters = parameters;
+
+export const NonLabellisableCOT = Template.bind({});
+NonLabellisableCOT.args = {
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    isCOT: true,
+    parcours: {
+      collectivite_id: 1,
+      referentiel: 'eci',
+      etoiles: '1',
+    },
+  },
+};
+
+export const LabellisableCOT = Template.bind({});
+LabellisableCOT.args = {
+  parcoursLabellisation: {
+    status: 'non_demandee',
+    labellisable: true,
+    isCOT: true,
+    parcours: {
+      collectivite_id: 1,
+      referentiel: 'eci',
+      etoiles: '1',
+    },
+  },
+};
