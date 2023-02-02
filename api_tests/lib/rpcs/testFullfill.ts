6d7b6a4edc682002b311a8f65a5733f44f461130
Florian d'Erfurth <florian@derfurth.com>
2023-02-02 12:13:48 +0100

    Ajoute un scénario de test pour l'audit.

---
+++ api_tests/lib/rpcs/testFullfill.ts
@@ -0,0 +1,17 @@
+import { supabase } from "../supabase.ts";
+import { Database } from "../database.types.ts";
+
+export async function testFullfill(
+  collectivite_id: number,
+  etoile: Database["labellisation"]["Enums"]["etoile"],
+): Promise<void> {
+  const { error } = await supabase.rpc(
+    "test_fulfill",
+    { collectivite_id, etoile },
+  ).single();
+  if (error) {
+    throw `La RPC 'test_fulfill' ne devrait pas renvoyer d'erreur.`;
+  }
+
+  return;
+}
