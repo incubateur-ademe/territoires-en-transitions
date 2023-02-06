e84c5eb1f296ea2197d9229d48b803c06621a781
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-02-06 11:18:13 +0100

    Envoie l'axe directement au lieu de juste l'id et le nom dans les fonctions plan_action et plan_action_profondeur

--- data_layer/sqitch/revert/plan_action/plan_action.sql
+++ data_layer/sqitch/revert/plan_action/plan_action.sql
@@ -2,6 +2,49 @@
 
 BEGIN;
 
+drop view plan_action;
+
+-- Fonction récursive pour afficher un plan d'action
+create or replace function plan_action(id integer) returns jsonb as
+$$
+declare
+    pa_enfant_id integer; -- Id d'un plan d'action enfant du plan d'action courant
+    pa_nom text; -- Nom du plan d'action courant
+    id_loop integer; -- Indice pour parcourir une boucle
+    enfants jsonb[]; -- Plans d'actions enfants du plan d'action courant;
+    fiches jsonb; -- Fiches actions du plan d'action courant
+    to_return jsonb; -- JSON retournant le plan d'action courant, ses fiches et ses enfants
+begin
+    fiches = to_jsonb((select array_agg(ff.*)
+                       from(
+                               select * from fiches_action fa
+                                                 join fiche_action_axe fapa on fa.id = fapa.fiche_id
+                               where fapa.axe_id = plan_action.id
+                               order by fa.modified_at desc
+                           )ff)) ;
+    pa_nom = (select nom from axe where axe.id = plan_action.id);
+    id_loop = 1;
+    for pa_enfant_id in
+        select pa.id
+        from axe pa
+        where pa.parent = plan_action.id
+        order by pa.created_at asc
+        loop
+            enfants[id_loop] = plan_action(pa_enfant_id);
+            id_loop = id_loop + 1;
+        end loop;
+
+    to_return = jsonb_build_object('id', plan_action.id,
+                                   'nom', pa_nom,
+                                   'fiches', fiches,
+                                   'enfants', enfants);
+    return to_return;
+end;
+$$ language plpgsql;
+comment on function plan_action is
+    'Fonction retournant un JSON contenant le plan d''action passé en paramètre,
+    ses fiches et ses plans d''actions enfants de manière récursive';
+
 drop view plan_action_chemin;
 drop view plan_action_profondeur;
 drop function plan_action_profondeur;
