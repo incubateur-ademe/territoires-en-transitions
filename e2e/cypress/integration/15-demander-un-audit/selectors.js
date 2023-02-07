c02db98c758447c8b9ffbff1842146b6b3883c22
Marc Rutkowski <marc@attractive-media.fr>
2023-02-07 09:11:05 +0100

    ajoute les scénarios de test e2e de la demande d'audit/labellisaiton

---
+++ e2e/cypress/integration/15-demander-un-audit/selectors.js
@@ -0,0 +1,26 @@
+export const LocalSelectors = {
+  'Demander un audit': {
+    selector: '[data-test=SubmitDemandeBtn]',
+  },
+  "choix du type d'audit/labellisation COT": {
+    selector: '[data-test=DemandeAuditModal]',
+  },
+  'Audit COT sans labellisation': {
+    selector: 'input[type=radio][value=cot]',
+  },
+  'Audit COT avec labellisation': {
+    selector: 'input[type=radio][value=labellisation_cot]',
+  },
+  'Audit de labellisation': {
+    selector: 'input[type=radio][value=labellisation]',
+  },
+  'Envoyer ma demande': {
+    selector: '[data-test=EnvoyerDemandeBtn]',
+  },
+  "message d'en-tête": {
+    selector: '[data-test=HeaderMessage]',
+  },
+  'Ajouter un document de labellisation': {
+    selector: '[data-test=AddDocsButton]',
+  },
+};
