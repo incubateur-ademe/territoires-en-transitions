339f7d0ac73545b22c85c8da96e992aa0d79b081
Marc Rutkowski <marc@attractive-media.fr>
2023-02-07 09:09:53 +0100

    déplace la création de la demande dans l'ajout de preuves de labellisation
    (pour créer la demande uniquement lorsqu'elle n'est pas présente dans le parcours de labellisation)

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/AddDocsButton.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/AddDocsButton.tsx
@@ -4,17 +4,13 @@ import Modal from 'ui/shared/floating-ui/Modal';
 import {AddPreuveModal} from 'ui/shared/preuves/AddPreuveModal';
 import {useAddPreuveToDemande} from './useAddPreuveToDemande';
 
-export type TAddDocsButtonProps = {
-  demande_id: number;
-};
-
 /**
  * Affiche un bouton permettant d'ouvrir le sélecteur de fichiers pour ajouter
  * des documents à une demande de labellisation
  */
-export const AddDocsButton = (props: TAddDocsButtonProps) => {
+export const AddDocsButton = () => {
   const [opened, setOpened] = useState(false);
-  const handlers = useAddPreuveToDemande(props.demande_id);
+  const handlers = useAddPreuveToDemande();
   const currentCollectivite = useCurrentCollectivite();
   if (!currentCollectivite || currentCollectivite.readonly) {
     return null;
