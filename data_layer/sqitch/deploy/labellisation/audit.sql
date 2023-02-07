62dfff160624da70eb7aa7bb64eebce16a3a045a
Florian d'Erfurth <florian@derfurth.com>
2023-02-07 20:27:13 +0100

    Un couple audit/demande est toujours accessible pour une collectivité.

--- data_layer/sqitch/deploy/labellisation/audit.sql
+++ data_layer/sqitch/deploy/labellisation/audit.sql
@@ -29,4 +29,114 @@ begin
 end ;
 $$ language plpgsql;
 
+alter table audit drop constraint audit_existant;
+
+create function
+    labellisation_commencer_audit(audit_id integer)
+    returns audit
+begin
+    atomic
+    update audit
+    set date_debut = now()
+    where id = audit_id
+      and (id in (select aa.audit_id from audit_auditeur aa where aa.auditeur = auth.uid()) or is_service_role())
+    returning *;
+end;
+comment on function labellisation_commencer_audit is
+    'Commence un audit, c''est à dire met la date de début à now(). '
+        'Seul l''auditeur et le service role peuvent commencer un audit.';
+
+create or replace function labellisation.current_audit(col integer, ref referentiel)
+    returns audit
+    security definer
+as
+$$
+# variable_conflict use_column
+declare
+    found audit;
+begin
+    select *
+    into found
+    from audit a
+    where a.collectivite_id = current_audit.col
+      and a.referentiel = current_audit.ref
+      and (
+            (now() between date_debut and date_fin)
+            or (date_debut <= now() and date_fin is null)
+            or (date_debut is null)
+        )
+    order by date_debut desc
+    limit 1;
+
+    if found is null
+    then
+        insert
+        into audit (collectivite_id, referentiel, demande_id, date_debut, date_fin)
+        select current_audit.col,
+               current_audit.ref,
+               null,
+               null,
+               null
+        returning * into found;
+    end if;
+
+    return found;
+end;
+$$ language plpgsql;
+
+create or replace function labellisation.upsert_action_audit()
+    returns trigger as
+$$
+declare
+    found_audit          audit;
+    existing_audit_state labellisation.action_audit_state;
+
+begin
+    if not have_edition_acces(new.collectivite_id)
+    then
+        perform set_config('response.status', '403', true);
+        raise 'L''utilisateur n''a pas de droit en édition sur la collectivité.';
+    end if;
+
+    found_audit = labellisation.current_audit(
+            new.collectivite_id,
+            (select ar.referentiel
+             from action_relation ar
+             where ar.id = new.action_id)
+        );
+
+    if found_audit.date_debut is null
+    then
+        raise 'Pas d''audit en cours.';
+    end if;
+
+    if not (select bool_or(auth.uid() = auditeur) from audit_auditeur where audit_id=found_audit.id)
+    then
+        perform set_config('response.status', '403', true);
+        raise 'L''utilisateur n''est pas auditeur sur l''audit de la collectivité.';
+    end if;
+
+    select *
+    from labellisation.action_audit_state
+    where audit_id = found_audit.id
+      and collectivite_id = new.collectivite_id
+      and action_id = new.action_id
+    into existing_audit_state;
+
+    if existing_audit_state is null
+    then
+        insert into labellisation.action_audit_state (audit_id, action_id, collectivite_id, avis, ordre_du_jour, statut)
+        values (found_audit.id, new.action_id, new.collectivite_id, coalesce(new.avis, ''), new.ordre_du_jour, new.statut);
+    else
+        update labellisation.action_audit_state
+        set avis          = coalesce(new.avis, ''),
+            ordre_du_jour = new.ordre_du_jour,
+            statut        = new.statut
+        where id = existing_audit_state.id;
+    end if;
+
+    return new;
+end
+$$ language plpgsql security definer;
+
 COMMIT;
