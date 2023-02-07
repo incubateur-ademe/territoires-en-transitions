feebcd608456aa63573e6dfec22d8a363c850cfc
Marc Rutkowski <marc@attractive-media.fr>
2023-02-07 14:15:37 +0100

    fixe les tests d'api suite au changement des fonctions de génération de données de test

--- api_tests/lib/rpcs/labellisationDemande.ts
+++ api_tests/lib/rpcs/labellisationDemande.ts
@@ -1,15 +1,13 @@
-import { supabase } from "../supabase.ts";
-import { Database } from "../database.types.ts";
+import { supabase } from '../supabase.ts';
+import { Database } from '../database.types.ts';
 
 export async function labellisationDemande(
   collectivite_id: number,
-  referentiel: Database["public"]["Enums"]["referentiel"],
-  etoiles: Database["labellisation"]["Enums"]["etoile"],
-): Promise<Database["labellisation"]["Tables"]["demande"]["Row"]> {
-  const { data } = await supabase.rpc(
-    "labellisation_demande",
-    { collectivite_id, etoiles, referentiel },
-  ).single();
+  referentiel: Database['public']['Enums']['referentiel']
+): Promise<Database['labellisation']['Tables']['demande']['Row']> {
+  const { data } = await supabase
+    .rpc('labellisation_demande', { collectivite_id, referentiel })
+    .select();
   if (!data) {
     throw `La RPC 'labellisation_demande' devrait renvoyer une demande d'audit.`;
   }
--- api_tests/lib/rpcs/testSetAuditeur.ts
+++ api_tests/lib/rpcs/testSetAuditeur.ts
@@ -1,14 +1,14 @@
-import { supabase } from "../supabase.ts";
-import { Database } from "../database.types.ts";
+import { supabase } from '../supabase.ts';
+import { Database } from '../database.types.ts';
 
 export async function testSetAuditeur(
   demande_id: number,
   user_id: string,
-): Promise<Database["public"]["Tables"]["audit_auditeur"]["Row"]> {
-  const { data } = await supabase.rpc(
-    "test_set_auditeur",
-    { demande_id, user_id },
-  ).single();
+  audit_en_cours: boolean = false
+): Promise<Database['public']['Tables']['audit_auditeur']['Row']> {
+  const { data } = await supabase
+    .rpc('test_set_auditeur', { demande_id, user_id, audit_en_cours })
+    .single();
   if (!data) {
     throw `La RPC 'test_set_auditeur' devrait renvoyer un audit-auditeur.`;
   }
