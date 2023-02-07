222d1aa3cef11b2a9ef5c5c3bdcda6684d802aec
Marc Rutkowski <marc@attractive-media.fr>
2023-02-07 09:09:53 +0100

    déplace la création de la demande dans l'ajout de preuves de labellisation
    (pour créer la demande uniquement lorsqu'elle n'est pas présente dans le parcours de labellisation)

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useAddPreuveToDemande.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useAddPreuveToDemande.ts
@@ -1,30 +1,71 @@
-import {useCollectiviteId} from 'core-logic/hooks/params';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useCollectiviteId, useReferentielId} from 'core-logic/hooks/params';
+import {Referentiel} from 'types/litterals';
 import {TAddFileFromLib} from 'ui/shared/preuves/AddPreuveModal/AddFile';
 import {useAddPreuveLabellisation} from 'ui/shared/preuves/Bibliotheque/useAddPreuves';
+import {TEtoiles, TLabellisationDemande} from './types';
+import {useCycleLabellisation} from './useCycleLabellisation';
 
-type TAddDocs = (demande_id: number) => {
+type TAddDocs = () => {
   /** ajoute un fichier sélectionné depuis la bibliothèque */
   addFileFromLib: TAddFileFromLib;
 };
 
 /** Renvoie les gestionnaires d'événements du dialogue d'ajout de
  * fichiers au parcours de labellisation en cours */
-export const useAddPreuveToDemande: TAddDocs = (demande_id: number) => {
+export const useAddPreuveToDemande: TAddDocs = () => {
   const collectivite_id = useCollectiviteId();
+  const referentiel = useReferentielId() as Referentiel;
   const {mutate: addPreuve} = useAddPreuveLabellisation();
+  const {parcours} = useCycleLabellisation(referentiel);
 
   // associe un fichier de la bibliothèque à la demande
-  const addFileFromLib: TAddFileFromLib = fichier_id => {
+  const addFileFromLib: TAddFileFromLib = async fichier_id => {
     if (collectivite_id) {
-      addPreuve({
-        demande_id,
+      const args = {
         collectivite_id,
         commentaire: '',
         fichier_id,
-      });
+      };
+
+      const demande_id = parcours?.demande?.id;
+      if (demande_id) {
+        addPreuve({...args, demande_id});
+      } else {
+        const demande = await createDemande(
+          collectivite_id,
+          referentiel,
+          parcours?.etoiles
+        );
+        if (demande?.id) {
+          addPreuve({...args, demande_id: demande.id});
+        }
+      }
     }
   };
   return {
     addFileFromLib,
   };
+};
+
+// charge la demande (ou la crée) associée au parcours de labellisation d'une
+// collectivité pour un référentiel et un niveau
+export const createDemande = async (
+  collectivite_id: number | null,
+  referentiel: Referentiel | null,
+  etoiles: TEtoiles | undefined
+) => {
+  if (!collectivite_id || !referentiel || !etoiles) {
+    return null;
+  }
+  const {error, data} = await supabaseClient
+    .rpc('labellisation_demande', {
+      collectivite_id,
+      referentiel,
+      etoiles,
+    })
+    .select();
+
+  // cast retour de la rpc car le typage généré n'est pas bon (tableau au lieu d'élément unique)
+  return error || !data ? null : (data as unknown as TLabellisationDemande);
 };
