e41c94389728a740311cfbd60420034f8acb5c4d
Arthur Molinos <arthur.molinos@gmail.com>
2023-02-10 09:38:30 +0100

    Amélioration du composant FilAriane générique

--- app.territoiresentransitions.react/src/ui/shared/FilAriane.tsx
+++ app.territoiresentransitions.react/src/ui/shared/FilAriane.tsx
@@ -11,26 +11,27 @@ type Props = {
 
 const FilAriane = ({links}: Props) => {
   return (
-    <div className="flex items-center pr-6 text-xs text-gray-500">
-      {links.map((link, i) =>
-        link.path ? (
-          <div key={link.displayedName} className="flex items-center">
+    <div className="flex items-center flex-wrap gap-y-0.5 pr-6 text-xs text-gray-500">
+      {links.map((link, i) => (
+        <div key={link.displayedName} className="flex items-center shrink-0">
+          {link.path ? (
             <Link
               className="p-1 underline shrink-0 text-gray-500 !shadow-none hover:text-gray-600"
               to={link.path}
             >
               {link.displayedName}
             </Link>
-            {i + 1 < links.length && (
-              <div className="fr-fi-arrow-down-s-line scale-75 -rotate-90" />
-            )}
-          </div>
-        ) : (
-          <div key={link.displayedName} className="line-clamp-1">
-            {link.displayedName}
-          </div>
-        )
-      )}
+          ) : (
+            <div key={link.displayedName}>{link.displayedName}</div>
+          )}
+          {i + 1 < links.length && (
+            <div
+              key={i}
+              className="fr-fi-arrow-down-s-line scale-75 shrink-0 -rotate-90"
+            />
+          )}
+        </div>
+      ))}
     </div>
   );
 };
