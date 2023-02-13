8c516f3b110acb503fe62f4dacc1edb3172195ee
Florian d'Erfurth <florian@derfurth.com>
2023-02-13 16:10:49 +0100

    Répare le deploy du type visite.

--- data_layer/sqitch/deploy/utilisateur/visite.sql
+++ data_layer/sqitch/deploy/utilisateur/visite.sql
@@ -3,7 +3,8 @@
 BEGIN;
 
 alter type visite_page
-    add value if not exists 'plan_axe',
+    add value if not exists 'plan_axe';
+alter type visite_page
     add value if not exists 'fiches_non_classees';
 
 COMMIT;
