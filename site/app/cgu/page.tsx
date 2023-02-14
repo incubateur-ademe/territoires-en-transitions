36dd8a21e6259edfb75ded7366a8697069ed34aa
Marc Rutkowski <marc@attractive-media.fr>
2023-02-14 19:07:39 +0100

    ajoute la page cgu [skip ci]

---
+++ site/app/cgu/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import CGU from './cgu.mdx';
+
+export default function Page() {
+  return (
+    <div className="fr-container">
+      <div className="fr-mt-1w fr-mt-md-4w fr-mb-5w">
+        <h1 className="fr-header__body">Conditions générales d’utilisation</h1>
+        <CGU />
+      </div>
+    </div>
+  );
+}
