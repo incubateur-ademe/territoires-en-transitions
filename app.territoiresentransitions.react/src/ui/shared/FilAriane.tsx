a2b2327cac9327857fb48cd3dae6eb9115f6d46f
Arthur Molinos <arthur.molinos@gmail.com>
2023-02-15 18:41:11 +0100

    Fix problème de key identique dans FilAriane

--- app.territoiresentransitions.react/src/ui/shared/FilAriane.tsx
+++ app.territoiresentransitions.react/src/ui/shared/FilAriane.tsx
@@ -13,7 +13,7 @@ const FilAriane = ({links}: Props) => {
   return (
     <div className="flex items-center flex-wrap gap-y-0.5 pr-6 text-xs text-gray-500">
       {links.map((link, i) => (
-        <div key={link.displayedName} className="flex items-center shrink-0">
+        <div key={i} className="flex items-center shrink-0">
           {link.path ? (
             <Link
               className="p-1 underline shrink-0 text-gray-500 !shadow-none hover:text-gray-600"
@@ -22,13 +22,10 @@ const FilAriane = ({links}: Props) => {
               {link.displayedName}
             </Link>
           ) : (
-            <div key={link.displayedName}>{link.displayedName}</div>
+            <div>{link.displayedName}</div>
           )}
           {i + 1 < links.length && (
-            <div
-              key={i}
-              className="fr-fi-arrow-down-s-line scale-75 shrink-0 -rotate-90"
-            />
+            <div className="fr-fi-arrow-down-s-line scale-75 shrink-0 -rotate-90" />
           )}
         </div>
       ))}
