a92eaa2790586953808863ed0325df7e60424c6f
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-02-15 16:22:59 +0100

    Ajout envoie à n8n : - Collectivité utilisant les plans pour la 1ère fois - Collectivité devenue inactive

--- data_layer/sqitch/verify/automatisation/crm.sql
+++ data_layer/sqitch/verify/automatisation/crm.sql
@@ -2,14 +2,8 @@
 
 BEGIN;
 
-select uri_type, uri
-from automatisation.automatisation_uri
-where false;
-
-select has_function_privilege('automatisation.send_insert_users_json_n8n()', 'execute');
-select has_function_privilege('automatisation.send_upsert_users_json_n8n()', 'execute');
-select has_function_privilege('automatisation.send_upsert_collectivites_json_n8n(interval)', 'execute');
-select has_function_privilege('automatisation.send_upsert_collectivite_membre_json_n8n()', 'execute');
+select has_function_privilege('automatisation.send_collectivite_plan_action_json_n8n()', 'execute');
+select has_function_privilege('automatisation.send_delete_collectivite_membre_json_n8n()', 'execute');
 
 
 ROLLBACK;
