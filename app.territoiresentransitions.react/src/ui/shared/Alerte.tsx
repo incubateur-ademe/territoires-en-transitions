da7268f1b62a48fdc76de46ed7bd8969e4a4eaf1
Arthur Molinos <arthur.molinos@gmail.com>
2023-02-15 19:29:34 +0100

    Améliorations graphiques suite aux retours de test

--- app.territoiresentransitions.react/src/ui/shared/Alerte.tsx
+++ app.territoiresentransitions.react/src/ui/shared/Alerte.tsx
@@ -1,12 +1,22 @@
 import classNames from 'classnames';
 import {ReactElement} from 'react';
 
+type TAlerteState = 'error' | 'information' | 'success' | 'warning';
+
+const alertStateClasses: Record<TAlerteState, string> = {
+  information: 'fr-alert--info',
+  success: 'fr-alert--success',
+  warning: 'fr-alert--warning',
+  error: 'fr-alert--error',
+};
+
 type AlerteProps = {
   state: 'error' | 'information' | 'success' | 'warning';
   titre?: string;
   description?: string;
   children?: ReactElement;
   classname?: string;
+  small?: boolean;
 };
 
 const Alerte = ({
@@ -15,13 +25,11 @@ const Alerte = ({
   description,
   children,
   classname,
+  small,
 }: AlerteProps) => (
   <div
-    className={classNames('fr-alert', classname, {
-      'fr-alert--info': state === 'information',
-      'fr-alert--success': state === 'success',
-      'fr-alert--warning': state === 'warning',
-      'fr-alert--error': state === 'error',
+    className={classNames('fr-alert', classname, alertStateClasses[state], {
+      'fr-alert--sm': small,
     })}
   >
     {titre && <h3 className="fr-alert__title">{titre}</h3>}
