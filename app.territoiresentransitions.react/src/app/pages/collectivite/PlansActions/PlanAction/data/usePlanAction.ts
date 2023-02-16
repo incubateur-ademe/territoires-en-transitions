a5709f02d4fd81e36b17c33c91e4e280eb8a148c
Arthur Molinos <arthur.molinos@gmail.com>
2023-02-16 12:19:21 +0100

    Suppression d'un axe de manière optimistic update

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/usePlanAction.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/usePlanAction.ts
@@ -3,25 +3,9 @@ import {useQuery} from 'react-query';
 import {supabaseClient} from 'core-logic/api/supabase';
 import {TPlanAction} from './types/PlanAction';
 
-type TFetchedData = {
-  plan: TPlanAction;
-};
-
-const fetchPlanAction = async (plan_id: number): Promise<TFetchedData> => {
-  const query = supabaseClient.rpc('plan_action', {id: plan_id});
-
-  const {error, data} = await query;
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return {plan: data as unknown as TPlanAction};
-};
-
 export const usePlanAction = (plan_id: number) => {
-  const {data} = useQuery(['plan_action', plan_id], () =>
-    fetchPlanAction(plan_id)
-  );
-
-  return data;
+  return useQuery(['plan_action', plan_id], async () => {
+    const {data} = await supabaseClient.rpc('plan_action', {id: plan_id});
+    return data as unknown as TPlanAction;
+  });
 };
