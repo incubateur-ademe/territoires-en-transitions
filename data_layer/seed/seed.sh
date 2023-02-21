cbfadb893f240e0e8603897cc00a3df9214487fb
Florian d'Erfurth <florian@derfurth.com>
2023-02-21 11:48:30 +0100

    Permet de lancer le seed indépendamment

--- data_layer/seed/seed.sh
+++ data_layer/seed/seed.sh
@@ -4,17 +4,17 @@ DATALAYER_DIR="./seed"
 
 echo "Loading content..."
 for file in "$DATALAYER_DIR"/content/*.sql; do
-    psql "$DB_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
+    psql "$PG_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
 done
 
 echo "Loading imports..."
 for file in "$DATALAYER_DIR"/imports/*.sql; do
-    psql "$DB_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
+    psql "$PG_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
 done
 
 echo "Loading fakes..."
 for file in "$DATALAYER_DIR"/fakes/*.sql; do
-    psql "$DB_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
+    psql "$PG_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
 done
 
 if [ "$SKIP_TEST_DOMAIN" = 1 ];
@@ -23,11 +23,11 @@ echo "Skipping test domain."
 else
 echo "Load test domain.."
 for file in "$DATALAYER_DIR"/test/*.sql; do
-    psql "$DB_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
+    psql "$PG_URL" -v ON_ERROR_STOP=1 --file "${file}" || exit 1
 done
 
 echo "Enabling evaluation API..."
-psql "$DB_URL" -v ON_ERROR_STOP=1 -c 'select test.enable_evaluation_api();' || exit 1
+psql "$PG_URL" -v ON_ERROR_STOP=1 -c 'select test.enable_evaluation_api();' || exit 1
 fi
 
 echo "Done!"
