8c3837dd33a674631e52f120d457c6af4e2af144
Marc Rutkowski <marc@attractive-media.fr>
2023-02-22 20:26:23 +0100

    corrige le regroupement des documents de labellisation/audit dans la bibliothèque

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/BibliothequeDocs/groupeParReferentielEtDemande.tsx
@@ -0,0 +1,55 @@
+import {Referentiel} from 'types/litterals';
+import {TPreuveAuditEtLabellisation} from 'ui/shared/preuves/Bibliotheque/types';
+
+// groupe les preuves par référentiel
+type TPreuvesParReferentiel = Record<
+  Referentiel,
+  TPreuveAuditEtLabellisation[]
+>;
+const groupeParReferentiel = (
+  preuves: TPreuveAuditEtLabellisation[]
+): TPreuvesParReferentiel =>
+  preuves.reduce((dict, preuve) => {
+    const referentiel =
+      preuve.demande?.referentiel || preuve.audit?.referentiel;
+    if (!referentiel) {
+      return dict;
+    }
+    return {
+      ...dict,
+      [referentiel]: [...(dict[referentiel] || []), preuve],
+    };
+  }, {} as TPreuvesParReferentiel);
+
+// groupe les preuves par id de demande ou d'audit
+type TPreuvesParDemande = Record<string, TPreuveAuditEtLabellisation[]>;
+const groupeParDemande = (
+  preuves: TPreuveAuditEtLabellisation[]
+): TPreuvesParDemande =>
+  preuves.reduce((dict, preuve) => {
+    const id = preuve.demande?.id || preuve.audit?.id;
+    if (!id) {
+      return dict;
+    }
+    return {
+      ...dict,
+      [id]: [...(dict[id] || []), preuve],
+    };
+  }, {} as TPreuvesParDemande);
+
+// groupe les preuves par référentiel et par demande
+type TPreuvesParReferentielEtDemande = Record<
+  string,
+  Record<string, TPreuveAuditEtLabellisation[]>
+>;
+export const groupeParReferentielEtDemande = (
+  preuves: TPreuveAuditEtLabellisation[]
+): TPreuvesParReferentielEtDemande => {
+  return Object.entries(groupeParReferentiel(preuves)).reduce(
+    (dict, [referentiel, preuvesReferentiel]) => ({
+      ...dict,
+      [referentiel]: groupeParDemande(preuvesReferentiel),
+    }),
+    {} as TPreuvesParReferentielEtDemande
+  );
+};
