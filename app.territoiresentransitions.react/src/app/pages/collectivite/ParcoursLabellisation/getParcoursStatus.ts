52fac4380b6043f4a71b5851a9fc3d74eea8c760
Marc Rutkowski <marc@attractive-media.fr>
2023-02-22 23:20:00 +0100

    change la condition définissant si un document audit/labellisation est en lecture seule

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/getParcoursStatus.ts
@@ -0,0 +1,24 @@
+import {TLabellisationParcours} from 'app/pages/collectivite/ParcoursLabellisation/types';
+import {TCycleLabellisationStatus} from './useCycleLabellisation';
+
+// détermine l'état consolidé du cycle
+type TDemandeEtOuAudit = Pick<TLabellisationParcours, 'demande' | 'audit'>;
+
+export const getParcoursStatus = (
+  demandeEtOuAudit: TDemandeEtOuAudit | null
+): TCycleLabellisationStatus => {
+  if (!demandeEtOuAudit) {
+    return 'non_demandee';
+  }
+  const {demande, audit} = demandeEtOuAudit;
+  if (audit?.valide) {
+    return 'audit_valide';
+  }
+  if (audit?.date_debut && !audit?.valide) {
+    return 'audit_en_cours';
+  }
+  if (demande && !demande.en_cours) {
+    return 'demande_envoyee';
+  }
+  return 'non_demandee';
+};
