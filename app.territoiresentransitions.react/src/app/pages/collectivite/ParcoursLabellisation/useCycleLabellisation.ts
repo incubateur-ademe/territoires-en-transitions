35d1121278e00216a1a7d931883b1506dba5505e
Marc Rutkowski <marc@attractive-media.fr>
2023-02-22 19:30:04 +0100

    améliore la lisibilité

--- app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useCycleLabellisation.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useCycleLabellisation.ts
@@ -1,11 +1,10 @@
-import {useQuery} from 'react-query';
 import {TLabellisationParcours} from 'app/pages/collectivite/ParcoursLabellisation/types';
-import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {useIsAuditeur} from '../Audit/useAudit';
 import {useCarteIdentite} from '../PersoReferentielThematique/useCarteIdentite';
 import {usePreuves} from 'ui/shared/preuves/Bibliotheque/usePreuves';
 import {TPreuveLabellisation} from 'ui/shared/preuves/Bibliotheque/types';
+import {useLabellisationParcours} from './useLabellisationParcours';
 
 // données du cycle de labellisation/audit actuel d'une collectivité
 export type TCycleLabellisation = {
@@ -33,13 +32,7 @@ export const useCycleLabellisation = (
   const identite = useCarteIdentite(collectivite_id);
 
   // charge les données du parcours
-  const {data: parcoursList} = useQuery(
-    ['labellisation_parcours', collectivite_id],
-    () => fetchParcours(collectivite_id)
-  );
-
-  // extrait le parcours correspondant au référentiel courant
-  const parcours = getReferentielParcours(parcoursList, referentiel);
+  const parcours = useLabellisationParcours({collectivite_id, referentiel});
   const {completude_ok, rempli} = parcours || {};
 
   // charge les documents de labellisation
@@ -89,47 +82,4 @@ const getParcoursStatus = (
     return 'demande_envoyee';
   }
   return 'non_demandee';
-};
-
-// charge les parcours (eci/cae) de labellisation d'une collectivité donnée
-const fetchParcours = async (
-  collectivite_id: number | null
-): Promise<TLabellisationParcours[] | null> => {
-  if (!collectivite_id) {
-    return null;
-  }
-
-  const {data, error} = await supabaseClient
-    .rpc('labellisation_parcours', {
-      collectivite_id,
-    })
-    .select();
-
-  if (error || !data) {
-    return null;
-  }
-  return data.map(d => ({
-    ...d,
-    collectivite_id, // on ajoute l'id qui n'est pas redonné par la vue
-  })) as TLabellisationParcours[];
-};
-
-export const getReferentielParcours = (
-  parcoursList: TLabellisationParcours[] | null | undefined,
-  referentiel: string | null
-) => {
-  const parcours: TLabellisationParcours | undefined = parcoursList?.find(
-    p => p.referentiel === referentiel
-  );
-
-  if (!parcours) {
-    return null;
-  }
-
-  const {criteres_action} = parcours;
-  return {
-    ...parcours,
-    // trie les critères action
-    criteres_action: criteres_action.sort((a, b) => a.prio - b.prio),
-  };
 };
---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/ParcoursLabellisation/useLabellisationParcours.ts
@@ -0,0 +1,68 @@
+import {useQuery} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {TLabellisationParcours} from './types';
+
+// charge les données du parcours
+export const useLabellisationParcours = ({
+  collectivite_id,
+  referentiel,
+}: {
+  collectivite_id: number | null;
+  referentiel: string | null;
+}) => {
+  // charge les données du parcours
+  const {data: parcoursList} = useAllLabellisationsParcours(collectivite_id);
+
+  // extrait le parcours correspondant au référentiel courant
+  return getReferentielParcours(parcoursList, referentiel);
+};
+
+// charge les données des parcours de tous les référentiels
+const useAllLabellisationsParcours = (collectivite_id: number | null) => {
+  return useQuery(['labellisation_parcours', collectivite_id], () =>
+    fetchParcours(collectivite_id)
+  );
+};
+
+// charge les parcours (eci/cae) de labellisation d'une collectivité donnée
+const fetchParcours = async (
+  collectivite_id: number | null
+): Promise<TLabellisationParcours[] | null> => {
+  if (!collectivite_id) {
+    return null;
+  }
+
+  const {data, error} = await supabaseClient
+    .rpc('labellisation_parcours', {
+      collectivite_id,
+    })
+    .select();
+
+  if (error || !data) {
+    return null;
+  }
+  return data.map(d => ({
+    ...d,
+    collectivite_id, // on ajoute l'id qui n'est pas redonné par la vue
+  })) as TLabellisationParcours[];
+};
+
+export const getReferentielParcours = (
+  parcoursList: TLabellisationParcours[] | null | undefined,
+  referentiel: string | null
+) => {
+  const parcours: TLabellisationParcours | undefined = parcoursList?.find(
+    p => p.referentiel === referentiel
+  );
+
+  if (!parcours) {
+    return null;
+  }
+
+  const {criteres_action} = parcours;
+  return {
+    ...parcours,
+    // trie les critères action
+    criteres_action: criteres_action.sort((a, b) => a.prio - b.prio),
+  };
+};
