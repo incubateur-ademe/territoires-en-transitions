800663ba09a26904d26b892a56ea03ebf8b5a951
Florian d'Erfurth <florian@derfurth.com>
2023-02-23 17:06:03 +0100

    RPC pour filtrer les fiches.

--- data_layer/sqitch/deploy/plan_action/plan_action.sql
+++ data_layer/sqitch/deploy/plan_action/plan_action.sql
@@ -73,5 +73,92 @@ comment on function plan_action_profondeur is
     'Fonction retournant un JSON contenant le plan d''action passé en paramètre,
     et ses plans d''actions enfants de manière récursive';
 
+create view axe_descendants as
+with recursive
+    parents as (select id,
+                       collectivite_id,
+                       array []::integer[] as parents,
+                       0                   as depth
+                from axe
+                where parent is null
+
+                union all
+
+                select a.id,
+                       a.collectivite_id,
+                       parents || a.parent,
+                       depth + 1
+                from parents
+                         join axe a on a.parent = parents.id),
+    descendants as (select a.id, array_agg(p.id) as descendants
+                    from axe a
+                             join parents p on a.id = any (p.parents)
+                    group by a.id)
+select id as axe_id, descendants, parents, depth
+from parents
+         left join descendants using (id);
+
+create function
+    filter_fiches_action(
+    collectivite_id integer,
+    axe_id integer default null,
+    pilote_tag_id integer default null,
+    pilote_user_id uuid default null,
+    niveau_priorite fiche_action_niveaux_priorite default null,
+    statut fiche_action_statuts default null,
+    referent_tag_id integer default null,
+    referent_user_id integer default null
+)
+    returns setof fiches_action
+as
+$$
+    # variable_conflict use_variable
+begin
+    return query
+        select *
+        from fiches_action fa
+        where fa.collectivite_id = collectivite_id
+          and case
+                  when axe_id is null then true
+                  else fa.id in (with child as (select unnest(descendants) as axe_id
+                                                from axe_descendants a
+                                                where axe_id = any (a.descendants)
+                                                   or axe_id = a.axe_id)
+                                 select fiche_id
+                                 from child
+                                          join fiche_action_axe using (axe_id))
+            end
+          and case
+                  when pilote_tag_id is null then true
+                  else fa.id in
+                       (select fap.fiche_id from fiche_action_pilote fap where fap.tag_id = pilote_tag_id)
+            end
+          and case
+                  when pilote_user_id is null then true
+                  else fa.id in
+                       (select fap.fiche_id from fiche_action_pilote fap where fap.user_id = pilote_user_id)
+            end
+          and case
+                  when referent_tag_id is null then true
+                  else fa.id in
+                       (select far.fiche_id from fiche_action_referent far where far.tag_id = referent_tag_id)
+            end
+          and case
+                  when referent_user_id is null then true
+                  else fa.id in
+                       (select far.fiche_id from fiche_action_referent far where far.user_id = referent_user_id)
+            end
+          and case
+                  when niveau_priorite is null then true
+                  else fa.niveau_priorite = niveau_priorite
+            end
+          and case
+                  when statut is null then true
+                  else statut = fa.statut
+            end;
+end;
+$$ language plpgsql;
+comment on function filter_fiches_action is
+    'Filtre la vue pour le client.';
 
 COMMIT;
