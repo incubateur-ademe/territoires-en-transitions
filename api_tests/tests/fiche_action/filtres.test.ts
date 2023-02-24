f4f390991f98004cfd913ff081d44aa01e95bcfe
Florian d'Erfurth <florian@derfurth.com>
2023-02-24 13:54:14 +0100

    Ajoute un test pour les filtres

--- api_tests/tests/fiche_action/filtres.test.ts
+++ api_tests/tests/fiche_action/filtres.test.ts
@@ -3,19 +3,55 @@
 import {supabase} from '../../lib/supabase.ts';
 import {signIn, signOut} from '../../lib/auth.ts';
 import {testReset} from '../../lib/rpcs/testReset.ts';
-import {assertExists} from 'https://deno.land/std@0.113.0/testing/asserts.ts';
+import {
+  assertEquals,
+  assertExists,
+} from 'https://deno.land/std@0.113.0/testing/asserts.ts';
+import {assertObjectMatch} from 'https://deno.land/std/testing/asserts.ts';
 
 Deno.test('Fiches par axe', async () => {
   await testReset();
   await signIn('yolododo');
 
-  const selectResponse1 = await supabase.rpc('filter_fiches_action', {
+  // les fiches de l'axe "Développer une culture vélo" qui compte des sous axes avec des fiches
+  const filterResponse = await supabase.rpc('filter_fiches_action', {
     collectivite_id: 1,
     axe_id: 16,
     // @ts-ignore
     pilote_tag_id: null, niveau_priorite: null,  pilote_user_id: null, referent_tag_id: null, referent_user_id: null, statut: null,
   });
-  assertExists(selectResponse1.data);
+  assertExists(filterResponse.data);
+  assertEquals(filterResponse.data.length, 6);
+
+  await signOut();
+});
+
+Deno.test('Fiches par pilote', async () => {
+  await testReset();
+  await signIn('yolododo');
+
+  // une des fiches de l'axe "Développer une culture vélo"
+  const selectPermisVelo = await supabase.from('fiches_action').select().eq('id', 7)
+  assertExists(selectPermisVelo.data);
+
+  // On ajoute un pilote à la fiche en l'insérant dans la vue.
+  const permisVelo = selectPermisVelo.data[0];
+  // @ts-ignore
+  permisVelo.pilotes = [{nom: "Lou Piote", collectivite_id: 1, tag_id: 1, user_id: null}]
+  const insert = await supabase.from('fiches_action').insert(permisVelo as never).select()
+  assertExists(insert.data);
+  assertEquals(insert.status, 201);
+
+  // on filtre avec l'axe et le tag pilote.
+  const filterResponse = await supabase.rpc('filter_fiches_action', {
+    collectivite_id: 1,
+    axe_id: 16,
+    pilote_tag_id: 1,
+    // @ts-ignore
+    niveau_priorite: null,  pilote_user_id: null, referent_tag_id: null, referent_user_id: null, statut: null,
+  });
+  assertExists(filterResponse.data);
+  assertEquals(filterResponse.data.length, 1);
 
   await signOut();
 });
