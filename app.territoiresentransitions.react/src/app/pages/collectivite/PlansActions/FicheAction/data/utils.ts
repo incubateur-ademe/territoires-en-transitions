f4b4002ac02914ad9d62048061ad9d2f52a51833
Arthur Molinos <arthur.molinos@gmail.com>
2023-03-02 10:35:17 +0100

    Ajout d'un utilitaire pour récupérer l'id d'un type Personne

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
@@ -1,3 +1,5 @@
+import {Personne} from './types/personne';
+
 /**
  * Formate un nouveau tag qui nécessite un type minimum collectivite_id, nom
  * @param inputValue
@@ -7,3 +9,11 @@ export const formatNewTag = (inputValue: string, collectivite_id: number) => ({
   collectivite_id,
   nom: inputValue,
 });
+
+/**
+ * Renvoie l'id en string d'un type Personne aui est soit un tag_id number ou un user_id string
+ * @param personne
+ * @returns id as string
+ */
+export const getPersonneId = (personne: Personne): string =>
+  personne.tag_id ? personne.tag_id.toString() : personne.user_id!;
