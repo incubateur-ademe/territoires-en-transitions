c16405278d584f771e06f4540b492ad2769b91f4
Florian d'Erfurth <florian@derfurth.com>
2023-03-03 18:19:26 +0100

    Ajoute le site au make dot env.

--- make_dot_env.sh
+++ make_dot_env.sh
@@ -5,3 +5,4 @@ envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.e
 envsubst '${SUPABASE_ANON_KEY}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
 envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./e2e/.env.sample > ./e2e/.env
 envsubst '${SUPABASE_ANON_KEY}' < ./api_tests/.env.example > ./api_tests/.env
+envsubst '${SUPABASE_ANON_KEY}' < ./site/.env.sample > ./site/.env
