91336753c05ccb49dc1111f589d406e9b7ddf396
Florian d'Erfurth <florian@derfurth.com>
2023-03-07 11:22:54 +0100

    Ajoute la vue `stats_collectivite_actives_locales_et_total_par_type`. Expose les vues matérialisées via des vues normales afin que les types soient générés.

--- data_layer/sqitch/deploy/stats/locales.sql
+++ data_layer/sqitch/deploy/stats/locales.sql
@@ -2,26 +2,26 @@
 
 BEGIN;
 
-create materialized view stats_evolution_total_activation_locales
+create materialized view stats.evolution_total_activation_locales
 as
 select -- permet de filtrer
        m.first_day                              as mois,
-       null:: varchar(2)                           code_region,
-       null::varchar(2)                            code_departement,
+       null:: varchar(2)                        as code_region,
+       null::varchar(2)                         as code_departement,
 
        -- stats nationales
-       (SELECT count(*) AS count
-        FROM stats.collectivite_utilisateur cu
-        WHERE cu.date_activation <= m.last_day) AS total,
-       (SELECT count(*) FILTER (WHERE cu.type_collectivite = 'EPCI'::type_collectivite) AS count
-        FROM stats.collectivite_utilisateur cu
-        WHERE cu.date_activation <= m.last_day) AS total_epci,
-       (SELECT count(*) FILTER (WHERE cu.type_collectivite = 'syndicat'::type_collectivite) AS count
-        FROM stats.collectivite_utilisateur cu
-        WHERE cu.date_activation <= m.last_day) AS total_syndicat,
-       (SELECT count(*) FILTER (WHERE cu.type_collectivite = 'commune'::type_collectivite) AS count
-        FROM stats.collectivite_utilisateur cu
-        WHERE cu.date_activation <= m.last_day) AS total_commune
+       (select count(*) as count
+        from stats.collectivite_utilisateur cu
+        where cu.date_activation <= m.last_day) as total,
+       (select count(*) filter (where cu.type_collectivite = 'epci'::type_collectivite) as count
+        from stats.collectivite_utilisateur cu
+        where cu.date_activation <= m.last_day) as total_epci,
+       (select count(*) filter (where cu.type_collectivite = 'syndicat'::type_collectivite) as count
+        from stats.collectivite_utilisateur cu
+        where cu.date_activation <= m.last_day) as total_syndicat,
+       (select count(*) filter (where cu.type_collectivite = 'commune'::type_collectivite) as count
+        from stats.collectivite_utilisateur cu
+        where cu.date_activation <= m.last_day) as total_commune
 from stats.monthly_bucket m
 
 union all
@@ -64,8 +64,53 @@ select m.first_day as mois,
         where cu.date_activation <= m.last_day)
 
 from imports.departement d
-         join stats.monthly_bucket m on true
-;
+         join stats.monthly_bucket m on true;
+
+-- on créé un vue car les vues matérialisées ne sont pas exportées dans les types.
+create view stats_evolution_total_activation_locales as select * from stats.evolution_total_activation_locales;
+
+
+create materialized view stats.collectivite_actives_locales_et_total_par_type
+as
+with collectivite_typologie as (select c.collectivite_id,
+                                       c.region_code,
+                                       c.departement_code,
+                                       case
+                                           when stats.is_fiscalite_propre(c.nature_collectivite) then 'EPCI'
+                                           when c.type_collectivite = 'syndicat' then 'syndicat'
+                                           when c.type_collectivite = 'commune' then 'commune' end as typologie
+                                from stats.collectivite c)
+
+select typologie,
+       null:: varchar(2)                        as code_region,
+       null::varchar(2)                         as code_departement,
+       count(*)                                                                                             as total,
+       count(*) filter ( where collectivite_id in (select collectivite_id from stats.collectivite_active) ) as actives
+from collectivite_typologie
+group by typologie
+
+union all
+
+select typologie,
+       region_code,
+       null,
+       count(*)                                                                                             as total,
+       count(*) filter ( where collectivite_id in (select collectivite_id from stats.collectivite_active) ) as actives
+from collectivite_typologie
+group by typologie, region_code
+
+union all
+
+select typologie,
+       null,
+       departement_code,
+       count(*)                                                                                             as total,
+       count(*) filter ( where collectivite_id in (select collectivite_id from stats.collectivite_active) ) as actives
+from collectivite_typologie
+group by typologie, departement_code;
+
+create view stats_collectivite_actives_locales_et_total_par_type as select * from stats.collectivite_actives_locales_et_total_par_type;
+
 
 
 create function
@@ -74,7 +119,8 @@ create function
 as
 $$
 begin
-    refresh materialized view stats_evolution_total_activation_locales;
+    refresh materialized view stats.evolution_total_activation_locales;
+    refresh materialized view stats.collectivite_actives_locales_et_total_par_type;
 end
 $$ language plpgsql;
 comment on function stats.refresh_stats_locales is
--- data_layer/sqitch/verify/stats/locales.sql
+++ data_layer/sqitch/verify/stats/locales.sql
@@ -6,6 +6,10 @@ select mois, code_region, code_departement, total, total_epci, total_syndicat, t
 from stats_evolution_total_activation_locales
 where false;
 
+select typologie, code_region, code_departement, total, actives
+from stats_collectivite_actives_locales_et_total_par_type
+where false;
+
 select has_function_privilege('stats.refresh_stats_locales()', 'execute');
 
 ROLLBACK;
