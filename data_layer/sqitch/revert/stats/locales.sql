32c35cef6d35a71b108598843ee33466ab484e06
Florian d'Erfurth <florian@derfurth.com>
2023-03-07 11:22:54 +0100

    Ajoute la vue `stats_collectivite_actives_locales_et_total_par_type`. Expose les vues matérialisées via des vues normales afin que les types soient générés.

--- data_layer/sqitch/revert/stats/locales.sql
+++ data_layer/sqitch/revert/stats/locales.sql
@@ -3,6 +3,9 @@
 BEGIN;
 
 drop function stats.refresh_stats_locales;
-drop materialized view stats_evolution_total_activation_locales;
+drop view stats_evolution_total_activation_locales;
+drop materialized view stats.evolution_total_activation_locales;
+drop view stats_collectivite_actives_locales_et_total_par_type;
+drop materialized view stats.collectivite_actives_locales_et_total_par_type;
 
 COMMIT;
