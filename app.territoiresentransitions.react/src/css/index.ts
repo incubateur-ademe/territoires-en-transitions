4b8d05a4eaef151a7ac06ac11900e49032989144
Marc Rutkowski <marc@attractive-media.fr>
2023-03-09 10:00:30 +0100

    rassemble les imports css dans un fichier séparé

---
+++ app.territoiresentransitions.react/src/css/index.ts
@@ -0,0 +1,13 @@
+// utilitaires tailwind
+import 'css/tailwind.css';
+
+// feuille de styles globale du dsfr
+import '@gouvfr/dsfr/dist/dsfr.css';
+
+// icônes
+import '@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.css';
+
+// patches
+import 'css/app.css';
