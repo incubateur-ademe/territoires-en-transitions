7723e5cff8c3839678e7d874455004ea01677cb3
Marc Rutkowski <marc@attractive-media.fr>
2023-03-15 12:24:59 +0100

    répare les tests e2e

--- e2e/cypress/integration/common/selectors.js
+++ e2e/cypress/integration/common/selectors.js
@@ -4,64 +4,63 @@
 
 export const Selectors = {
   header: {
-    selector: "header[role=banner]",
+    selector: 'header[role=banner]',
     children: {
-      "Se connecter": "[data-test=signin]",
-      "Créer un compte": "[data-test=signup]",
-      Profil: "[data-test=profil]",
+      'Se connecter': '.fr-header__tools-links [data-test=signin]',
+      'Créer un compte': '.fr-header__tools-links [data-test=signup]',
+      Profil: '[data-test=profil]',
     },
   },
   footer: {
-    selector: "#footer",
+    selector: '#footer',
   },
   home: {
-    selector: "[data-test=home]",
+    selector: '[data-test=home]',
   },
-  "le tableau de bord de la collectivité": {
-    selector: "[data-test=TableauBord]",
+  'le tableau de bord de la collectivité': {
+    selector: '[data-test=TableauBord]',
   },
-  "toutes les collectivités": {
-    selector: "[data-test=ToutesLesCollectivites]",
+  'toutes les collectivités': {
+    selector: '[data-test=ToutesLesCollectivites]',
   },
-  "bouton support": {
-    selector: ".crisp-client",
+  'bouton support': {
+    selector: '.crisp-client',
   },
-  "formulaire de connexion": {
-    selector: "[data-test=SignInPage]",
+  'formulaire de connexion': {
+    selector: '[data-test=SignInPage]',
     children: {
-      email: "input[name=email]",
-      mdp: "input[name=password]",
-      Valider: "button[type=submit]",
-      "Mot de passe oublié": "[data-test=forgotten-pwd]",
+      email: 'input[name=email]',
+      mdp: 'input[name=password]',
+      Valider: 'button[type=submit]',
+      'Mot de passe oublié': '[data-test=forgotten-pwd]',
     },
   },
-  "formulaire de connexion OTP": {
-    selector: "[data-test=OTPForm]",
+  'formulaire de connexion OTP': {
+    selector: '[data-test=OTPForm]',
     children: {
-      email: "input[name=email]",
-      code: "input[name=code]",
-      Valider: "button[type=submit]",
+      email: 'input[name=email]',
+      code: 'input[name=code]',
+      Valider: 'button[type=submit]',
     },
   },
-  "formulaire de création de compte": {
-    selector: "[data-test=SignUpPage]",
+  'formulaire de création de compte': {
+    selector: '[data-test=SignUpPage]',
     children: {
-      email: "input[name=email]",
-      mdp: "input[name=password]",
-      nom: "input[name=nom]",
-      prenom: "input[name=prenom]",
-      cgu: "input[name=vie_privee_conditions]",
-      Valider: "button[type=submit]",
+      email: 'input[name=email]',
+      mdp: 'input[name=password]',
+      nom: 'input[name=nom]',
+      prenom: 'input[name=prenom]',
+      cgu: 'input[name=vie_privee_conditions]',
+      Valider: 'button[type=submit]',
     },
   },
-  "tableau des membres": {
-    selector: "[data-test=MembreListTable]",
+  'tableau des membres': {
+    selector: '[data-test=MembreListTable]',
   },
-  "confirmation de creation de compte": {
-    selector: "[data-test=signup_success]",
+  'confirmation de creation de compte': {
+    selector: '[data-test=signup_success]',
     children: {
-      "se connecter": "[data-test=SeConnecter]",
+      'se connecter': '[data-test=SeConnecter]',
     },
   },
-
 };
