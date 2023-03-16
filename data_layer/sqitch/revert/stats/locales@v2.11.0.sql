4a3eec276b15b17caccf98701ddce5b2f47bcef9
Florian d'Erfurth <florian@derfurth.com>
2023-03-16 10:12:13 +0100

    Répare les stats départementales.

---
+++ data_layer/sqitch/revert/stats/locales@v2.11.0.sql
@@ -0,0 +1,7 @@
+-- Deploy tet:stats/locale to pg
+
+BEGIN;
+
+-- on ne veut pas revert les vues.
+
+COMMIT;
--- data_layer/sqitch/verify/stats/locales@v2.11.0.sql
+++ data_layer/sqitch/verify/stats/locales@v2.11.0.sql
@@ -6,6 +6,10 @@ select mois, code_region, code_departement, utilisateurs, total_utilisateurs
 from stats_locales_evolution_utilisateur
 where false;
 
+select mois, code_region, code_departement, moyen, maximum, median
+from stats_locales_evolution_nombre_utilisateur_par_collectivite
+where false;
+
 select has_function_privilege('stats.refresh_stats_locales()', 'execute');
 
 ROLLBACK;
