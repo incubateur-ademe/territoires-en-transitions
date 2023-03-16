8f49f56379a687d7486ff0032d09cdcfbd56c22b
Marc Rutkowski <marc@attractive-media.fr>
2023-03-16 14:46:29 +0100

    ajoute l'export des référentiels

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/Referentiels.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/Referentiels.tsx
@@ -1,9 +1,11 @@
-import {useParams} from 'react-router-dom';
-import {ReferentielParamOption, referentielParam} from 'app/paths';
 import ActionProgressBar from 'ui/referentiels/ActionProgressBar';
 import {useReferentielDownToAction} from 'core-logic/hooks/referentiel';
 import {ExpandableAction} from 'ui/shared/actions/ExpandableAction';
 import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
+import {useExportScores} from './export/useExportScores';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {useReferentielId} from 'core-logic/hooks/params';
+import {Referentiel} from 'types/litterals';
 
 const ReferentielHead = (props: {referentiel: ActionDefinitionSummary}) => {
   return (
@@ -17,25 +19,39 @@ const ReferentielHead = (props: {referentiel: ActionDefinitionSummary}) => {
 };
 
 export const ActionsReferentiels = () => {
-  const {referentielId} = useParams<{
-    [referentielParam]?: ReferentielParamOption;
-  }>();
+  const referentielId = useReferentielId();
   const current = referentielId ?? 'eci';
 
-  const actions = useReferentielDownToAction(current);
+  const actions = useReferentielDownToAction(current as Referentiel);
   const axes = actions.filter(a => a.type === 'axe');
   const referentiel = actions.find(a => a.type === 'referentiel')!;
+
+  const collectivite = useCurrentCollectivite();
+  const {exportScores, isLoading} = useExportScores(
+    referentielId,
+    collectivite
+  );
 
   if (!referentiel) return <></>;
 
   return (
-    <main data-test="ActionsReferentiels" className="fr-container mt-9 mb-16">
+    <main data-test="ActionsReferentiels" className="fr-container mt-9">
       <ReferentielHead referentiel={referentiel} />
       <section>
         {axes.map(axe => (
           <ExpandableAction action={axe} key={axe.id} />
         ))}
       </section>
+      <button
+        data-test="export-scores"
+        className="fr-btn fr-btn--icon-left fr-fi-download-line fr-mt-6w"
+        disabled={isLoading}
+        onClick={() => {
+          exportScores();
+        }}
+      >
+        Exporter
+      </button>
     </main>
   );
 };
