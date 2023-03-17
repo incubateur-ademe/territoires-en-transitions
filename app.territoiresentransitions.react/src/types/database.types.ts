7a6650b6f50c5a1492512847a28f257fdf0aa759
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-03-17 15:15:23 +0100

    MAJ database.types.ts

--- app.territoiresentransitions.react/src/types/database.types.ts
+++ app.territoiresentransitions.react/src/types/database.types.ts
@@ -5647,15 +5647,15 @@ export interface Database {
     Tables: {
       graphique_tranche: {
         Insert: {
-          label: string;
+          id: string;
           value: number;
         };
         Row: {
-          label: string;
+          id: string;
           value: number;
         };
         Update: {
-          label?: string;
+          id?: string;
           value?: number;
         };
       };
