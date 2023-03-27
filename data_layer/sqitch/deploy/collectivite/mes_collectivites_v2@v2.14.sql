4f03c7ac39cae17919064f4d15e084396ae38b72
Florian d'Erfurth <florian@derfurth.com>
2023-03-27 17:38:01 +0200

    Corrige la colonne `est_auditeur` de la vue `mes_collectivites` et ajoute la RPC `est_auditeur` avec le paramètre référentiel.

---
+++ data_layer/sqitch/deploy/collectivite/mes_collectivites_v2@v2.14.sql
@@ -0,0 +1,23 @@
+-- Deploy tet:collectivite/mes_collectivites_v2 to pg
+-- requires: utilisateur/niveaux_acces
+
+BEGIN;
+
+-- permet au client d'afficher les noms des collectivités et de les filtrer sur les critères de droits.
+create or replace view collectivite_niveau_acces
+as
+with current_droits as (select *
+                        from private_utilisateur_droit
+                        where user_id = auth.uid()
+                          and active)
+select named_collectivite.collectivite_id,
+       named_collectivite.nom,
+       niveau_acces,
+       est_auditeur(named_collectivite.collectivite_id) as est_auditeur,
+       c.access_restreint
+from named_collectivite
+         left join current_droits on named_collectivite.collectivite_id = current_droits.collectivite_id
+        left join collectivite c on named_collectivite.collectivite_id = c.id
+order by unaccent(nom);
+
+COMMIT;
