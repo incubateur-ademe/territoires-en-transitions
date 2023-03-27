18b5692baa1c1a617baa9da505902f62f055367c
Florian d'Erfurth <florian@derfurth.com>
2023-03-27 17:38:01 +0200

    Corrige la colonne `est_auditeur` de la vue `mes_collectivites` et ajoute la RPC `est_auditeur` avec le paramètre référentiel.

--- data_layer/sqitch/deploy/collectivite/mes_collectivites_v2.sql
+++ data_layer/sqitch/deploy/collectivite/mes_collectivites_v2.sql
@@ -3,21 +3,30 @@
 
 BEGIN;
 
--- permet au client d'afficher les noms des collectivités et de les filtrer sur les critères de droits.
-create or replace view collectivite_niveau_acces
-as
-with current_droits as (select *
-                        from private_utilisateur_droit
-                        where user_id = auth.uid()
-                          and active)
-select named_collectivite.collectivite_id,
-       named_collectivite.nom,
-       niveau_acces,
-       est_auditeur(named_collectivite.collectivite_id) as est_auditeur,
-       c.access_restreint
-from named_collectivite
-         left join current_droits on named_collectivite.collectivite_id = current_droits.collectivite_id
-        left join collectivite c on named_collectivite.collectivite_id = c.id
-order by unaccent(nom);
+alter function est_auditeur(integer) set schema private;
+
+create function est_auditeur(collectivite integer, referentiel referentiel)
+    returns boolean
+begin
+    atomic
+
+    select coalesce(bool_or(auth.uid() = aa.auditeur), false)
+    from labellisation.current_audit(est_auditeur.collectivite, est_auditeur.referentiel) ca
+             join audit_auditeur aa on ca.id = aa.audit_id;
+end;
+comment on function est_auditeur(integer, referentiel) is
+    'Vrai si l''utilisateur courant est auditeur sur une collectivité et un référentiel';
+
+create or replace function private.est_auditeur(col integer)
+    returns boolean
+begin
+    atomic
+    with ref as (select unnest(enum_range(null::referentiel)) as referentiel)
+    select bool_or(ea)
+    from ref
+             join est_auditeur(est_auditeur.col, ref.referentiel) ea on true;
+end;
+comment on function private.est_auditeur(integer) is
+    'Vrai si l''utilisateur courant est auditeur sur une collectivité pour au moins un référentiel';
 
 COMMIT;
