deb0efade1ba2ef92b70ab0c81185fc509da1cd7
Florian d'Erfurth <florian@derfurth.com>
2023-03-27 15:00:17 +0200

    Appelle la bonne fonction pour rafraichir les vues stats locales.

--- data_layer/sqitch/deploy/cron/refresh_stats_views_locales.sql
+++ data_layer/sqitch/deploy/cron/refresh_stats_views_locales.sql
@@ -2,8 +2,9 @@
 
 BEGIN;
 
+select cron.unschedule('refresh_stats_views_locales');
 select cron.schedule('refresh_stats_views_locales',
                      '0 2 * * *', -- tout les jours à 2h.
-                     $$select stats.refresh_views_utilisation();$$);
+                     $$select stats.refresh_stats_locales();$$);
 
 COMMIT;
