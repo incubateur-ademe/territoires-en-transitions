cd96866aa647d2c14d927e5ece2aec210db35c00
Florian d'Erfurth <florian@derfurth.com>
2023-03-28 12:22:07 +0200

    Raccourci le nom des variables d'environnements

--- make_dot_env.sh
+++ make_dot_env.sh
@@ -1,6 +1,5 @@
-envsubst '${SUPABASE_SERVICE_ROLE_KEY}, ${SUPABASE_ANON_KEY}' < ./data_layer/requests/http-client.sample.json > ./data_layer/requests/http-client.env.json
-envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
-envsubst '${SUPABASE_ANON_KEY}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
-envsubst '${SUPABASE_SERVICE_ROLE_KEY}' < ./e2e/.env.sample > ./e2e/.env
-envsubst '${SUPABASE_ANON_KEY}' < ./api_tests/.env.example > ./api_tests/.env
-envsubst '${SUPABASE_ANON_KEY}' < ./site/.env.sample > ./site/.env
+envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./data_layer/requests/http-client.sample.json > ./data_layer/requests/http-client.env.json
+envsubst '${SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
+envsubst '${ANON_KEY}, ${API_URL}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
+envsubst '${SERVICE_ROLE_KEY}, ${API_URL}' < ./e2e/.env.sample > ./e2e/.env
+envsubst '${ANON_KEY}, ${API_URL}' < ./api_tests/.env.example > ./api_tests/.env
