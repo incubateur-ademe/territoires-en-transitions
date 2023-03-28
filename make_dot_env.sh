cd6e93d2882b2fedc0498d2898fd8ead954340d4
Florian d'Erfurth <florian@derfurth.com>
2023-03-28 20:41:48 +0200

    Corrige les variables d'env des tests e2e

--- make_dot_env.sh
+++ make_dot_env.sh
@@ -1,5 +1,5 @@
 envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./data_layer/requests/http-client.sample.json > ./data_layer/requests/http-client.env.json
 envsubst '${SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
 envsubst '${ANON_KEY}, ${API_URL}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
-envsubst '${SERVICE_ROLE_KEY}, ${API_URL}' < ./e2e/.env.sample > ./e2e/.env
+envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./e2e/.env.sample > ./e2e/.env
 envsubst '${ANON_KEY}, ${API_URL}' < ./api_tests/.env.example > ./api_tests/.env
