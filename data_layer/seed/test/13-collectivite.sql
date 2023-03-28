3791c5c7d465fdf25cf0e5784740c99ff784f161
Florian d'Erfurth <florian@derfurth.com>
2023-03-28 16:11:38 +0200

    Rapatrie les nouveaux utilitaires de tests

---
+++ data_layer/seed/test/13-collectivite.sql
@@ -0,0 +1,46 @@
+create function
+    test_create_collectivite(
+    nom varchar(300)
+)
+    returns collectivite_test
+    security definer
+begin
+    atomic
+    insert into collectivite_test (nom)
+    values (test_create_collectivite.nom)
+    returning *;
+end;
+comment on function test_create_collectivite is
+    'Crée une collectivite de test, avec pour identité EPCI avec 0 habitant.';
+
+create function
+    test_set_cot(collectivite_id integer, actif bool)
+    returns cot
+    security definer
+begin
+    atomic
+    insert into cot
+    values (test_set_cot.collectivite_id, test_set_cot.actif)
+    on conflict (collectivite_id) do update set actif = excluded.actif
+    returning *;
+end;
+comment on function test_set_cot is
+    'Change le statut COT d''une collectivité.';
+
+create function
+    test_set_auditeur(demande_id integer, user_id uuid, audit_en_cours bool default false)
+    returns audit_auditeur
+    security definer
+begin
+    atomic
+    insert
+    into audit_auditeur (audit_id, auditeur)
+    select a.id, test_set_auditeur.user_id
+    from labellisation.demande d
+             join labellisation.current_audit(d.collectivite_id, d.referentiel) a on true
+    where d.id = test_set_auditeur.demande_id
+    returning *;
+end;
+comment on function test_set_auditeur is
+    'Ajoute un utilisateur en tant qu''auditeur à une collectivité. '
+        'L''utilisateur doit avoir des droits en écriture et une demande d''audit doit être validé.';
