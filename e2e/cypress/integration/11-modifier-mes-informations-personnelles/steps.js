e42d7db78577309c8d619d2b8986f2e6071b4e00
Florian d'Erfurth <florian@derfurth.com>
2023-03-28 13:55:13 +0200

    Utilise `inbucket` pour tester la reception de mail.

--- e2e/cypress/integration/11-modifier-mes-informations-personnelles/steps.js
+++ e2e/cypress/integration/11-modifier-mes-informations-personnelles/steps.js
@@ -1,16 +1,16 @@
 /// <reference types="Cypress" />
 
-import { LocalSelectors } from "./selectors";
+import {LocalSelectors} from './selectors';
 
 beforeEach(() => {
   // enregistre les définitions locales
-  cy.wrap(LocalSelectors).as("LocalSelectors");
+  cy.wrap(LocalSelectors).as('LocalSelectors');
 });
 
-Given("un formulaire de modification de compte est affiché", () => {
+Given('un formulaire de modification de compte est affiché', () => {
   cy.get(
-    LocalSelectors["formulaire de modification de compte"].selector
-  ).should("be.visible");
+    LocalSelectors['formulaire de modification de compte'].selector
+  ).should('be.visible');
 });
 
 Given(/je modifie le champ "([^"]+)" en "([^"]+)"/, (champ, value) => {
@@ -18,31 +18,43 @@ Given(/je modifie le champ "([^"]+)" en "([^"]+)"/, (champ, value) => {
 });
 
 Given(/le champ "([^"]+)" doit contenir "([^"]+)"/, (champ, value) => {
-  cy.get(`[data-test="${champ}"]`).should("have.value", value);
+  cy.get(`[data-test="${champ}"]`).should('have.value', value);
 });
 
 Given("la modale de modification d'email est affichée", () => {
   cy.get(LocalSelectors["modale de modification d'email"].selector).should(
-    "be.visible"
+    'be.visible'
   );
 });
 
-Given (/je vide ma boite de reception/, (email) => {
-  cy.origin(`http://localhost:8025/`, { args: { email } }, ({ email }) => {
-    cy.visit("/");
-    cy.get(`[ng-click="deleteAll()"]`).click();
-    cy.get(`[ng-click="deleteAllConfirm()"]`).click();
-  })
-  cy.visit("/");
+Given(/je vide la boite de reception de "([^"]+)"/, email => {
+  cy.origin(`http://localhost:54324/`, {args: {email}}, ({email}) => {
+    cy.visit(`/m/${email}`);
+    cy.get(`.fa-trash`).click();
+    cy.get(`.danger`).click();
+  });
+  cy.visit('/');
 });
 
-Given("je clique sur le bouton confirmer de la modale de modification d'email", () => {
-  cy.get(LocalSelectors["modale de modification d'email"].selector).get(`[aria-label="Confirmer"]`).click();
-});
+Given(
+  "je clique sur le bouton confirmer de la modale de modification d'email",
+  () => {
+    cy.get(LocalSelectors["modale de modification d'email"].selector)
+      .get(`[aria-label="Confirmer"]`)
+      .click();
+  }
+);
 
-Given (/ma boite de reception contient un mail adressé à "([^"]+)"/, (email) => {
-  cy.origin(`http://localhost:8025/`, { args: { email } }, ({ email }) => {
-    cy.visit("/");
-    cy.get(".msglist-message").contains(email);
-  })
-});
+Given(
+  /la boite de reception de "([^"]+)" contient un mail intitulé "([^"]+)"/,
+  (email, titre) => {
+    cy.origin(
+      `http://localhost:54324/`,
+      {args: {email, titre}},
+      ({email, titre}) => {
+        cy.visit(`/m/${email}`);
+        cy.get('.message-list').contains(titre);
+      }
+    );
+  }
+);
