8d6a8dce1570484e9dad8d1de08084764f9a8366
Florian d'Erfurth <florian@derfurth.com>
2023-03-29 11:11:32 +0200

    Ajoute la RPC `labellisation_peut_commencer_audit`

--- data_layer/sqitch/verify/labellisation/audit.sql
+++ data_layer/sqitch/verify/labellisation/audit.sql
@@ -3,6 +3,7 @@
 BEGIN;
 
 select has_function_privilege('labellisation.update_audit()', 'execute');
+select has_function_privilege('labellisation_peut_commencer_audit(integer)', 'execute');
 select has_function_privilege('labellisation_commencer_audit(integer, timestamptz)', 'execute');
 select audit_id, auditeur, created_at
 from audit_auditeur
