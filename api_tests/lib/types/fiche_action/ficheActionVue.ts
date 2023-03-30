0abc2d1b4dca5b42b582353e93ed741040d10c4c
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-03-30 18:29:58 +0200

    Adapte test ts ficheAction au nouveau typage généré et y ajoute fiches liées

--- api_tests/lib/types/fiche_action/ficheActionVue.ts
+++ api_tests/lib/types/fiche_action/ficheActionVue.ts
@@ -1,21 +1,19 @@
 import {Database} from "../../database.types.ts";
-import {Personne} from "./personne.ts";
-import {IndicateurGlobal} from "./indicateurGlobal.ts";
-import {FinanceurMontant} from "./financeurMontant.ts";
 
 export type FicheActionVueRow = Database["public"]["Views"]["fiches_action"]["Row"] & {
     thematiques: Database["public"]["Tables"]["thematique"]["Insert"][] | null,
     sous_thematiques: Database["public"]["Tables"]["sous_thematique"]["Insert"][] | null,
     partenaires: Database["public"]["Tables"]["partenaire_tag"]["Insert"][] | null,
     structures: Database["public"]["Tables"]["structure_tag"]["Insert"][] | null,
-    pilotes: Personne[] | null,
-    referents: Personne[] | null,
+    pilotes: Database["public"]["CompositeTypes"]["personne"][] | null,
+    referents: Database["public"]["CompositeTypes"]["personne"][] | null,
     annexes: Database["public"]["Tables"]["annexe"]["Insert"][]  | null,
     axes: Database["public"]["Tables"]["axe"]["Insert"][]  | null,
     actions: Database["public"]["Tables"]["action_relation"]["Insert"][]  | null,
-    indicateurs: IndicateurGlobal[] | null,
+    indicateurs: Database["public"]["CompositeTypes"]["indicateur_generique"][] | null,
     services : Database["public"]["Tables"]["service_tag"]["Insert"][]  | null,
-    financeurs : FinanceurMontant[]  | null
+    financeurs : Database["public"]["CompositeTypes"]["financeur_montant"][]  | null,
+    fiches_liees: Database["public"]["Views"]["fiche_resume"][]  | null
 };
 
 export type FicheActionVueInsert = FicheActionVueRow;
