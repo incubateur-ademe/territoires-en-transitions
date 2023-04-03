d085b8ce244e1ae160c8f1e404f4fe4febb530b9
Florian d'Erfurth <florian@derfurth.com>
2023-04-03 14:23:12 +0200

    Améliore la lisibilité du test `curl`.

--- data_layer/scripts/curl_test.sh
+++ data_layer/scripts/curl_test.sh
@@ -3,7 +3,7 @@
 echo "Essaye avec curl de télécharger les noms depuis la vue collectivite_card."
 
 curl -X GET \
-     -H "apikey: $ANON_KEY" \
-     -H "Authorization: Bearer $ANON_KEY" \
+     -H "apikey: $API_KEY" \
+     -H "Authorization: Bearer $API_KEY" \
      -H "Accept: text/csv" \
 "$URL/rest/v1/collectivite_card?select=nom"
