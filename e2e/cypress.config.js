3c5390c74de67535882326571c477380fd832122
Marc Rutkowski <marc@attractive-media.fr>
2023-04-04 11:08:56 +0200

    met à jour la configuration de Cypress

---
+++ e2e/cypress.config.js
@@ -0,0 +1,28 @@
+const {defineConfig} = require('cypress');
+const setupNodeEvents = require('./cypress/plugins');
+
+/** Configuration Cypress */
+module.exports = defineConfig({
+  e2e: {
+    // url de l'app
+    baseUrl: 'http://localhost:3000',
+    // désactive la génération de vidéos en mode "headless"
+    video: false,
+    // taille de la fenêtre dans laquelle l'app est chargée
+    viewportWidth: 1280,
+    viewportHeight: 1024,
+    // lorsqu'un test échoue...
+    retries: {
+      // ...on refait 2 tentatives en mode "headless"
+      runMode: 2,
+      // ...mais aucune en mode interactif (dev)
+      openMode: 0,
+    },
+    // scénarios de test à traiter
+    specPattern: '**/*.feature',
+    // configuration des plugins
+    setupNodeEvents,
+    // et des commandes personnalisées
+    supportFile: 'cypress/support/index.js',
+  },
+});
