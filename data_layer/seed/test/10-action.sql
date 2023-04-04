11f8439c0d06968a4da92d5ddde0e6f549fc5c0a
Florian d'Erfurth <florian@derfurth.com>
2023-04-04 09:27:18 +0200

    Nomme les triggers pour éviter `disable all`

--- data_layer/seed/test/10-action.sql
+++ data_layer/seed/test/10-action.sql
@@ -19,26 +19,39 @@ create function
 as
 $$
 -- désactive temporairement les triggers pour accélérer les inserts
-alter table action_statut disable trigger all;
-alter table action_commentaire disable trigger all;
+    alter table action_statut
+        disable trigger action_statut_check_insert;
+    alter table action_statut
+        disable trigger action_statut_check_update;
+    alter table action_statut
+        disable trigger after_action_statut_insert;
+    alter table action_statut
+        disable trigger save_history;
+    alter table action_commentaire
+        disable trigger save_history;
+    alter table action_commentaire
+        disable trigger set_modified_at_before_action_commentaire_update;
 
     -- Vide les tables
-truncate action_statut;
-truncate action_commentaire;
+    truncate action_statut;
+    truncate action_commentaire;
 
     -- Restaure les copies
-insert into public.action_statut
-select *
-from test.action_statut;
+    insert into public.action_statut
+    select *
+    from test.action_statut;
 
-insert into public.action_commentaire
-select *
-from test.action_commentaire;
+    insert into public.action_commentaire
+    select *
+    from test.action_commentaire;
 
 -- ré active les triggers
-alter table action_statut enable trigger all;
-alter table action_commentaire enable trigger all;
-
-$$ language sql security definer;
+    alter table action_statut
+        enable trigger all;
+    alter table action_commentaire
+        enable trigger all;
+    $$
+    language sql
+    security definer;
 comment on function test_reset_action_statut_and_desc is
     'Reinitialise les statuts et descriptions des actions du référentiel complétés par les collectivités.';
