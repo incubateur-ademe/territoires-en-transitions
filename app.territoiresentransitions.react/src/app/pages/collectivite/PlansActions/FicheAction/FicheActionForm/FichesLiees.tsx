b3b8a590386086534f81285429c307097ad55887
Arthur Molinos <arthur.molinos@gmail.com>
2023-04-12 10:44:49 +0200

    Les options pour les fiches liées à une fiche action apparaissent dans des sections plans d'actions

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/FichesLiees.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/FichesLiees.tsx
@@ -1,7 +1,7 @@
 import FormField from 'ui/shared/form/FormField';
 import ActionCard from '../../components/ActionCard';
 import AutocompleteInputSelect from 'ui/shared/select/AutocompleteInputSelect';
-import {TOption} from 'ui/shared/select/commons';
+import {TOptionSection} from 'ui/shared/select/commons';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {useFicheResumeListe} from '../data/useFicheResumeListe';
 import {
@@ -22,12 +22,56 @@ const FichesLiees = ({fiches, onSelect, isReadonly}: Props) => {
   const {data: ficheListe} = useFicheResumeListe();
   const collectiviteId = useCollectiviteId()!;
 
-  const formatOptions = (fiches?: FicheResume[] | null): TOption[] => {
-    const options = fiches
-      ? fiches.map((fiche: FicheResume) => ({
-          value: fiche.fiche_id!.toString(),
-          label: fiche.fiche_nom ?? 'Sans titre',
-        }))
+  const formatOptions = (fiches?: FicheResume[] | null): any[] => {
+    /** Récupère tous les plans liés aux fiches */
+    const plans = fiches?.reduce((acc: TAxeInsert[], fiche: FicheResume) => {
+      if (fiche.plans && fiche.plans[0] && fiche.plans[0] !== null) {
+        acc.push(fiche.plans[0]);
+      } else {
+        acc.push({
+          collectivite_id: fiche.collectivite_id!,
+          id: -1,
+          nom: 'Fiches non classées',
+        });
+      }
+      return acc;
+    }, []);
+
+    /** Supprime tous les doublons */
+    const uniquePlans = plans?.filter(
+      (plan, i, a) => a.findIndex(v => v.id === plan.id) === i
+    );
+
+    /** Génère les sections par plan avec fiches en options */
+    const options: TOptionSection[] = uniquePlans
+      ? uniquePlans.map(plan => {
+          // id -1 correspond aux fiches non classées
+          if (plan.id === -1) {
+            return {
+              title: 'Fiches non classées',
+              options:
+                fiches
+                  ?.filter(fiche => !fiche.plans || !fiche.plans[0])
+                  .map(fiche => ({
+                    value: fiche.fiche_id!.toString(),
+                    label: fiche.fiche_nom ?? 'Sans titre',
+                  })) ?? [],
+            };
+          } else {
+            return {
+              title: plan.nom ?? 'Sans titre',
+              options:
+                fiches
+                  ?.filter(
+                    fiche => fiche.plans && fiche.plans[0]?.id === plan.id
+                  )
+                  .map(fiche => ({
+                    value: fiche.fiche_id!.toString(),
+                    label: fiche.fiche_nom ?? 'Sans titre',
+                  })) ?? [],
+            };
+          }
+        })
       : [];
 
     return options;
