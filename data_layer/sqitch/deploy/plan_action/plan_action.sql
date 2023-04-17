cecc3716f8dc414d65f93249cf4e3127b783d28b
Florian d'Erfurth <florian@derfurth.com>
2023-04-17 14:33:45 +0200

    Répare le revert et le verify

--- data_layer/sqitch/deploy/plan_action/plan_action.sql
+++ data_layer/sqitch/deploy/plan_action/plan_action.sql
@@ -56,7 +56,9 @@ $$;
 drop function if exists enlever_annexe;
 
 -- Vue listant les fiches actions et ses données liées
-create or replace view fiches_action as
+drop function filter_fiches_action(collectivite_id integer, axes_id integer[], pilotes personne[], niveaux_priorite fiche_action_niveaux_priorite[], statuts fiche_action_statuts[], referents personne[]);
+drop view fiches_action;
+create  view fiches_action as
 select fa.*,
        t.thematiques,
        st.sous_thematiques,
@@ -386,5 +388,70 @@ end;
 $$ language plpgsql;
 
 drop table fiche_action_annexe;
+
+
+create or replace function
+    filter_fiches_action(
+    collectivite_id integer,
+    axes_id integer[] default null,
+    pilotes personne[] default null,
+    niveaux_priorite fiche_action_niveaux_priorite[] default null,
+    statuts fiche_action_statuts[] default null,
+    referents personne[] default null
+)
+    returns setof fiches_action
+as
+$$
+    # variable_conflict use_variable
+begin
+    if not can_read_acces_restreint(filter_fiches_action.collectivite_id) then
+        perform set_config('response.status', '403', true);
+        raise 'L''utilisateur n''a pas de droit en lecture sur la collectivité.';
+    end if;
+
+    return query
+        select *
+        from fiches_action fa
+        where fa.collectivite_id = collectivite_id
+          and case
+                  when axes_id is null then true
+                  else fa.id in (with child as (select unnest(array_append(a.descendants, a.axe_id)) as axe_id
+                                                from axe_descendants a
+                                                where a.descendants && (axes_id::integer[])
+                                                   or a.axe_id in (select * from unnest(axes_id::integer[])))
+                                 select fiche_id
+                                 from child
+                                          join fiche_action_axe using (axe_id))
+            end
+          and case
+                  when pilotes is null then true
+                  else fa.id in
+                       (select fap.fiche_id
+                        from fiche_action_pilote fap
+                        where fap.tag_id in (select (pi::personne).tag_id from unnest(pilotes) pi)
+                           or fap.user_id in (select (pi::personne).user_id from unnest(pilotes) pi))
+            end
+          and case
+                  when referents is null then true
+                  else fa.id in
+                       (select far.fiche_id
+                        from fiche_action_referent far
+                        where far.tag_id in (select (re::personne).tag_id from unnest(referents) re)
+                           or far.user_id in (select (re::personne).user_id from unnest(referents) re))
+            end
+          and case
+                  when niveaux_priorite is null then true
+                  else fa.niveau_priorite in (select * from unnest(niveaux_priorite::fiche_action_niveaux_priorite[]))
+            end
+          and case
+                  when statuts is null then true
+                  else fa.statut in (select * from unnest(statuts::fiche_action_statuts[]))
+            end;
+end;
+$$ language plpgsql security definer
+                    stable;
+comment on function filter_fiches_action is
+    'Filtre la vue pour le client.';
+
 
 COMMIT;
