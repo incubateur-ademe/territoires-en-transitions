04857d3eb8fcea1a923700feca616e963794be96
Florian d'Erfurth <florian@derfurth.com>
2023-04-17 17:24:29 +0200

    Revert la fonctionnalité `Amplitude`.

--- data_layer/sqitch/revert/stats/amplitude.sql
+++ data_layer/sqitch/revert/stats/amplitude.sql
@@ -2,6 +2,13 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+drop function stats.amplitude_send_yesterday_events;
+drop function stats.amplitude_send_visites;
+drop function stats.amplitude_visite;
+drop table stats.amplitude_log;
+drop view stats.release_version;
+drop type stats.amplitude_event;
+drop type stats.amplitude_event_type;
+drop table stats.amplitude_configuration;
 
 COMMIT;
