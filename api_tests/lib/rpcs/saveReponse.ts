a325cb23f54c20b4887859226b84f4c06ef507f3
Florian d'Erfurth <florian@derfurth.com>
2023-04-18 14:55:48 +0200

    Répare `saveReponse`.

--- api_tests/lib/rpcs/saveReponse.ts
+++ api_tests/lib/rpcs/saveReponse.ts
@@ -2,12 +2,13 @@ import { supabase } from "../supabase.ts";
 import { SaveReponseParams } from "../types/saveReponseParams.ts";
 
 export async function saveReponse(params: SaveReponseParams): Promise<void> {
-  const { status } = await supabase.rpc(
+  const { status, error } = await supabase.rpc(
     "save_reponse",
     // @ts-ignore()
-    params,
+    params
   );
-  if (status !== 200) {
-    throw `La RPC 'save_reponse' devrait renvoyer un code 200. (${status} != 200)`;
+  if (status !== 204) {
+    console.error(error);
+    throw `La RPC 'save_reponse' devrait renvoyer un code 204. (${status} != 204)`;
   }
 }
