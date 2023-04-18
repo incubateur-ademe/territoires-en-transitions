f16efb6eb4f914e7f7bba0c4413aacb9f08051ba
Florian d'Erfurth <florian@derfurth.com>
2023-04-18 14:18:28 +0200

    Corrige les types.

--- api_tests/lib/rpcs/labellisationCommencerAudit.ts
+++ api_tests/lib/rpcs/labellisationCommencerAudit.ts
@@ -1,13 +1,13 @@
 import { supabase } from "../supabase.ts";
 import { Database } from "../database.types.ts";
 
+type Audit = Database["public"]["Views"]["audit"]["Row"];
 export async function labellisationCommencerAudit(
-  audit_id: number,
-): Promise<Database["public"]["Tables"]["audit"]["Row"]> {
-  const { data } = await supabase.rpc(
-    "labellisation_commencer_audit",
-    { audit_id },
-  ).single();
+  audit_id: number
+): Promise<Audit> {
+  const { data } = await supabase
+    .rpc("labellisation_commencer_audit", { audit_id })
+    .single();
   if (!data) {
     throw `La RPC 'labellisation_commencer_audit' devrait renvoyer un audit.`;
   }
