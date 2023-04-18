4daa4bea0a8b5c03c63e9a6c092abff56521937d
Florian d'Erfurth <florian@derfurth.com>
2023-04-18 14:10:23 +0200

    Améliore la remontée d'erreur du `testReset`.

--- api_tests/lib/rpcs/testReset.ts
+++ api_tests/lib/rpcs/testReset.ts
@@ -1,10 +1,9 @@
 import { supabase } from "../supabase.ts";
 
 export async function testReset(): Promise<void> {
-  const { status } = await supabase.rpc(
-    "test_reset",
-  );
+  const { status, error } = await supabase.rpc("test_reset");
   if (status !== 200) {
+    console.error(error);
     throw `La RPC 'test_reset' devrait renvoyer un code 200. (${status} != 200)`;
   }
 }
