14e10f07d24dd18fa8eb8a0a4b114ac16b7fa151
Florian d'Erfurth <florian@derfurth.com>
2023-04-19 14:57:43 +0200

    Améliore les erreurs des RPCs.

--- api_tests/lib/rpcs/testCreateCollectivite.ts
+++ api_tests/lib/rpcs/testCreateCollectivite.ts
@@ -4,11 +4,11 @@ import { Database } from "../database.types.ts";
 export async function testCreateCollectivite(
   nom: string,
 ): Promise<Database["public"]["Tables"]["collectivite_test"]["Row"]> {
-  const { data } = await supabase.rpc(
-    "test_create_collectivite",
-    { nom },
-  ).single();
-  if (!data) {
+  const { data, error } = await supabase
+    .rpc("test_create_collectivite", { nom })
+    .single();
+  if (!data || error) {
+    console.error(error);
     throw `La RPC 'test_create_collectivite' devrait renvoyer une collectivité de test.`;
   }
 
--- api_tests/lib/rpcs/testFullfill.ts
+++ api_tests/lib/rpcs/testFullfill.ts
@@ -5,11 +5,11 @@ export async function testFullfill(
   collectivite_id: number,
   etoile: Database["labellisation"]["Enums"]["etoile"],
 ): Promise<void> {
-  const { error } = await supabase.rpc(
-    "test_fulfill",
-    { collectivite_id, etoile },
-  ).single();
+  const { error } = await supabase
+    .rpc("test_fulfill", { collectivite_id, etoile })
+    .single();
   if (error) {
+    console.error(error);
     throw `La RPC 'test_fulfill' ne devrait pas renvoyer d'erreur.`;
   }
 
