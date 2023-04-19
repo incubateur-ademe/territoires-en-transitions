2c9f2bd4d4b294c8d79fd23e0cdb79d910aaa07b
Marc Rutkowski <marc@attractive-media.fr>
2023-04-19 17:13:40 +0200

    met à jour les couleurs pour la jauge

--- app.territoiresentransitions.react/src/app/theme.ts
+++ app.territoiresentransitions.react/src/app/theme.ts
@@ -1,12 +1,12 @@
 import {TActionAvancementExt} from 'types/alias';
 
 export const actionAvancementColors: Record<TActionAvancementExt, string> = {
-  fait: '#0063CB',
-  programme: '#00A95F',
-  pas_fait: '#CE0500',
-  non_renseigne: '#565656',
-  non_concerne: '#D3D3D3',
-  detaille: '#E7A969',
+  non_concerne: '#929292',
+  non_renseigne: '#E5E5E5',
+  pas_fait: '#F95C5E',
+  programme: '#7AB1E8',
+  detaille: '#000091',
+  fait: '#34CB6A',
 };
 
 // Couleurs statuts actions référentiels
