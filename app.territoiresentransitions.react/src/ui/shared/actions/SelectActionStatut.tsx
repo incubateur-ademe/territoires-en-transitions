e6194a1ecc69ab51a1476bd2fdf1da87835cb238
Marc Rutkowski <marc@attractive-media.fr>
2023-04-19 17:28:46 +0200

    ajoute un nouveau sélecteur (partagé) du statut d'action

---
+++ app.territoiresentransitions.react/src/ui/shared/actions/SelectActionStatut.tsx
@@ -0,0 +1,74 @@
+import {TActionAvancement, TActionAvancementExt} from 'types/alias';
+import SelectDropdown from 'ui/shared/select/SelectDropdown';
+import {avancementToLabel} from 'app/labels';
+import ActionStatutBadge from './ActionStatutBadge';
+
+export type TSelectActionStatutProps = {
+  // item sélectionné (`non_renseigne` si `undefined` ou `null`)
+  value: TActionAvancementExt | undefined | null;
+  // appelée quand la sélection change
+  onChange: (value: TActionAvancementExt) => void;
+  // mode "lecture seule"
+  disabled?: boolean;
+  // pour afficher une liste différente d'items (`DEFAULT_ITEMS` si non spécifié)
+  items?: TActionAvancementExt[];
+};
+
+// transforme une liste de statuts en options pour la liste déroulante
+const getOptions = (items: TActionAvancementExt[]) =>
+  items.map(value => ({value, label: avancementToLabel[value]}));
+
+// les items par défaut (sans le "non concerné")
+export const DEFAULT_ITEMS: TActionAvancement[] = [
+  'non_renseigne',
+  'fait',
+  'programme',
+  'pas_fait',
+  'detaille',
+];
+
+export const ITEMS_AVEC_NON_CONCERNE: TActionAvancementExt[] = [
+  ...DEFAULT_ITEMS,
+  'non_concerne',
+];
+
+export const DEFAULT_OPTIONS = getOptions(DEFAULT_ITEMS);
+
+/**
+ * Affiche le sélecteur de statut d'une action
+ */
+export const SelectActionStatut = (props: TSelectActionStatutProps) => {
+  const {value, onChange, disabled, items} = props;
+
+  const options = items ? getOptions(items) : DEFAULT_OPTIONS;
+  const currentValue = value || 'non_renseigne';
+
+  if (disabled) {
+    return (
+      <ActionStatutBadge
+        statut={value as TActionAvancementExt}
+        small
+        className="mr-auto"
+      />
+    );
+  }
+
+  return (
+    <SelectDropdown
+      data-test="SelectStatut"
+      value={currentValue}
+      options={options}
+      onSelect={onChange}
+      buttonClassName="min-w-5rem"
+      renderOption={option => (
+        <ActionStatutBadge
+          statut={option.value as TActionAvancementExt}
+          small
+        />
+      )}
+      renderSelection={value => (
+        <ActionStatutBadge statut={value} small className="mr-auto" />
+      )}
+    />
+  );
+};
