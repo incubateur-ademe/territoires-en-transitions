7d6e3ba79023e2886107f9a934af2da1d496e65b
Arthur Molinos <arthur.molinos@gmail.com>
2023-04-20 19:09:26 +0200

    Donne un style différent si le tag a été créé par un utilisateur

--- app.territoiresentransitions.react/src/ui/shared/Tag.tsx
+++ app.territoiresentransitions.react/src/ui/shared/Tag.tsx
@@ -4,18 +4,25 @@ type TTag = {
   title: string;
   onCloseClick?: () => void;
   className?: string;
+  isUserCreated?: boolean;
 };
 
-const Tag = ({className, title, onCloseClick}: TTag) => {
+const Tag = ({className, title, onCloseClick, isUserCreated}: TTag) => {
+  if (!title || title.length === 0) {
+    return null;
+  }
   return (
     <div
       className={classNames(
-        'flex items-center px-4 py-1 text-white bg-bf500 rounded-full',
+        'group flex items-center px-4 py-1 text-white bg-bf500 rounded-full',
         {'pr-2': onCloseClick},
+        {
+          'bg-white border border-gray-300 text-gray-900': isUserCreated,
+        },
         className
       )}
     >
-      <span className="text-sm">{title}</span>
+      <span className="py-0.5 text-sm">{title}</span>
       {onCloseClick && (
         <div
           className="ml-1 rounded-full cursor-pointer"
