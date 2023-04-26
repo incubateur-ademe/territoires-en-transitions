5cfeaafb0341c5e3ebe31b4d592c3e8796040ac9
Florian d'Erfurth <florian@derfurth.com>
2023-04-26 09:13:42 +0200

    Nouvelle RPC `flat_axes` pour récupérer un plan d'action.

---
+++ data_layer/sqitch/deploy/plan_action/plan_action@v2.24.0.sql
@@ -0,0 +1,49 @@
+-- Deploy tet:plan_action to pg
+
+BEGIN;
+
+-- Fonction récursive pour afficher un plan d'action, ses axes, et ses fiches
+create or replace function plan_action(id integer) returns jsonb as
+$$
+declare
+    pa_enfant_id integer; -- Id d'un plan d'action enfant du plan d'action courant
+    pa_axe       axe; -- Axe courant
+    id_loop      integer; -- Indice pour parcourir une boucle
+    enfants      jsonb[]; -- Plans d'actions enfants du plan d'action courant;
+    fiches       jsonb; -- Fiches actions du plan d'action courant
+    to_return    jsonb; -- JSON retournant le plan d'action courant, ses fiches et ses enfants
+begin
+    fiches = to_jsonb((select array_agg(ff.*)
+                       from (select *
+                             from fiche_resume fa
+                                      join fiche_action_axe fapa on fa.id = fapa.fiche_id
+                             where fapa.axe_id = plan_action.id
+                             order by naturalsort(lower(fa.titre))) ff));
+    select * from axe where axe.id = plan_action.id limit 1 into pa_axe;
+    if not can_read_acces_restreint(pa_axe.collectivite_id) then
+        perform set_config('response.status', '403', true);
+        raise 'L''utilisateur n''a pas de droit en lecture sur la collectivité.';
+    end if;
+    id_loop = 1;
+    for pa_enfant_id in
+        select pa.id
+        from axe pa
+        where pa.parent = plan_action.id
+        order by naturalsort(lower(pa.nom))
+        loop
+            enfants[id_loop] = plan_action(pa_enfant_id);
+            id_loop = id_loop + 1;
+        end loop;
+
+    to_return = jsonb_build_object('axe', pa_axe,
+                                   'fiches', fiches,
+                                   'enfants', enfants);
+    return to_return;
+end;
+$$ language plpgsql security definer
+                    stable;
+comment on function plan_action is
+    'Fonction retournant un JSON contenant le plan d''action passé en paramètre,
+    ses fiches et ses plans d''actions enfants de manière récursive';
+
+COMMIT;
