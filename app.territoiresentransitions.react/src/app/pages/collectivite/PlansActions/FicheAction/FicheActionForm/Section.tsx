3b3fc29b7eaef2915cd5f1beae64d7715a0e2c7c
Arthur Molinos <arthur.molinos@gmail.com>
2023-04-26 12:12:38 +0200

    Corrige les couleurs de hover sur les sections de la fiche action et les axes du tableau nouvel emplacement de "Ranger la fiche"

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/Section.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/Section.tsx
@@ -17,9 +17,12 @@ const Section = ({dataTest, isDefaultOpen, icon, title, children}: Props) => {
     <div className="rounded-lg border border-gray-300">
       <button
         data-test={dataTest}
-        className={classNames('w-full flex items-center gap-3 py-4 px-6', {
-          'bg-indigo-100': isOpen,
-        })}
+        className={classNames(
+          'w-full flex items-center gap-3 py-3 px-6 hover:!bg-bf975',
+          {
+            'bg-indigo-100': isOpen,
+          }
+        )}
         onClick={() => setIsOpen(!isOpen)}
       >
         <IconArrowSFill
@@ -27,7 +30,7 @@ const Section = ({dataTest, isDefaultOpen, icon, title, children}: Props) => {
             '-rotate-90': !isOpen,
           })}
         />
-        <div className="w-8 h-8">{icon}</div>
+        <div className="w-12 h-12">{icon}</div>
         <span className="font-bold">{title}</span>
       </button>
       {isOpen && <div className="flex flex-col p-6">{children}</div>}
