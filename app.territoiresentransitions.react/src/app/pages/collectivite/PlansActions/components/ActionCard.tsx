cf8475662f63ed547b0b6a469040295bf2d03bd5
Arthur Molinos <arthur.molinos@gmail.com>
2023-04-27 14:54:49 +0200

    Ajout du skeleton des cartes fiche action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/components/ActionCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/components/ActionCard.tsx
@@ -15,7 +15,7 @@ const ActionCard = ({link, statutBadge, details, title}: TActionCard) => {
           {statutBadge && <div className="mb-4">{statutBadge}</div>}
           <div className="-mt-2 mb-3 text-xs text-gray-500">{details}</div>
           <div className="mb-auto font-bold line-clamp-3">{title}</div>
-          <span className="fr-fi-arrow-right-line ml-auto mt-4 text-bf500 scale-75" />
+          <span className="fr-fi-arrow-right-line ml-auto mt-3 text-bf500 scale-75" />
         </div>
       </NavLink>
     </div>
@@ -23,3 +23,12 @@ const ActionCard = ({link, statutBadge, details, title}: TActionCard) => {
 };
 
 export default ActionCard;
+
+export const ActionCardSkeleton = () => (
+  <div className="animate-pulse flex flex-col h-full p-6 border border-gray-200">
+    <div className="h-6 w-20 mb-2 rounded-md bg-gray-200" />
+    <div className="h-4 w-48 rounded-sm bg-gray-200" />
+    <div className="h-5 my-5 rounded-sm bg-gray-200" />
+    <span className="h-5 w-5 ml-auto bg-gray-200 rounded-full" />
+  </div>
+);
