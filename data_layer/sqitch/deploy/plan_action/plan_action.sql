068200618ac94d11b39e48c84b67c49ff2bd3496
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-04-28 15:43:39 +0200

    Ajout des nouvelles cibles dans les données

--- data_layer/sqitch/deploy/plan_action/plan_action.sql
+++ data_layer/sqitch/deploy/plan_action/plan_action.sql
@@ -2,52 +2,22 @@
 
 BEGIN;
 
-create type flat_axe_node as
-(
-    id        integer,
-    nom       text,
-    fiches    integer[],
-    ancestors integer[],
-    depth     integer
-);
-
-create function
-    flat_axes(plan_id integer)
-    returns setof flat_axe_node
-    stable
-begin
-    atomic
-    with recursive
-        parents as (select id,
-                           nom,
-                           collectivite_id,
-                           0                   as depth,
-                           array []::integer[] as ancestors
-                    from axe
-                    where parent is null
-                      and id = plan_id
-                      and can_read_acces_restreint(axe.collectivite_id)
-
-                    union all
-
-                    select a.id,
-                           a.nom,
-                           a.collectivite_id,
-                           depth + 1,
-                           ancestors || a.parent
-                    from parents
-                             join axe a on a.parent = parents.id),
-        fiches as (select a.id,
-                          array_agg(faa.fiche_id) as fiches
-                   from parents a
-                            join fiche_action_axe faa on a.id = faa.axe_id
-                   group by a.id)
-    select id, nom, fiches, ancestors, depth
-    from parents
-             left join fiches using (id)
-    order by depth, naturalsort(nom);
-end;
-comment on function flat_axes is
-    'Les axes ordonnancés par profondeur d''un plan donné sous forme de nodes prêtes pour reconstituer un arbre coté client.';
+alter type fiche_action_cibles
+    add value if not exists 'Public Scolaire'
+        after 'Grand public et associations';
+alter type fiche_action_cibles
+    add value if not exists 'Acteurs économiques du secteur primaire';
+alter type fiche_action_cibles
+    add value if not exists 'Acteurs économiques du secteur secondaire';
+alter type fiche_action_cibles
+    add value if not exists 'Acteurs économiques du secteur tertiaire';
+alter type fiche_action_cibles
+    add value if not exists 'Partenaires';
+alter type fiche_action_cibles
+    add value if not exists 'Collectivité elle-même';
+alter type fiche_action_cibles
+    add value if not exists 'Elus locaux';
+alter type fiche_action_cibles
+    add value if not exists 'Agents';
 
 COMMIT;
