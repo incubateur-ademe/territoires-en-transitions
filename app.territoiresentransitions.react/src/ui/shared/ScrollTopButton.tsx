ad97b06fbf8cd74ccfad99e5c3fd0ce4d9fcd431
Marc Rutkowski <marc@attractive-media.fr>
2023-05-03 14:48:48 +0200

    change le bouton "Haut de page" pour qu'il fonctionne quand le panneau latéral est ouvert

--- app.territoiresentransitions.react/src/ui/shared/ScrollTopButton.tsx
+++ app.territoiresentransitions.react/src/ui/shared/ScrollTopButton.tsx
@@ -1,6 +1,13 @@
 const ScrollTopButton = () => {
   return (
-    <button onClick={() => window.scrollTo(0, 0)}>
+    <button
+      onClick={() =>
+        document
+          .getElementsByClassName('fr-header')
+          .item(0)
+          ?.scrollIntoView({behavior: 'smooth'})
+      }
+    >
       <div className="flex items-center text-lg text-bf500 pb-1 border-b border-bf500">
         <div className="fr-fi-arrow-up-line scale-90 mt-0.5 mr-2" />
         <span>Haut de page</span>
