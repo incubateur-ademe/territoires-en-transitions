5f612b1e49d36e7ee2e3d282da046a27f529020e
Marc Rutkowski <marc@attractive-media.fr>
2023-05-03 14:45:15 +0200

    ajoute un conteneur pour le panneau latéral dans la mise en page générale

--- app.territoiresentransitions.react/src/app/Layout/Layout.tsx
+++ app.territoiresentransitions.react/src/app/Layout/Layout.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 
 import Header from 'app/Layout/Header';
+import {createPortal} from 'react-dom';
 import Footer from './Footer';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const RIGHT_PANEL_ID = 'right';
+
 const Layout = ({children}: Props) => {
   return (
-    <div className="overflow-hidden">
-      <Header />
-      {children}
-      <Footer />
+    <div className="flex flex-row h-[100vh] overflow-hidden">
+      <div className="w-full overflow-y-auto">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+      <aside
+        id={RIGHT_PANEL_ID}
+        className="shrink-0 max-w-md border-l border-l-gray-200 bg-white overflow-y-auto"
+      />
     </div>
   );
 };
 
 export default Layout;
+
+/** Permet de définir le contenu du panneau latéral */
+export const setRightPanelContent = (children: ReactNode) =>
+  createPortal(children, document.getElementById(RIGHT_PANEL_ID)!);
