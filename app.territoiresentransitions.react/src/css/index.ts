fad377318a71982dcd967b2748b9b111bc4d28e8
Marc Rutkowski <marc@attractive-media.fr>
2023-05-05 16:00:19 +0200

    ajoute une définition d'icône manquante

--- app.territoiresentransitions.react/src/css/index.ts
+++ app.territoiresentransitions.react/src/css/index.ts
@@ -13,6 +13,7 @@ import '@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-device/icons-device.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-design/icons-design.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-others/icons-others.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-communication/icons-communication.css';
 
 // patches
 import 'css/app.css';
