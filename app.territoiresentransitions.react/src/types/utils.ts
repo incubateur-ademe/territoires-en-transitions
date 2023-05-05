b312795ea9137d4c6d10cb2d5c8e7921fc85c85e
Marc Rutkowski <marc@attractive-media.fr>
2023-05-05 15:45:45 +0200

    ajoute un utilitaire de typage

--- app.territoiresentransitions.react/src/types/utils.ts
+++ app.territoiresentransitions.react/src/types/utils.ts
@@ -4,3 +4,16 @@
 export type NonNullableFields<T> = {
   [P in keyof T]: NonNullable<T[P]>;
 };
+
+/**
+ * Génère un type résultant de la concaténation de deux chaînes.
+ *
+ * Utilisable avec un type union, comme par exemple :
+ * ```
+ * type TItemId = 'exemples' | 'contexte'
+ * type TRPCName = Prefix<'action_', TItemId> // 'action_exemples' | 'action_contexte'
+ * ```
+ *
+ * Ref: https://stackoverflow.com/questions/73135992/add-a-prefix-to-each-type-in-a-string-union-type
+ */
+export type Prefix<P extends string, S extends string> = `${P}${S}`;
