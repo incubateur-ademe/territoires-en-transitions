97beb577d97cb969be3af118e78707049fa20462
Marc Rutkowski <marc@attractive-media.fr>
2023-05-09 10:20:39 +0200

    replace le positionnement au début du contenu lorsqu'on passe d'un item des infos associées à une action à un autre

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionInfo/ActionInfoDetail.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionInfo/ActionInfoDetail.tsx
@@ -1,3 +1,4 @@
+import {Ref, useEffect, useRef} from 'react';
 import {addTargetToContentAnchors} from 'utils/content';
 import {TTOCItem} from './type';
 import {useActionInfoData} from './useActionInfoData';
@@ -16,12 +17,18 @@ export const ActionInfoDetail = ({
   const titre = `${num}. ${label}`;
   const {data} = useActionInfoData(id, actionId);
 
+  // positionnement au début du contenu lorsqu'on passe d'un item à un autre
+  const ref: Ref<HTMLDivElement> = useRef(null);
+  useEffect(() => {
+    ref?.current?.scrollTo(0, 0);
+  }, [data]);
+
   return (
     <>
       <p className="bg-[#433EBA] text-white font-bold fr-px-3w fr-py-3v m-0">
         {titre}
       </p>
-      <div className="overflow-y-auto fr-px-3w fr-pb-2w fr-mt-2w">
+      <div ref={ref} className="overflow-y-auto fr-px-3w fr-pb-2w fr-mt-2w">
         {data ? (
           <div
             dangerouslySetInnerHTML={{__html: addTargetToContentAnchors(data)}}
