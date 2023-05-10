c8313a45072105a7d910998d4f86ad318686ee82
Florian d'Erfurth <florian@derfurth.com>
2023-05-10 09:37:01 +0200

    Commentaires sur les points potentiels.

--- business/business/utils/models/action_score.py
+++ business/business/utils/models/action_score.py
@@ -4,14 +4,26 @@ from typing import Optional
 from business.utils.models.actions import ActionId, ActionReferentiel
 
 
-@dataclass  # TODO : decide how to type this output for client !
+@dataclass
 class ActionScore:
-    action_id: ActionId  # eg.  "eci_1.0"
+    """ Le score d'une action.
+
+    Une action est un nœud de l'arbre constituant le référentiel.
+    Son score représente les points obtenus ainsi que la valeur de l'action.
+    """
+
+    action_id: ActionId
+    "L'id de l'action, ex: eci_1.1"
+
     point_fait: float
     point_programme: float
     point_pas_fait: float
     point_non_renseigne: float
+
     point_potentiel: float
+    "Le potentiel est calculé à partir des conséquences de la personnalisation, " \
+        "soit point référentiel x facteur de la personnalisation"
+
     point_referentiel: float
     concerne: bool
     total_taches_count: int
@@ -21,4 +33,8 @@ class ActionScore:
     pas_fait_taches_avancement: float
     pas_concerne_taches_avancement: float
     desactive: bool
+
     point_potentiel_perso: Optional[float]
+    "Le potentiel personnalisé est calculé à partir du potentiel, " \
+        "auquel on ajoute le reliquat des points des actions désactivées"
+
