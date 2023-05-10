3494034ef9c2f74e8cbaea38b35fe672a86dca79
Arthur Molinos <arthur.molinos@gmail.com>
2023-05-10 10:30:58 +0200

    Bouge les actions du footer d'un plan d'action dans le header

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionHeader.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionHeader.tsx
@@ -1,16 +1,54 @@
+import {makeCollectiviteFichesNonClasseesUrl} from 'app/paths';
+import SupprimerAxeModal from './SupprimerAxeModal';
 import {PlanNode} from './data/types';
+import {checkAxeHasFiche} from './data/utils';
+import {useExportPlanAction} from './export/useExportPlanAction';
 
 type TPlanActionHeader = {
   plan: PlanNode;
   collectivite_id: number;
+  isReadonly?: boolean;
 };
 
-const PlanActionHeader = ({collectivite_id, plan}: TPlanActionHeader) => {
+const PlanActionHeader = ({
+  collectivite_id,
+  plan,
+  isReadonly,
+}: TPlanActionHeader) => {
+  const {exportPlanAction, isLoading} = useExportPlanAction(plan.id);
+
   return (
     <div className="">
       <div className="py-6 flex justify-between">
         {/** Actions */}
-        <div className="flex items-center gap-4 ml-auto" />
+        {!isReadonly && (
+          <div className="flex items-center gap-4 ml-auto">
+            <SupprimerAxeModal
+              axe={plan}
+              plan={plan}
+              redirectURL={makeCollectiviteFichesNonClasseesUrl({
+                collectiviteId: collectivite_id!,
+              })}
+            >
+              <button
+                data-test="SupprimerPlanBouton"
+                className="fr-btn fr-btn--tertiary fr-btn--sm fr-fi-delete-line"
+                title="Supprimer ce plan d'action"
+              />
+            </SupprimerAxeModal>
+            {checkAxeHasFiche(plan) && !isReadonly ? (
+              <button
+                data-test="export-pa"
+                className="fr-btn fr-btn--tertiary fr-btn--sm fr-fi-download-line"
+                title="Exporter"
+                disabled={isLoading}
+                onClick={() => {
+                  exportPlanAction();
+                }}
+              />
+            ) : null}
+          </div>
+        )}
       </div>
     </div>
   );
