f8afa661c85398e97ca2d6ad693f46895a1a3f16
Marc Rutkowski <marc@attractive-media.fr>
2023-05-11 12:03:55 +0200

    améliore et commente les utilitaires de formatage de nombres

--- app.territoiresentransitions.react/src/utils/toFixed.ts
+++ app.territoiresentransitions.react/src/utils/toFixed.ts
@@ -1,6 +1,14 @@
+/**
+ * Transforme un nombre avec le nombre de décimales voulu
+ */
 export const toFixed = (n: number, d = 1) => {
-  return Number(Number(`${n}e${d}`).toFixed() + `e-${d}`);
+  return Number(`${Number(`${n}e${d}`).toFixed()}e-${d}`);
 };
 
+/**
+ * Formate un nombre avec le nombre de décimales voulu et en tenant compte de la
+ * langue du navigateur (utilise la virgule comme séparateur des décimales, et
+ * l'espace comme séparateur des milliers en français)
+ */
 export const toLocaleFixed = (n: number, maximumFractionDigits = 1): string =>
-  n.toLocaleString(undefined, {maximumFractionDigits});
+  n ? n.toLocaleString(undefined, {maximumFractionDigits}) : '0';
