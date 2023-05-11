5ab647b0644da5c0b0c28e7c1180fa501be8cde1
Marc Rutkowski <marc@attractive-media.fr>
2023-05-11 17:36:48 +0200

    remplace un typage manuel par un alias sur les types exportés depuis la base

--- app.territoiresentransitions.react/src/generated/dataLayer/departement_read/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Element de la liste `departement`, utilisée par la vue toutes les
- * collectivités.
- */
-export type DepartementRead = {
-  code: string;
-  libelle: string;
-  region_code: string;
-};
