f010a8c883d72a255231289232192353f1f3ae9d
Marc Rutkowski <marc@attractive-media.fr>
2023-05-11 17:50:01 +0200

    remplace un typage manuel par un alias sur les types exportés depuis la base

--- app.territoiresentransitions.react/src/generated/dataLayer/personnalisation_regle_read/index.ts
+++
@@ -1,8 +0,0 @@
-export type TPersonnalisationRegleRead = {
-  action_id: string;
-  type: TRegleType;
-  formule: string;
-  description: string;
-};
-
-type TRegleType = 'score' | 'desactivation' | 'reduction';
