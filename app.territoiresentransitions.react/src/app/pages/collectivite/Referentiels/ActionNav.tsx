2a7f77a87bb977927a9dfada7bc647df68486f41
Marc Rutkowski <marc@attractive-media.fr>
2023-05-11 11:34:36 +0200

    extrait la génération des liens Action précédente/suivante et crée une variante du composant pour l'affichage en haut de page

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionNav.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionNav.tsx
@@ -1,84 +1,63 @@
-import {useMemo} from 'react';
-import {Link, useParams} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-import {
-  ReferentielParamOption,
-  referentielParam,
-  makeCollectiviteActionUrl,
-} from 'app/paths';
-import {useReferentielDownToAction} from 'core-logic/hooks/referentiel';
-import {useCollectiviteId} from 'core-logic/hooks/params';
+/**
+ * Affiche les boutons "Action précédente" et "Action suivante" en haut de page
+ */
+export const ActionTopNav = ({
+  prevActionLink,
+  nextActionLink,
+}: {
+  prevActionLink: string | undefined;
+  nextActionLink: string | undefined;
+}) => (
+  <div className="flex justify-between fr-text--sm !m-0 fr-mt-2w overflow-hidden">
+    {prevActionLink ? (
+      <Link
+        to={prevActionLink}
+        className="fr-fi-arrow-left-line fr-btn--icon-left active-transparent"
+      >
+        Action précédente
+      </Link>
+    ) : (
+      <div />
+    )}
+    {nextActionLink && (
+      <Link
+        to={nextActionLink}
+        className="justify-self-end fr-fi-arrow-right-line fr-btn--icon-right active-transparent"
+      >
+        Action suivante
+      </Link>
+    )}
+  </div>
+);
 
-const ActionNav = ({actionId}: {actionId: string}) => {
-  /**
-   * Créer un tableau contenant uniquement les sous-axes et actions
-   */
-  const {referentielId} = useParams<{
-    [referentielParam]?: ReferentielParamOption;
-  }>();
-  const currentReferentiel = referentielId ?? 'eci';
-
-  const actions = useReferentielDownToAction(currentReferentiel);
-
-  const filteredActions = useMemo(() => {
-    return actions.filter(action => action.type === 'action');
-  }, [actions]);
-
-  /**
-   * Génération des lien "Précédent" et "Suivant"
-   */
-  const collectiviteId = useCollectiviteId();
-
-  const currentActionIndex = useMemo(() => {
-    return filteredActions.findIndex(({id}) => id === actionId);
-  }, [filteredActions]);
-
-  const prevActionLink = useMemo(() => {
-    if (currentActionIndex <= 0) {
-      return;
-    } else {
-      const prevAction = filteredActions[currentActionIndex - 1];
-      const link = makeCollectiviteActionUrl({
-        collectiviteId: collectiviteId!,
-        referentielId: currentReferentiel,
-        actionId: prevAction.id,
-      });
-      return link;
-    }
-  }, [currentActionIndex]);
-
-  const nextActionLink = useMemo(() => {
-    if (currentActionIndex >= filteredActions.length - 1) {
-      return;
-    } else {
-      const nextAction = filteredActions[currentActionIndex + 1];
-      const link = makeCollectiviteActionUrl({
-        collectiviteId: collectiviteId!,
-        referentielId: currentReferentiel,
-        actionId: nextAction.id,
-      });
-      return link;
-    }
-  }, [currentActionIndex]);
-
-  return (
-    <div className="flex justify-end mt-8">
-      {prevActionLink && (
-        <Link to={prevActionLink} className="mr-4 !shadow-none">
-          <button className="fr-btn fr-btn--secondary fr-fi-arrow-left-line fr-btn--icon-left">
-            Action précédente
-          </button>
-        </Link>
-      )}
-      {nextActionLink && (
-        <Link to={nextActionLink} className="!shadow-none">
-          <button className="fr-btn fr-btn fr-fi-arrow-right-line fr-btn--icon-right">
-            Action suivante
-          </button>
-        </Link>
-      )}
-    </div>
-  );
-};
-
-export default ActionNav;
+/**
+ * Affiche les boutons "Action précédente" et "Action suivante" en bas de page
+ */
+export const ActionBottomNav = ({
+  prevActionLink,
+  nextActionLink,
+}: {
+  prevActionLink: string | undefined;
+  nextActionLink: string | undefined;
+}) => (
+  <div className="flex justify-end mt-8 gap-4">
+    {prevActionLink && (
+      <Link
+        to={prevActionLink}
+        className="fr-btn fr-btn--secondary fr-fi-arrow-left-line fr-btn--icon-left"
+      >
+        Action précédente
+      </Link>
+    )}
+    {nextActionLink && (
+      <Link
+        to={nextActionLink}
+        className="fr-btn fr-btn fr-fi-arrow-right-line fr-btn--icon-right"
+      >
+        Action suivante
+      </Link>
+    )}
+  </div>
+);
