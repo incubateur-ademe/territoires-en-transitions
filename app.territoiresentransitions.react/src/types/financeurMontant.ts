2f6d13083c24bd933c5d8fc164d9e5847cfeec3a
Marc Rutkowski <marc@attractive-media.fr>
2023-05-15 09:44:44 +0200

    suppression de code inutilisé

--- app.territoiresentransitions.react/src/types/financeurMontant.ts
+++
@@ -1,7 +0,0 @@
-import {Database} from 'types/database.types';
-
-export type FinanceurMontant = {
-  id?: number;
-  montant_ttc?: number;
-  financeur_tag: Database['public']['Tables']['financeur_tag']['Insert'];
-};
