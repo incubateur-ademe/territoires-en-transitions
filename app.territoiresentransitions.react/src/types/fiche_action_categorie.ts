1a0c00f1f4579de3048260fdbc71844cc665ed35
Marc Rutkowski <marc@attractive-media.fr>
2023-05-15 09:44:44 +0200

    suppression de code inutilisé

--- app.territoiresentransitions.react/src/types/fiche_action_categorie.ts
+++
@@ -1,7 +0,0 @@
-export interface FicheActionCategorie {
-  epci_id: string;
-  uid: string;
-  parent_uid: string;
-  nom: string;
-  fiche_actions_uids: string[];
-}
--- app.territoiresentransitions.react/src/utils/score.ts
+++ app.territoiresentransitions.react/src/utils/score.ts
@@ -1,6 +1,1 @@
-import {ActionScore} from 'types/ClientScore';
-
-export const getNonConcernePoint = (score: ActionScore) =>
-  Math.max(score.point_referentiel - score.point_potentiel, 0);
-
 export const toPercentString = (value: number) => `${Math.round(value * 100)}%`;
