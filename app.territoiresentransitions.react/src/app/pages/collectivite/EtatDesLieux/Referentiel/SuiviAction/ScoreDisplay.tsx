5e8c6a6a0cf99d9f3b3c681bed8f5d4a6a5dc955
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-05-16 10:10:44 +0200

    Mise à jour de l'affichage du score

--- app.territoiresentransitions.react/src/app/pages/collectivite/EtatDesLieux/Referentiel/SuiviAction/ScoreDisplay.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/EtatDesLieux/Referentiel/SuiviAction/ScoreDisplay.tsx
@@ -1,11 +1,14 @@
 import classNames from 'classnames';
 import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
 import {CheckIcon} from 'ui/icons/CheckIcon';
+import {toLocaleFixed} from 'utils/toFixed';
 import {useScoreRealise} from '../data/useScoreRealise';
 
 type ScoreDisplayProps = {
   action: ActionDefinitionSummary;
+  legend?: string;
   size?: 'xs' | 'sm';
+  className?: string;
 };
 
 /**
@@ -13,22 +16,34 @@ type ScoreDisplayProps = {
  * pour une action donnée
  */
 
-const ScoreDisplay = ({action, size}: ScoreDisplayProps): JSX.Element => {
+const ScoreDisplay = ({
+  action,
+  legend,
+  size,
+  className,
+}: ScoreDisplayProps): JSX.Element => {
   const {pointsRealises, pointsMax} = useScoreRealise(action);
 
   return (
-    <div
+    <span
       className={classNames({
         visible: pointsRealises !== null && pointsMax !== null,
         invisible: pointsRealises === null || pointsMax === null,
         'text-xs': size === 'xs',
         'text-sm': size === 'sm',
         'text-base': size === undefined,
+        className,
       })}
     >
-      <CheckIcon className="h-4 inline-block" />
-      {pointsRealises} / {pointsMax} points
-    </div>
+      <CheckIcon
+        className={classNames('h-4 inline-block', {
+          'mr-1': size === 'xs',
+          'mr-2': size === 'sm',
+        })}
+      />
+      {legend ? `${legend} : ` : ''}
+      {toLocaleFixed(pointsRealises, 2)} / {toLocaleFixed(pointsMax, 2)} points
+    </span>
   );
 };
 
--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionHeader.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionHeader.tsx
@@ -1,13 +1,11 @@
 import classNames from 'classnames';
 import ActionProgressBar from 'ui/referentiels/ActionProgressBar';
 import {ActionDefinitionSummary} from 'core-logic/api/endpoints/ActionDefinitionSummaryReadEndpoint';
-import {CheckIcon} from 'ui/icons/CheckIcon';
-import {toLocaleFixed} from 'utils/toFixed';
 import {useScrollTop} from 'utils/useScrollTop';
 import {ActionTopNav} from './ActionNav';
 import {ActionSidePanel} from './ActionSidePanel';
-import {useScoreRealise} from '../EtatDesLieux/Referentiel/data/useScoreRealise';
 import {PersoPotentiel} from '../PersoPotentielModal/PersoPotentiel';
+import ScoreDisplay from '../EtatDesLieux/Referentiel/SuiviAction/ScoreDisplay';
 
 /**
  * Affiche la partie de l'en-tête de la page Action sensible à la position du
@@ -22,8 +20,6 @@ export const ActionHeader = ({
   nextActionLink: string | undefined;
   prevActionLink: string | undefined;
 }) => {
-  const {pointsRealises, pointsMax} = useScoreRealise(action);
-
   // détermine si la page a suffisamment défilée pour afficher la version
   // réduite de l'en-tête
   const scrollTop = useScrollTop('main');
@@ -62,16 +58,12 @@ export const ActionHeader = ({
         </div>
       </div>
       <div className="fr-container flex justify-between items-center fr-text--sm fr-my-2w">
-        <div className="flex gap-4 fr-pl-1v text-grey425">
+        <div className="flex gap-4 items-center fr-pl-1v text-grey425">
           <ActionProgressBar
             actionId={action.id}
             className="border-r border-r-[#ddd] fr-pr-5v"
           />
-          <span className="!mb-0">
-            <CheckIcon className="mr-2 h-4 inline-block" />
-            Score réalisé : {toLocaleFixed(pointsRealises, 2)} /{' '}
-            {toLocaleFixed(pointsMax, 2)} points
-          </span>
+          <ScoreDisplay action={action} legend="Score réalisé" size="sm" />
           {action.have_questions && (
             <div className="border-l border-l-[#ddd] fr-pl-3v">
               <PersoPotentiel actionDef={action} />
