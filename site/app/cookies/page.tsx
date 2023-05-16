fd06e5c2ee489f3278c0d5d313837f5bd6cfa317
Marc Rutkowski <marc@attractive-media.fr>
2023-05-16 17:06:40 +0200

    ajoute la page "Gestion des cookies"

---
+++ site/app/cookies/page.tsx
@@ -0,0 +1,16 @@
+export default function Cookies() {
+  return (
+    <div className="fr-container">
+      <div className="fr-mt-1w fr-mt-md-4w fr-mb-5w">
+        <h1 className="fr-header__body">Gestion des cookies</h1>
+        <p>Ce site n'utilise pas de cookie.</p>
+        <p>
+          Nous sommes ainsi en conformité avec la réglementation « Cookies » de
+          la CNIL et exemptés d’autorisation préalable. C’est pour cela que vous
+          n’avez pas eu besoin de cliquer sur un bloc pour accepter le dépôt de
+          cookies !
+        </p>
+      </div>
+    </div>
+  );
+}
