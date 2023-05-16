90e730c84c908037d234dc12833f2f34a4d6d1af
Marc Rutkowski <marc@attractive-media.fr>
2023-05-16 17:22:45 +0200

    met à jour le pied de page

--- site/app/AppFooter.tsx
+++ site/app/AppFooter.tsx
@@ -5,33 +5,25 @@ const AppFooter = () => {
     <footer className="fr-footer" role="contentinfo" id="footer">
       <div className="fr-container">
         <div className="fr-footer__body">
-          <div className="fr-footer__brand fr-enlarge-link">
+          <div className="fr-footer__brand">
             <p className="fr-logo">
               République
               <br />
               Française
             </p>
-            <a
-              href="/"
-              title="Accueil - Territoires en Transitions"
-              className="fr-footer__brand-link"
-            >
-              <Image src="/ademe.jpg" alt="ADEME" width="128" height="146" />
-            </a>
+            <Image
+              src="/ademe.jpg"
+              alt="ADEME"
+              width="128"
+              height="146"
+              className="fr-ml-2w"
+            />
           </div>
           <div className="fr-footer__content">
             <p className="fr-footer__content-desc">
               Territoires en Transitions est une startup d'État portée par
               l'Agence de la Transition Écologique (ADEME) avec le soutien de
-              l'Agence Nationale de la Cohésion des Territoires (ANCT).{' '}
-              <a
-                href="https://github.com/betagouv/territoires-en-transitions"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Le code source
-              </a>{' '}
-              est en licence libre.
+              l'Agence Nationale de la Cohésion des Territoires (ANCT).
             </p>
             <ul className="fr-footer__content-list">
               <li className="fr-footer__content-item">
@@ -70,21 +62,15 @@ const AppFooter = () => {
         <div className="fr-footer__bottom">
           <ul className="fr-footer__bottom-list">
             <li className="fr-footer__bottom-item">
-              <a
-                className="fr-footer__bottom-link"
-                href="https://aide.territoiresentransitions.fr/fr/article/comment-contacter-lequipe-territoires-en-transitions-ngu7tg/"
-              >
-                Nous contacter
-              </a>
-            </li>
-            <li className="fr-footer__bottom-item">
               <a className="fr-footer__bottom-link" href="/stats">
                 Statistiques
               </a>
             </li>
             <li className="fr-footer__bottom-item">
               <span className="fr-footer__bottom-link">
-                Accessibilité : partiellement conforme
+                <a className="fr-footer__bottom-link" href="/accessibilite">
+                  Accessibilité : non conforme
+                </a>
               </span>
             </li>
             <li className="fr-footer__bottom-item">
@@ -96,8 +82,25 @@ const AppFooter = () => {
               <a
                 className="fr-footer__bottom-link"
                 href="https://www.ademe.fr/donnees-personnelles/"
+                target="_blank"
+                rel="noreferrer"
               >
                 Données personnelles
+              </a>
+            </li>
+            <li className="fr-footer__bottom-item">
+              <a className="fr-footer__bottom-link" href="/cookies">
+                Gestion des cookies
+              </a>
+            </li>
+            <li className="fr-footer__bottom-item">
+              <a
+                className="fr-footer__bottom-link"
+                href="https://github.com/betagouv/territoires-en-transitions"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Code source
               </a>
             </li>
             <li className="fr-footer__bottom-item">
