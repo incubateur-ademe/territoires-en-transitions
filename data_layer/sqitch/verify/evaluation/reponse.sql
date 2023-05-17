600eac0b1b0cccf4bff6f6364486ad9915e6c2f8
Florian d'Erfurth <florian@derfurth.com>
2023-05-17 11:42:40 +0200

    Ajoute le texte de la justification dans la vue `reponse_display`.

--- data_layer/sqitch/verify/evaluation/reponse.sql
+++ data_layer/sqitch/verify/evaluation/reponse.sql
@@ -2,26 +2,8 @@
 
 BEGIN;
 
-select modified_at, collectivite_id, question_id, reponse
-from reponse_choix
-where false;
-
-select modified_at, collectivite_id, question_id, reponse
-from reponse_binaire
-where false;
-
-select modified_at, collectivite_id, question_id, reponse
-from reponse_proportion
-where false;
-
-select collectivite_id, reponses
-from business_reponse
-where false;
-
-select question_id, collectivite_id, reponse
+select question_id, collectivite_id, reponse, justification
 from reponse_display
 where false;
-
-select has_function_privilege('save_reponse(json)', 'execute');
 
 ROLLBACK;
