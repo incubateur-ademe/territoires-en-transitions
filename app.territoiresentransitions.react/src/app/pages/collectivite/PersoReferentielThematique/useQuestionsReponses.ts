75e52439962794b1f9f6f62c72c952b725710804
Marc Rutkowski <marc@attractive-media.fr>
2023-05-17 15:05:03 +0200

    sépare les hooks de chargement des questions/réponses

--- app.territoiresentransitions.react/src/app/pages/collectivite/PersoReferentielThematique/useQuestionsReponses.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PersoReferentielThematique/useQuestionsReponses.ts
@@ -1,17 +1,11 @@
-import {useQueries, useQuery} from 'react-query';
-import {supabaseClient} from 'core-logic/api/supabase';
-import {useCollectiviteId} from 'core-logic/hooks/params';
-import {TQuestionRead, TReponseRead, TReponse} from 'types/personnalisation';
-
-type TFilters = {
-  action_ids?: string[];
-  thematique_id?: string;
-};
+import {TReponse} from 'types/personnalisation';
+import {TFilters, useQuestions} from './useQuestions';
+import {useReponses} from './useReponses';
 
 /**
  * Charge la liste des questions de personnalisation et leur éventuelle réponse
  * pour la collectivité courante. La liste est filtrable par action(s) ou par
- * thématique
+ * thématique.
  */
 export const useQuestionsReponses = (filters: TFilters) => {
   // charge les questions et les réponses
@@ -34,104 +28,4 @@ export const useQuestionsReponses = (filters: TFilters) => {
   });
 
   return qrList || [];
-};
-
-// charge les questions
-const useQuestions = (filters: TFilters) => {
-  const collectivite_id = useCollectiviteId();
-
-  return useQuery(
-    ['questions', collectivite_id, filters],
-    () => fetchQuestions(collectivite_id!, filters),
-    {enabled: !!collectivite_id, initialData: []}
-  );
-};
-const fetchQuestions = async (collectivite_id: number, filters: TFilters) => {
-  const query = supabaseClient
-    .from('question_display')
-    .select()
-    .eq('collectivite_id', collectivite_id);
-
-  const {action_ids, thematique_id} = filters || {};
-  if (action_ids) {
-    query.contains('action_ids', action_ids);
-  }
-
-  if (thematique_id) {
-    query.eq('thematique_id', thematique_id);
-  }
-
-  // attends les données
-  const {error, data} = await query;
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return data as unknown as TQuestionRead[];
-};
-
-// charge les réponses correspondant aux questions données
-const useReponses = (questions: TQuestionRead[]) => {
-  const collectivite_id = useCollectiviteId();
-
-  // une requête par question pour permettre le rechargement individuel
-  const queries = questions.map(q => ({
-    queryKey: ['reponse', collectivite_id, q.id],
-    queryFn: () => fetchReponse(collectivite_id!, q.id),
-    enabled: !!collectivite_id,
-  }));
-
-  return useQueries(queries);
-};
-const fetchReponse = async (collectivite_id: number, question_id: string) => {
-  const query = supabaseClient
-    .from('reponse_display')
-    .select()
-    .match({collectivite_id, question_id});
-
-  // attends les données
-  const {error, data} = await query;
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return data?.length ? transform(data[0] as TReponseRead) : undefined;
-};
-
-// met à jour si nécessaire la valeur d'une réponse lue depuis la base
-const transform = (row: TReponseRead) => {
-  const {reponse} = row;
-  const {type, reponse: reponseValue} = reponse;
-
-  // transforme en pourcentage une réponse de type proportion
-  if (type === 'proportion') {
-    const value =
-      typeof reponseValue === 'number' ? (reponseValue * 100).toFixed(0) : '';
-    return setReponseValue(row, value);
-  }
-
-  // transforme une valeur booléen en id (oui/non) du bouton radio correspondant
-  if (reponseValue !== null && type === 'binaire') {
-    if (reponseValue === true) return setReponseValue(row, 'oui');
-    if (reponseValue === false) return setReponseValue(row, 'non');
-    return setReponseValue(row, null);
-  }
-
-  // autres cas: renvoi la réponse inchangée
-  return row;
-};
-
-// change la valeur dans une réponse et renvoi l'objet résultant
-const setReponseValue = (row: TReponseRead, reponseValue: TReponse) => {
-  const {reponse} = row;
-
-  return {
-    ...row,
-    reponse: {
-      ...reponse,
-      reponse: reponseValue,
-    },
-  };
 };
