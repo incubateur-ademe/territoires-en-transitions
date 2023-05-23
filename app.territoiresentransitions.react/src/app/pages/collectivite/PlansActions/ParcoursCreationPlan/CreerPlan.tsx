74928133cc1c4632f8c909e53e52ff5f8b887b06
Arthur Molinos <arthur.molinos@gmail.com>
2023-05-23 08:43:35 +0200

    Création du parcours import/création plan d'action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
@@ -0,0 +1,82 @@
+import {Form, Formik} from 'formik';
+import FormikInput from 'ui/shared/form/formik/FormikInput';
+import FormikSelect from 'ui/shared/form/formik/FormikSelect';
+import * as Yup from 'yup';
+import {useCreatePlanAction} from '../PlanAction/data/useUpsertAxe';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+
+const PlanTypeOptions = [
+  {
+    value: 'Plan Climat Air Énergie territorial',
+    label: 'Plan Climat Air Énergie territorial',
+  },
+  {value: 'Plan Économie circulaire', label: 'Plan Économie circulaire'},
+  {value: 'Plan Territoire Engagé', label: 'Plan Territoire Engagé'},
+  {value: 'Plan thématique', label: 'Plan thématique'},
+  {value: 'Projet de territoire', label: 'Projet de territoire'},
+  {value: 'Autre', label: 'Autre'},
+];
+
+const formValidation = Yup.object({
+  nom: Yup.string(),
+  type: Yup.string(),
+});
+
+type FormProps = {nom: string; type: string};
+
+type Props = {
+  onBackClick: () => void;
+};
+
+const CreerPlan = ({onBackClick}: Props) => {
+  const collectivite_id = useCollectiviteId();
+
+  const {mutate: createPlanAction} = useCreatePlanAction();
+
+  const handleFormSubmit = (values: FormProps) => {
+    createPlanAction({
+      collectivite_id: collectivite_id!,
+      nom: values.nom,
+      // type: values.type,
+    });
+  };
+
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto py-14 px-24 bg-gray-100">
+      <div className="mb-8 text-gray-500">Aucun champ n’est obligatoire</div>
+
+      <Formik
+        initialValues={{nom: '', type: ''}}
+        validationSchema={formValidation}
+        onSubmit={handleFormSubmit}
+      >
+        <Form>
+          <FormikInput
+            name="nom"
+            label="Nom du plan d’action"
+            hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
+          />
+          <FormikSelect
+            name="type"
+            label="Type de plan d’action"
+            hint="Cette information sera utile pour vous proposer une arborescence et un format de fiche adaptés"
+            options={PlanTypeOptions}
+          />
+          <div className="flex items-center justify-end gap-6 mt-12">
+            <button
+              className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line"
+              onClick={onBackClick}
+            >
+              Revenir à l’étape précédente
+            </button>
+            <button type="submit" className="fr-btn !mb-0">
+              Valider
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default CreerPlan;
---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/ImporterPlan.tsx
@@ -0,0 +1,63 @@
+import Alerte from 'ui/shared/Alerte';
+
+type Props = {
+  onBackClick: () => void;
+};
+
+const ImporterPlan = ({onBackClick}: Props) => {
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto py-14 px-24 bg-gray-100">
+      <div className="mb-1 text-sm">Étape 1</div>
+      <h6 className="mb-4">Téléchargez le modèle de plan d’action</h6>
+      <p>
+        Ce modèle correspond au format des fiches proposé dans la plateforme.
+      </p>
+      <a
+        className="fr-btn fr-btn--icon-left fr-icon-download-line"
+        href="https://docs.google.com/spreadsheets/d/1hmpj74Smtrzp-d5R3Rwlf9VRnokeKVDn/edit#gid=246577666"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Télécharger le modèle
+      </a>
+      <div className="h-[1px] my-8 bg-gray-300" />
+
+      <div className="mb-1 text-sm">Étape 2</div>
+      <h6 className="mb-4">
+        Adaptez votre plan d’action existant au format requis
+      </h6>
+      <p>
+        Renommez les colonnes de votre document tableur en suivant le modèle ;
+        rassemblez plusieurs contenus si nécessaire.
+      </p>
+      <Alerte
+        state="information"
+        description="Pour toute question, contactez-nous sur le support en ligne ou par email à contact@territoiresentransitions.fr Nous sommes là pour vous aider !"
+      />
+      <div className="h-[1px] my-8 bg-gray-300" />
+
+      <div className="mb-1 text-sm">Étape 3</div>
+      <h6 className="mb-4">Envoyez-nous ce document par email</h6>
+      <p>
+        Adresse :{' '}
+        <span className="font-bold">contact@territoiresentransitions.fr</span>
+      </p>
+      <p>
+        Nous nous chargerons de l’importer sur la plateforme pour vous permettre
+        de démarrer le suivi de votre plan d’action !
+      </p>
+
+      <div className="flex items-center gap-6 ml-auto mt-6">
+        <button
+          className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line"
+          onClick={onBackClick}
+        >
+          Revenir à l’étape précédente
+        </button>
+        <button className="fr-btn !mb-0">Valider</button>
+      </div>
+    </div>
+  );
+};
+
+export default ImporterPlan;
