d548c73c6f58707c41c2a98b9d8a2228e62d9902
Florian d'Erfurth <florian@derfurth.com>
2023-05-25 10:35:29 +0200

    Utilise pipenv pour parser les contenus.

--- business/referentiel_parse_all.sh
+++ business/referentiel_parse_all.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-python referentiel_cli.py parse-preuves\
+pipenv run python referentiel_cli.py parse-preuves\
     --input-markdown-folder "../markdown/preuves"\
     --output-json-file "../content/preuves.json"
 
-python referentiel_cli.py parse-actions\
+pipenv run python referentiel_cli.py parse-actions\
     --input-markdown-folder "../markdown/referentiels/cae"\
     --output-json-file "../content/cae.json"
 
-python referentiel_cli.py parse-actions\
+pipenv run python referentiel_cli.py parse-actions\
     --input-markdown-folder "../markdown/referentiels/eci"\
     --output-json-file "../content/eci.json"
 
-python referentiel_cli.py parse-indicateurs\
+pipenv run python referentiel_cli.py parse-indicateurs\
     --input-markdown-folder "../markdown/indicateurs/**"\
     --output-json-file "../content/indicateurs.json"
 
-python referentiel_cli.py parse-personnalisations\
+pipenv run python referentiel_cli.py parse-personnalisations\
     --questions-markdown-folder "../markdown/questions"\
     --regles-markdown-folder "../markdown/personnalisations"\
     --output-json-file "../content/personnalisations.json"
