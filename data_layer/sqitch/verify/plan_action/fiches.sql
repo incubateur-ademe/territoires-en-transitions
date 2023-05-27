e7a9887782986c9200467677f669db40826ff21d
Florian d'Erfurth <florian@derfurth.com>
2023-05-27 15:32:23 +0200

    Permet de filtrer les fiches par axes et sous-axes.

--- data_layer/sqitch/verify/plan_action/fiches.sql
+++ data_layer/sqitch/verify/plan_action/fiches.sql
@@ -2,45 +2,6 @@
 
 BEGIN;
 
-select has_function_privilege('upsert_fiche_action()', 'execute');
 select has_function_privilege('filter_fiches_action( integer,  integer[],  personne[],  fiche_action_niveaux_priorite[],  fiche_action_statuts[],  personne[])', 'execute');
-
-select
-       id,
-       titre,
-       description,
-       piliers_eci,
-       objectifs,
-       resultats_attendus,
-       cibles,
-       ressources,
-       financements,
-       budget_previsionnel,
-       statut,
-       niveau_priorite,
-       date_debut,
-       date_fin_provisoire,
-       amelioration_continue,
-       calendrier,
-       notes_complementaires,
-       maj_termine,
-       collectivite_id,
-       created_at,
-       modified_at,
-       modified_by,
-       thematiques,
-       sous_thematiques,
-       partenaires,
-       structures,
-       pilotes,
-       referents,
-       axes,
-       actions,
-       indicateurs,
-       services,
-       financeurs,
-       fiches_liees
-from fiches_action
-where false;
 
 ROLLBACK;
