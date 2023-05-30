823435b5ef8d8767ac44030b4a255595712bbda6
Arthur Molinos <arthur.molinos@gmail.com>
2023-05-30 11:44:57 +0200

    Ajoute le titre des étapes dans le parcours de création d'un plan

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
@@ -42,39 +42,44 @@ const CreerPlan = ({onBackClick}: Props) => {
   };
 
   return (
-    <div className="flex flex-col max-w-3xl mx-auto py-14 px-24 bg-gray-100">
-      <div className="mb-8 text-gray-500">Aucun champ n’est obligatoire</div>
-
-      <Formik
-        initialValues={{nom: '', type: ''}}
-        validationSchema={formValidation}
-        onSubmit={handleFormSubmit}
-      >
-        <Form>
-          <FormikInput
-            name="nom"
-            label="Nom du plan d’action"
-            hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
-          />
-          <FormikSelect
-            name="type"
-            label="Type de plan d’action"
-            hint="Cette information sera utile pour vous proposer une arborescence et un format de fiche adaptés"
-            options={PlanTypeOptions}
-          />
-          <div className="flex items-center justify-end gap-6 mt-12">
-            <button
-              className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line"
-              onClick={onBackClick}
-            >
-              Revenir à l’étape précédente
-            </button>
-            <button type="submit" className="fr-btn !mb-0">
-              Valider
-            </button>
-          </div>
-        </Form>
-      </Formik>
+    <div className="w-full max-w-3xl mx-auto">
+      <h3 className="mb-8">
+        <span className="fr-icon-edit-box-fill mr-2" />
+        Créer un plan d’action
+      </h3>
+      <div className="flex flex-col mt-2 mb-10 py-14 px-24 bg-gray-100">
+        <div className="mb-8 text-gray-500">Aucun champ n’est obligatoire</div>
+        <Formik
+          initialValues={{nom: '', type: ''}}
+          validationSchema={formValidation}
+          onSubmit={handleFormSubmit}
+        >
+          <Form>
+            <FormikInput
+              name="nom"
+              label="Nom du plan d’action"
+              hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
+            />
+            <FormikSelect
+              name="type"
+              label="Type de plan d’action"
+              hint="Cette information sera utile pour vous proposer une arborescence et un format de fiche adaptés"
+              options={PlanTypeOptions}
+            />
+            <div className="flex items-center justify-end gap-6 mt-12">
+              <button
+                className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line"
+                onClick={onBackClick}
+              >
+                Revenir à l’étape précédente
+              </button>
+              <button type="submit" className="fr-btn !mb-0">
+                Valider
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
