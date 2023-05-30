34dcd19cfad516cb946781e29cf72d2d2db64834
Arthur Molinos <arthur.molinos@gmail.com>
2023-05-30 19:09:10 +0200

    Refactorise les filtres et l'arborescence d'un plan d'action pour les mettre dans des composants dédiés

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanAction.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanAction.tsx
@@ -2,11 +2,7 @@ import {useParams} from 'react-router-dom';
 
 import PlanActionHeader from './PlanActionHeader';
 import PlanActionAxe from './PlanActionAxe';
-import PictoLeaf from 'ui/pictogrammes/PictoLeaf';
-import {AxeActions} from './AxeActions';
-import FicheActionCard from '../FicheAction/FicheActionCard';
 
-import {makeCollectivitePlanActionFicheUrl} from 'app/paths';
 import {usePlanAction} from './data/usePlanAction';
 import {useEditAxe} from './data/useEditAxe';
 import {PlanNode} from './data/types';
@@ -16,7 +12,7 @@ import PlanActionFiltres from './PlanActionFiltres/PlanActionFiltres';
 import {useFichesActionFiltresListe} from '../FicheAction/data/useFichesActionFiltresListe';
 import {checkAxeHasFiche} from './data/utils';
 import HeaderTitle from '../components/HeaderTitle';
-import PlanActionAxeFiches from './PlanActionAxeFiches';
+import PlanActionArborescence from './PlanActionArborescence';
 
 type PlanActionProps = {
   plan: PlanNode;
@@ -28,27 +24,23 @@ export const PlanAction = ({plan, axe}: PlanActionProps) => {
 
   const isReadonly = collectivite?.readonly ?? false;
 
+  const isAxePage = axe !== undefined;
+
   const {mutate: updateAxe} = useEditAxe(plan.id);
 
   const {items: fichesActionsListe, ...ficheFilters} =
     useFichesActionFiltresListe({plan, axe});
 
-  const displaySousAxe = (node: PlanNode) => (
-    <PlanActionAxe
-      key={node.id}
-      planActionGlobal={plan}
-      axe={node}
-      displayAxe={displaySousAxe}
-      isReadonly={isReadonly}
-    />
-  );
+  // On prend à partir de 2 éléments car
+  // les filtres "collectivite_id" et "plan/axe id" sont des constantes
+  const isFiltered = Object.keys(ficheFilters.filters).length > 2;
 
   return (
     <div data-test="PlanAction" className="w-full">
       <HeaderTitle
-        type={axe ? 'axe' : 'plan'}
-        titre={axe ? axe.nom : plan.nom}
-        onUpdate={nom => updateAxe({id: axe ? axe.id : plan.id, nom})}
+        type={isAxePage ? 'axe' : 'plan'}
+        titre={isAxePage ? axe.nom : plan.nom}
+        onUpdate={nom => updateAxe({id: isAxePage ? axe.id : plan.id, nom})}
         isReadonly={isReadonly}
       />
       <div className="max-w-4xl mx-auto px-10">
@@ -58,104 +50,25 @@ export const PlanAction = ({plan, axe}: PlanActionProps) => {
           axe={axe}
           isReadonly={isReadonly}
         />
-        {/** On vérifie si le plan contient des fiches pour afficher les filtres de fiche */}
-        {!axe && checkAxeHasFiche(plan) && (
+        {/** On vérifie si le plan ou l'axe contient des fiches pour afficher les filtres de fiche */}
+        {((!isAxePage && checkAxeHasFiche(plan)) ||
+          (isAxePage && checkAxeHasFiche(axe))) && (
           <PlanActionFiltres
+            planId={plan.id.toString()}
             itemsNumber={ficheFilters.total}
             initialFilters={ficheFilters.initialFilters}
             filters={ficheFilters.filters}
             setFilters={ficheFilters.setFilters}
+            fichesActionsListe={fichesActionsListe}
+            isFiltered={isFiltered}
           />
         )}
-        {/** Si il y a d'autres filtres activés en plus de la collectivite et le plan,
-         alors on affiche les fiches filtrées, sinon le plan d'action */}
-        {Object.keys(ficheFilters.filters).length > 2 ? (
-          fichesActionsListe.length > 0 ? (
-            <div className="grid grid-cols-2 gap-4">
-              {fichesActionsListe.map(fiche => (
-                <FicheActionCard
-                  key={fiche.id}
-                  ficheAction={fiche}
-                  link={makeCollectivitePlanActionFicheUrl({
-                    collectiviteId: collectivite!.collectivite_id!,
-                    planActionUid: plan.id.toString(),
-                    ficheUid: fiche.id!.toString(),
-                  })}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="mt-16 mb-8">
-              Aucune fiche ne correspond à votre recherche
-            </div>
-          )
-        ) : // Affiche les fiches et sous-axes s'il y en a, sinon un état vide
-        (!axe &&
-            (plan.children.length > 0 ||
-              (plan.fiches && plan.fiches.length > 0))) ||
-          (axe &&
-            (axe.children.length > 0 ||
-              (axe.fiches && axe.fiches.length > 0))) ? (
-          <>
-            <div className="mb-4">
-              {!isReadonly && (
-                <AxeActions
-                  planActionId={plan.id}
-                  axeId={axe ? axe.id : plan.id}
-                />
-              )}
-              {/** Affichage des fiches */}
-              {((plan.fiches && plan.fiches.length !== 0) ||
-                (axe && axe.fiches && axe.fiches.length !== 0)) && (
-                <div className="mt-6">
-                  <PlanActionAxeFiches
-                    ficheIds={axe ? axe.fiches : plan.fiches}
-                    axeId={axe ? axe.id : plan.id}
-                  />
-                </div>
-              )}
-            </div>
-            {/** Affichage des sous-axes */}
-            {axe &&
-              axe.children &&
-              axe.children.length > 0 &&
-              axe.children.map(enfant => (
-                <PlanActionAxe
-                  key={enfant.id}
-                  planActionGlobal={axe ? axe : plan}
-                  axe={enfant}
-                  displayAxe={displaySousAxe}
-                  isReadonly={isReadonly}
-                />
-              ))}
-            {!axe &&
-              plan.children &&
-              plan.children.length > 0 &&
-              plan.children.map(enfant => (
-                <PlanActionAxe
-                  key={enfant.id}
-                  planActionGlobal={plan}
-                  axe={enfant}
-                  displayAxe={displaySousAxe}
-                  isReadonly={isReadonly}
-                />
-              ))}
-          </>
-        ) : (
-          <div>
-            <div className="flex flex-col items-center my-8">
-              <PictoLeaf className="w-24 fill-gray-400" />
-              <div className="my-6 text-gray-500">
-                Aucune arborescence pour l'instant
-              </div>
-              {!isReadonly && (
-                <AxeActions
-                  planActionId={plan.id}
-                  axeId={axe ? axe.id : plan.id}
-                />
-              )}
-            </div>
-          </div>
+        {!isFiltered && (
+          <PlanActionArborescence
+            plan={plan}
+            axe={axe ?? plan}
+            isReadonly={isReadonly}
+          />
         )}
         <PlanActionFooter plan={plan} isReadonly={isReadonly} />
       </div>
--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionFiltres/PlanActionFiltres.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/PlanActionFiltres/PlanActionFiltres.tsx
@@ -8,24 +8,32 @@ import {DesactiverLesFiltres} from 'ui/shared/filters/DesactiverLesFiltres';
 
 import {TFilters} from '../../FicheAction/data/filters';
 import {Accordion} from 'ui/Accordion';
+import {FicheAction} from '../../FicheAction/data/types';
+import FicheActionCard from '../../FicheAction/FicheActionCard';
+import {makeCollectivitePlanActionFicheUrl} from 'app/paths';
+import {useCollectiviteId} from 'core-logic/hooks/params';
 
 type Props = {
+  planId: string;
   itemsNumber: number;
   initialFilters: TFilters;
   filters: TFilters;
   setFilters: (filters: TFilters) => void;
+  fichesActionsListe: FicheAction[];
+  isFiltered: boolean;
 };
 
 const PlanActionFiltres = ({
+  planId,
   itemsNumber,
   initialFilters,
   filters,
   setFilters,
+  fichesActionsListe,
+  isFiltered,
 }: Props) => {
+  const collectivite_id = useCollectiviteId();
   const [isFiltersOpen, setIsFiltersOpen] = useState(false);
-
-  // On prend à partir de 2 éléments car les filtres "collectivite_id" et "plan/axe id" sont des constantes
-  const isFiltered = Object.keys(filters).length > 2;
 
   useEffect(() => {
     if (isFiltered) {
@@ -34,7 +42,7 @@ const PlanActionFiltres = ({
   }, []);
 
   return (
-    <>
+    <div>
       <Accordion
         id="filtres-plan"
         className="mb-8"
@@ -50,14 +58,35 @@ const PlanActionFiltres = ({
         initialState={isFiltersOpen}
       />
       {isFiltered && (
-        <div className="flex items-baseline gap-6 my-8">
-          <span className="text-sm text-gray-400">
-            {itemsNumber} résultat{itemsNumber > 1 && 's'}
-          </span>
-          <DesactiverLesFiltres onClick={() => setFilters(initialFilters)} />
-        </div>
+        <>
+          <div className="flex items-baseline gap-6 my-8">
+            <span className="text-sm text-gray-400">
+              {itemsNumber} résultat{itemsNumber > 1 && 's'}
+            </span>
+            <DesactiverLesFiltres onClick={() => setFilters(initialFilters)} />
+          </div>
+          {fichesActionsListe.length > 0 ? (
+            <div className="grid grid-cols-2 gap-4">
+              {fichesActionsListe.map(fiche => (
+                <FicheActionCard
+                  key={fiche.id}
+                  ficheAction={fiche}
+                  link={makeCollectivitePlanActionFicheUrl({
+                    collectiviteId: collectivite_id!,
+                    planActionUid: planId,
+                    ficheUid: fiche.id!.toString(),
+                  })}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="mt-16 mb-8">
+              Aucune fiche ne correspond à votre recherche
+            </div>
+          )}
+        </>
       )}
-    </>
+    </div>
   );
 };
 
