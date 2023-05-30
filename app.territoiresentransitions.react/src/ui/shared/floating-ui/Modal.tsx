824e83d69c85f00172fbcd7b5e5f7a8a28e9a334
Arthur Molinos <arthur.molinos@gmail.com>
2023-05-30 11:29:55 +0200

    Améliore la fonction de changement de l'état d'ouverture de la Modale et ajoute la possibilité d'appeler une fonction à la fermeture

--- app.territoiresentransitions.react/src/ui/shared/floating-ui/Modal.tsx
+++ app.territoiresentransitions.react/src/ui/shared/floating-ui/Modal.tsx
@@ -19,11 +19,11 @@ export type RenderProps = {
 };
 
 export type ModalProps = {
-  /* où il faut mettre le contenu de la modale */
+  /** où il faut mettre le contenu de la modale */
   render: (props: RenderProps) => React.ReactNode;
-  /* l'élément qui permet d'afficher la modale au click */
+  /** l'élément qui permet d'afficher la modale au click */
   children?: JSX.Element;
-  /* s'il n'y a pas d'élément permettant d'afficher la modale,
+  /** s'il n'y a pas d'élément permettant d'afficher la modale,
      on peut lui passer un booléen mais celui ci doit être accompagné d'un setter (setExternalOpen)
      afin de pouvoir la fermer */
   externalOpen?: boolean;
@@ -31,7 +31,9 @@ export type ModalProps = {
   setExternalOpen?:
     | Dispatch<SetStateAction<boolean>>
     | ((opened: boolean) => void);
-  /* max-width prédéfinies dans le DSFR, valeur par défaut "md" */
+  /** fonction appelée lors de la fermeture de la modale */
+  onClose?: () => void;
+  /** max-width prédéfinies dans le DSFR, valeur par défaut "md" */
   size?: 'sm' | 'md' | 'lg' | 'xl';
   /** désactive la fermeture lors du clic sur le fond */
   disableDismiss?: boolean;
@@ -50,6 +52,7 @@ const Modal = ({
   children,
   externalOpen,
   setExternalOpen,
+  onClose,
   size = 'md',
   disableDismiss,
   noCloseButton,
@@ -57,9 +60,16 @@ const Modal = ({
 }: ModalProps) => {
   const [open, setOpen] = useState(false);
 
+  const handleOpenChange = () => {
+    setExternalOpen ? setExternalOpen(!externalOpen) : setOpen(!open);
+    if (externalOpen || open) {
+      onClose && onClose();
+    }
+  };
+
   const {refs, context} = useFloating({
     open: externalOpen ?? open,
-    onOpenChange: setExternalOpen ?? setOpen,
+    onOpenChange: handleOpenChange,
   });
 
   const id = useId();
@@ -71,10 +81,6 @@ const Modal = ({
     useRole(context),
     useDismiss(context, {enabled: !disableDismiss}),
   ]);
-
-  const handleCloseClick = () => {
-    setExternalOpen ? setExternalOpen(false) : setOpen(false);
-  };
 
   return (
     <>
@@ -117,7 +123,7 @@ const Modal = ({
                 <div className="flex flex-col p-4 bg-white md:px-8">
                   {noCloseButton ? null : (
                     <button
-                      onClick={handleCloseClick}
+                      onClick={handleOpenChange}
                       className="flex items-center ml-auto mb-2 pl-2 py-0.5 md:-mr-4 fr-btn--secondary !shadow-none"
                       data-test="close-modal"
                     >
