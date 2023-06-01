169d1f03e24c431366398b8d311994ec4bd32f0b
Marc Rutkowski <marc@attractive-media.fr>
2023-06-01 11:16:00 +0200

    gère l'enregistrement des fiches action liées à une action du référentiel

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/useUpdateFichesActionLiees.ts
@@ -0,0 +1,53 @@
+import {useMutation, useQueryClient} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {FicheResume} from '../PlansActions/FicheAction/data/types';
+
+type TUpdateFichesActionLieesArgs = {
+  /** liste courante des fiches associées à l'action */
+  fiches: FicheResume[];
+  /** liste mise à jour des fiches associées à l'action */
+  fiches_liees: FicheResume[];
+};
+
+/**
+ * Met à jour la liste des fiches action liées à une action
+ */
+export const useUpdateFichesActionLiees = (action_id: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async ({fiches, fiches_liees}: TUpdateFichesActionLieesArgs) => {
+      // extrait les ids des listes
+      const current_ids = fiches.map(f => f.id);
+      const new_ids = fiches_liees.map(f => f.id);
+      // extrait les ids des fiches à ajouter ou supprimer
+      const idsToDelete = diff(current_ids, new_ids);
+      const idsToAdd = diff(new_ids, current_ids);
+
+      // supprime les anciennes entrées
+      if (idsToDelete.length) {
+        await supabaseClient.from('fiche_action_action').delete().match({
+          fiche_id: idsToDelete,
+          action_id,
+        });
+      }
+
+      // et ajoute les nouvelles
+      if (idsToAdd.length) {
+        const toAdd = idsToAdd.map(fiche_id => ({
+          fiche_id: fiche_id!,
+          action_id,
+        }));
+        await supabaseClient.from('fiche_action_action').insert(toAdd);
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['fiche_action_action', action_id]);
+      },
+    }
+  );
+};
+
+const diff = <T>(array1: T[], array2: T[]) =>
+  array1.filter(v => !array2.includes(v));
