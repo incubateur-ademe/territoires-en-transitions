c18212d0a5e7c4c15edad585647a0640c8ab1e2f
Florian d'Erfurth <florian@derfurth.com>
2023-06-01 16:22:40 +0200

    Répare le bug de la demande absente.

--- data_layer/sqitch/revert/labellisation/preuve_v2@v2.31.0.sql
+++ data_layer/sqitch/revert/labellisation/preuve_v2@v2.31.0.sql
@@ -73,7 +73,8 @@ select 'labellisation',
        null,
        null
 from labellisation.demande d
-         join preuve_labellisation p on p.demande_id = d.id
+         left join preuve_labellisation p on p.demande_id = d.id
+         left join audit a on d.id = a.demande_id
          left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
 
 union all
@@ -106,12 +107,11 @@ select 'audit',
        utilisateur.modified_by_nom(p.modified_by),
        null,
        null,
-       case when d is not null then to_jsonb(d) end,
        null,
-       to_jsonb(a)
+       null,
+       to_jsonb(p) || to_jsonb(a)
 from audit a
          join preuve_audit p on p.audit_id = a.id
-         left join labellisation.demande d on a.demande_id = d.id
          left join labellisation.bibliotheque_fichier_snippet fs on fs.id = p.fichier_id
 ;
 
