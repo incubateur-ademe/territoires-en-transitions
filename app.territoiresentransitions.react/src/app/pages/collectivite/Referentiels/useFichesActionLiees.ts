f048f56ba3e3ff1b3583afdb3afbd8508373389b
Marc Rutkowski <marc@attractive-media.fr>
2023-06-05 17:05:10 +0200

    change la manière de charger les fiches action liées à une action (et ses sous-actions)

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/useFichesActionLiees.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/useFichesActionLiees.ts
@@ -1,38 +1,38 @@
 import {useQuery} from 'react-query';
 
 import {supabaseClient} from 'core-logic/api/supabase';
-import {useFicheResumeListe} from '../PlansActions/FicheAction/data/useFicheResumeListe';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {FicheResume} from '../PlansActions/FicheAction/data/types';
 
 // charge les fiches actions liées à une action du référentiel
 export const useFichesActionLiees = (actionId: string) => {
-  // charge et cache toutes les fiches résumées des plans
-  const {data: fichesResumees, isLoading: isLoading1} = useFicheResumeListe();
+  const collectivite_id = useCollectiviteId();
 
-  // charge les identifiants des fiches action liées à une action
-  const {data: fichesLiees, isLoading: isLoading2} = useQuery(
-    ['fiche_action_action', actionId],
-    () => fetch(actionId)
+  // charge les fiches action liées à une action
+  const {data: fichesLiees, isLoading} = useQuery(
+    ['fiche_action_liees', collectivite_id, actionId],
+    () => (collectivite_id ? fetch(collectivite_id, actionId) : [])
   );
 
   // chargement encore en cours ou pas de données
-  const isLoading = isLoading1 || isLoading2;
-  if (isLoading || !fichesResumees || !fichesLiees) {
+  if (isLoading || !fichesLiees) {
     return {data: [], isLoading};
   }
 
-  // renvoi les résumés correspondants aux id
+  // renvoi les données
   return {
     isLoading: false,
-    data: fichesResumees.filter(f => fichesLiees.includes(f.id!)),
+    data: fichesLiees,
   };
 };
 
-// charge les identifiants des fiches action liées à une action (ou ses sous-actions)
-const fetch = async (action_id: string) => {
+// charge les fiches action liées à une action (et ses sous-actions)
+const fetch = async (collectivite_id: number, action_id: string) => {
   const {data} = await supabaseClient
     .from('fiche_action_action')
-    .select('fiche_id')
+    .select('fiche_resume(*)')
+    .eq('fiche_resume.collectivite_id', collectivite_id)
     .like('action_id', `${action_id}%`);
 
-  return data?.map(({fiche_id}) => fiche_id);
+  return data?.map(({fiche_resume}) => fiche_resume as FicheResume);
 };
