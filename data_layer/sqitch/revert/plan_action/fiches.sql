b82b70bbb937025e3c8cc0cf56a04761197af50d
Florian d'Erfurth <florian@derfurth.com>
2023-06-05 17:35:54 +0200

    Ajoute la fonction `fiche_resume(fiche_action_action)` pour les fiches action liées.

--- data_layer/sqitch/revert/plan_action/fiches.sql
+++ data_layer/sqitch/revert/plan_action/fiches.sql
@@ -2,67 +2,6 @@
 
 BEGIN;
 
--- on ne revert pas la vue fiche resume
-
-create or replace function
-    filter_fiches_action(
-    collectivite_id integer,
-    axes_id integer[] default null,
-    pilotes personne[] default null,
-    niveaux_priorite fiche_action_niveaux_priorite[] default null,
-    statuts fiche_action_statuts[] default null,
-    referents personne[] default null
-)
-    returns setof fiches_action
-as
-$$
-    # variable_conflict use_variable
-begin
-    if not can_read_acces_restreint(filter_fiches_action.collectivite_id) then
-        perform set_config('response.status', '403', true);
-        raise 'L''utilisateur n''a pas de droit en lecture sur la collectivité.';
-    end if;
-
-    return query
-        select *
-        from fiches_action fa
-        where fa.collectivite_id = collectivite_id
-          and case
-                  when axes_id is null then true
-                  else fa.id in (with child as (select unnest(array_append(a.descendants, a.axe_id)) as axe_id
-                                                from axe_descendants a
-                                                where a.descendants && (axes_id::integer[])
-                                                   or a.axe_id in (select * from unnest(axes_id::integer[])))
-                                 select fiche_id
-                                 from child
-                                          join fiche_action_axe using (axe_id))
-            end
-          and case
-                  when pilotes is null then true
-                  else fa.id in
-                       (select fap.fiche_id
-                        from fiche_action_pilote fap
-                        where fap.tag_id in (select (pi::personne).tag_id from unnest(pilotes) pi)
-                           or fap.user_id in (select (pi::personne).user_id from unnest(pilotes) pi))
-            end
-          and case
-                  when referents is null then true
-                  else fa.id in
-                       (select far.fiche_id
-                        from fiche_action_referent far
-                        where far.tag_id in (select (re::personne).tag_id from unnest(referents) re)
-                           or far.user_id in (select (re::personne).user_id from unnest(referents) re))
-            end
-          and case
-                  when niveaux_priorite is null then true
-                  else fa.niveau_priorite in (select * from unnest(niveaux_priorite::fiche_action_niveaux_priorite[]))
-            end
-          and case
-                  when statuts is null then true
-                  else fa.statut in (select * from unnest(statuts::fiche_action_statuts[]))
-            end;
-end;
-$$ language plpgsql security definer
-                    stable;
+drop function fiche_resume;
 
 COMMIT;
--- data_layer/sqitch/verify/plan_action/fiches.sql
+++ data_layer/sqitch/verify/plan_action/fiches.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
-select has_function_privilege('filter_fiches_action( integer,  integer[],  personne[],  fiche_action_niveaux_priorite[],  fiche_action_statuts[],  personne[])', 'execute');
+select has_function_privilege('fiche_resume(fiche_action_action)', 'execute');
 
 ROLLBACK;
