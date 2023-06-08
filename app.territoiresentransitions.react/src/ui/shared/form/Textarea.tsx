4f395a1b1eed1a6f8d4c9a5ee370b2ecd26175e7
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-06-08 10:52:57 +0200

    Met à jour le redimensionnement des textarea

--- app.territoiresentransitions.react/src/ui/shared/form/Textarea.tsx
+++ app.territoiresentransitions.react/src/ui/shared/form/Textarea.tsx
@@ -15,6 +15,7 @@ export type TTextarea<T> = {
 } & TextareaHTMLAttributes<T>;
 
 /** Composant générique Textarea avec auto-resize */
+// Resize inspiré du post : https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/#comment-1794830
 const Textarea = forwardRef(
   <T extends HTMLTextAreaElement>(
     {
@@ -32,23 +33,25 @@ const Textarea = forwardRef(
       onInputChange && onInputChange(event.target.value);
     };
 
-    const textareaRef = useAutoSizeTextarea(
+    const {textareaRef, shadowRef} = useAutoSizeTextarea(
       value?.toString(),
-      minHeight,
       ref as RefObject<HTMLTextAreaElement>
     );
 
     return (
-      <textarea
-        ref={textareaRef}
-        className={classNames(
-          `w-full py-2 px-3 text-sm cursor-text rounded-lg outline outline-offset-0 !outline-1 outline-gray-300 focus:outline-blue-500 ${className}`
-        )}
-        onChange={textareaChange}
-        value={value}
-        placeholder={placeholder}
-        {...props}
-      />
+      <div className="flex items-start w-full">
+        <div ref={shadowRef} className="w-[1px] mr-[-1px]" />
+        <textarea
+          ref={textareaRef}
+          className={classNames(
+            `resize-none w-full py-2 px-3 text-sm cursor-text rounded-lg outline outline-offset-0 !outline-1 outline-gray-300 focus:outline-blue-500 ${className}`
+          )}
+          onChange={textareaChange}
+          value={value}
+          placeholder={placeholder}
+          {...props}
+        />
+      </div>
     );
   }
 );
