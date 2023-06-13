f735459a187f3e401d9fdb78f2a22182450474d4
Florian d'Erfurth <florian@derfurth.com>
2023-06-13 17:59:43 +0200

    Materialise la vue `crm_usages`.

--- data_layer/sqitch/deploy/automatisation/crm@v2.37.0.sql
+++ data_layer/sqitch/deploy/automatisation/crm@v2.37.0.sql
@@ -2,9 +2,22 @@
 
 BEGIN;
 
-drop view crm_usages;
+create view crm_labellisations
+as
+select l.id,
+       nom || ' (' || collectivite_id || ')' as collectivite_key,
+       l.referentiel,
+       l.obtenue_le,
+       l.annee,
+       l.etoiles,
+       l.score_realise,
+       l.score_programme
+from stats.collectivite
+         join labellisation l using (collectivite_id)
+where is_service_role()
+order by nom, obtenue_le desc;
 
-create materialized view crm_usages
+create view crm_usages
 as
 with premier_rattachements as (select collectivite_id,
                                       min(created_at
