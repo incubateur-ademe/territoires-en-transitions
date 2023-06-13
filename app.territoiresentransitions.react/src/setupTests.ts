a7465ebb2166170889a1f623f3d69e22e8b090c6
Arthur Molinos <arthur.molinos@gmail.com>
2023-06-13 09:25:26 +0200

    Répare les snapshots avec des ids différents suite au changement de version de floating-ui

--- app.territoiresentransitions.react/src/setupTests.ts
+++ app.territoiresentransitions.react/src/setupTests.ts
@@ -12,4 +12,15 @@ jest.mock('core-logic/hooks/useCurrentCollectivite.ts');
 // ce mock n'est importé qu'ici car la version d'origine fonctionne dans le navigateur
 jest.mock('utils/shasum256.ts');
 
+// corrige une génération aléatoire d'ids
+// https://github.com/floating-ui/floating-ui/issues/2319
+jest.mock('@floating-ui/react', () => {
+  const originalModule = jest.requireActual('@floating-ui/react');
+  return {
+    __esModule: true,
+    ...originalModule,
+    useId: jest.fn(() => 'mocked-id'),
+  };
+});
+
 jest.setTimeout(60000);
--- app.territoiresentransitions.react/src/ui/shared/floating-ui/Tooltip.stories.tsx
+++ app.territoiresentransitions.react/src/ui/shared/floating-ui/Tooltip.stories.tsx
@@ -4,7 +4,6 @@ import {Tooltip} from './Tooltip';
 
 export default {
   component: Tooltip,
-  parameters: {storyshots: false},
 } as Meta;
 
 export const Simple = () => (
