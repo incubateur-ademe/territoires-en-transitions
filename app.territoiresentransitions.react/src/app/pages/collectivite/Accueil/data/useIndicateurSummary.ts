637400b234824ed2edef3be63d2b4099c86cfbc4
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-06-14 16:06:28 +0200

    Réduction du nombre d'appels indicateur summary

--- app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/data/useIndicateurSummary.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/data/useIndicateurSummary.ts
@@ -8,14 +8,11 @@ import {useIndicateurPersonnaliseDefinitionList} from 'core-logic/hooks/indicate
  * Récupère les summary des indicateurs d'un groupe et d'une collectivité données
  */
 
-const fetchIndicateurSummary = async (
-  collectivite_id: number | null,
-  indicateur_group: string | null
-) => {
+const fetchIndicateurSummary = async (collectivite_id: number | null) => {
   const {error, data} = await supabaseClient
     .from('indicateur_summary')
     .select()
-    .match({collectivite_id, indicateur_group});
+    .match({collectivite_id});
 
   if (error) throw new Error(error.message);
 
@@ -26,13 +23,12 @@ const fetchIndicateurSummary = async (
  * Récupère les summary des indicateurs d'un groupe et d'une collectivité données
  */
 
-export const useIndicateurSummary = (indicateur_group: string) => {
+export const useIndicateurSummary = () => {
   const collectiviteId = useCollectiviteId();
 
   // Chargement des données
-  const {data} = useQuery(
-    ['indicateur_summary', collectiviteId, indicateur_group],
-    () => fetchIndicateurSummary(collectiviteId, indicateur_group)
+  const {data} = useQuery(['indicateur_summary', collectiviteId], () =>
+    fetchIndicateurSummary(collectiviteId)
   );
 
   return data;
@@ -52,9 +48,26 @@ export const useIndicateursCount = () => {
     collectiviteId!
   );
 
-  const caeIndicateursWithValue = useIndicateurSummary('cae');
-  const eciIndicateursWithValue = useIndicateurSummary('eci');
-  const crteIndicateursWithValue = useIndicateurSummary('crte');
+  const indicateursWithValue = useIndicateurSummary();
+  let caeIndicateursWithValue = [];
+  let eciIndicateursWithValue = [];
+  let crteIndicateursWithValue = [];
+
+  indicateursWithValue?.forEach(ind => {
+    switch (ind.indicateur_group) {
+      case 'cae':
+        caeIndicateursWithValue.push(ind);
+        break;
+      case 'eci':
+        eciIndicateursWithValue.push(ind);
+        break;
+      case 'crte':
+        crteIndicateursWithValue.push(ind);
+        break;
+      default:
+        break;
+    }
+  });
 
   return {
     cae: {
