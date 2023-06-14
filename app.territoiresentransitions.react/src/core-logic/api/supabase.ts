b0374540d96edc599bff3c9a7f4e2b30097cf670
Marc Rutkowski <marc@attractive-media.fr>
2023-06-14 17:13:02 +0200

    trace les erreurs supabase dans sentry

--- app.territoiresentransitions.react/src/core-logic/api/supabase.ts
+++ app.territoiresentransitions.react/src/core-logic/api/supabase.ts
@@ -1,4 +1,5 @@
 import {createClient} from '@supabase/supabase-js';
+import * as Sentry from '@sentry/react';
 import {ENV} from 'environmentVariables';
 import {Database} from 'types/database.types';
 
@@ -9,6 +10,28 @@ export const supabaseClient = createClient<Database>(
   ENV.supabase_url!,
   ENV.supabase_anon_key!,
   {
+    global: {
+      // intercepte les requêtes pour traiter les erreurs globalement
+      fetch: (...args) => {
+        return fetch(...args).then(res => {
+          // en cas d'erreur
+          if (res.status >= 400) {
+            res
+              // clone la réponse avant de la consommer
+              .clone()
+              .json()
+              // et log l'erreur dans sentry
+              .then(({code, message}) => {
+                Sentry.captureException(
+                  new Error(`Supabase error ${code}: ${message}`)
+                );
+              });
+          }
+          // renvoi la réponse originale
+          return res;
+        });
+      },
+    },
     db: {
       schema: 'public',
     },
