54805f3797a32610decc5b30506db46344c7b4b2
Florian d'Erfurth <florian@derfurth.com>
2023-06-15 16:48:21 +0200

    Change `reponse_display` pour utiliser les index.

--- data_layer/sqitch/deploy/evaluation/reponse.sql
+++ data_layer/sqitch/deploy/evaluation/reponse.sql
@@ -8,42 +8,56 @@
 BEGIN;
 
 drop view reponse_display;
+
 create view reponse_display
 as
-with qr as (select q.id                         as question_id,
-                   coalesce(rb.collectivite_id,
-                            rp.collectivite_id,
-                            rc.collectivite_id) as collectivite_id,
-                   case
-                       when q.type = 'binaire'
-                           then json_build_object('question_id', q.id,
-                                                  'collectivite_id', rb.collectivite_id,
-                                                  'type', q.type,
-                                                  'reponse', rb.reponse)
-                       when q.type = 'proportion'
-                           then json_build_object('question_id', q.id,
-                                                  'collectivite_id', rp.collectivite_id,
-                                                  'type', q.type,
-                                                  'reponse', rp.reponse)
-                       when q.type = 'choix'
-                           then json_build_object('question_id', q.id,
-                                                  'collectivite_id', rc.collectivite_id,
-                                                  'type', q.type,
-                                                  'reponse', rc.reponse)
-                       end
-                                                as reponse
-            from question q
-                     left join reponse_binaire rb on rb.question_id = q.id
-                     left join reponse_proportion rp on rp.question_id = q.id
-                     left join reponse_choix rc on rc.question_id = q.id)
-select collectivite_id,
-       question_id,
-       reponse                                as reponse,
+select r.collectivite_id,
+       q.id                                    as question_id,
+       json_build_object('question_id', q.id,
+                         'collectivite_id', r.collectivite_id,
+                         'type', q.type,
+                         'reponse', r.reponse) as reponse,
        (select texte
         from justification j
-        where j.collectivite_id = qr.collectivite_id
-          and j.question_id = qr.question_id) as justification
-from qr
+        where j.collectivite_id = r.collectivite_id
+          and j.question_id = r.question_id)   as justification
+from reponse_binaire r
+         join question q
+              on r.question_id = q.id
+where have_lecture_acces(collectivite_id)
+
+union all
+
+select r.collectivite_id,
+       q.id                                    as question_id,
+       json_build_object('question_id', q.id,
+                         'collectivite_id', r.collectivite_id,
+                         'type', q.type,
+                         'reponse', r.reponse) as reponse,
+       (select texte
+        from justification j
+        where j.collectivite_id = r.collectivite_id
+          and j.question_id = r.question_id)   as justification
+from reponse_proportion r
+         join question q
+              on r.question_id = q.id
+where have_lecture_acces(collectivite_id)
+
+union all
+
+select r.collectivite_id,
+       q.id                                    as question_id,
+       json_build_object('question_id', q.id,
+                         'collectivite_id', r.collectivite_id,
+                         'type', q.type,
+                         'reponse', r.reponse) as reponse,
+       (select texte
+        from justification j
+        where j.collectivite_id = r.collectivite_id
+          and j.question_id = r.question_id)   as justification
+from reponse_choix r
+         join question q
+              on r.question_id = q.id
 where have_lecture_acces(collectivite_id);
 
 COMMIT;
