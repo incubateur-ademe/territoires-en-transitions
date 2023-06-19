09787b15a326989615dc5e0ca5ce8cc583e750b3
Arthur Molinos <arthur.molinos@gmail.com>
2023-06-19 22:14:14 +0200

    Ajoute une taille au container des boutons "action précédente" et "action suivante"

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionNav.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionNav.tsx
@@ -10,7 +10,7 @@ export const ActionTopNav = ({
   prevActionLink: string | undefined;
   nextActionLink: string | undefined;
 }) => (
-  <div className="flex justify-between fr-text--sm !m-0 fr-mt-2w overflow-hidden">
+  <div className="min-h-[1.5rem] flex justify-between fr-text--sm !m-0 fr-mt-2w overflow-hidden">
     {prevActionLink ? (
       <Link
         to={prevActionLink}
