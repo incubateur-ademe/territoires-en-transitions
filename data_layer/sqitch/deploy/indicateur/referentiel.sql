d084c2cad607f3a7920e4c1de8562f2a857a450f
Florian d'Erfurth <florian@derfurth.com>
2023-06-19 17:39:09 +0200

    Mets à jour le modèle des définitions.

--- data_layer/sqitch/deploy/indicateur/referentiel.sql
+++ data_layer/sqitch/deploy/indicateur/referentiel.sql
@@ -3,16 +3,45 @@
 
 BEGIN;
 
-alter policy allow_read
-    on indicateur_resultat
-    using (can_read_acces_restreint(collectivite_id));
 
-alter policy allow_read
-    on indicateur_objectif
-    using (can_read_acces_restreint(collectivite_id));
+alter table indicateur_definition
+    drop column parent;
 
-alter policy allow_read
-    on indicateur_commentaire
-    using (can_read_acces_restreint(collectivite_id));
+alter table indicateur_definition
+    rename column indicateur_group to groupe;
+
+alter table indicateur_definition
+    add participation_score bool default false not null;
+
+comment on column indicateur_definition.participation_score is
+    'Vrai si il est prévu que l''indicateur participe au score du référentiel.';
+
+alter table indicateur_definition
+    add titre_long text default '' not null;
+
+comment on column indicateur_definition.titre_long is
+    'Le titre complet.';
+
+alter table indicateur_definition
+    add parent indicateur_id;
+
+comment on column indicateur_definition.parent is
+    'L''id de l''indicateur parent.';
+
+alter table indicateur_definition
+    add source text;
+
+comment on column indicateur_definition.source is
+    'La source de la donnée.';
+
+
+create type indicateur_referentiel_type as enum ('resultat', 'impact');
+
+alter table indicateur_definition
+    add type indicateur_referentiel_type;
+
+comment on column indicateur_definition.type is
+    'Le type d''indicateur, résultat ou impact.';
+
 
 COMMIT;
