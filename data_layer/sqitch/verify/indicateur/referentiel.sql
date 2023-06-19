94ecb052b56a447c68ba051538845a2b22c851a3
Florian d'Erfurth <florian@derfurth.com>
2023-06-19 17:39:09 +0200

    Mets à jour le modèle des définitions.

--- data_layer/sqitch/verify/indicateur/referentiel.sql
+++ data_layer/sqitch/verify/indicateur/referentiel.sql
@@ -2,6 +2,21 @@
 
 BEGIN;
 
--- Pas de changements autre que les droits
+select modified_at,
+       id,
+       groupe,
+       identifiant,
+       valeur_indicateur,
+       nom,
+       description,
+       unite,
+       obligation_eci,
+       participation_score,
+       titre_long,
+       parent,
+       source,
+       type
+from indicateur_definition
+where false;
 
 ROLLBACK;
