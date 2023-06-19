9008da0d9069cb411f3bb5adab8c2b645caae87e
Arthur Molinos <arthur.molinos@gmail.com>
2023-06-19 22:11:15 +0200

    Supprime la fake sidenav de la page action

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionPage.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/ActionPage.tsx
@@ -4,47 +4,10 @@ import {renderLoader} from 'utils/renderLoader';
 import {Referentiel} from 'types/litterals';
 import {useActionDownToTache} from 'core-logic/hooks/referentiel';
 import CollectivitePageLayout from '../CollectivitePageLayout/CollectivitePageLayout';
-import {SideNavLinks} from 'ui/shared/SideNav';
 
 const ActionReferentielAvancement = lazy(
   () => import('app/pages/collectivite/Referentiels/Action')
 );
-
-const fakeLinks: SideNavLinks = [
-  {displayName: 'Synthèse ', link: 'ehrthrth'},
-  {
-    displayName: 'Référentiel Climat Air Énergie',
-    link: 'tyjtyj',
-    enfants: [
-      {
-        displayName: 'Planification territoriale',
-        link: 'Planification territoriale',
-      },
-      {
-        displayName: 'Patrimoine de la collectivité',
-        link: 'Patrimoine de la collectivité',
-      },
-      {
-        displayName: 'Approvisionnement, énergie, eau, assainissement',
-        link: 'Approvisionnement, énergie, eau, assainissement',
-      },
-      {displayName: 'Mobilité', link: 'Mobilité'},
-      {displayName: 'Organisation interne', link: 'Organisation interne'},
-      {
-        displayName: 'Coopération, communication',
-        link: 'Coopération, communication',
-      },
-      {displayName: 'Lien', link: 'fgh'},
-      {displayName: 'Lien', link: 'kghkg'},
-      {displayName: 'Lien', link: 'sdfsdfsd'},
-      {displayName: 'Lien', link: 'fghfghgf'},
-      {displayName: 'Lien', link: 'dfgdfg'},
-      {displayName: 'Lien', link: 'hgfhfgh'},
-      {displayName: 'Lien', link: 'mnv'},
-    ],
-  },
-  {displayName: 'Lien', link: 'gfhngfn'},
-];
 
 export const ActionPage = () => {
   const {actionId} = useParams<{
@@ -59,7 +22,7 @@ export const ActionPage = () => {
 
   return (
     <Suspense fallback={renderLoader()}>
-      <CollectivitePageLayout sideNav={{links: fakeLinks}}>
+      <CollectivitePageLayout>
         {action && <ActionReferentielAvancement action={action} />}
       </CollectivitePageLayout>
     </Suspense>
