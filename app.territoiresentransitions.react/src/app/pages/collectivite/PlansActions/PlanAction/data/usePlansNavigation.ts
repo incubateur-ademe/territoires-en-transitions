bf98668ac2424c349490b44e9424d9d6bc613ffd
Arthur Molinos <arthur.molinos@gmail.com>
2023-06-19 14:12:37 +0200

    Ajout le nouveau layout CollectivitePageLayout aux plans d'action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/usePlansNavigation.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/usePlansNavigation.ts
@@ -4,6 +4,14 @@ import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {FlatAxe, PlanNode} from './types';
 import {buildPlans} from './utils';
+import {SideNavLinks} from 'ui/shared/SideNav';
+import {
+  makeCollectiviteFichesNonClasseesUrl,
+  makeCollectivitePlanActionAxeUrl,
+  makeCollectivitePlanActionUrl,
+  makeCollectivitePlansActionsSyntheseUrl,
+} from 'app/paths';
+import {generateTitle} from '../../FicheAction/data/utils';
 
 export const usePlansNavigation = () => {
   const collectivite_id = useCollectiviteId();
@@ -14,4 +22,68 @@ export const usePlansNavigation = () => {
     });
     return buildPlans(data as unknown as FlatAxe[]) as unknown as PlanNode[];
   });
+};
+
+/**
+ * Génère une liste de lien pour la SideNav à partir d'une liste de plans d'action
+ * @param plans
+ * @param fichesNonClasseesTotal
+ * @returns
+ */
+export const generatePlanActionNavigationLinks = (
+  collectiviteId: number,
+  plans?: PlanNode[],
+  fichesNonClasseesTotal?: number | null
+) => {
+  const plansLinks: SideNavLinks = [
+    {
+      link: makeCollectivitePlansActionsSyntheseUrl({
+        collectiviteId,
+      }),
+      displayName: 'Synthèse',
+    },
+  ];
+
+  if (plans) {
+    plansLinks.push(
+      ...plans.map(plan => {
+        if (plan.children && plan.children.length > 0) {
+          return {
+            link: makeCollectivitePlanActionUrl({
+              collectiviteId,
+              planActionUid: plan.id.toString(),
+            }),
+            displayName: generateTitle(plan.nom),
+            enfants: plan.children.map(e => ({
+              link: makeCollectivitePlanActionAxeUrl({
+                collectiviteId,
+                planActionUid: plan.id.toString(),
+                axeUid: e.id.toString(),
+              }),
+              displayName: generateTitle(e.nom),
+            })),
+          };
+        } else {
+          return {
+            link: makeCollectivitePlanActionUrl({
+              collectiviteId,
+              planActionUid: plan.id.toString(),
+            }),
+            displayName: generateTitle(plan.nom),
+          };
+        }
+      })
+    );
+  }
+
+  if (fichesNonClasseesTotal && fichesNonClasseesTotal > 0) {
+    plansLinks.push({
+      link: makeCollectiviteFichesNonClasseesUrl({
+        collectiviteId,
+      }),
+      displayName: 'Fiches non classées',
+    });
+  }
+
+  return plansLinks;
 };
--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlansActions.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlansActions.tsx
@@ -1,23 +1,62 @@
 import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
-import PlansActionsNavigation from './PlansActionsNavigation';
 import {PlansActionsRoutes} from './PlansActionsRoutes';
+import CollectivitePageLayout from '../CollectivitePageLayout/CollectivitePageLayout';
+import {
+  generatePlanActionNavigationLinks,
+  usePlansNavigation,
+} from './PlanAction/data/usePlansNavigation';
+import {useFichesNonClasseesListe} from './FicheAction/data/useFichesNonClasseesListe';
+import {useCreateFicheAction} from './FicheAction/data/useUpsertFicheAction';
+import {useCreatePlanAction} from './PlanAction/data/useUpsertAxe';
 
 const PlansActions = () => {
   const collectivite = useCurrentCollectivite();
 
+  const {data: planListe} = usePlansNavigation();
+  const fichesNonClasseesListe = useFichesNonClasseesListe(
+    collectivite?.collectivite_id!
+  );
+
+  const {mutate: createFicheAction} = useCreateFicheAction();
+
+  const {mutate: createPlanAction} = useCreatePlanAction();
+
+  if (!collectivite) return null;
+
   return (
-    <div data-test="PlansAction" className="fr-container !px-0">
-      <div className="flex items-start">
-        {collectivite && (
+    <CollectivitePageLayout
+      sideNav={{
+        links: generatePlanActionNavigationLinks(
+          collectivite.collectivite_id,
+          planListe,
+          fichesNonClasseesListe?.count
+        ),
+        actions: !collectivite.readonly && (
           <>
-            <PlansActionsNavigation collectivite={collectivite} />
-            <PlansActionsRoutes
-              collectivite_id={collectivite.collectivite_id}
-            />
+            <li className="fr-sidemenu_item p-0 list-none">
+              <button
+                data-test="CreerFicheAction"
+                className="fr-btn fr-btn--secondary"
+                onClick={() => createFicheAction()}
+              >
+                Créer une fiche action
+              </button>
+            </li>
+            <li className="fr-sidemenu_item mt-6 p-0 list-none">
+              <button
+                data-test="AjouterPlanAction"
+                className="fr-btn fr-btn--tertiary"
+                onClick={() => createPlanAction()}
+              >
+                Ajouter un plan d'action
+              </button>
+            </li>
           </>
-        )}
-      </div>
-    </div>
+        ),
+      }}
+    >
+      <PlansActionsRoutes collectivite_id={collectivite.collectivite_id} />
+    </CollectivitePageLayout>
   );
 };
 
