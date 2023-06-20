6b4bbd4b8d019890e2fca7b5128ebb294305c7e4
Florian d'Erfurth <florian@derfurth.com>
2023-06-20 12:17:46 +0200

    Ajoute les indicateurs au seed.

--- data_layer/seed/content/18-truncate.sql
+++ data_layer/seed/content/18-truncate.sql
@@ -1,3 +1,3 @@
 TRUNCATE epci, commune, collectivite, action_definition, action_computed_points, indicateur_action,
-    indicateur_commentaire,
+    indicateur_resultat_commentaire,
     indicateur_resultat, indicateur_objectif, indicateur_definition RESTART IDENTITY CASCADE;
