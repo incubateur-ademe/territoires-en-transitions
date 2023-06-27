a7ec7529edd668e92e4ee2bc4e28a1fe416d870a
Florian d'Erfurth <florian@derfurth.com>
2023-06-27 15:02:17 +0200

    Simplifie la construction de l'arbre.

--- business/business/utils/action_tree.py
+++ business/business/utils/action_tree.py
@@ -15,17 +15,24 @@ class ActionTree:
 
     def __init__(self, actions_children: list[ActionChildren]) -> None:
 
-        self.children_ids_by_action_id = {
-            action_children.action_id: action_children.children
-            for action_children in actions_children
-        }
+        self.depths: dict[ActionId, int] = {}
+        self.children: dict[ActionId, list[ActionId]] = {}
+        self.leafs: set[ActionId] = set()
 
-        self._depths_by_action_ids = self._build_depths()
-        self._backward_ids = self._build_backward_ids()
-        self._forward_ids = self._backward_ids[::-1]
+        for action in actions_children:
+            self.children[action.action_id] = action.children
+            self.depths[action.action_id] = action.action_id.count('_') + action.action_id.count('.')
+            if not action.children:
+                self.leafs.add(action.action_id)
+
+        self._forward_ids = sorted(
+            self.depths.keys(),
+            key=lambda action_id: self.depths[action_id],
+        )
+        self._backward_ids = self._forward_ids[::-1]
 
     def get_children(self, action_id: ActionId) -> List[ActionId]:
-        return self.children_ids_by_action_id.get(action_id, [])
+        return self.children.get(action_id, [])
 
     def get_siblings(self, action_id: ActionId) -> List[ActionId]:
         parent = self._get_parent(action_id)
@@ -39,7 +46,7 @@ class ActionTree:
             self, callback: Callable[[ActionId], None], action_depth: int
     ):
         for action_id in self._forward_ids:
-            this_depth = self._depths_by_action_ids[action_id]
+            this_depth = self.depths[action_id]
             if this_depth >= action_depth:
                 callback(action_id)
 
@@ -63,12 +70,6 @@ class ActionTree:
         if action_parent:
             self.map_from_action_to_root(callback, action_parent)
 
-    def infer_depth(self, action_id: ActionId) -> int:
-        parent_id = self._get_parent(action_id)
-        if not parent_id:
-            return 0
-        return 1 + self.infer_depth(parent_id)
-
     @staticmethod
     def _get_parent(action_id: ActionId) -> Optional[ActionId]:
         match = parent_pattern.match(action_id, 0)
@@ -83,24 +84,4 @@ class ActionTree:
         return self._backward_ids
 
     def is_leaf(self, action_id: ActionId):
-        return self.get_children(action_id) == []
-
-    def _build_depths(self) -> dict[ActionId, int]:
-        action_ids = list(
-            set(
-                flatten(list(self.children_ids_by_action_id.values()))
-                + list(self.children_ids_by_action_id.keys())
-            )
-        )
-        return {action_id: self.infer_depth(action_id) for action_id in action_ids}
-
-    def _build_backward_ids(self) -> List[ActionId]:
-        return sorted(
-            list(self._depths_by_action_ids.keys()),
-            key=lambda action_id: self._depths_by_action_ids[action_id],
-            reverse=True,
-        )
-
-
-def flatten(L: List[List]):
-    return [item for sublist in L for item in sublist]
+        return action_id in self.leafs
