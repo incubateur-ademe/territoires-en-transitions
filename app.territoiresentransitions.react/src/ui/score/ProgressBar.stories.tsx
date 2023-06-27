0a738656bd5c7c2385ec4a4a289bb15bda330de0
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-06-27 11:13:16 +0200

    Mise à jour des storyshots sur les progress bar

---
+++ app.territoiresentransitions.react/src/ui/score/ProgressBar.stories.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {Meta} from '@storybook/react';
+import ProgressBar from './ProgressBar';
+import {avancementToLabel} from '../../app/labels';
+import {actionAvancementColors} from '../../app/theme';
+
+export default {
+  component: ProgressBar,
+} as Meta;
+
+export const ToutFait = () => (
+  <ProgressBar
+    score={[
+      {
+        label: avancementToLabel.fait,
+        value: 100,
+        color: actionAvancementColors.fait,
+      },
+      {
+        label: avancementToLabel.programme,
+        value: 0,
+        color: actionAvancementColors.programme,
+      },
+      {
+        label: avancementToLabel.pas_fait,
+        value: 0,
+        color: actionAvancementColors.pas_fait,
+      },
+    ]}
+    total={100}
+    defaultScore={{
+      label: avancementToLabel.non_renseigne,
+      color: actionAvancementColors.non_renseigne,
+    }}
+  />
+);
+
+export const RienRenseigne = () => (
+  <ProgressBar
+    score={[
+      {
+        label: avancementToLabel.fait,
+        value: 0,
+        color: actionAvancementColors.fait,
+      },
+      {
+        label: avancementToLabel.programme,
+        value: 0,
+        color: actionAvancementColors.programme,
+      },
+      {
+        label: avancementToLabel.pas_fait,
+        value: 0,
+        color: actionAvancementColors.pas_fait,
+      },
+    ]}
+    total={100}
+    defaultScore={{
+      label: avancementToLabel.non_renseigne,
+      color: actionAvancementColors.non_renseigne,
+    }}
+  />
+);
+
+export const Mixte = () => (
+  <ProgressBar
+    score={[
+      {
+        label: avancementToLabel.fait,
+        value: 20,
+        color: actionAvancementColors.fait,
+      },
+      {
+        label: avancementToLabel.programme,
+        value: 28,
+        color: actionAvancementColors.programme,
+      },
+      {
+        label: avancementToLabel.pas_fait,
+        value: 20,
+        color: actionAvancementColors.pas_fait,
+      },
+    ]}
+    total={100}
+    defaultScore={{
+      label: avancementToLabel.non_renseigne,
+      color: actionAvancementColors.non_renseigne,
+    }}
+  />
+);
