71c0301888ee8d77f2540f495095e8d1cdeb3289
Florian d'Erfurth <florian@derfurth.com>
2023-07-06 09:25:24 +0200

    Ajoute l'agrandissement aux actions possible pour les graphiques.

--- data_layer/sqitch/deploy/utilisateur/usage.sql
+++ data_layer/sqitch/deploy/utilisateur/usage.sql
@@ -6,5 +6,6 @@ alter type usage_fonction add value if not exists 'cta_plan_creation';
 alter type usage_fonction add value if not exists 'cta_plan_maj';
 alter type usage_fonction add value if not exists 'cta_edl_commencer';
 alter type usage_fonction add value if not exists 'cta_edl_personnaliser';
+alter type usage_action add value if not exists 'agrandissement';
 
 COMMIT;
