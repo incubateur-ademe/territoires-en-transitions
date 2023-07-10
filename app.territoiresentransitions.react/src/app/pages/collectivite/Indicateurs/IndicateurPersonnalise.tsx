a9527283d847b24636f0c7a63721e295d1bcc6a9
Marc Rutkowski <marc@attractive-media.fr>
2023-07-10 19:34:08 +0200

    ajoute l'affichage du détail d'un indicateur personnalisé

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPersonnalise.tsx
@@ -0,0 +1,84 @@
+import {ChangeEvent} from 'react';
+import {BadgeACompleter} from 'ui/shared/Badge/BadgeACompleter';
+import TextareaControlled from 'ui/shared/form/TextareaControlled';
+import InputControlled from 'ui/shared/form/InputControlled';
+import ScrollTopButton from 'ui/buttons/ScrollTopButton';
+import FormField from 'ui/shared/form/FormField';
+import {TIndicateurPersoDefinition} from './types';
+import {useIndicateurPersoDefinition} from './useIndicateursPersoDefinitions';
+import {useIndicateurACompleter} from './useIndicateurDefinitions';
+import {useUpsertIndicateurPersoDefinition} from './useUpsertIndicateurPersoDefinition';
+import IndicateurChart from './IndicateurChart';
+import {HeaderIndicateur} from './Header';
+import {IndicateurValuesTabs} from './IndicateurValuesTabs';
+import {FichesActionLiees} from './FichesActionLiees';
+
+/** Affiche le détail d'un indicateur personnalisé */
+const IndicateurPersonnaliseBase = ({
+  definition,
+}: {
+  definition: TIndicateurPersoDefinition;
+}) => {
+  const {id, titre, description, unite} = definition;
+  const a_completer = useIndicateurACompleter(id);
+  const {mutate: saveDefinition} = useUpsertIndicateurPersoDefinition();
+
+  // l'objet à enregistrer ne peut pas contenir `isPerso` qui est un champ ajouté
+  // lors du chargement pour des besoins internes au front
+  const {isPerso, ...definitionToUpdate} = definition;
+
+  // génère les fonctions d'enregistrement des modifications
+  const handleUpdate =
+    (name: 'description' | 'unite') =>
+    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const nouveau = e.target.value?.trim();
+      if (nouveau !== definition[name]) {
+        saveDefinition({...definitionToUpdate, [name]: nouveau});
+      }
+    };
+
+  return (
+    <>
+      <HeaderIndicateur title={titre} />
+      <div className="px-10 py-4">
+        <IndicateurChart variant="zoomed" definition={definition} />
+        <BadgeACompleter
+          a_completer={a_completer}
+          className="fr-mt-5w fr-mb-3w"
+        />
+        <IndicateurValuesTabs definition={definition} />
+        <FormField
+          className="fr-mt-5w"
+          label="Description et méthodologie de calcul"
+        >
+          <TextareaControlled
+            className="fr-input fr-mt-1w !outline-none"
+            initialValue={description}
+            onBlur={handleUpdate('description')}
+          />
+        </FormField>
+        <FichesActionLiees definition={definition} />
+        <FormField label="Unité" className="fr-mt-3w fr-label">
+          <InputControlled
+            className="fr-input fr-mt-1w !outline-none"
+            initialValue={unite}
+            onBlur={handleUpdate('unite')}
+          />
+        </FormField>
+        <ScrollTopButton className="fr-mt-4w" />
+      </div>
+    </>
+  );
+};
+
+/** Charge les données et affiche le détail d'un indicateur personnalisé */
+export const IndicateurPersonnalise = ({
+  indicateurId,
+}: {
+  indicateurId: string;
+}) => {
+  const definition = useIndicateurPersoDefinition(parseInt(indicateurId));
+  if (!definition) return null;
+
+  return <IndicateurPersonnaliseBase definition={definition} />;
+};
