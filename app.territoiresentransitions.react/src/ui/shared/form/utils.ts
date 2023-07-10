6428b854c3d87a39d907b625efc123143840a71a
Marc Rutkowski <marc@attractive-media.fr>
2023-07-10 18:59:49 +0200

    supprime correctement les écouteurs d'événements dans le filtrage des saisies de nombres

--- app.territoiresentransitions.react/src/ui/shared/form/utils.ts
+++ app.territoiresentransitions.react/src/ui/shared/form/utils.ts
@@ -1,72 +1,76 @@
+import {useEffect, useRef} from 'react';
+
 export const onlyNumericWithFloatRegExp = new RegExp(/^-?\d*[.,]?\d*$/);
 export const onlyNumericRegExp = new RegExp(/^\d?\d*$/);
+
+const EVENTS = [
+  'input',
+  'keydown',
+  'keyup',
+  'mousedown',
+  'mouseup',
+  'select',
+  'contextmenu',
+  'drop',
+  'focusout',
+];
 
 /**
  * Permet de n'autoriser que certains caractères dans un input ou textarea.
  *
  * Basé sur cette solution:
  * https://stackoverflow.com/a/469362/16408731
- * @param ref La référence de l'input
  * @param inputFilter Regex que l'on souhaite appliquer au filtre
  * @param errMsg Si le message d'erreur est défini, affiche le message quand un caractère non autorisé est renseigné
+ * @returns ref La référence à associer l'input
  */
-export const setInputFilter = (
-  ref: React.RefObject<HTMLInputElement | HTMLTextAreaElement>,
-  inputFilter: (value: string) => boolean,
+export const useInputFilterRef = <
+  T extends HTMLInputElement | HTMLTextAreaElement
+>(
+  inputFilter: RegExp,
   errMsg?: string
-): void => {
-  [
-    'input',
-    'keydown',
-    'keyup',
-    'mousedown',
-    'mouseup',
-    'select',
-    'contextmenu',
-    'drop',
-    'focusout',
-  ].forEach(event => {
-    ref &&
-      ref.current &&
-      ref.current.addEventListener(
-        event,
-        function (
-          this: (HTMLInputElement | HTMLTextAreaElement) & {
-            oldValue: string;
-            oldSelectionStart: number | null;
-            oldSelectionEnd: number | null;
-          },
-          e: Event
-        ) {
-          if (inputFilter(this.value)) {
-            if (
-              errMsg &&
-              ['keydown', 'mousedown', 'focusout'].indexOf(e.type) >= 0
-            ) {
-              this.setCustomValidity('');
-            }
-            this.oldValue = this.value;
-            this.oldSelectionStart = this.selectionStart;
-            this.oldSelectionEnd = this.selectionEnd;
-          } else if (Object.prototype.hasOwnProperty.call(this, 'oldValue')) {
-            if (errMsg) {
-              this.setCustomValidity(errMsg);
-              this.reportValidity();
-            }
-            this.value = this.oldValue;
-            if (
-              this.oldSelectionStart !== null &&
-              this.oldSelectionEnd !== null
-            ) {
-              this.setSelectionRange(
-                this.oldSelectionStart,
-                this.oldSelectionEnd
-              );
-            }
-          } else {
-            this.value = '';
-          }
-        }
-      );
-  });
+) => {
+  const ref = useRef<T>(null);
+
+  function handleEvent(
+    this: (HTMLInputElement | HTMLTextAreaElement) & {
+      oldValue: string;
+      oldSelectionStart: number | null;
+      oldSelectionEnd: number | null;
+    },
+    e: Event
+  ) {
+    if (inputFilter.test(this.value)) {
+      if (errMsg && ['keydown', 'mousedown', 'focusout'].includes(e.type)) {
+        this.setCustomValidity('');
+      }
+      this.oldValue = this.value;
+      this.oldSelectionStart = this.selectionStart;
+      this.oldSelectionEnd = this.selectionEnd;
+    } else if (Object.prototype.hasOwnProperty.call(this, 'oldValue')) {
+      if (errMsg) {
+        this.setCustomValidity(errMsg);
+        this.reportValidity();
+      }
+      this.value = this.oldValue;
+      if (this.oldSelectionStart !== null && this.oldSelectionEnd !== null) {
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      }
+    } else {
+      this.value = '';
+    }
+  }
+
+  useEffect(() => {
+    EVENTS.forEach(event => {
+      ref?.current?.addEventListener(event, handleEvent);
+    });
+    return () => {
+      EVENTS.forEach(event => {
+        ref?.current?.removeEventListener(event, handleEvent);
+      });
+    };
+  }, [ref.current]);
+
+  return ref;
 };
