64ba871a29d63f8f2fc6d1fe376fa3bfba11d809
Marc Rutkowski <marc@attractive-media.fr>
2023-07-10 19:43:23 +0200

    extrait le composant d'affichage d'une action liée pour ré-utilisation

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLiees.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLiees.tsx
@@ -18,12 +18,6 @@ type Props = {
 
 const ActionsLiees = ({actions, onSelect, isReadonly}: Props) => {
   const {data: actionListe} = useActionListe();
-  const collectiviteId = useCollectiviteId()!;
-
-  const actionsLiees =
-    actionListe?.filter((action: TActionStatutsRow) =>
-      actions?.some(v => v.id === action.action_id)
-    ) ?? [];
 
   const formatOptions = (actions?: TActionStatutsRow[] | null): TOption[] => {
     const options = actions
@@ -63,30 +57,46 @@ const ActionsLiees = ({actions, onSelect, isReadonly}: Props) => {
           disabled={isReadonly}
         />
       </FormField>
-      {actionsLiees.length > 0 && (
-        <div className="grid grid-cols-2 gap-6 mb-8">
-          {actionsLiees.map(action => (
-            <ActionCard
-              key={action.action_id}
-              openInNewTab
-              link={makeCollectiviteTacheUrl({
-                collectiviteId: collectiviteId,
-                actionId: action.action_id,
-                referentielId: action.referentiel,
-              })}
-              statutBadge={
-                action.avancement && (
-                  <ActionStatutBadge statut={action.avancement} small />
-                )
-              }
-              details={`Référentiel ${referentielToName[action.referentiel]}`}
-              title={`${action.identifiant} ${action.nom}`}
-            />
-          ))}
-        </div>
-      )}
+      <ActionsLieesCards actions={actions} />
     </>
   );
+};
+
+export const ActionsLieesCards = ({
+  actions,
+}: {
+  actions: {id: string}[] | null;
+}) => {
+  const {data: actionListe} = useActionListe();
+  const collectiviteId = useCollectiviteId()!;
+
+  const actionsLiees =
+    actionListe?.filter((action: TActionStatutsRow) =>
+      actions?.some(v => v.id === action.action_id)
+    ) ?? [];
+
+  return actionsLiees.length > 0 ? (
+    <div className="grid grid-cols-2 gap-6 mb-8">
+      {actionsLiees.map(action => (
+        <ActionCard
+          key={action.action_id}
+          openInNewTab
+          link={makeCollectiviteTacheUrl({
+            collectiviteId: collectiviteId,
+            actionId: action.action_id,
+            referentielId: action.referentiel,
+          })}
+          statutBadge={
+            action.avancement && (
+              <ActionStatutBadge statut={action.avancement} small />
+            )
+          }
+          details={`Référentiel ${referentielToName[action.referentiel]}`}
+          title={`${action.identifiant} ${action.nom}`}
+        />
+      ))}
+    </div>
+  ) : null;
 };
 
 export default ActionsLiees;
