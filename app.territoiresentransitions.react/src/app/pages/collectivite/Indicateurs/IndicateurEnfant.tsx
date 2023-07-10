40242027b3edca1775e63f8a7d81646c7140f654
Marc Rutkowski <marc@attractive-media.fr>
2023-07-10 19:33:45 +0200

    ajoute l'affichage du détail d'un indicateur prédéfini (et de ses éventuels indicateurs "enfants")

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurEnfant.tsx
@@ -0,0 +1,111 @@
+import {useIndicateurACompleter} from './useIndicateurDefinitions';
+import {TIndicateurReferentielDefinition} from './types';
+import {IndicateurValuesTabs} from './IndicateurValuesTabs';
+import {Badge} from 'ui/shared/Badge';
+import classNames from 'classnames';
+import {useToggle} from 'ui/shared/useToggle';
+import {ExpandToggle} from 'ui/icons/ExpandToggle';
+import {BadgeACompleter} from '../../../../ui/shared/Badge/BadgeACompleter';
+import {ActionsLieesCards} from '../PlansActions/FicheAction/FicheActionForm/ActionsLiees';
+import IndicateurChart from './IndicateurChart';
+import {FichesActionLiees} from './FichesActionLiees';
+
+export const IndicateurEnfant = ({
+  definition,
+  actionsLieesCommunes,
+}: {
+  definition: TIndicateurReferentielDefinition;
+  actionsLieesCommunes: string[];
+}) => {
+  const [open, toggle] = useToggle(false);
+
+  return (
+    <div className="border border-[#e5e5e5] rounded-lg my-4">
+      <IndicateurEnfantHeader
+        definition={definition}
+        open={open}
+        toggle={toggle}
+      />
+      {open && (
+        <IndicateurEnfantContent
+          definition={definition}
+          actionsLieesCommunes={actionsLieesCommunes}
+        />
+      )}
+    </div>
+  );
+};
+/** Affiche l'en-tête du détail d'un indicateur enfant */
+const IndicateurEnfantHeader = ({
+  definition,
+  open,
+  toggle,
+}: {
+  definition: TIndicateurReferentielDefinition;
+  open: boolean;
+  toggle: () => void;
+}) => {
+  const a_completer = useIndicateurACompleter(definition.id);
+
+  return (
+    <div
+      className={classNames(
+        'flex justify-between items-center px-6 py-4 rounded-lg cursor-pointer',
+        {
+          'bg-[#f5f5fE]': open,
+          'hover:bg-grey975': !open,
+        }
+      )}
+      onClick={toggle}
+    >
+      <div>
+        <ExpandToggle open={open} />
+        <span className="font-bold">{definition.nom}</span>
+      </div>
+      <div>
+        {definition.participation_score && (
+          <Badge status="no-icon">Participe au score</Badge>
+        )}
+        <BadgeACompleter className="min-w-max" a_completer={a_completer} />
+      </div>
+    </div>
+  );
+};
+/** Affiche le contenu du détail d'un indicateur enfant */
+const IndicateurEnfantContent = ({
+  definition,
+  actionsLieesCommunes,
+}: {
+  definition: TIndicateurReferentielDefinition;
+  actionsLieesCommunes: string[];
+}) => {
+  //
+  const actionsLiees = definition.actions
+    .filter(action_id => !actionsLieesCommunes.includes(action_id))
+    .map(id => ({id}));
+
+  return (
+    <div className="p-6">
+      <IndicateurChart
+        className="fr-mb-3w"
+        variant="zoomed"
+        definition={definition}
+      />
+      <IndicateurValuesTabs definition={definition} />
+      {
+        /** actions liées */
+        actionsLiees.length ? (
+          <>
+            <p className="fr-mt-4w fr-mb-1w font-medium">
+              {actionsLiees.length > 1
+                ? 'Actions référentiel liées'
+                : 'Action référentiel liée'}
+            </p>
+            <ActionsLieesCards actions={actionsLiees} />
+          </>
+        ) : null
+      }
+      <FichesActionLiees definition={definition} />
+    </div>
+  );
+};
---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPredefini.tsx
@@ -0,0 +1,126 @@
+import {BadgeACompleter} from 'ui/shared/Badge/BadgeACompleter';
+import ScrollTopButton from 'ui/buttons/ScrollTopButton';
+import {Badge} from 'ui/shared/Badge';
+import {referentielToName} from 'app/labels';
+import {useIndicateur, useIndicateursEnfants} from './useIndicateurDefinitions';
+import {ActionsLieesCards} from '../PlansActions/FicheAction/FicheActionForm/ActionsLiees';
+import {IndicateurSidePanel} from './IndicateurSidePanel';
+import IndicateurChart from './IndicateurChart';
+import {HeaderIndicateur} from './Header';
+import {IndicateurValuesTabs} from './IndicateurValuesTabs';
+import {IndicateurEnfant} from './IndicateurEnfant';
+import {TIndicateurReferentielDefinition} from './types';
+import {FichesActionLiees} from './FichesActionLiees';
+
+/** Charge et affiche le détail d'un indicateur prédéfini et de ses éventuels "enfants" */
+export const IndicateurPredefini = ({indicateurId}: {indicateurId: string}) => {
+  const definition = useIndicateur(indicateurId);
+  const enfants = useIndicateursEnfants(indicateurId);
+  if (!definition) return null;
+
+  return (
+    <>
+      <HeaderIndicateur title={definition.nom} />
+      <div className="px-10 py-4">
+        <div className="flex flex-row justify-end fr-mb-2w">
+          <IndicateurSidePanel definition={definition} />
+        </div>
+        {/** affiche les indicateurs "enfants" */}
+        {enfants.length ? (
+          <IndicateurCompose definition={definition} enfants={enfants} />
+        ) : (
+          /** ou juste le détail si il n'y a pas d'enfants */
+          <IndicateurDetail definition={definition} />
+        )}
+        <ScrollTopButton className="fr-mt-4w" />
+      </div>
+    </>
+  );
+};
+
+/**
+ * Affiche le détail d'un indicateur sans enfant
+ */
+const IndicateurDetail = ({
+  definition,
+}: {
+  definition: TIndicateurReferentielDefinition & {a_completer: boolean};
+}) => {
+  const {a_completer, actions} = definition;
+
+  // converti la liste d'id en liste d'objets pour être compatible avec `ActionsLieesCards`
+  const actionsLiees = actions?.map(id => ({id}));
+
+  return (
+    <>
+      <IndicateurChart variant="zoomed" definition={definition} />
+      <div className="flex items-center fr-mt-5w fr-mb-3w gap-4">
+        <BadgeACompleter a_completer={a_completer} />
+        {definition.participation_score && (
+          <Badge className="!normal-case" status="no-icon">
+            Participe au score {referentielToName.cae}
+          </Badge>
+        )}
+      </div>
+      <IndicateurValuesTabs definition={definition} />
+      {/** TODO: personne et direction pilote */}
+      {
+        /** actions liées */
+        actionsLiees.length ? (
+          <>
+            <p className="fr-mt-5w fr-mb-1w font-medium">
+              {actionsLiees.length > 1 ? 'Actions liées' : 'Action liée'}
+            </p>
+            <ActionsLieesCards actions={actionsLiees} />
+          </>
+        ) : null
+      }
+      <FichesActionLiees definition={definition} />
+    </>
+  );
+};
+
+/**
+ * Affiche le détail d'un indicateur composé
+ */
+const IndicateurCompose = ({
+  definition,
+  enfants,
+}: {
+  definition: TIndicateurReferentielDefinition;
+  enfants: TIndicateurReferentielDefinition[];
+}) => {
+  const actionsLieesCommunes = findCommonLinkedActions([
+    definition,
+    ...enfants,
+  ]);
+
+  return (
+    <>
+      {/** TODO : ajouter ici le graphe combinant les indicateurs "enfant" */}
+      {enfants.map(enfant => (
+        <IndicateurEnfant
+          key={enfant.id}
+          definition={enfant}
+          actionsLieesCommunes={actionsLieesCommunes}
+        />
+      ))}
+    </>
+  );
+};
+
+// détermine les actions liées communes à un ensemble de définitions
+const findCommonLinkedActions = (
+  definitions: TIndicateurReferentielDefinition[]
+) => {
+  // extrait les tableaux d'ids
+  const actionsIds = definitions.map(({actions}) => actions);
+
+  return (
+    actionsIds
+      // enlève les tableaux vides
+      .filter(a => a.length)
+      // détermine le sous-ensemble des ids communs
+      .reduce((a, b) => a.filter(c => b.includes(c)))
+  );
+};
