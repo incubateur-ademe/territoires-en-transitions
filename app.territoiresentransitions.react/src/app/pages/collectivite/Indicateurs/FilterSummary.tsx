1ff84eb438d6ecf1035fc94bd689501fc563d997
Marc Rutkowski <marc@attractive-media.fr>
2023-07-10 19:25:03 +0200

    ajoute le composant d'affichage du nombre de résultats du filtrage et un bouton pour réinitialiser les filtres

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/FilterSummary.tsx
@@ -0,0 +1,27 @@
+import {ITEM_ALL} from 'ui/shared/filters/commons';
+import {DisableAllFilters} from '../ReferentielTable/DisableAllFilters';
+
+/**
+ * Affiche le nombre de résultats du filtrage et un bouton pour réinitialiser les filtres
+ */
+export const FilterSummary = ({
+  count,
+  resetSelection,
+  selection,
+}: {
+  count: number;
+  resetSelection: () => void;
+  selection: string[];
+}) =>
+  count && !selection.includes(ITEM_ALL) ? (
+    <p>
+      <span className="fr-mr-2w">
+        {count} {count > 1 ? 'indicateurs trouvés' : 'indicateur trouvé'}
+      </span>
+      <DisableAllFilters
+        filtersCount={selection?.length}
+        onClick={resetSelection}
+        label="Réinitialiser les filtres"
+      />
+    </p>
+  ) : null;
