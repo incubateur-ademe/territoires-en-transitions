dd8ab553ffb9456315e9a8117863b0863b89cc25
Arthur Molinos <arthur.molinos@gmail.com>
2023-07-11 16:05:19 +0200

    adapte les pages profil et indicateurs avec les nouveaux styles de la sidenav

--- app.territoiresentransitions.react/src/app/pages/Profil/Profil.tsx
+++ app.territoiresentransitions.react/src/app/pages/Profil/Profil.tsx
@@ -15,9 +15,11 @@ const Profil = () => {
   ];
   return (
     <div className="fr-container !px-0">
-      <div className="flex items-start">
-        <SideNav links={links} />
-        <div className="max-w-3xl w-full mt-14 mx-auto px-6">
+      <div className="grid grid-cols-[21rem_1fr] ml-6">
+        <div className="mt-6 border-r border-gray-100">
+          <SideNav links={links} />
+        </div>
+        <div className="max-w-3xl w-full my-14 mx-auto px-6">
           <ProfilRoutes />
         </div>
       </div>
