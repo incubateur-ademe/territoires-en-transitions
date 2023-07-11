147240696a5b369caf4d6d6b6405c6f8c4560bb4
Florian d'Erfurth <florian@derfurth.com>
2023-07-11 14:12:41 +0200

    Ajoute le tracking des actions du tableau de bord.

--- data_layer/sqitch/deploy/utilisateur/usage.sql
+++ data_layer/sqitch/deploy/utilisateur/usage.sql
@@ -2,8 +2,9 @@
 
 BEGIN;
 
-alter type usage_fonction add value if not exists 'cta_plan';
-alter type usage_fonction add value if not exists 'cta_indicateur';
-alter type usage_fonction add value if not exists 'cta_labellisation';
+alter type usage_fonction add value if not exists 'cta_plan_creation';
+alter type usage_fonction add value if not exists 'cta_plan_maj';
+alter type usage_fonction add value if not exists 'cta_edl_commencer';
+alter type usage_fonction add value if not exists 'cta_edl_personnaliser';
 
 COMMIT;
