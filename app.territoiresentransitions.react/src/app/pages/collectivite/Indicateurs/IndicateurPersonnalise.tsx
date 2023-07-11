be1dcd52d00c1239f1a0698757526547a64f106b
Marc Rutkowski <marc@attractive-media.fr>
2023-07-11 17:20:26 +0200

    rend le titre des indicateurs personnalisés éditable

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPersonnalise.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/IndicateurPersonnalise.tsx
@@ -1,4 +1,3 @@
-import {ChangeEvent} from 'react';
 import {BadgeACompleter} from 'ui/shared/Badge/BadgeACompleter';
 import TextareaControlled from 'ui/shared/form/TextareaControlled';
 import InputControlled from 'ui/shared/form/InputControlled';
@@ -12,6 +11,7 @@ import IndicateurChart from './IndicateurChart';
 import {HeaderIndicateur} from './Header';
 import {IndicateurValuesTabs} from './IndicateurValuesTabs';
 import {FichesActionLiees} from './FichesActionLiees';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
 
 /** Affiche le détail d'un indicateur personnalisé */
 const IndicateurPersonnaliseBase = ({
@@ -22,24 +22,31 @@ const IndicateurPersonnaliseBase = ({
   const {id, titre, description, unite} = definition;
   const a_completer = useIndicateurACompleter(id);
   const {mutate: saveDefinition} = useUpsertIndicateurPersoDefinition();
+  const collectivite = useCurrentCollectivite();
+  const isReadonly = !collectivite || collectivite?.readonly;
 
   // l'objet à enregistrer ne peut pas contenir `isPerso` qui est un champ ajouté
   // lors du chargement pour des besoins internes au front
   const {isPerso, ...definitionToUpdate} = definition;
 
   // génère les fonctions d'enregistrement des modifications
-  const handleUpdate =
-    (name: 'description' | 'unite') =>
-    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const nouveau = e.target.value?.trim();
-      if (nouveau !== definition[name]) {
-        saveDefinition({...definitionToUpdate, [name]: nouveau});
-      }
-    };
+  const handleUpdate = (
+    name: 'description' | 'unite' | 'titre',
+    value: string
+  ) => {
+    const nouveau = value?.trim();
+    if (nouveau !== definition[name]) {
+      saveDefinition({...definitionToUpdate, [name]: nouveau});
+    }
+  };
 
   return (
     <>
-      <HeaderIndicateur title={titre} />
+      <HeaderIndicateur
+        title={titre}
+        isReadonly={isReadonly}
+        onUpdate={value => handleUpdate('titre', value)}
+      />
       <div className="px-10 py-4">
         <IndicateurChart variant="zoomed" definition={definition} />
         <BadgeACompleter
@@ -54,7 +61,7 @@ const IndicateurPersonnaliseBase = ({
           <TextareaControlled
             className="fr-input fr-mt-1w !outline-none"
             initialValue={description}
-            onBlur={handleUpdate('description')}
+            onBlur={e => handleUpdate('description', e.target.value)}
           />
         </FormField>
         <FichesActionLiees definition={definition} />
@@ -62,7 +69,7 @@ const IndicateurPersonnaliseBase = ({
           <InputControlled
             className="fr-input fr-mt-1w !outline-none"
             initialValue={unite}
-            onBlur={handleUpdate('unite')}
+            onBlur={e => handleUpdate('unite', e.target.value)}
           />
         </FormField>
         <ScrollTopButton className="fr-mt-4w" />
