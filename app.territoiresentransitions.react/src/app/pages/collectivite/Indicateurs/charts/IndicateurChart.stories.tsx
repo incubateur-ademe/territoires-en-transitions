487efaf2c6b6960fed45875d0a9ac44c41206ba0
Marc Rutkowski <marc@attractive-media.fr>
2023-07-12 17:15:41 +0200

    répare les stories de IndicateurChart

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/IndicateurChart.stories.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/IndicateurChart.stories.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Story, Meta} from '@storybook/react';
-import {
-  IndicateurChartBase,
-  NoData,
-  TIndicateurChartBaseProps,
-} from './IndicateurChart';
+import {IndicateurChartBase} from './IndicateurChart';
+import {TIndicateurChartBaseProps} from './types';
+import {NoData} from './CardNoData';
 
 export default {
   component: IndicateurChartBase,
+  parameters: {storyshots: false}, // @nivo/line semble fait échoué storyshot :(
 } as Meta;
 
 const Template: Story<TIndicateurChartBaseProps> = args => (
@@ -22,17 +21,16 @@ IndicateurPerso.args = {
     isPerso: true,
     titre: 'Mon indicateur',
   },
-  objectifs: [
-    {annee: 2020, valeur: 1_800_000},
-    {annee: 2021, valeur: 1_700_000},
-    {annee: 2022, valeur: 1_600_000},
-    {annee: 2023, valeur: 1_500_000},
-  ],
-  resultats: [
-    {annee: 2020, valeur: 1_800_000},
-    {annee: 2021, valeur: 1_750_000},
-    {annee: 2022, valeur: 1_740_000},
-    {annee: 2023, valeur: 1_740_000},
+  valeurs: [
+    {annee: 2020, valeur: 1_800_000, type: 'objectif'},
+    {annee: 2021, valeur: 1_700_000, type: 'objectif'},
+    {annee: 2022, valeur: 1_600_000, type: 'objectif'},
+    {annee: 2023, valeur: 1_500_000, type: 'objectif'},
+    {annee: 2020, valeur: 1_800_000, type: 'resultat'},
+    {annee: 2020, valeur: 1_800_000, type: 'resultat'},
+    {annee: 2021, valeur: 1_750_000, type: 'resultat'},
+    {annee: 2022, valeur: 1_740_000, type: 'resultat'},
+    {annee: 2023, valeur: 1_740_000, type: 'resultat'},
   ],
 };
 
@@ -43,21 +41,21 @@ IndicateurRefentiel.args = {
     unite: '%',
     nom: 'Part du budget consacrée à la politique Economie Circulaire dans le budget global (%)',
   },
-  objectifs: [
-    {annee: 2016, valeur: 3},
-    {annee: 2018, valeur: 5},
-    {annee: 2019, valeur: 7},
-    {annee: 2020, valeur: 8},
-    {annee: 2021, valeur: 10},
-    {annee: 2022, valeur: 15},
-    {annee: 2023, valeur: 43},
-  ],
-  resultats: [
-    {annee: 2016, valeur: 1},
-    {annee: 2018, valeur: 3},
-    {annee: 2019, valeur: 5},
-    {annee: 2020, valeur: 1},
-    {annee: 2021, valeur: 0},
+  valeurs: [
+    {annee: 2016, valeur: 3, type: 'objectif'},
+    {annee: 2018, valeur: 5, type: 'objectif'},
+    {annee: 2019, valeur: 7, type: 'objectif'},
+    {annee: 2020, valeur: 8, type: 'objectif'},
+    {annee: 2021, valeur: 10, type: 'objectif'},
+    {annee: 2022, valeur: 15, type: 'objectif'},
+    {annee: 2023, valeur: 43, type: 'objectif'},
+    {annee: 2016, valeur: 1, type: 'resultat'},
+    {annee: 2016, valeur: 3, type: 'resultat'},
+    {annee: 2016, valeur: 4, type: 'import'},
+    {annee: 2018, valeur: 3, type: 'resultat'},
+    {annee: 2019, valeur: 5, type: 'resultat'},
+    {annee: 2020, valeur: 1, type: 'resultat'},
+    {annee: 2021, valeur: 0, type: 'resultat'},
   ],
 };
 
