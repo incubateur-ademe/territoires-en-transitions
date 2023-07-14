f82b4a19c15e12626092a84a1f1b05d4741f87a1
Florian d'Erfurth <florian@derfurth.com>
2023-07-14 15:51:26 +0200

    Importe les icônes

--- site/app/AppHeader.tsx
+++ site/app/AppHeader.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import {MenuPrincipal} from './MenuPrincipal';
+import '@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.css';
 
 function Brand() {
   return (
