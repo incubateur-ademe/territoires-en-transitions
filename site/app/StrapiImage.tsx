54142b177dd80ea9a0d15b090b2d7e6878ff5530
Florian d'Erfurth <florian@derfurth.com>
2023-08-02 18:40:13 +0200

    Composant pour afficher une image Strapi.

---
+++ site/app/StrapiImage.tsx
@@ -0,0 +1,15 @@
+const baseURL = process.env.NEXT_PUBLIC_STRAPI_URL;
+
+type Size = 'large' | 'medium' | 'small' | 'thumbnail';
+
+export function StrapiImage(props: {data: JSON; size: Size}) {
+  const attributes = props.data['attributes'];
+
+  return (
+    <img
+      className="fr-responsive-img"
+      src={`${baseURL}${attributes['formats'][props.size]['url']}`}
+      alt={attributes['alternativeText']}
+    />
+  );
+}
