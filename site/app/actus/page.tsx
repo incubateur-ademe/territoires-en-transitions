3c798ad389fe1d37e39068fcb16f766d7dd47747
Florian d'Erfurth <florian@derfurth.com>
2023-08-03 10:27:44 +0200

    Lien de la page liste vers le détail.

--- site/app/actus/page.tsx
+++ site/app/actus/page.tsx
@@ -8,29 +8,33 @@ export default async function Actus() {
     <div className="fr-container">
       <div className="fr-mt-1w fr-mt-md-4w fr-mb-5w">
         <h1 className="fr-header__body">Actualités</h1>
-        {data.map(actu => Actu(actu))}
+        {data.map(actu => (
+          <Actu key={actu['id']} actu={actu} />
+        ))}
       </div>
     </div>
   );
 }
 
-function Actu(actu: JSON) {
+async function Actu(props: {actu: JSON}) {
+  const actu = props.actu;
   return (
     <div key={actu['id']} className="fr-grid-row fr-mb-3w">
-      <div class="fr-col fr-col-md-6">
-        <div class="fr-card fr-enlarge-link">
-          <div class="fr-card__body">
-            <div class="fr-card__content">
-              <h3 class="fr-card__title">
-                <a href="#">{actu['attributes']['Titre']}</a>
+      <div className="fr-col fr-col-md-6">
+        <div className="fr-card fr-enlarge-link">
+          <div className="fr-card__body">
+            <div className="fr-card__content">
+              <h3 className="fr-card__title">
+                <a href={`/actus/${actu['id']}`}>
+                  {actu['attributes']['Titre']}
+                </a>
               </h3>
-
-              <p class="fr-card__desc">{actu['attributes']['Corps']}</p>
+              <p className="fr-card__desc">{actu['attributes']['Corps']}</p>
             </div>
           </div>
-          <div class="fr-card__header">
+          <div className="fr-card__header">
             {actu['attributes']['Couverture'] ? (
-              <div class="fr-card__img">
+              <div className="fr-card__img">
                 <StrapiImage
                   data={actu['attributes']['Couverture']['data']}
                   size="small"
