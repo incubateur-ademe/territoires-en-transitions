965ec7fea4ce4bb512989d4882ea6235283d2498
Florian d'Erfurth <florian@derfurth.com>
2023-08-04 11:30:01 +0200

    Remplace une apostrophe.

--- site/app/cookies/page.tsx
+++ site/app/cookies/page.tsx
@@ -3,7 +3,7 @@ export default function Cookies() {
     <div className="fr-container">
       <div className="fr-mt-1w fr-mt-md-4w fr-mb-5w">
         <h1 className="fr-header__body">Gestion des cookies</h1>
-        <p>Ce site n'utilise pas de cookie.</p>
+        <p>Ce site n&apos;utilise pas de cookie.</p>
         <p>
           Nous sommes ainsi en conformité avec la réglementation « Cookies » de
           la CNIL et exemptés d’autorisation préalable. C’est pour cela que vous
