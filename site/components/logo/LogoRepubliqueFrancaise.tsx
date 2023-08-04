ca00aa0bdf3662fba8ba054be049962387f4ffbe
Florian d'Erfurth <florian@derfurth.com>
2023-08-04 10:24:26 +0200

    Ajoute et utilise le `Footer`.

---
+++ site/components/logo/LogoRepubliqueFrancaise.tsx
@@ -0,0 +1,11 @@
+const LogoRepubliqueFrancaise = () => {
+  return (
+    <p className="fr-logo" title="république française">
+      république
+      <br />
+      française
+    </p>
+  );
+};
+
+export default LogoRepubliqueFrancaise;
