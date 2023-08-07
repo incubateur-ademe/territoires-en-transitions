3cf04b5c3b9f22c36d662f30aa37ff7b677d0cd2
Marc Rutkowski <marc@attractive-media.fr>
2023-08-07 17:37:38 +0200

    affiche le titre long sur le graphique de la page détail d'un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/CardNoData.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/CardNoData.tsx
@@ -10,26 +10,31 @@ export const CardNoData = ({
   variant,
   className,
   isReadonly,
-}: TIndicateurChartProps & {isReadonly: boolean}) => (
-  <Card
-    className={classNames(
-      'flex flex-col items-center justify-between px-10 py-6',
-      {'rounded-none': variant === 'zoomed'},
-      className
-    )}
-  >
-    <span className="font-bold text-[#161616]">
-      {getChartTitle(definition)}
-    </span>
-    <PictoPie className={classNames({'fr-my-4w': isReadonly})} />
-    {variant === 'zoomed' ? (
-      <span className="text-grey425 text-sm">
-        Aucune valeur renseignée pour l’instant
+}: TIndicateurChartProps & {isReadonly: boolean}) => {
+  const isZoomed = variant === 'zoomed';
+  return (
+    <Card
+      className={classNames(
+        'flex flex-col items-center justify-between px-10 py-6',
+        {'rounded-none': isZoomed},
+        className
+      )}
+    >
+      <span className="font-bold text-[#161616]">
+        {getChartTitle(definition, isZoomed)}
       </span>
-    ) : (
-      !isReadonly && (
-        <button className="fr-btn rounded-lg">Compléter cet indicateur</button>
-      )
-    )}
-  </Card>
-);
+      <PictoPie className={classNames({'fr-my-4w': isReadonly})} />
+      {isZoomed ? (
+        <span className="text-grey425 text-sm">
+          Aucune valeur renseignée pour l’instant
+        </span>
+      ) : (
+        !isReadonly && (
+          <button className="fr-btn rounded-lg">
+            Compléter cet indicateur
+          </button>
+        )
+      )}
+    </Card>
+  );
+};
