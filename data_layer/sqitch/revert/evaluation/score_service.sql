93746d8915ed980690678d2914bf380db179c3f4
Florian d'Erfurth <florian@derfurth.com>
2023-08-09 17:22:58 +0200

    Corrige les payloads construites par `audit_evaluation_payload` et `evaluation_payload`

--- data_layer/sqitch/revert/evaluation/score_service.sql
+++ data_layer/sqitch/revert/evaluation/score_service.sql
@@ -2,8 +2,34 @@
 
 BEGIN;
 
-drop function evaluation.service_regles;
-drop function evaluation.identite;
-drop function evaluation.current_service_configuration;
+create or replace function
+    evaluation.evaluation_payload(
+    in collectivite_id integer,
+    in referentiel referentiel,
+    out referentiel jsonb,
+    out statuts jsonb,
+    out consequences jsonb
+)
+as
+$$
+with statuts as (select s.data
+                 from evaluation.service_statuts s
+                 where s.referentiel = evaluation_payload.referentiel
+                   and s.collectivite_id = evaluation_payload.collectivite_id),
+     consequences as ( -- on ne garde que les conséquences du référentiel concerné
+         select jsonb_object_agg(tuple.key, tuple.value) as filtered
+         from personnalisation_consequence pc
+                  join jsonb_each(pc.consequences) tuple on true
+                  join action_relation ar on tuple.key = ar.id
+         where pc.collectivite_id = evaluation_payload.collectivite_id
+           and ar.referentiel = evaluation_payload.referentiel)
+select r.data                                        as referentiel,
+       coalesce(s.data, to_jsonb('{}'::jsonb[]))     as statuts,
+       coalesce(c.filtered, to_jsonb('{}'::jsonb[])) as consequences
+from evaluation.service_referentiel as r
+         left join statuts s on true
+         left join consequences c on true
+where r.referentiel = evaluation_payload.referentiel
+$$ language sql stable;
 
 COMMIT;
