ed8684f6718083a5bf573cca607db2e47999698e
Florian d'Erfurth <florian@derfurth.com>
2023-08-10 14:00:49 +0200

    Corrige le verify et le revert de la vue `indicateur_summary`

--- data_layer/sqitch/revert/indicateur/tableau_de_bord.sql
+++ data_layer/sqitch/revert/indicateur/tableau_de_bord.sql
@@ -3,18 +3,5 @@
 BEGIN;
 
 drop view indicateur_summary;
-create view indicateur_summary
-as
-with r as (select indicateur_id, collectivite_id, count(*)
-           from indicateur_resultat r
-           group by indicateur_id, collectivite_id)
-select collectivite_id,
-       indicateur_id,
-       indicateur_group,
-       r.count as resultats
-from indicateur_definition id
-         join r on id.id = r.indicateur_id
-where have_lecture_acces(collectivite_id);
-comment on view indicateur_summary is 'Permet d''obtenir le nombre de résultats saisis par indicateur pour chaque collectivité.';
 
 COMMIT;
