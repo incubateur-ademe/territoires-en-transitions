4a74f0dedc394a783106ea4bcc5587f2ef2be5b7
Marc Rutkowski <marc@attractive-media.fr>
2023-08-23 14:43:55 +0200

    essaye de rendre plus robuste un scénario de test e2e (statut d'audit des actions) en ajoutant une màj optimiste du cache

--- app.territoiresentransitions.react/src/app/pages/collectivite/Audit/useUpdateActionAuditStatut.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Audit/useUpdateActionAuditStatut.ts
@@ -10,27 +10,38 @@ export const useUpdateActionAuditStatut = () => {
     async (actionAuditStatut: TActionAuditStatut) => {
       const {collectivite_id, action_id, ordre_du_jour, avis, statut} =
         actionAuditStatut;
-      return supabaseClient
-        .from('action_audit_state')
-        .insert({
-          collectivite_id,
-          action_id,
-          ordre_du_jour,
-          avis,
-          statut,
-        } as never);
+      return supabaseClient.from('action_audit_state').insert({
+        collectivite_id,
+        action_id,
+        ordre_du_jour,
+        avis,
+        statut,
+      } as never);
     },
     {
       mutationKey: 'update_action_audit_state',
-      onSuccess: (data: unknown, variables: TActionAuditStatut) => {
-        const {collectivite_id, referentiel, action_id} = variables;
-        queryClient.invalidateQueries([
-          'action_audit_state',
-          collectivite_id,
-          referentiel,
-          action_id,
-        ]);
+      // màj optimiste du cache
+      onMutate: (variables: TActionAuditStatut) => {
+        const queryKey = getQueryKey(variables);
+        const previousValue = queryClient.getQueryData(queryKey);
+        queryClient.setQueryData(queryKey, variables);
+        return previousValue;
+      },
+      // recharge les données
+      onSettled: (data, error, variables: TActionAuditStatut, context) => {
+        const queryKey = getQueryKey(variables);
+        // restaure l'état précédent du cache en cas d'erreur
+        if (error) {
+          queryClient.setQueryData(queryKey, context);
+        }
+        // et refetch pour être sûr que le cache est à jour
+        queryClient.invalidateQueries(queryKey);
       },
     }
   );
+};
+
+const getQueryKey = (variables: TActionAuditStatut) => {
+  const {collectivite_id, referentiel, action_id} = variables;
+  return ['action_audit_state', collectivite_id, referentiel, action_id];
 };
