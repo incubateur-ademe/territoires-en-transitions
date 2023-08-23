bad9e2aac4ce6e3ae7a44d7f846eca09f6e354ab
Florian d'Erfurth <florian@derfurth.com>
2023-08-23 17:45:59 +0200

    wip import indicateurs

--- data_layer/emt_import/indicateurs_xlsx_to_csv.py
+++ data_layer/emt_import/indicateurs_xlsx_to_csv.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+def clean_up(value):
+    value = str(value)
+    value.replace(',', '. ');
+
+    return pd.to_numeric(value, errors='coerce"')
+
 if __name__ == '__main__':
     pathlist = Path("./").glob('**/*.xlsx')
 
@@ -11,21 +17,21 @@ if __name__ == '__main__':
         print(f'reading {path_in_str}...')
 
         # on lit la 2ᵉ feuille
-        sheet = pd.read_excel(path, 1)
-
-        # on ne conserve que les lignes à partir de la 11ᵉ ligne et jusqu'à la 12ᵉ colonne.
-        df = sheet.iloc[10:, :12]
+        df = pd.read_excel(path, 0)
 
         # on renomme les colonnes
-        df.columns = ['id', 'action', 'nom', 'description', 'valeur_1', 'annee_1', 'valeur_2', 'annee_2', 'source',
-                      'remarque', 'pcaet', 'obligatoire']
+        df.columns = ['id', 'action', 'nom', 'description',
+                      'valeur_1', 'annee_1', 'commentaire_1',
+                      'valeur_2', 'annee_2', 'commentaire_2',
+                      'valeur_3', 'annee_3', 'commentaire_3',
+                      'valeur_4', 'annee_4', 'commentaire_4']
 
         # on convertit les colonnes valeurs et années en valeurs numériques, les valeurs inconvertibles seront N/A
-        df[['valeur_1', 'valeur_2', 'annee_1', 'annee_2']] = \
-            df[['valeur_1', 'valeur_2', 'annee_1', 'annee_2']].apply(pd.to_numeric, errors='coerce')
+        df[['valeur_1', 'valeur_2', 'valeur_3', 'valeur_4', 'annee_1', 'annee_2', 'annee_3', 'annee_4']] = \
+            df[['valeur_1', 'valeur_2', 'valeur_3', 'valeur_4', 'annee_1', 'annee_2', 'annee_3', 'annee_4']].apply(pd.to_numeric, errors='coerce')
 
         # on convertit les années en entiers
-        df[['annee_1', 'annee_2']] = df[['annee_1', 'annee_2']].astype('Int64')
+        df[['annee_1', 'annee_2', 'annee_3', 'annee_4']] = df[['annee_1', 'annee_2', 'annee_3', 'annee_4']].astype('Int64')
 
         # enfin on sauvegarde la dataframe en csv.
         save_path = Path("./").joinpath(path.parts[-1].replace('.xlsx', '.csv'))
