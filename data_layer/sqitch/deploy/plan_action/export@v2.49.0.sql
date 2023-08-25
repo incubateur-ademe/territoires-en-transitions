0cea26b12cd4ae7ef551140359a04238c7cb3c04
Marc Rutkowski <marc@attractive-media.fr>
2023-08-25 13:57:24 +0200

    Ajoute le tri des fiches par titre dans l'export

--- data_layer/sqitch/deploy/plan_action/export@v2.49.0.sql
+++ data_layer/sqitch/deploy/plan_action/export@v2.49.0.sql
@@ -2,7 +2,19 @@
 
 BEGIN;
 
-create or replace function
+drop function plan_action_export;
+
+create type fiche_action_export as
+(
+    axe_id   integer,
+    axe_nom  text,
+    axe_path text[],
+    fiche    jsonb
+);
+comment on type fiche_action_export is
+    'Les informations pour lister une fiche dans l''export Excel des plans d''action.';
+
+create function
     plan_action_export(id integer)
     returns setof fiche_action_export
 begin
@@ -30,15 +42,14 @@ begin
                     from parents p
                              join axe a on a.parent = p.id),
         fiches as (select a.id as axe_id,
-                          f    as fiche,
-                          f.titre as titre
+                          f    as fiche
                    from parents a
                             join fiche_action_axe faa on a.id = faa.axe_id
                             join fiches_action f on faa.fiche_id = f.id)
     select p.id, p.nom, p.path, to_jsonb(f)
     from parents p
              left join fiches f on p.id = f.axe_id
-    order by naturalsort(sort_path || coalesce(titre, ''));
+    order by naturalsort(sort_path);
 end;
 comment on function plan_action_export is
     'Les fiches ordonnancées pour l''export des plans d''action.';
