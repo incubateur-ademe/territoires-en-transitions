9cd27b248a2de18c9534fefb4e4b1cb408eead2f
Arthur Molinos <arthur.molinos@gmail.com>
2023-08-28 15:55:39 +0200

    Création d'un hook spécifique pour update un axe de façon optimiste au drop de ce dernier

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/useDragAxe.ts
@@ -0,0 +1,50 @@
+import {useMutation, useQueryClient} from 'react-query';
+
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {dropAnimation} from '../DragAndDropNestedContainers/Arborescence';
+import {PlanNode} from './types';
+import {addAxeToPlan, getAxeInPlan, removeAxeFromPlan} from './utils';
+import {updateAxe} from './useEditAxe';
+
+/**
+ * Déplace un axe dans un autre axe
+ */
+export const useDragAxe = (planId: number) => {
+  const queryClient = useQueryClient();
+  const collectivite_id = useCollectiviteId();
+
+  return useMutation(updateAxe, {
+    onMutate: async args => {
+      const axe = args;
+
+      const planActionKey = ['plan_action', planId];
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({queryKey: planActionKey});
+
+      // Snapshot the previous value
+      const previousAction: {plan: PlanNode} | undefined =
+        queryClient.getQueryData(planActionKey);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(planActionKey, (old: any | PlanNode) => {
+        if (axe.id && axe.parent) {
+          const axeAsPlanNode = getAxeInPlan(old, axe.id);
+          const tempPlan = removeAxeFromPlan(old, axe.id);
+          const newPlan =
+            tempPlan && addAxeToPlan(tempPlan, axeAsPlanNode!, axe.parent);
+          return newPlan;
+        }
+      });
+
+      // Return a context object with the snapshotted value
+      return {previousAction};
+    },
+    onSuccess: (data, args) => {
+      queryClient.invalidateQueries(['plans_navigation', collectivite_id]);
+      queryClient.invalidateQueries(['plan_action', planId]);
+
+      args.id && dropAnimation(args.id.toString());
+    },
+  });
+};
