99edfb8fc91435db0a6f6f3e02ddf223b2906fe6
Arthur Molinos <arthur.molinos@gmail.com>
2023-08-29 16:59:26 +0200

    Replace le bouton de suppression d'une fiche action en haut de la fiche

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionFooter.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionFooter.tsx
@@ -1,32 +1,8 @@
 import ScrollTopButton from 'ui/buttons/ScrollTopButton';
-import {FicheAction} from './data/types';
-import {useDeleteFicheAction} from './data/useDeleteFicheAction';
-import FicheActionSupprimerModal from './FicheActionSupprimerModal';
 
-type TFicheActionFooter = {
-  fiche: FicheAction;
-  isReadonly: boolean;
-};
-
-const FicheActionFooter = ({fiche, isReadonly}: TFicheActionFooter) => {
-  const {mutate: deleteFiche} = useDeleteFicheAction();
-
+const FicheActionFooter = () => {
   return (
     <div className="py-16">
-      {!isReadonly && (
-        <FicheActionSupprimerModal
-          fiche={fiche}
-          onDelete={() => deleteFiche(fiche.id!)}
-        />
-      )}
-      <div className="flex justify-end gap-6 my-8">
-        {/* <button className="fr-btn fr-btn--secondary fr-fi-arrow-left-line fr-btn--icon-left">
-          Fiche précédente
-        </button>
-        <button className="fr-btn fr-btn fr-fi-arrow-right-line fr-btn--icon-right">
-          Fiche suivante
-        </button> */}
-      </div>
       <ScrollTopButton />
     </div>
   );
