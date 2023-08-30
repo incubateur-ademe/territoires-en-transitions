fd3127fab8e20d732d0da1653fa17fee711fc336
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-08-30 14:58:04 +0200

    Mise à jour de la page contact

---
+++ site/app/contact/data.ts
@@ -0,0 +1,51 @@
+export const options = [
+  {
+    group:
+      'Questions relatives au programme Territoire Engagé Transition Écologique',
+    options: [
+      {
+        value: 1,
+        label:
+          'Informations sur le programme Territoire Engagé Transition Écologique',
+      },
+      {
+        value: 2,
+        label: 'Processus d’audit et de labellisation',
+      },
+      {
+        value: 3,
+        label: 'Référentiel Climat Air Énergie',
+      },
+      {
+        value: 4,
+        label: 'Référentiel Économie circulaire',
+      },
+    ],
+  },
+  {
+    group: 'Questions relatives à la plateforme Territoires en transitions',
+    options: [
+      {
+        value: 5,
+        label: 'Création de compte ou connexion',
+      },
+      {
+        value: 6,
+        label: 'Suggestions d’améliorations',
+      },
+      {
+        value: 7,
+        label: 'Questions sur l’utilisation de la plateforme',
+      },
+    ],
+  },
+  {
+    group: 'Aucun de ces sujets ?',
+    options: [
+      {
+        value: 8,
+        label: 'Autre',
+      },
+    ],
+  },
+];
