2d45e42040903465ff4ae30ec05831a04810e83f
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-09-04 15:35:18 +0200

    Ajoute security definer aux fonctions fiche_resume

--- data_layer/sqitch/deploy/plan_action/fiches.sql
+++ data_layer/sqitch/deploy/plan_action/fiches.sql
@@ -2,77 +2,11 @@
 
 BEGIN;
 
-create or replace view private.fiche_resume(plans, titre, id, statut, collectivite_id, pilotes, modified_at) as
-select (with recursive chemin as (select axe.id as axe_id,
-                                         axe.parent,
-                                         axe    as plan
-                                  from axe
-                                  where axe.parent is null
-                                    and axe.collectivite_id = fa.collectivite_id
-                                  union all
-                                  select a.id as axe_id,
-                                         a.parent,
-                                         p.plan
-                                  from axe a
-                                           join chemin p on a.parent = p.axe_id)
-        select case when count(*) > 0 then array_agg(plan) end
-        from chemin
-        where chemin.axe_id in (select faa.axe_id from fiche_action_axe faa where faa.fiche_id = fa.id)) as plans,
-       fa.titre,
-       fa.id,
-       fa.statut,
-       fa.collectivite_id,
-       (select array_agg(row (pil.nom, pil.collectivite_id, pil.tag_id, pil.user_id)::personne) as array_agg
-        from (select coalesce(pt.nom, concat(dcp.prenom, ' ', dcp.nom)) as nom,
-                     pt.collectivite_id,
-                     fap.tag_id,
-                     fap.user_id
-              from fiche_action_pilote fap
-                       left join personne_tag pt on fap.tag_id = pt.id
-                       left join dcp on fap.user_id = dcp.user_id
-              where fap.fiche_id = fa.id) pil)                                                           as pilotes,
-       fa.modified_at
-from fiche_action fa
-group by fa.titre, fa.id, fa.statut, fa.collectivite_id;
-
-create or replace view public.fiche_resume(plans, titre, id, statut, collectivite_id, pilotes, modified_at) as
-select (with recursive chemin as (select axe.id as axe_id,
-                                         axe.parent,
-                                         axe    as plan
-                                  from axe
-                                  where axe.parent is null
-                                    and axe.collectivite_id = fa.collectivite_id
-                                  union all
-                                  select a.id as axe_id,
-                                         a.parent,
-                                         p.plan
-                                  from axe a
-                                           join chemin p on a.parent = p.axe_id)
-        select case when count(*) > 0 then array_agg(plan) end
-        from chemin
-        where chemin.axe_id in (select faa.axe_id from fiche_action_axe faa where faa.fiche_id = fa.id)) as plans,
-       fa.titre,
-       fa.id,
-       fa.statut,
-       fa.collectivite_id,
-       (select array_agg(row (pil.nom, pil.collectivite_id, pil.tag_id, pil.user_id)::personne) as array_agg
-        from (select coalesce(pt.nom, concat(dcp.prenom, ' ', dcp.nom)) as nom,
-                     pt.collectivite_id,
-                     fap.tag_id,
-                     fap.user_id
-              from fiche_action_pilote fap
-                       left join personne_tag pt on fap.tag_id = pt.id
-                       left join dcp on fap.user_id = dcp.user_id
-              where fap.fiche_id = fa.id) pil)                                                           as pilotes,
-       fa.modified_at
-from fiche_action fa
-where can_read_acces_restreint(fa.collectivite_id)
-group by fa.titre, fa.id, fa.statut, fa.collectivite_id;
-
 create or replace function fiche_resume(fiche_action_action fiche_action_action) returns SETOF fiche_resume
     rows 1
     language sql
     stable
+    security definer
 begin
     atomic
     select fr.plans,
@@ -91,6 +25,7 @@ create or replace function fiche_resume(fiche_action_indicateur fiche_action_ind
     rows 1
     language sql
     stable
+    security definer
 begin
     atomic
     select fr.plans,
--- data_layer/sqitch/revert/plan_action/fiches.sql
+++ data_layer/sqitch/revert/plan_action/fiches.sql
@@ -2,76 +2,40 @@
 
 BEGIN;
 
-create or replace view private.fiche_resume(plans, titre, id, statut, collectivite_id, pilotes, modified_at) as
-SELECT CASE
-           WHEN array_agg(a.*) = '{NULL}'::axe[] THEN NULL::axe[]
-           ELSE array_agg(a.*)
-           END                                 AS plans,
-       fa.titre,
-       fa.id,
-       fa.statut,
-       fa.collectivite_id,
-       (SELECT array_agg(ROW (pil.nom, pil.collectivite_id, pil.tag_id, pil.user_id)::personne) AS array_agg
-        FROM (SELECT COALESCE(pt.nom, concat(dcp.prenom, ' ', dcp.nom)) AS nom,
-                     pt.collectivite_id,
-                     fap.tag_id,
-                     fap.user_id
-              FROM fiche_action_pilote fap
-                       LEFT JOIN personne_tag pt ON fap.tag_id = pt.id
-                       LEFT JOIN dcp ON fap.user_id = dcp.user_id
-              WHERE fap.fiche_id = fa.id) pil) AS pilotes,
-       fa.modified_at
-FROM fiche_action fa
-         LEFT JOIN fiche_action_axe faa ON fa.id = faa.fiche_id
-         LEFT JOIN plan_action_chemin pac ON faa.axe_id = pac.axe_id
-         LEFT JOIN axe a ON pac.plan_id = a.id
-GROUP BY fa.titre, fa.id, fa.statut, fa.collectivite_id;
-
-create or replace view fiche_resume(plans, titre, id, statut, collectivite_id, pilotes, modified_at) as
-SELECT fiche_resume.plans,
-       fiche_resume.titre,
-       fiche_resume.id,
-       fiche_resume.statut,
-       fiche_resume.collectivite_id,
-       fiche_resume.pilotes,
-       fiche_resume.modified_at
-FROM private.fiche_resume
-WHERE can_read_acces_restreint(fiche_resume.collectivite_id);
-
--- Fonction fiche_resume
 create or replace function fiche_resume(fiche_action_action fiche_action_action) returns SETOF fiche_resume
     rows 1
     language sql
-BEGIN
-    ATOMIC
-    SELECT fiche_resume.plans,
-           fiche_resume.titre,
-           fiche_resume.id,
-           fiche_resume.statut,
-           fiche_resume.collectivite_id,
-           fiche_resume.pilotes,
-           fiche_resume.modified_at
-    FROM fiche_resume
-    WHERE (fiche_resume.id = ($1).fiche_id)
-      and can_read_acces_restreint(fiche_resume.collectivite_id);
-END;
+    stable
+begin
+    atomic
+    select fr.plans,
+           fr.titre,
+           fr.id,
+           fr.statut,
+           fr.collectivite_id,
+           fr.pilotes,
+           fr.modified_at
+    from private.fiche_resume as fr
+    where fr.id = fiche_action_action.fiche_id
+      and can_read_acces_restreint(fr.collectivite_id);
+end;
 
--- Fonction fiche_resume
 create or replace function fiche_resume(fiche_action_indicateur fiche_action_indicateur) returns SETOF fiche_resume
     rows 1
     language sql
-BEGIN
-    ATOMIC
-    SELECT fiche_resume.plans,
-           fiche_resume.titre,
-           fiche_resume.id,
-           fiche_resume.statut,
-           fiche_resume.collectivite_id,
-           fiche_resume.pilotes,
-           fiche_resume.modified_at
-    FROM fiche_resume
-    WHERE (fiche_resume.id = ($1).fiche_id)
-      and can_read_acces_restreint(fiche_resume.collectivite_id);
-END;
+    stable
+begin
+    atomic
+    select fr.plans,
+           fr.titre,
+           fr.id,
+           fr.statut,
+           fr.collectivite_id,
+           fr.pilotes,
+           fr.modified_at
+    from private.fiche_resume as fr
+    where fr.id = fiche_action_indicateur.fiche_id
+      and can_read_acces_restreint(fr.collectivite_id);
+end;
 
 COMMIT;
