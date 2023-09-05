cf4ffe4c44760c57474ba863b35585b7937a46a0
Arthur Molinos <arthur.molinos@gmail.com>
2023-09-05 18:09:24 +0200

    Améliore les optimistic updates sur la page Axe et le sort sur le drag n drop d'un axe dans un autre

---
+++ app.territoiresentransitions.react/src/utils/naturalSort.ts
@@ -0,0 +1,36 @@
+/**
+ * Regex dont le premier groupe revoie les suites de nombres
+ * et le deuxième le reste.
+ */
+const regex = /0*([0-9]+)|([^0-9]+)/g;
+
+/**
+ * Transforme un texte en un texte dont la comparaison permettra
+ * un ordonnancement naturel.
+ */
+function comparable(text: string): string {
+  return [...text.matchAll(regex)]
+    .map(m =>
+      m[2]
+        ? m[2]
+        : m[0].length.toString().length.toString() +
+          m[0].length.toString() +
+          m[0]
+    )
+    .join('');
+}
+
+/**
+ * La fonction de comparaison utilisée par la fonction "sort".
+ */
+export function naturalSort(a: string, b: string) {
+  const ca = comparable(a);
+  const cb = comparable(b);
+  if (ca < cb) {
+    return -1;
+  }
+  if (ca > cb) {
+    return 1;
+  }
+  return 0;
+}
