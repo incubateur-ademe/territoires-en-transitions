e8ec44bdd57d78f5294162a983422c22155c10a5
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-09-05 16:12:42 +0200

    Ajoute un ordonnancement à la vue private.fiche_resume

--- data_layer/sqitch/deploy/plan_action/fiches.sql
+++ data_layer/sqitch/deploy/plan_action/fiches.sql
@@ -2,6 +2,32 @@
 
 BEGIN;
 
+create or replace view private.fiche_resume(plans, titre, id, statut, collectivite_id, pilotes, modified_at) as
+SELECT CASE
+           WHEN array_agg(a.*) = '{NULL}'::axe[] THEN NULL::axe[]
+           ELSE array_agg(a.*)
+           END                                 AS plans,
+       fa.titre,
+       fa.id,
+       fa.statut,
+       fa.collectivite_id,
+       (SELECT array_agg(ROW (pil.nom, pil.collectivite_id, pil.tag_id, pil.user_id)::personne) AS array_agg
+        FROM (SELECT COALESCE(pt.nom, concat(dcp.prenom, ' ', dcp.nom)) AS nom,
+                     pt.collectivite_id,
+                     fap.tag_id,
+                     fap.user_id
+              FROM fiche_action_pilote fap
+                       LEFT JOIN personne_tag pt ON fap.tag_id = pt.id
+                       LEFT JOIN dcp ON fap.user_id = dcp.user_id
+              WHERE fap.fiche_id = fa.id) pil) AS pilotes,
+       fa.modified_at
+FROM fiche_action fa
+         LEFT JOIN fiche_action_axe faa ON fa.id = faa.fiche_id
+         LEFT JOIN plan_action_chemin pac ON faa.axe_id = pac.axe_id
+         LEFT JOIN axe a ON pac.plan_id = a.id
+GROUP BY fa.titre, fa.id, fa.statut, fa.collectivite_id
+ORDER BY naturalsort(fa.titre);
+
 create or replace function fiche_resume(fiche_action_action fiche_action_action) returns SETOF fiche_resume
     rows 1
     language sql
