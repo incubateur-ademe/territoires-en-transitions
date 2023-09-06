6c5f095bc6394ff61e1d9db08df265a6f7121579
Marc Rutkowski <marc@attractive-media.fr>
2023-09-06 17:45:27 +0200

    ajoute des utilitaires pour le typage généré depuis la base

---
+++ supabase/functions/_shared/typeUtils.ts
@@ -0,0 +1,16 @@
+import { Database } from './database.types.ts';
+
+// Alias génériques sur le typage de la base
+export type Tables<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Row'];
+export type Views<T extends keyof Database['public']['Views']> =
+  Database['public']['Views'][T]['Row'];
+export type Enums<T extends keyof Database['public']['Enums']> =
+  Database['public']['Enums'][T];
+
+// Génère, à partir d'un type, un nouveau type dont tous les champs sont non
+// null. Utile pour caster le typage de certaines Views exportées par supabase
+// gen types
+export type NonNullableFields<T> = {
+  [P in keyof T]: NonNullable<T[P]>;
+};
