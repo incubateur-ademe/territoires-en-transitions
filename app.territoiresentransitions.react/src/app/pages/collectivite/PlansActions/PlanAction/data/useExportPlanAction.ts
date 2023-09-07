1f2c5137b13aa54ae864acb4c8d9df279e806fe6
Florian d'Erfurth <florian@derfurth.com>
2023-09-07 09:36:56 +0200

    Ajout du tracker.

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/useExportPlanAction.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/data/useExportPlanAction.ts
@@ -1,9 +1,12 @@
 import {useMutation} from 'react-query';
-import {supabaseClient} from 'core-logic/api/supabase';
 import {format as formatDate} from 'date-fns';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useFonctionTracker} from 'core-logic/hooks/useFonctionTracker';
 import {saveBlob} from 'ui/shared/preuves/Bibliotheque/saveBlob';
 
 export const useExportPlanAction = (planId: number) => {
+  const tracker = useFonctionTracker();
+
   return useMutation(
     ['export_plan_action', planId],
     async (format: 'xlsx' | 'docx') => {
@@ -15,11 +18,19 @@ export const useExportPlanAction = (planId: number) => {
         }
       );
 
-      // on génère le nom du fichier car l'en-tête "content-disposition" de la
-      // fonction edge ne semble pas être transmis correctement au client...
-      const exportedAt = formatDate(new Date(), 'yyyy-MM-dd');
-      const filename = `Export_${titre}_${exportedAt}.${format}`;
-      saveBlob(data, filename);
+      if (data) {
+        // on génère le nom du fichier car l'en-tête "content-disposition" de la
+        // fonction edge ne semble pas être transmis correctement au client...
+        const exportedAt = formatDate(new Date(), 'yyyy-MM-dd');
+        const filename = `Export_${titre}_${exportedAt}.${format}`;
+        saveBlob(data, filename);
+
+        tracker({
+          page: 'plan',
+          action: 'telechargement',
+          fonction: `export_${format}`,
+        });
+      }
     }
   );
 };
