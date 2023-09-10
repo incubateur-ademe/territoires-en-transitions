ae72d0c39a8fd7058b29d9c1232c0be008e211f7
Florian d'Erfurth <florian@derfurth.com>
2023-09-10 17:46:59 +0200

    Page de test `/carto`

---
+++ site/app/carto/page.tsx
@@ -0,0 +1,11 @@
+'use server';
+
+import CarteCollectivites from './CarteCollectivites';
+
+/**
+ * Page de test
+ */
+
+export default async function Page() {
+  return <CarteCollectivites type="labellisees" />;
+}
