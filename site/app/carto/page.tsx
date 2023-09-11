38ed3c1315584c8200ed7858a61ec0d7425a8e56
Florian d'Erfurth <florian@derfurth.com>
2023-09-11 11:39:21 +0200

    Demo des différents filtres

--- site/app/carto/page.tsx
+++ site/app/carto/page.tsx
@@ -7,5 +7,19 @@ import CarteCollectivites from './CarteCollectivites';
  */
 
 export default async function Page() {
-  return <CarteCollectivites type="labellisees" />;
+  return (
+    <div>
+      <h2 className="mt-20">Labellisées</h2>
+      <CarteCollectivites filtre="labellisees" />
+
+      <h2 className="mt-20">Labellisées ECI</h2>
+      <CarteCollectivites filtre="labellisees_eci" />
+
+      <h2 className="mt-20">Labellisées CAE</h2>
+      <CarteCollectivites filtre="labellisees_cae" />
+
+      <h2 className="mt-20">COT</h2>
+      <CarteCollectivites filtre="cot" />
+    </div>
+  );
 }
