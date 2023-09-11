5740cb00ac67e309312e345b45cb94dccbb3949f
Florian d'Erfurth <florian@derfurth.com>
2023-09-11 11:22:04 +0200

    Ajoute les couleurs

--- site/app/carto/CollectiviteFeature.tsx
+++ site/app/carto/CollectiviteFeature.tsx
@@ -1,34 +1,35 @@
 'use client';
 
-import {FeatureGroup, GeoJSON, Popup} from "react-leaflet";
-import {GeoJsonObject} from "geojson";
-import {Database, Json} from "../database.types";
-
+import {FeatureGroup, GeoJSON, Popup} from 'react-leaflet';
+import {GeoJsonObject} from 'geojson';
+import {Database, Json} from '../database.types';
 
 type labellisation_w_geojson =
-  Database['public']['Views']['site_labellisation']['Row'] & { geojson?: Json };
+  Database['public']['Views']['site_labellisation']['Row'] & {geojson?: Json};
 
 /**
  * Une feature est un élément affiché dans une carte
  */
-export function CollectiviteFeature(props: { item: labellisation_w_geojson }) {
-  const {item} = props
+export function CollectiviteFeature(props: {item: labellisation_w_geojson}) {
+  const {item} = props;
   const geojson = item.geojson as unknown as GeoJsonObject;
-  const style = {
-    fillColor: "#98ff80",
-    color: "#50a759",
-  }
+  const style = item.labellise
+    ? {
+        fillColor: '#476c9b',
+        color: '#164988',
+      }
+    : {
+        fillColor: 'rgb(152,152,152)',
+        color: 'rgb(118,118,118)',
+      };
 
   return (
     <FeatureGroup key={item.collectivite_id}>
-      <GeoJSON data={geojson} style={style}/>
+      <GeoJSON data={geojson} style={style} />
       <Popup>
         <h4>{item.nom}</h4>
         <pre>{JSON.stringify({...item, geojson: '...'}, null, 2)}</pre>
-        <button
-          id="button"
-          className="fr-btn rounded-lg"
-        >
+        <button id="button" className="fr-btn rounded-lg">
           Page collectivité
         </button>
       </Popup>
