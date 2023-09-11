5949d060f3718b9a9dd6fe15b5be6b022278b9d2
Florian d'Erfurth <florian@derfurth.com>
2023-09-11 11:37:47 +0200

    Construit la query à partir de filtres

--- site/app/carto/CarteCollectivites.tsx
+++ site/app/carto/CarteCollectivites.tsx
@@ -3,29 +3,31 @@
 import dynamic from 'next/dynamic';
 import {supabase} from '../initSupabase';
 import {Database, Json} from '../database.types';
-import {CollectiviteFeature} from "./CollectiviteFeature";
+import {CollectiviteFeature} from './CollectiviteFeature';
 
 type labellisation_w_geojson =
-  Database['public']['Views']['site_labellisation']['Row'] & { geojson?: Json };
+  Database['public']['Views']['site_labellisation']['Row'] & {geojson?: Json};
 
 type Props = {
-  type: 'labellisees' | 'labellisees_eci' | 'labellisees_cae' | 'cot';
+  filtre: 'labellisees' | 'labellisees_eci' | 'labellisees_cae' | 'cot';
 };
 
 export default async function CarteCollectivites(props: Props) {
-
   // Import du composant uniquement client side.
   const CarteNoSSR = dynamic(() => import('./Carte'), {
     ssr: false,
   });
 
-  // todo utiliser le type pour modifier le select.
-  const {type} = props;
+  const {filtre} = props;
 
-  const {data, error} = await supabase
-    .from('site_labellisation')
-    .select('*, geojson')
-    .eq('labellise', true);
+  let query = supabase.from('site_labellisation').select('*, geojson');
+
+  if (filtre === 'labellisees') query = query.eq('labellise', true);
+  if (filtre === 'labellisees_cae') query = query.lte('cae_etoiles', 1);
+  if (filtre === 'labellisees_eci') query = query.lte('eci_etoiles', 1);
+  if (filtre === 'cot') query = query.eq('cot', true);
+
+  const {data, error} = await query;
 
   if (!data) {
     return null;
@@ -33,9 +35,13 @@ export default async function CarteCollectivites(props: Props) {
 
   const results = data as unknown as labellisation_w_geojson[];
 
-  return <CarteNoSSR>
-    {results.filter(c => !!c.geojson).map((c) =>
-      <CollectiviteFeature item={c} key={c.collectivite_id}/>
-    )}
-  </CarteNoSSR>;
+  return (
+    <CarteNoSSR>
+      {results
+        .filter(c => !!c.geojson)
+        .map(c => (
+          <CollectiviteFeature item={c} key={c.collectivite_id} />
+        ))}
+    </CarteNoSSR>
+  );
 }
