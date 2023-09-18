9136da737176ea07fcbab487844dbb009dba536c
Florian d'Erfurth <florian@derfurth.com>
2023-09-18 18:00:24 +0200

    Affiche les régions

--- site/app/carto/CarteCollectivites.tsx
+++ site/app/carto/CarteCollectivites.tsx
@@ -4,9 +4,14 @@ import dynamic from 'next/dynamic';
 import {supabase} from '../initSupabase';
 import {Database, Json} from '../database.types';
 import {CollectiviteFeature} from './CollectiviteFeature';
+import {RegionFeature} from './RegionFeature';
 
 type labellisation_w_geojson =
   Database['public']['Views']['site_labellisation']['Row'] & {geojson?: Json};
+
+type region_w_geojson = Database['public']['Views']['site_region']['Row'] & {
+  geojson?: Json;
+};
 
 type Props = {
   filtre: 'labellisees' | 'labellisees_eci' | 'labellisees_cae' | 'cot';
@@ -27,17 +32,25 @@ export default async function CarteCollectivites(props: Props) {
   if (filtre === 'labellisees_eci') query = query.lte('eci_etoiles', 1);
   if (filtre === 'cot') query = query.eq('cot', true);
 
-  const {data, error} = await query;
+  const collectivitesQuery = await query;
+  const regionsQuery = await supabase.from('site_region').select('*, geojson');
 
-  if (!data) {
+  if (!collectivitesQuery.data || !regionsQuery.data) {
     return null;
   }
 
-  const results = data as unknown as labellisation_w_geojson[];
+  const collectivites =
+    collectivitesQuery.data as unknown as labellisation_w_geojson[];
+  const regions = regionsQuery.data as unknown as region_w_geojson[];
 
   return (
     <CarteNoSSR>
-      {results
+      {regions
+        .filter(r => !!r.geojson)
+        .map(r => (
+          <RegionFeature item={r} key={r.insee} />
+        ))}
+      {collectivites
         .filter(c => !!c.geojson)
         .map(c => (
           <CollectiviteFeature item={c} key={c.collectivite_id} />
