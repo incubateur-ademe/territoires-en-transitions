d8fef11281fa84756ce6b2ff793897d1fa3ab721
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-09-20 14:48:06 +0200

    Corrections d'affichages sur la carte de labellisation et la page collectivité

--- site/app/collectivite/[code]/[name]/Citation.tsx
+++ site/app/collectivite/[code]/[name]/Citation.tsx
@@ -6,12 +6,14 @@ type CitationProps = {
 
 const Citation = ({data: {texte, auteur, description}}: CitationProps) => {
   return (
-    <div className="w-full md:w-4/5 mx-auto bg-[#f5f5fe] p-8 mb-6 rounded-xl">
-      <h2 className="text-center">Ils ont dit...</h2>
-      <p className="text-center">« {texte} »</p>
-      <p className="text-right font-bold mb-0">{auteur}</p>
-      <p className="text-right italic mb-0">{description}</p>
-    </div>
+    <>
+      <h2 className="text-center">Ils ont dit...</h2>{' '}
+      <div className="w-full md:w-4/5 mx-auto bg-[#f5f5fe] p-8 mb-6 border-x-2 border-x-[#000091]">
+        <p className="text-center">« {texte} »</p>
+        <p className="text-right font-bold mb-0">{auteur}</p>
+        <p className="text-right italic mb-0">{description}</p>
+      </div>
+    </>
   );
 };
 
