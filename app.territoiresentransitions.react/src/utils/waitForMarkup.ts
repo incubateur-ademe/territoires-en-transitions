13226aa311f07156d8f982f048d7239e4697e7f6
Arthur Molinos <>
2023-09-21 18:17:35 +0200

    Nouvel utilitaire pour attendre qu'un markup html soit présent dans le DOM

---
+++ app.territoiresentransitions.react/src/utils/waitForMarkup.ts
@@ -0,0 +1,22 @@
+// Solution trouvée: https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+
+/** Attend qu'une balise html soit présente dans le dom pour faire une action */
+export function waitForMarkup(selector: string) {
+  return new Promise((resolve: (el: Element | null) => void) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        resolve(document.querySelector(selector));
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
