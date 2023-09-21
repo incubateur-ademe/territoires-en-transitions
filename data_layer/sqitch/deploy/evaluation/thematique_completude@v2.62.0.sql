70f8fb2f4061838d0197da3f846ea96ceeb46523
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-09-21 17:33:08 +0200

    Corrige le count de private.reponse_count_by_thematique

---
+++ data_layer/sqitch/deploy/evaluation/thematique_completude@v2.62.0.sql
@@ -0,0 +1,24 @@
+-- Deploy tet:evaluation/thematique_completude to pg
+-- requires: evaluation/question
+
+BEGIN;
+
+create or replace view question_thematique_completude as
+select c.id    as collectivite_id,
+       qtd.id,
+       qtd.nom,
+       qtd.referentiels,
+       case
+           when private.reponse_count_by_thematique(c.id, qt.id) >=
+                private.question_count_for_thematique(c.id, qt.id)
+               then 'complete'::thematique_completude
+           else 'a_completer'::thematique_completude
+           end as completude
+from collectivite c
+         left join question_thematique qt on true
+         left join question_thematique_display qtd on qtd.id = qt.id
+where referentiels is not null
+  and est_verifie()
+order by (case when qtd.id = 'identite' then '0' else qtd.nom end);
+
+COMMIT;
