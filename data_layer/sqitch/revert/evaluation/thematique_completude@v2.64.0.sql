6b0f17013c9c8bc160fc173951bc1d1b212589c9
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-09-26 14:48:32 +0200

    Ne prends plus en compte les réponses nulls dans le count des réponses

--- data_layer/sqitch/revert/evaluation/thematique_completude@v2.64.0.sql
+++ data_layer/sqitch/revert/evaluation/thematique_completude@v2.64.0.sql
@@ -8,20 +8,17 @@ create or replace function private.reponse_count_by_thematique(collectivite_id i
     language sql
 begin
     atomic
-    select (count(rb) + count(rp) + count(rc)) as count
+    select count(*)
     from question q
              left join reponse_binaire rb
                        on rb.question_id = q.id
                            and rb.collectivite_id = reponse_count_by_thematique.collectivite_id
-                           and rb.reponse is not null
              left join reponse_proportion rp
                        on rp.question_id = q.id
                            and rp.collectivite_id = reponse_count_by_thematique.collectivite_id
-                           and rp.reponse is not null
              left join reponse_choix rc
                        on rc.question_id = q.id
                            and rc.collectivite_id = reponse_count_by_thematique.collectivite_id
-                           and rc.reponse is not null
     where q.thematique_id = reponse_count_by_thematique.thematique_id;
 end;
 
