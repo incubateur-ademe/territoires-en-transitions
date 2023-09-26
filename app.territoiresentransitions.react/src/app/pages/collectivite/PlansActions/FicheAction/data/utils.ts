a06566e9236d8f3724eb025806c4c67ef2535646
Arthur Molinos <>
2023-09-26 18:51:15 +0200

    Supprime de code wip non utilisé

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
@@ -1,4 +1,3 @@
-import {FicheAction, FicheResume, Personne} from './types';
 import {naturalSort} from 'utils/naturalSort';
 import {FicheResume, Personne} from './types';
 
@@ -23,58 +22,6 @@ export const getPersonneId = (personne: Personne): string =>
 /** Renvoie "Sans titre" si le string est undefined ou null */
 export const generateTitle = (title?: string | null) => title || 'Sans titre';
 
-export const createFicheResume = ({
-  collectivite_id,
-  axeId,
-}: {
-  collectivite_id: number;
-  axeId: number;
-}): FicheResume => {
-  const newFiche = {
-    id: null,
-    collectivite_id,
-    modified_at: null,
-    // plans: [{id: axeId, collectivite_id}],
-    plans: [
-      {
-        collectivite_id,
-        id: axeId,
-        // created_at: null,
-        // modified_at: string,
-        // modified_by: null,
-        // nom: string | null
-        // parent: number | null
-      },
-    ],
-    titre: null,
-    date_fin_provisoire: null,
-    niveau_priorite: null,
-    pilotes: null,
-    statut: null,
-  };
-  return newFiche;
-};
-
-export const updateFichesResumeFromFicheAction = ({
-  fichesResume,
-  ficheAction,
-}: {
-  fichesResume: FicheResume[];
-  ficheAction: FicheAction;
-}): FicheResume[] => {
-  const newFiche = {
-    id: ficheAction.id,
-    collectivite_id: ficheAction.collectivite_id,
-    modified_at: ficheAction.modified_at,
-    // plans: ficheAction?.map(f => (f.id !== fiche.id ? f : fiche)),
-    plans: [],
-    titre: null,
-    date_fin_provisoire: null,
-    niveau_priorite: null,
-    pilotes: null,
-    statut: null,
-  };
-  return fichesResume.map(f => (f.id !== null ? f : newFiche));
 /** Ordonne les fiches résumé par titre */
 export const ficheResumeByTitle = (a: FicheResume, b: FicheResume) => {
   if (!a.titre) return -1;
