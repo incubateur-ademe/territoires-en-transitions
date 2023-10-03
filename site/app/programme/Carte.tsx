b4a1f5595ad180d9a58a6560ab588c3c04368a85
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-10-03 15:50:05 +0200

    Corrige la redirection vers la carte

--- site/app/programme/Carte.tsx
+++ site/app/programme/Carte.tsx
@@ -4,32 +4,51 @@
 import ButtonWithLink from '@components/buttons/ButtonWithLink';
 import Section from '@components/sections/Section';
 import CarteAvecFiltres from '@components/carte/CarteAvecFiltres';
+import {useEffect, useRef} from 'react';
 
 const Carte = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const url = window.location.href;
+  const anchor = url.split('#')[1];
+
+  useEffect(() => {
+    if (anchor && anchor === 'carte' && ref && ref.current) {
+      setTimeout(() => {
+        ref.current?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }, 50);
+    }
+  }, [ref, anchor]);
+
   return (
-    <Section id="carte">
-      <h2>De nombreuses collectivités ont déjà franchi le cap !</h2>
+    <div ref={ref}>
+      <Section>
+        <h2>De nombreuses collectivités ont déjà franchi le cap !</h2>
 
-      <CarteAvecFiltres />
+        <CarteAvecFiltres />
 
-      {/* <div className="flex flex-col lg:flex-row justify-around items-center gap-y-8 mt-16">
-        <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
-          <span className="font-bold text-2xl">500</span> collectivités engagées
-        </div>
-        <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
-          <span className="font-bold text-2xl">20</span> millions d'habitants
-          concernés
-        </div>
-        <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
-          <span className="font-bold text-2xl">50 %</span> de la population
-          française
-        </div>
-      </div> */}
+        {/* <div className="flex flex-col lg:flex-row justify-around items-center gap-y-8 mt-16">
+      <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
+        <span className="font-bold text-2xl">500</span> collectivités engagées
+      </div>
+      <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
+        <span className="font-bold text-2xl">20</span> millions d'habitants
+        concernés
+      </div>
+      <div className="flex flex-col items-center bg-[#f5f5fe] p-x-6 py-8 min-w-[300px] w-fit rounded-lg">
+        <span className="font-bold text-2xl">50 %</span> de la population
+        française
+      </div>
+    </div> */}
 
-      <ButtonWithLink href="/faq" secondary rounded className="mt-16 mx-auto">
-        Lire les questions fréquentes
-      </ButtonWithLink>
-    </Section>
+        <ButtonWithLink href="/faq" secondary rounded className="mt-16 mx-auto">
+          Lire les questions fréquentes
+        </ButtonWithLink>
+      </Section>
+    </div>
   );
 };
 
