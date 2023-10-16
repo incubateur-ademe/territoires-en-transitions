5916e340f12d33b2ecd53e2a1edb151719f542a6
Marc Rutkowski <marc@attractive-media.fr>
2023-10-16 12:23:55 +0200

    Conserve l’intitulé de l’indicateur en haut de la page détail

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/IndicateurEnfant.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/IndicateurEnfant.tsx
@@ -60,7 +60,7 @@ const IndicateurEnfantHeader = ({
   return (
     <div
       className={classNames(
-        'flex justify-between items-center px-6 py-4 rounded-lg cursor-pointer',
+        'flex justify-between items-center px-6 py-4 rounded-lg cursor-pointer sticky top-[86px] z-30',
         {
           'bg-[#f5f5fE]': open,
           'hover:bg-grey975': !open,
