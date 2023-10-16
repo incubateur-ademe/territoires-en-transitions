9b438157aac9ccd92d7e304279f817960dc2fa9f
Marc Rutkowski <marc@attractive-media.fr>
2023-10-16 19:09:49 +0200

    affiche les boutons "Indicateur précédent" et "Indicateur suivant" en haut de la page détail d'un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/HeaderIndicateur.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/HeaderIndicateur.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import {Link} from 'react-router-dom';
 import HeaderTitle from 'ui/HeaderTitle';
+import {usePrevAndNextIndicateurLinks} from './usePrevAndNextIndicateurLinks';
 
 /** Affiche l'en-tête d'une page détail d'un indicateur */
 type HeaderProps = {
@@ -16,16 +17,50 @@ export const HeaderIndicateur = ({
   const readonly = isReadonly ?? true;
 
   return (
-    <HeaderTitle
-      titre={title}
-      customClass={{
-        container: classNames('!bg-bf925 sticky top-0 z-40', {
-          'pb-0': !readonly,
-        }),
-        text: classNames('!text-[#3a3a3a] text-[1rem]', {'pb-4': !readonly}),
-      }}
-      onUpdate={onUpdate}
-      isReadonly={readonly}
-    />
+    <>
+      <HeaderTitle
+        titre={title}
+        customClass={{
+          container: classNames('!bg-bf925 sticky top-0 z-40', {
+            'pb-0': !readonly,
+          }),
+          text: classNames('!text-[#3a3a3a] text-[1rem]', {'pb-4': !readonly}),
+        }}
+        onUpdate={onUpdate}
+        isReadonly={readonly}
+      />
+      <IndicateurTopNav />
+    </>
   );
+};
+
+/**
+ * Affiche les boutons "Indicateur précédent" et "Indicateur suivant" en haut de page
+ */
+export const IndicateurTopNav = () => {
+  const {prevIndicateurLink, nextIndicateurLink} =
+    usePrevAndNextIndicateurLinks();
+
+  return prevIndicateurLink || nextIndicateurLink ? (
+    <div className="!bg-bf925 min-h-[1.5rem] flex justify-between fr-text--sm !m-0 fr-pb-3w fr-px-5w overflow-hidden">
+      {prevIndicateurLink ? (
+        <Link
+          to={prevIndicateurLink}
+          className="fr-fi-arrow-left-line fr-btn--icon-left active-transparent"
+        >
+          Indicateur précédent
+        </Link>
+      ) : (
+        <div />
+      )}
+      {nextIndicateurLink && (
+        <Link
+          to={nextIndicateurLink}
+          className="justify-self-end fr-fi-arrow-right-line fr-btn--icon-right active-transparent"
+        >
+          Indicateur suivant
+        </Link>
+      )}
+    </div>
+  ) : null;
 };
