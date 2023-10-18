cc6392d94b7ab75ffdc4704d453170b51d6b641e
Florian d'Erfurth <florian@derfurth.com>
2023-10-18 17:18:11 +0200

    Ajoute `SUPABASE_SERVICE_ROLE_KEY` à l'env des tests API

--- make_dot_env.sh
+++ make_dot_env.sh
@@ -2,4 +2,4 @@ envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./data_layer/requests/
 envsubst '${SERVICE_ROLE_KEY}' < ./business/.env.sample > ./business/.env
 envsubst '${ANON_KEY}, ${API_URL}' < ./app.territoiresentransitions.react/.env.sample > ./app.territoiresentransitions.react/.env
 envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./e2e/.env.sample > ./e2e/.env
-envsubst '${ANON_KEY}, ${API_URL}' < ./api_tests/.env.example > ./api_tests/.env
+envsubst '${SERVICE_ROLE_KEY}, ${ANON_KEY}, ${API_URL}' < ./api_tests/.env.example > ./api_tests/.env
