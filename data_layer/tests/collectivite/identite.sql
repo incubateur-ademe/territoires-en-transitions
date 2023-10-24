099d04970b60dd3acca73c43b99e989600af0076
Amandine Jacquelin <conta@jacquelinamandine.fr>
2023-10-24 17:13:49 +0200

    Modification test SQL pour ajouter plus_de_20000

--- data_layer/tests/collectivite/identite.sql
+++ data_layer/tests/collectivite/identite.sql
@@ -29,7 +29,7 @@ where id = (select collectivite_id
 
 select ok((select count(*) = 1 from agen), 'Agen ne compte qu''une seule collectivité.');
 select ok((select '{commune}' = type from agen), 'Agen est une commune');
-select ok((select '{moins_de_50000, moins_de_100000}' = population from agen), 'Agen a moins de 50 000 habitants');
+select ok((select '{moins_de_100000,moins_de_50000,plus_de_20000}' = population from agen), 'Agen a moins de 50 000 habitants');
 
 select *
 into sictom
