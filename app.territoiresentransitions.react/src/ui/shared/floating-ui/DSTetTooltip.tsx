fd66c50aaf6da31c0488b23a43bc040b8cde41da
Arthur Molinos <unknown>
2023-10-24 18:46:51 +0100

    Modifie le composant Tooltip existant pour pouvoir le transformer en celui du nouveau design system

---
+++ app.territoiresentransitions.react/src/ui/shared/floating-ui/DSTetTooltip.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import {TTooltipProps, Tooltip} from './Tooltip';
+
+const DSTetTooltip = ({children, ...props}: TTooltipProps) => {
+  return (
+    <Tooltip
+      {...props}
+      className={classNames(
+        '!p-2 !text-grey-8 border-t border-principale rounded bg-white shadow-lg',
+        props.className
+      )}
+      offsetValue={props.offsetValue ?? 10}
+      triggerDuration={props.triggerDuration ?? 500}
+      withArrow
+    >
+      {children}
+    </Tooltip>
+  );
+};
+
+export default DSTetTooltip;
