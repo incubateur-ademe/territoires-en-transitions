42f34ebc16841b1a6d5137fe6f6b86e51d245a7c
Arthur Molinos <unknown>
2023-10-26 17:13:51 +0100

    Supprime du code inutile

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FichesNonClassees.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FichesNonClassees.tsx
@@ -2,29 +2,17 @@ import {useCollectiviteId} from 'core-logic/hooks/params';
 import FicheActionCard from './FicheAction/Carte/FicheActionCard';
 import {useFichesNonClasseesListe} from './FicheAction/data/useFichesNonClasseesListe';
 import {makeCollectiviteFicheNonClasseeUrl} from 'app/paths';
-import PictoLeaf from 'ui/pictogrammes/PictoLeaf';
-import {useCreateFicheAction} from './FicheAction/data/useUpsertFicheAction';
 
 const FichesNonClassees = () => {
   const collectivite_id = useCollectiviteId();
 
   const {data} = useFichesNonClasseesListe(collectivite_id!);
 
-  const {mutate: createFicheAction} = useCreateFicheAction();
-
   if (!collectivite_id || !data) return null;
 
   return (
     <div data-test="FichesNonClassees" className="p-10 grow">
-      {data.length === 0 ? (
-        <div className="flex flex-col items-center mt-8">
-          <PictoLeaf className="w-24 fill-gray-400" />
-          <div className="my-6 text-gray-500">Aucune fiche non classée</div>
-          <button className="fr-btn" onClick={() => createFicheAction()}>
-            Créer une fiche action
-          </button>
-        </div>
-      ) : (
+      {data.length !== 0 && (
         <div className="grid lg:grid-cols-2 gap-6">
           {data.map(f => (
             <FicheActionCard
