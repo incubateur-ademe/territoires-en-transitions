4037349f14408338520c22093d7f0b46e8aba259
Florian d'Erfurth <florian@derfurth.com>
2023-10-27 16:13:27 +0200

    Renvoie des listes vides si aucune donnée n'est présente.

--- data_layer/sqitch/deploy/site/labellisation.sql
+++ data_layer/sqitch/deploy/site/labellisation.sql
@@ -8,11 +8,15 @@ create function
     rows 1
 begin
     atomic
-    select array_agg(l) from labellisation l where collectivite_id = $1.collectivite_id;
+    select coalesce(
+                   (select array_agg(l)
+                    from labellisation l
+                    where collectivite_id = $1.collectivite_id),
+                   '{}':: labellisation[]
+           );
 end;
 comment on function labellisations(site_labellisation) is
-    'Permet de lier une collectivité à ses données de labellisation historique.';
-
+    'Données de labellisation historique.';
 
 create function
     indicateurs_gaz_effet_serre(site_labellisation)
@@ -20,13 +24,17 @@ create function
     rows 1
 begin
     atomic
-    select array_agg(iri)
-    from indicateur_resultat_import iri
-    where iri.collectivite_id = $1.collectivite_id
-      and iri.indicateur_id in
-          ('cae_1.g', 'cae_1.f', 'cae_1.h', 'cae_1.j', 'cae_1.i', 'cae_1.c', 'cae_1.e', 'cae_1.d', 'cae_1.a');
+    select coalesce(
+                   (select array_agg(iri)
+                    from indicateur_resultat_import iri
+                    where iri.collectivite_id = $1.collectivite_id
+                      and iri.indicateur_id in
+                          ('cae_1.g', 'cae_1.f', 'cae_1.h', 'cae_1.j', 'cae_1.i', 'cae_1.c', 'cae_1.e', 'cae_1.d',
+                           'cae_1.a')),
+                   '{}':: indicateur_resultat_import[]
+           );
 end;
 comment on function indicateurs_gaz_effet_serre(site_labellisation) is
-    'Permet de lier une collectivité à ses indicateurs gaz à effet de serre.';
+    'Indicateurs gaz à effet de serre.';
 
 COMMIT;
