3718e35a09d3eb124c55e56092156b55ec2864cc
Florian d'Erfurth <florian@derfurth.com>
2023-10-27 15:37:02 +0200

    Ajoute des relations calculées pour les données des pages collectivités

--- data_layer/sqitch/deploy/site/labellisation.sql
+++ data_layer/sqitch/deploy/site/labellisation.sql
@@ -2,80 +2,31 @@
 
 BEGIN;
 
-drop materialized view stats_derniere_labellisation;
-select cron.unschedule('refresh_stats_views_utilisation');
+create function
+    labellisations(site_labellisation)
+    returns setof labellisation[]
+    rows 1
+begin
+    atomic
+    select array_agg(l) from labellisation l where collectivite_id = $1.collectivite_id;
+end;
+comment on function labellisations(site_labellisation) is
+    'Permet de lier une collectivité à ses données de labellisation historique.';
 
-create materialized view site_labellisation
-as
-with dl as (select collectivite_id,
-                   referentiel,
-                   max(obtenue_le) as obtenue_le
-            from labellisation l
-            group by collectivite_id, referentiel)
-select c.collectivite_id,
-       c.nom,
-       c.type_collectivite,
-       c.nature_collectivite,
-       c.code_siren_insee,
-       c.region_name,
-       c.region_code,
-       c.departement_name,
-       c.departement_code,
-       c.population_totale,
 
-       ca is not null                as active,
-
-       coalesce(cot.actif, false)    as cot,
-
-       dl_cae.obtenue_le is not null or
-       dl_eci.obtenue_le is not null or
-       coalesce(cot.actif, false)    as engagee,
-
-       dl_cae.obtenue_le is not null or
-       dl_eci.obtenue_le is not null as labellisee,
-
-       dl_cae.obtenue_le             as cae_obtenue_le,
-       dl_cae.etoiles                as cae_etoiles,
-       dl_cae.score_realise          as cae_score_realise,
-       dl_cae.score_programme        as cae_score_programme,
-       dl_eci.obtenue_le             as eci_obtenue_le,
-       dl_eci.etoiles                as eci_etoiles,
-       dl_eci.score_realise          as eci_score_realise,
-       dl_eci.score_programme        as eci_score_programme
-
-from stats.collectivite c
-         left join stats.collectivite_active ca using (collectivite_id)
-         left join cot using (collectivite_id)
-         left join lateral (select l.*
-                            from dl
-                                     join labellisation l
-                                          on l.collectivite_id = dl.collectivite_id
-                                              and l.referentiel = dl.referentiel
-                                              and l.obtenue_le = dl.obtenue_le
-                            where dl.collectivite_id = c.collectivite_id
-                              and dl.referentiel = 'cae') dl_cae on true
-         left join lateral (select l.*
-                            from dl
-                                     join labellisation l
-                                          on l.collectivite_id = dl.collectivite_id
-                                              and l.referentiel = dl.referentiel
-                                              and l.obtenue_le = dl.obtenue_le
-                            where dl.collectivite_id = c.collectivite_id
-                              and dl.referentiel = 'eci') dl_eci on true;
-
-comment on materialized view site_labellisation is
-    'Les dernières info de labellisation affichées sur notre site.';
-
--- full text search
-create index site_labellisation_nom_idx on site_labellisation
-    using gin (to_tsvector('french', nom));
-
--- select *
--- from site_labellisation
--- where to_tsvector('french', nom) @@ to_tsquery('french', 'nantes & métro:*');
-
-select cron.schedule('site_labellisation',
-                     '0 1 * * *', -- tout les jours à 1h.
-                     $$refresh materialized view site_labellisation;$$);
+create function
+    indicateurs_gaz_effet_serre(site_labellisation)
+    returns setof indicateur_resultat_import[]
+    rows 1
+begin
+    atomic
+    select array_agg(iri)
+    from indicateur_resultat_import iri
+    where iri.collectivite_id = $1.collectivite_id
+      and iri.indicateur_id in
+          ('cae_1.g', 'cae_1.f', 'cae_1.h', 'cae_1.j', 'cae_1.i', 'cae_1.c', 'cae_1.e', 'cae_1.d', 'cae_1.a');
+end;
+comment on function indicateurs_gaz_effet_serre(site_labellisation) is
+    'Permet de lier une collectivité à ses indicateurs gaz à effet de serre.';
 
 COMMIT;
