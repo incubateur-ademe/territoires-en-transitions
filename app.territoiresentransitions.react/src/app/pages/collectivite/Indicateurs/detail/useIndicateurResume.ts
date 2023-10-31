85adf61a7177ab91808cee60bafa54ecde5b7c7d
Marc Rutkowski <marc@attractive-media.fr>
2023-10-31 17:33:44 +0100

    Utilise la nouvelle vue pour lire les informations complémentaires (pilotes, etc.) associées à un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/useIndicateurResume.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/useIndicateurResume.ts
@@ -1,11 +1,12 @@
 import {useQuery} from 'react-query';
 import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
-import {Tables, Views} from 'types/alias';
+import {Tables} from 'types/alias';
 
-type TFetchedData = Pick<Views<'indicateur_resume'>, 'pilotes'> & {
-  services: Tables<'service_tag'>[] | null;
-  thematiques: Tables<'thematique'>[] | null;
+type TFetchedData = {
+  pilotes: Tables<'personne_tag'>[];
+  services: Tables<'service_tag'>[];
+  thematiques: Tables<'thematique'>[];
 };
 
 /** Charge les informations complémentaires (pilotes, etc.) associées à un indicateur */
@@ -21,12 +22,12 @@ export const useIndicateurResume = ({
     if (collectivite_id === undefined || id === undefined) return;
 
     const query = supabaseClient
-      .from('indicateur_resume')
+      .from('indicateur_definitions')
       .select('pilotes,services,thematiques')
       .match(
         isPerso
-          ? {collectivite_id, indicateur_personnalise: id}
-          : {collectivite_id, indicateur_referentiel: id}
+          ? {collectivite_id, indicateur_perso_id: id}
+          : {collectivite_id, indicateur_id: id}
       )
       .returns<TFetchedData[]>();
 
