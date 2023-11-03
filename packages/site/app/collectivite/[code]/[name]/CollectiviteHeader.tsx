4574905ac940ce91e6944f6a532017757b788c3d
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-11-03 21:17:04 +0100

    Mise à jour de l'en-tête de la collectivité

--- packages/site/app/collectivite/[code]/[name]/CollectiviteHeader.tsx
+++ packages/site/app/collectivite/[code]/[name]/CollectiviteHeader.tsx
@@ -8,7 +8,9 @@ import {getFormattedNumber} from 'src/utils/getFormattedNumber';
 type CollectiviteHeaderProps = {
   nom: string;
   region?: string;
+  regionCode?: string;
   departement?: string;
+  departementCode?: string;
   type?: string;
   population?: number;
   url?: string;
@@ -19,7 +21,9 @@ type CollectiviteHeaderProps = {
 const CollectiviteHeader = ({
   nom,
   region,
+  regionCode,
   departement,
+  departementCode,
   type,
   population,
   url,
@@ -37,12 +41,21 @@ const CollectiviteHeader = ({
             displayCaption={false}
           />
         ) : (
-          <Image
-            src="/collectivites/placeholder.jpg"
-            alt=""
-            fill={true}
-            style={{objectFit: 'cover', minHeight: '100%', minWidth: '100%'}}
-          />
+          <div className="bg-primary-3 h-full w-full relative">
+            <div className="h-full w-full absolute top-0 left-0 flex flex-col justify-center items-center">
+              <p className="text-grey-9 font-bold text-[20px] px-6 text-center">
+                Envoyez-nous la photo de votre collectivité à
+                contact@territoiresentransitions.fr
+              </p>
+            </div>
+            <Image
+              src="/collectivites/placeholder.jpg"
+              alt=""
+              fill={true}
+              style={{objectFit: 'cover', minHeight: '100%', minWidth: '100%'}}
+              className="hover:opacity-10 transition-opacity duration-500"
+            />
+          </div>
         )}
       </div>
 
@@ -53,16 +66,26 @@ const CollectiviteHeader = ({
           <h1 className="text-grey-1 text-[25px] lg:text-[46px] leading-[30px] lg:leading-[48px] mb-3">
             {nom}
           </h1>
+
           {/* Région et département */}
           <p className="text-white font-bold text-[16px] lg:text-[18px] leading-[18px] lg:leading-[22px]">
-            {region && departement
-              ? `${region} / ${departement}`
-              : region
-              ? region
-              : departement
-              ? departement
-              : ''}
+            {region && departement ? (
+              <>
+                <a href={`/stats/region/${regionCode}`}>{region}</a>
+                {' / '}
+                <a href={`/stats/department/${departementCode}`}>
+                  {departement}
+                </a>
+              </>
+            ) : region ? (
+              <a href={`/stats/region/${regionCode}`}>{region}</a>
+            ) : departement ? (
+              <a href={`/stats/department/${departementCode}`}>{departement}</a>
+            ) : (
+              ''
+            )}
           </p>
+
           {/* Badges avec nombre d'habitants et type de collectivité */}
           <div className={classNames('flex gap-2 flex-wrap', {'mb-6': !!url})}>
             {population && (
@@ -76,6 +99,7 @@ const CollectiviteHeader = ({
               <Badge content={type} status="info" className="text-primary-7" />
             )}
           </div>
+
           {/* URL */}
           {url && (
             <a
@@ -92,7 +116,7 @@ const CollectiviteHeader = ({
         {/* Logo de la collectivité */}
         {logo && (
           <div
-            className="bg-white p-3 md:p-4 absolute md:static right-8 top-0 -translate-y-3/4 md:translate-y-0"
+            className="absolute md:static right-8 top-0 -translate-y-3/4 md:translate-y-0"
             style={{boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.3)'}}
           >
             <StrapiImage
