c76a18847e14a99f2d1d025fec8afa5b114a6ccb
Florian d'Erfurth <florian@derfurth.com>
2023-11-06 14:38:27 +0100

    Ajoute le verify

--- data_layer/sqitch/verify/indicateur/detail.sql
+++ data_layer/sqitch/verify/indicateur/detail.sql
@@ -2,6 +2,14 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+select collectivite_id, indicateur_id, indicateur_perso_id, nom, description, unite
+from indicateur_definitions
+where false;
+
+select has_function_privilege('services(indicateur_definitions)', 'execute');
+select has_function_privilege('pilotes(indicateur_definitions)', 'execute');
+select has_function_privilege('thematiques(indicateur_definitions)', 'execute');
+select has_function_privilege('private.get_personne(indicateur_pilote)', 'execute');
+select has_function_privilege('private.get_personne(indicateur_personnalise_pilote)', 'execute');
 
 ROLLBACK;
