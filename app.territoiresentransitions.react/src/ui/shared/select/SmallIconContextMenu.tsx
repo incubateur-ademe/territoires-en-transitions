c77a5b472e428e1100ca62a787b63765787f65b4
Marc Rutkowski <marc@attractive-media.fr>
2023-11-08 12:44:38 +0100

    Permet la réutilisation des menus contextuels

--- app.territoiresentransitions.react/src/ui/shared/select/SmallIconContextMenu.tsx
+++ app.territoiresentransitions.react/src/ui/shared/select/SmallIconContextMenu.tsx
@@ -1,8 +1,7 @@
-import {forwardRef, Ref} from 'react';
-import IconThreeDotHorizontal from 'ui/icons/IconThreeDotHorizontal';
-import DropdownFloater from 'ui/shared/floating-ui/DropdownFloater';
-import {TOption} from './commons';
 import classNames from 'classnames';
+import IconThreeDotHorizontal from 'ui/icons/IconThreeDotHorizontal';
+import {TOption} from './commons';
+import ContextMenu from './ContextMenu';
 
 type TOptionWithIcon = TOption & {icon?: string};
 
@@ -35,77 +34,22 @@ const SmallIconContextMenu = ({
   onSelect: (value: string) => void;
 }) => {
   return (
-    <DropdownFloater
-      placement="bottom-end"
-      render={({close}) => (
-        <nav data-test={dataTest}>
-          <ul className="m-0 p-0">
-            {options.map(({value, label, icon}) => (
-              <li
-                key={value}
-                className="fr-nav__item pb-0 border-b border-gray-200"
-              >
-                <button
-                  className="fr-nav__link !flex !items-center !text-bf500 !py-2 !px-3 before:!hidden !shadow-none"
-                  onClick={() => {
-                    onSelect(value);
-                    close();
-                  }}
-                >
-                  {icon && (
-                    <span className={classNames('scale-75 mr-1', icon)} />
-                  )}
-                  <span>{label}</span>
-                </button>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
-    >
-      <SmallIconButton
-        dataTest={dataTestButton}
-        className={buttonClassname}
+    <ContextMenu dataTest={dataTest} options={options} onSelect={onSelect}>
+      <button
+        data-test={dataTestButton}
+        className={classNames(
+          'border border-solid border-gray-200 p-1 text-bf500 h-8 w-8',
+          buttonClassname
+        )}
         title={title}
-        hideDefaultIcon={hideDefaultIcon}
         disabled={disabled}
-      />
-    </DropdownFloater>
+      >
+        {!hideDefaultIcon && (
+          <IconThreeDotHorizontal className="w-5 h-5 fill-bf500" />
+        )}
+      </button>
+    </ContextMenu>
   );
 };
 
 export default SmallIconContextMenu;
-
-const SmallIconButton = forwardRef(
-  (
-    {
-      isOpen,
-      dataTest,
-      className,
-      hideDefaultIcon,
-      ...props
-    }: {
-      isOpen?: boolean;
-      dataTest?: string;
-      className?: string;
-      title?: string;
-      disabled?: boolean;
-      hideDefaultIcon?: boolean;
-    },
-    ref?: Ref<HTMLButtonElement>
-  ) => (
-    <button
-      ref={ref}
-      data-test={dataTest}
-      className={classNames(
-        'border border-solid border-gray-200 p-1 text-bf500 h-8 w-8',
-        className
-      )}
-      {...props}
-    >
-      {!hideDefaultIcon && (
-        <IconThreeDotHorizontal className="w-5 h-5 fill-bf500" />
-      )}
-    </button>
-  )
-);
