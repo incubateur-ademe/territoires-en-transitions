f8951678bf2dfb98d205b753f661aa9d7cf7a48d
Marc Rutkowski <marc@attractive-media.fr>
2023-11-09 16:23:18 +0100

    Ajoute la configuration tailwind

---
+++ packages/ui/src/css/index.ts
@@ -0,0 +1,8 @@
+// utilitaires tailwind
+import './tailwind.css';
+
+// feuille de styles globale du dsfr
+import '@gouvfr/dsfr/dist/dsfr.css';
+
+// jeux d'icônes du dsfr
+// import '@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.css';
