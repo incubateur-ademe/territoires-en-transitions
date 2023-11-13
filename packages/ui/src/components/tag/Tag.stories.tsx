22e057bb080ee6d0352c8075a1ae3e4552ea97cf
Marc Rutkowski <marc@attractive-media.fr>
2023-11-13 09:40:44 +0100

    Ajoute le composant `Tag` dans le package `ui`

---
+++ packages/ui/src/components/tag/Tag.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Tag } from './Tag';
+
+export default {
+  component: Tag,
+};
+
+export const Default = () => (
+  <div className="flex">
+    <Tag title="Yolo" />
+  </div>
+);
+
+export const AvecBoutonFermer = () => (
+  <div className="flex">
+    <Tag title="Yolo" onClose={action('onClose')} />
+  </div>
+);
+
+export const CreeParUtilisateur = () => (
+  <div className="flex">
+    <Tag title="Yolo" isUserCreated onClose={action('onClose')} />
+  </div>
+);
+
+export const StylePersonnalise = () => (
+  <div className="flex">
+    <Tag title="Yolo" className="bg-error-1 font-bold" />
+  </div>
+);
