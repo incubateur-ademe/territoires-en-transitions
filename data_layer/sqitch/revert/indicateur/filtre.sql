2d1edf6759f1b483d871f5562bd44e41522488d1
Marc Rutkowski <marc@attractive-media.fr>
2023-11-14 15:37:51 +0100

    Ajoute une fonction pour obtenir les définitions des indicateurs enfants d'un indicateur composé

--- data_layer/sqitch/revert/indicateur/filtre.sql
+++ data_layer/sqitch/revert/indicateur/filtre.sql
@@ -8,6 +8,7 @@ drop function pilote(indicateur_definitions);
 drop function service(indicateur_definitions);
 drop function definition_referentiel(indicateur_definitions);
 drop function rempli(indicateur_definitions);
+drop function enfants(indicateur_definitions);
 drop function personne(indicateur_pilote);
 
 COMMIT;
