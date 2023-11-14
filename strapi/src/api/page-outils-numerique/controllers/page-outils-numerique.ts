01c37e5277c3fce5a246c4b5faec66512e781073
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-11-14 11:27:43 +0100

    Création de la page outil numérique dans Strapi

---
+++ strapi/src/api/page-outils-numerique/controllers/page-outils-numerique.ts
@@ -0,0 +1,7 @@
+/**
+ * page-outils-numerique controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::page-outils-numerique.page-outils-numerique');
---
+++ strapi/src/api/page-outils-numerique/routes/page-outils-numerique.ts
@@ -0,0 +1,7 @@
+/**
+ * page-outils-numerique router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-outils-numerique.page-outils-numerique');
