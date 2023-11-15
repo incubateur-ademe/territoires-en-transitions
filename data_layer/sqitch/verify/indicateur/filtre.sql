273c06c7740bdf380f8928546652b5c300e69846
Marc Rutkowski <marc@attractive-media.fr>
2023-11-15 12:28:08 +0100

    Complète le verify

--- data_layer/sqitch/verify/indicateur/filtre.sql
+++ data_layer/sqitch/verify/indicateur/filtre.sql
@@ -3,5 +3,13 @@
 BEGIN;
 
 select has_function_privilege('thematiques(indicateur_definitions)', 'execute');
+select has_function_privilege('thematiques(indicateur_definitions)', 'execute');
+select has_function_privilege('axes(indicateur_definitions)', 'execute');
+select has_function_privilege('pilotes(indicateur_definitions)', 'execute');
+select has_function_privilege('services(indicateur_definitions)', 'execute');
+select has_function_privilege('definition_referentiel(indicateur_definitions)', 'execute');
+select has_function_privilege('rempli(indicateur_definitions)', 'execute');
+select has_function_privilege('enfants(indicateur_definitions)', 'execute');
+select has_function_privilege('personne(indicateur_pilote)', 'execute');
 
 ROLLBACK;
