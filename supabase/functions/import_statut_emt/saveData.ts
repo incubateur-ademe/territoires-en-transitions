1487ad5081e9236c7f8d735f75a73df7e03e9305
Marc Rutkowski <marc@attractive-media.fr>
2023-11-15 14:50:02 +0100

    Répare quelques erreurs de typage

--- supabase/functions/import_statut_emt/saveData.ts
+++ supabase/functions/import_statut_emt/saveData.ts
@@ -10,34 +10,33 @@ import {TablesInsert} from "../_shared/typeUtils.ts";
  * @param commentaires commentaires déjà existant, pour garder l'ancien commentaire
  * @param tetId identifiant de l'équipe TeT pour le modified_by qui ne peut pas être null
  */
-export const commentaire = async(
-    supabaseClient : TSupabaseClient,
-    collectivite_id : number,
-    action_id : string,
-    commentaire : string,
-    commentaires : Map<string, TablesInsert<"action_commentaire">>,
+export const commentaire = async (
+    supabaseClient: TSupabaseClient,
+    collectivite_id: number,
+    action_id: string,
+    commentaire: string,
+    commentaires: Map<string, TablesInsert<"action_commentaire">>,
     tetId: string
-):
-    Promise<boolean> => {
-    let commentaireToSave : TablesInsert<"action_commentaire"> = commentaires.get(action_id);
-    if(commentaireToSave) {
-        commentaireToSave.commentaire = commentaire +'\n---\n' +commentaireToSave.commentaire;
-    }else{
+): Promise<boolean> => {
+    let commentaireToSave = commentaires.get(action_id);
+    if (commentaireToSave) {
+        commentaireToSave.commentaire = commentaire + "\n---\n" + commentaireToSave.commentaire;
+    } else {
         commentaireToSave = {
-            action_id: action_id,
-            collectivite_id: collectivite_id,
-            commentaire: commentaire
-        }
+            action_id,
+            collectivite_id,
+            commentaire,
+        };
     }
     commentaireToSave.modified_by = tetId;
-    commentaireToSave.commentaire = 'Import EMT: ' + commentaireToSave.commentaire;
+    commentaireToSave.commentaire = "Import EMT: " + commentaireToSave.commentaire;
 
-    const { error, data } = await supabaseClient.from('action_commentaire').upsert(commentaireToSave);
+    const { error } = await supabaseClient.from("action_commentaire").upsert(commentaireToSave);
     if (error) {
         throw new Error(error.message);
     }
     return true;
-}
+};
 
 /**
  * Sauvegarde un statut
@@ -48,31 +47,30 @@ export const commentaire = async(
  * @param statuts statuts déjà existant, pour garder la valeur dans "concerne"
  * @param tetId identifiant de l'équipe TeT pour le modified_by qui ne peut pas être null
  */
-export const statut = async(
-    supabaseClient : TSupabaseClient,
-    collectivite_id : number,
-    action_id : string,
-    statut : string,
-    statuts : Map<string, TablesInsert<"action_statut">>,
-    tetId : string
-):
-    Promise<boolean> => {
-    let statutToSave : TablesInsert<"action_statut"> = statuts.get(action_id);
+export const statut = async (
+    supabaseClient: TSupabaseClient,
+    collectivite_id: number,
+    action_id: string,
+    statut: string,
+    statuts: Map<string, TablesInsert<"action_statut">>,
+    tetId: string
+) => {
     // On n'écrase pas les statuts déjà renseignés.
-    if(!statutToSave) {
-        statutToSave = {
-            action_id: action_id,
-            collectivite_id: collectivite_id,
+    if (!statuts.get(action_id)) {
+        const statutToSave = {
+            action_id,
+            collectivite_id,
             avancement: statut,
-            concerne: true
-        }
+            concerne: true,
+        } as TablesInsert<"action_statut">;
+
         // Erreur "msg":"InvalidWorkerCreation: worker boot error" si ajouté dans l'instruction précédente
         statutToSave.modified_by = tetId;
 
-        const { error, data } = await supabaseClient.from('action_statut').upsert(statutToSave);
+        const { error } = await supabaseClient.from("action_statut").upsert(statutToSave);
         if (error) {
             throw new Error(error.message);
         }
     }
     return true;
-}
+};
