7c1b00f323462028a8f7608edb3baba5f5f9f4d4
Marine Heckler <marine.heckler@beta.gouv.fr>
2023-11-21 17:38:12 +0100

    Création de la page budget dans strapi

---
+++ strapi/src/api/page-budget/routes/page-budget.ts
@@ -0,0 +1,7 @@
+/**
+ * page-budget router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-budget.page-budget');
---
+++ strapi/src/api/page-budget/services/page-budget.ts
@@ -0,0 +1,7 @@
+/**
+ * page-budget service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::page-budget.page-budget');
