e385b1f0a62386741ed675411d84a1f8b3662d9f
Marc Rutkowski <marc@attractive-media.fr>
2023-11-22 15:15:26 +0100

    Ajoute les composants pour le filtrage des indicateurs

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/FiltreComplet.tsx
@@ -0,0 +1,28 @@
+import {DSFRbuttonClassname, TOption} from 'ui/shared/select/commons';
+import {ITEM_ALL} from 'ui/shared/filters/commons';
+import AutocompleteInputSelect from 'ui/shared/select/AutocompleteInputSelect';
+
+type Props = {
+  values: string[] | undefined;
+  onSelect: (values: string[]) => void;
+};
+
+const options: TOption[] = [
+  {value: ITEM_ALL, label: 'Tous'},
+  {value: 'oui', label: 'Complété'},
+  {value: 'non', label: 'A compléter'},
+];
+
+const FiltreComplet = ({values, onSelect}: Props) => {
+  return (
+    <AutocompleteInputSelect
+      data-test="complet"
+      buttonClassName={DSFRbuttonClassname}
+      values={values?.includes(ITEM_ALL) ? [] : values}
+      options={options}
+      onSelect={values => onSelect(values)}
+    />
+  );
+};
+
+export default FiltreComplet;
