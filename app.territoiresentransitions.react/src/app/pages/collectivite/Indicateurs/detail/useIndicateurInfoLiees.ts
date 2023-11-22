139c6881ba4d651cc5392af326e2c4c1d6282848
Marc Rutkowski <marc@attractive-media.fr>
2023-11-22 14:54:51 +0100

    Ajoute le chargement des infos liées à un indicateur (pilotes, thématiques, etc.)

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/useIndicateurInfoLiees.ts
@@ -0,0 +1,42 @@
+import {useQuery} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {CompositeTypes, Tables} from 'types/alias';
+
+type TFetchedData = {
+  pilotes: CompositeTypes<'personne'>[];
+  services: Tables<'service_tag'>[];
+  thematiques: Tables<'thematique'>[];
+};
+
+/**
+ * Charge les informations complémentaires (pilotes, etc.) associées à un indicateur.
+ * Cette requête est invalidée lorsque les infos sont éditéées par l'utilisateur.
+ */
+export const useIndicateurInfoLiees = ({
+  id,
+  isPerso,
+}: {
+  id: number | string | undefined;
+  isPerso?: boolean;
+}) => {
+  const collectivite_id = useCollectiviteId();
+  return useQuery(['indicateur_info_liees', collectivite_id, id], async () => {
+    if (collectivite_id === undefined || id === undefined) return;
+
+    const query = supabaseClient
+      .from('indicateur_definitions')
+      .select(
+        'pilotes(tag_id,user_id),services(id:service_tag_id),thematiques(id)'
+      )
+      .match(
+        isPerso
+          ? {collectivite_id, indicateur_perso_id: id}
+          : {collectivite_id, indicateur_id: id}
+      )
+      .returns<TFetchedData[]>();
+
+    const {data} = await query;
+    return data?.[0];
+  });
+};
