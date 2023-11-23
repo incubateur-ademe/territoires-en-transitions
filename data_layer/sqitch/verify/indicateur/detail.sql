f7f9a6fc9560e46f8d66e1d863ec5ad315901cb0
Florian d'Erfurth <florian@derfurth.com>
2023-11-23 10:37:35 +0100

    Corrige les verify

--- data_layer/sqitch/verify/indicateur/detail.sql
+++ data_layer/sqitch/verify/indicateur/detail.sql
@@ -6,10 +6,4 @@ select collectivite_id, indicateur_id, indicateur_perso_id, nom, description, un
 from indicateur_definitions
 where false;
 
-select has_function_privilege('services(indicateur_definitions)', 'execute');
-select has_function_privilege('pilotes(indicateur_definitions)', 'execute');
-select has_function_privilege('thematiques(indicateur_definitions)', 'execute');
-select has_function_privilege('private.get_personne(indicateur_pilote)', 'execute');
-select has_function_privilege('private.get_personne(indicateur_personnalise_pilote)', 'execute');
-
 ROLLBACK;
