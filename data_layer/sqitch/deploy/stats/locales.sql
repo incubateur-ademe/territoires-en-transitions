d789fff161e66de63ec7288790cc8bdc9fcdbcee
Florian d'Erfurth <florian@derfurth.com>
2023-11-23 18:19:23 +0100

    Enlève les screaming caps.

--- data_layer/sqitch/deploy/stats/locales.sql
+++ data_layer/sqitch/deploy/stats/locales.sql
@@ -7,36 +7,36 @@ drop view stats_locales_evolution_collectivite_avec_minimum_fiches;
 drop materialized view stats.locales_evolution_collectivite_avec_minimum_fiches;
 
 create materialized view stats.locales_evolution_collectivite_avec_minimum_fiches as
-WITH fiche_collectivite AS (SELECT mb.first_day                                                               AS mois,
+with fiche_collectivite as (select mb.first_day                                                               as mois,
                                    c.collectivite_id,
                                    c.region_code,
                                    c.departement_code,
-                                   COALESCE(count(*) FILTER (WHERE fa.created_at <= mb.last_day), 0::bigint) AS fiches
-                            FROM stats.monthly_bucket mb
-                                     JOIN stats.collectivite c ON true
-                                     LEFT JOIN fiche_action fa USING (collectivite_id)
-                            GROUP BY mb.first_day, c.collectivite_id, c.departement_code, c.region_code)
-SELECT fiche_collectivite.mois,
-       NULL::character varying(2)                            AS code_region,
-       NULL::character varying(2)                            AS code_departement,
-       count(*) FILTER (WHERE fiche_collectivite.fiches > 5) AS collectivites
-FROM fiche_collectivite
-GROUP BY fiche_collectivite.mois
-UNION ALL
-SELECT fiche_collectivite.mois,
-       fiche_collectivite.region_code                        AS code_region,
-       NULL::character varying                               AS code_departement,
-       count(*) FILTER (WHERE fiche_collectivite.fiches > 5) AS collectivites
-FROM fiche_collectivite
-GROUP BY fiche_collectivite.mois, fiche_collectivite.region_code
-UNION ALL
-SELECT fiche_collectivite.mois,
-       NULL::character varying                               AS code_region,
-       fiche_collectivite.departement_code                   AS code_departement,
-       count(*) FILTER (WHERE fiche_collectivite.fiches > 5) AS collectivites
-FROM fiche_collectivite
-GROUP BY fiche_collectivite.mois, fiche_collectivite.departement_code
-ORDER BY 1;
+                                   coalesce(count(*) filter (where fa.created_at <= mb.last_day), 0::bigint) as fiches
+                            from stats.monthly_bucket mb
+                                     join stats.collectivite c on true
+                                     left join fiche_action fa using (collectivite_id)
+                            group by mb.first_day, c.collectivite_id, c.departement_code, c.region_code)
+select fiche_collectivite.mois,
+       null::character varying(2)                            as code_region,
+       null::character varying(2)                            as code_departement,
+       count(*) filter (where fiche_collectivite.fiches > 5) as collectivites
+from fiche_collectivite
+group by fiche_collectivite.mois
+union all
+select fiche_collectivite.mois,
+       fiche_collectivite.region_code                        as code_region,
+       null::character varying                               as code_departement,
+       count(*) filter (where fiche_collectivite.fiches > 5) as collectivites
+from fiche_collectivite
+group by fiche_collectivite.mois, fiche_collectivite.region_code
+union all
+select fiche_collectivite.mois,
+       null::character varying                               as code_region,
+       fiche_collectivite.departement_code                   as code_departement,
+       count(*) filter (where fiche_collectivite.fiches > 5) as collectivites
+from fiche_collectivite
+group by fiche_collectivite.mois, fiche_collectivite.departement_code
+order by 1;
 
 create view stats_locales_evolution_collectivite_avec_minimum_fiches
 as
@@ -49,33 +49,33 @@ drop view stats_locales_evolution_nombre_fiches;
 drop materialized view stats.locales_evolution_nombre_fiches;
 
 create materialized view stats.locales_evolution_nombre_fiches as
-SELECT mb.first_day                                          AS mois,
-       NULL::character varying(2)                            AS code_region,
-       NULL::character varying(2)                            AS code_departement,
-       count(*) FILTER (WHERE fa.created_at <= mb.last_day) AS fiches
-FROM stats.monthly_bucket mb
-         JOIN stats.collectivite ca ON true
-         JOIN fiche_action fa USING (collectivite_id)
-GROUP BY mb.first_day
-UNION ALL
-SELECT mb.first_day                                          AS mois,
-       ca.region_code                                        AS code_region,
-       NULL::character varying                               AS code_departement,
-       count(*) FILTER (WHERE fa.created_at <= mb.last_day) AS fiches
-FROM stats.monthly_bucket mb
-         JOIN stats.collectivite ca ON true
-         LEFT JOIN fiche_action fa USING (collectivite_id)
-GROUP BY mb.first_day, ca.region_code
-UNION ALL
-SELECT mb.first_day                                          AS mois,
-       NULL::character varying                               AS code_region,
-       ca.departement_code                                   AS code_departement,
-       count(*) FILTER (WHERE fa.created_at <= mb.last_day) AS fiches
-FROM stats.monthly_bucket mb
-         JOIN stats.collectivite ca ON true
-         LEFT JOIN fiche_action fa USING (collectivite_id)
-GROUP BY mb.first_day, ca.departement_code
-ORDER BY 1;
+select mb.first_day                                          as mois,
+       null::character varying(2)                            as code_region,
+       null::character varying(2)                            as code_departement,
+       count(*) filter (where fa.created_at <= mb.last_day) as fiches
+from stats.monthly_bucket mb
+         join stats.collectivite ca on true
+         join fiche_action fa using (collectivite_id)
+group by mb.first_day
+union all
+select mb.first_day                                          as mois,
+       ca.region_code                                        as code_region,
+       null::character varying                               as code_departement,
+       count(*) filter (where fa.created_at <= mb.last_day) as fiches
+from stats.monthly_bucket mb
+         join stats.collectivite ca on true
+         left join fiche_action fa using (collectivite_id)
+group by mb.first_day, ca.region_code
+union all
+select mb.first_day                                          as mois,
+       null::character varying                               as code_region,
+       ca.departement_code                                   as code_departement,
+       count(*) filter (where fa.created_at <= mb.last_day) as fiches
+from stats.monthly_bucket mb
+         join stats.collectivite ca on true
+         left join fiche_action fa using (collectivite_id)
+group by mb.first_day, ca.departement_code
+order by 1;
 
 create view stats_locales_evolution_nombre_fiches
 as
