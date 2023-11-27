43d28bd7ed499c3e0dfa43f0f60a943391a40435
Marc Rutkowski <marc@attractive-media.fr>
2023-11-27 14:48:46 +0100

    Utilise les nouveaux styles pour les infobulles associées au picto "info"

--- app.territoiresentransitions.react/src/ui/shared/floating-ui/InfoTooltip.tsx
+++ app.territoiresentransitions.react/src/ui/shared/floating-ui/InfoTooltip.tsx
@@ -1,15 +1,17 @@
 import {ReactElement} from 'react';
-import {Tooltip} from './Tooltip';
+import DSTetTooltip from './DSTetTooltip';
 
 /** Affiche un picto (i) permettant d'ouvrir une infobulle au clic */
 export const InfoTooltip = ({
   label,
+  className,
 }: {
   label: string | (() => ReactElement);
+  className?: string;
 }) => (
-  <span onClick={evt => evt.stopPropagation()}>
-    <Tooltip label={label} activatedBy="click">
+  <span onClick={evt => evt.stopPropagation()} className={className}>
+    <DSTetTooltip label={label} activatedBy="click">
       <span className="fr-fi-information-line pl-2 text-bf500 cursor-pointer" />
-    </Tooltip>
+    </DSTetTooltip>
   </span>
 );
