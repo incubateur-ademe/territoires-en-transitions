e8e8669e3b40014dadef8f7bf2b474a11741a3a1
Marc Rutkowski <marc@attractive-media.fr>
2023-11-27 15:38:22 +0100

    Restaure le bouton "Créer un indicateur personnalisé" sur la page des indicateurs perso quand il n'y en a encore aucun

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/FiltersAndGrid.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/FiltersAndGrid.tsx
@@ -1,31 +1,61 @@
+import {Link} from 'react-router-dom';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {useExportIndicateurs} from '../useExportIndicateurs';
 import {ToolbarIconButton} from 'ui/buttons/ToolbarIconButton';
 import IndicateurChartsGrid from './IndicateurChartsGrid';
-import {useExportIndicateurs} from '../useExportIndicateurs';
 import {FilterSummary} from './FilterSummary';
-import {IndicateurViewParamOption} from 'app/paths';
+import {
+  IndicateurViewParamOption,
+  makeCollectiviteIndicateursUrl,
+} from 'app/paths';
 import {FiltresIndicateurs} from './FiltresIndicateurs';
 import {useFilteredIndicateurDefinitions} from './useFilteredIndicateurDefinitions';
 import {useIndicateursFilterState} from './useIndicateursFilterState';
 
 /** Affiche les filtres et la grille d'indicateurs donnés */
 export const FiltersAndGrid = ({view}: {view: IndicateurViewParamOption}) => {
+  const collectivite = useCurrentCollectivite();
   const filterState = useIndicateursFilterState();
   const {filters, resetFilterParams, filterParamsCount} = filterState;
 
   // charge et filtre les définitions
   const {data: definitions} = useFilteredIndicateurDefinitions(view, filters);
 
+  // fonction d'export
   const {mutate: exportIndicateurs, isLoading} = useExportIndicateurs(
     definitions,
     view
   );
+
+  // route non valide
+  if (!collectivite) {
+    return null;
+  }
+
+  // affiche un CTA quand il n'y a encore aucun indicateur perso
+  if (view === 'perso' && definitions?.length === 0 && !collectivite.readonly) {
+    return (
+      <div className="flex flex-col items-center mt-8 gap-8">
+        <p>Aucun indicateur personnalisé</p>
+        <Link
+          className="fr-btn"
+          to={makeCollectiviteIndicateursUrl({
+            collectiviteId: collectivite.collectivite_id,
+            indicateurView: 'perso',
+            indicateurId: 'nouveau',
+          })}
+        >
+          Créer un indicateur
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <>
       {view !== 'cles' && (
         <FiltresIndicateurs view={view} state={filterState} />
       )}
-
       {definitions && (
         <>
           <div className="flex flex-row items-center justify-between fr-mb-3w">
