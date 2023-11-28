a283b2460fa08457e31bced8289d3cc8bc7f517a
Florian d'Erfurth <florian@derfurth.com>
2023-11-28 18:20:20 +0100

    Permet de récupérer les données liées pour les indicateurs sans valeurs

--- data_layer/sqitch/revert/indicateur/filtre.sql
+++ data_layer/sqitch/revert/indicateur/filtre.sql
@@ -7,7 +7,6 @@ drop function axes(indicateur_definitions);
 drop function fiches_non_classees(indicateur_definitions);
 drop function pilotes(indicateur_definitions);
 drop function services(indicateur_definitions);
-drop function definition_referentiel(indicateur_definitions);
 drop function definition_perso(indicateur_definitions);
 drop function rempli(indicateur_definitions);
 drop function private.rempli(integer, indicateur_id);
@@ -16,5 +15,6 @@ drop function enfants(indicateur_definitions);
 drop function enfants(indicateur_definition);
 drop function personne(indicateur_pilote);
 drop function indicateur_action(indicateur_definitions);
+drop function definition_referentiel(indicateur_definitions);
 
 COMMIT;
