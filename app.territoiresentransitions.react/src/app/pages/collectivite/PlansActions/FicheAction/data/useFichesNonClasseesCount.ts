e793df28a416ff5f1c48e23cce86a81aa664a4b8
Marc Rutkowski <marc@attractive-media.fr>
2023-11-29 14:43:42 +0100

    N'affiche pas l'option de filtrage "fiches non classées" lorsqu'il n'y en a pas

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useFichesNonClasseesCount.ts
@@ -0,0 +1,17 @@
+import {useQuery} from 'react-query';
+import {supabaseClient} from 'core-logic/api/supabase';
+
+/** Renvoi le nombre de fiches action non classées */
+export const useFichesNonClasseesCount = (collectivite_id: number | null) => {
+  return useQuery(['axe_fiches_count', collectivite_id, null], async () => {
+    if (!collectivite_id) return 0;
+
+    const {count} = await supabaseClient
+      .from('fiche_resume')
+      .select('*', {count: 'exact', head: true})
+      .eq('collectivite_id', collectivite_id)
+      .is('plans', null);
+
+    return count || 0;
+  });
+};
