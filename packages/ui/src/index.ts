aaeb5f78af3ec269a576cfdcd1941c482c5a11a6
Marc Rutkowski <marc@attractive-media.fr>
2023-11-30 14:55:59 +0100

    Exporte la pré-configuration tailwind du module partagé

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
+// exporte les presets tailwind
+export { preset } from './tailwind-preset';
+
 // composants exportés
 export { HotPink } from './HotPink';
 export { Tag } from './components/tag/Tag';
