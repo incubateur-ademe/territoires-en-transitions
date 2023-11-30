31ca92c16b8772cfb19c8a0cd8e8b883587a1db7
Marc Rutkowski <marc@attractive-media.fr>
2023-11-30 13:35:36 +0100

    Change l'ordre de chargement de tailwind dans ui

--- packages/ui/src/css/index.ts
+++ packages/ui/src/css/index.ts
@@ -1,8 +1,8 @@
-// utilitaires tailwind
-import './tailwind.css';
-
 // feuille de styles globale du dsfr
 import '@gouvfr/dsfr/dist/dsfr.css';
+
+// utilitaires tailwind
+import './tailwind.css';
 
 // jeux d'icônes du dsfr
 // import '@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.css';
