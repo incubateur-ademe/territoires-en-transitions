37241c5100f9e1a00259c0ab8ac6035d526aa658
Marc Rutkowski <marc@attractive-media.fr>
2023-12-04 10:22:52 +0100

    Change le tag html utilisé pour le composant badge

--- app.territoiresentransitions.react/src/ui/shared/Badge/index.tsx
+++ app.territoiresentransitions.react/src/ui/shared/Badge/index.tsx
@@ -13,8 +13,8 @@ export type TBadgeProps = {
 export const Badge = (props: TBadgeProps) => {
   const {className, status, children} = props;
   return (
-    <p className={`fr-badge fr-badge--${status} ${className || ''}`}>
+    <span className={`fr-badge fr-badge--${status} ${className || ''}`}>
       {children}
-    </p>
+    </span>
   );
 };
