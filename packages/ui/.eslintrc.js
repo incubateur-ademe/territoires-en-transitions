f1196d6e399ef388d25b9864c2dd0c4307f26f38
Marc Rutkowski <marc@attractive-media.fr>
2023-12-04 16:23:13 +0100

    Ajoute le package `api`

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,25 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'jsx-expressions', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:storybook/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.json',
-  },
-  rules: {
-    'jsx-expressions/strict-logical-expressions': 'warn',
   },
 };
