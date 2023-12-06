02ba36a43fa47a468c25536bb3562edc95e2ca4d
Arthur Molinos <unknown>
2023-12-06 17:50:37 +0100

    Ajoute le type à la création d'un plan

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
@@ -1,41 +1,25 @@
-import {Form, Formik} from 'formik';
-import FormikInput from 'ui/shared/form/formik/FormikInput';
-// import FormikSelect from 'ui/shared/form/formik/FormikSelect';
-import * as Yup from 'yup';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
 import {useCreatePlanAction} from '../PlanAction/data/useUpsertAxe';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {makeCollectivitePlansActionsNouveauUrl} from 'app/paths';
-import {Link} from 'react-router-dom';
-
-// const PlanTypeOptions = [
-//   {
-//     value: 'Plan Climat Air Énergie territorial',
-//     label: 'Plan Climat Air Énergie territorial',
-//   },
-//   {value: 'Plan Économie circulaire', label: 'Plan Économie circulaire'},
-//   {value: 'Plan Territoire Engagé', label: 'Plan Territoire Engagé'},
-//   {value: 'Plan thématique', label: 'Plan thématique'},
-//   {value: 'Projet de territoire', label: 'Projet de territoire'},
-//   {value: 'Autre', label: 'Autre'},
-// ];
-
-const formValidation = Yup.object({
-  nom: Yup.string(),
-  type: Yup.string(),
-});
-
-type FormProps = {nom: string; type: string};
+import FormField from 'ui/shared/form/FormField';
+import PlanTypeDropdown from '../PlanAction/PlanTypeDropdown';
+import {TPlanType} from 'types/alias';
 
 const CreerPlan = () => {
   const collectivite_id = useCollectiviteId();
 
+  const [plan, setPlan] = useState<{nom?: string; type?: TPlanType}>({});
+
   const {mutate: createPlanAction} = useCreatePlanAction();
 
-  const handleFormSubmit = (values: FormProps) => {
+  const handleCreate = () => {
     createPlanAction({
       collectivite_id: collectivite_id!,
-      nom: values.nom,
-      // type: values.type,
+      nom: plan.nom,
+      type: plan.type?.id,
     });
   };
 
@@ -47,42 +31,33 @@ const CreerPlan = () => {
           Créer un plan d’action
         </h3>
         <div className="flex flex-col mt-2 mb-10 py-14 px-24 bg-[#f6f6f6]">
-          {/* À remettre lorsque l'on activera le champ type */}
-          {/* <div className="mb-8 text-gray-500">Aucun champ n’est obligatoire</div> */}
-          <Formik
-            initialValues={{nom: '', type: ''}}
-            validationSchema={formValidation}
-            onSubmit={handleFormSubmit}
+          <FormField
+            label="Nom du plan d’action"
+            hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
           >
-            <Form>
-              <FormikInput
-                data-test="PlanNomInput"
-                name="nom"
-                label="Nom du plan d’action"
-                hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
-              />
-              {/* <FormikSelect
-              name="type"
-              label="Type de plan d’action"
-              hint="Cette information sera utile pour vous proposer une arborescence et un format de fiche adaptés"
-              options={PlanTypeOptions}
-            /> */}
-              <div className="flex items-center justify-end gap-6 mt-12">
-                <Link
-                  className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line hover:!bg-[#EEEEEE]"
-                  to={makeCollectivitePlansActionsNouveauUrl({
-                    collectiviteId: collectivite_id!,
-                  })}
-                >
-                  Revenir à l’étape précédente
-                </Link>
+            <input
+              className="fr-input"
+              onChange={e => setPlan({...plan, nom: e.target.value})}
+            />
+          </FormField>
+          <PlanTypeDropdown
+            type={plan.type?.id}
+            onSelect={type => setPlan({...plan, type})}
+          />
+          <div className="flex items-center justify-end gap-6 mt-4">
+            <Link
+              className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line hover:!bg-[#EEEEEE]"
+              to={makeCollectivitePlansActionsNouveauUrl({
+                collectiviteId: collectivite_id!,
+              })}
+            >
+              Revenir à l’étape précédente
+            </Link>
 
-                <button className="fr-btn !mb-0" data-test="ok">
-                  Valider
-                </button>
-              </div>
-            </Form>
-          </Formik>
+            <button className="fr-btn !mb-0" onClick={handleCreate}>
+              Valider
+            </button>
+          </div>
         </div>
       </div>
     </div>
