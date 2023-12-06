4e0bcb0dd43c2cbbdcd1625d0b71198cd074e463
Marc Rutkowski <marc@attractive-media.fr>
2023-12-06 15:14:10 +0100

    Passe la story du composant Tag au format CSF pour que les contrôles fonctionnent dans le storybook
    Ref: https://storybook.js.org/docs/api/csf

--- packages/ui/src/components/tag/Tag.stories.tsx
+++ packages/ui/src/components/tag/Tag.stories.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Tag } from './Tag';
+import {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {Tag} from './Tag';
 
-export default {
+const meta: Meta<typeof Tag> = {
   component: Tag,
+  decorators: [story => <div className="flex">{story()}</div>],
 };
 
-export const Default = () => (
-  <div className="flex">
-    <Tag title="Yolo" />
-  </div>
-);
+export default meta;
 
-export const AvecBoutonFermer = () => (
-  <div className="flex">
-    <Tag title="Yolo" onClose={action('onClose')} />
-  </div>
-);
+type Story = StoryObj<typeof Tag>;
 
-export const CreeParUtilisateur = () => (
-  <div className="flex">
-    <Tag title="Yolo" isUserCreated onClose={action('onClose')} />
-  </div>
-);
+export const Default: Story = {
+  args: {title: 'Yolo'},
+};
 
-export const StylePersonnalise = () => (
-  <div className="flex">
-    <Tag title="Yolo" className="!bg-error-1 font-bold" />
-  </div>
-);
+export const AvecBoutonFermer: Story = {
+  args: {title: 'Yolo', onClose: action('onClose')},
+};
+
+export const CreeParUtilisateur: Story = {
+  args: {title: 'Yolo', isUserCreated: true, onClose: action('onClose')},
+};
+
+export const StylePersonnalise: Story = {
+  args: {title: 'Yolo', className: '!bg-error-1 font-bold'},
+};
