0998e6ebc0f34d11804cbd8275f6ebb3f5d99eba
Marc Rutkowski <marc@attractive-media.fr>
2023-12-07 18:54:33 +0100

    Ajoute des fonctions utilitaires pour les tests

---
+++ packages/api/src/tests/testReset.ts
@@ -0,0 +1,16 @@
+import retry from 'async-retry';
+import {supabase} from './supabase';
+
+async function _testReset(): Promise<void> {
+  const {status, error} = await supabase.rpc('test_reset');
+
+  if (error) {
+    console.error(error);
+    throw `La RPC 'test_reset' devrait renvoyer un code 20x. (${status})`;
+  }
+}
+
+export const testReset = () =>
+  retry(_testReset, {
+    retries: 5,
+  });
