b623373430df5d4d753c6cb5028a122dbd97e79c
Marc Rutkowski <marc@attractive-media.fr>
2023-12-07 18:54:33 +0100

    Ajoute des fonctions utilitaires pour les tests

---
+++ packages/api/src/tests/auth.ts
@@ -0,0 +1,45 @@
+import {supabase} from './supabase';
+
+type credentials = {
+  email: string;
+  password: string;
+};
+
+/**
+ * Génère l'email et le mot de passe pour les utilisateurs de test.
+ *
+ * @param nickname ex 'yolododo', 'youloudou'
+ */
+export function fakeCredentials(nickname: string): credentials {
+  const match = nickname.toLowerCase().match(/(y.+l.+)(d.+d.+)/)!;
+  const firstName = match[1];
+  const lastName = match[2];
+
+  return {
+    email: `${firstName}@${lastName}.com`,
+    password: `${firstName}${lastName}`,
+  };
+}
+
+/**
+ * Authentifie un utilisateur de test avec le client Supabase.
+ *
+ * @param nickname ex 'yolododo', 'youloudou'
+ */
+export async function signIn(nickname: string) {
+  const credentials = fakeCredentials(nickname);
+  return await supabase.auth.signInWithPassword(credentials);
+}
+
+/**
+ * Déconnecte l'utilisateur du client Supabase.
+ * Permet de libérer les ressources avant la fin des tests.
+ */
+export async function signOut() {
+  const response = await supabase.auth.signOut();
+  // parfois les ressources ne sont pas libérées assez rapidement.
+  await delay(1);
+  return response;
+}
+
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
---
+++ packages/api/src/tests/supabase.ts
@@ -0,0 +1,12 @@
+import {createClient} from '@supabase/supabase-js';
+import {Database} from '../database.types';
+
+export const supabase = createClient<Database>(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+  {
+    db: {
+      schema: 'public',
+    },
+  }
+);
