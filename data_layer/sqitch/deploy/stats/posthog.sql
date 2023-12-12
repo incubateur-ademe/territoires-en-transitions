1018a25f4930c10053c9cf1666774f4774d7c846
Florian d'Erfurth <florian@derfurth.com>
2023-12-12 10:48:40 +0100

    Répare les timestamps

--- data_layer/sqitch/deploy/stats/posthog.sql
+++ data_layer/sqitch/deploy/stats/posthog.sql
@@ -17,9 +17,9 @@ create or replace function
     rows 1
 begin
     atomic
-    select '$pageview'      as event,
-           to_json($1.time) as timestamp,
-           $1.user_id       as distinct_id,
+    select '$pageview'                                as event,
+           trim(both '"' from to_json($1.time)::text) as timestamp,
+           $1.user_id                                 as distinct_id,
            json_build_object(
                    '$current_url',
                    'app/' || (case when $1.collectivite_id is null then '' else 'collectivite/' end) || $1.page,
@@ -33,10 +33,30 @@ begin
                                       and pud.user_id = $1.user_id),
                    '$set', posthog.properties(p),
                    '$groups', json_build_object('collectivite', $1.collectivite_id::text)
-           )                as properties
+           )                                          as properties
     from dcp p
     where p.user_id = $1.user_id;
 end;
-comment on function posthog.event(visite) is 'Un event de type pageview.';
+
+create or replace function
+    posthog.event(dcp)
+    returns table
+            (
+                event       text,
+                "timestamp" text,
+                distinct_id text,
+                properties  jsonb
+            )
+    language sql
+    stable
+    security definer
+    rows 1
+begin
+    atomic
+    select '$identify'                                       as event,
+           trim(both '"' from to_json($1.modified_at)::text) as timestamp,
+           $1.user_id                                        as distinct_id,
+           json_build_object('$set', posthog.properties($1)) as properties;
+end;
 
 COMMIT;
