70471dd2ed05f6ba4b8adffc1118edea8fe7c885
Marc Rutkowski <marc@attractive-media.fr>
2023-12-18 18:32:41 +0100

    Passe la source de données voulue au graphique d'un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/types.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/charts/types.ts
@@ -5,6 +5,8 @@ export type TIndicateurChartProps = {
   definition: TIndicateurListItem;
   /** affichage par défaut (dans la grille) ou zoomé (téléchargeable) */
   variant?: 'default' | 'zoomed';
+  /** nom de la source de données (pour les données importées des indicateurs prédéfinis) */
+  importSource?: string;
   className?: string;
 };
 
