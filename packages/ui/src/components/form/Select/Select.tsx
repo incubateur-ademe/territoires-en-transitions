996c1ce22685c0cddc0cfed09a7b9b33938c61fe
Arthur Molinos <>
2023-12-19 16:17:35 +0100

    Ajoute le possibilité d'effectuer une recherche sur la liste d'option d'un selecteur (ancien AutocompleInputSelect)

--- packages/ui/src/components/form/Select/Select.tsx
+++ packages/ui/src/components/form/Select/Select.tsx
@@ -1,11 +1,11 @@
 import {Placement} from '@floating-ui/react';
-import {Ref, forwardRef} from 'react';
+import {Ref, forwardRef, useEffect, useRef, useState} from 'react';
 import classNames from 'classnames';
 
 import DropdownFloater from '../../floating-ui/DropdownFloater';
 import Options, {OptionValue, SelectOption} from './Options';
 
-import {getOptions} from './utils';
+import {filterOptions, getOptionLabel, getOptions} from './utils';
 import {Tag} from '../../tag/Tag';
 
 type SelectProps<T extends OptionValue> = {
@@ -19,10 +19,14 @@ type SelectProps<T extends OptionValue> = {
   values?: T | T[];
   /** Active la multi sélection */
   multiple?: boolean;
+  /** Permet la recherche dans la liste d'option */
+  hasSearch?: boolean;
   /** Afin de couplet avec un label */
   name?: string;
   /** Texte affiché quand rien n'est sélectionné */
   placeholder?: string;
+  /** Texte affiché quand aucune option ne correspond à la recherche */
+  emptySearchPlaceholder?: string;
   /** Change l'emplacement du dropdown menu */
   placement?: Placement;
   /** Pour que la largeur des options ne dépasse pas la largeur du bouton d'ouverture */
@@ -46,13 +50,21 @@ export const Select = <T extends OptionValue>(props: SelectProps<T>) => {
     options,
     onChange,
     placeholder,
+    emptySearchPlaceholder,
     placement,
     multiple = false,
+    hasSearch = false,
     containerWidthMatchButton = true,
     disabled = false,
   } = props;
 
-  /** Transforme une valeur simple en tableau */
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = (value: string) => {
+    setInputValue(value);
+  };
+
+  /** Transforme une valeur simple en tableau, qui est plus facile à traiter dans les sous composants */
   const arrayValues = values
     ? Array.isArray(values)
       ? values
@@ -65,6 +77,9 @@ export const Select = <T extends OptionValue>(props: SelectProps<T>) => {
       offsetValue={0}
       containerWidthMatchButton={containerWidthMatchButton}
       noDropdownStyles
+      multipleClickToggle={!hasSearch}
+      enterToToggle={!hasSearch}
+      disabled={disabled}
       render={({close}) => (
         <div
           data-test={`${dataTest}-options`}
@@ -73,24 +88,29 @@ export const Select = <T extends OptionValue>(props: SelectProps<T>) => {
           <Options
             dataTest={dataTest}
             values={arrayValues}
-            options={options}
+            options={filterOptions(options, inputValue)}
             onChange={value => {
               onChange(value);
+              setInputValue('');
               if (!multiple) {
                 close();
               }
             }}
+            noOptionPlaceholder={emptySearchPlaceholder}
           />
         </div>
       )}
     >
       <SelectButton
         dataTest={dataTest}
-        values={values}
+        values={arrayValues}
         options={options}
+        onChange={onChange}
+        hasSearch={hasSearch}
+        inputValue={inputValue}
+        onInputChange={onInputChange}
         placeholder={placeholder}
         disabled={disabled}
-        onChange={onChange}
       />
     </DropdownFloater>
   );
@@ -99,6 +119,10 @@ export const Select = <T extends OptionValue>(props: SelectProps<T>) => {
 type SelectButtonProps<T extends OptionValue> = SelectProps<T> & {
   /** Donné par le DropdownFloater */
   isOpen?: boolean;
+  /** Valeur de la saisie */
+  inputValue: string;
+  /** Change la valeur de saisie */
+  onInputChange: (value: string) => void;
 };
 
 /* Création d'un composant séparé pour passer la ref du boutton au floater */
@@ -109,15 +133,29 @@ const SelectButton = forwardRef(
       isOpen,
       values,
       options,
+      inputValue,
+      hasSearch,
+      onInputChange,
       multiple,
       placeholder,
       disabled,
+      onChange,
       ...props
     }: SelectButtonProps<T>,
     ref?: Ref<HTMLButtonElement>
   ) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {onChange, ...otherProps} = props;
+    const inputRef: Ref<HTMLInputElement> = useRef(null);
+
+    /** donne le focus à l'input quand l'utilisateur click sur le bouton d'ouverture du Select */
+    const handleWrapperClick = () => {
+      inputRef?.current?.focus();
+    };
+
+    useEffect(() => {
+      if (!isOpen) {
+        onInputChange('');
+      }
+    }, [isOpen]);
 
     return (
       <button
@@ -126,26 +164,32 @@ const SelectButton = forwardRef(
         aria-expanded={isOpen}
         aria-label="ouvrir le menu"
         className={classNames(
-          'flex items-center w-full min-h-[3rem] py-2 px-4 rounded-xl border border-solid border-grey-4 disabled:border-grey-3 bg-grey-1 hover:!bg-primary-0 disabled:hover:!bg-grey-1 overflow-hidden',
+          'w-full rounded-xl border border-solid border-grey-4 disabled:border-grey-3 bg-grey-1 hover:!bg-primary-0 disabled:hover:!bg-grey-1 overflow-hidden',
           {'rounded-b-none': isOpen}
         )}
         disabled={disabled}
-        {...otherProps}
+        {...props}
       >
-        <div className="flex mr-4">
-          {/** Listes des valeurs sélectionnées */}
-          {values ? (
-            // si le sélecteur multiple
-            Array.isArray(values) ? (
-              <div className="flex items-center gap-2">
+        {/**
+         * Cette `div` englobant les sous composant permet d'appliquer la fonction pour forcer le focus sur l'input.
+         * On ne peut pas l'appliquer sur le bouton car il reçoit déjà une propriété `onClick` du dropdown floater
+         */}
+        <div
+          className="flex min-h-[3rem] py-2 px-4"
+          onClick={handleWrapperClick}
+        >
+          <div className="flex grow flex-wrap gap-2 mr-4">
+            {values && Array.isArray(values) ? (
+              /** Listes des valeurs sélectionnées */
+              <div className="flex items-center gap-2 grow">
                 <Tag
+                  className="text-left"
                   trim
-                  title={
-                    getOptions(options).find(({value: v}) => v === values[0])
-                      ?.label || ''
-                  }
+                  title={getOptionLabel(values[0], getOptions(options))}
                   onClose={() => onChange(values[0])}
+                  disabled={disabled}
                 />
+                {/** Nombre de valeurs sélectionnées supplémentaires */}
                 {values.length > 1 && (
                   <Tag
                     title={`+${values.length - 1}`}
@@ -154,37 +198,48 @@ const SelectButton = forwardRef(
                 )}
               </div>
             ) : (
-              // si sélecteur simple
-              <Tag
-                trim
-                title={
-                  getOptions(options).find(({value: v}) => v === values)
-                    ?.label || ''
-                }
-                onClose={() => onChange(values)}
+              /** Si pas de valeur et que la recherche n'est pas activée, on affiche un placeholder */
+              !hasSearch && (
+                <span
+                  className={classNames(
+                    'my-auto text-xs text-grey-6 line-clamp-1',
+                    {
+                      '!text-grey-5': disabled,
+                    }
+                  )}
+                >
+                  {placeholder ?? multiple
+                    ? 'Sélectionner une ou plusieurs options'
+                    : 'Sélectionner une option'}
+                </span>
+              )
+            )}
+            {hasSearch && (
+              <input
+                ref={inputRef}
+                type="text"
+                className={classNames(
+                  'w-full text-sm outline-0 placeholder:text-grey-6',
+                  {hidden: disabled && values} // on n'affiche pas l'input si le sélecteur est désactivé et possède une valeur
+                )}
+                value={inputValue}
+                onChange={e => onInputChange(e.target.value)}
+                placeholder={placeholder ?? 'Rechercher par mots-clés'}
+                disabled={disabled}
               />
-            )
-          ) : (
-            /** Placeholder */
+            )}
+          </div>
+          {/** Icône flèche d'ouverture */}
+          <div className="mt-2 ml-auto">
             <span
-              className={classNames('text-xs text-grey-6 line-clamp-1', {
-                '!text-grey-5': disabled,
-              })}
-            >
-              {placeholder ?? multiple
-                ? 'Sélectionner une ou plusieurs options'
-                : 'Sélectionner une option'}
-            </span>
-          )}
+              className={classNames(
+                'flex fr-icon-arrow-down-s-line text-primary-9 before:h-4 before:w-4',
+                {'rotate-180': isOpen},
+                {'text-grey-4': disabled}
+              )}
+            />
+          </div>
         </div>
-        {/** Icône flèche d'ouverture */}
-        <span
-          className={classNames(
-            'flex ml-auto fr-icon-arrow-down-s-line text-primary-9 before:h-4 before:w-4',
-            {'rotate-180': isOpen},
-            {'text-grey-4': disabled}
-          )}
-        />
       </button>
     );
   }
