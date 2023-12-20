8a20473e7fe9048f4232b62cc49830c18367cdcd
Marc Rutkowski <marc@attractive-media.fr>
2023-12-20 11:48:32 +0100

    Corrige l'affichage du statut d'une sous-action liée à une fiche action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLieesCards.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLieesCards.tsx
@@ -1,7 +1,7 @@
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {makeCollectiviteTacheUrl} from 'app/paths';
 import {referentielToName} from 'app/labels';
-import {TActionStatutsRow} from 'types/alias';
+import {getAvancementExt} from 'ui/referentiels/utils';
 import ActionStatutBadge from 'ui/shared/actions/ActionStatutBadge';
 import ActionCard from '../../components/ActionCard';
 import {useActionListe} from '../data/options/useActionListe';
@@ -15,31 +15,46 @@ export const ActionsLieesCards = ({
   const collectiviteId = useCollectiviteId()!;
 
   const actionsLiees =
-    actionListe?.filter((action: TActionStatutsRow) =>
+    actionListe?.filter(action =>
       actions?.some(id => id === action.action_id)
     ) ?? [];
 
   return actionsLiees.length > 0 ? (
     <div className="grid grid-cols-2 gap-6 mb-8">
-      {actionsLiees.map(action => (
-        <div className="relative self-stretch" key={action.action_id}>
-          <ActionCard
-            openInNewTab
-            link={makeCollectiviteTacheUrl({
-              collectiviteId: collectiviteId,
-              actionId: action.action_id,
-              referentielId: action.referentiel,
-            })}
-            statutBadge={
-              action.avancement && (
-                <ActionStatutBadge statut={action.avancement} small />
-              )
-            }
-            details={`Référentiel ${referentielToName[action.referentiel]}`}
-            title={`${action.identifiant} ${action.nom}`}
-          />
-        </div>
-      ))}
+      {actionsLiees.map(action => {
+        const {
+          action_id,
+          avancement_descendants,
+          referentiel,
+          identifiant,
+          nom,
+        } = action;
+
+        const avancementExt = getAvancementExt(action);
+        // Une sous-action "non renseigné" avec des tâches renseignées
+        // a le statut "détaillé"
+        const statut =
+          (!avancementExt || avancementExt === 'non_renseigne') &&
+          avancement_descendants?.find(av => !!av && av !== 'non_renseigne')
+            ? 'detaille'
+            : avancementExt || 'non_renseigne';
+
+        return (
+          <div className="relative self-stretch" key={action_id}>
+            <ActionCard
+              openInNewTab
+              link={makeCollectiviteTacheUrl({
+                collectiviteId,
+                actionId: action_id,
+                referentielId: referentiel,
+              })}
+              statutBadge={<ActionStatutBadge statut={statut} small />}
+              details={`Référentiel ${referentielToName[referentiel]}`}
+              title={`${identifiant} ${nom}`}
+            />
+          </div>
+        );
+      })}
     </div>
   ) : null;
 };
