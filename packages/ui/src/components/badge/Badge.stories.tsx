b32dede78d4c4d0baa0d53d27525c9c751bb735f
Arthur Molinos <unknown>
2023-12-21 16:32:13 +0100

    Création du composant générique Badge

---
+++ packages/ui/src/components/badge/Badge.stories.tsx
@@ -0,0 +1,55 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {Badge, BadgeState} from './Badge';
+
+const meta: Meta<typeof Badge> = {
+  component: Badge,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  args: {title: 'Badge'},
+};
+
+export const Grid: Story = {
+  args: {title: 'Badge'},
+  render: args => {
+    const states: BadgeState[] = [
+      'default',
+      'standard',
+      'success',
+      'warning',
+      'new',
+      'error',
+      'info',
+      'grey',
+    ];
+    return (
+      <div className="flex gap-16 flex-wrap">
+        {states.map(state => (
+          <div key={state} className="col-span-1 flex flex-col gap-4">
+            <div className="capitalize text-sm font-medium">{state}</div>
+            <Badge {...args} state={state} />
+            <Badge {...args} state={state} onClose={action('onClose')} light />
+            <Badge
+              {...args}
+              state={state}
+              size="sm"
+              onClose={action('onClose')}
+              iconPosition="left"
+            />
+            <Badge
+              {...args}
+              state={state}
+              disabled
+              onClose={action('onClose')}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  },
+};
---
+++ packages/ui/src/components/badge/Badge.tsx
@@ -0,0 +1,90 @@
+import classNames from 'classnames';
+import {badgeClassnames} from './utils';
+
+export type BadgeState =
+  | 'default'
+  | 'standard'
+  | 'success'
+  | 'warning'
+  | 'new'
+  | 'error'
+  | 'info'
+  | 'grey';
+
+type Props = {
+  /** Donne un id pour les tests e2e */
+  dataTest?: string;
+  /** Libellé affiché dans le badge */
+  title: string;
+  /** État */
+  state?: BadgeState;
+  /** Taille du badge */
+  size?: 'sm' | 'md';
+  /** Affiche le badge en style light */
+  light?: boolean;
+  /** Position de l'icon dans le badge, à droite par défaut */
+  iconPosition?: 'left' | 'right';
+  /** Appelée lors du clic sur le bouton "Fermer". Ne pas spécifier pour masquer le bouton. */
+  onClose?: () => void;
+  /** Pour désactiver les interactions */
+  disabled?: boolean;
+  /** Permet de surcharger les styles du container */
+  className?: string;
+  /** Restreint le titre à une seule ligne, true par défaut */
+  trim?: boolean;
+};
+
+/** Affiche un badge */
+export const Badge = ({
+  title,
+  onClose,
+  state = 'default',
+  size = 'md',
+  iconPosition = 'right',
+  light = false,
+  disabled,
+  className,
+  trim = true,
+  dataTest,
+}: Props) => {
+  const {text, background, border, icon} = badgeClassnames[state];
+
+  return (
+    <div
+      data-test={`Badge-${dataTest}`}
+      className={classNames(
+        background,
+        border,
+        'flex items-center gap-1 w-max px-3 py-0.5 rounded',
+        {
+          'flex-row-reverse': iconPosition === 'left',
+          '!px-2': size === 'sm',
+          'border border-solid border-grey-4 !bg-white': light,
+        },
+        className
+      )}
+    >
+      <span
+        className={classNames(text, 'font-bold uppercase text-sm', {
+          'line-clamp-1': trim,
+          '!text-xs': size === 'sm',
+        })}
+      >
+        {title}
+      </span>
+      {onClose && !disabled && (
+        <div
+          className={classNames(
+            'flex fr-icon-close-circle-fill before:w-4 before:h-4 rounded-full cursor-pointer',
+            {'before:!w-3 before:!h-3': size === 'sm'},
+            icon
+          )}
+          onClick={evt => {
+            evt.stopPropagation();
+            onClose();
+          }}
+        ></div>
+      )}
+    </div>
+  );
+};
