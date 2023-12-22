8afa577043ddb0bd3e7f23bb02611466552c9b2a
Florian d'Erfurth <florian@derfurth.com>
2023-12-22 17:15:16 +0100

    Active l'audit des objectifs et des résultats des indicateurs.

--- data_layer/sqitch/revert/indicateur/referentiel.sql
+++ data_layer/sqitch/revert/indicateur/referentiel.sql
@@ -5,7 +5,10 @@ BEGIN;
 
 drop trigger modified_by on indicateur_resultat;
 alter table indicateur_resultat drop column modified_by;
+select audit.disable_tracking('public.indicateur_resultat'::regclass);
+
 drop trigger modified_by on indicateur_objectif;
 alter table indicateur_objectif drop column modified_by;
+select audit.disable_tracking('public.indicateur_objectif'::regclass);
 
 COMMIT;
