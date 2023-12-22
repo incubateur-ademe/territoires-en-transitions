aa479c2b63e5f90347de6e4c54c5718265b83431
Florian d'Erfurth <florian@derfurth.com>
2023-12-22 17:15:16 +0100

    Active l'audit des objectifs et des résultats des indicateurs.

--- data_layer/sqitch/deploy/indicateur/referentiel.sql
+++ data_layer/sqitch/deploy/indicateur/referentiel.sql
@@ -5,8 +5,10 @@ BEGIN;
 
 alter table indicateur_resultat add modified_by uuid;
 select private.add_modified_by_trigger('public', 'indicateur_resultat');
+select audit.enable_tracking('public.indicateur_resultat'::regclass);
 
 alter table indicateur_objectif add modified_by uuid;
 select private.add_modified_by_trigger('public', 'indicateur_objectif');
+select audit.enable_tracking('public.indicateur_objectif'::regclass);
 
 COMMIT;
--- data_layer/sqitch/revert/indicateur/personnalise.sql
+++ data_layer/sqitch/revert/indicateur/personnalise.sql
@@ -4,7 +4,10 @@ BEGIN;
 
 drop trigger modified_by on indicateur_personnalise_resultat;
 alter table indicateur_personnalise_resultat drop column modified_by;
+select audit.disable_tracking('public.indicateur_personnalise_resultat'::regclass);
+
 drop trigger modified_by on indicateur_personnalise_objectif;
 alter table indicateur_personnalise_objectif drop column modified_by;
+select audit.disable_tracking('public.indicateur_personnalise_objectif'::regclass);
 
 COMMIT;
