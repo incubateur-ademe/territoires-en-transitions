4e1dd73fd25888ca616d2ac4c53dfe1b214a00b5
Marc Rutkowski <marc@attractive-media.fr>
2024-01-08 12:45:27 +0100

    Déplace dans une fonction la détermination du statut d'une action liée à une FA

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLieesCards.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionForm/ActionsLieesCards.tsx
@@ -1,7 +1,7 @@
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {makeCollectiviteTacheUrl} from 'app/paths';
 import {referentielToName} from 'app/labels';
-import {getAvancementExt} from 'ui/referentiels/utils';
+import {getActionStatut} from 'ui/referentiels/utils';
 import ActionStatutBadge from 'ui/shared/actions/ActionStatutBadge';
 import ActionCard from '../../components/ActionCard';
 import {useActionListe} from '../data/options/useActionListe';
@@ -22,22 +22,9 @@ export const ActionsLieesCards = ({
   return actionsLiees.length > 0 ? (
     <div className="grid grid-cols-2 gap-6 mb-8">
       {actionsLiees.map(action => {
-        const {
-          action_id,
-          avancement_descendants,
-          referentiel,
-          identifiant,
-          nom,
-        } = action;
+        const {action_id, referentiel, identifiant, nom} = action;
 
-        const avancementExt = getAvancementExt(action);
-        // Une sous-action "non renseigné" avec des tâches renseignées
-        // a le statut "détaillé"
-        const statut =
-          (!avancementExt || avancementExt === 'non_renseigne') &&
-          avancement_descendants?.find(av => !!av && av !== 'non_renseigne')
-            ? 'detaille'
-            : avancementExt || 'non_renseigne';
+        const statut = getActionStatut(action);
 
         return (
           <div className="relative self-stretch" key={action_id}>
