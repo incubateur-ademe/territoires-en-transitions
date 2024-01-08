db8efdfe9c81893793d0b280280dd5eddd6fe49e
Amandine Jacquelin <conta@jacquelinamandine.fr>
2024-01-08 16:21:33 +0100

    Corrige les droits de indicateur_source

--- data_layer/sqitch/verify/indicateur/source.sql
+++ data_layer/sqitch/verify/indicateur/source.sql
@@ -5,10 +5,4 @@ BEGIN;
 select id, libelle
 from indicateur_source where false;
 
-select has_function_privilege('import_sources(indicateur_definitions)', 'execute');
-
-select collectivite_id, indicateur_id, annee, valeur, modified_at, source, source_id
-from indicateur_resultat_import
-where false;
-
 ROLLBACK;
