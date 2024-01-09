aeec414581bbc08dc8bb50e773436a9ce6aea52a
Arthur Molinos <>
2024-01-09 16:27:37 +0100

    Supprime la story ToutesLesCollectivites

--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/CollectivitesEngagees.tsx
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/CollectivitesEngagees.tsx
@@ -6,28 +6,28 @@ import AssocierCollectiviteBandeau from 'ui/collectivites/AssocierCollectiviteBa
 
 import {useSearchParams} from 'core-logic/hooks/query';
 import {useOwnedCollectivites} from 'core-logic/hooks/useOwnedCollectivites';
-import {TCollectiviteCarte} from './types';
 import {useFilteredCollectivites} from './data/useFilteredCollectivites';
 import {TCollectivitesFilters} from './data/filtreLibelles';
-import {TSetFilters, initialFilters, nameToShortNames} from './data/filters';
+import {initialFilters, nameToShortNames} from './data/filters';
 
-export type TRenderCollectivitesEngageesPage = {
-  collectivites: TCollectiviteCarte[];
-  collectivitesCount: number;
-  filters: TCollectivitesFilters;
-  setFilters: TSetFilters;
-  isLoading: boolean;
-};
-
-export const RenderCollectivitesEngageesPage = (
-  props: TRenderCollectivitesEngageesPage
-) => {
+const CollectivitesEngagees = () => {
   const ownedCollectivites = useOwnedCollectivites();
 
   const hasCollectivites = useMemo(
     () => ownedCollectivites?.length !== 0,
     [ownedCollectivites]
   );
+
+  /** Filters */
+  const [filters, setFilters] = useSearchParams<TCollectivitesFilters>(
+    'collectivites',
+    initialFilters,
+    nameToShortNames
+  );
+
+  /** Data */
+  const {collectivites, collectivitesCount, isLoading} =
+    useFilteredCollectivites(filters);
 
   return (
     <>
@@ -38,15 +38,15 @@ export const RenderCollectivitesEngageesPage = (
       >
         <div className="md:flex">
           {/* Filters column */}
-          <Filters filters={props.filters} setFilters={props.setFilters} />
+          <Filters filters={filters} setFilters={setFilters} />
           {/* Collectivites column */}
           <CollectivitesView
             initialFilters={initialFilters}
-            filters={props.filters}
-            setFilters={props.setFilters}
-            data={props.collectivites}
-            dataCount={props.collectivitesCount}
-            isLoading={props.isLoading}
+            filters={filters}
+            setFilters={setFilters}
+            data={collectivites}
+            dataCount={collectivitesCount}
+            isLoading={isLoading}
             canUserClickCard={hasCollectivites}
           />
         </div>
@@ -55,25 +55,4 @@ export const RenderCollectivitesEngageesPage = (
   );
 };
 
-const CollectivitesEngageesPage = () => {
-  // filtre initial
-  const [filters, setFilters] = useSearchParams<TCollectivitesFilters>(
-    'collectivites',
-    initialFilters,
-    nameToShortNames
-  );
-  const {collectivites, collectivitesCount, isLoading} =
-    useFilteredCollectivites(filters);
-
-  return (
-    <RenderCollectivitesEngageesPage
-      collectivites={collectivites}
-      collectivitesCount={collectivitesCount}
-      filters={filters}
-      setFilters={setFilters}
-      isLoading={isLoading}
-    />
-  );
-};
-
-export default CollectivitesEngageesPage;
+export default CollectivitesEngagees;
--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/CollectivitesEngageesPage.tsx
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/CollectivitesEngageesPage.tsx
@@ -2,14 +2,14 @@ import {Suspense} from 'react';
 import {lazy} from 'utils/lazy';
 import {renderLoader} from 'utils/renderLoader';
 
-const CollectivitesEngageesPage = lazy(
+const CollectivitesEngagees = lazy(
   () => import('app/pages/CollectivitesEngagees/CollectivitesEngagees')
 );
 
 export const CollectivitesEngageesPage = () => {
   return (
     <Suspense fallback={renderLoader()}>
-      <CollectivitesEngageesPage />
+      <CollectivitesEngagees />
     </Suspense>
   );
 };
