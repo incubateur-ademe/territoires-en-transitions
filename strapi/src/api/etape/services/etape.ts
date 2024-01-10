7ec82bbfa2819ad1aeebfa8858c05e76bb87168d
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-01-10 15:47:32 +0100

    Suppression de collections inutilisées

--- strapi/src/api/etape/services/etape.ts
+++
@@ -1,7 +0,0 @@
-/**
- * etape service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::etape.etape');
--- strapi/src/api/objectif/routes/objectif.ts
+++
@@ -1,7 +0,0 @@
-/**
- * objectif router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::objectif.objectif');
