e22a22701c08e57600ba144e4127275464ab9f69
Florian d'Erfurth <florian@derfurth.com>
2024-01-11 19:13:37 +0100

    Ajoute les relations pour la page collectivités engagées

--- data_layer/sqitch/verify/collectivite/toutes_les_collectivites.sql
+++ data_layer/sqitch/verify/collectivite/toutes_les_collectivites.sql
@@ -2,37 +2,5 @@
 
 BEGIN;
 
-select collectivite_id,
-       nom,
-       type_collectivite,
-       code_siren_insee,
-       region_code,
-       departement_code,
-       population,
-       etoiles_cae,
-       etoiles_eci,
-       etoiles_all,
-       score_fait_cae,
-       score_fait_eci,
-       score_fait_min,
-       score_fait_max,
-       score_fait_sum,
-       score_programme_cae,
-       score_programme_eci,
-       score_programme_max,
-       score_programme_sum,
-       completude_cae,
-       completude_eci,
-       completude_min,
-       completude_max,
-       population_intervalle,
-       completude_cae_intervalle,
-       completude_eci_intervalle,
-       completude_intervalles,
-       fait_cae_intervalle,
-       fait_eci_intervalle,
-       fait_intervalles
-from collectivite_card
-where false;
 
 ROLLBACK;
