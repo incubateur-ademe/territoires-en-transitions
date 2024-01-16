aaa8a299f92be7236f8dc1ac55ee4ffec0898d3c
Florian d'Erfurth <florian@derfurth.com>
2024-01-16 18:10:48 +0100

    Permet d'envoyer des messages depuis Aldo

--- supabase/functions/site_send_message/index.ts
+++ supabase/functions/site_send_message/index.ts
@@ -1,82 +1,71 @@
-import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
-import { getSupabaseClientWithServiceRole } from '../_shared/getSupabaseClient.ts';
-import { corsHeaders } from '../_shared/cors.ts';
+import {serve} from "https://deno.land/std@0.168.0/http/server.ts";
+import {getSupabaseClientWithServiceRole} from "../_shared/getSupabaseClient.ts";
+import {corsHeaders} from "../_shared/cors.ts";
 
 /**
- *
- */
+ * Permet d'envoyer des mails depuis les formulaires de contact.
+ **/
 serve(async (req: Request) => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders });
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {headers: corsHeaders});
   }
 
   try {
-    const { categorie, objet, prenom, nom, email, message } = await req.json();
+    const {categorie, objet, prenom, nom, email, message} = await req.json();
 
-    let destEmail = '';
+    // Par défaut, on envoie à notre adresse contact.
+    let destEmail = "contact@territoiresentransitions.fr";
+    let sujet = `Homepage : ${objet}`;
 
-    if (
-      categorie ===
-      'Questions relatives au programme Territoire Engagé Transition Écologique'
-    ) {
-      // Adresse à mettre à jour
-      destEmail = 'territoireengage@ademe.fr';
-    } else if (
-      categorie ===
-      'Questions relatives à la plateforme Territoires en transitions'
-    ) {
-      destEmail = 'contact@territoiresentransitions.fr';
-    } else {
-      destEmail = 'contact@territoiresentransitions.fr';
+    if (categorie.toLowerCase().includes("questions relatives au programme")) {
+      destEmail = "territoireengage@ademe.fr";
+    } else if (categorie.toLowerCase().includes("aldo")) {
+      destEmail = "aldo@abc-transitionbascarbone.fr";
+      sujet = objet;
     }
 
-    const res = await fetch('https://api.resend.com/emails', {
-      method: 'POST',
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${Deno.env.get('RESEND_API_KEY')}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Deno.env.get("RESEND_API_KEY")}`,
       },
       body: JSON.stringify({
-        from: 'contact@territoiresentransitions.fr',
+        from: "contact@territoiresentransitions.fr",
         to: destEmail,
-        subject: `Homepage : ${objet}`,
-        html: `<p>De : ${prenom} ${nom} (${email})</p><p>${message}</p>`,
+        subject: sujet,
+        reply_to: email,
+        html: `<p>De : ${prenom ? prenom : ''} ${nom} (${email})</p><p>${message}</p>`,
       }),
     });
 
-    const resendData = await res.json();
-
-    if (resendData.statusCode !== 200) throw resendData;
-
     const supabase = getSupabaseClientWithServiceRole();
-
-    const query = supabase
-      .from('site_contact')
+    const {error, data} = await supabase
+      .from("site_contact")
       .insert({
-        email: email,
-        formulaire: JSON.stringify({
+        email: destEmail,
+        formulaire: {
           categorie,
           objet,
           prenom,
           nom,
           email,
           message,
-        }),
+        },
       })
       .select();
 
-    const { error, data } = await query;
-
+    const resendData = await res.json();
     if (error) throw error;
 
     // Renvoie le statut pour persister dans les logs Supabase.
     return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json', ...corsHeaders },
+      headers: {"Content-Type": "application/json", ...corsHeaders},
       status: resendData.status || resendData.statusCode,
     });
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { ...corsHeaders },
+    return new Response(JSON.stringify({error: error.message}), {
+      headers: {...corsHeaders},
       status: error.status || error.statusCode,
     });
   }
