a8fe75ff772d0d3a889deddded1704279e1d1ee6
Florian d'Erfurth <florian@derfurth.com>
2024-01-16 10:47:11 +0100

    Corrige la policy pour`indicateur_resultat_import`

--- data_layer/sqitch/deploy/indicateur/confidentialite.sql
+++ data_layer/sqitch/deploy/indicateur/confidentialite.sql
@@ -3,5 +3,10 @@
 BEGIN;
 
 alter view indicateurs set (security_invoker = on);
+drop policy allow_read on indicateur_resultat_import;
+create policy allow_read on indicateur_resultat_import
+    as permissive
+    for select
+    using (can_read_acces_restreint(collectivite_id));
 
 COMMIT;
