a5cb76a5f7b9604309343ac2d7a6e75f613e01e3
Marc Rutkowski <marc@attractive-media.fr>
2024-01-17 13:00:24 +0100

    Ajoute les composants `Tabs` et `Tab`

---
+++ packages/ui/src/design-system/tabs/Tabs.stories.tsx
@@ -0,0 +1,71 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {Tabs} from './Tabs';
+import {Tab} from './Tab';
+
+const meta: Meta<typeof Tabs> = {
+  title: 'Design System/Tabs',
+  component: Tabs,
+  args: {
+    onChange: action('onChange'),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tabs>;
+
+export const OngletParDefaut: Story = {
+  args: {
+    children: [
+      <Tab label="Onglet 1">contenu onglet 1</Tab>,
+      <Tab label="Onglet 2">contenu onglet 2</Tab>,
+      <Tab label="Onglet 3">contenu onglet 3</Tab>,
+    ],
+  },
+};
+
+export const Onglet2: Story = {
+  args: {
+    defaultActiveTab: 1,
+    children: [
+      <Tab label="Onglet 1">contenu onglet 1</Tab>,
+      <Tab label="Onglet 2">contenu onglet 2</Tab>,
+      <Tab label="Onglet 3">contenu onglet 3</Tab>,
+    ],
+  },
+};
+
+export const AvecIcones: Story = {
+  args: {
+    defaultActiveTab: 1,
+    children: [
+      <Tab label="Onglet 1" icon="lock-fill">
+        contenu onglet 1
+      </Tab>,
+      <Tab label="Onglet 2" icon="user-line">
+        contenu onglet 2
+      </Tab>,
+      <Tab label="Onglet 3" icon="chat-1-line">
+        contenu onglet 3
+      </Tab>,
+    ],
+  },
+};
+
+export const TailleXS: Story = {
+  args: {
+    defaultActiveTab: 1,
+    size: 'xs',
+    children: AvecIcones.args.children,
+  },
+};
+
+export const AvecStyles: Story = {
+  args: {
+    defaultActiveTab: 2,
+    className: 'bg-secondary-1 mb-0 rounded-none',
+    tabPanelClassName: 'border-2 border-secondary-1 p-4',
+    children: AvecIcones.args.children,
+  },
+};
