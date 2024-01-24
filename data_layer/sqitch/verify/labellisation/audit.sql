fbf1d5b7939374d632590aaa319c0f44b375d035
Amandine Jacquelin <conta@jacquelinamandine.fr>
2024-01-24 14:51:00 +0100

    Ajoute des attributs pour la CNL et un flag clos

--- data_layer/sqitch/verify/labellisation/audit.sql
+++ data_layer/sqitch/verify/labellisation/audit.sql
@@ -2,6 +2,21 @@
 
 BEGIN;
 
-select has_function_privilege('labellisation.upsert_action_audit()', 'execute');
+select id,
+       collectivite_id,
+       referentiel,
+       demande_id,
+       date_debut,
+       date_fin,
+       valide,
+       date_cnl,
+       valide_labellisation,
+       clos
+from labellisation.audit
+where false;
+
+select has_function_privilege('labellisation.update_audit()', 'execute');
+select has_function_privilege('labellisation.audit_personnalisation_payload(integer, boolean, text)', 'execute');
+select has_function_privilege('labellisation.update_labellisation_after_scores()', 'execute');
 
 ROLLBACK;
