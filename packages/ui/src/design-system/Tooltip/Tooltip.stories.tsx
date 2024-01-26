6378c67367c627a6d8ed1ac28517788ddf540e38
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-01-26 12:14:26 +0100

    Ajout d'un composant tooltip dans le design system

---
+++ packages/ui/src/design-system/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,189 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {Tooltip} from './Tooltip';
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Design System/Tooltip',
+  component: Tooltip,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+/** Tooltip par défaut, sans aucune props renseignée. */
+export const Default: Story = {
+  render: () => (
+    <Tooltip label="Message">
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-10">
+        Survoler pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
+
+/** Tooltip avec différentes valeurs de placement. */
+export const Placement: Story = {
+  render: () => (
+    <div className="flex items-center">
+      <div className="my-10 flex flex-col gap-2">
+        <div className="flex gap-2">
+          <Tooltip label="Message" placement="top-start">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              top-start
+            </div>
+          </Tooltip>
+          <Tooltip label="Message" placement="top">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">top</div>
+          </Tooltip>
+          <Tooltip label="Message" placement="top-end">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              top-end
+            </div>
+          </Tooltip>
+        </div>
+
+        <div className="flex gap-2">
+          <Tooltip label="Message" placement="bottom-start">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              bottom-start
+            </div>
+          </Tooltip>
+          <Tooltip label="Message" placement="bottom">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              bottom
+            </div>
+          </Tooltip>
+          <Tooltip label="Message" placement="bottom-end">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              bottom-end
+            </div>
+          </Tooltip>
+        </div>
+      </div>
+
+      <div className="mx-20 flex flex-col gap-2">
+        <div className="flex gap-2">
+          <Tooltip label="Message" placement="left-start">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              left-start
+            </div>
+          </Tooltip>
+          <Tooltip label="Message" placement="right-start">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              right-start
+            </div>
+          </Tooltip>
+        </div>
+
+        <div className="flex gap-2">
+          <Tooltip label="Message" placement="left">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">left</div>
+          </Tooltip>
+          <Tooltip label="Message" placement="right">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">right</div>
+          </Tooltip>
+        </div>
+
+        <div className="flex gap-2">
+          <Tooltip label="Message" placement="left-end">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              left-end
+            </div>
+          </Tooltip>
+          <Tooltip label="Message" placement="right-end">
+            <div className="bg-primary-1 py-2 px-3 w-32 text-center">
+              right-end
+            </div>
+          </Tooltip>
+        </div>
+      </div>
+    </div>
+  ),
+};
+
+/** Tooltip avec texte long. */
+export const LongText: Story = {
+  render: () => (
+    <Tooltip label="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra erat non luctus porta. Aliquam vitae maximus enim, ut elementum arcu. Fusce euismod risus non eros sodales, nec sollicitudin justo rhoncus. Nulla commodo mauris vitae lectus elementum, sed iaculis elit tristique. Nam nec quam non quam viverra mattis vel vitae lorem. Etiam a ante sit amet urna elementum pretium et in enim. Mauris molestie dolor non sapien luctus, sed sollicitudin justo consequat.">
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-20">
+        Survoler pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
+
+/** Tooltip avec texte long avec largeur fixe. */
+export const LongTextWithFixedWidth: Story = {
+  render: () => (
+    <Tooltip
+      label={
+        <div className="w-96">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra
+          erat non luctus porta. Aliquam vitae maximus enim, ut elementum arcu.
+          Fusce euismod risus non eros sodales, nec sollicitudin justo rhoncus.
+          Nulla commodo mauris vitae lectus elementum, sed iaculis elit
+          tristique. Nam nec quam non quam viverra mattis vel vitae lorem. Etiam
+          a ante sit amet urna elementum pretium et in enim. Mauris molestie
+          dolor non sapien luctus, sed sollicitudin justo consequat.
+        </div>
+      }
+    >
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-32">
+        Survoler pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
+
+/** Tooltip sans flèche. */
+export const WithoutArrow: Story = {
+  render: () => (
+    <Tooltip label="Message" withArrow={false}>
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-10">
+        Survoler pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
+
+/** Tooltip activée par click. */
+export const ActivatedByClick: Story = {
+  render: () => (
+    <Tooltip label="Message" activatedBy="click">
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-10">
+        Cliquer pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
+
+/** Tooltip avec contenu custom. */
+export const CustomContent: Story = {
+  render: () => (
+    <Tooltip
+      label={
+        <div className="w-52">
+          <h6>Titre</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            viverra erat non luctus porta.
+          </p>
+          <div className="mt-4">
+            <div className="flex flex-row gap-1 items-center">
+              <div className="w-3 h-3 bg-primary-7" />
+              <div>Lorem ipsum dolor sit amet</div>
+            </div>
+            <div className="flex flex-row gap-1 items-center">
+              <div className="w-3 h-3 bg-secondary-1" />
+              <div>Lorem ipsum dolor sit amet</div>
+            </div>
+          </div>
+        </div>
+      }
+    >
+      <div className="bg-primary-1 py-2 px-3 w-fit mb-32">
+        Survoler pour voir l'info bulle
+      </div>
+    </Tooltip>
+  ),
+};
---
+++ packages/ui/src/design-system/Tooltip/Tooltip.tsx
@@ -0,0 +1,159 @@
+import {
+  FloatingArrow,
+  Placement,
+  ReferenceType,
+  arrow,
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useClick,
+  useDismiss,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+  useRole,
+} from '@floating-ui/react';
+import classNames from 'classnames';
+import {cloneElement, useCallback, useRef, useState} from 'react';
+import {preset} from '@tailwind-preset';
+
+const colorTheme = preset.theme.extend.colors;
+
+type TooltipProps = {
+  /** Élément sur lequel s'applique l'info-bulle */
+  children: JSX.Element;
+  /** Libellé de l'info-bulle (chaîne de caractère ou composant) */
+  label: string | React.ReactNode;
+  /** Valeur de décalage de la tooltip */
+  offsetValue?: number;
+  /** Événement déclenchant l'affichage de l'info-bulle */
+  activatedBy?: 'click' | 'hover';
+  /** Délai d'ouverture de la tooltip */
+  openingDelay?: number;
+  /** Position de la tooltip */
+  placement?: Placement;
+  /** Affichage d'une flèche sur la tooltip */
+  withArrow?: boolean;
+};
+
+/** Affiche une info bulle */
+
+export const Tooltip = ({
+  children,
+  label,
+  offsetValue = 10,
+  activatedBy = 'hover',
+  openingDelay = 500,
+  placement = 'bottom-start',
+  withArrow = true,
+}: TooltipProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const arrowRef = useRef(null);
+
+  // Configuration du comportement de la tooltip
+  const {x, y, refs, strategy, context} = useFloating({
+    placement,
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    middleware: [
+      offset(offsetValue),
+      flip(),
+      shift({padding: 8}),
+      arrow({
+        element: arrowRef,
+      }),
+    ],
+    whileElementsMounted: autoUpdate,
+  });
+
+  // Preserve the consumer's ref
+  const ref = useMergedRefs([refs.setReference, children as never]);
+
+  // Tooltip height
+  const tooltipHeight = refs.floating.current?.clientHeight;
+
+  const getStaticOffset = () => {
+    if (
+      context.placement.endsWith('start') ||
+      context.placement.endsWith('end')
+    ) {
+      if (
+        (context.placement.startsWith('right') ||
+          context.placement.startsWith('left')) &&
+        !!tooltipHeight &&
+        tooltipHeight < 40
+      ) {
+        return '8px';
+      }
+      return '16px';
+    }
+    return undefined;
+  };
+
+  // Configuration des interactions
+  const {getReferenceProps, getFloatingProps} = useInteractions([
+    (activatedBy === 'click' ? useClick : useHover)(context, {
+      delay: {open: openingDelay, close: 0},
+    }),
+    useFocus(context),
+    useRole(context, {role: 'tooltip'}),
+    useDismiss(context),
+  ]);
+
+  return (
+    <>
+      {cloneElement(children, getReferenceProps({ref, ...children.props}))}
+      {isOpen && (
+        <div
+          ref={refs.setFloating}
+          {...getFloatingProps()}
+          style={{
+            position: strategy,
+            top: y,
+            left: x,
+          }}
+          className={classNames(
+            'p-2 text-primary-10 border-primary rounded bg-white shadow-lg text-xs [&_*]:text-xs [&_*]:mb-0',
+            {
+              'border-b': context.placement.startsWith('top'),
+              'border-t': context.placement.startsWith('bottom'),
+              'border-r': context.placement.startsWith('left'),
+              'border-l': context.placement.startsWith('right'),
+            }
+          )}
+        >
+          {withArrow && (
+            <FloatingArrow
+              ref={arrowRef}
+              context={context}
+              staticOffset={getStaticOffset()}
+              width={8}
+              height={4}
+              fill={'white'}
+              strokeWidth={1}
+              stroke={colorTheme.primary.DEFAULT}
+            />
+          )}
+          {label}
+        </div>
+      )}
+    </>
+  );
+};
+
+// https://github.com/gregberge/react-merge-refs/issues/5#issuecomment-643341631
+const useMergedRefs = (refs: Array<unknown>) =>
+  useCallback((current: ReferenceType) => {
+    refs.forEach(ref => {
+      if (typeof ref === 'function') {
+        ref(current);
+      } else if (ref && !Object.isFrozen(ref)) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore:next-line
+        ref.current = current;
+      }
+    });
+  }, refs);
