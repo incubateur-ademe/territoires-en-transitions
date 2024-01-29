d0d1715811dfa082f23c8600c929e169e726a13c
Florian d'Erfurth <florian@derfurth.com>
2024-01-29 18:48:45 +0100

    Teste l'ajout d'action dans un panier et l'état consolidé.

--- packages/api/src/panier_action_impact/integration.test.ts
+++ packages/api/src/panier_action_impact/integration.test.ts
@@ -1,6 +1,20 @@
 import {assert} from 'chai';
 import {testReset} from '../tests/testReset';
 import {supabase} from '../tests/supabase';
+import {Database} from '../database.types';
+
+type Panier =
+/* Le panier en tant que tel */
+  Database['public']['Tables']['panier']['Row'] & {
+  /* Liste d'actions */
+  actions: Database['public']['Tables']['action_impact']['Row'][]
+}
+
+/**
+ * On sélectionne toutes les colonnes du panier : *
+ * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `actions`
+ */
+const panierSelect = '*, actions:action_impact!action_impact_panier(*)';
 
 describe('Création de panier', async () => {
   before(async () => {
@@ -35,3 +49,44 @@ describe('Création de panier', async () => {
         'La demande de panier suivant la création devrait renvoyer un panier avec le même id.');
     });
 });
+
+describe('État du panier', async () => {
+  it('On devrait pouvoir sélectionner un panier',
+    async () => {
+      const demandePanier = await supabase.rpc('panier_from_landing');
+      const panierId = demandePanier.data.id!;
+      const selectPanier = await supabase.from('panier').
+        select().
+        eq('id', panierId).single();
+      assert.isNull(selectPanier.error,
+        `Sélectionner nouveau panier ne devrait pas renvoyer d'erreur`);
+      assert.exists(demandePanier.data,
+        `Sélectionner un panier devrait renvoyer un panier.`);
+    });
+
+  it('On devrait pouvoir ajouter une action puis la retrouver dans le panier',
+    async () => {
+      const demandePanier = await supabase.rpc('panier_from_landing');
+      const panierId = demandePanier.data.id!;
+      const actionId = 0;
+      const ajoutAction = await supabase.from('action_impact_panier').insert({
+        action: actionId,
+        panier: panierId,
+      });
+      assert.isNull(ajoutAction.error,
+        `Ajouter une action au panier ne devrait pas renvoyer d'erreur`);
+
+      const selectPanierContenu = await supabase.from('panier').
+        select(panierSelect).
+        eq('id', panierId).single<Panier>();
+
+      assert.isNull(selectPanierContenu.error,
+        `Sélectionner un panier avec son contenu ne devrait pas renvoyer d'erreur`);
+      assert.exists(selectPanierContenu.data,
+        `Sélectionner un panier devrait renvoyer un panier.`);
+      assert.equal(selectPanierContenu.data.actions.length, 1,
+        `Le panier devrait contenir une action.`);
+    });
+
+});
+
