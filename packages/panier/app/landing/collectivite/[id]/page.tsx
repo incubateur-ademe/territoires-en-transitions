88a78fb8317ab4c2b9bb3b742b86e2c3376e8307
Florian d'Erfurth <florian@derfurth.com>
2024-01-30 16:25:10 +0100

    Landing pages

---
+++ packages/panier/app/landing/collectivite/[id]/page.tsx
@@ -0,0 +1,14 @@
+import {supabase} from '@tet/api/dist/src/tests/supabase';
+import {redirect} from 'next/navigation';
+
+async function Landing({params}: { params: { id: string } }) {
+  const collectivite_id = parseInt(params.id);
+  const {error, data} = await supabase.rpc('panier_from_landing', {collectivite_id});
+  if (data) {
+    redirect(`/panier/${data.id}`)
+  }
+
+  throw error;
+}
+
+export default Landing;
