672c633fc44aa5c0c6e7bbcda541e10eed7b7942
Florian d'Erfurth <florian@derfurth.com>
2024-01-30 19:03:36 +0100

    Récupère l'état des actions avec le panier.

--- packages/api/src/panier_action_impact/index.ts
+++ packages/api/src/panier_action_impact/index.ts
@@ -4,11 +4,13 @@ export type Panier =
 /* Le panier en tant que tel */
   Database['public']['Tables']['panier']['Row'] & {
   /* Liste d'actions */
-  actions: Database['public']['Tables']['action_impact']['Row'][]
+  contenuPanier: Database['public']['Tables']['action_impact']['Row'][],
+  /* Liste d'action et de states */
+  actionStates: Database['public']['Tables']['action_impact_state']['Row'][]
 }
 
 /**
  * On sélectionne toutes les colonnes du panier : *
- * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `actions`
+ * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `contenuPanier`
  */
-export const panierSelect = '*, actions:action_impact!action_impact_panier(*)';
+export const panierSelect = '*, contenuPanier:action_impact!action_impact_panier(*), states:action_impact_state(*)';
