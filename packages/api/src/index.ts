9fc7d4646a9c601cbcea95a54720f76b2157e883
Florian d'Erfurth <florian@derfurth.com>
2024-01-30 15:36:46 +0100

    WIP Exporte l'API

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,3 +3,4 @@ export type {Database, Json} from './database.types';
 export {unaccent} from './utils/unaccent';
 export * as Indicateurs from './indicateurs';
 export * as CollectiviteEngagee from './collectiviteEngagees';
+export * as PanierActionImpact from './panier_action_impact';
