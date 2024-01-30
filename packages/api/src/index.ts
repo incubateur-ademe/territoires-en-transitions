c1582d00b84eed8e12cb32fd49158612db787d81
Florian d'Erfurth <florian@derfurth.com>
2024-01-30 15:36:46 +0100

    WIP Exporte l'API

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,3 +5,4 @@ export * from './utils/authTokens';
 export * from './utils/pathUtils';
 export * as Indicateurs from './indicateurs';
 export * as CollectiviteEngagee from './collectiviteEngagees';
+export * as PanierActionImpact from './panier_action_impact';
