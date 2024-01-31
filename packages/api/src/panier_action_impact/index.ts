a7437bfd941de26b1bdd70415efd70c0442da998
Florian d'Erfurth <florian@derfurth.com>
2024-01-31 11:11:30 +0100

    Renomme les champs du Panier

--- packages/api/src/panier_action_impact/index.ts
+++ packages/api/src/panier_action_impact/index.ts
@@ -1,16 +1,25 @@
 import {Database} from '../database.types';
 
+export type ActionImpact = Database['public']['Tables']['action_impact']['Row'];
+export type ActionImpactStatut = Database['public']['Tables']['action_impact_statut']['Row'];
+export type ActionImpactState =
+  {
+    action: ActionImpact,
+    isinpanier: boolean,
+    statut: ActionImpactStatut | null
+  };
+
 export type Panier =
 /* Le panier en tant que tel */
   Database['public']['Tables']['panier']['Row'] & {
-  /* Liste d'actions */
-  contenuPanier: Database['public']['Tables']['action_impact']['Row'][],
-  /* Liste d'action et de states */
-  actionStates: Database['public']['Tables']['action_impact_state']['Row'][]
+  /* Liste des actions ajoutée au panier */
+  contenu: ActionImpact[],
+  /* Liste de toutes les actions avec leurs states. */
+  states: ActionImpactState[]
 }
 
 /**
  * On sélectionne toutes les colonnes du panier : *
  * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `contenuPanier`
  */
-export const panierSelect = '*, contenuPanier:action_impact!action_impact_panier(*), states:action_impact_state(*)';
+export const panierSelect = '*, contenu:action_impact!action_impact_panier(*), states:action_impact_state(*)';
