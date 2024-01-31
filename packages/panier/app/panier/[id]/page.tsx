56349c69324f2a44ae18621625ce8969b5e58cf4
Florian d'Erfurth <florian@derfurth.com>
2024-01-31 13:45:43 +0100

    Permet de sélectionner les catégories.

--- packages/panier/app/panier/[id]/page.tsx
+++ packages/panier/app/panier/[id]/page.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import {panierSelect} from '@tet/api/dist/src/panier_action_impact/index';
-import {PanierActionImpact} from '@tet/api';
-import PanierRealtime from '@components/panier/panier-realtime';
-import {notFound} from 'next/navigation';
-import {supabase} from 'lib/supabaseServer';
+import React from "react";
+import {
+  Categorie,
+  panierSelect,
+} from "@tet/api/dist/src/panier_action_impact/index";
+import { PanierActionImpact } from "@tet/api";
+import PanierRealtime from "@components/panier/panier-realtime";
+import { notFound } from "next/navigation";
+import { supabase } from "lib/supabaseServer";
 
-async function Panier({params}: { params: { id: string } }) {
+async function Panier({ params }: { params: { id: string } }) {
   const panierId = params.id;
-  const {data, error} = await supabase.from('panier').
-    select(panierSelect).
-    eq('id', panierId).single<PanierActionImpact.Panier>();
+  const { data, error } = await supabase.from("panier")
+    .select(panierSelect)
+    .eq("id", panierId).single<PanierActionImpact.Panier>();
+
+  const categories = await fetchCategories();
 
   if (data) {
     return (
       <div>
         <h1>Panier {panierId}</h1>
-        <PanierRealtime panier={data!} />
+        <PanierRealtime panier={data!} categories={categories} />
       </div>
     );
   }
@@ -23,3 +28,18 @@ async function Panier({params}: { params: { id: string } }) {
 }
 
 export default Panier;
+
+const apiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const apiUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+
+async function fetchCategories(): Promise<Categorie[]> {
+  const response = await fetch(`${apiUrl}/rest/v1/action_impact_categorie`, {
+    // todo @derfurth: use cache
+    cache: "no-store",
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
+  return await response.json();
+}
