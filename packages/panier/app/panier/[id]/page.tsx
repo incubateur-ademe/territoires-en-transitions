9f8683af21d8b1e6a732f7433264686253eef3dc
Florian d'Erfurth <florian@derfurth.com>
2024-02-01 11:53:44 +0100

    Fetch les categories pour filtrer

--- packages/panier/app/panier/[id]/page.tsx
+++ packages/panier/app/panier/[id]/page.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {
-  Categorie, Niveau,
-  panierSelect,
-} from "@tet/api/dist/src/panier_action_impact/index";
-import { PanierActionImpact } from "@tet/api";
-import PanierRealtime from "@components/panier/panier-realtime";
+import { ActionImpactCategorie, Niveau, Panier, panierSelect } from "@tet/api";
+import PanierRealtime from "@components/PanierRealtime";
 import { notFound } from "next/navigation";
 import { supabase } from "lib/supabaseServer";
 
-async function Panier({ params }: { params: { id: string } }) {
+async function Panier({ params, searchParams }: {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const panierId = params.id;
   const { data, error } = await supabase.from("panier")
     .select(panierSelect)
-    .eq("id", panierId).single<PanierActionImpact.Panier>();
+    .eq("id", panierId)
+    .single<Panier>();
 
   const categories = await fetchCategories();
 
@@ -32,7 +32,7 @@ export default Panier;
 const apiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const apiUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 
-async function fetchCategories(): Promise<Categorie[]> {
+async function fetchCategories(): Promise<ActionImpactCategorie[]> {
   const response = await fetch(`${apiUrl}/rest/v1/action_impact_categorie`, {
     method: "GET",
     headers: {
@@ -43,11 +43,14 @@ async function fetchCategories(): Promise<Categorie[]> {
 }
 
 async function fetchNiveaux(
-  table: 'action_impact_complexite' | 'action_impact_fourchette_budgetaire' | 'action_impact_tier',
+  table:
+    | "action_impact_complexite"
+    | "action_impact_fourchette_budgetaire"
+    | "action_impact_tier",
 ): Promise<Niveau[]> {
   const response = await fetch(`${apiUrl}/rest/v1/${table}`, {
-    cache: 'no-store',
-    method: 'GET',
+    cache: "no-store",
+    method: "GET",
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
