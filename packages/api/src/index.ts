6c10359cf72fbbd08ee846fe645c4d370db3e6dc
Florian d'Erfurth <florian@derfurth.com>
2024-02-01 11:52:47 +0100

    Exporte les éléments à la racine de `@tet/api`

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,4 +2,4 @@ export * from './typeUtils';
 export type {Database, Json} from './database.types';
 export {unaccent} from './utils/unaccent';
 export * as Indicateurs from './indicateurs';
-export * as PanierActionImpact from './panier_action_impact';
+export * from './panier_action_impact';
