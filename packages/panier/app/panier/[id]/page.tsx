d7eee9a06577eae8488fc1a1cc54a112c2e04e3a
Florian d'Erfurth <florian@derfurth.com>
2024-02-05 16:46:46 +0100

    Utilise "use server".

--- packages/panier/app/panier/[id]/page.tsx
+++ packages/panier/app/panier/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import React from "react";
 import { ActionImpactCategorie, Niveau, Panier, panierSelect } from "@tet/api";
 import PanierRealtime from "@components/PanierRealtime";
