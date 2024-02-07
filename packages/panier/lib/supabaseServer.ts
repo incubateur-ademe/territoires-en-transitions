0d2f241aa9bc303fce03d07d66b3a3164d656738
Florian d'Erfurth <florian@derfurth.com>
2024-02-07 10:27:36 +0100

    Répare le client Supabase coté server.

--- packages/panier/lib/supabaseServer.ts
+++ packages/panier/lib/supabaseServer.ts
@@ -1,15 +1,30 @@
 import {Database} from '@tet/api';
-import {createServerComponentClient} from '@supabase/auth-helpers-nextjs';
-import {cookies} from 'next/headers';
+import {
+  createServerComponentClient,
+  Session,
+  SupabaseClient,
+} from '@supabase/auth-helpers-nextjs';
+import {
+  ReadonlyRequestCookies,
+} from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 
-export const supabase = createServerComponentClient<Database>(
-  {cookies}, {
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    options: {
-      db: {
-        schema: 'public',
+export async function createServerClient(cookies: () => ReadonlyRequestCookies)
+  : Promise<{ supabase: SupabaseClient<Database>, session: Session | null }> {
+
+  const supabase = createServerComponentClient<Database>(
+    {cookies}, {
+      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      options: {
+        db: {
+          schema: 'public',
+        },
       },
     },
-  },
-);
+  );
+  const {error, data} = await supabase.auth.getSession();
+  if (error) {
+    throw error;
+  }
+  return {supabase, session: data.session};
+}
