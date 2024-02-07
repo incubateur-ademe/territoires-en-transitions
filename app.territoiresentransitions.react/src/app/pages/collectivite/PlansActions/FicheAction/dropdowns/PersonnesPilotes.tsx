6ade9143440a10d5e6b73ae32a64459d1d711148
Arthur Molinos <>
2024-02-07 12:18:50 +0100

    Création de la modale de modification de la fiche action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/dropdowns/PersonnesPilotes.tsx
@@ -0,0 +1,89 @@
+import {QueryKey} from 'react-query';
+
+import {OptionValue, SelectMultiple, getFlatOptions} from '@tet/ui';
+
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {Personne} from '../data/types';
+import {formatNewTag, getPersonneId} from '../data/utils';
+import {usePersonneListe} from '../data/options/usePersonneListe';
+import {useDeleteTag} from '../data/options/useTagDelete';
+import {useTagUpdate} from '../data/options/useTagUpdate';
+
+type Props = {
+  personnes: Personne[] | null;
+  onChange: (personnes: Personne[] | null) => void;
+  keysToInvalidate?: QueryKey[];
+  isReadonly?: boolean;
+};
+
+const PersonnesPilotes = ({
+  keysToInvalidate,
+  personnes,
+  onChange,
+  isReadonly,
+}: Props) => {
+  const collectivite_id = useCollectiviteId();
+
+  const {data: personneListe} = usePersonneListe();
+
+  const {mutate: updateTag} = useTagUpdate({
+    key: ['personnes', collectivite_id],
+    tagTableName: 'personne_tag',
+    keysToInvalidate,
+  });
+
+  const {mutate: deleteTag} = useDeleteTag({
+    key: ['personnes', collectivite_id],
+    tagTableName: 'personne_tag',
+    keysToInvalidate,
+  });
+
+  const options =
+    personneListe?.map(personne => ({
+      value: personne.tag_id ? personne.tag_id : personne.user_id!,
+      label: personne.nom,
+    })) ?? [];
+
+  const userCreatedOptions = getFlatOptions(options)
+    .filter(o => typeof o.value === 'number')
+    .map(o => o.value);
+
+  const formatPilotes = (values?: OptionValue[]) => {
+    const pilotes = personneListe?.filter(personne =>
+      values?.some(v => v.toString() === getPersonneId(personne))
+    );
+    return pilotes?.length ? pilotes : null;
+  };
+
+  return (
+    <SelectMultiple
+      data-test="PersonnePilote"
+      options={options}
+      values={personnes?.map((personne: Personne) =>
+        personne.tag_id ? personne.tag_id : personne.user_id!
+      )}
+      onChange={({values}) => onChange(formatPilotes(values))}
+      createProps={{
+        userCreatedOptions,
+        onCreate: inputValue => {
+          onChange(
+            personnes
+              ? [...personnes, formatNewTag(inputValue, collectivite_id!)]
+              : [formatNewTag(inputValue, collectivite_id!)]
+          );
+        },
+        onUpdate: (tag_id, tag_name) => {
+          updateTag({
+            collectivite_id: collectivite_id!,
+            id: parseInt(tag_id as string),
+            nom: tag_name,
+          });
+        },
+        onDelete: tag_id => deleteTag(parseInt(tag_id as string)),
+      }}
+      disabled={isReadonly}
+    />
+  );
+};
+
+export default PersonnesPilotes;
