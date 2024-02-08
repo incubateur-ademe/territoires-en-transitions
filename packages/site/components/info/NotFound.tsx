a7581e8eed6e04ece7c9c78315cbd35d5d96eab9
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-02-08 17:53:25 +0100

    Utilisation d'un composant NotFound custom

---
+++ packages/site/components/info/NotFound.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import Section from '@components/sections/Section';
+
+const NotFound = () => {
+  return (
+    <Section className="mt-32 md:mt-60">
+      <div className="flex items-center justify-center divide-x divide-black">
+        <h1 className="text-3xl font-normal pr-4">404</h1>
+        <h2 className="text-lg font-normal pl-4">Cette page n'existe pas</h2>
+      </div>
+    </Section>
+  );
+};
+
+export default NotFound;
