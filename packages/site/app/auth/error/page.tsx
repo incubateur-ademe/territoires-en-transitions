2b6373668e3b4c935badbf3b4a9fb7a4b34b11e4
Marc Rutkowski <marc@attractive-media.fr>
2024-02-08 17:20:29 +0100

    Ajoute une page pour les erreurs

---
+++ packages/site/app/auth/error/page.tsx
@@ -0,0 +1,9 @@
+import Section from '@components/sections/Section';
+
+export default function AuthError() {
+  return (
+    <Section>
+      <p>Une erreur est survenue. Veuillez contacter le support.</p>
+    </Section>
+  );
+}
