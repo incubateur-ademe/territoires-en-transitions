1350ae842a83cd30758e18349d9dfd58207a6200
Marc Rutkowski <marc@attractive-media.fr>
2024-02-08 17:15:03 +0100

    Ajoute les différents clients supabase pour l'utilisation en SSR

---
+++ packages/site/src/supabase/actions.ts
@@ -0,0 +1,31 @@
+import {type CookieOptions, createServerClient} from '@supabase/ssr';
+import {cookies} from 'next/headers';
+import {NextRequest, NextResponse} from 'next/server';
+
+/**
+ * Pour accéder à supabase depuis les Server Actions et les Route Handlers
+ */
+export function createClient(
+  cookieStore: NextRequest['cookies'] | ReturnType<typeof cookies>,
+  responseCookies?: NextResponse['cookies'],
+) {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({name, value, ...options});
+          responseCookies?.set({name, value, ...options});
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({name, value: '', ...options});
+          responseCookies?.set({name, value: '', ...options});
+        },
+      },
+    },
+  );
+}
