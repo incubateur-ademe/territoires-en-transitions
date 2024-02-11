071cd730310893ecc556ce64032aad986492206c
Florian d'Erfurth <florian@derfurth.com>
2024-02-11 17:13:11 +0100

    Utilise le client Supabase pour l'API, reorganise les fichiers.

--- packages/api/src/panier_action_impact/index.ts
+++ packages/api/src/panier_action_impact/index.ts
@@ -1,32 +1,4 @@
-import {Database} from '../database.types';
-
-export type ActionImpact = Database['public']['Tables']['action_impact']['Row'];
-export type ActionImpactStatut = Database['public']['Tables']['action_impact_statut']['Row'];
-export type ActionImpactState =
-  {
-    action: ActionImpact,
-    isinpanier: boolean,
-    statut: ActionImpactStatut | null
-  };
-export type ActionImpactCategorie = Database['public']['Tables']['action_impact_categorie']['Row']
-export type FNV = Database['public']['Tables']['categorie_fnv']['Row']
-
-export type Panier =
-/* Le panier en tant que tel */
-  Database['public']['Tables']['panier']['Row'] & {
-  /* Liste des actions ajoutée au panier */
-  contenu: ActionImpact[],
-  /* Liste de toutes les actions avec leurs states. */
-  states: ActionImpactState[]
-}
-
-export type Niveau = { niveau: number, nom: string };
-
-export type ActionImpactDetails = ActionImpact & { 'categoriesFNV': FNV[] };
-
-/**
- * On sélectionne toutes les colonnes du panier : *
- * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `contenuPanier`
- */
-export const panierSelect = '*, contenu:action_impact!action_impact_panier(*), states:action_impact_state(*)';
-export const actionDetailsSelect = '*, categoriesFNV:categorie_fnv(*)';
+export * from "./actionImpactAPI";
+export * from "./panierAPI";
+export * from "./types";
+export * from "./types";
---
+++ packages/api/src/panier_action_impact/panierAPI.ts
@@ -0,0 +1,91 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
+
+/**
+ * On sélectionne toutes les colonnes du panier : *
+ * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `contenuPanier`
+ */
+export const panierSelect =
+  "*, contenu:action_impact!action_impact_panier(*), states:action_impact_state(*)";
+
+type RealtimePayload<T> = {
+  type: string;
+  schema: string;
+  table: string;
+  commit_timestamp: string;
+  errors: string[];
+  new: T;
+  old: Partial<T>;
+};
+
+export class PanierAPI {
+  protected supabase: SupabaseClient<Database>;
+
+  constructor(supabase: SupabaseClient<Database>) {
+    this.supabase = supabase;
+  }
+
+  listenToPanierUpdates(
+    panier_id: string,
+    onChange: (
+      payload: RealtimePayload<Database["public"]["Tables"]["panier"]["Row"]>,
+    ) => void,
+  ) {
+    return this.supabase.channel("realtime panier").on(
+      // @ts-ignore: Le typage Supabase à un souci.
+      // Typer avec en important `@supabase/realtime-js` pour y remédier fait planter webpack.
+      "postgres_changes",
+      {
+        event: "UPDATE",
+        schema: "public",
+        table: "panier",
+        filter: `id=eq.${panier_id}`,
+      },
+      onChange,
+    ).subscribe();
+  }
+
+  async addActionToPanier(action_id: number, panier_id: string): Promise<void> {
+    await this.supabase.from(
+      "action_impact_panier",
+    ).insert({
+      "action_id": action_id,
+      "panier_id": panier_id,
+    });
+  }
+
+  async removeActionFromPanier(
+    action_id: number,
+    panier_id: string,
+  ): Promise<void> {
+    await this.supabase.from(
+      "action_impact_panier",
+    )
+      .delete()
+      .eq("action_id", action_id)
+      .eq("panier_id", panier_id);
+  }
+
+  async setActionStatut(
+    action_id: number,
+    panier_id: string,
+    category_id: string | null,
+  ): Promise<void> {
+    if (category_id) {
+      await this.supabase.from(
+        "action_impact_statut",
+      ).upsert({
+        "action_id": action_id,
+        "panier_id": panier_id,
+        "categorie_id": category_id,
+      });
+    } else {
+      await this.supabase.from(
+        "action_impact_statut",
+      )
+        .delete()
+        .eq("action_id", action_id)
+        .eq("panier_id", panier_id);
+    }
+  }
+}
