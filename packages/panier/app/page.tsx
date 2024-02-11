b3df09f94260a29fb3001fd383956aed9dbc798a
Florian d'Erfurth <florian@derfurth.com>
2024-02-11 17:59:49 +0100

    Améliore la landing page pour les testeurs.

--- packages/panier/app/page.tsx
+++ packages/panier/app/page.tsx
@@ -1,9 +1,21 @@
-"use server";
+'use server';
 
 const LandingPage = async () => {
   return (
     <main>
       <h1>Panier action impact</h1>
+      <p>
+        Pour créer un nouveau panier sans lien avec aucune collectivité :
+      </p>
+      <pre>/landing</pre>
+      <p>
+        Pour créer un nouveau panier pour une collectivité, ou revenir sur le panier récent créé pour cette collectivité :
+      </p>
+      <pre>/landing/collectivite/[collectivite_id]</pre>
+      <p>
+        Pour revenir sur un panier existant :
+      </p>
+      <pre>/panier/[collectivite_id]</pre>
     </main>
   );
 };
