302e1fd28887568c26f13fdc7808d1325c53e2bf
Florian d'Erfurth <florian@derfurth.com>
2024-02-11 17:13:11 +0100

    Utilise le client Supabase pour l'API, reorganise les fichiers.

---
+++ packages/api/src/panier_action_impact/actionImpactAPI.ts
@@ -0,0 +1,23 @@
+import { ActionImpactDetails, Database } from "@tet/api";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export const actionDetailsSelect = "*, categoriesFNV:categorie_fnv(*)";
+
+export class ActionImpactAPI {
+  protected supabase: SupabaseClient<Database>;
+
+  constructor(supabase: SupabaseClient<Database>) {
+    this.supabase = supabase;
+  }
+
+  async fetchActionImpactDetails(
+    id: number,
+  ): Promise<ActionImpactDetails | null> {
+    const { data, error } = await this.supabase.from("action_impact")
+      .select(actionDetailsSelect)
+      .eq("id", id)
+      .single<ActionImpactDetails>();
+    if (error) throw error;
+    return data;
+  }
+}
