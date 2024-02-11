a6b6309a74ea59e687ea87267fcac71ae9156d9a
Florian d'Erfurth <florian@derfurth.com>
2024-02-11 17:14:11 +0100

    Type les différents clients.

--- packages/panier/src/supabase/client.ts
+++ packages/panier/src/supabase/client.ts
@@ -1,10 +1,11 @@
 import {createBrowserClient} from '@supabase/ssr';
 import {Database} from '@tet/api';
+import {SupabaseClient} from '@supabase/supabase-js';
 
 /**
  * Pour accéder à supabase depuis les composants client
  */
-export function createClient() {
+export function createClient(): SupabaseClient<Database> {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
