99bd0d9cca2dab8a4feb548333e21151ab99b1b6
Marc Rutkowski <marc@attractive-media.fr>
2024-02-12 17:45:38 +0100

    Applique correctement la redirection après vérification d'un magiclink

--- packages/site/app/auth/confirm/route.ts
+++ packages/site/app/auth/confirm/route.ts
@@ -1,42 +1,38 @@
 import {type EmailOtpType} from '@supabase/supabase-js';
 import {cookies} from 'next/headers';
 import {type NextRequest, NextResponse} from 'next/server';
-
 import {createClient} from 'src/supabase/server';
 
 /**
  * URL appelée pour valider un lien d'auth. reçu par mail.
  *
- * Echange le code reçu par mail par le token d'auth.
+ * Echange le code reçu par mail par le token d'auth. et redirige vers la page
+ * dont l'url est donnée par `searchParams.next`.
  */
 export const GET = async (request: NextRequest) => {
   const cookieStore = cookies();
 
-  const {searchParams} = new URL(request.url);
+  const {searchParams, hostname} = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
 
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete('token_hash');
-  redirectTo.searchParams.delete('type');
-
   if (token_hash && type) {
     const supabase = createClient(cookieStore);
 
-    const {error} = await supabase.auth.verifyOtp({
+    const {data, error} = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
-    if (!error) {
-      redirectTo.searchParams.delete('next');
-      return NextResponse.redirect(redirectTo);
+    if (!error && data.session) {
+      const cookieStore = cookies();
+      cookieStore.set('tet-access-token', data.session.access_token);
+      cookieStore.set('tet-refresh-token', data.session.refresh_token);
+      return NextResponse.redirect(next);
     }
     console.log('/auth/confirm error:', error);
   }
 
   // renvoi vers une page d'erreur
-  redirectTo.pathname = '/auth/error';
-  return NextResponse.redirect(redirectTo);
+  return NextResponse.redirect('/auth/error');
 };
