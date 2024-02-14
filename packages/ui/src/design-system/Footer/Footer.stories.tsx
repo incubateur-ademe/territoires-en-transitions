a55f44ac0ca6abb19047e971754d36f573c55178
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-02-14 10:56:34 +0100

    Supprime une icône inutile

--- packages/ui/src/design-system/Footer/Footer.stories.tsx
+++ packages/ui/src/design-system/Footer/Footer.stories.tsx
@@ -1,13 +1,12 @@
 import {Meta, StoryObj} from '@storybook/react';
 import {Footer} from './Footer';
-import LeafIcon from '@assets/LeafIcon';
 import {APP_BASE_URL, BASE_URL} from 'utils/constants';
 
 const meta: Meta<typeof Footer> = {
   title: 'Design System/Footer',
   component: Footer,
   args: {
-    logos: [<LeafIcon className="text-primary-9 h-3/4" />],
+    logos: [<i className="ri-leaf-line text-primary-9 !text-8xl" />],
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget maximus nisl, eget ullamcorper mauris. Nulla sagittis tempus leo nec elementum.',
     contentLinks: [
