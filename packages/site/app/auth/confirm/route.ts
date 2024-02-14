1e019d14a6060f6620c9cf5dc4c8eb2c363b606d
Marc Rutkowski <marc@attractive-media.fr>
2024-02-14 18:04:05 +0100

    Rend dynamique la détermination du domain racine utilisé pour le partage de session entre les sous-domaines

--- packages/site/app/auth/confirm/route.ts
+++ packages/site/app/auth/confirm/route.ts
@@ -1,4 +1,5 @@
 import {type EmailOtpType} from '@supabase/supabase-js';
+import {ACCESS_TOKEN, REFRESH_TOKEN, formatAuthToken} from '@tet/api';
 import {cookies} from 'next/headers';
 import {type NextRequest, NextResponse} from 'next/server';
 import {createClient} from 'src/supabase/server';
@@ -12,15 +13,10 @@ import {createClient} from 'src/supabase/server';
 export const GET = async (request: NextRequest) => {
   const cookieStore = cookies();
 
-  const {searchParams, hostname, host} = new URL(request.url);
+  const {searchParams} = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
-
-  console.log(request.url);
-  console.log(searchParams, hostname, host);
-  console.log(request.nextUrl.searchParams, request.nextUrl.hostname,
-    request.nextUrl.host);
 
   if (token_hash && type) {
     const supabase = createClient(cookieStore);
@@ -30,16 +26,21 @@ export const GET = async (request: NextRequest) => {
       token_hash,
     });
     if (!error && data.session) {
+      // Le partage des tokens entre sous-domaines nécessite le nom du domaine racine.
+      // On extrait celui-ci depuis l'url vers laquelle on doit rediriger.
+      const domain =
+        new URL(next).hostname.split('.').toSpliced(0, 1).join('.') ||
+        'localhost';
+
       // Enregistre les tokens dans le domaine racine pour pouvoir les partager entre les sous-domaines
-      const domain = 'territoiresentransitions.fr';
-      const response = NextResponse.redirect(next)
+      const response = NextResponse.redirect(next);
       response.headers.append(
         'Set-Cookie',
-        `tet-access-token=${data.session.access_token}; Domain=${domain}; Max-Age=${60 * 60 * 24 * 365}; Path=/; SameSite=Lax; Secure`
+        formatAuthToken(ACCESS_TOKEN, data.session.access_token, domain),
       );
       response.headers.append(
         'Set-Cookie',
-        `tet-refresh-token=${data.session.refresh_token}; Domain=${domain}; Max-Age=${60 * 60 * 24 * 365}; Path=/; SameSite=Lax; Secure`
+        formatAuthToken(REFRESH_TOKEN, data.session.refresh_token, domain),
       );
       return response;
     }
