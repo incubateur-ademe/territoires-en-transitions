eb84232fc9003273b1e0833c5effc2786ce5eda6
Marc Rutkowski <marc@attractive-media.fr>
2024-02-14 18:59:28 +0100

    Ajoute dans l'app la restauration de la session depuis les cookies

--- app.territoiresentransitions.react/src/core-logic/api/auth/useCurrentSession.tsx
+++ app.territoiresentransitions.react/src/core-logic/api/auth/useCurrentSession.tsx
@@ -1,11 +1,15 @@
 import {useQuery} from 'react-query';
+import {restoreSessionFromAuthTokens} from '@tet/api';
 import {supabaseClient} from '../supabase';
 import {ENV} from 'environmentVariables';
 
 export const useCurrentSession = () => {
-  const {data, error} = useQuery(['session'], () =>
-    supabaseClient.auth.getSession()
-  );
+  const {data, error} = useQuery(['session'], async () => {
+    // restaure une éventuelle session précédente
+    await restoreSessionFromAuthTokens(supabaseClient);
+
+    return supabaseClient.auth.getSession();
+  });
   if (error || !data?.data?.session) {
     return null;
   }
