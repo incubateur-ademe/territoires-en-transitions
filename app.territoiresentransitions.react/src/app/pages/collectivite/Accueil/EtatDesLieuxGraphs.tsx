7c2bb8694a9bd041a95c1aa43da82941881b59e9
Arthur Molinos <>
2024-02-19 12:26:01 +0100

    Supprime un composant utilisé qu'une seule fois

--- app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/EtatDesLieuxGraphs.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/EtatDesLieuxGraphs.tsx
@@ -2,8 +2,11 @@ import {ReferentielParamOption} from 'app/paths';
 import classNames from 'classnames';
 import {TableOptions} from 'react-table';
 import {ProgressionRow} from '../EtatDesLieux/Synthese/data/useProgressionReferentiel';
-import ProgressionParPhase from '../EtatDesLieux/Synthese/ProgressionParPhase';
 import ProgressionReferentiel from '../EtatDesLieux/Synthese/ProgressionReferentiel';
+import {defaultColors} from 'ui/charts/chartsTheme';
+import {referentielToName} from 'app/labels';
+import {toLocaleFixed} from 'utils/toFixed';
+import ChartCard from 'ui/charts/ChartCard';
 
 type EtatDesLieuxGraphsProps = {
   referentiel: ReferentielParamOption;
@@ -34,6 +37,14 @@ const EtatDesLieuxGraphs = ({
     height: '415px',
   };
 
+  const scoreTotal =
+    Math.round(
+      repartitionPhases.reduce(
+        (total, currValue) => (total += currValue.value),
+        0
+      ) * 10
+    ) / 10;
+
   return displayEtatDesLieux ? (
     <div className={classNames('grid grid-cols-1 gap-6', className)}>
       <ProgressionReferentiel
@@ -47,9 +58,28 @@ const EtatDesLieuxGraphs = ({
         percentage
         customStyle={graphStyles}
       />
-      <ProgressionParPhase
-        repartitionPhases={repartitionPhases}
-        referentiel={referentiel}
+      {/** Progression par phase */}
+      <ChartCard
+        chartType="donut"
+        chartProps={{
+          data: repartitionPhases,
+          label: true,
+          displayPercentageValue: true,
+        }}
+        chartInfo={{
+          title: `Répartition du score "Réalisé" par phase (${
+            scoreTotal > 1
+              ? Math.round(scoreTotal)
+              : toLocaleFixed(scoreTotal, 2)
+          } point${Math.round(scoreTotal) <= 1 ? '' : 's'})`,
+          subtitle: referentielToName[referentiel],
+          legend: repartitionPhases.map((el, index) => ({
+            name: el.id,
+            color: defaultColors[index % defaultColors.length],
+          })),
+          expandable: true,
+          downloadedFileName: `${referentiel}-realise-par-phase`,
+        }}
         customStyle={graphStyles}
       />
     </div>
