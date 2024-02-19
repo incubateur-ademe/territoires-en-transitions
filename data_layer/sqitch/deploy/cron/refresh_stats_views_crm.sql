dfe5438ab4f5fa7ce0fc03223439d9c58df69db8
Florian d'Erfurth <florian@derfurth.com>
2024-02-19 11:16:42 +0100

    Ajoute le timeout au `refresh_stats_views_crm`.

--- data_layer/sqitch/deploy/cron/refresh_stats_views_crm.sql
+++ data_layer/sqitch/deploy/cron/refresh_stats_views_crm.sql
@@ -4,6 +4,6 @@ BEGIN;
 
 select cron.schedule('refresh_stats_views_crm',
                      '0 3 * * *', -- tout les jours à 3h.
-                     $$select stats.refresh_views_crm();$$);
+                     $$set statement_timeout to 3600000; select stats.refresh_views_crm();$$);
 
 COMMIT;
