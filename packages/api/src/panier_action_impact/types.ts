20aa228c635c385331d3a86dd861ea6b05b40edb
Florian d'Erfurth <florian@derfurth.com>
2024-02-20 09:04:12 +0100

    Ajoute les thématiques. Différentie le typage du contenu de la modale et du panier.

--- packages/api/src/panier_action_impact/types.ts
+++ packages/api/src/panier_action_impact/types.ts
@@ -1,26 +1,43 @@
 import { Database } from "../database.types";
 
-export type ActionImpact = Database["public"]["Tables"]["action_impact"]["Row"];
+export type ActionImpactCategorie =
+  Database["public"]["Tables"]["action_impact_categorie"]["Row"];
+
+export type ActionImpactThematique =
+  Database["public"]["Tables"]["action_impact_thematique"]["Row"];
+
+export type FNV = Database["public"]["Tables"]["categorie_fnv"]["Row"];
+
+export type ActionImpact =
+  Database["public"]["Tables"]["action_impact"]["Row"];
+
+/* Le resumé d'une action à impact, utilisé pour les cartes  */
+export type ActionImpactSnippet =
+  Omit<ActionImpact, 'description' | 'ressources_externes'> & {thematiques: ActionImpactThematique[]};
+
+/* Une action à impact avec des informations complémentaires, utilisé par la modale */
+export type ActionImpactDetails =
+  ActionImpact &
+  { thematiques: ActionImpactThematique[] } &
+  { categoriesFNV: FNV[] };
+
 export type ActionImpactStatut =
   Database["public"]["Tables"]["action_impact_statut"]["Row"];
+
 export type ActionImpactState = {
   action: ActionImpact;
   isinpanier: boolean;
   statut: ActionImpactStatut | null;
 };
-export type ActionImpactCategorie =
-  Database["public"]["Tables"]["action_impact_categorie"]["Row"];
-export type FNV = Database["public"]["Tables"]["categorie_fnv"]["Row"];
 
 export type Panier =
   /* Le panier en tant que tel */
   Database["public"]["Tables"]["panier"]["Row"] & {
     /* Liste des actions ajoutée au panier */
-    contenu: ActionImpact[];
+    contenu: ActionImpactSnippet[];
     /* Liste de toutes les actions avec leurs states. */
     states: ActionImpactState[];
   };
 
 export type Niveau = { niveau: number; nom: string };
 
-export type ActionImpactDetails = ActionImpact & { "categoriesFNV": FNV[] };
