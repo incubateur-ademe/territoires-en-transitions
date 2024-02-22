e8c2e55db3de7e5d887bc7f53a04ffb9d6376773
Florian d'Erfurth <florian@derfurth.com>
2024-02-22 09:30:22 +0100

    Permet de filter par thématique.

--- packages/api/src/panier_action_impact/panierAPI.ts
+++ packages/api/src/panier_action_impact/panierAPI.ts
@@ -1,13 +1,13 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Database } from "../database.types";
-import { Panier } from "./types";
+import {SupabaseClient} from '@supabase/supabase-js';
+import {Database} from '../database.types';
+import {Panier} from './types';
 
 /**
  * On sélectionne toutes les colonnes du panier : *
  * puis les `action_impact` par la relation `action_impact_panier` que l'on renomme `contenuPanier`
  */
 export const panierSelect =
-  "*, contenu:action_impact!action_impact_panier(*,thematiques:thematique(*)),states:action_impact_state(*)";
+  '*, contenu:action_impact!action_impact_panier(*,thematiques:thematique(*)),states:action_impact_state(*,thematiques:thematique(*)))';
 
 type RealtimePayload<T> = {
   type: string;
@@ -29,66 +29,87 @@ export class PanierAPI {
   listenToPanierUpdates(
     panier_id: string,
     onChange: (
-      payload: RealtimePayload<Database["public"]["Tables"]["panier"]["Row"]>,
-    ) => void,
+      payload: RealtimePayload<Database['public']['Tables']['panier']['Row']>
+    ) => void
   ) {
-    return this.supabase.channel("realtime panier").on(
-      // @ts-ignore: Le typage Supabase à un souci.
-      // Typer avec en important `@supabase/realtime-js` pour y remédier fait planter webpack.
-      "postgres_changes",
-      {
-        event: "UPDATE",
-        schema: "public",
-        table: "panier",
-        filter: `id=eq.${panier_id}`,
-      },
-      onChange,
-    ).subscribe();
+    return this.supabase
+      .channel('realtime panier')
+      .on(
+        // @ts-ignore: Le typage Supabase à un souci.
+        // Typer avec en important `@supabase/realtime-js` pour y remédier fait planter webpack.
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'panier',
+          filter: `id=eq.${panier_id}`,
+        },
+        onChange
+      )
+      .subscribe();
   }
 
   async addActionToPanier(action_id: number, panier_id: string): Promise<void> {
-    await this.supabase.from(
-      "action_impact_panier",
-    ).insert({
-      "action_id": action_id,
-      "panier_id": panier_id,
+    await this.supabase.from('action_impact_panier').insert({
+      action_id: action_id,
+      panier_id: panier_id,
     });
   }
 
   async removeActionFromPanier(
     action_id: number,
-    panier_id: string,
+    panier_id: string
   ): Promise<void> {
-    await this.supabase.from(
-      "action_impact_panier",
-    ).delete().eq("action_id", action_id).eq("panier_id", panier_id);
+    await this.supabase
+      .from('action_impact_panier')
+      .delete()
+      .eq('action_id', action_id)
+      .eq('panier_id', panier_id);
   }
 
   async setActionStatut(
     action_id: number,
     panier_id: string,
-    category_id: string | null,
+    category_id: string | null
   ): Promise<void> {
     if (category_id) {
-      await this.supabase.from(
-        "action_impact_statut",
-      ).upsert({
-        "action_id": action_id,
-        "panier_id": panier_id,
-        "categorie_id": category_id,
+      await this.supabase.from('action_impact_statut').upsert({
+        action_id: action_id,
+        panier_id: panier_id,
+        categorie_id: category_id,
       });
     } else {
-      await this.supabase.from(
-        "action_impact_statut",
-      ).delete().eq("action_id", action_id).eq("panier_id", panier_id);
+      await this.supabase
+        .from('action_impact_statut')
+        .delete()
+        .eq('action_id', action_id)
+        .eq('panier_id', panier_id);
     }
   }
 
-  async fetchPanier(panier_id: string): Promise<Panier | null> {
-    const { data, error } = await this.supabase.from("panier")
+  async fetchPanier(
+    panier_id: string,
+    thematique_ids: string[]
+  ): Promise<Panier | null> {
+    const builder = this.supabase
+      .from('panier')
       .select(panierSelect)
-      .eq("id", panier_id)
-      .single<Panier>();
+      .eq('id', panier_id);
+
+    if (thematique_ids && thematique_ids.length > 0) {
+      // @ts-expect-error Le client Supabase ne permet pas de filtrer à ce niveau
+      builder.url.searchParams.append(
+        'action_impact_state.thematiques.id',
+        `in.(${thematique_ids.join(',')})`
+      );
+      // @ts-expect-error Le client Supabase ne permet pas de filtrer à ce niveau
+      builder.url.searchParams.append(
+        'action_impact_state.thematiques',
+        'not.is.null'
+      );
+    }
+
+    const {data, error} = await builder.single<Panier>();
     if (error) throw error;
     return data;
   }
