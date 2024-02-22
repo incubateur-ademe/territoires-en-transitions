92e45071d4b7140f7ac250379a8ddb2b6bf4802e
Amandine Jacquelin <conta@jacquelinamandine.fr>
2024-02-22 16:21:17 +0100

    Ajoute la contrainte des quatre ans pour vérifier qu'une labellisation existe déjà

--- data_layer/sqitch/verify/labellisation/audit.sql
+++ data_layer/sqitch/verify/labellisation/audit.sql
@@ -2,22 +2,6 @@
 
 BEGIN;
 
-select id,
-       collectivite_id,
-       referentiel,
-       demande_id,
-       date_debut,
-       date_fin,
-       valide,
-       date_cnl,
-       valide_labellisation,
-       clos
-from labellisation.audit
-where false;
-
-select has_function_privilege('labellisation.update_audit()', 'execute');
-select has_function_privilege('labellisation.audit_personnalisation_payload(integer, boolean, text)', 'execute');
 select has_function_privilege('labellisation.update_labellisation_after_scores()', 'execute');
-select has_function_privilege('labellisation_validate_audit(integer, boolean)', 'execute');
 
 ROLLBACK;
---
+++ data_layer/sqitch/verify/labellisation/audit@v2.101.0.sql
@@ -0,0 +1,23 @@
+-- Verify tet:labellisation/audit on pg
+
+BEGIN;
+
+select id,
+       collectivite_id,
+       referentiel,
+       demande_id,
+       date_debut,
+       date_fin,
+       valide,
+       date_cnl,
+       valide_labellisation,
+       clos
+from labellisation.audit
+where false;
+
+select has_function_privilege('labellisation.update_audit()', 'execute');
+select has_function_privilege('labellisation.audit_personnalisation_payload(integer, boolean, text)', 'execute');
+select has_function_privilege('labellisation.update_labellisation_after_scores()', 'execute');
+select has_function_privilege('labellisation_validate_audit(integer, boolean)', 'execute');
+
+ROLLBACK;
