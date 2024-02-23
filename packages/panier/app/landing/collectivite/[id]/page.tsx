42c0d506481e44948efee85381e8f9748eeb5b85
Florian d'Erfurth <florian@derfurth.com>
2024-02-23 10:11:30 +0100

    Ajoute le tracking des landing pages.

--- packages/panier/app/landing/collectivite/[id]/page.tsx
+++ packages/panier/app/landing/collectivite/[id]/page.tsx
@@ -1,5 +1,12 @@
 import Landing from '@components/Landing';
+import React from 'react';
+import TrackPageView from '@components/TrackPageView';
 
 export default async function Page({params}: {params: {id: string}}) {
-  return <Landing />;
+  return (
+    <>
+      <TrackPageView pageName="landing/collectivite" />
+      <Landing />
+    </>
+  );
 }
