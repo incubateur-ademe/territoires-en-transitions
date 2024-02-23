fb5b8d453c4587b5235d0e85d2b21298c722028c
Florian d'Erfurth <florian@derfurth.com>
2024-02-23 10:11:30 +0100

    Ajoute le tracking des landing pages.

--- packages/panier/components/Landing/CestParti.tsx
+++ packages/panier/components/Landing/CestParti.tsx
@@ -2,14 +2,20 @@
 
 import {panierAPI} from 'src/clientAPI';
 import {useParams, useRouter} from 'next/navigation';
+import {useEventTracker} from 'src/tracking/useEventTracker';
 
 export default function CestParti() {
   const params = useParams();
   const router = useRouter();
   const id = params['id'];
   const collectivite_id = id ? parseInt(id as string) : undefined;
+  const tracker = useEventTracker(
+    collectivite_id ? 'landing/collectivite' : 'landing'
+  );
   const onClick = async () => {
     const base = await panierAPI.panierFromLanding(collectivite_id);
+    // @ts-ignore
+    tracker('cta_panier', {});
     router.push(`/panier/${base.id}`);
   };
 
--- packages/panier/src/tracking/trackingPlan.ts
+++ packages/panier/src/tracking/trackingPlan.ts
@@ -1,8 +1,6 @@
-interface EventProperties extends Record<string, string | number | null> {
-}
+interface EventProperties extends Record<string, string | number | null> {}
 
-interface Events extends Record<string, EventProperties> {
-}
+interface Events extends Record<string, EventProperties> {}
 
 interface Page {
   properties: EventProperties;
@@ -13,11 +11,7 @@ interface Page {
 /**
  * La liste des pages
  */
-export type PageName =
-  '/' |
-  'landing' |
-  'landing/collectivite' |
-  'panier';
+export type PageName = '/' | 'landing' | 'landing/collectivite' | 'panier';
 
 /**
  * Permet de respecter le plan de tracking.
@@ -25,46 +19,57 @@ export type PageName =
 export interface TrackingPlan extends Record<PageName, Page> {
   /* La page à la racine de https://panier.territoiresentransitions.fr */
   '/': {
-    onglets: never; events: never; properties: never;
+    properties: never;
+    onglets: never;
+    events: never;
   };
 
   /* La landing générique, qui crée et renvoie sur un nouveau panier */
   landing: {
-    onglets: never; events: never; properties: never;
+    properties: never;
+    onglets: never;
+    events: {
+      cta_panier_click: {};
+    };
   };
 
   /* La landing pour une collectivité, qui crée et renvoie sur un panier récent ou un nouveau panier. */
   'landing/collectivite': {
-    onglets: never; events: never; properties: never;
+    properties: {
+      collectivite_id: number;
+    };
+    onglets: never;
+    events: {
+      cta_panier_click: {};
+    };
   };
 
   /* La page d'un panier d'action */
-  'panier': {
+  panier: {
     /* les propriétés de tous événements de la page panier */
     properties: {
-      collectivite_preset: number | null,
-      panier_id: string,
-    }
+      collectivite_preset: number | null;
+      panier_id: string;
+    };
 
     /* les onglets principaux */
     onglets: 'selection' | 'réalisées' | 'en cours' | 'non pertinentes';
 
     /* les events du panier */
-    events:
-      {
-        /* Ajoute une action au panier */
-        ajout: {
-          action_id: number
-        },
-        /* Retire une action au panier */
-        retrait: {
-          action_id: number
-        }
-        /* Change le statut d'une action */
-        statut: {
-          action_id: number;
-          category_id: string | null;
-        }
-      }
+    events: {
+      /* Ajoute une action au panier */
+      ajout: {
+        action_id: number;
+      };
+      /* Retire une action au panier */
+      retrait: {
+        action_id: number;
+      };
+      /* Change le statut d'une action */
+      statut: {
+        action_id: number;
+        category_id: string | null;
+      };
+    };
   };
 }
