938e34c23471b0d80c09c90903668c98fcc815b3
Florian d'Erfurth <florian@derfurth.com>
2024-02-23 15:25:11 +0100

    Corrige le typage du tracker.

--- packages/panier/src/tracking/useEventTracker.ts
+++ packages/panier/src/tracking/useEventTracker.ts
@@ -6,15 +6,14 @@ import {usePostHog} from 'posthog-js/react';
  *
  * le typage permet de respecter le plan de tracking.
  */
-type PageEventTracker =
-  <
-    E extends TrackingPlan[PageName]['events'],
-    P extends TrackingPlan[PageName]['properties'],
-    K extends string & keyof TrackingPlan[PageName]['events']
-  >(
-    event: K,
-    properties: E[K] & P,
-  ) => Promise<void>
+type PageEventTracker<N extends PageName> = <
+  E extends TrackingPlan[N]['events'],
+  P extends TrackingPlan[N]['properties'],
+  K extends string & keyof TrackingPlan[N]['events']
+>(
+  event: K,
+  properties: E[K] & P
+) => Promise<void>;
 
 /**
  * Renvoi le tracker pour enregistrer les évenements d'une page
@@ -22,22 +21,19 @@ type PageEventTracker =
  * @param pageName le pathname de la page
  * @param onglet l'onglet à partir duquel les événements sont émis
  */
-export function useEventTracker(
-  pageName: PageName,
-  onglet?: TrackingPlan[PageName]['onglets'],
-): PageEventTracker {
+export function useEventTracker<N extends PageName>(
+  pageName: N,
+  onglet?: TrackingPlan[PageName]['onglets']
+): PageEventTracker<N> {
   const posthog = usePostHog();
 
   return async (event, properties) => {
     if (posthog) {
-      posthog.capture(
-        event,
-        {
-          '$current_url': pageName,
-          onglet: onglet,
-          ...properties,
-        },
-      );
+      posthog.capture(event, {
+        $current_url: pageName,
+        onglet: onglet,
+        ...properties,
+      });
     } else {
       console.error(`PostHog is not ready.`);
     }
