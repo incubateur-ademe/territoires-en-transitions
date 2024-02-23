0a32bd6218b14358e4ede9ff79d8e4b6701d9177
Florian d'Erfurth <florian@derfurth.com>
2024-02-23 10:11:30 +0100

    Ajoute le tracking des landing pages.

--- packages/panier/app/landing/page.tsx
+++ packages/panier/app/landing/page.tsx
@@ -1,5 +1,12 @@
 import Landing from '@components/Landing';
+import TrackPageView from '@components/TrackPageView/index';
+import React from 'react';
 
 export default async function Page() {
-  return <Landing />;
+  return (
+    <>
+      <TrackPageView pageName="landing" />
+      <Landing />
+    </>
+  );
 }
