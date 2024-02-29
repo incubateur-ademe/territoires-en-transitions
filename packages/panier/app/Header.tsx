cf0875918ab38f79e6fbc0d1c603822cc24838c0
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-02-29 11:38:09 +0100

    Mise à jour du header

--- packages/panier/app/Header.tsx
+++ packages/panier/app/Header.tsx
@@ -1,35 +1,22 @@
 'use client';
 
 import {Button, HeaderTeT} from '@tet/ui';
-import {APP_BASE_URL, SITE_BASE_URL} from 'src/utils/constants';
+import {SITE_BASE_URL} from 'src/utils/constants';
 
 const Header = () => {
   return (
     <HeaderTeT
+      customRootUrl="/landing"
       quickAccessButtons={props => [
         <Button
           {...props}
           key="outil"
           icon="seedling-line"
           href={`${SITE_BASE_URL}/outil-numerique`}
+          iconPosition="left"
+          external
         >
           Qui sommes-nous ?
-        </Button>,
-        <Button
-          {...props}
-          key="signup"
-          icon="user-add-line"
-          href={`${APP_BASE_URL}/auth/signup`}
-        >
-          Créer un compte
-        </Button>,
-        <Button
-          {...props}
-          key="signin"
-          icon="user-line"
-          href={`${APP_BASE_URL}/auth/signin`}
-        >
-          Se connecter
         </Button>,
       ]}
     />
