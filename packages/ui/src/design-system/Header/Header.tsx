13c831de482e41b0ffae59ba7cc35c95a1acc0db
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-03-05 11:23:43 +0100

    Ajout d'un composant Header au design system

---
+++ packages/ui/src/design-system/Header/Header.tsx
@@ -0,0 +1,55 @@
+import classNames from 'classnames';
+import {Button} from '@design-system/Button';
+import {ReactElement, useState} from 'react';
+import {ButtonProps} from '@design-system/Button/types';
+import HeaderBody from './HeaderBody';
+import HeaderMenu from './HeaderMenu';
+
+type HeaderProps = {
+  /** Titre du header */
+  title: string;
+  /** Sous-titre */
+  subtitle?: string;
+  /** Liste de logos à afficher à gauche du header. */
+  logos?: React.ReactNode[];
+  /** Url custom lors du clic sur le titre */
+  customRootUrl?: string;
+  /** Menu accès rapide */
+  quickAccessButtons?: (props: ButtonProps) => ReactElement<typeof Button>[];
+  /** Surcharge des classNames. */
+  className?: string;
+};
+
+/**
+ * Composant Header générique
+ */
+
+export const Header = ({
+  title,
+  subtitle,
+  logos,
+  customRootUrl,
+  quickAccessButtons,
+  className,
+}: HeaderProps) => {
+  const [openedMenu, setOpenedMenu] = useState(false);
+
+  return (
+    <header className={classNames('w-full bg-white relative', className)}>
+      {/* Partie suppérieure du header */}
+      <HeaderBody
+        {...{
+          title,
+          subtitle,
+          logos,
+          customRootUrl,
+          quickAccessButtons,
+          setOpenedMenu,
+        }}
+      />
+
+      {/* Partie inférieure du header - Barre de navigation */}
+      <HeaderMenu {...{quickAccessButtons, openedMenu, setOpenedMenu}} />
+    </header>
+  );
+};
