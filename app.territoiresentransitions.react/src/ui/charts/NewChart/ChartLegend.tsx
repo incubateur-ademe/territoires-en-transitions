0b81a542215362582298276856f231569c784262
Arthur Molinos <>
2024-03-05 16:45:50 +0100

    Ajoute une légende générique pour dans DonutChart

---
+++ app.territoiresentransitions.react/src/ui/charts/NewChart/ChartLegend.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+
+/** Élement représentatif d'une donnée dans la légende */
+export type ChartLegendItem = {
+  /** Nom affiché */
+  name: string;
+  /** Couleur du symbole */
+  color: string;
+  /** Symbole custom */
+  symbole?: (color: string) => React.ReactNode;
+};
+
+/** Propriété de la légende par défaut */
+export type ChartLegendProps = {
+  /** Etat d'ouverture */
+  isOpen: boolean;
+  /** Les éléments représentants les données de la légende */
+  items?: ChartLegendItem[];
+  /** Taille des éléments de la légende */
+  size?: 'sm' | 'md';
+  /** Styles donnés au container */
+  className?: string;
+  /** Limitation du nombre d'éléments visibles */
+  maxItems?: number;
+};
+
+/** Légende par défaut utilisée dans les charts */
+const ChartLegend = ({
+  className,
+  items = [],
+  size = 'md',
+  maxItems,
+}: ChartLegendProps) => {
+  const displayedItems = items.slice(0, maxItems) ?? [];
+
+  return (
+    <div
+      className={classNames(
+        'flex items-center justify-center flex-wrap gap-x-8 gap-y-4 mx-auto',
+        {
+          'gap-x-4 gap-y-2 text-sm': size === 'sm',
+        },
+        className
+      )}
+    >
+      {displayedItems.map(({name, color, symbole}) => (
+        <div key={name} className="flex items-center gap-2">
+          {!!symbole ? (
+            symbole(color)
+          ) : (
+            <div
+              className="w-3 h-3 rounded-full"
+              style={{backgroundColor: color}}
+            />
+          )}
+          <div>{name}</div>
+        </div>
+      ))}
+      {!!maxItems && items.length > maxItems && (
+        <div>+ {items.length - maxItems}</div>
+      )}
+    </div>
+  );
+};
+
+export default ChartLegend;
