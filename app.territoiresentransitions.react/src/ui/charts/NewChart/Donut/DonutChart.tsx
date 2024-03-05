6c322a685a47cbe4928a3f8d81a1929732ab62bd
Arthur Molinos <>
2024-03-05 16:15:41 +0100

    Re-organise les props du Donut

--- app.territoiresentransitions.react/src/ui/charts/NewChart/Donut/DonutChart.tsx
+++ app.territoiresentransitions.react/src/ui/charts/NewChart/Donut/DonutChart.tsx
@@ -1,13 +1,9 @@
-import {
-  CompletePieSvgProps,
-  MouseEventHandler,
-  PieSvgProps,
-  ResponsivePie,
-} from '@nivo/pie';
+import {ResponsivePie} from '@nivo/pie';
 
 import {defaultColors, nivoColorsSet, theme} from '../../chartsTheme';
 import {getPercentage, skipArcLinkLabel} from './utils';
 import {getDonutTooltip} from './DonutTooltip';
+import classNames from 'classnames';
 
 /** Format de données du composant Pie de nivo*/
 export type DonutData = {
@@ -30,6 +26,8 @@ export type DonutChartProps = {
   centeredElement?: React.ReactNode;
   /** Clique sur un arc */
   onClick?: () => void;
+  /** Classname du container, permet notamment de donner la hauteur du graphe.*/
+  className?: string;
 };
 
 /**
@@ -44,6 +42,7 @@ const DonutChart = ({
   displayPercentageValue = false,
   centeredElement,
   onClick,
+  className,
 }: DonutChartProps) => {
   /** Vérifie si le tableau de données est vide */
   const hasNoData =
@@ -63,56 +62,61 @@ const DonutChart = ({
   }));
 
   return (
-    <div className="h-full relative">
-      {/** Élement central */}
-      <div className="absolute inset-0 flex">
-        <div className="m-auto text-center">{centeredElement}</div>
-      </div>
-      {/** Chart */}
-      <ResponsivePie
-        data={hasNoData ? defaultData : localData}
-        valueFormat={value =>
-          displayPercentageValue
-            ? `${getPercentage(
-                value,
-                localData.map(d => d.value)
-              )} %`
-            : `${Math.round(value)}`
-        }
-        tooltip={datum => !hasNoData && getDonutTooltip(datum, localData, unit)}
-        layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends']}
-        onClick={onClick}
-        enableArcLinkLabels={
-          (displayOutsideLabel || hasNoData) && !skipArcLinkLabel(data)
-        }
-        enableArcLabels={hasNoData ? false : true}
-        /** Styles */
-        theme={theme}
-        onMouseEnter={(_datum, event) => {
-          if (!!onClick) {
-            event.currentTarget.style.cursor = 'pointer';
+    <div className="flex flex-col">
+      <div className={classNames('relative h-64', className)}>
+        {/** Élement central */}
+        <div className="absolute inset-0 flex">
+          <div className="m-auto text-center">{centeredElement}</div>
+        </div>
+        {/** Chart */}
+        <ResponsivePie
+          data={hasNoData ? defaultData : localData}
+          valueFormat={value =>
+            displayPercentageValue
+              ? `${getPercentage(
+                  value,
+                  localData.map(d => d.value)
+                )} %`
+              : `${Math.round(value)}`
           }
-        }}
-        animate={true}
-        margin={{top: 24, right: 24, bottom: 24, left: 24}}
-        // Taille du rond vide central
-        innerRadius={0.5}
-        // Distance entre les arcs
-        padAngle={1}
-        /** Styles des arcs */
-        cornerRadius={4}
-        // Agrandissement de l'arc au hover
-        activeOuterRadiusOffset={!hasNoData ? 8 : undefined}
-        borderWidth={1}
-        borderColor={{from: 'color', modifiers: [['darker', 0.2]]}}
-        arcLinkLabelsDiagonalLength={12}
-        arcLinkLabelsStraightLength={6}
-        arcLinkLabelsTextColor="#333"
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{from: 'color'}}
-        arcLabelsSkipAngle={12}
-        arcLabelsTextColor={{from: 'color', modifiers: [['darker', 2]]}}
-      />
+          tooltip={datum =>
+            !hasNoData && getDonutTooltip(datum, localData, unit)
+          }
+          layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends']}
+          onClick={onClick}
+          enableArcLinkLabels={
+            (displayOutsideLabel || hasNoData) && !skipArcLinkLabel(data)
+          }
+          enableArcLabels={hasNoData ? false : true}
+          /** Styles */
+          theme={theme}
+          colors={{datum: 'data.color'}}
+          onMouseEnter={(_datum, event) => {
+            if (!!onClick) {
+              event.currentTarget.style.cursor = 'pointer';
+            }
+          }}
+          animate={true}
+          margin={{top: 24, right: 24, bottom: 24, left: 24}}
+          // Taille du rond vide central
+          innerRadius={0.5}
+          // Distance entre les arcs
+          padAngle={1}
+          /** Styles des arcs */
+          cornerRadius={4}
+          // Agrandissement de l'arc au hover
+          activeOuterRadiusOffset={!hasNoData ? 8 : undefined}
+          borderWidth={1}
+          borderColor={{from: 'color', modifiers: [['darker', 0.2]]}}
+          arcLinkLabelsDiagonalLength={12}
+          arcLinkLabelsStraightLength={6}
+          arcLinkLabelsTextColor="#333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{from: 'color'}}
+          arcLabelsSkipAngle={12}
+          arcLabelsTextColor={{from: 'color', modifiers: [['darker', 2]]}}
+        />
+      </div>
     </div>
   );
 };
