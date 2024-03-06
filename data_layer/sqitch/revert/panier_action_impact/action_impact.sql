62033450ae4fe416a845ddca004f175d57fdc109
Amandine Jacquelin <conta@jacquelinamandine.fr>
2024-03-06 14:49:58 +0100

    Ajout du temps de mise en oeuvre à l'action

--- data_layer/sqitch/revert/panier_action_impact/action_impact.sql
+++ data_layer/sqitch/revert/panier_action_impact/action_impact.sql
@@ -12,6 +12,7 @@ drop table action_impact;
 drop table action_impact_tier;
 drop table action_impact_fourchette_budgetaire;
 drop table action_impact_complexite;
+drop table action_impact_temps_mise_en_oeuvre;
 drop table categorie_fnv;
 
 COMMIT;
