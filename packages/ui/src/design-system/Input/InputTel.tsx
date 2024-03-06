6b5417ab28b0869cf770fba15070fd8f9a7b595e
Marc Rutkowski <marc@attractive-media.fr>
2024-03-06 15:50:16 +0100

    Ajoute le composant de saisie de numéro de tel

---
+++ packages/ui/src/design-system/Input/InputTel.tsx
@@ -0,0 +1,35 @@
+import {Ref, forwardRef} from 'react';
+import {PatternFormat, PatternFormatProps} from 'react-number-format';
+import {InputBase, InputBaseProps} from './InputBase';
+import {validateInputNumLength} from '@design-system/Input/validateInputNumLength';
+
+export type InputTelProps = Omit<
+  PatternFormatProps,
+  'type' | 'value' | 'format' | 'mask' | 'allowEmptyFormatting'
+> & {
+  value?: InputBaseProps['value'];
+};
+
+// renvoi le numéro de tel saisi si il est valide
+export const validateTel = (value: string) => validateInputNumLength(value, 10);
+
+/**
+ * Affiche un champ de saisie d'un numéro de téléphone (10 chiffres)
+ *
+ * La saisie des caractères non valides est ignorée.
+ * La valeur est formatée automatiquement suivant le format donné.
+ */
+export const InputTel = forwardRef(
+  ({value, ...remainingProps}: InputTelProps, ref?: Ref<HTMLInputElement>) => {
+    return (
+      <PatternFormat
+        type="tel"
+        format="## ## ## ## ##"
+        customInput={InputBase}
+        getInputRef={ref}
+        value={value}
+        {...remainingProps}
+      />
+    );
+  }
+);
