90c897b22c546da7acbf3549bb1ee494f127877a
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-03-11 18:08:07 +0100

    Mise à jour de la modale de validation

--- packages/ui/src/design-system/Divider/Divider.tsx
+++ packages/ui/src/design-system/Divider/Divider.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 
 type DividerProps = {
+  /** Permet de modifier la couleur par défaut */
+  color?: 'light' | 'medium';
   /** Permet d'ajuster le style */
   className?: string;
   // todo? ajout d'une orientation et d'un variant
@@ -10,11 +12,15 @@ type DividerProps = {
  * Renvoie un <hr /> stylisé avec le thème du design system
  */
 
-export const Divider = ({className}: DividerProps) => {
+export const Divider = ({color = 'light', className}: DividerProps) => {
   return (
     <hr
       className={classNames(
-        'bg-gradient-to-r from-primary-3 to-primary-3',
+        'w-full bg-gradient-to-r',
+        {
+          'from-primary-3 to-primary-3': color === 'light',
+          'from-primary-5 to-primary-5': color === 'medium',
+        },
         className
       )}
     />
