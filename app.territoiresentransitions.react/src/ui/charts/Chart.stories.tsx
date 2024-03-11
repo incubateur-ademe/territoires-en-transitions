911f5a9541878357bb0d19d29f043331db788dc5
Arthur Molinos <>
2024-03-12 00:20:20 +0100

    Crèation d'un composant chart générique a utilisé pour les tout. type de graphique ainsi que leur téléchargement

---
+++ app.territoiresentransitions.react/src/ui/charts/Chart.stories.tsx
@@ -0,0 +1,125 @@
+import React, {useState} from 'react';
+import {Meta, StoryObj} from '@storybook/react';
+
+import {Button} from '@tet/ui';
+
+import Chart from './Chart.tsx';
+import {fakeComplexeDonutData, fakeSimpleDonutData} from './Donut/fixtures.tsx';
+import {additionalInfos, fakeChartInfos} from './fixtures.tsx';
+import {fakeManyXLineData} from './Line/fixtures.tsx';
+
+const meta: Meta<typeof Chart> = {
+  component: Chart,
+  args: {
+    donut: {
+      chart: {
+        data: fakeSimpleDonutData,
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Chart>;
+
+export const Defaut: Story = {};
+
+export const AvecLegend: Story = {
+  args: {
+    donut: {
+      chart: {
+        data: fakeSimpleDonutData,
+        legend: {
+          isOpen: true,
+        },
+      },
+    },
+  },
+};
+
+export const Charts: Story = {
+  args: {},
+  render: () => {
+    return (
+      <div className="flex flex-col gap-6">
+        <Chart
+          donut={{
+            chart: {data: fakeSimpleDonutData},
+          }}
+        />
+        <div className="h-px bg-grey-4" />
+        <Chart
+          line={{
+            chart: {data: fakeManyXLineData},
+          }}
+        />
+      </div>
+    );
+  },
+};
+
+export const Download = () => {
+  const [donutOpen, setDonutOpen] = useState(false);
+  const [lineOpen, setLineOpen] = useState(false);
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="w-80">
+        <Button
+          icon="zoom-in-line"
+          size="sm"
+          variant="outlined"
+          onClick={() => setDonutOpen(true)}
+        />
+        <Chart
+          donut={{
+            chart: {data: fakeComplexeDonutData},
+          }}
+          infos={{
+            ...fakeChartInfos,
+            modal: {
+              isOpen: donutOpen,
+              setIsOpen: setDonutOpen,
+            },
+            fileName: 'donut-chart',
+            additionalInfos,
+          }}
+        />
+      </div>
+      <div className="h-px bg-grey-4" />
+      <div>
+        <Button
+          icon="zoom-in-line"
+          size="sm"
+          variant="outlined"
+          onClick={() => setLineOpen(true)}
+          className="mb-6"
+        />
+        <Chart
+          line={{
+            chart: {
+              data: fakeManyXLineData,
+              colors: {datum: 'color'},
+              axisLeftLegend: 'Nombre de yolo en kw/h',
+            },
+            modalChart: {
+              axisBottom: {
+                tickValues: 5,
+              },
+            },
+          }}
+          infos={{
+            ...fakeChartInfos,
+            modal: {
+              isOpen: lineOpen,
+              setIsOpen: setLineOpen,
+              size: 'xl',
+            },
+            fileName: 'line-chart',
+            additionalInfos,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ app.territoiresentransitions.react/src/ui/charts/fixtures.tsx
@@ -0,0 +1,19 @@
+export const fakeChartInfos = {
+  title: 'Part des achats d’électricité renouvelable de la collectivité',
+  subtitle: 'Suivi de la consommation du budget',
+};
+
+export const additionalInfos = (
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec dui at
+      nunc tincidunt tincidunt. Donec auctor, nunc nec lacinia convallis, metus
+      urna tincidunt libero, vel accumsan justo lorem sit amet orci.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec dui at
+      nunc tincidunt tincidunt. Donec auctor, nunc nec lacinia convallis, metus
+      urna tincidunt libero, vel accumsan justo lorem sit amet orci.
+    </p>
+  </div>
+);
