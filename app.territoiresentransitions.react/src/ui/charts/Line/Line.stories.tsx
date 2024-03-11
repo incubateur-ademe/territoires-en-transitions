71c482678e1c9f7ef85a0bad79396a40f868d8b1
Arthur Molinos <>
2024-03-12 00:19:33 +0100

    Création d'un graphique générique Line

---
+++ app.territoiresentransitions.react/src/ui/charts/Line/Line.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Meta, StoryObj} from '@storybook/react';
+
+import {
+  fakeManyXLineData,
+  fakeNoLineData,
+  fakeSimpleLineData,
+} from './fixtures';
+
+import LineChart from './LineChart';
+
+const meta: Meta<typeof LineChart> = {
+  component: LineChart,
+  args: {},
+  render: args => <LineChart {...args} />,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LineChart>;
+
+export const Defaut: Story = {
+  args: {
+    data: fakeSimpleLineData,
+    axisLeftLegend: 'Nombre de yolo en kw/h',
+    legend: {
+      isOpen: true,
+    },
+  },
+};
+
+export const NoData: Story = {
+  args: {
+    data: fakeNoLineData,
+    axisLeftLegend: 'Nombre de yolo en kw/h',
+  },
+};
+
+export const AvecLegendeEtCustomColors: Story = {
+  args: {
+    data: fakeManyXLineData,
+    axisLeftLegend: 'Nombre de yolo en kw/h',
+    colors: {datum: 'color'},
+    legend: {
+      isOpen: true,
+    },
+  },
+};
