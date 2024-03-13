c9457f186db62e5c2dcb413f20c08ab3e7aa421e
Arthur Molinos <>
2024-03-13 13:04:40 +0100

    Utilise le nouveau Chart pour les plans d'action et synthèses

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/Synthese/SyntheseVue/SyntheseVueGraph.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/Synthese/SyntheseVue/SyntheseVueGraph.tsx
@@ -1,12 +1,11 @@
-import {Legend} from 'ui/charts/ChartCard';
-import {Graph, TSyntheseVue, getCustomLegend, getGraphData} from '../utils';
+import {useState} from 'react';
+
+import {TSyntheseVue, getGraphData} from '../utils';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {usePlanActionTableauDeBord} from '../data/usePlanActionTableauDeBord';
 import {PlanActionFilter} from '../FiltersPlanAction';
-import DonutChart, {DonutChartProps} from 'ui/charts/DonutChart';
-import DownloadCanvasButton from 'ui/buttons/DownloadCanvasButton';
-import {useRef} from 'react';
-import {useFonctionTracker} from 'core-logic/hooks/useFonctionTracker';
+import Chart, {ChartProps} from 'ui/charts/Chart';
+import {Button} from '@tet/ui';
 
 type Props = {
   vue: TSyntheseVue;
@@ -16,74 +15,71 @@ type Props = {
 const SyntheseVueGraph = ({vue, plan}: Props) => {
   const collectivite_id = useCollectiviteId();
 
-  const tracker = useFonctionTracker();
-
   const {data} = usePlanActionTableauDeBord(
     collectivite_id!,
     plan.id === 'tous' || plan.id === 'nc' ? null : plan.id,
     plan.id === 'nc' || null
   );
 
-  // Référence utilisée pour le téléchargement du graphe
-  const chartWrapperRef = useRef<HTMLDivElement>(null);
+  const [isChartOpen, setIsChartOpen] = useState(false);
 
-  const graph: Graph | null = data && {
-    id: vue.id,
-    title: vue.graphTitre,
-    data: getGraphData(vue.id, data),
+  const chart: ChartProps | null = data && {
+    donut: {
+      chart: {
+        data: getGraphData(vue.id, data),
+        className: 'h-80',
+        legend: {
+          isOpen: true,
+          size: 'md',
+          maxItems: 9,
+        },
+      },
+    },
+    infos: {
+      modal: {
+        isOpen: isChartOpen,
+        setIsOpen: setIsChartOpen,
+      },
+      title: vue.graphTitre,
+      subtitle:
+        plan.id === 'nc'
+          ? 'Fiches non classées'
+          : plan.id === 'tous'
+          ? 'Toutes les fiches'
+          : plan.name,
+      fileName: `repartition-${vue.id}${
+        plan.id === 'nc'
+          ? '-fiches-non-classees'
+          : plan.id !== 'tous'
+          ? `-${plan.name.toLowerCase().split(' ').join('-')}`
+          : undefined
+      }`,
+    },
   };
 
-  if (!graph) return null;
+  if (!chart) return null;
 
   return (
     <div className="relative">
       {/* Bouton de téléchargement, affiché si un nom de fichier est fourni */}
       <div className="absolute right-4 top-4 z-10">
-        <DownloadCanvasButton
-          containerRef={chartWrapperRef}
-          fileName={`repartition-${graph.id}${
-            plan.id === 'nc'
-              ? '-fiches-non-classees'
-              : plan.id !== 'tous'
-              ? `-${plan.name.toLowerCase().split(' ').join('-')}`
-              : ''
-          }`}
-          fileType="png"
-          onClick={() =>
-            tracker({fonction: 'graphique', action: 'telechargement'})
-          }
+        <Button
+          icon="zoom-in-line"
+          size="xs"
+          variant="outlined"
+          onClick={() => setIsChartOpen(true)}
         />
       </div>
 
-      <div ref={chartWrapperRef} className="p-6">
+      <div className="p-6">
         {/* Titre du graphe */}
-        <p className="mb-1 mr-12 font-bold">{graph.title}</p>
+        <p className="mb-1 mr-12 font-bold">{chart.infos?.title}</p>
 
-        {/** Titre du plan */}
-        <div className="text-gray-500">
-          {plan.id === 'nc'
-            ? 'Fiches non classées'
-            : plan.id === 'tous'
-            ? 'Toutes les fiches'
-            : plan.name}
-        </div>
+        {/** Sous-titre du plan */}
+        <div className="text-gray-500">{chart.infos?.subtitle}</div>
 
-        {/* Graphe agrandi */}
-        <div className="w-full h-80">
-          <DonutChart
-            {...({
-              data: graph.data,
-              label: true,
-            } as DonutChartProps)}
-          />
-        </div>
-
-        {/* Légende */}
-        {getCustomLegend(graph.data) && (
-          <div className="-mt-8 -mb-6">
-            <Legend legend={getCustomLegend(graph.data)} />
-          </div>
-        )}
+        {/* Chart */}
+        <Chart {...chart} />
       </div>
     </div>
   );
