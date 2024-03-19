d65de9fc7e8becbbde326c13cb9d6b8962ef702b
Marc Rutkowski <marc@attractive-media.fr>
2024-03-19 18:12:11 +0100

    Evite de créer plusieurs instances du client supabase

--- packages/site/src/supabase/client.ts
+++ packages/site/src/supabase/client.ts
@@ -1,12 +1,18 @@
 import {createBrowserClient} from '@supabase/ssr';
+import {SupabaseClient} from '@supabase/supabase-js';
 import {Database} from '@tet/api';
 
 /**
  * Pour accéder à supabase depuis les composants client
  */
+let client: SupabaseClient<Database> | null = null;
+
 export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  if (!client) {
+    client = createBrowserClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    );
+  }
+  return client;
 }
