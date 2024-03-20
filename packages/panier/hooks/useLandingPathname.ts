b58031af1741ddf50c18fc4c2420c0c66343330d
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-03-20 16:17:28 +0100

    Corrige la redirection vers la landing page

--- packages/panier/hooks/useLandingPathname.ts
+++ packages/panier/hooks/useLandingPathname.ts
@@ -1,19 +1,23 @@
 import {useEffect, useState} from 'react';
 import {useCollectiviteContext} from 'providers/collectivite';
 import {usePanierContext} from 'providers/panier';
+import {usePathname} from 'next/navigation';
 
 const useLandingPathname = () => {
   const [landingPathname, setLandingPathname] = useState('/landing');
   const {collectiviteId} = useCollectiviteContext();
+  const pathname = usePathname();
   const {panier} = usePanierContext();
 
   useEffect(() => {
-    if (collectiviteId) {
+    if (pathname.includes('landing')) {
+      setLandingPathname(pathname);
+    } else if (collectiviteId) {
       setLandingPathname(`/landing/collectivite/${collectiviteId}`);
     } else if (panier?.id && panier.contenu.length > 0) {
       setLandingPathname(`/landing/panier/${panier.id}`);
     }
-  }, [collectiviteId, panier?.id, panier?.contenu.length]);
+  }, [pathname, collectiviteId, panier?.id, panier?.contenu.length]);
 
   return landingPathname;
 };
