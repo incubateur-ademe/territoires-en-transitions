68a17ad49fff160f5aa1316a69a45a7f1a4aaa36
Marc Rutkowski <marc@attractive-media.fr>
2024-03-25 18:35:35 +0100

    Ajoute le composant de Login et ses différents sous-éléments

---
+++ packages/auth/components/Login/LoginModal.tsx
@@ -0,0 +1,44 @@
+import {useState} from 'react';
+import {Modal} from '@tet/ui';
+import {LoginProps, LoginView} from './type';
+import {Login} from './Login';
+
+/**
+ * Encapsule le panneau d'authentification dans une modale
+ */
+export const LoginModal = (props: LoginProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const {view, onCancel} = props;
+
+  const onClose = () => {
+    setIsOpen(false);
+    onCancel();
+  };
+
+  return (
+    <Modal
+      dataTest="SignInPage"
+      backdropBlur
+      size={mdDialog.includes(view) ? 'md' : 'lg'}
+      title={getTitle(view)}
+      openState={{isOpen, setIsOpen}}
+      onClose={onClose}
+      render={() => <Login {...props} onCancel={() => onClose()} />}
+    />
+  );
+};
+
+// la modale est en format "md" pour ces contenus
+const mdDialog: LoginView[] = [
+  'msg_init_mdp',
+  'msg_lien_envoye',
+  'verify',
+  'reset_mdp',
+];
+
+// retourne le titre approprié de la modale en fonction de son contenu
+const getTitle = (view: LoginView) => {
+  if (view === 'etape1') return 'Se connecter';
+  if (view === 'mdp_oublie') return 'Mot de passe oublié ?';
+  if (view === 'reset_mdp') return 'Choisissez votre nouveau mot de passe';
+};
---
+++ packages/auth/components/Login/LoginTabs.tsx
@@ -0,0 +1,157 @@
+import {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {z} from 'zod';
+import {
+  Button,
+  Input,
+  Field,
+  FieldMessage,
+  ModalFooterOKCancel,
+  Tab,
+  Tabs,
+} from '@tet/ui';
+import {Credentials, LoginPropsWithState} from './type';
+
+/** Gestionnaire d'état pour le formulaire de l'étape 1 */
+const useLoginForm = (isPasswordless: boolean, email: string) => {
+  const validationSchema = z.object({
+    email: z.string().email({message: 'Un email valide est requis'}),
+    password: z
+      .string()
+      .refine(value => (isPasswordless ? true : value.length >= 8), {
+        message: 'Le mot de passe doit comporter au moins 8 caractères',
+      }),
+  });
+
+  return useForm({
+    reValidateMode: 'onChange',
+    resolver: zodResolver(validationSchema),
+    defaultValues: {
+      password: '',
+      email: email || '',
+    },
+  });
+};
+
+/**
+ * Affiche l'étape 1 du panneau de connexion
+ * (saisir un email et éventuellement un mot de passe)
+ */
+export const LoginTabs = (props: LoginPropsWithState) => {
+  const {formState: signupState, withPassword} = props;
+  const {email} = signupState;
+  const [isPasswordless, setIsPasswordless] = useState(!withPassword);
+  const form = useLoginForm(isPasswordless, email);
+
+  return (
+    <Tabs
+      className="justify-center"
+      defaultActiveTab={isPasswordless ? 0 : 1}
+      onChange={activeTab => {
+        if (activeTab === 0) {
+          // reset le champ mdp qui peut être rempli quand on passe d'un onglet à l'autre
+          form.setValue('password', '');
+          setIsPasswordless(true);
+        } else {
+          setIsPasswordless(false);
+        }
+      }}
+    >
+      <Tab label="Connexion sans mot de passe">
+        <SignupStep1Form {...props} form={form} isPasswordless />
+      </Tab>
+      <Tab label="Connexion avec mot de passe">
+        <SignupStep1Form {...props} form={form} />
+      </Tab>
+    </Tabs>
+  );
+};
+
+/**
+ * Affiche le formulaire pour l'étape 1 la connnexion
+ */
+const SignupStep1Form = (
+  props: LoginPropsWithState & {
+    form: ReturnType<typeof useLoginForm>;
+    isPasswordless?: boolean;
+  },
+) => {
+  const {
+    error,
+    isLoading,
+    isPasswordless,
+    setView,
+    onSubmit,
+    onCancel,
+    form,
+    formState: {setEmail},
+  } = props;
+  const {
+    handleSubmit,
+    register,
+    formState: {isValid, errors},
+  } = form;
+
+  const onSubmitForm = (data: Credentials) => {
+    // enregistre les données car on a besoin de l'email pour vérifier l'otp à
+    // l'étape suivante, dans le cas où l'utilisateur saisi directement le code
+    // OTP plutôt que de cliquer sur le lien contenu dans le mail
+    setEmail(data.email);
+    // envoi les données
+    onSubmit(data);
+  };
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmitForm)}>
+      <Field
+        title="Email de connexion *"
+        htmlFor="email"
+        state={errors.email ? 'error' : isPasswordless ? 'info' : undefined}
+        message={
+          errors.email?.message?.toString() ||
+          (isPasswordless &&
+            'La connexion se fait par lien sécurisé envoyé sur votre boite mail.') ||
+          ''
+        }
+      >
+        <Input id="email" type="text" {...register('email')} />
+      </Field>
+      {!isPasswordless && (
+        <>
+          <Field
+            title="Mot de passe *"
+            htmlFor="password"
+            state={errors.password ? 'error' : undefined}
+            message={errors.password?.message?.toString()}
+          >
+            <Input id="password" type="password" {...register('password')} />
+          </Field>
+          <Button
+            data-test="forgotten-pwd"
+            type="button"
+            variant="underlined"
+            onClick={() => setView('mdp_oublie')}
+          >
+            Mot de passe oublié ?
+          </Button>
+        </>
+      )}
+      {!!error && (
+        <FieldMessage
+          data-test="error"
+          messageClassName="mt-4"
+          state="error"
+          message={error}
+        />
+      )}
+      <ModalFooterOKCancel
+        btnCancelProps={{onClick: onCancel}}
+        btnOKProps={{
+          type: 'submit',
+          disabled: !isValid || isLoading,
+        }}
+      />
+    </form>
+  );
+};
