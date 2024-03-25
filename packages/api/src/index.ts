bb4143dc9944de05ba8f350d43929e88cc94b51d
Marc Rutkowski <marc@attractive-media.fr>
2024-03-25 18:08:40 +0100

    Ajoute les utilitaires permettant de déterminer les URLs des pages d'authentification

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,5 +2,6 @@ export * from './typeUtils';
 export type {Database, Json} from './database.types';
 export {unaccent} from './utils/unaccent';
 export * from './utils/authTokens';
+export * from './utils/pathUtils';
 export * as Indicateurs from './indicateurs';
 export * as CollectiviteEngagee from './collectiviteEngagees';
