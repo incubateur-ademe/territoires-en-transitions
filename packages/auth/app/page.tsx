1b1fe8d85e07d4378b2c864cacd366354eb8001f
Marc Rutkowski <marc@attractive-media.fr>
2024-03-25 16:38:42 +0100

    Ajoute le package @tet/auth pour centraliser les écrans de connexion et de création de compte

---
+++ packages/auth/app/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React, {useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+import TrackPageView from '@components/TrackPageView/index';
+
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <TrackPageView pageName="/" />;
+};
+
+export default Page;
