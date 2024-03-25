8e020f2b64ad790665499c9eb3e56dde8d3e7c88
Marc Rutkowski <marc@attractive-media.fr>
2024-03-25 18:33:09 +0100

    Ajoute le composant de saisie d'un code OTP

---
+++ packages/auth/components/VerifyOTP/VerifyOTP.stories.tsx
@@ -0,0 +1,52 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {VerifyOTP} from './VerifyOTP';
+
+const meta: Meta<typeof VerifyOTP> = {
+  component: VerifyOTP,
+  args: {
+    onCancel: action('onCancel'),
+    onSubmit: action('onSubmit'),
+    onResend: action('onResend'),
+    defaultValues: {email: 'yolo@dodo.com', otp: ''},
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof VerifyOTP>;
+
+export const TypeLogin: Story = {
+  args: {type: 'login'},
+};
+
+export const TypeSignup: Story = {
+  args: {type: 'signup'},
+};
+
+export const TypeResetPassword: Story = {
+  args: {type: 'reset_password'},
+};
+
+export const PreRempli: Story = {
+  args: {
+    type: 'reset_password',
+    defaultValues: {email: 'yolo@dodo.com', otp: '123456'},
+  },
+};
+
+export const IsLoading: Story = {
+  args: {
+    type: 'reset_password',
+    isLoading: true,
+    defaultValues: {email: 'yolo@dodo.com', otp: '123456'},
+  },
+};
+
+export const AvecErreur: Story = {
+  args: {
+    type: 'reset_password',
+    defaultValues: {email: 'yolo@dodo.com', otp: '123456'},
+    error: "Message d'erreur",
+  },
+};
