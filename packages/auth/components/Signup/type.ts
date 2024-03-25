a37d0faf67c49e5743938469d0628d47fb6da504
Marc Rutkowski <marc@attractive-media.fr>
2024-03-25 18:35:56 +0100

    Ajoute le composant de Signup et ses différents sous-éléments

---
+++ packages/auth/components/Signup/type.ts
@@ -0,0 +1,76 @@
+import {ZxcvbnResult} from '@zxcvbn-ts/core';
+import {Enums} from '@tet/api';
+import {ResendFunction, VerifyOTPData} from '@components/VerifyOTP';
+import {FormState} from '@components/Login/useFormState';
+
+const ValidSignupView = [
+  'etape1',
+  'etape2',
+  'etape3',
+  'etape4',
+  'msg_lien_envoye',
+] as const;
+export const isValidSignupView = (view: string | null) =>
+  ValidSignupView.includes(view as SignupView);
+export type SignupView = (typeof ValidSignupView)[number];
+
+export type SignupDataStep1 = {
+  email: string;
+  password: string;
+};
+
+export type SignupDataStep2 = VerifyOTPData;
+
+export type SignupDataStep3 = {
+  email: string;
+  nom: string;
+  prenom: string;
+  telephone: string;
+  cgu_acceptees: boolean;
+};
+
+export type SignupDataStep4 = {
+  email: string;
+  collectivite_id?: number;
+  collectivite_engagee?: boolean;
+  role?: Enums<'membre_fonction'>;
+  poste?: string;
+};
+
+export type SignupData =
+  | SignupDataStep1
+  | SignupDataStep2
+  | SignupDataStep3
+  | SignupDataStep4;
+
+export type SignupProps = {
+  /** Vue courante */
+  view: SignupView;
+  /** Permet de passer d'une vue à une autre */
+  setView: (view: SignupView) => void;
+  /** Valeurs par défaut du formulaire (étape 1) */
+  defaultValues?: {email: string | null; otp?: string | null};
+  /** Erreur à afficher */
+  error: string | null;
+  /** Indique qu'un appel réseau est en cours */
+  isLoading?: boolean;
+  /** Indique que l'option "avec mot de passe" est activée */
+  withPassword?: boolean;
+  /** Liste de collectivités auxquelles le compte peut être rattaché */
+  collectivites: Array<{value: number; label: string}>;
+  /** Appelé pour filtrer la liste des collectivités */
+  onFilterCollectivites: (value: string) => void;
+  /** Appelé à l'envoi du formulaire */
+  onSubmit: (data: SignupData) => void;
+  /** Appelé à l'annulation du formulaire */
+  onCancel: () => void;
+  /** Fonction appelée pour renvoyer l'email contenant le code */
+  onResend: ResendFunction;
+  /** Pour contrôler la robustesse des mots de passe */
+  getPasswordStrength: (
+    password: string,
+    otherValues: string[],
+  ) => ZxcvbnResult | null;
+};
+
+export type SignupPropsWithState = SignupProps & {formState: FormState};
