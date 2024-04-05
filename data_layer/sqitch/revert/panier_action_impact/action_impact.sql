aeb44a2a4cd41713de05aac39e18459c9476269a
Florian d'Erfurth <florian@derfurth.com>
2024-04-05 18:19:57 +0200

    Ajoute les effets attendus aux actions à impact.

--- data_layer/sqitch/revert/panier_action_impact/action_impact.sql
+++ data_layer/sqitch/revert/panier_action_impact/action_impact.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
+drop table action_impact_effet_attendu;
 drop table action_impact_indicateur;
 drop table action_impact_action;
 drop table action_impact_categorie_fnv;
@@ -9,10 +10,11 @@ drop table action_impact_banatic_competence;
 drop table action_impact_sous_thematique;
 drop table action_impact_thematique;
 drop table action_impact;
+drop table effet_attendu;
+drop table action_impact_temps_de_mise_en_oeuvre;
 drop table action_impact_tier;
 drop table action_impact_fourchette_budgetaire;
 drop table action_impact_complexite;
-drop table action_impact_temps_mise_en_oeuvre;
 drop table categorie_fnv;
 
 COMMIT;
