44436d6d79c3804057a5dbb24961fd287d1a062c
Florian d'Erfurth <florian@derfurth.com>
2024-04-05 18:20:21 +0200

    Ajoute les effets attendus aux fiches action.

--- data_layer/sqitch/deploy/panier_action_impact/plan_action.sql
+++ data_layer/sqitch/deploy/panier_action_impact/plan_action.sql
@@ -2,6 +2,15 @@
 
 BEGIN;
 
+create table fiche_action_effet_attendu
+(
+    fiche_id         integer references fiche_action,
+    effet_attendu_id integer references effet_attendu,
+    primary key (fiche_id, effet_attendu_id)
+);
+comment on table fiche_action_effet_attendu is
+    'Lie une fiche action à un effet attendu';
+
 create function
     plan_from_panier(collectivite_id int, panier_id uuid)
     returns plan_action
@@ -31,22 +40,38 @@ begin
                  join action_impact ai on ap.action_id = ai.id
         where p.id = $2
         loop
-            -- on insert une fiche
+            -- on insère une fiche
             insert into fiche_action (collectivite_id, description)
             select $1, selected_action_impact.description || '\n' || selected_action_impact.description_complementaire
             returning id into fiche_id;
 
-            -- on ajoute la fiche nouvellement cau plan
+            -- puis on ajoute la fiche nouvellement créée au plan
             perform ajouter_fiche_action_dans_un_axe(fiche_id, axe_id);
 
-            -- todo
-            -- - thématiques → thématiques
-            -- - sous-thématiques → sous-thématiques
-            -- - elements budgétaires → champ “financements”
-            -- - effets_attendus → résultats attendus (qui va être renommé - ticket prévu)
-            -- - partenaires potentiels → partenaires
-            -- - Indicateurs macro parents → indicateurs liés
-            -- - Indicateurs macro enfants → indicateurs liés
+            -- ensuite on applique de l'action à la fiche:
+            -- - les thematiques
+            insert into fiche_action_thematique (fiche_id, thematique_id)
+            select fiche_id, ait.thematique_id
+            from action_impact_thematique ait
+            where ait.action_impact_id = selected_action_impact.id;
+
+            -- - les sous-thematiques
+            insert into fiche_action_sous_thematique (fiche_id, thematique_id)
+            select fiche_id, aist.sous_thematique_id
+            from action_impact_sous_thematique as aist
+            where aist.action_impact_id = selected_action_impact.id;
+
+            -- - les indicateurs
+            insert into fiche_action_indicateur (fiche_id, indicateur_id)
+            select fiche_id, aii.indicateur_id
+            from action_impact_indicateur as aii
+            where aii.action_impact_id = selected_action_impact.id;
+
+            -- - les effets attendus
+            insert into fiche_action_effet_attendu (fiche_id, effet_attendu_id)
+            select fiche_id, aiea.effet_attendu_id
+            from action_impact_effet_attendu as aiea
+            where aiea.action_impact_id = selected_action_impact.id;
         end loop;
 end;
 $$ language plpgsql;
