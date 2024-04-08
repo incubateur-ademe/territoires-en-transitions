9a144c8f439ac5233f01fbe059e8a3f39192f1d0
Marc Rutkowski <marc@attractive-media.fr>
2024-04-08 14:30:11 +0200

    Raccorde le nouveau dialogue "Rejoindre une collectivité" dans la page "Collectivités" de l'app

--- app.territoiresentransitions.react/src/ui/collectivites/AssocierCollectiviteBandeau.tsx
+++ app.territoiresentransitions.react/src/ui/collectivites/AssocierCollectiviteBandeau.tsx
@@ -1,25 +1,25 @@
-import {AssocierCollectiviteDialog} from 'ui/collectivites/AssocierCollectiviteDialog';
-import {getReferentContacts} from 'core-logic/api/procedures/collectiviteProcedures';
+import {getRejoindreCollectivitePath} from '@tet/api';
+import {Alert, Button} from '@tet/ui';
 
 const AssocierCollectiviteBandeau = () => {
   return (
-    <div className="bg-bf925">
-      <div className="fr-container py-4">
-        <div className="flex mb-4 text-tDefaultInfo">
-          <span className="fr-fi-information-fill mr-4 mt-0.5" />
-          <span className="font-bold text-tDefaultInfo">
-            Pour accéder à plus de détails sur chacune des collectivités
-            engagées dans le programme, vous devez être membre d’au moins une
-            collectivité.
-          </span>
-        </div>
-        <div className="flex justify-center sm:justify-start sm:ml-10">
-          <AssocierCollectiviteDialog
-            getReferentContacts={getReferentContacts}
-          />
-        </div>
-      </div>
-    </div>
+    <Alert
+      classname="px-0 lg:px-8"
+      state="info"
+      title="Pour accéder à plus de détails sur chacune des collectivités
+      engagées dans le programme, vous devez être membre d’au moins une
+      collectivité."
+      footer={
+        <Button
+          href={getRejoindreCollectivitePath(
+            document.location.hostname,
+            document.location.origin
+          )}
+        >
+          Rejoindre une collectivité
+        </Button>
+      }
+    />
   );
 };
 
