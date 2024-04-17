658a3b6c9196c26ac80f2073fb2aeee5dc2bee42
Marc Rutkowski <marc@attractive-media.fr>
2024-04-17 16:06:30 +0200

    Déplace le composant TrackPageView pour le rendre ré-utilisable

--- packages/panier/components/TrackPageView/index.tsx
+++ packages/panier/components/TrackPageView/index.tsx
@@ -1,29 +1,5 @@
 'use client';
+import {TrackPageView} from '@tet/ui';
 
-import {useEffect} from 'react';
-import {usePostHog} from 'posthog-js/react';
-import {PageName} from '@tet/ui';
-
-/**
- * Envoi une page view à PostHog lors du rendering.
- *
- * Note : en dev quand le mode strict est activé,
- * on envoie deux événementsà la suite.
- * https://react.dev/reference/react/StrictMode
- *
- * @param pageName
- * @constructor
- */
-export default function TrackPageView({pageName}: {pageName: PageName}): null {
-  const posthog = usePostHog();
-
-  useEffect(() => {
-    if (posthog) {
-      posthog.capture('$pageview', {
-        $current_url: pageName,
-      });
-    }
-  }, [posthog, pageName]);
-
-  return null;
-}
+// export pour utiliser le composant avec un import dynamique
+export default TrackPageView;
