a22774a1e7e961fc884c5f2dd125e8dcf317bd8b
Marc Rutkowski <marc@attractive-media.fr>
2024-04-17 17:08:57 +0200

    Rend ré-utilisable une fonction

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,6 +3,7 @@ export type {Database, Json} from './database.types';
 export {unaccent} from './utils/unaccent';
 export * from './utils/authTokens';
 export * from './utils/pathUtils';
+export * from './utils/makeSearchString';
 export * as Indicateurs from './indicateurs';
 export * as CollectiviteEngagee from './collectiviteEngagees';
 export * from './panier_action_impact';
