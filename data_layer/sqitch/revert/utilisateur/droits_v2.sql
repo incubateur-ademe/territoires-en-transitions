c07e173fb5542bc652792a943573b3767da3d8f0
Marc Rutkowski <marc@attractive-media.fr>
2024-04-23 14:55:06 +0200

    Evite une erreur liée à une fonction polymorphique

--- data_layer/sqitch/revert/utilisateur/droits_v2.sql
+++ data_layer/sqitch/revert/utilisateur/droits_v2.sql
@@ -3,14 +3,14 @@
 
 BEGIN;
 
-comment on function claim_collectivite is
-    'Claims an EPCI : '
-        'will succeed with a code 200 if this EPCI does not have referent yet.'
-        'If the EPCI was already claimed it will fail with a code 409.';
-
-drop function claim_collectivite(integer, membre_fonction, text, referentiel[], est_referent);
+drop function claim_collectivite(integer, membre_fonction, text, referentiel[], boolean);
 
 alter table private_collectivite_membre
     drop column est_referent;
+
+comment on function claim_collectivite(integer) is
+    'Claims an EPCI : '
+        'will succeed with a code 200 if this EPCI does not have referent yet.'
+        'If the EPCI was already claimed it will fail with a code 409.';
 
 COMMIT;
