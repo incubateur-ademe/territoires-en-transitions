b302c71a7a52f5961b2912b2c9cbdddf79307bbb
Arthur Molinos <>
2024-04-25 12:43:24 +0200

    Vérifie que l'utilisateur a bien les droits avant d'afficher le tableau de bord de l'accueil sinon on affiche un message

--- app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/Accueil.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/Accueil.tsx
@@ -1,16 +1,54 @@
 import {ReactNode} from 'react';
 import {referentielToName} from 'app/labels';
-import {useCollectiviteId} from 'core-logic/hooks/params';
 import {useProgressionReferentiel} from './data/useProgressionReferentiel';
 import EtatDesLieux from './EtatDesLieux/EtatDesLieux';
 import IndicateursCard from './IndicateursCard';
 import PlansActionCard from './PlansActionCard';
+import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {useAuth} from 'core-logic/api/auth/AuthProvider';
 
 /**
  * Affiche la page d'accueil d'une collectivité
  */
 const Accueil = (): JSX.Element => {
-  const collectiviteId = useCollectiviteId();
+  const collectivite = useCurrentCollectivite();
+
+  const {user} = useAuth();
+
+  if (!collectivite?.collectivite_id) return <></>;
+
+  /** Vérifi que l'utilisateur peut accéder à la collectivité */
+  const hasNoAccessToCollectivite =
+    collectivite.acces_restreint &&
+    collectivite.niveau_acces === null &&
+    !user?.isSupport &&
+    !collectivite.est_auditeur;
+
+  /** S'il ne peut pas, on affiche un message */
+  if (hasNoAccessToCollectivite) {
+    return (
+      <div className="flex-grow flex">
+        <div className="m-auto text-grey-7">
+          Cette collectivité n’est pas accessible en mode visite.
+        </div>
+      </div>
+    );
+  }
+
+  /** Sinon on affiche la page.
+   * Ceci permet de ne pas appeler `useProgressionReferentiel`
+   * qui ne marche pas si l'utilisateur n'a pas les droits */
+  return (
+    <AccueilNonConfidentielle collectiviteId={collectivite.collectivite_id} />
+  );
+};
+
+/** Affiche le tableau de bord de l'accueil pour les utilisateurs avec les droits nécessaires */
+const AccueilNonConfidentielle = ({
+  collectiviteId,
+}: {
+  collectiviteId: number;
+}) => {
   const {
     caeTable: caeProgressionScore,
     eciTable: eciProgressionScore,
@@ -19,7 +57,6 @@ const Accueil = (): JSX.Element => {
     caePotentiel,
     eciPotentiel,
   } = useProgressionReferentiel();
-
   return (
     <main data-test="TableauBord" className="bg-bf975 -mb-8">
       {!!collectiviteId && (
