2bf02704323ae79e5765eec48452acd01b28d176
Marc Rutkowski <marc@attractive-media.fr>
2024-04-29 12:34:40 +0200

    Redirige sur la page de login si l'utilisateur n'est pas  connecté au lieu d'afficher la page "Rejoindre une collectivité"

--- packages/auth/app/rejoindre-une-collectivite/page.tsx
+++ packages/auth/app/rejoindre-une-collectivite/page.tsx
@@ -4,6 +4,8 @@ import {useSearchParams} from 'next/navigation';
 import {RejoindreUneCollectiviteModal} from '@components/RejoindreUneCollectivite';
 import {useRejoindreUneCollectivite} from 'app/rejoindre-une-collectivite/useRejoindreUneCollectivite';
 import {useEffect} from 'react';
+import {getAuthPaths, restoreSessionFromAuthTokens} from '@tet/api';
+import {supabase} from 'src/clientAPI';
 
 /**
  * Affiche la page "rejoindre une collectivité"
@@ -15,6 +17,19 @@ const RejoindreUneCollectivitePage = () => {
   const searchParams = useSearchParams();
   const redirectTo = searchParams.get('redirect_to') || '/';
   const state = useRejoindreUneCollectivite({redirectTo});
+
+  // redirige sur la page de login si l'utilisateur n'est pas  connecté
+  useEffect(() => {
+    const restore = async () => {
+      const ret = await restoreSessionFromAuthTokens(supabase);
+      if (!ret?.data.session) {
+        document.location.replace(
+          getAuthPaths(document.location.hostname, redirectTo).login,
+        );
+      }
+    };
+    restore();
+  }, [redirectTo]);
 
   // initialement charge les 10 premières collectivités
   useEffect(() => {
