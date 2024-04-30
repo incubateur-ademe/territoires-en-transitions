fc27b8fdb6ce9b4d642b45c577583f71064bd3c5
Florian d'Erfurth <florian@derfurth.com>
2024-04-30 13:55:08 +0200

    Répare les permissions de la RPC `create_fiche`

--- data_layer/sqitch/deploy/plan_action/fiches.sql
+++ data_layer/sqitch/deploy/plan_action/fiches.sql
@@ -2,6 +2,63 @@
 
 BEGIN;
 
+create or replace function
+    create_fiche(
+    collectivite_id int,
+    axe_id int default null,
+    action_id action_id default null,
+    indicateur_referentiel_id indicateur_id default null,
+    indicateur_personnalise_id int default null
+)
+    returns fiche_resume
+    language plpgsql
+    security definer
+    volatile
+as
+$$
+declare
+    new_fiche_id int;
+    resume       fiche_resume;
+begin
+    if not have_edition_acces(create_fiche.collectivite_id) and not is_service_role()
+    then
+        perform set_config('response.status', '403', true);
+        raise 'L''utilisateur n''a pas de droit en édition sur la collectivité.';
+    end if;
+
+    insert into fiche_action (collectivite_id, titre)
+    values (create_fiche.collectivite_id, '')
+    returning id into new_fiche_id;
+
+    if create_fiche.axe_id is not null
+    then
+        insert into fiche_action_axe (fiche_id, axe_id)
+        values (new_fiche_id, create_fiche.axe_id);
+    end if;
+
+    if create_fiche.action_id is not null
+    then
+        insert into fiche_action_action (fiche_id, action_id)
+        values (new_fiche_id, create_fiche.action_id);
+    end if;
+
+    if create_fiche.indicateur_referentiel_id is not null
+    then
+        insert into fiche_action_indicateur (fiche_id, indicateur_id)
+        values (new_fiche_id, create_fiche.indicateur_referentiel_id);
+    end if;
+
+    if create_fiche.indicateur_personnalise_id is not null
+    then
+        insert into fiche_action_indicateur (fiche_id, indicateur_personnalise_id)
+        values (new_fiche_id, create_fiche.indicateur_personnalise_id);
+    end if;
+
+    select * from fiche_resume where id = new_fiche_id limit 1 into resume;
+    return resume;
+end;
+$$;
+
 alter table fiche_action_pilote
     drop constraint fiche_action_pilote_fiche_id_user_id_tag_id_key;
 
