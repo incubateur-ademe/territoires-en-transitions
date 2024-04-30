f95b684d4073d9254547400a1852e0250dcc2923
Florian d'Erfurth <florian@derfurth.com>
2024-04-30 17:17:43 +0200

    Utilise `Consent` et le callback `onConsentSave` dans le panier.

--- packages/panier/providers/posthog.tsx
+++ packages/panier/providers/posthog.tsx
@@ -1,13 +1,28 @@
 'use client';
 
 import {
-  TrackingProvider,
+  Consent,
   createTrackingClient,
+  getConsent,
   getNextTrackingEnv,
+  ScriptLikeProps,
+  TrackingProvider,
 } from '@tet/ui';
+import Script from 'next/script';
 
 const client = createTrackingClient(getNextTrackingEnv());
+const onConsentSave = () => {
+  client.set_config({persistence: getConsent() ? 'cookie' : 'memory'});
+};
 
 export const PHProvider = ({children}: {children: React.ReactNode}) => {
-  return <TrackingProvider client={client}>{children}</TrackingProvider>;
+  return (
+    <>
+      <TrackingProvider client={client}>{children}</TrackingProvider>
+      <Consent
+        onConsentSave={onConsentSave}
+        script={(props: ScriptLikeProps) => <Script {...props} />}
+      />
+    </>
+  );
 };
