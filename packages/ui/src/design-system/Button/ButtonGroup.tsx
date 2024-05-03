d6982142deeb73926b7b156ea6b8899d0c0a05ec
Arthur Molinos <>
2024-05-03 19:33:37 +0200

    Ajoute ButtonGroup au design system

---
+++ packages/ui/src/design-system/Button/ButtonGroup.tsx
@@ -0,0 +1,75 @@
+import {CSSProperties} from 'react';
+import classNames from 'classnames';
+
+import {ButtonProps, ButtonSize} from './types';
+import {Button} from './Button';
+
+type Props = {
+  /** Listes de boutons à afficher avec leurs propriétés */
+  buttons: ButtonProps[];
+  /** Permet d'ajuster les styles du container */
+  className?: string;
+  /** Taille des boutons */
+  size?: ButtonSize;
+  /** Id du bouton affiché dans une variante primary */
+  activeButtonId?: string;
+};
+
+/**
+ *  Affiche un groupe d'onglets.
+ */
+export const ButtonGroup = ({
+  className,
+  size = 'md',
+  buttons,
+  activeButtonId,
+}: Props) => {
+  /** Applique les bons styles pour les borders et border-radius
+   * en fonction de la position du bouton dans le tableau */
+  const getButtonStyles = (index: number): CSSProperties => {
+    if (buttons.length === 2) {
+      return index === 0
+        ? {
+            borderTopRightRadius: 0,
+            borderBottomRightRadius: 0,
+            borderRight: 0,
+          }
+        : {
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+            borderLeft: 0,
+          };
+    } else {
+      if (index === 0)
+        return {
+          borderTopRightRadius: 0,
+          borderBottomRightRadius: 0,
+        };
+      if (index > 0 && index < buttons.length - 1)
+        return {
+          borderRadius: 0,
+          borderRight: 0,
+          borderLeft: 0,
+        };
+      if (index === buttons.length - 1)
+        return {
+          borderTopLeftRadius: 0,
+          borderBottomLeftRadius: 0,
+        };
+    }
+  };
+
+  return (
+    <div className={classNames('flex items-center', className)}>
+      {buttons.map((props, index) => (
+        <Button
+          key={index}
+          {...props}
+          size={size}
+          variant={activeButtonId === props.id ? 'primary' : 'outlined'}
+          style={getButtonStyles(index)}
+        />
+      ))}
+    </div>
+  );
+};
