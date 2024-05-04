753904a99df6a3bdb44a0d4e27dc41ff37be09b5
Arthur Molinos <>
2024-05-04 14:21:26 +0200

    Modifie comment afficher les modules dans la page perso

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/data.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/data.ts
@@ -4,10 +4,14 @@ export type TDBModuleSlug = 'actions-dont-je-suis-pilote';
 
 /** Types d'un module tableau de bord plans d'action */
 export type TDBModuleTypes = {
-  id?: string;
+  /** Le titre du module */
   title: string;
+  /** Le nom donné à l'url du module,
+   * est utilisé comme id pour afficher la bonne page du module */
   slug: TDBModuleSlug;
+  /** Les différents filtres sélectionnés */
   filters?: any[];
+  /** le type de tri sélectionné */
   sortBy?: any;
 };
 
@@ -16,3 +20,6 @@ export const actionsDontJeSuisPilote: TDBModuleTypes = {
   title: 'Actions dont je suis le pilote',
   slug: 'actions-dont-je-suis-pilote',
 };
+
+/** Liste des modules d'un utilisateur avec les filtres sauvegardés */
+export const tdbPersonnelModules: TDBModuleTypes[] = [actionsDontJeSuisPilote];
