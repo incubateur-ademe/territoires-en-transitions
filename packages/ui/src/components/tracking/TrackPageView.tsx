7fc809d37f8fbdeff1950b4cf3986529a1f2e7e4
Florian d'Erfurth <florian@derfurth.com>
2024-05-07 15:02:29 +0200

    Ajoute le group aux events PostHog.

--- packages/ui/src/components/tracking/TrackPageView.tsx
+++ packages/ui/src/components/tracking/TrackPageView.tsx
@@ -3,11 +3,23 @@ import {usePostHog} from 'posthog-js/react';
 import {PageName, TrackingPlan} from './trackingPlan';
 
 /**
+ * Type guard pour les propriétés
+ */
+function isRelativeToCollectivite(object: unknown): object is {
+  collectivite_id?: number;
+  collectivite_preset?: number;
+} {
+  return (
+    typeof object === 'object' &&
+    ('collectivite_id' in object || 'collectivite_preset' in object)
+  );
+}
+
+/**
  * Les props du tracker de pageviews
  *
  * le typage permet de respecter le plan de tracking.
  */
-
 type TrackPageViewProps<N extends PageName> = {
   pageName: N;
 } & (TrackingPlan[N]['properties'] extends object
@@ -33,6 +45,16 @@ export function TrackPageView<N extends PageName>({
 
   useEffect(() => {
     if (posthog) {
+      // Spécifie le groupe des évenements qui suivent cet appel
+      // https://posthog.com/docs/getting-started/group-analytics
+      posthog.group(
+        'collectivite',
+        isRelativeToCollectivite(properties)
+          ? `${properties.collectivite_id ?? properties.collectivite_preset}`
+          : null
+      );
+
+      // Envoie la pageview manuellement à PostHog conformément au tracking plan
       posthog.capture('$pageview', {
         $current_url: pageName,
         ...(properties ?? {}),
