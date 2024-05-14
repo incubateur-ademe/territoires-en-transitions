bd5a1ff3a028faf91f1f9c9458852faefbe36665
Arthur Molinos <>
2024-05-14 17:53:57 +0200

    Ajoute une vue par défaut du tdb et revoie la façon de géré l'accès depuis le header de l'app

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/TableauDeBord.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/TableauDeBord.tsx
@@ -1,31 +1,43 @@
-import {Route, useParams} from 'react-router-dom';
+import {Redirect, Route} from 'react-router-dom';
 
 import {
-  TDBViewParam,
+  collectiviteTDBBasePath,
   collectiviteTDBCollectivitePath,
   collectiviteTDBModulePath,
   collectiviteTDBPersonnelPath,
+  makeTableauBordUrl,
 } from 'app/paths';
 import View from './View';
 import Personnel from './Personnel';
 import Modules from 'app/pages/collectivite/TableauDeBord/Module/Modules';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {tdbUtilisateur} from 'app/pages/collectivite/TableauDeBord/Module/data';
 
 /** Tableau de bord plans d'action */
 const TableauDeBord = () => {
-  const params: {tdbView: TDBViewParam} = useParams();
-  const view = params.tdbView;
+  const collectivite_id = useCollectiviteId();
 
   return (
     <div className="bg-grey-2 -mb-8 py-12">
       <div className="fr-container">
+        {/** Si l'on vient de la navigation principale de l'app,
+         * alors on redirige vers la vue par défaut enregistrée par l'utilisateur */}
+        <Route exact path={collectiviteTDBBasePath}>
+          <Redirect
+            to={makeTableauBordUrl({
+              collectiviteId: collectivite_id!,
+              view: tdbUtilisateur.defaultView,
+            })}
+          />
+        </Route>
         {/** Tableau de bord personnel */}
         <Route exact path={collectiviteTDBPersonnelPath}>
-          <Personnel />
+          <Personnel modules={tdbUtilisateur.tdbPersonnelModules} />
         </Route>
         {/** Tableau de bord de la collectivité */}
         <Route exact path={collectiviteTDBCollectivitePath}>
           <View
-            view={view}
+            view={'collectivite'}
             title="Le tableau de bord collaboratif de ma collectivité."
             description="Ce tableau de bord est destiné à l'ensemble des personnes de ma collectivité et peut être modifié par les administrateurs."
             settingButton={undefined}
@@ -34,7 +46,7 @@ const TableauDeBord = () => {
         </Route>
         {/** Modules */}
         <Route path={collectiviteTDBModulePath}>
-          <Modules view={view} />
+          <Modules />
         </Route>
       </div>
     </div>
