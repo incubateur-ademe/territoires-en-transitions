573831d48ee959c1406e8fc2342d560d07d4cd2a
Arthur Molinos <>
2024-05-15 15:43:11 +0200

    Ajoute des états de chargement et vide pour le module générique

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/Module.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/Module.tsx
@@ -1,33 +1,103 @@
-import React from 'react';
+import {Badge, Button} from '@tet/ui';
+import React, {useState} from 'react';
+import SpinnerLoader from 'ui/shared/SpinnerLoader';
+
+type ModalState = {
+  isOpen: boolean;
+  setIsOpen: (opened: boolean) => void;
+};
 
 type Props = {
+  /** Titre du module */
   title: string;
+  /** Symbole du module (picto svg) */
+  symbole?: React.ReactNode;
+  /** Fonction d'affichage de la modale avec les filtres du modules,
+   * à afficher au clique des boutons d'édition.
+   * Récupère le state d'ouverture en argument */
+  editModal: (modalState: ModalState) => React.ReactNode;
+  /** État de loading générique */
+  isLoading: boolean;
+  /** État vide générique */
+  isEmpty: boolean;
+  /** Affiche les filtres sélectionnés dans une liste de badges */
+  selectedFilters: string[];
   /** Le contenu (cartes, boutons, ... ) à afficher dans le module.
    * Les contenus sont trop différents pour tous les traiter ici.
-   * (voir le module ActionsDontJeSuisPilote pour un exemple) */
+   * (voir ModuleFichesActions pour un exemple) */
   children: React.ReactNode;
 };
 
 /** Composant générique d'un module du tableau de bord plans d'action */
-const Module = ({title, children}: Props) => {
-  /**
-   * TODO
-   * - [ ] Afficher un état vide
-   * - [ ] Afficher les filtres sélectionnés dans une liste de badges
-   */
+const Module = ({
+  title,
+  symbole,
+  editModal,
+  isLoading,
+  selectedFilters,
+  isEmpty,
+  children,
+}: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  /**
-   * À ne pas faire
-   * - le bouton pour changer les filtres (on fera cela dans un second temps)
-   */
+  if (isLoading) {
+    return (
+      <ModuleContainer>
+        <div className="m-auto">
+          <SpinnerLoader className="w-8 h-8" />
+        </div>
+      </ModuleContainer>
+    );
+  }
+
+  if (isEmpty) {
+    return (
+      <ModuleContainer>
+        <div className="m-auto flex flex-col items-center text-center">
+          <div className="mb-4">{symbole}</div>
+          <h4 className="mb-2 text-primary-8">{title}</h4>
+          <p className="m-0 font-bold text-primary-9">
+            Aucun résultat pour ce filtre !
+          </p>
+          <div className="flex gap-4 my-6">
+            {selectedFilters.map(filter => (
+              <Badge key={filter} title={filter} state="standard" />
+            ))}
+          </div>
+          <Button size="sm" onClick={() => setIsModalOpen(true)}>
+            Modifier le filtre
+          </Button>
+          {editModal({isOpen: isModalOpen, setIsOpen: setIsModalOpen})}
+        </div>
+      </ModuleContainer>
+    );
+  }
 
   return (
-    <div className="p-8 bg-primary-0 border border-primary-4 rounded-xl">
-      <h6>{title}</h6>
+    <ModuleContainer>
+      <div className="flex items-start gap-20">
+        <h6>{title}</h6>
+        <>
+          <Button
+            variant="outlined"
+            icon="settings-2-line"
+            size="xs"
+            className="ml-auto"
+            onClick={() => setIsModalOpen(true)}
+          />
+          {editModal({isOpen: isModalOpen, setIsOpen: setIsModalOpen})}
+        </>
+      </div>
       {/** Filters */}
       {children}
-    </div>
+    </ModuleContainer>
   );
 };
 
 export default Module;
+
+const ModuleContainer = ({children}: {children: React.ReactNode}) => (
+  <div className="min-h-[21rem] flex flex-col p-8 bg-primary-0 border border-primary-4 rounded-xl">
+    {children}
+  </div>
+);
