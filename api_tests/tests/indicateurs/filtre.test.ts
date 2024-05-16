6d805249601da2beb41cc7dd24dfcb27036147a1
Marc Rutkowski <marc@attractive-media.fr>
2024-05-16 17:59:38 +0200

    Fixe un test

--- api_tests/tests/indicateurs/filtre.test.ts
+++ api_tests/tests/indicateurs/filtre.test.ts
@@ -139,7 +139,7 @@ const expectations: {
     filter: {
       collectivite_id: 1,
     },
-    count: 200, // 199 indicateurs prédéfinis + 1 perso
+    count: 202, // 201 indicateurs prédéfinis + 1 perso
     examples: [
       {
         collectivite_id: 1,
