f505e1cbecf9cf7f180324bffdcf874bee8e6097
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-05-17 11:58:41 +0200

    Ajoute un composant pagination au design system

---
+++ packages/ui/src/design-system/Pagination/PaginationPageButton.tsx
@@ -0,0 +1,29 @@
+import {Button} from '@design-system/Button';
+
+type PaginationPageButtonProps = {
+  pageNumber?: number;
+  isSelected?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const PaginationPageButton = ({
+  pageNumber,
+  isSelected = false,
+  onClick,
+}: PaginationPageButtonProps) => {
+  return (
+    <Button
+      aria-current="page"
+      variant={isSelected ? 'primary' : 'outlined'}
+      disabled={pageNumber === undefined}
+      size="xs"
+      className="!p-2 !w-9 justify-center"
+      title={`Page ${pageNumber}`}
+      onClick={onClick}
+    >
+      {pageNumber ?? '...'}
+    </Button>
+  );
+};
+
+export default PaginationPageButton;
