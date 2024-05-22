7929676ed60f70a8f4d6d28516511b7b807e8c3d
Arthur Molinos <>
2024-05-22 17:11:00 +0200

    Ajoute la possibilité de masquer les graph des cartes indicateur et le nombre total d'indicateurs filtrés

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/ModuleIndicateurs/ModuleIndicateursPage.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/ModuleIndicateurs/ModuleIndicateursPage.tsx
@@ -8,6 +8,7 @@ import IndicateurCard from 'app/pages/collectivite/Indicateurs/lists/IndicateurC
 import {getIndicateurGroup} from 'app/pages/collectivite/Indicateurs/lists/IndicateurCard/utils';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {Pagination} from 'ui/shared/Pagination';
+import {Checkbox} from '@tet/ui';
 
 type Props = {
   view: TDBViewParam;
@@ -21,18 +22,46 @@ const ModuleIndicateursPage = ({view, plan_ids}: Props) => {
     plan_ids,
   });
 
+  /** Nombre total d'indicateurs filtrés */
+  const total = data?.length;
+
+  /** Nombre d'indicateurs par page */
   const perPage = 9;
 
-  const nbOfPages = data ? Math.ceil(data.length / perPage) : 1;
+  /** Nombre total de pages */
+  const nbOfPages = total ? Math.ceil(total / perPage) : 1;
 
+  /** Page courante */
   const [currentPage, setCurrentPage] = useState(1);
 
+  /** Liste filtrée des indicateurs à afficher */
   const currentDefs = data?.filter(
     (_, i) => Math.floor(i / perPage) + 1 === currentPage
   );
 
+  /** Affiche ou cache les graphiques des cartes */
+  const [displayGraphs, setDisplayGraphs] = useState(true);
+
   return (
     <ModulePage view={view} title={indicateursSuiviPlans.title}>
+      {/** Paramètres de la liste */}
+      <div className="flex items-center gap-8 mb-8 py-6 border-b border-primary-3">
+        <Checkbox
+          variant="switch"
+          label="Afficher les graphiques"
+          labelClassname="font-normal !text-grey-7"
+          checked={displayGraphs}
+          onChange={() => setDisplayGraphs(!displayGraphs)}
+        />
+        {total && (
+          <span className="text-grey-7">
+            {total}
+            {` `}
+            {`indicateur${total > 1 && 's'}`}
+          </span>
+        )}
+      </div>
+      {/** Liste d'indicateurs */}
       {currentDefs && (
         <>
           <div className="grid grid-cols-2 2xl:grid-cols-3 gap-4">
@@ -47,6 +76,7 @@ const ModuleIndicateursPage = ({view, plan_ids}: Props) => {
                 })}
                 className="hover:!bg-white"
                 card={{external: true}}
+                hideChart={!displayGraphs}
               />
             ))}
           </div>
