b89f9a19a34bfea1ecc4d26a5f4456ccaa7e2c39
Marc Rutkowski <marc@attractive-media.fr>
2024-05-23 12:16:49 +0200

    Ajoute la redirection des requêtes sur posthog dans l'app

--- app.territoiresentransitions.react/server.js
+++ app.territoiresentransitions.react/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 const zipUrls = require('./src/server/zipUrls');
 
 const app = express();
 const directory = '/' + (process.env.STATIC_DIR || 'build');
 app.use(express.static(__dirname + directory));
+
+// redirection des requêtes sur posthog
+app.use(
+  '/ingest',
+  createProxyMiddleware('/ingest', {
+    target: 'https://eu.posthog.com',
+    changeOrigin: true,
+  })
+);
 
 app.use(express.json()); // for parsing application/json
 app.post('/zip', zipUrls);
