a0f746615608f2599c950c4c197210a2f2354213
Fred <98240+farnoux@users.noreply.github.com>
2024-05-27 18:06:25 +0200

    TDB: Fixe et test le fetch et save des modules

--- packages/api/src/collectivites/tableau_de_bord.show/actions/modules.save.test.ts
+++ packages/api/src/collectivites/tableau_de_bord.show/actions/modules.save.test.ts
@@ -1,37 +1,68 @@
 import {beforeAll, expect, test} from 'vitest';
 import {signIn, signOut} from '../../../tests/auth';
 import {supabase} from '../../../tests/supabase';
+import {modulesFetch} from './modules.fetch';
 import {modulesSave} from './modules.save';
+import {Module} from '../domain/module.schema';
 
 const params = {
   dbClient: supabase,
   collectiviteId: 1,
+  userId: '17440546-f389-4d4f-bfdb-b0c94a1bd0f9',
 };
 
 beforeAll(async () => {
   await signIn('yolododo');
 
   return async () => {
+    await supabase
+      .from('tableau_de_bord_module')
+      .delete()
+      .eq('collectivite_id', params.collectiviteId)
+      .eq('user_id', params.userId);
+
+    const {data} = await supabase.from('tableau_de_bord_module').select('*');
+    expect(data).toHaveLength(0);
+
     await signOut();
   };
 });
 
 test('filtre vide', async () => {
-  const {data} = await modulesSave({
-    ...params,
+  const module: Module = {
+    id: crypto.randomUUID(),
+    collectiviteId: 1,
+    userId: '17440546-f389-4d4f-bfdb-b0c94a1bd0f9',
+    titre: 'Mon module personnalisé',
+    type: 'fiche_action.list',
     options: {
-      filter: {},
+      filtre: {
+        utilisateurPiloteIds: ['17440546-f389-4d4f-bfdb-b0c94a1bd0f9'],
+      },
     },
-  });
+  };
 
-  expect(data).toMatchObject({});
-});
-
-test('filtre sur un statut', async () => {
-  const {data} = await modulesSave({
+  const {data: noData} = await modulesSave({
     ...params,
-    options: {filter: {statuts: ['En cours']}},
+    module,
   });
 
-  expect(data).toMatchObject({});
+  expect(noData).toBeNull();
+
+  const {data} = await modulesFetch({...params});
+
+  expect(data).toHaveLength(1);
+  expect(data[0]).toMatchObject({
+    titre: module.titre,
+    type: module.type,
+    options: module.options,
+  });
 });
+
+// test('filtre sur un statut', async () => {
+//   const {data} = await modulesSave({
+//     ...params,
+//   });
+
+//   expect(data).toMatchObject({});
+// });
--- packages/api/src/collectivites/tableau_de_bord.show/actions/modules.save.ts
+++ packages/api/src/collectivites/tableau_de_bord.show/actions/modules.save.ts
@@ -1,33 +1,36 @@
-import {QueryOptions} from '../../../fiche_actions';
+import {z} from 'zod';
 import {DBClient} from '../../../typeUtils';
+import {
+  moduleCommonSchemaInsert,
+  moduleFicheActionsSchema,
+  moduleIndicateursSchema,
+} from '../domain/module.schema';
+import {objectToCamel, objectToSnake} from 'ts-case-convert';
+
+const insertSchema = moduleCommonSchemaInsert;
+type ModuleInsert = z.infer<typeof insertSchema>;
 
 type Props = {
   dbClient: DBClient;
-  collectiviteId: number;
-  userId: number;
-  options: QueryOptions;
+  module: ModuleInsert;
 };
 
-export async function modulesSave({
-  dbClient,
-  collectiviteId,
-  userId,
-  options,
-}: Props) {
+export async function modulesSave({dbClient, module: unsafeModule}: Props) {
+  const commonPart = moduleCommonSchemaInsert.parse(unsafeModule);
+  const specificPart = parseSpecificPart(unsafeModule);
+
+  const module = {
+    ...commonPart,
+    ...specificPart,
+  };
+
   try {
     const {data, error} = await dbClient
       .from('tableau_de_bord_module')
-      .upsert(
-        {
-          collectivite_id: collectiviteId,
-          user_id: userId,
-          type: 'fiche_actions',
-          query_options: options,
-        },
-        {onConflict: 'collectivite_id, user_id, type'}
-      )
-      .eq('collectivite_id', collectiviteId)
-      .eq('user_id', userId);
+      .upsert(objectToSnake(module), {onConflict: 'id'})
+      .eq('id', module.id);
+    // .eq('collectivite_id', module.collectiviteId)
+    // .eq('user_id', module.userId);
 
     if (error) {
       throw error;
@@ -38,4 +41,16 @@ export async function modulesSave({
     console.error(error);
     return {error};
   }
+}
+
+function parseSpecificPart(module) {
+  if (module.type === 'fiche_action.list') {
+    return moduleFicheActionsSchema.parse(module);
+  }
+
+  if (module.type === 'indicateur.list') {
+    return moduleIndicateursSchema.parse(module);
+  }
+
+  throw new Error('Invalid module type');
 }
