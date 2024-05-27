260bc99ea078c2fe9d7233189cc10328a46686b4
Fred <98240+farnoux@users.noreply.github.com>
2024-05-27 18:06:25 +0200

    TDB: Fixe et test le fetch et save des modules

--- packages/api/src/collectivites/tableau_de_bord.show/domain/module.schema.ts
+++ packages/api/src/collectivites/tableau_de_bord.show/domain/module.schema.ts
@@ -1,30 +1,37 @@
 import {z} from 'zod';
 import {fetchOptionsSchema} from '../../../fiche_actions/shared/domain/fetch_options.schema';
 
-const moduleCommonSchema = z.object({
+export const moduleCommonSchemaInsert = z.object({
   id: z.string().uuid(),
   collectiviteId: z.number(),
-  userId: z.number(),
+  userId: z.string().uuid(),
   titre: z.string(),
-  description: z.string().nullish(),
-  type: z.enum(['fiche_action.list', 'indicateur.list']),
+  options: z.any(),
+});
+
+export const moduleCommonSchemaSelect = moduleCommonSchemaInsert.extend({
   createdAt: z.string().datetime(),
   modifiedAt: z.string().datetime(),
 });
 
-export const moduleIndicateursSchema = moduleCommonSchema.extend({
+export const moduleIndicateursSchema = z.object({
   type: z.literal('indicateur.list'),
   options: z.object({}),
 });
 
-export const moduleFicheActionsSchema = moduleCommonSchema.extend({
+export const moduleFicheActionsSchema = z.object({
   type: z.literal('fiche_action.list'),
   options: fetchOptionsSchema,
 });
 
-export const moduleSchema = z.union([
-  moduleIndicateursSchema,
-  moduleFicheActionsSchema,
+export const moduleSchemaSelect = z.discriminatedUnion('type', [
+  moduleIndicateursSchema.merge(moduleCommonSchemaSelect),
+  moduleFicheActionsSchema.merge(moduleCommonSchemaSelect),
 ]);
 
-export type Module = z.infer<typeof moduleSchema>;
+// export const moduleSchema = z.union([
+//   moduleIndicateursSchema,
+//   moduleFicheActionsSchema,
+// ]);
+
+export type Module = z.infer<typeof moduleSchemaSelect>;
