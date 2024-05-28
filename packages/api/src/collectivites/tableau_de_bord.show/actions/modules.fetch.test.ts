b0a63585689f1255854ed0e8a7f1335b6b5e52ab
Fred <98240+farnoux@users.noreply.github.com>
2024-05-28 14:47:54 +0200

    TDB: Renvoie uniquement les modules par défaut manquants

--- packages/api/src/collectivites/tableau_de_bord.show/actions/modules.fetch.test.ts
+++ packages/api/src/collectivites/tableau_de_bord.show/actions/modules.fetch.test.ts
@@ -1,9 +1,10 @@
-import {beforeAll, expect, test} from 'vitest';
+import {beforeEach, expect, test} from 'vitest';
 import {signIn, signOut} from '../../../tests/auth';
 import {supabase} from '../../../tests/supabase';
+import {defaultSlugsSchema, getDefaultModules} from '../domain/module.schema';
 import {modulesFetch} from './modules.fetch';
 import {modulesSave} from './modules.save';
-import {moduleNew} from './modules.save.test';
+import {moduleNew, resetTableauDeBordModules} from './modules.test-fixture';
 
 const params = {
   dbClient: supabase,
@@ -11,10 +12,13 @@ const params = {
   userId: '17440546-f389-4d4f-bfdb-b0c94a1bd0f9',
 };
 
-beforeAll(async () => {
+const numberOfModulesByDefault = getDefaultModules(params).length;
+
+beforeEach(async () => {
   await signIn('yolododo');
 
   return async () => {
+    await resetTableauDeBordModules(params);
     await signOut();
   };
 });
@@ -39,6 +43,42 @@ test("Renvoie les 3 modules par défaut si aucun n'a été précédemment enregi
   });
 });
 
+test('Renvoie un module enregistré et les 2 autres par défaut', async () => {
+  const defaultSlug = defaultSlugsSchema.enum['actions-dont-je-suis-pilote'];
+  const module = {
+    ...moduleNew,
+    slug: defaultSlug,
+  };
+
+  await modulesSave({
+    ...params,
+    module,
+  });
+
+  const {data} = await modulesFetch(params);
+
+  expect([{foo: 'bar', hello: 1}, {baz: 1}]).toMatchObject([
+    {foo: 'bar'},
+    {baz: 1},
+  ]);
+
+  expect(data).toHaveLength(3);
+
+  expect(data).toMatchObject([
+    module,
+    {
+      titre: expect.stringMatching(/indicateurs/i),
+      type: 'indicateur.list',
+      options: expect.any(Object),
+    },
+    {
+      titre: expect.stringMatching(/actions/i),
+      type: 'fiche_action.list',
+      slug: expect.not.stringMatching(defaultSlug),
+    },
+  ]);
+});
+
 test("RLS: Vérifie l'accès en lecture sur la collectivité", async () => {
   const module = moduleNew;
 
@@ -48,7 +88,7 @@ test("RLS: Vérifie l'accès en lecture sur la collectivité", async () => {
   });
 
   const {data} = await modulesFetch(params);
-  expect(data).toHaveLength(1);
+  expect(data).toHaveLength(numberOfModulesByDefault + 1);
 
   // Se connecte avec un autre utilisateur de la collectivité, autorisé en lecture
   await signOut();
