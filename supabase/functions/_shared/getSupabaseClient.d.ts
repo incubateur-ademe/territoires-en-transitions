82b216cb5e60c29e06e8531f2034259fd94a7cf5
Fred <98240+farnoux@users.noreply.github.com>
2024-05-29 14:47:59 +0200

    Revert "TDB: Récupère les valeurs de filtre aussi pour celui du fetch des indcateurs"
    This reverts commit 1f8b0c028bf2e5ab39e6d97ce0f689d34f5caf21.
    Only bad files that were not supposed to be commited.

--- supabase/functions/_shared/getSupabaseClient.d.ts
+++
@@ -1,6 +0,0 @@
-/** Crée le client avec le contexte d'authentification de l'utilisateur connecté */
-export declare const getSupabaseClient: (req: Request) => any;
-/** Crée le client avec le contexte d'authentification "Service" */
-export declare const getSupabaseClientWithServiceRole: () => any;
-export type TSupabaseClient = ReturnType<typeof getSupabaseClient>;
-//# sourceMappingURL=getSupabaseClient.d.ts.map
