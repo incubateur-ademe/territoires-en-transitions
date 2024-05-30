eabe62ca0d707faeaafa90f1baa1c0b341212632
Marc Rutkowski <marc@attractive-media.fr>
2024-05-30 11:52:41 +0200

    Change les tests e2e pour tester l'envoi des invitations à rejoindre une collectivité

--- e2e/cypress/integration/07-ajouter-un-membre/steps.js
+++ e2e/cypress/integration/07-ajouter-un-membre/steps.js
@@ -20,23 +20,37 @@ When("un formulaire d'invitation est affiché", () => {
   );
 });
 
-When("un message d'invitation est affiché", () => {
-  cy.get(LocalSelectors["message d'invitation"].selector).should('be.visible');
-});
 When(
   /je renseigne l'email "([^"]+)" de la personne à inviter en "([^"]+)"/,
-  (email, acces) => {
+  (email, niveau) => {
     cy.get('input[name="email"]').type('{selectall}{backspace}' + email);
-    cy.get('select[name="acces"]').select(acces);
+    cy.get('[data-test=niveau]').click();
+    cy.get(`[data-test=niveau-options] button[data-test=${niveau}]`).click();
   }
 );
 
 When(/le tableau des membres doit contenir l'utilisateur "([^"]+)"/, mail => {
   cy.get(LocalSelectors['tableau des membres'].selector).should(
-    'contain',
+    'contain.text',
     mail
   );
 });
+
+When(/le tableau des membres n'inclus pas l'utilisateur "([^"]+)"/, mail => {
+  cy.get(LocalSelectors['tableau des membres'].selector).should(
+    'not.contain.text',
+    mail
+  );
+});
+
+When(
+  'le tableau des membres indique que le compte {string} est en attente de création',
+  mail => {
+    cy.get(`[data-test="MembreRow-${mail}"]`)
+      .should('contain.text', mail)
+      .should('contain.text', 'Création de compte en attente');
+  }
+);
 
 When(
   /la page contient (?:les|la) collectivités? "([^"]*)"/,
