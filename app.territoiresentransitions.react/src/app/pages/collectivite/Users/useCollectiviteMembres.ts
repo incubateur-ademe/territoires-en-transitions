fed0b127d444df6badc77cfebf6ee7bb27bb5bb7
Marc Rutkowski <marc@attractive-media.fr>
2024-05-30 15:55:09 +0200

    Ajoute la pagination dans la page Gestion des membres

--- app.territoiresentransitions.react/src/app/pages/collectivite/Users/useCollectiviteMembres.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Users/useCollectiviteMembres.ts
@@ -6,35 +6,41 @@ import {Membre} from './types';
 /**
  * Donne accès à la liste des membres de la collectivité courante
  */
-export const useCollectiviteMembres = (): {
-  membres: Membre[];
-  isLoading: boolean;
-} => {
+export const useCollectiviteMembres = (pageNum: number = 1) => {
   const collectiviteId = useCollectiviteId();
-
-  const {data, isLoading} = useQuery(getQueryKey(collectiviteId), () =>
-    collectiviteId ? fetchMembresForCollectivite(collectiviteId) : []
+  const {data, ...otherProps} = useQuery(
+    getQueryKey(collectiviteId, pageNum),
+    () =>
+      collectiviteId
+        ? fetchMembresForCollectivite(collectiviteId, pageNum)
+        : NO_RESULT
   );
 
-  return {membres: data || [], isLoading};
+  return {data: data || NO_RESULT, ...otherProps};
 };
 
-export const getQueryKey = (collectivite_id: number | null) => [
-  'collectivite_membres',
-  collectivite_id,
-];
+export const getQueryKey = (collectivite_id: number | null, pageNum?: number) =>
+  pageNum
+    ? ['collectivite_membres', collectivite_id, pageNum]
+    : ['collectivite_membres', collectivite_id];
+
+export const PAGE_SIZE = 30;
+const NO_RESULT = {membres: [], count: 0};
 
 const fetchMembresForCollectivite = async (
-  collectiviteId: number
-): Promise<Membre[]> => {
-  const {data, error} = await supabaseClient
-    .rpc('collectivite_membres', {
-      id: collectiviteId,
-    })
-    .select();
+  collectiviteId: number,
+  pageNum: number
+) => {
+  const from = (pageNum - 1) * PAGE_SIZE;
+  const {data, error, count} = await supabaseClient
+    .rpc('collectivite_membres', {id: collectiviteId}, {count: 'exact'})
+    .select()
+    .range(from, from + PAGE_SIZE - 1)
+    .returns<Membre[]>();
 
-  if (error) {
-    return [];
-  }
-  return (data as unknown as Membre[]) || null;
+  return error ? NO_RESULT : {membres: data, count: count as number};
 };
+
+export type CollectiviteMembres = Awaited<
+  ReturnType<typeof fetchMembresForCollectivite>
+>;
