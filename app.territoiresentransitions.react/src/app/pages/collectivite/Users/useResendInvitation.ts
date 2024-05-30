7dac52330d062f568259a0b5fb1930df3723344d
Marc Rutkowski <marc@attractive-media.fr>
2024-05-30 19:06:11 +0200

    Ajoute une fonction pour renvoyer une invitation

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Users/useResendInvitation.ts
@@ -0,0 +1,59 @@
+import {useMutation} from 'react-query';
+import {makeInvitationLandingPath} from 'app/paths';
+import {CurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
+import {getAuthBaseUrl} from '@tet/api';
+import {UserData} from 'core-logic/api/auth/AuthProvider';
+import {useAuthHeaders} from 'core-logic/api/auth/useCurrentSession';
+
+export type ResendInvitationArgs = {
+  email: string;
+  invitationId: string;
+};
+
+/**
+ * Ajoute un utilisateur à une collectivité donnée
+ */
+export const useResendInvitation = (
+  collectivite: CurrentCollectivite,
+  user: UserData
+) => {
+  const {nom: nomCollectivite} = collectivite;
+  const authHeaders = useAuthHeaders();
+
+  return useMutation(
+    async ({invitationId, email}: ResendInvitationArgs) => {
+      const invitationUrl = makeInvitationLandingPath(invitationId);
+
+      // envoi le mail d'invitation
+      if (invitationUrl) {
+        const invitePath = `${getAuthBaseUrl(
+          document.location.hostname
+        )}/invite`;
+        const {prenom, nom, email: emailFrom} = user;
+        const {status} = await fetch(invitePath, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            ...authHeaders,
+          },
+          body: JSON.stringify({
+            to: email?.toLowerCase(),
+            from: {prenom, nom, email: emailFrom},
+            collectivite: nomCollectivite,
+            invitationUrl,
+          }),
+        });
+        if (status > 200) {
+          throw Error("Echec de l'envoi d'email");
+        }
+      }
+
+      return true;
+    },
+    {
+      meta: {
+        success: `L'invitation a rejoindre la collectivité ${collectivite.nom} a bien été envoyée`,
+      },
+    }
+  );
+};
