753384eaf001f5e4ab1648c40022f2bc9878115a
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-05-31 11:38:51 +0200

    Mise à jour du module équipe de la page outil numérique

--- packages/site/app/outil-numerique/EquipePlateforme.tsx
+++ packages/site/app/outil-numerique/EquipePlateforme.tsx
@@ -1,49 +1,21 @@
 'use client';
 
 import Section from '@components/sections/Section';
-import {StrapiItem} from 'src/strapi/StrapiItem';
-import {useEffect, useRef, useState} from 'react';
 import {Button} from '@tet/ui';
 
 type EquipePlateformeProps = {
   titre: string;
   citation?: string;
   description?: string;
-  liste: {
-    id: number;
-    titre: string;
-    legende: string;
-    image: StrapiItem;
-  }[];
-  cta_contact: string;
+  cta: string;
 };
 
 const EquipePlateforme = ({
   titre,
   citation,
   description,
-  liste,
+  cta,
 }: EquipePlateformeProps) => {
-  const [carouselWidth, setCarouselWidth] = useState(0);
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    // Initialisation de carouselWidth au chargement de la page
-    if (ref && ref.current) {
-      const element = ref.current;
-      setCarouselWidth(element.clientWidth);
-
-      // Détecte le changement de taille de la fenêtre
-      window.addEventListener('resize', () =>
-        setCarouselWidth(element.clientWidth),
-      );
-      return () =>
-        window.removeEventListener('resize', () =>
-          setCarouselWidth(element.clientWidth),
-        );
-    }
-  }, []);
-
   return (
     <Section>
       <h2 className="text-primary-8 md:text-center mb-1">{titre}</h2>
@@ -52,11 +24,22 @@ const EquipePlateforme = ({
           {citation}
         </h4>
       )}
+
       {!!description && (
         <p className="text-primary-10 md:text-center md:text-[18px] leading-[30px] mb-0">
           {description}
         </p>
       )}
+
+      <Button
+        href="https://beta.gouv.fr/startups/territoires-en-transitions.html#equipe"
+        variant="outlined"
+        iconPosition="left"
+        external
+        className="!mx-auto"
+      >
+        {cta}
+      </Button>
     </Section>
   );
 };
