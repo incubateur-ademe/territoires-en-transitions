4fd6af12b3a1872e62a99e7e8a27a6985dd37273
Marc Rutkowski <marc@attractive-media.fr>
2024-06-04 19:02:05 +0200

    Utilise la fonction mutualisée pour l'envoi des invitations

--- app.territoiresentransitions.react/src/app/pages/collectivite/Users/useAddUserToCollectivite.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Users/useAddUserToCollectivite.ts
@@ -1,11 +1,9 @@
 import {useMutation, useQueryClient} from 'react-query';
 import {supabaseClient} from 'core-logic/api/supabase';
 import {TNiveauAcces} from 'types/alias';
-import {makeInvitationLandingPath} from 'app/paths';
 import {CurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
-import {getAuthBaseUrl} from '@tet/api';
 import {UserData} from 'core-logic/api/auth/AuthProvider';
-import {useAuthHeaders} from 'core-logic/api/auth/useCurrentSession';
+import {useSendInvitation} from 'app/pages/collectivite/Users/useSendInvitation';
 
 type AddUserToCollectiviteArgs = {
   email: string;
@@ -34,15 +32,15 @@ export const useAddUserToCollectivite = (
   collectivite: CurrentCollectivite,
   user: UserData
 ) => {
-  const {collectivite_id, nom: nomCollectivite} = collectivite;
-  const authHeaders = useAuthHeaders();
+  const {collectivite_id} = collectivite;
   const queryClient = useQueryClient();
+  const {mutate: sendInvitation} = useSendInvitation(collectivite, user);
 
   return useMutation(
     async ({email, niveau}: AddUserToCollectiviteArgs) => {
       const {data, error} = await supabaseClient.rpc('add_user', {
         collectivite_id,
-        email: email?.toLowerCase(),
+        email: email.toLowerCase(),
         niveau,
       });
 
@@ -54,38 +52,16 @@ export const useAddUserToCollectivite = (
       }
 
       const response = data as unknown as AddUserToCollectiviteData;
-      const invitationUrl = response?.invitation_id
-        ? makeInvitationLandingPath(response?.invitation_id)
-        : undefined;
+      const invitationId = response?.invitation_id;
 
       // envoi le mail d'invitation
-      if (invitationUrl) {
-        const invitePath = `${getAuthBaseUrl(
-          document.location.hostname
-        )}/invite`;
-        const {prenom, nom, email: emailFrom} = user;
-        const {status} = await fetch(invitePath, {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json',
-            ...authHeaders,
-          },
-          body: JSON.stringify({
-            to: email?.toLowerCase(),
-            niveau,
-            from: {prenom, nom, email: emailFrom},
-            collectivite: nomCollectivite,
-            invitationUrl,
-          }),
-        });
-        if (status > 200) {
-          return {error: "Echec de l'envoi d'email", added: false};
-        }
+      if (invitationId) {
+        await sendInvitation({email: email.toLowerCase(), invitationId});
       }
 
       return {
-        invitationUrl,
         added: response?.added ?? false,
+        invitationId,
       };
     },
     {
@@ -94,6 +70,9 @@ export const useAddUserToCollectivite = (
           'collectivite_membres',
           collectivite_id,
         ]);
+      },
+      meta: {
+        disableToast: true,
       },
     }
   );
