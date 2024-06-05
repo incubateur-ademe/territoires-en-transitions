3cababb24892bda77504037c7fdc154ef5735585
Arthur Molinos <arthur.molinos@gmail.com>
2024-06-05 11:10:51 +0200

    Rétabli z-index dans les dropdown

--- packages/ui/src/design-system/Select/components/DropdownFloater.tsx
+++ packages/ui/src/design-system/Select/components/DropdownFloater.tsx
@@ -14,8 +14,11 @@ import {
   FloatingPortal,
   FloatingFocusManager,
   useFloatingParentNodeId,
+  useFloatingNodeId,
+  FloatingNode,
 } from '@floating-ui/react';
 import classNames from 'classnames';
+import {preset} from '@tailwind-preset';
 
 type DropdownFloaterProps = {
   /** Élement qui reçoit la fonction d'ouverture du dropdown */
@@ -58,7 +61,10 @@ export const DropdownFloater = ({
 
   const [maxHeight, setMaxHeight] = useState(null);
 
+  const nodeId = useFloatingNodeId();
+
   const {x, y, strategy, refs, context} = useFloating({
+    nodeId,
     open: disabled ? false : isOpen,
     onOpenChange: disabled ? () => null : setIsOpen,
     placement: placement ?? 'bottom',
@@ -91,6 +97,10 @@ export const DropdownFloater = ({
     dismiss,
   ]);
 
+  /** si l'id n'est pas null alors le DropdownFloater est contenu dans un élément
+   * où le floating node est déjà présent (par exemple dans une modale) */
+  const parentNodeId = useFloatingParentNodeId();
+
   return (
     <>
       {cloneElement(
@@ -117,35 +127,40 @@ export const DropdownFloater = ({
           ...children.props,
         })
       )}
-      {isOpen && (
-        <FloaterContent parentId={parentId}>
-          <FloatingFocusManager context={context}>
-            <div
-              data-test={dataTest}
-              {...getFloatingProps({
-                ref: refs.setFloating,
-                style: {
-                  position: strategy,
-                  top: y,
-                  left: x,
-                },
-              })}
-            >
+      <FloatingNode id={nodeId}>
+        {isOpen && (
+          <FloaterContent parentId={parentId} parentNodeId={parentNodeId}>
+            <FloatingFocusManager context={context}>
               <div
-                className={classNames(
-                  'overflow-y-auto bg-white rounded-b-lg border border-grey-4 border-t-0',
-                  containerClassName
-                )}
-                style={{maxHeight: maxHeight - 16}}
-              >
-                {render({
-                  close: () => setIsOpen(false),
+                data-test={dataTest}
+                {...getFloatingProps({
+                  ref: refs.setFloating,
+                  style: {
+                    position: strategy,
+                    top: y,
+                    left: x,
+                    // Applique uniquement le z-index quand le dropdown
+                    // n'est pas contenu dans un autre floater (ex. modale)
+                    zIndex: parentNodeId ?? preset.theme.extend.zIndex.dropdown,
+                  },
                 })}
+              >
+                <div
+                  className={classNames(
+                    'overflow-y-auto bg-white rounded-b-lg border border-grey-4 border-t-0',
+                    containerClassName
+                  )}
+                  style={{maxHeight: maxHeight - 16}}
+                >
+                  {render({
+                    close: () => setIsOpen(false),
+                  })}
+                </div>
               </div>
-            </div>
-          </FloatingFocusManager>
-        </FloaterContent>
-      )}
+            </FloatingFocusManager>
+          </FloaterContent>
+        )}
+      </FloatingNode>
     </>
   );
 };
@@ -154,14 +169,11 @@ type Props = {
   children: React.ReactNode;
   /** Id custom de l'élément dans lequel rendre le portal si donné */
   parentId?: string;
+  parentNodeId: string | null;
 };
 
 /** Permet de rendre le dropdown dans un portal ou non si un parent existe déjà */
-const FloaterContent = ({children, parentId}: Props) => {
-  /** si l'id n'est pas null alors le DropdownFloater est contenu dans un élément
-   * où le floating node est déjà présent (par exemple dans une modale) */
-  const parentNodeId = useFloatingParentNodeId();
-
+const FloaterContent = ({children, parentId, parentNodeId}: Props) => {
   if (parentNodeId) {
     return children;
   } else {
