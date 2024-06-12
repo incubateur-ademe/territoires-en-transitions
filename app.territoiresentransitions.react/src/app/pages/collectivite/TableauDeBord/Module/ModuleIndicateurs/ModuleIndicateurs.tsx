60080397871675139d9eedf85d522338cf74795f
Fred <98240+farnoux@users.noreply.github.com>
2024-06-12 12:43:12 +0200

    TDB: Oubli d'un fichier

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/ModuleIndicateurs/ModuleIndicateurs.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/ModuleIndicateurs/ModuleIndicateurs.tsx
@@ -2,8 +2,6 @@ import {useHistory} from 'react-router-dom';
 
 import {Button} from '@tet/ui';
 
-import {useFiltreValuesFetch} from '@tet/api/dist/src/collectivites/shared/actions/filtre_values.fetch';
-import {FiltreValues} from '@tet/api/dist/src/collectivites/shared/domain/filtre_ressource_liees.schema';
 import {ModuleIndicateursSelect} from '@tet/api/dist/src/collectivites/tableau_de_bord.show/domain/module.schema';
 import {optionsToFilters} from '@tet/api/dist/src/indicateurs/fetchFilteredIndicateurs';
 import IndicateurCard from 'app/pages/collectivite/Indicateurs/lists/IndicateurCard/IndicateurCard';
@@ -15,25 +13,20 @@ import {
   makeCollectiviteIndicateursUrl,
   makeTableauBordModuleUrl,
 } from 'app/paths';
-import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import PictoIndicateurVide from 'ui/pictogrammes/PictoIndicateurVide';
 import Module from '../Module';
 
 type Props = {
   view: TDBViewParam;
-  planIds?: number[];
   module: ModuleIndicateursSelect;
 };
 
-const ModuleIndicateurs = ({view, module, planIds}: Props) => {
+const ModuleIndicateurs = ({view, module}: Props) => {
   const collectiviteId = useCollectiviteId();
   const history = useHistory();
 
-  const filtre = {
-    plan_ids: planIds,
-    ...optionsToFilters(module.options),
-  };
+  const filtre = optionsToFilters(module.options);
 
   const {data, isLoading} = useFilteredIndicateurDefinitions(null, filtre);
 
