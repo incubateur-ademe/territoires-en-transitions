b36713f6fee80a878c778ddb256b86636585ad1f
Fred <98240+farnoux@users.noreply.github.com>
2024-06-12 13:04:12 +0200

    TDB: Rafraichit les modules quand on change le filtre des indicateurs

--- app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/useModulesFetch.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/TableauDeBord/Module/useModulesFetch.tsx
@@ -13,28 +13,28 @@ export const useModulesFetch = () => {
   const collectiviteId = useCollectiviteId();
   const userId = useAuth().user?.id;
 
-  return useQuery(
-    ['collectivite_tableau_de_bord_modules', collectiviteId, userId],
-    async () => {
-      if (!collectiviteId) {
-        throw new Error('Aucune collectivité associée');
-      }
-
-      if (!userId) {
-        throw new Error('Aucun utilisateur connecté');
-      }
-
-      const {data, error} = await modulesFetch({
-        dbClient: supabaseClient,
-        collectiviteId,
-        userId: userId,
-      });
-
-      if (error) {
-        throw error;
-      }
-
-      return data;
+  return useQuery(getQueryKey(collectiviteId, userId), async () => {
+    if (!collectiviteId) {
+      throw new Error('Aucune collectivité associée');
     }
-  );
+
+    if (!userId) {
+      throw new Error('Aucun utilisateur connecté');
+    }
+
+    const {data, error} = await modulesFetch({
+      dbClient: supabaseClient,
+      collectiviteId,
+      userId: userId,
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  });
 };
+
+export const getQueryKey = (collectiviteId?: number | null, userId?: string) =>
+  ['collectivite_tableau_de_bord_modules', collectiviteId, userId] as const;
