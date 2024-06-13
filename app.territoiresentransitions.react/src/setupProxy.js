fc0566112b6ff0162487383bc3884d3ea6af7bad
Marc Rutkowski <marc@attractive-media.fr>
2024-06-13 16:58:42 +0200

    Supprime une route inutilisée (mode dev)

--- app.territoiresentransitions.react/src/setupProxy.js
+++ app.territoiresentransitions.react/src/setupProxy.js
@@ -1,18 +1,7 @@
 const express = require('express');
-const {createProxyMiddleware} = require('http-proxy-middleware');
 const zipUrls = require('./server/zipUrls');
 
 module.exports = function (app) {
-  if (process.env.REACT_APP_WITH_PROXY === 'TRUE') {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      })
-    );
-  }
-
   app.use(express.json()); // for parsing application/json
   app.post('/zip', zipUrls);
 };
