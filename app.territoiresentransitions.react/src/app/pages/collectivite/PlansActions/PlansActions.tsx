615934fc42712a7d0df465f638eb2f796c78c9cb
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-06-25 15:37:17 +0200

    Nouveau layout de la fiche action avec titre éditable

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlansActions.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlansActions.tsx
@@ -1,6 +1,9 @@
 import {Route, Switch} from 'react-router-dom';
 
 import {
+  collectiviteFicheNonClasseePath,
+  collectivitePlanActionAxeFichePath,
+  collectivitePlanActionFichePath,
   collectivitePlansActionsSynthesePath,
   collectivitePlansActionsSyntheseVuePath,
   collectiviteTDBBasePath,
@@ -14,6 +17,7 @@ import {PlansActionsRoutes} from './PlansActionsRoutes';
 import {SynthesePage} from './Synthese/SynthesePage';
 import {SyntheseVuePage} from './Synthese/SyntheseVue/SyntheseVuePage';
 import {ToutesLesFichesActionPage} from 'app/pages/collectivite/PlansActions/ToutesLesFichesAction/ToutesLesFichesActionPage';
+import FicheActionPage from './FicheActionNew/FicheActionPage';
 
 const PlansActions = () => {
   const collectivite = useCurrentCollectivite();
@@ -22,22 +26,26 @@ const PlansActions = () => {
 
   return (
     <Switch>
+      {/* Tableau de bord */}
       <RouteEnAccesRestreint path={collectiviteTDBBasePath}>
         <TableauDeBordPage />
       </RouteEnAccesRestreint>
 
-      {/* Synthèse */}
+      {/* Page de synthèse */}
       <Route exact path={[collectivitePlansActionsSynthesePath]}>
         <CollectivitePageLayout dataTest="PlansAction">
           <SynthesePage collectiviteId={collectivite.collectivite_id} />
         </CollectivitePageLayout>
       </Route>
+
+      {/* Page de synthèse d'une métrique */}
       <Route exact path={[collectivitePlansActionsSyntheseVuePath]}>
         <CollectivitePageLayout dataTest="PlansAction">
           <SyntheseVuePage />
         </CollectivitePageLayout>
       </Route>
 
+      {/* Page de visualisation de toutes les fiches */}
       <Route exact path={collectiviteToutesLesFichesPath}>
         <div className="grow bg-grey-2 -mb-8 py-12 px-4 lg:px-6 2xl:px-0">
           <CollectivitePageLayout dataTest="ToutesLesFichesAction">
@@ -46,6 +54,19 @@ const PlansActions = () => {
         </div>
       </Route>
 
+      {/* Pages fiche action - nouvelle version */}
+      <Route
+        exact
+        path={[
+          collectiviteFicheNonClasseePath,
+          collectivitePlanActionFichePath,
+          collectivitePlanActionAxeFichePath,
+        ]}
+      >
+        <FicheActionPage readonly={collectivite.readonly} />
+      </Route>
+
+      {/* Autres routes */}
       <PlansActionsRoutes
         collectivite_id={collectivite.collectivite_id}
         readonly={collectivite.readonly}
