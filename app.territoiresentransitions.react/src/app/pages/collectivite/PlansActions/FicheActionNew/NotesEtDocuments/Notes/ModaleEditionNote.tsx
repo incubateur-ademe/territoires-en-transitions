31d20de3bd61499270423c9b63b875f916c2214f
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-06-25 15:37:17 +0200

    Ajout des notes complémentaires à la nouvelle fiche action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/NotesEtDocuments/Notes/ModaleEditionNote.tsx
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import {Button, Field, Modal, ModalFooterOKCancel, Textarea} from '@tet/ui';
+
+type ModaleEditionNoteProps = {
+  notes: string;
+  updateNotes: (notes: string | null) => void;
+};
+
+const ModaleEditionNote = ({notes, updateNotes}: ModaleEditionNoteProps) => {
+  const [editedNotes, setEditedNotes] = useState(notes);
+
+  const handleSave = () => {
+    if (notes !== editedNotes) {
+      updateNotes(editedNotes);
+    }
+  };
+
+  return (
+    <Modal
+      title="Modifier la note"
+      size="lg"
+      render={({descriptionId}) => (
+        <div id={descriptionId} className="flex flex-col gap-8">
+          {/* Décommenter au passage à notes privées */}
+          {/* <Alert description="La note est privée, elle n’est pas consultable par les personnes n’étant pas membres de votre collectivité." /> */}
+
+          <Field title="Note">
+            <Textarea
+              className="min-h-[100px]"
+              value={editedNotes}
+              onChange={evt =>
+                setEditedNotes((evt.target as HTMLTextAreaElement).value)
+              }
+            />
+          </Field>
+        </div>
+      )}
+      renderFooter={({close}) => (
+        <ModalFooterOKCancel
+          btnCancelProps={{onClick: close}}
+          btnOKProps={{
+            onClick: () => {
+              handleSave();
+              close();
+            },
+          }}
+        />
+      )}
+    >
+      <Button
+        icon="edit-line"
+        title="Modifier la note"
+        variant="grey"
+        size="xs"
+      />
+    </Modal>
+  );
+};
+
+export default ModaleEditionNote;
