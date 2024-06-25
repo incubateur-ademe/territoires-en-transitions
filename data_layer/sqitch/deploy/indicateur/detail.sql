1a23f230c39ae9b968c1dc538b0598f753d76829
Fred <98240+farnoux@users.noreply.github.com>
2024-06-25 11:18:02 +0200

    TDB: Fixe le filtre des indicateurs par plan action
    Fixe en supprimant la vérification de droits sur les axes liés aux indicateurs, qui est déjà faite sur la vue `indicateur_definitions`.

--- data_layer/sqitch/deploy/indicateur/detail.sql
+++ data_layer/sqitch/deploy/indicateur/detail.sql
@@ -4,24 +4,27 @@ BEGIN;
 
 create or replace view indicateur_definitions
 as
-select c.id           as collectivite_id,
-       definition.id  as indicateur_id,
-       null::integer  as indicateur_perso_id,
-       definition.nom as nom,
-       definition.description,
-       definition.unite
-from collectivite c
-         cross join indicateur_definition definition
-where is_authenticated()
-union all
-select definition.collectivite_id as collectivite_id,
-       null::indicateur_id        as indicateur_id,
-       definition.id              as indicateur_perso_id,
-       definition.titre,
-       definition.description,
-       definition.unite
-from indicateur_personnalise_definition definition
-where can_read_acces_restreint(collectivite_id);
+SELECT * FROM (
+    select c.id           as collectivite_id,
+        definition.id  as indicateur_id,
+        null::integer  as indicateur_perso_id,
+        definition.nom as nom,
+        definition.description,
+        definition.unite
+    from collectivite c
+            cross join indicateur_definition definition
+    where is_authenticated()
+    union all
+    select definition.collectivite_id as collectivite_id,
+        null::indicateur_id        as indicateur_id,
+        definition.id              as indicateur_perso_id,
+        definition.titre,
+        definition.description,
+        definition.unite
+    from indicateur_personnalise_definition definition
+) AS f
+WHERE can_read_acces_restreint(f.collectivite_id);
+
 comment on view indicateur_definitions
     is 'Les définitions des indicateurs prédéfinis et personnalisés';
 
