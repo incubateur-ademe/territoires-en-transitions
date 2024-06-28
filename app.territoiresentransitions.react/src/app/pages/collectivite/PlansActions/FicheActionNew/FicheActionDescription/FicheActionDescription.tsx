582720b45eca63d3cfafa7f899368fdd5e6a746c
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-06-28 10:56:12 +0200

    Ajout d'un bloc description à la FA avec modales d'édition

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FicheActionDescription/FicheActionDescription.tsx
@@ -0,0 +1,88 @@
+import {Badge} from '@tet/ui';
+import classNames from 'classnames';
+import {FicheAction} from '../../FicheAction/data/types';
+import FicheActionMenu from './FicheActionMenu';
+
+type FicheActionDescriptionProps = {
+  isReadonly: boolean;
+  fiche: FicheAction;
+  className?: string;
+  updateFiche: (fiche: FicheAction) => void;
+};
+
+const FicheActionDescription = ({
+  isReadonly,
+  fiche,
+  className,
+  updateFiche,
+}: FicheActionDescriptionProps) => {
+  const {
+    thematiques,
+    sous_thematiques: sousThematiques,
+    description,
+    ressources,
+  } = fiche;
+
+  return (
+    <div
+      className={classNames(
+        'bg-primary-7 rounded-lg py-10 px-7 flex flex-col gap-7',
+        className
+      )}
+    >
+      <div className="flex justify-between items-start">
+        {/* Liste des thématiques et sous-thématiques sous forme de badges */}
+        {(thematiques?.length || sousThematiques?.length) && (
+          <div className="flex flex-wrap gap-4">
+            {thematiques?.map(thematique => (
+              <Badge
+                key={thematique.id}
+                title={thematique.nom}
+                uppercase={false}
+                state="info"
+              />
+            ))}
+            {sousThematiques?.map(ssThematique => (
+              <Badge
+                key={ssThematique.id}
+                title={ssThematique.sous_thematique}
+                uppercase={false}
+                state="info"
+              />
+            ))}
+          </div>
+        )}
+
+        {/* Boutons d'action sur la fiche */}
+        <FicheActionMenu
+          isReadonly={isReadonly}
+          fiche={fiche}
+          updateFiche={updateFiche}
+          className="ml-auto"
+        />
+      </div>
+
+      {/* Description de l'action */}
+      <div>
+        <h6 className="text-lg leading-6 text-grey-1 mb-2">
+          Description de l'action :
+        </h6>
+        <p className="text-base text-grey-1 mb-0">
+          {description || 'Non renseigné !'}
+        </p>
+      </div>
+
+      {/* Moyens humains et techniques */}
+      <div>
+        <h6 className="text-lg leading-6 text-grey-1 mb-2">
+          Moyens humains et techniques :
+        </h6>
+        <p className="text-base text-grey-1 mb-0">
+          {ressources || 'Non renseigné !'}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FicheActionDescription;
