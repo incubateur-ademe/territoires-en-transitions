755d57cd80007b169d078743966f707e9a800609
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-08 18:34:52 +0200

    Ajout d'un bloc acteurs à la FA avec modale d'édition

---
+++ app.territoiresentransitions.react/src/ui/dropdownLists/ServicesDropdown/useServicePiloteListe.ts
@@ -0,0 +1,18 @@
+import {useQuery} from 'react-query';
+
+import {supabaseClient} from 'core-logic/api/supabase';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {TFicheActionServicePiloteRow} from 'types/alias';
+
+export const useServiceListe = () => {
+  const collectivite_id = useCollectiviteId()!;
+
+  return useQuery(['services_pilotes', collectivite_id], async () => {
+    const {data} = await supabaseClient
+      .from('service_tag')
+      .select()
+      .eq('collectivite_id', collectivite_id)
+      .order('nom');
+    return data as unknown as TFicheActionServicePiloteRow[];
+  });
+};
