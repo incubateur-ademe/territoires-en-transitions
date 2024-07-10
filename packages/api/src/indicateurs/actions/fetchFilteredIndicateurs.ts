81ce54ebcca53d8a90bdb63a6a4df6939ad75d85
Marc Rutkowski <marc@attractive-media.fr>
2024-07-10 15:30:52 +0200

    Change les données retournées par la fonction de filtrage (+ prettier)

--- packages/api/src/indicateurs/actions/fetchFilteredIndicateurs.ts
+++ packages/api/src/indicateurs/actions/fetchFilteredIndicateurs.ts
@@ -1,19 +1,18 @@
 import {Enums, DBClient} from '../../typeUtils';
 import {unaccent} from '../../utils/unaccent';
-import {IndicateurListItem} from "../domain";
-import {selectGroupements} from "../../collectivites/shared/actions/groupement.fetch";
+import {selectGroupements} from '../../collectivites/shared/actions/groupement.fetch';
 import {FetchOptions} from '../domain/fetch_options.schema';
-import {Groupement} from "../../collectivites/shared/domain/groupement.schema";
+import {Groupement} from '../../collectivites/shared/domain/groupement.schema';
 
 // Sous-ensemble d'indicateurs
 export type Subset = 'cae' | 'eci' | 'crte' | 'perso' | 'cles' | 'selection';
 
-const subsetPassage= new Map<Subset, string>([
+const subsetPassage = new Map<Subset, string>([
   ['cae', 'cae'],
   ['eci', 'eci'],
   ['crte', 'crte'],
   ['cles', 'clef'],
-  ['selection','prioritaire']
+  ['selection', 'prioritaire'],
 ]);
 
 // Options de filtrage
@@ -38,22 +37,25 @@ export type Filters = {
 
 const filtresOptions: {[key in keyof Filters]?: string} = {
   thematique_ids: 'indicateur_thematique!inner(thematique_id)',
-  action_id : 'indicateur_action!inner(action_id)',
-  plan_ids: 'fiche_action_indicateur!inner(fiche_id, fiche_action!inner(fiche_action_axe!inner(axe!inner())))',
+  action_id: 'indicateur_action!inner(action_id)',
+  plan_ids:
+    'fiche_action_indicateur!inner(fiche_id, fiche_action!inner(fiche_action_axe!inner(axe!inner())))',
   pilote_user_ids: 'indicateur_pilote!inner()',
   pilote_tag_ids: 'indicateur_pilote!inner()',
   service_ids: 'indicateur_service_tag!inner(service_tag_id)',
-  rempli : 'indicateur_valeur()',
-  confidentiel : 'indicateur_collectivite(commentaire, confidentiel, collectivite_id)',
-  fiches_non_classees: 'fiche_action_indicateur!inner(fiche_id, fiche_action!inner(fiche_action_axe!inner(axe!inner())))'
+  rempli: 'indicateur_valeur()',
+  confidentiel:
+    'indicateur_collectivite(commentaire, confidentiel, collectivite_id)',
+  fiches_non_classees:
+    'fiche_action_indicateur!inner(fiche_id, fiche_action!inner(fiche_action_axe!inner(axe!inner())))',
 };
 
 export async function fetchFilteredIndicateurs(
-    dbClient : DBClient,
-    collectiviteId : number,
-    subset: Subset | null,
-    filters : Filters
-) :Promise<{data:IndicateurListItem[], status:any}> {
+  dbClient: DBClient,
+  collectiviteId: number,
+  subset: Subset | null,
+  filters: Filters
+) {
   const parts = new Set<string>();
   const isPerso = subset === 'perso';
   const groupements: Groupement[] = await selectGroupements(dbClient);
@@ -62,49 +64,57 @@ export async function fetchFilteredIndicateurs(
   let key: keyof Filters;
   for (key in filters) {
     if (
-        (filters[key] as Array<string | number>)?.length ||
-        (filters[key] !== undefined && !Array.isArray(filters[key]))
+      (filters[key] as Array<string | number>)?.length ||
+      (filters[key] !== undefined && !Array.isArray(filters[key]))
     ) {
       const part = filtresOptions[key];
       if (part) parts.add(part);
     }
   }
-  if(subset!==null && !isPerso) parts.add('indicateur_categorie_tag!inner(categorie_tag!inner(id,nom,collectivite_id,groupement_id))');
+  if (subset !== null && !isPerso)
+    parts.add(
+      'indicateur_categorie_tag!inner(categorie_tag!inner(id,nom,collectivite_id,groupement_id))'
+    );
   // construit la requête
   const query = dbClient
-      .from('indicateur_definition')
-      .select(
-          [
-            'id',
-            'identifiant_referentiel',
-            'titre',
-            'groupement_id',
-            'indicateur_parents(id)',
-            ...parts,
-          ].join(',')
-      );
+    .from('indicateur_definition')
+    .select(
+      [
+        'id',
+        'identifiant_referentiel',
+        'titre',
+        'groupement_id',
+        'indicateur_parents(id)',
+        ...parts,
+      ].join(',')
+    );
 
   // filtre par sous-ensemble voulu
   if (isPerso) {
     // On ne récupère que les indicateurs personnalisés
     query.eq('collectivite_id', collectiviteId);
   } else {
-    if(subset!== null ||filters.type?.length){
+    if (subset !== null || filters.type?.length) {
       // On ne récupère que les indicateurs prédéfinis
       query.is('collectivite_id', null);
       // S'il y a un tri sur les catégories prédéfinies,
       // on ne garde que les indicateurs ayant des catégories prédéfinies
       query.is('indicateur_categorie_tag.categorie_tag.collectivite_id', null);
-      if(subset !== null){
+      if (subset !== null) {
         if (filters.type?.length) {
           // S'il y a un tri sur le type et le programme
-          query.or(`nom.eq.${subsetPassage.get(subset)!}, nom.in.${filters.type}`,
-              {referencedTable : 'indicateur_categorie_tag.categorie_tag'});
-        }else {
+          query.or(
+            `nom.eq.${subsetPassage.get(subset)!}, nom.in.${filters.type}`,
+            {referencedTable: 'indicateur_categorie_tag.categorie_tag'}
+          );
+        } else {
           // S'il y a un tri que sur le programme
-          query.eq('indicateur_categorie_tag.categorie_tag.nom', subsetPassage.get(subset)!);
+          query.eq(
+            'indicateur_categorie_tag.categorie_tag.nom',
+            subsetPassage.get(subset)!
+          );
         }
-      }  else if (filters.type?.length) {
+      } else if (filters.type?.length) {
         // S'il y a un tri que sur le type
         query.in('indicateur_categorie_tag.categorie_tag.nom', filters.type);
       }
@@ -124,11 +134,11 @@ export async function fetchFilteredIndicateurs(
       }
     } else {
       const search = unaccent(text)
-          .split(' ')
-          .map(s => s.trim())
-          .filter(s => !!s)
-          .map(s => `"${s}":*`)
-          .join(' & ');
+        .split(' ')
+        .map(s => s.trim())
+        .filter(s => !!s)
+        .map(s => `"${s}":*`)
+        .join(' & ');
       // ou dans le nom ou la description
       query.or(`titre.fts.${search}, description.fts.${search}`);
     }
@@ -138,23 +148,23 @@ export async function fetchFilteredIndicateurs(
   // si une de ces conditions est vraie alors le filtre "parent uniquement" sera désactivé
   const filtrerPar = {
     participationAuScore:
-        subset === 'cae' && filters.participation_score !== undefined,
+      subset === 'cae' && filters.participation_score !== undefined,
     confidentiel: filters.confidentiel,
     planAction: !!filters.plan_ids?.length,
     service: !!filters.service_ids?.length,
     personne:
-        !!filters.pilote_user_ids?.length || !!filters.pilote_tag_ids?.length,
+      !!filters.pilote_user_ids?.length || !!filters.pilote_tag_ids?.length,
     fichesNonClassees: !!filters.fiches_non_classees,
   };
 
   // sélectionne uniquement les indicateurs parent (sauf pour CRTE et perso ou si on fait
   // une recherche par id ou si un des filtres complémentaires est actif)
   const filtrerParParent =
-      subset !== null &&
-      subset !== 'crte' &&
-      subset !== 'perso' &&
-      !searchById &&
-      !Object.values(filtrerPar).find(v => !!v);
+    subset !== null &&
+    subset !== 'crte' &&
+    subset !== 'perso' &&
+    !searchById &&
+    !Object.values(filtrerPar).find(v => !!v);
 
   if (filtrerParParent) {
     query.is('indicateur_parents', null);
@@ -172,27 +182,30 @@ export async function fetchFilteredIndicateurs(
 
   // par plan
   if (filtrerPar.planAction) {
-    query.in('fiche_action_indicateur.fiche_action.fiche_action_axe.axe.plan', filters.plan_ids!);
+    query.in(
+      'fiche_action_indicateur.fiche_action.fiche_action_axe.axe.plan',
+      filters.plan_ids!
+    );
   }
 
   // filtre les indicateurs confidentiels
   if (filters.confidentiel !== undefined) {
-    if(filters.confidentiel){
+    if (filters.confidentiel) {
       query.not('indicateur_collectivite', 'is', null);
       query.eq('indicateur_collectivite.collectivite_id', collectiviteId);
       query.is('indicateur_collectivite.confidentiel', true);
-    }else{
-    query.or(`indicateur_id.is.null, collectivite_id.eq.${collectiviteId})`,
-        { referencedTable: 'indicateur_collectivite' });
+    } else {
+      query.or(`indicateur_id.is.null, collectivite_id.eq.${collectiviteId})`, {
+        referencedTable: 'indicateur_collectivite',
+      });
     }
   }
 
   // par service pilote
   if (filtrerPar.service) {
-    query.eq('indicateur_service_tag.collectivite_id', collectiviteId)
+    query.eq('indicateur_service_tag.collectivite_id', collectiviteId);
     query.in('indicateur_service_tag.service_tag_id', filters.service_ids!);
   }
-
 
   // par personne pilote
   if (filtrerPar.personne) {
@@ -218,9 +231,9 @@ export async function fetchFilteredIndicateurs(
   // filtre les indicateurs complétés / à compléter
   if (filters.rempli !== undefined) {
     query.eq('indicateur_valeur.collectivite_id', collectiviteId);
-    if(filters.rempli){
+    if (filters.rempli) {
       query.not('indicateur_valeur', 'is', null);
-    }else{
+    } else {
       query.is('indicateur_valeur', null);
     }
   }
@@ -242,19 +255,32 @@ export async function fetchFilteredIndicateurs(
   });
   const {data, ...remaining} = await query;
   let rows = data || [];
-  if(filters.confidentiel===false){
+  if (filters.confidentiel === false) {
     // Filtre supplémentaire sur confidentiel car pas trouvé comment faire ma condition avec postgrest
-      rows = rows.filter((r: any)=>
-          r.indicateur_collectivite.length === 0 || r.indicateur_collectivite[0].confidentiel === false);
+    rows = rows.filter(
+      (r: any) =>
+        r.indicateur_collectivite.length === 0 ||
+        r.indicateur_collectivite[0].confidentiel === false
+    );
   }
   // Filtre sur les indicateurs d'un groupement
-  rows = rows.filter((r : any)=> r.groupement_id === null ||
-  groupements.filter((g : Groupement) => g.id === r.groupement_id)[0].collectivites?.includes(collectiviteId));
+  rows = rows.filter(
+    (r: any) =>
+      r.groupement_id === null ||
+      groupements
+        .filter((g: Groupement) => g.id === r.groupement_id)[0]
+        .collectivites?.includes(collectiviteId)
+  );
   return {
     ...remaining,
     data: rows
-        // et conserve un id unique
-        .map((d : any) => ({id : d.id as number, titre : d.titre as string}))
+      // et conserve un id unique
+      .map((d: any) => ({
+        id: d.id as number,
+        titre: d.titre as string,
+        estPerso: d.estPerso as boolean,
+        identifiant: d.identifiant_referentiel as string,
+      })),
   };
 }
 
