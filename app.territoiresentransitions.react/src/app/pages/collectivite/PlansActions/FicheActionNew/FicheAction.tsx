5f60e54cfe9cba27c3d6677bd5e420bd2f82816e
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-10 17:34:15 +0200

    Révision du layout de la fiche action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FicheAction.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FicheAction.tsx
@@ -28,7 +28,6 @@ const FicheAction = ({isReadonly}: FicheActionProps) => {
   if (!data || !data.fiche) return null;
 
   const {fiche} = data;
-  const isTwoColumns = !!fiche.calendrier;
 
   return (
     <div
@@ -36,42 +35,43 @@ const FicheAction = ({isReadonly}: FicheActionProps) => {
       className="w-full px-2 md:px-4 lg:px-6 py-12 bg-grey-2 -mb-8"
     >
       <div className="flex flex-col w-full px-2 mx-auto xl:max-w-7xl 2xl:max-w-8xl">
+        {/* Header de la fiche action (titre, fil d'ariane) */}
         <FicheActionHeader
           titre={fiche.titre}
           isReadonly={isReadonly}
           updateTitle={titre => updateFiche({...fiche, titre})}
         />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-9 xl:gap-11">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-10 gap-5 lg:gap-9 xl:gap-11">
+          {/* Description, moyens humains et techniques, et thématiques */}
           <FicheActionDescription
             isReadonly={isReadonly}
             fiche={fiche}
-            className="col-span-full lg:col-span-2 xl:col-span-3"
+            className="col-span-full lg:col-span-2 xl:col-span-7"
             updateFiche={updateFiche}
           />
-          <div
-            className={classNames(
-              'max-lg:col-span-full lg:row-span-3 max-lg:grid max-lg:grid-cols-1 lg:flex lg:flex-col gap-5',
-              {
-                'md:max-lg:grid-cols-3': !isTwoColumns,
-                'md:max-lg:grid-cols-2': isTwoColumns,
-              }
-            )}
-          >
+
+          {/* Colonne de droite */}
+          <div className="max-lg:col-span-full xl:col-span-3 lg:row-span-3 max-lg:grid max-md:grid-cols-1 md:max-lg:grid-cols-2 lg:flex lg:flex-col gap-5">
             <div className="flex flex-col gap-5">
+              {/* Information sur le mode public / privé */}
               <FicheActionRestreint
                 isRestreint={fiche.restreint ?? false}
                 updateRestreint={restreint =>
                   updateFiche({...fiche, restreint})
                 }
               />
+
+              {/* Date de dernière modification */}
               {fiche.modified_at && (
-                <div className="bg-white border border-grey-3 rounded-lg py-2 px-3.5 h-14 text-sm text-primary-10 font-medium italic flex items-center max-lg:justify-center">
+                <div className="bg-white border border-grey-3 rounded-lg py-2 px-3.5 h-14 text-sm text-primary-10 font-medium italic flex items-center max-md:justify-center">
                   <span>
                     Dernière modification le{' '}
                     {getFormattedDate(fiche.modified_at)}
                   </span>
                 </div>
               )}
+
+              {/* Planning prévisionnel */}
               <FicheActionPlanning
                 isReadonly={isReadonly}
                 fiche={fiche}
@@ -80,14 +80,16 @@ const FicheAction = ({isReadonly}: FicheActionProps) => {
               />
             </div>
 
+            {/* Acteurs du projet */}
             <FicheActionActeurs
               isReadonly={isReadonly}
               fiche={fiche}
               updateFiche={updateFiche}
-              className={classNames({'md:max-lg:col-span-2': !isTwoColumns})}
             />
           </div>
-          <div className="col-span-full lg:col-span-2 xl:col-span-3 bg-white border border-grey-3 rounded-lg py-10 px-8">
+
+          {/* Contenu de la fiche action */}
+          <div className="col-span-full lg:col-span-2 xl:col-span-7 bg-white border border-grey-3 rounded-lg py-10 px-8">
             Onglets
           </div>
         </div>
