8deaeaf5736120f6a5196e140dd68c91f7d477ff
Marc Rutkowski <marc@attractive-media.fr>
2024-07-10 16:21:44 +0200

    Ajoute des scénarios de tests e2e de la fonction "appliquer à mes objectifs/résultats"

--- e2e/cypress/plugins/supabase.js
+++ e2e/cypress/plugins/supabase.js
@@ -15,6 +15,16 @@ module.exports = (on, config) => {
       const res = await supabase.rpc(name, params);
       return res || null;
     },
+    supabase_select: async ({table, columns, match}) => {
+      const res = await supabase.from(table).select(columns).match(match);
+      return res || null;
+    },
+    supabase_insert: async ({table, values, columns}) => {
+      const query = supabase.from(table).insert(values);
+      if (typeof columns === 'string') query.select(columns);
+      const res = await query;
+      return res || null;
+    },
     supabase_generateLink: async ({type, email}) => {
       const res = await supabase.auth.api.generateLink(type, email);
       return res || null;
