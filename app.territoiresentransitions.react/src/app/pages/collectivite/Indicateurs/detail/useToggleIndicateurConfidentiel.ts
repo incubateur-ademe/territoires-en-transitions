dfc4eb7324f4703bf2ac725803ac154f16c717e9
Marc Rutkowski <marc@attractive-media.fr>
2024-07-10 16:06:28 +0200

    Utilise les nouvelles fonctions et typage dans l'édition des infos liées à un indicateur (pilotes, services, thématiques, confidentiel)

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/useToggleIndicateurConfidentiel.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/useToggleIndicateurConfidentiel.ts
@@ -1,7 +1,8 @@
 import {useMutation, useQueryClient} from 'react-query';
+import {Indicateurs} from '@tet/api';
 import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
-import {TIndicateurDefinition, TIndicateurPredefini} from '../types';
+import {TIndicateurDefinition} from '../types';
 
 /** Met à jour l'état "confidentiel" d'un indicateur */
 export const useToggleIndicateurConfidentiel = (
@@ -10,37 +11,19 @@ export const useToggleIndicateurConfidentiel = (
   const queryClient = useQueryClient();
   const collectivite_id = useCollectiviteId();
 
-  const {id, isPerso} = definition;
-  const indicateur_perso_id =
-    isPerso && typeof id === 'number' ? (id as number) : null;
-  const indicateur_id =
-    !isPerso && typeof id === 'string' && collectivite_id
-      ? // pour les indicateurs prédéfinis utilise l'id de l'indicateur lié si nécessaire
-        // car le trigger ne fonctionne pas pour le delete
-        (definition as TIndicateurPredefini).valeur_indicateur || (id as string)
-      : null;
-
   return useMutation({
     mutationKey: 'toggle_indicateur_confidentiel',
     mutationFn: async (confidentiel: boolean) => {
-      if (indicateur_perso_id !== null || indicateur_id !== null) {
-        // supprime la ligne
-        if (confidentiel) {
-          return supabaseClient
-            .from('indicateur_confidentiel')
-            .delete()
-            .match(isPerso ? {indicateur_perso_id} : {indicateur_id});
-        }
-
-        // ou ajoute la ligne
-        return supabaseClient
-          .from('indicateur_confidentiel')
-          .upsert(
-            isPerso ? {indicateur_perso_id} : {indicateur_id, collectivite_id},
-            {
-              onConflict: 'indicateur_id,indicateur_perso_id,collectivite_id',
-            }
-          );
+      if (collectivite_id) {
+        return Indicateurs.save.updateIndicateurDefinition(
+          supabaseClient,
+          {
+            ...definition,
+            collectiviteId: collectivite_id,
+            confidentiel: !confidentiel,
+          },
+          collectivite_id
+        );
       }
 
       return false;
@@ -48,9 +31,9 @@ export const useToggleIndicateurConfidentiel = (
     onSuccess: (data, variables) => {
       // recharge les infos complémentaires associées à l'indicateur
       queryClient.invalidateQueries([
-        'indicateur_info_liees',
+        'indicateur_definition',
         collectivite_id,
-        id,
+        definition.id,
       ]);
     },
   });
