5256f588acef6384af23d954055142a74dfa1d0a
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-10 16:26:26 +0200

    Ajout du mode privé sur la fiche action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FicheActionRestreint/RestreintModal.tsx
@@ -0,0 +1,64 @@
+import {Button, Checkbox, FormSectionGrid, Modal} from '@tet/ui';
+import {useState} from 'react';
+
+type RestreintModalProps = {
+  isOpen: boolean;
+  setIsOpen: (opened: boolean) => void;
+  isRestreint: boolean;
+  updateRestreint: (isRestreint: boolean) => void;
+};
+
+const RestreintModal = ({
+  isOpen,
+  setIsOpen,
+  isRestreint,
+  updateRestreint,
+}: RestreintModalProps) => {
+  const [editedRestreint, setEditedFiche] = useState(isRestreint);
+
+  const handleSave = () => {
+    updateRestreint(editedRestreint);
+  };
+
+  return (
+    <Modal
+      openState={{isOpen, setIsOpen}}
+      title="Restriction des accès à la fiche"
+      size="md"
+      render={({descriptionId, close}) => (
+        <div>
+          <div id={descriptionId} className="flex flex-col gap-8">
+            <FormSectionGrid>
+              <Checkbox
+                variant="switch"
+                label="Fiche action en mode privé"
+                message="Seulement les membres de la collectivité peuvent voir la fiche"
+                containerClassname="col-span-2"
+                checked={editedRestreint}
+                onChange={() => setEditedFiche(prevState => !prevState)}
+              />
+            </FormSectionGrid>
+          </div>
+
+          {/* Boutons pour valider / annuler les modifications */}
+          <div className="flex justify-end gap-4 mt-12">
+            <Button onClick={close} aria-label="Annuler" variant="outlined">
+              Annuler
+            </Button>
+            <Button
+              onClick={() => {
+                handleSave();
+                close();
+              }}
+              aria-label="Valider"
+            >
+              Valider
+            </Button>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default RestreintModal;
