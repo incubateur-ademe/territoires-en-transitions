eb409d77dfee6a67f5fd843f251979d331aca6f4
Marc Rutkowski <marc@attractive-media.fr>
2024-07-10 15:32:57 +0200

    Ajoute des exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,6 +6,9 @@ export * from './utils/pathUtils';
 export * from './utils/makeSearchString';
 export * from './utils/isAllowedOrigin';
 export * as Indicateurs from './indicateurs';
+export * as FicheAction from './fiche_actions';
 export * as CollectiviteEngagee from './collectiviteEngagees';
 export * as FicheActions from './fiche_actions';
 export * from './panier_action_impact';
+
+export * as SharedDomain from './shared/domain';
