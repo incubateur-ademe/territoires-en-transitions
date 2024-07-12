10494f9e9c3abdd484b0b6c8815b92ae0c06c25b
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-12 18:26:00 +0200

    Mise à jour des dropdowns

--- app.territoiresentransitions.react/src/ui/dropdownLists/ServicesDropdown/useServicesListe.ts
+++ app.territoiresentransitions.react/src/ui/dropdownLists/ServicesDropdown/useServicesListe.ts
@@ -1,18 +1,20 @@
 import {useQuery} from 'react-query';
-
 import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {TFicheActionServicePiloteRow} from 'types/alias';
 
-export const useServiceListe = () => {
+export const useServicesListe = () => {
   const collectivite_id = useCollectiviteId()!;
 
   return useQuery(['services_pilotes', collectivite_id], async () => {
-    const {data} = await supabaseClient
+    const {error, data} = await supabaseClient
       .from('service_tag')
       .select()
       .eq('collectivite_id', collectivite_id)
       .order('nom');
-    return data as unknown as TFicheActionServicePiloteRow[];
+
+    if (error) throw new Error(error.message);
+
+    return data as TFicheActionServicePiloteRow[];
   });
 };
