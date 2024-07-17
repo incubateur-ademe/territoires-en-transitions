b0e5f9c01a562c1ae5bec6186a70c6e74d39b729
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-17 17:08:21 +0200

    Ajout d'une carte vide générique

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FichesLiees/FichesLieesTab.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FichesLiees/FichesLieesTab.tsx
@@ -1,4 +1,6 @@
 import {FicheResume} from '../../FicheAction/data/types';
+import EmptyCard from '../EmptyCard';
+import FichePicto from './FichePicto';
 
 type FichesLieesTabProps = {
   fiches: FicheResume[] | null;
@@ -8,7 +10,15 @@ const FichesLieesTab = ({fiches}: FichesLieesTabProps) => {
   const isEmpty = !fiches || fiches.length === 0;
 
   return isEmpty ? (
-    <div>Aucune fiche action de vos plans d'actions n'est liée !</div>
+    <EmptyCard
+      picto={className => <FichePicto className={className} />}
+      title="Aucune fiche action de vos plans d'actions n'est liée !"
+      action={{
+        label: 'Lier une fiche action',
+        icon: 'link',
+        onClick: () => {},
+      }}
+    />
   ) : (
     <div>Fiches des plans liées</div>
   );
