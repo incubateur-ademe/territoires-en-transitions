715b303a9f22541e3270d9d3b388adcc5eaade70
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-17 17:08:21 +0200

    Ajout d'une carte vide générique

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/ActionsLiees/ActionsLieesTab.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/ActionsLiees/ActionsLieesTab.tsx
@@ -1,4 +1,6 @@
 import {TActionInsert} from 'types/alias';
+import EmptyCard from '../EmptyCard';
+import ActionPicto from './ActionPicto';
 
 type ActionsLieesTabProps = {
   actions: TActionInsert[] | null;
@@ -8,7 +10,15 @@ const ActionsLieesTab = ({actions}: ActionsLieesTabProps) => {
   const isEmpty = !actions || actions.length === 0;
 
   return isEmpty ? (
-    <div>Aucune action des référentiels n'est liée !</div>
+    <EmptyCard
+      picto={className => <ActionPicto className={className} />}
+      title="Aucune action des référentiels n'est liée !"
+      action={{
+        label: 'Lier une action des référentiels',
+        icon: 'link',
+        onClick: () => {},
+      }}
+    />
   ) : (
     <div>Actions des référentiels liées</div>
   );
