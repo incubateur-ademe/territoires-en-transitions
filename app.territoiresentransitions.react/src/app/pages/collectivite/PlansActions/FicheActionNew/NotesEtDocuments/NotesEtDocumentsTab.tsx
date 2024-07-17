5d8b9a8d7002ab197a9cf6d579888433e313b288
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-17 17:08:21 +0200

    Ajout d'une carte vide générique

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/NotesEtDocuments/NotesEtDocumentsTab.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/NotesEtDocuments/NotesEtDocumentsTab.tsx
@@ -1,3 +1,7 @@
+import EmptyCard from '../EmptyCard';
+import DocumentPicto from './DocumentPicto';
+import NotesPicto from './NotesPicto';
+
 type NotesEtDocumentsTabProps = {
   notes: string | null;
 };
@@ -6,7 +10,24 @@ const NotesEtDocumentsTab = ({notes}: NotesEtDocumentsTabProps) => {
   const isEmpty = !notes;
 
   return isEmpty ? (
-    <div>Aucune note complémentaire ajoutée</div>
+    <div className="flex flex-col gap-6">
+      <EmptyCard
+        picto={className => <NotesPicto className={className} />}
+        title="Aucune note complémentaire ajoutée"
+        action={{
+          label: 'Ajouter une note',
+          onClick: () => {},
+        }}
+      />
+      <EmptyCard
+        picto={className => <DocumentPicto className={className} />}
+        title="Aucun document ajouté"
+        action={{
+          label: 'Ajouter un document',
+          onClick: () => {},
+        }}
+      />
+    </div>
   ) : (
     <div>Notes et documents</div>
   );
