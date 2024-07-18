feacc4f3d92f195fbf944b313c309129b2cd0eee
Arthur Molinos <arthur.molinos@gmail.com>
2024-07-18 10:32:00 +0200

    Ajoute la sauvegarde des filtres dans l'URL

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ToutesLesFichesAction/ToutesLesFichesAction.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ToutesLesFichesAction/ToutesLesFichesAction.tsx
@@ -1,10 +1,40 @@
 import {Filtre} from '@tet/api/dist/src/fiche_actions/fiche_resumes.list/domain/fetch_options.schema';
 import ModalFiltresToutesLesFichesAction from 'app/pages/collectivite/PlansActions/ToutesLesFichesAction/ModalFiltresToutesLesFichesAction';
 import FichesActionListe from 'app/pages/collectivite/TableauDeBord/Module/ModuleFichesActions/FichesActionListe';
-import {useState} from 'react';
+import {makeCollectiviteToutesLesFichesUrl} from 'app/paths';
+import {useCollectiviteId} from 'core-logic/hooks/params';
+import {useSearchParams} from 'core-logic/hooks/query';
+
+// TODO: implémenter les filtres "sans" (ex. "sans_pilote")
+const nameToparams: Record<keyof Filtre | 'sort' | 'page', string> = {
+  statuts: 's',
+  priorites: 'prio',
+  modifiedSince: 'ms',
+  texteNomOuDescription: 'text',
+  budgetPrevisionnel: 'bp',
+  restreint: 'r',
+  hasIndicateurLies: 'il',
+  planActionIds: 'pa',
+  utilisateurPiloteIds: 'up',
+  personnePiloteIds: 'pp',
+  utilisateurReferentIds: 'ur',
+  personneReferenteIds: 'pr',
+  structurePiloteIds: 'sp',
+  servicePiloteIds: 'sv',
+  thematiqueIds: 't',
+  financeurIds: 'f',
+  page: 'p',
+  sort: 'sort',
+};
 
 const ToutesLesFichesAction = () => {
-  const [filters, setFilters] = useState<Filtre>({});
+  const collectiviteId = useCollectiviteId();
+
+  const [filters, setFilters] = useSearchParams<Filtre>(
+    makeCollectiviteToutesLesFichesUrl({collectiviteId: collectiviteId!}),
+    {},
+    nameToparams
+  );
 
   return (
     <div className="min-h-[44rem] flex flex-col gap-8">
