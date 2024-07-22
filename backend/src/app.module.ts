6cf672af3ddcead01b14a1d152fe1f7e8d9dc61d
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-22 16:40:58 +0200

    feat(common): ajout du database service, ajout d'un endpoint de version

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SheetModule } from './spreadsheets/SheetModule';
-
+import { SheetModule } from './spreadsheets/sheet.module';
+import { CommonModule } from './common/common.module';
 @Module({
-  imports: [SheetModule],
+  imports: [CommonModule, SheetModule],
   controllers: [AppController],
   providers: [AppService],
 })
