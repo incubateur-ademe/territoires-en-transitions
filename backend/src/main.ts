df2a4e264846d4815f1f0c2b2f3ab373ad326be1
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-22 14:04:30 +0200

    feat(spreadsheets): ajout de la capacité à lire/écrire dans un spreadsheet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,11 +8,25 @@ import {
 } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SENTRY_DSN } from './common/configs/SentryInit';
+import * as fs from 'fs';
 
 const port = process.env.PORT || 8080;
 console.log(`Launching NestJS app on port ${port}`);
 
 async function bootstrap() {
+  if (process.env.GCLOUD_SERVICE_ACCOUNT_KEY) {
+    const serviceAccountFile = `${__dirname}/keyfile.json`;
+    console.log(
+      'Writing Google Cloud credentials to file:',
+      serviceAccountFile,
+    );
+    fs.writeFileSync(
+      serviceAccountFile,
+      process.env.GCLOUD_SERVICE_ACCOUNT_KEY,
+    );
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = serviceAccountFile;
+  }
+
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
 
