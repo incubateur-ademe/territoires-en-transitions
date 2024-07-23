27496baf9ca5d75b0fbe76f4137d342583920279
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-23 10:52:27 +0200

    fix(backend): configure backend tsconfig to be able to use typing defined in api packages

--- backend/src/common/services/database.service.ts
+++ backend/src/common/services/database.service.ts
@@ -1,23 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import { createClient } from '@supabase/supabase-js';
 import * as postgres from 'postgres';
 
 @Injectable()
 export default class DatabaseService {
   private readonly logger = new Logger(DatabaseService.name);
   public readonly db: PostgresJsDatabase;
-
-  // TODO: Type it
-  public readonly supabaseDb = createClient(
-    process.env.SUPABASE_URL! || 'http://127.0.0.1:54321',
-    process.env.SUPABASE_SERVICE_ROLE_KEY! || 'pwd', // TODO: Use env var
-    {
-      db: {
-        schema: 'public',
-      },
-    },
-  );
 
   constructor() {
     if (!process.env.DATABASE_URL) {
@@ -26,7 +14,9 @@ export default class DatabaseService {
         'postgresql://postgres:postgres@localhost:54322/postgres';
     }
     this.logger.log(`Initializing database service`);
-    const client = postgres(process.env.DATABASE_URL!);
+    const client = postgres(process.env.DATABASE_URL!, {
+      prepare: false,
+    });
     this.db = drizzle(client);
   }
 }
