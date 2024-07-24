16769b0a8bf6795e36d7d075abb6faad5565112a
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-24 12:49:35 +0200

    feat(backend): ajout de tRPC

--- backend/src/indicateurs/indicateurs.module.ts
+++ backend/src/indicateurs/indicateurs.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { CollectivitesModule } from '../collectivites/collectivites.module';
 import { CommonModule } from '../common/common.module';
 import { SheetModule } from '../spreadsheets/sheet.module';
+import { TrpcModule } from '../trpc/trpc.module';
 import { TrajectoiresController } from './controllers/trajectoires.controller';
+import { TrajectoiresRouter } from './routers/trajectoires.router';
 import IndicateursService from './service/indicateurs.service';
 import TrajectoiresService from './service/trajectoires.service';
 
 @Module({
-  imports: [CommonModule, CollectivitesModule, SheetModule],
-  providers: [IndicateursService, TrajectoiresService],
-  exports: [IndicateursService, TrajectoiresService],
+  imports: [TrpcModule, CommonModule, CollectivitesModule, SheetModule],
+  providers: [IndicateursService, TrajectoiresService, TrajectoiresRouter],
+  exports: [IndicateursService, TrajectoiresService, TrajectoiresRouter],
   controllers: [TrajectoiresController],
 })
 export class IndicateursModule {}
