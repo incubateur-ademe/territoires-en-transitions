6bd45e382f32e90bac0295bd5e57825600c93c58
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-24 22:51:33 +0200

    feat: ajout d'une test app en next utilisant le tRPC

---
+++ web/app/trpc.ts
@@ -0,0 +1,9 @@
+import { AppRouter } from "@backend/trpc.router";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:8080/trpc", // TODO: you should update this to use env variables
+    }),
+  ],
+});
