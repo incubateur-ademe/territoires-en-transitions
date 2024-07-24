d342b6185f330112a03af4b23f25e4b721b208cc
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-07-24 10:34:17 +0200

    feat(backend): ajout de la documentation swagger et d'informations supplémentaires de version

--- backend/src/indicateurs/models/calcultrajectoire.request.ts
+++ backend/src/indicateurs/models/calcultrajectoire.request.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsOptional } from 'class-validator';
 import optionalBooleanMapper from '../../common/services/optionalBooleanMapper';
 
 export default class CalculTrajectoireRequest {
+  @ApiProperty({ description: 'Identifiant de la collectivité' })
   @IsInt()
   @Type(() => Number)
   collectivite_id: number;
 
+  @ApiProperty({
+    required: false,
+    description: 'Ne supprime pas le spreadsheet temporaire de calcul',
+  })
   @IsBoolean()
   @Transform(({ value }) => optionalBooleanMapper.get(value)) // Useful for query param
   @IsOptional()
