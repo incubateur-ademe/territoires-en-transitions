08e2ef2769cca6f37ea2df96d3b4bf326a678ca8
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-30 15:47:26 +0200

    Ajout du menu de rangement de la fiche action

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/FicheActionDescription/EmplacementFiche/NouvelEmplacement/ColonneTableauEmplacement.tsx
@@ -0,0 +1,41 @@
+import {generateTitle} from '../../../../FicheAction/data/utils';
+import {TProfondeurAxe} from '../../../../PlanAction/data/types';
+import BoutonTableauEmplacement from './BoutonTableauEmplacement';
+
+type ColonneTableauEmplacementProps = {
+  axesList: TProfondeurAxe[];
+  selectedAxesIds: number[];
+  maxSelectedDepth: number;
+  onSelectAxe: (axe: TProfondeurAxe) => void;
+};
+
+const ColonneTableauEmplacement = ({
+  axesList,
+  selectedAxesIds,
+  maxSelectedDepth = 0,
+  onSelectAxe,
+}: ColonneTableauEmplacementProps) => {
+  return (
+    <div className="flex flex-col gap-4 px-3">
+      {axesList.map(axe => (
+        <BoutonTableauEmplacement
+          key={axe.axe.id}
+          id={axe.axe.id}
+          label={generateTitle(axe.axe.nom)}
+          hasChildren={!!axe.enfants}
+          isSelected={
+            selectedAxesIds.includes(axe.axe.id) &&
+            axe.profondeur === maxSelectedDepth
+          }
+          containsSelectedAxe={
+            selectedAxesIds.includes(axe.axe.id) &&
+            axe.profondeur !== maxSelectedDepth
+          }
+          onSelect={() => onSelectAxe(axe)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ColonneTableauEmplacement;
