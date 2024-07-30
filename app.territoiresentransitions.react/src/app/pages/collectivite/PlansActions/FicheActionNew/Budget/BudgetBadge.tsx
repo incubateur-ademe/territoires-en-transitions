3f6cd0cd45be375426cc8042e7a89e4856aeadf6
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-07-30 17:42:08 +0200

    Bloc d'en-tête de l'onglet indicateurs

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/Budget/BudgetBadge.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheActionNew/Budget/BudgetBadge.tsx
@@ -2,21 +2,17 @@ import {Badge} from '@tet/ui';
 import {getFormattedNumber} from './BudgetTab';
 
 type BudgetBadgeProps = {
-  budgetPrevisionnel: number | null;
+  budgetPrevisionnel: number;
 };
 
 const BudgetBadge = ({budgetPrevisionnel}: BudgetBadgeProps) => {
   return (
     <Badge
       title={
-        budgetPrevisionnel !== null ? (
-          <div className="flex items-start gap-1">
-            <div>{getFormattedNumber(budgetPrevisionnel)} € </div>
-            <div className="text-[0.5rem] leading-[0.6rem]">TTC</div>
-          </div>
-        ) : (
-          'Non renseigné'
-        )
+        <div className="flex items-start gap-1">
+          <div>{getFormattedNumber(budgetPrevisionnel)} € </div>
+          <div className="text-[0.5rem] leading-[0.6rem]">TTC</div>
+        </div>
       }
       state="standard"
       uppercase={false}
