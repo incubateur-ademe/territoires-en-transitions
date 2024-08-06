df6731113441af58ba1e07b1cf51bcfbcf737d7e
Amandine Jacquelin <contact@jacquelinamandine.fr>
2024-08-06 19:01:25 +0200

    Ajoute les droits au service role pour la fonction personnes_collectivite

--- data_layer/sqitch/deploy/taxonomie/personne.sql
+++ data_layer/sqitch/deploy/taxonomie/personne.sql
@@ -2,25 +2,6 @@
 
 BEGIN;
 
-create view private.personnes_collectivite
-as
-select pt.nom,
-       pt.collectivite_id,
-       pt.id      as tag_id,
-       null::uuid as user_id
-from personne_tag pt
-union
-select u.prenom || ' ' || u.nom,
-       d.collectivite_id,
-       null,
-       d.user_id
-from private_utilisateur_droit d
-         join utilisateur.dcp_display u on u.user_id = d.user_id
-where d.active;
-comment on view private.personnes_collectivite
-    is 'Les personnes par collectivité, utilisées pour tagger des fiches ou des indicateurs';
-
--- todo exposer une vue et supprimer la RPC.
 create or replace function personnes_collectivite(
     collectivite_id integer
 )
@@ -31,7 +12,8 @@ begin
     atomic
     select p.nom, p.collectivite_id, p.tag_id, p.user_id
     from private.personnes_collectivite p
-    where can_read_acces_restreint(personnes_collectivite.collectivite_id)
+    where (can_read_acces_restreint(personnes_collectivite.collectivite_id)
+               or is_service_role())
       and p.collectivite_id = personnes_collectivite.collectivite_id;
 end;
 
--- data_layer/sqitch/verify/taxonomie/personne.sql
+++ data_layer/sqitch/verify/taxonomie/personne.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+select has_function_privilege('personnes_collectivite(integer)', 'execute');
 
 ROLLBACK;
