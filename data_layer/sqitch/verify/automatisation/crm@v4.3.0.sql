fcb5f7a78ba700c3a2870c0541343fe8e2807ca8
Fred <98240+farnoux@users.noreply.github.com>
2024-08-08 10:34:57 +0200

    🗃️[crm] met à jour les requêtes de stats d'usage pour le CRM

---
+++ data_layer/sqitch/verify/automatisation/crm@v4.3.0.sql
@@ -0,0 +1,50 @@
+-- Verify tet:utils/automatisation on pg
+
+BEGIN;
+
+select collectivite_id,
+       key,
+       completude_eci,
+       completude_cae,
+       fiches,
+       plans,
+       resultats_indicateurs,
+       indicateurs_perso,
+       resultats_indicateurs_perso,
+       premier_rattachement,
+       fiches_initiees,
+       fiches_pilotage,
+       fiches_indicateur,
+       fiches_action_referentiel,
+       fiches_fiche_liee,
+       fiches_mod_1mois,
+       fiches_mod_3mois,
+       fiches_mod_6mois,
+       pa_date_creation,
+       pa_view_1mois,
+       pa_view_3mois,
+       pa_view_6mois,
+       pa_non_vides,
+       pa_pilotables,
+       fiches_non_vides,
+       fiches_pilotables,
+       _5fiches_1pilotage,
+       fiches_changement_statut,
+       pourcentage_fa_privee,
+       pourcentage_fa_pilotable_privee,
+       indicateur_prive,
+       min1_indicateur_prive,
+       min1_indicateur_predef_prive,
+       min1_indicateur_perso_prive,
+       pourcentage_indicateur_predef_prives,
+       type_pa
+from crm_usages
+where false;
+
+select id, nom, nb_prive, pourcentage_prive
+from crm_indicateurs;
+
+select type, nb_plan, nb_plan_90pc_fa_privees
+from crm_plans;
+
+ROLLBACK;
