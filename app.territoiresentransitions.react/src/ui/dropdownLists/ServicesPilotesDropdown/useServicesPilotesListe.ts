a36ac3dd3f03c30504f437095d5aa973b433330c
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-08-08 17:51:52 +0200

    Mutualise le dropdown ServicesPilotes

--- app.territoiresentransitions.react/src/ui/dropdownLists/ServicesPilotesDropdown/useServicesPilotesListe.ts
+++ app.territoiresentransitions.react/src/ui/dropdownLists/ServicesPilotesDropdown/useServicesPilotesListe.ts
@@ -1,5 +1,4 @@
 import {useQuery} from 'react-query';
-
 import {supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {TFicheActionServicePiloteRow} from 'types/alias';
@@ -8,11 +7,14 @@ export const useServicesPilotesListe = () => {
   const collectivite_id = useCollectiviteId()!;
 
   return useQuery(['services_pilotes', collectivite_id], async () => {
-    const {data} = await supabaseClient
+    const {error, data} = await supabaseClient
       .from('service_tag')
       .select()
       .eq('collectivite_id', collectivite_id)
       .order('nom');
-    return data as unknown as TFicheActionServicePiloteRow[];
+
+    if (error) throw new Error(error.message);
+
+    return data as TFicheActionServicePiloteRow[];
   });
 };
