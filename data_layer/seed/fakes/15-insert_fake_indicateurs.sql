5bae305abc857f5d4e84045b1ec587eb696c9e94
Fred <98240+farnoux@users.noreply.github.com>
2024-08-20 15:09:13 +0200

    Revert "[db] Ajoute des seed en local pour les indicateurs open-data"
    This reverts commit 7de739ad85adf45b82746d1a984173ad0b6f3022.

--- data_layer/seed/fakes/15-insert_fake_indicateurs.sql
+++ data_layer/seed/fakes/15-insert_fake_indicateurs.sql
@@ -16,17 +16,3 @@ values
      1 , '01/01/2021', null, 12, null, 13, null),
     ((select id from indicateur_definition where titre = 'Mon indicateur perso' limit 1),
      1 , '01/01/2021', null, 22.33, null, 23.33, null);
-
-
--- Ajout d'indicateurs open-data
-INSERT INTO "public"."indicateur_source_metadonnee" 
-    ("id", "source_id", "date_version", "nom_donnees", "diffuseur", "producteur", "methodologie", "limites") 
-    VALUES
-    (1, 'citepa', '2023-01-01 00:00:00', '', 'Citepa', 'Citepa - Territoires en Transitions', 'Inventaire GES spatialisé (CITEPA 2023 - année 2021 et CITEPA 2021 - années 2016 et 2018) et périmètre (Banatic 2023)', '');
-
-INSERT INTO public.indicateur_valeur 
-    (indicateur_id, collectivite_id, date_valeur, metadonnee_id, resultat, resultat_commentaire, objectif, objectif_commentaire)
-VALUES
-    (48, 1, '2025-01-01', 1, 1.8, null, null, null),
-    (48, 1, '2024-01-01', 1, 1.5, null, null, null),
-    (17, 1, '2024-01-01', 1, null, null, 16, null);
