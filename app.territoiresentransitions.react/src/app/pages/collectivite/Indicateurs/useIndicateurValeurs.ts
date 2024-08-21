5d4850bc8b87cb7fce51041e4cb58950c3f14b27
Marc Rutkowski <marc@attractive-media.fr>
2024-08-21 11:51:37 +0200

    Ajoute la lecture des objectifs et résultats d'un indicateur prédéfini

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/useIndicateurValeurs.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/useIndicateurValeurs.ts
@@ -4,6 +4,7 @@ import {DISABLE_AUTO_REFETCH, supabaseClient} from 'core-logic/api/supabase';
 import {useCollectiviteId} from 'core-logic/hooks/params';
 import {SOURCE_COLLECTIVITE} from './constants';
 import {useIndicateurImportSources} from './Indicateur/detail/useImportSources';
+import {useIndicateurDefinition} from './Indicateur/useIndicateurDefinition';
 
 // export type TIndicateurValeur = Indicateurs.domain.Valeur;
 export type TIndicateurValeur = {
@@ -44,6 +45,39 @@ export const useIndicateurValeurs = ({
         id,
         collectivite_id,
         source === SOURCE_COLLECTIVITE ? null : source
+      );
+    },
+    autoRefresh ? undefined : DISABLE_AUTO_REFETCH
+  );
+};
+
+/** Charge toutes les valeurs associées à un indicateur (pour le graphe) */
+export const useIndicateurReferentielValeurs = ({
+  identifiant,
+  autoRefresh = false,
+}: {
+  /** Identifiant indicateur prédéfini */
+  identifiant: string;
+  /**
+   * Indique si le rafraichissement des datas du graphique doit se faire automatiquement.
+   * `false` par défaut.
+   */
+  autoRefresh?: boolean;
+}) => {
+  const collectivite_id = useCollectiviteId();
+  const source = SOURCE_COLLECTIVITE;
+  const indicateur = useIndicateurDefinition(identifiant);
+  const indicateurId = indicateur?.id;
+
+  return useQuery(
+    ['indicateur_valeurs', collectivite_id, indicateurId, source],
+    async () => {
+      if (!collectivite_id || indicateurId === undefined) return;
+      return Indicateurs.fetch.selectIndicateurValeurs(
+        supabaseClient,
+        indicateurId,
+        collectivite_id,
+        null
       );
     },
     autoRefresh ? undefined : DISABLE_AUTO_REFETCH
