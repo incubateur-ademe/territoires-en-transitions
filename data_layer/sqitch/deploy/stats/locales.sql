2d0eecccebb5e95b05f0b9957884cd65ff686c79
Amandine <contact@jacquelinamandine.fr>
2024-08-21 18:14:26 +0200

    Optimise les requêtes des vues matérialisées des stats locales liées aux indicateurs prédéfinis

--- data_layer/sqitch/deploy/stats/locales.sql
+++ data_layer/sqitch/deploy/stats/locales.sql
@@ -2,9 +2,10 @@
 
 BEGIN;
 
-create or replace function
-    stats.refresh_stats_locales()
-    returns void
+-- Enlève les vues à modifier de la fonction le temps de les modifier
+-- pour éviter de supprimer la fonction et le cron lié.
+create or replace function refresh_stats_locales() returns void
+    language plpgsql
 as
 $$
 begin
@@ -17,79 +18,124 @@ begin
     refresh materialized view stats.evolution_nombre_fiches;
     refresh materialized view stats.locales_evolution_collectivite_avec_minimum_fiches;
     refresh materialized view stats.locales_engagement_collectivite;
+    refresh materialized view stats.locales_labellisation_par_niveau;
     refresh materialized view stats.locales_evolution_indicateur_referentiel;
     refresh materialized view stats.locales_evolution_resultat_indicateur_personnalise;
-    refresh materialized view stats.locales_evolution_resultat_indicateur_referentiel;
     refresh materialized view stats.locales_evolution_nombre_fiches;
-    refresh materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 end
-$$ language plpgsql;
+$$;
 
-drop view stats_locales_labellisation_par_niveau;
-drop materialized view stats.locales_labellisation_par_niveau;
+drop view stats_locales_evolution_collectivite_avec_indicateur;
+drop materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
+drop view postgres.public.stats_locales_evolution_resultat_indicateur_referentiel;
+drop materialized view stats.locales_evolution_resultat_indicateur_referentiel;
 
-create materialized view stats.locales_labellisation_par_niveau as
-WITH latest_labellisation AS (
-                             SELECT l.collectivite_id,
-                                    l.referentiel,
-                                    (
-                                    SELECT ll.etoiles
-                                    FROM labellisation ll
-                                    WHERE ll.collectivite_id = l.collectivite_id
-                                      AND ll.referentiel = l.referentiel
-                                      AND ll.obtenue_le > (now() - interval '4 years')
-                                    ORDER BY ll.obtenue_le DESC
-                                    LIMIT 1
-                                    ) AS etoiles
-                             FROM labellisation l
-                             GROUP BY l.collectivite_id, l.referentiel
-                             ),
-     labellisation_locales AS (
-                             SELECT l.etoiles,
-                                    l.referentiel,
-                                    c.region_code,
-                                    c.departement_code
-                             FROM latest_labellisation l
-                             JOIN stats.collectivite c USING (collectivite_id)
-                             WHERE l.etoiles is not null and l.etoiles>0
-                             )
-SELECT NULL::character varying(2) AS code_region,
+create materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel as
+WITH indicateur_collectivite AS (
+                                SELECT mb.first_day                                                               AS mois,
+                                       c.collectivite_id,
+                                       c.region_code,
+                                       c.departement_code,
+                                       count(ir.*)                                                   AS resultats
+                                FROM stats.monthly_bucket mb
+                                JOIN stats.collectivite c ON true
+                                LEFT JOIN (
+                                          SELECT iv.*
+                                          FROM indicateur_valeur iv
+                                          JOIN indicateur_definition id ON iv.indicateur_id = id.id
+                                          where iv.resultat is not null and id.collectivite_id is null
+                                          ) ir on ir.collectivite_id = c.collectivite_id and ir.modified_at <= mb.first_day
+                                GROUP BY mb.first_day, c.collectivite_id, c.region_code, c.departement_code
+                                )
+SELECT indicateur_collectivite.mois,
+       NULL::character varying(2)                                    AS code_region,
+       NULL::character varying(2)                                    AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois
+UNION ALL
+SELECT indicateur_collectivite.mois,
+       indicateur_collectivite.region_code                           AS code_region,
+       NULL::character varying                                       AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois, indicateur_collectivite.region_code
+UNION ALL
+SELECT indicateur_collectivite.mois,
+       NULL::character varying                                       AS code_region,
+       indicateur_collectivite.departement_code                      AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois, indicateur_collectivite.departement_code
+ORDER BY 1;
+
+
+create materialized view stats.locales_evolution_resultat_indicateur_referentiel as
+WITH resultats AS (
+                  SELECT iv.collectivite_id,
+                         sc.region_code,
+                         sc.departement_code,
+                         iv.modified_at
+                  FROM indicateur_valeur iv
+                  JOIN indicateur_definition id ON iv.indicateur_id = id.id
+                  JOIN stats.collectivite sc ON sc.collectivite_id = iv.collectivite_id
+                  where iv.resultat is not null and id.collectivite_id is null
+                  ),
+     buck_by_cols as (
+                  SELECT m.first_day                AS mois,
+                         id.region_code code_region,
+                         id.code code_departement
+                  from stats.monthly_bucket m
+                  join imports.departement id on true
+                  ),
+     res_by_bucks as (
+                  SELECT
+                      m.mois,
+                      m.code_region,
+                      m.code_departement,
+                      count(i.*) AS indicateurs
+                  FROM buck_by_cols m
+                  LEFT JOIN resultats i ON i.modified_at <= m.mois and m.code_departement = i.departement_code
+                  GROUP BY m.mois, m.code_region, m.code_departement
+                  )
+SELECT m.mois,
+       NULL::character varying(2) AS code_region,
        NULL::character varying(2) AS code_departement,
-       labellisation_locales.referentiel,
-       labellisation_locales.etoiles,
-       count(*)                   AS labellisations
-FROM labellisation_locales
-GROUP BY labellisation_locales.referentiel, labellisation_locales.etoiles
+       sum(m.indicateurs)                 AS indicateurs
+FROM res_by_bucks m
+GROUP BY m.mois
 UNION ALL
-SELECT r.code                          AS code_region,
-       NULL::character varying         AS code_departement,
-       l.referentiel,
-       l.etoiles,
-       COALESCE(count(l.*), 0::bigint) AS labellisations
-FROM imports.region r
-JOIN labellisation_locales l ON l.region_code::text = r.code::text
-GROUP BY l.referentiel, l.etoiles, r.code
+SELECT m.mois,
+       m.code_region,
+       NULL::character varying AS code_departement,
+       sum(m.indicateurs)              AS indicateurs
+FROM res_by_bucks m
+GROUP BY m.mois, m.code_region
 UNION ALL
-SELECT NULL::character varying         AS code_region,
-       d.code                          AS code_departement,
-       l.referentiel,
-       l.etoiles,
-       COALESCE(count(l.*), 0::bigint) AS labellisations
-FROM imports.departement d
-JOIN labellisation_locales l ON l.departement_code::text = d.code::text
-GROUP BY l.referentiel, l.etoiles, d.code;
+SELECT m.mois,
+       NULL::character varying AS code_region,
+       m.code_departement,
+       sum(m.indicateurs)              AS indicateurs
+FROM res_by_bucks m
+GROUP BY m.mois, m.code_departement
+ORDER BY 1;
 
-create view stats_locales_labellisation_par_niveau as
-SELECT code_region,
+create view stats_locales_evolution_collectivite_avec_indicateur as
+SELECT mois,
+       code_region,
        code_departement,
-       referentiel,
-       etoiles,
-       labellisations
-FROM stats.locales_labellisation_par_niveau;
+       collectivites
+FROM stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 
-create or replace function
-    stats.refresh_stats_locales()
-    returns void
+create view stats_locales_evolution_resultat_indicateur_referentiel as
+SELECT mois,
+       code_region,
+       code_departement,
+       indicateurs
+FROM stats.locales_evolution_resultat_indicateur_referentiel;
+
+create or replace function refresh_stats_locales() returns void
+    language plpgsql
 as
 $$
 begin
@@ -109,7 +155,6 @@ begin
     refresh materialized view stats.locales_evolution_nombre_fiches;
     refresh materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 end
-$$ language plpgsql;
-
+$$;
 
 COMMIT;
--- data_layer/sqitch/revert/stats/locales.sql
+++ data_layer/sqitch/revert/stats/locales.sql
@@ -2,9 +2,10 @@
 
 BEGIN;
 
-create or replace function
-    stats.refresh_stats_locales()
-    returns void
+-- Enlève les vues à modifier de la fonction le temps de les modifier
+-- pour éviter de supprimer la fonction et le cron lié.
+create or replace function refresh_stats_locales() returns void
+    language plpgsql
 as
 $$
 begin
@@ -17,77 +18,127 @@ begin
     refresh materialized view stats.evolution_nombre_fiches;
     refresh materialized view stats.locales_evolution_collectivite_avec_minimum_fiches;
     refresh materialized view stats.locales_engagement_collectivite;
+    refresh materialized view stats.locales_labellisation_par_niveau;
     refresh materialized view stats.locales_evolution_indicateur_referentiel;
     refresh materialized view stats.locales_evolution_resultat_indicateur_personnalise;
-    refresh materialized view stats.locales_evolution_resultat_indicateur_referentiel;
     refresh materialized view stats.locales_evolution_nombre_fiches;
-    refresh materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 end
-$$ language plpgsql;
+$$;
 
-drop view stats_locales_labellisation_par_niveau;
-drop materialized view stats.locales_labellisation_par_niveau;
+drop view stats_locales_evolution_collectivite_avec_indicateur;
+drop materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
+drop view postgres.public.stats_locales_evolution_resultat_indicateur_referentiel;
+drop materialized view stats.locales_evolution_resultat_indicateur_referentiel;
 
-create materialized view stats.locales_labellisation_par_niveau as
-WITH latest_labellisation AS (
-                             SELECT l.collectivite_id,
-                                    l.referentiel,
-                                    (
-                                    SELECT ll.etoiles
-                                    FROM labellisation ll
-                                    WHERE ll.collectivite_id = l.collectivite_id
-                                      AND ll.referentiel = l.referentiel
-                                    ORDER BY ll.obtenue_le DESC
-                                    LIMIT 1
-                                    ) AS etoiles
-                             FROM labellisation l
-                             GROUP BY l.collectivite_id, l.referentiel
-                             ),
-     labellisation_locales AS (
-                             SELECT l.etoiles,
-                                    l.referentiel,
-                                    c.region_code,
-                                    c.departement_code
-                             FROM latest_labellisation l
-                             JOIN stats.collectivite c USING (collectivite_id)
-                             )
-SELECT NULL::character varying(2) AS code_region,
+create materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel as
+WITH indicateur_collectivite AS (
+                                SELECT mb.first_day                                                               AS mois,
+                                       c.collectivite_id,
+                                       c.region_code,
+                                       c.departement_code,
+                                       COALESCE(count(*) FILTER (WHERE ir.modified_at <= mb.last_day),
+                                                0::bigint)                                                        AS resultats
+                                FROM stats.monthly_bucket mb
+                                JOIN stats.collectivite c ON true
+                                LEFT JOIN (
+                                          SELECT iv.id,
+                                                 iv.indicateur_id,
+                                                 iv.collectivite_id,
+                                                 iv.date_valeur,
+                                                 iv.metadonnee_id,
+                                                 iv.resultat,
+                                                 iv.resultat_commentaire,
+                                                 iv.objectif,
+                                                 iv.objectif_commentaire,
+                                                 iv.estimation,
+                                                 iv.modified_at,
+                                                 iv.created_at,
+                                                 iv.modified_by,
+                                                 iv.created_by
+                                          FROM indicateur_valeur iv
+                                          JOIN indicateur_definition id ON iv.indicateur_id = id.id
+                                          WHERE id.collectivite_id IS NULL
+                                            AND iv.resultat IS NOT NULL
+                                          ) ir USING (collectivite_id)
+                                GROUP BY mb.first_day, c.collectivite_id, c.departement_code, c.region_code
+                                )
+SELECT indicateur_collectivite.mois,
+       NULL::character varying(2)                                    AS code_region,
+       NULL::character varying(2)                                    AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois
+UNION ALL
+SELECT indicateur_collectivite.mois,
+       indicateur_collectivite.region_code                           AS code_region,
+       NULL::character varying                                       AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois, indicateur_collectivite.region_code
+UNION ALL
+SELECT indicateur_collectivite.mois,
+       NULL::character varying                                       AS code_region,
+       indicateur_collectivite.departement_code                      AS code_departement,
+       count(*) FILTER (WHERE indicateur_collectivite.resultats > 0) AS collectivites
+FROM indicateur_collectivite
+GROUP BY indicateur_collectivite.mois, indicateur_collectivite.departement_code
+ORDER BY 1;
+
+create materialized view stats.locales_evolution_resultat_indicateur_referentiel as
+WITH resultats AS (
+                  SELECT iv.collectivite_id,
+                         sc.region_code,
+                         sc.departement_code,
+                         iv.modified_at
+                  FROM indicateur_valeur iv
+                  JOIN indicateur_definition id ON iv.indicateur_id = id.id
+                  JOIN stats.collectivite sc ON sc.collectivite_id = iv.collectivite_id
+                  WHERE iv.resultat IS NOT NULL
+                    AND id.collectivite_id IS NULL
+                  )
+SELECT m.first_day                AS mois,
+       NULL::character varying(2) AS code_region,
        NULL::character varying(2) AS code_departement,
-       labellisation_locales.referentiel,
-       labellisation_locales.etoiles,
-       count(*)                   AS labellisations
-FROM labellisation_locales
-GROUP BY labellisation_locales.referentiel, labellisation_locales.etoiles
+       count(i.*)                 AS indicateurs
+FROM stats.monthly_bucket m
+LEFT JOIN resultats i ON i.modified_at <= m.last_day
+GROUP BY m.first_day
 UNION ALL
-SELECT r.code                          AS code_region,
-       NULL::character varying         AS code_departement,
-       l.referentiel,
-       l.etoiles,
-       COALESCE(count(l.*), 0::bigint) AS labellisations
+SELECT m.first_day             AS mois,
+       r.code                  AS code_region,
+       NULL::character varying AS code_departement,
+       count(i.*)              AS indicateurs
 FROM imports.region r
-JOIN labellisation_locales l ON l.region_code::text = r.code::text
-GROUP BY l.referentiel, l.etoiles, r.code
+JOIN stats.monthly_bucket m ON true
+LEFT JOIN resultats i ON i.modified_at <= m.last_day AND i.region_code::text = r.code::text
+GROUP BY m.first_day, r.code
 UNION ALL
-SELECT NULL::character varying         AS code_region,
-       d.code                          AS code_departement,
-       l.referentiel,
-       l.etoiles,
-       COALESCE(count(l.*), 0::bigint) AS labellisations
+SELECT m.first_day             AS mois,
+       NULL::character varying AS code_region,
+       d.code                  AS code_departement,
+       count(i.*)              AS indicateurs
 FROM imports.departement d
-JOIN labellisation_locales l ON l.departement_code::text = d.code::text
-GROUP BY l.referentiel, l.etoiles, d.code;
+JOIN stats.monthly_bucket m ON true
+LEFT JOIN resultats i ON i.modified_at <= m.last_day AND i.departement_code::text = d.code::text
+GROUP BY m.first_day, d.code
+ORDER BY 1;
 
-create view stats_locales_labellisation_par_niveau as
-SELECT code_region,
+create view stats_locales_evolution_collectivite_avec_indicateur as
+SELECT mois,
+       code_region,
        code_departement,
-       referentiel,
-       etoiles,
-       labellisations
-FROM stats.locales_labellisation_par_niveau;
+       collectivites
+FROM stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 
-create or replace function
-    stats.refresh_stats_locales()
-    returns void
+create view stats_locales_evolution_resultat_indicateur_referentiel as
+SELECT mois,
+       code_region,
+       code_departement,
+       indicateurs
+FROM stats.locales_evolution_resultat_indicateur_referentiel;
+
+create or replace function refresh_stats_locales() returns void
+    language plpgsql
 as
 $$
 begin
@@ -107,6 +158,6 @@ begin
     refresh materialized view stats.locales_evolution_nombre_fiches;
     refresh materialized view stats.locales_evolution_collectivite_avec_indicateur_referentiel;
 end
-$$ language plpgsql;
+$$;
 
 COMMIT;
