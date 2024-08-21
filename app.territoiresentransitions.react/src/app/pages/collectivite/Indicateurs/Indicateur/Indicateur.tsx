2d6e5449e45990f958f28abd1d4e6d506d7e867f
Arthur Molinos <arthur.molinos@gmail.com>
2024-08-21 17:50:06 +0200

    Ajoute la redirection pour les anciennes pages subset des indicateurs

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/Indicateur.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/Indicateur.tsx
@@ -1,26 +1,27 @@
-import {useParams} from 'react-router-dom';
+import {Redirect, useParams} from 'react-router-dom';
 import {
   indicateurIdParam,
   indicateurIdentiantReferentielParam,
+  makeCollectiviteTousLesIndicateursUrl,
 } from 'app/paths';
 import {IndicateurPersonnalise} from './IndicateurPersonnalise';
 import {IndicateurPredefini} from './IndicateurPredefini';
-import IndicateurPersoNouveau from './IndicateurPersoNouveau';
 import {indicateurViewParam, IndicateurViewParamOption} from 'app/paths';
-
-// id d'un indicateur en cours de création
-const ID_NOUVEAU = -1;
+import {useCollectiviteId} from 'core-logic/hooks/params';
 
 /**
  * Affiche le détail d'un indicateur
  */
 const Indicateur = () => {
+  const collectiviteId = useCollectiviteId();
+
   const params = useParams<{
     [indicateurViewParam]?: IndicateurViewParamOption;
     [indicateurIdParam]?: string;
     [indicateurIdentiantReferentielParam]?: string;
   }>();
   let indicateurId, isPerso;
+
   if (params[indicateurIdParam] !== undefined) {
     indicateurId = parseInt(params[indicateurIdParam]);
     isPerso = true;
@@ -31,7 +32,13 @@ const Indicateur = () => {
   const view = params[indicateurViewParam] || 'perso';
 
   if (!indicateurId) {
-    return null;
+    return (
+      <Redirect
+        to={makeCollectiviteTousLesIndicateursUrl({
+          collectiviteId: collectiviteId!,
+        })}
+      />
+    );
   }
 
   return (
@@ -41,9 +48,7 @@ const Indicateur = () => {
         indicateurId !== undefined ? `ind-${indicateurId}` : `ind-v-${view}`
       }
     >
-      {indicateurId === ID_NOUVEAU ? (
-        <IndicateurPersoNouveau className="fr-p-6w" />
-      ) : isPerso ? (
+      {isPerso ? (
         <IndicateurPersonnalise indicateurId={indicateurId as number} />
       ) : (
         <IndicateurPredefini indicateurId={indicateurId} />
