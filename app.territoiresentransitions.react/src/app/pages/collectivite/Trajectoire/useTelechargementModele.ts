265885ba87c51b000001f2398a6d1464a44425e6
Marc Rutkowski <marc@attractive-media.fr>
2024-08-29 17:36:07 +0200

    Change le message affiché lors du téléchargement d'un fichier

--- app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/useTelechargementModele.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/useTelechargementModele.ts
@@ -1,17 +1,22 @@
 import {useMutation} from 'react-query';
 import {useApiClient} from 'core-logic/api/useApiClient';
 import {saveBlob} from 'ui/shared/preuves/Bibliotheque/saveBlob';
+import {DOWNLOAD_FILE_MUTATION_OPTIONS} from 'utils/useDownloadFile';
 
 /** Télécharge le fichier xlsx modèle */
 export const useTelechargementModele = () => {
   const api = useApiClient();
 
-  return useMutation('snbc/modele', async () => {
-    const {blob, filename} = await api.getAsBlob({
-      route: '/trajectoires/snbc/modele',
-    });
-    if (blob) {
-      await saveBlob(blob, filename || `Trajectoire SNBC - modèle.xlsx`);
-    }
-  });
+  return useMutation(
+    'snbc/modele',
+    async () => {
+      const {blob, filename} = await api.getAsBlob({
+        route: '/trajectoires/snbc/modele',
+      });
+      if (blob) {
+        await saveBlob(blob, filename || `Trajectoire SNBC - modèle.xlsx`);
+      }
+    },
+    DOWNLOAD_FILE_MUTATION_OPTIONS
+  );
 };
--- app.territoiresentransitions.react/src/utils/useDownloadFile.ts
+++ app.territoiresentransitions.react/src/utils/useDownloadFile.ts
@@ -3,8 +3,19 @@ import {saveBlob} from 'ui/shared/preuves/Bibliotheque/saveBlob';
 
 /** Télécharge un fichier du dossier "public" */
 export const useDownloadFile = () =>
-  useMutation('download_file', async (filename: string) => {
-    const response = await fetch(`/${filename}`);
-    const blob = await response.blob();
-    await saveBlob(blob, filename);
-  });
+  useMutation(
+    'download_file',
+    async (filename: string) => {
+      const response = await fetch(`/${filename}`);
+      const blob = await response.blob();
+      await saveBlob(blob, filename);
+    },
+    DOWNLOAD_FILE_MUTATION_OPTIONS
+  );
+
+export const DOWNLOAD_FILE_MUTATION_OPTIONS = {
+  meta: {
+    success: 'Fichier téléchargé',
+    error: 'Erreur de téléchargement',
+  },
+};
