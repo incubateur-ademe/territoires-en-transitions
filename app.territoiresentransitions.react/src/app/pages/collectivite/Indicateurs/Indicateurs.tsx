373c023ec2396521ac9ce48d06958337f6f2f49f
Arthur Molinos <arthur.molinos@gmail.com>
2024-08-29 18:23:49 +0200

    Crée la page "Indicateurs de la collectivité" et ajoute le menu pour changer la propriété "favoris" sur IndicateurCard

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateurs.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateurs.tsx
@@ -1,10 +1,12 @@
 import CollectivitePageLayout from 'app/pages/collectivite/CollectivitePageLayout/CollectivitePageLayout';
 import {RouteEnAccesRestreint} from 'app/pages/collectivite/CollectiviteRoutes';
 import {IndicateurPage} from 'app/pages/collectivite/Indicateurs/Indicateur/IndicateurPage';
+import {IndicateursCollectivitePage} from 'app/pages/collectivite/Indicateurs/IndicateursCollectivite/IndicateursCollectivitePage';
 import {TousLesIndicateursPage} from 'app/pages/collectivite/Indicateurs/TousLesIndicateurs/TousLesIndicateursPage';
 import {
   collectiviteIndicateurPath,
   collectiviteIndicateurReferentielPath,
+  collectiviteIndicateursCollectivitePath,
   collectiviteTousLesIndicateursPath,
 } from 'app/paths';
 import {Route, Switch} from 'react-router-dom';
@@ -17,6 +19,14 @@ const Indicateurs = () => {
         <div className="grow bg-grey-2 -mb-8 py-12 px-4 lg:px-6 2xl:px-0">
           <CollectivitePageLayout>
             <TousLesIndicateursPage />
+          </CollectivitePageLayout>
+        </div>
+      </Route>
+
+      <Route exact path={collectiviteIndicateursCollectivitePath}>
+        <div className="grow bg-grey-2 -mb-8 py-12 px-4 lg:px-6 2xl:px-0">
+          <CollectivitePageLayout>
+            <IndicateursCollectivitePage />
           </CollectivitePageLayout>
         </div>
       </Route>
