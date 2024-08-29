200be6f653a48d7a00650e8d2c1cb7c22c63bd60
Arthur Molinos <arthur.molinos@gmail.com>
2024-08-29 18:21:57 +0200

    Ajoute à IndicateurCard la possibilité d'être éditable

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/IndicateurCard/IndicateurCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/IndicateurCard/IndicateurCard.tsx
@@ -22,6 +22,7 @@ import PictoIndicateurComplet from 'ui/pictogrammes/PictoIndicateurComplet';
 import {getIndicateurRestant} from './utils';
 import {BadgeACompleter} from 'ui/shared/Badge/BadgeACompleter';
 import {transformeValeurs} from 'app/pages/collectivite/Indicateurs/Indicateur/detail/transformeValeurs';
+import IndicateurCardOptions from 'app/pages/collectivite/Indicateurs/lists/IndicateurCard/IndicateurCardOptions';
 
 /** Props de la carte Indicateur */
 export type IndicateurCardProps = {
@@ -48,6 +49,8 @@ export type IndicateurCardProps = {
   hideChart?: boolean;
   /** Affiche ou masque le graphique quand il n'y a pas de valeur */
   hideChartWithoutValue?: boolean;
+  /** Affiche les options de modification au hover de la carte */
+  isEditable?: boolean;
   /** Props du composant générique Card */
   card?: CardProps;
   /** Si l'utilisateur est lecteur ou non */
@@ -116,6 +119,7 @@ export const IndicateurCardBase = ({
   definition,
   chart,
   chartInfo,
+  isEditable = false,
   hideChart = false,
   hideChartWithoutValue = false,
   card,
@@ -149,16 +153,8 @@ export const IndicateurCardBase = ({
     : !chartInfo?.rempli;
 
   return (
-    <Card
-      dataTest={`chart-${definition.id}`}
-      className={classNames(
-        'relative font-normal !gap-3 !p-6',
-        {'border-primary-7': selectState?.checkbox && selectState?.selected},
-        className
-      )}
-      href={href}
-      {...card}
-    >
+    <div className="group relative h-full">
+      {/** Cadenas indicateur privé */}
       {chartInfo?.confidentiel && (
         <Tooltip label="La dernière valeur de cet indicateur est en mode privé">
           <div className="absolute -top-5 left-5">
@@ -166,177 +162,195 @@ export const IndicateurCardBase = ({
           </div>
         </Tooltip>
       )}
-      {selectState?.checkbox ? (
-        <Checkbox
-          checked={selectState.selected}
-          onChange={() =>
-            selectState.setSelected({
-              id: definition.id,
-              titre: definition.titre,
-              estPerso: definition.estPerso,
-              identifiant: definition.identifiant || null,
-              description: chartInfo?.titreLong ?? '',
-              unite: chartInfo?.unite ?? '',
-            })
-          }
-          label={chartInfo?.titre}
-          labelClassname="!font-bold"
+      {/** Menus d'édition */}
+      {!readonly && isEditable && (
+        <IndicateurCardOptions
+          definition={definition}
+          isFavoriCollectivite={chartInfo?.favoriCollectivite}
         />
-      ) : (
-        <>
-          <div className="flex items-center gap-6">
-            <BadgeACompleter a_completer={isACompleter} size="sm" />
-            {selectState?.setSelected && (
-              <Button
-                onClick={(
-                  evt: React.MouseEvent<HTMLButtonElement, MouseEvent>
-                ) => {
-                  evt.preventDefault();
-                  evt.stopPropagation();
-                  selectState.setSelected({
-                    id: definition.id,
-                    titre: definition.titre,
-                    estPerso: definition.estPerso,
-                    identifiant: definition.identifiant || null,
-                    description: chartInfo?.titreLong ?? '',
-                    unite: chartInfo?.unite ?? '',
-                  });
-                }}
-                icon="link-unlink"
-                title="Dissocier l'indicateur"
-                size="xs"
-                variant="grey"
-                className={classNames('ml-auto hidden -my-2', {
-                  'group-hover:flex': !readonly,
-                })}
-              />
-            )}
-          </div>
-          <div className="font-bold line-clamp-2">{chartInfo?.titre}</div>
-        </>
       )}
-      {/** Graphique */}
-      {showChart && (
-        <>
-          {isIndicateurParent &&
-          chartInfo?.sansValeur &&
-          indicateursACompleterRestant === 0 ? (
-            <div className="flex flex-col grow justify-center items-center gap-3 min-h-[18rem] bg-primary-0 rounded-lg">
-              <PictoIndicateurComplet />
-              <div className="font-medium text-primary-8">
-                {totalNbIndicateurs}/{totalNbIndicateurs} complétés
-              </div>
+      <Card
+        dataTest={`chart-${definition.id}`}
+        className={classNames(
+          'h-full font-normal !gap-3 !p-6',
+          {'border-primary-7': selectState?.checkbox && selectState?.selected},
+          className
+        )}
+        href={href}
+        {...card}
+      >
+        {selectState?.checkbox ? (
+          <Checkbox
+            checked={selectState.selected}
+            onChange={() =>
+              selectState.setSelected({
+                id: definition.id,
+                titre: definition.titre,
+                estPerso: definition.estPerso,
+                identifiant: definition.identifiant || null,
+                description: chartInfo?.titreLong ?? '',
+                unite: chartInfo?.unite ?? '',
+              })
+            }
+            label={chartInfo?.titre}
+            labelClassname="!font-bold"
+          />
+        ) : (
+          <>
+            <div className="flex items-center gap-6">
+              <BadgeACompleter a_completer={isACompleter} size="sm" />
+              {selectState?.setSelected && (
+                <Button
+                  onClick={(
+                    evt: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    selectState.setSelected({
+                      id: definition.id,
+                      titre: definition.titre,
+                      estPerso: definition.estPerso,
+                      identifiant: definition.identifiant || null,
+                      description: chartInfo?.titreLong ?? '',
+                      unite: chartInfo?.unite ?? '',
+                    });
+                  }}
+                  icon="link-unlink"
+                  title="Dissocier l'indicateur"
+                  size="xs"
+                  variant="grey"
+                  className={classNames('ml-auto hidden -my-2', {
+                    'group-hover:flex': !readonly,
+                  })}
+                />
+              )}
             </div>
-          ) : (
-            <div
-              className={classNames('flex flex-col grow', {
-                'min-h-[18rem] justify-center items-center gap-3 bg-primary-0 rounded-lg':
-                  isNotLoadingNotFilled,
-              })}
-            >
-              <IndicateurChart
-                data={data}
-                isLoading={isLoading}
-                className={classNames(
-                  {'grow-0': isNotLoadingNotFilled},
-                  {'!items-end': chartInfo?.rempli},
-                  chart?.className
-                )}
-                chartConfig={{
-                  className: '!h-[14rem]',
-                  theme: {
-                    axis: {
-                      ticks: {
-                        text: {
-                          fontSize: 12,
+            <div className="font-bold line-clamp-2">{chartInfo?.titre}</div>
+          </>
+        )}
+        {/** Graphique */}
+        {showChart && (
+          <>
+            {isIndicateurParent &&
+            chartInfo?.sansValeur &&
+            indicateursACompleterRestant === 0 ? (
+              <div className="flex flex-col grow justify-center items-center gap-3 min-h-[18rem] bg-primary-0 rounded-lg">
+                <PictoIndicateurComplet />
+                <div className="font-medium text-primary-8">
+                  {totalNbIndicateurs}/{totalNbIndicateurs} complétés
+                </div>
+              </div>
+            ) : (
+              <div
+                className={classNames('flex flex-col grow', {
+                  'min-h-[18rem] justify-center items-center gap-3 bg-primary-0 rounded-lg':
+                    isNotLoadingNotFilled,
+                })}
+              >
+                <IndicateurChart
+                  data={data}
+                  isLoading={isLoading}
+                  className={classNames(
+                    {'grow-0': isNotLoadingNotFilled},
+                    {'!items-end': chartInfo?.rempli},
+                    chart?.className
+                  )}
+                  chartConfig={{
+                    className: '!h-[14rem]',
+                    theme: {
+                      axis: {
+                        ticks: {
+                          text: {
+                            fontSize: 12,
+                          },
                         },
                       },
                     },
-                  },
-                  margin: {
-                    top: 16,
-                    right: 4,
-                    bottom: 32,
-                    left: getLeftLineChartMargin(data.valeurs) + 4,
-                  },
-                  gridXValues: 4,
-                  gridYValues: 4,
-                  ...chart?.chartConfig,
-                }}
-              />
-              {isNotLoadingNotFilled && !readonly && !!href && (
-                <Button size="xs" className="mx-auto">
-                  Compléter l'indicateur
-                </Button>
-              )}
-              {/** Légende */}
-              {hasValeurOrObjectif && (
-                <div className="flex flex-wrap gap-4 ml-2 mt-2">
-                  {generateLineLegendItems(prepareData(data.valeurs)).map(
-                    ({name, color, symbole}) => (
-                      <div key={name} className="flex items-center gap-2">
-                        {!!symbole ? (
-                          symbole(color)
-                        ) : (
-                          <div
-                            className="w-3 h-3 rounded-full"
-                            style={{backgroundColor: color}}
-                          />
-                        )}
-                        <div className="text-xs text-grey-8">{name}</div>
-                      </div>
-                    )
-                  )}
-                </div>
-              )}
-            </div>
-          )}
-        </>
-      )}
-      {/** Partie sous le séparateur horizontal */}
-      {chartInfo &&
-        (showChart ? (
-          <>
-            {/** Barre horizontale */}
-            {hasValeurOrObjectif &&
-              (isIndicateurParent || chartInfo.participationScore) && (
-                <div className="h-px bg-primary-3" />
-              )}
-            {(isIndicateurParent || chartInfo.participationScore) && (
-              <div className="flex flex-wrap gap-2 items-center text-sm text-grey-8">
-                {/* Nombre d'indicateurs */}
-                {isIndicateurParent && totalNbIndicateurs && (
-                  <div>
-                    {totalNbIndicateurs - indicateursACompleterRestant}/
-                    {totalNbIndicateurs} indicateur
-                    {totalNbIndicateurs > 1 && 's'}
-                  </div>
+                    margin: {
+                      top: 16,
+                      right: 4,
+                      bottom: 32,
+                      left: getLeftLineChartMargin(data.valeurs) + 4,
+                    },
+                    gridXValues: 4,
+                    gridYValues: 4,
+                    ...chart?.chartConfig,
+                  }}
+                />
+                {isNotLoadingNotFilled && !readonly && !!href && (
+                  <Button size="xs" className="mx-auto">
+                    Compléter l'indicateur
+                  </Button>
                 )}
-                {/** Barre verticale */}
-                {isIndicateurParent &&
-                  totalNbIndicateurs &&
-                  chartInfo.participationScore && (
-                    <div className="w-px h-3 bg-grey-5" />
-                  )}
-                {/** Participation au score */}
-                {chartInfo.participationScore && (
-                  <div>Participe au score Climat Air Énergie</div>
+                {/** Légende */}
+                {hasValeurOrObjectif && (
+                  <div className="flex flex-wrap gap-4 ml-2 mt-2">
+                    {generateLineLegendItems(prepareData(data.valeurs)).map(
+                      ({name, color, symbole}) => (
+                        <div key={name} className="flex items-center gap-2">
+                          {!!symbole ? (
+                            symbole(color)
+                          ) : (
+                            <div
+                              className="w-3 h-3 rounded-full"
+                              style={{backgroundColor: color}}
+                            />
+                          )}
+                          <div className="text-xs text-grey-8">{name}</div>
+                        </div>
+                      )
+                    )}
+                  </div>
                 )}
               </div>
             )}
           </>
-        ) : (
-          isACompleter &&
-          !readonly &&
-          href && (
+        )}
+        {/** Partie sous le séparateur horizontal */}
+        {chartInfo &&
+          (showChart ? (
             <>
               {/** Barre horizontale */}
-              <div className="h-px bg-primary-3" />
-              {/** Compléter indicateur bouton */}
-              <Button size="xs">Compléter l’indicateur</Button>
+              {hasValeurOrObjectif &&
+                (isIndicateurParent || chartInfo.participationScore) && (
+                  <div className="h-px bg-primary-3" />
+                )}
+              {(isIndicateurParent || chartInfo.participationScore) && (
+                <div className="flex flex-wrap gap-2 items-center text-sm text-grey-8">
+                  {/* Nombre d'indicateurs */}
+                  {isIndicateurParent && totalNbIndicateurs && (
+                    <div>
+                      {totalNbIndicateurs - indicateursACompleterRestant}/
+                      {totalNbIndicateurs} indicateur
+                      {totalNbIndicateurs > 1 && 's'}
+                    </div>
+                  )}
+                  {/** Barre verticale */}
+                  {isIndicateurParent &&
+                    totalNbIndicateurs &&
+                    chartInfo.participationScore && (
+                      <div className="w-px h-3 bg-grey-5" />
+                    )}
+                  {/** Participation au score */}
+                  {chartInfo.participationScore && (
+                    <div>Participe au score Climat Air Énergie</div>
+                  )}
+                </div>
+              )}
             </>
-          )
-        ))}
-    </Card>
+          ) : (
+            isACompleter &&
+            !readonly &&
+            href && (
+              <>
+                {/** Barre horizontale */}
+                <div className="h-px bg-primary-3" />
+                {/** Compléter indicateur bouton */}
+                <Button size="xs">Compléter l’indicateur</Button>
+              </>
+            )
+          ))}
+      </Card>
+    </div>
   );
 };
