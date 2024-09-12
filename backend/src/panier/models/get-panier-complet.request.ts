b1fe4de5bc211c429f50c65f209f01d0c703aff7
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:31:18 +0200

    Backend - Ajoute les paniers

---
+++ backend/src/panier/models/get-panier-complet.request.ts
@@ -0,0 +1,27 @@
+import { extendApi } from '@anatine/zod-openapi';
+import { z } from 'zod';
+import { createZodDto } from '@anatine/zod-nestjs';
+
+export const getPanierCompletRequestSchema = extendApi(
+  z
+    .object({
+      panierId: z.string().openapi({ description: `Identifiant du panier` }),
+      thematiquesIds: z
+        .array(z.number())
+        .openapi({ description: `Identifiant des thématiques` }),
+      niveauxBudget: z
+        .array(z.number())
+        .openapi({ description: `Niveaux des budgets` }),
+      niveauxTemps: z
+        .array(z.number())
+        .openapi({ description: `Niveaux des temps de mise en oeuvre` }),
+    })
+    .openapi({
+      title:
+        'Identifiant du panier et filtres à appliquer sur le contenu à récupérer.',
+    }),
+);
+
+export class GetPanierCompletRequestClass extends createZodDto(
+  getPanierCompletRequestSchema,
+) {}
