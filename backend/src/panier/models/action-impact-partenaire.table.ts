29f6238ed83fc76f0562736c5832d950d4230a16
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:30:57 +0200

    Backend - Ajoute les actions à impact

---
+++ backend/src/panier/models/action-impact-partenaire.table.ts
@@ -0,0 +1,22 @@
+import { integer, pgTable, primaryKey } from 'drizzle-orm/pg-core';
+import { panierPartenaireTable } from '../../taxonomie/models/panier-partenaire.table';
+import { actionImpactTable } from './action-impact.table';
+
+export const actionImpactPartenaireTable = pgTable(
+  'action_impact_partenaire',
+  {
+    actionImpactId: integer('action_impact_id')
+      .notNull()
+      .references(() => actionImpactTable.id),
+    partenaireId: integer('partenaire_id')
+      .notNull()
+      .references(() => panierPartenaireTable.id),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        columns: [table.actionImpactId, table.partenaireId],
+      }),
+    };
+  },
+);
