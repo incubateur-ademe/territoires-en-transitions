ea492c1b9e53fca80165f6675f1c01de2407edf0
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:26:46 +0200

    Backend - Ajoute les axes

--- backend/src/fiches/models/axe.table.ts
+++ backend/src/fiches/models/axe.table.ts
@@ -8,27 +8,28 @@ import {
   timestamp,
   uuid,
 } from 'drizzle-orm/pg-core';
-import { collectiviteTable } from '../../collectivites/models/collectivite.models';
+import { collectiviteTable } from '../../collectivites/models/collectivite.table';
+import { panierTable } from '../../panier/models/panier.table';
 import { planActionTypeTable } from './plan-action-type.table';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
-export const axeTable = pgTable('axe', {
+export const axeTable: ReturnType<typeof pgTable> = pgTable('axe', {
   id: serial('id').primaryKey(),
   nom: text('nom'),
-  collectivite_id: integer('collectivite_id')
+  collectiviteId: integer('collectivite_id')
     .notNull()
     .references(() => collectiviteTable.id),
-  parent: integer('parent').references((): AnyPgColumn => axeTable.id),
-  plan: integer('plan').references((): AnyPgColumn => axeTable.id),
+  parent: integer('parent').references(() => axeTable.id),
+  plan: integer('plan').references(() => axeTable.id),
   type: integer('type').references(() => planActionTypeTable.id),
-  created_at: timestamp('created_at', { withTimezone: true })
+  createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
-  modified_at: timestamp('modified_at', { withTimezone: true })
+  modifiedAt: timestamp('modified_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
-  modified_by: uuid('modified_by'), // TODO references auth.uid
-  panier_id: integer('panier_id'), // TODO references panier
+  modifiedBy: uuid('modified_by'), // TODO references auth.uid
+  panierId: integer('panier_id').references(() => panierTable.id),
 });
 export type CreateAxeType = InferInsertModel<typeof axeTable>;
 
