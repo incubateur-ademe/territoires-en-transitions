e24d32b989fc3fde93f3273beafc8009f51b11e4
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:27:19 +0200

    Backend - Ajoute les documents

---
+++ backend/src/documents/models/preuve-reglementaire-definition.table.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, varchar } from 'drizzle-orm/pg-core';
+
+export const preuveReglementaireDefinitionTable = pgTable(
+  'preuve_reglementaire_definition',
+  {
+    id: varchar('id').primaryKey(),
+    nom: text('nom').notNull(),
+    description: text('description').notNull(),
+  },
+);
