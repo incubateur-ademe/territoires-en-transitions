be56868110aa9fb834c2cebbf1fbd81d65054a1a
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:30:57 +0200

    Backend - Ajoute les actions à impact

---
+++ backend/src/panier/models/action-impact-action.table.ts
@@ -0,0 +1,26 @@
+import { integer, pgTable, primaryKey } from 'drizzle-orm/pg-core';
+import { actionRelationTable } from '../../referentiel/models/action-relation.table';
+import { actionImpactTable } from './action-impact.table';
+import { text } from 'drizzle-orm/pg-core';
+import { InferSelectModel } from 'drizzle-orm';
+
+export const actionImpactActionTable = pgTable(
+  'action_impact_action',
+  {
+    actionImpactId: integer('action_impact_id')
+      .notNull()
+      .references(() => actionImpactTable.id),
+    actionId: text('action_id')
+      .notNull()
+      .references(() => actionRelationTable.id),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.actionImpactId, table.actionId] }),
+    };
+  },
+);
+
+export type ActionImpactActionType = InferSelectModel<
+  typeof actionImpactActionTable
+>;
---
+++ backend/src/panier/models/action-impact-sous-thematique.table.ts
@@ -0,0 +1,27 @@
+import { integer, pgTable, primaryKey } from 'drizzle-orm/pg-core';
+import { sousThematiqueTable } from '../../taxonomie/models/sous-thematique.table';
+import { actionImpactTable } from './action-impact.table';
+import { InferSelectModel } from 'drizzle-orm';
+
+export const actionImpactSousThematiqueTable = pgTable(
+  'action_impact_sous_thematique',
+  {
+    actionImpactId: integer('action_impact_id')
+      .notNull()
+      .references(() => actionImpactTable.id),
+    sousThematiqueId: integer('sous_thematique_id')
+      .notNull()
+      .references(() => sousThematiqueTable.id),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        columns: [table.actionImpactId, table.sousThematiqueId],
+      }),
+    };
+  },
+);
+
+export type ActionImpactSousThematiqueType = InferSelectModel<
+  typeof actionImpactSousThematiqueTable
+>;
