ba40b42b081b424b6cb643032f0895cd0b7b5384
Amandine <contact@jacquelinamandine.fr>
2024-09-12 18:24:10 +0200

    Backend - Ajoute les tags

--- backend/src/taxonomie/models/tag.basetable.ts
+++ backend/src/taxonomie/models/tag.basetable.ts
@@ -1,10 +1,10 @@
 import { integer, serial, text } from 'drizzle-orm/pg-core';
-import { collectiviteTable } from '../../collectivites/models/collectivite.models';
+import { collectiviteTable } from '../../collectivites/models/collectivite.table';
 
 export const TagBase = {
   id: serial('id').primaryKey(),
   nom: text('nom').notNull(),
-  collectivite_id: integer('collectivite_id')
+  collectiviteId: integer('collectivite_id')
     .notNull()
     .references(() => collectiviteTable.id),
 };
