0e54ebb98159cf4dded77056cc9fe8cb8e66283b
Marc Rutkowski <marc@attractive-media.fr>
2024-09-17 12:10:24 +0200

    Met à jour les libellés

--- app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/ComparezLaTrajectoire.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/ComparezLaTrajectoire.tsx
@@ -13,10 +13,10 @@ export const ComparezLaTrajectoire = ({
     <Card>
       <h5>Comparez la trajectoire SNBC à vos objectifs et vos résultats</h5>
       <p className="text-sm font-normal">
-        Vos résultats et vos objectifs ne sont pas disponibles pour cet
-        indicateur. Veuillez les renseigner afin de pouvoir les comparer à votre
-        trajectoire SNBC territorialisée. Vous pourrez ainsi visualiser les
-        écarts plus facilement pour piloter votre stratégie.
+        Pour cela, il faut d'abord{' '}
+        <b>compléter vos objectifs et vos résultats dans vos Indicateurs</b>.
+        Vous avez le choix d'appliquer les données disponibles en open data, ou
+        bien de renseigner vos propres données.
       </p>
       <Button
         href={makeCollectiviteIndicateursUrl({
--- app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/DonneesPartiellementDisponibles.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/DonneesPartiellementDisponibles.tsx
@@ -12,14 +12,14 @@ export const DonneesPartiellementDisponibles = ({
   return (
     <Alert
       state="warning"
-      title="Données partiellement disponibles"
-      description="Certains secteurs manquent dans le graphique ci-dessus : nous ne disposons pas encore des données permettant de calculer leur trajectoire SNBC territorialisée pour votre collectivité. Nous y travaillons et espérons vous proposer une trajectoire complète automatiquement très prochainement. En attendant, vous pouvez calculer dès maintenant votre trajectoire pour l’ensemble des secteurs en complétant les données déjà disponibles."
+      title="Voici un premier calcul de votre trajectoire SNBC territorialisée, avec les données disponibles !"
+      description="Il manque des données pour certains secteurs : nous vous les mettrons à disposition prochainement. En attendant, complétez les données manquantes pour l'année 2015 afin de finaliser le calcul."
       footer={
         <Modal
           size="xl"
           render={props => <DonneesCollectivite modalProps={props} />}
         >
-          <Button size="sm">Compléter mes données</Button>
+          <Button size="sm">Compléter les données</Button>
         </Modal>
       }
     />
