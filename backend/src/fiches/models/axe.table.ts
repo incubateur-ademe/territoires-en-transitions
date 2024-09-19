6de48d3808cb5c762f0b217abc6cb722c7d126d4
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2024-09-19 12:23:40 +0200

    feat(backend/fiches): ajout d'une route de synthèse

---
+++ backend/src/fiches/models/axe.table.ts
@@ -0,0 +1,32 @@
+import { InferInsertModel } from 'drizzle-orm';
+import {
+  AnyPgColumn,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
+import { collectiviteTable } from '../../collectivites/models/collectivite.models';
+import { planActionTypeTable } from './plan-action-type.table';
+
+export const axeTable = pgTable('axe', {
+  id: serial('id').primaryKey(),
+  nom: text('nom'),
+  collectivite_id: integer('collectivite_id')
+    .notNull()
+    .references(() => collectiviteTable.id),
+  parent: integer('parent').references((): AnyPgColumn => axeTable.id),
+  plan: integer('plan').references((): AnyPgColumn => axeTable.id),
+  type: integer('type').references(() => planActionTypeTable.id),
+  created_at: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  modified_at: timestamp('modified_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  modified_by: uuid('modified_by'), // TODO references auth.uid
+  panier_id: integer('panier_id'), // TODO references panier
+});
+export type CreateAxeType = InferInsertModel<typeof axeTable>;
