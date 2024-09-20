001ca5513e8a7062cf250b47173e31629c7696b4
Amandine <contact@jacquelinamandine.fr>
2024-09-20 11:02:23 +0200

    Backend- Sépare les tables liées aux collectivités dans des fichiers différents

---
+++ backend/src/collectivites/models/epci.table.ts
@@ -0,0 +1,38 @@
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  varchar,
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { collectiviteTable } from './collectivite.table';
+
+export const epciNatureEnum = pgEnum('nature', [
+  'SMF',
+  'CU',
+  'CC',
+  'SIVOM',
+  'POLEM',
+  'METRO',
+  'SMO',
+  'CA',
+  'EPT',
+  'SIVU',
+  'PETR',
+]);
+
+export const epciTable = pgTable('epci', {
+  id: serial('id').primaryKey(),
+  collectiviteId: integer('collectivite_id')
+    .notNull()
+    .references(() => collectiviteTable.id),
+  nom: varchar('nom', { length: 300 }).notNull(),
+  siren: varchar('siren', { length: 9 }).unique().notNull(),
+  nature: epciNatureEnum('nature').notNull(),
+});
+export const epciSchema = createSelectSchema(epciTable);
+export const createEpciSchema = createInsertSchema(epciTable);
+export type EpciType = InferSelectModel<typeof epciTable>;
+export type CreateEpciType = InferInsertModel<typeof epciTable>;
