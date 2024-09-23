d15bea8c4881c27274f626d081fa11e9d078d5a8
Marc Rutkowski <marc@attractive-media.fr>
2024-09-23 08:34:42 +0200

    Utilise ECharts pour les graphes de la trajectoire

--- app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/graphes/GrapheSousSecteurs.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Trajectoire/graphes/GrapheSousSecteurs.tsx
@@ -1,15 +1,12 @@
-import {useState} from 'react';
-import {Button} from '@tet/ui';
-import Chart from 'ui/charts/Chart';
-import {LineData} from 'ui/charts/Line/LineChart';
-import {AreaSymbol} from 'ui/charts/ChartLegend';
-import {genInfobulleParAnnee} from './InfobulleParAnnee';
-import {COMMON_CHART_PROPS, COULEURS_SECTEUR} from './constants';
+import {COULEURS_SECTEUR} from './constants';
+import {ReactECharts} from './ReactECharts';
+import {makeOption, makeStackedSeries} from './utils';
+import {DatasetComponentOption} from 'echarts';
 
 export type GrapheSousSecteursProps = {
   titre: string;
   unite: string;
-  sousSecteurs: LineData[];
+  sousSecteurs: DatasetComponentOption[];
 };
 
 /**
@@ -22,57 +19,23 @@ export const GrapheSousSecteurs = ({
   unite,
   sousSecteurs,
 }: GrapheSousSecteursProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   // affecte d'abord les couleurs avant de filtrer les dataset vides
   const sousSecteursNonVides = sousSecteurs
     .map((s, i) => ({
       ...s,
+      dimensions: ['x', 'y'],
       color: COULEURS_SECTEUR[i % COULEURS_SECTEUR.length],
     }))
-    .filter(s => !!s.data?.length);
+    .filter(s => !!s.source?.length);
 
-  return (
-    <>
-      <div className="flex justify-between">
-        <h3 className="ml-2">{titre}</h3>
-        <Button
-          className="h-fit"
-          icon="download-fill"
-          title="Télécharger"
-          size="xs"
-          variant="outlined"
-          onClick={() => setIsOpen(true)}
-        />
-      </div>
-      <Chart
-        infos={{
-          title: titre,
-          fileName: titre,
-          modal: {isOpen, setIsOpen, size: 'xl'},
-        }}
-        line={{
-          chart: {
-            ...COMMON_CHART_PROPS,
-            axisLeftLegend: unite,
-            data: sousSecteursNonVides,
-            legend: {
-              isOpen: true,
-              className: 'text-primary-8 font-medium',
-              size: 'sm',
-              items: sousSecteursNonVides.map(({label, color}, i) => ({
-                name: label!,
-                color,
-                symbole: AreaSymbol,
-              })),
-            },
-            sliceTooltip: genInfobulleParAnnee({
-              objectifsEtResultats: [],
-              secteurs: sousSecteursNonVides,
-            }),
-          },
-        }}
-      />
-    </>
-  );
+  const option = makeOption({
+    option: {
+      dataset: sousSecteursNonVides,
+      series: makeStackedSeries(sousSecteursNonVides),
+    },
+    titre,
+    unite,
+  });
+
+  return <ReactECharts option={option} />;
 };
