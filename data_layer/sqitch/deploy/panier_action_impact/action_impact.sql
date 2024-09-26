d4b3c6cacf68c4354f6b5fd4a72e7b3142d1d7b9
Amandine <contact@jacquelinamandine.fr>
2024-09-26 11:33:32 +0200

    - Complète la fonction matches_competences pour prendre en compte les booléens independamment_competence et competences_communales, ainsi qu'inclure collectivite_preset. - Ajoute une relation calculée entre action_impact_state et action_definition

--- data_layer/sqitch/deploy/panier_action_impact/action_impact.sql
+++ data_layer/sqitch/deploy/panier_action_impact/action_impact.sql
@@ -39,7 +39,8 @@ create table action_impact_state
 (
     action     action_impact,
     statut     action_impact_statut,
-    isinpanier boolean
+    isinpanier boolean,
+    panier panier
 );
 comment on table action_impact_state is
     'L''état d''une action par rapport à un panier. On ne se sert pas de cette table pour stocker des données.';
@@ -50,10 +51,11 @@ create function action_impact_state(panier) returns SETOF action_impact_state
 BEGIN ATOMIC
 SELECT a.*::action_impact AS action,
        ais.*::action_impact_statut AS statut,
-       (aip.* IS NOT NULL) AS isinpanier
+       (aip.* IS NOT NULL) AS isinpanier,
+       ($1) as panier
 FROM action_impact a
-    LEFT JOIN action_impact_panier aip ON aip.action_id = a.id AND aip.panier_id = ($1).id
-    LEFT JOIN action_impact_statut ais ON ais.action_id = a.id AND ais.panier_id = ($1).id;
+LEFT JOIN action_impact_panier aip ON aip.action_id = a.id AND aip.panier_id = ($1).id
+LEFT JOIN action_impact_statut ais ON ais.action_id = a.id AND ais.panier_id = ($1).id;
 END;
 comment on function action_impact_state(panier) is 'La liste des actions et de leurs états pour un panier.';
 
@@ -85,11 +87,44 @@ create function matches_competences(action_impact_state) returns boolean
     stable
     language sql
 BEGIN ATOMIC
-SELECT (( SELECT (NOT (EXISTS ( SELECT 1
-                                FROM action_impact_banatic_competence c
-                                WHERE (c.action_impact_id = ($1).action.id))))) OR ( SELECT action_impact_matches_competences(( SELECT panier.collectivite_id
-                                                                                                                                FROM panier
-                                                                                                                                WHERE (panier.id = ($1).statut.panier_id)), ($1).action.id) AS action_impact_matches_competences));
+with collectivite_panier as (
+                            SELECT coalesce(($1).panier.collectivite_id, ($1).panier.collectivite_preset) as collectivite_id
+                            )
+SELECT case
+           when e is not null then
+               not (($1).action.competences_communales) and
+               (
+                   (
+                   SELECT (
+                              NOT (
+                                  EXISTS (
+                                         SELECT 1
+                                         FROM action_impact_banatic_competence c
+                                         WHERE (c.action_impact_id = ($1).action.id)
+                                         )
+                                  )
+                              )
+                   )
+                       OR
+                   (
+                   SELECT action_impact_matches_competences(
+                                  (
+                                  SELECT collectivite_id
+                                  FROM collectivite_panier
+                                  LIMIT 1
+                                  ),
+                                  ($1).action.id
+                          ) AS action_impact_matches_competences
+                   )
+                   )
+           when c is not null then
+               ($1).action.competences_communales or ($1).action.independamment_competences
+           else
+               true
+       end as result
+FROM collectivite_panier cp
+LEFT JOIN epci e on cp.collectivite_id = e.collectivite_id
+LEFT JOIN commune c on cp.collectivite_id = c.collectivite_id;
 END;
 
 create function thematique(action_impact_state) returns SETOF thematique
@@ -104,5 +139,19 @@ FROM (thematique t
 WHERE (ait.action_impact_id = ($1).action.id);
 END;
 comment on function thematique(action_impact_state) is 'La relation entre le state d''une action et ses thématiques.';
+
+create function action_definition(action_impact_state) returns SETOF action_definition
+    stable
+    rows 1
+    language sql
+    security definer
+BEGIN ATOMIC
+SELECT ad.*
+FROM action_definition ad
+JOIN action_impact_action aia on ad.action_id = aia.action_id
+WHERE (aia.action_impact_id = ($1).action.id);
+END;
+comment on function action_definition(action_impact_state) is 'La relation entre le state d''une action et ses actions du référentiel.';
+
 
 COMMIT;
