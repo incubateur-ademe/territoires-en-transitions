c71e0398a4800a9d2aace979963734fe8f709d8b
Fred <98240+farnoux@users.noreply.github.com>
2024-09-30 11:51:35 +0200

    fix(plan-actions): Fixe une erreur de typage

--- app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/PlansActionCard.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Accueil/PlansActionCard.tsx
@@ -1,24 +1,24 @@
-import {useHistory} from 'react-router-dom';
 import classNames from 'classnames';
+import { useHistory } from 'react-router-dom';
 
+import { Axe } from '@tet/api/plan-actions/domain';
+import { statutToColor } from 'app/pages/collectivite/PlansActions/Synthese/utils';
 import {
   makeCollectivitePlanActionUrl,
   makeCollectivitePlansActionsNouveauUrl,
   makeCollectivitePlansActionsSyntheseUrl,
   makeCollectivitePlansActionsSyntheseVueUrl,
 } from 'app/paths';
-import {TAxeRow} from 'types/alias';
+import { useCurrentCollectivite } from 'core-logic/hooks/useCurrentCollectivite';
+import { useFonctionTracker } from 'core-logic/hooks/useFonctionTracker';
 import ButtonWithLink from 'ui/buttons/ButtonWithLink';
-import {PictoPlansAction} from 'ui/pictogrammes/PictoPlansAction';
-import {usePlansActionsListe} from '../PlansActions/PlanAction/data/usePlansActionsListe';
-import {usePlanActionTableauDeBord} from '../PlansActions/Synthese/data/usePlanActionTableauDeBord';
+import Chart from 'ui/charts/Chart';
+import { PictoPlansAction } from 'ui/pictogrammes/PictoPlansAction';
+import KeyNumbers from 'ui/score/KeyNumbers';
+import { usePlansActionsListe } from '../PlansActions/PlanAction/data/usePlansActionsListe';
+import { usePlanActionTableauDeBord } from '../PlansActions/Synthese/data/usePlanActionTableauDeBord';
 import AccueilCard from './AccueilCard';
 import AccueilEmptyCardWithPicto from './AccueilEmptyCardWithPicto';
-import KeyNumbers from 'ui/score/KeyNumbers';
-import {useFonctionTracker} from 'core-logic/hooks/useFonctionTracker';
-import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
-import {statutToColor} from 'app/pages/collectivite/PlansActions/Synthese/utils';
-import Chart from 'ui/charts/Chart';
 
 type PlanActionCardProps = {
   collectiviteId: number;
@@ -26,7 +26,7 @@ type PlanActionCardProps = {
 
 type FilledPlansActionCardProps = {
   collectiviteId: number;
-  plans: TAxeRow[];
+  plans: Axe[];
 };
 
 type EmptyPlansActionCardProps = {
@@ -64,7 +64,7 @@ const FilledPlansActionCard = ({
 }: FilledPlansActionCardProps): JSX.Element => {
   const tracker = useFonctionTracker();
   const history = useHistory();
-  const {data: planActionsStats} = usePlanActionTableauDeBord(
+  const { data: planActionsStats } = usePlanActionTableauDeBord(
     collectiviteId,
     null,
     null
@@ -100,14 +100,14 @@ const FilledPlansActionCard = ({
 
         {/* Call to action */}
         <ButtonWithLink
-          onClick={() => tracker({fonction: 'cta_plan_maj', action: 'clic'})}
+          onClick={() => tracker({ fonction: 'cta_plan_maj', action: 'clic' })}
           href={
             plans.length === 1
               ? makeCollectivitePlanActionUrl({
                   collectiviteId,
                   planActionUid: `${plans[0].id}`,
                 })
-              : makeCollectivitePlansActionsSyntheseUrl({collectiviteId})
+              : makeCollectivitePlansActionsSyntheseUrl({ collectiviteId })
           }
           rounded
         >
@@ -124,7 +124,7 @@ const FilledPlansActionCard = ({
               chart: {
                 data:
                   planActionsStats && planActionsStats.statuts
-                    ? planActionsStats.statuts.map(st => ({
+                    ? planActionsStats.statuts.map((st) => ({
                         ...st,
                         id: st.id !== 'NC' ? st.id : 'Sans statut',
                         color: statutToColor[st.id],
@@ -176,9 +176,9 @@ const EmptyPlansActionCard = ({
         {collectivite && !collectivite.readonly && (
           <ButtonWithLink
             onClick={() =>
-              tracker({fonction: 'cta_plan_creation', action: 'clic'})
+              tracker({ fonction: 'cta_plan_creation', action: 'clic' })
             }
-            href={makeCollectivitePlansActionsNouveauUrl({collectiviteId})}
+            href={makeCollectivitePlansActionsNouveauUrl({ collectiviteId })}
             rounded
           >
             Créer ou importer un plan d'action
