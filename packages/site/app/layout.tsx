417b340d087455831889c081f2a6090da0d6219f
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-01 14:53:37 +0200

    Ajout des scripts de tracking au site

--- packages/site/app/layout.tsx
+++ packages/site/app/layout.tsx
@@ -1,5 +1,9 @@
+import './global.css';
+import AppHeader from './AppHeader';
+import { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 import { GoogleTagManager } from '@next/third-parties/google';
-import AppHeader from '@tet/site/components/layout/AppHeader';
+import { getMetaData } from './utils';
 import Footer from '@tet/site/components/layout/Footer';
 import { PHProvider } from '@tet/site/providers/posthog';
 import { Metadata, Viewport } from 'next';
@@ -112,13 +116,16 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
             _linkedin_partner_id = "1701996";
             window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
             window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-
-            (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]}
-              var s = document.getElementsByTagName("script")[0];
-              var b = document.createElement("script");
-              b.type = "text/javascript";b.async = true;
-              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-              s.parentNode.insertBefore(b, s);})(window.lintrk);
+            </script><script type="text/javascript">
+            (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "
+            https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
           `}
         </Script>
 
@@ -131,22 +138,9 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
               divider: encodeURIComponent('|'),
               pagename: encodeURIComponent('2024-09-territoiresentransitions.fr-PageArrivee-LP')
             });
-            (function () { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
+            (function () { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();            
           `}
         </Script>
-
-        {/* crisp widget */}
-        <Script id="crisp" type="text/javascript">{`
-          window.$crisp = [];
-          window.CRISP_WEBSITE_ID = '${process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID}';
-          (function () {
-            d = document;
-            s = d.createElement('script');
-            s.src = 'https://client.crisp.chat/l.js';
-            s.async = 1;
-            d.getElementsByTagName('head')[0].appendChild(s);
-          })();
-        `}</Script>
       </html>
     </PHProvider>
   );
