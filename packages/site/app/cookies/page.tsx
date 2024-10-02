a9369ec71d3b0b9c1e918d05d878ccbb6d354ea3
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-02 17:48:20 +0200

    Mise à jour de la politique de gestion des cookies

--- packages/site/app/cookies/page.tsx
+++ packages/site/app/cookies/page.tsx
@@ -2,6 +2,7 @@
 
 import Section from '@tet/site/components/sections/Section';
 import { Metadata } from 'next';
+import Cookies from './cookies.mdx';
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -9,17 +10,11 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default async function Cookies() {
+export default async function Page() {
   return (
     <Section>
       <h1 className="fr-header__body">Gestion des cookies</h1>
-      <p>Ce site n&apos;utilise pas de cookie.</p>
-      <p>
-        Nous sommes ainsi en conformité avec la réglementation « Cookies » de la
-        CNIL et exemptés d’autorisation préalable. C’est pour cela que vous
-        n’avez pas eu besoin de cliquer sur un bloc pour accepter le dépôt de
-        cookies !
-      </p>
+      <Cookies />
     </Section>
   );
 }
