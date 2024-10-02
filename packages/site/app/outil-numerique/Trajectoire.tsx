d63f9c6d31b3e154edd73f9d8e6b2a55b476b2f4
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-02 13:24:38 +0200

    Modifie le chemin de la page trajectoire

--- packages/site/app/outil-numerique/Trajectoire.tsx
+++ packages/site/app/outil-numerique/Trajectoire.tsx
@@ -25,10 +25,7 @@ const Trajectoire = ({ titre, description, cta, image }: TrajectoireProps) => {
           texte={description}
           className="paragraphe-primary-10 paragraphe-18 markdown_style colored_marker"
         />
-        <Button
-          href="/outil-numerique/trajectoire"
-          className="mt-6 max-lg:mx-auto"
-        >
+        <Button href="/trajectoire" className="mt-6 max-lg:mx-auto">
           {cta}
         </Button>
       </div>
