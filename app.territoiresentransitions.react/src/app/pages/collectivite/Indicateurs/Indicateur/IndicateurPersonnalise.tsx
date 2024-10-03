8e6241b3f3044b852fe3bc3dbae2bb56ccec0199
elisfainstein <elisabeth.fainstein.freelance@gmail.com>
2024-10-03 19:35:08 +0200

    feature: add a temp function to handle double update from description input

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/IndicateurPersonnalise.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/IndicateurPersonnalise.tsx
@@ -1,20 +1,20 @@
-import {useState} from 'react';
-import {Field, Modal, ModalFooterOKCancel} from '@tet/ui';
-import {BadgeACompleter} from 'ui/shared/Badge/BadgeACompleter';
+import { useState } from 'react';
+import { Field, Modal, ModalFooterOKCancel } from '@tet/ui';
+import { BadgeACompleter } from 'ui/shared/Badge/BadgeACompleter';
 import TextareaControlled from 'ui/shared/form/TextareaControlled';
 import InputControlled from 'ui/shared/form/InputControlled';
 import ScrollTopButton from 'ui/buttons/ScrollTopButton';
-import {ToolbarIconButton} from 'ui/buttons/ToolbarIconButton';
-import {useUpdateIndicateurDefinition} from './useUpdateIndicateurDefinition';
-import {HeaderIndicateur} from './detail/HeaderIndicateur';
-import {IndicateurValuesTabs} from './detail/IndicateurValuesTabs';
-import {FichesActionLiees} from './FichesActionLiees';
-import {useCurrentCollectivite} from 'core-logic/hooks/useCurrentCollectivite';
-import {IndicateurInfoLiees} from './detail/IndicateurInfoLiees';
-import {useIndicateurDefinition} from './useIndicateurDefinition';
+import { ToolbarIconButton } from 'ui/buttons/ToolbarIconButton';
+import { useUpdateIndicateurDefinition } from './useUpdateIndicateurDefinition';
+import { HeaderIndicateur } from './detail/HeaderIndicateur';
+import { IndicateurValuesTabs } from './detail/IndicateurValuesTabs';
+import { FichesActionLiees } from './FichesActionLiees';
+import { useCurrentCollectivite } from 'core-logic/hooks/useCurrentCollectivite';
+import { IndicateurInfoLiees } from './detail/IndicateurInfoLiees';
+import { useIndicateurDefinition } from './useIndicateurDefinition';
 import IndicateurDetailChart from 'app/pages/collectivite/Indicateurs/Indicateur/detail/IndicateurDetailChart';
-import {useDeleteIndicateurPerso} from './useRemoveIndicateurPerso';
-import {Indicateurs} from '@tet/api';
+import { useDeleteIndicateurPerso } from './useRemoveIndicateurPerso';
+import { Indicateurs } from '@tet/api';
 import BadgeIndicateurPerso from 'app/pages/collectivite/Indicateurs/components/BadgeIndicateurPerso';
 
 /** Affiche le détail d'un indicateur personnalisé */
@@ -23,8 +23,8 @@ const IndicateurPersonnaliseBase = ({
 }: {
   definition: Indicateurs.domain.IndicateurDefinition;
 }) => {
-  const {description, unite, titre, rempli} = definition;
-  const {mutate: updateDefinition} = useUpdateIndicateurDefinition();
+  const { description, unite, titre, rempli } = definition;
+  const { mutate: updateDefinition } = useUpdateIndicateurDefinition();
   const collectivite = useCurrentCollectivite();
   const isReadonly = !collectivite || collectivite?.readonly;
 
@@ -36,20 +36,31 @@ const IndicateurPersonnaliseBase = ({
     const collectivite_id = collectivite?.collectivite_id;
     const nouveau = value?.trim();
     if (collectivite_id && nouveau !== definition[name]) {
-      /**
-       * TEMPORARY: updating hardcoded commentaire prop, in order to link the description input
-       * to the `commentaire` column in `indicateur_collectivite` 
-       * -> step 2 of expand and contract pattern (
-       * https://www.prisma.io/dataguide/types/relational/expand-and-contract-pattern).
-       * Next step: remove hardcoded commentaire prop and change 
-       * handleUpdate('description', e.target.value) to handleUpdate('commentaire', e.target.value).
-       */
-      updateDefinition({...definition, [name]: nouveau, ['commentaire']: nouveau});
+      updateDefinition({ ...definition, [name]: nouveau });
     }
   };
 
+  /**
+   * TEMPORARY: currently, description input feeds two columns:
+   * `description` column in `indicateur_definition`
+   * `commentaire` column in `indicateur_collectivite` (via the harcoded ['commentaire'] prop).
+   * This is step 2 of expand and contract pattern
+   * (https://www.prisma.io/dataguide/types/relational/expand-and-contract-pattern).
+   *
+   * Next step: remove this function and change
+   * handleUpdate('description', e.target.value) to handleUpdate('commentaire', e.target.value).
+   */
+  const TEMPORARY_handleDescriptionUpdate = (value: string) => {
+    const trimmedValue = value.trim();
+    updateDefinition({
+      ...definition,
+      ['description']: trimmedValue,
+      ['commentaire']: trimmedValue,
+    });
+  };
+
   const [showConfirm, setShowConfirm] = useState(false);
-  const {mutate: deleteIndicateurPerso} = useDeleteIndicateurPerso(
+  const { mutate: deleteIndicateurPerso } = useDeleteIndicateurPerso(
     collectivite?.collectivite_id as number,
     definition.id
   );
@@ -59,7 +70,7 @@ const IndicateurPersonnaliseBase = ({
       <HeaderIndicateur
         title={titre}
         isReadonly={isReadonly}
-        onUpdate={value => handleUpdate('titre', value)}
+        onUpdate={(value) => handleUpdate('titre', value)}
       />
       <div className="px-10 py-6">
         <div className="flex flex-row items-center justify-between mb-6">
@@ -95,7 +106,7 @@ const IndicateurPersonnaliseBase = ({
               initialValue={description}
               readOnly={isReadonly}
               disabled={isReadonly}
-              onBlur={e => handleUpdate('description', e.target.value)}
+              onBlur={(e) => TEMPORARY_handleDescriptionUpdate(e.target.value)}
             />
           </Field>
           <IndicateurInfoLiees definition={definition} />
@@ -105,7 +116,7 @@ const IndicateurPersonnaliseBase = ({
               className="fr-input fr-mt-1w !outline-none"
               initialValue={unite}
               readOnly={isReadonly}
-              onBlur={e => handleUpdate('unite', e.target.value)}
+              onBlur={(e) => handleUpdate('unite', e.target.value)}
             />
           </Field>
         </div>
@@ -113,10 +124,10 @@ const IndicateurPersonnaliseBase = ({
       </div>
       {showConfirm && (
         <Modal
-          openState={{isOpen: showConfirm, setIsOpen: setShowConfirm}}
+          openState={{ isOpen: showConfirm, setIsOpen: setShowConfirm }}
           title={`Suppression indicateur "${definition.titre}"`}
           description="Êtes-vous sûr de vouloir supprimer cet indicateur personnalisé ? Vous perdrez définitivement les données associées à cet indicateur."
-          renderFooter={({close}) => (
+          renderFooter={({ close }) => (
             <ModalFooterOKCancel
               btnCancelProps={{
                 onClick: () => close(),
