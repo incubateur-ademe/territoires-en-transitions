c251bc2beeb1ed660bd9f79db66446f02a0dbf7c
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-04 17:19:55 +0200

    Corrige une grid du site sous safari

--- packages/site/app/Communaute.tsx
+++ packages/site/app/Communaute.tsx
@@ -25,30 +25,33 @@ const Communaute = ({ titre, cta }: CommunauteProps) => {
     <Section containerClassName="max-md:!py-6 md:max-lg:!py-12 lg:!py-18">
       <h3 className="text-primary-9 text-center">{titre}</h3>
 
-      <div className="grid grid-cols-3 max-md:grid-cols-1">
-        <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:px-8 max-md:pb-8 md:px-16 max-md:border-b md:border-r border-primary-5">
-          <span className="text-primary-8 text-7xl font-extrabold">
-            {engages}
-          </span>
-          <span className="text-primary-10 text-lg text-center">
-            Territoires Engagés Transition Écologique
-          </span>
-        </div>
-        <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:p-8 md:px-16 max-md:border-b md:border-r border-primary-5">
-          <span className="text-primary-8 text-7xl font-extrabold">
-            {labellises}
-          </span>
-          <span className="text-primary-10 text-lg text-center">
-            Collectivités labellisées
-          </span>
-        </div>
-        <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:px-8 max-md:pt-8 md:px-16">
-          <span className="text-primary-8 text-7xl font-extrabold">
-            {utilisateursActifs}
-          </span>
-          <span className="text-primary-10 text-lg text-center">
-            Utilisateurs de l’outil numérique
-          </span>
+      {/* besoin de cette div car `grid` semble rentrer en conflit avec le container `flex` sur Safari */}
+      <div>
+        <div className="grid grid-cols-3 max-md:grid-cols-1">
+          <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:px-8 max-md:pb-8 md:px-16 max-md:border-b md:border-r border-primary-5">
+            <span className="text-primary-8 text-7xl font-extrabold">
+              {engages}
+            </span>
+            <span className="text-primary-10 text-lg text-center">
+              Territoires Engagés Transition Écologique
+            </span>
+          </div>
+          <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:p-8 md:px-16 max-md:border-b md:border-r border-primary-5">
+            <span className="text-primary-8 text-7xl font-extrabold">
+              {labellises}
+            </span>
+            <span className="text-primary-10 text-lg text-center">
+              Collectivités labellisées
+            </span>
+          </div>
+          <div className="h-full w-full flex flex-col justify-center items-center gap-2 max-md:px-8 max-md:pt-8 md:px-16">
+            <span className="text-primary-8 text-7xl font-extrabold">
+              {utilisateursActifs}
+            </span>
+            <span className="text-primary-10 text-lg text-center">
+              Utilisateurs de l’outil numérique
+            </span>
+          </div>
         </div>
       </div>
 
