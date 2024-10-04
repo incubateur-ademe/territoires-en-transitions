e6b3b16b5bb45d9f1863cfbfb040131812ae873e
Fred <98240+farnoux@users.noreply.github.com>
2024-10-04 18:31:50 +0200

    build: Fixe vitest configuration

--- packages/api/vite.config.ts
+++ packages/api/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
@@ -20,6 +20,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    env: loadEnv('', process.cwd(), ''),
 
     reporters: ['default'],
     coverage: {
