b21fe32f16afcbb351488535ca621ef62110f1f4
Arthur Molinos <arthur.molinos@gmail.com>
2024-10-07 16:28:56 +0200

    fix : transforme un objet de snake à camel case à la réception des data pour les personnes

--- app.territoiresentransitions.react/src/ui/dropdownLists/PersonnesDropdown/usePersonneListe.ts
+++ app.territoiresentransitions.react/src/ui/dropdownLists/PersonnesDropdown/usePersonneListe.ts
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query';
 import { Personne } from '@tet/api/shared/domain';
 import { supabaseClient } from 'core-logic/api/supabase';
 import { useCollectiviteId } from 'core-logic/hooks/params';
+import { objectToCamel } from 'ts-case-convert';
 
 type TFetchedData = Personne[];
 
@@ -18,7 +19,7 @@ const fetchPersonneListe = async (
     throw new Error(error.message);
   }
 
-  return data as unknown as Personne[];
+  return objectToCamel(data) as unknown as Personne[];
 };
 
 export const usePersonneListe = () => {
