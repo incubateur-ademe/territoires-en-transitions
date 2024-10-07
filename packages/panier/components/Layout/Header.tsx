45abcb7c89fd38dd314ee8099fa4bb03630dfdc6
Marc Rutkowski <marc@attractive-media.fr>
2024-10-07 16:34:45 +0200

    Affiche dans l'en-tête le nom de la collectivité associée au panier

--- packages/panier/components/Layout/Header.tsx
+++ packages/panier/components/Layout/Header.tsx
@@ -2,24 +2,47 @@
 
 import useLandingPathname from '@tet/panier/hooks/useLandingPathname';
 import { Button, HeaderTeT, SITE_BASE_URL } from '@tet/ui';
+import { getAppBaseUrl } from '@tet/api';
+import { useCollectiviteInfo } from '@tet/panier/components/Landing/useCollectiviteInfo';
+import { usePanierContext } from '@tet/panier/providers';
 
 const Header = () => {
   const landingPathname = useLandingPathname();
+  const { panier } = usePanierContext();
+  const { data: collectiviteInfo } = useCollectiviteInfo(
+    panier?.collectivite_id ?? panier?.collectivite_preset ?? null
+  );
 
   return (
     <HeaderTeT
       customRootUrl={landingPathname}
-      quickAccessButtons={(props) => [
-        <Button
-          {...props}
-          key="outil"
-          href={`${SITE_BASE_URL}/outil-numerique`}
-          iconPosition="left"
-          external
-        >
-          Qui sommes-nous ?
-        </Button>,
-      ]}
+      quickAccessButtons={(props) => {
+        const buttons = [
+          <Button
+            {...props}
+            key="outil"
+            href={`${SITE_BASE_URL}/outil-numerique`}
+            iconPosition="left"
+            external
+          >
+            Qui sommes-nous ?
+          </Button>,
+        ];
+        if (collectiviteInfo) {
+          buttons.push(
+            <Button
+              {...props}
+              key="nom"
+              href={`${getAppBaseUrl(
+                document.location.hostname
+              )}/collectivite/${collectiviteInfo.collectivite_id}`}
+            >
+              {collectiviteInfo?.nom}
+            </Button>
+          );
+        }
+        return buttons;
+      }}
     />
   );
 };
