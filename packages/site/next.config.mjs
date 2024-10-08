d7eda0e37b33f8c1c7597e54bbf9b98971382886
Fred <98240+farnoux@users.noreply.github.com>
2024-10-08 09:22:34 +0200

    build(site): Nouvelle image Docker mieux adaptée à la poduction (#3330)

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
     svgr: false,
   },
 
+  // Useful for self-hosting in a Docker container
+  // See https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
+  output: 'standalone',
+
   // active le mode strict pour détecter le problèmes en dev
   reactStrictMode: true,
   // active la minification
@@ -23,7 +27,6 @@ const nextConfig = {
     esmExternals: 'loose',
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: '**.strapiapp.com' },
       { protocol: 'http', hostname: '127.0.0.1' },
