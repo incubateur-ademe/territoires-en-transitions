075fa07b7aab4b5c92f4190ab891110a0e48b260
amandinejacquelin <115977469+amandinejacquelin@users.noreply.github.com>
2024-10-10 11:42:00 +0200

    Corrige la mise à jour des stats (#3329)
    Divise la fonction refresh_stats_locales en deux fonctions, car elle dépassait 2min et timeout.

--- data_layer/sqitch/verify/stats/locales.sql
+++ data_layer/sqitch/verify/stats/locales.sql
@@ -2,32 +2,7 @@
 
 BEGIN;
 
-SELECT mois,
-       code_region,
-       code_departement,
-       collectivites
-FROM stats.locales_evolution_collectivite_avec_indicateur_referentiel
-where false;
-
-SELECT mois,
-       code_region,
-       code_departement,
-       indicateurs
-FROM stats.locales_evolution_resultat_indicateur_referentiel
-where false;
-
-SELECT mois,
-       code_region,
-       code_departement,
-       collectivites
-FROM stats_locales_evolution_collectivite_avec_indicateur
-where false;
-
-SELECT mois,
-       code_region,
-       code_departement,
-       indicateurs
-FROM stats_locales_evolution_resultat_indicateur_referentiel
-where false;
+select has_function_privilege('stats.refresh_stats_locales_indicateurs()', 'execute');
+select has_function_privilege('stats.refresh_stats_locales()', 'execute');
 
 ROLLBACK;
