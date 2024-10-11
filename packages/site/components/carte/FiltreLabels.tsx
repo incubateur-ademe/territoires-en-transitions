82ff550501ddb706f212c3daf422de2c2814de1e
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-11 14:55:22 +0200

    Ajout du radio button au DS et utilisation sur la page collectivités

--- packages/site/components/carte/FiltreLabels.tsx
+++ packages/site/components/carte/FiltreLabels.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { RadioButton } from '@tet/ui';
 import { FiltresLabels } from './CarteCollectivites';
 
 type FiltreLabelsProps = {
@@ -13,81 +14,50 @@ const FiltreLabels = ({
 }: FiltreLabelsProps) => {
   return (
     <div className="flex flex-col gap-4 w-fit">
-      <div className="flex flex-row gap-2">
-        <input
-          type="radio"
-          id="toutes"
-          value="toutes"
-          name="filtre"
-          checked={selectedFilter === 'toutes'}
-          onChange={() => onChangeFilter('toutes')}
-        />
-        <label htmlFor="toutes" className="text-base text-grey-8 font-medium">
-          Toutes les collectivités
-        </label>
-      </div>
+      <RadioButton
+        label="Toutes les collectivités"
+        id="toutes"
+        value="toutes"
+        name="filtre"
+        checked={selectedFilter === 'toutes'}
+        onChange={() => onChangeFilter('toutes')}
+      />
 
-      <div className="flex flex-row gap-2">
-        <input
-          type="radio"
-          id="labellisees_cae"
-          value="labellisees_cae"
-          name="filtre"
-          checked={selectedFilter === 'labellisees_cae'}
-          onChange={() => onChangeFilter('labellisees_cae')}
-        />
-        <label
-          htmlFor="labellisees_cae"
-          className="text-base text-grey-8 font-medium"
-        >
-          Labellisées Climat-Air-Énergie
-        </label>
-      </div>
+      <RadioButton
+        label="Labellisées Climat-Air-Énergie"
+        id="labellisees_cae"
+        value="labellisees_cae"
+        name="filtre"
+        checked={selectedFilter === 'labellisees_cae'}
+        onChange={() => onChangeFilter('labellisees_cae')}
+      />
 
-      <div className="flex flex-row gap-2">
-        <input
-          type="radio"
-          id="labellisees_eci"
-          value="labellisees_eci"
-          name="filtre"
-          checked={selectedFilter === 'labellisees_eci'}
-          onChange={() => onChangeFilter('labellisees_eci')}
-        />
-        <label
-          htmlFor="labellisees_eci"
-          className="text-base text-grey-8 font-medium"
-        >
-          Labellisées Économie Circulaire
-        </label>
-      </div>
+      <RadioButton
+        label="Labellisées Économie Circulaire"
+        id="labellisees_eci"
+        value="labellisees_eci"
+        name="filtre"
+        checked={selectedFilter === 'labellisees_eci'}
+        onChange={() => onChangeFilter('labellisees_eci')}
+      />
 
-      <div className="flex flex-row gap-2">
-        <input
-          type="radio"
-          id="cot"
-          value="cot"
-          name="filtre"
-          checked={selectedFilter === 'cot'}
-          onChange={() => onChangeFilter('cot')}
-        />
-        <label htmlFor="cot" className="text-base text-grey-8 font-medium">
-          Collectivités engagées non labellisées
-        </label>
-      </div>
+      <RadioButton
+        label="Collectivités engagées non labellisées"
+        id="cot"
+        value="cot"
+        name="filtre"
+        checked={selectedFilter === 'cot'}
+        onChange={() => onChangeFilter('cot')}
+      />
 
-      <div className="flex flex-row gap-2">
-        <input
-          type="radio"
-          id="actives"
-          value="actives"
-          name="filtre"
-          checked={selectedFilter === 'actives'}
-          onChange={() => onChangeFilter('actives')}
-        />
-        <label htmlFor="actives" className="text-base text-grey-8 font-medium">
-          Collectivités utilisatrices de la plateforme
-        </label>
-      </div>
+      <RadioButton
+        label="Collectivités utilisatrices de la plateforme"
+        id="actives"
+        value="actives"
+        name="filtre"
+        checked={selectedFilter === 'actives'}
+        onChange={() => onChangeFilter('actives')}
+      />
     </div>
   );
 };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -31,6 +31,7 @@ export * from './design-system/Input';
 export * from './design-system/Modal';
 export * from './design-system/Notification';
 export * from './design-system/Pagination';
+export * from './design-system/RadioButton';
 export * from './design-system/Select';
 export * from './design-system/Tabs';
 export * from './design-system/Tag';
