edd53b5c68aaadae1e8e34ef63cff176c1845c25
elisfainstein <elisabeth.fainstein.freelance@gmail.com>
2024-10-14 20:11:10 +0200

    refactor: change wording

--- app.territoiresentransitions.react/src/ui/labellisation/LabellisationInfo.tsx
+++ app.territoiresentransitions.react/src/ui/labellisation/LabellisationInfo.tsx
@@ -1,5 +1,5 @@
 import ScoreDisplay from '../referentiels/ScoreDisplay';
-import {TLabellisationParcours} from '../../app/pages/collectivite/ParcoursLabellisation/types';
+import { TLabellisationParcours } from '../../app/pages/collectivite/ParcoursLabellisation/types';
 import LabellisationStars from './LabellisationStars';
 
 type LabellisationInfoProps = {
@@ -18,21 +18,23 @@ const LabellisationInfo = ({
   return (
     <div className="flex flex-col gap-3 md:items-start items-center mb-4">
       <LabellisationStars parcours={parcours} />
-      <ScoreDisplay
-        score={score.realises / score.max_personnalise}
-        percent
-        legend="Score réalisé courant"
-        size="sm"
-        bold="value"
-      />
-      <ScoreDisplay
-        score={score.programmes / score.max_personnalise}
-        percent
-        icon="fr-icon-calendar-line"
-        legend="Score programmé"
-        size="sm"
-        bold="value"
-      />
+      <div className="flex flex-row gap-4">
+        <ScoreDisplay
+          score={score.realises / score.max_personnalise}
+          percent
+          legend="Score réalisé"
+          size="sm"
+          bold="value"
+        />
+        <ScoreDisplay
+          score={score.programmes / score.max_personnalise}
+          percent
+          icon="fr-icon-calendar-line"
+          legend="Score programmé"
+          size="sm"
+          bold="value"
+        />
+      </div>
     </div>
   );
 };
