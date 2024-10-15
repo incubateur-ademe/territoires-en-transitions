d5aeb0f02f9ee25adf67cede9d5283f72ab84013
Marc Rutkowski <marc@attractive-media.fr>
2024-10-15 19:13:05 +0200

    Distingue les fichiers marqués "confidentiel" dans le sélecteur d'ajout d'un document depuis la bibliothèque

--- app.territoiresentransitions.react/src/ui/shared/preuves/AddPreuveModal/AddFromLib.tsx
+++ app.territoiresentransitions.react/src/ui/shared/preuves/AddPreuveModal/AddFromLib.tsx
@@ -1,8 +1,9 @@
 import {useState} from 'react';
-import {Button, Field, Option, SelectFilter} from '@tet/ui';
-import {TBibliothequeFichier} from '../Bibliotheque/types';
-import {useFichiers, TFilters} from '../Bibliotheque/useFichiers';
-import {TAddFileFromLib} from './AddFile';
+import { Button, Field, Icon, Option, SelectFilter } from '@tet/ui';
+import { TBibliothequeFichier } from '../Bibliotheque/types';
+import { useFichiers, TFilters } from '../Bibliotheque/useFichiers';
+import { TAddFileFromLib } from './AddFile';
+import classNames from 'classnames';
 
 export type TAddFromLibProps = {
   items: TBibliothequeFichier[];
@@ -11,26 +12,30 @@ export type TAddFromLibProps = {
   onClose: () => void;
 };
 
+type OptionConfidentiel = Option & { confidentiel: boolean };
+
 /**
  * Affiche la bibliothèque de fichiers de la collectivité
  * et permet de sélectionner les fichiers à ajouter comme preuve
  */
 export const AddFromLib = (props: TAddFromLibProps) => {
-  const {items: fichiers, onAddFileFromLib, onClose, setFilters} = props;
+  const { items: fichiers, onAddFileFromLib, onClose, setFilters } = props;
 
   const [selectedFiles, setSelectedFiles] = useState<Option[] | undefined>();
 
   const availableOptions = fichiers
-    .filter(f => !(selectedFiles ?? []).some(file => file.value === f.id))
-    .map(f => ({
+    .filter((f) => !(selectedFiles ?? []).some((file) => file.value === f.id))
+    .map((f) => ({
       label: f.filename,
       value: f.id,
+      confidentiel: f.confidentiel,
     }));
 
   const options = [...(selectedFiles ?? []), ...availableOptions];
+  const values = (selectedFiles ?? []).map((f) => f.value);
 
   const onSubmit = () => {
-    (selectedFiles ?? []).map(file => onAddFileFromLib(file.value as number));
+    (selectedFiles ?? []).map((file) => onAddFileFromLib(file.value as number));
     onClose();
   };
 
@@ -40,13 +45,29 @@ export const AddFromLib = (props: TAddFromLibProps) => {
         <SelectFilter
           debounce={500}
           options={options}
-          values={(selectedFiles ?? []).map(f => f.value)}
-          onSearch={search => setFilters({search, page: 1})}
-          onChange={({values}) => {
+          customItem={(option) => (
+            <>
+              <span
+                className={classNames('leading-6 text-grey-8', {
+                  'text-primary-7': values.includes(option.value),
+                })}
+              >
+                {(option as OptionConfidentiel).confidentiel && (
+                  <Icon icon="lock-fill" size="sm" className="mr-2" />
+                )}
+                {option.label}
+              </span>
+            </>
+          )}
+          values={values}
+          onSearch={(search) => setFilters({ search, page: 1 })}
+          onChange={({ values }) => {
             setSelectedFiles(
-              options.filter(opt => (values ?? []).some(v => v === opt.value))
+              options.filter((opt) =>
+                (values ?? []).some((v) => v === opt.value)
+              )
             );
-            setFilters({search: '', page: 1});
+            setFilters({ search: '', page: 1 });
           }}
           placeholder="Rechercher par nom"
           isSearcheable
