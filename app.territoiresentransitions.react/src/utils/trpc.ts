12f85d249a3fae4929a82c092535f22de1ed1528
Fred <98240+farnoux@users.noreply.github.com>
2024-10-21 18:17:35 +0200

    🚧 Adapt env variables

--- app.territoiresentransitions.react/src/utils/trpc.ts
+++ app.territoiresentransitions.react/src/utils/trpc.ts
@@ -1,10 +1,12 @@
-// By using `import type` you ensure that the reference will be stripped at compile-time, meaning you don't inadvertently import server-side code into your client.
-// For more information, see the Typescript docs: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export
 import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
-import type { AppRouter } from './../../../backend/src/trpc.router';
 import { getAuthHeaders } from 'core-logic/api/auth/AuthProvider';
 
-const BASE_URL = `${process.env.NX_PUBLIC_BACKEND_URL}/trpc`;
+// By using `import type` you ensure that the reference will be stripped at compile-time, meaning you don't inadvertently import server-side code into your client.
+// For more information, see the Typescript docs: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import type { AppRouter } from './../../../backend/src/trpc.router';
+
+const BASE_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`;
 
 export const trpc = createTRPCReact<AppRouter>();
 
