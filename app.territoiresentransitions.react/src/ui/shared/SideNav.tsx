8ca941e5b12e40c2a620e50d7f4f0990bbd634d2
Fred <98240+farnoux@users.noreply.github.com>
2024-10-21 20:01:25 +0200

    🚧 Migrate <NavLink> components

--- app.territoiresentransitions.react/src/ui/shared/SideNav.tsx
+++ app.territoiresentransitions.react/src/ui/shared/SideNav.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
-import {useState} from 'react';
-import {NavLink} from 'react-router-dom';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export type TSideNavLink = {
   link: string;
@@ -24,33 +24,20 @@ type Props = {
   links: SideNavLinks;
 };
 
-
-
-const SideNav = ({links}: Props) => {
+const SideNav = ({ links }: Props) => {
   return (
     <nav data-test="SideNav" className="fr-sidemenu flex !pr-4">
       <div className="fr-sidemenu-wrapper w-full">
         <ul className="fr-sidemenu__list">
-          {links.map(element => {
+          {links.map((element) => {
             if (isSideNavSection(element)) {
               return <Section key={element.link} section={element} />;
             } else {
               return (
                 <li key={element.link} className="fr-sidemenu__item">
-                  {element.link.startsWith('http') ? (
-                    <a className="fr-sidemenu__link" href={element.link}>
-                      {element.displayName}
-                    </a>
-                  ) : (
-                    <NavLink
-                      className="fr-sidemenu__link"
-                      to={element.link}
-                      target="_self"
-                      aria-current="page"
-                    >
-                      {element.displayName}
-                    </NavLink>
-                  )}
+                  <Link href={element.link} className="fr-sidemenu__link">
+                    {element.displayName}
+                  </Link>
                 </li>
               );
             }
@@ -67,18 +54,18 @@ type SectionProps = {
   section: TSideNavSection;
 };
 
-const Section = ({section}: SectionProps) => {
+const Section = ({ section }: SectionProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <li key={section.link} data-test="SideNav-section">
       <div className="flex items-start w-full">
         <div className="w-full fr-sidemenu__item">
-          <NavLink
+          <Link
             data-test="SideNav-section-toggle-button"
             title="Ouvrir la section"
             className="fr-sidemenu__link justify-between"
-            to={section.link}
+            href={section.link}
             onClick={() => setIsOpen(!isOpen)}
             target="_self"
             aria-current="page"
@@ -88,11 +75,11 @@ const Section = ({section}: SectionProps) => {
               <span
                 className={classNames(
                   'fr-fi-arrow-right-s-line before:scale-75 before:transition-transform',
-                  {'before:rotate-90': isOpen}
+                  { 'before:rotate-90': isOpen }
                 )}
               />
             </div>
-          </NavLink>
+          </Link>
         </div>
       </div>
       {isOpen && (
@@ -100,16 +87,16 @@ const Section = ({section}: SectionProps) => {
           data-test="SideNav-section-liens"
           className="mx-4 fr-sidemenu__list"
         >
-          {section.enfants.map(enfant => (
+          {section.enfants.map((enfant) => (
             <li key={enfant.link} className="fr-sidemenu__item">
-              <NavLink
+              <Link
                 className="fr-sidemenu__link"
-                to={enfant.link}
+                href={enfant.link}
                 target="_self"
                 aria-current="page"
               >
                 {enfant.displayName}
-              </NavLink>
+              </Link>
             </li>
           ))}
         </ul>
