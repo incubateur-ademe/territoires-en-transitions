9903f466f150dded8c3c3f19e586f25d9452dd89
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-10-24 16:24:36 +0200

    Ajout des sections sur le pdf de la FA

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionPdf/Acteurs.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionPdf/Acteurs.tsx
@@ -1,19 +1,17 @@
 import Card from 'ui/exportPdf/components/Card';
+import Stack from 'ui/exportPdf/components/Stack';
 import { twToCss } from 'ui/exportPdf/utils';
 import { FicheActionPdfProps } from './FicheActionPdf';
 
 type ListeActeursProps = {
   titre: string;
   liste: string[] | undefined;
-  className?: string;
 };
 
-const ListeActeurs = ({ titre, liste, className }: ListeActeursProps) => {
+const ListeActeurs = ({ titre, liste }: ListeActeursProps) => {
   return (
-    <div style={className ? twToCss(className) : {}}>
-      <h6 style={twToCss('text-xs text-primary-9 uppercase mt-0 mb-2')}>
-        {titre}
-      </h6>
+    <Stack gap={2}>
+      <h6 style={twToCss('text-xs text-primary-9 uppercase my-0')}>{titre}</h6>
       <ul style={twToCss('text-sm my-0')}>
         {liste && liste.length ? (
           liste.map((elt, index) => (
@@ -30,7 +28,7 @@ const ListeActeurs = ({ titre, liste, className }: ListeActeursProps) => {
           </li>
         )}
       </ul>
-    </div>
+    </Stack>
   );
 };
 
@@ -39,33 +37,30 @@ const Acteurs = ({ fiche }: FicheActionPdfProps) => {
     fiche;
 
   return (
-    <Card className="w-2/5 ml-2 mb-4">
-      <ListeActeurs
-        titre="Personne pilote"
-        liste={pilotes?.map((pilote) => pilote.nom!)}
-        className="mb-4"
-      />
-      <ListeActeurs
-        titre="Direction ou service pilote"
-        liste={services?.map((service) => service.nom!)}
-        className="mb-4"
-      />
-      <ListeActeurs
-        titre="Structure pilote"
-        liste={structures?.map((structure) => structure.nom!)}
-        className="mb-4"
-      />
-      <ListeActeurs
-        titre="Élu·e référent·e"
-        liste={referents?.map((referent) => referent.nom!)}
-        className="mb-4"
-      />
-      <ListeActeurs
-        titre="Partenaires"
-        liste={partenaires?.map((partenaire) => partenaire.nom!)}
-        className="mb-4"
-      />
-      <ListeActeurs titre="Cibles" liste={cibles?.map((cible) => cible)} />
+    <Card className="w-2/5 ml-2">
+      <Stack>
+        <ListeActeurs
+          titre="Personne pilote"
+          liste={pilotes?.map((pilote) => pilote.nom!)}
+        />
+        <ListeActeurs
+          titre="Direction ou service pilote"
+          liste={services?.map((service) => service.nom!)}
+        />
+        <ListeActeurs
+          titre="Structure pilote"
+          liste={structures?.map((structure) => structure.nom!)}
+        />
+        <ListeActeurs
+          titre="Élu·e référent·e"
+          liste={referents?.map((referent) => referent.nom!)}
+        />
+        <ListeActeurs
+          titre="Partenaires"
+          liste={partenaires?.map((partenaire) => partenaire.nom!)}
+        />
+        <ListeActeurs titre="Cibles" liste={cibles?.map((cible) => cible)} />
+      </Stack>
     </Card>
   );
 };
