9fdcd1e6bffe3fdf9915bbee8ebf179f9941e456
elisfainstein <elisabeth.fainstein.freelance@gmail.com>
2024-10-24 15:53:43 +0200

    feat: add sous_thematiques handling

--- backend/src/fiches/models/update-fiche-action.request.ts
+++ backend/src/fiches/models/update-fiche-action.request.ts
@@ -2,12 +2,16 @@ import z from 'zod/lib';
 import { createFicheActionSchema } from './fiche-action.table';
 import { axeSchema } from './axe.table';
 import { thematiqueSchema } from './thematique.table';
+import { sousThematiqueSchema } from './sous-thematique.table';
 
+// Used for API documentation
 export const updateFicheActionRequestSchema = createFicheActionSchema.extend({
   axes: axeSchema.array().optional(),
   thematiques: thematiqueSchema.array().optional(),
+  sous_thematiques: sousThematiqueSchema.array().optional(),
 });
 
+// Used as DTO in the controller
 export type UpdateFicheActionRequestType = z.infer<
   typeof updateFicheActionRequestSchema
 >;
--- backend/src/fiches/services/fiches-action-update.service.ts
+++ backend/src/fiches/services/fiches-action-update.service.ts
@@ -6,13 +6,12 @@ import {
   ficheActionTable,
   UpdateFicheActionType,
 } from '../models/fiche-action.table';
-import { UpsertFicheActionRequestType } from '../models/upsert-fiche-action.request';
-// import { UpdateAxeRequestType } from '../models/update-fiche-action.request';
 import { ficheActionAxeTable } from '../models/fiche-action-axe.table';
 import { PgTransaction } from 'drizzle-orm/pg-core';
 import { PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js';
 import { UpdateFicheActionRequestType } from '../models/update-fiche-action.request';
 import { ficheActionThematiqueTable } from '../models/fiche-action-thematique.table';
+import { ficheActionSousThematiqueTable } from '../models/fiche-action-sous-thematique.table';
 
 @Injectable()
 export default class FichesActionUpdateService {
@@ -23,153 +22,93 @@ export default class FichesActionUpdateService {
     body: UpdateFicheActionRequestType,
     tokenInfo: SupabaseJwtPayload
   ) {
-    const { axes, thematiques, ...ficheAction } = body;
+    const { axes, thematiques, sous_thematiques, ...ficheAction } = body;
     return await this.databaseService.db.transaction(async (tx) => {
       const updatedFicheAction = await tx
         .update(ficheActionTable)
         .set(ficheAction as UpdateFicheActionType)
-        .where(eq(ficheActionTable.id, ficheActionId))
-        .returning();
+        .where(eq(ficheActionTable.id, ficheActionId));
 
-      /**
-       * ===== AXES: start =====
-       */
+      // Updates juction tables
 
-      // if (axes && axes.length > 0) this.updateAxes(ficheActionId, axes, tx);
       if (axes && axes.length > 0) {
-        const axesIds = this.collectIds(axes);
-
-        /**
-         * Axes update, step 1: removing all axes that are not linked to fiche action anymore
-         */
-        await tx
-          .delete(ficheActionAxeTable)
-          .where(
-            and(
-              eq(ficheActionAxeTable.fiche_id, ficheActionId),
-              notInArray(ficheActionAxeTable.axe_id, axesIds)
-            )
-          );
-
-        /**
-         * Axes update, step 2: add new axes to fiche action and leave all existing relevant axes
-         */
-
-        await tx
-          .insert(ficheActionAxeTable)
-          .values(
-            axesIds.map((axeId) => ({
-              fiche_id: ficheActionId,
-              axe_id: axeId,
-            }))
-          )
-          .onConflictDoNothing({
-            target: [ficheActionAxeTable.fiche_id, ficheActionAxeTable.axe_id],
-          });
+        await this.updateRelations(
+          ficheActionId,
+          axes,
+          tx,
+          ficheActionAxeTable,
+          ficheActionAxeTable.fiche_id,
+          ficheActionAxeTable.axe_id
+        );
       }
-
-      /**
-       * ===== AXES: end =====
-       */
-
-      /**
-       * ===== THEMATIQUES: start =====
-       */
 
       if (thematiques && thematiques.length > 0) {
-        const thematiquesIds = this.collectIds(thematiques);
-
-        await tx
-          .delete(ficheActionThematiqueTable)
-          .where(
-            and(
-              eq(ficheActionThematiqueTable.fiche_id, ficheActionId),
-              notInArray(
-                ficheActionThematiqueTable.thematique_id,
-                thematiquesIds
-              )
-            )
-          );
-
-        await tx
-          .insert(ficheActionThematiqueTable)
-          .values(
-            thematiquesIds.map((thematiqueId) => ({
-              fiche_id: ficheActionId,
-              thematique_id: thematiqueId,
-            }))
-          )
-          .onConflictDoNothing({
-            target: [
-              ficheActionThematiqueTable.fiche_id,
-              ficheActionThematiqueTable.thematique_id,
-            ],
-          });
+        await this.updateRelations(
+          ficheActionId,
+          thematiques,
+          tx,
+          ficheActionThematiqueTable,
+          ficheActionThematiqueTable.fiche_id,
+          ficheActionThematiqueTable.thematique_id
+        );
       }
 
-      /**
-       * ===== THEMATIQUES: end =====
-       */
+      if (sous_thematiques && sous_thematiques.length > 0) {
+        await this.updateRelations(
+          ficheActionId,
+          sous_thematiques,
+          tx,
+          ficheActionSousThematiqueTable,
+          ficheActionSousThematiqueTable.fiche_id,
+          ficheActionSousThematiqueTable.thematique_id
+        );
+      }
 
       return updatedFicheAction;
     });
   }
 
-  collectIds(objects: { id?: number }[]): number[] {
+  private collectIds(objects: { id?: number }[]): number[] {
     return objects
       .map((object) => object.id)
       .filter((id): id is number => id !== undefined);
   }
 
-  /**
-   * Not working (only one of the two actions is executed)
-   */
-  // async updateAxes(
-  //   ficheActionId: number,
-  //   axes: UpdateAxeRequestType[],
-  //   tx: PgTransaction<
-  //     PostgresJsQueryResultHKT,
-  //     Record<string, never>,
-  //     ExtractTablesWithRelations<Record<string, never>>
-  //   >
-  // ) {
-  //   const axesIds: number[] = axes
-  //     .map((axe) => axe.id)
-  //     .filter((id): id is number => id !== undefined);
+  private async updateRelations(
+    ficheActionId: number,
+    relations: { id?: number }[],
+    tx: PgTransaction<
+      PostgresJsQueryResultHKT,
+      Record<string, never>,
+      ExtractTablesWithRelations<Record<string, never>>
+    >,
+    table: any,
+    ficheIdColumn: any,
+    relationIdColumn: any
+  ) {
+    const relationIds = this.collectIds(relations);
 
-  //   console.log(axesIds);
+    // Deletes relations that are no longuer linked to fiche action
+    await tx
+      .delete(table)
+      .where(
+        and(
+          eq(ficheIdColumn, ficheActionId),
+          notInArray(relationIdColumn, relationIds)
+        )
+      );
 
-  //   /**
-  //    * Axes update, step 1: removing all axes that are not linked to fiche action anymore
-  //    */
-
-  //   await tx
-  //     .delete(ficheActionAxeTable)
-  //     .where(
-  //       and(
-  //         eq(ficheActionAxeTable.fiche_id, ficheActionId),
-  //         notInArray(ficheActionAxeTable.axe_id, axesIds)
-  //       )
-  //     );
-
-  //   /**
-  //    * Axes update, step 2: add new axes to fiche action and leave all existing relevant axes
-  //    */
-
-  //   console.log('Inserting axes into ficheActionAxeTable with ids:', axesIds);
-
-  //   await tx
-  //     .insert(ficheActionAxeTable)
-  //     .values(
-  //       axesIds.map((axeId) => ({
-  //         fiche_id: ficheActionId,
-  //         axe_id: axeId,
-  //       }))
-  //     )
-  //     .onConflictDoNothing({
-  //       target: [ficheActionAxeTable.fiche_id, ficheActionAxeTable.axe_id],
-  //     });
-
-  //   console.log('Inserting done');
-  // }
+    // Adds new relations to fiche action
+    await tx
+      .insert(table)
+      .values(
+        relationIds.map((relationId) => ({
+          fiche_id: ficheActionId,
+          [relationIdColumn.name]: relationId,
+        }))
+      )
+      .onConflictDoNothing({
+        target: [ficheIdColumn, relationIdColumn],
+      });
+  }
 }
