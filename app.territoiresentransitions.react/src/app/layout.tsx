0b1eb89266d7b02d6465f77bebf38d1ec54e7b02
Fred <98240+farnoux@users.noreply.github.com>
2024-10-28 15:19:42 +0100

    Install Stonly widget

--- app.territoiresentransitions.react/src/app/layout.tsx
+++ app.territoiresentransitions.react/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
-import Header from './Layout/Header';
+import Script from 'next/script';
 import Footer from './Layout/Footer';
+import Header from './Layout/Header';
 import AppProviders from './app-providers';
-
 import './global.css';
 
 export const dynamic = 'force-dynamic';
@@ -85,6 +85,14 @@ export default function RootLayout({
           </div>
         </div>
       </body>
+      <Script id="stonly-widget">
+        {`window.STONLY_WID = "1b1b2533-383c-11ef-a9d4-06cb0cb2a85e";!function(s,t,o,n,l,y,w,g,d,e){s.StonlyWidget||((d=s.StonlyWidget=function(){
+  d._api?d._api.apply(d,arguments):d.queue.push(arguments)}).scriptPath=n,d.apiPath=l,d.sPath=y,d.queue=[],
+  (g=t.createElement(o)).async=!0,(e=new XMLHttpRequest).open("GET",n+"version?v="+Date.now(),!0),
+  e.onreadystatechange=function(){4===e.readyState&&(g.src=n+"stonly-widget.js?v="+
+  (200===e.status?e.responseText:Date.now()),(w=t.getElementsByTagName(o)[0]).parentNode.insertBefore(g,w))},e.send())
+  }(window,document,"script","https://stonly.com/js/widget/v2/");`}
+      </Script>
     </html>
   );
 }
