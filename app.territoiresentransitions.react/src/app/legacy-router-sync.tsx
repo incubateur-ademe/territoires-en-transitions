09b72f75a783a6aaf62d77e91f615162b463b285
Fred <98240+farnoux@users.noreply.github.com>
2024-10-28 12:51:28 +0100

    Fix: previous / next browser navigation

--- app.territoiresentransitions.react/src/app/legacy-router-sync.tsx
+++ app.territoiresentransitions.react/src/app/legacy-router-sync.tsx
@@ -1,15 +1,25 @@
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function LegacyRouterSync() {
+  const router = useRouter();
   const history = useHistory();
   const pathname = usePathname();
   const search = useSearchParams();
 
   useEffect(() => {
-    history.push(`${pathname}?${search.toString()}`);
-  }, [pathname, history, search]);
+    history.replace(`${pathname}?${search.toString()}`);
+
+    const unlisten = history.listen((location, action) => {
+      if (action === 'POP' && pathname === location.pathname) {
+        router.back();
+      }
+    });
+
+    // stop the listener when component unmounts
+    return unlisten;
+  }, [pathname, history, search, router]);
 
   return null;
 }
