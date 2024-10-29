a72d58bef9a27d648d7f6ae7c3bb0f6f0fcee15d
Fred <98240+farnoux@users.noreply.github.com>
2024-10-29 18:02:46 +0100

    test(cypress): Handle Next.js redirects

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -21,3 +21,21 @@
 
 require('cy-verify-downloads').addCustomCommand();
 require('cypress-terminal-report/src/installLogsCollector')();
+
+// Cypress configuration to handle uncaught exceptions related to NEXT_REDIRECT
+Cypress.on('uncaught:exception', (err) => {
+  console.log('err.message', err.message);
+
+  // Check if the error message includes "NEXT_REDIRECT"
+  if (err.message.includes('NEXT_REDIRECT')) {
+    // This block is added to handle server-side redirects in Next.js.
+    // Next.js often performs server-side redirects for various reasons, such as:
+    // - Authentication flows (redirecting to login page if not authenticated)
+    // - Conditional rendering based on user data
+    // - Route changes based on server-side logic
+    // When these redirects occur, they can throw a "NEXT_REDIRECT" error,
+    // which is expected behavior and should not cause the test to fail.
+    // Returning false here prevents Cypress from failing the test when such an error is encountered.
+    return false;
+  }
+});
