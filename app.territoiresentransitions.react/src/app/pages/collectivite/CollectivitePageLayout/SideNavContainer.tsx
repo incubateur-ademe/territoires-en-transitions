1770ee0fa2ed805ddd450babe5db66a4076639d6
Arthur Molinos <arthur.molinos@gmail.com>
2024-10-29 17:30:08 +0100

    chore : simplifie l'utilisation de props dans CollectivitePageLayout et enlève un style lorsqu'il n'y a pas de lien dans la SideNav

--- app.territoiresentransitions.react/src/app/pages/collectivite/CollectivitePageLayout/SideNavContainer.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/CollectivitePageLayout/SideNavContainer.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
-import SideNav, {SideNavLinks} from 'ui/shared/SideNav';
-import {useFonctionTracker} from 'core-logic/hooks/useFonctionTracker';
+import SideNav, { SideNavLinks } from 'ui/shared/SideNav';
+import { useFonctionTracker } from 'core-logic/hooks/useFonctionTracker';
 
 export type SideNavContainerProps = {
   links: SideNavLinks;
@@ -15,8 +15,8 @@ type Props = {
   sideNav: SideNavContainerProps;
 };
 
-const SideNavContainer = ({isOpen, setIsOpen, sideNav}: Props) => {
-  const {links, isHideable = true, actions} = sideNav;
+const SideNavContainer = ({ isOpen, setIsOpen, sideNav }: Props) => {
+  const { links, isHideable = true, actions } = sideNav;
   const tracker = useFonctionTracker();
 
   return (
@@ -25,7 +25,7 @@ const SideNavContainer = ({isOpen, setIsOpen, sideNav}: Props) => {
         <div
           className={classNames(
             'flex flex-col shrink-0 min-h-full py-8 border-r border-gray-100',
-            {'pt-4 pb-8': isHideable},
+            { 'pt-4 pb-8': isHideable }
           )}
         >
           {isHideable && (
@@ -33,19 +33,24 @@ const SideNavContainer = ({isOpen, setIsOpen, sideNav}: Props) => {
               className="ml-auto mr-4 mb-4 fr-btn fr-btn--tertiary fr-btn--icon fr-btn--sm fr-fi-arrow-left-s-line-double"
               onClick={() => {
                 setIsOpen(false);
-                tracker({fonction: 'navigation_laterale', action: 'fermeture'});
+                tracker({
+                  fonction: 'navigation_laterale',
+                  action: 'fermeture',
+                });
               }}
             />
           )}
           <SideNav links={links} />
-          <div className="mb-8 pt-8">{actions}</div>
+          <div className={classNames('mb-8', { 'pt-8': links.length > 0 })}>
+            {actions}
+          </div>
         </div>
       ) : (
         <button
           className="mt-4 mx-auto fr-btn fr-btn--tertiary fr-btn--icon fr-btn--sm fr-fi-arrow-right-s-line-double"
           onClick={() => {
             setIsOpen(true);
-            tracker({fonction: 'navigation_laterale', action: 'ouverture'});
+            tracker({ fonction: 'navigation_laterale', action: 'ouverture' });
           }}
         />
       )}
