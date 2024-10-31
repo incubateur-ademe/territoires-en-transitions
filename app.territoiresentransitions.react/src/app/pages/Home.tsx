289f98e429b0ec33b13a22cf6604c87834cc7144
Arthur Molinos <arthur.molinos@gmail.com>
2024-10-31 10:13:23 +0100

    feat : nouvelle home page non connecté

--- app.territoiresentransitions.react/src/app/pages/Home.tsx
+++ app.territoiresentransitions.react/src/app/pages/Home.tsx
@@ -1,21 +1,44 @@
-import {Spacer} from 'ui/dividers/Spacer';
-import {SignInLink, RegisterLink} from 'ui/shared/Links';
+import { signInPath, signUpPath } from '@tet/app/paths';
+import { Button } from '@tet/ui';
 
 const Home = () => {
   return (
-    <section data-test="home" className="max-w-2xl mx-auto p-5">
-      <h1 className="fr-h1">À vous de jouer !</h1>
-
-      <p>
-        Territoires en Transitions est un service numérique gratuit et
-        open-source qui s’adresse à toutes les collectivités. Créez votre compte
-        en moins d'une minute et rejoignez le profil de votre collectivité. Une
-        question ? Utilisez le chat en bas à droite de votre écran !
-      </p>
-      <Spacer />
-      <div className="flex flex-row-reverse gap-3">
-        <SignInLink />
-        <RegisterLink />
+    <section
+      data-test="home"
+      className="pt-20 bg-gradient-to-b from-primary-1 to-white"
+    >
+      <div className="flex flex-col items-center max-w-6xl mx-auto px-6 text-center">
+        <h1 className="text-primary-9">À vous de jouer !</h1>
+        <p className="mb-0">
+          Territoires en Transitions est un outil public gratuit et open-source
+          pour les collectivités, financé par l'ADEME.
+          <br />
+          Actuellement à ses débuts, la plateforme a besoin de vous pour évoluer
+          dans le sens de vos besoins.
+          <br />
+          Rejoignez-nous dans sa co-construction en créant votre compte en moins
+          d'une minute.
+        </p>
+        <Button
+          href="https://www.territoiresentransitions.fr/outil-numerique"
+          external
+          variant="white"
+          size="xs"
+          className="my-6"
+        >
+          En savoir plus sur l’outil
+        </Button>
+        <div className="flex gap-3 mb-12">
+          <Button href={signUpPath} variant="outlined">
+            Créer un compte
+          </Button>
+          <Button href={signInPath}>Se connecter</Button>
+        </div>
+        <img
+          className="-mb-8 rounded-t-3xl border-t border-l border-r border-primary-4"
+          src="/public/home.png"
+          alt="interface territoires en transition"
+        />
       </div>
     </section>
   );
