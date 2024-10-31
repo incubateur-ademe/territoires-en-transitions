0e2e30cfc7a9d871f817579a17be2511a934e686
Marc Rutkowski <marc@attractive-media.fr>
2024-10-31 15:47:47 +0100

    Ajoute la modale d'édition d'un lien

---
+++ app.territoiresentransitions.react/src/ui/shared/preuves/Bibliotheque/EditerLienModal.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { Field, Input, Modal, ModalFooterOKCancel } from '@tet/ui';
+import { TPreuve } from './types';
+import { useUpdatePreuveLien } from './useEditPreuve';
+
+export type EditerLienProps = {
+  preuve: TPreuve;
+  isOpen: boolean;
+  setIsOpen: (opened: boolean) => void;
+};
+
+/**
+ * Affiche la modale d'édition d'un lien
+ */
+export const EditerLienModal = (props: EditerLienProps) => {
+  const { preuve, isOpen, setIsOpen } = props;
+  const { lien } = preuve;
+  const [titre, setTitre] = useState(lien?.titre || '');
+  const [url, setUrl] = useState(lien?.url || '');
+
+  const { mutate: editLien, isLoading } = useUpdatePreuveLien();
+
+  return (
+    !!lien && (
+      <Modal
+        dataTest="edit-lien"
+        openState={{ isOpen, setIsOpen }}
+        title="Editer le lien"
+        render={() => (
+          <>
+            <Field title="Titre du lien">
+              <Input
+                type="text"
+                value={titre}
+                onChange={(e) => setTitre(e.currentTarget.value)}
+              />
+            </Field>
+            <Field title="Lien">
+              <Input
+                type="text"
+                value={url}
+                onChange={(e) => setUrl(e.currentTarget.value.trim())}
+              />
+            </Field>
+          </>
+        )}
+        // Boutons pour valider / annuler les modifications
+        renderFooter={({ close }) => (
+          <ModalFooterOKCancel
+            btnCancelProps={{ onClick: close, disabled: isLoading }}
+            btnOKProps={{
+              disabled: isLoading || !titre.trim() || !url,
+              onClick: () => {
+                editLien({ ...preuve, lien: { titre, url } });
+                close();
+              },
+            }}
+          />
+        )}
+      />
+    )
+  );
+};
