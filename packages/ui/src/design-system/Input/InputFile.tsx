c760bb460ae234c38a8ff84795357fd1f266ac0f
Marc Rutkowski <marc@attractive-media.fr>
2024-10-31 17:05:55 +0100

    Ajoute la gestion du drag'n'drop de fichier(s)

--- packages/ui/src/design-system/Input/InputFile.tsx
+++ packages/ui/src/design-system/Input/InputFile.tsx
@@ -1,7 +1,8 @@
-import { Ref, forwardRef } from 'react';
+import { Ref, forwardRef, useState } from 'react';
 import classNames from 'classnames';
 import { InputBaseProps } from './InputBase';
 import { FieldState } from '../Field';
+import { Icon } from '../Icon';
 
 const stateToBorderColor: Record<FieldState, string> = {
   default: 'border-grey-4',
@@ -12,38 +13,71 @@ const stateToBorderColor: Record<FieldState, string> = {
   warning: 'border-warning-1',
 };
 
-export type InputFileProps = Omit<InputBaseProps, 'icon' | 'type'>;
+export type InputFileProps = Omit<InputBaseProps, 'icon' | 'type'> & {
+  // appelée quand l'utilisateur drop des fichiers
+  onDropFiles?: (files: FileList) => void;
+};
 
 /**
  * Affiche un champ de sélection d'un fichier
- * TODO: drag and drop des fichiers
  */
-
 export const InputFile = forwardRef(
   (
     {
       displaySize = 'sm',
       state,
       containerClassname,
+      onDropFiles,
       ...remainingProps
     }: InputFileProps,
     ref?: Ref<HTMLInputElement>
   ) => {
     const borderColor =
       state !== undefined ? stateToBorderColor[state] : stateToBorderColor.info;
+    const [dragOver, setDragOver] = useState(false);
+    const isDraggingOver = dragOver && onDropFiles;
 
     return (
       <div
         className={classNames(
-          'bg-grey-1 border border-dashed rounded-lg flex flex-col justify-center items-center',
+          'bg-grey-1 border rounded-lg flex flex-col justify-center items-center',
           {
             'p-4': displaySize === 'sm',
             'p-5': displaySize === 'md',
+            'border-dashed': !isDraggingOver,
+            'border-solid border-info-1 shadow-[0_4px_20px_0_rgba(0,0,0,0.05)':
+              isDraggingOver,
           },
           borderColor,
           containerClassname
         )}
+        onDrop={(e) => {
+          e.preventDefault();
+          onDropFiles?.(e.dataTransfer.files);
+          setDragOver(false);
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setDragOver(true);
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+          setDragOver(false);
+        }}
       >
+        {onDropFiles && (
+          <div className="text-grey-7 font-bold flex flex-col items-center">
+            <Icon icon="upload-line" className="text-grey-5" />
+            <p
+              className={classNames('mb-2', {
+                'text-sm': displaySize === 'sm',
+              })}
+            >
+              Glisser votre fichier
+            </p>
+            <p className="text-xs mb-3">OU</p>
+          </div>
+        )}
         <input
           ref={ref}
           id="input-file"
