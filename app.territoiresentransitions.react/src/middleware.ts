6ff42d3bbe1255b35ae0705da0fc88faa7522a14
Fred <98240+farnoux@users.noreply.github.com>
2024-11-05 19:27:40 +0100

    fix(app): add missing env vars on build

--- app.territoiresentransitions.react/src/middleware.ts
+++ app.territoiresentransitions.react/src/middleware.ts
@@ -25,14 +25,8 @@ export const config = {
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
-
   const pathname = url.pathname;
-  const hostname = request.headers.get('host') as string;
-  // const requestUrl = new URL(url, hostname);
-
-  console.info('middleware.requestUrl', url.href);
-  console.info('middleware.headers.host', hostname);
-  // console.log('middleware.nextPathname', pathname);
+  // const hostname = request.headers.get('host') as string; // e.g. 'app.territoiresentransitions.fr'
 
   if (isAuthUrl(pathname)) {
     return redirectToAuthDomain(url);
@@ -49,21 +43,12 @@ function isAuthUrl(pathname: string) {
 
 function redirectToAuthDomain(url: URL) {
   const searchParams = url.search.toString();
-  // if (!search.has('redirect_to')) {
-  // Returns to the current URL if nothing is specified.
-  // This ensures that the user is redirected to the domain it came from after authentication.
-  // search.append('redirect_to', '/');
-  // }
 
   const path = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ''
   }`;
 
-  console.info('path', path);
-  console.info('NEXT_PUBLIC_AUTH_URL', process.env.NEXT_PUBLIC_AUTH_URL);
   const authUrl = new URL(path, process.env.NEXT_PUBLIC_AUTH_URL);
-  console.info('authUrl', authUrl.href);
-  // authUrl.search = search.toString();
 
   return NextResponse.redirect(authUrl);
 }
