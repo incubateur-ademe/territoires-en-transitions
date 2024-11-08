cd29ff440df242e62a38e0935ffa6ef8c3b93a4d
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2024-11-08 17:38:36 +0100

    refactor: remove useless camelCase pipe

--- backend/src/common/pipes/camel-case.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
-import { objectToCamel } from 'ts-case-convert';
-
-@Injectable()
-export class CamelCasePipe implements PipeTransform {
-  transform(value: any) {
-    return objectToCamel(value);
-  }
-}
