3c98b8b85eb6446fbc351b3660eb969745a62e5b
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2024-11-08 17:31:18 +0100

    refactor: fix drizzle-zod conversion bug

--- backend/src/fiches/models/update-fiche-action.request.ts
+++ backend/src/fiches/models/update-fiche-action.request.ts
@@ -1,6 +1,5 @@
 import z from 'zod';
 import {
-  createFicheActionSchema,
   FicheActionCiblesEnumType,
   ficheActionPiliersEciEnumType,
   ficheActionResultatsAttendusEnumType,
@@ -21,33 +20,57 @@ import { sousThematiqueSchema } from '../../taxonomie/models/sous-thematique.tab
 // There is no proper Pilote or Referent tables, so we use a custom schema here
 const piloteOrReferentSchema = z.object({
   nom: z.string(),
-  collectivite_id: z.number(),
-  tag_id: z.number(),
-  user_id: z.string().uuid(),
+  collectiviteId: z.number(),
+  tagId: z.number(),
+  userId: z.string().uuid(),
 });
 
-// We're overriding piliersEci, resultatsAttendus and cibles because,
-// for some unknown reason (a bug with zod/drizzle ?), extend() looses enum's array
+const financeurSchema = financeurTagSchema.pick({ id: true }).extend({
+  id: financeurTagSchema.shape.id,
+});
+
+const financeurWithMontantSchema = z.object({
+  financeurTag: financeurSchema.optional(),
+  montantTtc: z.number().optional(),
+});
+
 export const updateFicheActionRequestSchema = updateFicheActionSchema.extend({
+  // We're overriding piliersEci, resultatsAttendus and cibles because,
+  // for some unknown reason (a bug with zod/drizzle ?), extend() looses enum's array
   piliersEci: z.nativeEnum(ficheActionPiliersEciEnumType).array().optional(),
   resultatsAttendus: z
     .nativeEnum(ficheActionResultatsAttendusEnumType)
     .array()
     .optional(),
   cibles: z.nativeEnum(FicheActionCiblesEnumType).array().optional(),
-  axes: axeSchema.array().optional(),
-  thematiques: thematiqueSchema.array().optional(),
-  sousThematiques: sousThematiqueSchema.array().optional(),
-  partenaires: partenaireTagSchema.array().optional(),
-  structures: structureTagSchema.array().optional(),
+
+  // Overriding because numeric and timestamp types are not properly converted otherwise (a bug with zod/drizzle ?)
+  budgetPrevisionnel: z
+    .string()
+    .refine((val) => !isNaN(Number(val)), {
+      message: "Expected 'budgetPrevisionnel' to be a numeric string",
+    })
+    .optional(),
+  dateDebut: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: "Invalid date format for 'dateDebut'",
+    })
+    .optional(),
+
+  axes: axeSchema.pick({ id: true }).array().optional(),
+  thematiques: thematiqueSchema.pick({ id: true }).array().optional(),
+  sousThematiques: sousThematiqueSchema.pick({ id: true }).array().optional(),
+  partenaires: partenaireTagSchema.pick({ id: true }).array().optional(),
+  structures: structureTagSchema.pick({ id: true }).array().optional(),
   pilotes: piloteOrReferentSchema.array().optional(),
   referents: piloteOrReferentSchema.array().optional(),
-  actions: actionRelationSchema.array().optional(),
-  indicateurs: indicateurDefinitionSchema.array().optional(),
-  services: serviceTagSchema.array().optional(),
-  financeurs: financeurTagSchema.array().optional(),
-  fichesLiees: ficheActionSchema.array().optional(),
-  resultatAttendu: effetAttenduSchema.array().optional(),
+  actions: actionRelationSchema.pick({ id: true }).array().optional(),
+  indicateurs: indicateurDefinitionSchema.pick({ id: true }).array().optional(),
+  services: serviceTagSchema.pick({ id: true }).array().optional(),
+  financeurs: z.array(financeurWithMontantSchema).optional(),
+  fichesLiees: ficheActionSchema.pick({ id: true }).array().optional(),
+  resultatAttendu: effetAttenduSchema.pick({ id: true }).array().optional(),
 });
 
 export type UpdateFicheActionRequestType = z.infer<
