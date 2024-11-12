5f756fb81ba3afda671b7de358aca2a3b3a666ed
Arthur Molinos <arthur.molinos@gmail.com>
2024-11-12 11:11:27 +0100

    :feat: Change l'image de la home page non connectée

--- app.territoiresentransitions.react/src/app/pages/Home.tsx
+++ app.territoiresentransitions.react/src/app/pages/Home.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image';
+
 import { signInPath, signUpPath } from '@tet/app/paths';
 import { Button } from '@tet/ui';
+import homeImage from 'app/static/img/home.jpg';
 
 const Home = () => {
   return (
@@ -11,13 +14,13 @@ const Home = () => {
         <h1 className="text-primary-9">À vous de jouer !</h1>
         <p className="mb-0">
           Territoires en Transitions est un outil public gratuit et open-source
-          pour les collectivités, financé par l'ADEME.
+          pour les collectivités, financé par l&apos;ADEME.
           <br />
           Actuellement à ses débuts, la plateforme a besoin de vous pour évoluer
           dans le sens de vos besoins.
           <br />
           Rejoignez-nous dans sa co-construction en créant votre compte en moins
-          d'une minute.
+          d&apos;une minute.
         </p>
         <Button
           href="https://www.territoiresentransitions.fr/outil-numerique"
@@ -34,9 +37,9 @@ const Home = () => {
           </Button>
           <Button href={signInPath}>Se connecter</Button>
         </div>
-        <img
+        <Image
           className="-mb-8 rounded-t-3xl border-t border-l border-r border-primary-4"
-          src="/public/home.png"
+          src={homeImage}
           alt="interface territoires en transition"
         />
       </div>
