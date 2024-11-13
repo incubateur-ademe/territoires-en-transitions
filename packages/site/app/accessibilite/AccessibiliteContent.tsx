034fb27d2bb9fc73f631126a19c8133af0b5ed54
Marine Heckler <marine.heckler@beta.gouv.fr>
2024-11-13 15:29:17 +0100

    Corrige les pages cgu, cookies et accessibilité du site

---
+++ packages/site/app/accessibilite/AccessibiliteContent.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Section from '@tet/site/components/sections/Section';
+import Content from './accessibilite.mdx';
+
+const AccessibiliteContent = () => {
+  return (
+    <Section>
+      <h1 className="fr-header__body">Déclaration d’accessibilité</h1>
+      <Content />
+    </Section>
+  );
+};
+
+export default AccessibiliteContent;
