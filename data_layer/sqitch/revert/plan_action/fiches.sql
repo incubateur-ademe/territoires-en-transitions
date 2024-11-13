cc329122b2f44a8eb50d122a9ee9733276c307a4
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2024-11-13 14:46:52 +0100

    fix: add libres_tag to FA view

--- data_layer/sqitch/revert/plan_action/fiches.sql
+++ data_layer/sqitch/revert/plan_action/fiches.sql
@@ -11,27 +11,6 @@ DROP TRIGGER IF EXISTS upsert ON public.fiches_action;
 DROP VIEW public.fiches_action;
 DROP VIEW private.fiches_action;
 
-
---
--- Revert 1. TAGS DE SUIVI LIBRES
-
-DROP TABLE fiche_action_libre_tag;
-DROP TABLE libre_tag;
-
---
--- Revert 2. INSTANCES DE GOUVERNANCE
-ALTER TABLE fiche_action DROP COLUMN instance_gouvernance;
-
---
--- Revert 3. PARTICIPATION CITOYENNE
-ALTER TABLE fiche_action DROP COLUMN participation_citoyenne;
-ALTER TABLE fiche_action DROP COLUMN participation_citoyenne_type;
-
---
--- Revert 4. TEMPS DE MISE EN OEUVRE
-ALTER TABLE fiche_action DROP COLUMN temps_de_mise_en_oeuvre_id;
-
-
 --
 -- AFTER. Recreate the views and functions
 create view private.fiches_action
@@ -54,6 +33,10 @@ SELECT fa.modified_at,
        fa.amelioration_continue,
        fa.calendrier,
        fa.notes_complementaires,
+       fa.instance_gouvernance,
+       fa.participation_citoyenne,
+       fa.participation_citoyenne_type,
+       jsonb_build_object('id', tmo.niveau, 'nom', tmo.nom) as temps_de_mise_en_oeuvre,
        fa.maj_termine,
        fa.collectivite_id,
        fa.created_at,
@@ -192,7 +175,10 @@ LEFT JOIN (
           FROM effet_attendu ea
           JOIN fiche_action_effet_attendu faea ON ea.id = faea.effet_attendu_id
           GROUP BY faea.fiche_id
-          ) eff ON eff.fiche_id = fa.id;
+          ) eff ON eff.fiche_id = fa.id
+LEFT JOIN action_impact_temps_de_mise_en_oeuvre tmo
+          ON tmo.niveau = fa.temps_de_mise_en_oeuvre_id
+;
 
 
 create or replace view public.fiches_action as
