677fe5b9080602f27552cb96cac1cc16145220ae
Marc Rutkowski <marc@attractive-media.fr>
2024-11-14 15:50:22 +0100

    Ajoute le hook pour ajouter/modifier/supprimer les notes de suivi

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useUpsertNoteSuivi.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { FicheActionNote } from '@tet/api/plan-actions';
+import { useCollectiviteId } from 'core-logic/hooks/params';
+import { useApiClient } from 'core-logic/api/useApiClient';
+
+export type EditedNote = Pick<FicheActionNote, 'note'> & { year: number };
+export type DeletedNote = { year: number };
+
+// renvoie une fonction de modification des notes de suivi
+export const useUpsertNoteSuivi = (ficheId: number) => {
+  const collectiviteId = useCollectiviteId();
+  const api = useApiClient();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async ({ note, year }: EditedNote) => {
+      return api.put({
+        route: `/collectivites/${collectiviteId}/fiches-action/${ficheId}/notes`,
+        params: {
+          notes: [{ note, dateNote: new Date(`${year}-01-01`).toISOString() }],
+        },
+      });
+    },
+    {
+      mutationKey: 'update_note_suivi',
+      onSuccess: () => {
+        queryClient.invalidateQueries(['fiche_action', ficheId.toString()]);
+      },
+    }
+  );
+};
+
+// renvoie une fonction de suppression d'une note de suivi
+export const useDeleteNoteSuivi = (ficheId: number) => {
+  const collectiviteId = useCollectiviteId();
+  const api = useApiClient();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async ({ year }: DeletedNote) => {
+      return api.delete({
+        route: `/collectivites/${collectiviteId}/fiches-action/${ficheId}/note`,
+        params: { dateNote: new Date(`${year}-01-01`).toISOString() },
+      });
+    },
+    {
+      mutationKey: 'delete_note_suivi',
+      onSuccess: () => {
+        queryClient.invalidateQueries(['fiche_action', ficheId.toString()]);
+      },
+    }
+  );
+};
