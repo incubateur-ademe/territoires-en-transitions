5103f6702da98d2285a8bfc6fe623941f985c249
Marc Rutkowski <marc@attractive-media.fr>
2024-11-18 11:07:46 +0100

    Met à jour la date de modification de la fiche action quand les notes de suivi sont modifiées

--- data_layer/sqitch/revert/plan_action/historique.sql
+++ data_layer/sqitch/revert/plan_action/historique.sql
@@ -2,26 +2,7 @@
 
 BEGIN;
 
--- supprime les triggers et la fonction associée
-drop trigger save_history on fiche_action_pilote;
-drop trigger update_fa_modification_date on annexe;
-drop function historique.set_fiche_action_modified_at;
-
--- recrée la fonction
-create or replace function historique.save_fiche_action_pilote() returns trigger
-as
-$$
-begin
-    update fiche_action set modified_at = now() where id = new.fiche_id or id = old.fiche_id;
-    return coalesce(new, old);
-end ;
-$$ language plpgsql security definer;
-
--- et le trigger
-create trigger save_history
-    after insert or delete
-    on fiche_action_pilote
-    for each row
-execute procedure historique.save_fiche_action_pilote();
+drop trigger update_fa_modification_date
+    on fiche_action_note;
 
 COMMIT;
