792e5b59173d2415d921442d2c59339b7bf1c4d4
Arthur Molinos <arthur.molinos@gmail.com>
2024-11-21 17:13:14 +0100

    Utilise les composants du DS dans CreerPlan

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ParcoursCreationPlan/CreerPlan.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react';
 import { useCreatePlanAction } from '../PlanAction/data/useUpsertAxe';
 import { useCollectiviteId } from 'core-logic/hooks/params';
 import { makeCollectivitePlansActionsNouveauUrl } from 'app/paths';
-import FormField from 'ui/shared/form/FormField';
 import PlanTypeDropdown from '../PlanAction/PlanTypeDropdown';
 import { TPlanType } from 'types/alias';
-import Link from 'next/link';
+import { Button, Field, Input } from '@tet/ui';
 
 const CreerPlan = () => {
   const collectivite_id = useCollectiviteId();
@@ -29,34 +28,32 @@ const CreerPlan = () => {
           <span className="fr-icon-edit-box-fill mr-2" />
           Créer un plan d’action
         </h3>
-        <div className="flex flex-col mt-2 mb-10 py-14 px-24 bg-[#f6f6f6]">
-          <FormField
-            label="Nom du plan d’action"
+        <div className="flex flex-col gap-6 mt-2 mb-10 pt-16 pb-20 px-24 bg-primary-0 rounded-2xl">
+          <Field
+            title="Nom du plan d’action"
             hint="Exemple : Plan Climat Air Énergie territorial 2022-2026"
           >
-            <input
+            <Input
               data-test="PlanNomInput"
-              className="fr-input"
+              type="text"
               onChange={(e) => setPlan({ ...plan, nom: e.target.value })}
             />
-          </FormField>
+          </Field>
           <PlanTypeDropdown
             type={plan.type?.id}
             onSelect={(type) => setPlan({ ...plan, type })}
           />
-          <div className="flex items-center justify-end gap-6 mt-4">
-            <Link
-              className="fr-btn fr-btn--tertiary fr-btn--icon-left !mb-0 fr-icon-arrow-left-line hover:!bg-[#EEEEEE]"
+          <div className="flex items-center justify-end gap-6 mt-6">
+            <Button
+              variant="outlined"
+              icon="arrow-left-line"
               href={makeCollectivitePlansActionsNouveauUrl({
                 collectiviteId: collectivite_id!,
               })}
             >
               Revenir à l’étape précédente
-            </Link>
-
-            <button className="fr-btn !mb-0" onClick={handleCreate}>
-              Valider
-            </button>
+            </Button>
+            <Button onClick={handleCreate}>Valider</Button>
           </div>
         </div>
       </div>
