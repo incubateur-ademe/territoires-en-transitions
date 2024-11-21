eacc1a204dbf9d054437d0593dac47707cc8f865
Arthur Molinos <arthur.molinos@gmail.com>
2024-11-21 16:56:12 +0100

    Remplace des vieux composants par des composants du DS dans header d'un plan d'action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/RestreindreFichesModal.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/PlanAction/RestreindreFichesModal.tsx
@@ -1,6 +1,6 @@
-import Modal from 'ui/shared/floating-ui/Modal';
-import {PlanNode} from './data/types';
-import {useRestreindreFiches} from '../FicheAction/data/useRestreindreFiches';
+import { PlanNode } from './data/types';
+import { useRestreindreFiches } from '../FicheAction/data/useRestreindreFiches';
+import { Modal, ModalFooterOKCancel } from '@tet/ui';
 
 type Props = {
   children: JSX.Element;
@@ -18,45 +18,35 @@ const RestreindreFichesModal = ({
   axes,
   restreindre,
 }: Props) => {
-  const {mutate: restreindrePlan} = useRestreindreFiches(axes);
+  const { mutate: restreindrePlan } = useRestreindreFiches(axes);
   return (
     <Modal
-      noCloseButton
-      render={({labelId, descriptionId, close}) => {
-        return (
-          <div data-test="SupprimerFicheModale" className="mt-2">
-            <h6 id={labelId} className="fr-h6">
-              {restreindre
-                ? 'Souhaitez-vous rendre privées toutes les fiches de ce plan ?'
-                : 'Souhaitez-vous rendre publiques toutes les fiches de ce plan ?'}
-            </h6>
-            <p id={descriptionId} className="mb-4">
-              {restreindre
-                ? "En passant en privé l'ensemble des fiches de ce plan, elles ne seront plus accessibles par les personnes n’étant pas membres de votre collectivité. Les fiches restent consultables par l’ADEME et le service support de la plateforme."
-                : "En passant en public l'ensemble des fiches de ce plan, elles seront accessibles à toutes les personnes n’étant pas membres de votre collectivité."}
-            </p>
-            <div className="mt-8 fr-btns-group fr-btns-group--left fr-btns-group--inline-reverse fr-btns-group--inline-lg">
-              <button
-                onClick={close}
-                className="fr-btn fr-btn--secondary"
-                aria-label="Annuler"
-              >
-                Annuler
-              </button>
-              <button
-                onClick={() => {
-                  restreindrePlan({plan_id: planId, restreindre});
-                  close();
-                }}
-                aria-label="Confirmer"
-                className="fr-btn"
-              >
-                Confirmer
-              </button>
-            </div>
-          </div>
-        );
-      }}
+      textAlign="left"
+      title={
+        restreindre
+          ? 'Souhaitez-vous rendre privées toutes les fiches de ce plan ?'
+          : 'Souhaitez-vous rendre publiques toutes les fiches de ce plan ?'
+      }
+      description={
+        restreindre
+          ? "En passant en privé l'ensemble des fiches de ce plan, elles ne seront plus accessibles par les personnes n’étant pas membres de votre collectivité. Les fiches restent consultables par l’ADEME et le service support de la plateforme."
+          : "En passant en public l'ensemble des fiches de ce plan, elles seront accessibles à toutes les personnes n’étant pas membres de votre collectivité."
+      }
+      renderFooter={({ close }) => (
+        <ModalFooterOKCancel
+          btnCancelProps={{
+            onClick: () => close(),
+          }}
+          btnOKProps={{
+            'aria-label': 'Confirmer',
+            children: 'Confirmer',
+            onClick: () => {
+              restreindrePlan({ plan_id: planId, restreindre });
+              close();
+            },
+          }}
+        />
+      )}
     >
       {children}
     </Modal>
