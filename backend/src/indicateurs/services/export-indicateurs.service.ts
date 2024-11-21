4d874fe0468976a423a2c271c381c319a625022c
Marc Rutkowski <marc@attractive-media.fr>
2024-11-21 14:57:23 +0100

    Evite que la colonne "type de données" soit vide

--- backend/src/indicateurs/services/export-indicateurs.service.ts
+++ backend/src/indicateurs/services/export-indicateurs.service.ts
@@ -284,15 +284,4 @@ export default class ExportIndicateursService {
       { numeric: true, sensitivity: 'base' }
     );
   }
-
-  private sortByDefinitionId(
-    a: MinimalIndicateurDefinitionType,
-    b: MinimalIndicateurDefinitionType
-  ) {
-    return `${a.identifiantReferentiel ?? a.id}`.localeCompare(
-      `${b.identifiantReferentiel ?? b.id}`,
-      undefined,
-      { numeric: true, sensitivity: 'base' }
-    );
-  }
 }
