9631909159c7f7cb6e5ec8803d8f5c4708cb2436
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2024-11-25 16:25:19 +0100

    fix: add forgotten columns

--- backend/src/fiches/models/fiche-action-libre-tag.table.ts
+++ backend/src/fiches/models/fiche-action-libre-tag.table.ts
@@ -1,12 +1,23 @@
-import { integer, pgTable, primaryKey } from 'drizzle-orm/pg-core';
 import { libreTagTable } from '../../taxonomie/models/libre-tag.table';
+import {
+  integer,
+  pgTable,
+  primaryKey,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
 import { ficheActionTable } from './fiche-action.table';
+import { sql } from 'drizzle-orm';
 
 export const ficheActionLibreTagTable = pgTable(
   'fiche_action_libre_tag',
   {
     ficheId: integer('fiche_id').references(() => ficheActionTable.id),
     libreTagId: integer('libre_tag_id').references(() => libreTagTable.id),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    createdBy: uuid('created_by').default(sql`auth.uid()`),
   },
   (table) => {
     return {
