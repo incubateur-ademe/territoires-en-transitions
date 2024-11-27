2d0c896616f845ce72d960385fa93a34159a6c1c
Amandine Jacquelin <contact@jacquelinamandine.fr>
2024-11-27 18:08:46 +0100

    Met à jour le module des fiches actions avec les étapes

--- backend/src/fiches/fiches-action.module.ts
+++ backend/src/fiches/fiches-action.module.ts
@@ -8,6 +8,8 @@ import FichesActionUpdateService from './services/fiches-action-update.service';
 import FicheService from './services/fiche.service';
 import TagService from '../taxonomie/services/tag.service';
 import { CountByStatutRouter } from './count-by-statut/count-by-statut.router';
+import { FicheActionEtapeRouter } from './fiche-action-etape/fiche-action-etape.router';
+import { FicheActionEtapeService } from './fiche-action-etape/fiche-action-etape.service';
 
 @Module({
   imports: [CommonModule, AuthModule, CollectivitesModule],
@@ -17,8 +19,15 @@ import { CountByStatutRouter } from './count-by-statut/count-by-statut.router';
     FichesActionUpdateService,
     TagService,
     CountByStatutRouter,
+    FicheActionEtapeService,
+    FicheActionEtapeRouter,
   ],
-  exports: [CountByStatutService, CountByStatutRouter],
+  exports: [
+    CountByStatutService,
+    CountByStatutRouter,
+    FicheActionEtapeService,
+    FicheActionEtapeRouter,
+  ],
   controllers: [FichesActionController],
 })
 export class FichesActionModule {}
