558e1bfe64bfce5f1d0527469f63c33c3bf01f7f
Amandine Jacquelin <contact@jacquelinamandine.fr>
2024-11-27 18:08:21 +0100

    Ajoute les routes CRUD de fiche-action-etape avec trpc

---
+++ backend/src/fiches/fiche-action-etape/fiche-action-etape.router.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { FicheActionEtapeService } from './fiche-action-etape.service';
+import { TrpcService } from '../../trpc/trpc.service';
+import { upsertFicheActionEtapeSchema } from '@tet/backend/fiches/fiche-action-etape/fiche-action-etape.table';
+import { z } from 'zod';
+
+
+@Injectable()
+export class FicheActionEtapeRouter {
+  constructor(
+    private readonly trpc: TrpcService,
+    private readonly service: FicheActionEtapeService
+  ) {}
+
+  router = this.trpc.router({
+    upsert: this.trpc.authedProcedure
+      .input(upsertFicheActionEtapeSchema)
+      .query(({ ctx, input }) => {
+        return this.service.upsertEtape(input, ctx.user);
+      }),
+    delete: this.trpc.authedProcedure
+      .input(z.object({etapeId : z.number()}))
+      .query(({ ctx, input }) => {
+        return this.service.deleteEtape(input.etapeId, ctx.user);
+      }),
+    list: this.trpc.authedProcedure
+      .input(z.object({ficheId : z.number()}))
+      .query(({ ctx, input }) => {
+        return this.service.getEtapesByFicheId(input.ficheId, ctx.user);
+      }),
+  });
+}
