d4c3f0f14f679a7f8f2e97e405c5b2f75259dcdc
Marc Rutkowski <marc@attractive-media.fr>
2024-11-28 19:16:27 +0100

    Ajoute un utilitaire pour améliorer la définition des enum

--- backend/src/collectivites/models/membre-fonction.enum.ts
+++ backend/src/collectivites/models/membre-fonction.enum.ts
@@ -1,15 +1,13 @@
 import { pgEnum } from 'drizzle-orm/pg-core';
+import { createEnumObject } from '../../common/models/enum.helpers';
 
-export enum MembreFonctionEnum {
-  CONSEILLER = 'conseiller',
-  TECHNIQUE = 'technique',
-  POLITIQUE = 'politique',
-  PARTENAIRE = 'partenaire',
-}
+export const MembreFonction = [
+  'conseiller',
+  'technique',
+  'politique',
+  'partenaire',
+] as const;
 
-export const membreFonctionEnum = pgEnum('membre_fonction', [
-  MembreFonctionEnum.CONSEILLER,
-  MembreFonctionEnum.TECHNIQUE,
-  MembreFonctionEnum.POLITIQUE,
-  MembreFonctionEnum.PARTENAIRE,
-] as const);
+export const MembreFonctionEnum = createEnumObject(MembreFonction);
+
+export const membreFonctionEnum = pgEnum('membre_fonction', MembreFonction);
