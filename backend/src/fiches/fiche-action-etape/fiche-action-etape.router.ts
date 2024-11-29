350c7b30fd6ae4d18c5d350632739a8d4efc535b
Fred <98240+farnoux@users.noreply.github.com>
2024-11-29 15:32:50 +0100

    Fix trpc router routes for fiches

--- backend/src/fiches/fiche-action-etape/fiche-action-etape.router.ts
+++ backend/src/fiches/fiche-action-etape/fiche-action-etape.router.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { FicheActionEtapeService } from './fiche-action-etape.service';
-import { TrpcService } from '../../trpc/trpc.service';
 import { upsertFicheActionEtapeSchema } from '@tet/backend/fiches/fiche-action-etape/fiche-action-etape.table';
 import { z } from 'zod';
-
+import { TrpcService } from '../../trpc/trpc.service';
+import { FicheActionEtapeService } from './fiche-action-etape.service';
 
 @Injectable()
 export class FicheActionEtapeRouter {
@@ -13,20 +12,22 @@ export class FicheActionEtapeRouter {
   ) {}
 
   router = this.trpc.router({
-    upsert: this.trpc.authedProcedure
-      .input(upsertFicheActionEtapeSchema)
-      .query(({ ctx, input }) => {
-        return this.service.upsertEtape(input, ctx.user);
-      }),
-    delete: this.trpc.authedProcedure
-      .input(z.object({etapeId : z.number()}))
-      .query(({ ctx, input }) => {
-        return this.service.deleteEtape(input.etapeId, ctx.user);
-      }),
-    list: this.trpc.authedProcedure
-      .input(z.object({ficheId : z.number()}))
-      .query(({ ctx, input }) => {
-        return this.service.getEtapesByFicheId(input.ficheId, ctx.user);
-      }),
+    etapes: {
+      upsert: this.trpc.authedProcedure
+        .input(upsertFicheActionEtapeSchema)
+        .query(({ ctx, input }) => {
+          return this.service.upsertEtape(input, ctx.user);
+        }),
+      delete: this.trpc.authedProcedure
+        .input(z.object({ etapeId: z.number() }))
+        .query(({ ctx, input }) => {
+          return this.service.deleteEtape(input.etapeId, ctx.user);
+        }),
+      list: this.trpc.authedProcedure
+        .input(z.object({ ficheId: z.number() }))
+        .query(({ ctx, input }) => {
+          return this.service.getEtapesByFicheId(input.ficheId, ctx.user);
+        }),
+    },
   });
 }
