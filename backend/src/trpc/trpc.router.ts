b33672d0c5a1b6923e08a71ef1484d0a524cea22
Fred <98240+farnoux@users.noreply.github.com>
2024-11-29 15:32:50 +0100

    Fix trpc router routes for fiches

--- backend/src/trpc/trpc.router.ts
+++ backend/src/trpc/trpc.router.ts
@@ -2,12 +2,12 @@ import { INestApplication, Injectable, Logger } from '@nestjs/common';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { PersonnesRouter } from '../collectivites/personnes.router';
 import SupabaseService from '../common/services/supabase.service';
-import { IndicateurFiltreRouter } from '../indicateurs/indicateur-filtre/indicateur-filtre.router';
-import { GetCategoriesByCollectiviteRouter } from '../taxonomie/routers/get-categories-by-collectivite.router';
-import { FicheActionEtapeRouter } from '../fiches/fiche-action-etape/fiche-action-etape.router';
 import { BulkEditRouter } from '../fiches/bulk-edit/bulk-edit.router';
 import { CountByStatutRouter } from '../fiches/count-by-statut/count-by-statut.router';
+import { FicheActionEtapeRouter } from '../fiches/fiche-action-etape/fiche-action-etape.router';
+import { IndicateurFiltreRouter } from '../indicateurs/indicateur-filtre/indicateur-filtre.router';
 import { TrajectoiresRouter } from '../indicateurs/routers/trajectoires.router';
+import { GetCategoriesByCollectiviteRouter } from '../taxonomie/routers/get-categories-by-collectivite.router';
 import { createContext, TrpcService } from './trpc.service';
 
 @Injectable()
@@ -32,12 +32,11 @@ export class TrpcRouter {
       filtre: this.indicateurFiltreRouter.router,
     },
     plans: {
-      fiches: {
-        countByStatut: this.countByStatutRouter.router.countByStatut,
-        etapes: this.ficheActionEtapeRouter.router,
-        ...this.countByStatutRouter.router,
-        ...this.bulkEditRouter.router,
-      },
+      fiches: this.trpc.mergeRouters(
+        this.countByStatutRouter.router,
+        this.bulkEditRouter.router,
+        this.ficheActionEtapeRouter.router
+      ),
     },
     collectivites: {
       personnes: this.personnes.router,
