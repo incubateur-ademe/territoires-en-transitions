092bb04ff931b495a3c1237e71e8d442478b6a74
Amandine Jacquelin <contact@jacquelinamandine.fr>
2024-12-02 12:06:15 +0100

    Enlève openapi

--- backend/src/indicateurs/indicateur-filtre/get-filtered-indicateurs.response.ts
+++ backend/src/indicateurs/indicateur-filtre/get-filtered-indicateurs.response.ts
@@ -1,17 +1,12 @@
-import { extendApi, extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { z } from 'zod';
 
-extendZodWithOpenApi(z);
-
-export const getFilteredIndicateurResponseSchema = extendApi(
-  z.object({
-    id: z.number(),
-    titre: z.string(),
-    estPerso: z.boolean(),
-    identifiant: z.string().nullable(),
-    hasOpenData: z.boolean(),
-  })
-);
+export const getFilteredIndicateurResponseSchema = z.object({
+  id: z.number(),
+  titre: z.string(),
+  estPerso: z.boolean(),
+  identifiant: z.string().nullable(),
+  hasOpenData: z.boolean(),
+});
 
 export type GetFilteredIndicateurResponseType = z.infer<
   typeof getFilteredIndicateurResponseSchema
