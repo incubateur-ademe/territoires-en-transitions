4de86ffe3fac2df516129ee0398a222f2ca7901d
Arthur Molinos <arthur.molinos@gmail.com>
2024-12-03 10:06:37 +0100

    Adapte useFilteredIndicateursDefinitions avec la nouvelle fonction tRPC

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/useFilteredIndicateurDefinitions.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/lists/useFilteredIndicateurDefinitions.ts
@@ -1,9 +1,8 @@
-import { Indicateurs } from '@tet/api';
 import { FetchOptions } from '@tet/api/indicateurs/domain';
-import { DISABLE_AUTO_REFETCH, supabaseClient } from 'core-logic/api/supabase';
+import { trpc } from '@tet/api/utils/trpc/client';
+import { DISABLE_AUTO_REFETCH } from 'core-logic/api/supabase';
 import { useCollectiviteId } from 'core-logic/hooks/params';
 import Fuse from 'fuse.js';
-import { useQuery } from 'react-query';
 
 /**
  * Charge la liste d'indicateurs en fonction du filtre donné
@@ -14,45 +13,54 @@ export const useFilteredIndicateurDefinitions = (
   options: FetchOptions,
   disableAutoRefresh?: boolean
 ) => {
-  const collectivite_id = useCollectiviteId();
+  const collectiviteId = useCollectiviteId();
 
   // état par défaut pour supporter les anciens appels (infinite scroll)
   const disableRefresh = disableAutoRefresh ?? true;
 
-  return useQuery(
-    ['indicateur_definitions', collectivite_id, options],
-    async () => {
-      if (!collectivite_id) return [];
+  //A search which starts by # is an identifier search done on backend side
+  const textSearch =
+    options.filtre?.text && !options.filtre?.text.startsWith('#')
+      ? options.filtre?.text
+      : null;
+  if (textSearch) {
+    delete options.filtre?.text; // Delete it, search is done locally for now due to backend reasons
+  }
 
-      //A search which starts by # is an identifier search done on backend side
-      const textSearch =
-        options.filtre?.text && !options.filtre?.text.startsWith('#')
-          ? options.filtre?.text
-          : null;
-      if (textSearch) {
-        delete options.filtre?.text; // Delete it, search is done locally for now due to backend reasons
-      }
-      const { data, error } = await Indicateurs.fetchFilteredIndicateurs(
-        supabaseClient,
-        collectivite_id,
-        options
-      );
-
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      if (textSearch) {
-        const fuse = new Fuse(data, {
-          keys: ['titre'],
-          threshold: 0.3,
-          shouldSort: false,
-        });
-        return fuse.search(textSearch).map((r) => r.item);
-      } else {
-        return data;
-      }
+  const { data, error, isLoading } = trpc.indicateurs.filtre.list.useQuery(
+    {
+      collectiviteId: collectiviteId!,
+      filtre: options.filtre ?? {},
+      queryOptions: {
+        page: options.page,
+        limit: options.limit,
+        sort: options.sort,
+      },
     },
     disableRefresh ? DISABLE_AUTO_REFETCH : {}
   );
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const returnedObject = { data: data ?? [], isLoading };
+
+  if (!data) {
+    return returnedObject;
+  }
+
+  if (textSearch) {
+    const fuse = new Fuse(data, {
+      keys: ['titre'],
+      threshold: 0.3,
+      shouldSort: false,
+    });
+    return {
+      ...returnedObject,
+      data: fuse.search(textSearch).map((r) => r.item),
+    };
+  } else {
+    return { ...returnedObject, data };
+  }
 };
