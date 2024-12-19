31a150ef5a9488e739a77640dba0f744f838eb2a
Arthur Molinos <arthur.molinos@gmail.com>
2024-12-19 13:46:08 +0100

    Ajoute des conditions d'affichage pour les informations de modification et création d'une fiche action

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/Header/Header.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/Header/Header.tsx
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 
 import { FicheAction } from '@/api/plan-actions';
 
-import { Icon } from '@/ui';
+import { Divider, Icon } from '@/ui';
 
 import CheminsFiche from './CheminsFiche';
 import TitreFiche from './TitreFiche';
@@ -29,25 +29,33 @@ const Header = ({ fiche, updateTitle, isReadonly }: FicheActionHeaderProps) => {
       <CheminsFiche titre={titre} collectiviteId={collectiviteId} axes={axes} />
 
       {/* Création et modification de la fiche */}
-      <div className="flex max-md:flex-col gap-2 items-center mt-3 py-3 text-sm text-grey-8 border-y boder-primary-3">
-        {fiche.modifiedAt && (
+      {fiche.modifiedBy || fiche.createdBy ? (
+        <div className="flex max-md:flex-col gap-2 items-center mt-3 py-3 text-sm text-grey-8 border-y boder-primary-3">
           <div className="flex gap-1">
             <Icon icon="calendar-2-line" size="sm" />
-            Modifiée le {format(
-              new Date(fiche.modifiedAt),
-              'dd/MM/yyyy'
-            )} par {fiche.modifiedBy?.prenom} {fiche.modifiedBy?.nom}
+            Modifiée{' '}
+            {fiche.modifiedAt
+              ? `le ${format(new Date(fiche.modifiedAt), 'dd/MM/yyyy')}`
+              : ''}{' '}
+            {fiche.modifiedBy
+              ? `par ${fiche.modifiedBy?.prenom} ${fiche.modifiedBy?.nom}`
+              : ''}
           </div>
-        )}
-        <div className="max-md:hidden w-[1px] h-5 bg-grey-5" />
-        {fiche.createdAt && (
+          <div className="max-md:hidden w-[1px] h-5 bg-grey-5" />
           <div className="flex gap-1">
             <Icon icon="file-add-line" size="sm" />
-            Créée le {format(new Date(fiche.createdAt), 'dd/MM/yyyy')} par{' '}
-            {fiche.createdBy?.prenom} {fiche.createdBy?.nom}
+            Créée{' '}
+            {fiche.createdAt
+              ? `le ${format(new Date(fiche.createdAt), 'dd/MM/yyyy')}`
+              : ''}{' '}
+            {fiche.createdBy
+              ? `par ${fiche.createdBy?.prenom} ${fiche.createdBy?.nom}`
+              : ''}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <Divider />
+      )}
     </div>
   );
 };
