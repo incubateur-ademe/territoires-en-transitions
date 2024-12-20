2ad33253c5f3d9f9507b9abaf9d1b3cc3866b25d
Fred <98240+farnoux@users.noreply.github.com>
2024-12-20 19:03:12 +0100

    Fix backend build (due to drizzle upgrade)

--- backend/src/fiches/models/axe.table.ts
+++ backend/src/fiches/models/axe.table.ts
@@ -1,19 +1,25 @@
 import { InferInsertModel } from 'drizzle-orm';
-import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
+import {
+  AnyPgColumn,
+  integer,
+  pgTable,
+  serial,
+  text,
+} from 'drizzle-orm/pg-core';
 import { createSelectSchema } from 'drizzle-zod';
 import { collectiviteTable } from '../../collectivites/models/collectivite.table';
 import { panierTable } from '../../panier/models/panier.table';
 import { createdAt, modifiedAt, modifiedBy } from '../../utils/column.utils';
 import { planActionTypeTable } from './plan-action-type.table';
 
-export const axeTable: ReturnType<typeof pgTable> = pgTable('axe', {
+export const axeTable = pgTable('axe', {
   id: serial('id').primaryKey(),
   nom: text('nom'),
   collectiviteId: integer('collectivite_id')
     .notNull()
     .references(() => collectiviteTable.id),
-  parent: integer('parent').references(() => axeTable.id),
-  plan: integer('plan').references(() => axeTable.id),
+  parent: integer('parent').references((): AnyPgColumn => axeTable.id),
+  plan: integer('plan').references((): AnyPgColumn => axeTable.id),
   type: integer('type').references(() => planActionTypeTable.id),
   createdAt,
   modifiedAt,
