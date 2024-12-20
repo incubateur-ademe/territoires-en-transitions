6040a1fc4be6c09d9e4f8f18097c073a748ce532
Fred <98240+farnoux@users.noreply.github.com>
2024-12-20 14:04:28 +0100

    Les transactions Drizzle supportent l'authentification Supabase et donc les RLS 🎉
    Permet notamment de gérer la bonne mise à jour du champ `modified_by` de la table `fiche_action`.

--- backend/src/common/services/database.service.ts
+++ backend/src/common/services/database.service.ts
@@ -1,24 +1,60 @@
+import { AuthUser } from '@/backend/auth/models/auth.models';
 import { Injectable, Logger, OnApplicationShutdown } from '@nestjs/common';
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import { default as postgres } from 'postgres';
 import ConfigurationService from '../../config/configuration.service';
 
 @Injectable()
 export default class DatabaseService implements OnApplicationShutdown {
   private readonly logger = new Logger(DatabaseService.name);
-  public readonly db: PostgresJsDatabase;
-  private readonly client: postgres.Sql;
 
-  constructor(private readonly configService: ConfigurationService) {
-    this.logger.log(`Initializing database service`);
-    this.client = postgres(this.configService.get('SUPABASE_DATABASE_URL'), {
+  private readonly client = postgres(
+    this.configService.get('SUPABASE_DATABASE_URL'),
+    {
       prepare: false,
       connection: {
         application_name: `Backend ${process.env.APPLICATION_VERSION}`,
       },
-    });
+    }
+  );
 
-    this.db = drizzle(this.client);
+  public readonly db = drizzle(this.client);
+
+  constructor(private readonly configService: ConfigurationService) {
+    this.logger.log(`Initializing database service`);
+  }
+
+  // Taken from https://orm.drizzle.team/docs/rls
+  rls(user: AuthUser) {
+    return (async (transaction, ...rest) => {
+      return await this.db.transaction(async (tx) => {
+        // Supabase exposes auth.uid() and auth.jwt()
+        // https://supabase.com/docs/guides/database/postgres/row-level-security#helper-functions
+        try {
+          await tx.execute(sql`
+          -- auth.jwt()
+          select set_config('request.jwt.claims', '${sql.raw(
+            JSON.stringify(user.jwtToken)
+          )}', TRUE);
+          -- auth.uid()
+          select set_config('request.jwt.claim.sub', '${sql.raw(
+            user.jwtToken.sub ?? ''
+          )}', TRUE);
+          -- set local role
+          set local role ${sql.raw(user.jwtToken.role ?? 'anon')};
+          `);
+          return await transaction(tx);
+        } finally {
+          await tx.execute(sql`
+            -- reset
+            select set_config('request.jwt.claims', NULL, TRUE);
+            select set_config('request.jwt.claim.sub', NULL, TRUE);
+            reset role;
+          `);
+        }
+      }, ...rest);
+    }) as typeof this.db.transaction;
   }
 
   async onApplicationShutdown(signal: string) {
