c4a4a3ba570f934b67cc1a2c1a349ae8ec375bbb
Amandine Jacquelin <contact@jacquelinamandine.fr>
2025-01-14 15:03:54 +0100

    Ajout save-score.modal.tsx, save-score.button.tsx et useSaveScore.ts

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/SaveScore/save-score.modal.tsx
@@ -0,0 +1,159 @@
+import { Button, Field, Input, Modal, Alert, ButtonGroup } from '@/ui';
+import { ReactNode, useRef, useState } from 'react';
+import { getIsoFormattedDate } from '@/app/utils/formatUtils';
+import { useSaveScore } from '@/app/app/pages/collectivite/Referentiels/SaveScore/useSaveScore';
+import { useBaseToast } from '@/app/core-logic/hooks/useBaseToast';
+import { RouterInput } from '@/api/utils/trpc/client';
+
+type computeScoreType = RouterInput['referentiels']['scores']['computeScore'];
+export type SaveScoreProps = {
+  referentielId: string;
+  collectiviteId: number;
+};
+
+const SaveScoreModal = ({
+  referentielId,
+  collectiviteId,
+  children,
+}: SaveScoreProps & {
+  children: ReactNode;
+}) => {
+  const [selectedButton, setSelectedButton] = useState<string>('now');
+  const [nomVersion, setNomVersion] = useState<string | undefined>();
+  const [dateVersion, setDateVersion] = useState<string | undefined>();
+  const { renderToast, setToast } = useBaseToast();
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  const getDisplayedYear = (): string => {
+    if (selectedButton === 'before' && dateVersion) {
+      return new Date(dateVersion).getFullYear().toString();
+    }
+    return new Date().getFullYear().toString();
+  };
+
+  const finalDateVersion = dateVersion || getIsoFormattedDate('');
+  const finalNomVersion = `${getDisplayedYear()} - ${nomVersion?.trim()}`;
+
+  const { refetch, isFetching } = useSaveScore(
+    referentielId as computeScoreType["referentielId"],
+    collectiviteId,
+    finalDateVersion,
+    finalNomVersion,
+  );
+
+  const handleSave = async () => {
+    if (!nomVersion?.trim()) return;
+    const result = await refetch();
+
+    if (result.error) {
+      if (result.error.message.includes('existe déjà')) {
+        setToast(
+          'error',
+          `Une sauvegarde du référentiel à la date ${
+            dateVersion ? dateVersion : `d'aujourd'hui`
+          } existe déjà.`,
+          5000
+        );
+      } else {
+        setToast('error', `Erreur lors de la sauvegarde`, 5000);
+      }
+    }
+    if (result.isSuccess) {
+      setToast('success', 'Référentiel sauvegardé', 5000);
+    }
+  };
+
+  return (
+    <>
+      {renderToast()}
+      <Modal
+        title="Figer le référentiel"
+        size="md"
+        render={({ descriptionId, close }) => (
+          <div id={descriptionId} className="space-y-6">
+            {/*Info */}
+            <Alert
+              description="Une sauvegarde sera automatiquement réalisée lors du démarrage d'un audit et lors de la clôture d'un audit.
+          Une sauvegarde sera proposée lors du dépôt du rapport de visite annuelle.
+          Vous pouvez figer le référentiel à la fin de l'état des lieux initial (ex: Etat-des-lieux-initial) ou à un autre moment clé (ex: pre-visite-annuelle), etc."
+              rounded
+            />
+            {/* Choix entre 'Date d'aujourd'hui' et 'A une date antérieure' */}
+            <ButtonGroup
+              size="xs"
+              buttons={[
+                {
+                  children: `Date d'aujourd'hui`,
+                  id: 'now',
+                  onClick: () => {
+                    setSelectedButton('now');
+                    setDateVersion(undefined);
+                  },
+                },
+                {
+                  children: `À une date antérieure`,
+                  id: 'before',
+                  onClick: () => setSelectedButton('before'),
+                },
+              ]}
+              fillContainer
+              activeButtonId={selectedButton}
+            />
+            {/* Date de la version à figer */}
+            {selectedButton === 'before' && (
+              <Field title="Date de la version à figer">
+                <Input
+                  type="date"
+                  max={getIsoFormattedDate('')}
+                  ref={ref}
+                  value={dateVersion}
+                  onChange={(e) => setDateVersion(e.target.value)}
+                />
+              </Field>
+            )}
+            {/* Nom de la version à enregistrer */}
+            <Field title="Nom de la version à enregistrer">
+              <div className="flex items-center border border-grey-4 rounded-lg bg-grey-1 focus-within:border-primary-5">
+                <span className="text-sm px-3 py-3 text-primary-7 border-r border-grey-4">
+                  {getDisplayedYear()} -
+                </span>
+                <Input
+                  type="text"
+                  placeholder="Entrez le nom de la version"
+                  containerClassname="flex-grow border-none"
+                  value={nomVersion}
+                  onChange={(e) => setNomVersion(e.target.value)}
+                />
+              </div>
+            </Field>
+            {/* Boutons annuler et valider */}
+            <div className="flex gap-4 justify-end">
+              <Button variant="grey" size="sm" onClick={close}>
+                Annuler
+              </Button>
+              <Button
+                variant="primary"
+                size="sm"
+                disabled={
+                  isFetching ||
+                  !nomVersion?.trim() ||
+                  (selectedButton !== 'now' && !dateVersion)
+                }
+                onClick={() => {
+                  handleSave();
+                }}
+              >
+                {isFetching ? 'Sauvegarde en cours...' : 'Figer cette version'}
+              </Button>
+            </div>
+          </div>
+        )}
+      >
+        {children as React.ReactElement}
+      </Modal>
+    </>
+  );
+};
+
+export default SaveScoreModal;
