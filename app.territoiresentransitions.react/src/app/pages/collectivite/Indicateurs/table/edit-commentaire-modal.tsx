d2b62fbec0c1cb329efdbaabb876a17a6337f9ea
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 16:21:38 +0100

    Ajoute la modale d'édition d'un commentaire d'une valeur d'un indicateur

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/edit-commentaire-modal.tsx
@@ -0,0 +1,61 @@
+import { Field, Modal, ModalFooterOKCancel, Textarea } from '@/ui';
+import { OpenState } from '@/ui/utils/types';
+import { useState } from 'react';
+import { getSourceTypeLabel } from '../constants';
+import { SourceType, TIndicateurDefinition } from '../types';
+
+export type EditCommentaireModalProps = {
+  annee: number;
+  commentaire: string;
+  definition: TIndicateurDefinition;
+  type: SourceType;
+  openState: OpenState;
+  onChange: (commentaire: string) => void;
+};
+
+/**
+ * Affiche la modale d'édition d'un commentaire d'une valeur d'un indicateur
+ */
+export const EditCommentaireModal = (props: EditCommentaireModalProps) => {
+  const {
+    definition,
+    annee,
+    commentaire: commentaireInitial,
+    type,
+    openState,
+    onChange,
+  } = props;
+  const [commentaire, setCommentaire] = useState<string | null>(
+    commentaireInitial
+  );
+
+  return (
+    <Modal
+      openState={openState}
+      disableDismiss
+      title={`Mes ${getSourceTypeLabel(type)} (${definition.unite}) : ${annee}`}
+      render={() => {
+        return (
+          <Field title="Commentaire">
+            <Textarea
+              rows={10}
+              value={commentaire ?? ''}
+              onChange={(e) => setCommentaire(e.target.value)}
+            />
+          </Field>
+        );
+      }}
+      renderFooter={({ close }) => (
+        <ModalFooterOKCancel
+          btnOKProps={{
+            onClick: () => {
+              onChange(commentaire ?? '');
+              close();
+            },
+          }}
+          btnCancelProps={{ onClick: close }}
+        />
+      )}
+    />
+  );
+};
