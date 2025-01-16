e07a2ff76450386690912ea318572666e56f1671
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 15:51:46 +0100

    Ajoute les fonctions CRUD des valeurs indicateur depuis tRPC

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/use-upsert-indicateur-valeur.ts
@@ -0,0 +1,26 @@
+import { trpc } from '@/api/utils/trpc/client';
+import { TIndicateurDefinition } from '../types';
+import { useInvalidateValeurs } from './use-invalidate-valeurs';
+
+export const useUpsertIndicateurValeur = (
+  definition: TIndicateurDefinition
+) => {
+  const utils = trpc.useUtils();
+  const invalidate = useInvalidateValeurs(definition);
+
+  return trpc.indicateurs.valeurs.upsert.useMutation({
+    onSuccess: (data, variables) => {
+      const { collectiviteId, indicateurId } = variables;
+      if (collectiviteId && indicateurId) {
+        utils.indicateurs.valeurs.list.invalidate({
+          collectiviteId,
+          indicateurIds: [indicateurId],
+        });
+
+        // TODO: à supprimer quand tous les composants utiliseront
+        // seulement l'appel trpc pour lire les données
+        invalidate({ collectiviteId, indicateurId });
+      }
+    },
+  });
+};
