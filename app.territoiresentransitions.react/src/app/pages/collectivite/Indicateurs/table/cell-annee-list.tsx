872bef43575c8a60d0bad67e155158c5e600a87a
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 16:21:01 +0100

    Ajoute le composant pour l'affichage des années dans l'en-tête du tableau

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/cell-annee-list.tsx
@@ -0,0 +1,94 @@
+import { IndicateurDefinition } from '@/api/indicateurs/domain/definition.schema';
+import { Button, HEAD_CELL_STYLE, Notification, TCell, Tooltip } from '@/ui';
+import classNames from 'classnames';
+import { useState } from 'react';
+import { SourceType } from '../types';
+import { ConfirmDelete } from './confirm-delete';
+import { PreparedData } from './prepare-data';
+import { IndicateurSourceValeur } from './use-indicateur-valeurs';
+
+type CellAnneeListProps = {
+  confidentiel?: boolean;
+  data: PreparedData;
+  definition: IndicateurDefinition;
+  type: SourceType;
+  onDelete: (valeur: IndicateurSourceValeur) => void;
+};
+
+/** Affiche les cellules des années dans l'en-tête du tableau */
+export const CellAnneeList = ({
+  confidentiel,
+  data,
+  definition,
+  type,
+  onDelete,
+}: CellAnneeListProps) => {
+  const { annees, anneeModePrive, valeursExistantes } = data;
+  const [toBeDeleted, setToBeDeleted] = useState<IndicateurSourceValeur | null>(
+    null
+  );
+
+  return annees?.map((annee) => {
+    const valeur = valeursExistantes.find((v) => v.annee === annee);
+
+    return (
+      <TCell
+        key={annee}
+        className={classNames(
+          HEAD_CELL_STYLE,
+          'font-bold text-center relative min-w-40'
+        )}
+      >
+        {confidentiel && type === 'resultat' && annee === anneeModePrive && (
+          <ResultatModePrive />
+        )}
+        <div className="flex items-center justify-between">
+          <span className="w-full">{annee}</span>
+          {valeur && (
+            <Button
+              className="!bg-transparent !border-none"
+              icon="delete-bin-6-line"
+              variant="outlined"
+              size="xs"
+              onClick={() => {
+                // demande confirmation avant de supprimer
+                if (
+                  (valeur.objectif ?? false) ||
+                  (valeur.resultat ?? false) ||
+                  valeur.resultatCommentaire ||
+                  valeur.objectifCommentaire
+                ) {
+                  setToBeDeleted(valeur);
+                } else {
+                  // sauf pour les lignes n'ayant ni valeur ni commentaire
+                  onDelete(valeur);
+                }
+              }}
+            />
+          )}
+        </div>
+        {toBeDeleted && (
+          <ConfirmDelete
+            annee={annee}
+            valeur={toBeDeleted}
+            unite={definition.unite}
+            onDismissConfirm={(confirmed) => {
+              if (confirmed) {
+                onDelete(toBeDeleted);
+              }
+              setToBeDeleted(null);
+            }}
+          />
+        )}
+      </TCell>
+    );
+  });
+};
+
+const ResultatModePrive = () => (
+  <Tooltip label={<p className="min-w-max">Le résultat est en mode privé</p>}>
+    <div className="absolute top-3 left-5">
+      <Notification icon="lock-fill" classname="!h-10 w-10" />
+    </div>
+  </Tooltip>
+);
