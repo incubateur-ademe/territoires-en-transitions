092cb011443ecf7aef0032a8b0cf4082dd9271eb
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 16:21:55 +0100

    Ajoute la modale d'édition des valeurs d'un indicateur

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/edit-valeurs-modal.tsx
@@ -0,0 +1,138 @@
+import { Button, Divider, Field, Input, Modal, ModalFooter } from '@/ui';
+import { OpenState } from '@/ui/utils/types';
+import { useState } from 'react';
+import { TIndicateurDefinition } from '../types';
+import { InputValue } from './input-value';
+import { PreparedData } from './prepare-data';
+import { IndicateurSourceValeur } from './use-indicateur-valeurs';
+import { useUpsertIndicateurValeur } from './use-upsert-indicateur-valeur';
+
+export type EditValeursModalProps = {
+  collectiviteId: number;
+  definition: TIndicateurDefinition;
+  openState: OpenState;
+  data: PreparedData;
+};
+
+/**
+ * Affiche la modale d'édition des valeurs d'un indicateur
+ */
+export const EditValeursModal = (props: EditValeursModalProps) => {
+  const { collectiviteId, data, definition, openState } = props;
+  const { valeursExistantes } = data;
+
+  const { mutate: upsertValeur, isPending } =
+    useUpsertIndicateurValeur(definition);
+  const [valeur, setValeur] = useState<Partial<IndicateurSourceValeur>>({});
+  const [annee, setAnnee] = useState<number | null>(null);
+  const { objectif, objectifCommentaire, resultat, resultatCommentaire } =
+    valeur || {};
+
+  const disabled = !annee || !(objectif || resultat) || isPending;
+
+  const upsert = () =>
+    upsertValeur({
+      id: valeur.id,
+      collectiviteId,
+      indicateurId: definition.id,
+      dateValeur: `${annee}-01-01`,
+      resultat,
+      resultatCommentaire,
+      objectif,
+      objectifCommentaire,
+    });
+
+  const reset = () => {
+    setAnnee(annee ? annee + 1 : null);
+    setValeur({});
+  };
+
+  return (
+    <Modal
+      size="lg"
+      openState={openState}
+      title="Compléter le tableau"
+      render={() => {
+        return (
+          <>
+            <Field title="Année">
+              <Input
+                type="text"
+                value={annee?.toString() ?? ''}
+                onChange={(e) => setAnnee(parseInt(e.target.value, 10))}
+                onBlur={() => {
+                  if (!annee) return;
+                  const valeurExistante = valeursExistantes.find(
+                    (v) => v.annee === annee
+                  );
+                  setValeur(valeurExistante || {});
+                }}
+              />
+            </Field>
+
+            <Divider />
+
+            <Field title="Objectif" className="-mt-8">
+              <InputValue
+                value={objectif ?? ''}
+                onChange={(value) => setValeur({ ...valeur, objectif: value })}
+              />
+            </Field>
+            <Field title="Ajouter un commentaire sur l’objectif">
+              <Input
+                type="text"
+                value={objectifCommentaire ?? ''}
+                onChange={(e) =>
+                  setValeur({ ...valeur, objectifCommentaire: e.target.value })
+                }
+              />
+            </Field>
+
+            <Divider />
+
+            <Field title="Résultat" className="-mt-8">
+              <InputValue
+                value={resultat ?? ''}
+                onChange={(value) => setValeur({ ...valeur, resultat: value })}
+              />
+            </Field>
+            <Field title="Ajouter un commentaire sur le résultat">
+              <Input
+                type="text"
+                value={resultatCommentaire ?? ''}
+                onChange={(e) =>
+                  setValeur({ ...valeur, resultatCommentaire: e.target.value })
+                }
+              />
+            </Field>
+          </>
+        );
+      }}
+      renderFooter={({ close }) => (
+        <ModalFooter>
+          <Button variant="outlined" onClick={close}>
+            Annuler
+          </Button>
+          <Button
+            disabled={disabled}
+            onClick={() => {
+              upsert();
+              reset();
+            }}
+          >
+            Valider et ajouter une année
+          </Button>
+          <Button
+            disabled={disabled}
+            onClick={() => {
+              upsert();
+              close();
+            }}
+          >
+            Valider
+          </Button>
+        </ModalFooter>
+      )}
+    />
+  );
+};
