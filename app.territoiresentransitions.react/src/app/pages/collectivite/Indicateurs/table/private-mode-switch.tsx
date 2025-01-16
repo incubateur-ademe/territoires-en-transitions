22641cb8201ce94c54f4df56e6450a40214aa493
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 16:24:47 +0100

    Ajoute le bouton "Résultat récent en mode privé"

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/private-mode-switch.tsx
@@ -0,0 +1,44 @@
+import { useCurrentCollectivite } from '@/app/core-logic/hooks/useCurrentCollectivite';
+import { Checkbox, Tooltip } from '@/ui';
+import { useToggleIndicateurConfidentiel } from '../Indicateur/detail/useToggleIndicateurConfidentiel';
+import { TIndicateurDefinition } from '../types';
+
+/** Affiche le bouton "Résultat récent en mode privé" */
+export const PrivateModeSwitch = ({
+  definition,
+}: {
+  definition: TIndicateurDefinition;
+}) => {
+  const collectivite = useCurrentCollectivite();
+  const isReadonly = !collectivite || collectivite.readonly;
+  const { mutate: toggleIndicateurConfidentiel, isLoading } =
+    useToggleIndicateurConfidentiel(definition);
+  const { confidentiel } = definition;
+
+  return (
+    !isReadonly && (
+      <div className="flex my-4">
+        <Tooltip
+          label="Si le mode privé est activé, le résultat le plus récent n'est plus
+              consultable par les personnes n’étant pas membres de votre
+              collectivité. Seuls les autres résultats restent accessibles pour
+              tous les utilisateurs et la valeur privée reste consultable par
+              l’ADEME et le service support de la plateforme."
+        >
+          {/** Permet de prendre en compte la checkbox + le label (autrement uniquement la checkbox trigger le tooltip) */}
+          <div>
+            <Checkbox
+              variant="switch"
+              label="Résultat récent en mode privé"
+              checked={confidentiel}
+              disabled={isLoading}
+              onChange={() =>
+                toggleIndicateurConfidentiel(confidentiel || false)
+              }
+            />
+          </div>
+        </Tooltip>
+      </div>
+    )
+  );
+};
