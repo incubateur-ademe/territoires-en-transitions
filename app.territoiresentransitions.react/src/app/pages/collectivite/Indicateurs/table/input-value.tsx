8e30cbb63995d97ca8ee5ad281da76ea95c0f55e
Marc Rutkowski <marc@attractive-media.fr>
2025-01-16 16:18:20 +0100

    Ajoute les composants pour l'affichage et la saisie d'une valeur d'indicateur

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/input-value.tsx
@@ -0,0 +1,36 @@
+import { Input } from '@/ui';
+import classNames from 'classnames';
+
+export type InputValueProps = {
+  className?: string;
+  value: number | '';
+  displaySize?: 'sm' | 'md';
+  disabled?: boolean;
+  onChange?: (value: number | null) => void;
+};
+
+/**
+ * Affiche un champ pour la saisie d'une valeur numérique
+ */
+export const InputValue = (props: InputValueProps) => {
+  const { className, disabled, displaySize, value, onChange } = props;
+  return (
+    <Input
+      className={classNames('w-fit', className)}
+      disabled={disabled}
+      displaySize={displaySize}
+      type="number"
+      numType="float"
+      value={value?.toString() ?? ''}
+      onBlur={(e) => {
+        const parsedValue = parseFloat(
+          e.currentTarget.value.trim().replaceAll(',', '.').replaceAll(' ', '')
+        );
+        const newValue = isNaN(parsedValue) ? null : parsedValue;
+        if (newValue !== (value ?? null)) {
+          onChange?.(newValue);
+        }
+      }}
+    />
+  );
+};
