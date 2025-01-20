02ceda45aff2488f8a4dc35d1bfea4ee2fd3e903
Fred <98240+farnoux@users.noreply.github.com>
2025-01-20 12:05:39 +0100

    Change env var name for supabase key
    Corresponding to the name existing in `.env.example`

--- api_tests/lib/supabase.ts
+++ api_tests/lib/supabase.ts
@@ -1,20 +1,20 @@
-import "https://deno.land/x/dotenv/load.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.0";
-import { Database } from "./database.types.ts";
+import 'https://deno.land/x/dotenv/load.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.0';
+import { Database } from './database.types.ts';
 
 /**
  * Supabase client
  */
 export const supabase = createClient<Database>(
-  Deno.env.get("SUPABASE_URL")!,
-  Deno.env.get("SUPABASE_KEY")!,
+  Deno.env.get('SUPABASE_URL')!,
+  Deno.env.get('SUPABASE_ANON_KEY')!,
   {
     db: {
-      schema: "public",
+      schema: 'public',
     },
     auth: {
       // Pour fonctionner avec deno hors du navigateur.
       persistSession: false,
     },
-  },
+  }
 );
