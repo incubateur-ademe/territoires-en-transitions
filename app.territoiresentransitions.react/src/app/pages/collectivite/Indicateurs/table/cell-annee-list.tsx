34ef7bf9c30fce2fde63caa294193c7b7dcbb152
Marc Rutkowski <marc@attractive-media.fr>
2025-01-21 12:02:48 +0100

    Evite de dupliquer le dialogue de confirmation de la suppression

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/cell-annee-list.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/table/cell-annee-list.tsx
@@ -1,9 +1,7 @@
 import { IndicateurDefinition } from '@/api/indicateurs/domain/definition.schema';
 import { Button, HEAD_CELL_STYLE, Notification, TCell, Tooltip } from '@/ui';
 import classNames from 'classnames';
-import { useState } from 'react';
 import { SourceType } from '../types';
-import { ConfirmDelete } from './confirm-delete';
 import { PreparedData, PreparedValue } from './prepare-data';
 
 type CellAnneeListProps = {
@@ -19,13 +17,11 @@ type CellAnneeListProps = {
 export const CellAnneeList = ({
   confidentiel,
   data,
-  definition,
   readonly,
   type,
   onDelete,
 }: CellAnneeListProps) => {
   const { annees, anneeModePrive, valeursExistantes } = data;
-  const [toBeDeleted, setToBeDeleted] = useState<PreparedValue | null>(null);
 
   return annees?.map((annee) => {
     const valeur = valeursExistantes.find((v) => v.annee === annee);
@@ -50,35 +46,10 @@ export const CellAnneeList = ({
               icon="delete-bin-6-line"
               variant="outlined"
               size="xs"
-              onClick={() => {
-                // demande confirmation avant de supprimer
-                if (
-                  (valeur.objectif ?? false) ||
-                  (valeur.resultat ?? false) ||
-                  valeur.resultatCommentaire ||
-                  valeur.objectifCommentaire
-                ) {
-                  setToBeDeleted(valeur);
-                } else {
-                  // sauf pour les lignes n'ayant ni valeur ni commentaire
-                  onDelete(valeur);
-                }
-              }}
+              onClick={() => onDelete(valeur)}
             />
           )}
         </div>
-        {toBeDeleted && (
-          <ConfirmDelete
-            valeur={toBeDeleted}
-            unite={definition.unite}
-            onDismissConfirm={(confirmed) => {
-              if (confirmed) {
-                onDelete(toBeDeleted);
-              }
-              setToBeDeleted(null);
-            }}
-          />
-        )}
       </TCell>
     );
   });
