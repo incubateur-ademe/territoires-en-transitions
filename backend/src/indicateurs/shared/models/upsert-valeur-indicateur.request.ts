f0ccfecc397efe6298886bd68108c5fa5673b9ae
Marc Rutkowski <marc@attractive-media.fr>
2025-01-22 16:41:25 +0100

    Simplifie le typage

--- backend/src/indicateurs/shared/models/upsert-valeur-indicateur.request.ts
+++ backend/src/indicateurs/shared/models/upsert-valeur-indicateur.request.ts
@@ -2,22 +2,19 @@ import { z } from 'zod';
 import { indicateurValeurSchemaInsert } from './indicateur-valeur.table';
 
 /** Upsert d'une valeur d'indicateur pour une collectivité */
-const shape = indicateurValeurSchemaInsert.shape;
-const upsertValeurIndicateurSchema = indicateurValeurSchemaInsert
+export const upsertValeurIndicateurSchema = indicateurValeurSchemaInsert
   .pick({
     collectiviteId: true,
     indicateurId: true,
+    id: true,
+    resultat: true,
+    resultatCommentaire: true,
+    objectif: true,
+    objectifCommentaire: true,
   })
-  .merge(
-    z.object({
-      id: shape.id.optional(), // pour un update
-      dateValeur: shape.dateValeur.optional(), // pour un insert
-      resultat: shape.resultat.nullish(),
-      resultatCommentaire: shape.resultatCommentaire.nullish(),
-      objectif: shape.objectif.nullish(),
-      objectifCommentaire: shape.objectifCommentaire.nullish(),
-    })
-  );
+  .extend({
+    dateValeur: indicateurValeurSchemaInsert.shape.dateValeur.optional(),
+  });
 
 export type UpsertValeurIndicateur = z.infer<
   typeof upsertValeurIndicateurSchema
