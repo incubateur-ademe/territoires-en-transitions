d5f0c64691f4db03adeaa537e75db08cf0e091ed
Fred <98240+farnoux@users.noreply.github.com>
2025-01-22 16:39:25 +0100

    Supprime les triggers de recalcul des snapshots

--- data_layer/sqitch/deploy/referentiel/score_snapshot@v4.36.0.sql
+++ data_layer/sqitch/deploy/referentiel/score_snapshot@v4.36.0.sql
@@ -2,21 +2,49 @@
 
 BEGIN;
 
--- Recrée la fonction supprimée
+CREATE TABLE IF NOT EXISTS "score_snapshot" (
+	"collectivite_id" integer NOT NULL,
+	"referentiel_id" varchar(30) NOT NULL,
+	"referentiel_version" varchar(16) NOT NULL,
+	"audit_id" integer,
+	"date" timestamp with time zone NOT NULL,
+	"ref" varchar(30) NOT NULL,
+	"nom" varchar(300) NOT NULL,
+	"type_jalon" varchar(30) NOT NULL,
+	"point_fait" double precision NOT NULL,
+	"point_programme" double precision NOT NULL,
+	"point_pas_fait" double precision NOT NULL,
+	"point_potentiel" double precision NOT NULL,
+	"referentiel_scores" jsonb NOT NULL,
+	"personnalisation_reponses" jsonb NOT NULL,
+	"created_by" uuid,
+	"created_at" timestamp with time zone  DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"modified_by" uuid,
+	"modified_at" timestamp with time zone  DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT "score_snapshot_collectivite_id_referentiel_id_ref_pkey" PRIMARY KEY("collectivite_id","referentiel_id", "ref"),
+	CONSTRAINT "score_snapshot_collectivite_id_referentiel_id_type_jalon_date_unique" UNIQUE("collectivite_id","referentiel_id","type_jalon","date")
+);
+comment on table score_snapshot is
+    'Sauvegarde des scores des collectivités déclenchées automatiquement ou manuellement';
 
-CREATE OR REPLACE FUNCTION public.action_preuve(id action_id, OUT id action_id, OUT preuve text)
- RETURNS record
- LANGUAGE sql
- STABLE
-AS $function$
-select action_definition.action_id, action_definition.preuve
-from action_definition
-where action_definition.action_id = action_preuve.id
-  and est_verifie()
-$function$
-;
+CREATE UNIQUE INDEX score_snapshot_collectivite_id_referentiel_id_score_courant ON score_snapshot (collectivite_id, referentiel_id ) WHERE type_jalon = 'score_courant';
 
--- Recrée la function initiale
+alter table score_snapshot enable row level security;
+drop policy if exists allow_read on score_snapshot;
+create policy allow_read on score_snapshot for select using(is_authenticated());
+
+ALTER TABLE "score_snapshot" ADD CONSTRAINT "score_snapshot_collectivite_id_fkey" FOREIGN KEY ("collectivite_id") REFERENCES "public"."collectivite"("id") ON DELETE cascade ON UPDATE no action;
+
+ALTER TABLE "score_snapshot" ADD CONSTRAINT "score_snapshot_referentiel_id_fkey" FOREIGN KEY ("referentiel_id") REFERENCES "public"."referentiel_definition"("id") ON DELETE cascade ON UPDATE no action;
+
+ALTER TABLE "score_snapshot" ADD CONSTRAINT "score_snapshot_audit_id_fkey" FOREIGN KEY ("audit_id") REFERENCES "labellisation"."audit"("id") ON DELETE cascade ON UPDATE no action;
+
+create trigger modified_at
+    before insert or update
+    on score_snapshot
+    for each row
+execute procedure update_modified_at();
+
 create or replace function automatisation.save_score_snapshot(
 	collectivite_id integer,
 	referentiel referentiel,
@@ -26,7 +54,7 @@ create or replace function automatisation.save_score_snapshot(
 )
 as
 $$
-with configuration as (select
+with configuration as (select 
                         CONCAT(service_url,'/api/v1/collectivites/',collectivite_id::text,'/referentiels/', referentiel, '/scores?mode=depuis_sauvegarde&snapshot=true&snapshotForceUpdate=true&jalon=', jalon, '&auditId=', audit_id) as full_url,
                         jsonb_build_object(
                             'Authorization', CONCAT('Bearer ', token)
@@ -53,8 +81,6 @@ comment on function automatisation.save_score_snapshot
     is 'Sauvegarde des scores dans la table score_snapshot';
 
 
--- Recrée l'ancien trigger sur la table `client_scores`
-
 CREATE OR REPLACE FUNCTION public.after_client_scores_save_snapshot()
  RETURNS trigger
  LANGUAGE plpgsql
@@ -76,8 +102,6 @@ create trigger after_save_snapshot
 execute procedure after_client_scores_save_snapshot();
 
 
--- Recrée l'ancien trigger sur la table `post_audit_scores`
-
 CREATE OR REPLACE FUNCTION public.after_post_audit_scores_save_snapshot()
  RETURNS trigger
  LANGUAGE plpgsql
@@ -97,9 +121,6 @@ create trigger after_save_snapshot
     on post_audit_scores
     for each row
 execute procedure after_post_audit_scores_save_snapshot();
-
-
--- Recrée l'ancien trigger sur la table `pre_audit_scores`
 
 CREATE OR REPLACE FUNCTION public.after_pre_audit_scores_save_snapshot()
  RETURNS trigger
