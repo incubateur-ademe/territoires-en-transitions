56f7118b1488ba731dd8b23890ef8f341b9a6364
Amandine Jacquelin <contact@jacquelinamandine.fr>
2025-01-27 15:01:49 +0100

    Intègre le bouton de la modale de sauvegarde des scores sur la page référentiel avec vérification des droits.

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/SaveScore/save-score.button.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/SaveScore/save-score.button.tsx
@@ -4,7 +4,7 @@ import SaveScoreModal, { SaveScoreProps } from '@/app/app/pages/collectivite/Ref
 const SaveScoreButton = ({ referentielId, collectiviteId }: SaveScoreProps) => {
   return (
     <SaveScoreModal collectiviteId={collectiviteId} referentielId={referentielId}>
-      <Button icon="save-3-line" variant="primary" size="xs">
+      <Button icon="save-3-line" variant="primary" size="sm" className="whitespace-nowrap">
         Figer le référentiel
       </Button>
     </SaveScoreModal>
