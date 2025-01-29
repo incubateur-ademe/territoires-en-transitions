a8cfba255c1e44ef2fa59983bd746e29b7a1c91e
Marc Rutkowski <marc@attractive-media.fr>
2025-01-29 16:09:18 +0100

    Déplace la fonction d'édition de valeurs et enlève l'invalidation des anciennes requêtes

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/use-upsert-indicateur-valeur.ts
@@ -0,0 +1,17 @@
+import { trpc } from '@/api/utils/trpc/client';
+
+export const useUpsertIndicateurValeur = () => {
+  const utils = trpc.useUtils();
+
+  return trpc.indicateurs.valeurs.upsert.useMutation({
+    onSuccess: (data, variables) => {
+      const { collectiviteId, indicateurId } = variables;
+      if (collectiviteId && indicateurId) {
+        utils.indicateurs.valeurs.list.invalidate({
+          collectiviteId,
+          indicateurIds: [indicateurId],
+        });
+      }
+    },
+  });
+};
