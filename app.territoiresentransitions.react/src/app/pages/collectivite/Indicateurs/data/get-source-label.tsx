4673fc13afe9756164f5c210668f0bdc6668e4c4
Marc Rutkowski <marc@attractive-media.fr>
2025-01-29 16:12:39 +0100

    Extrait la fonction de transformation du libellé d'une source de données

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/get-source-label.tsx
@@ -0,0 +1,19 @@
+import { SOURCE_TYPE_LABEL } from '../constants';
+import { SourceType } from '../types';
+
+export const getSourceLabel = (
+  sourceId: string,
+  libelle: string,
+  type: SourceType
+) => {
+  const label =
+    SOURCE_TYPE_LABEL[type][0].toUpperCase() + SOURCE_TYPE_LABEL[type].slice(1);
+  switch (sourceId) {
+    case 'collectivite':
+      return `${label} de la collectivité`;
+    case 'snbc':
+      return `${label} SNBC territorialisée`;
+    default:
+      return `${label} ${libelle}`;
+  }
+};
