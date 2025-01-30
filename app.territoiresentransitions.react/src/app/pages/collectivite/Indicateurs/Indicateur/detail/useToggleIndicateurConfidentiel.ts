00841bb7e022d6e68d90c83ac1346790ac79a0de
Marc Rutkowski <marc@attractive-media.fr>
2025-01-30 15:13:42 +0100

    Répare l'actualisation après une mise à jour du flag confidentiel

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/detail/useToggleIndicateurConfidentiel.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/Indicateur/detail/useToggleIndicateurConfidentiel.ts
@@ -1,40 +1,43 @@
 import { Indicateurs } from '@/api';
 import { supabaseClient } from '@/api/utils/supabase/browser-client';
+import { trpc } from '@/api/utils/trpc/client';
 import { useCollectiviteId } from '@/app/core-logic/hooks/params';
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { TIndicateurDefinition } from '../../types';
 
 /** Met à jour l'état "confidentiel" d'un indicateur */
 export const useToggleIndicateurConfidentiel = (
   definition: TIndicateurDefinition
 ) => {
-  const queryClient = useQueryClient();
-  const collectivite_id = useCollectiviteId();
+  const utils = trpc.useUtils();
+  const collectiviteId = useCollectiviteId();
 
   return useMutation({
     mutationKey: 'toggle_indicateur_confidentiel',
     mutationFn: async (confidentiel: boolean) => {
-      if (collectivite_id) {
+      if (collectiviteId) {
         return Indicateurs.save.updateIndicateurDefinition(
           supabaseClient,
           {
             ...definition,
-            collectiviteId: collectivite_id,
+            collectiviteId,
             confidentiel: !confidentiel,
           },
-          collectivite_id
+          collectiviteId
         );
       }
 
       return false;
     },
-    onSuccess: (data, variables) => {
+    onSuccess: () => {
       // recharge les infos complémentaires associées à l'indicateur
-      queryClient.invalidateQueries([
-        'indicateur_definition',
-        collectivite_id,
-        definition.identifiant || definition.id,
-      ]);
+      collectiviteId &&
+        utils.indicateurs.definitions.list.invalidate({
+          collectiviteId,
+          ...(definition.identifiant
+            ? { identifiantsReferentiel: [definition.identifiant] }
+            : { indicateurIds: [definition.id] }),
+        });
     },
   });
 };
