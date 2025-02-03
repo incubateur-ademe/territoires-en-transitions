fcbb4f68ee59e85eaab0f470b0804670dd9cf48c
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2025-02-03 15:21:13 +0100

    feat: add pointNonRenseigne to score snapshots

--- backend/src/referentiels/models/get-score-snapshots.response.ts
+++ backend/src/referentiels/models/get-score-snapshots.response.ts
@@ -3,43 +3,51 @@ import { SnapshotJalon } from '../snapshots/snapshot-jalon.enum';
 import { scoreSnapshotSchema } from '../snapshots/snapshot.table';
 import { referentielIdEnumSchema } from './referentiel-id.enum';
 
-export const scoreSnapshotInfo = scoreSnapshotSchema.pick({
-  ref: true,
-  nom: true,
-  date: true,
-  typeJalon: true,
-  pointFait: true,
-  pointProgramme: true,
-  pointPasFait: true,
-  pointPotentiel: true,
-  referentielVersion: true,
-  auditId: true,
-  createdAt: true,
-  createdBy: true,
-  modifiedAt: true,
-  modifiedBy: true,
-});
+export const scoreSnapshotInfo = scoreSnapshotSchema
+  .pick({
+    ref: true,
+    nom: true,
+    date: true,
+    typeJalon: true,
+    pointFait: true,
+    pointProgramme: true,
+    pointPasFait: true,
+    pointPotentiel: true,
+    referentielVersion: true,
+    auditId: true,
+    createdAt: true,
+    createdBy: true,
+    modifiedAt: true,
+    modifiedBy: true,
+  })
+  .extend({
+    pointNonRenseigne: z.number().optional().describe('Points non renseignés'),
+  });
 
 export type ScoreSnapshotInfoType = z.infer<typeof scoreSnapshotInfo>;
 
-export const scoreSnapshotCollectiviteInfo = scoreSnapshotSchema.pick({
-  collectiviteId: true,
-  referentielId: true,
-  ref: true,
-  nom: true,
-  date: true,
-  typeJalon: true,
-  pointFait: true,
-  pointProgramme: true,
-  pointPasFait: true,
-  pointPotentiel: true,
-  referentielVersion: true,
-  auditId: true,
-  createdAt: true,
-  createdBy: true,
-  modifiedAt: true,
-  modifiedBy: true,
-});
+export const scoreSnapshotCollectiviteInfo = scoreSnapshotSchema
+  .pick({
+    collectiviteId: true,
+    referentielId: true,
+    ref: true,
+    nom: true,
+    date: true,
+    typeJalon: true,
+    pointFait: true,
+    pointProgramme: true,
+    pointPasFait: true,
+    pointPotentiel: true,
+    referentielVersion: true,
+    auditId: true,
+    createdAt: true,
+    createdBy: true,
+    modifiedAt: true,
+    modifiedBy: true,
+  })
+  .extend({
+    pointNonRenseigne: z.number().optional(),
+  });
 
 export type ScoreSnapshotCollectiviteInfoType = z.infer<
   typeof scoreSnapshotCollectiviteInfo
