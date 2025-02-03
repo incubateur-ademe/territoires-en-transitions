563bd4fcf601d5fdd2c4ddd496031f06bb86fc92
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-02-03 17:23:10 +0100

    Affiche la carte planning si le champ calendrier est rempli

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionPlanning/FicheActionPlanning.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionPlanning/FicheActionPlanning.tsx
@@ -43,7 +43,8 @@ const FicheActionPlanning = ({
     !ameliorationContinue &&
     !dateDebut &&
     !dateFinPrevisionnelle &&
-    !tempsDeMiseEnOeuvre;
+    !tempsDeMiseEnOeuvre &&
+    !justificationCalendrier;
 
   const isLate =
     dateFinPrevisionnelle &&
