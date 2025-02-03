da1527ffe30ea63522583fc879efe01fc3bfaf7b
Marc Rutkowski <marc@attractive-media.fr>
2025-02-03 16:19:04 +0100

    Revert "Désactive le batching pour le chargement des valeurs indicateurs"
    (ré-essaye avec le batching maintenant qu'on a désactivé le pooler supabase sur sandbox)
    This reverts commit 1b120ee530d6b43285b242bfb03ccf50ac39d66a.

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/use-indicateur-valeurs.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/use-indicateur-valeurs.ts
@@ -11,9 +11,5 @@ export type IndicateurSourceData = IndicateurSources['sources'][number];
 export type IndicateurSourceValeur = IndicateurSourceData['valeurs'][number];
 
 export const useIndicateurValeurs = (input: ListIndicateurValeursInput) => {
-  return trpc.indicateurs.valeurs.list.useQuery(input, {
-    trpc: {
-      context: { skipBatch: true },
-    },
-  });
+  return trpc.indicateurs.valeurs.list.useQuery(input);
 };
