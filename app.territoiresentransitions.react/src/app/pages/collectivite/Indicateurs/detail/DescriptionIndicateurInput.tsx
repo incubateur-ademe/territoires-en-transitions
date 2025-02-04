0463cfffcd5f6bb999457cf369743002cf943e27
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-02-04 11:02:30 +0100

    Fix le champ Méthodologie de calcul sur la page indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/DescriptionIndicateurInput.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/DescriptionIndicateurInput.tsx
@@ -1,4 +1,5 @@
 import { Field, Textarea } from '@/ui';
+import { useEffect, useState } from 'react';
 
 type Props = {
   description: string | undefined | null;
@@ -11,12 +12,17 @@ const DescriptionIndicateurInput = ({
   disabled,
   updateDescription,
 }: Props) => {
+  const [value, setValue] = useState(description ?? undefined);
+
+  useEffect(() => setValue(description ?? undefined), [description]);
+
   return (
     <Field title="Description et méthodologie de calcul">
       <Textarea
         data-test="desc"
         rows={3}
-        value={description ?? undefined}
+        value={value}
+        onChange={(evt) => setValue(evt.currentTarget.value)}
         disabled={disabled}
         onBlur={(evt) => updateDescription(evt.currentTarget.value)}
       />
