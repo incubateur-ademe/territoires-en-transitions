65b2c62b742943e866e06bd1fc98c134a4d205f0
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2025-02-05 09:52:41 +0100

    refactor: update ScoreEvolutions component with improved snapshot selection and display

--- app.territoiresentransitions.react/src/referentiels/evolutions/VersionsDropdown.tsx
+++ app.territoiresentransitions.react/src/referentiels/evolutions/VersionsDropdown.tsx
@@ -1,42 +1,40 @@
-import { Option, SelectFilter, SelectMultipleProps } from '@/ui';
+import { Option, SelectMultiple, SelectMultipleProps } from '@/ui';
 
 type VersionsDropdownProps = Omit<
   SelectMultipleProps,
   'values' | 'onChange' | 'options'
 > & {
-  values?: number[];
+  values: Array<string>;
+  options: Array<Option>;
   onChange: ({
-    versions,
-    selectedVersion,
+    snapshots,
+    selectedSnapshot,
   }: {
-    versions: number[];
-    selectedVersion: number;
+    snapshots: Array<string>;
+    selectedSnapshot: string;
   }) => void;
 };
 
 const VersionsDropdown = (props: VersionsDropdownProps) => {
-  const options: Option[] = [
-    { value: 1, label: '2010 - Nom de la version' },
-    { value: 2, label: '2011 - Nom de la version' },
-    { value: 3, label: '2012 - Nom de la version' },
-  ];
-
-  const getSelectedVersions = (values?: (string | number)[]) =>
-    values?.map((v) => Number(v)) ?? [];
+  const values = props.values ?? [];
 
   return (
     <div>
       <label className="mb-2">Sélectionner les versions à afficher :</label>
-      <SelectFilter
-        {...props}
-        options={options}
-        dataTest="versions"
-        onChange={({ values, selectedValue }) =>
-          props.onChange({
-            versions: getSelectedVersions(values),
-            selectedVersion: getSelectedVersions([selectedValue])[0],
-          })
+      <SelectMultiple
+        displayAllBadges
+        dataTest="snapshots"
+        options={props.options}
+        placeholder={
+          props.placeholder ?? 'Sélectionnez une ou plusieurs versions'
         }
+        values={values}
+        onChange={({ values, selectedValue }) => {
+          props.onChange({
+            snapshots: values as string[],
+            selectedSnapshot: selectedValue as string,
+          });
+        }}
       />
     </div>
   );
--- app.territoiresentransitions.react/src/referentiels/evolutions/index.tsx
+++ app.territoiresentransitions.react/src/referentiels/evolutions/index.tsx
@@ -2,58 +2,47 @@
 
 import { useState } from 'react';
 import VersionsDropdown from './VersionsDropdown';
-import { ButtonGroup, EmptyCard } from '@/ui';
+import { EmptyCard, Badge } from '@/ui';
 import ScoreEvolutionsTotalChart from './charts/ScoreEvolutionsTotalChart';
 import PictoDashboard from '../../ui/pictogrammes/PictoDashboard';
-import { useSnapshotList } from '../use-snapshot';
-// import { fakeScoreSnapshots } from './mocks/fake-score-snapshots';
+import { SnapshotDetails, useSnapshotList } from '../use-snapshot';
+import SaveScoreModal from '../../app/pages/collectivite/Referentiels/SaveScore/save-score.modal';
+import { useCurrentCollectivite } from '@/app/core-logic/hooks/useCurrentCollectivite';
+import { useParams } from 'next/navigation';
 
 export const ScoreEvolutions = () => {
-  const [versions, setVersions] = useState<number[]>([1, 2]);
-  const [activeButtonId, setActiveButtonId] = useState<string>('total');
+  const { referentielId } = useParams();
+  const collectivite = useCurrentCollectivite();
+  const collectiviteId = collectivite?.collectiviteId;
 
-  const { data: snapshots } = useSnapshotList();
-  // console.log('snapshots', snapshots);
+  const [selectedSnapshots, setSelectedSnapshots] = useState<string[]>([]);
 
-  const hasSavedVersions = true;
+  const HOW_MANY_SNAPSHOTS_TO_SHOW = 4;
+
+  const { data: snapshotList } = useSnapshotList({
+    limit: HOW_MANY_SNAPSHOTS_TO_SHOW,
+    mostRecentFirst: true,
+  });
+  const hasSavedSnapshots = !!snapshotList;
+  const snapshots = snapshotList?.snapshots ?? [];
+
+  const snapshotNames = snapshots.map((snap) => snap?.nom);
+
+  const dropdownOptions =
+    snapshotNames?.map((snap) => ({
+      value: snap ?? '',
+      label: snap ?? '',
+    })) ?? [];
+
+  const getSelectedSnapshots = (names: string[]) => {
+    return names
+      ?.map((name) => snapshots.find((snap) => snap?.nom === name) ?? null)
+      .filter((snap): snap is SnapshotDetails => snap !== null);
+  };
 
   return (
     <>
-      {hasSavedVersions && (
-        <>
-          <div className="flex items-end justify-between">
-            <VersionsDropdown
-              values={versions}
-              onChange={({ versions }) => setVersions(versions)}
-            />
-            <ButtonGroup
-              activeButtonId={activeButtonId}
-              buttons={[
-                {
-                  id: 'total',
-                  children: 'Total',
-                  onClick: () => setActiveButtonId('total'),
-                },
-                {
-                  id: 'axe',
-                  children: 'Par axe',
-                  onClick: () => setActiveButtonId('axe'),
-                },
-                {
-                  id: 'action',
-                  children: 'Par action',
-                  onClick: () => setActiveButtonId('action'),
-                },
-              ]}
-              className="justify-end"
-            />
-          </div>
-          <div>
-            <ScoreEvolutionsTotalChart snapshots={snapshots?.snapshots || []} />
-          </div>
-        </>
-      )}
-      {!hasSavedVersions && (
+      {!hasSavedSnapshots && (
         <div className="flex items-center justify-center">
           <EmptyCard
             picto={(props) => <PictoDashboard {...props} />}
@@ -61,19 +50,53 @@ export const ScoreEvolutions = () => {
             description="Figer le référentiel vous permet de sauvegarder une version à une date donnée, afin de pouvoir comparer l'évolution du score sur plusieurs versions."
             actions={[
               {
-                children: 'Figer le référentiel à une date antérieure',
+                children: (
+                  <SaveScoreModal
+                    referentielId={referentielId as string}
+                    collectiviteId={collectiviteId as number}
+                    when="before"
+                  >
+                    <span>Figer le référentiel à une date antérieure</span>
+                  </SaveScoreModal>
+                ),
                 onClick: () => {},
-                variant: 'outlined',
                 size: 'sm',
+                variant: 'outlined',
               },
               {
-                children: 'Figer le référentiel',
+                children: (
+                  <SaveScoreModal
+                    referentielId={referentielId as string}
+                    collectiviteId={collectiviteId as number}
+                  >
+                    <span>Figer le référentiel</span>
+                  </SaveScoreModal>
+                ),
                 onClick: () => {},
                 size: 'sm',
               },
             ]}
           />
         </div>
+      )}
+      {hasSavedSnapshots && (
+        <>
+          <div className="flex items-end justify-between">
+            <VersionsDropdown
+              values={selectedSnapshots ?? []}
+              onChange={({ snapshots }) => {
+                setSelectedSnapshots(snapshots);
+              }}
+              options={dropdownOptions}
+            />
+            <Badge title="Score total" />
+          </div>
+          <div>
+            <ScoreEvolutionsTotalChart
+              snapshots={getSelectedSnapshots(selectedSnapshots)}
+            />
+          </div>
+        </>
       )}
     </>
   );
