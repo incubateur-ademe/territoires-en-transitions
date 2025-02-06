92d1cb24dd7224f78910a5bf11bf714ff7ee24bb
Elisabeth Fainstein <139369258+elisfainstein@users.noreply.github.com>
2025-02-06 15:38:14 +0100

    refactor: simplify snapshot sorting in SnapshotsDropdown

--- app.territoiresentransitions.react/src/referentiels/evolutions/SnapshotsDropdown.tsx
+++ app.territoiresentransitions.react/src/referentiels/evolutions/SnapshotsDropdown.tsx
@@ -26,58 +26,31 @@ const SnapshotsDropdown = (props: SnapshotsDropdownProps) => {
   const values = props.values ?? [];
 
   /**
-   * Needed to display options and values sorted by date
+   * Needed to always display a value in its right date order
    */
-  const sortedOptions = sortOptions(props.options);
-  const sortedValues = sortValues(values, sortedOptions);
+  const sortedValues = values
+    .map((value) => props.options.find((opt) => opt.value === value))
+    .filter((option): option is SnapshotOption => option !== undefined)
+    .sort((a, b) => sortByDate(a.date, b.date))
+    .map((option) => option.value);
 
   return (
-    <div>
-      <label className="mb-2">Sélectionner les versions à afficher :</label>
-      <SelectMultiple
-        {...props}
-        displayAllBadges
-        options={sortedOptions}
-        placeholder={
-          props.placeholder ?? 'Sélectionnez une ou plusieurs versions'
-        }
-        values={sortedValues}
-        onChange={({ values, selectedValue }) => {
-          props.onChange({
-            snapshots: values as string[],
-            selectedSnapshot: selectedValue as string,
-          });
-        }}
-      />
-    </div>
+    <SelectMultiple
+      {...props}
+      displayAllBadges
+      options={props.options}
+      placeholder={
+        props.placeholder ?? 'Sélectionnez une ou plusieurs versions'
+      }
+      values={sortedValues}
+      onChange={({ values, selectedValue }) => {
+        props.onChange({
+          snapshots: values as string[],
+          selectedSnapshot: selectedValue as string,
+        });
+      }}
+    />
   );
-};
-
-const sortOptions = (options: SnapshotOption[], ascending = true) => {
-  return [...options].sort((a, b) => sortByDate(a.date, b.date, ascending));
-};
-
-const matchSelectedOptionsWithValues = (
-  values: string[],
-  options: SnapshotOption[]
-): SnapshotOption[] => {
-  const matchedOptions = values.map((selectedValue) => {
-    return options.find((option) => option.value === selectedValue);
-  });
-
-  const optionsWithoutUndefined = matchedOptions.filter(
-    (option): option is SnapshotOption => option !== undefined
-  );
-
-  return optionsWithoutUndefined;
-};
-
-const sortValues = (values: string[], options: SnapshotOption[]): string[] => {
-  const selectedSnapshotOptions = matchSelectedOptionsWithValues(
-    values,
-    options
-  );
-  return sortOptions(selectedSnapshotOptions).map((option) => option.value);
 };
 
 export default SnapshotsDropdown;
