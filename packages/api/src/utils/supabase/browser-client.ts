d52ee0cc940716f23da819061b13f9c217839ea4
Fred <98240+farnoux@users.noreply.github.com>
2025-02-10 12:50:00 +0100

    Supprime les cookies posés par erreur sur auth.tet.fr au lieu de .tet.fr

--- packages/api/src/utils/supabase/browser-client.ts
+++ packages/api/src/utils/supabase/browser-client.ts
@@ -1,12 +1,12 @@
 import { Database } from '@/api';
 import { createBrowserClient } from '@supabase/ssr';
 import { ENV } from '../../environmentVariables';
-import { supabaseCookieOptions } from './cookie-options';
+import { getCookieOptions } from './cookie-options';
 
 export const supabaseClient = createBrowserClient<Database>(
   ENV.supabase_url as string,
   ENV.supabase_anon_key as string,
   {
-    cookieOptions: supabaseCookieOptions,
+    cookieOptions: getCookieOptions(),
   }
 );
--- packages/api/src/utils/supabase/middleware-client.ts
+++ packages/api/src/utils/supabase/middleware-client.ts
@@ -1,7 +1,7 @@
 import { Database } from '@/api/database.types';
 import { createServerClient } from '@supabase/ssr';
 import { type NextRequest, type NextResponse } from 'next/server';
-import { supabaseCookieOptions } from './cookie-options';
+import { getCookieOptions } from './cookie-options';
 
 export async function createClient(
   request: NextRequest,
@@ -11,7 +11,7 @@ export async function createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookieOptions: supabaseCookieOptions,
+      cookieOptions: getCookieOptions(),
       cookies: {
         getAll() {
           return request.cookies.getAll();
