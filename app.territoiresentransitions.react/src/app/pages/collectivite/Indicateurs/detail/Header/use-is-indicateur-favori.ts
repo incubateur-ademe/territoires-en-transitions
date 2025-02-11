f9ae3570ee639bc346a98c82bb9ea5a47e209d34
Arthur Molinos <arthur.molinos@gmail.com>
2025-02-11 10:35:46 +0100

    Fix import

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/Header/use-is-indicateur-favori.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/Header/use-is-indicateur-favori.ts
@@ -1,6 +1,7 @@
-import { Indicateurs } from '@/api';
-import { supabaseClient } from '@/app/core-logic/api/supabase';
 import { useQuery } from 'react-query';
+
+import { Indicateurs } from '@/api';
+import { supabaseClient } from '@/api/utils/supabase/browser-client';
 
 /** Détermine si un indicateur est favoris de la collectivité */
 export const useIsIndicateurFavori = (indicateurId: number) => {
