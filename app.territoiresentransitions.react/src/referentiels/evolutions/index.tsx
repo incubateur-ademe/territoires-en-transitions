ad51ab8fbcb024c98026288e8a8a4d262e6ef511
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-02-12 16:46:49 +0100

    refactor: move static functions to the beginning of the file

--- app.territoiresentransitions.react/src/referentiels/evolutions/index.tsx
+++ app.territoiresentransitions.react/src/referentiels/evolutions/index.tsx
@@ -10,6 +10,24 @@ import { useCurrentCollectivite } from '@/app/core-logic/hooks/useCurrentCollect
 import { useParams } from 'next/navigation';
 import SnapshotsDropdown from './SnapshotsDropdown';
 
+const removeScoreCourant = (snapshots: SnapshotDetails[]) => {
+  return snapshots.filter((snap) => snap?.nom !== 'Score courant');
+};
+
+const organizeSnaps = (snapshots: SnapshotDetails[]) => {
+  const invertedSnapshots = snapshots.reverse();
+  const limitedSnapshots = limitSnapshots(invertedSnapshots);
+  return limitedSnapshots;
+};
+
+const limitSnapshots = (snapshots: SnapshotDetails[]) => {
+  const HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT = 4;
+  if (snapshots.length > HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT) {
+    return snapshots.slice(0, HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT);
+  }
+  return snapshots;
+};
+
 export const ScoreEvolutions = () => {
   const { referentielId } = useParams();
   const collectivite = useCurrentCollectivite();
@@ -118,24 +136,6 @@ export const ScoreEvolutions = () => {
       )}
     </>
   );
-};
-
-const removeScoreCourant = (snapshots: SnapshotDetails[]) => {
-  return snapshots.filter((snap) => snap?.nom !== 'Score courant');
-};
-
-const organizeSnaps = (snapshots: SnapshotDetails[]) => {
-  const invertedSnapshots = snapshots.reverse();
-  const limitedSnapshots = limitSnapshots(invertedSnapshots);
-  return limitedSnapshots;
-};
-
-const limitSnapshots = (snapshots: SnapshotDetails[]) => {
-  const HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT = 4;
-  if (snapshots.length > HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT) {
-    return snapshots.slice(0, HOW_MANY_SNAPSHOTS_TO_SHOW_BY_DEFAULT);
-  }
-  return snapshots;
 };
 
 export default ScoreEvolutions;
