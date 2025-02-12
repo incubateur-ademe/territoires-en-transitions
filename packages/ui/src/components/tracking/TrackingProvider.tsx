ba118abac1456f1cbed0e17fc4fb3d9eeb345d3b
Fred <98240+farnoux@users.noreply.github.com>
2025-02-12 16:27:29 +0100

    Simplifie l'initialisation Posthog

--- packages/ui/src/components/tracking/TrackingProvider.tsx
+++ packages/ui/src/components/tracking/TrackingProvider.tsx
@@ -7,52 +7,6 @@ import { ReactNode, useEffect } from 'react';
 import { getConsent } from './Consent';
 
 /**
- * Crée le client de tracking
- */
-const createTrackingClient = ({
-  host,
-  key,
-  env,
-}: {
-  host?: string;
-  key?: string;
-  env?: string;
-}) => {
-  if (typeof window === 'undefined') {
-    return posthog;
-  }
-
-  const is_dev = env === 'development';
-
-  if (!key || !host) {
-    console.warn(
-      `Le tracking PostHog n'est pas configuré, les variables d'env sont absentes.`
-    );
-    return posthog;
-  }
-
-  // en mode dev, on envoie les données à PostHog, sinon on passe par le `rewrites` de `next.config.mjs`
-  const apiHost = is_dev ? host : `${window.location.origin}/ingest`;
-
-  posthog.init(key, {
-    api_host: apiHost,
-    ui_host: host,
-    // create profiles for authenticated users only
-    person_profiles: 'identified_only',
-    persistence: getConsent() ? 'cookie' : 'memory',
-    capture_pageview: false, // on utilise PostHogPageView pour capturer les `page views`
-
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug();
-    },
-  });
-
-  return posthog;
-};
-
-const posthogClient = createTrackingClient(ENV.posthog);
-
-/**
  * Encapsule le client de tracking dans un provider react
  */
 export const TrackingProvider = ({
@@ -63,8 +17,27 @@ export const TrackingProvider = ({
   children: ReactNode;
 }) => {
   useEffect(() => {
-    onClientInit?.(posthogClient);
-  });
+    // en mode dev, on envoie les données à PostHog, sinon on passe par le `rewrites` de `next.config.mjs`
+    const apiHost =
+      process.env.NODE_ENV !== 'production'
+        ? ENV.posthog.host
+        : `${window.location.origin}/ingest`;
 
-  return <PostHogProvider client={posthogClient}>{children}</PostHogProvider>;
+    posthog.init(ENV.posthog.key as string, {
+      api_host: apiHost,
+      ui_host: ENV.posthog.host,
+      // create profiles for authenticated users only
+      person_profiles: 'identified_only',
+      persistence: getConsent() ? 'cookie' : 'memory',
+      capture_pageview: false, // on utilise PostHogPageView pour capturer les `page views`
+
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug();
+      },
+    });
+
+    onClientInit?.(posthog);
+  }, []);
+
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 };
