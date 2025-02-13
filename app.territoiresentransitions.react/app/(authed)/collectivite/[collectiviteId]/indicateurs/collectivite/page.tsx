8ea48bf50f6f9fadd029feeef2b924c53b8d1b07
Marc Rutkowski <marc@attractive-media.fr>
2025-02-13 16:41:11 +0100

    Passe sous Next la route vers la page "Indicateurs de la collectivité"

---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/indicateurs/collectivite/page.tsx
@@ -0,0 +1,10 @@
+import IndicateursCollectivite from '@/app/app/pages/collectivite/Indicateurs/IndicateursCollectivite/IndicateursCollectivite';
+import PageContainer from '@/ui/components/layout/page-container';
+
+export default function Page() {
+  return (
+    <PageContainer>
+      <IndicateursCollectivite />
+    </PageContainer>
+  );
+}
