c8992cdd6ea1ff89a7d89a4d1b74c8a7d0aca6d5
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-02-13 16:05:09 +0100

    refactor: update SaveScoreModal to use controlled modal state

--- app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/SaveScore/save-score.button.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/SaveScore/save-score.button.tsx
@@ -1,13 +1,35 @@
+import SaveScoreModal, {
+  SaveScoreProps,
+} from '@/app/app/pages/collectivite/Referentiels/SaveScore/save-score.modal';
 import { Button } from '@/ui';
-import SaveScoreModal, { SaveScoreProps } from '@/app/app/pages/collectivite/Referentiels/SaveScore/save-score.modal';
+import { useState } from 'react';
 
 const SaveScoreButton = ({ referentielId, collectiviteId }: SaveScoreProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <SaveScoreModal collectiviteId={collectiviteId} referentielId={referentielId}>
-      <Button icon="save-3-line" variant="primary" size="sm" className="whitespace-nowrap">
+    <>
+      <Button
+        icon="save-3-line"
+        variant="primary"
+        size="sm"
+        className="whitespace-nowrap"
+        onClick={() => setIsOpen(true)}
+      >
         Figer le référentiel
       </Button>
-    </SaveScoreModal>
+      {isOpen && (
+        <SaveScoreModal
+          collectiviteId={collectiviteId}
+          referentielId={referentielId}
+          openState={{
+            isOpen,
+            setIsOpen: (value: boolean) => {
+              setIsOpen(value);
+            },
+          }}
+        />
+      )}
+    </>
   );
 };
 
