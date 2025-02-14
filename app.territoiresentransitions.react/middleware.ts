4717f1d9a22cb85e866a675943500a31eaaecfdc
Fred <98240+farnoux@users.noreply.github.com>
2025-02-14 09:26:14 +0100

    Bypass middleware for static files in /public

--- app.territoiresentransitions.react/middleware.ts
+++ app.territoiresentransitions.react/middleware.ts
@@ -30,7 +30,7 @@ export const config = {
      */
     {
       source:
-        '/((?!api|ingest|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!api|ingest|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|xlsx|docx|pdf|txt|ods|woff2)$).*)',
 
       missing: [
         { type: 'header', key: 'next-router-prefetch' },
