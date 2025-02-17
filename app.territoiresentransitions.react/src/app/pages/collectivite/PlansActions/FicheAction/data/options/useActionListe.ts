0f1ec28e369aaef1e154f246fa90e311b11b6215
Marc Rutkowski <marc@attractive-media.fr>
2025-02-17 10:55:51 +0100

    Ajoute un filtre sur le chargement des mesures liées à un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/options/useActionListe.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/options/useActionListe.ts
@@ -1,13 +1,15 @@
 import { useQuery } from 'react-query';
 
 import { DBClient } from '@/api';
+import { DISABLE_AUTO_REFETCH } from '@/api/utils/react-query/query-options';
 import { useSupabase } from '@/api/utils/supabase/use-supabase';
 import { useCollectiviteId } from '@/app/core-logic/hooks/params';
 import { TActionStatutsRow } from '@/app/types/alias';
 
 const fetchActionListe = async (
   supabase: DBClient,
-  collectivite_id: number
+  collectivite_id: number,
+  actionsIds?: string[]
 ) => {
   const query = supabase
     .from('action_statuts')
@@ -15,8 +17,13 @@ const fetchActionListe = async (
       'action_id, referentiel, nom, identifiant, avancement, avancement_descendants, desactive, concerne'
     )
     .eq('collectivite_id', collectivite_id)
-    .in('type', ['action', 'sous-action'])
-    .order('action_id', { ascending: true });
+    .in('type', ['action', 'sous-action']);
+
+  if (actionsIds?.length) {
+    query.in('action_id', actionsIds);
+  }
+
+  query.order('action_id', { ascending: true });
 
   const { error, data } = await query;
 
@@ -27,11 +34,13 @@ const fetchActionListe = async (
   return data as TActionStatutsRow[];
 };
 
-export const useActionListe = () => {
+export const useActionListe = (actionsIds?: string[]) => {
   const collectivite_id = useCollectiviteId();
   const supabase = useSupabase();
 
-  return useQuery(['actions_referentiels', collectivite_id], () =>
-    fetchActionListe(supabase, collectivite_id!)
+  return useQuery(
+    ['actions_referentiels', collectivite_id, actionsIds],
+    () => fetchActionListe(supabase, collectivite_id!, actionsIds),
+    DISABLE_AUTO_REFETCH
   );
 };
