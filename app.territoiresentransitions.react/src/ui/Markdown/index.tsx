e00db98a729226e6adae33ce252ee5bcd05e9010
Marc Rutkowski <marc@attractive-media.fr>
2025-02-20 16:06:31 +0100

    Utilise un wrapper explicite (et générique) pour le contenu markdown plutôt que le div ajouté implicitement par l'usage de la prop. `className` du composant react-markdown
    Ref: https://github.com/remarkjs/react-markdown/issues/781

--- app.territoiresentransitions.react/src/ui/Markdown/index.tsx
+++ app.territoiresentransitions.react/src/ui/Markdown/index.tsx
@@ -1,28 +1,49 @@
 import classNames from 'classnames';
-import MarkdownBase from 'react-markdown';
+import { ElementType } from 'react';
+import MarkdownBase, { Options } from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
-type MarkdownProps = {
+interface MarkdownProps<T extends ElementType> {
+  // container utilisé (par défaut `div`)
+  as?: T;
+  // contenu au format md
   content: string;
+  // classes supplémentaires appliquées au container
   className?: string;
-};
+  // options pour le composant react-markdown
+  options?: Options;
+}
 
-const Markdown = ({ content, className }: MarkdownProps) => (
-  <MarkdownBase
-    rehypePlugins={[rehypeRaw]}
-    remarkPlugins={[remarkGfm]}
-    className={classNames(
-      `
-      [&_ul]:list-disc [&_ul]:list-inside [&_ul]:pl-4
-      [&_ol]:list-inside [&_ol]:pl-4
-      [&>*:last-child]:mb-0
-      `,
-      className
-    )}
-  >
-    {content}
-  </MarkdownBase>
-);
+const Markdown = <T extends ElementType = 'div'>({
+  as,
+  content,
+  className,
+  options,
+}: MarkdownProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof MarkdownProps<T>>) => {
+  const Wrapper = as || 'div';
+
+  return (
+    <Wrapper
+      className={classNames(
+        `
+    [&_ul]:list-disc [&_ul]:list-inside [&_ul]:pl-4
+    [&_ol]:list-inside [&_ol]:pl-4
+    [&>*:last-child]:mb-0
+    `,
+        className
+      )}
+    >
+      <MarkdownBase
+        rehypePlugins={[rehypeRaw]}
+        remarkPlugins={[remarkGfm]}
+        {...options}
+      >
+        {content}
+      </MarkdownBase>
+    </Wrapper>
+  );
+};
 
 export default Markdown;
