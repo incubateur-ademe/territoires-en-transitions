5f06c7dc7dbab3f7fe1dea5b3c2b1f114120343e
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-02-20 16:00:56 +0100

    Personnalisation de la section notes de suivi

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ExportPdf/ExportModal/export-fa-table.tsx
@@ -0,0 +1,69 @@
+import { Checkbox, TBody, TCell, THead, TRow, Table } from '@/ui';
+import { PdfSectionKey, TSectionsValues, sectionsList } from '../utils';
+import ExportSuiviSelect from './export-suivi-select';
+
+type Props = {
+  options: TSectionsValues;
+  setOptions: (values: TSectionsValues) => void;
+};
+
+const ExportFicheActionTable = ({ options, setOptions }: Props) => {
+  const handleOnCheck = (key: PdfSectionKey, checked: boolean) => {
+    const newValue = {
+      ...options[key],
+      isChecked: checked,
+    };
+    const newOptions = {
+      ...options,
+      [key as string]: newValue,
+    };
+    setOptions(newOptions);
+  };
+
+  return (
+    <Table className="border-[0.5px] border-primary-4">
+      {/* En-tête du tableau */}
+      <THead>
+        <TRow className="bg-primary-2 text-primary-9 font-bold text-sm">
+          <TCell>Sections</TCell>
+          <TCell className="min-w-80">Personnaliser la section</TCell>
+          <TCell className="min-w-52">Ajouter à l’export PDF</TCell>
+        </TRow>
+      </THead>
+
+      {/* Contenu du tableau */}
+      <TBody>
+        {sectionsList.map((section) => (
+          <TRow key={section.key}>
+            {/* Sections */}
+            <TCell className="text-primary-10 font-bold text-sm">
+              {section.title}
+            </TCell>
+
+            {/* Personnalisation */}
+            <TCell className="!px-3 !py-1">
+              {section.key === 'suivi' ? (
+                <ExportSuiviSelect {...{ options, setOptions }} />
+              ) : (
+                ''
+              )}
+            </TCell>
+
+            {/* Sélection */}
+            <TCell>
+              <Checkbox
+                className="mx-auto"
+                checked={options[section.key].isChecked}
+                onChange={(evt) =>
+                  handleOnCheck(section.key, evt.currentTarget.checked)
+                }
+              />
+            </TCell>
+          </TRow>
+        ))}
+      </TBody>
+    </Table>
+  );
+};
+
+export default ExportFicheActionTable;
