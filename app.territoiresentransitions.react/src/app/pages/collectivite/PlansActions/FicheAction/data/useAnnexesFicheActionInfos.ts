b77ed0e89f27c5d0a08f88789ed93dabe5032532
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-02-21 12:01:37 +0100

    Evite des fetch inutiles sur les sections déselectionnées

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useAnnexesFicheActionInfos.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useAnnexesFicheActionInfos.ts
@@ -11,23 +11,30 @@ export type AnnexeInfo = {
   url: string | null;
 };
 
-export const useAnnexesFicheActionInfos = (fiche_id: number | null) => {
+export const useAnnexesFicheActionInfos = (
+  fiche_id: number | null,
+  requested = true
+) => {
   const supabase = useSupabase();
 
-  return useQuery(['annexes_fiche_action_infos', fiche_id], async () => {
-    if (!fiche_id) return [];
+  return useQuery(
+    ['annexes_fiche_action_infos', fiche_id],
+    async () => {
+      if (!fiche_id) return [];
 
-    const { data, error } = await supabase
-      .from('bibliotheque_annexe')
-      .select(
-        'id, created_at, created_by_nom, commentaire, fichier->>filename, lien->>titre, lien->>url'
-      )
-      .eq('fiche_id', fiche_id)
-      .order('lien->>titre' as 'lien', { ascending: true })
-      .order('fichier->>filename' as 'fichier', { ascending: true });
+      const { data, error } = await supabase
+        .from('bibliotheque_annexe')
+        .select(
+          'id, created_at, created_by_nom, commentaire, fichier->>filename, lien->>titre, lien->>url'
+        )
+        .eq('fiche_id', fiche_id)
+        .order('lien->>titre' as 'lien', { ascending: true })
+        .order('fichier->>filename' as 'fichier', { ascending: true });
 
-    if (error) throw new Error(error.message);
+      if (error) throw new Error(error.message);
 
-    return data as unknown as AnnexeInfo[];
-  });
+      return data as unknown as AnnexeInfo[];
+    },
+    { enabled: requested }
+  );
 };
