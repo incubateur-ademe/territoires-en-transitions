d01943e74c2049cad5806570ace250b454dc5eb1
Fred <98240+farnoux@users.noreply.github.com>
2025-02-25 15:21:38 +0100

    Fixe test 06

--- e2e/cypress/integration/06-personnaliser-le-referentiel/steps.js
+++ e2e/cypress/integration/06-personnaliser-le-referentiel/steps.js
@@ -1,14 +1,14 @@
-import {When} from '@badeball/cypress-cucumber-preprocessor';
+import { When } from '@badeball/cypress-cucumber-preprocessor';
 
-import {LocalSelectors} from './selectors';
+import { LocalSelectors } from './selectors';
 
 beforeEach(() => {
   // enregistre les définitions locales
-  cy.wrap(LocalSelectors).as('LocalSelectors', {type: 'static'});
+  cy.wrap(LocalSelectors).as('LocalSelectors', { type: 'static' });
 });
 
 /** Vérification des questions/réponse **/
-When(/la liste des questions contient les entrées suivantes/, dataString => {
+When(/la liste des questions contient les entrées suivantes/, (dataString) => {
   // sélectionne le dialogue
   cy.get(LocalSelectors['dialogue Personnaliser le potentiel'].selector).within(
     () => {
@@ -18,7 +18,7 @@ When(/la liste des questions contient les entrées suivantes/, dataString => {
 });
 When(
   'la page thématique contient entre autres les questions suivantes :',
-  dataString => {
+  (dataString) => {
     cy.get('[data-test=thematique]').within(() => {
       checkQuestionsReponses(dataString);
     });
@@ -26,33 +26,33 @@ When(
 );
 
 /** Saisie les réponses */
-When('je complète les questions avec les valeurs suivantes :', dataString =>
+When('je complète les questions avec les valeurs suivantes :', (dataString) =>
   setReponses(dataString)
 );
 
-When('je clique sur la thématique {string}', thematique => {
+When('je clique sur la thématique {string}', (thematique) => {
   cy.get('[data-test=personnalisation] a').contains(thematique).click();
 });
 
-When("je clique sur l'avant-dernière thématique", thematique => {
+When("je clique sur l'avant-dernière thématique", (thematique) => {
   cy.get('[data-test=personnalisation] a:nth(-2)').click();
 });
-When('je clique sur la dernière thématique', thematique => {
+When('je clique sur la dernière thématique', (thematique) => {
   cy.get('[data-test=personnalisation] a:nth(-1)').click();
 });
 
-When('la page contient plus de {int} thématiques', count =>
+When('la page contient plus de {int} thématiques', (count) =>
   cy.get('[data-test=personnalisation] a').should('have.length.gt', count)
 );
-When('la page contient moins de {int} thématiques', count =>
+When('la page contient moins de {int} thématiques', (count) =>
   cy.get('[data-test=personnalisation] a').should('have.length.lt', count)
 );
 
-const checkQuestionsReponses = dataString => {
+const checkQuestionsReponses = (dataString) => {
   const qr = parseQuestionsReponses(dataString);
 
   // pour chaque question/réponse attendue
-  cy.wrap(qr).each(({label, type, valeur, nbOptions}, index) => {
+  cy.wrap(qr).each(({ label, type, valeur, nbOptions }, index) => {
     // sélectionne la réponse à partir du libellé de la question
     getReponseByQuestionLabel(label).within(() => {
       // et vérifie la réponse en fonction du type
@@ -76,23 +76,23 @@ const checkQuestionsReponses = dataString => {
           break;
 
         case 'part':
-          cy.get('input[type=number]').should('have.value', valeur);
+          cy.get('input[inputmode=numeric]').should('have.value', valeur);
           break;
       }
     });
   });
 };
 
-const setReponses = dataString => {
+const setReponses = (dataString) => {
   const qr = parseQuestionsReponses(dataString);
 
   // pour chaque question/réponse attendue
-  cy.wrap(qr).each(({label, type, valeur}, index) => {
+  cy.wrap(qr).each(({ label, type, valeur }, index) => {
     // sélectionne la réponse à partir du libellé de la question
     getReponseByQuestionLabel(label).within(() => {
       // clic sur le bouton "Modifier" avant de sélectionner un nouvelle valeur
       // si nécessaire
-      const {modValue, modifyBeforeUpdate} = getModifyValue(valeur);
+      const { modValue, modifyBeforeUpdate } = getModifyValue(valeur);
       if (modifyBeforeUpdate) {
         cy.get('button').contains('Modifier').click();
       }
@@ -109,7 +109,7 @@ const setReponses = dataString => {
           break;
 
         case 'part':
-          cy.get('input[type=number]').type(
+          cy.get('input[inputmode=numeric]').type(
             '{selectall}{backspace}' + modValue
           );
           break;
@@ -123,23 +123,23 @@ const setReponses = dataString => {
 };
 
 // pour le type binaire ou choix
-const getModifyValue = valeur => {
+const getModifyValue = (valeur) => {
   // si la valeur est préfixée par `[MODIFIER]->` cela indique que le bouton
   // "Modifier" doit être cliqué avant que la valeur puisse être modifié
   const modify = valeur?.split('[MODIFIER]->');
   if (modify?.length === 2) {
-    return {modValue: modify[1], modifyBeforeUpdate: true};
+    return { modValue: modify[1], modifyBeforeUpdate: true };
   }
 
-  return {modValue: valeur};
+  return { modValue: valeur };
 };
 
 // sélectionne la réponse à partir du libellé de la question
-const getReponseByQuestionLabel = label =>
+const getReponseByQuestionLabel = (label) =>
   cy.get('legend').contains(label).parent().find(`[data-test=reponse]`);
 
 // vérifie qu'un bouton radio est sélectionné
-const assertRadioButtonIsOn = value => {
+const assertRadioButtonIsOn = (value) => {
   assertOneRadioButtonIsOn();
   cy.get(`input[type=radio][value=${value}]`).should('be.checked');
 };
@@ -151,7 +151,7 @@ const assertOneRadioButtonIsOn = () => {
 };
 
 // vérifie qu'aucun bouton radio n'est sélectionné
-const assertAllRadioButtonsAreOff = length => {
+const assertAllRadioButtonsAreOff = (length) => {
   // quand aucune option n'est sélectionnée tous les boutons radio sont visibles et non cochés
   cy.get('input[type=radio]')
     .should('have.length', length)
@@ -167,21 +167,21 @@ const assertAllRadioButtonsAreOff = length => {
 const VALID_TYPES = ['binaire', 'choix', 'part'];
 const VALID_BINARY_VALUES = ['oui', 'non', ''];
 const RE_CHOIX = /^choix\((\d+)\)$/;
-const CHOIX_BINAIRE = {oui: 'true', non: 'false'};
+const CHOIX_BINAIRE = { oui: 'true', non: 'false' };
 
-const parseQuestionsReponses = dataString => {
+const parseQuestionsReponses = (dataString) => {
   const qr = [];
   dataString
     .split('\n') // sépare les lignes
-    .map(s => s.trim()) // enlève les espaces en début/fin de chaine
-    .filter(s => !!s) // ignore les lignes vides
+    .map((s) => s.trim()) // enlève les espaces en début/fin de chaine
+    .filter((s) => !!s) // ignore les lignes vides
     .forEach((l, index, lines) => {
       // parcours les lignes 2 à 2
       if (index % 2 === 0 && index + 1 < lines.length) {
         // le libellé est sur la ligne courante
         const label = lines[index].trim();
         // le type et la valeur sur la suivante, séparés par ':'
-        const [t, v] = lines[index + 1].split(':').map(s => s.trim());
+        const [t, v] = lines[index + 1].split(':').map((s) => s.trim());
         const valeur = v === 'non définie' || v === undefined ? '' : v;
         const type = t?.startsWith('choix') ? 'choix' : t;
         assert(
@@ -200,7 +200,7 @@ const parseQuestionsReponses = dataString => {
                 '|'
               )}>" (reçu: ${JSON.stringify(valeur)})`
             );
-            props = {valeur};
+            props = { valeur };
             break;
 
           case 'choix':
@@ -210,7 +210,7 @@ const parseQuestionsReponses = dataString => {
               match?.length === 2,
               `syntaxe attendue "choix(<nombre d'options>): <valeur>"`
             );
-            props = {nbOptions: parseInt(match[1])};
+            props = { nbOptions: parseInt(match[1]) };
             break;
         }
 
