af6f9c1d2ca517cf8ec9ec197e846e942670ba67
Marc Rutkowski <marc@attractive-media.fr>
2025-03-03 18:48:35 +0100

    Ajoute un point d'entrée tRPC donnant la moyenne par date d'un indicateur pour les collectivités de même type

--- backend/src/indicateurs/valeurs/crud-valeurs.router.ts
+++ backend/src/indicateurs/valeurs/crud-valeurs.router.ts
@@ -4,12 +4,15 @@ import { deleteValeurIndicateurSchema } from '../shared/models/delete-valeur-ind
 import { getIndicateursValeursRequestSchema } from '../shared/models/get-indicateurs.request';
 import { upsertValeurIndicateurSchema } from '../shared/models/upsert-valeur-indicateur.request';
 import IndicateurValeursService from './crud-valeurs.service';
+import { getAverageValuesRequestSchema } from './get-average-values.request';
+import ValeursCalculeesService from './valeurs-calculees.service';
 
 @Injectable()
 export class IndicateurValeursRouter {
   constructor(
     private readonly trpc: TrpcService,
-    private readonly service: IndicateurValeursService
+    private readonly service: IndicateurValeursService,
+    private readonly valeursCalculees: ValeursCalculeesService
   ) {}
 
   router = this.trpc.router({
@@ -27,6 +30,11 @@ export class IndicateurValeursRouter {
       .input(deleteValeurIndicateurSchema)
       .mutation(({ input, ctx }) => {
         return this.service.deleteValeurIndicateur(input, ctx.user);
+      }),
+    average: this.trpc.authedProcedure
+      .input(getAverageValuesRequestSchema)
+      .query(({ ctx, input }) => {
+        return this.valeursCalculees.getAverageValues(input, ctx.user);
       }),
   });
 }
