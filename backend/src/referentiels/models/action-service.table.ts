8540286849595f1264e84943a449732d73bcab1d
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-03-03 17:23:00 +0100

    feat(database): add action_pilote and action_service tables for tracking action (mesure) pilots and services

---
+++ backend/src/referentiels/models/action-service.table.ts
@@ -0,0 +1,28 @@
+import { integer, pgTable, varchar, primaryKey } from 'drizzle-orm/pg-core';
+import {
+  collectiviteTable,
+  serviceTagTable,
+} from '../../collectivites/index-domain';
+import { actionRelationTable } from './action-relation.table';
+
+export const actionServiceTable = pgTable(
+  'action_service',
+  {
+    collectiviteId: integer('collectivite_id')
+      .notNull()
+      .references(() => collectiviteTable.id),
+    actionId: varchar('action_id', { length: 30 })
+      .references(() => actionRelationTable.id)
+      .notNull(),
+    serviceTagId: integer('service_tag_id')
+      .references(() => serviceTagTable.id, { onDelete: 'cascade' })
+      .notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({
+        columns: [table.collectiviteId, table.actionId],
+      }),
+    };
+  }
+);
