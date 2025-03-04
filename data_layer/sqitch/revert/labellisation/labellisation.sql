5ab29042f909ba1f03aa7953c9233f3d71fe1858
Fred <98240+farnoux@users.noreply.github.com>
2025-03-04 17:01:07 +0100

    Allow `authenticated` role to query table `labellisation.etoile_meta`

--- data_layer/sqitch/revert/labellisation/labellisation.sql
+++ data_layer/sqitch/revert/labellisation/labellisation.sql
@@ -1,7 +1,16 @@
--- Revert tet:labellisation/labellisation from pg
+-- Deploy tet:labellisation/labellisation to pg
 
 BEGIN;
 
-drop table labellisation;
+DROP POLICY IF EXISTS allow_read ON labellisation.etoile_meta;
+
+ALTER TABLE labellisation.etoile_meta
+  DISABLE ROW LEVEL SECURITY;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA labellisation FROM anon, authenticated, service_role;
+REVOKE ALL ON ALL ROUTINES IN SCHEMA labellisation FROM anon, authenticated, service_role;
+REVOKE ALL ON ALL SEQUENCES IN SCHEMA labellisation FROM anon, authenticated, service_role;
+
+REVOKE USAGE ON SCHEMA labellisation FROM anon, authenticated, service_role;
 
 COMMIT;
