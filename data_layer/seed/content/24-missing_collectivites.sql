a92b4d84231dd3765f946a2f1d3c36f239ccf2fe
Amandine Jacquelin <contact@jacquelinamandine.fr>
2025-03-04 14:17:04 +0100

    Adapte les données du seed au nouveau modèle des collectivités

--- data_layer/seed/content/24-missing_collectivites.sql
+++ data_layer/seed/content/24-missing_collectivites.sql
@@ -1,2 +1,2 @@
-insert into epci(nom, siren, nature) values ('PETR Figeac - Quercy - Vallée de la Dordogne', '200052397', 'PETR');
-insert into epci(nom, siren, nature) values ('SM à la carte de l''Avant-Pays Savoyard (SMAPS)', '257302182', 'SMF');
+insert into collectivite(nom, siren, nature_insee, type) values ('PETR Figeac - Quercy - Vallée de la Dordogne', '200052397', 'PETR', 'epci');
+insert into collectivite(nom, siren, nature_insee, type) values ('SM à la carte de l''Avant-Pays Savoyard (SMAPS)', '257302182', 'SMF', 'epci');
