d65e146f9b35bcde0b97c6bbb5dfd3c7f04785a5
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2025-03-05 08:58:51 +0100

    feat(tools-automation-api): set-up trpc client

--- apps/tools-automation-api/src/main.ts
+++ apps/tools-automation-api/src/main.ts
@@ -11,8 +11,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.setGlobalPrefix(globalPrefix, {
+    exclude: ['version'],
+  });
+  const port = process.env.PORT || 8081;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
