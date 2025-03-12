01970c6db66a0f6105fd8e4bd9c4ad1f67247925
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-12 12:10:30 +0100

    Ajoute les pilotes et services ainsi que la modale de modification au header d'une mesure

---
+++ app.territoiresentransitions.react/src/referentiels/actions/use-action-pilotes.ts
@@ -0,0 +1,37 @@
+import { trpc } from '@/api/utils/trpc/client';
+import { useCollectiviteId } from '@/app/collectivites/collectivite-context';
+
+/** Récupère la liste des pilotes d'une mesure */
+export const useActionPilotesList = (actionId: string) => {
+  const collectiviteId = useCollectiviteId();
+  return trpc.referentiels.actions.listPilotes.useQuery({
+    collectiviteId,
+    actionId,
+  });
+};
+
+/** Modifie la liste des pilotes d'une mesure */
+export const useActionPilotesUpsert = () => {
+  const utils = trpc.useUtils();
+  return trpc.referentiels.actions.upsertPilotes.useMutation({
+    onSuccess: (data, variables) => {
+      utils.referentiels.actions.listPilotes.invalidate({
+        collectiviteId: variables.collectiviteId,
+        actionId: variables.actionId,
+      });
+    },
+  });
+};
+
+/** Supprime les pilotes d'une mesure */
+export const useActionPilotesDelete = () => {
+  const utils = trpc.useUtils();
+  return trpc.referentiels.actions.deletePilotes.useMutation({
+    onSuccess: (data, variables) => {
+      utils.referentiels.actions.listPilotes.invalidate({
+        collectiviteId: variables.collectiviteId,
+        actionId: variables.actionId,
+      });
+    },
+  });
+};
