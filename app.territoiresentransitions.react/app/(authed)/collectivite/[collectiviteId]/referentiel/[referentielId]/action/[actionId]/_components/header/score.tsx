d6fa196cb624aa3e21169f05657eed420d37b95a
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-12 12:08:09 +0100

    Redécoupe le header d'une mesure et bouge un composant

---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/referentiel/[referentielId]/action/[actionId]/_components/header/score.tsx
@@ -0,0 +1,55 @@
+import { PersoPotentiel } from '@/app/referentiels/personnalisations/PersoPotentielModal/PersoPotentiel';
+import { ScoreProgressBar } from '@/app/referentiels/scores/score.progress-bar';
+import ScoreShow from '@/app/referentiels/scores/score.show';
+import { ActionDefinitionSummary } from '@/app/referentiels/ActionDefinitionSummaryReadEndpoint';
+import { ProgressionRow } from '@/app/referentiels/DEPRECATED_scores.types';
+import {
+  ActionDetailed,
+  useSnapshotFlagEnabled,
+} from '@/app/referentiels/use-snapshot';
+
+type Props = {
+  actionDefinition: ActionDefinitionSummary;
+  DEPRECATED_actionScore: ProgressionRow;
+  action?: ActionDetailed;
+};
+
+const Score = ({ actionDefinition, action, DEPRECATED_actionScore }: Props) => {
+  const FLAG_isSnapshotEnabled = useSnapshotFlagEnabled();
+  return (
+    <div className="flex gap-4 items-center text-grey-7">
+      <ScoreProgressBar
+        actionDefinition={actionDefinition}
+        className="border-r border-r-[#ddd] pr-6"
+        // TODO(temporary): Temporary patch to display percentage
+        TEMP_displayValue={true}
+      />
+      {FLAG_isSnapshotEnabled ? (
+        <>
+          <ScoreShow
+            score={action?.score.pointFait ?? null}
+            scoreMax={action?.score.pointPotentiel ?? null}
+            legend="Score réalisé"
+            size="sm"
+          />
+        </>
+      ) : (
+        <>
+          <ScoreShow
+            score={DEPRECATED_actionScore?.points_realises ?? null}
+            scoreMax={DEPRECATED_actionScore?.points_max_personnalises ?? null}
+            legend="Score réalisé"
+            size="sm"
+          />
+        </>
+      )}
+      {actionDefinition.have_questions && (
+        <div className="border-l border-l-[#ddd] pl-3">
+          <PersoPotentiel actionDef={actionDefinition} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Score;
