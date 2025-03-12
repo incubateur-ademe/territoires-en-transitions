66e01b273a36d49917d25df4c15974bcc021d019
Fred <98240+farnoux@users.noreply.github.com>
2025-03-12 21:13:44 +0100

    Track les pages de l'app automatiquement au changement d'URL

--- app.territoiresentransitions.react/app/(authed)/layout.tsx
+++ app.territoiresentransitions.react/app/(authed)/layout.tsx
@@ -2,6 +2,7 @@ import { fetchUserDetails } from '@/api/users/user-details.fetch.server';
 import { getAuthUser } from '@/api/utils/supabase/auth-user.server';
 import Header from '@/app/app/Layout/Header';
 import { getErrorDisplayComponent } from '@/app/shared/error-display';
+import NextPostHogPageView from '@/ui/components/tracking/NextPostHogPageView';
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -22,6 +23,7 @@ export default async function Layout({ children }: { children: ReactNode }) {
       <ErrorBoundary fallbackRender={getErrorDisplayComponent}>
         {children}
       </ErrorBoundary>
+      <NextPostHogPageView />
     </AppProviders>
   );
 }
