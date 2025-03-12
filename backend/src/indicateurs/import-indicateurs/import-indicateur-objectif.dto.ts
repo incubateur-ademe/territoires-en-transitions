ffe3db7cd0c36f6f41f8f2bc4f2791a468eaf9cf
Marc Rutkowski <marc@attractive-media.fr>
2025-03-12 15:44:03 +0100

    Ajoute dans l'import depuis le spreadsheet le traitement des valeurs cible et seuil

---
+++ backend/src/indicateurs/import-indicateurs/import-indicateur-objectif.dto.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { indicateurObjectifSchema } from '../shared/models/indicateur-objectif.table';
+
+export const importObjectifSchema = indicateurObjectifSchema
+  .omit({
+    indicateurId: true,
+    dateValeur: true,
+  })
+  .extend({
+    identifiantReferentiel: z.string(),
+    dateValeur: z.string().date(),
+  });
+
+export type ImportObjectifType = z.infer<typeof importObjectifSchema>;
