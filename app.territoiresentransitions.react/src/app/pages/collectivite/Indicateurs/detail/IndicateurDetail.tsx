14c80562fb26b5f94004246075122bdecce72bed
Marc Rutkowski <marc@attractive-media.fr>
2025-03-13 15:29:11 +0100

    Démarre automatiquement le calcul de la trajectoire au chargement de la page Détail d'un indicateur

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/IndicateurDetail.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/detail/IndicateurDetail.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
+import { useEffect } from 'react';
+import { useCalculTrajectoire } from '../../Trajectoire/useCalculTrajectoire';
+import { useStatutTrajectoire } from '../../Trajectoire/useStatutTrajectoire';
 import { useIndicateurDefinition } from '../Indicateur/useIndicateurDefinition';
 import IndicateurLayout from './IndicateurLayout';
 
@@ -16,6 +19,26 @@ export const IndicateurDetail = ({
   isPerso = false,
 }: Props) => {
   const { data: definition, isLoading } = useIndicateurDefinition(indicateurId);
+
+  const {
+    mutate: calcul,
+    isLoading: isLoadingTrajectoire,
+    isIdle,
+  } = useCalculTrajectoire();
+  const { data: trajectoire } = useStatutTrajectoire();
+  const status = trajectoire?.status;
+
+  // démarre le calcul de la trajectoire au chargement de la page
+  useEffect(() => {
+    if (
+      !isPerso &&
+      isIdle &&
+      status === 'pret_a_calculer' &&
+      !isLoadingTrajectoire
+    ) {
+      calcul();
+    }
+  }, [calcul, isPerso, isIdle, isLoadingTrajectoire, status]);
 
   if (isLoading) return <SpinnerLoader />;
   if (!definition) return null;
