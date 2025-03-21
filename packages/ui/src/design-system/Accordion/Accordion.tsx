6ad9c40f6375a1d2d7bb79214d684fc23e04f7b9
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-21 16:59:35 +0100

    Mise à jour du composant Accordion du DS

--- packages/ui/src/design-system/Accordion/Accordion.tsx
+++ packages/ui/src/design-system/Accordion/Accordion.tsx
@@ -1,111 +1,136 @@
 import { Icon, IconValue } from '@/ui/design-system/Icon';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
-type AccordionType = {
+export type AccordionType = {
+  dataTest?: string;
   id?: string;
+  ref?: React.Ref<HTMLDivElement>;
   /** Icône ajoutée avant le titre */
   icon?: IconValue;
   /** Pour changer la position de l'icône */
   iconPosition?: 'left' | 'right';
   /** Titre */
   title: string;
-  /** Contenu (chaîne ou composant React personnalisé) */
-  content: string | React.ReactNode;
-  /** Permet de styler le contenu (ignoré si le contenu n'est pas une chaîne) */
-  contentClassname?: string;
   /** Pour afficher un texte optionnel à côté du titre */
   subtitle?: string;
+  /** Affiche un contenu supplémentaire à la fin de l'en-tête */
+  headerContent?: React.ReactNode;
+  /** Contenu (chaîne ou composant React personnalisé) */
+  content: string | React.ReactNode;
+  /** Permet de styler le conteneur */
+  containerClassname?: string;
+  /** Permet de styler l'en-tête */
+  headerClassname?: string;
+  /** Permet de styler la flèche d'ouverture */
+  arrowClassname?: string;
+  /** Permet de styler le contenu (ignoré si le contenu n'est pas une chaîne) */
+  contentClassname?: string;
 };
-
-/** Styles du container autour du contenu quand l'accordéon est déplié */
-export const ACCORDION_CONTENT_STYLE =
-  'px-10 py-6 border border-t-0 border-grey-4 rounded-b-lg text-grey-8 [&_a]:break-all';
 
 /**
  * Affiche un contenu complémentaire
  */
-export const AccordionControlled = ({
-  icon,
-  iconPosition = 'left',
-  id,
-  title,
-  content,
-  contentClassname,
-  expanded,
-  setExpanded,
-  subtitle,
-}: AccordionType & {
-  /** le contenu est visible */
-  expanded: boolean;
-  /** appelée pour déplier/replier le contenu */
-  setExpanded: (value: boolean) => void;
-}) => {
-  return (
-    <div>
-      {/** EN-TËTE */}
+export const AccordionControlled = forwardRef<
+  HTMLDivElement,
+  AccordionType & {
+    /** le contenu est visible */
+    expanded: boolean;
+    /** appelée pour déplier/replier le contenu */
+    setExpanded: (value: boolean) => void;
+  }
+>(
+  (
+    {
+      dataTest,
+      id,
+      icon,
+      iconPosition = 'left',
+      title,
+      subtitle,
+      headerContent,
+      content,
+      containerClassname,
+      headerClassname,
+      arrowClassname,
+      contentClassname,
+      expanded,
+      setExpanded,
+    },
+    ref
+  ) => {
+    return (
       <div
-        role="button"
-        tabIndex={0}
-        aria-controls={id}
-        aria-expanded={expanded}
-        onKeyDown={(e) => {
-          if (e.code === 'Space') {
-            e.preventDefault();
-            setExpanded(!expanded);
-          }
-        }}
-        onClick={() => setExpanded(!expanded)}
-        className={classNames(
-          'border border-grey-4 rounded-lg flex gap-3 px-6 py-4 font-bold text-primary-10 hover:bg-primary-1',
-          {
-            'border-primary-2 rounded-b-none bg-primary-2': expanded,
-            'bg-white': !expanded,
-          }
-        )}
+        data-test={dataTest}
+        className={classNames('border-y border-grey-4', containerClassname)}
+        ref={ref}
       >
-        {/** picto indiquant l'état ouvert/fermé */}
-        <Icon
-          icon="arrow-right-s-line"
-          className={classNames('text-primary transition-transform', {
-            'transform: rotate-90': expanded,
-          })}
-        />
-        {/** icône optionnelle avant le titre */}
-        {!!icon && iconPosition === 'left' && (
-          <Icon icon={icon} className="text-primary" />
-        )}
-        <div className="-mt-0.5">
-          {/** titre */}
-          {title}
-          {/** sous-titre */}
-          {!!subtitle && (
-            <span className="font-normal text-xs mt-2 line-clamp-1">
-              {subtitle}
-            </span>
+        {/** EN-TÊTE */}
+        <div
+          role="button"
+          tabIndex={0}
+          aria-controls={id}
+          aria-expanded={expanded}
+          onKeyDown={(e) => {
+            if (e.code === 'Space') {
+              e.preventDefault();
+              setExpanded(!expanded);
+            }
+          }}
+          onClick={() => setExpanded(!expanded)}
+          className={classNames(
+            'flex gap-3 items-center py-6 font-bold text-primary-9 cursor-pointer',
+            headerClassname
           )}
-        </div>
-        {/** icône optionnelle alignée à droite */}
-        {!!icon && iconPosition === 'right' && (
-          <Icon icon={icon} className="ml-auto text-primary" />
-        )}
-      </div>
-
-      {/** CONTENU */}
-      {!!expanded &&
-        (typeof content === 'string' ? (
-          <div
-            className={classNames(ACCORDION_CONTENT_STYLE, contentClassname)}
-            id={id}
-          >
-            {content}
+        >
+          {/** picto indiquant l'état ouvert/fermé */}
+          <Icon
+            icon="arrow-right-s-line"
+            className={classNames(
+              'text-primary transition-transform',
+              {
+                'transform: rotate-90': expanded,
+              },
+              arrowClassname
+            )}
+          />
+          {/** icône optionnelle avant le titre */}
+          {!!icon && iconPosition === 'left' && (
+            <Icon icon={icon} className="text-primary" />
+          )}
+          <div>
+            {/** titre */}
+            {title}
+            {/** sous-titre */}
+            {!!subtitle && (
+              <span className="font-normal text-xs mt-2 line-clamp-1">
+                {subtitle}
+              </span>
+            )}
           </div>
-        ) : (
-          content
-        ))}
-    </div>
-  );
-};
+          {/** icône optionnelle alignée à droite */}
+          {!!icon && iconPosition === 'right' && (
+            <Icon icon={icon} className="ml-auto text-primary" />
+          )}
+          {!!headerContent && <div className="ml-auto">{headerContent}</div>}
+        </div>
+
+        {/** CONTENU */}
+        {expanded &&
+          (typeof content === 'string' ? (
+            <div
+              className={classNames('px-8 pb-6 text-grey-8', contentClassname)}
+              id={id}
+            >
+              {content}
+            </div>
+          ) : (
+            content
+          ))}
+      </div>
+    );
+  }
+);
 
 export const Accordion = ({
   initialState,
