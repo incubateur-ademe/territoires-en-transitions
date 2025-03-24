4ae091c851cc4e00e5fe60d56b2578a48e3a55aa
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-03-24 18:27:46 +0100

    refactor: enrich listActionDefinitions instead of creating a new dedicated method

--- backend/src/referentiels/list-actions/list-actions.request.ts
+++ backend/src/referentiels/list-actions/list-actions.request.ts
@@ -6,54 +6,55 @@ import {
   referentielIdEnumSchema,
 } from '../index-domain';
 
-export const listActionsWithDetailsRequestSchema = z.object({
+export const listActionsRequestOptionsSchema = z.object({
+  actionIds: z
+    .string()
+    .array()
+    .optional()
+    .describe(
+      'Liste des identifiants des mesures du référentiel à récupérer (ex : [cae_2.1, eci_1.1]).'
+    ),
+  actionTypes: actionTypeSchema
+    .array()
+    .optional()
+    .describe(
+      'Liste des types de mesure du référentiel à récupérer (ex : [action, sous_action]).'
+    ),
+  utilisateurPiloteIds: z
+    .string()
+    .array()
+    .optional()
+    .describe(
+      "Liste des identifiants des utilisateurs pilotes permettant de récupérer les mesures pilotées par un d'eux."
+    ),
+  personnePiloteIds: z.coerce
+    .number()
+    .array()
+    .optional()
+    .describe(
+      "Liste des identifiants des tags pilotes permettant de récupérer les mesures pilotées par un d'eux."
+    ),
+  servicePiloteIds: z.coerce
+    .number()
+    .array()
+    .optional()
+    .describe(
+      "Liste des identifiants des services pilotes permettant de récupérer les mesures pilotées par un d'eux."
+    ),
+  referentielIds: referentielIdEnumSchema
+    .array()
+    .optional()
+    .describe(
+      'Liste des identifiants des référentiels à récupérer (ex : [cae, eci]).'
+    ),
+});
+
+export const listActionsRequestSchema = z.object({
   collectiviteId: z.number(),
-  filters: z
-    .object({
-      actionIds: z
-        .string()
-        .array()
-        .optional()
-        .describe(
-          'Liste des identifiants des mesures du référentiel à récupérer (ex : [cae_2.1, eci_1.1]).'
-        ),
-      actionTypes: actionTypeSchema
-        .array()
-        .optional()
-        .default([ActionTypeEnum.ACTION, ActionTypeEnum.SOUS_ACTION])
-        .describe(
-          'Liste des types de mesure du référentiel à récupérer (ex : [action, sous_action]).'
-        ),
-      utilisateurPiloteIds: z
-        .string()
-        .array()
-        .optional()
-        .describe(
-          "Liste des identifiants des utilisateurs pilotes permettant de récupérer les mesures pilotées par un d'eux."
-        ),
-      personnePiloteIds: z.coerce
-        .number()
-        .array()
-        .optional()
-        .describe(
-          "Liste des identifiants des tags pilotes permettant de récupérer les mesures pilotées par un d'eux."
-        ),
-      servicePiloteIds: z.coerce
-        .number()
-        .array()
-        .optional()
-        .describe(
-          "Liste des identifiants des services pilotes permettant de récupérer les mesures pilotées par un d'eux."
-        ),
-      referentielIds: referentielIdEnumSchema
-        .array()
-        .optional()
-        .default([ReferentielIdEnum.CAE, ReferentielIdEnum.ECI])
-        .describe(
-          'Liste des identifiants des référentiels à récupérer (ex : [cae, eci]).'
-        ),
-    })
-    .optional(),
+  filters: listActionsRequestOptionsSchema.default({
+    actionTypes: [ActionTypeEnum.ACTION, ActionTypeEnum.SOUS_ACTION],
+    referentielIds: [ReferentielIdEnum.CAE, ReferentielIdEnum.ECI],
+  }),
 });
 
 export const listActionsWithStatusRequestSchema = z.object({
@@ -64,3 +65,8 @@ export const listActionsWithStatusRequestSchema = z.object({
     .optional()
     .default([ActionTypeEnum.ACTION, ActionTypeEnum.SOUS_ACTION]),
 });
+
+export type ListActionsRequestType = z.infer<typeof listActionsRequestSchema>;
+export type ListActionsRequestOptionsType = z.infer<
+  typeof listActionsRequestOptionsSchema
+>;
