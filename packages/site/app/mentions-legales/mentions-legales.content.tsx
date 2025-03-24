d40325fcebaf8cb99471f72c217d1726fb94aebf
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-24 19:11:48 +0100

    Mise à jour contenu mentions légales

---
+++ packages/site/app/mentions-legales/mentions-legales.content.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Section from '@/site/components/sections/Section';
+import MentionsLegales from './mentions-legales.mdx';
+
+const MentionsLegalesContent = () => {
+  return (
+    <Section>
+      <h1>Mentions légales</h1>
+      <MentionsLegales />
+    </Section>
+  );
+};
+
+export default MentionsLegalesContent;
