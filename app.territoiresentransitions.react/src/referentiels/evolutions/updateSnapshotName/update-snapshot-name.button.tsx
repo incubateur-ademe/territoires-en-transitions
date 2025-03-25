4a6a762557ddca44b224172bfeb4e3c48e20c88a
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-03-25 21:01:18 +0100

    feat: create update and delete actions

---
+++ app.territoiresentransitions.react/src/referentiels/evolutions/updateSnapshotName/update-snapshot-name.button.tsx
@@ -0,0 +1,38 @@
+import { Icon } from '@/ui';
+import { useState } from 'react';
+import { UpdateSnapshotNameModal } from './update-snapshot-name.modal';
+
+type UpdateSnapshotNameButtonProps = {
+  snapshotRef: string;
+  snapshotName: string;
+};
+
+export const UpdateSnapshotNameButton = ({
+  snapshotName,
+  snapshotRef,
+}: UpdateSnapshotNameButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        className="text-sm text-grey-8"
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsOpen(true);
+        }}
+      >
+        <Icon icon="edit-line" size="sm" className="mr-2" />
+      </button>
+      {isOpen && (
+        <UpdateSnapshotNameModal
+          openState={{
+            isOpen,
+            setIsOpen,
+          }}
+          snapshotName={snapshotName}
+          snapshotRef={snapshotRef}
+        />
+      )}
+    </>
+  );
+};
