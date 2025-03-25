c329836acbe8bb96b1ff6e40abee7500a0092d76
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-25 13:21:46 +0100

    Supprime legacy ActionDefinitionSummary de score progress bar et la carte action

--- app.territoiresentransitions.react/src/referentiels/scores/score.progress-bar.tsx
+++ app.territoiresentransitions.react/src/referentiels/scores/score.progress-bar.tsx
@@ -1,34 +1,40 @@
 import { avancementToLabel } from '@/app/app/labels';
 import { actionAvancementColors } from '@/app/app/theme';
-import { ActionDefinitionSummary } from '@/app/referentiels/ActionDefinitionSummaryReadEndpoint';
 import { useActionScore } from '@/app/referentiels/DEPRECATED_score-hooks';
 import ProgressBarWithTooltip from '@/app/referentiels/scores/progress-bar-with-tooltip';
+import { ActionType } from '@/domain/referentiels';
 import { useScore, useSnapshotFlagEnabled } from '../use-snapshot';
 
 type ScoreProgressBarProps = {
-  actionDefinition: ActionDefinitionSummary;
   className?: string;
   displayDoneValue?: boolean;
   valuePosition?: 'left' | 'right';
+  id: string;
+  identifiant: string;
+  type: ActionType;
 };
 
 export const ScoreProgressBar = ({
-  actionDefinition,
+  id,
+  identifiant,
+  type,
   className,
   displayDoneValue = false,
   valuePosition,
 }: ScoreProgressBarProps) => {
   const isSnapshotEnabled = useSnapshotFlagEnabled();
-  const score = useActionScore(actionDefinition.id, !isSnapshotEnabled);
-  const isReglementaire = actionDefinition.identifiant.split('.').includes('0');
+  const score = useActionScore(id, !isSnapshotEnabled);
+  const isReglementaire = identifiant.split('.').includes('0');
 
   if (isSnapshotEnabled) {
     return (
       <ActionProgressBar_Snapshot
-        actionDefinition={actionDefinition}
         displayDoneValue={displayDoneValue}
         valuePosition={valuePosition}
         className={className}
+        id={id}
+        identifiant={identifiant}
+        type={type}
       />
     );
   }
@@ -41,7 +47,7 @@ export const ScoreProgressBar = ({
     {
       label: avancementToLabel.fait,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.fait_taches_avancement
           : score.point_fait,
       color: actionAvancementColors.fait,
@@ -49,7 +55,7 @@ export const ScoreProgressBar = ({
     {
       label: avancementToLabel.programme,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.programme_taches_avancement
           : score.point_programme,
       color: actionAvancementColors.programme,
@@ -57,7 +63,7 @@ export const ScoreProgressBar = ({
     {
       label: avancementToLabel.pas_fait,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.pas_fait_taches_avancement
           : score.point_pas_fait,
       color: actionAvancementColors.pas_fait,
@@ -77,7 +83,7 @@ export const ScoreProgressBar = ({
         label: avancementToLabel.non_renseigne,
         color: actionAvancementColors.non_renseigne,
       }}
-      percent={actionDefinition.type === 'tache'}
+      percent={type === 'tache'}
       className={className}
       valueToDisplay={displayDoneValue ? avancementToLabel.fait : undefined}
       valuePosition={valuePosition}
@@ -87,22 +93,17 @@ export const ScoreProgressBar = ({
 
 export default ScoreProgressBar;
 
-type ActionProgressBarProps = {
-  actionDefinition: ActionDefinitionSummary;
-  className?: string;
-  displayDoneValue?: boolean;
-  valuePosition?: 'left' | 'right';
-};
-
 // TODO-FLAG promot this component to replace the old one when snapshots will be validated in production
 function ActionProgressBar_Snapshot({
-  actionDefinition,
+  id,
+  identifiant,
+  type,
   className,
   displayDoneValue = false,
   valuePosition,
-}: ActionProgressBarProps) {
-  const score = useScore(actionDefinition.id);
-  const isReglementaire = actionDefinition.identifiant.split('.').includes('0');
+}: ScoreProgressBarProps) {
+  const score = useScore(id);
+  const isReglementaire = identifiant.split('.').includes('0');
 
   if (
     score === undefined ||
@@ -115,7 +116,7 @@ function ActionProgressBar_Snapshot({
     {
       label: avancementToLabel.fait,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.faitTachesAvancement
           : score.pointFait,
       color: actionAvancementColors.fait,
@@ -123,7 +124,7 @@ function ActionProgressBar_Snapshot({
     {
       label: avancementToLabel.programme,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.programmeTachesAvancement
           : score.pointProgramme,
       color: actionAvancementColors.programme,
@@ -131,7 +132,7 @@ function ActionProgressBar_Snapshot({
     {
       label: avancementToLabel.pas_fait,
       value:
-        actionDefinition.type === 'tache' || isReglementaire
+        type === 'tache' || isReglementaire
           ? score.pasFaitTachesAvancement
           : score.pointPasFait,
       color: actionAvancementColors.pas_fait,
@@ -153,7 +154,7 @@ function ActionProgressBar_Snapshot({
       }}
       valueToDisplay={displayDoneValue ? avancementToLabel.fait : undefined}
       valuePosition={valuePosition}
-      percent={actionDefinition.type === 'tache'}
+      percent={type === 'tache'}
       className={className}
     />
   );
