c4f30de860986853f660726c025f46d10c2b2837
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-03-25 16:48:03 +0100

    Ajout de catégories aux actus via Strapi

---
+++ strapi/src/api/actualites-categorie/services/actualites-categorie.ts
@@ -0,0 +1,7 @@
+/**
+ * actualites-categorie service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::actualites-categorie.actualites-categorie');
