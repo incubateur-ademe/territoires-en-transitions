ffb201b5d7ad06d34a083c6ec3f783dc3861a9a6
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-03-25 21:01:18 +0100

    feat: create update and delete actions

---
+++ app.territoiresentransitions.react/src/referentiels/evolutions/deleteSnapshot/delete-snapshot.button.tsx
@@ -0,0 +1,35 @@
+import { Icon } from '@/ui';
+import { useState } from 'react';
+import { DeleteSnapshotModal } from './delete-snapshot.modal';
+
+type DeleteSnapshotButtonProps = {
+  snapshotRef: string;
+};
+
+export const DeleteSnapshotButton = ({
+  snapshotRef,
+}: DeleteSnapshotButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        className="text-sm text-grey-8"
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsOpen(true);
+        }}
+      >
+        <Icon icon="delete-bin-line" size="sm" className="mr-2" />
+      </button>
+      {isOpen && (
+        <DeleteSnapshotModal
+          snapshotRef={snapshotRef}
+          openState={{
+            isOpen,
+            setIsOpen,
+          }}
+        />
+      )}
+    </>
+  );
+};
