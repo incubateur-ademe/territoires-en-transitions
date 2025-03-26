efc6531fe447674498741dfaecfbd5d731e6a155
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-03-26 17:29:37 +0100

    Affiche les catégories sur la page de l'article

--- packages/site/components/cards/BlogCardContent.tsx
+++ packages/site/components/cards/BlogCardContent.tsx
@@ -62,7 +62,7 @@ const BlogCardContent = ({
             {categories.map((category, idx) => (
               <Badge
                 key={`${idx}-${category}`}
-                title={category.label}
+                title={category}
                 state="info"
                 size="sm"
                 light
