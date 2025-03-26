930f323cebca4cbb9e8d72ea9be6cf6c2852b473
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-26 12:01:39 +0100

    Refactorise l'onglet "Mesures" du référentiel et ajoute les filtres

---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/referentiel/[referentielId]/(overview)/@tabs/progression/_components/action.list.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useCollectiviteId } from '@/app/collectivites/collectivite-context';
+import { ActionListFilters } from '@/app/referentiels/actions/use-list-actions';
+import { useReferentielId } from '@/app/referentiels/referentiel-context';
+import { useExportScore } from '@/app/referentiels/useExportScore';
+import FilterBadges, { useFiltersToBadges } from '@/app/ui/lists/filter-badges';
+import { ActionTypeEnum } from '@/domain/referentiels';
+import { Button, ButtonMenu, Checkbox, Select } from '@/ui';
+import Filters from './filters';
+import List from './list';
+
+export type DisplayOption = 'axe' | 'action';
+
+const ActionList = () => {
+  const referentielId = useReferentielId();
+  const collectiviteId = useCollectiviteId();
+
+  const { mutate: exportScore, isLoading } = useExportScore(
+    referentielId,
+    collectiviteId
+  );
+
+  const initialFilters: ActionListFilters = {
+    actionTypes: [
+      ActionTypeEnum.AXE,
+      ActionTypeEnum.SOUS_AXE,
+      ActionTypeEnum.ACTION,
+    ],
+    referentielIds: [referentielId],
+  };
+
+  const [filters, setFilters] = useState(initialFilters);
+
+  const { data: filterBadges } = useFiltersToBadges({ filters });
+
+  const [showDescriptionOn, setShowDescription] = useState(false);
+
+  const [displayOption, setDisplayOption] = useState<DisplayOption>('axe');
+
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+  const onFilterChange = (filters: ActionListFilters) => {
+    if (filters) {
+      setFilters(filters);
+      setDisplayOption('action');
+    }
+  };
+
+  return (
+    <main data-test="ActionsReferentiels" className="flex flex-col gap-6">
+      <div className="relative flex max-md:flex-col md:items-center gap-6 pb-6 border-b border-grey-4">
+        <div className="w-full md:w-56">
+          <Select
+            options={[
+              { value: 'axe', label: 'Par axe' },
+              { value: 'action', label: 'Par mesure' },
+            ]}
+            onChange={(value) =>
+              setDisplayOption((value as DisplayOption) ?? 'action')
+            }
+            values={[displayOption]}
+            customItem={(v) => (
+              <span className="text-grey-8 font-normal">{v.label}</span>
+            )}
+            small
+          />
+        </div>
+
+        <Checkbox
+          label="Afficher la description des mesures"
+          variant="switch"
+          labelClassname="font-normal text-sm !text-grey-7"
+          containerClassname="items-center"
+          checked={showDescriptionOn}
+          onChange={() => setShowDescription(!showDescriptionOn)}
+          disabled={isLoading}
+        />
+
+        <ButtonMenu
+          openState={{
+            isOpen: isFilterOpen,
+            setIsOpen: setIsFilterOpen,
+          }}
+          className="ml-auto"
+          size="sm"
+          variant="outlined"
+          icon="equalizer-line"
+          text="Filtrer"
+        >
+          <Filters
+            filters={filters}
+            setFilters={(filters) => onFilterChange(filters)}
+          />
+        </ButtonMenu>
+
+        <div className="w-px h-8 bg-grey-4" />
+
+        <Button
+          data-test="export-scores"
+          icon={'download-fill'}
+          disabled={isLoading}
+          onClick={() => exportScore()}
+          size="sm"
+        />
+      </div>
+
+      <FilterBadges
+        badges={filterBadges}
+        resetFilters={() => onFilterChange(initialFilters)}
+      />
+
+      <List
+        filters={filters}
+        display={displayOption}
+        showDescriptionOn={showDescriptionOn}
+      />
+    </main>
+  );
+};
+
+export default ActionList;
---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/referentiel/[referentielId]/(overview)/@tabs/progression/_components/axe.tsx
@@ -0,0 +1,101 @@
+import classNames from 'classnames';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
+
+import { ActionCard } from '@/app/referentiels/actions/action.card';
+import { Action as TActionBase } from '@/app/referentiels/actions/use-list-actions';
+import ScoreProgressBar from '@/app/referentiels/scores/score.progress-bar';
+import { ScoreRatioBadge } from '@/app/referentiels/scores/score.ratio-badge';
+import { ActionType } from '@/domain/referentiels';
+import { AccordionControlled, AccordionType } from '@/ui';
+
+export type TAxe = TActionBase & {
+  children?: TAxe[];
+};
+
+type Props = {
+  axe: TAxe;
+  accordionProps?: Omit<AccordionType, 'title' | 'content'>;
+  showDescription?: boolean;
+};
+
+/** Axe et sous-axe de la liste de mesures du référentiel */
+const Axe = ({ axe, accordionProps, showDescription }: Props) => {
+  const searchParams = useSearchParams();
+  const idParam = searchParams.get('axe');
+
+  const [isOpen, setIsOpened] = useState(
+    idParam ? axe.actionId.includes(idParam) : false
+  );
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (axe.actionId === idParam) {
+      ref.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }, [idParam, axe.actionId]);
+
+  const isActionChildren = axe.children?.[0]?.actionType === 'action';
+
+  return (
+    <AccordionControlled
+      ref={ref}
+      dataTest={`ExpandableAction-${axe.identifiant}`}
+      title={axe.nom}
+      containerClassname="!border-t-0 last:border-b-0"
+      headerClassname={classNames({ '!pb-4': isOpen })}
+      expanded={isOpen}
+      setExpanded={() => setIsOpened(!isOpen)}
+      headerContent={
+        <div className="flex items-center">
+          <ScoreProgressBar
+            id={axe.actionId}
+            identifiant={axe.identifiant}
+            type={axe.actionType as ActionType}
+            className="w-48"
+          />
+          <ScoreRatioBadge
+            actionId={axe.actionId}
+            className="justify-end w-64"
+          />
+        </div>
+      }
+      content={
+        isActionChildren ? (
+          <div
+            className={classNames('grid grid-rows-1 grid-cols-3 gap-4 mb-4', {
+              '!grid-cols-1': showDescription,
+            })}
+          >
+            {axe.children?.map((child) => (
+              <ActionCard
+                key={child.actionId}
+                action={child}
+                showDescription={showDescription}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className={classNames('flex flex-col')}>
+            {axe.children?.map((child) => (
+              <Axe
+                key={child.actionId}
+                axe={{ ...child, nom: `${child.identifiant} ${child.nom}` }}
+                accordionProps={{
+                  containerClassname: 'ml-4 !border-0 !text-grey-7',
+                  headerClassname: '!py-4 !text-grey-7',
+                  arrowClassname: '!text-grey-7',
+                }}
+                showDescription={showDescription}
+              />
+            ))}
+          </div>
+        )
+      }
+      {...accordionProps}
+    />
+  );
+};
+
+export default Axe;
