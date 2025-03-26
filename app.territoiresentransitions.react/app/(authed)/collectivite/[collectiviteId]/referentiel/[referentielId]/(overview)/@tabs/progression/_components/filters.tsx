0202f18b43fdb6a29f8f0872b95c0593e2701b04
Arthur Molinos <arthur.molinos@gmail.com>
2025-03-26 12:01:39 +0100

    Refactorise l'onglet "Mesures" du référentiel et ajoute les filtres

---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/referentiel/[referentielId]/(overview)/@tabs/progression/_components/filters.tsx
@@ -0,0 +1,60 @@
+import { Field, FormSection } from '@/ui';
+
+import { ActionListFilters } from '@/app/referentiels/actions/use-list-actions';
+import PersonnesDropdown from '@/app/ui/dropdownLists/PersonnesDropdown/PersonnesDropdown';
+import {
+  getPilotesValues,
+  splitPilotePersonnesAndUsers,
+} from '@/app/ui/dropdownLists/PersonnesDropdown/utils';
+import ServicesPilotesDropdown from '@/app/ui/dropdownLists/ServicesPilotesDropdown/ServicesPilotesDropdown';
+
+type Props = {
+  filters: ActionListFilters;
+  setFilters: (filters: ActionListFilters) => void;
+};
+
+const Filters = ({ filters, setFilters }: Props) => {
+  const pilotes = [];
+  if (filters?.utilisateurPiloteIds) {
+    pilotes.push(...filters.utilisateurPiloteIds);
+  }
+  if (filters?.personnePiloteIds) {
+    pilotes.push(...filters.personnePiloteIds.map(String));
+  }
+
+  const pils = getPilotesValues({
+    personnePiloteIds: filters?.personnePiloteIds || [],
+    utilisateurPiloteIds: filters?.utilisateurPiloteIds || [],
+  });
+
+  return (
+    <div className="w-full sm:w-[28rem] lg:gap-12 p-4 lg:p-8">
+      <FormSection title="Filtrer :" className="!grid-cols-1">
+        <Field title="Personne pilote :">
+          <PersonnesDropdown
+            values={pilotes.length ? pilotes : undefined}
+            onChange={({ personnes }) => {
+              setFilters({
+                ...filters,
+                ...splitPilotePersonnesAndUsers(personnes),
+              });
+            }}
+          />
+        </Field>
+        <Field title="Direction ou service pilote :">
+          <ServicesPilotesDropdown
+            values={filters?.servicePiloteIds}
+            onChange={({ services }) => {
+              setFilters({
+                ...filters,
+                servicePiloteIds: services.map((s) => s.id),
+              });
+            }}
+          />
+        </Field>
+      </FormSection>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/referentiel/[referentielId]/(overview)/@tabs/progression/_components/list.tsx
@@ -0,0 +1,83 @@
+import { ActionCard } from '@/app/referentiels/actions/action.card';
+import {
+  ActionListFilters,
+  useListActions,
+} from '@/app/referentiels/actions/use-list-actions';
+import PictoDocument from '@/app/ui/pictogrammes/PictoDocument';
+import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
+import { ActionTypeEnum } from '@/domain/referentiels';
+import { EmptyCard } from '@/ui';
+import classNames from 'classnames';
+import { DisplayOption } from './action.list';
+import Axe from './axe';
+import { buildReferentiel } from './utils';
+
+type Props = {
+  filters: ActionListFilters;
+  display?: DisplayOption;
+  showDescriptionOn: boolean;
+};
+
+const List = ({ filters, display = 'action', showDescriptionOn }: Props) => {
+  const { data: actionList, isLoading: isLoadingActions } = useListActions({
+    ...filters,
+    actionTypes:
+      display === 'axe'
+        ? [ActionTypeEnum.AXE, ActionTypeEnum.SOUS_AXE, ActionTypeEnum.ACTION]
+        : [ActionTypeEnum.ACTION],
+  });
+
+  if (isLoadingActions)
+    return (
+      <div className="flex justify-center items-center h-80">
+        <SpinnerLoader />
+      </div>
+    );
+
+  if (!actionList) return <div className="min-h-80" />;
+
+  if (actionList.length === 0) {
+    return (
+      <EmptyCard
+        picto={(props) => <PictoDocument {...props} />}
+        title="Aucun résultat pour ce filtre !"
+      />
+    );
+  }
+
+  if (display === 'axe') {
+    return (
+      <div className="flex flex-col">
+        {buildReferentiel(actionList).map((axe) => (
+          <Axe
+            key={axe.actionId}
+            axe={{ ...axe, nom: `${axe.identifiant} - ${axe.nom}` }}
+            showDescription={showDescriptionOn}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  if (display === 'action') {
+    return (
+      <div
+        className={classNames('grid grid-cols-1 gap-4 grid-rows-1', {
+          'sm:grid-cols-2 lg:grid-cols-3': !showDescriptionOn,
+        })}
+      >
+        {actionList
+          .filter((action) => action.actionType === 'action')
+          .map((action) => (
+            <ActionCard
+              key={action.actionId}
+              action={action}
+              showDescription={showDescriptionOn}
+            />
+          ))}
+      </div>
+    );
+  }
+};
+
+export default List;
