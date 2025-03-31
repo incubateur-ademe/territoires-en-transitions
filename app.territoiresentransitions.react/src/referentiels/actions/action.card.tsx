1a373e14b3e05e4f2a22617b25cdb72dce03e622
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-03-31 13:00:17 +0200

    refactor: temporary comment pilotes to avoid type error

--- app.territoiresentransitions.react/src/referentiels/actions/action.card.tsx
+++ app.territoiresentransitions.react/src/referentiels/actions/action.card.tsx
@@ -4,7 +4,6 @@ import { makeReferentielActionUrl } from '@/app/app/paths';
 import { useCurrentCollectivite } from '@/app/collectivites/collectivite-context';
 import ActionEditModal from '@/app/referentiels/actions/action-edit.modal';
 import { Action } from '@/app/referentiels/actions/use-list-actions';
-import ListWithTooltip from '@/app/ui/lists/ListWithTooltip';
 import {
   ActionType,
   getReferentielIdFromActionId,
@@ -89,7 +88,7 @@ export const ActionCard = ({ action, showDescription }: ActionCardProps) => {
         </div>
 
         {/** Pilotes et services */}
-        {(action.pilotes.length > 0 || action.services.length > 0) && (
+        {/* {(action.pilotes.length > 0 || action.services.length > 0) && (
           <div className="flex flex-wrap items-center gap-x-3 gap-y-1 text-sm text-primary-10">
             {action.pilotes.length > 0 && (
               <ListWithTooltip
@@ -109,7 +108,7 @@ export const ActionCard = ({ action, showDescription }: ActionCardProps) => {
               />
             )}
           </div>
-        )}
+        )} */}
       </Card>
     </div>
   );
