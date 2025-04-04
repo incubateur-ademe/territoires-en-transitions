0090ed8b24951e7530b52c6a5cc4c03a6187af7d
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-04-04 16:26:31 +0200

    Renomme l'onglet collectivités en référentiels sur la page Collectivités

--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredReferentiels.ts
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredReferentiels.ts
@@ -1,16 +1,16 @@
-import { CollectiviteEngagee} from '@/api';
-import { NB_CARDS_PER_PAGE } from '@/app/app/pages/CollectivitesEngagees/data/utils';
+import { CollectiviteEngagee } from '@/api';
 import { trpc } from '@/api/utils/trpc/client';
+import { NB_CARDS_PER_PAGE } from '@/app/app/pages/CollectivitesEngagees/data/utils';
 
 /**
  * Renvoi une liste de collectivités en fonction d'un ensemble de filtres
  */
-export const useFilteredCollectivites = (args: CollectiviteEngagee.Filters) => {
+export const useFilteredReferentiels = (args: CollectiviteEngagee.Filters) => {
   const { data, isLoading } =
     trpc.collectivites.recherches.referentiels.useQuery({
       ...args,
       nbCards: NB_CARDS_PER_PAGE,
-      nom: Array.isArray(args.nom) ? (args.nom[0] || '') : args.nom || ''
+      nom: Array.isArray(args.nom) ? args.nom[0] || '' : args.nom || '',
     });
 
   return {
@@ -19,4 +19,3 @@ export const useFilteredCollectivites = (args: CollectiviteEngagee.Filters) => {
     collectivitesCount: data?.length || 0,
   };
 };
-
