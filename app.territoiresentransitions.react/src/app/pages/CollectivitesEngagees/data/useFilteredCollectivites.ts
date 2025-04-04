a4bc7c49b53e8d30cb7fb5bed2c08bd7a9016ab5
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-04-04 17:33:42 +0200

    Affiche la nouvelle liste de collectivités

--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredCollectivites.ts
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredCollectivites.ts
@@ -4,11 +4,10 @@ import { NB_CARDS_PER_PAGE } from '@/app/app/pages/CollectivitesEngagees/data/ut
 
 /**
  * Renvoi une liste de collectivités en fonction d'un ensemble de filtres
- * et basée sur les référentiels CAE / ECi
  */
-export const useFilteredReferentiels = (args: CollectiviteEngagee.Filters) => {
+export const useFilteredCollectivites = (args: CollectiviteEngagee.Filters) => {
   const { data, isLoading } =
-    trpc.collectivites.recherches.referentiels.useQuery({
+    trpc.collectivites.recherches.collectivites.useQuery({
       ...args,
       nbCards: NB_CARDS_PER_PAGE,
       nom: Array.isArray(args.nom) ? args.nom[0] || '' : args.nom || '',
