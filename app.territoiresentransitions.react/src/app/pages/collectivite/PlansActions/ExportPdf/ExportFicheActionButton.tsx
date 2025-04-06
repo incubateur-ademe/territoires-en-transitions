14338a4ab94a7e8288c9c7c11159976c3f4c9f85
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-06 23:12:20 +0200

    refactor: update useActionsLiees hook to handle undefined actionIds and simplify actions filtering

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ExportPdf/ExportFicheActionButton.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/ExportPdf/ExportFicheActionButton.tsx
@@ -38,7 +38,7 @@ export const FicheActionPdfContent = ({
     useFichesActionLiees(fiche.id, options.fiches.isChecked);
 
   const { actions: actionsLiees, isLoading: isLoadingActionsLiees } =
-    useActionsLiees(fiche?.actions?.map((action) => action.id) ?? []);
+    useActionsLiees(fiche?.actions?.map((action) => action.id));
 
   const { data: annexes, isLoading: isLoadingAnnexes } =
     useAnnexesFicheActionInfos(fiche.id, options.notes_docs.isChecked);
@@ -74,9 +74,7 @@ export const FicheActionPdfContent = ({
           indicateursListe,
           etapes,
           fichesLiees,
-          actionsLiees: actionsLiees.filter(
-            (action) => action.score !== undefined
-          ),
+          actionsLiees,
           annexes,
           notesSuivi,
         })
