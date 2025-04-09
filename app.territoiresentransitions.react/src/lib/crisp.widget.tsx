3d4c889b1769627c8b2edbf129bf2e8e22c59fa2
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2025-04-09 20:10:03 +0200

    fix(app): set crisp segment to chat

--- app.territoiresentransitions.react/src/lib/crisp.widget.tsx
+++ app.territoiresentransitions.react/src/lib/crisp.widget.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ENV } from '@/api/environmentVariables';
+import { setCrispSegments } from 'app.territoiresentransitions.react/app/(authed)/app-providers';
 import { Crisp } from 'crisp-sdk-web';
 import { useEffect } from 'react';
 
@@ -8,6 +9,8 @@ export default function CrispChat() {
   useEffect(() => {
     if (ENV.crisp_website_id && ENV.crisp_website_id.length > 0) {
       Crisp.configure(ENV.crisp_website_id);
+
+      setCrispSegments();
     }
   });
 
