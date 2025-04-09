2759091822438c2b132375389e5fa9388ac6078f
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-09 12:08:34 +0200

    refactor: turn TEST_MODE off

--- app.territoiresentransitions.react/src/ui/export-pdf/ExportPDFButton.tsx
+++ app.territoiresentransitions.react/src/ui/export-pdf/ExportPDFButton.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import { saveBlob } from '../../referentiels/preuves/Bibliotheque/saveBlob';
 import DocumentToExport from './DocumentToExport';
 
-const TEST_MODE = true;
+const TEST_MODE = false;
 
 export type ExportPDFButtonType = Pick<
   ButtonProps,
