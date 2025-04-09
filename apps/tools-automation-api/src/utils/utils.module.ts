cec0892fe7cfcdb42022fcfe6abda5ebc0dde201
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2025-04-09 14:15:34 +0200

    feat(tools-automation-api): add context store, use backend logger, add zod validation pipe, add sentry integration

--- apps/tools-automation-api/src/utils/utils.module.ts
+++ apps/tools-automation-api/src/utils/utils.module.ts
@@ -1,13 +1,22 @@
+import { ContextStoreService } from '@/backend/utils/context/context.service';
+import { VersionController } from '@/backend/utils/version/version.controller';
 import { Module } from '@nestjs/common';
 import MattermostNotificationService from 'apps/tools-automation-api/src/utils/mattermost-notification.service';
 import { ConfigurationModule } from '../config/configuration.module';
 import { TrpcClientService } from './trpc/trpc-client.service';
-import { VersionController } from './version/version.controller';
 
 @Module({
   imports: [ConfigurationModule],
   controllers: [VersionController],
-  providers: [TrpcClientService, MattermostNotificationService],
-  exports: [TrpcClientService, MattermostNotificationService],
+  providers: [
+    TrpcClientService,
+    MattermostNotificationService,
+    ContextStoreService,
+  ],
+  exports: [
+    TrpcClientService,
+    MattermostNotificationService,
+    ContextStoreService,
+  ],
 })
 export class UtilsModule {}
