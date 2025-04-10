441bd502a2e5995a6bc900ecb167545aaceb4221
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2025-04-10 14:35:12 +0200

    feat(strapi): add backup plugin

--- strapi/.strapi/client/app.js
+++ strapi/.strapi/client/app.js
@@ -4,6 +4,7 @@
  */
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import strapiBackupPlugin from "strapi-backup-plugin/strapi-admin";
 import reactIcons from "strapi-plugin-react-icons/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
@@ -11,6 +12,7 @@ renderAdmin(document.getElementById("strapi"), {
   plugins: {
     i18n: i18N,
     "users-permissions": usersPermissions,
+    "strapi-backup-plugin": strapiBackupPlugin,
     "react-icons": reactIcons,
   },
 });
--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,26 @@
-export default ({ env }) => ({ "react-icons": true });
+export default ({ env }) => ({
+  'react-icons': true,
+  'strapi-backup-plugin': {
+    enabled: true,
+    config: {
+      paths: [
+        './public/uploads', // usual
+        './config', // if database file is there
+        './data', // or wherever your .sqlite file is
+        './.tmp',
+      ],
+      database: {
+        type: 'sqlite',
+        databaseDriver: 'sqlite',
+        filePath: './.tmp/data.db', // Path to your SQLite database
+      },
+      include: {
+        files: true, // include public/uploads
+        database: true,
+      },
+      errorHandler: (error, strapi) => {
+        console.log(error);
+      },
+    },
+  },
+});
