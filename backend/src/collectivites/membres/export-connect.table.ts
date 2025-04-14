96e4af697f59c14958b94694f4c21277233e2047
Marc Rutkowski <marc@attractive-media.fr>
2025-04-14 10:57:52 +0200

    Ajoute une table pour enregistrer les utilisateurs exportés vers Connect

---
+++ backend/src/collectivites/membres/export-connect.table.ts
@@ -0,0 +1,23 @@
+import { authUsersTable, utilisateurSchema } from '@/backend/auth/index-domain';
+import { modifiedAt } from '@/backend/utils/column.utils';
+import { text, uuid } from 'drizzle-orm/pg-core';
+import { createSelectSchema } from 'drizzle-zod';
+import z from 'zod';
+
+export const exportConnectTable = utilisateurSchema.table('export_connect', {
+  userId: uuid('user_id')
+    .references(() => authUsersTable.id)
+    .notNull(),
+  /** l'email utilisé pour le dernier export et qui sert d'id pour les MàJ du contact */
+  exportId: text('export_id').notNull(),
+  checksum: text('checksum').notNull(),
+  modifiedAt,
+});
+
+export const exportConnectSchema = createSelectSchema(exportConnectTable);
+
+export const upsertExportConnectSchema = exportConnectSchema.omit({
+  modifiedAt: true,
+}).array();
+
+export type UpsertExportConnect = z.infer<typeof upsertExportConnectSchema>;
