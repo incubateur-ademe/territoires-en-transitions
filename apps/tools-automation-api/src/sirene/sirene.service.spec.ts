b6f3e8e30a9a947964cb3d8b76a2481c17cbcd52
Marc Rutkowski <marc@attractive-media.fr>
2025-04-16 14:12:42 +0200

    Corrige le chargement de l'env. pour les tests

--- apps/tools-automation-api/src/sirene/sirene.service.spec.ts
+++ apps/tools-automation-api/src/sirene/sirene.service.spec.ts
@@ -1,6 +1,5 @@
-import { ToolsAutomationApiConfigurationType } from '@/tools-automation-api/config/configuration.model';
-import ConfigurationService from '@/tools-automation-api/config/configuration.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
 import { SireneService } from './sirene.service';
 
 describe('SireneService', () => {
@@ -8,35 +7,14 @@ describe('SireneService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SireneService],
-    })
-    .useMocker((token) => {
-      if (token === ConfigurationService) {
-        return {
-          get(key: keyof ToolsAutomationApiConfigurationType) {
-            if (key === 'SIRENE_API_KEY') {
-              return 'fake';
-            }
-            if (key === 'SIRENE_AUTH_URL') {
-              return 'fake'
-            }
-            if (key === 'SIRENE_API_URL') {
-              return 'fake'
-            }
-          },
-        };
-      }
-    })
-    .compile();
+      imports: [AppModule],
+    }).compile();
 
     service = module.get<SireneService>(SireneService);
   });
 
-  // remplacer les valeurs `fake` dans le mock et enlever le `skip` pour éxécuter le test
-  test.skip("Détermine le NIC du siège légal de chaque collectivité à partir de son SIREN.", async () => {
+  test('Détermine le NIC du siège légal de chaque collectivité à partir de son SIREN.', async () => {
     const result = await service.getNIC(['200034825']);
-    expect(result).toMatchObject([
-      { siren: '200034825', nic: '00014' },
-    ]);
+    expect(result).toMatchObject([{ siren: '200034825', nic: '00014' }]);
   });
 });
