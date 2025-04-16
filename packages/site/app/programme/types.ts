91047e245b99445314b8a8220f067cac5350f499
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-04-16 12:01:33 +0200

    Mise en avant de l'annuaire sur la page programme

--- packages/site/app/programme/types.ts
+++ packages/site/app/programme/types.ts
@@ -7,39 +7,3 @@ export type Content = {
   image?: StrapiItem;
   href?: string;
 };
-
-export type ProgrammeData = {
-  titre: string;
-  description?: string;
-  couvertureURL?: string;
-  objectifs: {
-    titre: string;
-    description: string;
-    contenu: Content[] | null;
-  };
-  services: {
-    titre: string;
-    description: string;
-    contenu: Content[] | null;
-  };
-  compte: {
-    description: string;
-  };
-  benefices: {
-    titre: string;
-    description: string;
-    contenu: Content[] | null;
-  };
-  etapes: {
-    titre: string;
-    description: string;
-    contenu: Content[] | null;
-  };
-  ressources: {
-    description: string;
-    buttons: {
-      titre: string;
-      href: string;
-    }[];
-  };
-};
