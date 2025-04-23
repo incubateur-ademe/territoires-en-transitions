73da8b51faa63421bf53c9f60f0b21b14f46769f
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-23 17:37:27 +0200

    refactor: rename fetchFichesFilterRequestSchema to getFilteredFichesRequestSchema and update related imports and types

--- backend/src/plans/fiches/index-domain.ts
+++ backend/src/plans/fiches/index-domain.ts
@@ -2,6 +2,7 @@
 // Exports from this file can be imported with `@/domain/*` alias path.
 
 export * from './count-by/count-by-property-options.enum';
+export * from './shared/fetch-fiches-filter.request';
 export * from './shared/models//fiche-action-with-relations.dto';
 export * from './shared/models/axe.table';
 export * from './shared/models/fiche-action-effet-attendu.table';
