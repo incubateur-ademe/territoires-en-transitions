92c40cf98378b88312fc0ed3a638d5007916024f
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-23 10:56:26 +0200

    refactor: replace supabase call with trpc

---
+++ backend/src/plans/fiches/fiches.router.ts
@@ -0,0 +1,30 @@
+import FicheActionListService from '@/backend/plans/fiches/fiches-action-list.service';
+import { getFichesRequestSchema } from '@/backend/plans/fiches/shared/fetch-fiches-filter.request';
+import { TrpcService } from '@/backend/utils/trpc/trpc.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ListFichesRouter {
+  constructor(
+    private readonly trpc: TrpcService,
+    private readonly service: FicheActionListService
+  ) {}
+
+  router = this.trpc.router({
+    listResumes: this.trpc.authedProcedure
+      .input(getFichesRequestSchema)
+      .query(({ input, ctx }) => {
+        return this.service.getFichesActionResumes(
+          input.collectiviteId,
+          input.filters,
+          {
+            page: input.queryOptions?.page!,
+            limit: input.queryOptions?.limit!,
+            sort: input.queryOptions?.sort!,
+          }
+        );
+      }),
+  });
+
+  createCaller = this.trpc.createCallerFactory(this.router);
+}
