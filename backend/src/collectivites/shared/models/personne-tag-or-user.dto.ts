d361ad91ace9c7d4dcbb4007684faef3d2b56d92
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-23 11:00:29 +0200

    fix: update personneTagOrUserSchema to require nom and add collectiviteId field

--- backend/src/collectivites/shared/models/personne-tag-or-user.dto.ts
+++ backend/src/collectivites/shared/models/personne-tag-or-user.dto.ts
@@ -2,9 +2,10 @@ import { z } from 'zod';
 
 export const personneTagOrUserSchema = z
   .object({
-    nom: z.string().nullish(),
+    nom: z.string(),
     tagId: z.number().nullish(),
     userId: z.string().nullish(),
+    collectiviteId: z.number().nullish(),
   })
   .describe(
     'Une personne peut avoir soit un urserId (si elle vient de la table dcp), soit un tagId (si elle vient de la table personne_tag)'
