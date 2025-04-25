64564181e6d4dfe63b57395eef1ab13ad06889d6
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-25 10:23:44 +0200

    refactor: enhance FichesActionController to support pagination and filtering with new request and response schemas

--- backend/src/plans/fiches/shared/models/get-fiche-actions.response.ts
+++ backend/src/plans/fiches/shared/models/get-fiche-actions.response.ts
@@ -1,11 +1,23 @@
 import z from 'zod';
-import { ficheActionWithRelationsSchema } from './fiche-action-with-relations.dto';
+import {
+  ficheActionResumeSchema,
+  ficheActionWithRelationsSchema,
+} from './fiche-action-with-relations.dto';
 
 export const getFichesActionResponseSchema = z.object({
   count: z.number(),
   data: z.array(ficheActionWithRelationsSchema),
 });
 
+export const getFichesActionResumeResponseSchema = z.object({
+  count: z.number(),
+  data: z.array(ficheActionResumeSchema),
+});
+
 export type GetFichesActionResponse = z.infer<
   typeof getFichesActionResponseSchema
+>;
+
+export type GetFichesActionResumeResponse = z.infer<
+  typeof getFichesActionResumeResponseSchema
 >;
