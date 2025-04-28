ad86ec4ec96e4e41f8f68d938f106253afc840b9
Marc Rutkowski <marc@attractive-media.fr>
2025-04-28 14:27:21 +0200

    Répare l'appel du gestionnaire d'un traitement cron

--- apps/tools-automation-api/src/cron/cron-consumer.service.ts
+++ apps/tools-automation-api/src/cron/cron-consumer.service.ts
@@ -1,18 +1,13 @@
 import { CalendlySynchroService } from '@/tools-automation-api/calendly/calendly-synchro.service';
 import { ConnectSynchroService } from '@/tools-automation-api/connect/connect-synchro.service';
 import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { Logger } from '@nestjs/common';
+import { Logger, NotFoundException } from '@nestjs/common';
 import { Job } from 'bullmq';
 import { CRON_JOBS_QUEUE_NAME, JobName } from './cron.config';
 
 @Processor(CRON_JOBS_QUEUE_NAME)
 export class CronConsumerService extends WorkerHost {
   private readonly logger = new Logger(CronConsumerService.name);
-
-  private readonly handlers: Record<JobName, () => Promise<unknown>> = {
-    'calendly-synchro': this.calendlySynchroService.process,
-    'connect-synchro': this.connectSynchroService.process,
-  };
 
   constructor(
     private readonly calendlySynchroService: CalendlySynchroService,
@@ -23,6 +18,19 @@ export class CronConsumerService extends WorkerHost {
 
   async process(job: Job<unknown, unknown, JobName>): Promise<unknown> {
     this.logger.log(`Traitement du job "${job.name}"`);
-    return this.handlers[job.name]();
+    switch (job.name) {
+      case 'calendly-synchro':
+        return this.calendlySynchroService.process();
+      case 'connect-synchro':
+        return this.connectSynchroService.process();
+      default:
+        return this.handlerNotFound(job.name);
+    }
+  }
+
+  private handlerNotFound(name: JobName) {
+    const message = `Traitement non trouvé pour le job "${name}"`;
+    this.logger.warn(message);
+    return new NotFoundException(message);
   }
 }
