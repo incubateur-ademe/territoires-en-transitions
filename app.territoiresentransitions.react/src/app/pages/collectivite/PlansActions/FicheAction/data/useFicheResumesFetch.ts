4711e0c0ec895147cc55b2f830ff4e9a9570e142
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-28 17:07:16 +0200

    refactor: simplify collectiviteId assignment

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useFicheResumesFetch.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/data/useFicheResumesFetch.ts
@@ -1,6 +1,6 @@
 import { trpc } from '@/api/utils/trpc/client';
 import { useCollectiviteId } from '@/app/collectivites/collectivite-context';
-import { GetFichesRequestType } from '@/backend/plans/fiches/shared/fetch-fiches-filter.request';
+import { GetFichesRequestType } from '@/backend/plans/fiches/shared/get-fiches-filter.request';
 
 export type GetFichesOptions = Omit<GetFichesRequestType, 'collectiviteId'>;
 
@@ -8,7 +8,7 @@ export const useFicheResumesFetch = (options?: GetFichesOptions) => {
   const collectiviteId = useCollectiviteId();
 
   return trpc.plans.fiches.listResumes.useQuery({
-    collectiviteId: collectiviteId,
+    collectiviteId,
     filters: options?.filters,
     queryOptions: options?.queryOptions,
   });
