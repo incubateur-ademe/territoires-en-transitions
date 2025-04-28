513914be64bcb64c8a0d4b04943ff21cbb88abae
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-04-28 17:03:42 +0200

    refactor: update FicheAction types and imports to use FicheActionResumeType

--- app.territoiresentransitions.react/src/referentiels/action.show/useFichesActionLiees.ts
+++ app.territoiresentransitions.react/src/referentiels/action.show/useFichesActionLiees.ts
@@ -1,37 +1,13 @@
-import { ficheResumesFetch } from '@/api/plan-actions';
-import { useSupabase } from '@/api/utils/supabase/use-supabase';
-import { trpc } from '@/api/utils/trpc/client';
-import { useCollectiviteId } from '@/app/core-logic/hooks/params';
-import { useQuery } from 'react-query';
+import { useFicheResumesFetch } from '@/app/app/pages/collectivite/PlansActions/FicheAction/data/useFicheResumesFetch';
 
 // charge les fiches actions liées à une action du référentiel
 export const useFichesActionLiees = (actionId: string) => {
-  const collectiviteId = useCollectiviteId();
-  const supabase = useSupabase();
-  const trpcUtils = trpc.useUtils();
+  const { data, isLoading } = useFicheResumesFetch({
+    filters: {
+      mesureIds: [actionId],
+    },
+  });
 
-  // charge les fiches action liées à une action
-  const { data, isLoading } = useQuery(
-    ['fiche_action_liees', collectiviteId, actionId],
-    async () => {
-      if (!collectiviteId) {
-        throw new Error('Aucune collectivité associée');
-      }
-
-      return ficheResumesFetch({
-        dbClient: supabase,
-        trpcUtils,
-        collectiviteId,
-        options: {
-          filtre: {
-            referentielActionIds: [actionId],
-          },
-        },
-      });
-    }
-  );
-
-  // chargement encore en cours ou pas de données
   if (isLoading || !data) {
     return { data: [], isLoading };
   }
