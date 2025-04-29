280457458272dfbbe2754fc82930d88b007b0c18
Marc Rutkowski <marc@attractive-media.fr>
2025-04-29 11:55:00 +0200

    Ajoute une notification lors d'une tentative d'enregistrement en mode offline

--- app.territoiresentransitions.react/src/core-logic/hooks/useMutationToast.tsx
+++ app.territoiresentransitions.react/src/core-logic/hooks/useMutationToast.tsx
@@ -1,7 +1,11 @@
 import { getTrpcQueryClient } from '@/api/utils/trpc/client';
-import { Mutation } from '@tanstack/react-query';
+import { Mutation, onlineManager } from '@tanstack/react-query';
 import { useCallback, useEffect } from 'react';
-import { Mutation as OldMutation, useQueryClient } from 'react-query';
+import {
+  Mutation as OldMutation,
+  onlineManager as oldOnlineManager,
+  useQueryClient,
+} from 'react-query';
 import { useBaseToast } from './useBaseToast';
 
 // messages génériques
@@ -13,7 +17,7 @@ const DEFAULT_MESSAGE = {
 /**
  * Écoute toutes les mutations de l'application et fait apparaître un toast
  * avec un message par défaut si l'on a pas spécifié de message dans l'objet "meta" des options de "useMutation".
- * Passer "disableToast" à true dans l'objet "meta" si l'on ne vuet pas afficher de toast.
+ * Passer "disableToast" à true dans l'objet "meta" si l'on ne veut pas afficher de toast.
  */
 export const useMutationToast = () => {
   // TODO: fuse the two query clients. Not done for now because not the same version and a lot of changes
@@ -22,16 +26,24 @@ export const useMutationToast = () => {
   const { renderToast, setToast } = useBaseToast();
 
   const handleMutation = useCallback((mutation?: Mutation | OldMutation) => {
-    const status = mutation?.state.status;
-    if (
-      (status === 'success' || status === 'error') &&
-      !mutation?.meta?.disableToast
-    ) {
-      const message =
-        (mutation?.meta?.[status] as string) || DEFAULT_MESSAGE[status];
-      const hideDuration =
-        (mutation?.meta?.autoHideDuration as number) || undefined;
-      setToast(status, message, hideDuration);
+    if (!oldOnlineManager.isOnline() || !onlineManager.isOnline()) {
+      setToast(
+        'error',
+        "La connexion réseau semble être interrompue. Vos données ne peuvent pas être sauvegardées pour l'instant. Veuillez attendre que votre connexion soit rétablie pour utiliser l'application.",
+        10_000
+      );
+    } else {
+      const status = mutation?.state.status;
+      if (
+        (status === 'success' || status === 'error') &&
+        !mutation?.meta?.disableToast
+      ) {
+        const message =
+          (mutation?.meta?.[status] as string) || DEFAULT_MESSAGE[status];
+        const hideDuration =
+          (mutation?.meta?.autoHideDuration as number) || undefined;
+        setToast(status, message, hideDuration);
+      }
     }
   }, []);
 
