3ca4b83600f965442363cc8933721255092dacf9
Marc Rutkowski <marc@attractive-media.fr>
2025-04-29 11:53:50 +0200

    Ajoute un bandeau informant l'utilisateur de l'état offline

--- app.territoiresentransitions.react/src/app/Layout/Header/index.tsx
+++ app.territoiresentransitions.react/src/app/Layout/Header/index.tsx
@@ -3,6 +3,7 @@
 import { useUser } from '@/api/users/user-provider';
 import { useNbActionsDansPanier } from '@/app/app/Layout/Header/useNbActionsDansPanier';
 import { useCurrentCollectivite } from '@/app/core-logic/hooks/useCurrentCollectivite';
+import { Alert, useOnlineStatus } from '@/ui';
 import { Header as HeaderBase } from './Header';
 
 /**
@@ -14,13 +15,24 @@ const Header = () => {
   const { data: panier } = useNbActionsDansPanier(
     currentCollectivite?.collectiviteId ?? null
   );
+  const isOnline = useOnlineStatus();
 
   return (
-    <HeaderBase
-      user={user}
-      currentCollectivite={currentCollectivite}
-      panierId={panier?.panierId}
-    />
+    <>
+      {!isOnline && (
+        <Alert
+          className="sticky top-0 z-tooltip"
+          state="error"
+          customIcon="cloud-off-line"
+          title="Erreur de connexion réseau. Veuillez attendre que votre connexion soit rétablie pour utiliser l'application."
+        />
+      )}
+      <HeaderBase
+        user={user}
+        currentCollectivite={currentCollectivite}
+        panierId={panier?.panierId}
+      />
+    </>
   );
 };
 
