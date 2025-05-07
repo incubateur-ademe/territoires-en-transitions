93dabd8ed6d9ce43b889fe03a4af6fe6c9fcba2a
Marc Rutkowski <marc@attractive-media.fr>
2025-05-07 09:53:29 +0200

    Ajoute une story pour le RadioButton

---
+++ packages/ui/src/design-system/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { RadioButton } from './RadioButton';
+
+const meta: Meta<typeof RadioButton> = {
+  component: RadioButton,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RadioButton>;
+
+export const Unchecked: Story = {
+  args: {
+    label: 'Libellé'
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+    label: 'Libellé'
+  },
+};
+
+export const Group = {
+  render: () => <div className='flex gap-4'>
+    <RadioButton name="reponse" value="true" label="Oui" checked />
+    <RadioButton name="reponse" value="false" label="Non" />
+  </div>
+};
