6996f6da5929cc2357a757a3d0b50fe62b2686a6
Marc Rutkowski <marc@attractive-media.fr>
2025-05-15 16:49:33 +0200

    Utilise le code mutualisé du parser
    Fix Utilise le code mutualisé du parser

--- backend/src/indicateurs/valeurs/indicateur-valeur-expression-parser.service.ts
+++ backend/src/indicateurs/valeurs/indicateur-valeur-expression-parser.service.ts
@@ -1,235 +1,50 @@
-import { HttpException, Injectable, Logger } from '@nestjs/common';
 import {
-  createToken,
-  CstNode,
-  CstParser,
-  Lexer,
-  tokenMatcher,
-} from 'chevrotain';
+  common,
+  ExpressionParserBase,
+  getExpressionVisitor,
+} from '@/backend/personnalisations/services/expression-parser-base';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import { createToken, CstNode } from 'chevrotain';
 import { isNil } from 'es-toolkit';
-
-// Define all the tokens used in the grammar
-const VRAI = createToken({ name: 'VRAI', pattern: /vrai/ });
-const FAUX = createToken({ name: 'FAUX', pattern: /faux/ });
-const OUI = createToken({ name: 'OUI', pattern: /oui/ });
-const NON = createToken({ name: 'NON', pattern: /non/ });
-
-const CNAME = createToken({
-  name: 'CNAME',
-  pattern: /[a-zA-Z_][a-zA-Z_0-9.]*/,
-});
-const NUMBER = createToken({ name: 'NUMBER', pattern: /-?\d+(\.\d+)?/ });
-
-const SI = createToken({ name: 'SI', pattern: /si/ });
-const ALORS = createToken({ name: 'ALORS', pattern: /alors/ });
-const SINON = createToken({ name: 'SINON', pattern: /sinon/ });
 
 const VAL = createToken({ name: 'VAL', pattern: /val/ });
 const OPT_VAL = createToken({ name: 'OPT_VAL', pattern: /opt_val/ });
-const MIN = createToken({ name: 'MIN', pattern: /min/ });
-const MAX = createToken({ name: 'MAX', pattern: /max/ });
 
-const OU = createToken({ name: 'OU', pattern: /ou/ });
-const ET = createToken({ name: 'ET', pattern: /et/ });
+// tokens ajoutés au parser de base
+const tokens = [VAL, OPT_VAL];
 
-const ADDITION_OPERATOR = createToken({
-  name: 'ADDITION_OPERATOR',
-  pattern: Lexer.NA,
-});
-const PLUS = createToken({
-  name: 'PLUS',
-  pattern: /\+/,
-  categories: ADDITION_OPERATOR,
-});
-const MINUS = createToken({
-  name: 'MINUS',
-  pattern: /-/,
-  categories: ADDITION_OPERATOR,
-});
-
-const MULTIPLICATION_OPERATOR = createToken({
-  name: 'MULTIPLICATION_OPERATOR',
-  pattern: Lexer.NA,
-});
-const MULT = createToken({
-  name: 'MULT',
-  pattern: /\*/,
-  categories: MULTIPLICATION_OPERATOR,
-});
-const DIV = createToken({
-  name: 'DIV',
-  pattern: /\//,
-  categories: MULTIPLICATION_OPERATOR,
-});
-
-const LPAR = createToken({ name: 'LPAR', pattern: /\(/ });
-const RPAR = createToken({ name: 'RPAR', pattern: /\)/ });
-const COMMA = createToken({ name: 'COMMA', pattern: /,/ });
-
-// Whitespace to be skipped
-const WS = createToken({ name: 'WS', pattern: /\s+/, group: Lexer.SKIPPED });
-
-// All tokens
-const allTokens = [
-  WS,
-  VRAI,
-  FAUX,
-  OUI,
-  NON,
-  SINON,
-  SI,
-  ALORS,
-  VAL,
-  OPT_VAL,
-  MIN,
-  MAX,
-  OU,
-  ET,
-  PLUS,
-  MINUS,
-  ADDITION_OPERATOR,
-  MULT,
-  DIV,
-  MULTIPLICATION_OPERATOR,
-  LPAR,
-  RPAR,
-  COMMA,
-  CNAME,
-  NUMBER,
-];
-
-const indicateurValeurExprLexer = new Lexer(allTokens);
-
-class IndicateurValeurExprParser extends CstParser {
+class IndicateurValeurExprParser extends ExpressionParserBase {
   constructor() {
-    super(allTokens);
-    this.performSelfAnalysis();
+    super(tokens);
+    try {
+      this.performSelfAnalysis();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  // Statement
-  public statement = this.RULE('statement', () => {
+  call = this.OVERRIDE_RULE('call', () => {
     this.OR([
-      { ALT: () => this.SUBRULE(this.if_statement) },
-      { ALT: () => this.SUBRULE(this.expression) },
-    ]);
-  });
-
-  // If Statement
-  private if_statement = this.RULE('if_statement', () => {
-    this.CONSUME(SI);
-    this.SUBRULE(this.expression);
-    this.CONSUME(ALORS);
-    this.SUBRULE(this.statement);
-    this.OPTION(() => {
-      this.CONSUME(SINON);
-      this.SUBRULE2(this.statement);
-    });
-  });
-
-  // Expression
-  private expression = this.RULE('expression', () => {
-    this.SUBRULE(this.logic_or);
-  });
-
-  // Logic OR
-  private logic_or = this.RULE('logic_or', () => {
-    this.SUBRULE(this.logic_and);
-    this.MANY(() => {
-      this.CONSUME(OU);
-      this.SUBRULE2(this.logic_and);
-    });
-  });
-
-  // Logic AND
-  private logic_and = this.RULE('logic_and', () => {
-    this.SUBRULE(this.term);
-    this.MANY(() => {
-      this.CONSUME(ET);
-      this.SUBRULE2(this.term);
-    });
-  });
-
-  // Term
-  private term = this.RULE('term', () => {
-    this.SUBRULE(this.factor);
-    this.MANY(() => {
-      // consuming 'AdditionOperator' will consume either Plus or Minus as they are subclasses of AdditionOperator
-      this.CONSUME(ADDITION_OPERATOR);
-      this.SUBRULE2(this.factor);
-    });
-  });
-
-  // Factor
-  private factor = this.RULE('factor', () => {
-    this.SUBRULE(this.unary);
-    this.MANY(() => {
-      this.CONSUME(MULTIPLICATION_OPERATOR);
-      this.SUBRULE2(this.unary);
-    });
-  });
-
-  // Unary
-  private unary = this.RULE('unary', () => {
-    this.SUBRULE(this.call);
-  });
-
-  private call = this.RULE('call', () => {
-    this.OR([
-      { ALT: () => this.SUBRULE(this.opt_val) },
       { ALT: () => this.SUBRULE(this.val) },
-      { ALT: () => this.SUBRULE(this.primary) },
+      { ALT: () => this.SUBRULE(this.opt_val) },
       { ALT: () => this.SUBRULE(this.min) },
       { ALT: () => this.SUBRULE(this.max) },
+      ...this.getCallHandlers.apply(this),
     ]);
   });
 
   private val = this.RULE('val', () => {
     this.CONSUME(VAL);
-    this.CONSUME(LPAR);
+    this.CONSUME(common.LPAR);
     this.SUBRULE(this.identifier);
-    this.CONSUME(RPAR);
+    this.CONSUME(common.RPAR);
   });
 
   private opt_val = this.RULE('opt_val', () => {
     this.CONSUME(OPT_VAL);
-    this.CONSUME(LPAR);
+    this.CONSUME(common.LPAR);
     this.SUBRULE(this.identifier);
-    this.CONSUME(RPAR);
-  });
-
-  private min = this.RULE('min', () => {
-    this.CONSUME(MIN);
-    this.CONSUME(LPAR);
-    this.SUBRULE(this.term);
-    this.CONSUME(COMMA);
-    this.SUBRULE2(this.term);
-    this.CONSUME(RPAR);
-  });
-
-  private max = this.RULE('max', () => {
-    this.CONSUME(MAX);
-    this.CONSUME(LPAR);
-    this.SUBRULE(this.term);
-    this.CONSUME(COMMA);
-    this.SUBRULE2(this.term);
-    this.CONSUME(RPAR);
-  });
-
-  // Primary
-  private primary = this.RULE('primary', () => {
-    this.OR([
-      { ALT: () => this.CONSUME(VRAI) },
-      { ALT: () => this.CONSUME(FAUX) },
-      { ALT: () => this.CONSUME(OUI) },
-      { ALT: () => this.CONSUME(NON) },
-      { ALT: () => this.CONSUME(CNAME) },
-      { ALT: () => this.CONSUME(NUMBER) },
-    ]);
-  });
-
-  // Identifier
-  private identifier = this.RULE('identifier', () => {
-    this.CONSUME(CNAME);
+    this.CONSUME(common.RPAR);
   });
 }
 
@@ -237,7 +52,9 @@ const parser = new IndicateurValeurExprParser();
 
 const BaseCSTVisitor = parser.getBaseCstVisitorConstructor();
 
-class IndicateurValeurExpressionVisitor extends BaseCSTVisitor {
+class IndicateurValeurExpressionVisitor extends getExpressionVisitor(
+  BaseCSTVisitor
+) {
   sourceIndicateurValeurs: { [key: string]: number | null } | null = null;
 
   constructor() {
@@ -245,175 +62,15 @@ class IndicateurValeurExpressionVisitor extends BaseCSTVisitor {
     this.validateVisitor();
   }
 
-  statement(ctx: any) {
-    if (ctx.if_statement) {
-      return this.visit(ctx.if_statement);
-    } else {
-      return this.visit(ctx.expression);
-    }
-  }
-
-  if_statement(ctx: any) {
-    const condition = this.visit(ctx.expression);
-    const thenBranch = this.visit(ctx.statement[0]);
-    const elseBranch = ctx.statement[1] ? this.visit(ctx.statement[1]) : null;
-
-    if (condition) {
-      return thenBranch;
-    } else {
-      return elseBranch;
-    }
-  }
-
-  expression(ctx: any) {
-    return this.visit(ctx.logic_or);
-  }
-
-  logic_or(ctx: any) {
-    let result = this.visit(ctx.logic_and[0]);
-    for (let i = 1; i < ctx.logic_and.length; i++) {
-      const nextValue = this.visit(ctx.logic_and[i]);
-      result = result || nextValue;
-    }
-    return result;
-  }
-
-  logic_and(ctx: any) {
-    let result = this.visit(ctx.term[0]);
-    for (let i = 1; i < ctx.term.length; i++) {
-      const nextValue = this.visit(ctx.term[i]);
-      result = result && nextValue;
-    }
-    return result;
-  }
-
-  term(ctx: any) {
-    let result = this.visit(ctx.factor[0]);
-    for (let i = 1; i < ctx.factor.length; i++) {
-      const rightOperand = this.visit(ctx.factor[i]);
-      const operator = ctx.ADDITION_OPERATOR[i - 1];
-
-      if (tokenMatcher(operator, PLUS)) {
-        if (typeof result === 'string' || typeof rightOperand === 'string') {
-          result = `${result} - ${rightOperand}`;
-        } else if (isNil(result) || isNil(rightOperand)) {
-          result = null;
-        } else {
-          result += rightOperand;
-        }
-      } else {
-        if (typeof result === 'string' || typeof rightOperand === 'string') {
-          result = `${result} - ${rightOperand}`;
-        } else if (isNil(result) || isNil(rightOperand)) {
-          result = null;
-        } else {
-          result -= rightOperand;
-        }
-      }
-    }
-    return result;
-  }
-
-  factor(ctx: any) {
-    let result = this.visit(ctx.unary[0]);
-    for (let i = 1; i < ctx.unary.length; i++) {
-      const rightOperand = this.visit(ctx.unary[i]);
-      const operator = ctx.MULTIPLICATION_OPERATOR
-        ? ctx.MULTIPLICATION_OPERATOR[i - 1]
-        : null;
-      if (operator) {
-        if (tokenMatcher(operator, MULT)) {
-          if (typeof result === 'string' || typeof rightOperand === 'string') {
-            result = `${result} * ${rightOperand}`;
-          } else if (isNil(result) || isNil(rightOperand)) {
-            result = null;
-          } else {
-            result *= rightOperand;
-          }
-        } else if (tokenMatcher(operator, DIV)) {
-          if (typeof result === 'string' || typeof rightOperand === 'string') {
-            result = `${result} / ${rightOperand}`;
-          } else if (isNil(result) || isNil(rightOperand)) {
-            result = null;
-          } else {
-            result /= rightOperand;
-          }
-        }
-      }
-    }
-    return result;
-  }
-
-  unary(ctx: any) {
-    return this.visit(ctx.call);
-  }
-
   call(ctx: any) {
-    if (ctx.primary) {
-      return this.visit(ctx.primary);
-    } else if (ctx.opt_val) {
-      return this.visit(ctx.opt_val);
-    } else if (ctx.val) {
-      return this.visit(ctx.val);
-    } else if (ctx.min) {
-      return this.visit(ctx.min);
-    } else if (ctx.max) {
-      return this.visit(ctx.max);
-    }
-  }
-
-  primary(ctx: any) {
-    if (ctx.VRAI) {
-      return true;
-    } else if (ctx.FAUX) {
-      return false;
-    } else if (ctx.vrai) {
-      return true;
-    } else if (ctx.faux) {
-      return false;
-    } else if (ctx.OUI) {
-      return true;
-    } else if (ctx.NON) {
-      return false;
-    } else if (ctx.oui) {
-      return true;
-    } else if (ctx.non) {
-      return false;
-    } else if (ctx.CNAME) {
-      return ctx.CNAME[0].image; // Assuming string identifier
-    } else if (ctx.NUMBER) {
-      return parseFloat(ctx.NUMBER[0].image);
-    }
-  }
-
-  identifier(ctx: any) {
-    return ctx.CNAME[0].image;
-  }
-
-  // Implementing custom logic for functions
-  min(ctx: any) {
-    const term1 = this.visit(ctx.term[0]);
-    const term2 = this.visit(ctx.term[1]);
-    if (typeof term1 === 'string' || typeof term2 === 'string') {
-      // TODO: done in order to verify unit test, check if needed
-      return `min(${term1}, ${term2})`;
-    } else if (isNil(term1) || isNil(term2)) {
-      return null;
-    } else {
-      return Math.min(term1, term2);
-    }
-  }
-
-  max(ctx: any) {
-    const term1 = this.visit(ctx.term[0]);
-    const term2 = this.visit(ctx.term[1]);
-    if (typeof term1 === 'string' || typeof term2 === 'string') {
-      // TODO: done in order to verify unit test, check if needed
-      return `max(${term1}, ${term2})`;
-    } else if (isNil(term1) || isNil(term2)) {
-      return null;
-    } else {
-      return Math.max(term1, term2);
+    try {
+      return super.call(ctx);
+    } catch {
+      if (ctx.opt_val) {
+        return this.visit(ctx.opt_val);
+      } else if (ctx.val) {
+        return this.visit(ctx.val);
+      }
     }
   }
 
@@ -516,8 +173,7 @@ export default class IndicateurValeurExpressionParserService {
   }
 
   parseExpression(inputText: string): CstNode {
-    const lexingResult = indicateurValeurExprLexer.tokenize(inputText);
-    //console.log(JSON.stringify(lexingResult.tokens));
+    const lexingResult = parser.lexer.tokenize(inputText);
     parser.input = lexingResult.tokens;
     const cst = parser.statement();
 
