52bd0608c9b12dfe163815a24129780ae39beac9
Fred <98240+farnoux@users.noreply.github.com>
2025-05-21 14:44:03 +0200

    Corrige une erreur de cast venant des query params

--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredPlans.ts
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/data/useFilteredPlans.ts
@@ -1,16 +1,18 @@
 import { CollectiviteEngagee } from '@/api';
-import { NB_CARDS_PER_PAGE } from '@/app/app/pages/CollectivitesEngagees/data/utils';
 import { trpc } from '@/api/utils/trpc/client';
+import { NB_CARDS_PER_PAGE } from '@/app/app/pages/CollectivitesEngagees/data/utils';
 
 /**
  * Renvoi une liste de plans en fonction d'un ensemble de filtres
  */
 export const useFilteredPlans = (args: CollectiviteEngagee.Filters) => {
-
   const { data, isLoading } = trpc.collectivites.recherches.plans.useQuery({
     ...args,
     nbCards: NB_CARDS_PER_PAGE,
-    nom: Array.isArray(args.nom) ? (args.nom[0] || '') : args.nom || ''
+    nom: Array.isArray(args.nom) ? args.nom[0] || '' : args.nom || '',
+    // TODO: Supprimer ce cast manuel quand on utilisera la lib `nuqs`
+    // qui gérera automatiquement les cast de query params
+    typesPlan: args.typesPlan.map((type) => Number(type)),
   });
 
   return {
