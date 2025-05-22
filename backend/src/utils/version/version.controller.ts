b4b02a8facca7a48423d1e2f13f2ce2f59aba4c1
Marc Rutkowski <marc@attractive-media.fr>
2025-05-22 15:36:11 +0200

    Partage un utilitaire

--- backend/src/utils/version/version.controller.ts
+++ backend/src/utils/version/version.controller.ts
@@ -3,7 +3,11 @@ import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { ApiHideProperty, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AllowAnonymousAccess } from '../../auth/decorators/allow-anonymous-access.decorator';
 import { AllowPublicAccess } from '../../auth/decorators/allow-public-access.decorator';
-import { versionResponseSchema } from './version.models';
+import {
+  AppEnvironment,
+  versionResponseSchema,
+  VersionResponseType,
+} from './version.models';
 
 /**
  * Création des classes de réponse à partir du schema pour générer automatiquement la documentation OpenAPI
@@ -32,12 +36,16 @@ export class VersionController {
       'Informations de version (commit, version, environnement, date de déploiement, date du commit)',
   })
   async getVersion() {
-    return {
-      commit: process.env.GIT_COMMIT_SHORT_SHA,
-      version: process.env.APPLICATION_VERSION,
-      environment: process.env.ENV_NAME,
-      deploy_time: process.env.DEPLOYMENT_TIMESTAMP,
-      commit_time: process.env.GIT_COMMIT_TIMESTAMP,
-    };
+    return getVersion();
   }
+}
+
+export function getVersion(): VersionResponseType {
+  return {
+    commit: process.env.GIT_COMMIT_SHORT_SHA,
+    version: process.env.APPLICATION_VERSION,
+    environment: process.env.ENV_NAME as AppEnvironment,
+    deploy_time: process.env.DEPLOYMENT_TIMESTAMP,
+    commit_time: process.env.GIT_COMMIT_TIMESTAMP,
+  };
 }
