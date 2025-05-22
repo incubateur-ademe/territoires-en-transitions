a7b3a2bbff6265ed408d6af256747ab33c35b9ee
Arthur Molinos <arthur.molinos@gmail.com>
2025-05-22 17:32:33 +0200

    Refacto et utilisation des nouveaux compo de module pour les modules FichesActionCountBy et SuiviPlans

---
+++ app.territoiresentransitions.react/src/tableaux-de-bord/plans-action/fiches-action-count-by/utils/get-chart-option.ts
@@ -0,0 +1,157 @@
+import { EChartsOption, PieSeriesOption } from 'echarts';
+
+import { RouterOutput } from '@/api/utils/trpc/client';
+import { prioritesToState } from '@/app/app/pages/collectivite/PlansActions/components/BadgePriorite';
+import { statutFicheActionToColor } from '@/app/plans-action/fiches/utils';
+import { remToPx } from '@/app/ui/charts/echarts/remToPx';
+import {
+  CountByPropertyEnumType,
+  ficheActionForCountBySchema,
+  Priorite,
+  Statut,
+} from '@/domain/plans/fiches';
+import { preset } from '@/ui';
+import { cloneDeep } from 'es-toolkit';
+
+const getItemColor = (
+  countByProperty: CountByPropertyEnumType,
+  value: string | number | boolean | null,
+  nullColor: string
+): string | null => {
+  if (!value) {
+    return nullColor;
+  }
+
+  switch (countByProperty) {
+    case 'statut':
+      return statutFicheActionToColor[value as Statut];
+
+    case 'priorite': {
+      const prioriteState = prioritesToState[value as Priorite];
+      return preset.theme.extend.colors[prioriteState][1];
+    }
+
+    default:
+      return null;
+  }
+};
+
+type CountByResult =
+  RouterOutput['plans']['fiches']['countBy']['countByResult'];
+
+type Args = {
+  displayItemsLabel: boolean;
+  countByProperty: CountByPropertyEnumType;
+  countByTotal: number;
+  countByResult?: CountByResult;
+};
+
+export const getChartOption = ({
+  displayItemsLabel,
+  countByProperty,
+  countByTotal,
+  countByResult,
+}: Args): EChartsOption => {
+  const { colors, fontSize, fontWeight } = preset.theme.extend;
+
+  const pieChartData = countByResult
+    ? Object.entries(countByResult)
+        .map(([, { count, value, label }]) => {
+          const itemColor = getItemColor(
+            countByProperty,
+            value,
+            colors.grey[4]
+          );
+          return {
+            name: label || `${value}`,
+            name_id: value,
+            value: count,
+            itemStyle: itemColor
+              ? {
+                  color: itemColor,
+                }
+              : undefined,
+          };
+        })
+        .filter((item) => item.value)
+    : [];
+
+  const pieChartSeries: PieSeriesOption = {
+    name:
+      ficheActionForCountBySchema.shape[countByProperty].description ||
+      countByProperty,
+    type: 'pie',
+    radius: ['40%', '80%'],
+    avoidLabelOverlap: false,
+    labelLine: {
+      show: false,
+    },
+    label: {
+      show: true,
+      fontSize: `${remToPx(fontSize['base'])}px`,
+      position: 'inside',
+      formatter: '{c}',
+    },
+    emphasis: {
+      label: { show: true },
+      itemStyle: {
+        shadowBlur: 10,
+        shadowOffsetX: 0,
+        shadowColor: 'rgba(0, 0, 0, 0.5)',
+      },
+    },
+    data: pieChartData,
+  };
+
+  const series: PieSeriesOption[] = [];
+  if (displayItemsLabel) {
+    // A bit a hack, but the way to have two labels: https://github.com/apache/echarts/issues/18383
+    const itemlabelSeries: PieSeriesOption = cloneDeep(pieChartSeries);
+    delete itemlabelSeries.emphasis;
+    itemlabelSeries.label = {
+      show: true,
+      fontSize: `${remToPx(fontSize['base'])}px`,
+      position: 'outside',
+    };
+    itemlabelSeries.labelLine = {
+      show: true,
+    };
+    itemlabelSeries.avoidLabelOverlap = true;
+    series.push(itemlabelSeries);
+  }
+  series.push(pieChartSeries);
+
+  return {
+    tooltip: {
+      position: 'bottom',
+      trigger: 'item',
+      valueFormatter: (value) => {
+        if (typeof value === 'number') {
+          return `${value} (${((value / countByTotal) * 100).toFixed(0)}%)`;
+        }
+        return '';
+      },
+    },
+    legend: {
+      show: false,
+    },
+    title: {
+      left: 'center',
+      top: 'center',
+      text: `${countByTotal}`,
+      subtext: 'actions',
+      itemGap: 0,
+      textStyle: {
+        color: colors.primary['9'],
+        fontWeight: parseInt(fontWeight['bold']),
+        fontSize: `${remToPx(fontSize['2xl'])}px`, // Better to set px instead of rem for SSR
+      },
+      subtextStyle: {
+        color: colors.grey['6'],
+        fontWeight: parseInt(fontWeight['normal']),
+        fontSize: `${remToPx(fontSize['lg'])}px`, // Better to set px instead of rem for SSR
+      },
+    },
+    series: series,
+  };
+};
---
+++ app.territoiresentransitions.react/src/tableaux-de-bord/plans-action/fiches-action-count-by/utils/make-fiches-action-url-with-params.ts
@@ -0,0 +1,163 @@
+import { Filtre } from '@/api/plan-actions/dashboards/collectivite-dashboard/domain/fiches-synthese.schema';
+import { Filtre as FicheActionFiltre } from '@/api/plan-actions/fiche-resumes.list/domain/fetch-options.schema';
+import { nameToparams } from '@/app/app/pages/collectivite/PlansActions/ToutesLesFichesAction/ToutesLesFichesAction';
+import { makeCollectiviteToutesLesFichesUrl } from '@/app/app/paths';
+import { CountByPropertyEnumType } from '@/domain/plans/fiches';
+
+const getFicheActionFiltreKeyValue = (
+  countByProperty: CountByPropertyEnumType,
+  propertyValue: string | number | null | boolean
+): {
+  key: keyof FicheActionFiltre;
+  value: string | number | boolean | null;
+} | null => {
+  switch (countByProperty) {
+    case 'statut':
+      if (propertyValue) {
+        return { key: 'statuts', value: propertyValue };
+      } else {
+        return { key: 'noStatut', value: true };
+      }
+
+    case 'pilotes':
+      if (typeof propertyValue === 'string') {
+        return { key: 'utilisateurPiloteIds', value: propertyValue };
+      } else if (!propertyValue) {
+        return { key: 'noPilote', value: true };
+      } else {
+        return { key: 'personnePiloteIds', value: propertyValue };
+      }
+    case 'referents':
+      if (typeof propertyValue === 'string') {
+        return { key: 'utilisateurReferentIds', value: propertyValue };
+      } else if (!propertyValue) {
+        return null;
+      } else {
+        return { key: 'personneReferenteIds', value: propertyValue };
+      }
+    case 'services':
+      if (propertyValue) {
+        return { key: 'servicePiloteIds', value: propertyValue };
+      } else {
+        return { key: 'noServicePilote', value: true };
+      }
+    case 'cibles':
+      if (propertyValue) {
+        return { key: 'cibles', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'priorite':
+      if (propertyValue) {
+        return { key: 'priorites', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'partenaires':
+      if (propertyValue) {
+        return { key: 'partenaireIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'structures':
+      if (propertyValue) {
+        return { key: 'structurePiloteIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'tags':
+      if (propertyValue) {
+        return { key: 'libreTagsIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'financeurs':
+      if (propertyValue) {
+        return { key: 'financeurIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'thematiques':
+      if (propertyValue) {
+        return { key: 'thematiqueIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'plans':
+      if (propertyValue) {
+        return { key: 'planActionIds', value: propertyValue };
+      } else {
+        return null;
+      }
+    case 'indicateurs':
+      if (propertyValue) {
+        return { key: 'hasIndicateurLies', value: true };
+      } else {
+        return null;
+      }
+    case 'ameliorationContinue':
+      if (propertyValue) {
+        return { key: 'ameliorationContinue', value: true };
+      } else {
+        return null;
+      }
+    /*
+      TODO à remplacer avec les nouveaux filtres des budgets
+    case 'budgetPrevisionnel':
+      if (propertyValue) {
+        return { key: 'budgetPrevisionnel', value: true };
+      } else {
+        return null;
+      }
+       */
+
+    // Not supported for now
+    case 'participationCitoyenneType':
+    case 'effetsAttendus':
+    case 'sousThematiques':
+      return null;
+
+    default:
+      return null;
+  }
+};
+
+/** Permet de transformer les filtres de modules fiches action en paramètres d'URL */
+export const makeFichesActionUrlWithParams = (
+  collectiviteId: number,
+  filtres: Filtre,
+  countByProperty: CountByPropertyEnumType,
+  propertyValue: string | number | null | boolean
+): string | null => {
+  const filtre = getFicheActionFiltreKeyValue(countByProperty, propertyValue);
+
+  if (!filtre) {
+    // Prefer to return null instead of building an incomplete url
+    return null;
+  }
+
+  const baseUrl = `${makeCollectiviteToutesLesFichesUrl({
+    collectiviteId,
+  })}?${nameToparams[filtre.key]}=${filtre.value}`;
+
+  const searchParams = new URLSearchParams();
+
+  Object.keys(filtres).forEach((key) => {
+    const filterKey = key as keyof Filtre;
+    const value = filtres[filterKey];
+
+    const isArray = Array.isArray(value);
+
+    const paramKey = nameToparams[filterKey];
+
+    if (value !== undefined && !!paramKey) {
+      if (isArray && value.length > 0) {
+        searchParams.append(paramKey, isArray ? value.join(',') : value);
+      } else {
+        searchParams.append(paramKey, value.toString());
+      }
+    }
+  });
+
+  return `${baseUrl}&${searchParams.toString()}`;
+};
