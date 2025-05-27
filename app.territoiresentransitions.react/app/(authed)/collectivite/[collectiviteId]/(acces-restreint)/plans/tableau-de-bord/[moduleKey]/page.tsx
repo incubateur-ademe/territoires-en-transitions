72942a8bb36164de2bac5f600199166eee687df0
Arthur Molinos <arthur.molinos@gmail.com>
2025-05-27 14:53:59 +0200

    Passe la page d'un module tdb plans et actions en RSC

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/plans/tableau-de-bord/[moduleKey]/page.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/plans/tableau-de-bord/[moduleKey]/page.tsx
@@ -1,15 +1,43 @@
 import { CollectiviteDefaultModuleKeys } from '@/domain/collectivites';
 
-import TdbPAModulePage from './_components/tdb-PA-module.page';
+import { createServerClient } from '@/api/utils/trpc/server-client';
+import { makeTdbPlansEtActionsUrl } from '@/app/app/paths';
+import PlansActionList from '@/app/plans-action/plans/plans-action.list';
+import ModulePage from '@/app/tableaux-de-bord/modules/module.page';
 
 const Page = async ({
   params,
 }: {
-  params: Promise<{ moduleKey: CollectiviteDefaultModuleKeys }>;
+  params: Promise<{
+    moduleKey: CollectiviteDefaultModuleKeys;
+    collectiviteId: string;
+  }>;
 }) => {
-  const { moduleKey } = await params;
+  const { moduleKey, collectiviteId } = await params;
 
-  return <TdbPAModulePage moduleKey={moduleKey} />;
+  const parentPage = {
+    label: 'Tableau de bord Plans & Actions',
+    link: makeTdbPlansEtActionsUrl({
+      collectiviteId: parseInt(collectiviteId),
+    }),
+  };
+
+  const pageModule =
+    await createServerClient().collectivites.tableauDeBord.get.query({
+      collectiviteId: parseInt(collectiviteId),
+      defaultKey: moduleKey,
+    });
+
+  if (
+    moduleKey === 'suivi-plan-actions' &&
+    pageModule.type === 'plan-action.list'
+  ) {
+    return (
+      <ModulePage title={pageModule.titre} parentPage={parentPage}>
+        <PlansActionList filtres={pageModule.options.filtre ?? {}} />
+      </ModulePage>
+    );
+  }
 };
 
 export default Page;
