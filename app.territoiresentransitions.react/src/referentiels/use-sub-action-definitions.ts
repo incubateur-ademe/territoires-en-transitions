c39f98cfcf8ee0228b8ad7da1656a71217d79ebb
Marc Rutkowski <marc@attractive-media.fr>
2025-05-28 16:09:49 +0200

    Simplifie le type ActionDefinitionSummary

--- app.territoiresentransitions.react/src/referentiels/use-sub-action-definitions.ts
+++ app.territoiresentransitions.react/src/referentiels/use-sub-action-definitions.ts
@@ -1,65 +1,28 @@
-import { DBClient } from '@/api';
-import { useSupabase } from '@/api/utils/supabase/use-supabase';
+import { trpc } from '@/api/utils/trpc/client';
 import { TActionDef } from '@/app/referentiels/preuves/usePreuves';
-import { useQuery } from 'react-query';
-
-type TFetchedData = {
-  id: string;
-  identifiant: string;
-  nom: string;
-};
-
-/**
- * Toutes les sous-actions rattachées à une action
- */
-const useSubActions = (action: TActionDef) => {
-  const supabase = useSupabase();
-
-  // charge les données
-  const { data } = useQuery(
-    ['sub_actions', action.id],
-    () => fetch(supabase, action),
-    {
-      // il n'est pas nécessaire de recharger trop systématiquement ici
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-    }
-  );
-
-  return data || [];
-};
+import { ActionTypeEnum } from '@/domain/referentiels';
 
 /**
  * Liste de options pour la sélection d'une sous-action
  */
 export const useSubActionOptionsListe = (action: TActionDef) => {
-  const actions = useSubActions(action);
-  return actions.map(({ id, identifiant, nom }) => ({
+  const { data: actions } =
+    trpc.referentiels.actions.listActionSummaries.useQuery(
+      {
+        referentielId: action.referentiel,
+        identifiant: action.identifiant,
+        actionTypes: [ActionTypeEnum.SOUS_ACTION],
+      },
+      {
+        // il n'est pas nécessaire de recharger trop systématiquement ici, car
+        // les référentiels ne changent pas fréquemment
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      }
+    );
+
+  return (actions || []).map(({ id, identifiant, nom }) => ({
     value: id,
     label: `${identifiant} ${nom}`,
   }));
-};
-
-const fetch = async (
-  supabase: DBClient,
-  action: TActionDef
-): Promise<TFetchedData[]> => {
-  // extrait l'id du référentiel depuis l'id de l'action
-  const { referentiel, identifiant } = action;
-
-  // la requête
-  const query = supabase
-    .from('action_definition_summary')
-    .select('id,identifiant,nom')
-    .ilike('identifiant', `${identifiant}%`)
-    .eq('referentiel', referentiel)
-    .eq('type', 'sous-action');
-
-  // attends les données
-  const { error, data } = await query;
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return data as TFetchedData[];
 };
