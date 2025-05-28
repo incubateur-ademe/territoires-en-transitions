61bda48b95049860c610af86a1079b7cf45a7e18
Marc Rutkowski <marc@attractive-media.fr>
2025-05-28 14:53:14 +0200

    Ajoute des définitions de table

---
+++ backend/src/referentiels/models/question-action.table.ts
@@ -0,0 +1,16 @@
+import { questionTable } from '@/backend/personnalisations/models/question.table';
+import { actionRelationTable } from '@/backend/referentiels/index-domain';
+import { integer, pgTable, primaryKey, varchar } from 'drizzle-orm/pg-core';
+
+export const questionActionTable = pgTable(
+  'question_action',
+  {
+    actionId: varchar('action_id', { length: 30 })
+      .references(() => actionRelationTable.id)
+      .notNull(),
+    questionId: integer('question_id')
+      .references(() => questionTable.id, { onDelete: 'cascade' })
+      .notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.actionId, table.questionId] })]
+);
