9004ff41a438e47898aee2cd08f21c54e5469113
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-02 17:23:29 +0200

    Supprime les nouvelles actions depuis le select personnes

--- app.territoiresentransitions.react/src/ui/dropdownLists/PersonnesDropdown/PersonnesDropdown.tsx
+++ app.territoiresentransitions.react/src/ui/dropdownLists/PersonnesDropdown/PersonnesDropdown.tsx
@@ -1,13 +1,11 @@
 import { useCollectiviteId } from '@/api/collectivites';
 import { RouterOutput } from '@/api/utils/trpc/client';
-import InvitationModal from '@/app/app/pages/collectivite/Users/invitation/invitation-modal';
-import { useTagsList } from '@/app/app/pages/collectivite/Users/tags-liste/use-tags-list';
 import { useTagCreate } from '@/app/ui/dropdownLists/tags/useTagCreate';
 import { useDeleteTag } from '@/app/ui/dropdownLists/tags/useTagDelete';
 import { useTagUpdate } from '@/app/ui/dropdownLists/tags/useTagUpdate';
 import { PersonneTagOrUser } from '@/domain/collectivites';
 import { Option, OptionValue, SelectFilter, SelectMultipleProps } from '@/ui';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { QueryKey } from 'react-query';
 import { usePersonneListe } from './usePersonneListe';
 import { getPersonneStringId } from './utils';
@@ -30,24 +28,13 @@ type Props = Omit<SelectMultipleProps, 'values' | 'onChange' | 'options'> & {
 
 /** Sélecteur de personnes de la collectivité */
 const PersonnesDropdown = (props: Props) => {
-  const [tagForInvite, setTagForInvite] = useState<number | undefined>();
-  // const [tagToLink, setTagToLink] = useState<number | undefined>();
-
   const collectiviteId = useCollectiviteId();
 
   const { data: personneListe, isLoading, refetch } = usePersonneListe();
-  const { data: tags, refetch: refetchTags } = useTagsList(collectiviteId);
-
-  const getTagInfo = (id: number) => {
-    return (tags ?? []).find((t) => t.tagId === id);
-  };
 
   const getOptionIcon = (personne: Tag) => {
     if (personne.userId) {
       return { icon: 'user-follow-line', iconClassname: 'text-success-1' };
-    }
-    if (getTagInfo(personne.tagId)?.email) {
-      return { icon: 'hourglass-line', iconClassname: 'text-warning-1' };
     }
     return { icon: undefined, iconClassname: undefined };
   };
@@ -91,7 +78,6 @@ const PersonnesDropdown = (props: Props) => {
     tagTableName: 'personne_tag',
     onSuccess: () => {
       refetch();
-      refetchTags();
     },
   });
 
@@ -116,106 +102,61 @@ const PersonnesDropdown = (props: Props) => {
   }, [newTagId]);
 
   return (
-    <>
-      <SelectFilter
-        {...props}
-        dataTest={props.dataTest ?? 'personnes'}
-        isSearcheable
-        isLoading={isLoading}
-        options={options}
-        onChange={({ values, selectedValue }) =>
-          props.onChange({
-            personnes: getSelectedPersonnes(values),
-            selectedPersonne: getSelectedPersonnes([selectedValue])[0],
-          })
-        }
-        // actions={[
-        //   {
-        //     label: 'Associer ce compte à un tag',
-        //     icon: 'user-add-line',
-        //     action: () => alert('test !'),
-        //   },
-        // ]}
-        createProps={
-          !props.disableEdition
-            ? {
-                userCreatedOptions:
-                  (personneListe as Tag[])
-                    ?.filter((p) => p.tagId)
-                    .map((p) => p.tagId.toString()) ?? [],
-                onUpdate: (tagId, tagName) => {
-                  updateTag({
-                    collectiviteId: collectiviteId,
-                    id: parseInt(tagId as string),
-                    nom: tagName,
-                  });
-                },
-                onDelete: (tagId) => {
-                  props.onChange({
-                    personnes: getSelectedPersonnes(
-                      props.values?.filter((v) => v !== tagId)
-                    ),
-                    selectedPersonne: getSelectedPersonnes(
-                      props.values?.filter((v) => v === tagId)
-                    )[0],
-                  });
-                  deleteTag(parseInt(tagId as string));
-                },
-                onCreate: (inputValue) =>
-                  createTag({
-                    collectiviteId: collectiviteId,
-                    nom: inputValue,
-                  }),
-                updateModal: {
-                  title: 'Editer le tag pilote',
-                  fieldTitle: 'Nom du tag',
-                },
-                deleteModal: {
-                  title: 'Supprimer un tag pilote',
-                  message:
-                    'En confirmant la suppression, cela supprimera également l’association de ce tag aux fiches action, indicateurs et mesures des référentiels.',
-                },
-                actions: [
-                  {
-                    label: 'Inviter à créer un compte',
-                    icon: 'mail-send-line',
-                    action: (id) => setTagForInvite(parseInt(id as string)),
-                    disabledIds: (tags ?? [])
-                      .filter((t) => t?.email)
-                      .map((t) => t.tagId.toString() as OptionValue),
-                  },
-                  // {
-                  //   label: 'Associer ce tag à un compte',
-                  //   icon: 'user-add-line',
-                  //   action: (id) => setTagToLink(parseInt(id as string)),
-                  //   disabledIds: (tags ?? [])
-                  //     .filter((t) => t?.email)
-                  //     .map((t) => t.tagId.toString() as OptionValue),
-                  // },
-                ],
-              }
-            : undefined
-        }
-      />
-
-      <InvitationModal
-        openState={{
-          isOpen: !!tagForInvite,
-          setIsOpen: () => tagForInvite && setTagForInvite(undefined),
-        }}
-        tagIds={tagForInvite ? [tagForInvite] : undefined}
-      />
-
-      {/* {!!tagToLink && (
-        <LinkTagToAccountModal
-          openState={{
-            isOpen: !!tagToLink,
-            setIsOpen: () => tagToLink && setTagToLink(undefined),
-          }}
-          tag={getTagInfo(tagToLink)}
-        />
-      )} */}
-    </>
+    <SelectFilter
+      {...props}
+      dataTest={props.dataTest ?? 'personnes'}
+      isSearcheable
+      isLoading={isLoading}
+      options={options}
+      onChange={({ values, selectedValue }) =>
+        props.onChange({
+          personnes: getSelectedPersonnes(values),
+          selectedPersonne: getSelectedPersonnes([selectedValue])[0],
+        })
+      }
+      createProps={
+        !props.disableEdition
+          ? {
+              userCreatedOptions:
+                (personneListe as Tag[])
+                  ?.filter((p) => p.tagId)
+                  .map((p) => p.tagId.toString()) ?? [],
+              onUpdate: (tagId, tagName) => {
+                updateTag({
+                  collectiviteId: collectiviteId,
+                  id: parseInt(tagId as string),
+                  nom: tagName,
+                });
+              },
+              onDelete: (tagId) => {
+                props.onChange({
+                  personnes: getSelectedPersonnes(
+                    props.values?.filter((v) => v !== tagId)
+                  ),
+                  selectedPersonne: getSelectedPersonnes(
+                    props.values?.filter((v) => v === tagId)
+                  )[0],
+                });
+                deleteTag(parseInt(tagId as string));
+              },
+              onCreate: (inputValue) =>
+                createTag({
+                  collectiviteId: collectiviteId,
+                  nom: inputValue,
+                }),
+              updateModal: {
+                title: 'Editer le tag pilote',
+                fieldTitle: 'Nom du tag',
+              },
+              deleteModal: {
+                title: 'Supprimer un tag pilote',
+                message:
+                  'En confirmant la suppression, cela supprimera également l’association de ce tag aux fiches action, indicateurs et mesures des référentiels.',
+              },
+            }
+          : undefined
+      }
+    />
   );
 };
 
--- packages/ui/src/design-system/Select/components/Options.tsx
+++ packages/ui/src/design-system/Select/components/Options.tsx
@@ -4,12 +4,8 @@ import { Fragment } from 'react';
 import { Badge } from '@/ui/design-system/Badge';
 import { Icon } from '@/ui/design-system/Icon';
 
+import { ITEM_ALL } from '@/ui/design-system/Select/SelectFilter';
 import {
-  ITEM_ALL,
-  UNSELECT_LABEL,
-} from '@/ui/design-system/Select/SelectFilter';
-import {
-  CustomAction,
   OptionValue,
   SelectOption,
   Option as TOption,
@@ -23,8 +19,6 @@ type BaseProps = {
   values?: OptionValue[];
   /** Appelée au click d'une option (reçoit la valeur de l'option cliquée) */
   onChange: (value: OptionValue) => void;
-  /** Permet d'ajouter des actions custom sur les options */
-  actions?: CustomAction[];
   /** Permet de customiser l'item (label) d'une option */
   customItem?: (option: TOption) => React.ReactElement;
   /** Permet d'afficher des badges dans les options */
@@ -48,7 +42,6 @@ const Options = ({
   options,
   onChange,
   isLoading,
-  actions,
   customItem,
   isBadgeItem,
   createProps,
@@ -77,7 +70,6 @@ const Options = ({
                       option={option}
                       values={values}
                       onChange={onChange}
-                      actions={actions}
                       customItem={customItem}
                       isBadgeItem={isBadgeItem}
                       createProps={createProps}
@@ -94,7 +86,6 @@ const Options = ({
                 option={option}
                 values={values}
                 onChange={onChange}
-                actions={actions}
                 customItem={customItem}
                 isBadgeItem={isBadgeItem}
                 createProps={createProps}
@@ -122,7 +113,6 @@ const Option = ({
   values,
   option,
   onChange,
-  actions,
   customItem,
   isBadgeItem,
   createProps,
@@ -179,18 +169,9 @@ const Option = ({
           )}
         </div>
       </button>
-      {isUserCreated &&
-        (createProps?.onDelete ||
-          createProps?.onUpdate ||
-          (createProps?.actions && createProps.actions.length > 0)) && (
-          <OptionMenu option={option} {...createProps} />
-        )}
-      {actions &&
-        actions.length &&
-        !isUserCreated &&
-        option.label !== UNSELECT_LABEL && (
-          <OptionMenu option={option} actions={actions} />
-        )}
+      {isUserCreated && (createProps?.onDelete || createProps?.onUpdate) && (
+        <OptionMenu option={option} {...createProps} />
+      )}
     </div>
   );
 };
