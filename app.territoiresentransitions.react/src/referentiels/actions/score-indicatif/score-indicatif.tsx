4b391b802675cc3ccc08c79e87d2ea5c7a399d34
Marc Rutkowski <marc@attractive-media.fr>
2025-06-10 14:30:15 +0200

    Ajoute les composants d'édition/sélection des valeurs d'indicateur associées à un calcul de score indicatif

--- app.territoiresentransitions.react/src/referentiels/actions/score-indicatif/score-indicatif.tsx
+++ app.territoiresentransitions.react/src/referentiels/actions/score-indicatif/score-indicatif.tsx
@@ -1,11 +1,18 @@
 import { useCurrentCollectivite } from '@/api/collectivites';
 import { avancementToLabel } from '@/app/app/labels';
+import { ScoreIndicatifBadge } from '@/app/referentiels/actions/score-indicatif/score-indicatif-badge';
+import {
+  prepareScoreIndicatifData,
+  texteValeurUtilisee,
+} from '@/app/referentiels/actions/score-indicatif/utils';
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { toPercentString } from '@/app/utils/to-percent-string';
-import { TypeScoreIndicatif } from '@/domain/referentiels';
+import {
+  TypeScoreIndicatif,
+  typeScoreIndicatifEnum,
+} from '@/domain/referentiels';
 import { Button } from '@/ui';
 import { useState } from 'react';
-import { typeScoreToLabel, typeScoreToName } from './labels';
 import { ScoreIndicatifModal } from './score-indicatif-modal';
 import {
   ScoreIndicatifAction,
@@ -31,51 +38,58 @@ export const ScoreIndicatif = (props: ScoreIndicatifProps) => {
   if (isLoading) return <SpinnerLoader />;
   const scoreIndicatif = scoreIndicatifParActionId?.[actionId];
   if (!scoreIndicatif) return;
+  const nbIndicateurs = scoreIndicatif.indicateurs?.length || 0;
+  if (!nbIndicateurs) return;
 
-  const unite = scoreIndicatif.indicateurs?.[0]?.unite;
+  const unite = scoreIndicatif.indicateurs[0].unite;
 
   return (
-    <div className="mt-4 flex flex-col gap-2">
-      <LibelleScoreIndicatif
-        typeScore="fait"
-        unite={unite}
-        scoreIndicatif={scoreIndicatif}
-      />
-      <LibelleScoreIndicatif
-        typeScore="programme"
-        unite={unite}
-        scoreIndicatif={scoreIndicatif}
-      />
-      {!collectivite.isReadOnly && (
-        <>
-          <Button
-            variant="underlined"
-            size="sm"
-            onClick={() => {
-              setIsOpen(true);
-            }}
-          >
-            {`${
-              scoreIndicatif.fait?.valeursUtilisees?.length ||
-              scoreIndicatif.programme?.valeursUtilisees?.length
-                ? 'Modifier'
-                : 'Renseigner'
-            } les données de l'indicateur`}
-          </Button>
-          {isOpen && (
-            <ScoreIndicatifModal
-              scoreIndicatif={scoreIndicatif}
-              openState={{ isOpen, setIsOpen }}
-            />
-          )}
-        </>
-      )}
+    <div className="col-span-full">
+      <ScoreIndicatifBadge nbIndicateurs={nbIndicateurs} />
+      <div className="mt-4 flex flex-col gap-2">
+        <LibelleScoreIndicatif
+          typeScore="fait"
+          unite={unite}
+          scoreIndicatif={scoreIndicatif}
+        />
+        <LibelleScoreIndicatif
+          typeScore="programme"
+          unite={unite}
+          scoreIndicatif={scoreIndicatif}
+        />
+        {!collectivite.isReadOnly && (
+          <>
+            <Button
+              variant="underlined"
+              size="sm"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            >
+              {`${
+                scoreIndicatif.fait?.valeursUtilisees?.length ||
+                scoreIndicatif.programme?.valeursUtilisees?.length
+                  ? 'Modifier'
+                  : 'Renseigner'
+              } les données ${
+                nbIndicateurs > 1 ? 'des indicateurs' : "de l'indicateur"
+              }`}
+            </Button>
+            {isOpen && (
+              <ScoreIndicatifModal
+                scoreIndicatif={scoreIndicatif}
+                openState={{ isOpen, setIsOpen }}
+              />
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
 /**
- * Affiche le score indicatif fait ou programmé
+ * Affiche le libellé du score indicatif (fait ou programmé)
  */
 const LibelleScoreIndicatif = ({
   typeScore,
@@ -86,39 +100,80 @@ const LibelleScoreIndicatif = ({
   unite: string;
   scoreIndicatif: ScoreIndicatifAction;
 }) => {
-  if (!scoreIndicatif?.[typeScore]) return;
-  const { score, valeursUtilisees } = scoreIndicatif[typeScore];
+  const donnees = prepareScoreIndicatifData(typeScore, scoreIndicatif);
+  if (!donnees) return null;
+
+  const { valeurPrincipale, valeurSecondaire, noSource, score } = donnees;
+  const dateValeur =
+    valeurSecondaire?.dateValeur || valeurPrincipale.dateValeur;
 
   return (
-    <p className="mb-0" key={typeScore}>
-      Score indicatif {avancementToLabel[typeScore]} de{' '}
-      <b>{toPercentString(score)}</b> calculé pour :{' '}
-      {valeursUtilisees[0] && (
-        <LibelleValeurUtilisee
-          typeScore={typeScore}
-          unite={unite}
-          valeurUtilisee={valeursUtilisees[0]}
-        />
-      )}
-      {valeursUtilisees[1] && (
+    <p className="mb-0">
+      {typeScore === typeScoreIndicatifEnum.FAIT ? (
+        <LibelleScoreFait score={score} />
+      ) : (
+        <LibelleScoreProgramme score={score} dateValeur={dateValeur} />
+      )}{' '}
+      <LibelleValeurUtilisee
+        typeScore={typeScore}
+        unite={unite}
+        valeurUtilisee={valeurPrincipale}
+        noSource={noSource}
+      />
+      {valeurSecondaire && (
         <>
           {' '}
           et{' '}
           <LibelleValeurUtilisee
             typeScore={typeScore}
             unite={unite}
-            valeurUtilisee={valeursUtilisees[1]}
+            valeurUtilisee={valeurSecondaire}
           />
         </>
       )}
+      {typeScore === typeScoreIndicatifEnum.PROGRAMME &&
+        ` atteint${valeurSecondaire ? 's' : ''}`}
     </p>
   );
 };
+
+/**
+ * Génère le texte principal pour le score indicatif "fait"
+ */
+const LibelleScoreFait = ({ score }: { score: number }) => {
+  return (
+    <>
+      Pourcentage indicatif <b>Fait</b> de {toPercentString(score)} calculé sur
+      la base de :
+    </>
+  );
+};
+
+/**
+ * Génère le texte principal pour le score indicatif "programme"
+ */
+export function LibelleScoreProgramme({
+  score,
+  dateValeur,
+}: {
+  score: number;
+  dateValeur: string;
+}) {
+  const annee = new Date(dateValeur).getFullYear();
+  return (
+    <>
+      Pourcentage indicatif <b>Fait</b> en {isNaN(Number(annee)) ? '' : annee}{' '}
+      de {toPercentString(score)} calculé si
+    </>
+  );
+}
 
 type LibelleValeurUtiliseeProps = {
   typeScore: TypeScoreIndicatif;
   unite: string;
   valeurUtilisee: ScoreIndicatifValeurUtilisee;
+  noSource?: boolean;
+  noYear?: boolean;
 };
 
 /**
@@ -128,23 +183,29 @@ const LibelleValeurUtilisee = ({
   typeScore,
   unite,
   valeurUtilisee,
+  noSource,
+  noYear,
 }: LibelleValeurUtiliseeProps) => {
-  const { valeur, dateValeur, sourceLibelle } = valeurUtilisee;
+  const segments = texteValeurUtilisee({
+    valeurUtilisee,
+    unite,
+    typeScore,
+    noSource,
+    noYear,
+  });
+
   return (
     <span>
-      <b>
-        {valeur} {unite}
-      </b>{' '}
-      en {new Date(dateValeur).getFullYear()}, source{' '}
-      {sourceLibelle ? sourceLibelle : typeScoreToLabel[typeScore]}
+      <b>{segments.valeurEtUnite}</b> {segments.annee} {segments.source}{' '}
     </span>
   );
 };
 
+/** Affiche le libellé d'une valeur sélectionnée */
 export const LibelleValeurSelectionnee = (props: {
   indicateurId: number;
   unite: string;
-  valeurUtilisees?: ScoreIndicatifValeursUtilisees | null;
+  valeurUtilisees?: ScoreIndicatifValeursUtilisees;
   typeScore: TypeScoreIndicatif;
 }) => {
   const { unite, valeurUtilisees, typeScore } = props;
@@ -155,8 +216,8 @@ export const LibelleValeurSelectionnee = (props: {
   return (
     <>
       <span>
-        Valeur sélectionnée pour le calcul du score indicatif{' '}
-        {typeScoreToName[typeScore]} :
+        Valeur sélectionnée pour le calcul du pourcentage indicatif{' '}
+        {avancementToLabel[typeScore]} :
       </span>{' '}
       {valeurUtilisee ? (
         <LibelleValeurUtilisee
