0164a5e70426161fce4dff756bf86e5d90aaed0a
Marc Rutkowski <marc@attractive-media.fr>
2025-06-10 11:53:21 +0200

    Permet de demander les valeurs de limite/cible de plusieurs indicateurs à la fois

--- backend/src/indicateurs/valeurs/get-valeurs-reference.request.ts
+++ backend/src/indicateurs/valeurs/get-valeurs-reference.request.ts
@@ -6,11 +6,14 @@ export const getValeursReferenceRequestSchema = z
       .number()
       .int()
       .describe('Identifiant de la collectivité'),
-    indicateurId: z.coerce
+    indicateurIds: z.coerce
       .number()
       .int()
+      .array()
       .describe("Identifiant de l'indicateur"),
   })
-  .describe("Donne les valeurs de référence (cible et/ou seuil) d'un indicateur pour une collectivité");
+  .describe(
+    'Donne les valeurs de référence (cible et/ou seuil) des indicateurs pour une collectivité'
+  );
 
 export type GetValeursReferenceRequest = z.infer<typeof getValeursReferenceRequestSchema>;
