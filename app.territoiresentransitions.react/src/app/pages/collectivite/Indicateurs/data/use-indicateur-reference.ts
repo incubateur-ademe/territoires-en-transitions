1e198e9264fc23afebb82f1aa7733e80a0172824
Marc Rutkowski <marc@attractive-media.fr>
2025-06-10 11:53:21 +0200

    Permet de demander les valeurs de limite/cible de plusieurs indicateurs à la fois

--- app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/use-indicateur-reference.ts
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Indicateurs/data/use-indicateur-reference.ts
@@ -3,10 +3,26 @@ import { RouterInput, RouterOutput, trpc } from '@/api/utils/trpc/client';
 type IndicateurReferenceInput =
   RouterInput['indicateurs']['valeurs']['reference'];
 export type IndicateurReferenceOutput =
-  RouterOutput['indicateurs']['valeurs']['reference'];
+  RouterOutput['indicateurs']['valeurs']['reference'][number];
 
-export const useIndicateurReference = (input: IndicateurReferenceInput) => {
+export const useIndicateursListReferences = (
+  input: IndicateurReferenceInput
+) => {
   return trpc.indicateurs.valeurs.reference.useQuery(input);
+};
+
+export const useIndicateurReference = ({
+  collectiviteId,
+  indicateurId,
+}: {
+  collectiviteId: number;
+  indicateurId: number;
+}) => {
+  const { data, ...other } = useIndicateursListReferences({
+    collectiviteId,
+    indicateurIds: [indicateurId],
+  });
+  return { data: data?.[0], ...other };
 };
 
 export const hasValeurCible = (valeursReference?: IndicateurReferenceOutput) =>
