63ba3a4a83253dee4e42bdbf8a943a1ef835453c
Gael S <servauga@gmail.com>
2025-06-11 12:30:39 +0200

    revert formSectionGrid to remain pure layout component

--- app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionDescription/FicheDescriptionForm.tsx
+++ app.territoiresentransitions.react/src/app/pages/collectivite/PlansActions/FicheAction/FicheActionDescription/FicheDescriptionForm.tsx
@@ -96,118 +96,119 @@ export const FicheDescriptionForm = ({
   );
 
   return (
-    <FormSectionGrid
-      formSectionId={formId}
-      onSubmit={handleSubmit(handleSave)}
-      className="max-h-[50vh] overflow-y-auto"
-    >
-      <Field title="Nom de la fiche action" className="col-span-2">
-        <Input type="text" {...register('titre')} />
-      </Field>
+    <form id={formId} onSubmit={handleSubmit(handleSave)}>
+      <FormSectionGrid>
+        <Field title="Nom de la fiche action" className="col-span-2">
+          <Input type="text" {...register('titre')} />
+        </Field>
 
-      <Field title="Thématique">
-        <Controller
-          control={control}
-          name="thematiques"
-          render={({ field }) => (
-            <SelectFilter
-              options={thematiqueOptions}
-              values={field.value?.map((t) => t.id)}
-              onChange={({ values }) =>
-                field.onChange(
-                  thematiqueListe?.filter((thematique) =>
-                    values?.some((v) => v === thematique.id)
+        <Field title="Thématique">
+          <Controller
+            control={control}
+            name="thematiques"
+            render={({ field }) => (
+              <SelectFilter
+                options={thematiqueOptions}
+                values={field.value?.map((t) => t.id)}
+                onChange={({ values }) =>
+                  field.onChange(
+                    thematiqueListe?.filter((thematique) =>
+                      values?.some((v) => v === thematique.id)
+                    )
                   )
-                )
-              }
-            />
-          )}
-        />
-      </Field>
+                }
+              />
+            )}
+          />
+        </Field>
 
-      <Field title="Sous-thématique">
-        <Controller
-          control={control}
-          name="sousThematiques"
-          render={({ field }) => (
-            <SelectFilter
-              options={sousThematiqueOptions}
-              values={field.value?.map((t) => t.id)}
-              onChange={({ values }) =>
-                field.onChange(
-                  sousThematiqueListe?.filter((sousThematique) =>
-                    values?.some((v) => v === sousThematique.id)
+        <Field title="Sous-thématique">
+          <Controller
+            control={control}
+            name="sousThematiques"
+            render={({ field }) => (
+              <SelectFilter
+                options={sousThematiqueOptions}
+                values={field.value?.map((t) => t.id)}
+                onChange={({ values }) =>
+                  field.onChange(
+                    sousThematiqueListe?.filter((sousThematique) =>
+                      values?.some((v) => v === sousThematique.id)
+                    )
                   )
-                )
-              }
-            />
+                }
+              />
+            )}
+          />
+        </Field>
+
+        <Field title="Mes tags de suivi" className="col-span-2">
+          <Controller
+            control={control}
+            name="libreTags"
+            render={({ field }) => (
+              <TagsSuiviPersoDropdown
+                values={(field.value ?? []).map((t) => t.id)}
+                onChange={({ libresTag }) => field.onChange(libresTag)}
+                additionalKeysToInvalidate={[
+                  ['fiche_action', fiche.id.toString()],
+                ]}
+              />
+            )}
+          />
+        </Field>
+
+        <Field
+          title="Description de l'action"
+          className="col-span-2"
+          state={
+            description?.length === DESCRIPTION_MAX_LENGTH ? 'info' : 'default'
+          }
+          message={getMaxLengthMessage(
+            description ?? '',
+            DESCRIPTION_MAX_LENGTH
           )}
-        />
-      </Field>
+        >
+          <AutoResizedTextarea
+            className="min-h-[100px]"
+            maxLength={DESCRIPTION_MAX_LENGTH}
+            {...register('description')}
+          />
+        </Field>
 
-      <Field title="Mes tags de suivi" className="col-span-2">
-        <Controller
-          control={control}
-          name="libreTags"
-          render={({ field }) => (
-            <TagsSuiviPersoDropdown
-              values={(field.value ?? []).map((t) => t.id)}
-              onChange={({ libresTag }) => field.onChange(libresTag)}
-              additionalKeysToInvalidate={[
-                ['fiche_action', fiche.id.toString()],
-              ]}
-            />
+        <Field
+          title="Moyens humains et techniques"
+          className="col-span-2"
+          state={ressources?.length === MOYENS_MAX_LENGTH ? 'info' : 'default'}
+          message={getMaxLengthMessage(ressources ?? '', MOYENS_MAX_LENGTH)}
+        >
+          <AutoResizedTextarea
+            className="min-h-[100px]"
+            maxLength={MOYENS_MAX_LENGTH}
+            {...register('ressources')}
+          />
+        </Field>
+
+        <Field
+          title="Instances de gouvernance"
+          className="col-span-2"
+          state={
+            instanceGouvernance?.length === INSTANCES_MAX_LENGTH
+              ? 'info'
+              : 'default'
+          }
+          message={getMaxLengthMessage(
+            instanceGouvernance ?? '',
+            INSTANCES_MAX_LENGTH
           )}
-        />
-      </Field>
-
-      <Field
-        title="Description de l'action"
-        className="col-span-2"
-        state={
-          description?.length === DESCRIPTION_MAX_LENGTH ? 'info' : 'default'
-        }
-        message={getMaxLengthMessage(description ?? '', DESCRIPTION_MAX_LENGTH)}
-      >
-        <AutoResizedTextarea
-          className="min-h-[100px]"
-          maxLength={DESCRIPTION_MAX_LENGTH}
-          {...register('description')}
-        />
-      </Field>
-
-      <Field
-        title="Moyens humains et techniques"
-        className="col-span-2"
-        state={ressources?.length === MOYENS_MAX_LENGTH ? 'info' : 'default'}
-        message={getMaxLengthMessage(ressources ?? '', MOYENS_MAX_LENGTH)}
-      >
-        <AutoResizedTextarea
-          className="min-h-[100px]"
-          maxLength={MOYENS_MAX_LENGTH}
-          {...register('ressources')}
-        />
-      </Field>
-
-      <Field
-        title="Instances de gouvernance"
-        className="col-span-2"
-        state={
-          instanceGouvernance?.length === INSTANCES_MAX_LENGTH
-            ? 'info'
-            : 'default'
-        }
-        message={getMaxLengthMessage(
-          instanceGouvernance ?? '',
-          INSTANCES_MAX_LENGTH
-        )}
-      >
-        <AutoResizedTextarea
-          className="min-h-[100px]"
-          maxLength={INSTANCES_MAX_LENGTH}
-          {...register('instanceGouvernance')}
-        />
-      </Field>
-    </FormSectionGrid>
+        >
+          <AutoResizedTextarea
+            className="min-h-[100px]"
+            maxLength={INSTANCES_MAX_LENGTH}
+            {...register('instanceGouvernance')}
+          />
+        </Field>
+      </FormSectionGrid>
+    </form>
   );
 };
