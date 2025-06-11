0b9d73c4af795e49ac7785b038f3696e9c7cec1b
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-11 14:41:22 +0200

    Fix sur le site suite au changement de version de next

--- packages/site/app/outil-numerique/page.tsx
+++ packages/site/app/outil-numerique/page.tsx
@@ -13,7 +13,7 @@ import TemoignagesPlateforme from './TemoignagesPlateforme';
 import { getStrapiData } from './utils';
 
 export async function generateMetadata(
-  { params }: { params: unknown },
+  { params }: { params: Promise<unknown> },
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const metadata = (await parent) as Metadata;
