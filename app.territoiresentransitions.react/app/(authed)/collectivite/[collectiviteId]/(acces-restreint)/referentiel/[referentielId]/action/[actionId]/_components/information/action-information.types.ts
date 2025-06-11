7214e0f6575e37830a00a9e479b86f4fe1bc1f7c
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-11 18:18:44 +0200

    Déplace la section informations dans la page mesure

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/information/action-information.types.ts
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/information/action-information.types.ts
@@ -1,10 +1,8 @@
 import { Prefix } from '@/api';
-import { TOC_ITEMS } from './action-information.sommaire.items';
+import { TOC_ITEMS } from './action-information.utils';
 
 // type d'info associées à une action
 export type TActionInfo = (typeof TOC_ITEMS)[number]['id'];
-// noms des champs dans l'objet action
-export type TFieldName = Prefix<'have_', Exclude<TActionInfo, 'desc'>>;
 // et des rpc correspondantes dans la base
 export type TRPCName = Prefix<'action_', Exclude<TActionInfo, 'desc'>>;
 
