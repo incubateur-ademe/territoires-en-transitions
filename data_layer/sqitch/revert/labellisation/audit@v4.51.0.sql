5d4c254012ca26de16a0dfd5536e118d91995928
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-06-12 16:26:11 +0200

    Rend la vue PG des preuves indépendante de `client_scores`

---
+++ data_layer/sqitch/revert/labellisation/audit@v4.51.0.sql
@@ -0,0 +1,23 @@
+-- Deploy tet:labellisation/audit to pg
+BEGIN;
+
+create or replace function labellisation_peut_commencer_audit(
+  collectivite_id integer, referentiel referentiel
+) returns boolean
+  stable
+  security definer
+  language sql
+BEGIN
+  ATOMIC
+  SELECT ((a.date_debut IS NULL) AND (auth.uid() IN (SELECT aa.auditeur
+                                                     FROM audit_auditeur aa
+                                                     WHERE (a.id = aa.audit_id))))
+  FROM labellisation.audit a
+  WHERE a.collectivite_id = labellisation_peut_commencer_audit.collectivite_id
+    AND a.referentiel = labellisation_peut_commencer_audit.referentiel
+    AND now() <@ tstzrange(a.date_debut, a.date_fin)
+  ORDER BY a.date_debut DESC NULLS LAST
+  LIMIT 1;
+END;
+
+COMMIT;
