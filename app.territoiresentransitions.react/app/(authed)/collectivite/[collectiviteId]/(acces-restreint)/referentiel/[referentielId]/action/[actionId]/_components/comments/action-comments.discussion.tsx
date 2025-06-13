d39faafb45c8a8c04ed10d10e0ef2f52cf76ac6a
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-13 17:37:30 +0200

    Supprime les anciens fichiers commentaires

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/comments/action-comments.discussion.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/comments/action-comments.discussion.tsx
@@ -1,4 +1,4 @@
-import { useAddCommentaireToDiscussion } from '@/app/referentiels/actions/action-discussion/data/useAddCommentaireToDiscussion';
+import { useAddCommentaireToDiscussion } from 'app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/comments/data/useAddCommentaireToDiscussion';
 import ActionCommentInput from './action-comments.input';
 import ActionCommentsItemsList from './action-comments.items-list';
 import { TActionDiscussion } from './action-comments.types';
