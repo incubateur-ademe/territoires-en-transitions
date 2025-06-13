260fad8320872d983e3126b759b67179994f4b59
Marc Rutkowski <marc@attractive-media.fr>
2025-06-13 15:18:45 +0200

    Supprime une colonne inutilisée

--- data_layer/sqitch/deploy/indicateur/indicateur_action_expr@v4.51.0.sql
+++ data_layer/sqitch/deploy/indicateur/indicateur_action_expr@v4.51.0.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+alter table action_definition
+  add column expr_score text;
+comment on column action_definition.expr_score is 'Formule permettant de calculer une proposition de score à partir d''un indicateur';
+
 alter table indicateur_action
   add column utilise_par_expr_score boolean;
 comment on column indicateur_action.utilise_par_expr_score is 'Indique que l''indicateur est utilisé dans une formule de proposition de score';
