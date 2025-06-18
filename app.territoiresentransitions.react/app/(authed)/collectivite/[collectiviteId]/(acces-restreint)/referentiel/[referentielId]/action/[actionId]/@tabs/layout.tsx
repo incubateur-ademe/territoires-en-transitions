589601cb75a4b3c56cd06c2de458bd00f209ced7
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-18 14:25:45 +0200

    Mise à jour du layout sur l'onglet suivi de la mesure

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/@tabs/layout.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/@tabs/layout.tsx
@@ -184,7 +184,7 @@ function ActionLayout({
         </Tabs>
 
         {/** Action précédente / suivante */}
-        <div className="flex justify-end mt-8 gap-4">
+        <div className="flex justify-between mt-8 gap-4">
           {!!prevActionLink && (
             <Button
               variant="outlined"
@@ -200,6 +200,7 @@ function ActionLayout({
               icon="arrow-right-line"
               iconPosition="right"
               size="sm"
+              className="ml-auto"
               href={nextActionLink}
             >
               Mesure suivante
