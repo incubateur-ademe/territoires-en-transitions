7cb2a4d6a69ac6342334f5b15e94638eaabdaaec
Arthur Molinos <arthur.molinos@gmail.com>
2025-06-18 18:27:28 +0200

    Ajout des metrics au suivi personnel

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/tableau-de-bord/personnel/_components/personnel.page.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/tableau-de-bord/personnel/_components/personnel.page.tsx
@@ -1,19 +1,21 @@
 'use client';
 
 import { useCurrentCollectivite } from '@/api/collectivites';
+import { useUser } from '@/api/users/user-provider';
 import { usePlansActionsListe } from '@/app/app/pages/collectivite/PlansActions/PlanAction/data/usePlansActionsListe';
 import SansPlanPlaceholder from '@/app/tableaux-de-bord/plans-action/sans-plan.placeholder';
 
-import { useUser } from '@/api/users/user-provider';
-
 import Header from '../../_components/header';
-import Modules from '../_components/modules';
+import Metrics from './metrics';
+import Modules from './modules';
 
 const PersonnelPage = () => {
   const { prenom } = useUser();
   const { nom, isReadOnly } = useCurrentCollectivite();
 
   const { data: plansActions } = usePlansActionsListe({});
+
+  const hasPlan = plansActions ? plansActions.plans.length > 0 : false;
 
   return (
     <>
@@ -29,13 +31,11 @@ const PersonnelPage = () => {
         activeTab="personnel"
       />
       <div className="mt-8">
-        {plansActions?.plans.length === 0 ? (
-          <SansPlanPlaceholder />
-        ) : (
-          <div className="flex flex-col gap-8 mt-6">
-            <Modules />
-          </div>
-        )}
+        <div className="flex flex-col gap-8 mt-8">
+          <Metrics />
+          {plansActions?.plans.length === 0 && <SansPlanPlaceholder />}
+          <Modules hasPlan={hasPlan} />
+        </div>
       </div>
     </>
   );
---
+++ app.territoiresentransitions.react/src/tableaux-de-bord/metrics/metric.card-skeleton.tsx
@@ -0,0 +1,11 @@
+const MetricCardSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-4 p-6 bg-white border border-grey-3 rounded-xl animate-pulse">
+      <div className="h-14 w-10 bg-grey-3 rounded" />
+      <div className="h-6 w-5/6 bg-grey-3 rounded" />
+      <div className="h-4 w-3/4 bg-grey-3 rounded" />
+    </div>
+  );
+};
+
+export default MetricCardSkeleton;
