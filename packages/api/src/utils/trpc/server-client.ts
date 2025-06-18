339ad3518eb6d968f75f9bb1800ef21db7ee9c35
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-06-18 16:36:58 +0200

    Authenticate user in trpc when called from route handlers

--- packages/api/src/utils/trpc/server-client.ts
+++ packages/api/src/utils/trpc/server-client.ts
@@ -6,35 +6,35 @@ import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import { cache } from 'react';
 import { makeQueryClient } from './query-client';
 
+async function authenticatedHeaders() {
+  const supabaseClient = await createClient();
+  const {
+    data: { session },
+  } = await supabaseClient.auth.getSession();
+
+  return getAuthHeaders(session);
+}
+
+const TRPC_LINK = httpLink({
+  url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`,
+  headers: authenticatedHeaders,
+});
+
 // IMPORTANT: Create a stable getter for the query client that
 // will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
 
-// TRPC client utilisable dans les RSC
+// TRPC client utilisable dans les RSC avec le QueryClient
+// ce qui permet de partager la data entre server et front,
+// ou bien de faire du prefetching
 export const trpcInServerComponent = createTRPCOptionsProxy({
   client: createTRPCClient<AppRouter>({
-    links: [
-      httpLink({
-        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`,
-        async headers() {
-          const supabaseClient = await createClient();
-          const {
-            data: { session },
-          } = await supabaseClient.auth.getSession();
-
-          return getAuthHeaders(session);
-        },
-      }),
-    ],
+    links: [TRPC_LINK],
   }),
   queryClient: getQueryClient,
 });
 
 // TRPC client utilisable dans les server actions et les route handlers
 export const trpcInServerFunction = createTRPCClient<AppRouter>({
-  links: [
-    httpLink({
-      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`,
-    }),
-  ],
+  links: [TRPC_LINK],
 });
