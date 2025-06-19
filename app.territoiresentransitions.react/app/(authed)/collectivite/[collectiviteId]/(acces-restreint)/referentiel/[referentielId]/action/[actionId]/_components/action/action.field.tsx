6fbee56dd1faf821e71da12bd55e07a3a1c6427e
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-19 16:04:37 +0200

    Mise à jour du champ justification dans la mesure

---
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/action/action.field.tsx
@@ -0,0 +1,66 @@
+import { useCurrentCollectivite } from '@/api/collectivites';
+import {
+  useActionCommentaire,
+  useSaveActionCommentaire,
+} from '@/app/referentiels/use-action-commentaire';
+import { AutoResizedTextarea, Field, TextareaProps } from '@/ui';
+import { useEffect, useState } from 'react';
+
+type Props = TextareaProps & {
+  actionId: string;
+  title?: string;
+  hint?: string;
+  fieldClassName?: string;
+};
+
+const ActionField = ({
+  actionId,
+  title,
+  hint,
+  fieldClassName,
+  disabled,
+  ...props
+}: Props) => {
+  const collectivite = useCurrentCollectivite();
+  const { actionCommentaire, isLoading } = useActionCommentaire(actionId);
+  const { saveActionCommentaire } = useSaveActionCommentaire();
+
+  const [commentaire, setCommentaire] = useState(
+    actionCommentaire?.commentaire
+  );
+
+  useEffect(() => {
+    setCommentaire(actionCommentaire?.commentaire);
+  }, [actionCommentaire?.commentaire]);
+
+  const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentaire(evt.currentTarget.value);
+  };
+
+  const handleBlur = () => {
+    const initialValue = actionCommentaire?.commentaire;
+    const newValue = commentaire?.trim();
+    if (initialValue !== newValue) {
+      saveActionCommentaire({
+        action_id: actionId,
+        collectivite_id: collectivite.collectiviteId,
+        commentaire: newValue ?? '',
+      });
+    }
+  };
+
+  return (
+    <Field title={title} hint={hint} className={fieldClassName}>
+      <AutoResizedTextarea
+        value={commentaire}
+        onClick={(evt) => evt.stopPropagation()}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        disabled={collectivite.isReadOnly || isLoading || disabled}
+        {...props}
+      />
+    </Field>
+  );
+};
+
+export default ActionField;
