884fd1413ec24bbf1b1f6f587685a9fc900bb1d3
Arthur Molinos <arthur.molinos@gmail.com>
2025-06-19 19:00:12 +0200

    Remplace l'état vide de module TDB par EmptyCard

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/tableau-de-bord/synthetique/_components/suivi-plans.module.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/tableau-de-bord/synthetique/_components/suivi-plans.module.tsx
@@ -1,15 +1,10 @@
 import { useCollectiviteId } from '@/api/collectivites';
 import { FetchedPlanAction } from '@/api/plan-actions';
 import { usePlansActionsListe } from '@/app/app/pages/collectivite/PlansActions/PlanAction/data/usePlansActionsListe';
-import {
-  makeCollectivitePlansActionsCreerUrl,
-  makeTdbPlansEtActionsModuleUrl,
-} from '@/app/app/paths';
+import { makeTdbPlansEtActionsModuleUrl } from '@/app/app/paths';
 import { useFichesActionCountBy } from '@/app/plans-action/fiches/_data/use-fiches-action-count-by';
 import Statuts from '@/app/plans-action/plans/card/statuts';
 import ModuleContainer from '@/app/tableaux-de-bord/modules/module/module.container';
-import ModuleEmpty from '@/app/tableaux-de-bord/modules/module/module.empty';
-import PictoDocument from '@/app/ui/pictogrammes/PictoDocument';
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { Statut } from '@/domain/plans/fiches';
 import { CountByRecordType } from '@/domain/utils';
@@ -26,6 +21,8 @@ const SuiviPlansModule = () => {
 
   const { data, isLoading } = usePlansActionsListe({});
 
+  const displayedPlans = data?.plans.slice(0, 4) || [];
+
   if (isLoading) {
     return (
       <ModuleContainer>
@@ -36,41 +33,28 @@ const SuiviPlansModule = () => {
     );
   }
 
-  if (data?.count === 0) {
-    return (
-      <ModuleEmpty
-        symbole={<PictoDocument className="w-16 h-16" />}
-        title="Suivi de l’avancée des plans"
-        emptyButtons={[
-          {
-            children: "Créer un plan d'action",
-            href: makeCollectivitePlansActionsCreerUrl({ collectiviteId }),
-          },
-        ]}
-      />
-    );
-  }
-
   return (
     <ModuleContainer className="gap-8">
       <h6 className="mb-0">Suivi de l’avancée des plans</h6>
       <div className="flex flex-col gap-8">
-        {data?.plans.map((plan) => (
+        {displayedPlans.map((plan) => (
           <Plan key={plan.id} plan={plan} />
         ))}
       </div>
-      <Button
-        className="ml-auto mt-auto"
-        variant="underlined"
-        size="sm"
-        onClick={() => tracker(Event.tdb.viewTousLesPlans)}
-        href={makeTdbPlansEtActionsModuleUrl({
-          collectiviteId,
-          module: 'suivi-plan-actions',
-        })}
-      >
-        Voir tous les plans
-      </Button>
+      <div className="flex mt-auto">
+        <Button
+          className="ml-auto"
+          variant="underlined"
+          size="sm"
+          onClick={() => tracker(Event.tdb.viewTousLesPlans)}
+          href={makeTdbPlansEtActionsModuleUrl({
+            collectiviteId,
+            module: 'suivi-plan-actions',
+          })}
+        >
+          Voir tous les plans
+        </Button>
+      </div>
     </ModuleContainer>
   );
 };
