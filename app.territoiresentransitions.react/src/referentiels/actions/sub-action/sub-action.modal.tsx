f2688c8b86a26433b0d853bfecef55cfbba9ffa6
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-20 12:09:48 +0200

    Mise à jour des cartes tâches dans une sous mesure

--- app.territoiresentransitions.react/src/referentiels/actions/sub-action/sub-action.modal.tsx
+++ app.territoiresentransitions.react/src/referentiels/actions/sub-action/sub-action.modal.tsx
@@ -2,9 +2,9 @@ import { ActionDefinitionSummary } from '@/app/referentiels/ActionDefinitionSumm
 import { Modal, ModalFooterOKCancel } from '@/ui';
 import { OpenState } from '@/ui/utils/types';
 import { useState } from 'react';
+import TasksList from '../../../../app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/task/task.list';
 import { useActionSummaryChildren } from '../../referentiel-hooks';
 import AvancementDetailleSliderAutoSave from '../avancement-detaille/avancement-detaille.slider.auto-save';
-import SubActionTasksList from '../sub-action-task/sub-action-task.list';
 
 type Props = {
   actionDefinition: ActionDefinitionSummary;
@@ -45,7 +45,7 @@ const SubActionModal = ({ actionDefinition, openState }: Props) => {
 
           {/* Liste des tâches */}
           {tasks.length > 0 && (
-            <SubActionTasksList tasks={tasks} hideStatus={hideStatus} />
+            <TasksList tasks={tasks} hideStatus={hideStatus} />
           )}
         </div>
       )}
