d2b0fddf062732001d8a03dc75b6d7bd253bbddf
Dimitri Valax <dvalax.pro@gmail.com>
2025-06-23 16:33:35 +0200

    add filter "date de fin prévisionnelle"

--- backend/src/plans/fiches/list-fiches/list-fiches.service.ts
+++ backend/src/plans/fiches/list-fiches/list-fiches.service.ts
@@ -1051,6 +1051,11 @@ export default class ListFichesService {
     } else {
       conditions.push(isNull(sql`indicateur_ids`));
     }
+    if (filters.hasDateDeFinPrevisionnelle) {
+      conditions.push(isNotNull(ficheActionTable.dateFin));
+    } else {
+      conditions.push(isNull(ficheActionTable.dateFin));
+    }
     if (filters.indicateurIds?.length) {
       this.addArrayOverlapsConditionForIntArray(
         conditions,
