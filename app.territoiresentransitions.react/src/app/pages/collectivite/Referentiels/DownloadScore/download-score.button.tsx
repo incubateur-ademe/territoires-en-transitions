79d93d41b2cd5d9779a5f4737eb1f2f2e8254fe4
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-06-25 16:45:42 +0200

    feat: add download button and modal

---
+++ app.territoiresentransitions.react/src/app/pages/collectivite/Referentiels/DownloadScore/download-score.button.tsx
@@ -0,0 +1,42 @@
+import { DownloadScoreModal } from '@/app/app/pages/collectivite/Referentiels/DownloadScore/download-score.modal';
+import { SaveScoreProps } from '@/app/app/pages/collectivite/Referentiels/SaveScore/save-score.modal';
+import { Button } from '@/ui';
+import { useState } from 'react';
+
+type DownloadScoreButtonProps = SaveScoreProps & {
+  label?: string;
+};
+
+const DownloadScoreButton = ({
+  referentielId,
+  collectiviteId,
+  label = 'Télécharger',
+}: DownloadScoreButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        icon="download-line"
+        variant="primary"
+        size="sm"
+        className="whitespace-nowrap"
+        onClick={() => setIsOpen(true)}
+        data-test="referentiels.snapshots.figer-referentiel-button"
+      >
+        {label}
+      </Button>
+      {isOpen && (
+        <DownloadScoreModal
+          collectiviteId={collectiviteId}
+          referentielId={referentielId}
+          openState={{
+            isOpen,
+            setIsOpen,
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default DownloadScoreButton;
