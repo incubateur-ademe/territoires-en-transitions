6c59f2d8f4bd8936242e7aa0ca3515b7a872b779
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-06-25 14:49:20 +0200

    refactor: softens the revert

--- data_layer/sqitch/deploy/referentiel/vues.sql
+++ data_layer/sqitch/deploy/referentiel/vues.sql
@@ -5,7 +5,7 @@ BEGIN;
 drop function referentiel_down_to_action(referentiel referentiel);
 drop function action_down_to_tache(referentiel referentiel, identifiant text);
 
-drop view action_title;
-drop view action_definition_summary;
+drop view if exists action_title;
+drop view if exists action_definition_summary;
 
 COMMIT;
