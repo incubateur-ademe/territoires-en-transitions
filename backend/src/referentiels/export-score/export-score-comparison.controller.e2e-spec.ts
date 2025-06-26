3bceb31e62a8fe734e7f7e7a981e6134788dba57
Marc Rutkowski <marc@attractive-media.fr>
2025-06-26 19:30:45 +0200

    Ajoute le score indicatif dans l'export des scores

--- backend/src/referentiels/export-score/export-score-comparison.controller.e2e-spec.ts
+++ backend/src/referentiels/export-score/export-score-comparison.controller.e2e-spec.ts
@@ -1,13 +1,25 @@
+import { AuthenticatedUser } from '@/backend/users/index-domain';
+import { DatabaseService } from '@/backend/utils';
+import { TrpcRouter } from '@/backend/utils/trpc/trpc.router';
 import { INestApplication } from '@nestjs/common';
+import { getAuthUser } from 'backend/test/auth-utils';
+import { insertFixturePourScoreIndicatif } from 'backend/test/indicateurs-utils';
+import { CellValue, Workbook } from 'exceljs';
 import { DateTime } from 'luxon';
 import { default as request } from 'supertest';
-import { getTestApp } from '../../../test/app-utils';
+import { getTestApp, getTestRouter } from '../../../test/app-utils';
 
 describe('Referentiels scoring routes', () => {
   let app: INestApplication;
+  let router: TrpcRouter;
+  let databaseService: DatabaseService;
+  let yoloDodoUser: AuthenticatedUser;
 
   beforeAll(async () => {
     app = await getTestApp();
+    router = await getTestRouter(app);
+    databaseService = app.get(DatabaseService);
+    yoloDodoUser = await getAuthUser();
   });
 
   test(`Export du snapshot pour un utilisateur non authentifié`, async () => {
@@ -49,7 +61,80 @@ describe('Referentiels scoring routes', () => {
     const exportFileSize = parseInt(
       responseSnapshotExport.headers['content-length']
     );
+
     expect(exportFileSize / 1000).toBeCloseTo(expectedExportSize, 0);
+  }, 30000);
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  test(`Export du snapshot avec un score indicatif`, async () => {
+    const collectiviteId = 2; // sur la 1 CAE est verrouillé par une demande de labellisation
+    const fixture = {
+      collectiviteId,
+      actionId: 'cae_1.2.3.3.1',
+      identifiantReferentiel: 'cae_6.a',
+      dateValeur: '2020-01-01',
+      exprScore: `si val(cae_6.a) > limite(cae_6.a) alors 0
+sinon si val(cae_6.a) < cible(cae_6.a) alors 1
+sinon ((limite(cae_6.a) - val(cae_6.a)) / (limite(cae_6.a) - cible(cae_6.a)))`,
+      objectif: 300,
+      resultat: 500,
+    };
+
+    // insère les données de test
+    const cleanup = await insertFixturePourScoreIndicatif(
+      databaseService,
+      fixture
+    );
+    onTestFinished(async () => {
+      await cleanup();
+    });
+
+    // regénère le snapshot score courant pour que le score indicatif pour les
+    // données de tests soit inclus
+    const caller = router.createCaller({ user: yoloDodoUser });
+    await caller.referentiels.snapshots.computeAndUpsert({
+      referentielId: 'cae',
+      collectiviteId,
+    });
+
+    const responseSnapshotExport = await request(app.getHttpServer())
+      .get(
+        `/collectivites/${collectiviteId}/referentiels/cae/score-snapshots/export/score-courant`
+      )
+      .set('Authorization', `Bearer ${process.env.SUPABASE_ANON_KEY}`)
+      .expect(200)
+      .responseType('blob');
+
+    const currentDate = DateTime.now().toISODate();
+    const exportFileName = responseSnapshotExport.headers['content-disposition']
+      .split('filename=')[1]
+      .split(';')[0];
+
+    expect(exportFileName).toBe(`"Export_CAE_Arbent_${currentDate}.xlsx"`);
+    const expectedExportSize = 203.375;
+    const exportFileSize = parseInt(
+      responseSnapshotExport.headers['content-length']
+    );
+    expect(exportFileSize / 1000).toBeCloseTo(expectedExportSize, 0);
+
+    const body = responseSnapshotExport.body as Buffer;
+    const wb = new Workbook();
+    await wb.xlsx.load(body);
+    const ws = wb.getWorksheet(1);
+    expect(ws).toBeDefined();
+    const row = ws
+      ?.getRows(0, 200)
+      ?.find((r) => (r.values as CellValue[])?.[1] === '1.2.3.3.1');
+    expect(row).toBeDefined();
+    const cell = row?.getCell('O');
+    expect(cell?.value)
+      .toEqual(`Pourcentage indicatif Fait de 80% calculé sur la base de : 500 kg/hab en 2020 (source : CITEPA)
+Pourcentage indicatif Fait en 2020 de 100% calculé si 300 kg/hab en 2020 (source : Objectifs de la collectivité) atteint`);
   }, 30000);
 
   afterAll(async () => {
