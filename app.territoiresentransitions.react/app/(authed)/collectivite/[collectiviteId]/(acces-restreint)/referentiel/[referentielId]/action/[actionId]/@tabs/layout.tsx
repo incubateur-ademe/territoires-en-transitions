374baaeb5a836577a69a26221bda02a0818747e7
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-26 18:25:06 +0200

    Améliorations sur le comportement responsif de la page mesure

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/@tabs/layout.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/@tabs/layout.tsx
@@ -92,7 +92,7 @@ function ActionLayout({
 
         <Tabs>
           <div className="flex justify-between">
-            <TabsList className="!justify-start pl-0 mt-6 flex-nowrap">
+            <TabsList className="!justify-start pl-0 mt-6 flex-nowrap overflow-x-auto">
               <TabsTab
                 href={makeReferentielActionUrl({
                   collectiviteId,
