04ca0299da792ebd370729e915cfca8041ef31c5
Gaël Servaud <servauga@gmail.com>
2025-06-26 17:40:39 +0200

    Make "Créer une fiche action" button operational

--- app.territoiresentransitions.react/src/plans/plans/all-plans-view/components/Actions/index.tsx
+++ app.territoiresentransitions.react/src/plans/plans/all-plans-view/components/Actions/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useCreateFicheAction } from '@/app/app/pages/collectivite/PlansActions/FicheAction/data/useCreateFicheAction';
 import { PlanCreationButton } from '@/app/plans/plans/create-plan/components/PlanCreationButton';
 import { Button } from '@/ui/design-system/Button';
 
@@ -9,16 +10,11 @@ export const Actions = ({
   collectiviteId: number;
   panierId: string | undefined;
 }) => {
+  const { mutate: createFicheAction } = useCreateFicheAction();
+
   return (
     <div className="flex items-center gap-4">
-      <Button
-        size="xs"
-        variant="outlined"
-        disabled
-        onClick={() => {
-          console.log('click');
-        }}
-      >
+      <Button size="xs" variant="outlined" onClick={() => createFicheAction()}>
         Créer une fiche action
       </Button>
       <PlanCreationButton collectiviteId={collectiviteId} panierId={panierId} />
