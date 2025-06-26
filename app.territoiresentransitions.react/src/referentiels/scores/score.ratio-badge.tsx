ed09f59070c0c1ccd3c73c1afced2327829515ca
Marine Heckler <marine.heckler@beta.gouv.fr>
2025-06-26 12:19:44 +0200

    Remplace les points par des virgules dans l'affichage des scores

--- app.territoiresentransitions.react/src/referentiels/scores/score.ratio-badge.tsx
+++ app.territoiresentransitions.react/src/referentiels/scores/score.ratio-badge.tsx
@@ -1,3 +1,4 @@
+import { toLocaleFixed } from '@/app/utils/toFixed';
 import { Badge, BadgeSize } from '@/ui';
 import classNames from 'classnames';
 import { useScore } from '../use-snapshot';
@@ -18,7 +19,9 @@ export const ScoreRatioBadge = ({ actionId, size, className }: Props) => {
   const { pointFait, pointPotentiel } = score;
 
   const troncateIfZero = (value: string) => {
-    return value.endsWith('.0') ? value.slice(0, -2) : value;
+    return value.endsWith('.0')
+      ? value.slice(0, -2)
+      : toLocaleFixed(parseFloat(value), 2);
   };
 
   const calculatePercentage = (
