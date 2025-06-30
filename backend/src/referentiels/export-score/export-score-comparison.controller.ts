b5f2a3dbaaa8f9f3f864df4e6c79159a9272ce99
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-06-30 14:47:47 +0200

    refactor: rename export score controller

--- backend/src/referentiels/export-score/export-score-comparison.controller.ts
+++ backend/src/referentiels/export-score/export-score-comparison.controller.ts
@@ -1,47 +1,64 @@
 import { COLLECTIVITE_ID_PARAM_KEY } from '@/backend/collectivites/shared/models/collectivite-api.constants';
-import { AllowAnonymousAccess } from '@/backend/users/decorators/allow-anonymous-access.decorator';
-import { TokenInfo } from '@/backend/users/decorators/token-info.decorators';
-import { AuthUser } from '@/backend/users/index-domain';
 import { ApiUsageEnum } from '@/backend/utils/api/api-usage-type.enum';
 import { ApiUsage } from '@/backend/utils/api/api-usage.decorator';
-import { Controller, Get, Logger, Next, Param, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  Next,
+  Param,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { ApiExcludeController, ApiTags } from '@nestjs/swagger';
 import { NextFunction, Response } from 'express';
 import { ReferentielId } from '../index-domain';
 import { REFERENTIEL_ID_PARAM_KEY } from '../models/referentiel-api.constants';
-import { ExportScoreComparaisonService } from './export-score-comparaison.service';
+import { ExportScoreComparisonService } from './export-score-comparison.service';
 
 @ApiTags('Referentiels')
 @ApiExcludeController()
 @Controller('')
-export class ExportScoreComparaisonController {
-  private readonly logger = new Logger(ExportScoreComparaisonController.name);
+export class ExportScoreComparisonController {
+  private readonly logger = new Logger(ExportScoreComparisonController.name);
 
   constructor(
-    private readonly exportScoreComparaisonService: ExportScoreComparaisonService
+    private readonly exportScoreComparisonService: ExportScoreComparisonService
   ) {}
 
-  @AllowAnonymousAccess()
+  // @AllowAnonymousAccess()
   @Get(
-    `collectivites/:${COLLECTIVITE_ID_PARAM_KEY}/referentiels/:${REFERENTIEL_ID_PARAM_KEY}/score-snapshots/export-comparaison/audit`
+    `collectivites/:${COLLECTIVITE_ID_PARAM_KEY}/referentiels/:${REFERENTIEL_ID_PARAM_KEY}/score-snapshots/export-comparison`
   )
   @ApiUsage([ApiUsageEnum.APP])
   async exportAuditScore(
     @Param(COLLECTIVITE_ID_PARAM_KEY) collectiviteId: number,
     @Param(REFERENTIEL_ID_PARAM_KEY) referentielId: ReferentielId,
-    @TokenInfo() user: AuthUser,
+    @Query('isAudit') isAudit: string,
+    @Query('snapshotReferences') snapshotReferencesQuery: string,
     @Res() res: Response,
     @Next() next: NextFunction
   ) {
     this.logger.log(
-      `Export de comparaison des scores d'audit du referentiel ${referentielId} pour la collectivite ${collectiviteId}`
+      `Export de comparaison des scores du referentiel ${referentielId} pour la collectivite ${collectiviteId}`
     );
+
+    // Parse les paramètres
+    const isAuditBoolean = isAudit === 'true';
+    const snapshotReferences = snapshotReferencesQuery
+      ? snapshotReferencesQuery.split(',')
+      : [];
+
+    console.log('isAudit', isAuditBoolean);
+    console.log('snapshotReferences', snapshotReferences);
 
     try {
       const { fileName, content } =
-        await this.exportScoreComparaisonService.exportAuditScore(
+        await this.exportScoreComparisonService.exportComparisonScore(
           collectiviteId,
-          referentielId
+          referentielId,
+          isAuditBoolean,
+          snapshotReferences
         );
 
       res.attachment(fileName);
