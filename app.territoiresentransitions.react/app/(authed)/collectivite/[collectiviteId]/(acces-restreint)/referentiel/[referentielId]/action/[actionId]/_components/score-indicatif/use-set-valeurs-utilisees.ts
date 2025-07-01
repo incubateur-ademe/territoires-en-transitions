af69c374200f827c0f7f40404f795ec6ffb4af09
Marc Rutkowski <marc@attractive-media.fr>
2025-07-01 12:15:13 +0200

    Enregistre le snapshot quand la sélection des valeurs utilisées par un score indicatif change

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-set-valeurs-utilisees.ts
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-set-valeurs-utilisees.ts
@@ -1,9 +1,13 @@
 import { trpc } from "@/api/utils/trpc/client";
+import { useSnapshotComputeAndUpdate } from '@/app/referentiels/use-snapshot';
+import { getReferentielIdFromActionId } from '@/domain/referentiels';
 import { Event, useEventTracker } from '@/ui';
 
 export function useSetValeursUtilisees() {
   const trpcUtils = trpc.useUtils();
   const trackEvent = useEventTracker();
+  const { computeScoreAndUpdateCurrentSnapshot } =
+    useSnapshotComputeAndUpdate();
 
   return trpc.referentiels.actions.setValeursUtilisees.useMutation({
     onSuccess: (data, variables) => {
@@ -15,6 +19,10 @@ export function useSetValeursUtilisees() {
       trpcUtils.referentiels.actions.getValeursUtilisees.invalidate(input);
       trpcUtils.referentiels.actions.getValeursUtilisables.invalidate(input);
       trpcUtils.referentiels.actions.getScoreIndicatif.invalidate(input);
+      computeScoreAndUpdateCurrentSnapshot({
+        collectiviteId,
+        referentielId: getReferentielIdFromActionId(actionId),
+      });
       return trackEvent(
         Event.referentiels.submitValeursUtiliseesScoreIndicatif,
         variables
