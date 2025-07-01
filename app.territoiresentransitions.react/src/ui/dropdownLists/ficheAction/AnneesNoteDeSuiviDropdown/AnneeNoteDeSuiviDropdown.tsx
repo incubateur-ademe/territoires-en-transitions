50efd9e829bb7e4d485f0eca9c24ae205664a30a
Dimitri Valax <dvalax.pro@gmail.com>
2025-07-01 14:21:36 +0200

    feedback from Gael

--- app.territoiresentransitions.react/src/ui/dropdownLists/ficheAction/AnneesNoteDeSuiviDropdown/AnneeNoteDeSuiviDropdown.tsx
+++ app.territoiresentransitions.react/src/ui/dropdownLists/ficheAction/AnneesNoteDeSuiviDropdown/AnneeNoteDeSuiviDropdown.tsx
@@ -6,17 +6,15 @@ type Props = Omit<SelectProps, 'values' | 'onChange' | 'options'> & {
   onChange: (value: string[]) => void;
 };
 
-const AnneesNoteDeSuiviDropdown = ({ onChange, values }: Props) => {
+export const AnneesNoteDeSuiviDropdown = ({ onChange, values }: Props) => {
   return (
     <SelectFilter
       values={values ?? undefined}
       dataTest={'hasNoteDeSuivi'}
       options={getYearsOptions().yearsOptions}
-      onChange={({ values, selectedValue }) => {
+      onChange={({ values }) => {
         onChange(values as string[]);
       }}
     />
   );
 };
-
-export default AnneesNoteDeSuiviDropdown;
