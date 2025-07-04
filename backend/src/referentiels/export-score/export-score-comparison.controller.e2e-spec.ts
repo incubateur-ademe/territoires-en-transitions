ae64a1c05bdf883fd5a5371f4e27025854dc08bf
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-07-04 12:33:03 +0200

    test: adapt existing export score test to new routes

---
+++ backend/src/referentiels/export-score/export-score-comparison.controller.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { INestApplication } from '@nestjs/common';
+import { DateTime } from 'luxon';
+import { default as request } from 'supertest';
+import { getTestApp } from '../../../test/app-utils';
+
+describe('Referentiels scoring routes', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await getTestApp();
+  });
+
+  test(`Export du snapshot pour un utilisateur non authentifié`, async () => {
+    await request(app.getHttpServer())
+      .get(
+        `/collectivites/1/referentiels/eci/score-snapshots/export-comparison`
+      )
+      .query({ isAudit: 'false', snapshotReferences: ['score-courant'] })
+      .expect(401);
+  });
+
+  test(`Export du snapshot pour un utilisateur anonyme`, async () => {
+    const responseSnapshotExport = await request(app.getHttpServer())
+      .get(
+        `/collectivites/1/referentiels/eci/score-snapshots/export-comparison`
+      )
+      .set('Authorization', `Bearer ${process.env.SUPABASE_ANON_KEY}`)
+      .query({ isAudit: 'false', snapshotReferences: ['score-courant'] })
+      .expect(200)
+      .responseType('blob');
+
+    const currentDate = DateTime.now().toISODate();
+    const exportFileName = responseSnapshotExport.headers['content-disposition']
+      .split('filename=')[1]
+      .split(';')[0];
+
+    expect(exportFileName).toBe(
+      `"Export_ECI_Amberieu-en-Bugey_${currentDate}.xlsx"`
+    );
+    const expectedExportSize = 49.489;
+    const exportFileSize = parseInt(
+      responseSnapshotExport.headers['content-length']
+    );
+    expect(exportFileSize / 1000).toBeCloseTo(expectedExportSize, 0);
+  }, 30000);
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+});
