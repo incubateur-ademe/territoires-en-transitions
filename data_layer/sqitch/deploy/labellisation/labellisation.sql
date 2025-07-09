632344fc5fcdf48cc757fff1813e6b2b63cdcfbb
Marc Rutkowski <marc@attractive-media.fr>
2025-07-09 14:23:18 +0200

    Ajoute un trigger pour que la table public.labellisation soit à nouveau mise à jour après une labellisation

--- data_layer/sqitch/deploy/labellisation/labellisation.sql
+++ data_layer/sqitch/deploy/labellisation/labellisation.sql
@@ -1,30 +1,75 @@
 -- Deploy tet:labellisation/labellisation to pg
--- requires: referentiel/contenu
--- requires: collectivite/collectivite
--- requires: utils/auth
 
 BEGIN;
 
--- Enable access to `etoile_meta`.
--- Code taken from https://supabase.com/docs/guides/api/using-custom-schemas
+-- Met à jour la table public.labellisation après une labellisation
+create or replace function
+  labellisation.update_labellisation()
+    returns trigger
+    security definer
+as
+$$
+declare
+  demande_rec RECORD;
+  snapshot_rec RECORD;
+begin
+    -- charge la demande associée à l'audit
+    select into demande_rec *
+      from labellisation.demande ld
+      where ld.id = new.demande_id;
+    if not found then
+      raise log 'demande % not found', new.demande_id;
+      return new;
+    end if;
 
-GRANT USAGE ON SCHEMA labellisation TO anon, authenticated, service_role;
+    -- charge le snapshot associé à l'audit (il doit avoir été créé par
+    -- `ValidateAuditService.validateAudit`)
+    select into snapshot_rec *
+      from score_snapshot s
+      where s.audit_id = new.id and s.type_jalon = 'post_audit';
+    if not found then
+      raise log 'post_audit snapshot % not found', new.id;
+      return new;
+    end if;
 
-GRANT ALL ON ALL TABLES IN SCHEMA labellisation TO anon, authenticated, service_role;
-GRANT ALL ON ALL ROUTINES IN SCHEMA labellisation TO anon, authenticated, service_role;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA labellisation TO anon, authenticated, service_role;
+    -- si l'audit est dans le cadre d'une demande de labellisation
+    -- et que l'audit est clôturé, validé
+    -- et que la labellisation est actée avec une date valide
+    if (
+        demande_rec.sujet in ('labellisation', 'labellisation_cot')
+        and new.clos
+        and new.valide
+        and new.valide_labellisation
+        and new.date_cnl is not null
+      )
+    then -- alors on insère la ligne dans la table labellisation
+        insert into public.labellisation (
+          collectivite_id,
+          referentiel,
+          obtenue_le,
+          etoiles,
+          score_realise,
+          score_programme
+        ) values (
+          new.collectivite_id,
+          new.referentiel,
+          new.date_cnl,
+          cast(demande_rec.etoiles::text as integer),
+          round((snapshot_rec.point_fait / snapshot_rec.point_potentiel * 100)::numeric, 1),
+          round((snapshot_rec.point_programme / snapshot_rec.point_potentiel * 100)::numeric, 1)
+        )
+		on conflict do nothing;
+    end if;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA labellisation GRANT ALL ON TABLES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA labellisation GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA labellisation GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
+    return new;
+end ;
+$$ language plpgsql;
 
-
-ALTER TABLE labellisation.etoile_meta
-  ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY allow_read
-  ON labellisation.etoile_meta
-  FOR SELECT
-  USING (is_authenticated());
+-- ajoute le trigger pour mettre à jour la table après une labellisation validée
+create trigger update_labellisation_before_audit_update
+    before update
+    on labellisation.audit
+    for each row
+execute procedure labellisation.update_labellisation();
 
 COMMIT;
