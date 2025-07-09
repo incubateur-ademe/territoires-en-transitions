4e2bce4f0647cfdfee104c6d41f87b29e28718b0
Dimitri Valax <dimitri.valax@beta.gouv.fr>
2025-07-09 14:42:21 +0200

    add accueil link to 404 page

--- packages/site/app/not-found.tsx
+++ packages/site/app/not-found.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 import Section from '@/site/components/sections/Section';
+import { Button } from '@/ui';
 
-const NotFound = () => {
+export const NotFound = () => {
   return (
     <Section className="my-16">
       <div className="flex items-center justify-center divide-x divide-black">
         <h1 className="text-3xl font-normal pr-4">404</h1>
         <h2 className="text-lg font-normal pl-4">Cette page n'existe pas</h2>
       </div>
+      <div className="flex items-center justify-center">
+        <Button
+          variant="underlined"
+          href={'/'}
+          className="!text-grey-8 border-b-transparent hover:border-b-grey-8 hover:!border-b !font-normal"
+        >
+          Retourner à la page d'accueil
+        </Button>
+      </div>
     </Section>
   );
 };
-
-export default NotFound;
