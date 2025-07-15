42132a064c4f6ad4cad34c15c6089837edd00644
Marc Rutkowski <marc@attractive-media.fr>
2025-07-15 10:20:28 +0200

    Supprime les anciennes fonctions et triggers qui mettaient à jour la table labellisation

--- data_layer/sqitch/revert/labellisation/audit.sql
+++ data_layer/sqitch/revert/labellisation/audit.sql
@@ -1,163 +1,120 @@
 -- Deploy tet:labellisation/audit to pg
 BEGIN;
 
--- Drop the existing views
-drop view if exists retool_preuves;
-drop view if exists preuve;
-
--- Recreate the preuve view with updated action field access
-create or replace view preuve
-            (preuve_type, id, collectivite_id, fichier, lien, commentaire, created_at, created_by, created_by_nom,
-             action, preuve_reglementaire, demande, rapport, audit)
+create or replace function labellisation.update_labellisation_after_scores() returns trigger
+    language plpgsql
+    security definer
 as
-SELECT 'complementaire'::preuve_type               AS preuve_type,
-       pc.id,
-       pc.collectivite_id,
-       fs.snippet                                  AS fichier,
-       pc.lien,
-       pc.commentaire,
-       pc.modified_at                              AS created_at,
-       pc.modified_by                              AS created_by,
-       utilisateur.modified_by_nom(pc.modified_by) AS created_by_nom,
-       snippet.snippet                             AS action,
-       NULL::jsonb                                 AS preuve_reglementaire,
-       NULL::jsonb                                 AS demande,
-       NULL::jsonb                                 AS rapport,
-       NULL::jsonb                                 AS audit
-FROM preuve_complementaire pc
-LEFT JOIN labellisation.bibliotheque_fichier_snippet fs ON fs.id = pc.fichier_id
-LEFT JOIN labellisation.action_snippet snippet
-          ON snippet.action_id::text = pc.action_id::text AND snippet.collectivite_id = pc.collectivite_id
-WHERE can_read_acces_restreint(pc.collectivite_id)
-  AND (
-    fs.snippet is null
-        or (fs.snippet ->> 'confidentiel'::text)::boolean is false
-        or have_lecture_acces(pc.collectivite_id)
-        or private.est_auditeur(pc.collectivite_id)
-    )
-UNION ALL
-SELECT 'reglementaire'::preuve_type                AS preuve_type,
-       pr.id,
-       c.id                                        AS collectivite_id,
-       fs.snippet                                  AS fichier,
-       pr.lien,
-       pr.commentaire,
-       pr.modified_at                              AS created_at,
-       pr.modified_by                              AS created_by,
-       utilisateur.modified_by_nom(pr.modified_by) AS created_by_nom,
-       snippet.snippet                             AS action,
-       to_jsonb(prd.*)                             AS preuve_reglementaire,
-       NULL::jsonb                                 AS demande,
-       NULL::jsonb                                 AS rapport,
-       NULL::jsonb                                 AS audit
-FROM collectivite c
-LEFT JOIN preuve_reglementaire_definition prd ON true
-LEFT JOIN preuve_reglementaire pr ON prd.id::text = pr.preuve_id::text AND c.id = pr.collectivite_id
-LEFT JOIN preuve_action pa ON prd.id::text = pa.preuve_id::text
-LEFT JOIN labellisation.bibliotheque_fichier_snippet fs ON fs.id = pr.fichier_id
-LEFT JOIN labellisation.action_snippet snippet
-          ON snippet.action_id::text = pa.action_id::text AND snippet.collectivite_id = c.id
-WHERE can_read_acces_restreint(c.id)
-  AND (
-    fs.snippet is null
-        or (fs.snippet ->> 'confidentiel'::text)::boolean is false
-        or have_lecture_acces(c.id)
-        or private.est_auditeur(c.id)
-    )
-UNION ALL
-SELECT 'labellisation'::preuve_type               AS preuve_type,
-       p.id,
-       d.collectivite_id,
-       fs.snippet                                 AS fichier,
-       p.lien,
-       p.commentaire,
-       p.modified_at                              AS created_at,
-       p.modified_by                              AS created_by,
-       utilisateur.modified_by_nom(p.modified_by) AS created_by_nom,
-       NULL::jsonb                                AS action,
-       NULL::jsonb                                AS preuve_reglementaire,
-       to_jsonb(d.*)                              AS demande,
-       NULL::jsonb                                AS rapport,
-       NULL::jsonb                                AS audit
-FROM labellisation.demande d
-JOIN preuve_labellisation p ON p.demande_id = d.id
-LEFT JOIN labellisation.bibliotheque_fichier_snippet fs ON fs.id = p.fichier_id
-WHERE can_read_acces_restreint(d.collectivite_id)
-  AND (
-    fs.snippet is null
-        or (fs.snippet ->> 'confidentiel'::text)::boolean is false
-        or have_lecture_acces(d.collectivite_id)
-        or private.est_auditeur(d.collectivite_id)
-    )
-UNION ALL
-SELECT 'rapport'::preuve_type                     AS preuve_type,
-       p.id,
-       p.collectivite_id,
-       fs.snippet                                 AS fichier,
-       p.lien,
-       p.commentaire,
-       p.modified_at                              AS created_at,
-       p.modified_by                              AS created_by,
-       utilisateur.modified_by_nom(p.modified_by) AS created_by_nom,
-       NULL::jsonb                                AS action,
-       NULL::jsonb                                AS preuve_reglementaire,
-       NULL::jsonb                                AS demande,
-       to_jsonb(p.*)                              AS rapport,
-       NULL::jsonb                                AS audit
-FROM preuve_rapport p
-LEFT JOIN labellisation.bibliotheque_fichier_snippet fs ON fs.id = p.fichier_id
-WHERE can_read_acces_restreint(p.collectivite_id)
-  AND (
-    fs.snippet is null
-        or (fs.snippet ->> 'confidentiel'::text)::boolean is false
-        or have_lecture_acces(p.collectivite_id)
-        or private.est_auditeur(p.collectivite_id)
-    )
-UNION ALL
-SELECT 'audit'::preuve_type                       AS preuve_type,
-       p.id,
-       a.collectivite_id,
-       fs.snippet                                 AS fichier,
-       p.lien,
-       p.commentaire,
-       p.modified_at                              AS created_at,
-       p.modified_by                              AS created_by,
-       utilisateur.modified_by_nom(p.modified_by) AS created_by_nom,
-       NULL::jsonb                                AS action,
-       NULL::jsonb                                AS preuve_reglementaire,
-       CASE
-           WHEN d.id IS NOT NULL THEN to_jsonb(d.*)
-           ELSE NULL::jsonb
-       END                                        AS demande,
-       NULL::jsonb                                AS rapport,
-       to_jsonb(a.*)                              AS audit
-FROM audit a
-JOIN preuve_audit p ON p.audit_id = a.id
-LEFT JOIN labellisation.demande d ON a.demande_id = d.id
-LEFT JOIN labellisation.bibliotheque_fichier_snippet fs ON fs.id = p.fichier_id
-WHERE can_read_acces_restreint(a.collectivite_id)
-  AND (
-    fs.snippet is null
-        or (fs.snippet ->> 'confidentiel'::text)::boolean is false
-        or have_lecture_acces(a.collectivite_id)
-        or private.est_auditeur(a.collectivite_id)
+$$
+declare
+    etoile integer;
+begin
+    select etoiles::text::integer
+    from labellisation.demande d
+             join labellisation.audit a on d.id = a.demande_id
+    where a.id = new.audit_id
+    limit 1
+    into etoile;
+    if
+        -- Vérifie si la labellisation de l'audit a été validé
+        (select a.valide_labellisation from labellisation.audit a where a.id = new.audit_id) and
+            -- Vérifie si la labellisation n'existe pas déjà
+        (select count(*)=0
+         from labellisation l
+         where l.collectivite_id = new.collectivite_id
+           and l.referentiel = new.referentiel
+           and l.etoiles = etoile)
+    then
+        with
+            score AS (
+                SELECT Round((case
+                                  when (sc ->> 'point_potentiel'::text)::float= 0 then 0
+                                  else (sc ->> 'point_fait'::text)::float / (sc ->> 'point_potentiel'::text)::float
+                                  end * 100)::numeric, 1) as score_realise,
+                       Round((case
+                                  when (sc ->> 'point_potentiel'::text)::float = 0 then 0
+                                  else (sc ->> 'point_programme'::text)::float / (sc ->> 'point_potentiel'::text)::float
+                                  end * 100)::numeric, 1) as score_programme
+                FROM jsonb_array_elements(new.scores) sc
+                WHERE case when new.referentiel = 'eci'then
+                               sc @> '{"action_id": "eci"}'::jsonb
+                           else
+                               sc @> '{"action_id": "cae"}'::jsonb
+                          end
+            )
+        insert into labellisation (collectivite_id, referentiel, obtenue_le, etoiles, score_realise, score_programme)
+        select new.collectivite_id, new.referentiel, now(), etoile, score.score_realise, score.score_programme
+        from score
+        on conflict (collectivite_id, annee, referentiel) do update
+            set obtenue_le = excluded.obtenue_le,
+                etoiles = excluded.etoiles,
+                score_realise = excluded.score_realise,
+                score_programme = excluded.score_programme;
+
+    end if;
+    return new;
+end
+$$;
+comment on function labellisation.update_labellisation_after_scores is
+    'Ajoute automatiquement la labellisation si cette dernière a été validé et n''existe pas déjà';
+
+create trigger update_labellisation_after_scores
+    after insert or update
+    on post_audit_scores
+    for each row
+execute procedure labellisation.update_labellisation_after_scores();
+
+create or replace function labellisation.update_audit_score_on_personnalisation() returns trigger
+    language plpgsql
+as
+$$
+begin
+    perform (
+        with
+            ref as (
+                select unnest(enum_range(null::referentiel)) as referentiel
+            ),
+            audit as (
+                select ca.id
+                from ref
+                         join labellisation.current_audit(new.collectivite_id, ref.referentiel) ca on true
+            ),
+            query as (
+                select labellisation.evaluate_audit_statuts(audit.id, true, 'pre_audit_scores') as id
+                from audit
+            )
+        select count(query)
+        from query
     );
+    return new;
+exception
+    when others then return new;
+end
+$$;
 
--- Recreate the retool_preuves view
-create view retool_preuves
+create or replace function labellisation.update_audit_scores() returns trigger
+    language plpgsql
 as
- SELECT preuve.collectivite_id,
-    nc.nom,
-    preuve.action ->> 'referentiel'::text AS referentiel,
-    preuve.action ->> 'action_id'::text AS action,
-    preuve.preuve_type,
-    preuve.fichier ->> 'filename'::text AS fichier,
-    preuve.lien ->> 'url'::text AS lien,
-    preuve.created_at
-   FROM preuve
-     JOIN named_collectivite nc ON nc.collectivite_id = preuve.collectivite_id AND preuve.created_at IS NOT NULL
-  WHERE ( SELECT is_service_role() AS is_service_role)
-  ORDER BY preuve.collectivite_id, (preuve.action ->> 'referentiel'::text), (naturalsort(preuve.action ->> 'action_id'::text));
-  ;
+$$
+begin
+    perform labellisation.evaluate_audit_statuts(new.id, true, 'pre_audit_scores');
+    if new.date_fin is not null then
+        perform labellisation.evaluate_audit_statuts(new.id, false, 'post_audit_scores');
+    end if;
+    return new;
+end
+$$;
+
+create trigger after_write_update_audit_scores
+    after insert or update
+    on personnalisation_consequence
+    for each row
+execute procedure labellisation.update_audit_score_on_personnalisation();
+
+create trigger after_write_update_audit_scores
+    after insert or update
+    on labellisation.audit
+    for each row
+execute procedure labellisation.update_audit_scores();
 
 COMMIT;
