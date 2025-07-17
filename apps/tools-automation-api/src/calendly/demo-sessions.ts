cf9ff29effc566f06cab40bb90cec59cab3d8c5a
Marc Rutkowski <marc@attractive-media.fr>
2025-07-17 15:42:43 +0200

    Précise le typage des données à insérer dans Airtable
    et :
    - ajoute de manière plus lisible la valeur à insérer dans la colonne "Origine de l'échange" dans Airtable
    - améliore la vérification de ces données

--- apps/tools-automation-api/src/calendly/demo-sessions.ts
+++ apps/tools-automation-api/src/calendly/demo-sessions.ts
@@ -1,24 +1,52 @@
-// Correspondances entre les id calendly et la colonne "source" de la table
+// Correspondances entre les id Calendly et les données à insérer dans Airtable
 
-import { keyBy } from "es-toolkit";
+import { keyBy } from 'es-toolkit';
+import { z } from 'zod';
+
+export const demoSessionSchema = z.object({
+  slug: z.string().min(1).describe("Slug de l'événement Calendly"),
+  source: z
+    .string()
+    .min(1)
+    .describe('Valeur à insérer dans la colonne Source dans Airtable'),
+  origin: z
+    .string()
+    .min(1)
+    .describe(
+      `Valeur à insérer dans la colonne "Origine de l'échange" dans Airtable`
+    ),
+});
+
+type DemoSession = z.infer<typeof demoSessionSchema>;
 
 // Feedback dans Airtable
-export const DemoSessions = [
-  { slug: 'session-accueil', source: 'Démo Accueil' },
+export const DemoSessions: DemoSession[] = [
+  {
+    slug: 'session-accueil',
+    source: 'Démo Accueil',
+    origin: 'Activation continue',
+  },
   {
     slug: 'demo-fonctionnalite-plans-d-action',
     source: 'Démo Pilotage (Calendly)',
+    origin: 'Activation continue',
   },
   {
     slug: 'session-demo-nouvelles-fonctionnalites',
     source: 'Démo Nouvelles Fonctionnalités',
+    origin: 'Activation continue',
   },
   {
     slug: 'territoires-en-transitions-webinaire-de-presentation',
     source: 'Démo pilotage (collective)',
+    origin: 'Activation continue',
   },
-  { slug: 'entretien-support-plan-d-action', source: 'Support (Calendly)' },
+  {
+    slug: 'entretien-support-plan-d-action',
+    source: 'Support (Calendly)',
+    origin: 'Support et REX',
+  },
 ];
 
 export const DemoSessionSlugs = DemoSessions.map((s) => s.slug);
-export const DemoSessionBySlug = keyBy(DemoSessions, ds => ds.slug);
+export const DemoSessionBySlug = keyBy(DemoSessions, (ds) => ds.slug);
