26ca439783a9c3d3f88f2aa53b5cc7e8b5d5c8e6
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-07-18 18:15:27 +0200

    Supprime les warnings de build next dûs à la config Sentry dépréciée

--- app.territoiresentransitions.react/instrumentation-client.ts
+++ app.territoiresentransitions.react/instrumentation-client.ts
@@ -23,3 +23,7 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: true,
 });
+
+// This export will instrument router navigations, and is only relevant if you enable tracing.
+// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
