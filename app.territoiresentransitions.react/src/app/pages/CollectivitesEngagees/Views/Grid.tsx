bcb3e6b46799e16d6c7f3f793d0a000e413d3472
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-07-18 18:12:42 +0200

    Supprime les warnings de build next dûs au mauvais support de svgr par Nx

--- app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/Views/Grid.tsx
+++ app.territoiresentransitions.react/src/app/pages/CollectivitesEngagees/Views/Grid.tsx
@@ -1,6 +1,6 @@
 import { Data } from '@/app/app/pages/CollectivitesEngagees/Views/View';
 import { RecherchesViewParam } from '@/app/app/paths';
-import noResultIllustration from '@/app/app/static/img/no-results-astronaut-bro.svg';
+import noResultIllustration from '@/app/app/static/img/no-results-astronaut-bro.svg?url';
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import Image from 'next/image';
 
--- packages/auth/next.config.mjs
+++ packages/auth/next.config.mjs
@@ -4,11 +4,6 @@ import { composePlugins, withNx } from '@nx/next';
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
 
   typescript: {
     tsconfigPath: './tsconfig.json',
