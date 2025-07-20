ed8a0c98266056b558ad3277fef61f7780b8b6c5
Marc Rutkowski <marc@attractive-media.fr>
2025-07-20 12:10:12 +0200

    Répare la page "Accessibilité"
    en évitant d'utiliser le mdx

--- packages/site/app/accessibilite/AccessibiliteContent.tsx
+++ packages/site/app/accessibilite/AccessibiliteContent.tsx
@@ -1,13 +1,13 @@
 'use client';
 
+import Markdown from '@/site/components/markdown/Markdown';
 import Section from '@/site/components/sections/Section';
-import Content from './accessibilite.mdx';
 
-const AccessibiliteContent = () => {
+const AccessibiliteContent = ({ content }: { content: string }) => {
   return (
     <Section>
       <h1 className="fr-header__body">Déclaration d’accessibilité</h1>
-      <Content />
+      <Markdown texte={content} openLinksInNewTab />
     </Section>
   );
 };
--- packages/site/components/markdown/Markdown.tsx
+++ packages/site/components/markdown/Markdown.tsx
@@ -1,28 +1,77 @@
+// copie du composant du même nom dans l'app
+// TODO: à mutualiser dans le package ui ?
+
 import classNames from 'classnames';
-import MarkdownBase from 'react-markdown';
+import { ElementType } from 'react';
+import MarkdownBase, { Options } from 'react-markdown';
+import rehypeExternalLinks from 'rehype-external-links';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
-type MarkdownProps = {
-  texte: string;
-  className?: string;
-};
+// pour enlever les espaces en trop
+const RE_CLEANUP_EXTRA_SPACES = / +/gm;
+// pour enlever les sauts de ligne en trop entre les items listes
+const RE_CLEANUP_EXTRA_LINES = /^ {0,1}- (.*)$(?:\n ?){2,}/gm;
 
-const Markdown = ({ texte, className }: MarkdownProps) => (
-  <MarkdownBase
-    rehypePlugins={[rehypeRaw]}
-    remarkPlugins={[remarkGfm]}
-    className={classNames(
-      `
-      [&_ul]:list-disc [&_ul]:pl-6
-      [&_ol]:pl-6
-      [&>*:last-child]:mb-0
-      `,
-      className
-    )}
-  >
-    {texte}
-  </MarkdownBase>
-);
+function cleanupMarkdownContent(content: string) {
+  return content
+    ?.replaceAll(RE_CLEANUP_EXTRA_SPACES, ' ')
+    .replaceAll(RE_CLEANUP_EXTRA_LINES, ' - $1\n');
+}
+
+interface MarkdownProps<T extends ElementType> {
+  // container utilisé (par défaut `div`)
+  as?: T;
+  // contenu au format md
+  texte: string;
+  // classes supplémentaires appliquées au container
+  className?: string;
+  // options pour le composant react-markdown
+  options?: Options;
+  // force l'ouverture des liens dans un nouvel onglet
+  openLinksInNewTab?: boolean;
+}
+
+const Markdown = <T extends ElementType = 'div'>({
+  as,
+  texte: content,
+  className,
+  options,
+  openLinksInNewTab,
+}: MarkdownProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof MarkdownProps<T>>) => {
+  const Wrapper = as || 'div';
+
+  const rehypePlugins: Options['rehypePlugins'] = [rehypeRaw];
+  if (openLinksInNewTab) {
+    rehypePlugins.push([
+      // @ts-expect-error rehypeExternalLinks génère une erreur de typage mais c'est bien ok
+      rehypeExternalLinks,
+      { rel: ['noreferrer', 'noopener'], target: '_blank' },
+    ]);
+  }
+
+  return (
+    <Wrapper
+      className={classNames(
+        `
+    [&_ul]:list-disc [&_ul]:list-inside [&_ul]:pl-4
+    [&_ol]:list-inside [&_ol]:pl-4
+    [&>*:last-child]:mb-0
+    [&_li_p]:inline
+    `,
+        className
+      )}
+    >
+      <MarkdownBase
+        rehypePlugins={rehypePlugins}
+        remarkPlugins={[remarkGfm]}
+        {...options}
+      >
+        {cleanupMarkdownContent(content)}
+      </MarkdownBase>
+    </Wrapper>
+  );
+};
 
 export default Markdown;
