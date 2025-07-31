6aabcdb3fb40060d774926d4434ca0dd27ec2171
Gaël Servaud <servauga@gmail.com>
2025-07-31 17:20:30 +0200

    Bring back "import plan button" for support team

--- app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/plans/(creation)/importer/page.tsx
+++ app.territoiresentransitions.react/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/plans/(creation)/importer/page.tsx
@@ -1,5 +1,18 @@
-import { PlanImportView } from '@/app/plans/plans/import-plan/import-plan.view';
+import { RequestPlanImportView } from '@/app/plans/plans/import-plan/request-plan-import-view';
+import { notFound } from 'next/navigation';
+import { z } from 'zod';
 
-export default function ImporterPlanPage() {
-  return <PlanImportView />;
+export default async function RequestPlanImportPage({
+  params,
+}: {
+  params: Promise<{ collectiviteId: string }>;
+}) {
+  const rawParams = await params;
+  const collectiviteId = z.number().safeParse(rawParams.collectiviteId);
+
+  if (!collectiviteId.success) {
+    return notFound();
+  }
+
+  return <RequestPlanImportView />;
 }
--- app.territoiresentransitions.react/src/plans/plans/list-all-plans/all-plans.view.tsx
+++ app.territoiresentransitions.react/src/plans/plans/list-all-plans/all-plans.view.tsx
@@ -1,4 +1,5 @@
 import { CreatePlanButton } from '@/app/plans/plans/create-plan/components/create-plan.button';
+import { ImportPlanButton } from '@/app/plans/plans/import-plan/support';
 import { EmptyAllPlansView } from '@/app/plans/plans/list-all-plans/empty-all-plans.view';
 import { Plan } from '@/domain/plans/plans';
 import { Spacer } from '@/ui';
@@ -18,14 +19,17 @@ export const AllPlansView = ({ plans, collectiviteId, panierId }: Props) => {
       <Header
         title="Tous les plans"
         actionButtons={
-          <VisibleWhen condition={plans.length !== 0}>
-            <CreatePlanButton
-              collectiviteId={collectiviteId}
-              panierId={panierId}
-            >
-              {"Créer un plan d'action"}
-            </CreatePlanButton>
-          </VisibleWhen>
+          <div className="flex gap-2">
+            <VisibleWhen condition={plans.length !== 0}>
+              <CreatePlanButton
+                collectiviteId={collectiviteId}
+                panierId={panierId}
+              >
+                {"Créer un plan d'action"}
+              </CreatePlanButton>
+            </VisibleWhen>
+            <ImportPlanButton collectiviteId={collectiviteId} />
+          </div>
         }
       />
       <VisibleWhen condition={plans.length === 0}>
