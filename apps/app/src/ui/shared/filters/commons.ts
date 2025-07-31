e719a8baf39b742d320f79ab0be42e7cfc149a43
Arthur Molinos <arthur.molinos@gmail.com>
2025-07-31 11:56:13 +0200

    Supprime le dossier /shared/filters

--- apps/app/src/ui/shared/filters/commons.ts
+++
@@ -1,6 +0,0 @@
-/** constante pour gérer la sélection de tous les filtres */
-export const ITEM_ALL = 'tous';
-
-/** vérifie si ITEM_ALL est présent dans la liste de valeurs */
-export const getIsAllSelected = (values: string[]) =>
-  !values.length || values.includes(ITEM_ALL);
