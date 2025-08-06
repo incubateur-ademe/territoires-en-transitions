2a8325bf29b561ffa60a0f714c4f35b55071ef7d
Gaël Servaud <servauga@gmail.com>
2025-08-06 19:09:25 +0200

    improve plan filtering

--- apps/app/src/plans/plans/show-plan/index.tsx
+++ apps/app/src/plans/plans/show-plan/index.tsx
@@ -1,5 +1,4 @@
 import { CurrentCollectivite } from '@/api/collectivites/fetch-current-collectivite';
-import { makeCollectivitePlanActionUrl } from '@/app/app/paths';
 import { Plan as DetailedPlanType } from '@/domain/plans/plans';
 import { PlanFiltersProvider } from './filters/plan-filters.context';
 import { PlanView } from './plan.view';
@@ -10,14 +9,8 @@ type PlanActionProps = {
 };
 
 export const Plan = (props: PlanActionProps) => {
-  const { collectiviteId } = props.currentCollectivite;
-  const url = makeCollectivitePlanActionUrl({
-    collectiviteId,
-    planActionUid: props.plan.id.toString(),
-  });
   return (
     <PlanFiltersProvider
-      url={url}
       collectivite={props.currentCollectivite}
       plan={props.plan}
     >
