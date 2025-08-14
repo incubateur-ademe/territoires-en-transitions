996f5a6cc908869f662098f0f30256005fe7f21d
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-08-14 17:11:35 +0200

    fix: update Tailwind configuration to include .tsx

--- apps/panier/tailwind.config.ts
+++ apps/panier/tailwind.config.ts
@@ -7,7 +7,7 @@ import { preset } from '../../packages/ui/src/tailwind-preset';
 export default {
   presets: [preset as Config],
   content: [
-    '../../packages/ui/src/**/*.{js,ts,jsx,ts}',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
     './app/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
