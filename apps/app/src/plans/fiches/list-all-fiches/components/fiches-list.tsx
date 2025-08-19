cc58787242fa46f1ca512a30ae2581bf9ee4b048
Arthur Molinos <arthur.molinos@gmail.com>
2025-08-19 11:16:54 +0200

    Sépare un composant pour facilité la lecture

--- apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.tsx
+++ apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.tsx
@@ -1,7 +1,5 @@
 import ActionsGroupeesMenu from '@/app/app/pages/collectivite/PlansActions/ActionsGroupees/ActionsGroupeesMenu';
 import FicheActionCard from '@/app/app/pages/collectivite/PlansActions/FicheAction/Carte/FicheActionCard';
-import EmptyFichePicto from '@/app/app/pages/collectivite/PlansActions/FicheAction/FichesLiees/EmptyFichePicto';
-import { useCreateFicheAction } from '@/app/app/pages/collectivite/PlansActions/FicheAction/data/useCreateFicheAction';
 import {
   makeCollectiviteFicheNonClasseeUrl,
   makeCollectivitePlanActionFicheUrl,
@@ -10,7 +8,7 @@ import { FiltersMenuButton } from '@/app/plans/fiches/list-all-fiches/filters/fi
 import { CustomFilterBadges } from '@/app/ui/lists/filter-badges/use-filters-to-badges';
 import PictoExpert from '@/app/ui/pictogrammes/PictoExpert';
 
-import { useCreatePlan } from '@/app/plans/plans/show-plan/data/use-create-plan';
+import { FichesListEmpty } from '@/app/plans/fiches/list-all-fiches/components/fiches-list.empty';
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { ListFichesSortValue } from '@/domain/plans/fiches';
 import {
@@ -35,46 +33,6 @@ import {
   useGetFiches,
 } from '../hooks';
 import { FilterBadges } from './filter-badges';
-
-const EmptyState = ({
-  isReadOnly,
-  collectiviteId,
-}: {
-  isReadOnly: boolean;
-  collectiviteId: number;
-}) => {
-  const { mutate: createFicheAction } = useCreateFicheAction();
-  const { mutate: createPlan } = useCreatePlan({
-    collectiviteId,
-  });
-
-  return (
-    <div className="col-span-full flex flex-col items-center p-12 text-center bg-primary-0 border border-primary-4 rounded-xl">
-      <EmptyCard
-        picto={(props) => <EmptyFichePicto {...props} />}
-        title="Vous n'avez pas encore créé de fiche action !"
-        subTitle="Une fois vos fiches action créées, vous les retrouvez toutes dans cette vue où vous pourrez les filtrer sur de nombreux critères."
-        isReadonly={isReadOnly}
-        actions={[
-          {
-            children: "Créer un plan d'action",
-            onClick: () =>
-              createPlan({
-                collectiviteId,
-                nom: 'Sans titre',
-              }),
-            variant: 'outlined',
-          },
-          {
-            children: 'Créer une fiche action',
-            onClick: () => createFicheAction(),
-          },
-        ]}
-        variant="transparent"
-      />
-    </div>
-  );
-};
 
 type Props = {
   filters: FormFilters;
@@ -143,7 +101,7 @@ export const FichesList = ({
   }
   if (hasFiches === false) {
     return (
-      <EmptyState
+      <FichesListEmpty
         isReadOnly={isReadOnly ?? false}
         collectiviteId={collectivite.collectiviteId}
       />
