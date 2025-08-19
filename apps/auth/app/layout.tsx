f87b97280b4a14e469b0ecb0f60619db0963c358
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-08-19 12:19:38 +0200

    fix(env): add NEXT_PUBLIC_APP_URL et APP_URL vars to auth CI/CD, use useUser hook in auth to check if user is verified

--- apps/auth/app/layout.tsx
+++ apps/auth/app/layout.tsx
@@ -1,4 +1,6 @@
+import { fetchUserDetails } from '@/api/users/user-details.fetch.server';
 import { UserProvider } from '@/api/users/user-provider';
+import { getAuthUser } from '@/api/utils/supabase/auth-user.server';
 import { getCookieOptions } from '@/api/utils/supabase/cookie-options';
 import { SupabaseProvider } from '@/api/utils/supabase/use-supabase';
 import { ReactQueryAndTRPCProvider } from '@/api/utils/trpc/client';
@@ -39,6 +41,10 @@ export default async function RootLayout({
   const hostname = (await headers()).get('host');
   const supabaseCookieOptions = getCookieOptions(hostname ?? undefined);
 
+  const authUser = await getAuthUser();
+
+  const user = authUser ? await fetchUserDetails(authUser) : null;
+
   return (
     <html lang="fr">
       <PostHogProvider
@@ -50,7 +56,7 @@ export default async function RootLayout({
         <body className="min-h-screen overflow-x-visible flex flex-col">
           <div className="flex flex-col grow">
             <SupabaseProvider cookieOptions={supabaseCookieOptions}>
-              <UserProvider user={null}>
+              <UserProvider user={user}>
                 <ReactQueryAndTRPCProvider>
                   <Header />
                   <div className="bg-grey-2 grow flex flex-col">
