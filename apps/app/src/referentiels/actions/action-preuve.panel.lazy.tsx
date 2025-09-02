2f86bc1a04e34ed946660971951c715eeecf5f0f
Arthur Molinos <arthur.molinos@gmail.com>
2025-09-02 17:50:58 +0200

    Supprime le fichier loading à la base de l'app ainsi que la fonction renderLoader

--- apps/app/src/referentiels/actions/action-preuve.panel.lazy.tsx
+++ apps/app/src/referentiels/actions/action-preuve.panel.lazy.tsx
@@ -1,12 +1,18 @@
+import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { lazy } from '@/app/utils/lazy';
-import { renderLoader } from '@/app/utils/renderLoader';
 import { Suspense } from 'react';
 import { TActionPreuvePanelProps } from './action-preuve.panel';
 
 const Panel = lazy(() => import('./action-preuve.panel'));
 
 export const ActionPreuvePanel = (props: TActionPreuvePanelProps) => (
-  <Suspense fallback={renderLoader()}>
+  <Suspense
+    fallback={
+      <div className="flex flex-col p-8">
+        <SpinnerLoader containerClassName="m-auto" />
+      </div>
+    }
+  >
     <Panel {...props} />
   </Suspense>
 );
