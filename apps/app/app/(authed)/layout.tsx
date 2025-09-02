646c27a51364bf3e16b8f51eb20e5df1199f0d61
Arthur Molinos <arthur.molinos@gmail.com>
2025-09-02 17:50:58 +0200

    Supprime le fichier loading à la base de l'app ainsi que la fonction renderLoader

--- apps/app/app/(authed)/layout.tsx
+++ apps/app/app/(authed)/layout.tsx
@@ -1,7 +1,6 @@
 import { getUser } from '@/api/users/user-details.fetch.server';
 import Header from '@/app/app/Layout/Header';
-import { renderLoader } from '@/app/utils/renderLoader';
-import { ReactNode, Suspense } from 'react';
+import { ReactNode } from 'react';
 import AppProviders from './app-providers';
 
 export default async function Layout({ children }: { children: ReactNode }) {
@@ -10,7 +9,6 @@ export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <AppProviders user={user}>
       <Header />
-      <Suspense fallback={renderLoader()}>{children}</Suspense>
     </AppProviders>
   );
 }
