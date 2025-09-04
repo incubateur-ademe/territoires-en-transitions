19be831caf9e8c56fecd5ca622bee162dd622feb
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-09-04 16:31:09 +0200

    Corrige le déclenchement du calcul de trajectoire

--- apps/app/src/app/pages/collectivite/Trajectoire/useCalculTrajectoire.ts
+++ apps/app/src/app/pages/collectivite/Trajectoire/useCalculTrajectoire.ts
@@ -1,11 +1,11 @@
 import { useCurrentCollectivite } from '@/api/collectivites';
 import { useTRPC } from '@/api/utils/trpc/client';
 import {
-  CalculTrajectoireReset,
+  CalculTrajectoireResultatMode,
   IndicateurAvecValeurs,
 } from '@/domain/indicateurs';
 import { Event, useEventTracker } from '@/ui';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export type ResultatTrajectoire = {
   indentifiantsReferentielManquantsDonneesEntree: string[];
@@ -16,65 +16,47 @@ export type ResultatTrajectoire = {
 };
 
 /** Charge la trajectoire */
-export const useGetTrajectoire = () => {
-  const { collectiviteId } = useCurrentCollectivite();
-  const trpc = useTRPC();
-
-  return useQuery(
-    trpc.indicateurs.trajectoires.snbc.getOrCompute.queryOptions(
-      {
-        collectiviteId,
-      },
-      {
-        retry: false,
-        refetchOnMount: false,
-      }
-    )
-  );
-};
-
-/** Déclenche le calcul de la trajectoire */
-export const useCalculTrajectoire = ({
-  nouveauCalcul,
+export const useGetTrajectoire = ({
   enabled = true,
 }: {
-  nouveauCalcul?: boolean;
   enabled?: boolean;
 } = {}) => {
   const { collectiviteId } = useCurrentCollectivite();
-  const queryClient = useQueryClient();
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
   const trackEvent = useEventTracker();
 
   return useQuery(
     trpc.indicateurs.trajectoires.snbc.getOrCompute.queryOptions(
       {
         collectiviteId,
-        ...(nouveauCalcul
-          ? {
-              mode: CalculTrajectoireReset.MAJ_SPREADSHEET_EXISTANT,
-              forceUtilisationDonneesCollectivite: true,
-            }
-          : {}),
       },
       {
         enabled,
         retry: false,
+        refetchOnMount: false,
+
         // Utilise le `select` comme un `onSuccess` pour mettre à jour les caches de valeurs des indicateurs.
         // Utile dans le cas de l'affichage de la trajectoire sur le graphe d'un indicateur concerné (cae_1.a, cae_2.a)
         // et son sélecteur de sources.
         select: (data) => {
-          queryClient.invalidateQueries({
-            queryKey: trpc.indicateurs.valeurs.list.queryKey({
-              collectiviteId,
-            }),
-          });
+          const nouveauCalcul =
+            data.mode ===
+            CalculTrajectoireResultatMode.MAJ_SPREADSHEET_EXISTANT;
 
-          queryClient.invalidateQueries({
-            queryKey: trpc.indicateurs.sources.available.queryKey({
-              collectiviteId,
-            }),
-          });
+          if (nouveauCalcul) {
+            queryClient.invalidateQueries({
+              queryKey: trpc.indicateurs.valeurs.list.queryKey({
+                collectiviteId,
+              }),
+            });
+
+            queryClient.invalidateQueries({
+              queryKey: trpc.indicateurs.sources.available.queryKey({
+                collectiviteId,
+              }),
+            });
+          }
 
           trackEvent(Event.trajectoire.triggerSnbcCalculation, {
             source: nouveauCalcul ? 'collectivite' : 'open_data',
@@ -84,5 +66,30 @@ export const useCalculTrajectoire = ({
         },
       }
     )
+  );
+};
+
+/** Déclenche le calcul de la trajectoire */
+export const useComputeTrajectoire = ({
+  onSuccess,
+}: {
+  onSuccess?: () => void;
+} = {}) => {
+  const { collectiviteId } = useCurrentCollectivite();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.indicateurs.trajectoires.snbc.compute.mutationOptions({
+      onSuccess: async (data) => {
+        await queryClient.setQueryData(
+          trpc.indicateurs.trajectoires.snbc.getOrCompute.queryKey({
+            collectiviteId,
+          }),
+          data
+        );
+        onSuccess?.();
+      },
+    })
   );
 };
