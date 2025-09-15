36137f53b9c5f0f81879e6eaa495d74135e1ae1e
Marc Rutkowski <marc@attractive-media.fr>
2025-09-15 12:03:38 +0200

    Ajoute un utilitaire pour transformer les contenus HTML en texte

---
+++ apps/backend/src/utils/html-to-text.utils.ts
@@ -0,0 +1,8 @@
+import { htmlToText as htmlToTextBase } from 'html-to-text';
+
+export function htmlToText(
+  html: string,
+  options: { wordwrap: number | false } = { wordwrap: false }
+) {
+  return htmlToTextBase(html, options);
+}
