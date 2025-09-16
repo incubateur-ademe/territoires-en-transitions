23bec9541496600b42283e166e6229a45db49742
Arthur Molinos <arthur.molinos@gmail.com>
2025-09-16 10:13:41 +0200

    Sort une fonction d'un composant pour lisibilité

--- apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.scheduler/fiche-list.scheduler.tsx
+++ apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.scheduler/fiche-list.scheduler.tsx
@@ -1,8 +1,6 @@
-import { DateHelper } from '@bryntum/scheduler';
-
-import { generateTitle } from '@/app/app/pages/collectivite/PlansActions/FicheAction/data/utils';
 import { ListFicheResumesOutput } from '@/app/plans/fiches/_data/types';
 import { Scheduler } from '@/app/plans/fiches/list-all-fiches/components/fiches-list.scheduler/bryntum-scheduler/scheduler';
+import { toSchedulerEvent } from '@/app/plans/fiches/list-all-fiches/components/fiches-list.scheduler/bryntum-scheduler/utils';
 import { Alert, Badge, ButtonMenu } from '@/ui';
 import { cn } from '@/ui/utils/cn';
 
@@ -11,28 +9,12 @@ type Props = {
   isLoading: boolean;
 };
 
-export const FicheListScheduler = ({ fiches, isLoading }: Props) => {
-  // On transforme les fiches en events pour le scheduler
-  const events = fiches
-    .map((fiche) => {
-      const startDate = fiche.dateDebut ? new Date(fiche.dateDebut) : null;
-      const endDate = fiche.dateFin ? new Date(fiche.dateFin) : null;
+export const FicheListScheduler = ({
+  fiches,
+  isLoading,
+}: Props) => {
+  const events = toSchedulerEvent(fiches);
 
-      if (!startDate && !endDate) return null;
-
-      const start = startDate || new Date(endDate!.getTime() - 86400000); // -1 jour
-      const end = endDate || new Date(startDate!.getTime() + 86400000); // +1 jour
-
-      return {
-        id: fiche.id,
-        resourceId: fiche.id,
-        name: generateTitle(fiche.titre),
-        fiche,
-        startDate: DateHelper.format(start),
-        endDate: DateHelper.format(end),
-      };
-    })
-    .filter((e) => !!e);
 
   const height = 800;
 
