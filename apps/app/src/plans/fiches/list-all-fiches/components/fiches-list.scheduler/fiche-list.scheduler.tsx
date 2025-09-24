02541439553002a32728446e1c9b64c2b97fc390
Arthur Molinos <arthur.molinos@gmail.com>
2025-09-24 18:24:58 +0200

    Divise un composant

--- apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.scheduler/fiche-list.scheduler.tsx
+++ apps/app/src/plans/fiches/list-all-fiches/components/fiches-list.scheduler/fiche-list.scheduler.tsx
@@ -32,43 +32,46 @@ export const FicheListScheduler = ({
       className="relative flex flex-col"
       style={{ height, minHeight: '80vh' }}
     >
-      <ButtonMenu
-        className={cn('!absolute top-20 right-4 z-[1]')}
-        variant="outlined"
-        size="xs"
-        menuPlacement="bottom-end"
-        openOnHover
-        icon="information-line"
-        text="Aide"
-      >
-        <Alert
-          rounded
-          description={
-            <div className="flex flex-col gap-3 mt-0.5 pr-8">
-              <p className="mb-0 text-sm">
-                Les fiches n&apos;ayant ni date de début, ni date de fin ne sont
-                pas affichées
-              </p>
-              <div className="h-px bg-primary-3" />
-              <p className="mb-0 font-bold text-info-1">
-                Raccourcis pour utilisation à la souris
-              </p>
-              <ul className="mb-0 text-sm [&>li]:flex [&>li]:items-center [&>li]:gap-1 [&>li]:pb-3">
-                <li>
-                  Zoomer ou dé-zoomer → <Badge title="ctrl" size="sm" /> +
-                  <Badge title="scroll" size="sm" />
-                </li>
-                <li>
-                  Se déplacer dans le temps → <Badge title="shift" size="sm" />{' '}
-                  +
-                  <Badge title="scroll" size="sm" />
-                </li>
-              </ul>
-            </div>
-          }
-        />
-      </ButtonMenu>
+      <MenuAide />
       <Scheduler events={events} isLoading={isLoading} />
     </div>
   );
 };
+
+const MenuAide = () => (
+  <ButtonMenu
+    className={cn('absolute top-20 right-4 z-[1]')}
+    variant="outlined"
+    size="xs"
+    menuPlacement="bottom-end"
+    openOnHover
+    icon="information-line"
+    text="Aide"
+  >
+    <Alert
+      rounded
+      description={
+        <div className="flex flex-col gap-3 mt-0.5 pr-8">
+          <p className="mb-0 text-sm">
+            Les fiches n&apos;ayant ni date de début, ni date de fin ne sont pas
+            affichées
+          </p>
+          <div className="h-px bg-primary-3" />
+          <p className="mb-0 font-bold text-info-1">
+            Raccourcis pour utilisation à la souris
+          </p>
+          <ul className="mb-0 text-sm [&>li]:flex [&>li]:items-center [&>li]:gap-1 [&>li]:pb-3">
+            <li>
+              Zoomer ou dé-zoomer → <Badge title="ctrl" size="sm" /> +
+              <Badge title="scroll" size="sm" />
+            </li>
+            <li>
+              Se déplacer dans le temps → <Badge title="shift" size="sm" /> +
+              <Badge title="scroll" size="sm" />
+            </li>
+          </ul>
+        </div>
+      }
+    />
+  </ButtonMenu>
+);
