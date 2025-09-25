3bd3494e10d33cdf96c7f5cc88245c8bc8652083
Marc Rutkowski <marc@attractive-media.fr>
2025-09-25 11:35:07 +0200

    Evite la duplication de la nouvelle entrée dans le journal d'activité
    quand l'éditeur de la précision d'avancement dans l'EDL est différent de l'utilisateur ayant fait la dernière modification

--- data_layer/sqitch/revert/referentiel/action_commentaire.sql
+++ data_layer/sqitch/revert/referentiel/action_commentaire.sql
@@ -1,7 +1,9 @@
--- Revert tet:referentiel/action_commentaire from pg
+-- Deploy tet:referentiel/action_commentaire to pg
+-- requires: referentiel/contenu
 
 BEGIN;
 
-drop table action_commentaire;
+drop trigger set_modified_by_before_action_commentaire_update
+  on public.action_commentaire;
 
 COMMIT;
