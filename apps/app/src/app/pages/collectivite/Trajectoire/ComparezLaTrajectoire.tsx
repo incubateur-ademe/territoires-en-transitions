52d9c046f7ba8261e18a09d68cc76108db545486
Gaël Servaud <servauga@gmail.com>
2025-10-07 16:48:03 +0200

    Diminution de la taille du bandeau "Compare le trajectoire SNBC à vos ..."

--- apps/app/src/app/pages/collectivite/Trajectoire/ComparezLaTrajectoire.tsx
+++ apps/app/src/app/pages/collectivite/Trajectoire/ComparezLaTrajectoire.tsx
@@ -18,8 +18,8 @@ export const ComparezLaTrajectoire = ({
       <h3 className="text-lg mb-0">
         Comparez la trajectoire SNBC à vos objectifs et vos résultats
       </h3>
-      <div className="flex justify-between">
-        <p className="text-sm font-normal w-3/4">
+      <div className="flex justify-between flex-col lg:flex-row lg:items-center">
+        <p className="text-sm font-normal w-full lg:w-3/4 lg:mb-0 mb-2">
           Pour cela, il faut d&apos;abord{' '}
           {readonly ? (
             <>
@@ -41,7 +41,7 @@ export const ComparezLaTrajectoire = ({
             </>
           )}
         </p>
-        <div>
+        <div className="w-full lg:w-1/4 flex justify-end lg:items-center">
           <Button
             disabled={readonly}
             href={makeCollectiviteIndicateursUrl({
