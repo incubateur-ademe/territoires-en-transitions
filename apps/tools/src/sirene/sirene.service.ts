a5da2af4fe623f72b279c7f2d6a4825d0bf6cdb9
Marc Rutkowski <marc@attractive-media.fr>
2025-10-07 15:59:40 +0200

    Répare l’accès à l’API Sirene
    Il n'est plus nécessaire de créer un jeton d'accès intermédiaire, l'authent. se faisant maintenant directement à partir de l'API key générée.
    
    Ref: https://portail-api.insee.fr/catalog/api/2ba0e549-5587-3ef1-9082-99cd865de66f/doc?page=85c5657d-b1a1-4466-8565-7db1a194667b

--- apps/tools/src/sirene/sirene.service.ts
+++ apps/tools/src/sirene/sirene.service.ts
@@ -1,17 +1,8 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { chunk, flatten } from 'es-toolkit';
-import { objectToCamel } from 'ts-case-convert';
 import { ZodSchema } from 'zod';
 import ConfigurationService from '../config/configuration.service';
-import {
-  GetNICResponse,
-  getNICResponseSchema,
-  getTokenResponseSchema,
-} from './sirene.model';
+import { GetNICResponse, getNICResponseSchema } from './sirene.model';
 
 // l'API autorise au maximum 1000 éléments par réponse
 const MAX_RESULTS = 1000;
@@ -19,73 +10,29 @@ const MAX_RESULTS = 1000;
 @Injectable()
 export class SireneService {
   private logger = new Logger(SireneService.name);
-  private accessToken: string | undefined = undefined;
-  private expiresAt: number | undefined = undefined;
 
   constructor(private readonly configurationService: ConfigurationService) {}
 
-  /** Récupère/rafraichit le jeton d'accès */
-  private async getAccessToken(refresh = false) {
-    // renvoi le jeton courant si il est encore valide et que le
-    // rafraichissement n'est pas forcé
-    const expired = Date.now() >= (this.expiresAt ?? 0);
-    if (this.accessToken && !expired && !refresh) {
-      this.logger.log(
-        `Jeton accès SIRENE valide jusqu'à ${new Date(
-          this.expiresAt as number
-        ).toISOString()}`
-      );
-      return this.accessToken;
-    }
-
-    // demande le jeton
-    const options ={
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        authorization: `Basic ${this.configurationService.get(
-          'SIRENE_API_KEY'
-        )}`,
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({ grant_type: 'client_credentials' }).toString(),
-    }
-    const response = await fetch(this.configurationService.get('SIRENE_AUTH_URL'), options);
-
-    if (!response.ok) {
-      throw new InternalServerErrorException(
-        `Erreur de création du jeton SIRENE (${response.status}: ${response.statusText})\n${JSON.stringify(options)}`
-      );
-    }
-
-    const content = await response.json();
-    const result = getTokenResponseSchema.parse(content);
-    const { accessToken, expiresIn } = objectToCamel(result);
-
-    this.accessToken = accessToken;
-    this.expiresAt = Date.now() + expiresIn * 1000;
-    this.logger.log(
-      `Nouveau jeton d'accès SIRENE, valide jusqu'à ${new Date(
-        this.expiresAt as number
-      ).toISOString()}`
-    );
-
-    return this.accessToken;
-  }
-
   /** Fait un appel POST à l'API */
-  private async post<T>(endpoint: string, body: Record<string, string>, schema: ZodSchema) {
-    const token = await this.getAccessToken();
-    const response = await fetch(`${this.configurationService.get('SIRENE_API_URL')}/${endpoint}`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        accept: 'application/json',
-        'accept-encoding': 'gzip',
-        authorization: `Bearer ${token}`,
-      },
-      body: new URLSearchParams(body).toString(),
-    });
+  private async post<T>(
+    endpoint: string,
+    body: Record<string, string>,
+    schema: ZodSchema
+  ) {
+    const response = await fetch(
+      `${this.configurationService.get('SIRENE_API_URL')}/${endpoint}`,
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          accept: 'application/json',
+          'accept-encoding': 'gzip',
+          'X-INSEE-Api-Key-Integration':
+            this.configurationService.get('SIRENE_API_KEY'),
+        },
+        body: new URLSearchParams(body).toString(),
+      }
+    );
 
     if (!response.ok) {
       this.logger.log(
