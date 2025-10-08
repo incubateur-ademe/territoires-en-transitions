efbd4a5340548348f0fca0b47549d0bd48a0408a
Marc Rutkowski <marc@attractive-media.fr>
2025-10-08 15:59:29 +0200

    Essaye d'éviter les blocages de posthog
    Utilise un segment d'URL non standard pour le proxy next vers posthog pour éviter les blocages
    Ref: https://posthog.com/docs/advanced/proxy/nextjs

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -16,11 +16,10 @@ const nextConfig = {
   },
 
   webpack(config) {
-  
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    )
+      rule.test?.test?.('.svg')
+    );
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
@@ -35,13 +34,13 @@ const nextConfig = {
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ['@svgr/webpack'],
-      },
-    )
+      }
+    );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
+    fileLoaderRule.exclude = /\.svg$/i;
 
-    return config
+    return config;
   },
 
   typescript: {
@@ -61,16 +60,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
+        source: '/phtr/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
       },
       {
-        source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://eu.i.posthog.com/decide",
+        source: '/phtr/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
       },
     ];
   },
@@ -121,9 +116,7 @@ const sentryConfig = {
   automaticVercelMonitors: true,
 };
 
-const plugins = [
-
-];
+const plugins = [];
 
 export default withSentryConfig(
   composePlugins(...plugins)(nextConfig),
