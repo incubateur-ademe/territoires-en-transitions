816ac7942543c7cb894e3ecd6446e792c19f3139
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-10-14 15:49:06 +0200

    Redirect to last selected collectivite when logging in

--- apps/app/app/(authed)/collectivite/tableau-de-bord/page.tsx
+++ apps/app/app/(authed)/collectivite/tableau-de-bord/page.tsx
@@ -5,12 +5,12 @@ import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function RedirectToPlansPage() {
+export default function RedirectToTdbPage() {
   const currentCollectiviteId = useCollectiviteId();
   const router = useRouter();
 
   useEffect(() => {
-    router.replace(`/collectivite/${currentCollectiviteId}/plans`);
+    router.replace(`/collectivite/${currentCollectiviteId}/tableau-de-bord`);
   }, [currentCollectiviteId, router]);
 
   return <SpinnerLoader />;
