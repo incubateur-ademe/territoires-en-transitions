7b4314eb0e5499697584ee607e044106055ee691
Dimitri Valax <dimitri.valax@beta.gouv.fr>
2025-10-14 16:33:05 +0200

    rename commentaire table to discussion + add discussionsCount and preuvesCount to list-actions service

--- apps/backend/src/referentiels/list-actions/list-actions.service.ts
+++ apps/backend/src/referentiels/list-actions/list-actions.service.ts
@@ -1,3 +1,6 @@
+import { preuveComplementaireTable } from '@/backend/collectivites/documents/models/preuve-complementaire.table';
+import { discussionMessageTable } from '@/backend/collectivites/shared/models/discussion-messsage.table';
+import { discussionTable } from '@/backend/collectivites/shared/models/discussion.table';
 import { PersonneTagOrUser } from '@/backend/collectivites/shared/models/personne-tag-or-user.dto';
 import { personneTagTable } from '@/backend/collectivites/tags/personnes/personne-tag.table';
 import { serviceTagTable } from '@/backend/collectivites/tags/service-tag.table';
@@ -38,7 +41,7 @@ export class ListActionsService {
   constructor(
     private readonly databaseService: DatabaseService,
     private readonly snapshotService: SnapshotsService
-  ) {}
+  ) { }
 
   private db = this.databaseService.db;
 
@@ -336,6 +339,7 @@ export class ListActionsService {
 
     const actionChildren = await this.getActionChildren({ referentielId });
 
+
     return actionDefinitions.map((definition) => {
       return {
         id: definition.actionId,
@@ -357,6 +361,8 @@ export class ListActionsService {
           definition.exprScore && definition.exprScore !== ''
         ),
         phase: definition.categorie,
+        preuvesCount: definition.preuvesCount,
+        discussionsCount: definition.discussionsCount,
       };
     });
   }
@@ -396,6 +402,19 @@ export class ListActionsService {
           sql<boolean>`(${subQuery.actionId} in (select ${questionActionTable.actionId} from ${questionActionTable}))`.as(
             'haveQuestions'
           ),
+        discussionsCount:
+          sql<number>`(
+            select COUNT(*)
+            from ${discussionMessageTable}
+            inner join ${discussionTable} on ${discussionMessageTable.discussionId} = ${discussionTable.id}
+            where ${discussionTable.actionId} = ${subQuery.actionId}
+          )`.as('discussionsCount'),
+        preuvesCount:
+          sql<number>`(
+            select COUNT(*)
+            from ${preuveComplementaireTable}
+            where ${preuveComplementaireTable.actionId} = ${subQuery.actionId}
+          )`.as('preuvesCount'),
       })
       .from(subQuery)
       .innerJoin(
@@ -410,9 +429,9 @@ export class ListActionsService {
           eq(subQuery.referentiel, referentielId),
           identifiant
             ? or(
-                eq(subQuery.identifiant, identifiant),
-                like(subQuery.identifiant, `${identifiant}.%`)
-              )
+              eq(subQuery.identifiant, identifiant),
+              like(subQuery.identifiant, `${identifiant}.%`)
+            )
             : undefined
         )
       );
