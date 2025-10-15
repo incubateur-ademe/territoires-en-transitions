50587e2a15f2c7a4f2f8adecdc958d1c63f202ab
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2025-10-15 17:05:29 +0200

    feat(e2e): utilisation des page object models pour faciliter la réutisabilité

---
+++ e2e/tests/plans/fiches/list-fiches/filter-fiches.pom.ts
@@ -0,0 +1,15 @@
+import { Locator, Page } from '@playwright/test';
+import { Statut } from '@/domain/plans/fiches';
+export class FilterFichesPom {
+  readonly page: Page;
+  readonly statutsSelector: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.statutsSelector = page.locator('[data-test="statuts"]');
+  }
+  async selectStatut(statut: Statut) {
+    await this.statutsSelector.click();
+    await this.page.locator(`[data-test="${statut}"]`).click();
+  }
+}
