c063699ede6bd0e19c339d7c5de8a76143dc9846
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-10-17 10:08:36 +0200

    Lazy load Crisp SDK

--- apps/app/src/lib/crisp.widget.tsx
+++ apps/app/src/lib/crisp.widget.tsx
@@ -1,17 +1,30 @@
 'use client';
 
 import { ENV } from '@/api/environmentVariables';
-import { Crisp } from 'crisp-sdk-web';
 import { useEffect } from 'react';
 
 export default function CrispChat() {
   useEffect(() => {
-    if (typeof window === 'undefined') return;
+    const loadCrispLazily = async () => {
+      if (typeof window === 'undefined') {
+        return;
+      }
 
-    if (ENV.crisp_website_id && ENV.crisp_website_id.length > 0) {
+      const alreadyLoaded = '$crisp' in window;
+      if (alreadyLoaded) {
+        return;
+      }
+
+      if (!ENV.crisp_website_id || ENV.crisp_website_id.length === 0) {
+        return;
+      }
+
+      const { Crisp } = await import('crisp-sdk-web');
       Crisp.configure(ENV.crisp_website_id);
-    }
-  });
+    };
+
+    loadCrispLazily();
+  }, []);
 
   return null;
 }
