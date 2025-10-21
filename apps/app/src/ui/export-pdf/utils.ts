4d36da900e884b1fa9e5f2343fb347eff0a5baca
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-10-21 21:24:28 +0200

    Add @react-pdf/types to make it build with new config

--- apps/app/src/ui/export-pdf/utils.ts
+++ apps/app/src/ui/export-pdf/utils.ts
@@ -1,4 +1,5 @@
 import { preset } from '@/ui';
+import { Style } from '@react-pdf/types';
 import { createTw } from 'react-pdf-tailwind';
 
-export const tw = createTw({ theme: preset.theme });
+export const tw: (input: string) => Style = createTw({ theme: preset.theme });
