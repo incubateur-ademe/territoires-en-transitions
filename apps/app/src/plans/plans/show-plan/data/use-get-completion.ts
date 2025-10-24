8a1f00518078b5c18b6f6ff9b851faabf75a93c6
elisfain <elisabeth.fainstein.freelance@gmail.com>
2025-10-24 20:51:07 +0200

    refactor: rename hook to be more explicit

--- apps/app/src/plans/plans/show-plan/data/use-get-completion.ts
+++ apps/app/src/plans/plans/show-plan/data/use-get-completion.ts
@@ -1,10 +1,9 @@
 import { RouterOutput, useTRPC } from '@/api/utils/trpc/client';
 import { useQuery } from '@tanstack/react-query';
-
 type FieldToComplete =
   RouterOutput['plans']['completionAnalytics']['getFieldsToComplete'][number];
 
-export const useGetCompletion = (planId: number): FieldToComplete[] => {
+export const useGetPlanCompletion = (planId: number): FieldToComplete[] => {
   const trpc = useTRPC();
   const { data } = useQuery(
     trpc.plans.completionAnalytics.getFieldsToComplete.queryOptions({ planId })
