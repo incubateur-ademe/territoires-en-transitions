8418afb83a083370883cebcc95739565fe01df39
Dimitri Valax <dimitri.valax@beta.gouv.fr>
2025-10-29 10:07:35 +0100

    rename sqitch revert file

--- data_layer/sqitch/revert/collectivite/remove_supprimer_commentaire_via_table_tigger.sql
+++
@@ -1,9 +0,0 @@
--- Revert tet:collectivite/remove_supprimer_commentaire_via_table_trigger from pg
-
-BEGIN;
-
-create trigger supprimer_commentaire_via_table after
-delete
-    on
-    public.discussion_message for each row execute function supprimer_discussion();
-COMMIT;
