b212fe964b4b4aebd236fa2e925b2dbb5e8cae97
Marc Rutkowski <marc@attractive-media.fr>
2025-10-29 16:16:09 +0100

    Permet de faire la création d'une fiche et son update en un seul appel

--- apps/backend/src/plans/fiches/create-fiche/create-fiche.router.ts
+++ apps/backend/src/plans/fiches/create-fiche/create-fiche.router.ts
@@ -1,6 +1,8 @@
 import { TrpcService } from '@/backend/utils/trpc/trpc.service';
 import { Injectable } from '@nestjs/common';
+import z from 'zod';
 import { ficheSchemaCreate } from '../shared/models/fiche-action.table';
+import { updateFicheRequestSchema } from '../update-fiche/update-fiche.request';
 import { CreateFicheService } from './create-fiche.service';
 
 @Injectable()
@@ -11,11 +13,18 @@ export class CreateFicheRouter {
   ) {}
 
   router = this.trpc.router({
-    // TODO Brancher ce endpoint aux hooks front qui utilisent encore supabase en direct
     create: this.trpc.authedProcedure
-      .input(ficheSchemaCreate)
+      .input(
+        z.object({
+          fiche: ficheSchemaCreate,
+          ficheFields: updateFicheRequestSchema.optional(),
+        })
+      )
       .mutation(async ({ input, ctx }) => {
-        return this.service.createFiche(input, { user: ctx.user });
+        return this.service.createFiche(input.fiche, {
+          ficheFields: input.ficheFields,
+          user: ctx.user,
+        });
       }),
   });
 }
