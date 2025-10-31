bb1e45dbfa2cbef777ee1d0c45e09ae317424d91
Gaël Servaud <servauga@gmail.com>
2025-10-31 16:27:35 +0100

    Harmonisation du wording des indicateurs

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/indicateurs/liste/@tabs/collectivite/page.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/indicateurs/liste/@tabs/collectivite/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import IndicateursListView from '@/app/app/pages/collectivite/Indicateurs/lists/indicateurs-list/indicateurs-list-view';
+import { getFiltersForFavoritesIndicateurs } from '@/app/indicateurs/definitions/use-list-indicateur-definitions';
 
 export default function Page() {
   return (
     <IndicateursListView
       listId="collectivite"
-      defaultFilters={{ estFavori: true }}
+      defaultFilters={getFiltersForFavoritesIndicateurs()}
     />
   );
 }
--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/indicateurs/liste/@tabs/tabs-list.ts
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/indicateurs/liste/@tabs/tabs-list.ts
@@ -1,25 +1,28 @@
+import { INDICATEUR_LABELS } from '@/app/app/pages/collectivite/Indicateurs/constants';
 import { IndicateursListParamOption } from '@/app/app/paths';
 import { TabProps } from '@/ui/design-system/Tabs/Tabs.next';
 
 type TabParams = Omit<TabProps, 'href'> & {
   listId: IndicateursListParamOption;
+  isPrivate?: boolean;
 };
 
 /** Liste des onglets et de leurs paramètres */
 export const TabsListParams: TabParams[] = [
-  { listId: 'cles', label: 'Indicateurs clés' },
-  { listId: 'perso', label: 'Indicateurs personnalisés' },
+  { listId: 'cles', label: INDICATEUR_LABELS.keys.plural },
+  { listId: 'perso', label: INDICATEUR_LABELS.personalized.plural },
   {
     listId: 'collectivite',
-    label: 'Indicateurs favoris',
+    label: INDICATEUR_LABELS.favorites.plural,
     icon: 'star-fill',
     iconClassName: 'text-secondary-1',
-    tooltip: 'Indicateurs favoris de la collectivité',
+    tooltip: INDICATEUR_LABELS.favorites.tooltip,
   },
   {
     listId: 'mes-indicateurs',
-    label: 'Mes indicateurs',
-    tooltip: 'Indicateurs dont je suis la personne pilote',
+    label: INDICATEUR_LABELS.myIndicateurs.plural,
+    tooltip: INDICATEUR_LABELS.myIndicateurs.tooltip,
+    isPrivate: true,
   },
-  { listId: 'tous', label: 'Tous les indicateurs' },
+  { listId: 'tous', label: INDICATEUR_LABELS.all.plural },
 ];
