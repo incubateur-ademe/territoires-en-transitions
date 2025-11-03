17902554498a4a7f1e720d95925ac7e977052b35
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-11-03 18:06:31 +0100

    Add support for `svg?url` in auth app

--- apps/auth/next.config.mjs
+++ apps/auth/next.config.mjs
@@ -1,4 +1,3 @@
-import { composePlugins, withNx } from '@nx/next';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -36,9 +35,4 @@ const nextConfig = {
   skipTrailingSlashRedirect: true,
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-export default composePlugins(...plugins)(nextConfig);
+export default nextConfig;
