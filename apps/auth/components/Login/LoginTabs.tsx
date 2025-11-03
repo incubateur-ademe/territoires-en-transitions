2e6db701196f3ec242b5c62600b73c1dbaf9148f
Gaël Servaud <servauga@gmail.com>
2025-11-03 16:03:40 +0100

    Utilisation de la nouvelle API zod

--- apps/auth/components/Login/LoginTabs.tsx
+++ apps/auth/components/Login/LoginTabs.tsx
@@ -18,16 +18,13 @@ import { Credentials, LoginPropsWithState } from './type';
 /** Gestionnaire d'état pour le formulaire de l'étape 1 */
 const useLoginForm = (isPasswordless: boolean, email: string) => {
   const validationSchema = z.object({
-    email: z
-      .string()
-      .email({
-        message: 'Un email valide est requis',
-      })
-      .trim(),
+    email: z.email({
+      error: 'Un email valide est requis',
+    }),
     password: z
       .string()
       .refine((value) => (isPasswordless ? true : value.length >= 8), {
-        message: 'Le mot de passe doit comporter au moins 8 caractères',
+        error: 'Le mot de passe doit comporter au moins 8 caractères',
       }),
   });
 
