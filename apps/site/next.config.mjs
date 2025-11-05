72668a7f59457bebe09220c04d40f6330974a0a9
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-11-05 12:54:29 +0100

    Configure Typescript project references
    config
    
    config
    
    config
    
    config
    
    config

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
 
+  typescript: {
+    // We safely disable the internal type checking of Next.js because
+    // all apps are type checked during the first steps of our CI.
+    // This avoids redundancy as well as Next.js
+    // incomplete support for TypeScript project references.
+    ignoreBuildErrors: true,
+  },
+
   // Useful for self-hosting in a Docker container
   // See https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
   output: 'standalone',
