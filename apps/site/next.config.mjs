85c34e9bd454098a2dc1301614c79d1c85442c51
Marc Rutkowski <marc@attractive-media.fr>
2025-11-05 10:19:57 +0100

    Enlève le wrapper nx dans les config next
    (pour avoir une config similaire à celle de app et auth)

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -1,8 +1,3 @@
-import { composePlugins, withNx } from '@nx/next';
-
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
 const nextConfig = {
   // This avoids warnings when ProjectGraph isn't cached during build
   eslint: {
@@ -51,9 +46,4 @@ const nextConfig = {
   poweredByHeader: false,
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-export default composePlugins(...plugins)(nextConfig);
+export default nextConfig;
