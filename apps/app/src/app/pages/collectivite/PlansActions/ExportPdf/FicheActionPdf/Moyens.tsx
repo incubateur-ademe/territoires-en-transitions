4ff967501868a87326368bb42d11219dc23247f3
Gaël Servaud <servauga@gmail.com>
2025-11-05 16:42:45 +0100

    Utilisation des types du domaines + déplacement de la section ressources sous l'onget "Budgets" qui devient "moyens"

--- apps/app/src/app/pages/collectivite/PlansActions/ExportPdf/FicheActionPdf/Moyens.tsx
+++ apps/app/src/app/pages/collectivite/PlansActions/ExportPdf/FicheActionPdf/Moyens.tsx
@@ -93,6 +93,16 @@ export const Moyens = ({ fiche, budgets = [] }: MoyensProps) => {
           </Paragraph>
           {!emptyFinancements ? financements : 'Non renseignés '}
         </Paragraph>
+        <Paragraph
+          className={classNames({
+            'text-grey-7': emptyRessources,
+          })}
+        >
+          <Paragraph className="text-primary-9 font-bold uppercase">
+            Moyens humains et techniques :{' '}
+          </Paragraph>
+          {ressources ? htmlToText(ressources) : 'Non renseignés '}
+        </Paragraph>
       </Stack>
     </>
   );
---
+++ apps/app/src/plans/fiches/update-fiche/moyens/budget/budget.tsx
@@ -0,0 +1,48 @@
+import { FicheActionBudget } from '@/domain/plans';
+import { EditableSection } from '../components/EditableSection';
+import { BudgetInvestissementContent } from './budget-investissement.content';
+import { BudgetPerYearContent } from './budget-per-year.content';
+import { toBudgetProperties } from './to-budget-properties';
+import { PerYearBudgetProperties } from './types';
+
+function getSectionLabel(type: BudgetProps['type']) {
+  const labels = {
+    investissement: 'd’investissement',
+    fonctionnement: 'de fonctionnement',
+  };
+  return `Budget ${labels[type]} : `;
+}
+
+type BudgetProps = {
+  type: 'investissement' | 'fonctionnement';
+  isReadonly?: boolean;
+  budgets: FicheActionBudget[];
+  onEdit: () => void;
+};
+
+export const Budget = (props: BudgetProps) => {
+  const { type, isReadonly, budgets, onEdit } = props;
+
+  const budgetsToConsider = toBudgetProperties(budgets, type);
+  const hasBudgets = budgetsToConsider.length > 0;
+  const perYearBudgets = budgetsToConsider.filter(
+    (b): b is PerYearBudgetProperties => !!b.year
+  );
+
+  const perYearView = perYearBudgets.length > 0;
+  return (
+    <EditableSection
+      label={getSectionLabel(type)}
+      isReadonly={isReadonly}
+      hasContent={hasBudgets}
+      onEdit={onEdit}
+      editButtonTitle="Modifier le budget"
+    >
+      {perYearView ? (
+        <BudgetPerYearContent budgets={perYearBudgets} />
+      ) : (
+        <BudgetInvestissementContent budgets={budgetsToConsider} />
+      )}
+    </EditableSection>
+  );
+};
