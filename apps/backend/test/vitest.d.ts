36d4520752cc153d457ac297bf9eab8cdafd9b15
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-11-12 16:06:40 +0100

    Update Vitest matchers definition to match Vitest 3.2

--- apps/backend/test/vitest.d.ts
+++ apps/backend/test/vitest.d.ts
@@ -2,8 +2,6 @@ import 'vitest';
 import type { CustomMatchers } from './vitest-matchers';
 
 declare module 'vitest' {
-  type Assertion<T = any> = CustomMatchers<T>;
-
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends CustomMatchers<T> {}
+  interface Matchers<T = any> extends CustomMatchers<T> {}
 }
