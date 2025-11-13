7c092462aa73f73a5c8a64b58a3d76e9086aa53a
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-11-13 15:32:12 +0100

    eslint config

--- e2e/eslint.config.mjs
+++ e2e/eslint.config.mjs
@@ -2,9 +2,6 @@ import playwright from 'eslint-plugin-playwright';
 import baseConfig from '../eslint.config.mjs';
 
 export default [
-  {
-    ignores: ['**/dist'],
-  },
   ...baseConfig,
   {
     ...playwright.configs['flat/recommended'],
