17ec8e80dd885d0f8f125242928eaf124a3603fb
Marc Rutkowski <marc@attractive-media.fr>
2025-11-13 14:39:40 +0100

    Ajoute le modèle d'email pour notifier le pilote affecté à une fiche

---
+++ apps/backend/src/notifications/templates/components/email-container.tsx
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Container,
+  Html,
+  Tailwind,
+  pixelBasedPreset,
+} from '@react-email/components';
+import { toMerged } from 'es-toolkit';
+import * as React from 'react';
+
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { preset } from '@/ui/tailwind-preset';
+
+/**
+ * Génère l'enveloppe globale du mail avec
+ * - la config pour utiliser tailwind (config react-email + couleurs de notre thème)
+ * - un fond blanc sur un encadré gris
+ */
+export const EmailContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Html>
+      <Tailwind
+        config={toMerged(pixelBasedPreset, {
+          presets: [
+            { theme: { extend: { colors: preset.theme.extend.colors } } },
+          ],
+        })}
+      >
+        <Body className="bg-grey-2 text-grey-10 font-sans text-sm">
+          <Container className="bg-white px-8 py-4">{children}</Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
