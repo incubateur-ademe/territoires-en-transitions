e23fa4ea5c6d36a48cffa613330400736fc76d7f
Gaël Servaud <servauga@gmail.com>
2025-11-20 16:37:56 +0100

    Duplication des composants relatifs à la fiche action pour pouvoir commencer le refactoring d'ergo derrière un feature flag

---
+++ apps/app/src/plans/fiches/show-fiche/data/use-get-fiche-notes.ts
@@ -0,0 +1,24 @@
+import { FicheActionNote } from '@/api/plan-actions';
+import { useApiClient } from '@/app/utils/use-api-client';
+import { Fiche } from '@/domain/plans';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetFicheNotes = (
+  { id: ficheId, collectiviteId }: Pick<Fiche, 'id' | 'collectiviteId'>,
+  requested = true
+) => {
+  const api = useApiClient();
+
+  return useQuery({
+    queryKey: ['fiche_action_notes', collectiviteId, ficheId],
+
+    queryFn: async () => {
+      if (!collectiviteId || !ficheId) return;
+      return api.get<FicheActionNote[]>({
+        route: `/collectivites/${collectiviteId}/fiches-action/${ficheId}/notes`,
+      });
+    },
+
+    enabled: requested,
+  });
+};
---
+++ apps/app/src/plans/fiches/show-fiche/data/use-upsert-note.ts
@@ -0,0 +1,40 @@
+import { useApiClient } from '@/app/utils/use-api-client';
+import { Fiche, FicheActionNote } from '@/domain/plans';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export type EditedNote = Pick<FicheActionNote, 'note'> & {
+  id?: number;
+  year: number;
+};
+
+export const useUpsertNote = ({
+  id: ficheId,
+  collectiviteId,
+}: Pick<Fiche, 'id' | 'collectiviteId'>) => {
+  const api = useApiClient();
+  const queryClient = useQueryClient();
+
+  // TODO: use trpc
+  return useMutation({
+    mutationKey: ['upsert_note'],
+    mutationFn: async ({ id, note, year }: EditedNote) => {
+      return api.put({
+        route: `/collectivites/${collectiviteId}/fiches-action/${ficheId}/notes`,
+        params: {
+          notes: [
+            { id, note, dateNote: new Date(`${year}-01-01`).toISOString() },
+          ],
+        },
+      });
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['fiche_action', ficheId.toString()],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['fiche_action_notes', collectiviteId, ficheId],
+      });
+    },
+  });
+};
