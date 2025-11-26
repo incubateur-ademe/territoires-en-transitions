3ca659b74e19961b2bd6d6d4ebc9db05d6af69a6
Marc Rutkowski <marc@attractive-media.fr>
2025-11-26 17:09:19 +0100

    Ajoute la mise à jour des indicateurs associés à un axe

--- apps/backend/src/plans/axes/mutate-axe/mutate-axe.service.ts
+++ apps/backend/src/plans/axes/mutate-axe/mutate-axe.service.ts
@@ -2,6 +2,7 @@ import { PermissionOperationEnum } from '@/backend/users/authorizations/permissi
 import { PermissionService } from '@/backend/users/authorizations/permission.service';
 import { ResourceType } from '@/backend/users/authorizations/resource-type.enum';
 import { AuthenticatedUser } from '@/backend/users/models/auth.models';
+import { DatabaseService } from '@/backend/utils/database/database.service';
 import { Transaction } from '@/backend/utils/database/transaction.utils';
 import { MethodResult } from '@/backend/utils/result.type';
 import { Injectable, Logger } from '@nestjs/common';
@@ -16,7 +17,8 @@ export class MutateAxeService {
 
   constructor(
     private readonly permissionService: PermissionService,
-    private readonly mutateAxeRepository: MutateAxeRepository
+    private readonly mutateAxeRepository: MutateAxeRepository,
+    private readonly databaseService: DatabaseService
   ) {}
 
   async mutateAxe(
@@ -38,17 +40,48 @@ export class MutateAxeService {
       };
     }
 
-    const result =
-      'id' in axe
-        ? await this.mutateAxeRepository.update(axe, user.id, tx)
-        : await this.mutateAxeRepository.create(axe, user.id, tx);
+    const executeInTransaction = async (
+      transaction: Transaction
+    ): Promise<MethodResult<AxeType, MutateAxeError>> => {
+      const { indicateurs, ...axeProps } = axe;
 
-    if (!result.success) {
-      return {
-        success: false,
-        error: result.error,
-      };
-    }
-    return { success: true, data: result.data };
+      const result =
+        'id' in axeProps
+          ? await this.mutateAxeRepository.update(
+              axeProps,
+              user.id,
+              transaction
+            )
+          : await this.mutateAxeRepository.create(
+              axeProps,
+              user.id,
+              transaction
+            );
+
+      if (!result.success) {
+        return result;
+      }
+
+      if (indicateurs === null || indicateurs) {
+        const setIndicateursResult =
+          await this.mutateAxeRepository.setIndicateurs(
+            result.data.id,
+            indicateurs,
+            transaction
+          );
+        if (!setIndicateursResult.success) {
+          return setIndicateursResult;
+        }
+      }
+
+      return { success: true, data: result.data };
+    };
+
+    // Utiliser la transaction fournie ou en crée une nouvelle
+    return tx
+      ? executeInTransaction(tx)
+      : this.databaseService.db.transaction(async (newTx) =>
+          executeInTransaction(newTx)
+        );
   }
 }
