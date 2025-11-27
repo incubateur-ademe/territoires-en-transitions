7ef2693c112d2d7be98cf0359803c60780718669
Marc Rutkowski <marc@attractive-media.fr>
2025-11-27 17:04:32 +0100

    Ajoute le service et le routeur get-axe

---
+++ apps/backend/src/plans/axes/get-axe/get-axe.input.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const getAxeInputSchema = z.object({
+  axeId: z.number().positive("L'ID de l'axe doit être positif"),
+});
+
+export type GetAxeInput = z.infer<typeof getAxeInputSchema>;
