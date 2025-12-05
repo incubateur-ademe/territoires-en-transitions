42191c6a3871b255235a83b4d05278a43b3c42f8
Marc Rutkowski <marc@attractive-media.fr>
2025-12-05 14:05:25 +0100

    Met à jour le routeur avec les nouveaux sous-routeurs

--- apps/backend/src/plans/plans/plans.router.ts
+++ apps/backend/src/plans/plans/plans.router.ts
@@ -1,107 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { createTrpcErrorHandler } from '@tet/backend/utils/trpc/trpc-error-handler';
 import { TrpcService } from '@tet/backend/utils/trpc/trpc.service';
-import {
-  createAxeRequestSchema,
-  createPlanSchema,
-  deleteAxeSchema,
-  deletePlanSchema,
-  getPlanSchema,
-  listPlansRequestSchema,
-  updateAxeRequestSchema,
-  updatePlanSchema,
-} from '@tet/domain/plans';
+import { DeletePlanRouter } from './delete-plan/delete-plan.router';
+import { GetPlanRouter } from './get-plan/get-plan.router';
+import { ListPlansRouter } from './list-plans/list-plans.router';
 import { MutatePlanRouter } from './mutate-plan/mutate-plan.router';
-import { planErrorConfig } from './plans.errors';
-import { PlanService } from './plans.service';
 
 @Injectable()
 export class PlanRouter {
   constructor(
     private readonly trpc: TrpcService,
-    private readonly planService: PlanService,
-    private readonly mutatePlanRouter: MutatePlanRouter
+    private readonly mutatePlanRouter: MutatePlanRouter,
+    private readonly getPlanRouter: GetPlanRouter,
+    private readonly listPlansRouter: ListPlansRouter,
+    private readonly deletePlanRouter: DeletePlanRouter
   ) {}
 
-  private readonly getResultDataOrThrowError =
-    createTrpcErrorHandler(planErrorConfig);
-
   router = this.trpc.mergeRouters(
-    this.trpc.router({
-      create: this.trpc.authedProcedure
-        .input(createPlanSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.createPlan(input, ctx.user);
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      update: this.trpc.authedProcedure
-        .input(updatePlanSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.updatePlan(input, ctx.user);
-
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      get: this.trpc.authedProcedure
-        .input(getPlanSchema)
-        .query(async ({ input, ctx }) => {
-          const result = await this.planService.findById(
-            input.planId,
-            ctx.user
-          );
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      list: this.trpc.authedProcedure
-        .input(listPlansRequestSchema)
-        .query(async ({ input, ctx }) => {
-          const result = await this.planService.listPlans(
-            input.collectiviteId,
-            ctx.user,
-            {
-              limit: input.limit,
-              page: input.page,
-              sort: input.sort,
-            }
-          );
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      createAxe: this.trpc.authedProcedure
-        .input(createAxeRequestSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.upsertAxe(input, ctx.user);
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      updateAxe: this.trpc.authedProcedure
-        .input(updateAxeRequestSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.upsertAxe(input, ctx.user);
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      deleteAxe: this.trpc.authedProcedure
-        .input(deleteAxeSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.deleteAxe(
-            input.axeId,
-            ctx.user
-          );
-          return this.getResultDataOrThrowError(result);
-        }),
-
-      deletePlan: this.trpc.authedProcedure
-        .input(deletePlanSchema)
-        .mutation(async ({ input, ctx }) => {
-          const result = await this.planService.deletePlan(
-            input.planId,
-            ctx.user
-          );
-          return this.getResultDataOrThrowError(result);
-        }),
-    }),
-    this.mutatePlanRouter.router
+    this.mutatePlanRouter.router,
+    this.getPlanRouter.router,
+    this.listPlansRouter.router,
+    this.deletePlanRouter.router
   );
 }
