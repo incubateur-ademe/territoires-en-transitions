6d1d0f4e546507153d7c29a363cc08ac5537c20d
Marc Rutkowski <marc@attractive-media.fr>
2025-12-05 14:05:48 +0100

    Enlève les anciens fichiers

--- apps/backend/src/plans/plans/plans.module.ts
+++ apps/backend/src/plans/plans/plans.module.ts
@@ -1,34 +1,79 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { CollectivitesModule } from '@tet/backend/collectivites/collectivites.module';
+import { AxeModule } from '../axes/axe.module';
+import { DeleteAxeRepository } from '../axes/delete-axe/delete-axe.repository';
+import { DeleteAxeRouter } from '../axes/delete-axe/delete-axe.router';
+import { DeleteAxeService } from '../axes/delete-axe/delete-axe.service';
+import { GetAxeRepository } from '../axes/get-axe/get-axe.repository';
+import { GetAxeRouter } from '../axes/get-axe/get-axe.router';
+import { GetAxeService } from '../axes/get-axe/get-axe.service';
+import { ListAxesRepository } from '../axes/list-axes/list-axes.repository';
+import { ListAxesRouter } from '../axes/list-axes/list-axes.router';
+import { ListAxesService } from '../axes/list-axes/list-axes.service';
+import { MutateAxeRepository } from '../axes/mutate-axe/mutate-axe.repository';
+import { MutateAxeRouter } from '../axes/mutate-axe/mutate-axe.router';
+import { MutateAxeService } from '../axes/mutate-axe/mutate-axe.service';
 import { FichesModule } from '../fiches/fiches.module';
 import { CompletionAnalyticsRouter } from './completion-analytics/completion-analytics.router';
 import { CompletionAnalyticsService } from './completion-analytics/completion-analytics.service';
+import { DeletePlanRepository } from './delete-plan/delete-plan.repository';
+import { DeletePlanRouter } from './delete-plan/delete-plan.router';
+import { DeletePlanService } from './delete-plan/delete-plan.service';
+import { GetPlanRepository } from './get-plan/get-plan.repository';
+import { GetPlanRouter } from './get-plan/get-plan.router';
+import { GetPlanService } from './get-plan/get-plan.service';
+import { ListPlansRepository } from './list-plans/list-plans.repository';
+import { ListPlansRouter } from './list-plans/list-plans.router';
+import { ListPlansService } from './list-plans/list-plans.service';
 import { MutatePlanRepository } from './mutate-plan/mutate-plan.repository';
 import { MutatePlanRouter } from './mutate-plan/mutate-plan.router';
 import { MutatePlanService } from './mutate-plan/mutate-plan.service';
-import { PlansRepository } from './plans.repository';
-import { PlanRouter } from './plans.router';
-import { PlanService } from './plans.service';
 
 @Module({
   imports: [
-    forwardRef(() => FichesModule),
     forwardRef(() => CollectivitesModule),
+    forwardRef(() => FichesModule),
+    AxeModule,
   ],
   providers: [
-    PlanService,
-    {
-      provide: 'PlansRepositoryInterface',
-      useClass: PlansRepository,
-    },
-    PlansRepository,
-    PlanRouter,
     CompletionAnalyticsService,
     CompletionAnalyticsRouter,
+    DeleteAxeService,
+    DeleteAxeRouter,
+    DeleteAxeRepository,
+    GetAxeService,
+    GetAxeRouter,
+    GetAxeRepository,
+    ListAxesRepository,
+    ListAxesService,
+    ListAxesRouter,
+    MutateAxeService,
+    MutateAxeRouter,
+    MutateAxeRepository,
+    DeletePlanRepository,
+    DeletePlanService,
+    DeletePlanRouter,
+    GetPlanRepository,
+    GetPlanService,
+    GetPlanRouter,
+    ListPlansRepository,
+    ListPlansService,
+    ListPlansRouter,
     MutatePlanRepository,
     MutatePlanService,
     MutatePlanRouter,
   ],
-  exports: [PlanService, PlanRouter, CompletionAnalyticsRouter, MutatePlanRouter],
+  exports: [
+    CompletionAnalyticsRouter,
+    DeleteAxeRouter,
+    GetAxeRouter,
+    ListAxesRouter,
+    MutateAxeRouter,
+    DeletePlanRouter,
+    GetPlanRouter,
+    ListPlansRouter,
+    MutatePlanRouter,
+    MutatePlanService,
+  ],
 })
 export class PlanModule {}
--- apps/backend/src/plans/plans/plans.result.ts
+++
@@ -1,4 +0,0 @@
-import { MethodResult } from '@tet/backend/utils/result.type';
-import { PlanError } from './plans.errors';
-
-export type Result<T, E = PlanError> = MethodResult<T, E>;
