848d2d27e7c7ab0274c0bf2ce84ae8cee1b9e1a4
Marc Rutkowski <marc@attractive-media.fr>
2025-12-08 19:13:45 +0100

    Ajoute la mise à jour des indicateurs associés à un axe

--- apps/backend/src/plans/axes/upsert-axe/upsert-axe.errors.ts
+++ apps/backend/src/plans/axes/upsert-axe/upsert-axe.errors.ts
@@ -3,7 +3,11 @@ import {
   TrpcErrorHandlerConfig,
 } from '@tet/backend/utils/trpc/trpc-error-handler';
 
-const specificErrors = ['CREATE_AXE_ERROR', 'UPDATE_AXE_ERROR'] as const;
+const specificErrors = [
+  'CREATE_AXE_ERROR',
+  'UPDATE_AXE_ERROR',
+  'UPDATE_INDICATEURS_ERROR',
+] as const;
 type SpecificError = (typeof specificErrors)[number];
 
 export const upsertAxeErrorConfig: TrpcErrorHandlerConfig<SpecificError> = {
@@ -15,6 +19,10 @@ export const upsertAxeErrorConfig: TrpcErrorHandlerConfig<SpecificError> = {
     UPDATE_AXE_ERROR: {
       code: 'INTERNAL_SERVER_ERROR',
       message: "La mise à jour de l'axe a échoué",
+    },
+    UPDATE_INDICATEURS_ERROR: {
+      code: 'INTERNAL_SERVER_ERROR',
+      message: "La mise à jour des indicateurs associés à l'axe a échoué",
     },
   },
 };
