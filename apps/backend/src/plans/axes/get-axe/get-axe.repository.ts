b86090b6a4c70f4bd175c644327f893bf35e9baf
Marc Rutkowski <marc@attractive-media.fr>
2025-12-10 22:13:17 +0100

    Ajoute la lecture des indicateurs associés à un axe

--- apps/backend/src/plans/axes/get-axe/get-axe.repository.ts
+++ apps/backend/src/plans/axes/get-axe/get-axe.repository.ts
@@ -1,11 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { indicateurDefinitionTable } from '@tet/backend/indicateurs/definitions/indicateur-definition.table';
 import { DatabaseService } from '@tet/backend/utils/database/database.service';
 import { Transaction } from '@tet/backend/utils/database/transaction.utils';
 import { Result } from '@tet/backend/utils/result.type';
 import { AxeLight } from '@tet/domain/plans';
 import { eq } from 'drizzle-orm';
+import { axeIndicateurTable } from '../../fiches/shared/models/axe-indicateur.table';
 import { axeTable } from '../../fiches/shared/models/axe.table';
 import { GetAxeError, GetAxeErrorEnum } from './get-axe.errors';
+import { Indicateur } from './get-axe.output';
 
 @Injectable()
 export class GetAxeRepository {
@@ -43,6 +46,37 @@ export class GetAxeRepository {
       return {
         success: false,
         error: GetAxeErrorEnum.SERVER_ERROR,
+      };
+    }
+  }
+
+  async geIndicateurs(
+    axeId: number,
+    tx?: Transaction
+  ): Promise<Result<Indicateur[], GetAxeError>> {
+    try {
+      const indicateurs = await (tx || this.databaseService.db)
+        .select({
+          id: indicateurDefinitionTable.id,
+          titre: indicateurDefinitionTable.titre,
+          unite: indicateurDefinitionTable.unite,
+        })
+        .from(axeIndicateurTable)
+        .innerJoin(
+          indicateurDefinitionTable,
+          eq(indicateurDefinitionTable.id, axeIndicateurTable.indicateurId)
+        )
+        .where(eq(axeIndicateurTable.axeId, axeId));
+
+      return {
+        success: true,
+        data: indicateurs,
+      };
+    } catch (error) {
+      this.logger.error(`Error getting indicateurs for axe ${axeId}: ${error}`);
+      return {
+        success: false,
+        error: GetAxeErrorEnum.GET_INDICATEURS_ERROR,
       };
     }
   }
