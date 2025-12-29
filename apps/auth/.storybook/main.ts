17eda9424a4dd1779765e09777b222e628dc6773
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2025-12-29 12:41:22 +0100

    Upgrade storybook to version 10

--- apps/auth/.storybook/main.ts
+++ apps/auth/.storybook/main.ts
@@ -1,21 +1,13 @@
-import { StorybookConfig } from '@storybook/nextjs';
-import { createRequire } from 'node:module';
-import { dirname, join } from 'node:path';
-
-const require = createRequire(import.meta.url);
+import { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  framework: getAbsolutePath('@storybook/nextjs'),
+  framework: '@storybook/nextjs-vite',
 
-  stories: ['../**/*.@(stories.@(js|jsx|ts|tsx))'],
+  stories: ['../**/*.stories.@(ts|tsx)'],
 
-  addons: [getAbsolutePath('@storybook/addon-docs')],
+  addons: ['@storybook/addon-docs'],
 
   core: { disableTelemetry: true },
 };
 
 export default config;
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,29 +1,17 @@
-import type { StorybookConfig } from '@storybook/nextjs';
-import { createRequire } from 'node:module';
-import { dirname, join } from 'node:path';
-
-const require = createRequire(import.meta.url);
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  framework: {
-    name: getAbsolutePath('@storybook/nextjs'),
-    options: {},
-  },
+  framework: '@storybook/nextjs-vite',
 
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
 
-  addons: [getAbsolutePath('@storybook/addon-docs')],
+  addons: ['@storybook/addon-docs'],
 
   core: { disableTelemetry: true },
 
   typescript: {
     check: true,
-    checkOptions: {},
   },
 };
 
 export default config;
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
