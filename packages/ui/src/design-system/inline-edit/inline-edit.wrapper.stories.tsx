56ee07d3d521428579784ce87b15e49cb72ce7b1
Arthur Molinos <arthur.molinos@gmail.com>
2025-12-30 10:03:40 +0100

    Améliore la story inline edit wrapper

--- packages/ui/src/design-system/inline-edit/inline-edit.wrapper.stories.tsx
+++ packages/ui/src/design-system/inline-edit/inline-edit.wrapper.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/nextjs';
+import { useState } from 'react';
 import { Select } from '../Select';
 import { InlineEditWrapper } from './inline-edit.wrapper';
 
@@ -12,29 +13,35 @@ export default meta;
 type Story = StoryObj<typeof InlineEditWrapper>;
 
 export const Default: Story = {
-  render: () => (
-    <InlineEditWrapper
-      renderOnEdit={({ openState }) => (
-        <div className="w-80">
-          <Select
-            values={undefined}
-            onChange={() => null}
-            buttonClassName="border-0 border-b"
-            displayOptionsWithoutFloater
-            openState={openState}
-            options={[
-              { label: 'Admin', value: 'admin' },
-              { label: 'Editor', value: 'editor' },
-              { label: 'Viewer', value: 'viewer' },
-            ]}
-          />
+  render: () => {
+    const options = [
+      { label: 'Victor', value: 'victor' },
+      { label: 'Elsa', value: 'elsa' },
+      { label: 'Camille', value: 'camille' },
+    ];
+
+    const [value, setValue] = useState(options[0].value);
+
+    return (
+      <InlineEditWrapper
+        renderOnEdit={({ openState }) => (
+          <div className="w-80">
+            <Select
+              values={value}
+              onChange={(value) => setValue(value as string)}
+              buttonClassName="border-0 border-b"
+              displayOptionsWithoutFloater
+              openState={openState}
+              options={options}
+            />
+          </div>
+        )}
+      >
+        <div className="inline-flex items-center gap-2 py-3 px-2 border border-primary-3 rounded-lg">
+          <div className="w-8 h-8 bg-primary-6 rounded-full" />
+          <div className="text-primary-8 font-medium">{value}</div>
         </div>
-      )}
-    >
-      <div className="inline-flex items-center gap-2 py-3 px-2 border border-primary-3 rounded-lg">
-        <div className="w-8 h-8 bg-primary-6 rounded-full" />
-        <div className="text-primary-8 font-medium">Harry Cot</div>
-      </div>
-    </InlineEditWrapper>
-  ),
+      </InlineEditWrapper>
+    );
+  },
 };
