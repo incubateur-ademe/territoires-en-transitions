e431100003b6994563b4acf62bffc444ada0014f
Marc Rutkowski <marc@attractive-media.fr>
2026-01-02 18:11:25 +0100

    Ajoute le bouton "Ouvrir/fermer tous les axes/sous-axes"

--- apps/app/src/plans/plans/show-plan/plan.view.tsx
+++ apps/app/src/plans/plans/show-plan/plan.view.tsx
@@ -4,12 +4,13 @@ import { PiloteOrReferentLabel } from '@/app/plans/plans/components/PiloteOrRefe
 import { EmptyPlanView } from '@/app/plans/plans/show-plan/empty-plan.view';
 import { usePlanFilters } from '@/app/plans/plans/show-plan/filters/plan-filters.context';
 import { PlanArborescence } from '@/app/plans/plans/show-plan/plan-arborescence.view';
+import { useToggleAllAxes } from '@/app/plans/plans/show-plan/plan-arborescence.view/use-toggle-all-axes';
 import ScrollTopButton from '@/app/ui/buttons/ScrollTopButton';
 import { hasPermission } from '@/app/users/authorizations/permission-access-level.utils';
 import { useCurrentCollectivite } from '@tet/api/collectivites';
 import { useUser } from '@tet/api/users';
 import { Plan } from '@tet/domain/plans';
-import { Spacer, VisibleWhen } from '@tet/ui';
+import { Button, Spacer, VisibleWhen } from '@tet/ui';
 import { Header } from '../components/header';
 import { checkAxeHasFiche } from '../utils';
 import { Actions } from './actions';
@@ -54,6 +55,10 @@ export const PlanView = ({ plan: initialPlanData }: Props) => {
     initialData: initialPlanData,
   });
   const rootAxe = plan.axes.find((axe) => axe.parent === null);
+
+  const { areAllClosed, toggleAll } = useToggleAllAxes(rootAxe, plan.axes);
+  const hasAxesToExpand = plan.axes.some((axe) => axe.depth > 0);
+
   if (!rootAxe) {
     return <div>Plan non trouvé</div>;
   }
@@ -64,6 +69,7 @@ export const PlanView = ({ plan: initialPlanData }: Props) => {
   const hasFiches = rootAxe.fiches && rootAxe.fiches.length > 0;
   const hasAxes = plan.axes.some((axe) => axe.depth > 0);
   const isPlanEmpty = !hasFiches && !hasAxes;
+
   return (
     <div className="w-full">
       <Header
@@ -100,6 +106,20 @@ export const PlanView = ({ plan: initialPlanData }: Props) => {
           title="Détail du plan"
           headerActionButtons={
             <>
+              <VisibleWhen condition={hasAxesToExpand && !isFiltered}>
+                <Button
+                  size="xs"
+                  variant="outlined"
+                  icon={areAllClosed ? 'arrow-down-line' : 'arrow-up-line'}
+                  iconPosition="right"
+                  onClick={toggleAll}
+                  dataTest="ToggleAllAxes"
+                >
+                  {areAllClosed
+                    ? 'Ouvrir tous les axes/sous-axes'
+                    : 'Fermer tous les axes/sous-axes'}
+                </Button>
+              </VisibleWhen>
               <VisibleWhen
                 condition={
                   currentCollectivite.isReadOnly === false &&
