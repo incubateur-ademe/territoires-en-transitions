f9f08400b8e7b17845c03b2355f7e253963488ad
Marc Rutkowski <marc@attractive-media.fr>
2026-01-02 15:50:29 +0100

    Front
    Raccorde le bouton/menu, la liste et le panneau d'édition des indicateurs associés à un axe
    
    Sépare hooks de création/màj des axes
    
    et supprime un ancien hook qui faisait un appel postgrest
    
    Ajoute le hook de chargement/màj des indicateurs associés à un axe
    
    Ajoute le bouton/menu d'édition d'un axe
    
    Ajoute la liste des indicateurs associés à un axe
    
    Ajoute le panneau de sélection des indicateurs associés à un axe

---
+++ apps/app/src/plans/plans/show-plan/data/use-axe-indicateurs.ts
@@ -0,0 +1,48 @@
+import {
+  IndicateurDefinitionListItem,
+  useListIndicateurs,
+} from '@/app/indicateurs/indicateurs/use-list-indicateurs';
+import { PlanNode } from '@tet/domain/plans';
+import { useUpdateAxe } from './use-update-axe';
+
+/**
+ * Charge la liste des indicateurs associés à un axe et fourni une
+ * fonction pour mettre à jour celle-ci
+ */
+export function useAxeIndicateurs({
+  axe,
+  collectiviteId,
+  enabled,
+}: {
+  axe: PlanNode;
+  collectiviteId: number;
+  enabled: boolean;
+}) {
+  const { data } = useListIndicateurs(
+    { collectiviteId, filters: { axeIds: [axe.id] } },
+    { enabled: enabled && axe.id > 0 }
+  );
+  const selectedIndicateurs = data?.data || [];
+
+  const { mutateAsync: updateAxe } = useUpdateAxe({
+    collectiviteId,
+    axe,
+  });
+
+  const toggleIndicateur = async (indicateur: IndicateurDefinitionListItem) => {
+    const isAssocie =
+      selectedIndicateurs.some((i) => i.id === indicateur.id) ?? false;
+
+    // Ajoute ou retire l'indicateur de la liste
+    const indicateurs = isAssocie
+      ? selectedIndicateurs.filter((i) => i.id !== indicateur.id) ?? []
+      : [...selectedIndicateurs, { id: indicateur.id }];
+
+    await updateAxe({ ...axe, nom: axe.nom || '', indicateurs });
+  };
+
+  return {
+    selectedIndicateurs,
+    toggleIndicateur,
+  };
+}
