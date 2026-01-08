f5ec8d66013ba5e2076625e3985a75f543c65764
Marc Rutkowski <marc@attractive-media.fr>
2026-01-08 11:18:20 +0100

    Revert "Ajout d'un filtre pour ne garder que les axes directement liés à la fiche"
    This reverts commit ce2ac057ca5e13755ec06be8a41626133eb22c3a.

--- apps/app/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
+++ apps/app/src/app/pages/collectivite/PlansActions/FicheAction/data/utils.ts
@@ -58,16 +58,7 @@ export const ficheResumeFactory = ({
     modifiedAt: new Date().toISOString(),
     sharedWithCollectivites: [],
     axes: axeId
-      ? [
-          {
-            id: axeId,
-            collectiviteId,
-            nom: '',
-            parentId: null,
-            planId: null,
-            axeLevel: 0,
-          },
-        ]
+      ? [{ id: axeId, collectiviteId, nom: '', parentId: null, planId: null }]
       : null,
     description: null,
     piliersEci: null,
