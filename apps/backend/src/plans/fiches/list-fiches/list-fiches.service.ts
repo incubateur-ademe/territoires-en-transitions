b71139ea8c37894c2cd58b87f670f710b2607e6a
Gaël Servaud <servauga@gmail.com>
2026-01-08 12:13:16 +0100

    fix action fetching

--- apps/backend/src/plans/fiches/list-fiches/list-fiches.service.ts
+++ apps/backend/src/plans/fiches/list-fiches/list-fiches.service.ts
@@ -723,17 +723,16 @@ export default class ListFichesService {
 
     const { data: fichesAction } = await this.listFichesQuery(null, {
       ficheIds: [ficheId],
-      withChildren: true,
     });
+    const ficheAction = fichesAction[0];
 
-    if (!fichesAction?.length) {
+    if (!ficheAction) {
       return {
         success: false,
         error: `Aucune action trouvée avec l'id ${ficheId}`,
       };
     }
 
-    const ficheAction = fichesAction[0];
     if (user) {
       await this.fichePermissionService.canReadFicheObject(ficheAction, user);
     }
