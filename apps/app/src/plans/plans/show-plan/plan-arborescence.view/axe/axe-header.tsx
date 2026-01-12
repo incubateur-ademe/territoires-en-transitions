0573718e45a7fd60e910b0968d8addb4c337206e
Marc Rutkowski <marc@attractive-media.fr>
2026-01-12 14:48:09 +0100

    Extrait les différents composants nécessaires à l'affichage d'un axe et de son contenu (titre, description, indicateurs, fiches)

---
+++ apps/app/src/plans/plans/show-plan/plan-arborescence.view/axe/axe-header.tsx
@@ -0,0 +1,106 @@
+import { Button, cn, Icon } from '@tet/ui';
+import { AxeIndicateursPanel } from './axe-indicateurs.panel';
+import { AxeMenuButton } from './axe-menu.button';
+import { AxeTitleInput } from './axe-title.input';
+import { useAxeContext } from './axe.context';
+
+export const AxeHeader = () => {
+  const {
+    createFicheResume,
+    intersectionRef,
+    isMainAxe,
+    isReadOnly,
+    isOpen,
+    setIsOpen,
+    isOpenPanelIndicateurs,
+    isOpenEditTitle,
+    setIsOpenEditTitle,
+    providerProps,
+  } = useAxeContext();
+  const { axe } = providerProps;
+
+  const axeFontColor = cn({
+    'text-primary-8 disabled:text-primary-10': isMainAxe,
+    'text-grey-8 disabled:text-grey-7': !isMainAxe,
+  });
+
+  return (
+    <div
+      role="heading"
+      ref={intersectionRef}
+      aria-level={axe.depth}
+      className={cn(
+        'relative py-2 pr-4 pl-2 overflow-hidden rounded-md hover:bg-grey-2 group/heading',
+        { 'rounded-b-none': isOpen }
+      )}
+    >
+      <div className="flex content-between group/title">
+        {/** Titre + picto permettant d'ouvrir/fermer l'axe */}
+        <button
+          className="flex grow hover:!bg-transparent active:!bg-transparent"
+          onClick={(e) => {
+            // shift-click passe le titre en mode édition
+            if (e.shiftKey && !isReadOnly && !isOpenEditTitle) {
+              setIsOpenEditTitle(true);
+            }
+            // ne permet pas de refermer l'axe si le volet "associer des
+            // indicateurs" est ouvert ou si l'édition du titre est en cours
+            // (évite l'ouverture/fermeture involontaire de l'axe)
+            else if (!isOpenPanelIndicateurs && !isOpenEditTitle) {
+              setIsOpen(!isOpen);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (
+              e.shiftKey &&
+              e.code === 'Enter' &&
+              !isReadOnly &&
+              !isOpenEditTitle
+            ) {
+              e.preventDefault();
+              setIsOpenEditTitle(true);
+            }
+          }}
+          title="Cliquer pour ouvrir/fermer l'axe. Shift+clic pour éditer le titre."
+        >
+          {/** Picto flèche reflétant l'état d'ouverture de l'axe */}
+          <div
+            className={cn('self-center mr-2', {
+              'rotate-90': isOpen,
+            })}
+          >
+            <Icon
+              icon="arrow-right-s-line"
+              size="lg"
+              className={axeFontColor}
+            />
+          </div>
+
+          {/** Titre */}
+          <AxeTitleInput fontColor={axeFontColor} />
+        </button>
+
+        {/** Boutons d'édition (au survol ou si la barre de titre a le focus) */}
+        {!isReadOnly && (
+          <>
+            <div className="invisible group-hover/heading:visible group-focus-within/title:visible flex self-center gap-3 ml-3 min-w-max">
+              <Button
+                variant="grey"
+                size="xs"
+                title="Créer une action"
+                onClick={() => {
+                  setIsOpen(true);
+                  createFicheResume.mutateAsync();
+                }}
+              >
+                Créer une action
+              </Button>
+              <AxeMenuButton />
+            </div>
+            <AxeIndicateursPanel />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ apps/app/src/plans/plans/show-plan/plan-arborescence.view/axe/axe-menu.button.tsx
@@ -0,0 +1,129 @@
+import { ButtonMenu, MenuAction } from '@tet/ui';
+import { useState } from 'react';
+import { checkAxeHasFiche } from '../../../utils';
+import { DeletePlanOrAxeModal } from '../../actions/delete-axe-or-plan.modal';
+import { MoveAxeModal } from '../../actions/move-axe.modal';
+import { useCreateAxe } from '../../data/use-create-axe';
+import { useUpdateAxe } from '../../data/use-update-axe';
+import {
+  PlanDisplayOptionsEnum,
+  usePlanOptions,
+} from '../plan-options.context';
+import { useAxeContext } from './axe.context';
+
+export const AxeMenuButton = () => {
+  const {
+    isReadOnly,
+    setIsOpen,
+    setIsOpenPanelIndicateurs,
+    setIsOpenEditTitle,
+    providerProps,
+  } = useAxeContext();
+  const { axe, axes, rootAxe, collectivite } = providerProps;
+  const hasDescription = axe.description !== null;
+
+  const { isOptionEnabled } = usePlanOptions();
+
+  const { mutateAsync: createAxe } = useCreateAxe({
+    collectiviteId: collectivite.collectiviteId,
+    planId: rootAxe.id,
+    parentAxe: axe,
+  });
+  const { mutateAsync: updateAxe } = useUpdateAxe({
+    axe,
+    collectiviteId: collectivite.collectiviteId,
+  });
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+  const [isOpenMoveModal, setIsOpenMoveModal] = useState(false);
+
+  if (isReadOnly) {
+    return null;
+  }
+
+  const menuActions: MenuAction[] = [
+    isOptionEnabled(PlanDisplayOptionsEnum.INDICATEURS)
+      ? {
+          label: 'Lier un indicateur',
+          icon: 'line-chart-line',
+          onClick: () => {
+            setIsOpen(true);
+            setIsOpenPanelIndicateurs(true);
+          },
+        }
+      : null,
+    isOptionEnabled(PlanDisplayOptionsEnum.DESCRIPTION)
+      ? {
+          label: hasDescription
+            ? 'Supprimer la description'
+            : 'Ajouter une description',
+          icon: 'file-text-line',
+          onClick: () => {
+            setIsOpen(true);
+            updateAxe({ ...axe, description: hasDescription ? null : '' });
+          },
+        }
+      : null,
+    {
+      label: 'Modifier le titre',
+      icon: 'edit-line',
+      onClick: () => {
+        setIsOpenEditTitle(true);
+      },
+    },
+    {
+      label: 'Créer un axe',
+      icon: 'folder-add-line',
+      onClick: () => {
+        setIsOpen(true);
+        createAxe();
+      },
+    },
+    {
+      label: 'Déplacer',
+      icon: 'drag-move-2-line',
+      onClick: () => {
+        setIsOpenMoveModal(true);
+      },
+    },
+    {
+      label: 'Supprimer',
+      icon: 'delete-bin-2-line',
+      onClick: () => {
+        setIsOpenDeleteModal(true);
+      },
+    },
+  ].filter((item) => item !== null);
+
+  return (
+    <>
+      <ButtonMenu
+        title="Editer cet axe"
+        icon="more-line"
+        variant="grey"
+        size="xs"
+        menu={{
+          actions: menuActions,
+        }}
+      />
+      <DeletePlanOrAxeModal
+        planId={rootAxe.id}
+        axeId={axe.id}
+        axeHasFiche={checkAxeHasFiche(axe, axes)}
+        openState={{
+          isOpen: isOpenDeleteModal,
+          setIsOpen: setIsOpenDeleteModal,
+        }}
+      />
+      <MoveAxeModal
+        collectiviteId={collectivite.collectiviteId}
+        axe={axe}
+        rootAxe={rootAxe}
+        axes={axes}
+        openState={{
+          isOpen: isOpenMoveModal,
+          setIsOpen: setIsOpenMoveModal,
+        }}
+      />
+    </>
+  );
+};
