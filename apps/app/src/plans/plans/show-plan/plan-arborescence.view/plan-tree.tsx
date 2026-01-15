7cf44c6bb522bedc0ebde1fac4d99341ab0939fb
Marc Rutkowski <marc@attractive-media.fr>
2026-01-15 09:13:27 +0100

    Masque les fiches à la racine du plan quand l'option d'affichage "Actions" est décochée

--- apps/app/src/plans/plans/show-plan/plan-arborescence.view/plan-tree.tsx
+++ apps/app/src/plans/plans/show-plan/plan-arborescence.view/plan-tree.tsx
@@ -3,6 +3,7 @@ import { CollectiviteAccess } from '@tet/domain/users';
 import { childrenOfPlanNodes } from '../../utils';
 import { Axe } from './axe/axe';
 import { FichesList } from './fiches.list';
+import { PlanDisplayOptionsEnum, usePlanOptions } from './plan-options.context';
 
 interface Props {
   plan: PlanNode;
@@ -11,15 +12,19 @@ interface Props {
 }
 
 export const PlanTree = ({ plan, axes, collectivite }: Props) => {
+  const planOptions = usePlanOptions();
+
   return (
     <div className="flex flex-col gap-2 mt-4">
       {/** Fiches à la racine du plan */}
-      <FichesList
-        collectivite={collectivite}
-        ficheIds={plan.fiches}
-        axeId={plan.id}
-        planId={plan.id}
-      />
+      {planOptions.isOptionEnabled(PlanDisplayOptionsEnum.ACTIONS) && (
+        <FichesList
+          collectivite={collectivite}
+          ficheIds={plan.fiches}
+          axeId={plan.id}
+          planId={plan.id}
+        />
+      )}
       {/** Arborescence des axes */}
       <div className="flex flex-col gap-4">
         {childrenOfPlanNodes(plan, axes).map((axe) => (
