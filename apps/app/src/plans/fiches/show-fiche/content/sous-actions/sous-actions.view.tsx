a84cdfafdb86531e4a680391567abb06ee2fa056
Arthur Molinos <arthur.molinos@gmail.com>
2026-01-15 09:38:29 +0100

    Rend le tableau des sous-actions ré-utilisable et ajoute la possibilité de masquer des colonnes

---
+++ apps/app/src/plans/fiches/show-fiche/content/sous-actions/sous-actions.view.tsx
@@ -0,0 +1,52 @@
+import { useCreateSousAction } from '@/app/plans/sous-actions/data/use-create-sous-action';
+import { SousActionTable } from '@/app/plans/sous-actions/list/table/sous-action.table';
+import { useCurrentCollectivite } from '@tet/api/collectivites';
+import { Button } from '@tet/ui';
+import { useListFiches } from '../../../list-all-fiches/data/use-list-fiches';
+import { useFicheContext } from '../../context/fiche-context';
+
+export const SousActionsView = () => {
+  const collectivite = useCurrentCollectivite();
+
+  const { fiche } = useFicheContext();
+
+  const { mutate: createSousFiche } = useCreateSousAction(fiche.id);
+
+  const { fiches: sousActions, isLoading } = useListFiches(
+    collectivite.collectiviteId,
+    {
+      filters: {
+        parentsId: [fiche.id],
+      },
+      queryOptions: {
+        sort: [{ field: 'titre', direction: 'asc' }],
+        limit: 'all',
+      },
+    }
+  );
+
+  const isEmpty = sousActions.length === 0;
+
+  return (
+    <div className="p-2 bg-white rounded-lg border border-grey-3 overflow-x-auto">
+      <SousActionTable
+        sousActions={sousActions}
+        isLoading={isLoading}
+        isEmpty={isEmpty}
+        createSousFiche={createSousFiche}
+        hiddenColumns={['parentId']}
+        isReadOnly={collectivite.isReadOnly}
+      />
+      {!isEmpty && !collectivite.isReadOnly && (
+        <Button
+          className="m-4"
+          icon="add-line"
+          size="xs"
+          onClick={() => createSousFiche()}
+        >
+          Ajouter une sous-action
+        </Button>
+      )}
+    </div>
+  );
+};
