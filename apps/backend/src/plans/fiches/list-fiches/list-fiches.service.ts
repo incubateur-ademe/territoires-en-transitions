d8d8e01738e7a64721919767db83be7061ef1d1e
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2026-01-19 16:44:59 +0100

    feat(backend/metrics): ajoute le count des sous-actions dans les métriques personnelles

--- apps/backend/src/plans/fiches/list-fiches/list-fiches.service.ts
+++ apps/backend/src/plans/fiches/list-fiches/list-fiches.service.ts
@@ -755,7 +755,7 @@ export default class ListFichesService {
     return { success: true, data: ficheAction };
   }
 
-  async countPiloteFiches(collectiviteId: number, user: AuthUser) {
+  async countPiloteFiches(collectiviteId: number, user: AuthUser, subFiches = false) {
     if (!user.id) {
       throw new BadRequestException(
         `Seulement supporté pour les utilisateurs authentifiés`
@@ -776,13 +776,17 @@ export default class ListFichesService {
           eq(ficheActionTable.collectiviteId, collectiviteId),
           eq(ficheActionPiloteTable.userId, user.id),
           eq(ficheActionTable.deleted, false),
-          isNull(ficheActionTable.parentId)
+          subFiches ? isNotNull(ficheActionTable.parentId) : isNull(ficheActionTable.parentId)
         )
       );
 
     const queryResult = await query;
 
     return queryResult[0]?.count ?? 0;
+  }
+
+  async countPiloteSubFiches(collectiviteId: number, user: AuthUser) {
+    return this.countPiloteFiches(collectiviteId, user, true);
   }
 
   async countPiloteFichesIndicateurs(collectiviteId: number, user: AuthUser) {
