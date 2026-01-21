bc1749fe6c09886051feeaa7c90c8c2a641b262b
Gaël Servaud <servauga@gmail.com>
2026-01-21 10:06:29 +0100

    fix e2e test

--- packages/ui/src/hooks/use-open-state.ts
+++ packages/ui/src/hooks/use-open-state.ts
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import { OpenState } from '../utils/types';
 
 /** Hook to manage open state when a component can be controlled or uncontrolled */
-export const useOpenState = (openState?: OpenState) => {
+export const useOpenState = (openState?: OpenState | { isOpen: boolean }) => {
   const [open, setOpen] = useState(openState?.isOpen ?? false);
 
-  const isControlled = !!openState?.setIsOpen;
+  const isControlled = openState !== undefined && 'setIsOpen' in openState;
 
-  const isOpen = isControlled ? openState.isOpen : open;
-
-  const setIsOpen = openState?.setIsOpen ? openState.setIsOpen : setOpen;
+  const { isOpen, setIsOpen } = isControlled
+    ? openState
+    : { isOpen: open, setIsOpen: setOpen };
 
   const toggleIsOpen = () => {
     const currentState = isControlled ? openState.isOpen : open;
