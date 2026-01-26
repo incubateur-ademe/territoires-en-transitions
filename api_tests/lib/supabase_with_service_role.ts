97202e3e9a33d3b2db2bf25f9ac04881dacdf8c8
Marc Rutkowski <marc@attractive-media.fr>
2026-01-26 16:01:55 +0100

    Change la version de supabase-js utilisée dans les anciens tests deno

--- api_tests/lib/supabase_with_service_role.ts
+++ api_tests/lib/supabase_with_service_role.ts
@@ -1,6 +1,6 @@
-import "https://deno.land/x/dotenv/load.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.0";
-import { Database } from "./database.types.ts";
+import 'https://deno.land/x/dotenv/load.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { Database } from './database.types.ts';
 
 /**
  * Client Supabase avec un accès privilégié, utilisé pour de l'admin
@@ -10,11 +10,11 @@ export const supabaseWithServiceRole = createClient<Database>(
   Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!,
   {
     db: {
-      schema: "public",
+      schema: 'public',
     },
     auth: {
       // Pour fonctionner avec deno hors du navigateur.
       persistSession: false,
     },
-  },
+  }
 );
