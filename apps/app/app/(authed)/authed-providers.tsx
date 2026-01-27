9595634d8f81c655f3c563e8b5455d15ac91fbde
Marc Rutkowski <marc@attractive-media.fr>
2026-01-27 19:04:15 +0100

    Ajoute un contexte/provider pour faire le rendu des notifications "toast" à un seul endroit

--- apps/app/app/(authed)/authed-providers.tsx
+++ apps/app/app/(authed)/authed-providers.tsx
@@ -3,7 +3,6 @@
 import AccepterCGUModal from '@/app/app/pages/Auth/AccepterCGUModal';
 import { SupportModeProvider } from '@/app/users/authorizations/support-mode/support-mode.provider';
 import { NPSTracker } from '@/app/utils/nps/nps-tracker';
-import { Toasters } from '@/app/utils/toast/toasters';
 import { CollectiviteProvider } from '@tet/api/collectivites';
 import { UserProviderStoreClient } from '@tet/api/users';
 import { UserWithCollectiviteAccesses } from '@tet/domain/users';
@@ -25,7 +24,6 @@ export function AuthedProviders({
     <UserProviderStoreClient user={user}>
       <CollectiviteProvider user={user}>
         <SupportModeProvider>
-          <Toasters />
           <NPSTracker />
           <AccepterCGUModal />
           {children}
--- apps/app/src/utils/toast/toasters.tsx
+++
@@ -1,7 +0,0 @@
-import { useMutationToast } from '@/app/utils/toast/use-mutation-toast';
-
-/** Affiche les notifications déclenchées par l'enregistrement de données */
-export const Toasters = () => {
-  const { renderToast } = useMutationToast();
-  return renderToast();
-};
