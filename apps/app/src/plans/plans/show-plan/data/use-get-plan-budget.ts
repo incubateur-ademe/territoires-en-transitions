0a81896d0bf6f5ce22e0bbb74cee700510727305
Marc Rutkowski <marc@attractive-media.fr>
2026-01-28 11:44:23 +0100

    Supprime un fichier inutile

--- apps/app/src/plans/plans/show-plan/data/use-get-plan-budget.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { useTRPC } from '@tet/api';
-
-// TODO
-export const useGetPlanBudget = (planId: number) => {
-  const trpc = useTRPC();
-  return useQuery(trpc.plans.plans.get.queryOptions({ planId }));
-};
