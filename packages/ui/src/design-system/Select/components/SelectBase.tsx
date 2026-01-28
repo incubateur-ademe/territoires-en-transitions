1908c3c0bab8c0a24f37e09bcf0b595adf9d8713
Arthur Molinos <arthur.molinos@gmail.com>
2026-01-28 10:47:18 +0100

    Corrige les input/select/textarea après la suppression du CSS DSFR

--- packages/ui/src/design-system/Select/components/SelectBase.tsx
+++ packages/ui/src/design-system/Select/components/SelectBase.tsx
@@ -516,7 +516,7 @@ const SelectButton = forwardRef(
                     data-test={`${dataTest}-input`}
                     type="text"
                     className={cn(
-                      'w-full text-sm outline-0 placeholder:text-grey-6 placeholder:text-xs',
+                      'w-full text-sm bg-inherit outline-0 placeholder:text-grey-6 placeholder:text-xs',
                       { 'py-1': values }
                     )}
                     value={inputValue}
