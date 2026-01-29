5c0f28acb74508476eed78d420a1dbb050257dd3
Marc Rutkowski <marc@attractive-media.fr>
2026-01-29 12:27:54 +0100

    Rend les composants en-tête et pied de page plus conforme aux emails transactionnels Brevo

--- apps/backend/src/utils/notifications/components/footer.tsx
+++ apps/backend/src/utils/notifications/components/footer.tsx
@@ -1,11 +1,34 @@
-import { Hr, Text } from '@react-email/components';
+import { Container, Hr, Link, Text } from '@react-email/components';
 import * as React from 'react';
 
-export const Footer = (): React.ReactNode => (
+type FooterProps = {
+  toEmail: string;
+  unsubscribeUrl?: string;
+};
+
+export const Footer = ({
+  toEmail,
+  unsubscribeUrl,
+}: FooterProps): React.ReactNode => (
   <>
     <Hr className="border-grey-3 my-4" />
-    <Text className="text-grey-6 text-xs text-center m-0">
-      Cet email a été envoyé automatiquement par Territoires en Transitions.
-    </Text>
+    <Container className="bg-primary-2 p-[20px] text-center text-grey-9 text-xs">
+      <Link
+        href="https://www.territoiresentransitions.fr/outil-numerique"
+        className="text-lg font-bold"
+      >
+        Territoires en Transitions
+      </Link>
+      <Text>
+        Aider les collectivités à prioriser et mettre en œuvre
+        <br /> une politique de transition écologique à la hauteur des enjeux
+      </Text>
+      <Text className="font-bold">contact@territoiresentransitions.fr</Text>
+      <Text>Cet email a été envoyé à {toEmail}</Text>
+      <Text>
+        Vous l'avez reçu car vous êtes inscrit sur Territoiresentransitions.fr
+      </Text>
+      {unsubscribeUrl && <Link href={unsubscribeUrl}>Se désinscrire</Link>}
+    </Container>
   </>
 );
