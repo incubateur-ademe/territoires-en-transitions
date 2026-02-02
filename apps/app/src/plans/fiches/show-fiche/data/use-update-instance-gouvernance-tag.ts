f6847c7c23726a4cbc3c2ae9e4ca356b628932e3
Gaël Servaud <servauga@gmail.com>
2026-02-02 16:15:07 +0100

    Gestion des tags d'instance de gouvernance côté frontend

---
+++ apps/app/src/plans/fiches/show-fiche/data/use-update-instance-gouvernance-tag.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useTRPC } from '@tet/api';
+
+export const useUpdateInstanceGouvernanceTag = (collectiviteId: number) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { mutateAsync, isPending } = useMutation(
+    trpc.collectivites.tags.instanceGouvernance.update.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: trpc.collectivites.tags.instanceGouvernance.list.queryKey({
+            collectiviteId,
+          }),
+        });
+      },
+    })
+  );
+
+  return {
+    mutate: mutateAsync,
+    isPending,
+  };
+};
