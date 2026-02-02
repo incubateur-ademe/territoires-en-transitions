9817db56d66a1c5e76d2b5354bdc9a5cb73b4353
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2026-02-02 14:55:10 +0100

    Conditionne l'affichage des boutons d'audit aux bonnes permissions

--- apps/app/src/referentiels/labellisations/HeaderLabellisation.tsx
+++ apps/app/src/referentiels/labellisations/HeaderLabellisation.tsx
@@ -1,6 +1,7 @@
 /**
  * Affiche l'en-tête de page contenant l'objectif et le bouton pour candidater
  */
+import { useCurrentCollectivite } from '@tet/api/collectivites';
 import { Button } from '@tet/ui';
 import { useState } from 'react';
 import { TAuditeur, useAuditeurs } from '../audits/useAudit';
@@ -8,27 +9,106 @@ import { ValiderAuditButton } from '../audits/valider-audit.button';
 import { DemandeAuditModal } from './DemandeAuditModal';
 import { DemandeLabellisationModal } from './DemandeLabellisationModal';
 import { numLabels } from './numLabels';
+import { TLabellisationParcours } from './types';
 import { TCycleLabellisation } from './useCycleLabellisation';
 import { useStartAudit } from './useStartAudit';
 
-export type THeaderLabellisationProps = {
+function StartAuditButton({ auditId }: { auditId: number }) {
+  const { mutate: onStartAudit } = useStartAudit();
+  return (
+    <Button
+      dataTest="StartAuditBtn"
+      size="sm"
+      onClick={() => {
+        onStartAudit({
+          auditId,
+        });
+      }}
+    >
+      {"Commencer l'audit"}
+    </Button>
+  );
+}
+
+function ApplyAuditButton({
+  parcoursLabellisation,
+  parcours,
+}: {
+  parcoursLabellisation: TCycleLabellisation;
+  parcours: TLabellisationParcours;
+}) {
+  const [opened, setOpened] = useState(false);
+  const [opened_1ereEtoileCOT, setOpened_1ereEtoileCOT] = useState(false);
+
+  const { isCOT, peutDemander1ereEtoileCOT, peutDemanderEtoile, status } =
+    parcoursLabellisation;
+
+  const { etoiles, completude_ok } = parcours;
+
+  const canSubmitDemande = peutDemanderEtoile || (isCOT && completude_ok);
+
+  const DemandeModal = isCOT ? DemandeAuditModal : DemandeLabellisationModal;
+
+  return (
+    <>
+      {etoiles === 1 && isCOT && (
+        <Button
+          className="mb-4"
+          dataTest="1ereEtoileCOT"
+          size="sm"
+          disabled={!peutDemander1ereEtoileCOT}
+          onClick={() => setOpened_1ereEtoileCOT(true)}
+        >
+          Demander la première étoile
+        </Button>
+      )}
+      <Button
+        dataTest="SubmitDemandeBtn"
+        size="sm"
+        disabled={!canSubmitDemande}
+        onClick={() => setOpened(true)}
+      >
+        {etoiles === 1 && !isCOT
+          ? 'Demander la première étoile'
+          : 'Demander un audit'}
+      </Button>
+
+      {status === 'non_demandee' ||
+        (status === 'demande_envoyee' && (
+          <>
+            <DemandeModal
+              parcoursLabellisation={parcoursLabellisation}
+              isCOT={isCOT}
+              opened={opened}
+              setOpened={setOpened}
+            />
+            {etoiles === 1 && isCOT && (
+              <DemandeLabellisationModal
+                parcoursLabellisation={parcoursLabellisation}
+                opened={opened_1ereEtoileCOT}
+                setOpened={setOpened_1ereEtoileCOT}
+                isCOT={isCOT}
+              />
+            )}
+          </>
+        ))}
+    </>
+  );
+}
+
+type THeaderLabellisationProps = {
   parcoursLabellisation: TCycleLabellisation;
 };
 
 export const HeaderLabellisation = (props: THeaderLabellisationProps) => {
-  const [opened, setOpened] = useState(false);
-  const [opened_1ereEtoileCOT, setOpened_1ereEtoileCOT] = useState(false);
-  const { mutate: onStartAudit } = useStartAudit();
+  const { hasCollectivitePermission } = useCurrentCollectivite();
+
+  const canMutateReferentiel = hasCollectivitePermission('referentiels.mutate');
+  const canAuditReferentiel = hasCollectivitePermission('referentiels.audit');
+
   const { parcoursLabellisation } = props;
-  const {
-    parcours,
-    status,
-    isAuditeur,
-    isCOT,
-    peutDemanderEtoile,
-    peutCommencerAudit,
-    peutDemander1ereEtoileCOT,
-  } = parcoursLabellisation;
+  const { parcours, status, isAuditeur, peutCommencerAudit } =
+    parcoursLabellisation;
 
   const { data: auditeurs } = useAuditeurs();
 
@@ -40,83 +120,39 @@ export const HeaderLabellisation = (props: THeaderLabellisationProps) => {
     parcoursLabellisation,
     auditeurs
   );
-  const { etoiles, audit, completude_ok, labellisation } = parcours;
-  const canSubmitDemande = peutDemanderEtoile || (isCOT && completude_ok);
-  const DemandeModal = isCOT ? DemandeAuditModal : DemandeLabellisationModal;
-  const auditId = audit?.id;
+  const { etoiles, audit, labellisation } = parcours;
+
   return (
     <div className="sticky top-0 z-40 w-full my-8 p-4 bg-primary-3">
       <DerniereLabellisation parcoursLabellisation={parcoursLabellisation} />
+
       <h2 className="mb-4">
         Objectif :{' '}
         {labellisation && labellisation.etoiles === 5
           ? 'renouveler la labellisation'
           : `${numLabels[etoiles]} étoile`}
       </h2>
-      {status === 'non_demandee' && !isAuditeur ? (
-        <>
-          {etoiles === 1 && isCOT ? (<Button
-            className="mb-4"
-            dataTest="1ereEtoileCOT"
-            size="sm"
-            disabled={!peutDemander1ereEtoileCOT}
-            onClick={() => setOpened_1ereEtoileCOT(true)}
-          >
-            Demander la première étoile
-          </Button>) : null}
-          <Button
-            dataTest="SubmitDemandeBtn"
-            size="sm"
-            disabled={!canSubmitDemande}
-            onClick={() => setOpened(true)}
-          >
-            {etoiles === 1 && !isCOT
-              ? 'Demander la première étoile'
-              : 'Demander un audit'}
-          </Button>
-        </>
-      ) : null}
-      {audit && peutCommencerAudit ? (
-        <Button
-          dataTest="StartAuditBtn"
-          size="sm"
-          onClick={() => {
-            if (auditId) {
-              onStartAudit({
-                auditId,
-              });
-            }
-          }}
-        >
-          {"Commencer l'audit"}
-        </Button>
-      ) : null}
+
+      {canMutateReferentiel && status === 'non_demandee' && !isAuditeur && (
+        <ApplyAuditButton
+          parcoursLabellisation={parcoursLabellisation}
+          parcours={parcours}
+        />
+      )}
+
+      {canAuditReferentiel && audit && peutCommencerAudit && (
+        <StartAuditButton auditId={audit.id} />
+      )}
+
       {!!headerMessageContent && (
         <p className="text-grey-8" data-test="HeaderMessage">
           {headerMessageContent}
         </p>
       )}
-      {audit && status === 'audit_en_cours' && isAuditeur && auditId ? (
-        <ValiderAuditButton auditId={auditId} demandeId={audit.demande_id} />
-      ) : null}
-      {status === 'non_demandee' || status === 'demande_envoyee' ? (
-        <>
-          <DemandeModal
-            parcoursLabellisation={parcoursLabellisation}
-            isCOT={isCOT}
-            opened={opened}
-            setOpened={setOpened}
-          />
-          {etoiles === 1 && isCOT ? (
-            <DemandeLabellisationModal
-              parcoursLabellisation={parcoursLabellisation}
-              opened={opened_1ereEtoileCOT}
-              setOpened={setOpened_1ereEtoileCOT}
-              isCOT={isCOT}
-            />
-          ) : null}
-        </>
-      ) : null}
+
+      {canAuditReferentiel && audit && status === 'audit_en_cours' && (
+        <ValiderAuditButton auditId={audit.id} demandeId={audit.demande_id} />
+      )}
     </div>
   );
 };
