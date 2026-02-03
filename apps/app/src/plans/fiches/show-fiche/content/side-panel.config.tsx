9c558bde26d112363682c06e234c4fc435025046
Gaël Servaud <servauga@gmail.com>
2026-02-03 17:55:04 +0100

    Update de label

--- apps/app/src/plans/fiches/show-fiche/content/side-panel.config.tsx
+++ apps/app/src/plans/fiches/show-fiche/content/side-panel.config.tsx
@@ -16,7 +16,7 @@ export const SIDE_PANEL_CONFIG: Record<
   SidePanelConfig
 > = {
   indicateurs: {
-    title: 'Lier des indicateurs',
+    title: 'Lier des indicateurs existants',
     content: (fiche) => (
       <FicheProvider fiche={fiche}>
         <LinkIndicateursView />
