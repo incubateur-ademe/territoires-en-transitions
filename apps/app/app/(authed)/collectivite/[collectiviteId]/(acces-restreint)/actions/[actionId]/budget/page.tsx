5d37ee655acbce7bb0064b9087043e3571170833
Gaël Servaud <servauga@gmail.com>
2026-02-04 09:36:26 +0100

    Fix import

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/actions/[actionId]/budget/page.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/actions/[actionId]/budget/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { MoyensView } from '@/app/plans/fiches/show-fiche/content/moyens/moyens.view';
+import { BudgetView } from '@/app/plans/fiches/show-fiche/content/moyens/budget';
 
 export default function Page() {
-  return <MoyensView />;
+  return <BudgetView />;
 }
