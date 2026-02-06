b9d66166c580f0e0b1fe0fc3f740f94ce899b574
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2026-02-06 15:46:20 +0100

    Update next.config.mjs

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,5 +1,5 @@
 import { uuid4 } from '@sentry/core';
-import { withSentryConfig } from '@sentry/nextjs';
+import { SentryBuildOptions, withSentryConfig } from '@sentry/nextjs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -94,10 +94,10 @@ const nextConfig = {
   skipTrailingSlashRedirect: true,
 };
 
-const sentryConfig = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const sentryConfig: SentryBuildOptions = {
   org: 'betagouv',
   project: 'territoires-en-transitions',
   sentryUrl: 'https://sentry.incubateur.net/',
@@ -108,32 +108,35 @@ const sentryConfig = {
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
+  widenClientFileUpload: isProduction,
 
   // Hides source maps from generated client bundles
-  hideSourceMaps: false,
+  sourcemaps: {
+    disable: !isProduction,
+  },
+
+  bundleSizeOptimizations: {
+    excludeDebugStatements: !isProduction,
+    excludeTracing: !isProduction,
+    excludeReplayShadowDom: !isProduction,
+    excludeReplayIframe: !isProduction,
+    excludeReplayWorker: !isProduction,
+  },
+
+  disableManifestInjection: !isProduction,
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  webpack: {
+    disableSentryConfig: !isProduction,
+    autoInstrumentServerFunctions: !isProduction,
+    autoInstrumentMiddleware: !isProduction,
+    autoInstrumentAppDirectory: !isProduction,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
 };
-
-// const plugins = [];
-// const config = composePlugins(...plugins)(nextConfig);
 
 export default (process.env.NODE_ENV === 'production') ? withSentryConfig(nextConfig, sentryConfig) : nextConfig;
