0a162b4e185a568161013f82feab635207cdc47e
Gaël Servaud <servauga@gmail.com>
2026-02-09 14:23:38 +0100

    fix verify

--- data_layer/sqitch/verify/creation_dune_table_instance_de_gouvernance.sql
+++ data_layer/sqitch/verify/creation_dune_table_instance_de_gouvernance.sql
@@ -3,5 +3,5 @@
 BEGIN;
 
 SELECT id, nom, collectivite_id, created_by, created_at FROM instance_de_gouvernance_tag WHERE false;
-SELECT fiche_action_id, instance_de_gouvernance_id, created_by, created_at FROM fiche_action_instance_de_gouvernance_tag     WHERE false;
+SELECT fiche_action_id, instance_de_gouvernance_tag_id, created_by, created_at FROM fiche_action_instance_de_gouvernance_tag     WHERE false;
 ROLLBACK;
