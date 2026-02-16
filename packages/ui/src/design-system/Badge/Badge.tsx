26100d3db3a1880465c90afe7643320012290a52
Arthur Molinos <arthur.molinos@gmail.com>
2026-02-16 11:35:58 +0100

    Ajoute forwardRef à Badge

--- packages/ui/src/design-system/Badge/Badge.tsx
+++ packages/ui/src/design-system/Badge/Badge.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 
 import { Icon, IconValue } from '../Icon';
 
+import { forwardRef } from 'react';
 import { cn } from '../../utils/cn';
 import { badgeClassnames } from './utils';
 
@@ -48,81 +49,89 @@ export type BadgeProps = {
 };
 
 /** Affiche un badge */
-export const Badge = ({
-  title,
-  onClose,
-  state = 'default',
-  size = 'md',
-  icon,
-  iconPosition = 'right',
-  iconClassname,
-  light = false,
-  disabled,
-  className,
-  trim = true,
-  uppercase = true,
-  dataTest,
-}: BadgeProps) => {
-  const styles = badgeClassnames[state];
-  return (
-    <div
-      data-test={`Badge-${dataTest}`}
-      className={cn(
-        styles.background,
-        styles.border,
-        'flex items-center gap-1 max-w-max h-fit px-3 py-1 border border-solid rounded-md',
-        {
-          'flex-row-reverse': iconPosition === 'left',
-          'border-grey-4 bg-white': light,
-          'px-1.5 py-0.5': size === 'sm',
-        },
-        className
-      )}
-    >
-      {(!!title || onClose) && (
-        <div className="flex items-center gap-1">
-          {!!title && (
-            <span
-              className={classNames(
-                styles.text,
-                'font-bold leading-4 text-left',
-                {
-                  'line-clamp-1': trim,
-                  'text-xs': size === 'sm',
-                  'mt-0.5 text-sm': size === 'md',
-                  uppercase,
-                }
-              )}
-            >
-              {title}
-            </span>
-          )}
+export const Badge = forwardRef<HTMLDivElement, BadgeProps>(
+  (
+    {
+      title,
+      onClose,
+      state = 'default',
+      size = 'md',
+      icon,
+      iconPosition = 'right',
+      iconClassname,
+      light = false,
+      disabled,
+      className,
+      trim = true,
+      uppercase = true,
+      dataTest,
+    },
+    ref
+  ) => {
+    const styles = badgeClassnames[state];
+    return (
+      <div
+        ref={ref}
+        data-test={`Badge-${dataTest}`}
+        className={cn(
+          styles.background,
+          styles.border,
+          'flex items-center gap-1 max-w-max h-fit px-3 py-1 border border-solid rounded-md',
+          {
+            'flex-row-reverse': iconPosition === 'left',
+            'border-grey-4 bg-white': light,
+            'px-1.5 py-0.5': size === 'sm',
+          },
+          className
+        )}
+      >
+        {(!!title || onClose) && (
+          <div className="flex items-center gap-1">
+            {!!title && (
+              <span
+                className={classNames(
+                  styles.text,
+                  'font-bold leading-4 text-left',
+                  {
+                    'line-clamp-1': trim,
+                    'text-xs': size === 'sm',
+                    'mt-0.5 text-sm': size === 'md',
+                    uppercase,
+                  }
+                )}
+              >
+                {title}
+              </span>
+            )}
 
-          {onClose && !disabled && (
-            <div
-              className="flex rounded-full cursor-pointer"
-              onClick={(evt) => {
-                evt.stopPropagation();
-                onClose?.();
-              }}
-            >
-              <Icon
-                icon="close-circle-line"
-                size={size === 'sm' ? 'xs' : 'sm'}
-                className={cn(styles.icon)}
-              />
-            </div>
-          )}
-        </div>
-      )}
+            {onClose && !disabled && (
+              <div
+                className="flex rounded-full cursor-pointer"
+                onClick={(evt) => {
+                  evt.stopPropagation();
+                  onClose?.();
+                }}
+              >
+                <Icon
+                  icon="close-circle-line"
+                  size={size === 'sm' ? 'xs' : 'sm'}
+                  className={cn(styles.icon)}
+                />
+              </div>
+            )}
+          </div>
+        )}
 
-      {icon && (
-        <Icon
-          icon={icon}
-          size={size === 'sm' ? 'xs' : 'sm'}
-          className={cn(styles.icon, iconClassname)}
-        />
-      )}
-    </div>
-  );
-};
+        {icon && (
+          <Icon
+            icon={icon}
+            size={size === 'sm' ? 'xs' : 'sm'}
+            className={cn(styles.icon, iconClassname)}
+          />
+        )}
+      </div>
+    );
+  }
+);
+
+Badge.displayName = 'Badge';
