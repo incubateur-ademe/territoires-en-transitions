84df1d3b0d19079c936f40d7c69397c77972ac29
Arthur Molinos <arthur.molinos@gmail.com>
2026-02-19 10:40:54 +0100

    Mets à jour les styles et enlève la prop light au profit de type pour les Badges

--- packages/ui/src/design-system/Badge/utils.ts
+++ packages/ui/src/design-system/Badge/utils.ts
@@ -1,4 +1,4 @@
-import { BadgeVariant } from './index';
+import { BadgeType, BadgeVariant } from './index';
 
 /**
  * Thème couleurs du composant Badge
@@ -8,65 +8,214 @@ import { BadgeVariant } from './index';
  **/
 export const badgeClassnames: Record<
   BadgeVariant,
-  {
-    text: string;
-    background: string;
-    border: string;
-    icon: string;
-  }
+  Record<
+    BadgeType,
+    {
+      text: string;
+      background: string;
+      border: string;
+      icon: string;
+    }
+  >
 > = {
   default: {
-    text: 'text-grey-8',
-    background: 'bg-grey-1',
-    border: 'border-grey-8',
-    icon: 'text-grey-8',
+    outlined: {
+      text: 'text-grey-8',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-primary-7',
+    },
+    solid: {
+      text: 'text-grey-8',
+      background: 'bg-primary-1',
+      border: 'border-grey-5',
+      icon: 'text-primary-7',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-primary-7',
+      border: 'border-primary-7',
+      icon: 'text-white',
+    },
   },
   standard: {
-    text: 'text-primary-7',
-    background: 'bg-primary-2',
-    border: 'border-primary-7',
-    icon: 'text-primary-7',
+    outlined: {
+      text: 'text-primary-7',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-primary-7',
+    },
+    solid: {
+      text: 'text-primary-7',
+      background: 'bg-primary-2',
+      border: 'border-primary-7',
+      icon: 'text-primary-7',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-primary-7',
+      border: 'border-primary-7',
+      icon: 'text-white',
+    },
+  },
+  hight: {
+    outlined: {
+      text: 'text-primary-9',
+      background: 'bg-white',
+      border: 'border-primary-9',
+      icon: 'text-primary-9',
+    },
+    solid: {
+      text: 'text-primary-9',
+      background: 'bg-primary-2',
+      border: 'border-primary-9',
+      icon: 'text-primary-9',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-primary-9',
+      border: 'border-primary-9',
+      icon: 'text-white',
+    },
   },
   success: {
-    text: 'text-success-1',
-    background: 'bg-success-2',
-    border: 'border-success-1',
-    icon: 'text-success-1',
+    outlined: {
+      text: 'text-success-1',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-success-1',
+    },
+    solid: {
+      text: 'text-success-1',
+      background: 'bg-success-2',
+      border: 'border-success-1',
+      icon: 'text-success-1',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-success-1',
+      border: 'border-success-1',
+      icon: 'text-white',
+    },
   },
   warning: {
-    text: 'text-warning-1',
-    background: 'bg-warning-2',
-    border: 'border-warning-1',
-    icon: 'text-warning-1',
+    outlined: {
+      text: 'text-warning-1',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-warning-1',
+    },
+    solid: {
+      text: 'text-warning-1',
+      background: 'bg-warning-2',
+      border: 'border-warning-1',
+      icon: 'text-warning-1',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-warning-1',
+      border: 'border-warning-1',
+      icon: 'text-white',
+    },
   },
   new: {
-    text: 'text-new-1',
-    background: 'bg-new-2',
-    border: 'border-new-1',
-    icon: 'text-new-1',
+    outlined: {
+      text: 'text-new-1',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-new-1',
+    },
+    solid: {
+      text: 'text-new-1',
+      background: 'bg-new-2',
+      border: 'border-new-1',
+      icon: 'text-new-1',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-new-1',
+      border: 'border-new-1',
+      icon: 'text-white',
+    },
   },
   error: {
-    text: 'text-error-1',
-    background: 'bg-transparent',
-    border: 'border-error-1',
-    icon: 'text-error-1',
+    outlined: {
+      text: 'text-error-1',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-error-1',
+    },
+    solid: {
+      text: 'text-error-1',
+      background: 'bg-error-2',
+      border: 'border-error-1',
+      icon: 'text-error-1',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-error-1',
+      border: 'border-error-1',
+      icon: 'text-white',
+    },
   },
   info: {
-    text: 'text-info-1',
-    background: 'bg-info-2',
-    border: 'border-info-1',
-    icon: 'text-info-1',
+    outlined: {
+      text: 'text-info-1',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-info-1',
+    },
+    solid: {
+      text: 'text-info-1',
+      background: 'bg-info-2',
+      border: 'border-info-1',
+      icon: 'text-info-1',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-info-1',
+      border: 'border-info-1',
+      icon: 'text-white',
+    },
   },
   grey: {
-    text: 'text-grey-6',
-    background: 'bg-grey-3',
-    border: 'border-grey-6',
-    icon: 'text-grey-6',
+    outlined: {
+      text: 'text-grey-8',
+      background: 'bg-white',
+      border: 'border-grey-5',
+      icon: 'text-grey-8',
+    },
+    solid: {
+      text: 'text-grey-8',
+      background: 'bg-grey-3',
+      border: 'border-grey-8',
+      icon: 'text-grey-8',
+    },
+    inverted: {
+      text: 'text-white',
+      background: 'bg-grey-8',
+      border: 'border-grey-8',
+      icon: 'text-white',
+    },
   },
   custom: {
-    text: '',
-    background: '',
-    border: '',
-    icon: '',
+    outlined: {
+      text: '',
+      background: '',
+      border: '',
+      icon: '',
+    },
+    solid: {
+      text: '',
+      background: '',
+      border: '',
+      icon: '',
+    },
+    inverted: {
+      text: '',
+      background: '',
+      border: '',
+      icon: '',
+    },
   },
 };
