fffefa1e69c71335a3d8d039e7aee90d64ad572d
Marc Rutkowski <marc@attractive-media.fr>
2026-03-09 08:49:52 +0100

    Permet de filtrer la liste des questions de personnalisation (par mesure, référentiel, question et thématique)

--- apps/backend/src/collectivites/personnalisations/models/question-with-choices.dto.ts
+++ apps/backend/src/collectivites/personnalisations/models/question-with-choices.dto.ts
@@ -4,6 +4,8 @@ import * as z from 'zod/mini';
 export const questionWithChoicesSchema = z.object({
   ...questionSchema.shape,
 
+  actionIds: z.nullish(z.array(z.string())),
+  referentielIds: z.nullish(z.array(z.string())),
   thematiqueNom: z.nullish(z.string()),
   choix: z.nullish(
     z.array(z.omit(questionChoixSchema, { questionId: true, version: true }))
