3f34c500219b52927001fa9e85727cd4a8344d3b
Marc Rutkowski <marc@attractive-media.fr>
2026-03-10 16:20:29 +0100

    Ajoute le point tRPC pour lire les réponses et les justifications

--- apps/backend/src/collectivites/personnalisations/models/reponse-choix.table.ts
+++ apps/backend/src/collectivites/personnalisations/models/reponse-choix.table.ts
@@ -1,14 +1,22 @@
 import { modifiedAt } from '@tet/backend/utils/column.utils';
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { integer, pgTable, primaryKey, varchar } from 'drizzle-orm/pg-core';
 import { collectiviteTable } from '../../shared/models/collectivite.table';
+import { questionChoixTable } from './question-choix.table';
+import { questionTable } from './question.table';
 
-export const reponseChoixTable = pgTable('reponse_choix', {
-  modifiedAt,
-  collectiviteId: integer('collectivite_id')
-    .references(() => collectiviteTable.id)
-    .notNull(),
-  // TODO: Reference question
-  questionId: varchar('question_id', { length: 30 }).notNull(),
-  // TODO: Reference choix_id
-  reponse: varchar('reponse', { length: 30 }),
-});
+export const reponseChoixTable = pgTable(
+  'reponse_choix',
+  {
+    modifiedAt,
+    collectiviteId: integer('collectivite_id')
+      .references(() => collectiviteTable.id)
+      .notNull(),
+    questionId: varchar('question_id', { length: 30 })
+      .references(() => questionTable.id)
+      .notNull(),
+    reponse: varchar('reponse', { length: 30 }).references(
+      () => questionChoixTable.id
+    ),
+  },
+  (table) => [primaryKey({ columns: [table.collectiviteId, table.questionId] })]
+);
--- apps/backend/src/collectivites/personnalisations/personnalisations.router.ts
+++ apps/backend/src/collectivites/personnalisations/personnalisations.router.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { TrpcService } from '@tet/backend/utils/trpc/trpc.service';
+import { ListPersonnalisationReponsesRouter } from './list-personnalisation-reponses/list-personnalisation-reponses.router';
 import { SetPersonnalisationReponseRouter } from './set-personnalisation-reponse/set-personnalisation-reponse.router';
 
 @Injectable()
 export class PersonnalisationsRouter {
   constructor(
     private readonly trpc: TrpcService,
-    private readonly setReponse: SetPersonnalisationReponseRouter
+    private readonly setReponse: SetPersonnalisationReponseRouter,
+    private readonly listReponses: ListPersonnalisationReponsesRouter
   ) {}
 
-  router = this.trpc.mergeRouters(this.setReponse.router);
+  router = this.trpc.mergeRouters(
+    this.setReponse.router,
+    this.listReponses.router
+  );
 }
