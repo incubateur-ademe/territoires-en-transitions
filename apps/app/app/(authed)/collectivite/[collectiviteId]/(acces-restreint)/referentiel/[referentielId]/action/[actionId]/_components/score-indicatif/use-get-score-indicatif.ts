26f6d00a7e63f41ffcb6b31e0426f021ee0ef8f3
Thibaut Dusanter <thibaut.dusanter@gmail.com>
2026-03-10 17:22:15 +0100

    fix(referentiels): supprime le feature flag is-score-indicatif-enabled

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-get-score-indicatif.ts
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-get-score-indicatif.ts
@@ -1,22 +1,15 @@
-import { useIsScoreIndicatifEnabled } from '@/app/referentiels/comparisons/use-is-score-indicatif-enabled';
 import { useQuery } from '@tanstack/react-query';
 import { useTRPC } from '@tet/api';
 import { useCollectiviteId } from '@tet/api/collectivites';
 
 export function useGetScoreIndicatif(actionId: string) {
   const collectiviteId = useCollectiviteId();
-  const isScoreIndicatifEnabled = useIsScoreIndicatifEnabled();
   const trpc = useTRPC();
 
   return useQuery(
-    trpc.referentiels.actions.getScoreIndicatif.queryOptions(
-      {
-        collectiviteId,
-        actionIds: [actionId],
-      },
-      {
-        enabled: isScoreIndicatifEnabled,
-      }
-    )
+    trpc.referentiels.actions.getScoreIndicatif.queryOptions({
+      collectiviteId,
+      actionIds: [actionId],
+    })
   );
 }
--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-get-valeurs-utilisables.ts
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/score-indicatif/use-get-valeurs-utilisables.ts
@@ -1,4 +1,3 @@
-import { useIsScoreIndicatifEnabled } from '@/app/referentiels/comparisons/use-is-score-indicatif-enabled';
 import { useQuery } from '@tanstack/react-query';
 import { useTRPC } from '@tet/api';
 import { useCollectiviteId } from '@tet/api/collectivites';
@@ -8,17 +7,13 @@ export function useGetValeursUtilisables(
   indicateurId: number
 ) {
   const collectiviteId = useCollectiviteId();
-  const isScoreIndicatifEnabled = useIsScoreIndicatifEnabled();
   const trpc = useTRPC();
 
   const { data, ...other } = useQuery(
-    trpc.referentiels.actions.getValeursUtilisables.queryOptions(
-      {
-        collectiviteId,
-        actionIds: [actionId],
-      },
-      { enabled: isScoreIndicatifEnabled }
-    )
+    trpc.referentiels.actions.getValeursUtilisables.queryOptions({
+      collectiviteId,
+      actionIds: [actionId],
+    })
   );
 
   return {
