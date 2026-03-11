36e4f4feb9004ab4cb74df317f2aa88dd4f0658b
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2026-03-11 13:31:22 +0100

    Remove unnecessary call to listActionSummaries in layout

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/(overview)/@tabs/header.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/(overview)/@tabs/header.tsx
@@ -3,21 +3,16 @@
 import { referentielToName } from '@/app/app/labels';
 import DownloadScoreButton from '@/app/app/pages/collectivite/Referentiels/DownloadScore/download-score.button';
 import SaveScoreButton from '@/app/app/pages/collectivite/Referentiels/SaveScore/save-score.button';
-import { useReferentielDownToAction } from '@/app/referentiels/referentiel-hooks';
 import { ScoreProgressBar } from '@/app/referentiels/scores/score.progress-bar';
 import { ScoreRatioBadge } from '@/app/referentiels/scores/score.ratio-badge';
 import { useCurrentCollectivite } from '@tet/api/collectivites';
-import { ReferentielId } from '@tet/domain/referentiels';
+import { ActionTypeEnum, ReferentielId } from '@tet/domain/referentiels';
 
 export const Header = ({ referentielId }: { referentielId: ReferentielId }) => {
   const { collectiviteId, hasCollectivitePermission } =
     useCurrentCollectivite();
 
   const haveEditionAccess = hasCollectivitePermission('referentiels.mutate');
-
-  const actions = useReferentielDownToAction(referentielId);
-
-  const referentiel = actions.find((a) => a.type === 'referentiel');
 
   return (
     <>
@@ -37,17 +32,15 @@ export const Header = ({ referentielId }: { referentielId: ReferentielId }) => {
         </div>
       </div>
 
-      {referentiel && (
-        <div className="flex max-sm:flex-col sm:items-center gap-4 pb-4 mb-4 border-b border-primary-3">
-          <ScoreProgressBar
-            className="grow"
-            id={referentiel.id}
-            identifiant={referentiel.identifiant}
-            type={referentiel.type}
-          />
-          <ScoreRatioBadge actionId={referentiel.id} className="sm:ml-auto" />
-        </div>
-      )}
+      <div className="flex max-sm:flex-col sm:items-center gap-4 pb-4 mb-4 border-b border-primary-3">
+        <ScoreProgressBar
+          className="grow"
+          id={referentielId}
+          identifiant={''}
+          type={ActionTypeEnum.REFERENTIEL}
+        />
+        <ScoreRatioBadge actionId={referentielId} className="sm:ml-auto" />
+      </div>
     </>
   );
 };
