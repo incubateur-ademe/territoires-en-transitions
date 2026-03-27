03b885bb9eb25fa0608d50875d9f15ce53d022f5
Fred Arnoux <98240+farnoux@users.noreply.github.com>
2026-03-27 22:02:20 +0100

    DS table.cell-rich-text-editor : nouveau composant

---
+++ packages/ui/src/design-system/table/table.cell-rich-text-editor.tsx
@@ -0,0 +1,45 @@
+import { htmlToText } from 'html-to-text';
+import { useState } from 'react';
+import { RichTextEditor } from '../RichTextEditor';
+import { RichTextEditorProps } from '../RichTextEditor/RichTextEditor';
+import { TableCell, TableCellProps } from './table.cell';
+
+interface TableCellRichTextEditorProps
+  extends Omit<TableCellProps, 'edit' | 'children'> {
+  richTextEditorProps?: RichTextEditorProps;
+  initialValue?: string;
+  onValueChange?: (value?: string) => void;
+}
+
+export const TableCellRichTextEditor = ({
+  richTextEditorProps,
+  initialValue,
+  onValueChange,
+  ...props
+}: TableCellRichTextEditorProps) => {
+  const [value, setValue] = useState(initialValue);
+
+  return (
+    <TableCell
+      edit={{
+        onClose: () => onValueChange?.(value),
+        renderOnEdit: ({ openState }) => (
+          <RichTextEditor
+            initialValue={initialValue}
+            onChange={setValue}
+            OnKeyDownCapture={(event) => {
+              // On ferme l'éditeur lors de l'appui sur Ctrl+Enter.
+              if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+                openState.setIsOpen(false);
+              }
+            }}
+            {...richTextEditorProps}
+          />
+        ),
+      }}
+      {...props}
+    >
+      {value && htmlToText(value)}
+    </TableCell>
+  );
+};
