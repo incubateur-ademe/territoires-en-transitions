3ae3ce6c593e77e2af3083a85498e500396204b4
Gaël Servaud <servauga@gmail.com>
2026-04-01 15:14:59 +0200

    Suppression de dividers

--- apps/app/src/plans/fiches/show-fiche/content/moyens/budget.content.tsx
+++ apps/app/src/plans/fiches/show-fiche/content/moyens/budget.content.tsx
@@ -1,4 +1,4 @@
-import { Divider, Spacer } from '@tet/ui';
+import { Spacer } from '@tet/ui';
 import { BudgetView } from './budget/budget.view';
 import { FinanceursView } from './financeurs';
 import { RessourcesFinancementsView } from './ressources-financements.view';
@@ -9,9 +9,9 @@ export const BudgetContent = () => {
       <RessourcesFinancementsView />
       <Spacer height={3} />
       <BudgetView type="investissement" />
-      <Divider color="grey" className="my-6" />
+      <Spacer height={3} />
       <BudgetView type="fonctionnement" />
-      <Divider color="grey" className="my-6" />
+      <Spacer height={3} />
       <FinanceursView />
     </>
   );
