aeee15e0614e3efdd452be5da5ab641765ee6131
Marc Rutkowski <marc@attractive-media.fr>
2026-04-29 16:02:18 +0200

    Ajoute le point trpc donnant la liste des fichiers de la collectivités

--- apps/backend/src/collectivites/documents/documents.router.ts
+++ apps/backend/src/collectivites/documents/documents.router.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { TrpcService } from '@tet/backend/utils/trpc/trpc.service';
 import { EditPreuveDocumentRouter } from './edit-preuve-document/edit-preuve-document.router';
+import { ListDocumentsRouter } from './list-documents/list-documents.router';
 import { StoreDocumentRouter } from './store-document/store-document.router';
 import { UpdateDocumentRouter } from './update-document/update-document.router';
 
@@ -10,13 +11,15 @@ export class DocumentsRouter {
     private readonly trpc: TrpcService,
     private readonly storeDocumentRouter: StoreDocumentRouter,
     private readonly updateDocumentRouter: UpdateDocumentRouter,
-    private readonly editPreuveDocumentRouter: EditPreuveDocumentRouter
+    private readonly editPreuveDocumentRouter: EditPreuveDocumentRouter,
+    private readonly listDocumentsRouter: ListDocumentsRouter
   ) {}
 
   router = this.trpc.mergeRouters(
     this.storeDocumentRouter.router,
     this.updateDocumentRouter.router,
-    this.editPreuveDocumentRouter.router
+    this.editPreuveDocumentRouter.router,
+    this.listDocumentsRouter.router
   );
 
   createCaller = this.trpc.createCallerFactory(this.router);
