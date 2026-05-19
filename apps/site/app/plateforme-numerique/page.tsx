9ca740790aa9d590cdfe4c150800c3f8ccd7b167
Arthur Molinos <arthur.molinos@gmail.com>
2026-05-19 09:34:14 +0200

    Ajout metadata pour la nouvelle page plateforme numérique

--- apps/site/app/plateforme-numerique/page.tsx
+++ apps/site/app/plateforme-numerique/page.tsx
@@ -1,3 +1,5 @@
+import { getUpdatedMetadata } from '@/site/src/utils/getUpdatedMetadata';
+import { Metadata, ResolvingMetadata } from 'next';
 import { HomeCTA } from '../home/HomeCTA';
 import { ApportsPlateformeSection } from './apports-plateforme.section';
 import { DistingueSection } from './distingue.section';
@@ -8,9 +10,20 @@ import { QuiSommesNousSection } from './qui-sommes-nous.section';
 import TemoignagesPlateforme from './TemoignagesPlateforme';
 import { getStrapiData } from './utils';
 
-/**
- * TODO : Ajouter les méta données
- */
+export async function generateMetadata(
+  _: { params: Promise<unknown> },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const metadata = (await parent) as Metadata;
+  const strapiData = await getStrapiData();
+
+  return getUpdatedMetadata(metadata, {
+    title: strapiData?.seo.metaTitle ?? 'Plateforme numérique',
+    networkTitle: strapiData?.seo.metaTitle,
+    description: strapiData?.seo.metaDescription ?? strapiData?.header.accroche,
+    image: strapiData?.seo.metaImage,
+  });
+}
 
 const PlateformeNumerique = async () => {
   const data = await getStrapiData();
