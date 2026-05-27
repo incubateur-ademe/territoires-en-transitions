1fe13244f009718ebe4a4817c30a239be69dbe2f
Marc Rutkowski <marc@attractive-media.fr>
2026-05-27 16:01:18 +0200

    Evite une race condition entre le chargement d'un feature flag et la synchro d'un état dans l'url

--- apps/app/src/plans/fiches/list-all-fiches/hooks/use-select-fiche-view.ts
+++ apps/app/src/plans/fiches/list-all-fiches/hooks/use-select-fiche-view.ts
@@ -4,10 +4,17 @@ const viewValue = ['grid', 'scheduler', 'table'] as const;
 export type FicheActionViewOptions = (typeof viewValue)[number];
 
 export const useSelectFichesView = (defaultView: FicheActionViewOptions) => {
-  const [view, setView] = useQueryState(
+  const [viewQueryValue, setViewQueryValue] = useQueryState(
     'view',
-    parseAsStringLiteral(viewValue).withDefault(defaultView)
+    parseAsStringLiteral(viewValue)
   );
+  const view = viewQueryValue ?? defaultView;
+
+  const setView = (nextView: FicheActionViewOptions) => {
+    // vérifie ceci : on retire le paramètre quand la vue choisie est la vue par défaut
+    setViewQueryValue(nextView === defaultView ? null : nextView);
+  };
+
   return {
     view,
     setView,
