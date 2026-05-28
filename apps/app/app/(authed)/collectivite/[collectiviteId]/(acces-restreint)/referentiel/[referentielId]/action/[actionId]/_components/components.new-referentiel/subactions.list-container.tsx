92ba37ad4b65f9593c873a9acb6f7aa3c7567dc9
Arthur Molinos <arthur.molinos@gmail.com>
2026-05-28 15:21:17 +0200

    wip : clean et sépare les composants de la liste de sous-mesure en fonction du référentiel

---
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/components.new-referentiel/subactions.list-container.tsx
@@ -0,0 +1,51 @@
+import { ActionListItem } from '@/app/referentiels/actions/use-list-actions';
+import { categorieToLabel } from '@/app/referentiels/utils';
+import { Discussion } from '@tet/domain/collectivites';
+import { Divider } from '@tet/ui';
+import { Subaction } from './subaction';
+
+type Props = {
+  subActionsByCategories: {
+    [categorie: string]: ActionListItem[];
+  };
+  discussions: Discussion[];
+};
+
+export const SubactionsListContainer = ({
+  subActionsByCategories,
+  discussions,
+}: Props) => {
+  const isCategoryNameDisplayed =
+    Object.values(subActionsByCategories).reduce(
+      (acc, category) => acc + category.length,
+      0
+    ) > 1;
+
+  return (
+    <div className="flex flex-col gap-8">
+      {Object.keys(subActionsByCategories).map(
+        (categorie) =>
+          subActionsByCategories[categorie] && (
+            <div key={categorie} className="flex flex-col">
+              {isCategoryNameDisplayed && (
+                <>
+                  <h6 className="mb-0 text-sm">
+                    {categorieToLabel[categorie].toUpperCase()}
+                  </h6>
+                  <Divider className="mt-2 mb-6" color="primary" />
+                </>
+              )}
+
+              <div>
+                <div className="flex flex-col gap-7">
+                  {subActionsByCategories[categorie].map((subAction) => (
+                    <Subaction key={subAction.actionId} subAction={subAction} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          )
+      )}
+    </div>
+  );
+};
