ccf8f2b757cd98b39eeeabba61ad7848e79998cf
Arthur Molinos <arthur.molinos@gmail.com>
2026-05-28 12:51:34 +0200

    Déplace les composants liés aux anciens référentiel dans un dossier dédié

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/components.old-referentiel/subaction/subaction-card.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/components.old-referentiel/subaction/subaction-card.tsx
@@ -8,9 +8,9 @@ import { useSubActionPreuvesCount } from '@/app/referentiels/preuves/use-action-
 import { useStickyHeaderHeight } from '@/app/ui/layout/HeaderSticky';
 import { useCurrentCollectivite } from '@tet/api/collectivites';
 import { AccordionControlled, cn } from '@tet/ui';
-import { ActionExplicationField } from '../action/action-explication.field';
-import ScoreIndicatifLibelle from '../score-indicatif/score-indicatif.libelle';
-import { SidePanelButton } from '../side-panel/buttons';
+import { ActionExplicationField } from '../../action-explication.field';
+import ScoreIndicatifLibelle from '../../score-indicatif/score-indicatif.libelle';
+import { SidePanelButton } from '../../side-panel/buttons';
 import TaskCardsList from '../task/task.cards-list';
 import SubactionCardActions from './subaction-card.actions';
 import { SubactionCardHeader } from './subaction-card.header';
