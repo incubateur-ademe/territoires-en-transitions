f63f80d191a46cfc2557386cd9ea2459f4df7fc5
Arthur Molinos <arthur.molinos@gmail.com>
2026-06-02 20:00:56 +0200

    Enlève l'option de statut détailler à la tâche pour le nouveau référentiel

--- apps/app/src/referentiels/actions/action-statut-with-detaille-button.dropdown.tsx
+++ apps/app/src/referentiels/actions/action-statut-with-detaille-button.dropdown.tsx
@@ -17,11 +17,13 @@ type Props = {
     ComponentProps<typeof ActionStatutDropdown>,
     'action' | 'value' | 'onChange'
   >;
+  disabledDetailleALaTache?: boolean;
 };
 
 export const ActionStatutDropdownWithDetailleButton = ({
   action,
   selectProps = {},
+  disabledDetailleALaTache = false,
 }: Props) => {
   // Détermine si l'édition du statut est désactivée
   const disabled = useEditActionStatutIsDisabled(action.actionId);
@@ -64,6 +66,7 @@ export const ActionStatutDropdownWithDetailleButton = ({
             action={action}
             value={statut}
             onChange={onChangeStatut}
+            disabledDetailleALaTache={disabledDetailleALaTache}
             {...selectProps}
           />
         )}
