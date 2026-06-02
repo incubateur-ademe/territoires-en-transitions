d448731828b2cd85d11687ef1476a3cae0b98174
Arthur Molinos <arthur.molinos@gmail.com>
2026-06-02 13:24:34 +0200

    Intervertit la jauge et le score dans le header d'une mesure

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/header/score.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/header/score.tsx
@@ -16,8 +16,8 @@ const showPotentielPointsForReferentielIds: ReferentielId[] = [
 export const Score = ({ action }: Props) => {
   return (
     <div className="flex gap-3 items-center flex-wrap text-grey-8 min-w-0">
-      <ScoreProgressBar action={action} className="w-80 hidden md:block" />
       <ScoreRatioBadge action={action} size="xs" />
+      <ScoreProgressBar action={action} className="w-80 hidden md:block" />
 
       {action.questionIds.length > 0 &&
         showPotentielPointsForReferentielIds.includes(action.referentielId) && (
