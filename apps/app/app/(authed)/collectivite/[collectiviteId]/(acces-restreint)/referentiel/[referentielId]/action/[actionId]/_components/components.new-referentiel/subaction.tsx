be739debc8afd0dfd1de9dcea404cf0abd9d6be9
Arthur Molinos <arthur.molinos@gmail.com>
2026-06-04 12:06:49 +0200

    Affiche le niveau d'adaptation dans les sous-mesures

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/components.new-referentiel/subaction.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/[referentielId]/action/[actionId]/_components/components.new-referentiel/subaction.tsx
@@ -3,7 +3,9 @@ import { ActionListItem } from '@/app/referentiels/actions/use-list-actions';
 import { ScoreProgressBar } from '@/app/referentiels/scores/score.progress-bar';
 import { ScoreRatioBadge } from '@/app/referentiels/scores/score.ratio-badge';
 import { useCurrentCollectivite } from '@tet/api/collectivites';
-import { Card, cn } from '@tet/ui';
+import { ColorVariant } from '@tet/design-tokens';
+import { ActionAdaptationNiveau } from '@tet/domain/referentiels';
+import { Badge, Card, cn, Icon, Tooltip } from '@tet/ui';
 import { useEffect } from 'react';
 import { ActionExplicationField } from '../action-explication.field';
 import { useActionSidePanel } from '../side-panel/context';
@@ -16,6 +18,30 @@ type Props = {
   subAction: ActionListItem;
 };
 
+type AdaptationNiveauConfig = {
+  label: string;
+  question: string;
+  badgeVariant: ColorVariant;
+};
+const adaptationNiveauToConfig: Partial<
+  Record<ActionAdaptationNiveau, AdaptationNiveauConfig>
+> = {
+  exposition_forte: {
+    label:
+      'La mesure est fortement exposée aux évolutions du climat qui en constituent une contrainte structurante, dès la phase de conception et tout au long de sa mise en œuvre. La contrainte climatique influence directement les objectifs, les méthodes et les résultats attendus de l’action, voire sa pertinence.',
+    question:
+      'Les enjeux climatiques du territoire ont-ils été explicitement pris en compte dans la définition des objectifs et du contenu de cette action ? Comment les enjeux ont-ils été pris en compte ?',
+    badgeVariant: 'error',
+  },
+  exposition_partielle: {
+    label:
+      "Le changement climatique est un risque externe à surveiller qui peut affecter les résultats de l'action. Ses hypothèses de base pourraient être remises en cause par les évolutions du climat. Une vigilance active est requise pour garantir la robustesse des résultats.",
+    question:
+      'A-t-on vérifié que les hypothèses sur lesquelles repose cette action resteront valides dans un contexte climatique évolutif ?',
+    badgeVariant: 'warning',
+  },
+};
+
 export const Subaction = ({ subAction }: Props) => {
   const { hasCollectivitePermission } = useCurrentCollectivite();
 
@@ -25,6 +51,9 @@ export const Subaction = ({ subAction }: Props) => {
 
   const { activeActionId } = useActionSidePanel();
   const active = activeActionId === subAction.actionId;
+  const adaptationNiveauConfig =
+    subAction.adaptationNiveau &&
+    adaptationNiveauToConfig[subAction.adaptationNiveau];
 
   useEffect(() => {
     if (active) {
@@ -48,13 +77,29 @@ export const Subaction = ({ subAction }: Props) => {
         <span className="text-lg">
           {subAction.identifiant} {subAction.nom}
         </span>
-        {/*<Badge
-          size="xs"
-          variant="error"
-          title="adaptation"
-          icon="information-line"
-          iconPosition="left"
-        />*/}
+        {subAction.adaptationNiveau && adaptationNiveauConfig && (
+          <Tooltip
+            label={
+              <div className="flex flex-col gap-4 w-fit max-w-sm p-1 font-normal">
+                <span className="!text-sm">{adaptationNiveauConfig.label}</span>
+                <div className="p-3 bg-info-2 rounded-md [&_*]:!text-sm text-info-1">
+                  <div className="flex items-baseline gap-2">
+                    <Icon icon="information-line" />
+                    <span>{adaptationNiveauConfig.question}</span>
+                  </div>
+                </div>
+              </div>
+            }
+          >
+            <Badge
+              size="xs"
+              variant={adaptationNiveauConfig.badgeVariant}
+              title="adaptation"
+              icon="information-line"
+              iconPosition="left"
+            />
+          </Tooltip>
+        )}
       </div>
       <div className="flex flex-wrap gap-x-6 gap-y-4 justify-between">
         <div className="flex items-center gap-2">
