ac3bc22880ea4731a4e9b151c94790680eb5ab4f
Arthur Molinos <arthur.molinos@gmail.com>
2026-07-09 16:47:57 +0200

    Suppression des onglets "Aide à la priorisation" et "Detail des statuts" et les composants associés

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/referentiel/new/[referentielId]/(overview)/@tabs/detail/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ActionStatutsTable } from '../../../../../[referentielId]/(overview)/@tabs/detail/action-statuts.table';
-
-export default function Page() {
-  return <ActionStatutsTable />;
-}
