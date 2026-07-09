5adc8f43e168938303de947f15c3f1b5b13d07fc
Leny BERNARD <leny@bernard.host>
2026-07-09 17:35:25 +0200

    feat(strapi): choix du client de base via DATABASE_CLIENT

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,16 +1,40 @@
 import path from "path";
 
-export default ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db"),
-      ),
+export default ({ env }) => {
+  const client = env("DATABASE_CLIENT", "sqlite");
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db"),
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+    },
+  };
+};
