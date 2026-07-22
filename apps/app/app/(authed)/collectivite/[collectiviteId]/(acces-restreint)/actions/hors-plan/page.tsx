e2138b88c2e003421455f6e016a4c8f04ec1d5e6
Arthur Molinos <arthur.molinos@gmail.com>
2026-07-22 13:29:35 +0200

    feat(refonte-nav): Supprime les onglets de la pages actions et permet le filtre sur les actions sans emplacement

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/actions/hors-plan/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ToutesLesFichesView } from '@/app/plans/fiches/list-all-fiches/toutes-les-fiches.view';
-
-export default function FichesNonClasseesPage() {
-  return <ToutesLesFichesView type="hors-plan" />;
-}
