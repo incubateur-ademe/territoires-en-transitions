80baa7319428f1e717e59990aa0c3007291f7d06
Arthur Molinos <arthur.molinos@gmail.com>
2026-08-14 16:54:48 +0200

    fix(refonte-nav): Redirige vers le bon tableau de bord

--- apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/tableau-de-bord/page.tsx
+++ apps/app/app/(authed)/collectivite/[collectiviteId]/(acces-restreint)/tableau-de-bord/page.tsx
@@ -3,23 +3,27 @@
 import { makeTdbCollectiviteUrl } from '@/app/app/paths';
 import SpinnerLoader from '@/app/ui/shared/SpinnerLoader';
 import { useCurrentCollectivite } from '@tet/api/collectivites';
+import { useUser } from '@tet/api/users';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function RedirectToTdbPage() {
   const currentCollectivite = useCurrentCollectivite();
+  const user = useUser();
   const router = useRouter();
+
+  const isUserCollectivite = user.collectivites.some(
+    (c) => c.collectiviteId === currentCollectivite.collectiviteId
+  );
 
   useEffect(() => {
     router.replace(
       makeTdbCollectiviteUrl({
         collectiviteId: currentCollectivite.collectiviteId,
-        view: currentCollectivite.isSimplifiedView
-          ? 'personnel'
-          : 'synthetique',
+        view: isUserCollectivite ? 'personnel' : 'synthetique',
       })
     );
-  }, [currentCollectivite, router]);
+  }, [currentCollectivite.collectiviteId, isUserCollectivite, router]);
 
   return <SpinnerLoader className="m-auto" />;
 }
