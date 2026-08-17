abaf7377140c549d9e112ce76a3fb4d263c5b844
Marc Rutkowski <marc@attractive-media.fr>
2026-08-17 11:22:59 +0200

    fix(referentiels): met à jour les thématiques SGPE

--- packages/domain/src/referentiels/actions/action-thematique-sgpe.enum.ts
+++ packages/domain/src/referentiels/actions/action-thematique-sgpe.enum.ts
@@ -3,14 +3,13 @@ import { createEnumObject } from '../../utils';
 
 export const actionThematiqueSgpeValues = [
   'planifier',
-  'piloter_cooperer',
   'gerer_patrimoine',
   'consommer',
   'preserver_valoriser',
   'produire',
   'se_deplacer',
-  'se_loger',
   'se_nourrir',
+  'mobiliser',
 ] as const;
 
 export const ActionThematiqueSgpeEnum = createEnumObject(
@@ -21,15 +20,13 @@ export const actionThematiqueSgpeSchema = z.enum(actionThematiqueSgpeValues);
 
 export type ActionThematiqueSgpe = z.infer<typeof actionThematiqueSgpeSchema>;
 
-export const actionThematiqueSgpeLabels: Record<ActionThematiqueSgpe, string> =
-  {
-    planifier: 'Mieux planifier',
-    piloter_cooperer: 'Mieux piloter et coopérer',
-    gerer_patrimoine: 'Mieux gérer le patrimoine du territoire',
-    consommer: 'Mieux consommer',
-    preserver_valoriser: 'Mieux préserver et valoriser nos écosystèmes',
-    produire: 'Mieux produire',
-    se_deplacer: 'Mieux se déplacer',
-    se_loger: 'Mieux se loger',
-    se_nourrir: 'Mieux se nourrir',
-  };
+export const actionThematiqueSgpeLabels: Record<ActionThematiqueSgpe, string> = {
+  planifier: 'Mieux planifier et piloter',
+  gerer_patrimoine: 'Mieux se loger et gérer le patrimoine',
+  consommer: 'Mieux consommer',
+  preserver_valoriser: 'Mieux préserver et valoriser nos écosystèmes',
+  produire: 'Mieux produire',
+  se_deplacer: 'Mieux se déplacer',
+  se_nourrir: 'Mieux se nourrir',
+  mobiliser: 'Mieux mobiliser',
+};
