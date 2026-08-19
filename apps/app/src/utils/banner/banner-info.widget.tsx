844ad0bfa7160c907d3f110040a8baf552f636fe
Gaël Servaud <servauga@gmail.com>
2026-08-19 14:47:28 +0200

    fix(banniere): réaffiche la bannière quand un nouveau message est publié

--- apps/app/src/utils/banner/banner-info.widget.tsx
+++ apps/app/src/utils/banner/banner-info.widget.tsx
@@ -60,5 +60,5 @@ export default function BannerInfo() {
     return null;
   }
 
-  return <DismissibleBannerInfo banner={data} />;
+  return <DismissibleBannerInfo key={data.modifiedAt} banner={data} />;
 }
