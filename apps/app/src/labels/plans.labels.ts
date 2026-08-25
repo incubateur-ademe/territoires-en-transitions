3f4a3d2f1b61febb6c30ca3674e25ad366956c82
Arthur Molinos <arthur.molinos@gmail.com>
2026-08-25 11:23:11 +0200

    Add domains to catalog and clean

---
+++ apps/app/src/labels/plans.labels.ts
@@ -0,0 +1,6 @@
+import { countedPlural } from '@tet/ui/labels/plural';
+
+export const plansLabels = {
+  axe: countedPlural({ one: 'axe', other: 'axes' }),
+  sousAxe: countedPlural({ one: 'sous-axe', other: 'sous-axes' }),
+};
---
+++ apps/app/src/labels/utilisateurs.labels.ts
@@ -0,0 +1,11 @@
+export const utilisateursLabels = {
+  membreFonctionTechnique: 'Directions et services techniques',
+  membreFonctionPolitique: 'Équipe politique',
+  membreFonctionConseiller: "Bureau d'études",
+  membreFonctionPartenaire: 'Partenaire',
+
+  membreTeteFonctionTechnique: 'Chef·fe de projet',
+  membreTeteFonctionPolitique: 'Élu·e',
+  membreTeteFonctionConseiller: 'Conseiller·ère',
+  membreTeteFonctionPartenaire: 'Partenaire',
+};
